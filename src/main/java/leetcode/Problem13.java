47b4bddad7d53f5cc2118f29049938c883e8ae28
Fredy Wijaya <fredy.wijaya@gmail.com>
2014-12-24 18:10:29 -0600

    Solve problem 13

---
+++ src/main/java/leetcode/Problem13.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * https://oj.leetcode.com/problems/roman-to-integer/
+ */
+public class Problem13 {
+    public int romanToInt(String s) {
+        Map<Character, Integer> romans = new HashMap<>();
+        romans.put('I', 1);
+        romans.put('V', 5);
+        romans.put('X', 10);
+        romans.put('L', 50);
+        romans.put('C', 100);
+        romans.put('D', 500);
+        romans.put('M', 1000);
+        
+        String upperCaseStr = s.toUpperCase();
+        char[] chars = upperCaseStr.toCharArray();
+        int result = 0;
+        for (int i = 0; i < chars.length; i++) {
+            if (i+1 == chars.length) {
+                result += romans.get(chars[i]);
+                break;
+            }
+            if (romans.get(chars[i]) >= romans.get(chars[i+1])) {
+                result += romans.get(chars[i]);
+            } else {
+                result -= romans.get(chars[i]);
+            }
+        }
+        return result;
+    }
+}
