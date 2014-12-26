54ac913f72730bb2a74df651916e9431a2927011
Fredy Wijaya <fredy.wijaya@gmail.com>
2014-12-26 17:21:21 -0600

    Solve problem 20

---
+++ src/main/java/leetcode/Problem20.java
@@ -0,0 +1,46 @@
+package leetcode;
+
+import java.util.Stack;
+
+/**
+ * https://oj.leetcode.com/problems/valid-parentheses/
+ */
+public class Problem20 {
+    public boolean isValid(String s) {
+        char[] chars = s.toCharArray();
+        Stack<Character> stack = new Stack<>();
+        for (char c : chars) {
+            if (c == '}') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char openChar = stack.pop();
+                if (openChar != '{') {
+                    return false;
+                }
+            } else if (c == ']') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char openChar = stack.pop();
+                if (openChar != '[') {
+                    return false;
+                }
+            } else if (c == ')') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char openChar = stack.pop();
+                if (openChar != '(') {
+                    return false;
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+        if (!stack.isEmpty()) {
+            return false;
+        }
+        return true;
+    }
+}
