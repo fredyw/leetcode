dfa426c2c1176e44bb4998024e9680e3990f8620
Fredy Wijaya <fredy.wijaya@gmail.com>
2014-12-27 17:59:45 -0600

    Solve problem 83

---
+++ src/main/java/leetcode/Problem83.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+/**
+ * https://oj.leetcode.com/problems/remove-duplicates-from-sorted-list/
+ */
+public class Problem83 {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode tmp = head;
+        for (ListNode n = head; n != null; n = n.next) {
+            if (n.val == tmp.val) {
+                continue;
+            }
+            tmp.next = n;
+            tmp = tmp.next;
+        }
+        tmp.next = null;
+        return head;
+    }
+}
