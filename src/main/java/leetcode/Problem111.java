e8a1ffb7cb35b1a466c14aabd79ed0d7e6cd911b
Fredy Wijaya <fredy.wijaya@gmail.com>
2014-12-26 19:01:17 -0600

    Solve problem 111

---
+++ src/main/java/leetcode/Problem111.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+/**
+ * https://oj.leetcode.com/problems/maximum-depth-of-binary-tree/
+ */
+public class Problem111 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = maxDepth(root.left) + 1;
+        int right = maxDepth(root.right) + 1;
+        return Math.max(left, right);
+    }
+}
