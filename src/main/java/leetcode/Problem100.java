8f942c15994dccd50931c9e0f1fb7590fff072b5
Fredy Wijaya <fredy.wijaya@gmail.com>
2014-12-28 02:17:03 -0600

    Solve problem 100

---
+++ src/main/java/leetcode/Problem100.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+/**
+ * https://oj.leetcode.com/problems/same-tree/
+ */
+public class Problem100 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p != null && q != null && p.val == q.val) {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+        return false;
+    }
+}
