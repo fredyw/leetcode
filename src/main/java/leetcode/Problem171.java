f0d46d77123b915524ea5da7e9311452ed0bf6aa
Fredy Wijaya <fredy.wijaya@gmail.com>
2014-12-28 13:57:57 -0600

    Solve problem 171

---
+++ src/main/java/leetcode/Problem171.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+/**
+ * https://oj.leetcode.com/problems/excel-sheet-column-number/
+ */
+public class Problem171 {
+    public int titleToNumber(String s) {
+        int n =  0;
+        int exp = 0;
+        for (int i = s.length()-1; i >= 0; i--) {
+            n += getNumber(s.charAt(i)) * (int) Math.pow(26, exp);
+            exp++;
+        }
+        return n;
+    }
+    
+    private int getNumber(char c) {
+        return c - 64;
+    }
+}
