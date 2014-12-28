f071c99bc57a6a4081c3403c8296c4f4b8796f49
Fredy Wijaya <fredy.wijaya@gmail.com>
2014-12-28 11:58:24 -0600

    Solve problem 28

---
+++ src/main/java/leetcode/Problem28.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+/**
+ * https://oj.leetcode.com/problems/implement-strstr/
+ */
+public class Problem28 {
+    public int strStr(String haystack, String needle) {
+        if (haystack.equals(needle)) {
+            return 0;
+        }
+        int idx = -1;
+        for (int i = 0; i < haystack.length(); i++) {
+            boolean found = true;
+            for (int j = 0; j < needle.length(); j++) {
+                if (i+j >= haystack.length()) {
+                    return -1;
+                }
+                if (haystack.charAt(i+j) != needle.charAt(j)) {
+                    found = false;
+                    break;
+                }
+            }
+            if (found) {
+                idx = i;
+                break;
+            }
+        }
+        return idx;
+    }
+}
