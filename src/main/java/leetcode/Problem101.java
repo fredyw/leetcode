e1e839da3b93015ab0a8870343a3f2b1ecefd835
Fredy Wijaya <fredy.wijaya@gmail.com>
2014-12-28 18:10:57 -0600

    Solve problem 101

---
+++ src/main/java/leetcode/Problem101.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+/**
+ * https://oj.leetcode.com/problems/symmetric-tree/
+ */
+public class Problem101 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isSymmetric(root.left, root.right);
+    }
+    
+    private boolean isSymmetric(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p != null && q != null && p.val == q.val) {
+            return isSymmetric(p.left, q.right) && isSymmetric(p.right, q.left);
+        }
+        return false;
+    }
+}
