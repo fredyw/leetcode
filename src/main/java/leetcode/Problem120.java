5fc571416065a11ecbac87f0a92d642be416bf6d
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-01-06 21:13:30 -0600

    Solve problem 120 (finally!)

--- src/main/java/leetcode/Problem120.java
+++ src/main/java/leetcode/Problem120.java
@@ -1,51 +1,34 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * https://oj.leetcode.com/problems/triangle/
  */
 public class Problem120 {
     public int minimumTotal(List<List<Integer>> triangle) {
-        List<Integer> result = new ArrayList<>();
-        minimumTotal(triangle, 0, 0, 0, result);
-        return Collections.min(result);
+        Map<String, Integer> memo = new HashMap<>();
+        return minimumTotal(triangle, 0, 0, memo);
     }
     
-    private void minimumTotal(List<List<Integer>> triangle, int idx1, int idx2,
-        int sum, List<Integer> result) {
-        if (idx1 == triangle.size()) {
-            result.add(sum);
-            return;
+    private int minimumTotal(List<List<Integer>> triangle, int row, int col,
+        Map<String, Integer> memo) {
+        String key = row + "|" + col;
+        if (row == triangle.size()-1) {
+            return triangle.get(row).get(col);
         }
-        if (idx1 < triangle.size()) {
-            if (idx2 < triangle.get(idx1).size()) {
-                minimumTotal(triangle, idx1+1, idx2,
-                    triangle.get(idx1).get(idx2)+sum, result);
-            }
-            if (idx2+1 < triangle.get(idx1).size()) {
-                minimumTotal(triangle, idx1+1, idx2+1,
-                    triangle.get(idx1).get(idx2+1)+sum, result);
-            }
+        if (memo.containsKey(key)) {
+            return memo.get(key);
         }
-    }
-    
-    public static void main(String[] args) {
-        Problem120 prob = new Problem120();
-        List<List<Integer>> triangle = new ArrayList<>();
-        triangle.add(Arrays.asList(2));
-        triangle.add(Arrays.asList(3, 4));
-        triangle.add(Arrays.asList(6, 5, 7));
-        triangle.add(Arrays.asList(4, 1, 8, -3));
-        System.out.println(prob.minimumTotal(triangle));
-
-        triangle = new ArrayList<>();
-        triangle.add(Arrays.asList(-1));
-        triangle.add(Arrays.asList(2, 3));
-        triangle.add(Arrays.asList(1, -1, -3));
-        System.out.println(prob.minimumTotal(triangle));
+        int min = Integer.MAX_VALUE;
+        if (row < triangle.size()) {
+            int min1 = minimumTotal(triangle, row+1, col, memo);
+            int min2 = minimumTotal(triangle, row+1, col+1, memo);
+            min = Math.min(min1, min2) + triangle.get(row).get(col);
+            memo.put(key, min);
+        }
+        return min;
     }
 }
