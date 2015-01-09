b145bb86b73e84e828cf379035fc025dcef9f4af
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-01-09 00:22:34 -0600

    Solve problem 71

---
+++ src/main/java/leetcode/Problem71.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * https://oj.leetcode.com/problems/simplify-path/
+ */
+public class Problem71 {
+    public String simplifyPath(String path) {
+        Stack<String> stack = new Stack<>();
+        String[] strs = path.split("/");
+        for (String str : strs) {
+            if (str.equals(".") || str.isEmpty()) {
+                continue;
+            } else if (str.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else {
+                stack.push(str);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append("/");
+        List<String> paths = new ArrayList<>(stack);
+        for (int i = 0; i < paths.size(); i++) {
+            sb.append(paths.get(i));
+            if (i < paths.size()-1) {
+                sb.append("/");
+            }
+        }
+        return sb.toString();
+    }
+}
