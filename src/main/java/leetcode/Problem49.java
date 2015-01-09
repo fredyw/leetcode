073df3f64bee883aa8effd4922493456162d15f9
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-01-09 01:02:47 -0600

    Solve problem 49

---
+++ src/main/java/leetcode/Problem49.java
@@ -0,0 +1,52 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * https://oj.leetcode.com/problems/anagrams/
+ */
+public class Problem49 {
+    public List<String> anagrams(String[] strs) {
+        class Anagram {
+            String sorted;
+            String original;
+            
+            Anagram(String sorted, String original) {
+                this.sorted = sorted;
+                this.original = original;
+            }
+        }
+        
+        List<Anagram> sortedStrings = new ArrayList<>();
+        for (String s : strs) {
+            if (s.isEmpty()) {
+                sortedStrings.add(new Anagram(s, s));
+            } else {
+                char[] chars = s.toCharArray();
+                Arrays.sort(chars);
+                sortedStrings.add(new Anagram(new String(chars), s));
+            }
+        }
+        
+        List<String> result = new ArrayList<>();
+        Set<Integer> ignored = new HashSet<>();
+        for (int i = 0; i < sortedStrings.size(); i++) {
+            for (int j = 0; j < sortedStrings.size(); j++) {
+                if (i == j) {
+                    continue;
+                }
+                if (sortedStrings.get(i).sorted.equals(sortedStrings.get(j).sorted)) {
+                    if (!ignored.contains(i)) {
+                        result.add(sortedStrings.get(i).original);
+                    }
+                    ignored.add(i);
+                }
+            }
+        }
+        return result;
+    }
+}
