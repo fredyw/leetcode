349b57ad660e9fbf239c3f9afaf7d9ce185b0db0
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-01-09 18:50:35 -0600

    Put the skeleton for problem 93

---
+++ src/main/java/leetcode/Problem93.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://oj.leetcode.com/problems/restore-ip-addresses/
+ */
+public class Problem93 {
+    public List<String> restoreIpAddresses(String s) {
+        List<String> result = new ArrayList<>();
+        
+        return result;
+    }
+    
+    public static void main(String[] args) {
+        Problem93 prob = new Problem93();
+        System.out.println(prob.restoreIpAddresses("25525511135"));
+    }
+}
