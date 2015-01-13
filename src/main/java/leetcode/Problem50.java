995283b13ed9a903da2bdfbb5172a1b1195291fc
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-01-12 20:40:13 -0600

    Solve problem 50

---
+++ src/main/java/leetcode/Problem50.java
@@ -0,0 +1,39 @@
+package leetcode;
+
+/**
+ * https://oj.leetcode.com/problems/powx-n/
+ */
+public class Problem50 {
+    public double pow(double x, int n) {
+        if (n >= 0) {
+            return powPositive(x, n);
+        }
+        return powNegative(x, n);
+    }
+    
+    private double powPositive(double x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n == 1) {
+            return x;
+        }
+        if (n % 2 == 0) {
+            return powPositive(x * x, n / 2);
+        }
+        return x * powPositive(x * x, n / 2);
+    }
+    
+    private double powNegative(double x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n == 1) {
+            return x;
+        }
+        if (n % 2 == 0) {
+            return powNegative(x * x, n / 2);
+        }
+        return powNegative(x * x, n / 2) / x;
+    }
+}
