b4c1cd35f43e9a4e708ab0a440224d8ee46b52e2
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-01-12 20:00:49 -0600

    Solve problem 43

---
+++ src/main/java/leetcode/Problem43.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+import java.math.BigInteger;
+
+/**
+ * https://oj.leetcode.com/problems/multiply-strings/
+ */
+public class Problem43 {
+    public String multiply(String num1, String num2) {
+        BigInteger result = new BigInteger(num1).multiply(new BigInteger(num2));
+        return result.toString();
+    }
+}
