c8354cf85167e249328de42b737bf7372acd003a
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-01-13 20:56:16 -0600

    Solve problem 142

---
+++ src/main/java/leetcode/Problem142.java
@@ -0,0 +1,41 @@
+package leetcode;
+
+/**
+ * https://oj.leetcode.com/problems/linked-list-cycle/
+ */
+public class Problem142 {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+     
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        ListNode one = head;
+        ListNode two = head; // iterate two times the speed of one
+        while (true) {
+            if (one == null) {
+                break;
+            }
+            if (two == null) {
+                break;
+            }
+            one = one.next;
+            if (two.next == null) {
+                break;
+            }
+            two = two.next.next;
+            if (one == two) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
