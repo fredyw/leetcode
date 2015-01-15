28269e25c5865ea7ebf88feb5f46a6a65e50d64f
fredy <fredy.wijaya@amd.com>
2015-01-15 17:45:29 -0600

    Solve problem 109

--- src/main/java/leetcode/Problem109.java
+++ src/main/java/leetcode/Problem109.java
@@ -1,5 +1,8 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * https://oj.leetcode.com/problems/convert-sorted-list-to-binary-search-tree/
  */
@@ -24,12 +27,41 @@ public class Problem109 {
         }
     }
 
+    private TreeNode root;
+    
     public TreeNode sortedListToBST(ListNode head) {
-        // TODO: to be implemented
-        return null;
+        List<ListNode> listNodes = new ArrayList<>();
+        for (ListNode n = head; n != null; n = n.next) {
+            listNodes.add(n);
+        }
+        sortedListToBST(listNodes, 0, listNodes.size());
+        return root;
     }
     
-    public static void main(String[] args) {
-        Problem109 prob = new Problem109();
+    private void sortedListToBST(List<ListNode> listNodes, int lo, int hi) {
+        if (lo >= hi) {
+            return;
+        }
+        int mid = (int) Math.ceil((hi + lo) / 2);
+        int val = listNodes.get(mid).val;
+        if (root == null) {
+            root = new TreeNode(val);
+        } else {
+            add(root, val);
+        }
+        sortedListToBST(listNodes, lo, mid);
+        sortedListToBST(listNodes, mid+1, hi);
+    }
+    
+    private TreeNode add(TreeNode node, int val) {
+        if (node == null) {
+            return new TreeNode(val);
+        }
+        if (val <= node.val) {
+            node.left = add(node.left, val);
+        } else {
+            node.right = add(node.right, val);
+        }
+        return node;
     }
 }
