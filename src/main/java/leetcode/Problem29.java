54988fb8375767e7ae2a21291f44527a345b21e8
fredy <fredy.wijaya@amd.com>
2015-01-15 17:47:17 -0600

    Put skeleton for problem 29

---
+++ src/main/java/leetcode/Problem29.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://oj.leetcode.com/problemset/algorithms/
+ */
+public class Problem29 {
+    public int divide(int dividend, int divisor) {
+        // TODO: to be implemented
+        return 0;
+    }
+    
+    public static void main(String[] args) {
+        Problem29 prob = new Problem29();
+    }
+}
