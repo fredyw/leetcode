5a5d657115f024e765c9d0cba77fa272067e99e1
fredy <fredy.wijaya@amd.com>
2015-01-15 16:34:58 -0600

    Solve problem 108

--- src/main/java/leetcode/Problem108.java
+++ src/main/java/leetcode/Problem108.java
@@ -13,13 +13,38 @@ public class Problem108 {
             val = x;
         }
     }
-
+    
+    private TreeNode root;
+    
     public TreeNode sortedArrayToBST(int[] num) {
-        // TODO: to be implemented
-        return null;
+        sortedArrayToBST(num, 0, num.length);
+        return root;
     }
     
-    public static void main(String[] args) {
-        Problem108 prob = new Problem108();
+    private void sortedArrayToBST(int[] num, int lo, int hi) {
+        if (lo >= hi) {
+            return;
+        }
+        int mid = (int) Math.ceil((hi + lo) / 2);
+        int val = num[mid];
+        if (root == null) {
+            root = new TreeNode(val);
+        } else {
+            add(root, val);
+        }
+        sortedArrayToBST(num, lo, mid);
+        sortedArrayToBST(num, mid+1, hi);
+    }
+    
+    private TreeNode add(TreeNode node, int val) {
+        if (node == null) {
+            return new TreeNode(val);
+        }
+        if (val <= node.val) {
+            node.left = add(node.left, val);
+        } else {
+            node.right = add(node.right, val);
+        }
+        return node;
     }
 }
