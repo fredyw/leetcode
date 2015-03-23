902192a220c41f98defd576c9675d92073d52799
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-03-23 18:04:40 -0500

    Write a skeleton for problem 134

---
+++ src/main/java/leetcode/Problem134.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/gas-station/
+ */
+public class Problem134 {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        // TODO: to be implemented
+        return 0;
+    }
+}
