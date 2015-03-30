bb529e289222172a14b394bf4c7488c58402559b
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-03-30 11:14:46 -0500

    Solve problem 162

---
+++ src/main/java/leetcode/Problem162.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/find-peak-element/
+ */
+public class Problem162 {
+    public int findPeakElement(int[] num) {
+        return findPeakElement(num, num.length-1, 0);
+    }
+    
+    private int findPeakElement(int[] num, int left, int right) {
+        if (right >= left) {
+            return right;
+        }
+        int mid = (left + right) / 2;
+        if (num[mid] < num[mid+1]) {
+            return findPeakElement(num, left, mid+1);
+        } else {
+            return findPeakElement(num, mid, right);
+        }
+    }
+}
