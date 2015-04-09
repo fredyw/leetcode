f83bcd0d3ccbb98f516d1761126add73da5ce295
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-04-09 17:12:08 -0500

    Add a skeleton for problem 139

---
+++ src/main/java/leetcode/Problem139.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+import java.util.Set;
+
+/**
+ * https://leetcode.com/problems/word-break/
+ */
+public class Problem139 {
+    public boolean wordBreak(String s, Set<String> dict) {
+        // TODO:
+        return false;
+    }
+    
+    public static void main(String[] args) {
+        Problem139 prob = new Problem139();
+        
+    }
+}
