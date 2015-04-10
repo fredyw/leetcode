4d89cee409a71bc372691b50effc157c6b8df9d2
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-04-09 23:05:36 -0500

    Solve problem 139

--- src/main/java/leetcode/Problem139.java
+++ src/main/java/leetcode/Problem139.java
@@ -1,5 +1,6 @@
 package leetcode;
 
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -7,12 +8,25 @@ import java.util.Set;
  */
 public class Problem139 {
     public boolean wordBreak(String s, Set<String> dict) {
-        // TODO:
-        return false;
+        return wordBreak(s, dict, new HashSet<>());
     }
     
-    public static void main(String[] args) {
-        Problem139 prob = new Problem139();
-        
+    private boolean wordBreak(String s, Set<String> dict, Set<String> memo) {
+        for (String d : dict) {
+            if (s.equals(d)) {
+                return true;
+            }
+            if (memo.contains(s)) {
+                return false;
+            }
+            if (s.startsWith(d)) {
+                boolean found = wordBreak(s.substring(d.length()), dict, memo);
+                if (found) {
+                    return true;
+                }
+            }
+        }
+        memo.add(s);
+        return false;
     }
 }
