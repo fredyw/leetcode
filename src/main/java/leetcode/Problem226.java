ad5c74526060b814303f38bc66df22293f0d8054
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-06-29 00:55:08 -0500

    Solve problem 226

---
+++ src/main/java/leetcode/Problem226.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/invert-binary-tree/
+ */
+public class Problem226 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        invert(root);
+        return root;
+    }
+    
+    private void invert(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        TreeNode tmp = node.left;
+        node.left = node.right;
+        node.right = tmp;
+        invertTree(node.left);
+        invertTree(node.right);
+    }
+}
