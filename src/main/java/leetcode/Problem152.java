d334665e1dd07f3f1b6d798ff549d1be46483316
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-07-02 12:10:54 -0500

    Initial commit for problem 152

---
+++ src/main/java/leetcode/Problem152.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/maximum-product-subarray/
+ */
+public class Problem152 {
+    public int maxProduct(int[] nums) {
+        // TODO:
+        return 0;
+    }
+    
+    public static void main(String[] args) {
+        Problem152 prob = new Problem152();
+        System.out.println(prob.maxProduct(new int[]{ 2, 3, -2, 4 }));
+    }
+}
