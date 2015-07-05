c8730945545b0713e1222ea3afbf162a5e305a1c
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-07-05 12:49:54 -0500

    Solve problem 144

---
+++ src/main/java/leetcode/Problem144.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/binary-tree-preorder-traversal/
+ */
+public class Problem144 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        preOrderTraversal(root, result);
+        return result;
+    }
+    
+    private void preOrderTraversal(TreeNode node, List<Integer> result) {
+        if (node == null) {
+            return;
+        }
+        result.add(node.val);
+        preOrderTraversal(node.left, result);
+        preOrderTraversal(node.right, result);
+    }
+}
