634c6f35217544d2bbc4d38b2895181a38d42d71
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-07-17 00:10:52 -0500

    Solve problem 237

--- src/main/java/leetcode/Problem237.java
+++ src/main/java/leetcode/Problem237.java
@@ -14,17 +14,7 @@ public class Problem237 {
     }
 
     public void deleteNode(ListNode node) {
-        // TODO:
-    }
-
-    public static void main(String[] args) {
-        Problem237 prob = new Problem237();
-        
-        ListNode head = new ListNode(1);
-        head.next = new ListNode(2);
-        head.next.next = new ListNode(3);
-        head.next.next.next = new ListNode(4);
-        
-        prob.deleteNode(head.next.next);
+        node.val = node.next.val;
+        node.next = node.next.next;
     }
 }
