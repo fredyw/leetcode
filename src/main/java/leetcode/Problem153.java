79df684999b701e8f7886b07e460247327de525b
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-07-19 11:19:26 -0500

    Solve problem 153 (log n)

--- src/main/java/leetcode/Problem153.java
+++ src/main/java/leetcode/Problem153.java
@@ -5,12 +5,30 @@ package leetcode;
  */
 public class Problem153 {
     public int findMin(int[] nums) {
-        int min = nums[0];
-        for (int n : nums) {
-            if (n < min) {
-                min = Math.min(n, min);
-            }
+        return findMin(nums, 0, nums.length-1);
+    }
+    
+    private int findMin(int[] nums, int left, int right) {
+        if (left >= right) {
+            return nums[left];
         }
-        return min;
+        int mid = (left + right) / 2;
+        int minLeft = nums[left];
+        if (mid-1 >= 0) {
+            minLeft = Math.min(minLeft, nums[mid-1]);
+        }
+        int minRight = nums[right];
+        if (mid+1 < nums.length) {
+            minRight = Math.min(minRight, nums[mid+1]);
+        }
+        int min = Math.min(minLeft, Math.min(nums[mid], minRight));
+        if (min == nums[mid]) {
+            return nums[mid];
+        }
+        if (minLeft < minRight) {
+            return findMin(nums, left, mid-1);
+        } else {
+            return findMin(nums, mid+1, right);
+        }
     }
 }
