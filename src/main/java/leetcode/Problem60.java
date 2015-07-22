e3e712be849407e989a39ed4006f1d27483cc630
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-07-22 00:15:51 -0500

    Solve problem 60 (time limit exceeded)

--- src/main/java/leetcode/Problem60.java
+++ src/main/java/leetcode/Problem60.java
@@ -1,26 +1,43 @@
 package leetcode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * https://leetcode.com/problems/permutation-sequence/
  */
 public class Problem60 {
     public String getPermutation(int n, int k) {
-        getPermutation(n, "");
-        return "";
+        return getPermutation(n, k, n, new IntRef(), new HashSet<>(), "");
     }
     
-    public void getPermutation(int n, String accu) {
-        if (n == 0) {
-            System.out.println(accu);
-            return;
+    private static class IntRef {
+        int k;
+    }
+    
+    private String getPermutation(int n, int k, int m, IntRef intRef, Set<Integer> set, String accu) {
+        if (m == 0) {
+            intRef.k++;
+            if (intRef.k == k) {
+                return accu;
+            }
+            return null;
         }
         for (int i = 1; i <= n; i++) {
-            getPermutation(n-1, accu + i);
+            if (!set.contains(i)) {
+                Set<Integer> newSet = new HashSet<>(set);
+                newSet.add(i);
+                String result = getPermutation(n, k, m-1, intRef, newSet, accu+i);
+                if (result != null) {
+                    return result;
+                }
+            }
         }
+        return null;
     }
     
     public static void main(String[] args) {
         Problem60 prob = new Problem60();
-        System.out.println(prob.getPermutation(3, 6));
+        System.out.println(prob.getPermutation(9, 171669));
     }
 }
