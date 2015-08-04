34cd2b93daa942c258bb3700a4a2bd14e363d85f
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-08-04 00:06:10 -0500

    Fix formatting for problem 48 and 60

--- src/main/java/leetcode/Problem48.java
+++ src/main/java/leetcode/Problem48.java
@@ -5,17 +5,17 @@ package leetcode;
  */
 public class Problem48 {
     public void rotate(int[][] matrix) {
-    	int n = matrix.length;
+        int n = matrix.length;
         int[][] tmp = new int[n][n];
         for (int i = 0; i < n; i++) {
-        	for (int j = 0; j < n; j++) {
-        		tmp[i][j] = matrix[i][j];
-        	}
+            for (int j = 0; j < n; j++) {
+                tmp[i][j] = matrix[i][j];
+            }
         }
         for (int i = 0; i < n; i++) {
-        	for (int j = 0; j < n; j++) {
-        		matrix[j][i] = tmp[n-1-i][j];
-        	}
+            for (int j = 0; j < n; j++) {
+                matrix[j][i] = tmp[n-1-i][j];
+            }
         }
     }
 }
--- src/main/java/leetcode/Problem60.java
+++ src/main/java/leetcode/Problem60.java
@@ -7,28 +7,28 @@ import java.util.List;
  * https://leetcode.com/problems/permutation-sequence/
  */
 public class Problem60 {
-	public String getPermutation(int n, int k) {
-		int[] factorials = new int[n];
-		for (int i = 0; i < n; i++) {
-			factorials[i] = factorial(i);
-		}
-		List<Integer> numbers = new ArrayList<>();
-		for (int i = 1; i <= n; i++) {
-			numbers.add(i);
-		}
-		StringBuilder sb = new StringBuilder();
-		for (int i = n-1; i >= 0; i--) {
-			int idx = k / factorials[i];
-			if (k % factorials[i] == 0) {
-				idx -= 1;
-			}
-			sb.append(numbers.get(idx));
-			numbers.remove(idx);
-			k = k - (idx * factorials[i]);
-		}
-		return sb.toString();
-	}
-	
+    public String getPermutation(int n, int k) {
+        int[] factorials = new int[n];
+        for (int i = 0; i < n; i++) {
+            factorials[i] = factorial(i);
+        }
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            numbers.add(i);
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = n-1; i >= 0; i--) {
+            int idx = k / factorials[i];
+            if (k % factorials[i] == 0) {
+                idx -= 1;
+            }
+            sb.append(numbers.get(idx));
+            numbers.remove(idx);
+            k = k - (idx * factorials[i]);
+        }
+        return sb.toString();
+    }
+
     private int factorial(int n) {
         if (n == 0 || n == 1) {
             return 1;
