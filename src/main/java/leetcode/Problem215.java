02e05e1cf64e5cb3648fb66df10b73bfde309136
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-08-07 13:52:01 -0500

    Create skeleton for problem 215

---
+++ src/main/java/leetcode/Problem215.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/kth-largest-element-in-an-array/
+ */
+public class Problem215 {
+    public int findKthLargest(int[] nums, int k) {
+        
+    }
+    
+    public static void main(String[] args) {
+        Problem215 prob = new Problem215();
+        System.out.println(prob.findKthLargest(new int[]{2, 7, 4, 5, 6, 3, 1}, 4));
+    }
+}
