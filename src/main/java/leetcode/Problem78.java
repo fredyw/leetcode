edf72a9c3e79c531d93201be72585d4b84c8473b
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-08-07 11:41:39 -0500

    Solve problem 78

--- src/main/java/leetcode/Problem78.java
+++ src/main/java/leetcode/Problem78.java
@@ -1,19 +1,51 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * https://leetcode.com/problems/subsets/
  */
 public class Problem78 {
     public List<List<Integer>> subsets(int[] nums) {
-        return null;
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> numList = Arrays.stream(nums)
+            .boxed()
+            .collect(Collectors.toList());
+        Collections.sort(numList);
+        Set<List<Integer>> set = new HashSet<>();
+        subsets(numList, set);
+        result.addAll(set);
+        result.add(numList);
+        return result;
     }
     
-    public static void main(String[] args) {
-        Problem78 prob = new Problem78();
-        for (List<Integer> list : prob.subsets(new int[]{1, 2, 3})) {
-            System.out.println(list);
+    private void subsets(List<Integer> nums, Set<List<Integer>> set) {
+        if (nums.size() == 0) {
+            return;
         }
+        for (int i = 0; i < nums.size(); i++) {
+            List<Integer> newList = createList(nums, i);
+            if (set.contains(newList)) {
+                continue;
+            }
+            set.add(newList);
+            subsets(newList, set);
+        }
+    }
+    
+    private List<Integer> createList(List<Integer> nums, int idx) {
+        List<Integer> newList = new ArrayList<>();
+        for (int i = 0; i < nums.size(); i++) {
+            if (i != idx) {
+                newList.add(nums.get(i));
+            }
+        }
+        return newList;
     }
 }
