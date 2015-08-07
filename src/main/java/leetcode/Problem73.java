22a9b9f20cc646f620420d219947d3f3adb3e3d0
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-08-07 00:17:49 -0500

    Update problem 73 (not finished yet)

--- src/main/java/leetcode/Problem73.java
+++ src/main/java/leetcode/Problem73.java
@@ -7,7 +7,26 @@ import java.util.Arrays;
  */
 public class Problem73 {
     public void setZeroes(int[][] matrix) {
-        
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == 0) {
+                    setRowZeroes(matrix, i);
+                    setColZeroes(matrix, j);
+                }
+            }
+        }
+    }
+    
+    private void setRowZeroes(int[][] matrix, int row) {
+        for (int i = 0; i < matrix[row].length; i++) {
+            matrix[row][i] = 0;
+        }
+    }
+    
+    private void setColZeroes(int[][] matrix, int col) {
+        for (int i = 0; i < matrix.length; i++) {
+            matrix[i][col] = 0;
+        }
     }
     
     public static void main(String[] args) {
@@ -15,7 +34,7 @@ public class Problem73 {
         int[][] matrix = new int[][] {
             new int[]{1, 0, 1, 1},
             new int[]{0, 1, 1, 1},
-            new int[]{0, 1, 1, 1}
+            new int[]{1, 1, 1, 1}
         };
         prob.setZeroes(matrix);
         for (int i = 0; i < matrix.length; i++) {
