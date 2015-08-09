d3e18e1ffa3e7d46810c55139c834776df61de26
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-08-08 20:21:30 -0500

    Add skeleton for problem 55

---
+++ src/main/java/leetcode/Problem55.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+public class Problem55 {
+    public boolean canJump(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+        }
+        return false;
+    }
+    
+    public static void main(String[] args) {
+        Problem55 prob = new Problem55();
+        System.out.println(prob.canJump(new int[]{2,3,1,1,4}));
+//        System.out.println(prob.canJump(new int[]{3,2,1,0,4}));
+    }
+}
