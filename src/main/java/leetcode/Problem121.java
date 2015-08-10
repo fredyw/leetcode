36cb0a156ce7e97ae2578b14e5eef23e2ee442c6
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-08-10 09:51:58 -0500

    Add problem 121

---
+++ src/main/java/leetcode/Problem121.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+ */
+public class Problem121 {
+    public int maxProfit(int[] prices) {
+        return 0;
+    }
+    
+    public static void main(String[] args) {
+        Problem121 prob = new Problem121();
+        System.out.println(prob.maxProfit(new int[]{1, 2, 4, 2, 1, 3}));
+    }
+}
