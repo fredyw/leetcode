61c09268b423d455e9278c5f9d2893c9d70919c8
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-08-11 16:58:49 -0500

    Create skeleton for problem 122

---
+++ src/main/java/leetcode/Problem122.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+ */
+public class Problem122 {
+    public int maxProfit(int[] prices) {
+        return 0;
+    }
+    
+    public static void main(String[] args) {
+        Problem122 prob = new Problem122();
+        System.out.println(prob.maxProfit(new int[]{1, 2, 4, 2, 1, 3}));
+        System.out.println(prob.maxProfit(new int[]{3, 2, 6, 5, 0, 3}));
+    }
+}
