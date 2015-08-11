a36f9e61e615d69e2d313a418e5a3563629c202e
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-08-11 14:35:36 -0500

    Solve problem 121

--- src/main/java/leetcode/Problem121.java
+++ src/main/java/leetcode/Problem121.java
@@ -6,8 +6,8 @@ package leetcode;
 public class Problem121 {
     public int maxProfit(int[] prices) {
         int[] max = new int[prices.length];
-        int m = -1;
-        for (int i = prices.length-1; i != 0; i--) {
+        int m = 0;
+        for (int i = prices.length-1; i >= 0; i--) {
             max[i] = Math.max(m, prices[i]);
             m = max[i];
         }
@@ -18,11 +18,5 @@ public class Problem121 {
             profit = Math.max(profit, sell-buy);
         }
         return profit;
-    }
-    
-    public static void main(String[] args) {
-        Problem121 prob = new Problem121();
-        System.out.println(prob.maxProfit(new int[]{1, 2, 4, 2, 1, 3}));
-        System.out.println(prob.maxProfit(new int[]{3, 2, 6, 5, 0, 3}));
     }
 }
