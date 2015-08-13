857931357fb5113e9679cb0b14c3e924e4dd426c
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-08-13 00:17:23 -0500

    Update problem 122

--- src/main/java/leetcode/Problem122.java
+++ src/main/java/leetcode/Problem122.java
@@ -12,5 +12,7 @@ public class Problem122 {
         Problem122 prob = new Problem122();
         System.out.println(prob.maxProfit(new int[]{1, 2, 4, 2, 1, 3}));
         System.out.println(prob.maxProfit(new int[]{3, 2, 6, 5, 0, 3}));
+        System.out.println(prob.maxProfit(new int[]{3, 1, 2, 6, 1, 6, 8}));
+        
     }
 }
