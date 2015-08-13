8c9fc8ea65f63bb400ec63ec25a95ba950e680d9
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-08-13 00:50:48 -0500

    Create skeleton for problem 238

---
+++ src/main/java/leetcode/Problem238.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/product-of-array-except-self/
+ */
+public class Problem238 {
+   public int[] productExceptSelf(int[] nums) {
+       return null;
+   }
+   
+   public static void main(String[] args) {
+       Problem238 prob = new Problem238();
+       System.out.println(prob.productExceptSelf(new int[]{1, 2, 3, 4})); // [24,12,8,6]
+   }
+}
