3e9824e0e3c3917e273a192b9c89601d30beefc1
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-08-16 12:59:07 -0500

    Create a skeleton for problem 204

---
+++ src/main/java/leetcode/Problem204.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/count-primes/
+ */
+public class Problem204 {
+    public int countPrimes(int n) {
+        // TODO:
+        return 0;
+    }
+    
+    public static void main(String[] args) {
+        Problem204 prob = new Problem204();
+        System.out.println(prob.countPrimes(10));
+    }
+}
