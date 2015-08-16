2ce0d91b8302f6fd3493dd4892be4a6f790b1c11
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-08-16 12:23:47 -0500

    Solve problem 258

---
+++ src/main/java/leetcode/Problem258.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+public class Problem258 {
+    public int addDigits(int num) {
+        int n = num;
+        String s = Integer.toString(n);
+        while (s.length() != 1) {
+            n = 0;
+            for (char c : s.toCharArray()) {
+                int i = Integer.parseInt(Character.toString(c));
+                n += i;
+            }
+            s = Integer.toString(n);
+        }
+        return n;
+    }
+}
