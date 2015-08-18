0403707fc379663e042b2809c00ada72e2d598c5
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-08-18 17:26:08 -0500

    Create a skeleton for problem 263

---
+++ src/main/java/leetcode/Problem263.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/ugly-number/
+ */
+public class Problem263 {
+    public boolean isUgly(int num) {
+        return false;
+    }
+    
+    public static void main(String[] args) {
+        Problem263 prob = new Problem263();
+        System.out.println(prob.isUgly(6)); // true
+        System.out.println(prob.isUgly(8)); // true
+        System.out.println(prob.isUgly(14)); // false
+    }
+}
