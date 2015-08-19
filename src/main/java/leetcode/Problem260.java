3b6193cdc11de462dc5dcd1dd2eeae1adef7e9ff
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-08-19 00:09:00 -0500

    Create skeleton for problem 260

---
+++ src/main/java/leetcode/Problem260.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/single-number-iii/
+ */
+public class Problem260 {
+    public int[] singleNumber(int[] nums) {
+        return null;
+    }
+    
+    public static void main(String[] args) {
+        Problem260 prob = new Problem260();
+        System.out.println(Arrays.toString(prob.singleNumber(new int[]{1, 2, 1, 3, 2, 5}))); // [3, 5]
+    }
+}
