25116686254211e73ee1fa800b9c67c9281a656b
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-08-20 00:02:22 -0500

    Create a skeleton for problem 42

---
+++ src/main/java/leetcode/Problem42.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/trapping-rain-water/
+ */
+public class Problem42 {
+    public int trap(int[] height) {
+        return 0;
+    }
+    
+    public static void main(String[] args) {
+        Problem42 prob = new Problem42();
+        System.out.println(prob.trap(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1})); // 6
+    }
+}
