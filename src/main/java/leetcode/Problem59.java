c2f1fd8cb938c20186ff202ce42913f4e1406e7d
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-08-22 00:18:03 -0500

    Create a skeleton for problem 59

---
+++ src/main/java/leetcode/Problem59.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/spiral-matrix-ii/
+ */
+public class Problem59 {
+    public int[][] generateMatrix(int n) {
+        return null;
+    }
+    
+    public static void main(String[] args) {
+        Problem59 prob = new Problem59();
+        for (int[] a : prob.generateMatrix(3)) {
+            System.out.println(Arrays.toString(a));
+        }
+    }
+}
