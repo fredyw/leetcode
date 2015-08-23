883fe0cb31280d212267dba08bbd2e0fbf41d9fa
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-08-23 00:28:23 -0500

    Wrong understanding of problem 61

--- src/main/java/leetcode/Problem61.java
+++ src/main/java/leetcode/Problem61.java
@@ -1,7 +1,5 @@
 package leetcode;
 
-import java.awt.List;
-
 /**
  * https://leetcode.com/problems/rotate-list/
  */
@@ -16,22 +14,51 @@ public class Problem61 {
     }
 
     public ListNode rotateRight(ListNode head, int k) {
-        return head;
+        if (head == null || k == 0) {
+            return head;
+        }
+        int size = 0;
+        for (ListNode node = head; node != null; node = node.next) {
+            size++;
+        }
+        if (size <= k) {
+            return head;
+        }
+        int n = size - k - 1;
+        ListNode node = head;
+        for (int i = 0; i < n; i++) {
+            node = node.next;
+        }
+        ListNode newHead = node.next;
+        node.next = null;
+        ListNode tail = newHead.next;
+        if (tail != null) {
+            while (tail.next != null) {
+                tail = tail.next;
+            }
+        }
+        if (tail != null) {
+            tail.next = head;
+        } else {
+            newHead.next = head;
+        }
+        return newHead;
     }
     
     public static void main(String[] args) {
         Problem61 prob = new Problem61();
         
-        ListNode node = new ListNode(1);
-        node.next = new ListNode(2);
-        node.next.next = new ListNode(3);
-        node.next.next.next = new ListNode(4);
-        node.next.next.next.next = new ListNode(5);
-        
-        node = prob.rotateRight(node, 2);
-        for (ListNode n = node; n != null; n = n.next) {
-            System.out.print(n.val + " ");
+        for (int i = 1; i <= 5; i++) {
+            ListNode node = new ListNode(1);
+            node.next = new ListNode(2);
+            node.next.next = new ListNode(3);
+            node.next.next.next = new ListNode(4);
+            node.next.next.next.next = new ListNode(5);
+            node = prob.rotateRight(node, i);
+            for (ListNode n = node; n != null; n = n.next) {
+                System.out.print(n.val + " ");
+            }
+            System.out.println();
         }
-        System.out.println();
     }
 }
