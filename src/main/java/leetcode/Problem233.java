5179f32cdb734c06f32703dca0dcba2d5547dba0
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-08-23 21:25:46 -0500

    Commit what I have for now

--- src/main/java/leetcode/Problem233.java
+++ src/main/java/leetcode/Problem233.java
@@ -5,20 +5,39 @@ package leetcode;
  */
 public class Problem233 {
     public int countDigitOne(int n) {
-        int result = 0;
-        for (int i = 1; i <= n; i++) {
-            int digit = countDigit(i);
-            int tmp = i;
-            for (int d = digit-1; d >= 0; d--) {
-                int divisor = (int) Math.pow(10, d);
-                int a = tmp / divisor;
-                if (a == 1) {
-                    result++;
-                }
-                tmp = tmp % divisor;
-            }
+        if (n <= 0)
+            return 0;
+        if (n < 10)
+            return 1;
+        System.out.println("n="+ n);
+        int t = (n % 10 + 1) * countDigitOne2(new Integer(n / 10).toString());
+        if (n % 10 >= 1) {
+            return countDigitOne(n / 10 - 1) * 10 + (n / 10 + 1) + t;
+        } else {
+            return countDigitOne(n / 10 - 1) * 10 + n / 10 + t;
         }
-        return result;
+        
+//        int result = 0;
+//        for (int i = 1; i <= n; i *= 10) {
+//            int j = n / i;
+//            System.out.println(j);
+//        }
+//        return result;
+//        
+//        int result = 0;
+//        for (int i = 1; i <= n; i++) {
+//            int digit = countDigit(i);
+//            int tmp = i;
+//            for (int d = digit-1; d >= 0; d--) {
+//                int divisor = (int) Math.pow(10, d);
+//                int a = tmp / divisor;
+//                if (a == 1) {
+//                    result++;
+//                }
+//                tmp = tmp % divisor;
+//            }
+//        }
+//        return result;
     }
 
     private int countDigit(int n) {
@@ -31,11 +50,27 @@ public class Problem233 {
         return digit;
     }
     
+    public static int countDigitOne2(String s) {
+        int res = 0; 
+        char[] arry = s.toCharArray();
+        for(char c : arry) {
+            if(c == '1') {
+                res += 1;
+            }
+        }
+        return res;
+    }
+    
     public static void main(String[] args) {
         Problem233 prob = new Problem233();
-//        System.out.println(prob.countDigit(824883294));
-//        System.out.println(prob.countDigitOne(12341));
-//        System.out.println(prob.countDigitOne(13));
-        System.out.println(prob.countDigitOne(824883294));
+//        System.out.println(prob.countDigitOne(100)); // 21
+//        System.out.println(prob.countDigitOne(1000)); // 301
+//        System.out.println(prob.countDigitOne(10000)); // 4001
+        System.out.println(prob.countDigitOne(1811)); // 1376
+//        System.out.println(prob.countDigitOne(181)); // 121
+//        System.out.println(prob.countDigitOne(121)); // 55
+//        System.out.println(prob.countDigitOne(12341)); // 8117
+//        System.out.println(prob.countDigitOne(13)); // 6
+//        System.out.println(prob.countDigitOne(824883294)); // 767944060
     }
 }
