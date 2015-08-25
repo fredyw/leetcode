9a68803aa8a09312315d25f31778e404b60d9c56
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-08-25 00:42:35 -0500

    Create a skeleton for problem 34

---
+++ src/main/java/leetcode/Problem34.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/search-for-a-range/
+ */
+public class Problem34 {
+    public int[] searchRange(int[] nums, int target) {
+        return null;
+    }
+    
+    public static void main(String[] args) {
+        Problem34 prob = new Problem34();
+        System.out.println(Arrays.toString(prob.searchRange(new int[]{5, 7, 7, 8, 8, 10}, 8))); // [3, 4]
+    }
+}
