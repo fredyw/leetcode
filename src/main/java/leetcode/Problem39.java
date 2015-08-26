2b4a71dd1bf41a5f9322f7d0f919c5a5a6c47dde
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-08-25 20:41:27 -0500

    Solve problem 39

--- src/main/java/leetcode/Problem39.java
+++ src/main/java/leetcode/Problem39.java
@@ -1,7 +1,10 @@
 package leetcode;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * https://leetcode.com/problems/combination-sum/
@@ -9,14 +12,28 @@ import java.util.List;
 public class Problem39 {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> result = new ArrayList<>();
-        // TODO: implement this
+        Set<List<Integer>> set = new HashSet<>();
+        combinationSum(candidates, target, new ArrayList<>(), set);
+        for (List<Integer> list : set) {
+            result.add(list);
+        }
         return result;
     }
     
-    public static void main(String[] args) {
-        Problem39 prob = new Problem39();
-        for (List<Integer> list : prob.combinationSum(new int[]{2, 3, 6, 7}, 7)) {
-            System.out.println(list);
+    private void combinationSum(int[] candidates, int target, List<Integer> accu,
+        Set<List<Integer>> result) {
+        if (target == 0) {
+            Collections.sort(accu);
+            result.add(accu);
+            return;
+        }    
+        if (target < 0) {
+            return;
+        }
+        for (int i = 0; i < candidates.length; i++) {
+            List<Integer> newAccu = new ArrayList<>(accu);
+            newAccu.add(candidates[i]);
+            combinationSum(candidates, target-candidates[i], newAccu, result);
         }
     }
 }
