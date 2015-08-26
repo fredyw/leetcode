4e93b7ca003e8602572b51e78bc9458d5d635ed6
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-08-26 11:44:09 -0500

    Create a skeleton for problem 201

---
+++ src/main/java/leetcode/Problem201.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/bitwise-and-of-numbers-range/
+ */
+public class Problem201 {
+    public int rangeBitwiseAnd(int m, int n) {
+        return 0;
+    }
+    
+    public static void main(String[] args) {
+        Problem201 prob = new Problem201();
+        System.out.println(prob.rangeBitwiseAnd(5, 7)); // 4
+    }
+}
