58bbee76b4126b712a0dad18e631154b7def75f2
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-08-26 20:51:12 -0500

    Create a skeleton for problem 106

---
+++ src/main/java/leetcode/Problem106.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/
+ */
+public class Problem106 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem106 prob = new Problem106();
+    }
+}
