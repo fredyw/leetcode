66f72fcf0d93d615160753ae096d116c1bb47407
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-09-09 02:02:19 -0500

    Solve problem 278

--- src/main/java/leetcode/Problem278.java
+++ src/main/java/leetcode/Problem278.java
@@ -6,7 +6,7 @@ package leetcode;
 public class Problem278 {
     public static class VersionControl {
         public boolean isBadVersion(int version) {
-            if (version <= 4) {
+            if (version >= 1) {
                 return true;
             }
             return false;
@@ -15,12 +15,31 @@ public class Problem278 {
     
     public static class Solution extends VersionControl {
         public int firstBadVersion(int n) {
-            return 0;
+            int left = 1;
+            int right = n;
+            while (left <= right) {
+                int mid = (int) (((long) left + (long) right) / 2);
+                if (isBadVersion(mid)) {
+                    // go left
+                    right = mid - 1;
+                } else {
+                    // go right
+                    left = mid + 1;
+                }
+                if (mid-1 >= 1) {
+                    boolean b = isBadVersion(mid-1) ^ isBadVersion(mid);
+                    if (b) {
+                        return mid;
+                    }
+                }
+                if (mid+1 <= n) {
+                    boolean b = isBadVersion(mid+1) ^ isBadVersion(mid);
+                    if (b) {
+                        return mid + 1;
+                    }
+                }
+            }
+            return left;
         }
-    }
-    
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        System.out.println(solution.firstBadVersion(10));
     }
 }
