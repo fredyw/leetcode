797277ea0ba4a9626eaa2ef31356d6a30c0ceaef
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-10-03 09:43:05 -0500

    Add skeleton for problem 95

---
+++ src/main/java/leetcode/Problem95.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/unique-binary-search-trees-ii/
+ */
+public class Problem95 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public List<TreeNode> generateTrees(int n) {
+        List<TreeNode> list = new ArrayList<>();
+        return list;
+    }
+
+    public static void main(String[] args) {
+        Problem95 prob = new Problem95();
+
+    }
+}
