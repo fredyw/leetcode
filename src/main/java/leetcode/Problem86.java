6a90baf20936fcd3ececb5aa82f413626bf55b4c
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-10-04 12:56:28 -0500

    Add problem 86

---
+++ src/main/java/leetcode/Problem86.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/partition-list/
+ */
+public class Problem86 {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode partition(ListNode head, int x) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
