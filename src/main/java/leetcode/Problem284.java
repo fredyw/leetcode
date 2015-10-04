4e8f0934f7994729902fdd783c6ee5a05eb50b0f
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-10-04 01:00:09 -0500

    Add a skeleton for problem 284

---
+++ src/main/java/leetcode/Problem284.java
@@ -0,0 +1,38 @@
+package leetcode;
+
+import java.util.Iterator;
+
+/**
+ * https://leetcode.com/problems/peeking-iterator/
+ */
+public class Problem284 {
+    private static class PeekingIterator implements Iterator<Integer> {
+        public PeekingIterator(Iterator<Integer> iterator) {
+            // initialize any member here.
+        }
+
+        // Returns the next element in the iteration without advancing the iterator.
+        public Integer peek() {
+            // TODO
+            return null;
+        }
+
+        // hasNext() and next() should behave the same as in the Iterator interface.
+        // Override them if needed.
+        @Override
+        public Integer next() {
+            // TODO
+            return null;
+        }
+
+        @Override
+        public boolean hasNext() {
+            // TODO
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
