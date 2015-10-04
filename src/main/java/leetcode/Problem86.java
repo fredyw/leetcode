e0d65e0531ab48efd2406cedd6c2fda5aaacbbba
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-10-04 17:11:34 -0500

    Solve problem 86

--- src/main/java/leetcode/Problem86.java
+++ src/main/java/leetcode/Problem86.java
@@ -4,7 +4,7 @@ package leetcode;
  * https://leetcode.com/problems/partition-list/
  */
 public class Problem86 {
-    public class ListNode {
+    public static class ListNode {
         int val;
         ListNode next;
         ListNode(int x) {
@@ -13,11 +13,33 @@ public class Problem86 {
     }
 
     public ListNode partition(ListNode head, int x) {
-        // TODO
-        return null;
-    }
-
-    public static void main(String[] args) {
-
+        ListNode left = null;
+        ListNode right = null;
+        ListNode headLeft = null;
+        ListNode headRight = null;
+        for (ListNode n = head; n != null; n = n.next) {
+            if (n.val < x) {
+                if (left == null) {
+                    left = new ListNode(n.val);
+                    headLeft = left;
+                } else {
+                    left.next = new ListNode(n.val);
+                    left = left.next;
+                }
+            } else {
+                if (right == null) {
+                    right = new ListNode(n.val);
+                    headRight = right;
+                } else {
+                    right.next = new ListNode(n.val);
+                    right = right.next;
+                }
+            }
+        }
+        if (left != null) {
+            left.next = headRight;
+            return headLeft;
+        }
+        return head;
     }
 }
