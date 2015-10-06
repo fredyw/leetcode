f7c6d1503bf779f0d9aadada673401989272231e
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-10-05 22:58:37 -0500

    Create a skeleton for problem 91

---
+++ src/main/java/leetcode/Problem91.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/decode-ways/
+ */
+public class Problem91 {
+    public int numDecodings(String s) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem91 prob = new Problem91();
+        System.out.println(prob.numDecodings("12"));
+    }
+}
