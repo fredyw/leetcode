913c0e3f97560410c900b5977f3ab7776308ae28
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-10-08 17:24:51 -0500

    Create a skeleton for problem 81

---
+++ src/main/java/leetcode/Problem81.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+ */
+public class Problem81 {
+    public boolean search(int[] nums, int target) {
+        return false;
+    }
+    
+    public static void main(String[] args) {
+        Problem81 prob = new Problem81();
+//        System.out.println(prob.search(nums, target));
+    }
+}
