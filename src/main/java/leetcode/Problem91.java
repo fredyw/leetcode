85c33e943c254a22fbbf446f09eac8b018c11493
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-10-08 17:13:44 -0500

    Solve problem 91

--- src/main/java/leetcode/Problem91.java
+++ src/main/java/leetcode/Problem91.java
@@ -1,75 +1,43 @@
 package leetcode;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * https://leetcode.com/problems/decode-ways/
  */
 public class Problem91 {
     public int numDecodings(String s) {
-        System.out.println("Input: " + s);
-        Map<String, Set<String>> memo = new HashMap<>();
-        Set<String> set = decode(s, memo);
-        if (set.size() == 0) {
-            return set.size();
+        if (s.length() == 0) {
+            return 0;
         }
-        System.out.println("Output:");
-        for (String str : set) {
-            System.out.println(str);
-        }
-        if (s.length() == 1) {
-            return set.size();
-        }
-        return set.size() + 1;
+        Map<Integer, Integer> memo = new HashMap<>();
+        return decode(s, 0, memo);
     }
-
-    private Set<String> decode(String s, Map<String, Set<String>> memo) {
-        if (s.length() == 1) {
-            Set<String> set = new HashSet<>();
-            int i = Integer.parseInt(s);
-            if (i >= 1 && i <= 9) {
-                set.add(s);
-            }
-            return set;
+    
+    private int decode(String s, int idx, Map<Integer, Integer> memo) {
+        if (memo.containsKey(idx)) {
+            return memo.get(idx);
         }
-        if (s.length() == 2) {
-            Set<String> set = new HashSet<>();
-            int i = Integer.parseInt(s);
-            if (i >= 10 && i <= 26) {
-                set.add(s);
-            }
-            return set;
+        if (idx == s.length()) {
+            return 1;
         }
-        if (memo.containsKey(s)) {
-            System.out.println("memo: " + s + " --> " + memo.get(s));
-            return memo.get(s);
-        }
-        Set<String> result = new HashSet<>();
-        for (int i = 0; i < s.length()-1; i++) {
-            String left = s.substring(0, i+1);
-            Set<String> leftSet = decode(left, memo);
-            String right = s.substring(i+1);
-            Set<String> rightSet = decode(right, memo);
-            for (String l : leftSet) {
-                for (String r : rightSet) {
-                    if (!result.add(l + " " + r)) {
-                        System.out.println("duplicate: " + l + " " + r);
-                    }
-                }
+        int result = 0;
+        if (idx+1 <= s.length()) {
+            String one = s.substring(idx, idx+1);
+            int num = Integer.parseInt(one);
+            if (num >= 1 && num <= 9) {
+                result += decode(s, idx+1, memo);
             }
         }
-        memo.put(s, result);
+        if (idx+2 <= s.length()) {
+            String two = s.substring(idx, idx+2);
+            int num =  Integer.parseInt(two);
+            if (num >= 10 && num <= 26) {
+                result += decode(s, idx+2, memo);
+            }
+        }
+        memo.put(idx, result);
         return result;
-    }
-
-    public static void main(String[] args) {
-        Problem91 prob = new Problem91();
-//        System.out.println(prob.numDecodings("12")); // 2
-//        System.out.println(prob.numDecodings("215")); // 3
-//        System.out.println(prob.numDecodings("2135")); // 3
-//        System.out.println(prob.numDecodings("2125")); // 5
-//        System.out.println(prob.numDecodings("21215")); // 8
-        System.out.println(prob.numDecodings("21105")); // 3
-//        System.out.println(prob.numDecodings("6065812287883668764831544958683283296479682877898293612168136334983851946827579555449329483852397155"));
     }
 }
