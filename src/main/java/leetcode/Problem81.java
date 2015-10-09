e6e771213ab0940b55b340055ea2776dac8c0b5e
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-10-09 00:45:53 -0500

    Solve problem 81

--- src/main/java/leetcode/Problem81.java
+++ src/main/java/leetcode/Problem81.java
@@ -5,11 +5,37 @@ package leetcode;
  */
 public class Problem81 {
     public boolean search(int[] nums, int target) {
-        return false;
+        int prev = 0;
+        int current;
+        for (current = 1; current < nums.length; current++) {
+            if (nums[current] < nums[prev]) {
+                break;
+            }
+            prev = current;
+        }
+        int startIdx = current;
+        if (current == nums.length) {
+            startIdx = 0;
+        }
+        int result = binarySearch(nums, target, 0, nums.length-1, startIdx);
+        return result != -1;
     }
-    
-    public static void main(String[] args) {
-        Problem81 prob = new Problem81();
-//        System.out.println(prob.search(nums, target));
+
+    private int binarySearch(int[] nums, int target, int lo, int hi, int startIdx) {
+        if (lo > hi) {
+            return -1;
+        }
+        int mid = (lo + hi) / 2;
+        int midIdx = startIdx + mid;
+        if (midIdx >= nums.length) {
+            midIdx = midIdx - nums.length;
+        }
+        if (nums[midIdx] == target) {
+            return midIdx;
+        }
+        if (nums[midIdx] > target) {
+            return binarySearch(nums, target, lo, mid-1, startIdx);
+        }
+        return binarySearch(nums, target, mid+1, hi, startIdx);
     }
 }
