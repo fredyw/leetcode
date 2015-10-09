33af70e2bf9aa1bd793c7c4ea120fdde2e419b0c
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-10-09 16:49:11 -0500

    Solve problem 31

--- src/main/java/leetcode/Problem31.java
+++ src/main/java/leetcode/Problem31.java
@@ -1,15 +1,28 @@
 package leetcode;
 
+import java.util.Arrays;
+
 /**
  * https://leetcode.com/problems/next-permutation/
  */
 public class Problem31 {
     public void nextPermutation(int[] nums) {
-        //TODO
-    }
-
-    public static void main(String[] args) {
-        Problem31 prob = new Problem31();
-        // TODO
+        for (int current = nums.length-2; current >= 0; current--) {
+            if (nums[current] < nums[current+1]) {
+                int val = nums[current];
+                Arrays.sort(nums, current, nums.length);
+                int idx = Arrays.binarySearch(nums, current, nums.length, val);
+                while (idx+1 < nums.length && nums[idx+1] == val) {
+                    idx++;
+                }
+                int tmp = nums[idx+1];
+                for (int i = idx+1; i >= current+1; i--) {
+                    nums[i] = nums[i-1];
+                }
+                nums[current] = tmp;
+                return;
+            }
+        }
+        Arrays.sort(nums);
     }
 }
