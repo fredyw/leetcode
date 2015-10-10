5231cc7b2c7dbaf20498e5e8d918d8e183dfd22a
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-10-10 00:56:56 -0500

    Add skeletons for problems 69 and 79

---
+++ src/main/java/leetcode/Problem69.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/sqrtx/
+ */
+public class Problem69 {
+    public int mySqrt(int x) {
+        // TODO:
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem69 prob = new Problem69();
+    }
+}
---
+++ src/main/java/leetcode/Problem79.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/word-search/
+ */
+public class Problem79 {
+    public boolean exist(char[][] board, String word) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem79 prob = new Problem79();
+    }
+}
