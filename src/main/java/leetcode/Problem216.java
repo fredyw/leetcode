169d1c766c3ff9a294c4149a39b84910b8f6c30f
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-10-10 00:59:51 -0500

    Add a skeleton for problem 216

---
+++ src/main/java/leetcode/Problem216.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/combination-sum-iii/
+ */
+public class Problem216 {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> result = new ArrayList<>();
+        // TODO
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Problem216 prob = new Problem216();
+    }
+}
