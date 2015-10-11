c41f57de6343d03713efa04ef85a7f2347c41b54
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-10-11 17:36:34 -0500

    Solve problem 69

--- src/main/java/leetcode/Problem69.java
+++ src/main/java/leetcode/Problem69.java
@@ -5,11 +5,29 @@ package leetcode;
  */
 public class Problem69 {
     public int mySqrt(int x) {
-        // TODO:
-        return 0;
+        // using Newton's method
+        // https://www.youtube.com/watch?v=2158QbsunA8
+        double sqrt = sqrt(x, x);
+        while (true) {
+            double tmp = sqrt;
+            sqrt = sqrt(sqrt, x);
+            if (Double.isNaN(sqrt) || tmp - sqrt < 1) {
+                break;
+            }
+        }
+        return (int) sqrt;
     }
 
-    public static void main(String[] args) {
-        Problem69 prob = new Problem69();
+    private double fx(double x, double y) {
+        return (x * x) - y;
+    }
+
+    private double dfx(double x) {
+        // the derivative of fx
+        return 2 * x;
+    }
+
+    private double sqrt(double x, double y) {
+        return x - (fx(x, y) / dfx(x));
     }
 }
