81bdd1f8077dba2092bcf47707454fa79cf4071c
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-10-12 15:42:17 -0500

    Add a skeleton for problem 200

---
+++ src/main/java/leetcode/Problem200.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/number-of-islands/
+ */
+public class Problem200 {
+    public int numIslands(char[][] grid) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem200 prob = new Problem200();
+
+    }
+}
