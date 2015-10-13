7fab36319c11fb577ffdab20ebeff0b331fd0e5d
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-10-13 00:24:52 -0500

    Solve problem 210

--- src/main/java/leetcode/Problem210.java
+++ src/main/java/leetcode/Problem210.java
@@ -12,25 +12,61 @@ import java.util.Set;
  */
 public class Problem210 {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        Map<Integer, List<Integer>> graph = new HashMap<>();
+        Map<Integer, List<Integer>> adjList = new HashMap<>();
+        Set<Integer> vertices = new HashSet<>();
         for (int i = 0; i < prerequisites.length; i++) {
             int[] array = prerequisites[i];
+            if (array.length != 2) {
+                continue;
+            }
             int from = array[0];
             int to = array[1];
-            if (graph.containsKey(from)) {
-                graph.get(from).add(to);
+            if (adjList.containsKey(from)) {
+                adjList.get(from).add(to);
             } else {
                 List<Integer> edges = new ArrayList<>();
                 edges.add(to);
-                graph.put(from, edges);
+                adjList.put(from, edges);
             }
         }
-        boolean cycle = hasCycle(graph);
+        for (int i = 0; i < numCourses; i++) {
+            vertices.add(i);
+        }
+        boolean cycle = hasCycle(adjList);
         if (cycle) {
             return new int[]{};
         }
-        // TODO: topological sort
-        return null;
+        List<Integer> path = topologicalSort(adjList, vertices);
+        int[] result = new int[path.size()];
+        for (int i = 0; i < path.size(); i++) {
+            result[i] = path.get(i);
+        }
+        return result;
+    }
+
+    private List<Integer> topologicalSort(Map<Integer, List<Integer>> adjList,
+                                          Set<Integer> vertices) {
+        Set<Integer> marked = new HashSet<>();
+        List<Integer> path = new ArrayList<>();
+        for (int vertex : vertices) {
+            if (!marked.contains(vertex)) {
+                topologicalSort(adjList, vertex, marked, path);
+            }
+        }
+        return path;
+    }
+
+    private void topologicalSort(Map<Integer, List<Integer>> adjList, int source,
+                                 Set<Integer> marked, List<Integer> path) {
+        marked.add(source);
+        if (adjList.containsKey(source)) {
+            for (int vertex : adjList.get(source)) {
+                if (!marked.contains(vertex)) {
+                    topologicalSort(adjList, vertex, marked, path);
+                }
+            }
+        }
+        path.add(source);
     }
 
     private static class Cycle {
@@ -46,7 +82,7 @@ public class Problem210 {
         }
         for (int vertex : graph.keySet()) {
             if (!marked.contains(vertex)) {
-                dfs(graph, vertex, onStack, marked, cycle);
+                hasCycle(graph, vertex, onStack, marked, cycle);
                 if (cycle.cycle) {
                     return true;
                 }
@@ -55,9 +91,9 @@ public class Problem210 {
         return false;
     }
 
-    private void dfs(Map<Integer, List<Integer>> graph, int source,
-                     Map<Integer, Boolean> onStack, Set<Integer> marked,
-                     Cycle cycle) {
+    private void hasCycle(Map<Integer, List<Integer>> graph, int source,
+                          Map<Integer, Boolean> onStack, Set<Integer> marked,
+                          Cycle cycle) {
         if (!graph.containsKey(source)) {
             return;
         }
@@ -65,7 +101,7 @@ public class Problem210 {
         onStack.put(source, true);
         for (int vertex : graph.get(source)) {
             if (!marked.contains(vertex)) {
-                dfs(graph, vertex, onStack, marked, cycle);
+                hasCycle(graph, vertex, onStack, marked, cycle);
             } else {
                 if (onStack.get(vertex)) {
                     cycle.cycle = true;
@@ -73,9 +109,5 @@ public class Problem210 {
             }
         }
         onStack.put(source, false);
-    }
-
-    public static void main(String[] args) {
-        Problem210 prob = new Problem210();
     }
 }
