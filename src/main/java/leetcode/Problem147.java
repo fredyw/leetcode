8e4679a587a666dd8db33f9fb14f12f496a08446
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-10-13 00:33:17 -0500

    Add skeletons for problems 147 and 211

---
+++ src/main/java/leetcode/Problem147.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/insertion-sort-list/
+ */
+public class Problem147 {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode insertionSortList(ListNode head) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem147 prob = new Problem147();
+    }
+}
---
+++ src/main/java/leetcode/Problem211.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/add-and-search-word-data-structure-design/
+ */
+public class Problem211 {
+    public static class WordDictionary {
+        // Adds a word into the data structure.
+        public void addWord(String word) {
+            // TODO
+        }
+
+        // Returns if the word is in the data structure. A word could
+        // contain the dot character '.' to represent any one letter.
+        public boolean search(String word) {
+            // TODO
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
