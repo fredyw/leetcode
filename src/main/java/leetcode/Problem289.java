626c29a97c5383cb210d8211b3edecafa83893eb
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-10-13 13:38:58 -0500

    Add skeleton for problem 289

---
+++ src/main/java/leetcode/Problem289.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/game-of-life/
+ */
+public class Problem289 {
+    public void gameOfLife(int[][] board) {
+        // TODO
+    }
+
+    public static void main(String[] args) {
+        Problem289 prob = new Problem289();
+    }
+}
