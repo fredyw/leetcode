4ba85142980a263b8634fcc971d221840ffda8f2
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-10-14 23:23:50 -0500

    Add a skeleton for problem 274

---
+++ src/main/java/leetcode/Problem274.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/h-index/
+ */
+public class Problem274 {
+    public int hIndex(int[] citations) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem274 prob = new Problem274();
+    }
+}
