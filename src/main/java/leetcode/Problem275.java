7f8d448211fd9aa0c570e446619e2df923885f5b
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-10-18 00:42:23 -0500

    Add skeleton for problem 275

---
+++ src/main/java/leetcode/Problem275.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/h-index-ii/
+ */
+public class Problem275 {
+    public int hIndex(int[] citations) {
+        // TODO:
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem275 prob = new Problem275();
+    }
+}
