5ac21c85a347e60d84d6681fcc72f4287431d586
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-10-18 02:19:36 -0500

    Solve problem 275

--- src/main/java/leetcode/Problem275.java
+++ src/main/java/leetcode/Problem275.java
@@ -5,8 +5,22 @@ package leetcode;
  */
 public class Problem275 {
     public int hIndex(int[] citations) {
-        // TODO:
-        return 0;
+        int idx = binarySearch(citations, 0, citations.length-1);
+        return citations.length - idx;
+    }
+
+    private int binarySearch(int[] citations, int lo, int hi) {
+        if (lo > hi) {
+            return lo;
+        }
+        int mid = (lo + hi) / 2;
+        if (citations[mid] == citations.length-mid) {
+            return mid;
+        }
+        if (citations[mid] > citations.length-mid) {
+            return binarySearch(citations, lo, mid-1);
+        }
+        return binarySearch(citations, mid+1, hi);
     }
 
     public static void main(String[] args) {
