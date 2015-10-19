973eed1ce293d91cf01adac9dfd65473402de80c
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-10-18 20:46:12 -0500

    Update problem 209

--- src/main/java/leetcode/Problem209.java
+++ src/main/java/leetcode/Problem209.java
@@ -5,11 +5,24 @@ package leetcode;
  */
 public class Problem209 {
     public int minSubArrayLen(int s, int[] nums) {
-        // TODO
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                int sum = 0;
+                for (int x = j; x < j+i && x < nums.length; x++) {
+                    sum += nums[x];
+                }
+                if (sum >= s) {
+                    return i;
+                }
+            }
+        }
         return 0;
     }
 
     public static void main(String[] args) {
         Problem209 prob = new Problem209();
+//        System.out.println(prob.minSubArrayLen(7, new int[]{2, 3, 1, 2, 4, 3}));
+//        System.out.println(prob.minSubArrayLen(3, new int[]{2, 3, 1, 2, 4, 5}));
+        System.out.println(prob.minSubArrayLen(7, new int[]{2, 3, 1, 2, 4, 5}));
     }
 }
