3fab6d6e2452a3a22615ebbcb9fbb368c9ac5f9f
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-10-20 19:52:43 -0500

    Add a skeleton for problem 213

---
+++ src/main/java/leetcode/Problem213.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/house-robber-ii/
+ */
+public class Problem213 {
+    public int rob(int[] nums) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem213 prob = new Problem213();
+    }
+}
