4ca897ebf17b104e66e8e175d9ad1468dcb93bdb
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-10-21 21:29:41 -0500

    Add skeletons for problems 229 and 264

---
+++ src/main/java/leetcode/Problem229.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/majority-element-ii/
+ */
+public class Problem229 {
+    public List<Integer> majorityElement(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        // TODO
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Problem229 prob = new Problem229();
+    }
+}
---
+++ src/main/java/leetcode/Problem264.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/ugly-number-ii/
+ */
+public class Problem264 {
+    public int nthUglyNumber(int n) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem264 prob = new Problem264();
+    }
+}
