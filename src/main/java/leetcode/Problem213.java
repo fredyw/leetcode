d3638600723a2b650ed1ee3deea8ec819087c7eb
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-10-21 21:06:58 -0500

    Solve problem 213

--- src/main/java/leetcode/Problem213.java
+++ src/main/java/leetcode/Problem213.java
@@ -9,50 +9,65 @@ import java.util.Map;
 public class Problem213 {
     public int rob(int[] nums) {
         int max = 0;
-        if (nums.length <= 3) {
-            for (int num : nums) {
-                max = Math.max(max, num);
-            }
-            return max;
+        if (nums.length == 1) {
+            return nums[nums.length-1];
         }
-        Map<Integer, Integer> memo = new HashMap<>();
-        int a = rob(nums, 0, 0, memo);
-        int b = rob(nums, 1, 1, memo);
-        int lastVal = nums[nums.length-1];
-        if (nums.length-3 % 2 == 0 || nums.length-3 % 3 == 0) {
-            max = Math.max(a+lastVal, b);
-        } else {
-            max = Math.max(a, b+lastVal);
+        for (int i = 0; i < nums.length-1; i++) {
+            Map<Integer, Value> memo = new HashMap<>();
+            Value a = rob(nums, i, memo);
+            Value b = rob(nums, i+1, memo);
+            int tmpMax = Math.max(a.value0, b.value1);
+            max = Math.max(max, tmpMax);
         }
         return max;
     }
 
-    private int rob(int[] nums, int begIdx, int idx, Map<Integer, Integer> memo) {
+    private static class Value {
+        int value0;
+        int value1;
+    }
+
+    private Value rob(int[] nums, int idx, Map<Integer, Value> memo) {
         if (memo.containsKey(idx)) {
             return memo.get(idx);
         }
-        if (idx >= nums.length-1) {
-            return 0;
+        if (idx == nums.length-1) {
+            Value value = new Value();
+            value.value0 = 0;
+            value.value1 = nums[idx];
+            return value;
         }
-        int a = rob(nums, begIdx, idx+2, memo);
-        int b = rob(nums, begIdx, idx+3, memo);
+        if (idx >= nums.length) {
+            return new Value();
+        }
+        Value a = rob(nums, idx+2, memo);
+        Value b = rob(nums, idx+3, memo);
         int val = nums[idx];
-        int result = Math.max(a+val, b+val);
-        memo.put(idx, result);
-        return result;
+        int max0 = Math.max(a.value0 + val, b.value0 + val);
+        int max1 = Math.max(a.value1 + val, b.value1 + val);
+        Value value = new Value();
+        value.value0 = max0;
+        value.value1 = max1;
+        memo.put(idx, value);
+        return value;
     }
 
     public static void main(String[] args) {
         Problem213 prob = new Problem213();
-//        System.out.println(prob.rob(new int[]{2, 1})); // 2
-//        System.out.println(prob.rob(new int[]{1, 2})); // 2
-//        System.out.println(prob.rob(new int[]{1, 1, 1})); // 1
-//        System.out.println(prob.rob(new int[]{1})); // 1
-//        System.out.println(prob.rob(new int[]{6, 6, 4, 8, 4, 3, 3, 10})); // 27
-//        System.out.println(prob.rob(new int[]{2, 1, 1, 2})); // 3
-//        System.out.println(prob.rob(new int[]{2, 1, 1, 1})); // 3
-//        System.out.println(prob.rob(new int[]{2, 1, 1, 4})); // 5
-//        System.out.println(prob.rob(new int[]{1, 1, 3, 6, 7, 10, 7, 1, 8, 5, 9, 1, 6, 4, 3})); // 43
+        System.out.println(prob.rob(new int[]{})); // 0
+        System.out.println(prob.rob(new int[]{2, 1})); // 2
+        System.out.println(prob.rob(new int[]{1, 2})); // 2
+        System.out.println(prob.rob(new int[]{1, 1, 1})); // 1
+        System.out.println(prob.rob(new int[]{1})); // 1
+        System.out.println(prob.rob(new int[]{6, 6, 4, 8, 4, 3, 3, 10})); // 27
+        System.out.println(prob.rob(new int[]{2, 1, 1, 2})); // 3
+        System.out.println(prob.rob(new int[]{2, 1, 1, 1})); // 3
+        System.out.println(prob.rob(new int[]{2, 1, 1, 4})); // 5
+        System.out.println(prob.rob(new int[]{1, 1, 3, 6, 7, 10, 7, 1, 8, 5, 9, 1, 6, 4, 3})); // 43
         System.out.println(prob.rob(new int[]{1, 3, 1, 3, 100})); // 103
+        System.out.println(prob.rob(new int[]{1, 2, 3, 4, 5, 1, 2, 3, 4, 5})); // 16
+        System.out.println(prob.rob(new int[]{1, 2, 3, 4, 6, 1, 2, 3, 4, 5})); // 17
+        System.out.println(prob.rob(new int[]{1, 7, 3})); // 7
+        System.out.println(prob.rob(new int[]{2, 7, 7, 7, 4})); // 14
     }
 }
