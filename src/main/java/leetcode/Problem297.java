71ae1febd780c5a970450ac651b913930534885d
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-10-26 14:16:38 -0500

    Solve problem 297

--- src/main/java/leetcode/Problem297.java
+++ src/main/java/leetcode/Problem297.java
@@ -1,5 +1,8 @@
 package leetcode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * https://leetcode.com/problems/serialize-and-deserialize-binary-tree/
  */
@@ -12,25 +15,82 @@ public class Problem297 {
         TreeNode(int x) {
             val = x;
         }
+
+        @Override
+        public String toString() {
+            return Integer.toString(val);
+        }
     }
 
     public static class Codec {
         // Encodes a tree to a single string.
         public String serialize(TreeNode root) {
-            return "";
+            StringBuilder result = new StringBuilder();
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.add(root);
+            while (!queue.isEmpty()) {
+                TreeNode node = queue.remove();
+                if (node == null) {
+                    result.append("null,");
+                    continue;
+                } else {
+                    result.append(node.val + ",");
+                }
+                if (node.left != null) {
+                    queue.add(node.left);
+                } else {
+                    queue.add(null);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                } else {
+                    queue.add(node.right);
+                }
+            }
+            return result.toString();
         }
 
         // Decodes your encoded data to tree.
         public TreeNode deserialize(String data) {
-            return null;
+            String[] nodes = data.split(",");
+            int i = 0;
+            if (nodes[i].equals("null")) {
+                return null;
+            }
+            TreeNode root = new TreeNode(Integer.parseInt(nodes[i]));
+            LinkedList<TreeNode> parents = new LinkedList<>();
+            parents.add(root);
+            i++;
+            out:
+            while (!parents.isEmpty()) {
+                LinkedList<TreeNode> newParents = new LinkedList<>();
+                for (TreeNode parent : parents) {
+                    if (i >= nodes.length) {
+                        break out;
+                    }
+                    // left
+                    if (!nodes[i].equals("null")) {
+                        int val = Integer.parseInt(nodes[i]);
+                        TreeNode node = new TreeNode(val);
+                        parent.left = node;
+                        newParents.add(node);
+                    }
+                    i++;
+                    if (i >= nodes.length) {
+                        break out;
+                    }
+                    // right
+                    if (!nodes[i].equals("null")) {
+                        int val = Integer.parseInt(nodes[i]);
+                        TreeNode node = new TreeNode(val);
+                        parent.right = node;
+                        newParents.add(node);
+                    }
+                    i++;
+                }
+                parents = newParents;
+            }
+            return root;
         }
-    }
-
-    public static void main(String[] args) {
-        Codec codec = new Codec();
-        TreeNode root = new TreeNode(1);
-        String serializedTree = codec.serialize(root);
-        System.out.println(serializedTree);
-        codec.deserialize(serializedTree);
     }
 }
