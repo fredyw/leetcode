e740d5767901a5f415f84f5b6cbfe0ea2cd3b6e3
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-10-30 14:59:10 -0500

    Add skeleton for problem 222

---
+++ src/main/java/leetcode/Problem222.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/count-complete-tree-nodes/
+ */
+public class Problem222 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public int countNodes(TreeNode root) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem222 prob = new Problem222();
+    }
+}
