0ad758ebd27c4f5a90374097983ed55cf19bfbb1
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-11-01 16:00:44 -0600

    Create a skeleton for problem 299

--- src/main/java/leetcode/Problem299.java
+++ src/main/java/leetcode/Problem299.java
@@ -4,4 +4,12 @@ package leetcode;
  * https://leetcode.com/problems/bulls-and-cows/
  */
 public class Problem299 {
+    public String getHint(String secret, String guess) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem299 prob = new Problem299();
+    }
 }
