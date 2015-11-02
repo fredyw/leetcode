3e4d578618df9fc38500b0d49a8ee14b6adc1b74
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-11-01 20:14:01 -0600

    Create a skeleton for problem 187

---
+++ src/main/java/leetcode/Problem187.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/repeated-dna-sequences/
+ */
+public class Problem187 {
+    public List<String> findRepeatedDnaSequences(String s) {
+        List<String> result = new ArrayList<>();
+        // TODO
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Problem187 prob = new Problem187();
+    }
+}
