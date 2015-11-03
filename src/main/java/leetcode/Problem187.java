ab1beb8f71cfc91b5f3c08685921510daa525f00
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-11-03 16:12:13 -0600

    Solve problem 187

--- src/main/java/leetcode/Problem187.java
+++ src/main/java/leetcode/Problem187.java
@@ -1,22 +1,28 @@
 package leetcode;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * https://leetcode.com/problems/repeated-dna-sequences/
  */
 public class Problem187 {
     public List<String> findRepeatedDnaSequences(String s) {
-        List<String> result = new ArrayList<>();
-        // TODO
-        return result;
-    }
-
-    public static void main(String[] args) {
-        Problem187 prob = new Problem187();
-//                                                             CCCCCAAAAA CCCCCAAAAA
-//                                                       AAAAACCCCC AAAAACCCCC
-        System.out.println(prob.findRepeatedDnaSequences("AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"));
+        Set<String> set = new HashSet<>();
+        Set<String> result = new HashSet<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (i + 10 > s.length()) {
+                break;
+            }
+            String str = s.substring(i, i + 10);
+            if (set.contains(str)) {
+                result.add(str);
+            } else {
+                set.add(str);
+            }
+        }
+        return new ArrayList<>(result);
     }
 }
