7e042fdfb9d458438af2fffaedbfc401bad98f4a
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-11-04 16:40:40 -0600

    Create a skeleton for problem 220

---
+++ src/main/java/leetcode/Problem220.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/contains-duplicate-iii/
+ */
+public class Problem220 {
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem220 prob = new Problem220();
+
+    }
+}
