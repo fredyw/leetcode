a5c3e8393028369ed24dcedfa78ab7f68366f11d
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-11-04 20:00:21 -0600

    Create a skeleton for problem 301

---
+++ src/main/java/leetcode/Problem301.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/remove-invalid-parentheses/
+ */
+public class Problem301 {
+    public List<String> removeInvalidParentheses(String s) {
+        List<String> result = new ArrayList<>();
+        // TODO
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Problem301 prob = new Problem301();
+    }
+}
