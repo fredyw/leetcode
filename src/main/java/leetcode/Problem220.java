efb09e2d3364edb3694651d57730eec7171e73a2
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-11-08 12:34:09 -0600

    Initial implementation for problem 220

--- src/main/java/leetcode/Problem220.java
+++ src/main/java/leetcode/Problem220.java
@@ -5,12 +5,23 @@ package leetcode;
  */
 public class Problem220 {
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-        // TODO
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j <= (i + k) && j < nums.length; j++) {
+                int diff = Math.abs(nums[j] - nums[i]);
+                if (diff <= t) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
     public static void main(String[] args) {
         Problem220 prob = new Problem220();
-
+        System.out.println(prob.containsNearbyAlmostDuplicate(new int[]{0}, 0, 0)); // false
+        System.out.println(prob.containsNearbyAlmostDuplicate(new int[]{1}, 1, 1)); // false
+        System.out.println(prob.containsNearbyAlmostDuplicate(new int[]{-1, -1}, 1, 0)); // true
+        System.out.println(prob.containsNearbyAlmostDuplicate(new int[]{2, 1}, 1, 1)); // true
+        System.out.println(prob.containsNearbyAlmostDuplicate(new int[]{4, 2}, 2, 1)); // false
     }
 }
