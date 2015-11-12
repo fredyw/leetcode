8982fb04ad31f5eefb9d50a85b7e647bb7960964
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-11-12 14:20:14 -0600

    Solve problem 303

--- src/main/java/leetcode/Problem303.java
+++ src/main/java/leetcode/Problem303.java
@@ -1,20 +1,43 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * https://leetcode.com/problems/range-sum-query-immutable/
  */
 public class Problem303 {
     public static class NumArray {
-        public NumArray(int[] nums) {
+        private final Map<Integer, Integer> map = new HashMap<>();
+        private final int[] nums;
 
+        public NumArray(int[] nums) {
+            this.nums = nums;
+            if (nums.length > 0) {
+                int sum = nums[0];
+                for (int i = 1; i < nums.length; i++) {
+                    sum += nums[i];
+                    map.put(i, sum);
+                }
+            }
         }
 
         public int sumRange(int i, int j) {
-            return 0;
+            if (i == j) {
+                return nums[i];
+            }
+            Integer a = map.get(i);
+            if (a == null) {
+                a = 0;
+            }
+            if (i > 0) {
+                a -= nums[i];
+            }
+            Integer b = map.get(j);
+            if (b == null) {
+                b = 0;
+            }
+            return b - a;
         }
-    }
-
-    public static void main(String[] args) {
-
     }
 }
