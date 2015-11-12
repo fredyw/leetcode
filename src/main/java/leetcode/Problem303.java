1c5d3e6746f661ee0f615cfed843392b324dd35a
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-11-12 13:46:45 -0600

    Add skeleton for problem 303

---
+++ src/main/java/leetcode/Problem303.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/range-sum-query-immutable/
+ */
+public class Problem303 {
+    public static class NumArray {
+        public NumArray(int[] nums) {
+
+        }
+
+        public int sumRange(int i, int j) {
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
