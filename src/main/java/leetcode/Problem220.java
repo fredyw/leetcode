f78e908248fbd071a80ecba2bc863089db2b51fd
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-11-13 02:20:49 -0600

    Solve problem 220

--- src/main/java/leetcode/Problem220.java
+++ src/main/java/leetcode/Problem220.java
@@ -7,14 +7,6 @@ import java.util.TreeSet;
  */
 public class Problem220 {
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-//        for (int i = 0; i < nums.length; i++) {
-//            for (int j = i + 1; j <= (i + k) && j < nums.length; j++) {
-//                int diff = Math.abs(nums[j] - nums[i]);
-//                if (diff <= t) {
-//                    return true;
-//                }
-//            }
-//        }
         if (k <= 0 || t < 0 || nums.length <= 1) {
             return false;
         }
@@ -29,7 +21,6 @@ public class Problem220 {
         if (set.size() != size) {
             return true;
         }
-        System.out.println(set);
         int a = set.first();
         int b = 0;
         if (set.size() > 1) {
@@ -46,7 +37,6 @@ public class Problem220 {
             if (set.size() != size) {
                 return true;
             }
-            System.out.println(set);
             a = set.first();
             b = 0;
             if (set.size() > 1) {
@@ -68,23 +58,5 @@ public class Problem220 {
             a = b;
         }
         return false;
-    }
-
-    public static void main(String[] args) {
-        Problem220 prob = new Problem220();
-//        System.out.println(prob.containsNearbyAlmostDuplicate(new int[]{0}, 0, 0)); // false
-//        System.out.println(prob.containsNearbyAlmostDuplicate(new int[]{1}, 1, 1)); // false
-//        System.out.println(prob.containsNearbyAlmostDuplicate(new int[]{-1, -1}, 1, 0)); // true
-//        System.out.println(prob.containsNearbyAlmostDuplicate(new int[]{2, 1}, 1, 1)); // true
-//        System.out.println(prob.containsNearbyAlmostDuplicate(new int[]{4, 2}, 2, 1)); // false
-//        System.out.println(prob.containsNearbyAlmostDuplicate(new int[]{6, 2, 4, 8, 1, 3, 2}, 3, 1)); // true
-//        System.out.println(prob.containsNearbyAlmostDuplicate(new int[]{6, 2, 4, 8, 1, 3, 2}, 2, 1)); // true
-//        System.out.println(prob.containsNearbyAlmostDuplicate(new int[]{6, 2, 4, 8, 1, 3, 2}, 2, 1)); // true
-//        System.out.println(prob.containsNearbyAlmostDuplicate(new int[]{2, 6, 4, 2}, 3, 1)); // true
-//        System.out.println(prob.containsNearbyAlmostDuplicate(new int[]{2, 6, 4, 2}, 2, 1)); // false
-//        System.out.println(prob.containsNearbyAlmostDuplicate(new int[]{2, 2}, 3, 0)); // true
-//        System.out.println(prob.containsNearbyAlmostDuplicate(new int[]{3, 6, 0, 4}, 2, 2)); // true
-//        System.out.println(prob.containsNearbyAlmostDuplicate(new int[]{2, 2}, 3, 0)); // true
-//        System.out.println(prob.containsNearbyAlmostDuplicate(new int[]{-1, 2147483647}, 1, 2147483647)); // false
     }
 }
