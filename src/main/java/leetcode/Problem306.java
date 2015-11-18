6aed3ff617548baaffaa014e894991f98cdf1bb7
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-11-18 00:16:06 -0600

    Create a skeleton for problem 306

---
+++ src/main/java/leetcode/Problem306.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/additive-number/
+ */
+public class Problem306 {
+    public boolean isAdditiveNumber(String num) {
+        // TODO:
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem306 prob = new Problem306();
+        System.out.println(prob.isAdditiveNumber("112358")); // true
+        System.out.println(prob.isAdditiveNumber("199100199")); // true
+    }
+}
