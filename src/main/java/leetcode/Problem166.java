7eefa8c3574488cba8dafcad5c1cf4d4e5a3cff3
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-11-19 00:30:52 -0600

    Create a skeleton for problem 166

---
+++ src/main/java/leetcode/Problem166.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/fraction-to-recurring-decimal/
+ */
+public class Problem166 {
+    public String fractionToDecimal(int numerator, int denominator) {
+        // TODO
+        return "";
+    }
+
+    public static void main(String[] args) {
+        Problem166 prob = new Problem166();
+        System.out.println(prob.fractionToDecimal(1, 2)); // 0.5
+        System.out.println(prob.fractionToDecimal(2, 1)); // 2
+        System.out.println(prob.fractionToDecimal(2, 3)); // 0.(6)
+    }
+}
