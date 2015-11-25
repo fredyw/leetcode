53c9c8549fd67d4457d0888a64fec3ba0b390b76
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-11-25 10:14:43 -0600

    Add a skeleton for problem 309

---
+++ src/main/java/leetcode/Problem309.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/
+ */
+public class Problem309 {
+    public int maxProfit(int[] prices) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem309 prob = new Problem309();
+        System.out.println(prob.maxProfit(new int[]{1, 2, 3, 0, 2})); // 3
+    }
+}
