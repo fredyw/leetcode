a15dbc8d5ce5939c43bde357ac94ae26c57fccaf
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-11-26 14:26:01 -0600

    Create a skeleton for problem 310

---
+++ src/main/java/leetcode/Problem310.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/minimum-height-trees/
+ */
+public class Problem310 {
+    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+        List<Integer> result = new ArrayList<>();
+        // TODO:
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Problem310 prob = new Problem310();
+    }
+}
