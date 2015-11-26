307df22aaee24a9fa72ad3ac803ff0e78ec3b447
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-11-26 14:56:58 -0600

    Update problem 309

--- src/main/java/leetcode/Problem309.java
+++ src/main/java/leetcode/Problem309.java
@@ -12,5 +12,9 @@ public class Problem309 {
     public static void main(String[] args) {
         Problem309 prob = new Problem309();
         System.out.println(prob.maxProfit(new int[]{1, 2, 3, 0, 2})); // 3
+        System.out.println(prob.maxProfit(new int[]{1, 2, 3, 0, 2, 4})); // 5
+        System.out.println(prob.maxProfit(new int[]{3, 2, 1})); // 0
+        System.out.println(prob.maxProfit(new int[]{1, 2, 3})); // 2
+        System.out.println(prob.maxProfit(new int[]{3, 0, 0, 0, 5})); // 5
     }
 }
