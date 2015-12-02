08d09fd8219cd1cd49f1b6a7bc5750ae34d93964
Fredy Wijaya <fredy.wijaya@gmail.com>
2015-12-02 01:55:39 -0600

    Create a skeleton for problem 312

---
+++ src/main/java/leetcode/Problem312.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/burst-balloons/
+ */
+public class Problem312 {
+    public int maxCoins(int[] nums) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem312 prob = new Problem312();
+        System.out.println(prob.maxCoins(new int[]{3, 1, 5, 8})); // 167
+    }
+}
