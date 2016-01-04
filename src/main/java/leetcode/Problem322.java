f97fc02655542103554b9478f920b5a8374893f2
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-01-04 13:25:56 -0600

    Add skeleton for problem 322

---
+++ src/main/java/leetcode/Problem322.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/coin-change/
+ */
+public class Problem322 {
+    public int coinChange(int[] coins, int amount) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
