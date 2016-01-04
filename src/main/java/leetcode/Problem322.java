61e0b6c1a623625a2d6608ecf2b9d34b7ccf6022
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-01-04 16:25:17 -0600

    Solve problem 322

--- src/main/java/leetcode/Problem322.java
+++ src/main/java/leetcode/Problem322.java
@@ -1,15 +1,41 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * https://leetcode.com/problems/coin-change/
  */
 public class Problem322 {
     public int coinChange(int[] coins, int amount) {
-        // TODO
-        return 0;
+        Map<Integer, Integer> memo = new HashMap<>();
+        int result = coinChange(coins, amount, memo);
+        if (result == Integer.MAX_VALUE) {
+            return -1;
+        }
+        return result - 1;
     }
 
-    public static void main(String[] args) {
-
+    private int coinChange(int[] coins, int amount, Map<Integer, Integer> memo) {
+        if (amount < 0) {
+            return Integer.MAX_VALUE;
+        } else if (amount == 0) {
+            return 1;
+        }
+        if (memo.containsKey(amount)) {
+            return memo.get(amount);
+        }
+        int result = Integer.MAX_VALUE;
+        for (int coin : coins) {
+            int tmp = coinChange(coins, amount - coin, memo);
+            if (tmp != Integer.MAX_VALUE) {
+                result = Math.min(result, tmp);
+            }
+        }
+        if (result != Integer.MAX_VALUE) {
+            result++;
+        }
+        memo.put(amount, result);
+        return result;
     }
 }
