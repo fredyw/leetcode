31b3c77a9bb42f7b99cd45ee41b54015189c0e19
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-01-07 11:16:00 -0600

    Create a skeleton for problem 313

---
+++ src/main/java/leetcode/Problem313.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/super-ugly-number/
+ */
+public class Problem313 {
+    public int nthSuperUglyNumber(int n, int[] primes) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem313 prob = new Problem313();
+    }
+}
