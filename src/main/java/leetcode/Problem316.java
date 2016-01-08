487bd5429d7b199f36a8620be2900c94c149cbda
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-01-07 22:11:54 -0600

    Create a skeleton for problem 316

---
+++ src/main/java/leetcode/Problem316.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/remove-duplicate-letters/
+ */
+public class Problem316 {
+    public String removeDuplicateLetters(String s) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem316 prob = new Problem316();
+        System.out.println(prob.removeDuplicateLetters("bcabc")); // abc
+        System.out.println(prob.removeDuplicateLetters("cbacdcbc")); // acdb
+    }
+}
