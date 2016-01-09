06784533680000036a37a2f8ddf1e46abc77330b
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-01-08 23:22:31 -0600

    Update problem 316

--- src/main/java/leetcode/Problem316.java
+++ src/main/java/leetcode/Problem316.java
@@ -1,26 +1,11 @@
 package leetcode;
 
-import java.util.Arrays;
-
 /**
  * https://leetcode.com/problems/remove-duplicate-letters/
  */
 public class Problem316 {
     public String removeDuplicateLetters(String s) {
-        char[] chars = s.toCharArray();
-        Arrays.sort(chars);
-        int set = 0;
-        int dup = 0;
-        for (char c : chars) {
-            int idx = 1 << (c - 97);
-            if ((idx & set) > 0) {
-                dup |= idx;
-            }
-            set |= idx;
-        }
-        System.out.println(Integer.toBinaryString(dup));
         StringBuilder sb = new StringBuilder();
-        // TODO:
         return sb.toString();
     }
 
