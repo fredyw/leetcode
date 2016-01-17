a8e6fe73d0f969bbaca7d812f5ef93e1c9fd05f3
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-01-17 02:45:59 -0600

    Create a skeleton for problem 328

---
+++ src/main/java/leetcode/Problem328.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/odd-even-linked-list/
+ */
+public class Problem328 {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode oddEvenList(ListNode head) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem328 prob = new Problem328();
+    }
+}
