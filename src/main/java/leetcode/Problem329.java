398339e30c9c07fe888681179a9a7fab8df8d97e
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-01-22 00:26:34 -0600

    Solve problem 329

--- src/main/java/leetcode/Problem329.java
+++ src/main/java/leetcode/Problem329.java
@@ -1,25 +1,51 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * https://leetcode.com/problems/longest-increasing-path-in-a-matrix/
  */
 public class Problem329 {
     public int longestIncreasingPath(int[][] matrix) {
-        // TODO
-        return 0;
+        int max = 0;
+        Map<String, Integer> memo = new HashMap<>();
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                int val = longestIncreasingPath(matrix, matrix.length, matrix[row].length,
+                    row, col, Integer.MIN_VALUE, memo);
+                max = Math.max(max, val);
+            }
+        }
+        return max;
     }
 
-    public static void main(String[] args) {
-        Problem329 prob = new Problem329();
-        System.out.println(prob.longestIncreasingPath(new int[][]{
-            {9, 9, 4},
-            {6, 6, 8},
-            {2, 1, 1}
-        })); // 4
-        System.out.println(prob.longestIncreasingPath(new int[][]{
-            {3, 4, 5},
-            {3, 2, 6},
-            {2, 2, 1}
-        })); // 4
+    public int longestIncreasingPath(int[][] matrix, int maxRow, int maxCol,
+                                     int row, int col, int prevValue, Map<String, Integer> memo) {
+        if (0 > row || row >= maxRow || 0 > col || col >= maxCol
+            || prevValue >= matrix[row][col]) {
+            return 0;
+        }
+        String key = row + "|" + col;
+        if (memo.containsKey(key)) {
+            return memo.get(key);
+        }
+        int value = matrix[row][col];
+        // top
+        int a = longestIncreasingPath(matrix, maxRow, maxCol, row - 1, col, value, memo) + 1;
+        // down
+        int b = longestIncreasingPath(matrix, maxRow, maxCol, row + 1, col, value, memo) + 1;
+        // left
+        int c = longestIncreasingPath(matrix, maxRow, maxCol, row, col - 1, value, memo) + 1;
+        // right
+        int d = longestIncreasingPath(matrix, maxRow, maxCol, row, col + 1, value, memo) + 1;
+
+        int max = 0;
+        max = Math.max(max, a);
+        max = Math.max(max, b);
+        max = Math.max(max, c);
+        max = Math.max(max, d);
+        memo.put(key, max);
+        return max;
     }
 }
