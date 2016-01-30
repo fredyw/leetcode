124bd5a56fb13cc16dc5476d8d8ecd31f05e0be4
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-01-30 01:37:14 -0600

    Initial attempt for problem 324

--- src/main/java/leetcode/Problem324.java
+++ src/main/java/leetcode/Problem324.java
@@ -7,7 +7,51 @@ import java.util.Arrays;
  */
 public class Problem324 {
     public void wiggleSort(int[] nums) {
+        Arrays.sort(nums);
+        boolean lessThan = true;
+        int i = 0;
+        int j = 1;
+        while (j < nums.length) {
+            if (lessThan) {
+                if (nums[i] < nums[j]) {
+                    i++;
+                    swap(nums, i, j);
+                    j = i + 1;
+                    lessThan = false;
+                } else {
+                    j++;
+                }
+            } else {
+                if (nums[i] > nums[j]) {
+                    i++;
+                    swap(nums, i, j);
+                    j = i + 1;
+                    lessThan = true;
+                } else {
+                    j++;
+                }
+            }
+        }
+        if (lessThan) {
+            if (nums[i] > nums[j - 1]) {
+                swap(nums, i, j - 1);
+            }
+        } else {
+            if (nums[i] < nums[j - 1]) {
+                swap(nums, i, j - 1);
+            }
+        }
+        System.out.println("less than: " + lessThan);
+        System.out.println(nums[i] + " -- " + nums[j-1]);
         // TODO:
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        if (i < nums.length && j < nums.length) {
+            int tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
+        }
     }
 
     public static void main(String[] args) {
