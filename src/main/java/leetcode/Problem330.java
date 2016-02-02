44b6b54fc3e0f0c790389bb7c16cefde6022f864
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-02-01 23:58:10 -0600

    Create a skeleton for problem 330

---
+++ src/main/java/leetcode/Problem330.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/patching-array/
+ */
+public class Problem330 {
+    public int minPatches(int[] nums, int n) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem330 prob = new Problem330();
+        System.out.println(prob.minPatches(new int[]{1, 3}, 6)); // 1
+        prob.minPatches(new int[]{1, 5, 10}, 20); // 2
+        prob.minPatches(new int[]{1, 2, 2}, 5); // 0
+    }
+}
