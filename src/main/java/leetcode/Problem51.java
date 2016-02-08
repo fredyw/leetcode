2b0819ff2f5f59c3c65c843f2c0560b954ab67f6
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-02-07 23:57:20 -0600

    Solve problem 51

--- src/main/java/leetcode/Problem51.java
+++ src/main/java/leetcode/Problem51.java
@@ -9,15 +9,55 @@ import java.util.List;
 public class Problem51 {
     public List<List<String>> solveNQueens(int n) {
         List<List<String>> result = new ArrayList<>();
-        // TODO
+        char[][] board = new char[n][n];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                board[i][j] = '.';
+            }
+        }
+        nQueens(board, 0, result);
         return result;
     }
 
-    public static void main(String[] args) {
-        Problem51 prob = new Problem51();
-        List<List<String>> result = prob.solveNQueens(4);
-        for (List<String> row : result) {
-            System.out.println(row);
+    private void nQueens(char[][] board, int row, List<List<String>> result) {
+        if (row == board.length) {
+            List<String> solution = new ArrayList<>();
+            for (char[] chars : board) {
+                solution.add(new String(chars));
+            }
+            result.add(solution);
+            return;
         }
+        for (int col = 0; col < board[row].length; col++) {
+            if (!isPossible(board, row, col)) {
+                continue;
+            }
+            board[row][col] = 'Q';
+            nQueens(board, row + 1, result);
+            // reset everything
+            board[row][col] = '.';
+        }
+    }
+
+    private boolean isPossible(char[][] board, int row, int col) {
+        // up row
+        for (int i = row; i >= 0; i--) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+        // left diagonal
+        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        // right diagonal
+        for (int i = row, j = col; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        return true;
     }
 }
