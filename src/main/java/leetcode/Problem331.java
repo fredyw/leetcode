d57e98f70d306b8a3681c8e7672e50e61d030049
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-02-16 23:25:18 -0600

    Update problem 331

--- src/main/java/leetcode/Problem331.java
+++ src/main/java/leetcode/Problem331.java
@@ -8,40 +8,19 @@ w * https://leetcode.com/problems/verify-preorder-serialization-of-a-binary-tree
 public class Problem331 {
     public boolean isValidSerialization(String preorder) {
         String[] nodes = preorder.split(",");
-        Stack<Node> stack = new Stack<>();
-        Direction direction = Direction.LEFT;
+        Stack<String> stack = new Stack<>();
         for (int i = 0; i < nodes.length; i++) {
             if (nodes[i].equals("#")) {
-                if (direction == Direction.LEFT) {
-                    direction = Direction.RIGHT;
-                } else {
-                    direction = Direction.LEFT;
-                }
                 if (stack.size() == 0 && i < nodes.length - 1) {
                     return false;
                 }
                 if (i < nodes.length - 1) {
-                    stack.pop().direction = direction;
+                    stack.pop();
                 }
                 continue;
             }
-            Node node = new Node(nodes[i], direction);
-            stack.add(node);
+            stack.add(nodes[i]);
         }
         return stack.isEmpty();
-    }
-
-    private enum Direction {
-        LEFT, RIGHT
-    }
-
-    private static class Node {
-        private String node;
-        private Direction direction;
-
-        public Node(String node, Direction direction) {
-            this.node = node;
-            this.direction = direction;
-        }
     }
 }
