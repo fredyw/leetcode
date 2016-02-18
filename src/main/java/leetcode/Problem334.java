5efb7a29c8eaaaa7c617628af08f18704f02eab6
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-02-18 02:29:11 -0600

    Update problem 334

--- src/main/java/leetcode/Problem334.java
+++ src/main/java/leetcode/Problem334.java
@@ -18,21 +18,4 @@ public class Problem334 {
         }
         return false;
     }
-
-    public static void main(String[] args) {
-        Problem334 prob = new Problem334();
-//        System.out.println(prob.increasingTriplet(new int[]{1, 2, 3, 4, 5})); // true
-//        System.out.println(prob.increasingTriplet(new int[]{5, 4, 3, 2, 1})); // false
-//        System.out.println(prob.increasingTriplet(new int[]{1, 9, 8, 7, 6, 15, 3, 2, 4})); // true
-//        System.out.println(prob.increasingTriplet(new int[]{10, 9, 8, 7, 6, 15, 1, 3, 2, 4})); // true
-//        System.out.println(prob.increasingTriplet(new int[]{5, 1, 5, 5, 2, 5, 4})); // true
-//        System.out.println(prob.increasingTriplet(new int[]{0, 4, 2, 1, 0, -1, -3})); // false
-//        System.out.println(prob.increasingTriplet(new int[]{2, 1, 5, 0, 3})); // false
-//        System.out.println(prob.increasingTriplet(new int[]{1, 2, 1, 2, 1, 2, 1, 2, 1, 2})); // false;
-//        System.out.println(prob.increasingTriplet(new int[]{1, 0, 0, 0, 10, 0, 1000})); // true
-//        System.out.println(prob.increasingTriplet(new int[]{1, 8, 4, 0, 5})); // true
-//        System.out.println(prob.increasingTriplet(new int[]{1, 4, 0, 1, 2})); // true
-//        System.out.println(prob.increasingTriplet(new int[]{1, 4, 0, 8, 5})); // true
-//        System.out.println(prob.increasingTriplet(new int[]{1, 2, -10, -8, -7})); // true
-    }
 }
