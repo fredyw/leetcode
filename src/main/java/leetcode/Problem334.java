6e7e3cfe50dbea3ce3252d4f16ae89ba41dd2d5a
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-02-18 02:28:26 -0600

    Solve problem 334

--- src/main/java/leetcode/Problem334.java
+++ src/main/java/leetcode/Problem334.java
@@ -5,29 +5,14 @@ package leetcode;
  */
 public class Problem334 {
     public boolean increasingTriplet(int[] nums) {
-        int n = 3;
-        if (nums.length < n) {
-            return false;
-        }
-        int count = 0;
-        int prev = nums[0];
-        int lower = Integer.MIN_VALUE;
-        for (int i = 1; i < nums.length; i++) {
-//            System.out.println("i=" + i + ", val=" + nums[i]);
-//            System.out.println(" - " + prev + " < " + nums[i]);
-            if (prev < nums[i]) {
-                lower = prev;
-                count++;
-//                System.out.println(" - increment counter: " + count);
-//                System.out.println(" - prev: " + prev);
-            } else if (prev > nums[i]) {
-                if (lower >= nums[i]) {
-                    count--;
-                }
-            }
-//            System.out.println(" - setting prev to: " + nums[i]);
-            prev = nums[i];
-            if (count == n - 1) {
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] <= min) {
+                min = nums[i];
+            } else if (nums[i] <= max) {
+                max = nums[i];
+            } else {
                 return true;
             }
         }
@@ -44,6 +29,10 @@ public class Problem334 {
 //        System.out.println(prob.increasingTriplet(new int[]{0, 4, 2, 1, 0, -1, -3})); // false
 //        System.out.println(prob.increasingTriplet(new int[]{2, 1, 5, 0, 3})); // false
 //        System.out.println(prob.increasingTriplet(new int[]{1, 2, 1, 2, 1, 2, 1, 2, 1, 2})); // false;
-        System.out.println(prob.increasingTriplet(new int[]{1, 0, 0, 0, 10, 0, 1000})); // true
+//        System.out.println(prob.increasingTriplet(new int[]{1, 0, 0, 0, 10, 0, 1000})); // true
+//        System.out.println(prob.increasingTriplet(new int[]{1, 8, 4, 0, 5})); // true
+//        System.out.println(prob.increasingTriplet(new int[]{1, 4, 0, 1, 2})); // true
+//        System.out.println(prob.increasingTriplet(new int[]{1, 4, 0, 8, 5})); // true
+//        System.out.println(prob.increasingTriplet(new int[]{1, 2, -10, -8, -7})); // true
     }
 }
