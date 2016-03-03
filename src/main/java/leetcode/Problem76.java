d0199407371c8e2983161fb40ce6dad09701adc0
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-03-03 00:58:48 -0600

    Create a skeleton for problem 76

---
+++ src/main/java/leetcode/Problem76.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/minimum-window-substring/
+ */
+public class Problem76 {
+    public String minWindow(String s, String t) {
+        // TODO
+        return "";
+    }
+
+    public static void main(String[] args) {
+        Problem76 prob = new Problem76();
+        System.out.println(prob.minWindow("ADOBECODEBANC", "ABC")); // BANC
+    }
+}
