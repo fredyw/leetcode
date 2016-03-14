b8e165b7d4241cbe9f4d27332b43225d663a2339
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-03-14 01:21:11 -0500

    Update problem 337

--- src/main/java/leetcode/Problem337.java
+++ src/main/java/leetcode/Problem337.java
@@ -16,7 +16,23 @@ public class Problem337 {
 
     public int rob(TreeNode root) {
         // TODO
-        return 0;
+        int a = rob(root, false);
+        int b = rob(root, true);
+        System.out.println(a + " vs " + b);
+        return Math.max(a, b);
+    }
+
+    private int rob(TreeNode node, boolean skip) {
+        if (node == null) {
+            return 0;
+        }
+        int val = 0;
+        if (!skip) {
+            val = node.val;
+        }
+        int a = rob(node.left, !skip);
+        int b = rob(node.right, !skip);
+        return a + b + val;
     }
 
     public static void main(String[] args) {
@@ -36,5 +52,11 @@ public class Problem337 {
         root.right = new TreeNode(5);
         root.right.right = new TreeNode(1);
         System.out.println(prob.rob(root)); // 9
+
+        root = new TreeNode(4);
+        root.left = new TreeNode(1);
+        root.left.left = new TreeNode(2);
+        root.left.left.left = new TreeNode(3);
+        System.out.println(prob.rob(root)); // 7
     }
 }
