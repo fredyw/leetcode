d68b916f8b30605cae4629dbf3a3cb5cd49ccdb0
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-03-14 17:12:32 -0500

    Update problem 337

--- src/main/java/leetcode/Problem337.java
+++ src/main/java/leetcode/Problem337.java
@@ -1,5 +1,8 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * https://leetcode.com/problems/house-robber-iii/
  */
@@ -15,24 +18,36 @@ public class Problem337 {
     }
 
     public int rob(TreeNode root) {
-        // TODO
-        int a = rob(root, false);
-        int b = rob(root, true);
-        System.out.println(a + " vs " + b);
+        Map<Integer, Integer> memo = new HashMap<>();
+        int a = rob(root, 0, 0, 0, memo);
+        int b1 = rob(root.left, 0, 0, 0, memo);
+        int b2 = rob(root.right, 0, 0, 0, memo);
+        int b = b1 + b2;
         return Math.max(a, b);
     }
 
-    private int rob(TreeNode node, boolean skip) {
+    private int rob(TreeNode node, int idx, int step, int targetStep, Map<Integer, Integer> memo) {
         if (node == null) {
             return 0;
         }
         int val = 0;
-        if (!skip) {
+        int newStep = step;
+        if (step == targetStep) {
             val = node.val;
+            newStep = 0;
         }
-        int a = rob(node.left, !skip);
-        int b = rob(node.right, !skip);
-        return a + b + val;
+        int leftIdx = idx + 1;
+        int left1 = rob(node.left, leftIdx, newStep + 1, 2, memo);
+        int left2 = rob(node.left, leftIdx, newStep + 1, 3, memo);
+        int maxLeft = Math.max(left1, left2);
+
+        int rightIdx = idx + 2;
+        int right1 = rob(node.right, rightIdx, newStep + 1, 2, memo);
+        int right2 = rob(node.right, rightIdx, newStep + 1, 3, memo);
+        int maxRight = Math.max(right1, right2);
+
+        int max = maxLeft + maxRight;
+        return max + val;
     }
 
     public static void main(String[] args) {
@@ -58,5 +73,13 @@ public class Problem337 {
         root.left.left = new TreeNode(2);
         root.left.left.left = new TreeNode(3);
         System.out.println(prob.rob(root)); // 7
+
+        root = new TreeNode(3);
+        System.out.println(prob.rob(root)); // 3
+
+        root = new TreeNode(3);
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(3);
+        System.out.println(prob.rob(root)); // 4
     }
 }
