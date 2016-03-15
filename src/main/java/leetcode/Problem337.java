2b74e7ed325bd84baa644259e6374e0c650c1408
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-03-15 00:30:24 -0500

    Update problem 337

--- src/main/java/leetcode/Problem337.java
+++ src/main/java/leetcode/Problem337.java
@@ -29,36 +29,53 @@ public class Problem337 {
         if (root == null) {
             return 0;
         }
-        Map<String, Integer> memo = new HashMap<>();
-        int a = rob(root, false, memo);
-        int b = rob(root, true, memo);
+        Map<TreeNodeWrapper, Integer> memo = new HashMap<>();
+        int a = rob(root, false, 0, memo);
+        int b = rob(root, true, 0, memo);
 //        System.out.println("a: " + a + " vs b: " + b);
         return Math.max(a, b);
     }
 
-    private int rob(TreeNode node, boolean skipped, Map<String, Integer> memo) {
+    private static class TreeNodeWrapper {
+        private TreeNode node;
+        private boolean skipped;
+
+        public TreeNodeWrapper(TreeNode node, boolean skipped) {
+            this.node = node;
+            this.skipped = skipped;
+        }
+    }
+
+    private int rob(TreeNode node, boolean skipped, int idx, Map<TreeNodeWrapper, Integer> memo) {
         if (node == null) {
             return 0;
         }
-        int max;
+        TreeNodeWrapper tnw = new TreeNodeWrapper(node, skipped);
+        if (memo.containsKey(tnw)) {
+            return memo.get(tnw);
+        }
+        int max = 0;
+        int leftIdx = idx + 1;
+        int rightIdx = idx + 2;
         if (skipped) {
-            int left1 = rob(node.left, skipped, memo); // continue skipping
-            int left2 = rob(node.left, !skipped, memo); // start processing
+            int left1 = rob(node.left, skipped, leftIdx, memo); // continue skipping
+            int left2 = rob(node.left, !skipped, leftIdx, memo); // start processing
             int maxLeft = Math.max(left1, left2);
 
-            int right1 = rob(node.right, skipped, memo); // continue skipping
-            int right2 = rob(node.right, !skipped, memo); // start processing
+            int right1 = rob(node.right, skipped, rightIdx, memo); // continue skipping
+            int right2 = rob(node.right, !skipped, rightIdx, memo); // start processing
             int maxRight = Math.max(right1, right2);
 
             max = maxLeft + maxRight;
         } else { // not skipped
-            int maxLeft = rob(node.left, !skipped, memo); // start skipping
-            int maxRight = rob(node.right, !skipped, memo); // start skipping
+            int maxLeft = rob(node.left, !skipped, leftIdx, memo); // start skipping
+            int maxRight = rob(node.right, !skipped, rightIdx, memo); // start skipping
             max = maxLeft + maxRight;
         }
         if (!skipped) {
             max += node.val;
         }
+        memo.put(tnw, max);
         return max;
     }
 
