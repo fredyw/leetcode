90850978a00f94f9947da8cb04953bc03f0d89d8
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-03-20 00:51:20 -0500

    Solve problem 338

--- src/main/java/leetcode/Problem338.java
+++ src/main/java/leetcode/Problem338.java
@@ -1,17 +1,22 @@
 package leetcode;
 
-import java.util.Arrays;
-
 /**
  * https://leetcode.com/problems/counting-bits/
  */
 public class Problem338 {
     public int[] countBits(int num) {
-
-    }
-
-    public static void main(String[] args) {
-        Problem338 prob = new Problem338();
-        System.out.println(Arrays.toString(prob.countBits(5)));
+        int[] result = new int[num + 1];
+        for (int i = 0; i <= num; i++) {
+            int n = i;
+            int count = 0;
+            while (n > 0) {
+                if ((n & 1) == 1) {
+                    count++;
+                }
+                n >>= 1;
+            }
+            result[i] = count;
+        }
+        return result;
     }
 }
