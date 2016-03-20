afe5d23f10825327c444589f4f08cfc08f41e510
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-03-20 00:12:55 -0500

    Create a skeleton for problem 338

---
+++ src/main/java/leetcode/Problem338.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/counting-bits/
+ */
+public class Problem338 {
+    public int[] countBits(int num) {
+
+    }
+
+    public static void main(String[] args) {
+        Problem338 prob = new Problem338();
+        System.out.println(Arrays.toString(prob.countBits(5)));
+    }
+}
