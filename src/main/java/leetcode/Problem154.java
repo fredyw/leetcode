f42862940b1a46224e8954ec95f3c8d607edfca9
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-04-03 23:46:44 -0500

    Create a skeleton for problem 154

---
+++ src/main/java/leetcode/Problem154.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/
+ */
+public class Problem154 {
+    public int findMin(int[] nums) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem154 prob = new Problem154();
+
+    }
+}
