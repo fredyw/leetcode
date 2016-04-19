2f4a8c408fa0ee477a97b8c34fcbd65c92f16802
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-04-18 23:55:30 -0500

    Create a skeleton for problem 343

---
+++ src/main/java/leetcode/Problem343.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/integer-break/
+ */
+public class Problem343 {
+    public int integerBreak(int n) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem343 prob = new Problem343();
+        System.out.println(prob.integerBreak(2)); // 1
+        System.out.println(prob.integerBreak(36)); // 36
+    }
+}
