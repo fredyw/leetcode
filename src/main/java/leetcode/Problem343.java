9b0b7e061be052803becd7743e67015a18e9f79a
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-04-19 15:48:46 -0500

    Update problem 343

--- src/main/java/leetcode/Problem343.java
+++ src/main/java/leetcode/Problem343.java
@@ -5,13 +5,24 @@ package leetcode;
  */
 public class Problem343 {
     public int integerBreak(int n) {
-        // TODO
-        return 0;
+        if (n == 1) {
+            return 1;
+        }
+        int x = n / 2;
+        int y = n - x;
+        int n1 = integerBreak(x);
+        int n2 = integerBreak(y);
+        int multiply1 = x * y;
+        int multiple2 = n1 * n2;
+        int max = Math.max(multiply1, multiple2);
+        return max;
     }
 
     public static void main(String[] args) {
         Problem343 prob = new Problem343();
         System.out.println(prob.integerBreak(2)); // 1
-        System.out.println(prob.integerBreak(36)); // 36
+        System.out.println(prob.integerBreak(10)); // 36
+        System.out.println(prob.integerBreak(1)); // 1
+        System.out.println(prob.integerBreak(14)); // 162
     }
 }
