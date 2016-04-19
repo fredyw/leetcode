9dde750bca532be7a86059d009942ab6ce5eadc0
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-04-18 23:46:19 -0500

    Create a skeleton for problem 342

---
+++ src/main/java/leetcode/Problem342.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/power-of-four/
+ */
+public class Problem342 {
+    public boolean isPowerOfFour(int num) {
+        // TODO:
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem342 prob = new Problem342();
+        System.out.println(prob.isPowerOfFour(16)); // true
+        System.out.println(prob.isPowerOfFour(5)); // false
+    }
+}
