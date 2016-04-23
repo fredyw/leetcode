1dc7bd2d19d78da576eb030730b0d2043db2e099
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-04-23 01:46:21 -0500

    Solve problem 345

--- src/main/java/leetcode/Problem345.java
+++ src/main/java/leetcode/Problem345.java
@@ -5,13 +5,43 @@ package leetcode;
  */
 public class Problem345 {
     public String reverseVowels(String s) {
-        // TODO
-        return "";
+        char[] chars = s.toCharArray();
+        int leftIdx = 0;
+        int rightIdx = chars.length - 1;
+        int leftVowelIdx = 0;
+        int rightVowelIdx = rightIdx;
+        while (leftIdx <= rightIdx) {
+            boolean leftVowel = false;
+            while (leftIdx <= rightIdx) {
+                if (isVowel(chars[leftIdx])) {
+                    leftVowel = true;
+                    leftVowelIdx = leftIdx;
+                    break;
+                }
+                leftIdx++;
+            }
+            boolean rightVowel = false;
+            while (rightIdx >= leftIdx) {
+                if (isVowel(chars[rightIdx])) {
+                    rightVowel = true;
+                    rightVowelIdx = rightIdx;
+                    break;
+                }
+                rightIdx--;
+            }
+            if (leftVowel && rightVowel) {
+                char tmp = chars[leftVowelIdx];
+                chars[leftVowelIdx] = chars[rightIdx];
+                chars[rightVowelIdx] = tmp;
+            }
+            leftIdx++;
+            rightIdx--;
+        }
+        return new String(chars);
     }
 
-    public static void main(String[] args) {
-        Problem345 prob = new Problem345();
-        System.out.println(prob.reverseVowels("hello")); // holle
-        System.out.println(prob.reverseVowels("leetcode")); // leotcede
+    private boolean isVowel(char c) {
+        return (c == 'a' || c == 'i' || c == 'u' || c == 'e' || c == 'o' ||
+                c == 'A' || c == 'I' || c == 'U' || c == 'E' || c == 'O');
     }
 }
