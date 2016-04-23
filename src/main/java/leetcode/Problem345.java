23623310ed9993a8208d820dd9536d30d099dda4
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-04-23 00:21:18 -0500

    Create a skeleton for problem 345

---
+++ src/main/java/leetcode/Problem345.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/reverse-vowels-of-a-string/
+ */
+public class Problem345 {
+    public String reverseVowels(String s) {
+        // TODO
+        return "";
+    }
+
+    public static void main(String[] args) {
+        Problem345 prob = new Problem345();
+        System.out.println(prob.reverseVowels("hello")); // holle
+        System.out.println(prob.reverseVowels("leetcode")); // leotcede
+    }
+}
