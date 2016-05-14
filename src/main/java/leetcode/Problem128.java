9ad078d9adaaf2100997338d7c828bda5450d80f
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-05-14 00:03:34 -0500

    Create a skeleton for problem 128

---
+++ src/main/java/leetcode/Problem128.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/longest-consecutive-sequence/
+ */
+public class Problem128 {
+    public int longestConsecutive(int[] nums) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem128 prob = new Problem128();
+        System.out.println(new int[]{100, 4, 200, 1, 3, 2}); // 4
+        System.out.println(new int[]{100, 4, 200, 1, 3, 2, 101, 99}); // 4
+    }
+}
