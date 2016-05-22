ea6fdb0121cb6b26760129052e30f3ed36f98a74
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-05-22 16:23:12 -0500

    Create skeleton for problem 315

---
+++ src/main/java/leetcode/Problem315.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/count-of-smaller-numbers-after-self/
+ */
+public class Problem315 {
+    public List<Integer> countSmaller(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        // TODO:
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Problem315 prob = new Problem315();
+        System.out.println(prob.countSmaller(new int[]{5, 2, 6, 1})); // [2, 1, 1, 0]
+    }
+}
