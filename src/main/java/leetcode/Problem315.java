eba27be700a564cc412a67dcdd8841850dda22da
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-06-12 00:53:21 -0500

    Put solution for problem 315

--- src/main/java/leetcode/Problem315.java
+++ src/main/java/leetcode/Problem315.java
@@ -1,6 +1,6 @@
 package leetcode;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -8,13 +8,31 @@ import java.util.List;
  */
 public class Problem315 {
     public List<Integer> countSmaller(int[] nums) {
-        List<Integer> result = new ArrayList<>();
-        // TODO:
-        return result;
+        int[] tmp = nums.clone();
+        Arrays.sort(tmp);
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = Arrays.binarySearch(tmp, nums[i]) + 1;
+        }
+        int[] bit = new int[nums.length + 1];
+        Integer[] ans = new Integer[nums.length];
+        for (int i = nums.length - 1; i >= 0; i--) {
+            ans[i] = query(bit, nums[i] - 1);
+            add(bit, nums[i], 1);
+        }
+        return Arrays.asList(ans);
     }
 
-    public static void main(String[] args) {
-        Problem315 prob = new Problem315();
-        System.out.println(prob.countSmaller(new int[]{5, 2, 6, 1})); // [2, 1, 1, 0]
+    private void add(int[] bit, int i, int val) {
+        for (; i < bit.length; i += i & -i) {
+            bit[i] += val;
+        }
+    }
+
+    private int query(int[] bit, int i) {
+        int ans = 0;
+        for (; i > 0; i -= i & -i) {
+            ans += bit[i];
+        }
+        return ans;
     }
 }
