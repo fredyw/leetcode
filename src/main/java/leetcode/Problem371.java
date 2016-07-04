864bbd2361b5821f9b0fc1e3b60b057f376b3bf7
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-07-04 01:30:47 -0500

    Create a skeleton for problem 371

---
+++ src/main/java/leetcode/Problem371.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/sum-of-two-integers/
+ */
+public class Problem371 {
+    public int getSum(int a, int b) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem371 prob = new Problem371();
+        System.out.println(prob.getSum(1, 3)); // 4
+        System.out.println(prob.getSum(1, -3)); // -2
+        System.out.println(prob.getSum(1, 1)); // 2
+    }
+}
