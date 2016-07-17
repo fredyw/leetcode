a57c317413e14c799df4d05d6bdb24ae5026a3b9
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-07-17 13:23:13 -0500

    Solve problem 374

---
+++ src/main/java/leetcode/Problem374.java
@@ -0,0 +1,38 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/guess-number-higher-or-lower/
+ */
+public class Problem374 {
+    // test case
+    private static int guess(int num) {
+        if (num == 1702766719) {
+            return 0;
+        } else if (num < 1702766719) {
+            return 1;
+        }
+        return -1;
+    }
+
+    public int guessNumber(int n) {
+        long lo = 1;
+        long hi = n;
+        while (lo <= hi) {
+            long lmid = (lo + hi) / 2;
+            int mid = (int) lmid;
+            if (guess(mid) == 0) {
+                return mid;
+            } else if (guess(mid) < 0) {
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        throw new RuntimeException("Number not found");
+    }
+
+    public static void main(String[] args) {
+        Problem374 prob = new Problem374();
+        System.out.println(prob.guessNumber(2126753390));
+    }
+}
