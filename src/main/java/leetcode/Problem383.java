d4306d4b6c003f92d7682b276e87cb3124787d9a
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-12 01:17:28 -0500

    Solve problem 383

--- src/main/java/leetcode/Problem383.java
+++ src/main/java/leetcode/Problem383.java
@@ -1,12 +1,36 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * https://leetcode.com/problems/ransom-note/
  */
 public class Problem383 {
     public boolean canConstruct(String ransomNote, String magazine) {
-        // TODO
-        return false;
+        Map<Character, Integer> charCount = new HashMap<>();
+        for (int i = 0; i < magazine.length(); i++) {
+            char c = magazine.charAt(i);
+            if (!charCount.containsKey(c)) {
+                charCount.put(c, 1);
+            } else {
+                charCount.put(c, charCount.get(c) + 1);
+            }
+        }
+        for (int i = 0; i < ransomNote.length(); i++) {
+            char c = ransomNote.charAt(i);
+            if (!charCount.containsKey(c)) {
+                return false;
+            }
+            int count = charCount.get(c);
+            int newCount = count - 1;
+            if (newCount == 0) {
+                charCount.remove(c);
+            } else {
+                charCount.put(c, newCount);
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
