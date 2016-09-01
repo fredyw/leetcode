e1628ed3d7f46c8534f4d498b7ab2f4f1a0643e7
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-09-01 12:59:53 -0500

    Skeleton for problem 386

---
+++ src/main/java/leetcode/Problem386.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/lexicographical-numbers/
+ */
+public class Problem386 {
+    public List<Integer> lexicalOrder(int n) {
+        List<Integer> result = new ArrayList<>();
+        // TODO
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Problem386 prob = new Problem386();
+        System.out.println(prob.lexicalOrder(13));
+    }
+}
