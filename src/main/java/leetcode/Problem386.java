a61a3ac67fc6daee90e3a69980cd1bbda8a874fd
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-09-02 16:32:13 -0500

    Update problem 386

--- src/main/java/leetcode/Problem386.java
+++ src/main/java/leetcode/Problem386.java
@@ -1,25 +1,25 @@
 package leetcode;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.TreeSet;
 
 /**
  * https://leetcode.com/problems/lexicographical-numbers/
  */
 public class Problem386 {
     public List<Integer> lexicalOrder(int n) {
-        List<Integer> result = new ArrayList<>();
+        TreeSet<Integer> result = new TreeSet<>((x, y) -> x.toString().compareTo(y.toString()));
         for (int i = 1; i <= n; i++) {
             result.add(i);
         }
-        Collections.sort(result, (x, y) -> x.toString().compareTo(y.toString()));
-        return result;
+        return new ArrayList<>(result);
     }
 
     public static void main(String[] args) {
         Problem386 prob = new Problem386();
         System.out.println(prob.lexicalOrder(13));
-        System.out.println(prob.lexicalOrder(5000000));
+        System.out.println(prob.lexicalOrder(5000));
+//        System.out.println(prob.lexicalOrder(5000000));
     }
 }
