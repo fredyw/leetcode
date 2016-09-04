0ab8777bdc0eb820cf8f26dddc717e28ac251fea
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-09-04 13:16:10 -0500

    Skeleton for problem 357

---
+++ src/main/java/leetcode/Problem357.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/count-numbers-with-unique-digits/
+ */
+public class Problem357 {
+    public int countNumbersWithUniqueDigits(int n) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem357 prob = new Problem357();
+        System.out.println(prob.countNumbersWithUniqueDigits(2)); // 91
+    }
+}
