59a588cf0ab09aa62f1268effbe3c6ff57d5f97e
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-09-04 12:58:29 -0500

    Skeleton for problem 167

---
+++ src/main/java/leetcode/Problem167.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+ */
+public class Problem167 {
+    public int[] twoSum(int[] numbers, int target) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem167 prob = new Problem167();
+        System.out.println(Arrays.toString(prob.twoSum(new int[]{2, 7, 11, 15}, 9)));
+    }
+}
