074381d1a84d3064f910bb4fea664eb8d8b6d720
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-09-06 10:46:31 -0500

    Update problem 378

--- src/main/java/leetcode/Problem378.java
+++ src/main/java/leetcode/Problem378.java
@@ -5,16 +5,34 @@ package leetcode;
  */
 public class Problem378 {
     public int kthSmallest(int[][] matrix, int k) {
-        // TODO
-        return 0;
+        int rowSize = matrix.length;
+        int colSize = matrix[0].length;
+        int row = (int) Math.ceil(k / rowSize);
+        System.out.println("k: " + k);
+        System.out.println("rowSize: " + rowSize);
+        System.out.println("row: " + row);
+        int col = (row * colSize) - k + 1;
+        System.out.println("col: " + col);
+//        System.out.println("col: " + ((row * colSize)));
+        return matrix[row][col];
     }
 
     public static void main(String[] args) {
         Problem378 prob = new Problem378();
+//        System.out.println(prob.kthSmallest(new int[][]{
+//            {1, 5, 9},
+//            {10, 11, 13},
+//            {12, 13, 15}
+//        }, 8)); // 13
         System.out.println(prob.kthSmallest(new int[][]{
             {1, 5, 9},
             {10, 11, 13},
             {12, 13, 15}
-        }, 8)); // 13
+        }, 6)); // 13
+//        System.out.println(prob.kthSmallest(new int[][]{
+//            {1, 5, 9},
+//            {10, 11, 13},
+//            {12, 13, 15}
+//        }, 7)); // 12
     }
 }
