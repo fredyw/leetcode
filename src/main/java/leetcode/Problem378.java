f985452e5cc174cb544e26f04b0268d7942bf51a
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-09-06 13:53:47 -0500

    Update problem 378

--- src/main/java/leetcode/Problem378.java
+++ src/main/java/leetcode/Problem378.java
@@ -7,32 +7,20 @@ public class Problem378 {
     public int kthSmallest(int[][] matrix, int k) {
         int rowSize = matrix.length;
         int colSize = matrix[0].length;
-        int row = (int) Math.ceil(k / rowSize);
-        System.out.println("k: " + k);
-        System.out.println("rowSize: " + rowSize);
-        System.out.println("row: " + row);
-        int col = (row * colSize) - k + 1;
-        System.out.println("col: " + col);
-//        System.out.println("col: " + ((row * colSize)));
+        int row = (k - 1) / rowSize;
+        int col = (k - 1) - (row * colSize);
+        // TODO
         return matrix[row][col];
     }
 
     public static void main(String[] args) {
         Problem378 prob = new Problem378();
-//        System.out.println(prob.kthSmallest(new int[][]{
-//            {1, 5, 9},
-//            {10, 11, 13},
-//            {12, 13, 15}
-//        }, 8)); // 13
-        System.out.println(prob.kthSmallest(new int[][]{
-            {1, 5, 9},
-            {10, 11, 13},
-            {12, 13, 15}
-        }, 6)); // 13
-//        System.out.println(prob.kthSmallest(new int[][]{
-//            {1, 5, 9},
-//            {10, 11, 13},
-//            {12, 13, 15}
-//        }, 7)); // 12
+        for (int i = 0; i < 9; i++) {
+            System.out.println(prob.kthSmallest(new int[][]{
+                {1, 5, 9},
+                {10, 11, 13},
+                {12, 13, 15}
+            }, i + 1));
+        }
     }
 }
