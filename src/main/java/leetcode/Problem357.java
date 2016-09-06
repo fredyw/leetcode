6e3e73d458d3c5fa1d5132d8a94f8fb1d84d7fb6
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-09-06 00:28:03 -0500

    Solve problem 357

--- src/main/java/leetcode/Problem357.java
+++ src/main/java/leetcode/Problem357.java
@@ -5,14 +5,30 @@ package leetcode;
  */
 public class Problem357 {
     public int countNumbersWithUniqueDigits(int n) {
-        // TODO
-        return 0;
+        return countNumbersWithUniqueDigits(n, 0, new boolean[10]);
+    }
+
+    private int countNumbersWithUniqueDigits(int n, int m, boolean[] visited) {
+        if (n == m) {
+            return 1;
+        }
+        int count = 1;
+        for (int i = (m == 0) ? 1 : 0; i <= 9; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                count += countNumbersWithUniqueDigits(n, m + 1, visited);
+                visited[i] = false;
+            }
+        }
+        return count;
     }
 
     public static void main(String[] args) {
         Problem357 prob = new Problem357();
+        System.out.println(prob.countNumbersWithUniqueDigits(4)); // 5275
+        System.out.println(prob.countNumbersWithUniqueDigits(3)); // 739
         System.out.println(prob.countNumbersWithUniqueDigits(2)); // 91
-        System.out.println(prob.countNumbersWithUniqueDigits(1)); // 10
-        System.out.println(prob.countNumbersWithUniqueDigits(0)); // 1
+//        System.out.println(prob.countNumbersWithUniqueDigits(1)); // 10
+//        System.out.println(prob.countNumbersWithUniqueDigits(0)); // 1
     }
 }
