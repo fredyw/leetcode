ac1c43c44ab86dfc2f7c2687d8bae46308a60e1b
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-09-06 18:38:00 -0500

    Update problem 378

--- src/main/java/leetcode/Problem378.java
+++ src/main/java/leetcode/Problem378.java
@@ -1,41 +1,22 @@
 package leetcode;
 
+import java.util.PriorityQueue;
+
 /**
  * https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/
  */
 public class Problem378 {
     public int kthSmallest(int[][] matrix, int k) {
-        int rowSize = matrix.length;
-        int colSize = matrix[0].length;
-        int row = (k - 1) / rowSize;
-        int col = (k - 1) - (row * colSize);
-        // TODO
-        return matrix[row][col];
-    }
-
-    public static void main(String[] args) {
-        Problem378 prob = new Problem378();
-        for (int i = 0; i < 9; i++) {
-            System.out.println(prob.kthSmallest(new int[][]{
-                {1, 5, 9},
-                {10, 11, 13},
-                {12, 13, 15}
-            }, i + 1));
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                queue.add(matrix[i][j]);
+            }
         }
-
-        for (int i = 0; i < 4; i++) {
-            System.out.println(prob.kthSmallest(new int[][]{
-                {1, 2},
-                {1, 3},
-            }, i + 1));
+        int result = 0;
+        for (int i = 0; i < k; i++) {
+            result = queue.remove();
         }
-
-        for (int i = 0; i < 4; i++) {
-            System.out.println(prob.kthSmallest(new int[][]{
-                {1, 1, 1},
-                {1, 1, 3},
-                {1, 2, 3},
-            }, i + 1));
-        }
+        return result;
     }
 }
