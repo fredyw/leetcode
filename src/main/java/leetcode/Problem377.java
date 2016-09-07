055bf30ac0a5ada0b395bca394e203fd41287a4f
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-09-07 00:42:22 -0500

    Solve problem 377

--- src/main/java/leetcode/Problem377.java
+++ src/main/java/leetcode/Problem377.java
@@ -1,12 +1,32 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * https://leetcode.com/problems/combination-sum-iv/
  */
 public class Problem377 {
     public int combinationSum4(int[] nums, int target) {
-        // TODO
-        return 0;
+        return combinationSum(nums, target, new HashMap<>());
+    }
+
+    private int combinationSum(int[] nums, int target, Map<Integer, Integer> memo) {
+        if (target < 0) {
+            return 0;
+        }
+        if (target == 0) {
+            return 1;
+        }
+        if (memo.containsKey(target)) {
+            return memo.get(target);
+        }
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            count += combinationSum(nums, target - nums[i], memo);
+        }
+        memo.put(target, count);
+        return count;
     }
 
     public static void main(String[] args) {
