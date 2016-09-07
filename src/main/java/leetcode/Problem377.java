b16126decabb3da41a9f76d2183352821728f846
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-09-07 00:18:56 -0500

    Skeleton for problem 377

---
+++ src/main/java/leetcode/Problem377.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/combination-sum-iv/
+ */
+public class Problem377 {
+    public int combinationSum4(int[] nums, int target) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem377 prob = new Problem377();
+        System.out.println(prob.combinationSum4(new int[]{1, 2, 3}, 4)); // 7
+    }
+}
