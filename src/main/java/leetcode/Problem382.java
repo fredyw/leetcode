a94d040b28b4cad2ae6c6cbecf9adffec7d4b2dc
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-09-06 23:24:32 -0500

    Solve poblem 382

--- src/main/java/leetcode/Problem382.java
+++ src/main/java/leetcode/Problem382.java
@@ -1,5 +1,9 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /**
  * https://leetcode.com/problems/linked-list-random-node/
  */
@@ -14,31 +18,25 @@ public class Problem382 {
     }
 
     public static class Solution {
+        private final List<Integer> values = new ArrayList<>();
+        private final Random random = new Random();
+
         /**
          * @param head The linked list's head.
          *             Note that the head is guaranteed to be not null, so it contains at least one node.
          */
         public Solution(ListNode head) {
-
+            for (ListNode node = head; node != null; node = node.next) {
+                values.add(node.val);
+            }
         }
 
         /**
          * Returns a random node's value.
          */
         public int getRandom() {
-            // TODO
-            return 0;
+            int randomIdx = random.nextInt(values.size());
+            return values.get(randomIdx);
         }
-    }
-
-    public static void main(String[] args) {
-        ListNode head = new ListNode(1);
-        head.next = new ListNode(2);
-        head.next.next = new ListNode(3);
-        Solution solution = new Solution(head);
-
-        System.out.println(solution.getRandom());
-        System.out.println(solution.getRandom());
-        System.out.println(solution.getRandom());
     }
 }
