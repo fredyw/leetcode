b3b0aa28245247092daef7937db9e0f24903a91d
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-09-09 00:42:32 -0500

    Update problem 368

--- src/main/java/leetcode/Problem368.java
+++ src/main/java/leetcode/Problem368.java
@@ -1,6 +1,7 @@
 package leetcode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -9,13 +10,35 @@ import java.util.List;
 public class Problem368 {
     public List<Integer> largestDivisibleSubset(int[] nums) {
         List<Integer> result = new ArrayList<>();
-        // TODO
+        Arrays.sort(nums);
+        for (int i = nums.length - 1; i >= 0; i--) {
+            List<Integer> tmp = new ArrayList<>();
+            for (int j = 0; j < i; j++) {
+                if (nums[i] % nums[j] == 0) {
+                    if (tmp.size() == 0) {
+                        tmp.add(nums[j]);
+                    } else {
+                        if (nums[j] % tmp.get(tmp.size() - 1) == 0) {
+                            tmp.add(nums[j]);
+                        }
+                    }
+                }
+            }
+            tmp.add(nums[i]);
+            if (result.size() < tmp.size()) {
+                result = tmp;
+            }
+        }
         return result;
     }
 
     public static void main(String[] args) {
         Problem368 prob = new Problem368();
+        System.out.println(prob.largestDivisibleSubset(new int[]{546, 669})); // [546]
         System.out.println(prob.largestDivisibleSubset(new int[]{1, 2, 3})); // [1, 2]
-        System.out.println(prob.largestDivisibleSubset(new int[]{1, 2, 4, 8})); // [1 ,2, 4, 8]
+        System.out.println(prob.largestDivisibleSubset(new int[]{1, 2, 3, 6})); // [1, 2, 6]
+        System.out.println(prob.largestDivisibleSubset(new int[]{1, 2, 4, 8})); // [1, 2, 4, 8]
+        System.out.println(prob.largestDivisibleSubset(new int[]{1, 2, 4, 8, 9, 72})); // [1, 2, 4, 8, 72]
+        System.out.println(prob.largestDivisibleSubset(new int[]{1, 2, 3, 4, 6, 24})); // [1, 2, 4, 24]
     }
 }
