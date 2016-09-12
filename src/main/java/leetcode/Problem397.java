b1c583e681ecefd0bef04b27f7b82068340ad12f
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-09-12 09:54:21 -0500

    Skeleton for problem 397

---
+++ src/main/java/leetcode/Problem397.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/integer-replacement/
+ */
+public class Problem397 {
+    public int integerReplacement(int n) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem397 prob = new Problem397();
+        System.out.println(prob.integerReplacement(8)); // 3
+        System.out.println(prob.integerReplacement(7)); // 4
+    }
+}
