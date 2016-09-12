d060c5d2cb70373c2d8e8acd57b08c0808675747
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-09-12 10:45:24 -0500

    Skeleton for problem 396

---
+++ src/main/java/leetcode/Problem396.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/rotate-function/
+ */
+public class Problem396 {
+    public int maxRotateFunction(int[] A) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem396 prob = new Problem396();
+        System.out.println(prob.maxRotateFunction(new int[]{4, 3, 2, 6})); // 26
+    }
+}
