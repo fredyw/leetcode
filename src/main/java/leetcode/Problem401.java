e08b9a3e968b99967cd843c529be9c848f85bcf8
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-09-23 14:13:19 -0500

    Skeleton for problem 401

---
+++ src/main/java/leetcode/Problem401.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/binary-watch/
+ */
+public class Problem401 {
+    public List<String> readBinaryWatch(int num) {
+        List<String> result = new ArrayList<>();
+        // TODO
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Problem401 prob = new Problem401();
+        System.out.println(prob.readBinaryWatch(1));
+    }
+}
