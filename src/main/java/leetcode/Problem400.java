677c3deca5f54fe9f0d2ea820239def5df207112
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-09-24 16:51:37 -0500

    Skeleton for problem 400

---
+++ src/main/java/leetcode/Problem400.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/nth-digit/
+ */
+public class Problem400 {
+    public int findNthDigit(int n) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem400 prob = new Problem400();
+        System.out.println(prob.findNthDigit(3)); // 3
+        System.out.println(prob.findNthDigit(11)); // 0
+    }
+}
