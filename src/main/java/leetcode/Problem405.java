3fd7df54ed35783a6cbe078ba85725b01bedaf19
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-09-26 01:12:05 -0500

    Skeleton for problem 405

---
+++ src/main/java/leetcode/Problem405.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/convert-a-number-to-hexadecimal/
+ */
+public class Problem405 {
+    public String toHex(int num) {
+        // TODO
+        return "";
+    }
+
+    public static void main(String[] args) {
+        Problem405 prob = new Problem405();
+        System.out.println(prob.toHex(26)); // 1a
+        System.out.println(prob.toHex(-1)); // ffffffff
+    }
+}
