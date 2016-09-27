e4682fc84b4d1e01034e8bba0dbc235d46a75704
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-09-26 19:23:39 -0500

    Skeleton for problem 406

---
+++ src/main/java/leetcode/Problem406.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/queue-reconstruction-by-height/
+ */
+public class Problem406 {
+    public int[][] reconstructQueue(int[][] people) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem406 prob = new Problem406();
+        System.out.println(prob.reconstructQueue(
+            new int[][]{{7, 0}, {4, 4}, {7, 1}, {5, 0}, {6, 1}, {5, 2}})); // [[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
+    }
+}
