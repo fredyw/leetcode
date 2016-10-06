94a0229e1406ef6800d7aff1862c2791faa3c37b
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-10-06 00:44:34 -0500

    Solve problem 393

--- src/main/java/leetcode/Problem393.java
+++ src/main/java/leetcode/Problem393.java
@@ -5,13 +5,44 @@ package leetcode;
  */
 public class Problem393 {
     public boolean validUtf8(int[] data) {
-        // TODO
-        return false;
+        int i = 0;
+        int count = 0;
+        while (i < data.length) {
+            int d = data[i];
+            if (count > 0) {
+                if ((d & 0b10000000) == 0b10000000) {
+                    count--;
+                } else {
+                    return false;
+                }
+            } else {
+                if ((d & 0b11110000) == 0b11110000) { // 4 bytes
+                    count = 3;
+                } else if ((d & 0b11100000) == 0b11100000) { // 3 bytes
+                    count = 2;
+                } else if ((d & 0b11000000) == 0b11000000) { // 2 bytes
+                    count = 1;
+                } else { // 1 byte
+                    if (d > 0b1111111) {
+                        return false;
+                    }
+                    count = 0;
+                }
+            }
+            i++;
+        }
+        if (count > 0) {
+            return false;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
         Problem393 prob = new Problem393();
         System.out.println(prob.validUtf8(new int[]{197, 130, 1})); // true
         System.out.println(prob.validUtf8(new int[]{235, 140, 4})); // false
+        System.out.println(prob.validUtf8(new int[]{127})); // true
+        System.out.println(prob.validUtf8(new int[]{255})); // false
+        System.out.println(prob.validUtf8(new int[]{145})); // false
     }
 }
