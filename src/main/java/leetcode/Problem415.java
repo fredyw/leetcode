378bce140a7076b6c6fdba6282afc54f36fdb97f
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-10-16 01:38:39 -0500

    Skeleton for problem 415

---
+++ src/main/java/leetcode/Problem415.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/add-strings/
+ */
+public class Problem415 {
+    public String addStrings(String num1, String num2) {
+        // TODO
+        return "";
+    }
+
+    public static void main(String[] args) {
+        Problem415 prob = new Problem415();
+        System.out.println(prob.addStrings("124", "54321")); // 54445
+    }
+}
