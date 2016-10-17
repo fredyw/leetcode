c69afea2cf271e92456872caa7ef3095e7b27bbc
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-10-16 23:34:24 -0500

    Skeleton for problem 413

---
+++ src/main/java/leetcode/Problem413.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/arithmetic-slices/
+ */
+public class Problem413 {
+    public int numberOfArithmeticSlices(int[] A) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem413 prob = new Problem413();
+        System.out.println(prob.numberOfArithmeticSlices(new int[]{1, 1, 2, 5, 7})); // 0
+        System.out.println(prob.numberOfArithmeticSlices(new int[]{1, 1, 1, 2, 5, 7})); // 1
+        System.out.println(prob.numberOfArithmeticSlices(new int[]{1, 2, 3, 4})); // 3
+        System.out.println(prob.numberOfArithmeticSlices(new int[]{1, 3, 5, 7, 9})); // 6
+        System.out.println(prob.numberOfArithmeticSlices(new int[]{7, 7, 7, 7})); // 3
+        System.out.println(prob.numberOfArithmeticSlices(new int[]{3, -1, -5, -9})); // 3
+
+    }
+}
