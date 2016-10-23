8532926724853df4ff9951b7364e6732396fb202
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-10-23 13:31:27 -0500

    Solve problem 409

--- src/main/java/leetcode/Problem409.java
+++ src/main/java/leetcode/Problem409.java
@@ -1,16 +1,33 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * https://leetcode.com/problems/longest-palindrome/
  */
 public class Problem409 {
     public int longestPalindrome(String s) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem409 prob = new Problem409();
-        System.out.println(prob.longestPalindrome("abccccdd")); // 7
+        Map<Character, Integer> charCount = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (!charCount.containsKey(c)) {
+                charCount.put(c, 1);
+            } else {
+                charCount.put(c, charCount.get(c) + 1);
+            }
+        }
+        int result = 0;
+        boolean hasOdd = false;
+        for (Map.Entry<Character, Integer> entry : charCount.entrySet()) {
+            if (entry.getValue() % 2 != 0) {
+                hasOdd = true;
+            }
+            result += (entry.getValue() / 2) * 2;
+        }
+        if (hasOdd) {
+            result++;
+        }
+        return result;
     }
 }
