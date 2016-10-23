c34480a725b999b901665b4b4ea3e49632af11d3
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-10-23 01:32:31 -0500

    Solve problem 438

--- src/main/java/leetcode/Problem438.java
+++ src/main/java/leetcode/Problem438.java
@@ -10,24 +10,24 @@ import java.util.List;
 public class Problem438 {
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> result = new ArrayList<>();
-        char[] sortedP = p.toCharArray();
-        Arrays.sort(sortedP);
+        int[] pArray = new int[26];
+        for (int i = 0; i < p.length(); i++) {
+            int idx = p.charAt(i) - 'a';
+            pArray[idx]++;
+        }
         for (int i = 0; i < s.length(); i++) {
             if (i + p.length() > s.length()) {
                 break;
             }
-            char[] sortedSub = s.substring(i, i + p.length()).toCharArray();
-            Arrays.sort(sortedSub);
-            if (Arrays.equals(sortedP, sortedSub)) {
+            int[] sArray = new int[26];
+            for (int j = i; j < i + p.length(); j++) {
+                int idx = s.charAt(j) - 'a';
+                sArray[idx]++;
+            }
+            if (Arrays.equals(pArray, sArray)) {
                 result.add(i);
             }
         }
         return result;
-    }
-
-    public static void main(String[] args) {
-        Problem438 prob = new Problem438();
-        System.out.println(prob.findAnagrams("cbaebabacd", "abc")); // [0, 6]
-        System.out.println(prob.findAnagrams("abab", "ab")); // [0, 1, 2]
     }
 }
