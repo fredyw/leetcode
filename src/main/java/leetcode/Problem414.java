aceefe7467e7569f39f59331201546fd8675fbca
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-10-23 00:01:21 -0500

    Skeleton for problem 414

---
+++ src/main/java/leetcode/Problem414.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/third-maximum-number/
+ */
+public class Problem414 {
+    public int thirdMax(int[] nums) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem414 prob = new Problem414();
+        System.out.println(prob.thirdMax(new int[]{3, 2, 1})); // 1
+        System.out.println(prob.thirdMax(new int[]{1, 2})); // 2
+        System.out.println(prob.thirdMax(new int[]{2, 2, 3, 1})); // 1
+    }
+}
