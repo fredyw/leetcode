28b0b5ede2d0c547472dc178befa5cc7463c0111
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-10-23 01:39:08 -0500

    Skeleton for problem 409

---
+++ src/main/java/leetcode/Problem409.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/longest-palindrome/
+ */
+public class Problem409 {
+    public int longestPalindrome(String s) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem409 prob = new Problem409();
+        System.out.println(prob.longestPalindrome("abccccdd")); // 7
+    }
+}
