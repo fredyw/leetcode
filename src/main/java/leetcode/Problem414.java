f6ed6bd5c6abcd8fd642069f090061ed030dd5e9
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-10-23 00:35:37 -0500

    Solve problem 414

--- src/main/java/leetcode/Problem414.java
+++ src/main/java/leetcode/Problem414.java
@@ -5,14 +5,46 @@ package leetcode;
  */
 public class Problem414 {
     public int thirdMax(int[] nums) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem414 prob = new Problem414();
-        System.out.println(prob.thirdMax(new int[]{3, 2, 1})); // 1
-        System.out.println(prob.thirdMax(new int[]{1, 2})); // 2
-        System.out.println(prob.thirdMax(new int[]{2, 2, 3, 1})); // 1
+        Integer max1 = null;
+        Integer max2 = null;
+        Integer max3 = null;
+        for (int i = 0; i < nums.length; i++) {
+            if (max1 == null) {
+                max1 = nums[i];
+                continue;
+            }
+            if (max1 == nums[i]) {
+                continue;
+            }
+            if (max1 < nums[i]) {
+                max3 = max2;
+                max2 = max1;
+                max1 = nums[i];
+                continue;
+            }
+            if (max2 == null) {
+                max2 = nums[i];
+                continue;
+            }
+            if (max2 == nums[i]) {
+                continue;
+            }
+            if (max2 < nums[i]) {
+                max3 = max2;
+                max2 = nums[i];
+                continue;
+            }
+            if (max3 == null) {
+                max3 = nums[i];
+            }
+            if (max3 == nums[i]) {
+                continue;
+            }
+            if (max3 < nums[i]) {
+                max3 = nums[i];
+                continue;
+            }
+        }
+        return (max3 == null) ? max1 : max3;
     }
 }
