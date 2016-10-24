9eb96882f7b70b321c8b6113177f948d416ff8d9
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-10-23 19:39:02 -0500

    Skeleton for problem 423

---
+++ src/main/java/leetcode/Problem423.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/reconstruct-original-digits-from-english/
+ */
+public class Problem423 {
+    public String originalDigits(String s) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem423 prob = new Problem423();
+        System.out.println(prob.originalDigits("owoztneoer")); // 012
+        System.out.println(prob.originalDigits("fviefuro")); // 45
+    }
+}
