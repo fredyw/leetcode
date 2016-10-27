393413d2d34fff1837d7da003ba8c86de7b7035c
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-10-26 23:05:19 -0500

    Skeleton for problem 416

---
+++ src/main/java/leetcode/Problem416.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/partition-equal-subset-sum/
+ */
+public class Problem416 {
+    public boolean canPartition(int[] nums) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem416 prob = new Problem416();
+        System.out.println(prob.canPartition(new int[]{1, 5, 11, 5})); // true
+        System.out.println(prob.canPartition(new int[]{1, 2, 3, 5})); // false
+    }
+}
