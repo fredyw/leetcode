0a3047883c7d943e2bb7a6227e4637857cd38738
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-10-28 00:01:32 -0500

    Skeleton for problem 424

---
+++ src/main/java/leetcode/Problem424.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/longest-repeating-character-replacement/
+ */
+public class Problem424 {
+    public int characterReplacement(String s, int k) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem424 prob = new Problem424();
+        System.out.println(prob.characterReplacement("ABAB", 2)); // 4
+        System.out.println(prob.characterReplacement("AABABBA", 1)); // 4
+        System.out.println(prob.characterReplacement("ACABD", 2)); // 4
+    }
+}
