5c734ae58a0931d280be2840f05dc858701c8999
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-11-07 00:07:45 -0600

    Solve problem 441

--- src/main/java/leetcode/Problem441.java
+++ src/main/java/leetcode/Problem441.java
@@ -5,13 +5,15 @@ package leetcode;
  */
 public class Problem441 {
     public int arrangeCoins(int n) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem441 prob = new Problem441();
-        System.out.println(prob.arrangeCoins(5)); // 2
-        System.out.println(prob.arrangeCoins(8)); // 3
+        long sum = 0;
+        int result = 0;
+        for (int i = 1; i <= Integer.MAX_VALUE; i++) {
+            sum += i;
+            if (n < sum) {
+                break;
+            }
+            result++;
+        }
+        return result;
     }
 }
