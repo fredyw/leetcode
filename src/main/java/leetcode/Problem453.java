025bd6d049284f62184473038a1e366961087eb5
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-11-06 23:39:35 -0600

    Skeleton for problem 453

---
+++ src/main/java/leetcode/Problem453.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/minimum-moves-to-equal-array-elements/
+ */
+public class Problem453 {
+    public int minMoves(int[] nums) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem453 prob = new Problem453();
+        System.out.println(prob.minMoves(new int[]{1, 2, 3})); // 3
+    }
+}
