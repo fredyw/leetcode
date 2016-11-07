75acf179c63bcd10312da144f007e71a5c6d6e3e
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-11-07 00:00:26 -0600

    Skeleton for problem 441

---
+++ src/main/java/leetcode/Problem441.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/arranging-coins/
+ */
+public class Problem441 {
+    public int arrangeCoins(int n) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem441 prob = new Problem441();
+        System.out.println(prob.arrangeCoins(5)); // 2
+        System.out.println(prob.arrangeCoins(8)); // 3
+    }
+}
