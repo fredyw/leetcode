a16172a01382dc6f3d5c4e8433a6e02b78be45bb
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-11-10 23:53:43 -0600

    Skeleton for problem 447

---
+++ src/main/java/leetcode/Problem447.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/number-of-boomerangs/
+ */
+public class Problem447 {
+    public int numberOfBoomerangs(int[][] points) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem447 prob = new Problem447();
+        System.out.println(prob.numberOfBoomerangs(new int[][]{{0, 0}, {1, 0}, {2, 0}})); // 2
+    }
+}
