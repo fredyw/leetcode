c971943700516e448222609bbb6b57b19a8ceab0
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-11-10 23:51:27 -0600

    Solve problem 436

--- src/main/java/leetcode/Problem436.java
+++ src/main/java/leetcode/Problem436.java
@@ -21,35 +21,45 @@ public class Problem436 {
         }
     }
 
-    public int[] findRightInterval(Interval[] intervals) {
-        // TODO
-        return new int[0];
+    private static class IntervalWrapper {
+        private final int idx;
+        private final Interval interval;
+
+        public IntervalWrapper(int idx, Interval interval) {
+            this.idx = idx;
+            this.interval = interval;
+        }
     }
 
-    public static void main(String[] args) {
-        Problem436 prob = new Problem436();
-        System.out.println(Arrays.toString(prob.findRightInterval(new Interval[]{
-            new Interval(1, 2)
-        }))); // [-1]
-        System.out.println(Arrays.toString(prob.findRightInterval(new Interval[]{
-            new Interval(3, 4),
-            new Interval(2, 3),
-            new Interval(1, 2)
-        }))); // [-1, 0, 1]
-        System.out.println(Arrays.toString(prob.findRightInterval(new Interval[]{
-            new Interval(1, 4),
-            new Interval(2, 3),
-            new Interval(3, 4)
-        }))); // [-1, 2, -1]
-        System.out.println(Arrays.toString(prob.findRightInterval(new Interval[]{
-            new Interval(4, 8),
-            new Interval(3, 4),
-            new Interval(1, 4)
-        }))); // [-1, 0, 0]
-        System.out.println(Arrays.toString(prob.findRightInterval(new Interval[]{
-            new Interval(1, 2),
-            new Interval(3, 5),
-            new Interval(4, 5)
-        }))); // [1, -1, -1]
+    public int[] findRightInterval(Interval[] intervals) {
+        IntervalWrapper[] iws = new IntervalWrapper[intervals.length];
+        for (int i = 0; i < intervals.length; i++) {
+            iws[i] = new IntervalWrapper(i, intervals[i]);
+        }
+        Arrays.sort(iws, (a, b) -> Integer.compare(a.interval.start, b.interval.start));
+        int[] result = new int[intervals.length];
+        for (int i = 0; i < intervals.length; i++) {
+            result[i] = search(iws, intervals[i].end);
+        }
+        return result;
+    }
+
+    private static int search(IntervalWrapper[] iws, int val) {
+        int lo = 0;
+        int hi = iws.length - 1;
+        while (lo <= hi) {
+            int mid = (lo + hi) / 2;
+            if (iws[mid].interval.start == val) {
+                return iws[mid].idx;
+            } else if (iws[mid].interval.start > val) {
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        if (lo >= iws.length || hi < 0) {
+            return -1;
+        }
+        return iws[lo].idx;
     }
 }
