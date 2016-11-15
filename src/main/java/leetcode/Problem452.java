15b8af3307df32181255cfcb584c573eb3325911
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-11-14 23:31:01 -0600

    Skeleton for problem 452

---
+++ src/main/java/leetcode/Problem452.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/minimum-number-of-arrows-to-burst-balloons/
+ */
+public class Problem452 {
+    public int findMinArrowShots(int[][] points) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem452 prob = new Problem452();
+        System.out.println(prob.findMinArrowShots(new int[][]{{10, 16}, {2, 8}, {1, 6}, {7, 12}})); // 2
+    }
+}
