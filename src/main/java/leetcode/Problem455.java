7c2398d27fa272748a8ff2dd603300b03796d800
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-11-15 23:16:09 -0600

    Update problem 455

---
+++ src/main/java/leetcode/Problem455.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/assign-cookies/
+ */
+public class Problem455 {
+    public int findContentChildren(int[] g, int[] s) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem455 prob = new Problem455();
+        System.out.println(prob.findContentChildren(new int[]{1, 2, 3}, new int[]{1, 1})); // 1
+        System.out.println(prob.findContentChildren(new int[]{1, 2}, new int[]{1, 2, 3})); // 2
+    }
+}
