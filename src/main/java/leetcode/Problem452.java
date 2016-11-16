db27cd337bbb52fafa42fad7e91688f2c1cdda54
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-11-15 18:47:49 -0600

    Solve problem 452

--- src/main/java/leetcode/Problem452.java
+++ src/main/java/leetcode/Problem452.java
@@ -17,31 +17,21 @@ public class Problem452 {
             }
             return comp;
         });
-        for (int i = 0; i < points.length; i++) {
-            System.out.println(points[i][0] + ":" + points[i][1]);
-        }
+        int start1 = points[0][0];
         int end1 = points[0][1];
         int result = 1;
         for (int i = 1; i < points.length; i++) {
             int start2 = points[i][0];
             int end2 = points[i][1];
-            if (start2 > end1) {
+            if (start1 <= start2 && start2 <= end1) {
+                start1 = Math.max(start1, start2);
+                end1 = Math.min(end1, end2);
+            } else {
                 result++;
+                start1 = start2;
                 end1 = end2;
             }
         }
         return result;
-    }
-
-    public static void main(String[] args) {
-        Problem452 prob = new Problem452();
-//        System.out.println(prob.findMinArrowShots(new int[][]{{10, 16}, {2, 8}, {1, 6}, {7, 12}})); // 2
-//        System.out.println(prob.findMinArrowShots(new int[][]{{10, 16}, {2, 8}, {1, 6}, {7, 9}})); // 3
-//        System.out.println(prob.findMinArrowShots(new int[][]{{1, 6}, {7, 8}, {9, 10}})); // 3
-//        System.out.println(prob.findMinArrowShots(new int[][]{{1, 6}, {7, 8}, {8, 10}})); // 2
-//        System.out.println(prob.findMinArrowShots(new int[][]{{1, 6}})); // 1
-//        System.out.println(prob.findMinArrowShots(new int[][]{})); // 0
-//        System.out.println(prob.findMinArrowShots(new int[][]{{3, 9}, {7, 12}, {3, 8}, {6, 8}, {9, 10}, {2, 9}, {0, 9}, {3, 9}, {0, 6}, {2, 8}})); // 2
-        System.out.println(prob.findMinArrowShots(new int[][]{{9, 12}, {1, 10}, {4, 11}, {8, 12}, {3, 9}, {6, 9}, {6, 7}})); // 2
     }
 }
