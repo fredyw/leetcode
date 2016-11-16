3d09e99902c73c9ceb650e7e68016de78130e967
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-11-16 12:01:51 -0600

    Solve problem 459

--- src/main/java/leetcode/Problem459.java
+++ src/main/java/leetcode/Problem459.java
@@ -5,35 +5,25 @@ package leetcode;
  */
 public class Problem459 {
     public boolean repeatedSubstringPattern(String str) {
-        if (str.isEmpty() || str.length() == 1) {
-            return false;
-        }
-        String sub1 = "" + str.charAt(0);
-        String sub2 = "";
-        int i = 0;
-        i++;
-        while (i < str.length() && i + sub1.length() <= str.length()) {
-            sub2 = str.substring(i, i + sub1.length());
-            if (sub1.equals(sub2)) {
-                i += sub1.length();
-            } else {
-                sub1 += str.charAt(i);
-                i++;
+        for (int i = 1; i <= str.length() / 2; i++) {
+            String sub = null;
+            boolean found = true;
+            int j = 0;
+            for (; j + i <= str.length(); j += i) {
+                String tmpSub = str.substring(j, j + i);
+                if (sub == null) {
+                    sub = tmpSub;
+                } else {
+                    if (!sub.equals(tmpSub)) {
+                        found = false;
+                        break;
+                    }
+                }
+            }
+            if (found && j == str.length()) {
+                return true;
             }
         }
-        return sub1.equals(sub2) && i == str.length();
-    }
-
-    public static void main(String[] args) {
-        Problem459 prob = new Problem459();
-//        System.out.println(prob.repeatedSubstringPattern("abab")); // true
-//        System.out.println(prob.repeatedSubstringPattern("aba")); // false
-//        System.out.println(prob.repeatedSubstringPattern("abcabcabcabc")); // true
-//        System.out.println(prob.repeatedSubstringPattern("abcabca")); // false
-//        System.out.println(prob.repeatedSubstringPattern("")); // false
-//        System.out.println(prob.repeatedSubstringPattern("abac")); // false
-//        System.out.println(prob.repeatedSubstringPattern("a")); // false
-//        System.out.println(prob.repeatedSubstringPattern("abababaaba")); // false
-        System.out.println(prob.repeatedSubstringPattern("aaaabaaaab")); // true
+        return false;
     }
 }
