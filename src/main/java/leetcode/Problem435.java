ff22f2f1e7b68ce41e50194b49b976d6684b1a94
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-11-18 00:35:11 -0600

    Update problem 435

--- src/main/java/leetcode/Problem435.java
+++ src/main/java/leetcode/Problem435.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.Arrays;
+
 /**
  * https://leetcode.com/problems/non-overlapping-intervals/
  */
@@ -20,8 +22,28 @@ public class Problem435 {
     }
 
     public int eraseOverlapIntervals(Interval[] intervals) {
-        // TODO
-        return 0;
+        Arrays.sort(intervals, (a, b) -> {
+            int cmp = Integer.compare(a.start, b.start);
+            if (cmp == 0) {
+                return Integer.compare(a.end, b.end);
+            }
+            return cmp;
+        });
+        int result = 0;
+        int i = 0;
+        int j = i + 1;
+        while (i < intervals.length && j < intervals.length) {
+            Interval a = intervals[i];
+            Interval b = intervals[j];
+            if (b.start < a.end) {
+                j++;
+                result++;
+            } else {
+                i++;
+                j++;
+            }
+        }
+        return result;
     }
 
     public static void main(String[] args) {
@@ -35,5 +57,13 @@ public class Problem435 {
         System.out.println(prob.eraseOverlapIntervals(new Interval[] {
             new Interval(1, 2), new Interval(2, 3)
         })); // 0
+        System.out.println(prob.eraseOverlapIntervals(new Interval[] {
+            new Interval(1, 2), new Interval(2, 3), new Interval(3, 4), new Interval(1, 3),
+            new Interval(2, 5), new Interval(4, 5), new Interval(3, 5)
+        })); // 3
+        System.out.println(prob.eraseOverlapIntervals(new Interval[] {
+            new Interval(0, 2), new Interval(1, 3), new Interval(2, 4), new Interval(3, 5),
+            new Interval(4, 6)
+        })); // 2
     }
 }
