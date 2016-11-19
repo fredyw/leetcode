af817ef391398e1afd82ac11de39f7cb314f4f82
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-11-19 00:49:32 -0600

    Skeleton for problem 454

---
+++ src/main/java/leetcode/Problem454.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/4sum-ii/
+ */
+public class Problem454 {
+    public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem454 prob = new Problem454();
+        System.out.println(prob.fourSumCount(
+            new int[]{1, 2}, new int[]{-2, -1}, new int[]{-1, 2}, new int[]{0, 2}
+        )); // 2
+    }
+}
