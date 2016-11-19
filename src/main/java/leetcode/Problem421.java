de7847451616d4f2f522471a1ea709b4db8ab32e
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-11-19 01:17:55 -0600

    Skeleton for problem 421

---
+++ src/main/java/leetcode/Problem421.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/maximum-xor-of-two-numbers-in-an-array/
+ */
+public class Problem421 {
+    public int findMaximumXOR(int[] nums) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem421 prob = new Problem421();
+        System.out.println(prob.findMaximumXOR(new int[]{3, 10, 5, 25, 2, 8})); // 28
+    }
+}
