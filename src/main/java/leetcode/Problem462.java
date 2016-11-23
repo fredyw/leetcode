62885d49a29ecbf26820c99f4935b07679f83185
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-11-23 00:54:13 -0600

    Solve problem 462

--- src/main/java/leetcode/Problem462.java
+++ src/main/java/leetcode/Problem462.java
@@ -1,16 +1,21 @@
 package leetcode;
 
+import java.util.Arrays;
+
 /**
  * https://leetcode.com/problems/minimum-moves-to-equal-array-elements-ii/
  */
 public class Problem462 {
     public int minMoves2(int[] nums) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem462 prob = new Problem462();
-        System.out.println(prob.minMoves2(new int[]{1, 2, 3})); // 2
+        Arrays.sort(nums);
+        int result = 0;
+        int mid = nums.length / 2;
+        for (int i = 0; i < mid; i++) {
+            result += nums[mid] - nums[i];
+        }
+        for (int i = mid + 1; i < nums.length; i++) {
+            result += nums[i] - nums[mid];
+        }
+        return result;
     }
 }
