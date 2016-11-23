474cd029d71f859494ae5b4fd6d07fe637cc72ed
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-11-23 00:06:57 -0600

    Skeleton for problem 463

---
+++ src/main/java/leetcode/Problem463.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/island-perimeter/
+ */
+public class Problem463 {
+    public int islandPerimeter(int[][] grid) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem463 prob = new Problem463();
+        System.out.println(prob.islandPerimeter(new int[][]{
+            {0,1,0,0},
+            {1,1,1,0},
+            {0,1,0,0},
+            {1,1,0,0}
+        })); // 16
+    }
+}
