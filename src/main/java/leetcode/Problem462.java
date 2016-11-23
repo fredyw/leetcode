893c69a0fe0111941557c74340bc4526d55063d9
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-11-23 00:38:22 -0600

    Skeleton for problem 462

---
+++ src/main/java/leetcode/Problem462.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/minimum-moves-to-equal-array-elements-ii/
+ */
+public class Problem462 {
+    public int minMoves2(int[] nums) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem462 prob = new Problem462();
+        System.out.println(prob.minMoves2(new int[]{1, 2, 3})); // 2
+    }
+}
