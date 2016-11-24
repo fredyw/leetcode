e062b549f4a85d6050eed5775e67fec07565d4cd
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-11-24 00:16:34 -0600

    Skeleton fo problem 464

---
+++ src/main/java/leetcode/Problem464.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/can-i-win/
+ */
+public class Problem464 {
+    public boolean canIWin(int maxChoosableInteger, int desiredTotal) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem464 prob = new Problem464();
+        System.out.println(prob.canIWin(10, 11)); // false
+        System.out.println(prob.canIWin(4, 6)); // true
+    }
+}
