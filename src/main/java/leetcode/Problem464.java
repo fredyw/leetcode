e597187f8c37bb7e52f285ad0bf9481e70ffc186
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-11-24 12:55:40 -0600

    Update problem 464

--- src/main/java/leetcode/Problem464.java
+++ src/main/java/leetcode/Problem464.java
@@ -1,6 +1,8 @@
 package leetcode;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -14,40 +16,46 @@ public class Problem464 {
         for (int i = 1; i <= maxChoosableInteger; i++) {
             nums.add(i);
         }
-        return canIWin(nums, desiredTotal, 0, true);
+        return canIWin(nums, desiredTotal, 0, true, new HashMap<>());
     }
 
-    private static boolean canIWin(Set<Integer> nums, int desired, int accu, boolean player1) {
+    private static boolean canIWin(Set<Integer> nums, int desired, int accu, boolean player1,
+                                   Map<String, Boolean> memo) {
         if (accu >= desired) {
             if (!player1) {
                 return true;
             }
             return false;
         }
+        String key = accu + "|" + player1;
+        if (memo.containsKey(key)) {
+            return memo.get(key);
+        }
         boolean win = (player1) ? false : true;
         for (int num : nums) {
             Set<Integer> newNums = new HashSet<>(nums);
             newNums.remove(num);
             if (player1) {
-                win |= canIWin(newNums, desired, accu + num, !player1);
+                win |= canIWin(newNums, desired, accu + num, !player1, memo);
             } else {
-                win &= canIWin(newNums, desired, accu + num, !player1);
+                win &= canIWin(newNums, desired, accu + num, !player1, memo);
             }
         }
+        memo.put(key, win);
         return win;
     }
 
     public static void main(String[] args) {
         Problem464 prob = new Problem464();
-        System.out.println(prob.canIWin(10, 11)); // false
-        System.out.println(prob.canIWin(4, 6)); // true
-        System.out.println(prob.canIWin(10, 100)); // false
-        System.out.println(prob.canIWin(10, 55)); // false
-        System.out.println(prob.canIWin(6, 20)); // false
-        System.out.println(prob.canIWin(6, 15)); // true
-        System.out.println(prob.canIWin(6, 10)); // true
+//        System.out.println(prob.canIWin(10, 11)); // false
+//        System.out.println(prob.canIWin(4, 6)); // true
+//        System.out.println(prob.canIWin(10, 100)); // false
+//        System.out.println(prob.canIWin(10, 55)); // false
+//        System.out.println(prob.canIWin(6, 20)); // false
+//        System.out.println(prob.canIWin(6, 15)); // true
+//        System.out.println(prob.canIWin(6, 10)); // true
         System.out.println(prob.canIWin(5, 12)); // true
-        System.out.println(prob.canIWin(5, 15)); // true
+//        System.out.println(prob.canIWin(5, 15)); // true
 //        System.out.println(prob.canIWin(20, 200)); // false
     }
 }
