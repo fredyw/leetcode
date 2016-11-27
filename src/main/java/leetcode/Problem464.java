cb5af48238b8970593262ce5e65217627a320848
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-11-27 00:45:54 -0600

    Solve roblem 464 (finally!)

--- src/main/java/leetcode/Problem464.java
+++ src/main/java/leetcode/Problem464.java
@@ -1,6 +1,5 @@
 package leetcode;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,70 +18,45 @@ public class Problem464 {
         if (sum < desiredTotal) {
             return false;
         }
-        int[] nums = new int[maxChoosableInteger + 1];
-        return canIWin(nums, desiredTotal, 0, Player.P2, new HashMap<>()) == Player.P1;
+        return canIWin(0, maxChoosableInteger, desiredTotal, 0, Player.P2,
+            new HashMap<>()) == Player.P1;
     }
 
     private enum Player {
         P1, P2
     }
 
-    private static Player canIWin(int[] nums, int desired, int accu, Player player,
-                                  Map<String, Player> memo) {
+    private static Player canIWin(int bits, int max, int desired, int accu,
+                                  Player player, Map<Integer, Player> memo) {
         if (accu >= desired) {
             return player;
         }
-        // TODO: the memo isn't optimized
-        String key = Arrays.toString(nums);
-        if (memo.containsKey(key)) {
-            return memo.get(key);
+        if (memo.containsKey(bits)) {
+            return memo.get(bits);
         }
         Player win = player;
-        for (int num = 1; num < nums.length; num++) {
-            if (nums[num] == 1) {
+        for (int num = 0; num < max; num++) {
+            if ((bits & (1 << num)) == (1 << num)) {
                 continue;
             }
-            nums[num] = 1;
+            bits |= 1 << num;
             Player nextPlayer = (player == Player.P1) ? Player.P2 : Player.P1;
             if (player == Player.P1) {
-                win = canIWin(nums, desired, accu + num, nextPlayer, memo);
+                win = canIWin(bits, max, desired, accu + num + 1, nextPlayer, memo);
                 if (win == Player.P2) {
-                    nums[num] = 0;
+                    bits &= ~(1 << num);
                     break;
                 }
             } else if (player == Player.P2) {
-                win = canIWin(nums, desired, accu + num, nextPlayer, memo);
+                win = canIWin(bits, max, desired, accu + num + 1, nextPlayer, memo);
                 if (win == Player.P1) {
-                    nums[num] = 0;
+                    bits &= ~(1 << num);
                     break;
                 }
             }
-            nums[num] = 0;
+            bits &= ~(1 << num);
         }
-        memo.put(key, win);
+        memo.put(bits, win);
         return win;
-    }
-
-    public static void main(String[] args) {
-        Problem464 prob = new Problem464();
-//        System.out.println(prob.canIWin(10, 11)); // false
-//        System.out.println(prob.canIWin(4, 6)); // true
-//        System.out.println(prob.canIWin(10, 100)); // false
-//        System.out.println(prob.canIWin(10, 55)); // false
-//        System.out.println(prob.canIWin(6, 20)); // false
-//        System.out.println(prob.canIWin(6, 15)); // true
-//        System.out.println(prob.canIWin(6, 10)); // true
-//        System.out.println(prob.canIWin(5, 12)); // true
-//        System.out.println(prob.canIWin(5, 15)); // true
-//        System.out.println(prob.canIWin(10, 0)); // true
-//        System.out.println(prob.canIWin(18, 79)); // true
-//        System.out.println(prob.canIWin(20, 200)); // false
-//        System.out.println(prob.canIWin(11, 25)); // true
-//        System.out.println(prob.canIWin(18, 79)); // true
-//        System.out.println(prob.canIWin(10, 40)); // false
-//        System.out.println(prob.canIWin(20, 54)); // true
-//        System.out.println(prob.canIWin(20, 210)); // false
-//        System.out.println(prob.canIWin(10, 0)); // true
-        System.out.println(prob.canIWin(5, 50)); // true
     }
 }
