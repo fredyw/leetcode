d8a83d9cc4955f8c1225b0888f2cd8ce4f9b3ccf
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-11-28 00:19:10 -0600

    Skeleton for problem 4

---
+++ src/main/java/leetcode/Problem4.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/median-of-two-sorted-arrays/
+ */
+public class Problem4 {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        // TODO
+        return 0.0;
+    }
+
+    public static void main(String[] args) {
+        Problem4 prob = new Problem4();
+        System.out.println(prob.findMedianSortedArrays(new int[]{1, 3}, new int[]{2})); // 2.0
+        System.out.println(prob.findMedianSortedArrays(new int[]{1, 2}, new int[]{3, 4})); // 2.5
+    }
+}
