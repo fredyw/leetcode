110dc0ae105039cd251a9fed90ca84102926e45b
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-11-29 00:55:03 -0600

    Update problem 312

--- src/main/java/leetcode/Problem312.java
+++ src/main/java/leetcode/Problem312.java
@@ -37,14 +37,15 @@ public class Problem312 {
                 right = nums.get(i + 1);
             }
             int val = left * nums.get(i) * right;
-            List<Integer> newNums = new ArrayList<>();
-            newNums.addAll(nums.subList(0, i));
-            newNums.addAll(nums.subList(i + 1, nums.size()));
+
+            int tmp = nums.get(i);
 //            System.out.println("index=" + i);
 //            System.out.println(" - before=" + nums);
 //            System.out.println(" - val=" + val + " (" + left + " * " + nums.get(i) + " * " + right + ")");
-//            System.out.println(" - after=" + newNums);
-            max = Math.max(max, maxCoins(newNums, memo) + val);
+            nums.remove(i);
+//            System.out.println(" - after=" + nums);
+            max = Math.max(max, maxCoins(nums, memo) + val);
+            nums.add(i, tmp);
         }
         memo.put(key, max);
         return max;
@@ -52,9 +53,9 @@ public class Problem312 {
 
     public static void main(String[] args) {
         Problem312 prob = new Problem312();
-//        System.out.println(prob.maxCoins(new int[]{3, 1, 5, 8})); // 167
+        System.out.println(prob.maxCoins(new int[]{3, 1, 5, 8})); // 167
 //        System.out.println(prob.maxCoins(new int[]{2, 4, 8})); // 88
 //        System.out.println(prob.maxCoins(new int[]{2, 4, 8, 4, 0, 7, 8, 9, 1, 2, 4, 7, 1, 7, 3, 6})); // 3304
-        System.out.println(prob.maxCoins(new int[]{8, 2, 6, 8, 9, 8, 1, 4, 1, 5, 3, 0, 7, 7, 0, 4, 2})); // 3414
+//        System.out.println(prob.maxCoins(new int[]{8, 2, 6, 8, 9, 8, 1, 4, 1, 5, 3, 0, 7, 7, 0, 4, 2})); // 3414
     }
 }
