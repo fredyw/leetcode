1209dde9fffa21eccabdd35352fff4601f4ed54f
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-11-30 14:30:31 -0600

    Solve problem 398

--- src/main/java/leetcode/Problem398.java
+++ src/main/java/leetcode/Problem398.java
@@ -1,22 +1,30 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
 /**
  * https://leetcode.com/problems/random-pick-index/
  */
 public class Problem398 {
     public static class Solution {
+        private final Map<Integer, List<Integer>> map = new HashMap<>();
+        private final Random random = new Random();
 
         public Solution(int[] nums) {
-
+            for (int i = 0; i < nums.length; i++) {
+                map.putIfAbsent(nums[i], new ArrayList<>());
+                map.get(nums[i]).add(i);
+            }
         }
 
         public int pick(int target) {
-            // TODO
-            return 0;
+            List<Integer> indices = map.get(target);
+            int idx = random.nextInt(indices.size());
+            return indices.get(idx);
         }
-    }
-
-    public static void main(String[] args) {
-        // TODO
     }
 }
