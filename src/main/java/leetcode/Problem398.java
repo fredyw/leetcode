3c040ad2b3ef08999aaa51a9951e9066a6e43a1b
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-11-30 00:28:55 -0600

    Skeleton for problem 398

---
+++ src/main/java/leetcode/Problem398.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/random-pick-index/
+ */
+public class Problem398 {
+    public static class Solution {
+
+        public Solution(int[] nums) {
+
+        }
+
+        public int pick(int target) {
+            // TODO
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        // TODO
+    }
+}
