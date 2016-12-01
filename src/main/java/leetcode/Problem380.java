4c7cd0795be56298335669877e966dab65518a93
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-12-01 17:35:37 -0600

    Skeleton for problem 380

---
+++ src/main/java/leetcode/Problem380.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/insert-delete-getrandom-o1/
+ */
+public class Problem380 {
+    public static class RandomizedSet {
+
+        /** Initialize your data structure here. */
+        public RandomizedSet() {
+
+        }
+
+        /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+        public boolean insert(int val) {
+            return false;
+        }
+
+        /** Removes a value from the set. Returns true if the set contained the specified element. */
+        public boolean remove(int val) {
+            return false;
+        }
+
+        /** Get a random element from the set. */
+        public int getRandom() {
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
