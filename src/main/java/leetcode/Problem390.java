37006089d28fa31d4abb1c45ff74a005d1da162d
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-12-01 22:28:11 -0600

    Update problem 390

--- src/main/java/leetcode/Problem390.java
+++ src/main/java/leetcode/Problem390.java
@@ -1,10 +1,18 @@
 package leetcode;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+
 /**
  * https://leetcode.com/problems/elimination-game/
  */
 public class Problem390 {
     public int lastRemaining(int n) {
+        LinkedList<Integer> list = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            list.add(i);
+        }
+        Iterator<Integer> iterator = list.iterator();
         // TODO
         return 0;
     }
