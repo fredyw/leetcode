133d7dd36a62b3e8a1e504056cc0afa2af5bbcbc
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-12-01 18:19:57 -0600

    Skeleton for problem 390

---
+++ src/main/java/leetcode/Problem390.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/elimination-game/
+ */
+public class Problem390 {
+    public int lastRemaining(int n) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem390 prob = new Problem390();
+        System.out.println(prob.lastRemaining(9)); // 6
+    }
+}
