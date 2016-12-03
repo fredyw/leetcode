34da268bf179ed10a95862392062baec07e6ae5c
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-12-02 20:32:49 -0600

    Update problem 390

--- src/main/java/leetcode/Problem390.java
+++ src/main/java/leetcode/Problem390.java
@@ -13,12 +13,33 @@ public class Problem390 {
             list.add(i);
         }
         Iterator<Integer> iterator = list.iterator();
-        // TODO
-        return 0;
+        Integer last = null;
+        boolean delete = true;
+        boolean asc = true;
+        while (iterator.hasNext()) {
+            while (iterator.hasNext()) {
+                last = iterator.next();
+                if (delete) {
+                    iterator.remove();
+                }
+                delete = !delete;
+            }
+            if (asc) {
+                iterator = list.descendingIterator();
+            } else {
+                iterator = list.iterator();
+            }
+            asc = !asc;
+            delete = true;
+        }
+        return last;
     }
 
     public static void main(String[] args) {
         Problem390 prob = new Problem390();
         System.out.println(prob.lastRemaining(9)); // 6
+        System.out.println(prob.lastRemaining(10)); // 8
+        System.out.println(prob.lastRemaining(5)); // 2
+        System.out.println(prob.lastRemaining(10000000)); // 6150102
     }
 }
