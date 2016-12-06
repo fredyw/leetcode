2d5537ce8c424f98970251813163f8d71e5ebe47
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-12-06 00:49:57 -0600

    Skeleton for problem 434

---
+++ src/main/java/leetcode/Problem434.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/number-of-segments-in-a-string/
+ */
+public class Problem434 {
+    public int countSegments(String s) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem434 prob = new Problem434();
+        System.out.println(prob.countSegments("Hello, my name is John")); // 5
+    }
+}
