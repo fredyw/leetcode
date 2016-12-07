ce2e98e3dab034b4a42afb6ed6b33b603c5b4c7e
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-12-07 00:07:47 -0600

    Skeleton for problem 395

---
+++ src/main/java/leetcode/Problem395.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/longest-substring-with-at-least-k-repeating-characters/
+ */
+public class Problem395 {
+    public int longestSubstring(String s, int k) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem395 prob = new Problem395();
+        System.out.println(prob.longestSubstring("aaabb", 3)); // 3
+        System.out.println(prob.longestSubstring("ababbc", 2)); // 5
+    }
+}
