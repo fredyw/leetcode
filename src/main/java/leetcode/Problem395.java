1ffb9ede1f4880ccc28ff59584dca80442575bba
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-12-07 00:27:59 -0600

    Update problem 395

--- src/main/java/leetcode/Problem395.java
+++ src/main/java/leetcode/Problem395.java
@@ -13,5 +13,10 @@ public class Problem395 {
         Problem395 prob = new Problem395();
         System.out.println(prob.longestSubstring("aaabb", 3)); // 3
         System.out.println(prob.longestSubstring("ababbc", 2)); // 5
+        System.out.println(prob.longestSubstring("aaabcb", 3)); // 3
+        System.out.println(prob.longestSubstring("aaabcbc", 2)); // 7
+        System.out.println(prob.longestSubstring("aadabcbc", 2)); // 4
+        System.out.println(prob.longestSubstring("aadabcbcd", 2)); // 9
+        System.out.println(prob.longestSubstring("aadabdbbdc", 2)); // 9
     }
 }
