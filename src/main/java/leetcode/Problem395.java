c9bebfb39e83e4b58f7b3a1cf94ba203bc621ff3
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-12-08 00:12:32 -0600

    Update problem 395

--- src/main/java/leetcode/Problem395.java
+++ src/main/java/leetcode/Problem395.java
@@ -8,40 +8,30 @@ import java.util.Map;
  */
 public class Problem395 {
     public int longestSubstring(String s, int k) {
-        return longestSubstring(s, k, 0, s.length(), new HashMap<>());
-    }
-
-    private static int longestSubstring(String s, int k, int left, int right,
-                                        Map<String, Integer> memo) {
-        if (left >= right) {
-            return 0;
-        }
-        String key = left + "|" + right;
-        if (memo.containsKey(key)) {
-            return memo.get(key);
-        }
-        String sub = s.substring(left, right);
-        Map<Character, Integer> map = new HashMap<>();
-        for (char c : sub.toCharArray()) {
-            if (!map.containsKey(c)) {
-                map.put(c, 1);
-            } else {
-                map.put(c, map.get(c) + 1);
+        int max = 0;
+        for (int i = 0; i < s.length(); i++) {
+            String sub = "";
+            Map<Character, Integer> map = new HashMap<>();
+            for (int j = i; j < s.length(); j++) {
+                char c = s.charAt(j);
+                sub += c;
+                if (!map.containsKey(c)) {
+                    map.put(c, 1);
+                } else {
+                    map.put(c, map.get(c) + 1);
+                }
+                boolean valid = true;
+                for (int val : map.values()) {
+                    if (val < k) {
+                        valid = false;
+                        break;
+                    }
+                }
+                if (valid) {
+                    max = Math.max(max, sub.length());
+                }
             }
         }
-        boolean valid = true;
-        for (int val : map.values()) {
-            if (val < k) {
-                valid = false;
-                break;
-            }
-        }
-        int a = (!valid) ? 0 : sub.length();
-        int b = longestSubstring(s, k, left + 1, right, memo);
-        int c = longestSubstring(s, k, left, right - 1, memo);
-        int d = longestSubstring(s, k, left + 1, right - 1, memo);
-        int max = Math.max(a, Math.max(b, Math.max(c, d)));
-        memo.put(key, max);
         return max;
     }
 
@@ -54,5 +44,6 @@ public class Problem395 {
         System.out.println(prob.longestSubstring("aadabcbc", 2)); // 4
         System.out.println(prob.longestSubstring("aadabcbcd", 2)); // 9
         System.out.println(prob.longestSubstring("aadabdbbdc", 2)); // 9
+        System.out.println(prob.longestSubstring("bababababababababababababababababaabababababaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbcbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", 30)); // 216
     }
 }
