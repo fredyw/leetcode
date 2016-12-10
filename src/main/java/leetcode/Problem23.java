d5fca82833adb61aa151e3968b22ff2827286ad1
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-12-09 22:17:28 -0600

    Solve problem 23

--- src/main/java/leetcode/Problem23.java
+++ src/main/java/leetcode/Problem23.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.PriorityQueue;
+
 /**
  * https://leetcode.com/problems/merge-k-sorted-lists/
  */
@@ -14,102 +16,25 @@ public class Problem23 {
     }
 
     public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> queue = new PriorityQueue<>((a, b) -> Integer.compare(a.val, b.val));
+        for (ListNode node : lists) {
+            for (ListNode n = node; n != null; n = n.next) {
+                queue.add(n);
+            }
+        }
         ListNode result = null;
-        for (ListNode n : lists) {
+        ListNode resultHead = null;
+        while (!queue.isEmpty()) {
+            ListNode n = queue.remove();
+            n.next = null;
             if (result == null) {
                 result = n;
-            } else {
-                result = merge(result, n);
-            }
-        }
-        return result;
-    }
-
-    private ListNode merge(ListNode node1, ListNode node2) {
-        ListNode n1 = node1;
-        ListNode n2 = node2;
-        ListNode resultHead = null;
-        ListNode result = null;
-        while (true) {
-            while (n1 != null && n2 != null && n1.val <= n2.val) {
-                if (result == null) {
-                    result = n1;
-                    resultHead = result;
-                } else {
-                    result.next = n1;
-                    result = result.next;
-                }
-                n1 = n1.next;
-            }
-            if (n1 == null) {
-                break;
-            }
-            while (n1 != null && n2 != null && n1.val > n2.val) {
-                if (result == null) {
-                    result = n2;
-                    resultHead = result;
-                } else {
-                    result.next = n2;
-                    result = result.next;
-                }
-                n2 = n2.next;
-            }
-            if (n2 == null) {
-                break;
-            }
-        }
-        while (n1 != null) {
-            if (result == null) {
-                result = n1;
                 resultHead = result;
             } else {
-                result.next = n1;
+                result.next = n;
                 result = result.next;
             }
-            n1 = n1.next;
-        }
-        while (n2 != null) {
-            if (result == null) {
-                result = n2;
-                resultHead = result;
-            } else {
-                result.next = n2;
-                result = result.next;
-            }
-            n2 = n2.next;
         }
         return resultHead;
-    }
-
-    private static void print(ListNode node) {
-        for (ListNode n = node; n != null; n = n.next) {
-            System.out.print(n.val + " ");
-        }
-        System.out.println();
-    }
-
-    public static void main(String[] args) {
-        Problem23 prob = new Problem23();
-
-        ListNode list1 = new ListNode(1);
-        list1.next = new ListNode(3);
-        list1.next.next = new ListNode(5);
-
-        ListNode list2 = new ListNode(2);
-        list2.next = new ListNode(7);
-        list2.next.next = new ListNode(9);
-        list2.next.next.next = new ListNode(10);
-        list2.next.next.next.next = new ListNode(12);
-
-        ListNode list3 = new ListNode(4);
-        list3.next = new ListNode(6);
-        list3.next.next = new ListNode(8);
-        list3.next.next.next = new ListNode(11);
-
-        ListNode result = prob.mergeKLists(new ListNode[]{list1, list2, list3});
-        print(result);
-
-        result = prob.mergeKLists(new ListNode[]{list1, null, list3});
-        print(result);
     }
 }
