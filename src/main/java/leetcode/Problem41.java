42fb635266cfbb42b07705ca9885205fbe0637bb
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-12-09 22:23:03 -0600

    Skeleton for problem 41

---
+++ src/main/java/leetcode/Problem41.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/first-missing-positive/
+ */
+public class Problem41 {
+    public int firstMissingPositive(int[] nums) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem41 prob = new Problem41();
+        System.out.println(prob.firstMissingPositive(new int[]{1, 2, 0})); // 3
+        System.out.println(prob.firstMissingPositive(new int[]{3, 4, -1, 1})); // 2
+    }
+}
