3ba4772f6cc44d54b6a686ec44a0d34be1796c27
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-12-11 23:39:12 -0600

    Skeleton for problem 475

---
+++ src/main/java/leetcode/Problem475.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/heaters/
+ */
+public class Problem475 {
+    public int findRadius(int[] houses, int[] heaters) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem475 prob = new Problem475();
+        System.out.println(prob.findRadius(new int[]{1, 2, 3}, new int[]{2})); // 1
+        System.out.println(prob.findRadius(new int[]{1, 2, 3, 4}, new int[]{1, 4})); // 1
+    }
+}
