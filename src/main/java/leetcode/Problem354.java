73248344d82c2bfd0fcb558e288b88cad1312916
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-12-19 13:33:26 -0600

    Skeleton for problem 354

---
+++ src/main/java/leetcode/Problem354.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/russian-doll-envelopes/
+ */
+public class Problem354 {
+    public int maxEnvelopes(int[][] envelopes) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem354 prob = new Problem354();
+        System.out.println(prob.maxEnvelopes(new int[][]{
+            {5, 4},
+            {6, 4},
+            {6, 7},
+            {2, 3}
+        }));
+    }
+}
