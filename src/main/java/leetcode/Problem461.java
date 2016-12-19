3b689a399853a527c44ff0badf9cf623c418b531
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-12-19 13:26:20 -0600

    Skeleton for problem 461

---
+++ src/main/java/leetcode/Problem461.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/hamming-distance/
+ */
+public class Problem461 {
+    public int hammingDistance(int x, int y) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem461 prob = new Problem461();
+        System.out.println(prob.hammingDistance(1, 4)); // 2
+    }
+}
