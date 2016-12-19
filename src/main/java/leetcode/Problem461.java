36314a507fa8afe580fee926c246e4c76689ea07
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-12-19 13:30:23 -0600

    Solve problem 461

--- src/main/java/leetcode/Problem461.java
+++ src/main/java/leetcode/Problem461.java
@@ -5,12 +5,12 @@ package leetcode;
  */
 public class Problem461 {
     public int hammingDistance(int x, int y) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem461 prob = new Problem461();
-        System.out.println(prob.hammingDistance(1, 4)); // 2
+        int xor = x ^ y;
+        int result = 0;
+        while (xor > 0) {
+            result += xor & 1;
+            xor >>= 1;
+        }
+        return result;
     }
 }
