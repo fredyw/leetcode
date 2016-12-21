daed1bdfb2a25cc5e68117720019da61752221f4
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-12-21 16:33:32 -0600

    Skeleton for problem 448

---
+++ src/main/java/leetcode/Problem448.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+ */
+public class Problem448 {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        // TODO
+        return new ArrayList<>();
+    }
+
+    public static void main(String[] args) {
+        Problem448 prob = new Problem448();
+        System.out.println(prob.findDisappearedNumbers(new int[]{4, 3, 2, 7, 8, 2, 3, 1})); // [5, 6]
+    }
+}
