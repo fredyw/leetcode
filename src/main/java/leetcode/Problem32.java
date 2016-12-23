b24f2a2876229f2f2e6ad96933330a1d35f29c17
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-12-22 18:56:36 -0600

    Skeleton for problem 32

---
+++ src/main/java/leetcode/Problem32.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/longest-valid-parentheses/
+ */
+public class Problem32 {
+    public int longestValidParentheses(String s) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem32 prob = new Problem32();
+        System.out.println(prob.longestValidParentheses("(()")); // 2
+        System.out.println(prob.longestValidParentheses(")()())")); // 4
+    }
+}
