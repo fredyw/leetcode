a95b02bfe929f44e751e6c8b56427acf3f577af5
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-12-28 01:29:10 -0600

    Skeleton for problem 442

---
+++ src/main/java/leetcode/Problem442.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/find-all-duplicates-in-an-array/
+ */
+public class Problem442 {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        // TODO
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Problem442 prob = new Problem442();
+        System.out.println(prob.findDuplicates(new int[]{4, 3, 2, 7, 8, 2, 3, 1})); // [2, 3]
+    }
+}
