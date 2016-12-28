2e9ef567a8144b44e200d1262a926c9e69cb271a
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-12-28 00:25:04 -0600

    Update problem 32

--- src/main/java/leetcode/Problem32.java
+++ src/main/java/leetcode/Problem32.java
@@ -1,58 +1,47 @@
 package leetcode;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * https://leetcode.com/problems/longest-valid-parentheses/
  */
 public class Problem32 {
     public int longestValidParentheses(String s) {
-        return longestValidParentheses(s, 0, s.length(), new HashMap<>());
-    }
-
-    private static int longestValidParentheses(String s, int i, int j, Map<String, Integer> memo) {
-        if (i >= j) {
-            return 0;
-        }
-        String key = i + "|" + j;
-        if (memo.containsKey(key)) {
-            return memo.get(key);
-        }
-        String sub = s.substring(i, j);
-        int max = 0;
-        if (isValid(sub)) {
-            max = sub.length();
-        } else {
-            int a = longestValidParentheses(s, i + 1, j, memo);
-            int b = 0;
-            if (a < sub.length() - 1) {
-                b = longestValidParentheses(s, i, j - 1, memo);
-            }
-            int c = 0;
-            if (a < sub.length() - 2 && b < sub.length() - 2) {
-                c = longestValidParentheses(s, i + 1, j - 1, memo);
-            }
-            max = Math.max(a, Math.max(b, c));
-        }
-        memo.put(key, max);
-        return max;
-    }
-
-    private static boolean isValid(String sub) {
-        int count = 0;
-        for (int i = 0; i < sub.length(); i++) {
-            char c = sub.charAt(i);
-            if (c == '(') {
-                count++;
+        int[] nCloses = new int[s.length()];
+        int n = 0;
+        for (int j = s.length() - 1; j >= 0; j--) {
+            char c = s.charAt(j);
+            if (c == ')') {
+                nCloses[j] = ++n;
             } else {
-                if (count <= 0) {
-                    return false;
-                }
-                count--;
+                nCloses[j] = n;
             }
         }
-        return count == 0;
+//        System.out.println(Arrays.toString(nCloses));
+        int max = 0;
+        int i = 0;
+        while (i < s.length()) {
+            int count = 0;
+            int nextI = i + 1;
+            for (int j = i; j < s.length(); j++) {
+                char c = s.charAt(j);
+                if (c == '(') {
+                    count++;
+                    if (nCloses[j] < count) {
+                        break;
+                    }
+                } else {
+                    if (count <= 0) {
+                        break;
+                    }
+                    count--;
+                }
+                if (count == 0) {
+                    max = Math.max(max, j - i + 1);
+                    nextI = j + 1;
+                }
+            }
+            i = nextI;
+        }
+        return max;
     }
 
     public static void main(String[] args) {
@@ -62,7 +51,10 @@ public class Problem32 {
         System.out.println(prob.longestValidParentheses("(")); // 0
         System.out.println(prob.longestValidParentheses("()(())")); // 6
         System.out.println(prob.longestValidParentheses("((())))")); // 6
-        System.out.println(prob.longestValidParentheses("()(()")); // 2
+        System.out.println(prob.longestValidParentheses("()(()(")); // 2
+        System.out.println(prob.longestValidParentheses(")()()(")); // 4
+        System.out.println(prob.longestValidParentheses("()((())")); // 4
+        System.out.println(prob.longestValidParentheses("()(())")); // 6
         System.out.println(prob.longestValidParentheses("()()(()(((()))()))()))))()(())))()(()())()()()))())))())())))(()()()))))()((()(())(())))((()())(()()()((((()(())))))((()()((())(())(()(())))))()()())(())(()())((()())()(((())))()(()()))")); // 96
         System.out.println(prob.longestValidParentheses(")((()()((((()((((())(())(((()((((())(()((())())())(()))))))))))))(())(()())((())))))(((((()))())))(()()))(())))((()()(()()()()())))()(())((())()(())(((()())((())))(())))()())))))())()())())(((()")); // 108
     }
