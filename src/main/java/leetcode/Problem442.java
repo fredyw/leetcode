9b46108d0f35f6bd45473460168d03d441006206
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-12-29 01:33:51 -0600

    Solve problem 442

--- src/main/java/leetcode/Problem442.java
+++ src/main/java/leetcode/Problem442.java
@@ -9,12 +9,28 @@ import java.util.List;
 public class Problem442 {
     public List<Integer> findDuplicates(int[] nums) {
         List<Integer> result = new ArrayList<>();
-        // TODO
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1) {
+                int to = nums[i] - 1;
+                nums[i] = 0;
+                int n = swap(nums, to);
+                if (n != 0) {
+                    result.add(n);
+                }
+            }
+        }
         return result;
     }
 
-    public static void main(String[] args) {
-        Problem442 prob = new Problem442();
-        System.out.println(prob.findDuplicates(new int[]{4, 3, 2, 7, 8, 2, 3, 1})); // [2, 3]
+    private static int swap(int[] nums, int from) {
+        if (nums[from] == from + 1) {
+            return nums[from];
+        }
+        int val = nums[from];
+        nums[from] = from + 1;
+        if (val == 0) {
+            return 0;
+        }
+        return swap(nums, val - 1);
     }
 }
