3d9b9e66bc394c8cd8e305754ef0b40f996a24b4
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-12-29 13:31:54 -0600

    Update problem 445

--- src/main/java/leetcode/Problem445.java
+++ src/main/java/leetcode/Problem445.java
@@ -14,8 +14,31 @@ public class Problem445 {
     }
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        Num n1 = getNumber(l1);
+        Num n2 = getNumber(l2);
+        long sum = n1.n + n2.n;
         // TODO
         return null;
+    }
+
+    private static class Num {
+        private long n;
+        private int digit;
+
+        public Num(long n, int digit) {
+            this.n = n;
+            this.digit = digit;
+        }
+    }
+
+    private static Num getNumber(ListNode node) {
+        if (node == null) {
+            return new Num(0, 1);
+        }
+        Num num = getNumber(node.next);
+        num.n += (node.val * num.digit);
+        num.digit *= 10;
+        return num;
     }
 
     public static void main(String[] args) {
