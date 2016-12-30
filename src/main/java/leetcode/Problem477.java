aa6f7d9530da0ab1b7149cd2373e4b0ccaa029ac
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-12-29 21:19:05 -0600

    Skeleton for problem 477

---
+++ src/main/java/leetcode/Problem477.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/total-hamming-distance/
+ */
+public class Problem477 {
+    public int totalHammingDistance(int[] nums) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem477 prob = new Problem477();
+        System.out.println(prob.totalHammingDistance(new int[]{4, 14, 2})); // 6
+    }
+}
