69f9802d2f56ab30b98e940d0132dfe7eab22cad
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-12-29 22:26:33 -0600

    Solve problem 477

--- src/main/java/leetcode/Problem477.java
+++ src/main/java/leetcode/Problem477.java
@@ -5,12 +5,14 @@ package leetcode;
  */
 public class Problem477 {
     public int totalHammingDistance(int[] nums) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem477 prob = new Problem477();
-        System.out.println(prob.totalHammingDistance(new int[]{4, 14, 2})); // 6
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            int count = 0;
+            for (int j = 0; j < nums.length; j++) {
+                count += 1 & (nums[j] >> i);
+            }
+            result += (nums.length - count) * count;
+        }
+        return result;
     }
 }
