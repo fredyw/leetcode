688f49f501b1c82602a887eb610450413084f7e5
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-12-30 19:21:08 -0600

    Solve problem 449 (Bye 2016, Happy New Year 2017!)

--- src/main/java/leetcode/Problem449.java
+++ src/main/java/leetcode/Problem449.java
@@ -1,5 +1,9 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
 /**
  * https://leetcode.com/problems/serialize-and-deserialize-bst/
  */
@@ -12,51 +16,78 @@ public class Problem449 {
         TreeNode(int x) {
             val = x;
         }
+
+        @Override
+        public String toString() {
+            return Integer.toString(val);
+        }
+    }
+
+    private static class LevelNode {
+        private final int level;
+        private final TreeNode node;
+
+        public LevelNode(int level, TreeNode node) {
+            this.level = level;
+            this.node = node;
+        }
+
+        @Override
+        public String toString() {
+            return level + ":" + ((node == null) ? "#" : node.val);
+        }
     }
 
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-        // TODO
-        return null;
+        StringBuilder sb = new StringBuilder();
+        LinkedList<LevelNode> queue = new LinkedList<>();
+        queue.add(new LevelNode(1, root));
+        while (!queue.isEmpty()) {
+            LevelNode current = queue.remove();
+            if (sb.length() > 0) {
+                sb.append("|");
+            }
+            sb.append(current);
+            if (current.node == null) {
+                continue;
+            }
+            queue.add(new LevelNode(current.level + 1, current.node.left));
+            queue.add(new LevelNode(current.level + 1, current.node.right));
+        }
+        return sb.toString();
     }
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        // TODO
-        return null;
+        Map<Integer, LinkedList<String>> map = new HashMap<>();
+        String[] nodes = data.split("\\|");
+        for (String node : nodes) {
+            String[] levelNode = node.split(":");
+            int level = Integer.parseInt(levelNode[0]);
+            if (!map.containsKey(level)) {
+                LinkedList list = new LinkedList();
+                list.add(levelNode[1]);
+                map.put(level, list);
+            } else {
+                map.get(level).add(levelNode[1]);
+            }
+        }
+        return deserialize(map, 1);
     }
 
-    private static boolean equals(TreeNode n1, TreeNode n2) {
-        if (n1 == null && n2 != null) {
-            return false;
+    private static TreeNode deserialize(Map<Integer, LinkedList<String>> map, int level) {
+        LinkedList<String> list = map.get(level);
+        if (list == null || list.isEmpty()) {
+            return null;
         }
-        if (n1 != null && n2 == null) {
-            return false;
+        String val = list.remove();
+        if (val.equals("#")) {
+            return null;
         }
-        if (n1 == null && n2 == null) {
-            return true;
-        }
-        if (n1.val != n2.val) {
-            return false;
-        }
-        return equals(n1.left, n2.left) && equals(n1.right, n2.right);
-    }
-
-    public static void main(String[] args) {
-        Problem449 prob = new Problem449();
-
-        TreeNode root = new TreeNode(5);
-        root.left = new TreeNode(2);
-        root.left.left = new TreeNode(1);
-        root.left.right = new TreeNode(3);
-        root.left.right.right = new TreeNode(4);
-        root.right = new TreeNode(9);
-        root.right.left = new TreeNode(6);
-        root.right.left.right = new TreeNode(7);
-        root.right.left.right.right = new TreeNode(8);
-        root.right.right = new TreeNode(10);
-
-        TreeNode newRoot = prob.deserialize(prob.serialize(root));
-        System.out.println(equals(root, newRoot));
+        TreeNode node = new TreeNode(Integer.parseInt(val));
+        node.left = deserialize(map, level + 1);
+        node.right = deserialize(map, level + 1);
+        return node;
     }
 }
