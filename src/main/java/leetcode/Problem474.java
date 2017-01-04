404efab15bf2fe327a7f729be44b78193ca9f5f7
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-01-04 12:16:22 -0600

    Skeleton for problem 474

---
+++ src/main/java/leetcode/Problem474.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/ones-and-zeroes/
+ */
+public class Problem474 {
+    public int findMaxForm(String[] strs, int m, int n) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem474 prob = new Problem474();
+        System.out.println(prob.findMaxForm(new String[]{"10", "0001", "111001", "1", "0"}, 5, 3)); // 4
+        System.out.println(prob.findMaxForm(new String[]{"10", "0", "1"}, 1, 1)); // 2
+    }
+}
