40d497640a75accd34fdd01c929ad4ad1ad04625
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-01-04 00:03:16 -0600

    Solve problem 451

--- src/main/java/leetcode/Problem451.java
+++ src/main/java/leetcode/Problem451.java
@@ -1,18 +1,34 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
 /**
  * https://leetcode.com/problems/sort-characters-by-frequency/
  */
 public class Problem451 {
     public String frequencySort(String s) {
-        // TODO
-        return null;
-    }
-
-    public static void main(String[] args) {
-        Problem451 prob = new Problem451();
-        System.out.println(prob.frequencySort("tree")); // eert
-        System.out.println(prob.frequencySort("cccaaa")); // cccaaa
-        System.out.println(prob.frequencySort("Aabb")); // bbAa
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (!map.containsKey(c)) {
+                map.put(c, 1);
+            } else {
+                map.put(c, map.get(c) + 1);
+            }
+        }
+        List<Entry<Character, Integer>> entries = new ArrayList<>(map.entrySet());
+        Collections.sort(entries, (a, b) -> Integer.compare(b.getValue(), a.getValue()));
+        StringBuilder sb = new StringBuilder();
+        for (Entry<Character, Integer> entry : entries) {
+            for (int i = 0; i < entry.getValue(); i++) {
+                sb.append(entry.getKey());
+            }
+        }
+        return sb.toString();
     }
 }
