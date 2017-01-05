5299368f0436e66cdf0d0c2cee588a5edcf58b9a
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-01-05 14:39:23 -0600

    Solve problem 450

--- src/main/java/leetcode/Problem450.java
+++ src/main/java/leetcode/Problem450.java
@@ -12,11 +12,6 @@ public class Problem450 {
         TreeNode(int x) {
             val = x;
         }
-
-        @Override
-        public String toString() {
-            return Integer.toString(val);
-        }
     }
 
     public TreeNode deleteNode(TreeNode root, int key) {
@@ -100,90 +95,12 @@ public class Problem450 {
     private static TreeNode getSmallest(TreeNode node, TreeNode parent, Direction direction) {
         if (node.left == null) {
             if (direction == Direction.LEFT) {
-                parent.left = null;
+                parent.left = node.right;
             } else {
-                parent.right = null;
+                parent.right = node.right;
             }
             return node;
         }
         return getSmallest(node.left, node, Direction.LEFT);
-    }
-
-    private static TreeNode getLargest(TreeNode node, TreeNode parent, Direction direction) {
-        if (node.right == null) {
-            if (direction == Direction.LEFT) {
-                parent.left = null;
-            } else {
-                parent.right = null;
-            }
-            return node;
-        }
-        return getLargest(node.right, node, Direction.RIGHT);
-    }
-
-    public static void main(String[] args) {
-        Problem450 prob = new Problem450();
-
-//        TreeNode root = new TreeNode(5);
-//        root.left = new TreeNode(3);
-//        root.left.left = new TreeNode(2);
-//        root.left.left.left = new TreeNode(1);
-//        root.left.right = new TreeNode(4);
-//        root.right = new TreeNode(6);
-//        root.right.right = new TreeNode(7);
-//
-//        root = prob.deleteNode(root, 3);
-//        root = prob.deleteNode(root, 5);
-//        root = prob.deleteNode(root, 1);
-//        System.out.println(root);
-//        System.out.println(root.left);
-//        System.out.println(root.left.left);
-//        System.out.println(root.left.left.left);
-//        System.out.println(root.left.right);
-//        System.out.println(root.right);
-//        System.out.println(root.right.right);
-
-//        TreeNode root = new TreeNode(3);
-//        root.left = new TreeNode(1);
-//        root.left.right = new TreeNode(2);
-//        root.right = new TreeNode(4);
-//
-//        root = prob.deleteNode(root, 1);
-//        System.out.println(root);
-//        System.out.println(root.left);
-//        System.out.println(root.right);
-
-        TreeNode root = new TreeNode(3);
-        root.right = new TreeNode(5);
-        root.right.left = new TreeNode(4);
-        root.right.right = new TreeNode(10);
-        root.right.right.left = new TreeNode(7);
-        root.right.right.left.right = new TreeNode(8);
-        root.right.right.left.right.right = new TreeNode(9);
-        root.right.right.right = new TreeNode(11);
-
-        root = prob.deleteNode(root, 5);
-        System.out.println(root);
-        System.out.println(root.right);
-        System.out.println(root.right.right);
-        System.out.println(root.right.right.left);
-        System.out.println(root.right.right.left.right);
-        System.out.println(root.right.right.right);
-
-//        TreeNode root = new TreeNode(5);
-//        root.right = new TreeNode(10);
-//        root.right.left = new TreeNode(7);
-//        root.right.right = new TreeNode(15);
-//        root.right.right.left = new TreeNode(12);
-//        root.right.right.left.right = new TreeNode(13);
-//        root.right.right.right = new TreeNode(16);
-//
-//        root = prob.deleteNode(root, 15);
-//        System.out.println(root);
-//        System.out.println(root.right);
-//        System.out.println(root.right.left);
-//        System.out.println(root.right.right);
-//        System.out.println(root.right.right.left);
-//        System.out.println(root.right.right.left.right);
     }
 }
