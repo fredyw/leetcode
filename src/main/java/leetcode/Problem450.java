5375861aca9c3e7dc26cc7c6a2a7e607ee33aab2
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-01-05 13:06:50 -0600

    Update problem 450

--- src/main/java/leetcode/Problem450.java
+++ src/main/java/leetcode/Problem450.java
@@ -20,41 +20,77 @@ public class Problem450 {
     }
 
     public TreeNode deleteNode(TreeNode root, int key) {
-        if (root == null || (root.left == null && root.right == null && root.val == key)) {
-            return null;
-        }
-        deleteNode(root, null, Direction.MIDDLE, key);
-        return root;
+        NodeRef ref = new NodeRef(root);
+        deleteNode(root, null, Direction.MIDDLE, key, ref);
+        return ref.node;
     }
 
     private enum Direction {
         LEFT, RIGHT, MIDDLE
     }
 
-    private static void deleteNode(TreeNode node, TreeNode parent, Direction direction, int key) {
+    private static class NodeRef {
+        private TreeNode node;
+
+        public NodeRef(TreeNode node) {
+            this.node = node;
+        }
+    }
+
+    private static void deleteNode(TreeNode node, TreeNode parent, Direction direction, int key, NodeRef ref) {
         if (node == null) {
             return;
         }
         if (key == node.val) {
             if (node.left == null && node.right == null) {
-                if (direction == Direction.LEFT) {
-                    parent.left = null;
+                if (parent != null) {
+                    if (direction == Direction.LEFT) {
+                        parent.left = null;
+                    } else {
+                        parent.right = null;
+                    }
                 } else {
-                    parent.right = null;
+                    ref.node = null;
                 }
             } else if (node.left == null && node.right != null) {
                 TreeNode smallest = getSmallest(node.right, node, Direction.RIGHT);
-                smallest.left = node.left;
-                parent.right = smallest;
+                if (node.left != null) {
+                    smallest.left = node.left;
+                }
+                if (node.right != null) {
+                    smallest.right = node.right;
+                }
+                if (parent != null) {
+                    if (direction == Direction.LEFT) {
+                        parent.left = smallest;
+                    } else {
+                        parent.right = smallest;
+                    }
+                } else {
+                    ref.node = smallest;
+                }
             } else {
                 TreeNode largest = getLargest(node.left, node, Direction.LEFT);
-                parent.left = largest;
-                largest.right = node.right;
+                if (parent != null) {
+                    if (direction == Direction.LEFT) {
+                        parent.left = largest;
+                    } else {
+                        parent.right = largest;
+                    }
+                } else {
+                    ref.node = largest;
+                }
+                if (node.left != null) {
+                    largest.left = node.left;
+                }
+                if (node.right != null) {
+                    largest.right = node.right;
+                }
             }
         } else if (key < node.val) {
-            deleteNode(node.left, node, Direction.LEFT, key);
+            deleteNode(node.left, node, Direction.LEFT, key, ref);
         } else {
-            deleteNode(node.right, node, Direction.RIGHT, key);
+            deleteNode(node.right, node, Direction.RIGHT, key, ref);
         }
     }
 
@@ -85,22 +121,34 @@ public class Problem450 {
     public static void main(String[] args) {
         Problem450 prob = new Problem450();
 
-        TreeNode root = new TreeNode(5);
-        root.left = new TreeNode(3);
-        root.left.left = new TreeNode(2);
-        root.left.left.left = new TreeNode(1);
-        root.left.right = new TreeNode(4);
-        root.right = new TreeNode(6);
-        root.right.right = new TreeNode(7);
+//        TreeNode root = new TreeNode(5);
+//        root.left = new TreeNode(3);
+//        root.left.left = new TreeNode(2);
+//        root.left.left.left = new TreeNode(1);
+//        root.left.right = new TreeNode(4);
+//        root.right = new TreeNode(6);
+//        root.right.right = new TreeNode(7);
 
-        root = prob.deleteNode(root, 5);
+//        root = prob.deleteNode(root, 3);
+//        root = prob.deleteNode(root, 5);
+//        root = prob.deleteNode(root, 1);
 
-        System.out.println(root);
+//        System.out.println(root);
 //        System.out.println(root.left);
 //        System.out.println(root.left.left);
 //        System.out.println(root.left.left.left);
 //        System.out.println(root.left.right);
 //        System.out.println(root.right);
 //        System.out.println(root.right.right);
+
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(1);
+        root.left.right = new TreeNode(2);
+        root.right = new TreeNode(4);
+
+        root = prob.deleteNode(root, 1);
+        System.out.println(root);
+        System.out.println(root.left);
+        System.out.println(root.right);
     }
 }
