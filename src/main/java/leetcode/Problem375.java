82d81b1006e6c283074d23c7ebd210feadba445b
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-01-10 00:01:01 -0600

    Skeleton for problem 375

---
+++ src/main/java/leetcode/Problem375.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/guess-number-higher-or-lower-ii/
+ */
+public class Problem375 {
+    public int getMoneyAmount(int n) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem375 prob = new Problem375();
+        System.out.println(prob.getMoneyAmount(10)); // 21
+    }
+}
