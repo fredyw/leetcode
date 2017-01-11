bb57aa79145b5439eaa97674c11176bcf7b35be4
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-01-10 19:10:02 -0600

    Skeleton for problem 476

---
+++ src/main/java/leetcode/Problem476.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/number-complement/
+ */
+public class Problem476 {
+    public int findComplement(int num) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem476 prob = new Problem476();
+        System.out.println(prob.findComplement(5)); // 2
+        System.out.println(prob.findComplement(1)); // 0
+    }
+}
