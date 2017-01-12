2ae49b8c194b10dbd80ab14b15e01346b7d3c072
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-01-12 01:02:21 -0600

    Solve problem 481

--- src/main/java/leetcode/Problem481.java
+++ src/main/java/leetcode/Problem481.java
@@ -1,16 +1,41 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * https://leetcode.com/problems/magical-string/
  */
 public class Problem481 {
     public int magicalString(int n) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem481 prob = new Problem481();
-        System.out.println(prob.magicalString(6)); // 3
+        if (n <= 0) {
+            return 0;
+        }
+        if (n <= 2) {
+            return 1;
+        }
+        int result = 1;
+        List<Integer> array = new ArrayList<>();
+        array.add(1);
+        array.add(2);
+        array.add(2);
+        for (int i = 2; array.size() < n; i++) {
+            int a = array.get(i);
+            int add = array.get(array.size() - 1);
+            if (add == 1) {
+                add = 2;
+            } else {
+                add = 1;
+                if (array.size() + (add * a) <= n) {
+                    result += (add * a);
+                } else {
+                    result++;
+                }
+            }
+            for (int j = 0; j < a; j++) {
+                array.add(add);
+            }
+        }
+        return result;
     }
 }
