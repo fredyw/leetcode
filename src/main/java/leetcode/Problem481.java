8fb9a88a8c587d89e98df9cc43de3d0c37a83505
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-01-12 00:17:16 -0600

    Skeleton for problem 481

---
+++ src/main/java/leetcode/Problem481.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/magical-string/
+ */
+public class Problem481 {
+    public int magicalString(int n) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem481 prob = new Problem481();
+        System.out.println(prob.magicalString(6)); // 3
+    }
+}
