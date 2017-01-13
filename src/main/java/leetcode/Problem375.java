a5541f0b471c4ebf8d112345bef76b13ebdb6e54
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-01-13 13:39:12 -0600

    Solve problem 375

--- src/main/java/leetcode/Problem375.java
+++ src/main/java/leetcode/Problem375.java
@@ -1,18 +1,30 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * https://leetcode.com/problems/guess-number-higher-or-lower-ii/
  */
 public class Problem375 {
     public int getMoneyAmount(int n) {
-        // TODO
-        return 0;
+        return getMoneyAmount(n, 1, n, new HashMap<>());
     }
 
-    public static void main(String[] args) {
-        Problem375 prob = new Problem375();
-        System.out.println(prob.getMoneyAmount(10)); // 16
-        System.out.println(prob.getMoneyAmount(15)); // 30
-        System.out.println(prob.getMoneyAmount(5)); // 6
+    private static int getMoneyAmount(int n, int start, int end, Map<String, Integer> memo) {
+        if (start >= end) {
+            return 0;
+        }
+        String key = start + "|" + end;
+        if (memo.containsKey(key)) {
+            return memo.get(key);
+        }
+        int min = Integer.MAX_VALUE;
+        for (int i = start; i <= end; i++) {
+            int max = Math.max(getMoneyAmount(n, start, i - 1, memo), getMoneyAmount(n, i + 1, end, memo)) + i;
+            min = Math.min(min, max);
+        }
+        memo.put(key, min);
+        return min;
     }
 }
