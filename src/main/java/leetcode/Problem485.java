f7d4d83e0778b47389f0ae1da98f79cd96cbdff5
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-01-17 23:04:38 -0600

    Skeleton for problem 485

---
+++ src/main/java/leetcode/Problem485.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/max-consecutive-ones/
+ */
+public class Problem485 {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem485 prob = new Problem485();
+        System.out.println(prob.findMaxConsecutiveOnes(new int[]{1, 1, 0, 1, 1, 1})); // 3
+    }
+}
