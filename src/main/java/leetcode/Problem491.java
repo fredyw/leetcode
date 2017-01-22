99243dc77f13aadaf799389547a938e331936831
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-01-21 23:55:50 -0600

    Update problem 491

--- src/main/java/leetcode/Problem491.java
+++ src/main/java/leetcode/Problem491.java
@@ -13,6 +13,8 @@ public class Problem491 {
 
     public static void main(String[] args) {
         Problem491 prob = new Problem491();
+        System.out.println(prob.findSubsequences(new int[]{1, 1, 1})); // [[1, 1], [1, 1, 1]]
+        System.out.println(prob.findSubsequences(new int[]{1, 2, 2})); // [[1, 2], [2, 2], [1, 2, 2]]
         System.out.println(prob.findSubsequences(new int[]{4, 6, 7, 7})); // [[4, 6], [4, 7], [4, 6, 7], [4, 6, 7, 7], [6, 7], [6, 7, 7], [7,7], [4,7,7]]
     }
 }
