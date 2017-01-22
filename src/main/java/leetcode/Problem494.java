8cf8a9222593bffb73ececbfa8c47e6f15c2abd4
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-01-21 23:02:48 -0600

    Skeleton for problem 494

---
+++ src/main/java/leetcode/Problem494.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/target-sum/
+ */
+public class Problem494 {
+    public int findTargetSumWays(int[] nums, int S) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem494 prob = new Problem494();
+        System.out.println(prob.findTargetSumWays(new int[]{1, 1, 1, 1, 1}, 3)); // 5
+    }
+}
