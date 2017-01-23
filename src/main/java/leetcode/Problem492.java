f61265cff3f28b39657fb0d393c1e0cb5a8ccb93
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-01-23 11:32:16 -0600

    Skeleton for problem 492

---
+++ src/main/java/leetcode/Problem492.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/construct-the-rectangle/
+ */
+public class Problem492 {
+    public int[] constructRectangle(int area) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem492 prob = new Problem492();
+        System.out.println(Arrays.toString(prob.constructRectangle(4))); // [2, 2]
+    }
+}
