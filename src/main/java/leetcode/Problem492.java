610e524e315b8d33c714a55abe9fa82dce3048a1
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-01-23 23:59:21 -0600

    Solve problem 492

--- src/main/java/leetcode/Problem492.java
+++ src/main/java/leetcode/Problem492.java
@@ -1,18 +1,23 @@
 package leetcode;
 
-import java.util.Arrays;
-
 /**
  * https://leetcode.com/problems/construct-the-rectangle/
  */
 public class Problem492 {
     public int[] constructRectangle(int area) {
-        // TODO
-        return null;
-    }
-
-    public static void main(String[] args) {
-        Problem492 prob = new Problem492();
-        System.out.println(Arrays.toString(prob.constructRectangle(4))); // [2, 2]
+        int[] result = new int[2];
+        double sqrt = Math.sqrt(area);
+        int i = (int) sqrt;
+        while (i <= area) {
+            double a = area / (double) i;
+            int j = (int) a;
+            if (a == j) {
+                result[0] = Math.max(j, i);
+                result[1] = Math.min(j, i);
+                break;
+            }
+            i++;
+        }
+        return result;
     }
 }
