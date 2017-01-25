4aac3f9993257a507337ba86c8e8879c3a088755
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-01-25 00:22:30 -0600

    Skeleton for problem 486

---
+++ src/main/java/leetcode/Problem486.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/predict-the-winner/
+ */
+public class Problem486 {
+    public boolean PredictTheWinner(int[] nums) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem486 prob = new Problem486();
+        System.out.println(prob.PredictTheWinner(new int[]{1, 5, 2})); // false
+        System.out.println(prob.PredictTheWinner(new int[]{1, 5, 233, 7})); // true
+    }
+}
