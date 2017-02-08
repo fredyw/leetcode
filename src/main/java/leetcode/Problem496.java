2dd3d9f9e5c94e5a227f78015573706f514ae502
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-02-07 23:06:26 -0600

    Update problem 496

--- src/main/java/leetcode/Problem496.java
+++ src/main/java/leetcode/Problem496.java
@@ -14,6 +14,8 @@ public class Problem496 {
     public static void main(String[] args) {
         Problem496 prob = new Problem496();
         System.out.println(Arrays.toString(prob.nextGreaterElement(new int[]{4, 1, 2}, new int[]{1, 3, 4, 2}))); // [-1,3,-1]
+        System.out.println(Arrays.toString(prob.nextGreaterElement(new int[]{4, 1, 3}, new int[]{1, 3, 2, 4}))); // [-1,3,4]
+        System.out.println(Arrays.toString(prob.nextGreaterElement(new int[]{4, 1, 3}, new int[]{4, 3, 2, 1}))); // [-1,-1,-1]
         System.out.println(Arrays.toString(prob.nextGreaterElement(new int[]{2, 4}, new int[]{1, 2, 3, 4}))); // [3,-1]
     }
 }
