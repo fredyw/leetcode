9bb507471c1018e349b465b847b70d4a3b34725e
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-02-07 23:38:49 -0600

    Solve problem 501

--- src/main/java/leetcode/Problem501.java
+++ src/main/java/leetcode/Problem501.java
@@ -1,6 +1,10 @@
 package leetcode;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * https://leetcode.com/problems/find-mode-in-binary-search-tree/
@@ -16,9 +20,45 @@ public class Problem501 {
         }
     }
 
+    private static class IntRef {
+        int val;
+    }
+
     public int[] findMode(TreeNode root) {
-        // TODO
-        return new int[0];
+        Map<Integer, Integer> map = new HashMap<>();
+        IntRef max = new IntRef();
+        findMode(root, map, max);
+        List<Integer> list = new ArrayList<>();
+        map.forEach((val, count) -> {
+            if (count == max.val) {
+                list.add(val);
+            }
+        });
+        int[] result = new int[list.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = list.get(i);
+        }
+        return result;
+    }
+
+    private void findMode(TreeNode node, Map<Integer, Integer> map, IntRef max) {
+        if (node == null) {
+            return;
+        }
+        if (!map.containsKey(node.val)) {
+            if (max.val < 1) {
+                max.val = 1;
+            }
+            map.put(node.val, 1);
+        } else {
+            int newCount = map.get(node.val) + 1;
+            if (max.val < newCount) {
+                max.val = newCount;
+            }
+            map.put(node.val, newCount);
+        }
+        findMode(node.left, map, max);
+        findMode(node.right, map, max);
     }
 
     public static void main(String[] args) {
