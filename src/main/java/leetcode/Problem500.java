a8e7689de356b8f786877c036735c19405ce9009
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-02-07 22:37:45 -0600

    Skeleton for problem 500

---
+++ src/main/java/leetcode/Problem500.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/keyboard-row/
+ */
+public class Problem500 {
+    public String[] findWords(String[] words) {
+        // TODO
+        return new String[]{};
+    }
+
+    public static void main(String[] args) {
+        Problem500 prob = new Problem500();
+        System.out.println(prob.findWords(new String[]{"Hello", "Alaska", "Dad", "Peace"})); // ["Alaska", "Dad"]
+    }
+}
