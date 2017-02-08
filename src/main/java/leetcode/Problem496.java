bff0a6733d57f7b788e3b3b8f67c752a12da7a01
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-02-07 22:56:58 -0600

    Skeleton for problem 496

---
+++ src/main/java/leetcode/Problem496.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/next-greater-element-i/
+ */
+public class Problem496 {
+    public int[] nextGreaterElement(int[] findNums, int[] nums) {
+        // TODO
+        return new int[0];
+    }
+
+    public static void main(String[] args) {
+        Problem496 prob = new Problem496();
+        System.out.println(Arrays.toString(prob.nextGreaterElement(new int[]{4, 1, 2}, new int[]{1, 3, 4, 2}))); // [-1,3,-1]
+        System.out.println(Arrays.toString(prob.nextGreaterElement(new int[]{2, 4}, new int[]{1, 2, 3, 4}))); // [3,-1]
+    }
+}
