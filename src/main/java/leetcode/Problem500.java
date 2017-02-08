15ffc5f249f16b53c7be828c0512f22e1abbd222
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-02-07 22:54:43 -0600

    Solve problem 500

--- src/main/java/leetcode/Problem500.java
+++ src/main/java/leetcode/Problem500.java
@@ -1,16 +1,44 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * https://leetcode.com/problems/keyboard-row/
  */
 public class Problem500 {
     public String[] findWords(String[] words) {
-        // TODO
-        return new String[]{};
+        List<String> result = new ArrayList<>();
+        Map<Character, Integer> map = new HashMap<>();
+        update(map, "qwertyuiop", 1);
+        update(map, "asdfghjkl", 2);
+        update(map, "zxcvbnm", 3);
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i].toLowerCase();
+            int row = 0;
+            boolean valid = true;
+            for (int j = 0; j < word.length(); j++) {
+                if (j == 0) {
+                    row = map.get(word.charAt(j));
+                } else {
+                    if (row != map.get(word.charAt(j))) {
+                        valid = false;
+                        break;
+                    }
+                }
+            }
+            if (valid) {
+                result.add(words[i]);
+            }
+        }
+        return result.toArray(new String[0]);
     }
 
-    public static void main(String[] args) {
-        Problem500 prob = new Problem500();
-        System.out.println(prob.findWords(new String[]{"Hello", "Alaska", "Dad", "Peace"})); // ["Alaska", "Dad"]
+    private static void update(Map<Character, Integer> map, String str, int row) {
+        for (char c : str.toCharArray()) {
+            map.put(c, row);
+        }
     }
 }
