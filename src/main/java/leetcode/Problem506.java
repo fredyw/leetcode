2ba27ba61211282643294fc89d1699e762d690ed
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-02-07 23:41:43 -0600

    Skeleton for problem 506

---
+++ src/main/java/leetcode/Problem506.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/relative-ranks/
+ */
+public class Problem506 {
+    public String[] findRelativeRanks(int[] nums) {
+        // TODO
+        return new String[0];
+    }
+
+    public static void main(String[] args) {
+        Problem506 prob = new Problem506();
+        System.out.println(Arrays.toString(prob.findRelativeRanks(new int[]{5, 4, 3, 2, 1}))); // ["Gold Medal", "Silver Medal", "Bronze Medal", "4", "5"]
+    }
+}
