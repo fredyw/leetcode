935905fe18ac9797b109656b1c5cbadec47a8048
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-02-08 22:45:33 -0600

    Skeleton for problem 495

---
+++ src/main/java/leetcode/Problem495.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/teemo-attacking/
+ */
+public class Problem495 {
+    public int findPoisonedDuration(int[] timeSeries, int duration) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem495 prob = new Problem495();
+        System.out.println(prob.findPoisonedDuration(new int[]{1, 4}, 2)); // 4
+        System.out.println(prob.findPoisonedDuration(new int[]{1, 2}, 2)); // 3
+    }
+}
