71bcb9c0a8401d0efa27c9a573e540e406221edd
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-02-08 23:41:50 -0600

    Solve problem 495

--- src/main/java/leetcode/Problem495.java
+++ src/main/java/leetcode/Problem495.java
@@ -5,13 +5,18 @@ package leetcode;
  */
 public class Problem495 {
     public int findPoisonedDuration(int[] timeSeries, int duration) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem495 prob = new Problem495();
-        System.out.println(prob.findPoisonedDuration(new int[]{1, 4}, 2)); // 4
-        System.out.println(prob.findPoisonedDuration(new int[]{1, 2}, 2)); // 3
+        int total = 0;
+        if (timeSeries.length == 0) {
+            return total;
+        }
+        for (int i = 0; i < timeSeries.length - 1; i++) {
+            if (timeSeries[i] + duration <= timeSeries[i + 1]) {
+                total += duration;
+            } else {
+                total += timeSeries[i + 1] - timeSeries[i];
+            }
+        }
+        total += duration;
+        return total;
     }
 }
