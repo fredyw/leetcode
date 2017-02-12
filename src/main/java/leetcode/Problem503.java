bdb77079f6cb405ec36fb2595b2270f0bceb90f4
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-02-12 17:00:55 -0600

    Skeleton for problem 503

---
+++ src/main/java/leetcode/Problem503.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/next-greater-element-ii/
+ */
+public class Problem503 {
+    public int[] nextGreaterElements(int[] nums) {
+        // TODO
+        return new int[0];
+    }
+
+    public static void main(String[] args) {
+        Problem503 prob = new Problem503();
+        System.out.println(prob.nextGreaterElements(new int[]{1, 2, 1})); // [2,-1,2]
+    }
+}
