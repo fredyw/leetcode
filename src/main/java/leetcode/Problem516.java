27025d037f947c5cb82e83f979886542f0b07e06
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-02-13 23:41:35 -0600

    Skeleton for problem 516

---
+++ src/main/java/leetcode/Problem516.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/longest-palindromic-subsequence/
+ */
+public class Problem516 {
+    public int longestPalindromeSubseq(String s) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem516 prob = new Problem516();
+        System.out.println(prob.longestPalindromeSubseq("bbbab")); // 4
+        System.out.println(prob.longestPalindromeSubseq("cbbd")); // 2
+    }
+}
