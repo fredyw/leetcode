fd5c39cf63968d219072fca7e4b4a867f32d595f
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-02-13 22:37:17 -0600

    Skeleton for problem 504

---
+++ src/main/java/leetcode/Problem504.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/base-7/
+ */
+public class Problem504 {
+    public String convertToBase7(int num) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem504 prob = new Problem504();
+        System.out.println(prob.convertToBase7(100)); // 202
+        System.out.println(prob.convertToBase7(-7)); // -10
+    }
+}
