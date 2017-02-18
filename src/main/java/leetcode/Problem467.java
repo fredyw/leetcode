1a424d7eedf45cd9850bcddd775ca4043da615a5
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-02-17 23:55:05 -0600

    Skeleton for problem 467

---
+++ src/main/java/leetcode/Problem467.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/unique-substrings-in-wraparound-string/
+ */
+public class Problem467 {
+    public int findSubstringInWraproundString(String p) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem467 prob = new Problem467();
+        System.out.println(prob.findSubstringInWraproundString("a")); // 1
+        System.out.println(prob.findSubstringInWraproundString("cac")); // 2
+        System.out.println(prob.findSubstringInWraproundString("zab")); // 6
+    }
+}
