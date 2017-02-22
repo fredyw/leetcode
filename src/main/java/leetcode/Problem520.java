2fdc2da8b464b2019adf774a1718df112a2a7dea
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-02-22 10:27:48 -0600

    Solve problem 520

--- src/main/java/leetcode/Problem520.java
+++ src/main/java/leetcode/Problem520.java
@@ -5,15 +5,33 @@ package leetcode;
  */
 public class Problem520 {
     public boolean detectCapitalUse(String word) {
+        if (word.length() == 1) {
+            return true;
+        }
+        boolean allLowerCase = false;
+        boolean allUpperCase = false;
+        boolean pascalCase = false;
+        int i = 0;
+        if (word.charAt(i) >= 'A' && word.charAt(i) <= 'Z' && word.charAt(i + 1) >= 'A' && word.charAt(i + 1) <= 'Z') {
+            allUpperCase = true;
+        } else if (word.charAt(i) >= 'A' && word.charAt(i) <= 'Z' && word.charAt(i + 1) >= 'A' && word.charAt(i + 1) <= 'z') {
+            pascalCase = true;
+        } else {
+            allLowerCase = true;
+        }
+        i++;
 
-    }
-
-    public static void main(String[] args) {
-        Problem520 prob = new Problem520();
-        System.out.println(prob.detectCapitalUse("USA")); // true
-        System.out.println(prob.detectCapitalUse("leetcode")); // true
-        System.out.println(prob.detectCapitalUse("Google")); // true
-        System.out.println(prob.detectCapitalUse("ABc")); // false
-        System.out.println(prob.detectCapitalUse("aBc")); // false
+        for (; i < word.length(); i++) {
+            if (allUpperCase) {
+                if (word.charAt(i) < 'A' || word.charAt(i) > 'Z') {
+                    return false;
+                }
+            } else { // all lower case or pascal case
+                if (word.charAt(i) < 'a' || word.charAt(i) > 'z') {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
