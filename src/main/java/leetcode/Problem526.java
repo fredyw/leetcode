c969f3653d50fa081f8cafdd3b0a0389d8ce2037
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-02-23 00:17:18 -0600

    Skeleton for problem 526

---
+++ src/main/java/leetcode/Problem526.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/beautiful-arrangement/
+ */
+public class Problem526 {
+    public int countArrangement(int N) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem526 prob = new Problem526();
+        System.out.println(prob.countArrangement(2)); // 2
+    }
+}
