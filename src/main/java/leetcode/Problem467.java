b6610fe4afdbe32ffcb03a31ede3bd1a2f17452f
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-03-03 14:48:04 -0600

    Update problem 467

--- src/main/java/leetcode/Problem467.java
+++ src/main/java/leetcode/Problem467.java
@@ -1,47 +1,36 @@
 package leetcode;
 
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * https://leetcode.com/problems/unique-substrings-in-wraparound-string/
  */
 public class Problem467 {
     public int findSubstringInWraproundString(String p) {
-        Set<String> set = new HashSet<>();
-        for (int i = 0; i < p.length(); i++) {
-            int j = i;
-            while (j + 1 < p.length() && (p.charAt(j) + 1 - 'a') % 26 == p.charAt(j + 1) - 'a') {
-                j++;
+        int max = 0;
+        int count = 0;
+        for (int prev = 0, current = 1; current < p.length(); prev++, current++) {
+            if ((p.charAt(prev) + 1 - 'a') % 26 == p.charAt(current) - 'a') {
+                count += current - prev;
+            } else {
+                count = 1;
             }
-            String sub = p.substring(i, j + 1);
-            if (!set.contains(sub.toString())) {
-                set.add(sub.toString());
-                substrings(sub.toString(), set);
-            }
-            i = j;
+            prev = current;
+            max = Math.max(max, count);
         }
-        return set.size();
-    }
-
-    private static void substrings(String s, Set<String> set) {
-        for (int i = 0; i < s.length(); i++) {
-            for (int j = i + 1; j <= s.length(); j++) {
-                set.add(s.substring(i, j));
-            }
-        }
+        int sum = 0;
+        // TODO
+        return sum;
     }
 
     public static void main(String[] args) {
         Problem467 prob = new Problem467();
-        System.out.println(prob.findSubstringInWraproundString("a")); // 1
+//        System.out.println(prob.findSubstringInWraproundString("a")); // 1
         System.out.println(prob.findSubstringInWraproundString("cac")); // 2
-        System.out.println(prob.findSubstringInWraproundString("zab")); // 6
-        System.out.println(prob.findSubstringInWraproundString("cab")); // 4
-        System.out.println(prob.findSubstringInWraproundString("dabd")); // 4
-        System.out.println(prob.findSubstringInWraproundString("zaba")); // 6
-        System.out.println(prob.findSubstringInWraproundString("abcd")); // 10
-        System.out.println(prob.findSubstringInWraproundString("abcdefghijklmnopqrstuvwxyz")); // 351
-        System.out.println(prob.findSubstringInWraproundString("abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz")); // 11843
+//        System.out.println(prob.findSubstringInWraproundString("zab")); // 6
+//        System.out.println(prob.findSubstringInWraproundString("cab")); // 4
+//        System.out.println(prob.findSubstringInWraproundString("dabd")); // 4
+//        System.out.println(prob.findSubstringInWraproundString("zaba")); // 6
+//        System.out.println(prob.findSubstringInWraproundString("abcd")); // 10
+//        System.out.println(prob.findSubstringInWraproundString("abcdefghijklmnopqrstuvwxyz")); // 351
+//        System.out.println(prob.findSubstringInWraproundString("abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz")); // 11843
     }
 }
