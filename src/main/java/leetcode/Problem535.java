b317601592ee7e885cedfd5e63448e0b2c35fb82
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-03-04 14:44:02 -0600

    Skeleton for problem 535

---
+++ src/main/java/leetcode/Problem535.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/encode-and-decode-tinyurl/
+ */
+public class Problem535 {
+    public static class Codec {
+        // Encodes a URL to a shortened URL.
+        public String encode(String longUrl) {
+            // TODO
+            return "";
+        }
+
+        // Decodes a shortened URL to its original URL.
+        public String decode(String shortUrl) {
+            // TODO
+            return "";
+        }
+    }
+
+    public static void main(String[] args) {
+        Codec codec = new Codec();
+        codec.decode(codec.encode("https://leetcode.com/problems/design-tinyurl"));
+    }
+}
