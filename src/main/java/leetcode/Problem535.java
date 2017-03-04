9df3e7926baddb485df8f275ed6e9f5d58ff57a8
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-03-04 16:20:44 -0600

    Solve problem 535

--- src/main/java/leetcode/Problem535.java
+++ src/main/java/leetcode/Problem535.java
@@ -1,25 +1,43 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * https://leetcode.com/problems/encode-and-decode-tinyurl/
  */
 public class Problem535 {
     public static class Codec {
+        private static final String PREFIX_URL = "http://tinyurl.com/";
+        private static final String ALPHANUMERIC = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        private Map<Integer, String> map = new HashMap<>();
+        private int id;
+
         // Encodes a URL to a shortened URL.
         public String encode(String longUrl) {
-            // TODO
-            return "";
+            StringBuilder shortUrl = new StringBuilder();
+            int key = id;
+            if (key == 0) {
+                shortUrl.append(ALPHANUMERIC.charAt(key % ALPHANUMERIC.length()));
+            } else {
+                while (key > 0) {
+                    shortUrl.append(ALPHANUMERIC.charAt(key % ALPHANUMERIC.length()));
+                    key = key / ALPHANUMERIC.length();
+                }
+            }
+            map.put(id++, longUrl);
+            return PREFIX_URL + shortUrl.toString();
         }
 
         // Decodes a shortened URL to its original URL.
         public String decode(String shortUrl) {
-            // TODO
-            return "";
+            String url = shortUrl.substring(PREFIX_URL.length());
+            int key = 0;
+            System.out.println(shortUrl);
+            for (int i = url.length() - 1; i >= 0; i--) {
+                key += ALPHANUMERIC.indexOf(url.charAt(i)) * (int) Math.pow(ALPHANUMERIC.length(), i);
+            }
+            return map.get(key);
         }
-    }
-
-    public static void main(String[] args) {
-        Codec codec = new Codec();
-        codec.decode(codec.encode("https://leetcode.com/problems/design-tinyurl"));
     }
 }
