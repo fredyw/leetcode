a6d79578a402545d39372775bd8762c64d479695
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-03-08 17:13:38 -0600

    Skeleton for problem 525

---
+++ src/main/java/leetcode/Problem525.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/contiguous-array/
+ */
+public class Problem525 {
+    public int findMaxLength(int[] nums) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem525 prob = new Problem525();
+        System.out.println(prob.findMaxLength(new int[]{0, 1})); // 2
+        System.out.println(prob.findMaxLength(new int[]{0, 1, 0})); // 2
+    }
+}
