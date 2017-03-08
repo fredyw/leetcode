82c3e19ce68d319f9b1c978b827e353ed7bf5dd4
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-03-08 17:36:50 -0600

    Update problem 525 (brute-force)

--- src/main/java/leetcode/Problem525.java
+++ src/main/java/leetcode/Problem525.java
@@ -5,8 +5,22 @@ package leetcode;
  */
 public class Problem525 {
     public int findMaxLength(int[] nums) {
-        // TODO
-        return 0;
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int zero = 0;
+            int one = 0;
+            for (int j = i; j < nums.length; j ++) {
+                if (nums[j] == 0) {
+                    zero++;
+                } else {
+                    one++;
+                }
+                if (zero == one) {
+                    max = Math.max(max, zero + one);
+                }
+            }
+        }
+        return max;
     }
 
     public static void main(String[] args) {
@@ -14,5 +28,7 @@ public class Problem525 {
         System.out.println(prob.findMaxLength(new int[]{0, 1})); // 2
         System.out.println(prob.findMaxLength(new int[]{0, 1, 0})); // 2
         System.out.println(prob.findMaxLength(new int[]{0, 1, 1, 0, 1})); // 4
+        System.out.println(prob.findMaxLength(new int[]{1, 1, 1, 0, 0})); // 4
+        System.out.println(prob.findMaxLength(new int[]{1, 1, 1, 1, 1})); // 0
     }
 }
