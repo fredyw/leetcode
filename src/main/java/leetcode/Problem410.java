19f6d98fb5ff88274e0262042bdf4fb76b7bf56d
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-03-10 18:29:03 -0600

    Update problem 410

---
+++ src/main/java/leetcode/Problem410.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/split-array-largest-sum/
+ */
+public class Problem410 {
+    public int splitArray(int[] nums, int m) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem410 prob = new Problem410();
+        System.out.println(prob.splitArray(new int[]{7, 2, 5, 10, 8}, 2)); // 18
+        System.out.println(prob.splitArray(new int[]{7, 2, 5, 10, 8}, 3)); // 14
+    }
+}
