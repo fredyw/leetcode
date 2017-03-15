b48587795b7102f6479749695bb888e17a4fafe9
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-03-15 16:17:27 -0500

    Solve problem 541

--- src/main/java/leetcode/Problem541.java
+++ src/main/java/leetcode/Problem541.java
@@ -5,8 +5,46 @@ package leetcode;
  */
 public class Problem541 {
     public String reverseStr(String s, int k) {
-        // TODO
-        return "";
+        StringBuilder sb = new StringBuilder();
+        if (k >= s.length()) {
+            for (int i = s.length() - 1; i >= 0; i--) {
+                sb.append(s.charAt(i));
+            }
+            return sb.toString();
+        }
+        boolean reverse = true;
+        int i = 0;
+        for (; i < s.length(); i += k) {
+            if (reverse) {
+                if (i + k - 1 < s.length()) {
+                    for (int j = i + k - 1; j >= i; j--) {
+                        sb.append(s.charAt(j));
+                    }
+                }
+            } else {
+                for (int j = i; j < i + k && j < s.length(); j++) {
+                    sb.append(s.charAt(j));
+                }
+            }
+            if (i + k >= s.length()) {
+                break;
+            }
+            reverse = !reverse;
+        }
+        String result = sb.toString();
+        if (s.length() > result.length()) {
+            if (reverse) {
+                for (int j = s.length() - 1; j >= i; j--) {
+                    sb.append(s.charAt(j));
+                }
+            } else {
+                i -= k;
+                for (; i < s.length(); i++) {
+                    sb.append(s.charAt(i));
+                }
+            }
+        }
+        return sb.toString();
     }
 
     public static void main(String[] args) {
@@ -15,5 +53,8 @@ public class Problem541 {
         System.out.println(prob.reverseStr("abcdefghij", 2)); // bacdfeghji
         System.out.println(prob.reverseStr("abcdefghij", 3)); // cbadefihgj
         System.out.println(prob.reverseStr("abcdefghij", 20)); // jihgfedcba
+        System.out.println(prob.reverseStr("abc", 3)); // cba
+        System.out.println(prob.reverseStr("abcdefg", 3)); // cbadefg
+        System.out.println(prob.reverseStr("hyzqyljrnigxvdtneasepfahmtyhlohwxmkqcdfehybknvdmfrfvtbsovjbdhevlfxpdaovjgunjqlimjkfnqcqnajmebeddqsgl", 39)); // fdcqkmxwholhytmhafpesaentdvxginrjlyqzyhehybknvdmfrfvtbsovjbdhevlfxpdaovjgunjqllgsqddebemjanqcqnfkjmi
     }
 }
