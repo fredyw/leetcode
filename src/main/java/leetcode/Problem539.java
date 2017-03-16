d632176a4a857fcad42b39501a9602a853136e95
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-03-16 00:11:59 -0500

    Skeleton for problem 539

---
+++ src/main/java/leetcode/Problem539.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/minimum-time-difference/
+ */
+public class Problem539 {
+    public int findMinDifference(List<String> timePoints) {
+        // TODO:
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem539 prob = new Problem539();
+        System.out.println(prob.findMinDifference(Arrays.asList("23:59", "00:00"))); // 1
+    }
+}
