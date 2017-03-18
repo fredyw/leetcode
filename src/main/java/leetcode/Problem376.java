e17ee86e08df7f071073340f0586124e74daa2d2
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-03-18 14:44:21 -0500

    Solve problem 376

--- src/main/java/leetcode/Problem376.java
+++ src/main/java/leetcode/Problem376.java
@@ -1,8 +1,5 @@
 package leetcode;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * https://leetcode.com/problems/wiggle-subsequence/
  */
@@ -11,20 +8,28 @@ public class Problem376 {
         if (nums.length == 0) {
             return 0;
         }
-        Map<String, Integer> memo = new HashMap<>();
-        int a = wiggleMaxLength(nums, 1, 0, true, memo);
-        int b = wiggleMaxLength(nums, 1, 0, false, memo);
+        int a = wiggleMaxLength(nums, 1, 0, true, createMemo(nums.length));
+        int b = wiggleMaxLength(nums, 1, 0, false, createMemo(nums.length));
         return Math.max(a, b);
     }
 
+    private static int[][] createMemo(int length) {
+        int[][] memo = new int[length][length];
+        for (int i = 0; i < memo.length; i++) {
+            for (int j = 0; j < memo.length; j++) {
+                memo[i][j] = -1;
+            }
+        }
+        return memo;
+    }
+
     private static int wiggleMaxLength(int[] nums, int idx, int prevIdx, boolean positive,
-                                       Map<String, Integer> memo) {
+                                       int[][] memo) {
         if (idx == nums.length) {
             return 1;
         }
-        String key = idx + "|" + prevIdx + "|" + positive;
-        if (memo.containsKey(key)) {
-            return memo.get(key);
+        if (memo[idx][prevIdx] != -1) {
+            return memo[idx][prevIdx];
         }
         int max = 0;
         if ((nums[idx] - nums[prevIdx] > 0 && positive) ||
@@ -32,7 +37,7 @@ public class Problem376 {
             max = Math.max(max, wiggleMaxLength(nums, idx + 1, idx, !positive, memo) + 1);
         }
         max = Math.max(max, wiggleMaxLength(nums, idx + 1, prevIdx, positive, memo));
-        memo.put(key, max);
+        memo[idx][prevIdx] = max;
         return max;
     }
 
@@ -44,6 +49,7 @@ public class Problem376 {
         System.out.println(prob.wiggleMaxLength(new int[]{2, 1, 3})); // 3
         System.out.println(prob.wiggleMaxLength(new int[]{9, 8, 7, 6, 5, 4, 3, 2, 1})); // 2
         System.out.println(prob.wiggleMaxLength(new int[]{1})); // 1
+        System.out.println(prob.wiggleMaxLength(new int[]{3, 3, 3, 2, 5})); // 3
         System.out.println(prob.wiggleMaxLength(new int[]{33, 53, 12, 64, 50, 41, 45, 21, 97, 35, 47, 92, 39, 0, 93, 55, 40, 46, 69, 42, 6, 95, 51, 68, 72, 9, 32, 84, 34, 64, 6, 2, 26, 98, 3, 43, 30, 60, 3, 68, 82, 9, 97, 19, 27, 98, 99, 4, 30, 96, 37, 9, 78, 43, 64, 4, 65, 30, 84, 90, 87, 64, 18, 50, 60, 1, 40, 32, 48, 50, 76, 100, 57, 29, 63, 53, 46, 57, 93, 98, 42, 80, 82, 9, 41, 55, 69, 84, 82, 79, 30, 79, 18, 97, 67, 23, 52, 38, 74, 15})); // 67
     }
 }
