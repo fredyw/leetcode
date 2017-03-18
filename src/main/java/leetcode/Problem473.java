c7b289826c45acdd7aa750e26af5bc3d65b6cf66
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-03-18 14:50:55 -0500

    Skeleton for problem 473

---
+++ src/main/java/leetcode/Problem473.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/matchsticks-to-square/
+ */
+public class Problem473 {
+    public boolean makesquare(int[] nums) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem473 prob = new Problem473();
+        System.out.println(prob.makesquare(new int[]{1, 1, 2, 2, 2})); // true
+        System.out.println(prob.makesquare(new int[]{3, 3, 3, 3, 4})); // false
+    }
+}
