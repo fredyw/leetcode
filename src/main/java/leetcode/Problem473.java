66bfd7267e9f1ff27987fc4495a5736961b64c25
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-03-20 19:52:31 -0500

    Update problem 473

--- src/main/java/leetcode/Problem473.java
+++ src/main/java/leetcode/Problem473.java
@@ -5,14 +5,38 @@ package leetcode;
  */
 public class Problem473 {
     public boolean makesquare(int[] nums) {
-        // TODO
-        return false;
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        int length = sum / 4;
+        int remainder = sum % 4;
+        if (remainder != 0) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private static boolean makeSquare(int[] nums, int length) {
+        if (length < 0) {
+            return false;
+        }
+        if (length == 0) {
+            return true;
+        }
+        boolean valid = false;
+        for (int i = 0; i < nums.length; i++) {
+            valid |= makeSquare(nums, length - nums[i]);
+        }
+        return valid;
     }
 
     public static void main(String[] args) {
         Problem473 prob = new Problem473();
         System.out.println(prob.makesquare(new int[]{1, 1, 2, 2, 2})); // true
         System.out.println(prob.makesquare(new int[]{3, 3, 3, 3, 4})); // false
+        System.out.println(prob.makesquare(new int[]{3, 3, 3, 3, 3})); // false
         System.out.println(prob.makesquare(new int[]{1, 1, 1, 1, 1, 1, 1, 1})); // true
         System.out.println(prob.makesquare(new int[]{3, 5, 4, 4, 6, 2, 1, 1, 1, 5})); // true
     }
