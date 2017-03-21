ca57ff6c56f21b4df2bbf79627f941f779825448
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-03-21 00:43:42 -0500

    Skeleton for problem 372

---
+++ src/main/java/leetcode/Problem372.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/super-pow/
+ */
+public class Problem372 {
+    public int superPow(int a, int[] b) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem372 prob = new Problem372();
+        System.out.println(prob.superPow(2, new int[]{3})); // 8
+        System.out.println(prob.superPow(2, new int[]{1, 0})); // 1024
+    }
+}
