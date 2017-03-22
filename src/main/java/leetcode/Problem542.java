c52455f8d6ccd870e18c7809aca4a6753482ca7a
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-03-22 13:27:05 -0500

    Skeleton for problem 542

---
+++ src/main/java/leetcode/Problem542.java
@@ -0,0 +1,43 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/01-matrix/
+ */
+public class Problem542 {
+    public List<List<Integer>> updateMatrix(List<List<Integer>> matrix) {
+        // TODO
+        return new ArrayList<>();
+    }
+
+    private static void print(List<List<Integer>> matrix) {
+        for (int i = 0; i < matrix.size(); i++) {
+            System.out.println(matrix.get(i));
+        }
+    }
+
+    public static void main(String[] args) {
+        Problem542 prob = new Problem542();
+
+        // 0 0 0
+        // 0 1 0
+        // 0 0 0
+        print(prob.updateMatrix(Arrays.asList(
+            Arrays.asList(0, 0, 0),
+            Arrays.asList(0, 1, 0),
+            Arrays.asList(0, 0, 0)
+        )));
+
+        // 0 0 0
+        // 0 1 0
+        // 1 2 1
+        print(prob.updateMatrix(Arrays.asList(
+            Arrays.asList(0, 0, 0),
+            Arrays.asList(0, 1, 0),
+            Arrays.asList(1, 1, 1)
+        )));
+    }
+}
