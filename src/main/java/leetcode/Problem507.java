8e2aebf515e4a68582826e46ac27ec9e598d4006
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-03-28 13:56:27 -0500

    Solve problem 507

--- src/main/java/leetcode/Problem507.java
+++ src/main/java/leetcode/Problem507.java
@@ -5,12 +5,21 @@ package leetcode;
  */
 public class Problem507 {
     public boolean checkPerfectNumber(int num) {
-        // TODO
-        return false;
-    }
-
-    public static void main(String[] args) {
-        Problem507 prob = new Problem507();
-        System.out.println(prob.checkPerfectNumber(28)); // true
+        if (num == 0) {
+            return false;
+        }
+        int sum = 0;
+        for (int i = 1; i < Math.sqrt(num); i++) {
+            if (i == 1) {
+                sum += i;
+            } else {
+                // find the divisor
+                if (num % i == 0) {
+                    sum += i;
+                    sum += num / i;
+                }
+            }
+        }
+        return (sum == num);
     }
 }
