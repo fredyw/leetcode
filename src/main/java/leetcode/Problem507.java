881b03da4c49bd40e3852d4c3695309f4831fc61
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-03-28 13:06:54 -0500

    Skeleton for problem 507

---
+++ src/main/java/leetcode/Problem507.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/perfect-number/
+ */
+public class Problem507 {
+    public boolean checkPerfectNumber(int num) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem507 prob = new Problem507();
+        System.out.println(prob.checkPerfectNumber(28)); // true
+    }
+}
