aa081a3155a9e6a0001bc273f9841f5915cd3684
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-04-04 00:15:32 -0500

    Solve problem 547

--- src/main/java/leetcode/Problem547.java
+++ src/main/java/leetcode/Problem547.java
@@ -1,27 +1,43 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
 /**
  * https://leetcode.com/problems/friend-circles/
  */
 public class Problem547 {
     public int findCircleNum(int[][] M) {
-        // TODO
-        return 0;
+        UnionFind uf = new UnionFind();
+        for (int i = 0; i < M.length; i++) {
+            for (int j = 0; j < M[i].length; j++) {
+                if (M[i][j] == 1) {
+                    uf.union(i, j);
+                }
+            }
+        }
+        return new HashSet<>(uf.map.values()).size();
     }
 
-    public static void main(String[] args) {
-        Problem547 prob = new Problem547();
+    private static class UnionFind {
+        private final Map<Integer, Integer> map = new HashMap<>();
 
-        System.out.println(prob.findCircleNum(new int[][]{
-            {1, 1, 0},
-            {1, 1, 0},
-            {0, 0, 1}
-        })); // 2
-
-        System.out.println(prob.findCircleNum(new int[][]{
-            {1, 1, 0},
-            {1, 1, 1},
-            {0, 1, 1}
-        })); // 1
+        public void union(int a, int b) {
+            if (!map.containsKey(a)) {
+                map.put(a, a);
+            }
+            if (!map.containsKey(b)) {
+                map.put(b, b);
+            }
+            if (map.get(a) == map.get(b)) {
+                return; // already connected
+            }
+            for (Map.Entry<Integer, Integer> e : map.entrySet()) {
+                if (e.getValue().equals(map.get(a))) {
+                    map.put(e.getKey(), map.get(b));
+                }
+            }
+        }
     }
 }
