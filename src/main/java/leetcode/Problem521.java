d376415691741ee5644a15c4f7c01da98bbc940f
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-04-04 00:28:32 -0500

    Solve problem 521

--- src/main/java/leetcode/Problem521.java
+++ src/main/java/leetcode/Problem521.java
@@ -5,13 +5,6 @@ package leetcode;
  */
 public class Problem521 {
     public int findLUSlength(String a, String b) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem521 prob = new Problem521();
-
-        System.out.println(prob.findLUSlength("aba", "cdc")); // 3
+        return a.equals(b) ? -1 : Math.max(a.length(), b.length());
     }
 }
