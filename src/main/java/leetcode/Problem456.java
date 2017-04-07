8c52d85c4ed374f12d432fa8288f44d08c9e3074
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-04-06 23:20:12 -0500

    Solve problem 456

--- src/main/java/leetcode/Problem456.java
+++ src/main/java/leetcode/Problem456.java
@@ -1,18 +1,39 @@
 package leetcode;
 
+import java.util.Stack;
+
 /**
  * https://leetcode.com/problems/132-pattern/
  */
 public class Problem456 {
     public boolean find132pattern(int[] nums) {
-        // TODO
-        return false;
-    }
+        int[] ones = new int[nums.length];
+        boolean[] candidates = new boolean[nums.length];
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            if (i == 0) {
+                min = nums[i];
+            } else if (min < nums[i]) {
+                ones[i] = min;
+                candidates[i] = true;
+            } else {
+                min = nums[i];
+            }
+        }
 
-    public static void main(String[] args) {
-        Problem456 prob = new Problem456();
-        System.out.println(prob.find132pattern(new int[]{1, 2, 3, 4})); // false
-        System.out.println(prob.find132pattern(new int[]{3, 1, 4, 2})); // true
-        System.out.println(prob.find132pattern(new int[]{-1, 3, 2, 0})); // true
+        Stack<Integer> stack = new Stack<>();
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (candidates[i]) {
+                int three = nums[i];
+                while (!stack.isEmpty() && three > stack.peek()) {
+                    int two = stack.pop();
+                    if (ones[i] < two && three > two) {
+                        return true;
+                    }
+                }
+            }
+            stack.add(nums[i]);
+        }
+        return false;
     }
 }
