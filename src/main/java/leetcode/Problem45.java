0407db1aa80be884186c73ea52a89ba2091fdccc
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-04-12 23:55:58 -0500

    Skeleton for problem 45

---
+++ src/main/java/leetcode/Problem45.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/jump-game-ii/
+ */
+public class Problem45 {
+    public int jump(int[] nums) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem45 prob = new Problem45();
+        System.out.println(prob.jump(new int[]{2, 3, 1, 1, 4})); // 2
+    }
+}
