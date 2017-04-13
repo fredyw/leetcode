7257f9045a600b4e24d2629161d29944dc4823fe
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-04-12 23:58:46 -0500

    Update problem 45

--- src/main/java/leetcode/Problem45.java
+++ src/main/java/leetcode/Problem45.java
@@ -12,5 +12,8 @@ public class Problem45 {
     public static void main(String[] args) {
         Problem45 prob = new Problem45();
         System.out.println(prob.jump(new int[]{2, 3, 1, 1, 4})); // 2
+        System.out.println(prob.jump(new int[]{2, 6, 1, 1, 4})); // 2
+        System.out.println(prob.jump(new int[]{100, 5, 1, 1, 4})); // 1
+        System.out.println(prob.jump(new int[]{5})); // 0
     }
 }
