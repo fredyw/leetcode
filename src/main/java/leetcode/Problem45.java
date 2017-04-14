689f1738a413ae72147b4089c862ee9821d6f2ab
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-04-14 01:05:51 -0500

    Update problem 45

--- src/main/java/leetcode/Problem45.java
+++ src/main/java/leetcode/Problem45.java
@@ -8,19 +8,20 @@ public class Problem45 {
         int result = 0;
         int i = 0;
         while (i < nums.length - 1) {
-            int max = 0;
-            int maxIdx = 0;
-            for (int j = i + 1; j <= i + nums[i] && j < nums.length; j++) {
-                if (nums[j] >= max) {
-                    max = nums[j];
-                    maxIdx = j;
+            int idx = 0;
+            int nextIdx = 0;
+            for (int j = i; j <= i + nums[i] && j < nums.length; j++) {
+                if (j + nums[j] > nextIdx) {
+                    idx = j;
+                    nextIdx = j + nums[j];
                 }
             }
             result++;
-            if (i + nums[i] >= nums.length - 1) {
-                break;
+            if (idx == i) {
+                i = i + nums[i];
+            } else {
+                i = idx;
             }
-            i = maxIdx;
         }
         return result;
     }
@@ -29,12 +30,12 @@ public class Problem45 {
         Problem45 prob = new Problem45();
 //        System.out.println(prob.jump(new int[]{2, 3, 1, 1, 4})); // 2
 //        System.out.println(prob.jump(new int[]{2, 6, 1, 1, 4})); // 2
-//        System.out.println(prob.jump(new int[]{2, 6, 1, 1, 4})); // 2
 //        System.out.println(prob.jump(new int[]{2, 2, 3, 1, 2, 4, 1})); // 3
 //        System.out.println(prob.jump(new int[]{100, 5, 1, 1, 4})); // 1
 //        System.out.println(prob.jump(new int[]{5})); // 0
-//        System.out.println(prob.jump(new int[]{2, 3, 1})); // 1
+        System.out.println(prob.jump(new int[]{2, 3, 1})); // 1
 //        System.out.println(prob.jump(new int[]{1, 2, 1, 1, 1})); // 3
-        System.out.println(prob.jump(new int[]{10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 0})); // 2
+//        System.out.println(prob.jump(new int[]{10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 0})); // 2
+//        System.out.println(prob.jump(new int[]{10, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0})); // 2
     }
 }
