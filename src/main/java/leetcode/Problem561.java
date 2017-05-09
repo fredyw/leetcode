de63db5311479f1732a569432c4058719b64e291
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-05-08 23:20:38 -0500

    Solve problem 561

--- src/main/java/leetcode/Problem561.java
+++ src/main/java/leetcode/Problem561.java
@@ -1,16 +1,19 @@
 package leetcode;
 
+import java.util.Arrays;
+
 /**
  * https://leetcode.com/problems/array-partition-i
  */
 public class Problem561 {
     public int arrayPairSum(int[] nums) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem561 prob = new Problem561();
-        System.out.println(prob.arrayPairSum(new int[]{1, 4, 3, 2})); // 4
+        Arrays.sort(nums);
+        int i = 0;
+        int sum = 0;
+        while (i < nums.length) {
+            sum += Math.min(nums[i], nums[i + 1]);
+            i += 2;
+        }
+        return sum;
     }
 }
