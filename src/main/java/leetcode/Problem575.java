ce0ea60bbf8148f81b4bc4c2c2e56bc5e4571d6e
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-05-08 23:41:10 -0500

    Solve problem 575

--- src/main/java/leetcode/Problem575.java
+++ src/main/java/leetcode/Problem575.java
@@ -1,17 +1,20 @@
 package leetcode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * https://leetcode.com/problems/distribute-candies/
  */
 public class Problem575 {
     public int distributeCandies(int[] candies) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem575 prob = new Problem575();
-        System.out.println(prob.distributeCandies(new int[]{1, 1, 2, 2, 3, 3})); // 3
-        System.out.println(prob.distributeCandies(new int[]{1, 1, 2, 3})); // 2
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < candies.length; i++) {
+            if (set.size() == candies.length / 2) {
+                break;
+            }
+            set.add(candies[i]);
+        }
+        return set.size();
     }
 }
