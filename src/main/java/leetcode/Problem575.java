f7850c9672815dfc657da9a79385357a4126c5e3
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-05-08 23:25:28 -0500

    Skeleton for problem 575

---
+++ src/main/java/leetcode/Problem575.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/distribute-candies/
+ */
+public class Problem575 {
+    public int distributeCandies(int[] candies) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem575 prob = new Problem575();
+        System.out.println(prob.distributeCandies(new int[]{1, 1, 2, 2, 3, 3})); // 3
+        System.out.println(prob.distributeCandies(new int[]{1, 1, 2, 3})); // 2
+    }
+}
