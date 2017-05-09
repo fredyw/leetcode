4a0cb112eae8642c0b79e2c0a567f80f5f6edf36
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-05-08 23:00:49 -0500

    Skeleton for problem 561

---
+++ src/main/java/leetcode/Problem561.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/array-partition-i
+ */
+public class Problem561 {
+    public int arrayPairSum(int[] nums) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem561 prob = new Problem561();
+        System.out.println(prob.arrayPairSum(new int[]{1, 4, 3, 2})); // 4
+    }
+}
