32134332bfea6f7b9dffcaf9ef322824908c7ad5
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-05-11 21:59:00 -0500

    Skeleton for problem 560

---
+++ src/main/java/leetcode/Problem560.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/subarray-sum-equals-k/
+ */
+public class Problem560 {
+    public int subarraySum(int[] nums, int k) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem560 prob = new Problem560();
+        System.out.println(prob.subarraySum(new int[]{1, 1, 1}, 2)); // 2
+    }
+}
