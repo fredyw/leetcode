473635e49f9da9c5d117e181de49dc251586d950
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-05-11 22:26:10 -0500

    Update problem 560

--- src/main/java/leetcode/Problem560.java
+++ src/main/java/leetcode/Problem560.java
@@ -5,12 +5,30 @@ package leetcode;
  */
 public class Problem560 {
     public int subarraySum(int[] nums, int k) {
-        // TODO
-        return 0;
+        int result = 0;
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (sum + nums[i] < k) {
+                sum += nums[i];
+            } else if (sum + nums[i] == k) {
+                if (nums[i] != k) {
+                    sum = nums[i];
+                }
+                result++;
+            } else if (nums[i] == k) {
+                result++;
+                sum = 0;
+            } else {
+                sum = nums[i];
+            }
+        }
+        return result;
     }
 
     public static void main(String[] args) {
         Problem560 prob = new Problem560();
-        System.out.println(prob.subarraySum(new int[]{1, 1, 1}, 2)); // 2
+//        System.out.println(prob.subarraySum(new int[]{1, 1, 1}, 2)); // 2
+//        System.out.println(prob.subarraySum(new int[]{1, 1, 1, 2, 1, 1, 1}, 2)); // 5
+        System.out.println(prob.subarraySum(new int[]{-1, -1, 1}, 0)); // 1
     }
 }
