75dc37ee943b380223cd7582c6e9b59ab2640a53
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-05-15 17:12:10 -0500

    Update problem 581

--- src/main/java/leetcode/Problem581.java
+++ src/main/java/leetcode/Problem581.java
@@ -5,12 +5,16 @@ package leetcode;
  */
 public class Problem581 {
     public int findUnsortedSubarray(int[] nums) {
-        // TODO
+
+        for (int i = 0; i < nums.length; i++) {
+
+        }
         return 0;
     }
 
     public static void main(String[] args) {
         Problem581 prob = new Problem581();
         System.out.println(prob.findUnsortedSubarray(new int[]{2, 6, 4, 8, 10, 9, 15})); // 5
+        System.out.println(prob.findUnsortedSubarray(new int[]{2, 6, 4, 8, 10, 7, 9})); // 6    
     }
 }
