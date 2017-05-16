91bbce6649d386f3d00d18247750822a41c857e2
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-05-15 19:31:35 -0500

    Skeleton for problem 553

---
+++ src/main/java/leetcode/Problem553.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/optimal-division/
+ */
+public class Problem553 {
+    public String optimalDivision(int[] nums) {
+        // TODO
+        return "";
+    }
+
+    public static void main(String[] args) {
+        Problem553 prob = new Problem553();
+        System.out.println(prob.optimalDivision(new int[]{1000, 100, 10, 2})); // 1000/(100/10/2)
+    }
+}
