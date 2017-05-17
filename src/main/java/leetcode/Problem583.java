ea5e909ac914ceb0f2f0a303efd95fb2a69444d8
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-05-16 23:40:57 -0500

    Skeleton for problem 583

---
+++ src/main/java/leetcode/Problem583.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/delete-operation-for-two-strings/
+ */
+public class Problem583 {
+    public int minDistance(String word1, String word2) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem583 prob = new Problem583();
+        System.out.println(prob.minDistance("sea", "eat")); // 2
+    }
+}
