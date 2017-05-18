a4f59541ad087ff8c64e971e6677c948c58c8040
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-05-18 00:10:45 -0500

    Update problem 567

--- src/main/java/leetcode/Problem567.java
+++ src/main/java/leetcode/Problem567.java
@@ -5,13 +5,40 @@ package leetcode;
  */
 public class Problem567 {
     public boolean checkInclusion(String s1, String s2) {
-        // TODO
-        return false;
+        return checkInclusion(s1.toCharArray(), 0, s1.length() - 1, s2);
+    }
+
+    private static void swap(char[] chars, int i, int j) {
+        if (j >= chars.length) {
+            return;
+        }
+        char tmp = chars[i];
+        chars[i] = chars[j];
+        chars[j] = tmp;
+    }
+
+    private static boolean checkInclusion(char[] chars, int left, int right, String string) {
+        if (left == right) {
+            if (string.contains(new String(chars))) {
+                return true;
+            }
+        }
+        boolean found = false;
+        for (int i = left; i <= right; i++) {
+            swap(chars, left, i);
+            found |= checkInclusion(chars, left + 1, right, string);
+            if (found) {
+                return found;
+            }
+            swap(chars, left, i);
+        }
+        return found;
     }
 
     public static void main(String[] args) {
         Problem567 prob = new Problem567();
         System.out.println(prob.checkInclusion("ab", "eidbaooo")); // true
         System.out.println(prob.checkInclusion("ab", "eidboaoo")); // false
+        System.out.println(prob.checkInclusion("dinitrophenylhydrazine", "acetylphenylhydrazine")); // false
     }
 }
