a8000f98482688a3e1989c201ed28950d37eaa12
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-05-17 22:02:02 -0500

    Solve problem 583

--- src/main/java/leetcode/Problem583.java
+++ src/main/java/leetcode/Problem583.java
@@ -5,14 +5,32 @@ package leetcode;
  */
 public class Problem583 {
     public int minDistance(String word1, String word2) {
-        // TODO
-        return 0;
+        int[][] memo = new int[word1.length() + 1][word2.length() + 1];
+        for (int i = 0; i < memo.length; i++) {
+            for (int j = 0; j < memo[i].length; j++) {
+                memo[i][j] = -1;
+            }
+        }
+        return minDistance(word1, word2, word1.length(), word2.length(), memo);
     }
 
-    public static void main(String[] args) {
-        Problem583 prob = new Problem583();
-        System.out.println(prob.minDistance("sea", "eat")); // 2
-        System.out.println(prob.minDistance("abdm", "yxabcd")); // 4
-        System.out.println(prob.minDistance("abdm", "cyxabcd")); // 5
+    private static int minDistance(String word1, String word2, int i, int j, int[][] memo) {
+        if (memo[i][j] != -1) {
+            return memo[i][j];
+        }
+        if (i == 0) {
+            return j;
+        }
+        if (j == 0) {
+            return i;
+        }
+        if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+            return minDistance(word1, word2, i - 1, j - 1, memo);
+        }
+        int a = minDistance(word1, word2, i - 1, j, memo);
+        int b = minDistance(word1, word2, i, j - 1, memo);
+        int min = 1 + Math.min(a, b);
+        memo[i][j] = min;
+        return min;
     }
 }
