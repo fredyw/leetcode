90757144e9963bfb143523c85efd3c1c0760ebbb
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-05-21 14:16:33 -0500

    Update problem 57

--- src/main/java/leetcode/Problem57.java
+++ src/main/java/leetcode/Problem57.java
@@ -30,22 +30,95 @@ public class Problem57 {
 
     public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
         List<Interval> result = new ArrayList<>();
-        // TODO
+        if (intervals.isEmpty()) {
+            result.add(newInterval);
+            return result;
+        }
+        boolean addNewInterval = true;
+        for (int i = 0; i < intervals.size(); i++) {
+            Interval interval = intervals.get(i);
+            if (interval.start <= newInterval.start && newInterval.start <= interval.end &&
+                newInterval.end > interval.end) {
+                for (int j = i; j < intervals.size(); j++) {
+                    if (intervals.get(j).start > newInterval.end) {
+                        addNewInterval = false;
+                        result.add(new Interval(interval.start, newInterval.end));
+                        break;
+                    }
+                    if (newInterval.end <= intervals.get(j).end) {
+                        addNewInterval = false;
+                        result.add(new Interval(interval.start, intervals.get(j).end));
+                        i = j;
+                        break;
+                    }
+                }
+                if (result.isEmpty()) {
+                    addNewInterval = false;
+                    result.add(new Interval(interval.start, newInterval.end));
+                }
+            } else if (newInterval.end < interval.start) {
+                if (addNewInterval) {
+                    addNewInterval = false;
+                    result.add(newInterval);
+                }
+                result.add(interval);
+            } else if (newInterval.start < interval.start) {
+                for (int j = i; j < intervals.size(); j++) {
+                    if (newInterval.end <= intervals.get(j).end) {
+                        addNewInterval = false;
+                        result.add(new Interval(newInterval.start, intervals.get(j).end));
+                        i = j;
+                        break;
+                    }
+                }
+                if (result.isEmpty()) {
+                    addNewInterval = false;
+                    result.add(newInterval);
+                }
+            } else {
+                result.add(interval);
+            }
+        }
+        if (addNewInterval) {
+            result.add(newInterval);
+        }
         return result;
     }
 
     public static void main(String[] args) {
         Problem57 prob = new Problem57();
+//        System.out.println(prob.insert(Arrays.asList(
+//            new Interval(1, 3),
+//            new Interval(6, 9)),
+//            new Interval(2, 5))); // [1,5],[6,9]
+//        System.out.println(prob.insert(Arrays.asList(
+//            new Interval(1, 2),
+//            new Interval(3, 5),
+//            new Interval(6, 7),
+//            new Interval(8, 10),
+//            new Interval(12, 16)),
+//            new Interval(4, 9))); // [1,2],[3,10],[12,16]
+//        System.out.println(prob.insert(Arrays.asList(
+//            new Interval(2, 3),
+//            new Interval(5, 7)),
+//            new Interval(1, 9))); // [1,9]
+//        System.out.println(prob.insert(Arrays.asList(
+//            new Interval(2, 3),
+//            new Interval(5, 7)),
+//            new Interval(1, 6))); // [1,7]
+//        System.out.println(prob.insert(Arrays.asList(
+//            new Interval(4, 5)),
+//            new Interval(1, 2))); // [1,2],[4,5]
+//        System.out.println(prob.insert(Arrays.asList(
+//            new Interval(1, 5)),
+//            new Interval(2, 7))); // [1,7]
+//        System.out.println(prob.insert(Arrays.asList(),
+//            new Interval(5, 7))); // [5,7]
+//        System.out.println(prob.insert(Arrays.asList(
+//            new Interval(1, 5)),
+//            new Interval(6, 8))); // [1,5],[6,8]
         System.out.println(prob.insert(Arrays.asList(
-            new Interval(1, 3),
-            new Interval(6, 9)),
-            new Interval(2, 5))); // [1,5],[6,9]
-        System.out.println(prob.insert(Arrays.asList(
-            new Interval(1, 2),
-            new Interval(3, 5),
-            new Interval(6, 7),
-            new Interval(8, 10),
-            new Interval(12, 16)),
-            new Interval(4, 9))); // [1,2],[3,10],[12,16]
+            new Interval(1, 5)),
+            new Interval(2, 3))); // [1,5]
     }
 }
