954163215704d2477e1f0f94a4ae8ed44af8a9d7
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-05-23 00:19:46 -0500

    Skeleton for problem 597

---
+++ src/main/java/leetcode/Problem597.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/longest-harmonious-subsequence/
+ */
+public class Problem597 {
+    public int findLHS(int[] nums) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem597 prob = new Problem597();
+        System.out.println(prob.findLHS(new int[]{1, 3, 2, 2, 5, 2, 3, 7})); // 5
+    }
+}
