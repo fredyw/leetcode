eb5d920d199b80ce778b347b5aa023c73e03aec5
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-05-24 00:09:52 -0500

    Skeleton for problem 593

---
+++ src/main/java/leetcode/Problem593.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/valid-square/
+ */
+public class Problem593 {
+    public boolean validSquare(int[] p1, int[] p2, int[] p3, int[] p4) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem593 prob = new Problem593();
+        System.out.println(prob.validSquare(new int[]{0, 0}, new int[]{1, 1}, new int[]{1, 0}, new int[]{0, 1})); // true
+    }
+}
