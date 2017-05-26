f9d36cfaaecf721fba5b1ac0e0a19408ba9c8647
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-05-26 01:49:24 -0500

    Update problem 593

--- src/main/java/leetcode/Problem593.java
+++ src/main/java/leetcode/Problem593.java
@@ -11,25 +11,45 @@ public class Problem593 {
         if (d1 == 0 || d2 == 0 || d3 == 0) {
             return false;
         }
-        // Pythagoras
         if (d1 == d2) {
-            double d4 = Math.sqrt((d3 * d3) - (d1 * d1));
-            double d5 = Math.sqrt((d3 * d3) - (d2 * d2));
+            // hypotenuse: d3
+            double d4 = distance(p2, p4);
+            if (!isRightAngled(d1, d4, d3)) {
+                return false;
+            }
+            double d5 = distance(p3, p4);
+            if (!isRightAngled(d2, d5, d3)) {
+                return false;
+            }
             return toInt(d1) == toInt(d4) && toInt(d1) == toInt(d5);
         } else if (d1 == d3) {
-            double d4 = Math.sqrt((d2 * d2) - (d1 * d1));
-            double d5 = Math.sqrt((d2 * d2) - (d3 * d3));
+             // hypotenuse: d2
+            double d4 = distance(p2, p3);
+            if (!isRightAngled(d1, d4, d2)) {
+                return false;
+            }
+            double d5 = distance(p4, p3);
+            if (!isRightAngled(d3, d5, d2)) {
+                return false;
+            }
             return toInt(d1) == toInt(d4) && toInt(d1) == toInt(d5);
         } else if (d2 == d3) {
-            double d4 = Math.sqrt((d1 * d1) - (d2 * d2));
-            double d5 = Math.sqrt((d1 * d1) - (d3 * d3));
+            // hypotenuse: d1
+            double d4 = distance(p3, p2);
+            if (!isRightAngled(d2, d4, d1)) {
+                return false;
+            }
+            double d5 = distance(p4, p2);
+            if (!isRightAngled(d3, d5, d1)) {
+                return false;
+            }
             return toInt(d2) == toInt(d4) && toInt(d2) == toInt(d5);
         }
         return false;
     }
 
     private static int toInt(double d) {
-        return (int) (d * 1e3);
+        return (int) (d * 1e6);
     }
 
     private static double distance(int[] p1, int[] p2) {
@@ -37,15 +57,22 @@ public class Problem593 {
         return Math.sqrt(((p1[0] - p2[0]) * (p1[0] - p2[0])) + ((p1[1] - p2[1]) * (p1[1] - p2[1])));
     }
 
+    private boolean isRightAngled(double a, double b, double c) {
+        return toInt(((a * a) + (b * b))) == toInt((c * c));
+    }
+
     public static void main(String[] args) {
         Problem593 prob = new Problem593();
-//        System.out.println(prob.validSquare(new int[]{0, 0}, new int[]{1, 1}, new int[]{1, 0}, new int[]{0, 1})); // true
-//        System.out.println(prob.validSquare(new int[]{0, 0}, new int[]{1, 1}, new int[]{1, 5}, new int[]{0, 1})); // false
-//        System.out.println(prob.validSquare(new int[]{0, 1}, new int[]{0, 0}, new int[]{1, 1}, new int[]{1, 0})); // true
-//        System.out.println(prob.validSquare(new int[]{0, 1}, new int[]{0, 0}, new int[]{1, -1}, new int[]{1, 0})); // false
-//        System.out.println(prob.validSquare(new int[]{1, 0}, new int[]{-1, 0}, new int[]{0, 1}, new int[]{0, -1})); // true
-//        System.out.println(prob.validSquare(new int[]{0, 0}, new int[]{-1, 0}, new int[]{1, 0}, new int[]{0, 1})); // false
-//        System.out.println(prob.validSquare(new int[]{0, 0}, new int[]{0, 0}, new int[]{0, 0}, new int[]{0, 0})); // false
+        System.out.println(prob.validSquare(new int[]{0, 0}, new int[]{1, 1}, new int[]{1, 0}, new int[]{0, 1})); // true
+        System.out.println(prob.validSquare(new int[]{0, 0}, new int[]{1, 1}, new int[]{1, 5}, new int[]{0, 1})); // false
+        System.out.println(prob.validSquare(new int[]{0, 1}, new int[]{0, 0}, new int[]{1, 1}, new int[]{1, 0})); // true
+        System.out.println(prob.validSquare(new int[]{0, 1}, new int[]{0, 0}, new int[]{1, -1}, new int[]{1, 0})); // false
+        System.out.println(prob.validSquare(new int[]{1, 0}, new int[]{-1, 0}, new int[]{0, 1}, new int[]{0, -1})); // true
+        System.out.println(prob.validSquare(new int[]{0, 0}, new int[]{-1, 0}, new int[]{1, 0}, new int[]{0, 1})); // false
+        System.out.println(prob.validSquare(new int[]{0, 0}, new int[]{0, 0}, new int[]{0, 0}, new int[]{0, 0})); // false
         System.out.println(prob.validSquare(new int[]{0, 1}, new int[]{1, 2}, new int[]{0, 2}, new int[]{0, 0})); // false
+        System.out.println(prob.validSquare(new int[]{1, 0}, new int[]{0, 1}, new int[]{-1, 0}, new int[]{0, -1})); // true
+        System.out.println(prob.validSquare(new int[]{1, 1}, new int[]{5, 3}, new int[]{3, 5}, new int[]{7, 7})); // false
+        System.out.println(prob.validSquare(new int[]{-2009, 2747}, new int[]{-1566, 2436}, new int[]{-2320, 2304}, new int[]{-1877, 1993})); // true
     }
 }
