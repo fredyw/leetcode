33189559e66b5c8c5680bc11885c220d4d0181e8
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-05-26 00:42:26 -0500

    Update problem 593

--- src/main/java/leetcode/Problem593.java
+++ src/main/java/leetcode/Problem593.java
@@ -1,58 +1,40 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * https://leetcode.com/problems/valid-square/
  */
 public class Problem593 {
     public boolean validSquare(int[] p1, int[] p2, int[] p3, int[] p4) {
-        List<int[]> list = new ArrayList<>();
-        list.add(p2);
-        list.add(p3);
-        list.add(p4);
-        int[] a = p1;
-        for (int i = 0; i < list.size(); i++) {
-            int[] b = list.get(i);
-            if (a[0] == b[0]) {
-                // a <--> b (y-axis)
-                int dist1 = Math.abs(a[1] - b[1]);
-                list.remove(i);
-                int[] c = null;
-                for (int j = 0; j < list.size(); j++) {
-                    if (a[1] == list.get(j)[1]) {
-                        c = list.get(j);
-                        list.remove(j);
-                        break;
-                    }
-                }
-                if (c == null) {
-                    return false;
-                }
-                // a <--> c (x-axis)
-                int dist2 = Math.abs(a[0] - c[0]);
-                if (dist1 != dist2) {
-                    return false;
-                }
-                int[] d = list.get(0);
-                if (b[1] != d[1]) {
-                    return false;
-                }
-                // b <--> d (x-axis)
-                int dist3 = Math.abs(b[0] - d[0]);
-                if (dist2 != dist3) {
-                    return false;
-                }
-                // c <--> d (y-axis)
-                int dist4 = Math.abs(c[1] - d[1]);
-                if (dist3 != dist4) {
-                    return false;
-                }
-                return true;
-            }
+        double d1 = distance(p1, p2);
+        double d2 = distance(p1, p3);
+        double d3 = distance(p1, p4);
+        if (d1 == 0 || d2 == 0 || d3 == 0) {
+            return false;
+        }
+        // Pythagoras
+        if (d1 == d2) {
+            double d4 = Math.sqrt((d3 * d3) - (d1 * d1));
+            double d5 = Math.sqrt((d3 * d3) - (d2 * d2));
+            return toInt(d1) == toInt(d4) && toInt(d1) == toInt(d5);
+        } else if (d1 == d3) {
+            double d4 = Math.sqrt((d2 * d2) - (d1 * d1));
+            double d5 = Math.sqrt((d2 * d2) - (d3 * d3));
+            return toInt(d1) == toInt(d4) && toInt(d1) == toInt(d5);
+        } else if (d2 == d3) {
+            double d4 = Math.sqrt((d1 * d1) - (d2 * d2));
+            double d5 = Math.sqrt((d1 * d1) - (d3 * d3));
+            return toInt(d2) == toInt(d4) && toInt(d2) == toInt(d5);
         }
         return false;
+    }
+
+    private static int toInt(double d) {
+        return (int) (d * 1e3);
+    }
+
+    private static double distance(int[] p1, int[] p2) {
+        // Euclidean
+        return Math.sqrt(((p1[0] - p2[0]) * (p1[0] - p2[0])) + ((p1[1] - p2[1]) * (p1[1] - p2[1])));
     }
 
     public static void main(String[] args) {
@@ -61,6 +43,9 @@ public class Problem593 {
 //        System.out.println(prob.validSquare(new int[]{0, 0}, new int[]{1, 1}, new int[]{1, 5}, new int[]{0, 1})); // false
 //        System.out.println(prob.validSquare(new int[]{0, 1}, new int[]{0, 0}, new int[]{1, 1}, new int[]{1, 0})); // true
 //        System.out.println(prob.validSquare(new int[]{0, 1}, new int[]{0, 0}, new int[]{1, -1}, new int[]{1, 0})); // false
-        System.out.println(prob.validSquare(new int[]{1, 0}, new int[]{-1, 0}, new int[]{0, 1}, new int[]{0, -1})); // true
+//        System.out.println(prob.validSquare(new int[]{1, 0}, new int[]{-1, 0}, new int[]{0, 1}, new int[]{0, -1})); // true
+//        System.out.println(prob.validSquare(new int[]{0, 0}, new int[]{-1, 0}, new int[]{1, 0}, new int[]{0, 1})); // false
+//        System.out.println(prob.validSquare(new int[]{0, 0}, new int[]{0, 0}, new int[]{0, 0}, new int[]{0, 0})); // false
+        System.out.println(prob.validSquare(new int[]{0, 1}, new int[]{1, 2}, new int[]{0, 2}, new int[]{0, 0})); // false
     }
 }
