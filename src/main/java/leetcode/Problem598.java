36b68374bd71ecf344da3fe4735e32e9e46172c1
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-05-29 14:58:29 -0500

    Skeleton for problem 598

---
+++ src/main/java/leetcode/Problem598.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/range-addition-ii/#/description
+ */
+public class Problem598 {
+    public int maxCount(int m, int n, int[][] ops) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem598 prob = new Problem598();
+        System.out.println(prob.maxCount(3, 3, new int[][]{
+            {2, 2},
+            {3, 3}
+        })); // 4
+    }
+}
