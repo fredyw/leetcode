589c39c44371acb33fc0dcf4ebd85ed4bc035e09
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-05-30 00:09:22 -0500

    Update problem 577

--- src/main/java/leetcode/Problem577.java
+++ src/main/java/leetcode/Problem577.java
@@ -1,13 +1,12 @@
 package leetcode;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 /**
- * https://leetcode.com/problems/minimum-index-sum-of-two-lists/#/description
+ * https://leetcode.com/problems/minimum-index-sum-of-two-lists/
  */
 public class Problem577 {
     public String[] findRestaurant(String[] list1, String[] list2) {
