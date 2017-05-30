f0271216d238cddc2d36fd9cc06458280092649e
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-05-30 00:18:56 -0500

    Update problem 598

--- src/main/java/leetcode/Problem598.java
+++ src/main/java/leetcode/Problem598.java
@@ -5,8 +5,27 @@ package leetcode;
  */
 public class Problem598 {
     public int maxCount(int m, int n, int[][] ops) {
-        // TODO
-        return 0;
+        if (ops.length == 0) {
+            return m * n;
+        }
+        int[][] matrix = new int[m][n];
+        int max = 0;
+        int count = 0;
+        for (int x = 0; x < ops.length; x++) {
+            int[] op = ops[x];
+            for (int i = 0; i < op[0]; i++) {
+                for (int j = 0; j < op[1]; j++) {
+                    matrix[i][j]++;
+                    if (max < matrix[i][j]) {
+                        max = matrix[i][j];
+                        count = 1;
+                    } else if (max == matrix[i][j]) {
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
     }
 
     public static void main(String[] args) {
@@ -15,5 +34,11 @@ public class Problem598 {
             {2, 2},
             {3, 3}
         })); // 4
+        System.out.println(prob.maxCount(3, 3, new int[][]{
+            {2, 1},
+            {1, 3}
+        })); // 1
+        System.out.println(prob.maxCount(3, 3, new int[][]{
+        })); // 9
     }
 }
