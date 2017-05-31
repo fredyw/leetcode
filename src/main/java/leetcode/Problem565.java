0165968228c51e2c8e089a4772d255e9db948c16
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-05-30 22:29:04 -0500

    Skeleton for problem 565

---
+++ src/main/java/leetcode/Problem565.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/array-nesting/
+ */
+public class Problem565 {
+    public int arrayNesting(int[] nums) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem565 prob = new Problem565();
+        System.out.println(prob.arrayNesting(new int[]{5, 4, 0, 3, 1, 6, 2})); // 4
+    }
+}
