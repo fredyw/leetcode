460a8ac6515ffbf27aaa97e20f33187d91449eae
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-06-04 16:02:57 -0500

    Skeleton for problem 605

---
+++ src/main/java/leetcode/Problem605.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/can-place-flowers/
+ */
+public class Problem605 {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem605 prob = new Problem605();
+        System.out.println(prob.canPlaceFlowers(new int[]{1, 0, 0, 0, 1}, 1)); // true
+        System.out.println(prob.canPlaceFlowers(new int[]{1, 0, 0, 0, 1}, 2)); // false
+    }
+}
