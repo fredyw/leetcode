1636e57f8693e31f8916d1c1968200057c845f80
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-06-04 15:41:41 -0500

    Sovle problem 609

--- src/main/java/leetcode/Problem609.java
+++ src/main/java/leetcode/Problem609.java
@@ -1,23 +1,43 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * https://leetcode.com/problems/find-duplicate-file-in-system/
  */
 public class Problem609 {
     public List<List<String>> findDuplicate(String[] paths) {
-        // TODO
-        return null;
-    }
-
-    public static void main(String[] args) {
-        Problem609 prob = new Problem609();
-        // ["root/a/2.txt","root/c/d/4.txt","root/4.txt"],["root/a/1.txt","root/c/3.txt"]
-        System.out.println(prob.findDuplicate(new String[]{
-            "root/a 1.txt(abcd) 2.txt(efgh)",
-            "root/c 3.txt(abcd)",
-            "root/c/d 4.txt(efgh)",
-            "root 4.txt(efgh)"}));
+        Map<String, List<String>> map = new HashMap<>();
+        Pattern pattern = Pattern.compile("(.*)(\\(.*\\))");
+        for (String path : paths) {
+            String[] split = path.split(" ");
+            for (int i = 1; i < split.length; i++) {
+                Matcher matcher = pattern.matcher(split[i]);
+                if (matcher.matches()) {
+                    String file = matcher.group(1);
+                    String content = matcher.group(2);
+                    String fullPath = split[0] + "/" + file;
+                    if (!map.containsKey(content)) {
+                        List<String> list = new ArrayList<>();
+                        list.add(fullPath);
+                        map.put(content, list);
+                    } else {
+                        map.get(content).add(fullPath);
+                    }
+                }
+            }
+        }
+        List<List<String>> result = new ArrayList<>();
+        for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+            if (entry.getValue().size() > 1) {
+                result.add(entry.getValue());
+            }
+        }
+        return result;
     }
 }
