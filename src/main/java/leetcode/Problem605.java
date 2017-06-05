45f0dfead1b23fcb82669b1b293de333d012d7ed
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-06-04 20:02:15 -0500

    Solve problem 605

--- src/main/java/leetcode/Problem605.java
+++ src/main/java/leetcode/Problem605.java
@@ -38,24 +38,4 @@ public class Problem605 {
         }
         return flowers >= n;
     }
-
-    public static void main(String[] args) {
-        Problem605 prob = new Problem605();
-        System.out.println(prob.canPlaceFlowers(new int[]{1, 0, 0, 0, 1}, 1)); // true
-        System.out.println(prob.canPlaceFlowers(new int[]{1, 0, 0, 0, 1}, 2)); // false
-        System.out.println(prob.canPlaceFlowers(new int[]{1, 0, 1, 0, 1}, 2)); // false
-        System.out.println(prob.canPlaceFlowers(new int[]{1, 0, 1, 0, 1}, 3)); // false
-        System.out.println(prob.canPlaceFlowers(new int[]{0, 0, 0, 0, 0, 0, 1}, 3)); // true
-        System.out.println(prob.canPlaceFlowers(new int[]{0, 0, 0, 1, 0, 0, 1}, 1)); // true
-        System.out.println(prob.canPlaceFlowers(new int[]{0, 0, 0, 1, 0, 0, 1}, 2)); // false
-        System.out.println(prob.canPlaceFlowers(new int[]{1, 0, 0, 0, 1, 0, 0}, 2)); // true
-        System.out.println(prob.canPlaceFlowers(new int[]{0}, 1)); // true
-        System.out.println(prob.canPlaceFlowers(new int[]{0, 0}, 1)); // true
-        System.out.println(prob.canPlaceFlowers(new int[]{1}, 1)); // false
-        System.out.println(prob.canPlaceFlowers(new int[]{1}, 0)); // true
-        System.out.println(prob.canPlaceFlowers(new int[]{1, 0}, 1)); // false
-        System.out.println(prob.canPlaceFlowers(new int[]{0, 0, 0}, 2)); // true
-        System.out.println(prob.canPlaceFlowers(new int[]{0, 0, 0, 0}, 2)); // true
-        System.out.println(prob.canPlaceFlowers(new int[]{0, 0, 0, 0}, 3)); // false
-    }
 }
