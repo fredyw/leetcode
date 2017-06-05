42af4247a858b3a4f60bbaf87ba1ca6ae70e428c
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-06-04 19:09:25 -0500

    Update problem 605

--- src/main/java/leetcode/Problem605.java
+++ src/main/java/leetcode/Problem605.java
@@ -15,5 +15,6 @@ public class Problem605 {
         System.out.println(prob.canPlaceFlowers(new int[]{1, 0, 0, 0, 1}, 2)); // false
         System.out.println(prob.canPlaceFlowers(new int[]{1, 0, 1, 0, 1}, 2)); // false
         System.out.println(prob.canPlaceFlowers(new int[]{1, 0, 1, 0, 1}, 3)); // false
+        System.out.println(prob.canPlaceFlowers(new int[]{0, 0, 0, 0, 0, 0, 1}, 3)); // true
     }
 }
