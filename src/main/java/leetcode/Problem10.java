62d981c535399ae13d32dcd9ae155b2c18f95e6a
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-06-05 20:09:20 -0500

    Update problem 10

--- src/main/java/leetcode/Problem10.java
+++ src/main/java/leetcode/Problem10.java
@@ -9,20 +9,21 @@ public class Problem10 {
     }
 
     private static boolean isMatch(String s, String p, int i, int j) {
-        if (i == s.length() && j == p.length()) {
+        if ((i == s.length() && j == p.length()) ||
+            (i == s.length() && j + 2 == p.length() && p.charAt(j + 1) == '*')) {
             return true;
         }
-        if (i >= s.length() || j >= p.length()) {
+        if (j >= p.length()) {
             return false;
         }
         boolean match = false;
         if (j + 1 < p.length() && p.charAt(j + 1) == '*') {
-            if (s.charAt(i) == p.charAt(j)) {
+            if (i < s.length() && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.')) {
                 match |= isMatch(s, p, i + 1, j);
             }
-            match |= isMatch(s, p, i + 1, j + 2);
+            match |= isMatch(s, p, i, j + 2);
         } else {
-            if (s.charAt(i) != p.charAt(j) && p.charAt(j) != '.') {
+            if (i < s.length() && s.charAt(i) != p.charAt(j) && p.charAt(j) != '.') {
                 return false;
             }
             match |= isMatch(s, p, i + 1, j + 1);
@@ -37,10 +38,14 @@ public class Problem10 {
         System.out.println(prob.isMatch("aaa", "aa")); // false
         System.out.println(prob.isMatch("aa", "a*")); // true
         System.out.println(prob.isMatch("caab", "c.a.")); // true
-//        System.out.println(prob.isMatch("aab", "c*a*b")); // true
-//        System.out.println(prob.isMatch("caab", "c*a*b")); // true
-//        System.out.println(prob.isMatch("aa", ".*")); // true
-//        System.out.println(prob.isMatch("ab", ".*")); // true
-//        System.out.println(prob.isMatch("abb", "a.*b")); // true
+        System.out.println(prob.isMatch("aab", "c*a*b")); // true
+        System.out.println(prob.isMatch("caab", "c*a*b")); // true
+        System.out.println(prob.isMatch("aa", ".*")); // true
+        System.out.println(prob.isMatch("ab", ".*")); // true
+        System.out.println(prob.isMatch("abb", "a.*b")); // true
+        System.out.println(prob.isMatch("abb", "a.*b")); // true
+        System.out.println(prob.isMatch("abb", "a.*")); // true
+        System.out.println(prob.isMatch("aaa", "ab*a")); // false
+        System.out.println(prob.isMatch("", "c*c*")); // true
     }
 }
