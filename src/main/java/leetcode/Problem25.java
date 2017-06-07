95428bc2d3c8ea122d36c17778a70c08404ca0b6
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-06-06 23:36:51 -0500

    Solve problem 25

--- src/main/java/leetcode/Problem25.java
+++ src/main/java/leetcode/Problem25.java
@@ -11,59 +11,47 @@ public class Problem25 {
         ListNode(int x) {
             val = x;
         }
-
-        @Override
-        public String toString() {
-            return "" + val;
-        }
     }
 
     public ListNode reverseKGroup(ListNode head, int k) {
-        ListNodeRef ref = new ListNodeRef();
-        ListNode n = reverse(head, k, 1, ref);
+        int size = 0;
+        for (ListNode n = head; n != null; n = n.next) {
+            size++;
+        }
+        if (head == null || size < k) {
+            return head;
+        }
+        Ref ref = new Ref();
+        ListNode n = reverse(head, k, 1, (size / k) * k, ref);
         n.next = ref.second;
         return ref.first;
     }
 
-    private static class ListNodeRef {
+    private static class Ref {
         private ListNode first;
         private ListNode second;
+        private int count;
     }
 
-    private static ListNode reverse(ListNode node, int k, int i, ListNodeRef ref) {
-        if (i == k) {
+    private static ListNode reverse(ListNode node, int k, int i, int j, Ref ref) {
+        if (i == j) {
             ref.first = node;
             ref.second = node.next;
+            ref.count++;
             return node;
         }
-        ListNode n = reverse(node.next, k, i + 1, ref);
-        n.next = node;
-        node.next = null;
-        return node;
-    }
-
-    private static void print(ListNode node) {
-        for (ListNode n = node; n != null; n = n.next) {
-            System.out.print(n + " ");
+        ListNode n = reverse(node.next, k, i + 1, j, ref);
+        if (ref.count < k) {
+            n.next = node;
+            node.next = null;
+            ref.count++;
+        } else {
+            node.next = ref.first;
+            n.next = ref.second;
+            ref.first = node;
+            ref.second = node.next;
+            ref.count = 1;
         }
-        System.out.println();
-    }
-
-    public static void main(String[] args) {
-        Problem25 prob = new Problem25();
-
-        ListNode head = new ListNode(1);
-        head.next = new ListNode(2);
-        head.next.next = new ListNode(3);
-        head.next.next.next = new ListNode(4);
-        head.next.next.next.next = new ListNode(5);
-        print(prob.reverseKGroup(head, 2)); // 2 1 4 3 5
-
-        head = new ListNode(1);
-        head.next = new ListNode(2);
-        head.next.next = new ListNode(3);
-        head.next.next.next = new ListNode(4);
-        head.next.next.next.next = new ListNode(5);
-        print(prob.reverseKGroup(head, 3)); // 3 2 1 4 5 
+        return node;
     }
 }
