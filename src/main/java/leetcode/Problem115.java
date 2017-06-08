f4d5122f80710d90434ab604ba3565232db8b793
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-06-08 00:32:55 -0500

    Skeleton for problem 115`

---
+++ src/main/java/leetcode/Problem115.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/distinct-subsequences/
+ */
+public class Problem115 {
+    public int numDistinct(String s, String t) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem115 prob = new Problem115();
+        System.out.println(prob.numDistinct("rabbbit", "rabbit")); // 3
+    }
+}
