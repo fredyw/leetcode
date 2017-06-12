1a8957b66fab4cd1ae879b99618e52ee2277fb1b
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-06-11 21:31:13 -0500

    Update problem 146

--- src/main/java/leetcode/Problem146.java
+++ src/main/java/leetcode/Problem146.java
@@ -42,26 +42,34 @@ public class Problem146 {
             return node.value;
         }
 
-        // 1 (tail) <-> 2 <-> 3 (node) <-> 4 <-> 5 (head)
+        // Case 1:
+        // 1 (tail) <-> 2 <-> 3 <-> 4 <-> 5 (head)
+        // get(5)
+        // 1 (tail) <-> 2 <-> 3 <-> 4 <-> 5 (head)
+        //
+        // Case 2:
+        // 1 (tail) <-> 2 <-> 3 <-> 4 <-> 5 (head)
         // get(3)
         // 1 (tail) <-> 2 <-> 4 <-> 5 <-> 3 (head)
+        //
+        // Case 3:
+        // 1 (tail) <-> 2 <-> 3 <-> 4 <-> 5 (head)
+        // get(1)
+        // 2 (tail) <-> 3 <-> 4 <-> 5 <-> 1 (head)
         private Node getNode(int key) {
             if (!map.containsKey(key)) {
                 return null;
             }
             Node node = map.get(key);
-            if (node.previous != null) {
-                node.previous.next = node.next;
-            } else { // node.previous == tail
-                if (tail.next != null) {
-                    // there's no previous, we need to update the tail
-                    tail = tail.next;
-                    if (tail != null) {
-                        tail.previous = null;
-                    }
-                }
+            // case 1
+            if (head == node) {
+                return node;
             }
-            if (node.next != null) {
+            if (tail == node) { // case 3
+                tail = tail.next;
+                tail.previous = null;
+            } else { // case 2
+                node.previous.next = node.next;
                 node.next.previous = node.previous;
             }
             // update the new head
@@ -94,9 +102,7 @@ public class Problem146 {
                     if (map.size() == capacity) {
                         map.remove(tail.key);
                         tail = tail.next;
-                        if (tail != null) {
-                            tail.previous = null; // to be a new tail, there should not be a previous
-                        }
+                        tail.previous = null; // to be a new tail, there should not be a previous
                     }
                 }
                 map.put(key, newNode);
@@ -104,70 +110,5 @@ public class Problem146 {
                 node.value = value;
             }
         }
-    }
-
-    public static void main(String[] args) {
-        LRUCache lru = new LRUCache(2);
-//        lru.put(1, 1);
-//        lru.put(2, 2);
-//        System.out.println(lru.get(1)); // 1
-//        lru.put(3, 3);
-//        System.out.println(lru.get(2)); // -1
-//        lru.put(4, 4);
-//        System.out.println(lru.get(1)); // -1
-//        System.out.println(lru.get(3)); // 3
-//        System.out.println(lru.get(4)); // 4
-//
-//        lru = new LRUCache(1);
-//        lru.put(2, 1);
-//        System.out.println(lru.get(2)); // 1;
-//        lru.put(3, 2);
-//        System.out.println(lru.get(2)); // -1
-//        System.out.println(lru.get(3)); // 2
-
-        lru = new LRUCache(10);
-        lru.put(10, 13);
-        lru.put(3, 17);
-        lru.put(6, 11);
-        lru.put(10, 5);
-        lru.put(9, 10);
-        System.out.println(lru.get(13)); // -1
-        lru.put(2, 19);
-        System.out.println(lru.get(2)); // 19
-        System.out.println(lru.get(3)); // 17
-        lru.put(5, 25);
-        System.out.println(lru.get(8)); // -1
-        lru.put(9, 22);
-        lru.put(5, 5);
-        lru.put(1, 30);
-        System.out.println(lru.get(11)); // -1
-        lru.put(9, 12);
-        System.out.println(lru.get(7)); // -1
-        System.out.println(lru.get(5)); // 5
-        System.out.println(lru.get(8)); // -1
-        System.out.println(lru.get(9)); // 12
-        lru.put(4, 30);
-        lru.put(9, 3);
-        System.out.println(lru.get(9));
-        System.out.println(lru.get(10));
-        System.out.println(lru.get(10));
-        lru.put(6, 14);
-        lru.put(3, 1);
-        System.out.println(lru.get(3));
-        lru.put(10, 11);
-        System.out.println(lru.get(8));
-        lru.put(2, 14);
-        System.out.println(lru.get(1));
-        System.out.println(lru.get(5));
-        System.out.println(lru.get(4));
-        lru.put(11, 4);
-        System.out.println(lru.map + ": " + lru.tail);
-        lru.put(12, 24);
-        System.out.println(lru.map + ": " + lru.tail);
-        lru.put(5, 18);
-        System.out.println(lru.map + ": " + lru.tail);
-        System.out.println(lru.get(13));
-        lru.put(7, 23);
-        System.out.println(lru.map + ": " + lru.tail);
     }
 }
