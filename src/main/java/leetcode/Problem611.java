81b269e575ac5892db4c9c71f0976d2be4113758
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-06-14 00:16:13 -0500

    Skeleton for problem 611

---
+++ src/main/java/leetcode/Problem611.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/valid-triangle-number/
+ */
+public class Problem611 {
+    public int triangleNumber(int[] nums) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem611 prob = new Problem611();
+        System.out.println(prob.triangleNumber(new int[]{2, 2, 3, 4})); // 3
+    }
+}
