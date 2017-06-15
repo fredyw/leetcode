08938ec2a7e79a59c534de83d4a8609395de870f
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-06-15 18:52:16 -0500

    Skeleton for problem 522

---
+++ src/main/java/leetcode/Problem522.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/longest-uncommon-subsequence-ii/
+ */
+public class Problem522 {
+    public int findLUSlength(String[] strs) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem522 prob = new Problem522();
+        System.out.println(prob.findLUSlength(new String[]{"aba", "cdc", "eae"})); // 3
+    }
+}
