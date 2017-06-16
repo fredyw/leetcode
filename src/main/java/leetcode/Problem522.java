f49f173802d6987274fd342314889a2051a8f84f
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-06-16 08:35:14 -0500

    Update problem 522

--- src/main/java/leetcode/Problem522.java
+++ src/main/java/leetcode/Problem522.java
@@ -5,14 +5,35 @@ package leetcode;
  */
 public class Problem522 {
     public int findLUSlength(String[] strs) {
-        // TODO
-        return 0;
+        int max = 0;
+        for (int i = 0; i < strs.length; i++) {
+            for (int j = i + 1; j < strs.length; j++) {
+                String a = strs[i];
+                String b = strs[j];
+                if (a.length() < b.length()) {
+                    a = strs[j];
+                    b = strs[i];
+                }
+                int count = 0;
+                for (int x = 0; x < a.length(); x++) {
+                    int y = 0;
+                    while (y < b.length() && a.charAt(x) != b.charAt(y)) {
+                        y++;
+                    }
+                    if (y < b.length()) {
+                        count++;
+                    }
+                }
+                max = Math.max(max, a.length() - count);
+            }
+        }
+        return max;
     }
 
     public static void main(String[] args) {
         Problem522 prob = new Problem522();
-        System.out.println(prob.findLUSlength(new String[]{"aba", "cdc", "eae"})); // 3
+//        System.out.println(prob.findLUSlength(new String[]{"aba", "cdc", "eae"})); // 3
         System.out.println(prob.findLUSlength(new String[]{"aba", "eae"})); // 2
-        System.out.println(prob.findLUSlength(new String[]{"aabbcc", "aabbcc","cb","abc"})); // 2
+//        System.out.println(prob.findLUSlength(new String[]{"aabbcc", "aabbcc","cb","abc"})); // 2
     }
 }
