e3b51ef598b8a1b7c4d912c7a724cdf5311be1fa
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-06-18 10:28:18 -0500

    Solve problem 522

--- src/main/java/leetcode/Problem522.java
+++ src/main/java/leetcode/Problem522.java
@@ -8,21 +8,20 @@ import java.util.Set;
  */
 public class Problem522 {
     public int findLUSlength(String[] strs) {
-        int max = -1;
         Set<String> ignored =  new HashSet<>();
         for (int i = 0; i < strs.length; i++) {
             for (int j = i + 1; j < strs.length; j++) {
                 if (isSubsequence(strs[i], strs[j])) {
                     ignored.add(strs[i].length() <= strs[j].length() ? strs[i] : strs[j]);
-                } else {
-                    if (!ignored.contains(strs[i])) {
-                        max = Math.max(max, strs[i].length());
-                    }
-                    if (!ignored.contains(strs[j])) {
-                        max = Math.max(max, strs[j].length());
-                    }
                 }
             }
+        }
+        int max = -1;
+        for (int i = 0; i < strs.length; i++) {
+            if (ignored.contains(strs[i])) {
+                continue;
+            }
+            max = Math.max(max, strs[i].length());
         }
         return max;
     }
@@ -47,32 +46,9 @@ public class Problem522 {
                 break;
             }
         }
-        if (i == str1.length() || str1.charAt(i) == str2.charAt(j - 1)) {
+        if (i == str1.length() || (i == (str1.length() - 1) && str1.charAt(i) == str2.charAt(j - 1))) {
             return true;
         }
         return false;
-    }
-
-    public static void main(String[] args) {
-        Problem522 prob = new Problem522();
-//        System.out.println(isSubsequence("abcde", "xyz"));
-//        System.out.println(isSubsequence("aaabbbccc", "abc"));
-//        System.out.println(isSubsequence("aaabbbccc", "aabcc"));
-//        System.out.println(isSubsequence("aaabbbccc", "aabccd"));
-//        System.out.println(isSubsequence("abc", "xyz"));
-//        System.out.println(isSubsequence("aaa", "acb"));
-//        System.out.println(isSubsequence("aabbcc", "bcc"));
-        System.out.println(prob.findLUSlength(new String[]{"aba", "cdc", "eae"})); // 3
-        System.out.println(prob.findLUSlength(new String[]{"aba", "eae"})); // 3
-        System.out.println(prob.findLUSlength(new String[]{"xbc", "acd"})); // 3
-        System.out.println(prob.findLUSlength(new String[]{"abc", "x"})); // 3
-        System.out.println(prob.findLUSlength(new String[]{"aabbcc", "aabbcc", "cb", "abc"})); // 2
-        System.out.println(prob.findLUSlength(new String[]{"aabbcc", "aabbcc", "abc"})); // -1
-        System.out.println(prob.findLUSlength(new String[]{"abc", "abc", "xyzdef", "xyzd"})); // 6
-        System.out.println(prob.findLUSlength(new String[]{"aaa", "aaa", "aa"})); // -1
-        System.out.println(prob.findLUSlength(new String[]{"aabbcc", "aabbcc", "cb"})); // 2
-        System.out.println(prob.findLUSlength(new String[]{"aaa", "acb"})); // 3
-        System.out.println(prob.findLUSlength(new String[]{"aabbcc", "aabbcc", "bc", "bcc"})); // -1
-        System.out.println(prob.findLUSlength(new String[]{"aabbcc", "aabbcc","bc","bcc","aabbccc"})); // 7
     }
 }
