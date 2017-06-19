7ed275d0a12eaad4a7b6ff1c1d67afb33c8a6a36
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-06-18 23:25:47 -0500

    Solve problem 623

--- src/main/java/leetcode/Problem623.java
+++ src/main/java/leetcode/Problem623.java
@@ -12,33 +12,35 @@ public class Problem623 {
         TreeNode(int x) {
             val = x;
         }
-
-        @Override
-        public String toString() {
-            return "" + val;
-        }
     }
 
     public TreeNode addOneRow(TreeNode root, int v, int d) {
-        // TODO
+        if (d == 1) {
+            TreeNode newRoot = new TreeNode(v);
+            newRoot.left = root;
+            return newRoot;
+        }
+        addOneRow(root, v, d, 1);
         return root;
     }
 
-    public static void main(String[] args) {
-        Problem623 prob = new Problem623();
+    private static void addOneRow(TreeNode node, int v, int d, int i) {
+        if (node == null) {
+            return;
+        }
+        if (d - 1 == i) {
+            TreeNode tmpLeft = node.left;
+            TreeNode tmpRight = node.right;
+            node.left = new TreeNode(v);
+            node.left.left = tmpLeft;
+            node.right = new TreeNode(v);
+            node.right.right = tmpRight;
 
-        TreeNode root = new TreeNode(4);
-        root.left = new TreeNode(2);
-        root.left.left = new TreeNode(3);
-        root.left.right = new TreeNode(1);
-        root.right = new TreeNode(6);
-        root.right.left = new TreeNode(5);
-        System.out.println(prob.addOneRow(root, 1, 2));
-
-        root = new TreeNode(4);
-        root.left = new TreeNode(2);
-        root.left.left = new TreeNode(1);
-        root.left.right = new TreeNode(1);
-        System.out.println(prob.addOneRow(root, 1, 3));
+            addOneRow(tmpLeft, v, d, i + 1);
+            addOneRow(tmpRight, v, d, i + 1);
+        } else {
+            addOneRow(node.left, v, d, i + 1);
+            addOneRow(node.right, v, d, i + 1);
+        }
     }
 }
