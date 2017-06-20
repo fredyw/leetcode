93c585f9e609fd15eae31e130711532916e6d5f6
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-06-19 21:06:13 -0500

    Skeleton for problem 621

---
+++ src/main/java/leetcode/Problem621.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/task-scheduler/
+ */
+public class Problem621 {
+    public int leastInterval(char[] tasks, int n) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem621 prob = new Problem621();
+        System.out.println(prob.leastInterval(new char[]{'A','A','A','B','B','B'}, 2)); // 8
+    }
+}
