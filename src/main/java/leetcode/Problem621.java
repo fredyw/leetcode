5427ea933e8ba4532ce4969ed95c350bc6ad62aa
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-06-19 21:17:33 -0500

    Update problem 621

--- src/main/java/leetcode/Problem621.java
+++ src/main/java/leetcode/Problem621.java
@@ -12,5 +12,8 @@ public class Problem621 {
     public static void main(String[] args) {
         Problem621 prob = new Problem621();
         System.out.println(prob.leastInterval(new char[]{'A','A','A','B','B','B'}, 2)); // 8
+        System.out.println(prob.leastInterval(new char[]{'A','A','A','B','B','B'}, 1)); // 6
+        System.out.println(prob.leastInterval(new char[]{'A','B','B','B'}, 2)); // 7
+        System.out.println(prob.leastInterval(new char[]{'A','B','B','B'}, 1)); // 5
     }
 }
