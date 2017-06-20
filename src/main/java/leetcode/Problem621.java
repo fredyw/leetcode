9f6aea7ad094c71b90fbe01f0d9a0c7944bf8b4c
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-06-19 23:48:58 -0500

    Update problem 621

--- src/main/java/leetcode/Problem621.java
+++ src/main/java/leetcode/Problem621.java
@@ -13,7 +13,14 @@ public class Problem621 {
         Problem621 prob = new Problem621();
         System.out.println(prob.leastInterval(new char[]{'A','A','A','B','B','B'}, 2)); // 8
         System.out.println(prob.leastInterval(new char[]{'A','A','A','B','B','B'}, 1)); // 6
+        System.out.println(prob.leastInterval(new char[]{'A','A','A','B','B','B'}, 3)); // 10
         System.out.println(prob.leastInterval(new char[]{'A','B','B','B'}, 2)); // 7
         System.out.println(prob.leastInterval(new char[]{'A','B','B','B'}, 1)); // 5
+        System.out.println(prob.leastInterval(new char[]{'A','B','C'}, 1)); // 3
+        System.out.println(prob.leastInterval(new char[]{'A','B','C'}, 2)); // 3
+        System.out.println(prob.leastInterval(new char[]{'A','B','C'}, 3)); // 3
+        System.out.println(prob.leastInterval(new char[]{'A','B','C','A'}, 3)); // 4
+        System.out.println(prob.leastInterval(new char[]{'A','B','C','A','C'}, 3)); // 6
+        System.out.println(prob.leastInterval(new char[]{'A','B','C','D','C'}, 3)); // 5
     }
 }
