65e6224d0c533231ca7b8f3641e8e7c2fa7e4a42
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-06-24 00:10:13 -0500

    Skeleton for problem 514

---
+++ src/main/java/leetcode/Problem514.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/freedom-trail/
+ */
+public class Problem514 {
+    public int findRotateSteps(String ring, String key) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem514 prob = new Problem514();
+        System.out.println(prob.findRotateSteps("godding", "gd")); // 4
+    }
+}
