1fe790e90095332b5d50de6c6618978e398e4c0c
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-06-26 01:03:57 -0500

    Skeleton for problem 628

---
+++ src/main/java/leetcode/Problem628.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/maximum-product-of-three-numbers/
+ */
+public class Problem628 {
+    public int maximumProduct(int[] nums) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem628 prob = new Problem628();
+        System.out.println(prob.maximumProduct(new int[]{1, 2, 3})); // 6
+        System.out.println(prob.maximumProduct(new int[]{1, 2, 3, 4})); // 24
+    }
+}
