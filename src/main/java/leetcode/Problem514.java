62a5b3d4305e08e696210d3450afff69c0194a61
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-06-25 23:55:49 -0500

    Update problem 514

--- src/main/java/leetcode/Problem514.java
+++ src/main/java/leetcode/Problem514.java
@@ -6,35 +6,50 @@ package leetcode;
 public class Problem514 {
     public int findRotateSteps(String ring, String key) {
         // TODO
-        return 0;
+        return findRotateSteps(ring, key, 0, 0);
     }
 
-    private static int findRotateSteps(String ring, String key, int idx) {
-        int count1 = 0;
-        int i = idx;
-        while (key.charAt(idx) != ring.charAt(i)) {
-            i++;
-            if (i == ring.length()) {
-                i = 0;
-            }
-            count1++;
+    private static int findRotateSteps(String ring, String key, int keyIdx, int startIdx) {
+        if (keyIdx == key.length()) {
+            return 0;
         }
-        int count2 = 0;
-        int j = idx;
-        while (key.charAt(idx) != ring.charAt(j)) {
-            j--;
-            if (j == -1) {
-                j = ring.length() - 1;
+        int count1 = 0;
+        int i = startIdx;
+        if (key.charAt(keyIdx) == ring.charAt(i)) {
+            count1++;
+        } else {
+            while (key.charAt(keyIdx) != ring.charAt(i)) {
+                i++;
+                if (i == ring.length()) {
+                    i = 0;
+                }
+                count1++;
             }
+        }
+
+        int count2 = 0;
+        int j = startIdx;
+        if (key.charAt(keyIdx) == ring.charAt(j)) {
             count2++;
+        } else {
+            while (key.charAt(keyIdx) != ring.charAt(j)) {
+                j--;
+                if (j == -1) {
+                    j = ring.length() - 1;
+                }
+                count2++;
+            }
         }
         int min = Math.min(count1, count2);
+        int a = findRotateSteps(ring, key, keyIdx + 1, i) + min;
+        int b = findRotateSteps(ring, key, keyIdx + 1, j) + min;
+        min = Math.min(a, b);
         return min;
     }
 
     public static void main(String[] args) {
         Problem514 prob = new Problem514();
         System.out.println(prob.findRotateSteps("godding", "gd")); // 4
-        System.out.println(prob.findRotateSteps("godding", "gdo")); // 6
+//        System.out.println(prob.findRotateSteps("godding", "gdo")); // 6
     }
 }
