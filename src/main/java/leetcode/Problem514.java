f90c34fc1c554935051a58937aac2876039c3c8c
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-06-26 00:54:50 -0500

    Solve problem 514

--- src/main/java/leetcode/Problem514.java
+++ src/main/java/leetcode/Problem514.java
@@ -5,51 +5,45 @@ package leetcode;
  */
 public class Problem514 {
     public int findRotateSteps(String ring, String key) {
-        // TODO
-        return findRotateSteps(ring, key, 0, 0);
+        int[][] memo = new int[ring.length()][key.length()];
+        for (int i = 0; i < ring.length(); i++) {
+           for (int j = 0; j < key.length(); j++) {
+               memo[i][j] = -1;
+           }
+        }
+        return findRotateSteps(ring, key, 0, 0, memo);
     }
 
-    private static int findRotateSteps(String ring, String key, int keyIdx, int startIdx) {
+    private static int findRotateSteps(String ring, String key, int keyIdx, int startIdx, int[][] memo) {
         if (keyIdx == key.length()) {
             return 0;
         }
-        int count1 = 0;
+        if (memo[startIdx][keyIdx] != -1) {
+            return memo[startIdx][keyIdx];
+        }
+        int count1 = 1;
         int i = startIdx;
-        if (key.charAt(keyIdx) == ring.charAt(i)) {
+        while (key.charAt(keyIdx) != ring.charAt(i)) {
+            i++;
+            if (i == ring.length()) {
+                i = 0;
+            }
             count1++;
-        } else {
-            while (key.charAt(keyIdx) != ring.charAt(i)) {
-                i++;
-                if (i == ring.length()) {
-                    i = 0;
-                }
-                count1++;
-            }
         }
 
-        int count2 = 0;
+        int count2 = 1;
         int j = startIdx;
-        if (key.charAt(keyIdx) == ring.charAt(j)) {
-            count2++;
-        } else {
-            while (key.charAt(keyIdx) != ring.charAt(j)) {
-                j--;
-                if (j == -1) {
-                    j = ring.length() - 1;
-                }
-                count2++;
+        while (key.charAt(keyIdx) != ring.charAt(j)) {
+            j--;
+            if (j == -1) {
+                j = ring.length() - 1;
             }
+            count2++;
         }
-        int min = Math.min(count1, count2);
-        int a = findRotateSteps(ring, key, keyIdx + 1, i) + min;
-        int b = findRotateSteps(ring, key, keyIdx + 1, j) + min;
-        min = Math.min(a, b);
+        int a = findRotateSteps(ring, key, keyIdx + 1, i, memo) + count1;
+        int b = findRotateSteps(ring, key, keyIdx + 1, j, memo) + count2;
+        int min = Math.min(a, b);
+        memo[startIdx][keyIdx] = min;
         return min;
-    }
-
-    public static void main(String[] args) {
-        Problem514 prob = new Problem514();
-        System.out.println(prob.findRotateSteps("godding", "gd")); // 4
-//        System.out.println(prob.findRotateSteps("godding", "gdo")); // 6
     }
 }
