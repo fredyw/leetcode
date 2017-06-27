96628073764c681519ba7e37fcf256066d89251e
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-06-26 22:53:06 -0500

    Skeleton fofr problem 407

---
+++ src/main/java/leetcode/Problem407.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/trapping-rain-water-ii/
+ */
+public class Problem407 {
+    public int trapRainWater(int[][] heightMap) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem407 prob = new Problem407();
+        System.out.println(prob.trapRainWater(new int[][]{
+            {1, 4, 3, 1, 3, 2},
+            {3, 2, 1, 3, 2, 4},
+            {2, 3, 3, 2, 3, 1}
+        })); // 4
+    }
+}
