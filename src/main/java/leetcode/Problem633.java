241a12b36d3093520c7ea75bdd4eb89b5a0f0ee7
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-07-06 00:16:01 -0500

    Skeleton for problem 633

---
+++ src/main/java/leetcode/Problem633.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/sum-of-square-numbers/
+ */
+public class Problem633 {
+    public boolean judgeSquareSum(int c) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem633 prob = new Problem633();
+        System.out.println(prob.judgeSquareSum(5)); // true
+        System.out.println(prob.judgeSquareSum(3)); // false
+    }
+}
