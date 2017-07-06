95883f06c7fd0ae67fd4d0db382ea6594b29cea8
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-07-06 01:07:34 -0500

    Solve problem 633

--- src/main/java/leetcode/Problem633.java
+++ src/main/java/leetcode/Problem633.java
@@ -5,20 +5,19 @@ package leetcode;
  */
 public class Problem633 {
     public boolean judgeSquareSum(int c) {
-        for (int i = 1; i <= c / 2; i++) {
-            double a = Math.sqrt(i);
-            double b = Math.sqrt(c - i);
-            if (a == (int) a && b == (int) b) {
+        if (c == 0) {
+            return true;
+        }
+        for (int i = 1; i <= Math.sqrt(c); i++) {
+            int a = i * i;
+            if (a > c) {
+                break;
+            }
+            double b = Math.sqrt(c - a);
+            if (b == (int) b) {
                 return true;
             }
         }
         return false;
-    }
-
-    public static void main(String[] args) {
-        Problem633 prob = new Problem633();
-        System.out.println(prob.judgeSquareSum(5)); // true
-        System.out.println(prob.judgeSquareSum(3)); // false
-        System.out.println(prob.judgeSquareSum(Integer.MAX_VALUE)); // false
     }
 }
