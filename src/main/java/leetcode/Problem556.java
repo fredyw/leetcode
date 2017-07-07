ab5dc6d3e9d238e33e6896795559cd1f5fb0028f
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-07-07 01:09:07 -0500

    Update problem 556

--- src/main/java/leetcode/Problem556.java
+++ src/main/java/leetcode/Problem556.java
@@ -13,5 +13,12 @@ public class Problem556 {
         Problem556 prob = new Problem556();
         System.out.println(prob.nextGreaterElement(12)); // 21
         System.out.println(prob.nextGreaterElement(21)); // -1
+        System.out.println(prob.nextGreaterElement(14526)); // 14562
+        System.out.println(prob.nextGreaterElement(14526)); // 14562
+        System.out.println(prob.nextGreaterElement(1452)); // 1524
+        System.out.println(prob.nextGreaterElement(14520)); // 15024
+        System.out.println(prob.nextGreaterElement(1002)); // 1020
+        System.out.println(prob.nextGreaterElement(1233)); // 1323
+        System.out.println(prob.nextGreaterElement(4321)); // -1
     }
 }
