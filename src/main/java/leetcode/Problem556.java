1a8c72051473b0a9881737950ed6b4516afb97a5
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-07-07 00:20:19 -0500

    Skeleton for problem 556

---
+++ src/main/java/leetcode/Problem556.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/next-greater-element-iii/
+ */
+public class Problem556 {
+    public int nextGreaterElement(int n) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem556 prob = new Problem556();
+        System.out.println(prob.nextGreaterElement(12)); // 21
+        System.out.println(prob.nextGreaterElement(21)); // -1
+    }
+}
