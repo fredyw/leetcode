153984af8b2f568428814e8bac72f4170832b5f1
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-07-07 23:36:09 -0500

    Solve problem 556

--- src/main/java/leetcode/Problem556.java
+++ src/main/java/leetcode/Problem556.java
@@ -29,20 +29,10 @@ public class Problem556 {
         if (!found) {
             return -1;
         }
-        return Integer.parseInt(new String(c));
-    }
-
-    public static void main(String[] args) {
-        Problem556 prob = new Problem556();
-        System.out.println(prob.nextGreaterElement(12)); // 21
-        System.out.println(prob.nextGreaterElement(21)); // -1
-        System.out.println(prob.nextGreaterElement(14526)); // 14562
-        System.out.println(prob.nextGreaterElement(1452)); // 1524
-        System.out.println(prob.nextGreaterElement(14520)); // 15024
-        System.out.println(prob.nextGreaterElement(1002)); // 1020
-        System.out.println(prob.nextGreaterElement(1233)); // 1323
-        System.out.println(prob.nextGreaterElement(4321)); // -1
-        System.out.println(prob.nextGreaterElement(12750)); // 15027
-        System.out.println(prob.nextGreaterElement(16750)); // 17056
+        try {
+            return Integer.parseInt(new String(c));
+        } catch (NumberFormatException e) {
+            return -1;
+        }
     }
 }
