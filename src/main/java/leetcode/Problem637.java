e6e82ba56140fdff4b41925bcc5543f2b38a28b0
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-07-11 15:44:34 -0500

    Solve problem 637

--- src/main/java/leetcode/Problem637.java
+++ src/main/java/leetcode/Problem637.java
@@ -1,14 +1,18 @@
 package leetcode;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Queue;
 
 /**
  * https://leetcode.com/problems/average-of-levels-in-binary-tree/
  */
 public class Problem637 {
     public static class TreeNode {
-        int val;
+        long val;
         TreeNode left;
         TreeNode right;
 
@@ -18,8 +22,51 @@ public class Problem637 {
     }
 
     public List<Double> averageOfLevels(TreeNode root) {
-        // TODO
-        return new ArrayList<>();
+        Map<Integer, ValueCount> map = new HashMap<>();
+        Queue<LevelNode> queue = new LinkedList<>();
+        queue.add(new LevelNode(1, root));
+        while (!queue.isEmpty()) {
+            LevelNode current = queue.remove();
+            if (!map.containsKey(current.level)) {
+                map.put(current.level, new ValueCount(current.node.val, 1));
+            } else {
+                ValueCount valueCount = map.get(current.level);
+                valueCount.count++;
+                valueCount.value += current.node.val;
+            }
+            if (current.node.left != null) {
+                queue.add(new LevelNode(current.level + 1, current.node.left));
+            }
+            if (current.node.right != null) {
+                queue.add(new LevelNode(current.level + 1, current.node.right));
+            }
+        }
+        List<Double> result = new ArrayList<>();
+        for (int i = 1; i <= map.size(); i++) {
+            ValueCount valueCount = map.get(i);
+            result.add((double) valueCount.value / (double) valueCount.count);
+        }
+        return result;
+    }
+
+    private static class ValueCount {
+        private long value;
+        private int count;
+
+        public ValueCount(long value, int count) {
+            this.value = value;
+            this.count = count;
+        }
+    }
+
+    private static class LevelNode {
+        private final int level;
+        private final TreeNode node;
+
+        public LevelNode(int level, TreeNode node) {
+            this.level = level;
+            this.node = node;
+        }
     }
 
     public static void main(String[] args) {
