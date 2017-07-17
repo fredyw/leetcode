0a413cc318f30ac299cd8346c58248382c261935
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-07-17 02:07:12 -0500

    Skeleton for problem 643

---
+++ src/main/java/leetcode/Problem643.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/maximum-average-subarray-i/
+ */
+public class Problem643 {
+    public double findMaxAverage(int[] nums, int k) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem643 prob = new Problem643();
+        System.out.println(prob.findMaxAverage(new int[]{1, 12, -5, -6, 50, 3}, 4)); // 12.75
+    }
+}
