39cd5fb70ad07dd33e59919c6bece0150502c04a
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-07-23 13:22:26 -0500

    Skeleton for problem 647

---
+++ src/main/java/leetcode/Problem647.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/palindromic-substrings/
+ */
+public class Problem647 {
+    public int countSubstrings(String s) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem647 prob = new Problem647();
+        System.out.println(prob.countSubstrings("abc")); // 3
+        System.out.println(prob.countSubstrings("aaa")); // 6
+    }
+}
