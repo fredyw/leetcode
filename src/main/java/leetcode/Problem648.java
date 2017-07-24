6af749321f09d32057388a01022c29d95cd611d7
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-07-24 00:06:14 -0500

    Skeleton for problem 648

---
+++ src/main/java/leetcode/Problem648.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/replace-words/
+ */
+public class Problem648 {
+    public String replaceWords(List<String> dict, String sentence) {
+        // TODO
+        return "";
+    }
+
+    public static void main(String[] args) {
+        Problem648 prob = new Problem648();
+        System.out.println(prob.replaceWords(Arrays.asList("cat", "bat", "rat"),
+            "the cattle was rattled by the battery")); // the cat was rat by the bat
+    }
+}
