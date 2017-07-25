749b4eacb123705b742cd1639f8ac36f6015aa1e
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-07-24 21:06:10 -0500

    Skeleton for problem 645

---
+++ src/main/java/leetcode/Problem645.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/set-mismatch/
+ */
+public class Problem645 {
+    public int[] findErrorNums(int[] nums) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem645 prob = new Problem645();
+        System.out.println(Arrays.toString(prob.findErrorNums(new int[]{1, 2, 2, 4}))); // [2, 3]
+    }
+}
