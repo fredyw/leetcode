1db140583310534e55ac4c1eea39dd053f50ee53
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-07-25 20:07:52 -0500

    Skeleton for problem 646

---
+++ src/main/java/leetcode/Problem646.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/maximum-length-of-pair-chain/
+ */
+public class Problem646 {
+    public int findLongestChain(int[][] pairs) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem646 prob = new Problem646();
+        System.out.println(prob.findLongestChain(new int[][]{{1, 2}, {2, 3}, {3, 4}})); // 2
+    }
+}
