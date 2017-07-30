4468a706f6ef89cdd46a9db6303867e1ed33cc6e
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-07-30 02:08:59 -0500

    Update problem 407

--- src/main/java/leetcode/Problem407.java
+++ src/main/java/leetcode/Problem407.java
@@ -8,25 +8,77 @@ import java.util.PriorityQueue;
 public class Problem407 {
     public int trapRainWater(int[][] heightMap) {
         // https://www.youtube.com/watch?v=cJayBq38VYw
+        int maxRow = heightMap.length;
+        int maxCol = 0;
+        if (heightMap.length > 0) {
+            maxCol = heightMap[0].length;
+        }
+        boolean[][] visited = new boolean[maxRow][maxCol];
         PriorityQueue<RowCol> queue = new PriorityQueue<>((a, b) -> Integer.compare(a.value, b.value));
         // top
-        for (int col = 0; col < heightMap[0].length; col++) {
-            queue.add(new RowCol(0, col, heightMap[0][col]));
+        for (int col = 0; col < maxCol; col++) {
+            visited[0][col] = true;
+            int val = heightMap[0][col];
+            queue.add(new RowCol(0, col, val));
         }
         // left
-        for (int row = 1; row < heightMap.length - 1; row++) {
-            queue.add(new RowCol(row, 0, heightMap[row][0]));
+        for (int row = 1; row < maxRow - 1; row++) {
+            visited[row][0] = true;
+            int val = heightMap[row][0];
+            queue.add(new RowCol(row, 0, val));
         }
         // bottom
-        for (int col = 0; col < heightMap[heightMap.length - 1].length; col++) {
-            queue.add(new RowCol(heightMap.length - 1, col, heightMap[heightMap.length - 1][col]));
+        for (int col = 0; col < maxCol; col++) {
+            visited[maxRow - 1][col] = true;
+            int val = heightMap[maxRow - 1][col];
+            queue.add(new RowCol(maxRow - 1, col, val));
         }
         // right
-        for (int row = 1; row < heightMap.length - 1; row++) {
-            queue.add(new RowCol(row, heightMap[row].length - 1, heightMap[row][heightMap[row].length - 1]));
+        for (int row = 1; row < maxRow - 1; row++) {
+            visited[row][maxCol - 1] = true;
+            int val = heightMap[row][maxCol - 1];
+            queue.add(new RowCol(row, maxCol - 1, val));
         }
-        // TODO:
-        return 0;
+        int max = 0;
+        while (!queue.isEmpty()) {
+            RowCol rowCol = queue.remove();
+            max = Math.max(max, rowCol.value);
+            // top
+            int row = rowCol.row - 1;
+            int col = rowCol.col;
+            process(heightMap, queue, visited, row, col, maxRow, maxCol, max);
+            // left
+            row = rowCol.row;
+            col = rowCol.col - 1;
+            process(heightMap, queue, visited, row, col, maxRow, maxCol, max);
+            // bottom
+            row = rowCol.row + 1;
+            col = rowCol.col;
+            process(heightMap, queue, visited, row, col, maxRow, maxCol, max);
+            // right
+            row = rowCol.row;
+            col = rowCol.col + 1;
+            process(heightMap, queue, visited, row, col, maxRow, maxCol, max);
+        }
+        return max;
+    }
+
+    private static void process(int[][] heightMap,
+                                PriorityQueue<RowCol> queue,
+                                boolean[][] visited,
+                                int row,
+                                int col,
+                                int maxRow,
+                                int maxCol,
+                                int max) {
+        if (row >= 0 && row < maxRow && col >= 0 && col < maxCol && !visited[row][col]) {
+            int val = heightMap[row][col];
+            if (val < max) {
+                val = max - val;
+            }
+            visited[row][col] = true;
+            queue.add(new RowCol(row, col, val));
+        }
     }
 
     private static class RowCol {
@@ -39,6 +91,11 @@ public class Problem407 {
             this.col = col;
             this.value = value;
         }
+
+        @Override
+        public String toString() {
+            return "(" + row + ", " + col + ") => " + value;
+        }
     }
 
     public static void main(String[] args) {
@@ -46,7 +103,17 @@ public class Problem407 {
         System.out.println(prob.trapRainWater(new int[][]{
             {1, 4, 3, 1, 3, 2},
             {2, 3, 3, 2, 3, 1},
+            {2, 3, 3, 2, 3, 1},
             {2, 3, 3, 2, 3, 1}
+        })); // 4
+        System.out.println(prob.trapRainWater(new int[][]{
+            {3, 10, 8, 12, 2, 7, 9},
+            {7, 1, 11, 3, 8, 1, 10},
+            {9, 7, 3, 10, 2, 5, 6},
+            {7, 11, 1, 4, 6, 11, 9},
+            {4, 5, 8, 12, 3, 4, 2},
+            {12, 2, 12, 1, 5, 9, 6},
+            {6, 5, 8, 12, 4, 11, 10},
         })); // 4
     }
 }
