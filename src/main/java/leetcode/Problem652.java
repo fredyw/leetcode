7fcb28523fa9b0c20e27dc503aa5bb2d3c888f5d
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-08-03 01:11:40 -0500

    Update problem 652

--- src/main/java/leetcode/Problem652.java
+++ src/main/java/leetcode/Problem652.java
@@ -48,5 +48,14 @@ public class Problem652 {
         root.right.left.right = new TreeNode(5);
         root.right.right = new TreeNode(4);
         System.out.println(prob.findDuplicateSubtrees(root)); // [[2,4,5],[4],[5]]
+
+        root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.right.left = new TreeNode(2);
+        root.right.left.left = new TreeNode(2);
+        root.right.right = new TreeNode(4);
+        System.out.println(prob.findDuplicateSubtrees(root)); // [[2,2],[2]]
     }
 }
