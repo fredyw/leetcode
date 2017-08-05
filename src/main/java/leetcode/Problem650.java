832da57094fdfa94d459c41d401451c2b75313a4
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-08-04 23:18:43 -0500

    Skeleton for problem 650

---
+++ src/main/java/leetcode/Problem650.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/2-keys-keyboard/
+ */
+public class Problem650 {
+    public int minSteps(int n) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem650 prob = new Problem650();
+        System.out.println(prob.minSteps(3)); // 3
+    }
+}
