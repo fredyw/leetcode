21b8ae6c1007a9baf9c71e71f7e58abf7b03a7fc
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-08-05 21:44:52 -0500

    Update problem 650

--- src/main/java/leetcode/Problem650.java
+++ src/main/java/leetcode/Problem650.java
@@ -5,8 +5,19 @@ package leetcode;
  */
 public class Problem650 {
     public int minSteps(int n) {
-        // TODO
-        return 0;
+        return minSteps(1, n, 1);
+    }
+
+    private static int minSteps(int n, int m, int x) {
+        if (n == m) {
+            return 1;
+        } else if (n > m) {
+            return 0;
+        }
+        int a = minSteps(n - x, m, x) + 1;
+        int b = minSteps(n, m, n);
+        int min = Math.min(a, b);
+        return min;
     }
 
     public static void main(String[] args) {
