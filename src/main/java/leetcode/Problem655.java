c7176966dedbeb5c58f4037c4b30c8b3bfe454eb
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-08-10 00:59:29 -0500

    Solve problem 655

--- src/main/java/leetcode/Problem655.java
+++ src/main/java/leetcode/Problem655.java
@@ -1,5 +1,6 @@
 package leetcode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -14,42 +15,43 @@ public class Problem655 {
         TreeNode(int x) {
             val = x;
         }
-
-        @Override
-        public String toString() {
-            return "" + val;
-        }
     }
 
     public List<List<String>> printTree(TreeNode root) {
-        // TODO
-        return null;
+        List<List<String>> result = new ArrayList<>();
+        int rowSize = height(root);
+        int colSize = (int) Math.pow(2, rowSize) - 1;
+        for (int i = 0; i < rowSize; i++) {
+            List<String> newList = new ArrayList<>();
+            for (int j = 0; j < colSize; j++) {
+                newList.add("");
+            }
+            result.add(newList);
+        }
+        printTree(root, 0, 0, colSize - 1, result);
+        return result;
+    }
+
+    private static void printTree(TreeNode node, int row, int colLeft, int colRight, List<List<String>> result) {
+        if (node == null) {
+            return;
+        }
+        int mid = (colLeft + colRight) / 2;
+        result.get(row).set(mid, Integer.toString(node.val));
+        printTree(node.left, row + 1, colLeft, mid - 1, result);
+        printTree(node.right, row + 1, mid + 1, colRight, result);
+    }
+
+    private static int height(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + Math.max(height(root.left), height(root.right));
     }
 
     private static void print(List<List<String>> list) {
         for (List<String> l : list) {
             System.out.println(l);
         }
-    }
-
-    public static void main(String[] args) {
-        Problem655 prob = new Problem655();
-
-        TreeNode root = new TreeNode(1);
-        root.left = new TreeNode(2);
-        print(prob.printTree(root));
-
-        root = new TreeNode(1);
-        root.left = new TreeNode(2);
-        root.right = new TreeNode(3);
-        root.left.right = new TreeNode(4);
-        print(prob.printTree(root));
-
-        root = new TreeNode(1);
-        root.left = new TreeNode(2);
-        root.right = new TreeNode(5);
-        root.left.left = new TreeNode(3);
-        root.left.left.left = new TreeNode(4);
-        print(prob.printTree(root));
     }
 }
