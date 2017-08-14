ae0f3ef03994ee11fa4915e1d03d3f7a6a7f2225
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-08-14 00:14:42 -0500

    Skeleton for problem 657

---
+++ src/main/java/leetcode/Problem657.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/judge-route-circle/
+ */
+public class Problem657 {
+    public boolean judgeCircle(String moves) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem657 prob = new Problem657();
+        System.out.println(prob.judgeCircle("UD")); // true
+        System.out.println(prob.judgeCircle("LL")); // false
+    }
+}
