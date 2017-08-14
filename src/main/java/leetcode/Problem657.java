d8baccd1bccf1358efe35e8e35b6ef916abadded
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-08-14 00:22:14 -0500

    Solve problem 657

--- src/main/java/leetcode/Problem657.java
+++ src/main/java/leetcode/Problem657.java
@@ -5,13 +5,19 @@ package leetcode;
  */
 public class Problem657 {
     public boolean judgeCircle(String moves) {
-        // TODO
-        return false;
-    }
-
-    public static void main(String[] args) {
-        Problem657 prob = new Problem657();
-        System.out.println(prob.judgeCircle("UD")); // true
-        System.out.println(prob.judgeCircle("LL")); // false
+        int row = 0;
+        int col = 0;
+        for (int i = 0; i < moves.length(); i++) {
+            if (moves.charAt(i) == 'U') {
+                row--;
+            } else if (moves.charAt(i) == 'D') {
+                row++;
+            } else if (moves.charAt(i) == 'L') {
+                col--;
+            } else if (moves.charAt(i) == 'R') {
+                col++;
+            }
+        }
+        return row == 0 && col == 0;
     }
 }
