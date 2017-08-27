d7b359b626b2d7e6aa6cfc93bff7402f2333945d
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-08-27 01:23:20 -0500

    Update problem 659

--- src/main/java/leetcode/Problem659.java
+++ src/main/java/leetcode/Problem659.java
@@ -1,19 +1,43 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
 /**
  * https://leetcode.com/problems/split-array-into-consecutive-subsequences/
  */
 public class Problem659 {
     public boolean isPossible(int[] nums) {
-        // TODO
-        return false;
+        List<PriorityQueue<Integer>> listQueue = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            boolean newQueue = true;
+            for (PriorityQueue<Integer> queue : listQueue) {
+                if (queue.peek() < nums[i]) {
+                    newQueue = false;
+                    queue.add(nums[i]);
+                    break;
+                }
+            }
+            if (newQueue) {
+                PriorityQueue<Integer> queue = new PriorityQueue<>((a, b) -> Integer.compare(b, a));
+                queue.add(nums[i]);
+                listQueue.add(queue);
+            }
+        }
+        for (PriorityQueue<Integer> queue : listQueue) {
+            if (queue.size() < 3) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
         Problem659 prob = new Problem659();
         System.out.println(prob.isPossible(new int[]{1, 2, 3, 3, 4, 5})); // true
-        System.out.println(prob.isPossible(new int[]{1, 2, 3, 3, 4, 4, 5, 5})); // true
-        System.out.println(prob.isPossible(new int[]{1, 2, 3, 4, 4, 5})); // false
-        System.out.println(prob.isPossible(new int[]{1, 2, 3, 3, 4, 4, 5, 6})); // true
+//        System.out.println(prob.isPossible(new int[]{1, 2, 3, 3, 4, 4, 5, 5})); // true
+//        System.out.println(prob.isPossible(new int[]{1, 2, 3, 4, 4, 5})); // false
+//        System.out.println(prob.isPossible(new int[]{1, 2, 3, 3, 4, 4, 5, 6})); // true
     }
 }
