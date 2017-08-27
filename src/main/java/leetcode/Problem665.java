47f3ffc429e8d3c190f4d9dca53350e73ebbe208
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-08-27 18:08:15 -0500

    Skeleton for problem 665

---
+++ src/main/java/leetcode/Problem665.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/non-decreasing-array/
+ */
+public class Problem665 {
+    public boolean checkPossibility(int[] nums) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem665 prob = new Problem665();
+        System.out.println(prob.checkPossibility(new int[]{4, 2, 3})); // true
+        System.out.println(prob.checkPossibility(new int[]{4, 2, 1})); // false
+    }
+}
