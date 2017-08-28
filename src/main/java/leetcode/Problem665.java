746ef7fc610162fc29a830021e2c0c1a3758544d
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-08-27 19:28:04 -0500

    Solve problem 665

--- src/main/java/leetcode/Problem665.java
+++ src/main/java/leetcode/Problem665.java
@@ -13,15 +13,12 @@ public class Problem665 {
         for (int curr = 1; curr < nums.length; curr++) {
             if (nums[prev] > nums[curr]) {
                 if (prev == 0) {
-                    nums[prev] = Integer.MIN_VALUE;
+                    nums[prev] = nums[curr];
                 } else {
-                    if (nums[prev] == nums[prev - 1]) {
+                    if (nums[prev - 1] >= nums[curr]) {
                         nums[curr] = nums[prev];
                     } else {
-                        nums[prev] = nums[prev - 1];
-                    }
-                    if (nums[prev] > nums[curr]) {
-                        return false;
+                        nums[prev] = nums[curr];
                     }
                 }
                 count++;
@@ -32,17 +29,5 @@ public class Problem665 {
             prev = curr;
         }
         return true;
-    }
-
-    public static void main(String[] args) {
-        Problem665 prob = new Problem665();
-        System.out.println(prob.checkPossibility(new int[]{4, 2, 3})); // true
-        System.out.println(prob.checkPossibility(new int[]{4, 2, 1})); // false
-        System.out.println(prob.checkPossibility(new int[]{1, 5, 2, 4})); // true
-        System.out.println(prob.checkPossibility(new int[]{1})); // true
-        System.out.println(prob.checkPossibility(new int[]{3, 4, 2, 3})); // false
-        System.out.println(prob.checkPossibility(new int[]{-1, 4, 2, 3})); // true
-        System.out.println(prob.checkPossibility(new int[]{2, 3, 3, 2, 4})); // true
-        System.out.println(prob.checkPossibility(new int[]{1, 2, 4, 5, 3})); // true
     }
 }
