46bdae4a502bf6d6c2223d13af05278b896c08e3
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-08-27 23:10:55 -0500

    Update problem 659

--- src/main/java/leetcode/Problem659.java
+++ src/main/java/leetcode/Problem659.java
@@ -2,35 +2,56 @@ package leetcode;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.PriorityQueue;
 
 /**
  * https://leetcode.com/problems/split-array-into-consecutive-subsequences/
  */
 public class Problem659 {
     public boolean isPossible(int[] nums) {
-        List<PriorityQueue<Integer>> listQueue = new ArrayList<>();
+        List<ValCount> list = new ArrayList<>();
         for (int i = 0; i < nums.length; i++) {
-            boolean newQueue = true;
-            for (PriorityQueue<Integer> queue : listQueue) {
-                if (queue.peek() < nums[i]) {
-                    newQueue = false;
-                    queue.add(nums[i]);
-                    break;
+            boolean newSub = true;
+            int idx = -1;
+            for (int j = 0; j < list.size(); j++) {
+                ValCount vc = list.get(j);
+                if (vc.val + 1 == nums[i]) {
+                    newSub = false;
+                    if (idx == -1) {
+                        idx = j;
+                    } else {
+                        if (list.get(idx).count > list.get(j).count) {
+                            idx = j;
+                        }
+                    }
                 }
             }
-            if (newQueue) {
-                PriorityQueue<Integer> queue = new PriorityQueue<>((a, b) -> Integer.compare(b, a));
-                queue.add(nums[i]);
-                listQueue.add(queue);
+            if (newSub) {
+                list.add(new ValCount(nums[i], 1));
+            } else {
+                list.set(idx, new ValCount(nums[i], list.get(idx).count + 1));
             }
         }
-        for (PriorityQueue<Integer> queue : listQueue) {
-            if (queue.size() < 3) {
+        for (ValCount vc : list) {
+            if (vc.count < 3) {
                 return false;
             }
         }
         return true;
+    }
+
+    private static class ValCount {
+        private int val;
+        private int count;
+
+        public ValCount(int val, int count) {
+            this.val = val;
+            this.count = count;
+        }
+
+        @Override
+        public String toString() {
+            return "" + val + " --> " + count;
+        }
     }
 
     public static void main(String[] args) {
