f76c537dcfbe1f6de784f751af2f5dbdc6082189
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-08-29 01:44:53 -0500

    Skeleton for problem 667

---
+++ src/main/java/leetcode/Problem667.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/beautiful-arrangement-ii/
+ */
+public class Problem667 {
+    public int[] constructArray(int n, int k) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem667 prob = new Problem667();
+        System.out.println(prob.constructArray(3, 1)); // [1, 2, 3]
+        System.out.println(prob.constructArray(3, 2)); // [1, 3, 2]
+    }
+}
