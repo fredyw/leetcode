288575d025ab93a2e02b549e3fb943b431315a3b
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-08-30 00:23:27 -0500

    Update problem 667

--- src/main/java/leetcode/Problem667.java
+++ src/main/java/leetcode/Problem667.java
@@ -11,7 +11,9 @@ public class Problem667 {
 
     public static void main(String[] args) {
         Problem667 prob = new Problem667();
-        System.out.println(prob.constructArray(3, 1)); // [1, 2, 3]
-        System.out.println(prob.constructArray(3, 2)); // [1, 3, 2]
+        System.out.println(prob.constructArray(3, 1)); // [1,2,3]
+        System.out.println(prob.constructArray(3, 2)); // [1,3,2]
+        System.out.println(prob.constructArray(10, 9)); // [1,10,2,9,3,8,4,7,5,6]
+        System.out.println(prob.constructArray(10, 5)); // [1,6,2,5,3,4,7,8,9,10]
     }
 }
