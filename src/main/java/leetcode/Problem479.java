e7857f484bead29e0e9a74d2549c2acea78b90e1
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-08-31 01:22:58 -0500

    Skeleton for problem 479

---
+++ src/main/java/leetcode/Problem479.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/largest-palindrome-product/
+ */
+public class Problem479 {
+    public int largestPalindrome(int n) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem479 prob = new Problem479();
+        System.out.println(prob.largestPalindrome(2)); // 987
+    }
+}
