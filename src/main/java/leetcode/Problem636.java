67d9aa5aae5966f1883bf03daecb943ba3afc866
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-09-03 22:13:53 -0500

    Update problem 636

--- src/main/java/leetcode/Problem636.java
+++ src/main/java/leetcode/Problem636.java
@@ -2,6 +2,7 @@ package leetcode;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * https://leetcode.com/problems/exclusive-time-of-functions/
@@ -9,17 +10,31 @@ import java.util.List;
 public class Problem636 {
     public int[] exclusiveTime(int n, List<String> logs) {
         int[] result = new int[n];
-        // TODO
+        Stack<Integer>[] stack = new Stack[n];
+        for (String log : logs) {
+            String[] split = log.split(":");
+            int id = Integer.parseInt(split[0]);
+            String type = split[1];
+            int time = Integer.parseInt(split[2]);
+            if (stack[id] == null) {
+                stack[id] = new Stack<>();
+            }
+            if (type.equals("start")) {
+                stack[id].add(time);
+            } else {
+                result[id] += time - stack[id].pop();
+            }
+        }
         return result;
     }
 
     public static void main(String[] args) {
         Problem636 prob = new Problem636();
-        System.out.println(prob.exclusiveTime(2, Arrays.asList(
+        System.out.println(Arrays.toString(prob.exclusiveTime(2, Arrays.asList(
             "0:start:0",
             "1:start:2",
             "1:end:5",
             "0:end:6"
-        ))); // [3, 4]
+        )))); // [3, 4]
     }
 }
