afe4c846ec4039f8e8ebb4124bdc71950f51e745
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-09-05 22:29:47 -0500

    Update problem 671

--- src/main/java/leetcode/Problem671.java
+++ src/main/java/leetcode/Problem671.java
@@ -15,8 +15,34 @@ public class Problem671 {
     }
 
     public int findSecondMinimumValue(TreeNode root) {
-        // TODO
-        return 0;
+        FirstSecondMinimum fsm = new FirstSecondMinimum();
+        findSecondMinimumValue(root, fsm);
+        return (fsm.secondMin == null || fsm.firstMin == fsm.secondMin) ? -1 : fsm.secondMin;
+    }
+
+    private static class FirstSecondMinimum {
+        private Integer firstMin;
+        private Integer secondMin;
+    }
+
+    private static void findSecondMinimumValue(TreeNode node, FirstSecondMinimum fsm) {
+        if (node == null) {
+            return;
+        }
+        if (fsm.firstMin == null) {
+            fsm.firstMin = node.val;
+        } else if (fsm.secondMin == null) {
+            fsm.secondMin = node.val;
+        } else {
+            if (fsm.firstMin < node.val) {
+                fsm.secondMin = fsm.firstMin;
+                fsm.firstMin = node.val;
+            } else if (fsm.secondMin < node.val) {
+                fsm.secondMin = fsm.secondMin;
+            }
+        }
+        findSecondMinimumValue(node.left, fsm);
+        findSecondMinimumValue(node.right, fsm);
     }
 
     public static void main(String[] args) {
@@ -33,5 +59,14 @@ public class Problem671 {
         root.left = new TreeNode(2);
         root.right = new TreeNode(2);
         System.out.println(prob.findSecondMinimumValue(root)); // -1
+
+        root = new TreeNode(2);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(5);
+        root.right.left = new TreeNode(5);
+        root.right.right = new TreeNode(7);
+        root.right.right.left = new TreeNode(8);
+        root.right.right.right = new TreeNode(8);
+        System.out.println(prob.findSecondMinimumValue(root)); // 5
     }
 }
