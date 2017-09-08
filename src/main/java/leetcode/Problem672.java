4f647289ca5ab46258920708d9de9226faeec689
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-09-08 00:13:01 -0500

    Update problem 672

--- src/main/java/leetcode/Problem672.java
+++ src/main/java/leetcode/Problem672.java
@@ -11,8 +11,15 @@ public class Problem672 {
 
     public static void main(String[] args) {
         Problem672 prob = new Problem672();
+        System.out.println(prob.flipLights(1, 0)); // 1
         System.out.println(prob.flipLights(1, 1)); // 2
         System.out.println(prob.flipLights(2, 1)); // 3
         System.out.println(prob.flipLights(3, 1)); // 4
+        System.out.println(prob.flipLights(3, 2)); // 7
+        System.out.println(prob.flipLights(3, 3)); // 8
+        System.out.println(prob.flipLights(3, 4)); // 8
+        System.out.println(prob.flipLights(15, 1)); // 4
+        System.out.println(prob.flipLights(15, 2)); // 7
+        System.out.println(prob.flipLights(15, 3)); // 8
     }
 }
