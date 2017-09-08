3d89121fe5dfca944cc9e3590f1e5c73537e4982
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-09-07 23:38:11 -0500

    Skeleton for problem 672

---
+++ src/main/java/leetcode/Problem672.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/bulb-switcher-ii/
+ */
+public class Problem672 {
+    public int flipLights(int n, int m) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem672 prob = new Problem672();
+        System.out.println(prob.flipLights(1, 1)); // 2
+        System.out.println(prob.flipLights(2, 1)); // 3
+        System.out.println(prob.flipLights(3, 1)); // 4
+    }
+}
