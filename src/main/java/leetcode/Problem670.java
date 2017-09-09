18b2f6144b4ea6c7dc25525dc348bb28056730d0
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-09-09 00:33:33 -0500

    Solve problem 670

--- src/main/java/leetcode/Problem670.java
+++ src/main/java/leetcode/Problem670.java
@@ -5,17 +5,27 @@ package leetcode;
  */
 public class Problem670 {
     public int maximumSwap(int num) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem670 prob = new Problem670();
-        System.out.println(prob.maximumSwap(2736)); // 7236
-        System.out.println(prob.maximumSwap(9973)); // 9973
-        System.out.println(prob.maximumSwap(32579)); // 92573
-        System.out.println(prob.maximumSwap(12345)); // 52341
-        System.out.println(prob.maximumSwap(12345)); // 52341
-        System.out.println(prob.maximumSwap(54321)); // 54321
+        String str = Integer.toString(num);
+        char[] chars = str.toCharArray();
+        for (int i = 0; i < chars.length - 1; i++) {
+            int j = i;
+            int k = chars.length - 1;
+            char max = chars[j];
+            int maxIdx = chars.length;
+            while (j < k) {
+                if (max < chars[k]) {
+                    max = chars[k];
+                    maxIdx = k;
+                }
+                k--;
+            }
+            if (maxIdx != chars.length) {
+                char tmp = chars[j];
+                chars[j] = chars[maxIdx];
+                chars[maxIdx] = tmp;
+                break;
+            }
+        }
+        return Integer.parseInt(new String(chars));
     }
 }
