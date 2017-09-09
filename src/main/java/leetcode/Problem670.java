8955bc814b659f26d4fd88d75273ae0c51c85870
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-09-09 00:16:47 -0500

    Update problem 670

--- src/main/java/leetcode/Problem670.java
+++ src/main/java/leetcode/Problem670.java
@@ -13,5 +13,9 @@ public class Problem670 {
         Problem670 prob = new Problem670();
         System.out.println(prob.maximumSwap(2736)); // 7236
         System.out.println(prob.maximumSwap(9973)); // 9973
+        System.out.println(prob.maximumSwap(32579)); // 92573
+        System.out.println(prob.maximumSwap(12345)); // 52341
+        System.out.println(prob.maximumSwap(12345)); // 52341
+        System.out.println(prob.maximumSwap(54321)); // 54321
     }
 }
