7d42f3735a35be3d9339e9b9cfc995bab6d44433
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-09-09 00:14:39 -0500

    Skeleton for problem 670

---
+++ src/main/java/leetcode/Problem670.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/maximum-swap/
+ */
+public class Problem670 {
+    public int maximumSwap(int num) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem670 prob = new Problem670();
+        System.out.println(prob.maximumSwap(2736)); // 7236
+        System.out.println(prob.maximumSwap(9973)); // 9973
+    }
+}
