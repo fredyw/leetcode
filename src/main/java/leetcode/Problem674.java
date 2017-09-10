ec62fce2002af7b6ed6e8eb73037e97f1bfdaa91
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-09-10 12:32:49 -0500

    Solve problem 674

--- src/main/java/leetcode/Problem674.java
+++ src/main/java/leetcode/Problem674.java
@@ -5,13 +5,20 @@ package leetcode;
  */
 public class Problem674 {
     public int findLengthOfLCIS(int[] nums) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem674 prob = new Problem674();
-        System.out.println(prob.findLengthOfLCIS(new int[]{1, 3, 5, 4, 7})); // 3
-        System.out.println(prob.findLengthOfLCIS(new int[]{2, 2, 2, 2, 2})); // 1
+        if (nums.length == 0) {
+            return 0;
+        }
+        int max = 1;
+        int tmpMax = 1;
+        for (int prev = 0, current = 1; current < nums.length; current++, prev++) {
+            if (nums[prev] < nums[current]) {
+                tmpMax++;
+            } else {
+                max = Math.max(max, tmpMax);
+                tmpMax = 1;
+            }
+        }
+        max = Math.max(max, tmpMax);
+        return max;
     }
 }
