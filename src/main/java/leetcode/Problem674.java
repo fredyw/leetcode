c5ecb2a0e8401cb715079a172fcb8777d586381d
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-09-10 12:22:22 -0500

    Skeleton for problem 674

---
+++ src/main/java/leetcode/Problem674.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/longest-continuous-increasing-subsequence/
+ */
+public class Problem674 {
+    public int findLengthOfLCIS(int[] nums) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem674 prob = new Problem674();
+        System.out.println(prob.findLengthOfLCIS(new int[]{1, 3, 5, 4, 7})); // 3
+        System.out.println(prob.findLengthOfLCIS(new int[]{2, 2, 2, 2, 2})); // 1
+    }
+}
