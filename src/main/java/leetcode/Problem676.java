6f5caa773375c0cd2d56714654680c39191e532c
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-09-11 00:44:26 -0500

    Update problem 676

--- src/main/java/leetcode/Problem676.java
+++ src/main/java/leetcode/Problem676.java
@@ -5,15 +5,48 @@ package leetcode;
  */
 public class Problem676 {
     static class MagicDictionary {
+        private static class Node {
+            private char ch;
+            private boolean end;
+            private Node[] children = new Node[26];
+
+            public Node(char ch) {
+                this.ch = ch;
+            }
+
+            @Override
+            public String toString() {
+                return "" + ch;
+            }
+        }
+
+        private final Node root = new Node(' ');
 
         /** Initialize your data structure here. */
         public MagicDictionary() {
-            // TODO
         }
 
         /** Build a dictionary through a list of words */
         public void buildDict(String[] dict) {
-            // TODO
+            for (String str : dict) {
+                root.children[str.charAt(0) - 'a'] = build(str, 0, root);
+            }
+        }
+
+        private static Node build(String str, int idx, Node node) {
+            if (idx == str.length()) {
+                return null;
+            }
+            Node n = node.children[str.charAt(idx) - 'a'];
+            if (n == null) {
+                n = new Node(str.charAt(idx));
+                node.children[str.charAt(idx) - 'a'] = n;
+            }
+            Node m = build(str, idx + 1, n);
+            if (m == null) {
+                n.end = true;
+            }
+            return n;
         }
 
         /** Returns if there is any word in the trie that equals to the given word after modifying exactly one character */
