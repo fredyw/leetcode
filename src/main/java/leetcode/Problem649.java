8efa2c319f4e202556f6029642c4a671f88d2411
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-09-11 23:13:41 -0500

    Skeleton for problem 649

---
+++ src/main/java/leetcode/Problem649.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/dota2-senate/
+ */
+public class Problem649 {
+    public String predictPartyVictory(String senate) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem649 prob = new Problem649();
+        System.out.println(prob.predictPartyVictory("RD")); // Radiant
+        System.out.println(prob.predictPartyVictory("RDD")); // Dire
+    }
+}
