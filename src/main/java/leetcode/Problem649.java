459e60dc32f67be99eb3bf0169ae4558d630e280
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-09-14 00:56:40 -0500

    Update problem 649

--- src/main/java/leetcode/Problem649.java
+++ src/main/java/leetcode/Problem649.java
@@ -1,63 +1,63 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 /**
  * https://leetcode.com/problems/dota2-senate/
  */
 public class Problem649 {
     public String predictPartyVictory(String senate) {
-        List<Integer> radiants = new ArrayList<>();
-        List<Integer> dires = new ArrayList<>();
+        int radiantCount = 0;
+        int direCount = 0;
         char[] chars = senate.toCharArray();
         for (int i = 0; i < chars.length; i++) {
             if (chars[i] == 'R') {
-                radiants.add(i);
+                radiantCount++;
             } else {
-                dires.add(i);
+                direCount++;
             }
         }
-        int radiantIdx = 0;
-        int direIdx = 0;
-        Set<Integer> bannedRadiants = new HashSet<>();
-        Set<Integer> bannedDires = new HashSet<>();
-        while (bannedRadiants.size() <= radiants.size() || bannedDires.size() <= dires.size()) {
+        while (direCount > 0 || radiantCount > 0) {
             for (int i = 0; i < chars.length; i++) {
-                if (bannedRadiants.size() == radiants.size()) {
-                    return "Dire";
-                }
-                if (bannedDires.size() == dires.size()) {
+                if (direCount == 0) {
                     return "Radiant";
                 }
+                if (radiantCount == 0) {
+                    return "Dire";
+                }
                 if (chars[i] == 'R') {
-                    if (bannedRadiants.contains(i)) {
-                        continue;
+                    int j = (i + 1) % chars.length;
+                    while (chars[j] != 'D') {
+                        j++;
+                        j = j % chars.length;
                     }
-                    bannedDires.add(dires.get(direIdx++));
-                } else {
-                    if (bannedDires.contains(i)) {
-                        continue;
+                    chars[j] = ' ';
+                    direCount--;
+                } else if (chars[i] == 'D') {
+                    int j = (i + 1) % chars.length;
+                    while (chars[j] != 'R') {
+                        j++;
+                        j = j % chars.length;
                     }
-                    bannedRadiants.add(radiants.get(radiantIdx++));
+                    chars[j] = ' ';
+                    radiantCount--;
                 }
             }
         }
-        return "";
+        if (direCount == 0) {
+            return "Radiant";
+        }
+        return "Dire";
     }
 
     public static void main(String[] args) {
         Problem649 prob = new Problem649();
-//        System.out.println(prob.predictPartyVictory("RD")); // Radiant
-//        System.out.println(prob.predictPartyVictory("RDD")); // Dire
-//        System.out.println(prob.predictPartyVictory("RDRD")); // Radiant
-//        System.out.println(prob.predictPartyVictory("RDDR")); // Radiant
-//        System.out.println(prob.predictPartyVictory("R")); // Radiant
-//        System.out.println(prob.predictPartyVictory("D")); // Dire
-//        System.out.println(prob.predictPartyVictory("DDRRR")); // Dire
-//        System.out.println(prob.predictPartyVictory("RDDRDDDRRRDDRDRDRDDDDRRDR")); // Dire
+        System.out.println(prob.predictPartyVictory("RD")); // Radiant
+        System.out.println(prob.predictPartyVictory("RDD")); // Dire
+        System.out.println(prob.predictPartyVictory("RDRD")); // Radiant
+        System.out.println(prob.predictPartyVictory("RDDR")); // Radiant
+        System.out.println(prob.predictPartyVictory("R")); // Radiant
+        System.out.println(prob.predictPartyVictory("D")); // Dire
+        System.out.println(prob.predictPartyVictory("DDRRR")); // Dire
+        System.out.println(prob.predictPartyVictory("RDDRDDDRRRDDRDRDRDDDDRRDR")); // Dire
         System.out.println(prob.predictPartyVictory("DRRDRDRDRDDRDRDR")); // Radiant
     }
 }
