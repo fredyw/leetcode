6360d1877580705b1d019c814ea275b82469baca
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-09-14 23:04:24 -0500

    Skeleton for problem 673

---
+++ src/main/java/leetcode/Problem673.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/number-of-longest-increasing-subsequence/
+ */
+public class Problem673 {
+    public int findNumberOfLIS(int[] nums) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem673 prob = new Problem673();
+        System.out.println(prob.findNumberOfLIS(new int[]{1, 3, 5, 4, 7})); // 2
+        System.out.println(prob.findNumberOfLIS(new int[]{2, 2, 2, 2, 2})); // 5
+    }
+}
