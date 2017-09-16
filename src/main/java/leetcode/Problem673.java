3ccf48bb4eff5aafd5bd62a62fd2a78b5c9191b8
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-09-16 01:07:18 -0500

    Update problem 673

--- src/main/java/leetcode/Problem673.java
+++ src/main/java/leetcode/Problem673.java
@@ -8,49 +8,42 @@ import java.util.Map;
  */
 public class Problem673 {
     public int findNumberOfLIS(int[] nums) {
-        Integer[] memo = new Integer[nums.length];
         Map<Integer, Integer> map = new HashMap<>();
-        int max = longestIncreasingSubsequence(nums, 0, memo, map);
+        int max = 0;
+        Integer[] memo = new Integer[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            int longest = longestIncreasingSubsequence(nums, i, memo) + 1;
+            max = Math.max(max, longest);
+            if (!map.containsKey(longest)) {
+                map.put(longest, 1);
+            } else {
+                map.put(longest, map.get(longest) + 1);
+            }
+        }
         return map.get(max);
     }
 
-    private static int longestIncreasingSubsequence(int[] nums, int idx, Integer[] memo,
-                                                    Map<Integer, Integer> map) {
-        if (idx == nums.length) {
-            return 0;
+    private static int longestIncreasingSubsequence(int[] nums, int idx, Integer[] memo) {
+        if (memo[idx] != null) {
+            return memo[idx];
         }
-//        if (memo[idx] != null) {
-//            int max = memo[idx];
-//            if (!map.containsKey(max)) {
-//                map.put(max, 1);
-//            } else {
-//                map.put(max, map.get(max) + 1);
-//            }
-//            System.out.println("max: " + max);
-//            return max;
-//        }
         int max = 0;
-        for (int i = idx + 1; i < nums.length; i++) {
+        for (int i = idx; i < nums.length; i++) {
             if (nums[idx] < nums[i]) {
-                max = Math.max(max, longestIncreasingSubsequence(nums, i, memo, map) + 1);
+                max = Math.max(max, longestIncreasingSubsequence(nums, i, memo) + 1);
             }
-            max = Math.max(max, longestIncreasingSubsequence(nums, i, memo, map));
         }
         memo[idx] = max;
-        if (!map.containsKey(max)) {
-            map.put(max, 1);
-        } else {
-            map.put(max, map.get(max) + 1);
-        }
-//        System.out.println("max: " + max);
         return max;
     }
 
     public static void main(String[] args) {
         Problem673 prob = new Problem673();
-//        System.out.println(prob.findNumberOfLIS(new int[]{1, 3, 5, 4, 7})); // 2
+//        System.out.println(prob.findNumberOfLIS(new int[]{1, 2})); // 1
+//        System.out.println(prob.findNumberOfLIS(new int[]{3, 5, 2, 4})); // 2
+        System.out.println(prob.findNumberOfLIS(new int[]{1, 3, 5, 4, 7})); // 2
 //        System.out.println(prob.findNumberOfLIS(new int[]{2, 2, 2, 2, 2})); // 5
-        System.out.println(prob.findNumberOfLIS(new int[]{1, 2, 3, 4, 5})); // 1
-        System.out.println(prob.findNumberOfLIS(new int[]{1, 3, 3, 4, 5})); // 2
+//        System.out.println(prob.findNumberOfLIS(new int[]{1, 2, 3, 4, 5})); // 1
+//        System.out.println(prob.findNumberOfLIS(new int[]{1, 3, 3, 4, 5})); // 2
     }
 }
