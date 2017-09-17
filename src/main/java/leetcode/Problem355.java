9cdfd9081c94a140ad37aff71283b7e0766584ba
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-09-17 09:41:42 -0500

    Skeleton for problem 355

---
+++ src/main/java/leetcode/Problem355.java
@@ -0,0 +1,63 @@
+package leetcode;
+
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/design-twitter/
+ */
+public class Problem355 {
+    private static class Twitter {
+
+        /** Initialize your data structure here. */
+        public Twitter() {
+
+        }
+
+        /** Compose a new tweet. */
+        public void postTweet(int userId, int tweetId) {
+            // TODO
+        }
+
+        /** Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent. */
+        public List<Integer> getNewsFeed(int userId) {
+            // TODO
+            return null;
+        }
+
+        /** Follower follows a followee. If the operation is invalid, it should be a no-op. */
+        public void follow(int followerId, int followeeId) {
+            // TODO
+        }
+
+        /** Follower unfollows a followee. If the operation is invalid, it should be a no-op. */
+        public void unfollow(int followerId, int followeeId) {
+            // TODO
+        }
+    }
+
+    public static void main(String[] args) {
+        Twitter twitter = new Twitter();
+        // User 1 posts a new tweet (id = 5).
+        twitter.postTweet(1, 5);
+
+        // User 1's news feed should return a list with 1 tweet id -> [5].
+        twitter.getNewsFeed(1);
+
+        // User 1 follows user 2.
+        twitter.follow(1, 2);
+
+        // User 2 posts a new tweet (id = 6).
+        twitter.postTweet(2, 6);
+
+        // User 1's news feed should return a list with 2 tweet ids -> [6, 5].
+        // Tweet id 6 should precede tweet id 5 because it is posted after tweet id 5.
+        twitter.getNewsFeed(1);
+
+        // User 1 unfollows user 2.
+        twitter.unfollow(1, 2);
+
+        // User 1's news feed should return a list with 1 tweet id -> [5],
+        // since user 1 is no longer following user 2.
+        twitter.getNewsFeed(1);
+    }
+}
