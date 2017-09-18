c07ed598f12224a363dd840acbc038215d1d903f
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-09-17 22:57:14 -0500

    Skeleton for problem 680

---
+++ src/main/java/leetcode/Problem680.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/valid-palindrome-ii/
+ */
+public class Problem680 {
+    public boolean validPalindrome(String s) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem680 prob = new Problem680();
+        System.out.println(prob.validPalindrome("aba")); // true
+        System.out.println(prob.validPalindrome("abca")); // true
+    }
+}
