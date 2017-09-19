6af16314b2e0f57c1fff956504cb4f3efd437bbc
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-09-18 23:48:40 -0500

    Solve problem 677

--- src/main/java/leetcode/Problem677.java
+++ src/main/java/leetcode/Problem677.java
@@ -5,28 +5,76 @@ package leetcode;
  */
 public class Problem677 {
     private static class MapSum {
+        private static class Node {
+            private final char ch;
+            private final Node[] nodes = new Node[26];
+            private boolean end;
+            private int value;
+
+            public Node(char ch) {
+                this.ch = ch;
+            }
+        }
+
+        private final Node[] root = new Node[26];
+
         /**
          * Initialize your data structure here.
          */
         public MapSum() {
-            // TODO
         }
 
         public void insert(String key, int val) {
-            // TODO
+            insert(key, val, 0, root);
+        }
+
+        private static void insert(String key, int val, int idx, Node[] nodes) {
+            if (idx == key.length()) {
+                return;
+            }
+            Node n = nodes[key.charAt(idx) - 'a'];
+            if (n == null) {
+                n = new Node(key.charAt(idx));
+                nodes[key.charAt(idx) - 'a'] = n;
+            }
+            if (idx == key.length() - 1) {
+                n.value = val;
+                n.end = true;
+            }
+            insert(key, val, idx + 1, n.nodes);
         }
 
         public int sum(String prefix) {
-            // TODO
-            return 0;
+            return sum(prefix, 0, root);
         }
-    }
 
-    public static void main(String[] args) {
-        MapSum mapSum = new MapSum();
-        mapSum.insert("apple", 3);
-        System.out.println(mapSum.sum("ap")); // 3
-        mapSum.insert("app", 2);
-        System.out.println(mapSum.sum("ap")); // 5
+        private static int sum(String prefix, int idx, Node[] nodes) {
+            if (idx == prefix.length()) {
+                return sum(nodes);
+            }
+            Node n = nodes[prefix.charAt(idx) - 'a'];
+            if (n == null) {
+                return 0;
+            }
+            int val = 0;
+            if (idx == prefix.length() - 1 && nodes[prefix.charAt(idx) - 'a'].end) {
+                val = nodes[prefix.charAt(idx) - 'a'].value;
+            }
+            return sum(prefix, idx + 1, n.nodes) + val;
+        }
+
+        private static int sum(Node[] nodes) {
+            int total = 0;
+            for (int i = 0; i < nodes.length; i++) {
+                if (nodes[i] != null) {
+                    int val = 0;
+                    if (nodes[i].end) {
+                        val = nodes[i].value;
+                    }
+                    total += sum(nodes[i].nodes) + val;
+                }
+            }
+            return total;
+        }
     }
 }
