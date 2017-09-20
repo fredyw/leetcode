a5ec447a96d1ab385fb043fb74eb6f7e58382113
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-09-20 00:59:25 -0500

    Update problem 355

--- src/main/java/leetcode/Problem355.java
+++ src/main/java/leetcode/Problem355.java
@@ -41,7 +41,7 @@ public class Problem355 {
         twitter.postTweet(1, 5);
 
         // User 1's news feed should return a list with 1 tweet id -> [5].
-        twitter.getNewsFeed(1);
+        System.out.println(twitter.getNewsFeed(1));
 
         // User 1 follows user 2.
         twitter.follow(1, 2);
@@ -51,13 +51,13 @@ public class Problem355 {
 
         // User 1's news feed should return a list with 2 tweet ids -> [6, 5].
         // Tweet id 6 should precede tweet id 5 because it is posted after tweet id 5.
-        twitter.getNewsFeed(1);
+        System.out.println(twitter.getNewsFeed(1));
 
         // User 1 unfollows user 2.
         twitter.unfollow(1, 2);
 
         // User 1's news feed should return a list with 1 tweet id -> [5],
         // since user 1 is no longer following user 2.
-        twitter.getNewsFeed(1);
+        System.out.println(twitter.getNewsFeed(1));
     }
 }
