26beb10bce6dedbca4086c74e5390d379302a281
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-09-21 20:10:57 -0500

    Skeleton for problem 365

---
+++ src/main/java/leetcode/Problem365.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/water-and-jug-problem/
+ */
+public class Problem365 {
+    public boolean canMeasureWater(int x, int y, int z) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem365 prob = new Problem365();
+        System.out.println(prob.canMeasureWater(3, 5, 4)); // true
+        System.out.println(prob.canMeasureWater(2, 6, 5)); // false
+    }
+}
