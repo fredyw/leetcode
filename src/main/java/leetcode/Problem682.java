edf20c17e66d54d330d3c39f95758108e3f6851f
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-09-24 21:12:34 -0500

    Update problem 682

--- src/main/java/leetcode/Problem682.java
+++ src/main/java/leetcode/Problem682.java
@@ -1,12 +1,37 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * https://leetcode.com/problems/baseball-game/
  */
 public class Problem682 {
     public int calPoints(String[] ops) {
-        // TODO
-        return 0;
+        int sum = 0;
+        List<Integer> list = new ArrayList<>();
+        int index = 0;
+        for (int i = 0; i < ops.length; i++) {
+            String op = ops[i];
+            if (op.equals("D")) {
+                int a = list.get(list.size() - 1) * 2;
+                sum += a;
+                list.add(sum);
+            } else if (op.equals("C")) {
+                sum -= Integer.parseInt(ops[index]);
+                list.remove(list.size() - 1);
+            } else if (op.equals("+")) {
+                int a = list.get(list.size() - 1);
+                int b = list.get(list.size() - 2);
+                sum += (a + b);
+                list.add(sum);
+            } else {
+                index = i;
+                sum += Integer.parseInt(op);
+                list.add(sum);
+            }
+        }
+        return sum;
     }
 
     public static void main(String[] args) {
