9cf20bb45672d8b54f1cb25828035777275fa85a
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-09-24 21:32:16 -0500

    Solve problem 682

--- src/main/java/leetcode/Problem682.java
+++ src/main/java/leetcode/Problem682.java
@@ -10,33 +10,26 @@ public class Problem682 {
     public int calPoints(String[] ops) {
         int sum = 0;
         List<Integer> list = new ArrayList<>();
-        int index = 0;
         for (int i = 0; i < ops.length; i++) {
             String op = ops[i];
             if (op.equals("D")) {
                 int a = list.get(list.size() - 1) * 2;
+                list.add(a);
                 sum += a;
-                list.add(sum);
             } else if (op.equals("C")) {
-                sum -= Integer.parseInt(ops[index]);
+                int val = list.get(list.size() - 1);
                 list.remove(list.size() - 1);
+                sum -= val;
             } else if (op.equals("+")) {
                 int a = list.get(list.size() - 1);
                 int b = list.get(list.size() - 2);
+                list.add(a + b);
                 sum += (a + b);
-                list.add(sum);
             } else {
-                index = i;
+                list.add(Integer.parseInt(op));
                 sum += Integer.parseInt(op);
-                list.add(sum);
             }
         }
         return sum;
-    }
-
-    public static void main(String[] args) {
-        Problem682 prob = new Problem682();
-        System.out.println(prob.calPoints(new String[]{"5", "2", "C", "D", "+"})); // 15
-        System.out.println(prob.calPoints(new String[]{"5", "-2", "4", "C", "D", "9", "+", "+"})); // 27
     }
 }
