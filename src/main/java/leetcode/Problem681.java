333c55db7649182c91478ea602669f58c916867c
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-09-26 23:24:03 -0500

    Skeleton for problem 681

---
+++ src/main/java/leetcode/Problem681.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/next-closest-time/
+ */
+public class Problem681 {
+    public String nextClosestTime(String time) {
+        // TODO
+        return "";
+    }
+
+    public static void main(String[] args) {
+        Problem681 prob = new Problem681();
+        System.out.println(prob.nextClosestTime("19:34")); // 19:39
+        System.out.println(prob.nextClosestTime("23:59")); // 22:22
+    }
+}
