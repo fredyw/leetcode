1b6e303b9b789fbe4b17db6f62010f6f4628f16b
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-09-27 23:44:03 -0500

    Skeleton for problem 576

---
+++ src/main/java/leetcode/Problem576.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/out-of-boundary-paths/
+ */
+public class Problem576 {
+    public int findPaths(int m, int n, int N, int i, int j) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem576 prob = new Problem576();
+        System.out.println(prob.findPaths(2, 2, 2, 0, 0)); // 6
+        System.out.println(prob.findPaths(1, 3, 3, 0, 1)); // 12
+    }
+}
