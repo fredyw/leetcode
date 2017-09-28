04e582f9840ee838ac97605b351fc04d430233a5
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-09-28 00:22:17 -0500

    Skeleton for problem 678

---
+++ src/main/java/leetcode/Problem678.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/valid-parenthesis-string/
+ */
+public class Problem678 {
+    public boolean checkValidString(String s) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem678 prob = new Problem678();
+        System.out.println(prob.checkValidString("()")); // true
+        System.out.println(prob.checkValidString("(*)")); // true
+        System.out.println(prob.checkValidString("(*))")); // true
+        System.out.println(prob.checkValidString(")(")); // false
+    }
+}
