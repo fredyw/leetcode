57eb5b52ff21feac662fa6c694899b5fed661a78
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-09-29 00:16:19 -0500

    Solve problem 678

--- src/main/java/leetcode/Problem678.java
+++ src/main/java/leetcode/Problem678.java
@@ -5,38 +5,24 @@ package leetcode;
  */
 public class Problem678 {
     public boolean checkValidString(String s) {
-        return check(s, 0, 0);
-    }
-
-    private static boolean check(String s, int idx, int opening) {
-        if (opening < 0) {
-            return false;
-        }
-        if (s.length() == idx) {
-            if (opening == 0) {
-                return true;
+        int opening = 0;
+        int closing = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                opening++;
+            } else {
+                opening--;
             }
-            return false;
+            if (s.charAt(i) == ')') {
+                closing--;
+            } else {
+                closing++;
+            }
+            if (closing < 0) {
+                return false;
+            }
+            opening = Math.max(opening, 0);
         }
-        if (s.charAt(idx) == '(') {
-            return check(s, idx + 1, opening + 1);
-        } else if (s.charAt(idx) == ')') {
-            return check(s, idx + 1, opening - 1);
-        } else {
-            boolean a = check(s, idx + 1, opening + 1); // (
-            boolean b = check(s, idx + 1, opening); // )
-            boolean c = check(s, idx + 1, opening - 1); // empty
-            return a || b || c;
-        }
-    }
-
-    public static void main(String[] args) {
-        Problem678 prob = new Problem678();
-        System.out.println(prob.checkValidString("()")); // true
-        System.out.println(prob.checkValidString("(*)")); // true
-        System.out.println(prob.checkValidString("(*))")); // true
-        System.out.println(prob.checkValidString(")(")); // false
-        System.out.println(prob.checkValidString("()()")); // true
-        System.out.println(prob.checkValidString("(**())")); // true
+        return opening == 0;
     }
 }
