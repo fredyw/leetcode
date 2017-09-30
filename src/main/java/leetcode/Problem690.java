35a83d2658720ca88a231da6c4b5069c6b3cbced
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-09-30 16:59:17 -0500

    Skeleton for problem 690

---
+++ src/main/java/leetcode/Problem690.java
@@ -0,0 +1,40 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/employee-importance/
+ */
+public class Problem690 {
+    private static class Employee {
+        // It's the unique id of each node;
+        // unique id of this employee
+        private int id;
+        // the importance value of this employee
+        private int importance;
+        // the id of direct subordinates
+        private List<Integer> subordinates;
+
+        public Employee(int id, int importance, List<Integer> subordinates) {
+            this.id = id;
+            this.importance = importance;
+            this.subordinates = subordinates;
+        }
+    };
+
+    public int getImportance(List<Employee> employees, int id) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem690 prob = new Problem690();
+        System.out.println(Arrays.asList(
+            new Employee(1, 5, Arrays.asList(2, 3)),
+            new Employee(2, 3, Collections.emptyList()),
+            new Employee(3, 3, Collections.emptyList()),
+            1)); // 11
+    }
+}
