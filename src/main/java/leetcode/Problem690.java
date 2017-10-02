35874b34d85173a742c4fd3f66dfd07ab371cca1
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-10-01 19:58:49 -0500

    Solve problem 690

--- src/main/java/leetcode/Problem690.java
+++ src/main/java/leetcode/Problem690.java
@@ -1,8 +1,11 @@
 package leetcode;
 
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * https://leetcode.com/problems/employee-importance/
@@ -12,7 +15,7 @@ public class Problem690 {
         // It's the unique id of each node;
         // unique id of this employee
         private int id;
-        // the importance value of this employee
+        // the importance importance of this employee
         private int importance;
         // the id of direct subordinates
         private List<Integer> subordinates;
@@ -22,19 +25,51 @@ public class Problem690 {
             this.importance = importance;
             this.subordinates = subordinates;
         }
-    };
-
-    public int getImportance(List<Employee> employees, int id) {
-        // TODO
-        return 0;
     }
 
-    public static void main(String[] args) {
-        Problem690 prob = new Problem690();
-        System.out.println(prob.getImportance(Arrays.asList(
-            new Employee(1, 5, Arrays.asList(2, 3)),
-            new Employee(2, 3, Collections.emptyList()),
-            new Employee(3, 3, Collections.emptyList())),
-            1)); // 11
+    public int getImportance(List<Employee> employees, int id) {
+        Map<Integer, List<Node>> adjList = new HashMap<>();
+        Map<Integer, Node> nodeMap = new HashMap<>();
+        for (Employee employee : employees) {
+            if (!nodeMap.containsKey(employee.id)) {
+                nodeMap.put(employee.id, new Node(employee.id, employee.importance));
+            }
+        }
+        for (Employee employee : employees) {
+            if (!adjList.containsKey(employee.id)) {
+                adjList.put(employee.id, new ArrayList<>());
+            }
+            for (int subordinate : employee.subordinates) {
+                adjList.get(employee.id).add(nodeMap.get(subordinate));
+            }
+        }
+        return getImportance(adjList, id, nodeMap, new HashSet<>());
+    }
+
+    private static int getImportance(Map<Integer, List<Node>> adjList, int id,
+                                     Map<Integer, Node> nodeMap, Set<Integer> visited) {
+        if (visited.contains(id)) {
+            return 0;
+        }
+        visited.add(id);
+        Node root = nodeMap.get(id);
+        List<Node> children = adjList.get(root.id);
+        int total = root.importance;
+        if (children != null) {
+            for (Node child : children) {
+                total += getImportance(adjList, child.id, nodeMap, visited);
+            }
+        }
+        return total;
+    }
+
+    private static class Node {
+        private final int id;
+        private final int importance;
+
+        public Node(int id, int importance) {
+            this.id = id;
+            this.importance = importance;
+        }
     }
 }
