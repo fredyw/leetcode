e428e418b63becb113aca9fe102b94d6aead6283
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-10-02 22:53:39 -0500

    Skeleton for problem 686

---
+++ src/main/java/leetcode/Problem686.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/repeated-string-match/
+ */
+public class Problem686 {
+    public int repeatedStringMatch(String A, String B) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem686 prob = new Problem686();
+        System.out.println(prob.repeatedStringMatch("abcd", "cdabcdab")); // 3
+    }
+}
