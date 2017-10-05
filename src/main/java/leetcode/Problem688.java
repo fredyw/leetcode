96e5430539f36b75ed45186d138286644abf1403
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-10-05 01:04:42 -0500

    Skeleton for problem 688

---
+++ src/main/java/leetcode/Problem688.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/knight-probability-in-chessboard/
+ */
+public class Problem688 {
+    public double knightProbability(int N, int K, int r, int c) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem688 prob = new Problem688();
+        System.out.println(prob.knightProbability(3, 2, 0, 0)); // 0.0625
+    }
+}
