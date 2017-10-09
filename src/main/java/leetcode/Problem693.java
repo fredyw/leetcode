ed48fa390618e4a76ecfbc0aaea9b8bb698964f8
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-10-08 23:39:18 -0500

    Skeleton for problem 693

---
+++ src/main/java/leetcode/Problem693.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/binary-number-with-alternating-bits/
+ */
+public class Problem693 {
+    public boolean hasAlternatingBits(int n) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem693 prob = new Problem693();
+        System.out.println(prob.hasAlternatingBits(5)); // true
+        System.out.println(prob.hasAlternatingBits(7)); // false
+        System.out.println(prob.hasAlternatingBits(11)); // false
+        System.out.println(prob.hasAlternatingBits(10)); // true
+    }
+}
