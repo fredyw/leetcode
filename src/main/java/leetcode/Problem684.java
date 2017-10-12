4e4a058f88635e3d169a4dcd9c476c80a228acfa
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-10-12 00:11:27 -0500

    Update problem 684

--- src/main/java/leetcode/Problem684.java
+++ src/main/java/leetcode/Problem684.java
@@ -1,5 +1,8 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * https://leetcode.com/problems/redundant-connection/
  */
@@ -7,6 +10,31 @@ public class Problem684 {
     public int[] findRedundantConnection(int[][] edges) {
         // TODO
         return new int[0];
+    }
+
+    private static class UnionFind {
+        private final Map<Integer, Integer> map = new HashMap<>();
+
+        public void union(int a, int b) {
+            if (!map.containsKey(a)) {
+                map.put(a, a);
+            }
+            if (!map.containsKey(b)) {
+                map.put(b, b);
+            }
+            while (!connected(a, b)) {
+                int value = map.get(a);
+                for (Map.Entry<Integer, Integer> e : map.entrySet()) {
+                    if (e.getValue().equals(value)) {
+                        map.put(e.getKey(), map.get(b));
+                    }
+                }
+            }
+        }
+
+        public boolean connected(int a, int b) {
+            return map.get(a).equals(map.get(b));
+        }
     }
 
     public static void main(String[] args) {
