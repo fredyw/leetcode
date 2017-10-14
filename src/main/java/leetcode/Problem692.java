7723e8bfe02a96de5d51005b27de0dbb5046a125
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-10-13 21:24:03 -0500

    Update problem 692

--- src/main/java/leetcode/Problem692.java
+++ src/main/java/leetcode/Problem692.java
@@ -1,14 +1,46 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * https://leetcode.com/problems/top-k-frequent-words/
  */
 public class Problem692 {
     public List<String> topKFrequent(String[] words, int k) {
-        // TODO
-        return null;
+        Map<String, WordCount> wordCounts = new HashMap<>();
+        for (String word : words) {
+            if (!wordCounts.containsKey(word)) {
+                wordCounts.put(word, new WordCount(word));
+            } else {
+                wordCounts.get(word).count++;
+            }
+        }
+        List<WordCount> values = new ArrayList<>(wordCounts.values());
+        Collections.sort(values, (a, b) -> {
+            int cmp = Integer.compare(b.count, a.count);
+            if (cmp == 0) {
+                return a.word.compareTo(b.word);
+            }
+            return cmp;
+        });
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < k; i++) {
+            result.add(values.get(i).word);
+        }
+        return result;
+    }
+
+    private static class WordCount {
+        private final String word;
+        private int count;
+
+        public WordCount(String word) {
+            this.word = word;
+        }
     }
 
     public static void main(String[] args) {
