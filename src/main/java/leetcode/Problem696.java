1add2c28c7a9fdec57dd4b9c8dae59ffda7b440c
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-10-15 19:54:02 -0500

    Skeleton for problem 696

---
+++ src/main/java/leetcode/Problem696.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/count-binary-substrings/
+ */
+public class Problem696 {
+    public int countBinarySubstrings(String s) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem696 prob = new Problem696();
+        System.out.println(prob.countBinarySubstrings("00110011")); // 6
+        System.out.println(prob.countBinarySubstrings("10101")); // 4
+    }
+}
