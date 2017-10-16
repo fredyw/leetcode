8dd6de0c5ec72cd46e8b107c47d851330b17a83b
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-10-16 00:10:14 -0500

    Skeleton for problem 697

---
+++ src/main/java/leetcode/Problem697.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/degree-of-an-array/
+ */
+public class Problem697 {
+    public int findShortestSubArray(int[] nums) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem697 prob = new Problem697();
+        System.out.println(prob.findShortestSubArray(new int[]{1, 2, 2, 3, 1})); // 2
+        System.out.println(prob.findShortestSubArray(new int[]{1, 2, 2, 3, 1, 4, 2})); // 6
+    }
+}
