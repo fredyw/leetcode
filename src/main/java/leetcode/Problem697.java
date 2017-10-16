43699d69715df06d2d4f8cf2f7cf1a80572752ec
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-10-16 00:14:20 -0500

    Update problem 697

--- src/main/java/leetcode/Problem697.java
+++ src/main/java/leetcode/Problem697.java
@@ -13,5 +13,6 @@ public class Problem697 {
         Problem697 prob = new Problem697();
         System.out.println(prob.findShortestSubArray(new int[]{1, 2, 2, 3, 1})); // 2
         System.out.println(prob.findShortestSubArray(new int[]{1, 2, 2, 3, 1, 4, 2})); // 6
+        System.out.println(prob.findShortestSubArray(new int[]{1, 1, 1, 1, 1})); // 5
     }
 }
