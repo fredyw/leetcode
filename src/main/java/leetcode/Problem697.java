def9bf7cc3c24843e2435e10175aca89307bad99
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-10-16 01:01:16 -0500

    Solve problem 697

--- src/main/java/leetcode/Problem697.java
+++ src/main/java/leetcode/Problem697.java
@@ -11,11 +11,21 @@ import java.util.Map;
 public class Problem697 {
     public int findShortestSubArray(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> left = new HashMap<>();
+        Map<Integer, Integer> right = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             if (!map.containsKey(nums[i])) {
                 map.put(nums[i], 1);
             } else {
                 map.put(nums[i], map.get(nums[i]) + 1);
+            }
+            if (!left.containsKey(nums[i])) {
+                left.put(nums[i], i);
+            }
+        }
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (!right.containsKey(nums[i])) {
+                right.put(nums[i], i);
             }
         }
         int max = 0;
@@ -31,24 +41,7 @@ public class Problem697 {
         }
         int result = Integer.MAX_VALUE;
         for (int num : list) {
-            int tmpResult = 0;
-            int count = 0;
-            boolean first = true;
-            for (int i = 0; i < nums.length; i++) {
-                if (count == max) {
-                    break;
-                }
-                if (nums[i] == num) {
-                    count++;
-                    if (first) {
-                        first = false;
-                    }
-                }
-                if (!first) {
-                    tmpResult++;
-                }
-            }
-            result = Math.min(result, tmpResult);
+            result = Math.min(result, right.get(num) - left.get(num) + 1);
         }
         return result;
     }
