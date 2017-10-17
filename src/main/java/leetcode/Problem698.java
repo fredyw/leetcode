e4683853b70f49690f89e261d84c5c03c173c165
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-10-17 00:17:55 -0500

    Skeleton for problem 698

---
+++ src/main/java/leetcode/Problem698.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/partition-to-k-equal-sum-subsets/
+ */
+public class Problem698 {
+    public boolean canPartitionKSubsets(int[] nums, int k) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem698 prob = new Problem698();
+        System.out.println(prob.canPartitionKSubsets(new int[]{4, 3, 2, 3, 5, 2, 1}, 4)); // true
+    }
+}
