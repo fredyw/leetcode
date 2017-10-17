8e87bca901427386c20187c3ecc59bac79e61208
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-10-17 00:35:28 -0500

    Update problem 698

--- src/main/java/leetcode/Problem698.java
+++ src/main/java/leetcode/Problem698.java
@@ -9,8 +9,22 @@ public class Problem698 {
         return false;
     }
 
+    private static boolean canPartition(int[] nums, int k, int idx) {
+        if (k == 0 && idx == nums.length) {
+            return true;
+        }
+        if (k == 0 && idx < nums.length) {
+            return false;
+        }
+        for (int i = idx; i < nums.length; i++) {
+            // TODO
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         Problem698 prob = new Problem698();
         System.out.println(prob.canPartitionKSubsets(new int[]{4, 3, 2, 3, 5, 2, 1}, 4)); // true
+        System.out.println(prob.canPartitionKSubsets(new int[]{5, 3, 2, 3, 4, 2, 1},4)); // true
     }
 }
