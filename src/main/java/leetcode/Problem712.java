d1dfe1d7a53592cf9f0c27b4b0290e5946c90af8
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-10-23 23:24:47 -0500

    Skeleton for problem 712

---
+++ src/main/java/leetcode/Problem712.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/minimum-ascii-delete-sum-for-two-strings/
+ */
+public class Problem712 {
+    public int minimumDeleteSum(String s1, String s2) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem712 prob = new Problem712();
+        System.out.println(prob.minimumDeleteSum("sea", "eat")); // 231
+        System.out.println(prob.minimumDeleteSum("delete", "leet")); // 403
+    }
+}
