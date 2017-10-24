0b1bba8b8a314d40ca9795f0eebea9e5831d16a4
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-10-23 23:44:46 -0500

    Update problem 72

--- src/main/java/leetcode/Problem72.java
+++ src/main/java/leetcode/Problem72.java
@@ -1,21 +1,17 @@
 package leetcode;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * https://leetcode.com/problems/edit-distance/
  */
 public class Problem72 {
     public int minDistance(String word1, String word2) {
-        Map<String, Integer> memo = new HashMap<>();
+        Integer[][] memo = new Integer[word1.length() + 1][word2.length() + 1];
         return minDistance(word1, word2, word1.length(), word2.length(), memo);
     }
 
-    private int minDistance(String word1, String word2, int i, int j, Map<String, Integer> memo) {
-        String key = i + "|" + j;
-        if (memo.containsKey(key)) {
-            return memo.get(key);
+    private int minDistance(String word1, String word2, int i, int j, Integer[][] memo) {
+        if (memo[i][j] != null) {
+            return memo[i][j];
         }
         if (i == 0) {
             return j;
@@ -33,7 +29,7 @@ public class Problem72 {
         // replace
         int c = minDistance(word1, word2, i - 1, j - 1, memo);
         int min = 1 + Math.min(a, Math.min(b, c));
-        memo.put(key, min);
+        memo[i][j] = min;
         return min;
     }
 }
