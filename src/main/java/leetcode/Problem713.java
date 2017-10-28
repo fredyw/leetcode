c7e51f839904bbcf2f1161644997d9f1c351350f
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-10-28 12:55:54 -0500

    Update problem 713

--- src/main/java/leetcode/Problem713.java
+++ src/main/java/leetcode/Problem713.java
@@ -5,8 +5,28 @@ package leetcode;
  */
 public class Problem713 {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
-        // TODO
-        return 0;
+        int result = 0;
+        int totalProduct = 1;
+        for (int i = 0; i < nums.length; i++) {
+            totalProduct *= nums[i];
+        }
+        if (totalProduct < k) {
+            result++;
+        }
+        for (int i = 0, j = nums.length - 1; i < j; i++, j--) {
+            int leftProduct = 1;
+            for (int x = i; x < j; x++) {
+                leftProduct *= nums[x];
+                if (leftProduct < k) {
+                    result++;
+                }
+                if (totalProduct / leftProduct < k) {
+                    result++;
+                }
+            }
+            totalProduct /= nums[i];
+        }
+        return result;
     }
 
     public static void main(String[] args) {
