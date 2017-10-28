a1d937764eb7fabd319d0c683674241f3ef747e3
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-10-28 11:43:28 -0500

    Skeleton for problem 713

---
+++ src/main/java/leetcode/Problem713.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/subarray-product-less-than-k/
+ */
+public class Problem713 {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem713 prob = new Problem713();
+        System.out.println(prob.numSubarrayProductLessThanK(new int[]{10, 5, 2, 6}, 100)); // 8
+    }
+}
