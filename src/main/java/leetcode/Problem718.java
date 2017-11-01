4402eca58a9807642a4182d6293b2e90503a0e96
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-10-31 23:37:33 -0500

    Skeleton for problem 718

---
+++ src/main/java/leetcode/Problem718.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/maximum-length-of-repeated-subarray/
+ */
+public class Problem718 {
+    public int findLength(int[] A, int[] B) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem718 prob = new Problem718();
+        System.out.println(prob.findLength(new int[]{1, 2, 3, 2, 1}, new int[]{3, 2, 1, 4, 7})); // 3
+    }
+}
