0bb9bd50d8676abc609d787daa953d071fb0db82
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-11-03 00:38:57 -0500

    Update problem 718

--- src/main/java/leetcode/Problem718.java
+++ src/main/java/leetcode/Problem718.java
@@ -17,4 +17,33 @@ public class Problem718 {
         }
         return max;
     }
+
+//    public int findLength(int[] A, int[] B) {
+//        int max = 0;
+//        Integer[][] memo = new Integer[A.length + 1][B.length + 1];
+//        for (int i = 0; i < A.length; i++) {
+//            for (int j = 0; j < B.length; j++) {
+//                max = Math.max(max, findLength(A, B, i, j, memo));
+//            }
+//        }
+//        return max;
+//    }
+//
+//    private static int findLength(int[] a, int[] b, int i, int j, Integer[][] memo) {
+//        if (i == a.length) {
+//            return 0;
+//        }
+//        if (j == b.length) {
+//            return 0;
+//        }
+//        int max = 0;
+//        if (memo[i][j] != null) {
+//            return memo[i][j];
+//        }
+//        if (a[i] == b[j]) {
+//            max = Math.max(max, findLength(a, b, i + 1, j + 1, memo) + 1);
+//        }
+//        memo[i][j] = max;
+//        return max;
+//    }
 }
