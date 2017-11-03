3e69a64615f862f5dea930cb1ef50f285ed94abb
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-11-03 01:46:40 -0500

    Update problem 300

--- src/main/java/leetcode/Problem300.java
+++ src/main/java/leetcode/Problem300.java
@@ -5,25 +5,44 @@ package leetcode;
  */
 public class Problem300 {
     public int lengthOfLIS(int[] nums) {
-        int max = 0;
-        Integer[] memo = new Integer[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            max = Math.max(max, lengthOfLIS(nums, i, memo) + 1);
+        if (nums.length == 0) {
+            return 0;
         }
-        return max;
+        int max = 0;
+        int[] memo = new int[nums.length];
+        for (int i = nums.length - 1; i >= 0; i--) {
+            int tmpMax = 0;
+            for (int j = i; j < nums.length; j++) {
+                if (nums[i] < nums[j]) {
+                    tmpMax = Math.max(tmpMax, memo[j] + 1);
+                }
+            }
+            memo[i] = tmpMax;
+            max = Math.max(max, tmpMax);
+        }
+        return max + 1;
     }
 
-    private static int lengthOfLIS(int[] nums, int idx, Integer[] memo) {
-        if (memo[idx] != null) {
-            return memo[idx];
-        }
-        int max = 0;
-        for (int i = idx; i < nums.length; i++) {
-            if (nums[idx] < nums[i]) {
-                max = Math.max(max, lengthOfLIS(nums, i, memo) + 1);
-            }
-        }
-        memo[idx] = max;
-        return max;
-    }
+//    public int lengthOfLIS(int[] nums) {
+//        int max = 0;
+//        Integer[] memo = new Integer[nums.length];
+//        for (int i = 0; i < nums.length; i++) {
+//            max = Math.max(max, lengthOfLIS(nums, i, memo) + 1);
+//        }
+//        return max;
+//    }
+//
+//    private static int lengthOfLIS(int[] nums, int idx, Integer[] memo) {
+//        if (memo[idx] != null) {
+//            return memo[idx];
+//        }
+//        int max = 0;
+//        for (int i = idx; i < nums.length; i++) {
+//            if (nums[idx] < nums[i]) {
+//                max = Math.max(max, lengthOfLIS(nums, i, memo) + 1);
+//            }
+//        }
+//        memo[idx] = max;
+//        return max;
+//    }
 }
