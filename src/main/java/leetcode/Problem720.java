46b751d34625b93d8cd5f9e4481c6939301c4a96
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-11-06 00:27:17 -0600

    Update problem 720

--- src/main/java/leetcode/Problem720.java
+++ src/main/java/leetcode/Problem720.java
@@ -1,51 +1,51 @@
 package leetcode;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * https://leetcode.com/problems/longest-word-in-dictionary/
  */
 public class Problem720 {
     public String longestWord(String[] words) {
-        Arrays.sort(words, (a, b) -> a.compareTo(b));
-        String tmp = "";
-        List<String> result = new ArrayList<>();
-        for (int i = 0; i < words.length; i++) {
-            if (words[i].length() == 1) {
-                if (!tmp.isEmpty()) {
-                    result.add(tmp);
-                }
-                tmp = words[i];
-            } else {
-                if (tmp.length() + 1 == words[i].length() && words[i].startsWith(tmp)) {
-                    tmp = words[i];
-                }
-            }
-        }
-        result.add(tmp);
-        result.sort((a, b) -> {
-            if (a.length() < b.length()) {
-                return 1;
-            } else if (a.length() > b.length()) {
-                return -1;
-            } else {
+        Arrays.sort(words, (a, b) -> {
+            int cmp = Integer.compare(b.length(), a.length());
+            if (cmp == 0) {
                 return a.compareTo(b);
             }
+            return cmp;
         });
-        return result.get(0);
+        Set<String> set = new HashSet<>();
+        for (String word : words) {
+            set.add(word);
+        }
+        for (String word : words) {
+            boolean found = true;
+            String tmp = "";
+            for (int i = 0; i < word.length(); i++) {
+                tmp += word.charAt(i);
+                if (!set.contains(tmp)) {
+                    found = false;
+                    break;
+                }
+            }
+            if (found) {
+                return word;
+            }
+        }
+        return "";
     }
 
     public static void main(String[] args) {
         Problem720 prob = new Problem720();
-//        System.out.println(prob.longestWord(new String[]{"w","wo","wor","worl", "world"})); // world
-//        System.out.println(prob.longestWord(new String[]{"a", "banana", "app", "appl", "ap", "apply", "apple"})); // apple
-//        System.out.println(prob.longestWord(new String[]{"a"})); // a
-//        System.out.println(prob.longestWord(new String[]{"a", "b"})); // a
-//        System.out.println(prob.longestWord(new String[]{"m", "mo", "moc", "moch", "mocha", "l", "la", "lat", "latt", "latte", "c", "ca", "cat"})); // latte
-//        System.out.println(prob.longestWord(new String[]{"ogz","eyj","e","ey","hmn","v","hm","ogznkb","ogzn","hmnm","eyjuo","vuq","ogznk","og","eyjuoi","d"})); // eyj
-//        System.out.println(prob.longestWord(new String[]{"yo","ew","fc","zrc","yodn","fcm","qm","qmo","fcmz","z","ewq","yod","ewqz","y"})); // yodn
+        System.out.println(prob.longestWord(new String[]{"w","wo","wor","worl", "world"})); // world
+        System.out.println(prob.longestWord(new String[]{"a", "banana", "app", "appl", "ap", "apply", "apple"})); // apple
+        System.out.println(prob.longestWord(new String[]{"a"})); // a
+        System.out.println(prob.longestWord(new String[]{"a", "b"})); // a
+        System.out.println(prob.longestWord(new String[]{"m", "mo", "moc", "moch", "mocha", "l", "la", "lat", "latt", "latte", "c", "ca", "cat"})); // latte
+        System.out.println(prob.longestWord(new String[]{"ogz","eyj","e","ey","hmn","v","hm","ogznkb","ogzn","hmnm","eyjuo","vuq","ogznk","og","eyjuoi","d"})); // eyj
+        System.out.println(prob.longestWord(new String[]{"yo","ew","fc","zrc","yodn","fcm","qm","qmo","fcmz","z","ewq","yod","ewqz","y"})); // yodn
         System.out.println(prob.longestWord(new String[]{"rac","rs","ra","on","r","otif","o","onpdu","rsf","rs","ot","oti","racy","onpd"})); // otif
     }
 }
