96eee6577b9cd59fafe885d0010f777de4a67348
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-11-06 21:40:24 -0600

    Skeleton for problem 714

---
+++ src/main/java/leetcode/Problem714.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/
+ */
+public class Problem714 {
+    public int maxProfit(int[] prices, int fee) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem714 prob = new Problem714();
+        System.out.println(prob.maxProfit(new int[]{1, 3, 2, 8, 4, 9}, 2)); // 8
+    }
+}
