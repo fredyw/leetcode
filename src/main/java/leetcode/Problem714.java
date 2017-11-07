f950189295ea23814ea15f671c9e45239008768f
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-11-06 23:40:30 -0600

    Update problem 714

--- src/main/java/leetcode/Problem714.java
+++ src/main/java/leetcode/Problem714.java
@@ -12,5 +12,7 @@ public class Problem714 {
     public static void main(String[] args) {
         Problem714 prob = new Problem714();
         System.out.println(prob.maxProfit(new int[]{1, 3, 2, 8, 4, 9}, 2)); // 8
+        System.out.println(prob.maxProfit(new int[]{1, 3, 2, 6, 8, 4, 9}, 2)); // 8
+        System.out.println(prob.maxProfit(new int[]{1, 4, 2, 1, 4, 8, 4, 9}, 2)); // 9
     }
 }
