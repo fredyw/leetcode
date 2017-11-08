528813b437d2121b2c52f51884746e4cbeeeacf8
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-11-08 00:19:55 -0600

    Solve problem 714 (not really)

--- src/main/java/leetcode/Problem714.java
+++ src/main/java/leetcode/Problem714.java
@@ -5,14 +5,12 @@ package leetcode;
  */
 public class Problem714 {
     public int maxProfit(int[] prices, int fee) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem714 prob = new Problem714();
-        System.out.println(prob.maxProfit(new int[]{1, 3, 2, 8, 4, 9}, 2)); // 8
-        System.out.println(prob.maxProfit(new int[]{1, 3, 2, 6, 8, 4, 9}, 2)); // 8
-        System.out.println(prob.maxProfit(new int[]{1, 4, 2, 1, 4, 8, 4, 9}, 2)); // 9
+        int cash = 0;
+        int hold = -prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            cash = Math.max(cash, hold + prices[i] - fee);
+            hold = Math.max(hold, cash - prices[i]);
+        }
+        return cash;
     }
 }
