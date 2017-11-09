e4d8c317e20a239bed5d6c48ce90a5c9bc4c2013
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-11-08 23:02:12 -0600

    Solve problem 722

--- src/main/java/leetcode/Problem722.java
+++ src/main/java/leetcode/Problem722.java
@@ -23,11 +23,13 @@ public class Problem722 {
                     } else if (!skip && src.charAt(j + 1) == '*') {
                         skip = true;
                         j++;
+                        continue;
                     }
                 } else if (j + 1 < src.length() && src.charAt(j) == '*') {
                     if (skip && src.charAt(j + 1) == '/') {
                         skip = false;
-                        j += 2;
+                        j++;
+                        continue;
                     }
                 }
                 if (!skip && j < src.length()) {
@@ -40,45 +42,5 @@ public class Problem722 {
             }
         }
         return result;
-    }
-
-    public static void main(String[] args) {
-        Problem722 prob = new Problem722();
-        System.out.println(prob.removeComments(new String[]{
-            "/*Test program */",
-            "int main()",
-            "{ ",
-            "  // variable declaration ",
-            "int a, b, c;",
-            "/* This is a test",
-            "   multiline  ",
-            "   comment for ",
-            "   testing */",
-            "a = b + c;",
-            "}"})); // ["int main()","{ ","  ","int a, b, c;","a = b + c;","}"]
-        System.out.println(prob.removeComments(new String[]{
-            "a/*comment",
-            "line",
-            "more_comment*/b"})); // [ab]
-        System.out.println(prob.removeComments(new String[]{
-            "a/b/*comment*/c+d/*",
-            "line",
-            "more_comment*/b"})); // [a/bc+db]
-        System.out.println(prob.removeComments(new String[]{
-            "void func(int k) {",
-            "// this function does nothing /*",
-            "   k = k*2/4;",
-            "   k = k/2;*/", "}"
-        })); // ["void func(int k) {","   k = k*2/4;","   k = k/2;*/","}"]
-        System.out.println(prob.removeComments(new String[]{
-            "a//*b/*/c",
-            "blank",
-            "d/*/e/*/f"
-        })); // ["a","blank","df"]
-        System.out.println(prob.removeComments(new String[]{
-            "a/*/b//*c",
-            "blank",
-            "d//*e/*/f"
-        })); // ["af"]
     }
 }
