d16a1a89ecae4929a6bb5cc4b865d2f139fb00c1
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-11-12 14:44:18 -0600

    Update problem 721

--- src/main/java/leetcode/Problem721.java
+++ src/main/java/leetcode/Problem721.java
@@ -1,7 +1,9 @@
 package leetcode;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * https://leetcode.com/problems/accounts-merge/
@@ -10,6 +12,37 @@ public class Problem721 {
     public List<List<String>> accountsMerge(List<List<String>> accounts) {
         // TODO
         return null;
+    }
+
+    private static class UnionFind {
+        private final Map<String, String> map = new HashMap<>();
+
+        public void connect(String a, String b) {
+            if (!map.containsKey(a)) {
+                map.put(a, a);
+            }
+            if (!map.containsKey(b)) {
+                map.put(b, b);
+            }
+            if (isConnected(a, b)) {
+                return;
+            }
+            for (Map.Entry<String, String> entry : map.entrySet()) {
+                if (entry.getValue().equals(map.get(a))) {
+                    map.put(entry.getKey(), map.get(b));
+                }
+            }
+        }
+
+        public boolean isConnected(String a, String b) {
+            if (!map.containsKey(a)) {
+                return false;
+            }
+            if (!map.containsKey(b)) {
+                return false;
+            }
+            return map.get(a).equals(map.get(b));
+        }
     }
 
     public static void main(String[] args) {
