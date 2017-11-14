32c0f55013cd342223c8ad4f210b3a8228fe9720
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-11-13 18:44:04 -0600

    Skeleton for problem 724

---
+++ src/main/java/leetcode/Problem724.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/find-pivot-index/description/
+ */
+public class Problem724 {
+    public int pivotIndex(int[] nums) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem724 prob = new Problem724();
+        System.out.println(prob.pivotIndex(new int[]{1, 7, 3, 6, 5, 6})); // 3
+        System.out.println(prob.pivotIndex(new int[]{1, 2, 3})); // -1
+    }
+}
