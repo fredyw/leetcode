186cad58d7ef0c3498039c1cb3fe3bab9bbd83cd
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-11-17 00:25:40 -0600

    Skeleton for problem 725

---
+++ src/main/java/leetcode/Problem725.java
@@ -0,0 +1,48 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/split-linked-list-in-parts/
+ */
+public class Problem725 {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+
+        @Override
+        public String toString() {
+            return "" + val;
+        }
+    }
+
+    public ListNode[] splitListToParts(ListNode root, int k) {
+        // TODO
+        return null;
+    }
+
+    private static ListNode build(int... nums) {
+        ListNode head = null;
+        ListNode next = null;
+        for (int i = 0; i < nums.length; i++) {
+            if (i == 0) {
+                head = new ListNode(nums[i]);
+                next = head;
+            } else {
+                next.next = new ListNode(nums[i]);
+                next = next.next;
+            }
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        Problem725 prob = new Problem725();
+        System.out.println(Arrays.toString(prob.splitListToParts(build(1, 2, 3), 5))); // [[1],[2],[3],[],[]]
+        System.out.println(Arrays.toString(prob.splitListToParts(build(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), 3))); // [[1, 2, 3, 4], [5, 6, 7], [8, 9, 10]]
+    }
+}
