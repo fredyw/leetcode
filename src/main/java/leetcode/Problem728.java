55de7ddff3f682d326d40dd49d6183f2a94e1371
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-11-19 22:37:44 -0600

    Skeleton for problem 728

---
+++ src/main/java/leetcode/Problem728.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/self-dividing-numbers/
+ */
+public class Problem728 {
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem728 prob = new Problem728();
+        System.out.println(prob.selfDividingNumbers(1, 22)); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22]
+    }
+}
