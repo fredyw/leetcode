b44cd212e17e487ae331c43da279a8971c8b15aa
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-11-22 00:03:34 -0600

    Solve problem 729

--- src/main/java/leetcode/Problem729.java
+++ src/main/java/leetcode/Problem729.java
@@ -1,25 +1,26 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * https://leetcode.com/problems/my-calendar-i/
  */
 public class Problem729 {
     private static class MyCalendar {
+        private final List<int[]> list = new ArrayList<>();
 
         public MyCalendar() {
-
         }
 
         public boolean book(int start, int end) {
-            // TODO
-            return false;
+            for (int[] i : list) {
+                if (i[0] < end && start < i[1]) {
+                    return false;
+                }
+            }
+            list.add(new int[]{start, end});
+            return true;
         }
-    }
-
-    public static void main(String[] args) {
-        MyCalendar calendar = new MyCalendar();
-        calendar.book(10, 20); // returns true
-        calendar.book(15, 25); // returns false
-        calendar.book(20, 30); // returns true
     }
 }
