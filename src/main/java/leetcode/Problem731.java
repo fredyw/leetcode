e04a8eaa4e99ec59b501a2c3c5012b6726e13ab2
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-11-24 23:23:13 -0600

    Update problem 731

--- src/main/java/leetcode/Problem731.java
+++ src/main/java/leetcode/Problem731.java
@@ -1,18 +1,30 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * https://leetcode.com/problems/my-calendar-ii/
  */
 public class Problem731 {
     private static class MyCalendarTwo {
+        private final List<int[]> list = new ArrayList<>();
 
         public MyCalendarTwo() {
-
         }
 
         public boolean book(int start, int end) {
-            // TODO
-            return false;
+            for (int[] i : list) {
+                if (i[0] < end && start < i[1]) {
+                    if (i[2] >= 1) {
+                        return false;
+                    } else {
+                        i[2]++;
+                    }
+                }
+            }
+            list.add(new int[]{start, end, 1});
+            return true;
         }
     }
 
