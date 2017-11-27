84ea26d4d5e3a59e5a4f811ad99c70ce87d0986f
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-11-26 23:49:57 -0600

    Skeleton for problem 733

---
+++ src/main/java/leetcode/Problem733.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/flood-fill/
+ */
+public class Problem733 {
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem733 prob = new Problem733();
+        System.out.println(prob.floodFill(new int[][]{{1, 1, 1}, {1, 1, 0}, {1, 0, 1}}, 1, 1, 2)); // [[2,2,2],[2,2,0],[2,0,1]]
+    }
+}
