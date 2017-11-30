b0834ec1641a245a582bb165cdffdd7a474de10e
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-11-30 01:27:16 -0600

    Solve problem 733

--- src/main/java/leetcode/Problem733.java
+++ src/main/java/leetcode/Problem733.java
@@ -5,15 +5,37 @@ package leetcode;
  */
 public class Problem733 {
     public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
-        // TODO
-        return null;
+        int maxRow = image.length;
+        int maxCol = 0;
+        if (image.length > 0) {
+            maxCol = image[0].length;
+        }
+        boolean[][] visited = new boolean[maxRow][maxCol];
+        floodFill(image, sr, sc, newColor, image[sr][sc], maxRow, maxCol, visited);
+        return image;
     }
 
-    public static void main(String[] args) {
-        Problem733 prob = new Problem733();
-        System.out.println(prob.floodFill(new int[][]{
-            {1, 1, 1},
-            {1, 1, 0},
-            {1, 0, 1}}, 1, 1, 2)); // [[2,2,2],[2,2,0],[2,0,1]]
+    private static void floodFill(int[][] image, int row, int col, int newColor, int originalColor,
+                                  int maxRow, int maxCol, boolean[][] visited) {
+        if (row < 0 || col < 0 || row >= maxRow || col >= maxCol) {
+            return;
+        }
+        if (visited[row][col]) {
+            return;
+        }
+        visited[row][col] = true;
+        if (image[row][col] == originalColor) {
+            image[row][col] = newColor;
+        } else {
+            return;
+        }
+        // top
+        floodFill(image, row - 1, col, newColor, originalColor, maxRow, maxCol, visited);
+        // right
+        floodFill(image, row, col + 1, newColor, originalColor, maxRow, maxCol, visited);
+        // down
+        floodFill(image, row + 1, col, newColor, originalColor, maxRow, maxCol, visited);
+        // left
+        floodFill(image, row, col - 1, newColor, originalColor, maxRow, maxCol, visited);
     }
 }
