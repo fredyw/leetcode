24efa253a2f340b6ad92947b97fa9a5755991b41
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-12-02 16:25:32 -0600

    Solve problem 737

--- src/main/java/leetcode/Problem737.java
+++ src/main/java/leetcode/Problem737.java
@@ -19,8 +19,10 @@ public class Problem737 {
             if (!map.containsKey(pair[1])) {
                 map.put(pair[1], pair[1]);
             }
-            if (!find(map, pair[0]).equals(find(map, pair[1]))) {
-                map.put(pair[0], pair[1]);
+            String val1 = find(map, pair[0]);
+            String val2 = find(map, pair[1]);
+            if (!val1.equals(val2)) {
+                map.put(val1, val2);
             }
         }
         for (int i = 0; i < words1.length; i++) {
@@ -29,13 +31,12 @@ public class Problem737 {
             if (word1.equals(word2)) {
                 continue;
             }
-            if (!map.containsKey(word1)) {
+            String val1 = find(map, word1);
+            String val2 = find(map, word2);
+            if (val1 == null || val2 == null) {
                 return false;
             }
-            if (!map.containsKey(word2)) {
-                return false;
-            }
-            if (!find(map, word1).equals(find(map, word2))) {
+            if (!val1.equals(val2)) {
                 return false;
             }
         }
@@ -45,34 +46,13 @@ public class Problem737 {
     private static String find(Map<String, String> map, String word) {
         String key = word;
         String value = map.get(key);
+        if (value == null) {
+            return null;
+        }
         while (!key.equals(value)) {
             key = value;
             value = map.get(key);
         }
         return value;
-    }
-
-    public static void main(String[] args) {
-        Problem737 prob = new Problem737();
-        System.out.println(prob.areSentencesSimilarTwo(
-            new String[]{"great", "acting", "skills"},
-            new String[]{"fine", "drama", "talent"},
-            new String[][]{{"great", "good"}, {"fine", "good"}, {"acting","drama"}, {"skills","talent"}}
-        )); // true
-        System.out.println(prob.areSentencesSimilarTwo(
-            new String[]{"great"},
-            new String[]{"great"},
-            new String[][]{}
-        )); // true
-        System.out.println(prob.areSentencesSimilarTwo(
-            new String[]{"great"},
-            new String[]{"fine"},
-            new String[][]{{"great", "good"}, {"fine", "good"}}
-        )); // true
-        System.out.println(prob.areSentencesSimilarTwo(
-            new String[]{"great"},
-            new String[]{"great", "blah"},
-            new String[][]{}
-        )); // false
     }
 }
