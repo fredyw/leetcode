b768ea1f13d31b667e514885cd901e364850e4b8
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-12-03 12:41:30 -0600

    Update problem 735

--- src/main/java/leetcode/Problem735.java
+++ src/main/java/leetcode/Problem735.java
@@ -1,19 +1,41 @@
 package leetcode;
 
+import java.util.Arrays;
+import java.util.Stack;
+
 /**
  * https://leetcode.com/problems/asteroid-collision/
  */
 public class Problem735 {
     public int[] asteroidCollision(int[] asteroids) {
-        // TODO
-        return null;
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < asteroids.length; i++) {
+            if (stack.isEmpty()) {
+                stack.push(asteroids[i]);
+            } else {
+                int a = stack.pop();
+                if (a > 0 && asteroids[i] < 0) {
+                    int sum = a + asteroids[i];
+                    if (sum < 0) {
+                        stack.push(asteroids[i]);
+                    } else if (sum > 0) {
+                        stack.push(a);
+                    }
+                }
+            }
+        }
+        int[] result = new int[stack.size()];
+        for (int i = stack.size() - 1; i >= 0; i--) {
+            result[i] = stack.pop();
+        }
+        return result;
     }
 
     public static void main(String[] args) {
         Problem735 prob = new Problem735();
-        System.out.println(prob.asteroidCollision(new int[]{5, 10, -5})); // [5, 10]
-        System.out.println(prob.asteroidCollision(new int[]{8, -8})); // []
-        System.out.println(prob.asteroidCollision(new int[]{10, 2, -5})); // [8]
-        System.out.println(prob.asteroidCollision(new int[]{-2, -1, 1, 2})); // [-2, -1, 1, 2]
+        System.out.println(Arrays.toString(prob.asteroidCollision(new int[]{5, 10, -5}))); // [5, 10]
+        System.out.println(Arrays.toString(prob.asteroidCollision(new int[]{8, -8}))); // []
+        System.out.println(Arrays.toString(prob.asteroidCollision(new int[]{10, 2, -5}))); // [10]
+        System.out.println(Arrays.toString(prob.asteroidCollision(new int[]{-2, -1, 1, 2}))); // [-2, -1, 1, 2]
     }
 }
