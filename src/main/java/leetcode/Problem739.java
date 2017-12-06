a806de74cfc552cb279ddf24566e9fb522a8fa8f
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-12-05 23:11:15 -0600

    Update problem 739

--- src/main/java/leetcode/Problem739.java
+++ src/main/java/leetcode/Problem739.java
@@ -1,16 +1,39 @@
 package leetcode;
 
+import java.util.Arrays;
+
 /**
  * https://leetcode.com/problems/daily-temperatures/
  */
 public class Problem739 {
     public int[] dailyTemperatures(int[] temperatures) {
-        // TODO
-        return null;
+        int[] result = new int[temperatures.length];
+        int i = 0;
+        int j = 1;
+        int count = 1;
+        while (i < temperatures.length) {
+            if (j == temperatures.length) {
+                count = 1;
+                i++;
+                j = i + 1;
+                continue;
+            }
+            if (temperatures[i] < temperatures[j++]) {
+                result[i++] = count;
+                count = 1;
+                j = i + 1;
+            } else {
+                count++;
+            }
+        }
+        return result;
     }
 
     public static void main(String[] args) {
         Problem739 prob = new Problem739();
-        System.out.println(prob.dailyTemperatures(new int[]{73, 74, 75, 71, 69, 72, 76, 73})); // [1, 1, 4, 2, 1, 1, 0, 0]
+        System.out.println(Arrays.toString(prob.dailyTemperatures(new int[]{73, 74, 75, 71, 69, 72, 76, 73}))); // [1, 1, 4, 2, 1, 1, 0, 0]
+        System.out.println(Arrays.toString(prob.dailyTemperatures(new int[]{73, 73}))); // [0, 0]
+        System.out.println(Arrays.toString(prob.dailyTemperatures(new int[]{73, 72, 71, 70}))); // [0, 0, 0, 0]
+        System.out.println(Arrays.toString(prob.dailyTemperatures(new int[]{55, 38, 53, 81, 61, 93, 97, 32, 43, 78}))); // [3,1,1,2,1,1,0,1,1,0]
     }
 }
