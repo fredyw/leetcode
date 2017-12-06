409bd894ef14e5d94177f2f0b52b40a06fe42931
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-12-05 21:40:22 -0600

    Skeleton for problem 739

---
+++ src/main/java/leetcode/Problem739.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/daily-temperatures/
+ */
+public class Problem739 {
+    public int[] dailyTemperatures(int[] temperatures) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem739 prob = new Problem739();
+        System.out.println(prob.dailyTemperatures(new int[]{73, 74, 75, 71, 69, 72, 76, 73})); // [1, 1, 4, 2, 1, 1, 0, 0]
+    }
+}
