59d3eb5160896221e092408e915001d9834078eb
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-12-06 23:46:55 -0600

    Update problem 739

--- src/main/java/leetcode/Problem739.java
+++ src/main/java/leetcode/Problem739.java
@@ -1,6 +1,7 @@
 package leetcode;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 /**
  * https://leetcode.com/problems/daily-temperatures/
@@ -8,32 +9,42 @@ import java.util.Arrays;
 public class Problem739 {
     public int[] dailyTemperatures(int[] temperatures) {
         int[] result = new int[temperatures.length];
-        int i = 0;
+        Stack<Integer> stack = new Stack<>();
         int j = 1;
-        int count = 1;
-        while (i < temperatures.length) {
-            if (j == temperatures.length) {
-                count = 1;
-                i++;
-                j = i + 1;
-                continue;
-            }
-            if (temperatures[i] < temperatures[j++]) {
-                result[i++] = count;
-                count = 1;
-                j = i + 1;
+        for (int i = 0; i < temperatures.length - 1; i++) {
+            if (temperatures[i] < temperatures[j]) {
+                result[i] = j - i;
             } else {
-                count++;
+                stack.push(i);
             }
         }
+
+//        int i = 0;
+//        int j = 1;
+//        int count = 1;
+//        while (i < temperatures.length) {
+//            if (j == temperatures.length) {
+//                count = 1;
+//                i++;
+//                j = i + 1;
+//                continue;
+//            }
+//            if (temperatures[i] < temperatures[j++]) {
+//                result[i++] = count;
+//                count = 1;
+//                j = i + 1;
+//            } else {
+//                count++;
+//            }
+//        }
         return result;
     }
 
     public static void main(String[] args) {
         Problem739 prob = new Problem739();
         System.out.println(Arrays.toString(prob.dailyTemperatures(new int[]{73, 74, 75, 71, 69, 72, 76, 73}))); // [1, 1, 4, 2, 1, 1, 0, 0]
-        System.out.println(Arrays.toString(prob.dailyTemperatures(new int[]{73, 73}))); // [0, 0]
-        System.out.println(Arrays.toString(prob.dailyTemperatures(new int[]{73, 72, 71, 70}))); // [0, 0, 0, 0]
-        System.out.println(Arrays.toString(prob.dailyTemperatures(new int[]{55, 38, 53, 81, 61, 93, 97, 32, 43, 78}))); // [3,1,1,2,1,1,0,1,1,0]
+//        System.out.println(Arrays.toString(prob.dailyTemperatures(new int[]{73, 73}))); // [0, 0]
+//        System.out.println(Arrays.toString(prob.dailyTemperatures(new int[]{73, 72, 71, 70}))); // [0, 0, 0, 0]
+//        System.out.println(Arrays.toString(prob.dailyTemperatures(new int[]{55, 38, 53, 81, 61, 93, 97, 32, 43, 78}))); // [3,1,1,2,1,1,0,1,1,0]
     }
 }
