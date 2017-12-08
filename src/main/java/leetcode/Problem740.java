b4253d9ae74f97323eaa8107b5202e1bbdb41f2d
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-12-07 20:56:35 -0600

    Skeleton for problem 740

---
+++ src/main/java/leetcode/Problem740.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/delete-and-earn/
+ */
+public class Problem740 {
+    public int deleteAndEarn(int[] nums) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem740 prob = new Problem740();
+        System.out.println(prob.deleteAndEarn(new int[]{3, 4, 2})); // 6
+        System.out.println(prob.deleteAndEarn(new int[]{2, 2, 3, 3, 3, 4})); // 9
+    }
+}
