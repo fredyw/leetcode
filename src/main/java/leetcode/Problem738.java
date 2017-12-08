9276064f3566a646d470e494f53c7dbad4fb7d83
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-12-07 20:54:45 -0600

    Skeleton for problem 738

---
+++ src/main/java/leetcode/Problem738.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/monotone-increasing-digits/
+ */
+public class Problem738 {
+    public int monotoneIncreasingDigits(int N) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem738 prob = new Problem738();
+        System.out.println(prob.monotoneIncreasingDigits(10)); // 9
+        System.out.println(prob.monotoneIncreasingDigits(1234)); // 1234
+        System.out.println(prob.monotoneIncreasingDigits(332)); // 299
+    }
+}
