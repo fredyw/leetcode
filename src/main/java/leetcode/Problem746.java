3f32839f6a58249029f90992723780c2365a5d98
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-12-26 01:55:29 -0600

    Skeleton for problem 746

---
+++ src/main/java/leetcode/Problem746.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/min-cost-climbing-stairs/
+ */
+public class Problem746 {
+    public int minCostClimbingStairs(int[] cost) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem746 prob = new Problem746();
+        System.out.println(prob.minCostClimbingStairs(new int[]{10, 15, 20})); // 15
+        System.out.println(prob.minCostClimbingStairs(new int[]{1, 100, 1, 1, 1, 100, 1, 1, 100, 1})); // 6
+    }
+}
