9d3668047188138ba76db64cce7e29959f579abc
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-12-26 01:32:47 -0600

    Solve problem 744

--- src/main/java/leetcode/Problem744.java
+++ src/main/java/leetcode/Problem744.java
@@ -1,21 +1,21 @@
 package leetcode;
 
+import java.util.Arrays;
+
 /**
  * https://leetcode.com/problems/find-smallest-letter-greater-than-target/description/
  */
 public class Problem744 {
     public char nextGreatestLetter(char[] letters, char target) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem744 prob = new Problem744();
-        System.out.println(prob.nextGreatestLetter(new char[]{'c', 'f', 'j'}, 'a')); // c
-        System.out.println(prob.nextGreatestLetter(new char[]{'c', 'f', 'j'}, 'c')); // f
-        System.out.println(prob.nextGreatestLetter(new char[]{'c', 'f', 'j'}, 'd')); // f
-        System.out.println(prob.nextGreatestLetter(new char[]{'c', 'f', 'j'}, 'g')); // j
-        System.out.println(prob.nextGreatestLetter(new char[]{'c', 'f', 'j'}, 'j')); // c
-        System.out.println(prob.nextGreatestLetter(new char[]{'c', 'f', 'j'}, 'k')); // c
+        int index = Arrays.binarySearch(letters, target);
+        if (index >= 0) {
+            index = (index + 1) % letters.length;
+            while (letters[index] == target) {
+                index = (index + 1) % letters.length;
+            }
+            return letters[index];
+        }
+        index = (-index - 1) % letters.length;
+        return letters[index];
     }
 }
