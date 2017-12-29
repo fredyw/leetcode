2ca2d66604b2dc054f628fed033679878f2a5fbc
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-12-29 07:08:15 -0600

    Solve problem 749

--- src/main/java/leetcode/Problem749.java
+++ src/main/java/leetcode/Problem749.java
@@ -1,17 +1,45 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * https://leetcode.com/problems/shortest-completing-word/
  */
 public class Problem749 {
     public String shortestCompletingWord(String licensePlate, String[] words) {
-        // TODO
-        return null;
-    }
-
-    public static void main(String[] args) {
-        Problem749 prob = new Problem749();
-        System.out.println(prob.shortestCompletingWord("1s3 PSt", new String[]{"step", "steps", "stripe", "stepple"})); // steps
-        System.out.println(prob.shortestCompletingWord("1s3 456", new String[]{"looks", "pest", "stew", "show"})); // pest
+        String plate = licensePlate.toLowerCase();
+        Map<Character, Integer> charCounts = new HashMap<>();
+        for (int i = 0; i < plate.length(); i++) {
+            if (plate.charAt(i) >= 'a' && plate.charAt(i) <= 'z') {
+                if (!charCounts.containsKey(plate.charAt(i))) {
+                    charCounts.put(plate.charAt(i), 1);
+                } else {
+                    charCounts.put(plate.charAt(i), charCounts.get(plate.charAt(i)) + 1);
+                }
+            }
+        }
+        int min = Integer.MAX_VALUE;
+        String result = "";
+        for (String word : words) {
+            Map<Character, Integer> copyCharCounts = new HashMap<>(charCounts);
+            for (char c : word.toCharArray()) {
+                if (copyCharCounts.containsKey(c)) {
+                    Integer newCount = copyCharCounts.get(c) - 1;
+                    if (newCount == 0) {
+                        copyCharCounts.remove(c);
+                    } else {
+                        copyCharCounts.put(c, newCount);
+                    }
+                }
+            }
+            if (copyCharCounts.isEmpty()) {
+                if (word.length() < min) {
+                    min = word.length();
+                    result = word;
+                }
+            }
+        }
+        return result;
     }
 }
