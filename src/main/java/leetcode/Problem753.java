512dca2e6867230778bbfff255f88fb2d9b44c78
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-01-01 18:01:25 -0600

    Solve problem 753 (first problem in 2018, happy new year)

--- src/main/java/leetcode/Problem753.java
+++ src/main/java/leetcode/Problem753.java
@@ -16,49 +16,52 @@ public class Problem753 {
         for (String dead : deadends) {
             deads.add(dead);
         }
-        int result = 0;
         Set<String> visited = new HashSet<>();
-        Queue<String> queue = new LinkedList<>();
-        queue.add("0000");
+        Queue<NodeDepth> queue = new LinkedList<>();
+        queue.add(new NodeDepth("0000", 0));
         while (!queue.isEmpty()) {
-            String current = queue.remove();
-            System.out.println(current);
-            if (deads.contains(current)) {
+            NodeDepth current = queue.remove();
+            if (deads.contains(current.node)) {
                 continue;
             }
-            if (current.equals(target)) {
-                return result;
+            if (current.node.equals(target)) {
+                return current.level;
             }
-            for (String neighbor : getNeighbors(current)) {
-                if (!visited.contains(neighbor)) {
+            for (NodeDepth neighbor : getNeighbors(current)) {
+                if (!visited.contains(neighbor.node)) {
                     queue.add(neighbor);
-                    visited.add(neighbor);
+                    visited.add(neighbor.node);
                 }
             }
         }
         return -1;
     }
 
-    private static List<String> getNeighbors(String node) {
-        List<String> neighbors = new ArrayList<>();
-        for (int i = 0; i < node.length(); i++) {
-            int n1 = (node.charAt(i) - '0' - 1) % 10;
+    private static class NodeDepth {
+        private final String node;
+        private final int level;
+
+        public NodeDepth(String node, int depth) {
+            this.node = node;
+            this.level = depth;
+        }
+    }
+
+    private static List<NodeDepth> getNeighbors(NodeDepth nodeDepth) {
+        List<NodeDepth> neighbors = new ArrayList<>();
+        for (int i = 0; i < nodeDepth.node.length(); i++) {
+            int n1 = (nodeDepth.node.charAt(i) - '0' - 1) % 10;
             if (n1 < 0) {
                 n1 = 9;
             }
-            neighbors.add(node.substring(0, i) + n1 + node.substring(i + 1));
-            int n2 = (node.charAt(i) - '0' + 1) % 10;
-            neighbors.add(node.substring(0, i) + n2 + node.substring(i + 1));
+            neighbors.add(new NodeDepth(
+                nodeDepth.node.substring(0, i) + n1 + nodeDepth.node.substring(i + 1),
+                nodeDepth.level + 1));
+            int n2 = (nodeDepth.node.charAt(i) - '0' + 1) % 10;
+            neighbors.add(new NodeDepth(
+                nodeDepth.node.substring(0, i) + n2 + nodeDepth.node.substring(i + 1),
+                nodeDepth.level + 1));
         }
         return neighbors;
-    }
-
-    public static void main(String[] args) {
-        Problem753 prob = new Problem753();
-        System.out.println(prob.openLock(new String[]{"0201", "0101", "0102", "1212", "2002"}, "0202")); // 6
-//        System.out.println(prob.openLock(new String[]{"8888"}, "0009")); // 1
-//        System.out.println(prob.openLock(new String[]{"8887", "8889", "8878", "8898", "8788", "8988", "7888", "9888"}, "8888")); // -1
-//        System.out.println(prob.openLock(new String[]{"0000"}, "8888")); // -1
-//        System.out.println("1234".substring(0, 1) + '0' + "1234".substring(2));
     }
 }
