ae094dd49893c9073c813d8f752439b5f450b964
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-01-06 18:52:34 -0600

    Update problem 756

--- src/main/java/leetcode/Problem756.java
+++ src/main/java/leetcode/Problem756.java
@@ -12,11 +12,10 @@ public class Problem756 {
             // left
             int index = K;
             boolean found = false;
-            for (int j = K; j > 0; j--) {
-                int left = heights[j - 1];
-                if (heights[j] > left) {
+            for (int j = K - 1; j >= 0; j--) {
+                if (heights[j] < heights[index]) {
                     found = true;
-                    index = left;
+                    index = j;
                 }
             }
             if (found) {
@@ -27,11 +26,10 @@ public class Problem756 {
             // right
             index = K;
             found = false;
-            for (int j = K; j < heights.length - 1; j++) {
-                int right = heights[j + 1];
-                if (heights[j] > right) {
+            for (int j = K + 1; j < heights.length; j++) {
+                if (heights[j] < heights[index]) {
                     found = true;
-                    index = right;
+                    index = j;
                 }
             }
             if (found) {
@@ -48,9 +46,10 @@ public class Problem756 {
         Problem756 prob = new Problem756();
 //        System.out.println(Arrays.toString(prob.pourWater(new int[]{2, 1, 2, 2, 1, 2, 2}, 1, 3))); // [2,2,2,2,1,2,2]
 //        System.out.println(Arrays.toString(prob.pourWater(new int[]{2, 1, 1, 2, 1, 2, 2}, 4, 3))); // [2,2,2,3,2,2,2]
-        System.out.println(Arrays.toString(prob.pourWater(new int[]{2, 1, 1, 2, 1, 2, 2}, 1, 3))); // [2,1,2,2,1,2,2]
+//        System.out.println(Arrays.toString(prob.pourWater(new int[]{2, 1, 1, 2, 1, 2, 2}, 1, 3))); // [2,1,2,2,1,2,2]
 //        System.out.println(Arrays.toString(prob.pourWater(new int[]{1, 2, 3, 4}, 2, 2))); // [2,3,3,4]
 //        System.out.println(Arrays.toString(prob.pourWater(new int[]{3, 1, 3}, 5, 1))); // [4,4,4]
 //        System.out.println(Arrays.toString(prob.pourWater(new int[]{2, 1, 2, 3, 1, 2, 2}, 1, 3))); // [2,2,2,3,1,2,2]
+        System.out.println(Arrays.toString(prob.pourWater(new int[]{1, 2, 3, 4, 3, 2, 1, 2, 3, 4, 3, 2, 1}, 2, 5))); // [1,2,3,4,3,3,2,2,3,4,3,2,1]
     }
 }
