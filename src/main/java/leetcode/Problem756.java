722c72c287eb96363d876b8848660520ce5fb338
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-01-07 17:34:55 -0600

    Update problem 756

--- src/main/java/leetcode/Problem756.java
+++ src/main/java/leetcode/Problem756.java
@@ -17,5 +17,6 @@ public class Problem756 {
         System.out.println(prob.pyramidTransition("XYZ", Arrays.asList("XYD", "YZE", "DEA", "FFF"))); // true
         System.out.println(prob.pyramidTransition("XXYX", Arrays.asList("XXX", "XXY", "XYX", "XYY", "YXZ"))); // false
         System.out.println(prob.pyramidTransition("ABC", Arrays.asList("ABD", "BCE", "DEF", "FFF"))); // true
+        System.out.println(prob.pyramidTransition("XXX", Arrays.asList("XXX", "XXX", "XXX", "FFF"))); // true
     }
 }
