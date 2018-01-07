fc177b2fbc794691de600412091f8e9ca75bdcd9
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-01-07 13:29:21 -0600

    Skeleton for problem 756

---
+++ src/main/java/leetcode/Problem756.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/pyramid-transition-matrix/
+ */
+public class Problem756 {
+    public boolean pyramidTransition(String bottom, List<String> allowed) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem756 prob = new Problem756();
+        System.out.println(prob.pyramidTransition("XYZ", Arrays.asList("XYD", "YZE", "DEA", "FFF"))); // true
+        System.out.println(prob.pyramidTransition("XXYX", Arrays.asList("XXX", "XXY", "XYX", "XYY", "YXZ"))); // false
+    }
+}
