feacb2baa7aea11ea35c1e9b8a441905a6395ac6
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-01-10 00:41:05 -0600

    Skeleton for problem 760

---
+++ src/main/java/leetcode/Problem760.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/find-anagram-mappings/
+ */
+public class Problem760 {
+    public int[] anagramMappings(int[] A, int[] B) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem760 prob = new Problem760();
+        System.out.println(prob.anagramMappings(new int[]{12, 28, 46, 32, 50}, new int[]{50, 12, 32, 46, 28})); // [1, 4, 3, 2, 0]
+    }
+}
