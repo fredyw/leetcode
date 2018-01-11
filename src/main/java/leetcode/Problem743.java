fb84d2f2aa7d70e0bf921e4f2f2d2cc17495d28e
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-01-10 22:56:10 -0600

    Skeleton for problem 743

---
+++ src/main/java/leetcode/Problem743.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/network-delay-time/
+ */
+public class Problem743 {
+    public int networkDelayTime(int[][] times, int N, int K) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem743 prob = new Problem743();
+    }
+}
