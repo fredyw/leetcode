a50f6fc8ce6940c639852df9cd3fb984ab51e2dd
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-01-14 23:35:23 -0600

    Skeleton for problem 762

---
+++ src/main/java/leetcode/Problem762.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/prime-number-of-set-bits-in-binary-representation/
+ */
+public class Problem762 {
+    public int countPrimeSetBits(int L, int R) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem762 prob = new Problem762();
+        System.out.println(prob.countPrimeSetBits(6, 10)); // 4
+        System.out.println(prob.countPrimeSetBits(10, 15)); // 5
+    }
+}
