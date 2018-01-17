d7670cf1ff314f755589ae8556c2a69750c10b7b
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-01-17 00:23:20 -0600

    Skeleton for problem 763

---
+++ src/main/java/leetcode/Problem763.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/partition-labels/
+ */
+public class Problem763 {
+    public List<Integer> partitionLabels(String S) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem763 prob = new Problem763();
+        System.out.println(prob.partitionLabels("ababcbacadefegdehijhklij")); // [9,7,8]
+    }
+}
