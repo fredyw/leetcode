86f9073fbd83fd353ca30820b910d737ccc005d4
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-01-17 23:14:45 -0600

    Update problem 763

--- src/main/java/leetcode/Problem763.java
+++ src/main/java/leetcode/Problem763.java
@@ -1,14 +1,50 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * https://leetcode.com/problems/partition-labels/
  */
 public class Problem763 {
     public List<Integer> partitionLabels(String S) {
-        // TODO
-        return null;
+        List<Map<Character, Integer>> list = new ArrayList<>();
+        list.add(new HashMap<>());
+        for (int i = 0; i < S.length(); i++) {
+            char c = S.charAt(i);
+            for (int j = 0; j < list.size(); j++) {
+                Map<Character, Integer> map = list.get(j);
+                if (map.containsKey(c)) {
+                    map.put(c, map.get(c) + 1);
+                    int size = list.size();
+                    for (int k = size - 1; k >= j + 1; k--) {
+                        Map<Character, Integer> map2 = list.get(k);
+                        map2.forEach((key, value) -> {
+                            if (map.containsKey(key)) {
+                                map.put(key, map.get(key) + value);
+                            }
+                        });
+                        list.remove(k);
+                    }
+                    break;
+                } else {
+                    Map<Character, Integer> newMap = new HashMap<>();
+                    newMap.put(c, 1);
+                    list.add(newMap);
+                }
+            }
+        }
+        List<Integer> result = new ArrayList<>();
+        for (Map<Character, Integer> map : list) {
+            int total = 0;
+            for (int value : map.values()) {
+                total += value;
+            }
+            result.add(total);
+        }
+        return result;
     }
 
     public static void main(String[] args) {
