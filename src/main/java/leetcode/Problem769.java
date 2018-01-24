f5cd45a727552c4dfd63f3978066a4828a9d66b8
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-01-24 00:02:53 -0600

    Skeleton for problem 769

---
+++ src/main/java/leetcode/Problem769.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/max-chunks-to-make-sorted/
+ */
+public class Problem769 {
+    public int maxChunksToSorted(int[] arr) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem769 prob = new Problem769();
+        System.out.println(prob.maxChunksToSorted(new int[]{4, 3, 2, 1, 0})); // 1
+        System.out.println(prob.maxChunksToSorted(new int[]{1, 0, 2, 3, 4})); // 4
+    }
+}
