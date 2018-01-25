65c515b0ca39fd26bdf21c73c9d013a9748c73de
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-01-25 00:11:13 -0600

    Update problem 769

--- src/main/java/leetcode/Problem769.java
+++ src/main/java/leetcode/Problem769.java
@@ -1,17 +1,35 @@
 package leetcode;
 
+import java.util.Arrays;
+
 /**
  * https://leetcode.com/problems/max-chunks-to-make-sorted/
  */
 public class Problem769 {
     public int maxChunksToSorted(int[] arr) {
-        // TODO
-        return 0;
+        int[] sorted = Arrays.copyOf(arr, arr.length);
+        Arrays.sort(sorted);
+        int result = 0;
+        int i = 0;
+        int j = i;
+        while (i < sorted.length) {
+            while (j < arr.length && arr[j] != sorted[i]) {
+                j++;
+            }
+            i = j;
+            result++;
+        }
+        return result;
     }
 
     public static void main(String[] args) {
         Problem769 prob = new Problem769();
-        System.out.println(prob.maxChunksToSorted(new int[]{4, 3, 2, 1, 0})); // 1
-        System.out.println(prob.maxChunksToSorted(new int[]{1, 0, 2, 3, 4})); // 4
+//        System.out.println(prob.maxChunksToSorted(new int[]{4, 3, 2, 1, 0})); // 1
+//        System.out.println(prob.maxChunksToSorted(new int[]{1, 0, 2, 3, 4})); // 4
+//        System.out.println(prob.maxChunksToSorted(new int[]{1, 0, 1, 2, 3, 4})); // 5
+        System.out.println(prob.maxChunksToSorted(new int[]{2, 0, 1})); // 1
+        System.out.println(prob.maxChunksToSorted(new int[]{2, 0, 1, 5})); // 2
+//        System.out.println(prob.maxChunksToSorted(new int[]{1, 2})); // 2
+//        System.out.println(prob.maxChunksToSorted(new int[]{2, 1})); // 1
     }
 }
