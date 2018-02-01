9b5236341b4e2f337b716e5b43298ebea2ef65ce
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-01-31 23:38:57 -0600

    Skeleton for problem 771

---
+++ src/main/java/leetcode/Problem771.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/jewels-and-stones/
+ */
+public class Problem771 {
+    public int numJewelsInStones(String J, String S) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem771 prob = new Problem771();
+        System.out.println(prob.numJewelsInStones("aA", "aAAbbbb")); // 3
+        System.out.println(prob.numJewelsInStones("z", "ZZ")); // 0
+    }
+}
