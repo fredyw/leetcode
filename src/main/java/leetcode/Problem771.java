07d4f7eb7c0df07f96d4d79faf5761c57024e5b8
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-02-01 00:12:02 -0600

    Solve problem 771

--- src/main/java/leetcode/Problem771.java
+++ src/main/java/leetcode/Problem771.java
@@ -1,17 +1,27 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * https://leetcode.com/problems/jewels-and-stones/
  */
 public class Problem771 {
     public int numJewelsInStones(String J, String S) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem771 prob = new Problem771();
-        System.out.println(prob.numJewelsInStones("aA", "aAAbbbb")); // 3
-        System.out.println(prob.numJewelsInStones("z", "ZZ")); // 0
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < S.length(); i++) {
+            if (!map.containsKey(S.charAt(i))) {
+                map.put(S.charAt(i), 1);
+            } else {
+                map.put(S.charAt(i), map.get(S.charAt(i)) + 1);
+            }
+        }
+        int result = 0;
+        for (int i = 0; i < J.length(); i++) {
+            if (map.containsKey(J.charAt(i))) {
+                 result += map.get(J.charAt(i));
+            }
+        }
+        return result;
     }
 }
