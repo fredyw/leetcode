47d3cf4e736d24e7ccb761ee9f9a895a422204fd
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-02-03 01:00:24 -0600

    Update problem 767

--- src/main/java/leetcode/Problem767.java
+++ src/main/java/leetcode/Problem767.java
@@ -12,6 +12,8 @@ public class Problem767 {
     public static void main(String[] args) {
         Problem767 prob = new Problem767();
         System.out.println(prob.reorganizeString("aab")); // aba
-        System.out.println(prob.reorganizeString("aaab")); // 
+        System.out.println(prob.reorganizeString("aaab")); //
+        System.out.println(prob.reorganizeString("aaabb")); // ababa
+        System.out.println(prob.reorganizeString("babab")); // ababa
     }
 }
