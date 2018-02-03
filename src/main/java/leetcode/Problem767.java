12bd5d1b5e043553f6ccc6bcf375eabbf6ebadec
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-02-03 00:16:31 -0600

    Skeleton for problem 767

---
+++ src/main/java/leetcode/Problem767.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/reorganize-string/
+ */
+public class Problem767 {
+    public String reorganizeString(String S) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem767 prob = new Problem767();
+        System.out.println(prob.reorganizeString("aab")); // aba
+        System.out.println(prob.reorganizeString("aaab")); // 
+    }
+}
