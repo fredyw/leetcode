322916b2b168c1a5fb8cac11a2a582beb2037891
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-02-04 16:26:53 -0600

    Skeleton for problem 458

---
+++ src/main/java/leetcode/Problem458.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/poor-pigs/
+ */
+public class Problem458 {
+    public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem458 prob = new Problem458();
+        System.out.println(prob.poorPigs(1000, 15, 60)); // 5
+    }
+}
