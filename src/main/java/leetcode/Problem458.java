9c7002a35117694b9e909170d6698b9056faa8a9
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-02-07 01:05:36 -0600

    Solve problem 458

--- src/main/java/leetcode/Problem458.java
+++ src/main/java/leetcode/Problem458.java
@@ -5,12 +5,11 @@ package leetcode;
  */
 public class Problem458 {
     public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem458 prob = new Problem458();
-        System.out.println(prob.poorPigs(1000, 15, 60)); // 5
+        // https://leetcode.com/problems/poor-pigs/discuss/94266/Another-explanation-and-solution
+        int pigs = 0;
+        while (Math.pow((minutesToTest / minutesToDie + 1), pigs) < buckets) {
+            pigs += 1;
+        }
+        return pigs;
     }
 }
