a8078fcd73a22ba0da55164a90b83fe2b3aa2b1d
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-02-08 23:23:56 -0600

    Skeleton for problem 779

---
+++ src/main/java/leetcode/Problem779.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/k-th-symbol-in-grammar/
+ */
+public class Problem779 {
+    public int kthGrammar(int N, int K) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem779 prob = new Problem779();
+        System.out.println(prob.kthGrammar(1, 1)); // 0
+        System.out.println(prob.kthGrammar(2, 1)); // 0
+        System.out.println(prob.kthGrammar(2, 2)); // 1
+        System.out.println(prob.kthGrammar(4, 5)); // 1
+    }
+}
