34acba139a097877df35b80487ae0d8eae8b610a
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-02-11 23:22:50 -0600

    Solve problem 180

--- src/main/sql/Problem180.sql
+++ src/main/sql/Problem180.sql
@@ -1,1 +1,6 @@
 -- https://leetcode.com/problems/consecutive-numbers/description/
+select distinct(s.num) as ConsecutiveNums
+from (select l1.Num as num
+      from Logs l1, Logs l2, Logs l3
+      where l2.id = l1.id + 1 and l1.Num = l2.Num and
+            l3.id = l2.id + 1 and l2.Num = l3.Num) as s;
