e70db6d7e4aa8bafe29d882beb20ac2b1678d7b4
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-02-11 22:50:13 -0600

    Solve problem 178

--- src/main/sql/Problem178.sql
+++ src/main/sql/Problem178.sql
@@ -1,1 +1,6 @@
 -- https://leetcode.com/problems/rank-scores/
+select Score, (select count(distinct Score)
+               from Scores
+               where Score >= s.Score) Rank
+from Scores s
+order by Score desc;
