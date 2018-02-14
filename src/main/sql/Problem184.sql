efc66569f73b2b66a7f0c4ef9adf1911559acb43
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-02-14 02:35:31 -0600

    Solve prloblem 184

--- src/main/sql/Problem184.sql
+++ src/main/sql/Problem184.sql
@@ -1,1 +1,6 @@
 -- https://leetcode.com/problems/department-highest-salary/
+select d.Name as Department, e.Name as Employee, e.Salary as Salary
+from Employee e join Department d on e.DepartmentId = d.Id
+where (e.DepartmentId, e.Salary) in (select e1.DepartmentId, max(e1.Salary)
+                                     from Employee e1
+                                     group by e1.DepartmentId);
