7ab8cedf7f8dbd6298572bc7ef059bb89f5b8705
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-02-16 22:15:52 -0600

    Update problem 781

--- src/main/java/leetcode/Problem781.java
+++ src/main/java/leetcode/Problem781.java
@@ -14,5 +14,16 @@ public class Problem781 {
         System.out.println(prob.numRabbits(new int[]{1, 1, 2})); // 5
         System.out.println(prob.numRabbits(new int[]{10, 10, 10})); // 11
         System.out.println(prob.numRabbits(new int[]{})); // 0
+        System.out.println(prob.numRabbits(new int[]{1, 0, 1, 0, 0})); // 5
+        System.out.println(prob.numRabbits(new int[]{1, 2, 3})); // 9
+        System.out.println(prob.numRabbits(new int[]{1, 2, 2, 3})); // 9
+        System.out.println(prob.numRabbits(new int[]{1, 2, 2, 2, 3})); // 9
+        System.out.println(prob.numRabbits(new int[]{1, 2, 2, 2, 2, 3})); // 12
+        System.out.println(prob.numRabbits(new int[]{1, 2, 2, 2, 2, 2, 3})); // 12
+        System.out.println(prob.numRabbits(new int[]{1, 2, 2, 2, 2, 2, 2, 3})); // 15
+        System.out.println(prob.numRabbits(new int[]{0})); // 1
+        System.out.println(prob.numRabbits(new int[]{0, 0})); // 2
+        System.out.println(prob.numRabbits(new int[]{0, 0, 0})); // 3
+        System.out.println(prob.numRabbits(new int[]{0, 0, 0, 0})); // 4
     }
 }
