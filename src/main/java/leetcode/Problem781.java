fa484123fdb8f44130e8527547403d200f498c33
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-02-16 21:42:00 -0600

    Skeleton for problem 781

---
+++ src/main/java/leetcode/Problem781.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/rabbits-in-forest/
+ */
+public class Problem781 {
+    public int numRabbits(int[] answers) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem781 prob = new Problem781();
+        System.out.println(prob.numRabbits(new int[]{1, 1, 2})); // 5
+        System.out.println(prob.numRabbits(new int[]{10, 10, 10})); // 11
+        System.out.println(prob.numRabbits(new int[]{})); // 0
+    }
+}
