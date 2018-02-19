7ba2c2ef1ed3af12669143a81a9ed4f04284ad93
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-02-19 12:25:30 -0600

    Update problem 140

--- src/main/java/leetcode/Problem140.java
+++ src/main/java/leetcode/Problem140.java
@@ -1,5 +1,6 @@
 package leetcode;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
@@ -15,7 +16,30 @@ public class Problem140 {
         for (String word : wordDict) {
             words.add(word);
         }
-        return null;
+        List<String> result = new ArrayList<>();
+        wordBreak(s, words, 0, 1, "", result);
+        return result;
+    }
+
+    private static void wordBreak(String s, Set<String> words, int i, int j, String accu,
+                                  List<String> result) {
+        if (j > s.length()) {
+            if (i == s.length()) {
+                result.add(accu);
+            }
+            return;
+        }
+        String sub = s.substring(i, j);
+        if (words.contains(sub)) {
+            String newAccu = accu;
+            if (newAccu.isEmpty()) {
+                newAccu = sub;
+            } else {
+                newAccu += " " + sub;
+            }
+            wordBreak(s, words, j, j + 1, newAccu, result);
+        }
+        wordBreak(s, words, i, j + 1, accu, result);
     }
 
     public static void main(String[] args) {
