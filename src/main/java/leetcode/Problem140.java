c6a92489c67223d74e0bcb96a79b8641e3f9a4a1
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-02-19 17:21:44 -0600

    Solve problem 140

--- src/main/java/leetcode/Problem140.java
+++ src/main/java/leetcode/Problem140.java
@@ -1,9 +1,10 @@
 package leetcode;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -15,41 +16,30 @@ public class Problem140 {
         for (String word : wordDict) {
             words.add(word);
         }
+        return wordBreak(s, words, new HashMap<>());
+    }
+
+    private static List<String> wordBreak(String s, Set<String> words, Map<String, List<String>> memo) {
         List<String> result = new ArrayList<>();
-        wordBreak(s, words, 0, 1, new ArrayList<>(), result);
-        return result;
-    }
-
-    private static void wordBreak(String s, Set<String> words, int i, int j, List<String> tmp,
-                                  List<String> result) {
-        if (j > s.length()) {
-            if (i == s.length()) {
-                StringBuilder sb = new StringBuilder();
-                for (int k = 0; k < tmp.size(); k++) {
-                    if (k == 0) {
-                        sb.append(tmp.get(k));
-                    } else {
-                        sb.append(" " + tmp.get(k));
-                    }
+        if (s == null || s.isEmpty()) {
+            return result;
+        }
+        if (memo.containsKey(s)) {
+            return memo.get(s);
+        }
+        if (words.contains(s)) {
+            result.add(s);
+        }
+        for (int i = 1; i < s.length(); i++) {
+            String sub = s.substring(i);
+            if (words.contains(sub)) {
+                List<String> strings = wordBreak(s.substring(0, i), words, memo);
+                for (int j = 0; j < strings.size(); j++) {
+                    result.add(strings.get(j) + " " + sub);
                 }
-                result.add(sb.toString());
             }
-            return;
         }
-        String sub = s.substring(i, j);
-        if (words.contains(sub)) {
-            tmp.add(sub);
-            wordBreak(s, words, j, j + 1, tmp, result);
-            tmp.remove(tmp.size() - 1);
-        }
-        wordBreak(s, words, i, j + 1, tmp, result);
-    }
-
-    public static void main(String[] args) {
-        Problem140 prob = new Problem140();
-        System.out.println(prob.wordBreak("catsanddog",
-            Arrays.asList("cat", "cats", "and", "sand", "dog"))); // ["cats and dog", "cat sand dog"]
-        System.out.println(prob.wordBreak("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-            Arrays.asList("a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa", "aaaaaaaa", "aaaaaaaaa", "aaaaaaaaaa")));
+        memo.put(s, result);
+        return result;
     }
 }
