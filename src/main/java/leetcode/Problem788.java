ff73c29323e7d1f3a010eb9d565a0c06d7c813a1
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-02-26 23:58:42 -0600

    Update problem 788

--- src/main/java/leetcode/Problem788.java
+++ src/main/java/leetcode/Problem788.java
@@ -11,8 +11,8 @@ public class Problem788 {
 
     public static void main(String[] args) {
         Problem788 prob = new Problem788();
-        System.out.println(10); // 4
-        System.out.println(11); // 4
-        System.out.println(12); // 5
+        System.out.println(prob.rotatedDigits(10)); // 4
+        System.out.println(prob.rotatedDigits(11)); // 4
+        System.out.println(prob.rotatedDigits(12)); // 5
     }
 }
