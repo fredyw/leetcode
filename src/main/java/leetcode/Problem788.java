0300527a181c189e153fa050af99131894dda62b
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-02-27 00:14:53 -0600

    Solve problem 788

--- src/main/java/leetcode/Problem788.java
+++ src/main/java/leetcode/Problem788.java
@@ -5,14 +5,33 @@ package leetcode;
  */
 public class Problem788 {
     public int rotatedDigits(int N) {
-        // TODO
-        return 0;
+        int result = 0;
+        for (int i = 1; i <= N; i++) {
+            boolean found = true;
+            boolean rotatedToEachOther = false;
+            for (char c : ("" + i).toCharArray()) {
+                if (canRotate(c)) {
+                    if (isRotatedToEachOther(c)) {
+                        rotatedToEachOther = true;
+                    }
+                    found = true;
+                } else {
+                    found = false;
+                    break;
+                }
+            }
+            if (found && rotatedToEachOther) {
+                result++;
+            }
+        }
+        return result;
     }
 
-    public static void main(String[] args) {
-        Problem788 prob = new Problem788();
-        System.out.println(prob.rotatedDigits(10)); // 4
-        System.out.println(prob.rotatedDigits(11)); // 4
-        System.out.println(prob.rotatedDigits(12)); // 5
+    private static boolean canRotate(char c) {
+        return c == '0' || c == '1' || c == '2' || c == '5' || c == '6' || c == '8' || c == '9';
+    }
+
+    private static boolean isRotatedToEachOther(char c) {
+        return c == '2' || c == '5' || c == '6' || c == '9';
     }
 }
