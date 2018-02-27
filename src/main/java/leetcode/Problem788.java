9f2ee40256e497495e75222fc6d95fec42a4b12d
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-02-26 23:27:31 -0600

    Skeleton for problem 788

---
+++ src/main/java/leetcode/Problem788.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/rotated-digits/
+ */
+public class Problem788 {
+    public int rotatedDigits(int N) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem788 prob = new Problem788();
+        System.out.println(10); // 4
+    }
+}
