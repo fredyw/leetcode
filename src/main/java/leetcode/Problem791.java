a07617367fe280e63d3c9ac7790c102e9251ffae
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-02-28 00:04:04 -0600

    Solve problem 791

--- src/main/java/leetcode/Problem791.java
+++ src/main/java/leetcode/Problem791.java
@@ -5,12 +5,24 @@ package leetcode;
  */
 public class Problem791 {
     public String customSortString(String S, String T) {
-        // TODO
-        return null;
-    }
-
-    public static void main(String[] args) {
-        Problem791 prob = new Problem791();
-        System.out.println(prob.customSortString("cba", "abcd")); // cbad
+        int[] counts = new int[26];
+        for (char c : T.toCharArray()) {
+            counts[c - 'a']++;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (Character c : S.toCharArray()) {
+            for (int i = 0; i < counts[c - 'a']; i++) {
+                sb.append(c);
+            }
+            counts[c - 'a'] = 0;
+        }
+        for (int i = 0; i < counts.length; i++) {
+            if (counts[i] > 0) {
+                for (int j = 0; j < counts[i]; j++) {
+                    sb.append((char) (i + 'a'));
+                }
+            }
+        }
+        return sb.toString();
     }
 }
