3463d1130a2accc68702a2cbbd88a608a25f9141
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-02-27 23:20:55 -0600

    Skeleton for problem 791

---
+++ src/main/java/leetcode/Problem791.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/custom-sort-string/
+ */
+public class Problem791 {
+    public String customSortString(String S, String T) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem791 prob = new Problem791();
+        System.out.println(prob.customSortString("cba", "abcd")); // cbad
+    }
+}
