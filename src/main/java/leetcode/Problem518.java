75aabc7f76d213309878eca168d9e7ef07b2f369
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-03-07 00:32:01 -0600

    Update problem 518

--- src/main/java/leetcode/Problem518.java
+++ src/main/java/leetcode/Problem518.java
@@ -5,14 +5,19 @@ package leetcode;
  */
 public class Problem518 {
     public int change(int amount, int[] coins) {
-        if (amount == 0) {
-            return 1;
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int i = 0; i < coins.length; i++) {
+            for (int j = 1; j <= amount; j++) {
+                int diff = j - coins[i];
+                if (diff >= 0) {
+                    dp[j] += dp[diff];
+                }
+            }
         }
-        if (coins.length == 0) {
-            return 0;
-        }
-        Integer[][] memo = new Integer[amount + 1][coins.length];
-        return coinChange(amount, coins, 0, memo);
+        return dp[amount];
+//        Integer[][] memo = new Integer[amount + 1][coins.length];
+//        return coinChange(amount, coins, 0, memo);
     }
 
     private static int coinChange(int amount, int[] coins, int idx, Integer[][] memo) {
@@ -36,8 +41,8 @@ public class Problem518 {
     public static void main(String[] args) {
         Problem518 prob = new Problem518();
         System.out.println(prob.change(5, new int[]{1, 2, 5})); // 4
-        System.out.println(prob.change(3, new int[]{2})); // 0
-        System.out.println(prob.change(10, new int[]{10})); // 1
-        System.out.println(prob.change(5000, new int[]{1})); // 1
+//        System.out.println(prob.change(3, new int[]{2})); // 0
+//        System.out.println(prob.change(10, new int[]{10})); // 1
+//        System.out.println(prob.change(5000, new int[]{1})); // 1
     }
 }
