87f7ba99d79a4531cd1089677f4076b505e60c9c
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-03-07 00:51:15 -0600

    Solve problem 518

--- src/main/java/leetcode/Problem518.java
+++ src/main/java/leetcode/Problem518.java
@@ -16,33 +16,5 @@ public class Problem518 {
             }
         }
         return dp[amount];
-//        Integer[][] memo = new Integer[amount + 1][coins.length];
-//        return coinChange(amount, coins, 0, memo);
-    }
-
-    private static int coinChange(int amount, int[] coins, int idx, Integer[][] memo) {
-        if (amount <= 0) {
-            if (amount == 0) {
-                return 1;
-            }
-            return 0;
-        }
-        if (memo[amount][idx] != null) {
-            return memo[amount][idx];
-        }
-        int count = 0;
-        for (int i = idx; i < coins.length; i++) {
-            count += coinChange(amount - coins[i], coins, i, memo);
-        }
-        memo[amount][idx] = count;
-        return count;
-    }
-
-    public static void main(String[] args) {
-        Problem518 prob = new Problem518();
-        System.out.println(prob.change(5, new int[]{1, 2, 5})); // 4
-//        System.out.println(prob.change(3, new int[]{2})); // 0
-//        System.out.println(prob.change(10, new int[]{10})); // 1
-//        System.out.println(prob.change(5000, new int[]{1})); // 1
     }
 }
