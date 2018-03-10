15576dd4be8eea20706efe1b28cf45382ff2566c
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-03-10 10:38:50 -0600

    Update problem 795

--- src/main/java/leetcode/Problem795.java
+++ src/main/java/leetcode/Problem795.java
@@ -5,12 +5,23 @@ package leetcode;
  */
 public class Problem795 {
     public int numSubarrayBoundedMax(int[] A, int L, int R) {
-        // TODO
-        return 0;
+        int count = 0;
+        int max = -1;
+        for (int i = 0; i < A.length; i++) {
+            max = Math.max(max, A[i]);
+            if (L <= max && max <= R) {
+                count++;
+            } else {
+                max = -1;
+            }
+        }
+        return count;
     }
 
     public static void main(String[] args) {
         Problem795 prob = new Problem795();
         System.out.println(prob.numSubarrayBoundedMax(new int[]{2, 1, 4, 3}, 2, 3)); // 3
+        System.out.println(prob.numSubarrayBoundedMax(new int[]{1, 1, 1, 1}, 2, 3)); // 0
+        System.out.println(prob.numSubarrayBoundedMax(new int[]{1, 1, 2, 1}, 2, 3)); // 6
     }
 }
