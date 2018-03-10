85d9a06116c193e729c3dde27e00ec022955ee7b
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-03-10 10:21:25 -0600

    Skeleton for problem 795

---
+++ src/main/java/leetcode/Problem795.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/number-of-subarrays-with-bounded-maximum/
+ */
+public class Problem795 {
+    public int numSubarrayBoundedMax(int[] A, int L, int R) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem795 prob = new Problem795();
+        System.out.println(prob.numSubarrayBoundedMax(new int[]{2, 1, 4, 3}, 2, 3)); // 3
+    }
+}
