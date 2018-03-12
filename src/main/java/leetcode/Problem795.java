d4095ca57983179f25b18481e982413bb4070b72
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-03-11 21:26:40 -0500

    Update problem 795

--- src/main/java/leetcode/Problem795.java
+++ src/main/java/leetcode/Problem795.java
@@ -7,13 +7,19 @@ public class Problem795 {
     public int numSubarrayBoundedMax(int[] A, int L, int R) {
         int result = 0;
         int count = 0;
-        int max = -1;
+        boolean found = false;
         for (int i = 0; i < A.length; i++) {
-            max = Math.max(max, A[i]);
-            if (L <= max && max <= R) {
+            if (L <= A[i] && A[i] <= R) {
+                found = true;
                 count++;
+                result += count;
+            } else if (A[i] > R) {
+                count = 0;
             } else {
-                max = -1;
+                count++;
+                if (found) {
+                    result += count - 1;
+                }
             }
         }
         return result;
@@ -21,15 +27,16 @@ public class Problem795 {
 
     public static void main(String[] args) {
         Problem795 prob = new Problem795();
-        System.out.println(prob.numSubarrayBoundedMax(new int[]{2, 1, 4, 3}, 2, 3)); // 3
-        System.out.println(prob.numSubarrayBoundedMax(new int[]{1, 1, 1, 1}, 2, 3)); // 0
-        System.out.println(prob.numSubarrayBoundedMax(new int[]{1, 1, 2, 1}, 2, 3)); // 6
-        System.out.println(prob.numSubarrayBoundedMax(new int[]{1, 2, 1}, 2, 3)); // 4
-        System.out.println(prob.numSubarrayBoundedMax(new int[]{1, 2, 3, 1}, 2, 3)); // 8
+//        System.out.println(prob.numSubarrayBoundedMax(new int[]{2, 1, 4, 3}, 2, 3)); // 3
+//        System.out.println(prob.numSubarrayBoundedMax(new int[]{1, 1, 1, 1}, 2, 3)); // 0
+//        System.out.println(prob.numSubarrayBoundedMax(new int[]{1, 1, 2, 1}, 2, 3)); // 6
+//        System.out.println(prob.numSubarrayBoundedMax(new int[]{1, 2, 1}, 2, 3)); // 4
+//        System.out.println(prob.numSubarrayBoundedMax(new int[]{1, 2, 3, 1}, 2, 3)); // 8
         System.out.println(prob.numSubarrayBoundedMax(new int[]{1, 1, 2, 1, 1}, 2, 3)); // 9
-        System.out.println(prob.numSubarrayBoundedMax(new int[]{1, 3, 2, 1, 1}, 2, 3)); // 11
-        System.out.println(prob.numSubarrayBoundedMax(new int[]{1, 3, 2, 3, 1}, 2, 3)); // 13
-        System.out.println(prob.numSubarrayBoundedMax(new int[]{1, 1, 2, 1, 1, 4, 1, 2, 1}, 2, 3)); // 13
-        System.out.println(prob.numSubarrayBoundedMax(new int[]{1, 1, 2, 3, 1, 4, 1, 2, 1}, 2, 3)); // 15
+//        System.out.println(prob.numSubarrayBoundedMax(new int[]{1, 1, 2, 1, 1, 1}, 2, 3)); // 12
+//        System.out.println(prob.numSubarrayBoundedMax(new int[]{1, 3, 2, 1, 1}, 2, 3)); // 11
+//        System.out.println(prob.numSubarrayBoundedMax(new int[]{1, 3, 2, 3, 1}, 2, 3)); // 13
+//        System.out.println(prob.numSubarrayBoundedMax(new int[]{1, 1, 2, 1, 1, 4, 1, 2, 1}, 2, 3)); // 13
+//        System.out.println(prob.numSubarrayBoundedMax(new int[]{1, 1, 2, 3, 1, 4, 1, 2, 1}, 2, 3)); // 15
     }
 }
