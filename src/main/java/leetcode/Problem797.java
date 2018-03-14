e672e79c26f77dddadce1fb0be3d8e021fab2eaa
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-03-14 02:29:18 -0500

    Solve problem 797

--- src/main/java/leetcode/Problem797.java
+++ src/main/java/leetcode/Problem797.java
@@ -1,30 +1,28 @@
 package leetcode;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 
 /**
  * https://leetcode.com/problems/all-paths-from-source-to-target/
  */
 public class Problem797 {
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        // TODO
         List<List<Integer>> result = new ArrayList<>();
-        boolean[] visited = new boolean[graph.length];
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(0);
-        while (!queue.isEmpty()) {
-            Integer current = queue.remove();
-            visited[current] = true;
-            for (int neighbor : graph[current]) {
-                if (!visited[neighbor]) {
-                    visited[neighbor] = true;
-                }
+        allPaths(graph, 0, new ArrayList<>(), result);
+        return result;
+    }
+
+    private static void allPaths(int[][] graph, int source, List<Integer> accu,
+                                 List<List<Integer>> result) {
+        accu.add(source);
+        if (graph[source].length == 0) {
+            result.add(accu);
+        } else {
+            for (int neighbor : graph[source]) {
+                allPaths(graph, neighbor, new ArrayList<>(accu), result);
             }
         }
-        return result;
     }
 
     public static void main(String[] args) {
