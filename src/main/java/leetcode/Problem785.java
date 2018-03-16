c22317af6606e8d2dd254a551b39befeb9abc689
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-03-16 00:36:12 -0500

    Skeleton for problem 785

---
+++ src/main/java/leetcode/Problem785.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/is-graph-bipartite/
+ */
+public class Problem785 {
+    public boolean isBipartite(int[][] graph) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem785 prob = new Problem785();
+    }
+}
