de188e8d0ccc65d2a15b26e1d6044157979e0dfb
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-03-17 10:37:13 -0500

    Solve problem 785

--- src/main/java/leetcode/Problem785.java
+++ src/main/java/leetcode/Problem785.java
@@ -5,8 +5,35 @@ package leetcode;
  */
 public class Problem785 {
     public boolean isBipartite(int[][] graph) {
-        // TODO
-        return false;
+        boolean[] visited = new boolean[graph.length];
+        boolean[] map = new boolean[graph.length];
+        Bipartite bipartite = new Bipartite();
+        for (int vertex = 0; vertex < graph.length; vertex++) {
+            if (!visited[vertex]) {
+                isBipartite(graph, vertex, visited, map, bipartite);
+            }
+        }
+        return bipartite.isBipartite;
+    }
+
+    private static class Bipartite {
+        private boolean isBipartite = true;
+    }
+
+    private static void isBipartite(int[][] graph, int source,
+                                    boolean[] visited, boolean[] map,
+                                    Bipartite bipartite) {
+        visited[source] = true;
+        for (int adj : graph[source]) {
+            if (!visited[adj]) {
+                map[adj] = !map[source];
+                isBipartite(graph, adj, visited, map, bipartite);
+            } else {
+                if (map[source] == map[adj]) {
+                    bipartite.isBipartite = false;
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
