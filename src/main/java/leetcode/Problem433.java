3da5205a4a8031b9a8d8569c1a46e3ca3461cf47
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-03-19 20:14:38 -0500

    Update problem 433

--- src/main/java/leetcode/Problem433.java
+++ src/main/java/leetcode/Problem433.java
@@ -11,8 +11,8 @@ public class Problem433 {
 
     public static void main(String[] args) {
         Problem433 prob = new Problem433();
-        prob.minMutation("AACCGGTT", "AACCGGTA", new String[]{"AACCGGTA"}); // 1
-        prob.minMutation("AACCGGTT", "AAACGGTA", new String[]{"AACCGGTA", "AACCGCTA", "AAACGGTA"}); // 2
-        prob.minMutation("AAAAACCC", "AACCCCCC", new String[]{"AAAACCCC", "AAACCCCC", "AACCCCCC"}); // 3
+        System.out.println(prob.minMutation("AACCGGTT", "AACCGGTA", new String[]{"AACCGGTA"})); // 1
+        System.out.println(prob.minMutation("AACCGGTT", "AAACGGTA", new String[]{"AACCGGTA", "AACCGCTA", "AAACGGTA"})); // 2
+        System.out.println(prob.minMutation("AAAAACCC", "AACCCCCC", new String[]{"AAAACCCC", "AAACCCCC", "AACCCCCC"})); // 3
     }
 }
