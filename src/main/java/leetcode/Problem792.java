e6e46c2c0237d6e9b195de043b64717e59ff1360
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-03-21 00:21:28 -0500

    Update problem 792

--- src/main/java/leetcode/Problem792.java
+++ src/main/java/leetcode/Problem792.java
@@ -5,8 +5,27 @@ package leetcode;
  */
 public class Problem792 {
     public int numMatchingSubseq(String S, String[] words) {
-        // TODO
-        return 0;
+        int result = 0;
+        for (String word : words) {
+            int i = 0;
+            int j = 0;
+            int count = 0;
+            for (; i < word.length(); i++) {
+                while (j < S.length()) {
+                    if (word.charAt(i) == S.charAt(j++)) {
+                        count++;
+                        break;
+                    }
+                }
+                if (j == S.length()) {
+                    break;
+                }
+            }
+            if (count == word.length()) {
+                result++;
+            }
+        }
+        return result;
     }
 
     public static void main(String[] args) {
