e79c865e64070f34a4b23dfb097875f5afb8b0f6
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-03-20 21:06:36 -0500

    Skeleton for problem 792

---
+++ src/main/java/leetcode/Problem792.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/number-of-matching-subsequences/
+ */
+public class Problem792 {
+    public int numMatchingSubseq(String S, String[] words) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
