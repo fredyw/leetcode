2b9e786e7d6a86f4d3e1ec98fde4cb767d193fde
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-03-23 16:01:59 -0500

    Update problem 792

--- src/main/java/leetcode/Problem792.java
+++ src/main/java/leetcode/Problem792.java
@@ -1,30 +1,55 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * https://leetcode.com/problems/number-of-matching-subsequences/
  */
 public class Problem792 {
     public int numMatchingSubseq(String S, String[] words) {
         int result = 0;
+        Map<Character, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < S.length(); i++) {
+            char c = S.charAt(i);
+            if (!map.containsKey(c)) {
+                List<Integer> list = new ArrayList<>();
+                list.add(i);
+                map.put(c, list);
+            } else {
+                map.get(c).add(i);
+            }
+        }
+
         for (String word : words) {
-            int i = 0;
-            int j = 0;
-            int count = 0;
-            for (; i < word.length(); i++) {
-                while (j < S.length()) {
-                    if (word.charAt(i) == S.charAt(j++)) {
-                        count++;
+            int lastIndex = 0;
+            boolean found = true;
+            for (int i = 0; i < word.length(); i++) {
+                char c = word.charAt(i);
+                if (!map.containsKey(c)) {
+                    found = false;
+                    break;
+                }
+                List<Integer> indices = map.get(c);
+                int index = Collections.binarySearch(indices, lastIndex);
+                if (index < 0) {
+                    index = (-(index) - 1);
+                    if (index >= S.length()) {
+                        found = false;
                         break;
                     }
                 }
-                if (j == S.length()) {
-                    break;
-                }
+                lastIndex = index + 1;
             }
-            if (count == word.length()) {
+
+            if (found) {
                 result++;
             }
         }
+
         return result;
     }
 
