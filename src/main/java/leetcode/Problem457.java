87d69bc4fbceadf467045df201ce8805c20ba0f4
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-03-25 10:25:42 -0500

    Skeleton for problem 457

---
+++ src/main/java/leetcode/Problem457.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/circular-array-loop/
+ */
+public class Problem457 {
+    public boolean circularArrayLoop(int[] nums) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem457 prob = new Problem457();
+        System.out.println(prob.circularArrayLoop(new int[]{2, -1, 1, 2, 2})); // true
+        System.out.println(prob.circularArrayLoop(new int[]{-1, 2})); // false
+    }
+}
