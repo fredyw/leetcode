06d7a4ef230e6c849697313c439932fe0d7d0e00
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-03-27 00:17:27 -0500

    Solve probem 804

--- src/main/java/leetcode/Problem804.java
+++ src/main/java/leetcode/Problem804.java
@@ -1,16 +1,25 @@
 package leetcode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * https://leetcode.com/problems/unique-morse-code-words/
  */
 public class Problem804 {
     public int uniqueMorseRepresentations(String[] words) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem804 prob = new Problem804();
-        System.out.println(prob.uniqueMorseRepresentations(new String[]{"gin", "zen", "gig", "msg"})); // 2
+        String[] morse = new String[]{
+            ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..",
+            "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-",
+            "-.--", "--.."};
+        Set<String> set = new HashSet<>();
+        for (String word : words) {
+            StringBuilder sb = new StringBuilder();
+            for (char c : word.toCharArray()) {
+                sb.append(morse[c - 'a']);
+            }
+            set.add(sb.toString());
+        }
+        return set.size();
     }
 }
