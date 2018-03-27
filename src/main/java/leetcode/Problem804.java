704bdab5a9f96b40f224413c0d4bd71a744d5ffe
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-03-26 23:41:27 -0500

    Skeleton for problem 804

---
+++ src/main/java/leetcode/Problem804.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/unique-morse-code-words/
+ */
+public class Problem804 {
+    public int uniqueMorseRepresentations(String[] words) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem804 prob = new Problem804();
+        System.out.println(prob.uniqueMorseRepresentations(new String[]{"gin", "zen", "gig", "msg"})); // 2
+    }
+}
