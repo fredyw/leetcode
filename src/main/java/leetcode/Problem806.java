c93e9364981fb963ee2167d145dc00eafbe6412f
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-03-28 00:32:59 -0500

    Update problem 806

--- src/main/java/leetcode/Problem806.java
+++ src/main/java/leetcode/Problem806.java
@@ -1,7 +1,5 @@
 package leetcode;
 
-import java.util.Arrays;
-
 /**
  * https://leetcode.com/problems/number-of-lines-to-write-string/
  */
@@ -20,16 +18,5 @@ public class Problem806 {
         }
         result[1] = count;
         return result;
-    }
-
-    public static void main(String[] args) {
-        Problem806 prob = new Problem806();
-        System.out.println(Arrays.toString(prob.numberOfLines(new int[]{
-                10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10},
-            "abcdefghijklmnopqrstuvwxyz"))); // [3, 60]
-        System.out.println(Arrays.toString(prob.numberOfLines(new int[]{
-                4, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10},
-            "bbbcccdddaaa"
-        ))); // [2, 4]
     }
 }
