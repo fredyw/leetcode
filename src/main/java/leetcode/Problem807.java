a9de21d47c0ccc5aebb8a328a93a80c399a35430
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-03-28 17:03:57 -0500

    Solve problem 807

--- src/main/java/leetcode/Problem807.java
+++ src/main/java/leetcode/Problem807.java
@@ -5,14 +5,31 @@ package leetcode;
  */
 public class Problem807 {
     public int maxIncreaseKeepingSkyline(int[][] grid) {
-        // TODO
-        return 0;
-    }
+        int nRow = grid.length;
+        int nCol = 0;
+        if (grid.length > 0) {
+            nCol = grid[0].length;
+        }
+        int[] maxRows = new int[nRow];
+        int[] maxCols = new int[nCol];
 
-    public static void main(String[] args) {
-        Problem807 prob = new Problem807();
-        System.out.println(prob.maxIncreaseKeepingSkyline(new int[][]{
-            {3, 0, 8, 4}, {2, 4, 5, 7}, {9, 2, 6, 3}, {0, 3, 1, 0}
-        })); // 35
+        for (int row = 0; row < nRow; row++) {
+            for (int col = 0; col < nCol; col++) {
+                maxRows[row] = Math.max(maxRows[row], grid[row][col]);
+                maxCols[col] = Math.max(maxCols[col], grid[row][col]);
+            }
+        }
+
+        int result = 0;
+        for (int row = 0; row < nRow; row++) {
+            for (int col = 0; col < nCol; col++) {
+                int max = Math.max(maxRows[row], maxCols[col]);
+                int min = Math.min(maxRows[row], maxCols[col]);
+                if (grid[row][col] < max) {
+                    result += min - grid[row][col];
+                }
+            }
+        }
+        return result;
     }
 }
