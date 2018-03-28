e3674c4d80f411c4e06499b94e28b39f1f1badb4
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-03-28 13:18:30 -0500

    Skeleton for problem 807

---
+++ src/main/java/leetcode/Problem807.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/max-increase-to-keep-city-skyline/
+ */
+public class Problem807 {
+    public int maxIncreaseKeepingSkyline(int[][] grid) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem807 prob = new Problem807();
+        System.out.println(prob.maxIncreaseKeepingSkyline(new int[][]{
+            {3, 0, 8, 4}, {2, 4, 5, 7}, {9, 2, 6, 3}, {0, 3, 1, 0}
+        })); // 35
+    }
+}
