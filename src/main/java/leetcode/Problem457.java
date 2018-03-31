cae40a58bff72e3e45ddd4e47c7cd05b6e2dbd65
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-03-31 10:58:14 -0500

    Update problem 457

--- src/main/java/leetcode/Problem457.java
+++ src/main/java/leetcode/Problem457.java
@@ -5,20 +5,40 @@ package leetcode;
  */
 public class Problem457 {
     public boolean circularArrayLoop(int[] nums) {
-        // TODO
+        boolean[] visited = new boolean[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            if (visited[i]) {
+                continue;
+            }
+            visited[i] = true;
+            int start = i;
+            int j = i + 1;
+            boolean found = true;
+            while (j != start) {
+                if (nums[i] < 0 != nums[j] < 0) {
+                    found = false;
+                }
+                visited[j] = true;
+                j++;
+                j %= nums.length;
+            }
+            if (found) {
+                return true;
+            }
+        }
         return false;
     }
 
     public static void main(String[] args) {
         Problem457 prob = new Problem457();
         System.out.println(prob.circularArrayLoop(new int[]{2, -1, 1, 2, 2})); // true
-        System.out.println(prob.circularArrayLoop(new int[]{2, -1, 1, 1, 2})); // false
-        System.out.println(prob.circularArrayLoop(new int[]{-1, 2})); // false
-        System.out.println(prob.circularArrayLoop(new int[]{-1, 1})); // false
-        System.out.println(prob.circularArrayLoop(new int[]{-1, -2})); // false
-        System.out.println(prob.circularArrayLoop(new int[]{-1, -1, -2})); // true
-        System.out.println(prob.circularArrayLoop(new int[]{1, -1})); // false
-        System.out.println(prob.circularArrayLoop(new int[]{3, 1, 2})); // true
-        System.out.println(prob.circularArrayLoop(new int[]{2, 1, -1, -2, -2})); // false
+//        System.out.println(prob.circularArrayLoop(new int[]{2, -1, 1, 1, 2})); // false
+//        System.out.println(prob.circularArrayLoop(new int[]{-1, 2})); // false
+//        System.out.println(prob.circularArrayLoop(new int[]{-1, 1})); // false
+//        System.out.println(prob.circularArrayLoop(new int[]{-1, -2})); // false
+//        System.out.println(prob.circularArrayLoop(new int[]{-1, -1, -2})); // true
+//        System.out.println(prob.circularArrayLoop(new int[]{1, -1})); // false
+//        System.out.println(prob.circularArrayLoop(new int[]{3, 1, 2})); // true
+//        System.out.println(prob.circularArrayLoop(new int[]{2, 1, -1, -2, -2})); // false
     }
 }
