815acae2788f9f2babbe141ef2fa1cccc037ed57
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-04-01 23:20:59 -0500

    Skeleton for problem 811

---
+++ src/main/java/leetcode/Problem811.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/subdomain-visit-count/description/
+ */
+public class Problem811 {
+    public List<String> subdomainVisits(String[] cpdomains) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
