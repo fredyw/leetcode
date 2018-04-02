b8cb902117b8d42bb13f618df79ff0074cdbb093
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-04-02 00:02:43 -0500

    Update problem 811

--- src/main/java/leetcode/Problem811.java
+++ src/main/java/leetcode/Problem811.java
@@ -12,6 +12,9 @@ public class Problem811 {
     }
 
     public static void main(String[] args) {
+        Problem811 prob = new Problem811();
+        System.out.println(prob.subdomainVisits(new String[]{"9001 discuss.leetcode.com"})); // ["9001 discuss.leetcode.com", "9001 leetcode.com", "9001 com"]
+        System.out.println(prob.subdomainVisits(new String[]{"900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org"})); // ["901 mail.com","50 yahoo.com","900 google.mail.com","5 wiki.org","5 org","1 intel.mail.com","951 com"]
 
     }
 }
