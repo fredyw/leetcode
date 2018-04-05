d286678fa3e4bcf78d3e87463fcc83fe49dc430b
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-04-04 23:05:13 -0500

    Skeleton for problem 754

---
+++ src/main/java/leetcode/Problem754.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/reach-a-number/
+ */
+public class Problem754 {
+    public int reachNumber(int target) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem754 prob = new Problem754();
+        System.out.println(prob.reachNumber(3)); // 2
+        System.out.println(prob.reachNumber(2)); // 3
+    }
+}
