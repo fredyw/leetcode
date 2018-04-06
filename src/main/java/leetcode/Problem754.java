a88eba98f6b6d5897188cc7b64fa1600bfc6ce8c
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-04-06 14:00:44 -0500

    Update problem 754

--- src/main/java/leetcode/Problem754.java
+++ src/main/java/leetcode/Problem754.java
@@ -5,12 +5,17 @@ package leetcode;
  */
 public class Problem754 {
     public int reachNumber(int target) {
-        // TODO
-        return 0;
+        target = Math.abs(target);
+        int k = 0;
+        while (target > 0) {
+            target -= ++k;
+        }
+        return target % 2 == 0 ? k : k + 1 + k % 2;
     }
 
     public static void main(String[] args) {
         Problem754 prob = new Problem754();
+        System.out.println(prob.reachNumber(0)); // 0
         System.out.println(prob.reachNumber(1)); // 1
         System.out.println(prob.reachNumber(2)); // 3
         System.out.println(prob.reachNumber(3)); // 2
