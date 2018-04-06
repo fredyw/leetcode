cabe81092b4bcbc8d6630e1240cc65d86b607dd5
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-04-05 23:59:48 -0500

    Update problem 754

--- src/main/java/leetcode/Problem754.java
+++ src/main/java/leetcode/Problem754.java
@@ -11,7 +11,14 @@ public class Problem754 {
 
     public static void main(String[] args) {
         Problem754 prob = new Problem754();
-        System.out.println(prob.reachNumber(3)); // 2
+        System.out.println(prob.reachNumber(1)); // 1
         System.out.println(prob.reachNumber(2)); // 3
+        System.out.println(prob.reachNumber(3)); // 2
+        System.out.println(prob.reachNumber(4)); // 3
+        System.out.println(prob.reachNumber(5)); // 5
+        System.out.println(prob.reachNumber(6)); // 3
+        System.out.println(prob.reachNumber(7)); // 5
+        System.out.println(prob.reachNumber(8)); // 4
+        System.out.println(prob.reachNumber(9)); // 5
     }
 }
