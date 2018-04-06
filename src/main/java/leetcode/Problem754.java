6735837e0cc164fd5f4c8ae25b0915ec79fc28f8
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-04-06 18:34:23 -0500

    Solve problem 754

--- src/main/java/leetcode/Problem754.java
+++ src/main/java/leetcode/Problem754.java
@@ -12,18 +12,4 @@ public class Problem754 {
         }
         return target % 2 == 0 ? k : k + 1 + k % 2;
     }
-
-    public static void main(String[] args) {
-        Problem754 prob = new Problem754();
-        System.out.println(prob.reachNumber(0)); // 0
-        System.out.println(prob.reachNumber(1)); // 1
-        System.out.println(prob.reachNumber(2)); // 3
-        System.out.println(prob.reachNumber(3)); // 2
-        System.out.println(prob.reachNumber(4)); // 3
-        System.out.println(prob.reachNumber(5)); // 5
-        System.out.println(prob.reachNumber(6)); // 3
-        System.out.println(prob.reachNumber(7)); // 5
-        System.out.println(prob.reachNumber(8)); // 4
-        System.out.println(prob.reachNumber(9)); // 5
-    }
 }
