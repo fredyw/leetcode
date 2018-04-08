434300a0c4f8623dcfdfec2c6afd0f95f270d6ae
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-04-08 13:36:31 -0500

    Solve problem 812

--- src/main/java/leetcode/Problem812.java
+++ src/main/java/leetcode/Problem812.java
@@ -5,14 +5,30 @@ package leetcode;
  */
 public class Problem812 {
     public double largestTriangleArea(int[][] points) {
-        // TODO
-        return 0;
+        // https://en.wikipedia.org/wiki/Heron%27s_formula
+        double maxArea = 0;
+        for (int i = 0; i < points.length; i++) {
+            for (int j = 0; j < points.length; j++) {
+                if (j == i) {
+                    continue;
+                }
+                for (int k = 0; k < points.length; k++) {
+                    if (k == i || k == j) {
+                        continue;
+                    }
+                    double a = distance(points[i], points[j]);
+                    double b = distance(points[i], points[k]);
+                    double c = distance(points[j], points[k]);
+                    double s = (a + b + c) / 2;
+                    double area = Math.sqrt(s * Math.abs(s - a) * Math.abs(s - b) * Math.abs(s - c));
+                    maxArea = Math.max(maxArea, area);
+                }
+            }
+        }
+        return maxArea;
     }
 
-    public static void main(String[] args) {
-        Problem812 prob = new Problem812();
-        System.out.println(prob.largestTriangleArea(new int[][]{
-            {0, 0}, {0, 1}, {1, 0}, {0, 2}, {2, 0}
-        })); // 2
+    private static double distance(int[] x, int[] y) {
+        return Math.sqrt(Math.pow(x[0] - y[0], 2) + Math.pow(x[1] - y[1], 2));
     }
 }
