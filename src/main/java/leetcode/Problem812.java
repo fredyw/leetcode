5f7146ab544b2ab1df18864813b3c20cd5fcbfec
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-04-08 12:32:07 -0500

    Sketon for problem 812

---
+++ src/main/java/leetcode/Problem812.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/largest-triangle-area/
+ */
+public class Problem812 {
+    public double largestTriangleArea(int[][] points) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem812 prob = new Problem812();
+        System.out.println(prob.largestTriangleArea(new int[][]{
+            {0, 0}, {0, 1}, {1, 0}, {0, 2}, {2, 0}
+        })); // 2
+    }
+}
