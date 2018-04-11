61c6e01c9533a505b71c7fac232d216b5fe012d3
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-04-10 21:56:06 -0500

    Skeleton for problem 814

---
+++ src/main/java/leetcode/Problem814.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/binary-tree-pruning/
+ */
+public class Problem814 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode pruneTree(TreeNode root) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem814 prob = new Problem814();
+    }
+}
