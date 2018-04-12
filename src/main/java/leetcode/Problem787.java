6bb7fb9dcd4358b46e5083167ed4eced89cc2571
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-04-11 23:07:36 -0500

    Skeleton for problem 787

---
+++ src/main/java/leetcode/Problem787.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/cheapest-flights-within-k-stops/
+ */
+public class Problem787 {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int K) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem787 prob = new Problem787();
+    }
+}
