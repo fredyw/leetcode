3734e589fa073204ff9d3292c9a13092a806ebe4
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-04-13 00:23:07 -0500

    Update problem 813

--- src/main/java/leetcode/Problem813.java
+++ src/main/java/leetcode/Problem813.java
@@ -11,5 +11,7 @@ public class Problem813 {
 
     public static void main(String[] args) {
         Problem813 prob = new Problem813();
+        System.out.println(prob.largestSumOfAverages(new int[]{9, 1, 2, 3, 9}, 5)); // 20
+        System.out.println(prob.largestSumOfAverages(new int[]{1, 2, 3, 4, 5, 6, 7}, 4)); // 20.5
     }
 }
