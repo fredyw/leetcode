a21799c367c284dfeba1015aa6b6e2f5966dc746
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-04-12 23:15:48 -0500

    Skeleton for problem 813

---
+++ src/main/java/leetcode/Problem813.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/largest-sum-of-averages/
+ */
+public class Problem813 {
+    public double largestSumOfAverages(int[] A, int K) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem813 prob = new Problem813();
+    }
+}
