ab6adf92f2fc1590f2a1dda71f26a0dbae40d242
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-04-15 10:41:36 -0500

    Skeleton for problem 819

---
+++ src/main/java/leetcode/Problem819.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/most-common-word/
+ */
+public class Problem819 {
+    public String mostCommonWord(String paragraph, String[] banned) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem819 prob = new Problem819();
+        System.out.println(prob.mostCommonWord(
+            "Bob hit a ball, the hit BALL flew far after it was hit.",
+            new String[]{"hit"})); // ball
+    }
+}
