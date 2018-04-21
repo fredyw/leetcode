d4989ac33a42c6ab25e4573f6607a75cb16c94f1
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-04-21 15:35:12 -0500

    Update problem 787

--- src/main/java/leetcode/Problem787.java
+++ src/main/java/leetcode/Problem787.java
@@ -11,5 +11,8 @@ public class Problem787 {
 
     public static void main(String[] args) {
         Problem787 prob = new Problem787();
+        System.out.println(prob.findCheapestPrice(3, new int[][]{
+            {0, 1, 100}, {1, 2, 100}, {0, 2, 500}
+        }, 0, 2, 1)); // 200
     }
 }
