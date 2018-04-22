eaec38c3c85be1a10c9150ffb056c4fb41d36afe
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-04-22 14:56:38 -0500

    Update problem 787

--- src/main/java/leetcode/Problem787.java
+++ src/main/java/leetcode/Problem787.java
@@ -1,9 +1,7 @@
 package leetcode;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 
 /**
  * https://leetcode.com/problems/cheapest-flights-within-k-stops/
@@ -11,38 +9,34 @@ import java.util.Queue;
 public class Problem787 {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int K) {
         List<Edge>[] graph = buildGraph(flights);
-        Queue<NodeCount> queue = new LinkedList<>();
-        queue.add(new NodeCount(src, -1, 0));
-        int min = Integer.MAX_VALUE;
-        while (!queue.isEmpty()) {
-            NodeCount current = queue.remove();
-            if (current.node == dst) {
-                min = Math.min(min, current.weight);
-            }
-            List<Edge> neighbors = graph[current.node];
-            if (neighbors == null) {
-                continue;
-            }
-            for (Edge edge : neighbors) {
-                if (current.count + 1 <= K) {
-                    queue.add(new NodeCount(edge.to, current.count + 1,
-                        current.weight + edge.weight));
-                }
-            }
-        }
-        return min == Integer.MAX_VALUE ? -1 : min;
+        int price = findCheapestPrice(graph, src, dst, K, new Integer[100]);
+        return price == Integer.MAX_VALUE ? - 1 : price;
     }
 
-    private static class NodeCount {
-        private final int node;
-        private final int count;
-        private final int weight;
-
-        public NodeCount(int node, int count, int weight) {
-            this.node = node;
-            this.count = count;
-            this.weight = weight;
+    private static int findCheapestPrice(List<Edge>[] graph, int src, int dst, int k,
+                                         Integer[] memo) {
+        if (src == dst && k >= -1) {
+            return 0;
         }
+        if (k < -1) {
+            return Integer.MAX_VALUE;
+        }
+        if (memo[src] != null) {
+            return memo[src];
+        }
+        List<Edge> neighbors = graph[src];
+        if (neighbors == null) {
+            return Integer.MAX_VALUE;
+        }
+        int min = Integer.MAX_VALUE;
+        for (Edge neighbor : neighbors) {
+            int price = findCheapestPrice(graph, neighbor.to, dst, k - 1, memo);
+            if (price != Integer.MAX_VALUE) {
+                min = Math.min(min, price + neighbor.price);
+            }
+        }
+        memo[src] = min;
+        return min;
     }
 
     private static List<Edge>[] buildGraph(int[][] flights) {
@@ -64,11 +58,11 @@ public class Problem787 {
 
     private static class Edge {
         private final int to;
-        private final int weight;
+        private final int price;
 
-        public Edge(int to, int weight) {
+        public Edge(int to, int price) {
             this.to = to;
-            this.weight = weight;
+            this.price = price;
         }
     }
 
