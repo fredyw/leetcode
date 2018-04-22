8c67f348d008cdcc207c13a6c388022009cf7331
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-04-22 15:37:38 -0500

    Skeleton for problem 821

---
+++ src/main/java/leetcode/Problem821.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/shortest-distance-to-a-character/
+ */
+public class Problem821 {
+    public int[] shortestToChar(String S, char C) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem821 prob = new Problem821();
+        System.out.println(prob.shortestToChar("loveleetcode", 'e')); // [3, 2, 1, 0, 1, 0, 0, 1, 2, 2, 1, 0]
+    }
+}
