8994e283419d1d1411c01055a6e9863e6f120f04
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-04-24 00:54:12 -0500

    Solve problem 817

--- src/main/java/leetcode/Problem817.java
+++ src/main/java/leetcode/Problem817.java
@@ -14,11 +14,37 @@ public class Problem817 {
     }
 
     public int numComponents(ListNode head, int[] G) {
-        // TODO
-        return 0;
+        boolean[] nodes = new boolean[10000];
+        for (int g : G) {
+            nodes[g] = true;
+        }
+        int result = 0;
+        boolean found = false;
+        for (ListNode node = head; node != null; node = node.next) {
+            if (nodes[node.val]) {
+                if (!found) {
+                    result++;
+                }
+                found = true;
+            } else {
+                found = false;
+            }
+        }
+        return result;
     }
 
-    public static void main(String[] args) {
-        Problem817 prob = new Problem817();
+    private static ListNode build(int[] nodes) {
+        ListNode head = null;
+        ListNode node = null;
+        for (int i : nodes) {
+            if (head == null) {
+                head = new ListNode(i);
+                node = head;
+            } else {
+                node.next = new ListNode(i);
+                node = node.next;
+            }
+        }
+        return head;
     }
 }
