986c746dfda55036685163948aa13020d9d1a3bf
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-04-24 00:18:28 -0500

    Skeleton for problem 820

---
+++ src/main/java/leetcode/Problem820.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/short-encoding-of-words/
+ */
+public class Problem820 {
+    public int minimumLengthEncoding(String[] words) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem820 prob = new Problem820();
+    }
+}
