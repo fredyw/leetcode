d628d739010fdb70d486555de302717cbce23337
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-04-24 00:34:25 -0500

    Skeleton for problem 817

---
+++ src/main/java/leetcode/Problem817.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/linked-list-components/
+ */
+public class Problem817 {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public int numComponents(ListNode head, int[] G) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem817 prob = new Problem817();
+    }
+}
