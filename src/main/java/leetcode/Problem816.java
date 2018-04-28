ddbd469d2fd7ef5512b9522c98eca8454285884c
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-04-28 13:21:26 -0500

    Update problem 816

--- src/main/java/leetcode/Problem816.java
+++ src/main/java/leetcode/Problem816.java
@@ -14,5 +14,8 @@ public class Problem816 {
     public static void main(String[] args) {
         Problem816 prob = new Problem816();
         System.out.println(prob.ambiguousCoordinates("(123)")); // "(1, 23)", "(12, 3)", "(1.2, 3)", "(1, 2.3)"
+        System.out.println(prob.ambiguousCoordinates("(00011)")); // "(0.001, 1)", "(0, 0.011)"
+        System.out.println(prob.ambiguousCoordinates("(0123)")); // "(0, 123)", "(0, 12.3)", "(0, 1.23)", "(0.1, 23)", "(0.1, 2.3)", "(0.12, 3)"
+        System.out.println(prob.ambiguousCoordinates("(100)")); // "(10, 0)"
     }
 }
