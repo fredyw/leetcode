96406f67ba4d4a0deba0e1152166766aedb06769
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-04-28 10:04:49 -0500

    Skeleton for problem 816

---
+++ src/main/java/leetcode/Problem816.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/ambiguous-coordinates/
+ */
+public class Problem816 {
+    public List<String> ambiguousCoordinates(String S) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem816 prob = new Problem816();
+        System.out.println(prob.ambiguousCoordinates("(123)")); // "(1, 23)", "(12, 3)", "(1.2, 3)", "(1, 2.3)"
+    }
+}
