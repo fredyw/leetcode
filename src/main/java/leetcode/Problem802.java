5919605fbc8ed1fa676af5af4c931e9498bd643e
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-04-29 00:49:11 -0500

    Update problem 802

--- src/main/java/leetcode/Problem802.java
+++ src/main/java/leetcode/Problem802.java
@@ -8,17 +8,46 @@ import java.util.List;
  */
 public class Problem802 {
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        // TODO
-        return new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < graph.length; i++) {
+            boolean[] visited = new boolean[10000];
+            boolean[] onStack = new boolean[10000];
+            Cycle cycle = new Cycle();
+            dfs(graph, i, visited, onStack, cycle);
+            if (!cycle.cycle) {
+                result.add(i);
+            }
+        }
+        return result;
     }
 
-    public static void main(String[] args) {
+    private static class Cycle {
+        private boolean cycle;
+    }
+
+    private static void dfs(int[][] graph, int source, boolean[] visited,
+                            boolean[] onStack, Cycle cycle) {
+        visited[source] = true;
+        onStack[source] = true;
+        for (int neighbor : graph[source]) {
+            if (!visited[neighbor]) {
+                dfs(graph, neighbor, visited, onStack, cycle);
+            } else {
+                if (onStack[neighbor]) {
+                    cycle.cycle = true;
+                }
+            }
+        }
+        onStack[source] = false;
+    }
+
+    public static void main(String[ ] args) {
         Problem802 prob = new Problem802();
         System.out.println(prob.eventualSafeNodes(new int[][]{
             {1, 2}, {2, 3}, {5}, {0}, {5}, {}, {}
         })); // [2,4,5,6]
         System.out.println(prob.eventualSafeNodes(new int[][]{
-            {1,2},{1},{0}
+            {1, 2}, {1}, {0}
         })); // []
     }
 }
