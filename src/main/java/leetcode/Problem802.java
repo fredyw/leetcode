b7546d7ed273326fff27cc81bb2074ccc579bea3
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-04-29 00:12:53 -0500

    Skeleton for problem 802

---
+++ src/main/java/leetcode/Problem802.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/find-eventual-safe-states/
+ */
+public class Problem802 {
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        // TODO
+        return new ArrayList<>();
+    }
+
+    public static void main(String[] args) {
+        Problem802 prob = new Problem802();
+        System.out.println(prob.eventualSafeNodes(new int[][]{
+            {1, 2}, {2, 3}, {5}, {0}, {5}, {}, {}
+        })); // [2,4,5,6]
+    }
+}
