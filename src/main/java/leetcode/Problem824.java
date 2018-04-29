537c820f3930a6b965bd2b3497bf573b700477d9
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-04-29 11:41:01 -0500

    Skeleton for problem 824

---
+++ src/main/java/leetcode/Problem824.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/goat-latin/
+ */
+public class Problem824 {
+    public String toGoatLatin(String S) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem824 prob = new Problem824();
+        System.out.println(prob.toGoatLatin("I speak Goat Latin")); // "Imaa peaksmaaa oatGmaaaa atinLmaaaaa"
+        System.out.println(prob.toGoatLatin("The quick brown fox jumped over the lazy dog")); //  "heTmaa uickqmaaa rownbmaaaa oxfmaaaaa umpedjmaaaaaa overmaaaaaaa hetmaaaaaaaa azylmaaaaaaaaa ogdmaaaaaaaaaa"
+    }
+}
