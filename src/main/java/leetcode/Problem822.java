5a1b30b868485fe4f0f1fd94becaf77c56e463e5
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-05-01 00:04:41 -0500

    Update problem 822

--- src/main/java/leetcode/Problem822.java
+++ src/main/java/leetcode/Problem822.java
@@ -5,8 +5,29 @@ package leetcode;
  */
 public class Problem822 {
     public int flipgame(int[] fronts, int[] backs) {
-        // TODO
-        return 0;
+        int[] set = new int[2000];
+        int result = Integer.MAX_VALUE;
+        for (int i = 0; i < fronts.length; i++) {
+            set[fronts[i]]++;
+        }
+        for (int i = 0; i < fronts.length; i++) {
+            if (fronts[i] == backs[i]) {
+                continue;
+            }
+            if (set[fronts[i]] == 1) {
+                result = Math.min(result, fronts[i]);
+            }
+            // Flip
+            set[backs[i]]++;
+            set[fronts[i]]--;
+            if (set[backs[i]] == 1) {
+                result = Math.min(result, backs[i]);
+            }
+            // Restore
+            set[backs[i]]--;
+            set[fronts[i]]++;
+        }
+        return result == Integer.MAX_VALUE ? 0 : result;
     }
 
     public static void main(String[] args) {
@@ -14,5 +35,8 @@ public class Problem822 {
         System.out.println(prob.flipgame(new int[]{1, 2, 4, 4, 7}, new int[]{1, 3, 4, 1, 3})); // 2
         System.out.println(prob.flipgame(new int[]{2}, new int[]{3})); // 2
         System.out.println(prob.flipgame(new int[]{3}, new int[]{2})); // 2
+        System.out.println(prob.flipgame(new int[]{3}, new int[]{3})); // 0
+        System.out.println(prob.flipgame(new int[]{1, 1}, new int[]{1, 2})); // 2
+        System.out.println(prob.flipgame(new int[]{1, 1}, new int[]{2, 2})); // 1
     }
 }
