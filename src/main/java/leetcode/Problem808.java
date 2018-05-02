d7253bf52a89401191d700dc19f90558b6feb1b7
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-05-02 00:19:10 -0500

    Skeleton for problem 808

---
+++ src/main/java/leetcode/Problem808.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/soup-servings/
+ */
+public class Problem808 {
+    public double soupServings(int N) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem808 prob = new Problem808();
+    }
+}
