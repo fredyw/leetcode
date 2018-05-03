3a7082cd0d711d4540e114901b331c31e9e0c5cd
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-05-03 01:21:15 -0500

    Update problem 808

--- src/main/java/leetcode/Problem808.java
+++ src/main/java/leetcode/Problem808.java
@@ -6,11 +6,30 @@ package leetcode;
 public class Problem808 {
     public double soupServings(int N) {
         // TODO
-        return 0;
+        return soupServings(N, N);
+    }
+
+    private double soupServings(int a, int b) {
+        if (a <= 0 && b <= 0) {
+            return 0.5;
+        }
+        if (a <= 0) {
+            return 0;
+        }
+        if (b <= 0) {
+            return 1;
+        }
+        double result = 0.25 * (soupServings(a, b - 100) +
+            soupServings(a - 25, b - 75) +
+            soupServings(a - 50, b - 50) +
+            soupServings(a - 75, b - 25));
+        return result;
     }
 
     public static void main(String[] args) {
         Problem808 prob = new Problem808();
         System.out.println(prob.soupServings(50)); // 0.625
+        System.out.println(prob.soupServings(40)); // 0.625
+        System.out.println(prob.soupServings(100)); // 0.71875
     }
 }
