aad2cb820795db23155fb0ae87d4c9ae65aa1985
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-05-02 22:45:49 -0500

    Update problem 808

--- src/main/java/leetcode/Problem808.java
+++ src/main/java/leetcode/Problem808.java
@@ -11,5 +11,6 @@ public class Problem808 {
 
     public static void main(String[] args) {
         Problem808 prob = new Problem808();
+        System.out.println(prob.soupServings(50)); // 0.625
     }
 }
