0c8133674ca925b242b613d254e88d7d4bca7abb
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-05-04 00:07:38 -0500

    Skeleton for problem 775

---
+++ src/main/java/leetcode/Problem775.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/global-and-local-inversions/
+ */
+public class Problem775 {
+    public boolean isIdealPermutation(int[] A) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem775 prob = new Problem775();
+    }
+}
