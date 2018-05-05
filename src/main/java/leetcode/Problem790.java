b4ee98665da1ee295396a372e6724c51f390b0ec
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-05-05 14:06:04 -0500

    Skeleton for problem 790

---
+++ src/main/java/leetcode/Problem790.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/domino-and-tromino-tiling/
+ */
+public class Problem790 {
+    public int numTilings(int N) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem790 prob = new Problem790();
+    }
+}
