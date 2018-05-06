77f9c1683d2404268136dc7c41c32a16fe49ab02
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-05-06 10:19:16 -0500

    Update problem 830

--- src/main/java/leetcode/Problem830.java
+++ src/main/java/leetcode/Problem830.java
@@ -7,10 +7,14 @@ import java.util.List;
  */
 public class Problem830 {
     public List<List<Integer>> largeGroupPositions(String S) {
+        // TODO
         return null;
     }
 
     public static void main(String[] args) {
         Problem830 prob = new Problem830();
+        System.out.println(prob.largeGroupPositions("abbxxxxzzy")); // [[3,6]]
+        System.out.println(prob.largeGroupPositions("abc")); // []
+        System.out.println(prob.largeGroupPositions("abcdddeeeeaabbbcd")); // [[3,5],[6,9],[12,14]]
     }
 }
