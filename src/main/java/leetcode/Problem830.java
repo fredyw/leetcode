0300860e36029367242cf2fa0238c44e4072e065
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-05-06 10:15:47 -0500

    Skeleton for problem 830

---
+++ src/main/java/leetcode/Problem830.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/positions-of-large-groups/
+ */
+public class Problem830 {
+    public List<List<Integer>> largeGroupPositions(String S) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem830 prob = new Problem830();
+    }
+}
