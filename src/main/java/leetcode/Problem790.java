7983054a869a50e429e356b09ec045a87ad85195
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-05-10 00:04:24 -0500

    Update problem 790

--- src/main/java/leetcode/Problem790.java
+++ src/main/java/leetcode/Problem790.java
@@ -11,6 +11,9 @@ public class Problem790 {
 
     public static void main(String[] args) {
         Problem790 prob = new Problem790();
+        System.out.println(prob.numTilings(1)); // 1
+        System.out.println(prob.numTilings(2)); // 2
         System.out.println(prob.numTilings(3)); // 5
+        System.out.println(prob.numTilings(4)); // 11
     }
 }
