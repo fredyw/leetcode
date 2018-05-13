cdd1a45d9d0726fb0cc20ee1782eb848c3bdc702
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-05-12 21:02:57 -0500

    Skeleton for problem 826

---
+++ src/main/java/leetcode/Problem826.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/most-profit-assigning-work/
+ */
+public class Problem826 {
+    public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem826 prob = new Problem826();
+    }
+}
