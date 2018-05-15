0c9a715ad2aa47b3c6aa98107eeeb752d5fbfebb
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-05-14 23:36:57 -0500

    Solve problem 832

--- src/main/java/leetcode/Problem832.java
+++ src/main/java/leetcode/Problem832.java
@@ -5,11 +5,17 @@ package leetcode;
  */
 public class Problem832 {
     public int[][] flipAndInvertImage(int[][] A) {
-        // TODO
-        return null;
-    }
-
-    public static void main(String[] args) {
-        Problem832 prob = new Problem832();
+        int nRow = A.length;
+        int nCol = 0;
+        if (nRow > 0) {
+            nCol = A[0].length;
+        }
+        int[][] result = new int[nRow][nCol];
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0, k = A.length - 1; j < A.length; j++, k--) {
+                result[i][k] = (A[i][j] == 0) ? 1 : 0;
+            }
+        }
+        return result;
     }
 }
