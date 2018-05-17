08c89362ae82b19bdc36c50be536df5c83f22045
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-05-16 21:54:37 -0700

    Update problem 826

--- src/main/java/leetcode/Problem826.java
+++ src/main/java/leetcode/Problem826.java
@@ -11,5 +11,7 @@ public class Problem826 {
 
     public static void main(String[] args) {
         Problem826 prob = new Problem826();
+        System.out.println(prob.maxProfitAssignment(new int[]{2, 4, 6, 8, 10},
+            new int[]{10, 20, 30, 40, 50}, new int[]{4, 5, 6, 7})); // 100
     }
 }
