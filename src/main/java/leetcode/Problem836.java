95581b0f999ec109140c2ca1dd9b9c38f7939067
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-05-19 23:04:36 -0500

    Skeleton for problem 836

---
+++ src/main/java/leetcode/Problem836.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/rectangle-overlap/
+ */
+public class Problem836 {
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem836 prob = new Problem836();
+    }
+}
