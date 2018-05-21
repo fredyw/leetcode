805314e4782692520a462ac2fa089473e1387cac
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-05-21 00:37:05 -0500

    Update problem 826

--- src/main/java/leetcode/Problem826.java
+++ src/main/java/leetcode/Problem826.java
@@ -1,12 +1,40 @@
 package leetcode;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
  * https://leetcode.com/problems/most-profit-assigning-work/
  */
 public class Problem826 {
     public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
         // TODO
+        DifficultyProfit[] dp = new DifficultyProfit[difficulty.length];
+        for (int i = 0; i < dp.length; i++) {
+            dp[i] = new DifficultyProfit(difficulty[i], profit[i]);
+        }
+        Arrays.sort(dp, Comparator.comparingInt(a -> a.difficulty));
+        int maxProfit = 0;
+        for (int i = 0; i < dp.length; i++) {
+            maxProfit = Math.max(maxProfit, dp[i].profit);
+            dp[i].profit = maxProfit;
+        }
         return 0;
+    }
+
+    private static class DifficultyProfit {
+        private final int difficulty;
+        private int profit;
+
+        public DifficultyProfit(int difficulty, int profit) {
+            this.difficulty = difficulty;
+            this.profit = profit;
+        }
+
+        @Override
+        public String toString() {
+            return difficulty + " -> " + profit;
+        }
     }
 
     public static void main(String[] args) {
