8335e6c38e515bd0a578c3e8c968cc450d11c55e
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-05-22 00:34:28 -0500

    Update problem 826

--- src/main/java/leetcode/Problem826.java
+++ src/main/java/leetcode/Problem826.java
@@ -8,7 +8,6 @@ import java.util.Comparator;
  */
 public class Problem826 {
     public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
-        // TODO
         DifficultyProfit[] dp = new DifficultyProfit[difficulty.length];
         for (int i = 0; i < dp.length; i++) {
             dp[i] = new DifficultyProfit(difficulty[i], profit[i]);
@@ -19,7 +18,20 @@ public class Problem826 {
             maxProfit = Math.max(maxProfit, dp[i].profit);
             dp[i].profit = maxProfit;
         }
-        return 0;
+        int result = 0;
+        for (int i = 0; i < worker.length; i++) {
+            int idx = Arrays.binarySearch(dp, new DifficultyProfit(worker[i], 0),
+                Comparator.comparingInt(a -> a.difficulty));
+            if (idx < 0) {
+                idx = -idx - 2;
+                if (idx >= 0) {
+                    result += dp[idx].profit;
+                }
+            } else {
+                result += dp[idx].profit;
+            }
+        }
+        return result;
     }
 
     private static class DifficultyProfit {
@@ -39,9 +51,21 @@ public class Problem826 {
 
     public static void main(String[] args) {
         Problem826 prob = new Problem826();
-        System.out.println(prob.maxProfitAssignment(new int[]{2, 4, 6, 8, 10},
-            new int[]{10, 20, 30, 40, 50}, new int[]{4, 5, 6, 7})); // 100
-        System.out.println(prob.maxProfitAssignment(new int[]{2, 4, 6, 8, 10},
-            new int[]{10, 5, 30, 40, 50}, new int[]{4, 5, 6, 7})); // 80
+//        System.out.println(prob.maxProfitAssignment(
+//            new int[]{2, 4, 6, 8, 10},
+//            new int[]{10, 20, 30, 40, 50},
+//            new int[]{4, 5, 6, 7})); // 100
+        System.out.println(prob.maxProfitAssignment(
+            new int[]{4, 4, 6, 8, 10},
+            new int[]{10, 30, 30, 40, 50},
+            new int[]{4, 5, 6, 7})); // 120
+//        System.out.println(prob.maxProfitAssignment(
+//            new int[]{2, 4, 6, 8, 10},
+//            new int[]{10, 5, 30, 40, 50},
+//            new int[]{4, 5, 6, 7})); // 80
+//        System.out.println(prob.maxProfitAssignment(
+//            new int[]{23, 30, 35, 35, 43, 46, 47, 81, 83, 98},
+//            new int[]{8, 11, 11, 20, 33, 37, 60, 72, 87, 95},
+//            new int[]{95, 46, 47, 97, 11, 35, 99, 56, 41, 92})); // 553
     }
 }
