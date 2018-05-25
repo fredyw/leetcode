0118188fba25e952699c2c04f1ee8c9ee61ffefd
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-05-25 00:50:22 -0500

    Solve problem 185

--- src/main/sql/Problem185.sql
+++ src/main/sql/Problem185.sql
@@ -1,1 +1,6 @@
 # https://leetcode.com/problems/department-top-three-salaries/
+select d.Name as 'Department', e1.Name as 'Employee', e1.Salary
+from Employee e1 join Department d on e1.DepartmentId = d.Id
+where 3 > (select count(distinct e2.Salary)
+           from Employee e2
+           where e2.Salary > e1.Salary and e1.DepartmentId = e2.DepartmentId);
