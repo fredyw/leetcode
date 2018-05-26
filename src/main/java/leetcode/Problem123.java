62ba26cc0f6a396d24787ec6bbffa6f544a81916
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-05-26 00:49:08 -0500

    Skeleton for problem 123

---
+++ src/main/java/leetcode/Problem123.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/
+ */
+public class Problem123 {
+    public int maxProfit(int[] prices) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem123 prob = new Problem123();
+    }
+}
