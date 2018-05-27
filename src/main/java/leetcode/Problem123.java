91e26244950d45c1d654fe5f2556c8e6e42a58d6
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-05-27 10:40:34 -0500

    Update problem 123

--- src/main/java/leetcode/Problem123.java
+++ src/main/java/leetcode/Problem123.java
@@ -9,7 +9,18 @@ public class Problem123 {
         return 0;
     }
 
+//    private static int maxProfit(int[] prices, int i) {
+//        for (int j = i + 1; j < prices.length; j++) {
+//            if (prices[i] > prices[j]) {
+//                maxProfit(prices, j + 1);
+//            }
+//        }
+//    }
+
     public static void main(String[] args) {
         Problem123 prob = new Problem123();
+        System.out.println(prob.maxProfit(new int[]{3, 3, 5, 0, 0, 3, 1, 4})); // 6
+        System.out.println(prob.maxProfit(new int[]{1, 2, 3, 4, 5})); // 4
+        System.out.println(prob.maxProfit(new int[]{7, 6, 4, 3, 1})); // 0
     }
 }
