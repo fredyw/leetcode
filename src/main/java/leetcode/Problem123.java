ce43a1df51dbb103ffbb71b948448c82a85fb156
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-05-27 11:50:21 -0500

    Update problem 123

--- src/main/java/leetcode/Problem123.java
+++ src/main/java/leetcode/Problem123.java
@@ -1,26 +1,48 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/
  */
 public class Problem123 {
     public int maxProfit(int[] prices) {
-        // TODO
-        return 0;
+        return maxProfit(prices, 0, 0, new HashMap<>());
     }
 
-//    private static int maxProfit(int[] prices, int i) {
-//        for (int j = i + 1; j < prices.length; j++) {
-//            if (prices[i] > prices[j]) {
-//                maxProfit(prices, j + 1);
-//            }
-//        }
-//    }
+    private static int maxProfit(int[] prices, int i, int count,
+                                 Map<Integer, Integer> memo) {
+        if (count >= 2) {
+            return 0;
+        }
+        if (memo.containsKey(i)) {
+            return memo.get(i);
+        }
+        int max = 0;
+        for (int x = i; x < prices.length; x++) {
+            for (int y = x + 1; y < prices.length; y++) {
+                if (prices[x] < prices[y]) {
+                    int diff = prices[y] - prices[x];
+                    int profit = maxProfit(prices, y + 1, count + 1, memo) + diff;
+                    max = Math.max(max, profit);
+                }
+            }
+        }
+        memo.put(i, max);
+        return max;
+    }
 
     public static void main(String[] args) {
         Problem123 prob = new Problem123();
         System.out.println(prob.maxProfit(new int[]{3, 3, 5, 0, 0, 3, 1, 4})); // 6
+        System.out.println(prob.maxProfit(new int[]{3, 3, 5, 0, 0, 3, 1, 4, 4, 9, 8, 10})); // 12
         System.out.println(prob.maxProfit(new int[]{1, 2, 3, 4, 5})); // 4
         System.out.println(prob.maxProfit(new int[]{7, 6, 4, 3, 1})); // 0
+        System.out.println(prob.maxProfit(new int[]{5, 2, 3})); // 1
+        System.out.println(prob.maxProfit(new int[]{3, 2, 6, 5, 0, 3})); // 7
+        System.out.println(prob.maxProfit(new int[]{2, 6, 5, 0, 3})); // 7
+        System.out.println(prob.maxProfit(new int[]{2, 5, 6, 0, 3})); // 7
+        System.out.println(prob.maxProfit(new int[]{3, 2, 5, 6, 0, 3})); // 7
     }
 }
