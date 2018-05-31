59b2ea37464de4d5b1a0b97d6bdb55ac7ed36bed
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-05-30 22:35:44 -0500

    Skeleton for problem 840

---
+++ src/main/java/leetcode/Problem840.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/magic-squares-in-grid/
+ */
+public class Problem840 {
+    public int numMagicSquaresInside(int[][] grid) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem840 prob = new Problem840();
+    }
+}
