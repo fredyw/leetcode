d8bf08f87da00df736515a8cafbcb0464e2b379f
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-06-02 00:06:41 -0500

    Skeleton for problem 841

---
+++ src/main/java/leetcode/Problem841.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/keys-and-rooms/
+ */
+public class Problem841 {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem841 prob = new Problem841();
+    }
+}
