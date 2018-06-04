146714de5fcd871390b259cb83a86c3bac0d5e91
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-06-04 00:54:15 -0500

    Skeleton for problem 844

---
+++ src/main/java/leetcode/Problem844.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/backspace-string-compare/
+ */
+public class Problem844 {
+    public boolean backspaceCompare(String S, String T) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem844 prob = new Problem844();
+    }
+}
