7651818fc9065ff1e12723b030819ff2268472c1
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-06-04 00:32:18 -0500

    Update problem 841

--- src/main/java/leetcode/Problem841.java
+++ src/main/java/leetcode/Problem841.java
@@ -20,5 +20,11 @@ public class Problem841 {
             Arrays.asList(3),
             Arrays.asList())
         )); // true
+        System.out.println(prob.canVisitAllRooms(Arrays.asList(
+            Arrays.asList(1, 3),
+            Arrays.asList(3, 0, 1),
+            Arrays.asList(2),
+            Arrays.asList(0)
+        ))); // false
     }
 }
