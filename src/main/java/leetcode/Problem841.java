761eb63b15515dca4398555188b0550af47aeba9
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-06-04 00:49:04 -0500

    Solve problem 841

--- src/main/java/leetcode/Problem841.java
+++ src/main/java/leetcode/Problem841.java
@@ -8,8 +8,27 @@ import java.util.List;
  */
 public class Problem841 {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        // TODO
-        return false;
+        boolean[] visited = new boolean[rooms.size()];
+        visited[0] = true;
+        canVisitAllRooms(rooms, rooms.get(0), visited);
+        boolean result = true;
+        for (boolean v : visited) {
+            if (!v) {
+                return false;
+            }
+        }
+        return result;
+    }
+
+    private static void canVisitAllRooms(List<List<Integer>> rooms,
+                                         List<Integer> keys,
+                                         boolean[] visited) {
+        for (int key : keys) {
+            if (!visited[key]) {
+                visited[key] = true;
+                canVisitAllRooms(rooms, rooms.get(key), visited);
+            }
+        }
     }
 
     public static void main(String[] args) {
