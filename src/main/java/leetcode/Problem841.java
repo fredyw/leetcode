37ceb02e15566ece0829d364ec7dc2bb77ca2d58
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-06-03 23:59:54 -0500

    Update problem 841

--- src/main/java/leetcode/Problem841.java
+++ src/main/java/leetcode/Problem841.java
@@ -1,5 +1,6 @@
 package leetcode;
 
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -13,5 +14,11 @@ public class Problem841 {
 
     public static void main(String[] args) {
         Problem841 prob = new Problem841();
+        System.out.println(prob.canVisitAllRooms(Arrays.asList(
+            Arrays.asList(1),
+            Arrays.asList(2),
+            Arrays.asList(3),
+            Arrays.asList())
+        )); // true
     }
 }
