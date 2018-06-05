e9aaea64007ee301653f8c385568aa19f39cca54
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-06-04 23:05:44 -0500

    Skeleton for problem 846

---
+++ src/main/java/leetcode/Problem846.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/hand-of-straights/
+ */
+public class Problem846 {
+    public boolean isNStraightHand(int[] hand, int W) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem846 prob = new Problem846();
+    }
+}
