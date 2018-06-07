24c20fb4d832bbd1533b85e2ebe60c7d411c9c22
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-06-06 21:18:28 -0500

    Skeleton for problem 845

---
+++ src/main/java/leetcode/Problem845.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/longest-mountain-in-array/
+ */
+public class Problem845 {
+    public int longestMountain(int[] A) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem845 prob = new Problem845();
+    }
+}
