f8287e1d917eefa77216341043b95ac1a202376f
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-06-08 01:32:11 -0500

    Update problem 845

--- src/main/java/leetcode/Problem845.java
+++ src/main/java/leetcode/Problem845.java
@@ -5,10 +5,13 @@ package leetcode;
  */
 public class Problem845 {
     public int longestMountain(int[] A) {
-        int left = 0;
+        if (A.length == 0) {
+            return 0;
+        }
+        int left = -1;
         int right = -1;
         int i = 0;
-        int prev = Integer.MIN_VALUE;
+        int prev = A[0];
         int result = 0;
         while (i < A.length) {
             if (prev < A[i]) {
@@ -16,13 +19,18 @@ public class Problem845 {
                     result = Math.max(result, right - left + 1);
                     left = right;
                     right = -1;
+                } else {
+                    if (left == -1) {
+                        left = i - 1;
+                    }
                 }
                 prev = Math.max(prev, A[i]);
             } else if (prev > A[i]) {
                 prev = Math.min(prev, A[i]);
                 right = i;
             } else {
-                // TODO
+                left = -1;
+                right = -1;
             }
             i++;
         }
@@ -37,9 +45,11 @@ public class Problem845 {
 //        System.out.println(prob.longestMountain(new int[]{2, 1, 4, 7, 3, 2, 5})); // 5
 //        System.out.println(prob.longestMountain(new int[]{2, 2, 2})); // 0
 //        System.out.println(prob.longestMountain(new int[]{1, 1, 2, 3, 4, 2, 3, 2})); // 5
-        System.out.println(prob.longestMountain(new int[]{2, 1})); // 0
+//        System.out.println(prob.longestMountain(new int[]{2, 1})); // 0
 //        System.out.println(prob.longestMountain(new int[]{1, 2})); // 0
 //        System.out.println(prob.longestMountain(new int[]{1, 2, 1})); // 3
 //        System.out.println(prob.longestMountain(new int[]{1, 2, 2, 1})); // 0
+//        System.out.println(prob.longestMountain(new int[]{1, 2, 2, 1, 5, 3, 6, 1, 0})); // 0
+        System.out.println(prob.longestMountain(new int[]{7, 4, 8})); // 0
     }
 }
