134b350a87718f4a10d8a0b31c01956244a4eaf9
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-06-08 01:12:45 -0500

    Update problem 845

--- src/main/java/leetcode/Problem845.java
+++ src/main/java/leetcode/Problem845.java
@@ -5,13 +5,40 @@ package leetcode;
  */
 public class Problem845 {
     public int longestMountain(int[] A) {
-        // TODO
-        return 0;
+        int left = 0;
+        int right = -1;
+        int i = 0;
+        int prev = Integer.MIN_VALUE;
+        int result = 0;
+        while (i < A.length) {
+            if (prev < A[i]) {
+                if (left >= 0 && right >= 0) {
+                    result = Math.max(result, right - left + 1);
+                    left = right;
+                    right = -1;
+                }
+                prev = Math.max(prev, A[i]);
+            } else if (prev > A[i]) {
+                prev = Math.min(prev, A[i]);
+                right = i;
+            } else {
+                // TODO
+            }
+            i++;
+        }
+        if (left >= 0 && right >= 0) {
+            result = Math.max(result, right - left + 1);
+        }
+        return result;
     }
 
     public static void main(String[] args) {
         Problem845 prob = new Problem845();
-        System.out.println(prob.longestMountain(new int[]{2, 1, 4, 7, 3, 2, 5})); // 5
-        System.out.println(prob.longestMountain(new int[]{2, 2, 2})); // 0
+//        System.out.println(prob.longestMountain(new int[]{2, 1, 4, 7, 3, 2, 5})); // 5
+//        System.out.println(prob.longestMountain(new int[]{2, 2, 2})); // 0
+//        System.out.println(prob.longestMountain(new int[]{1, 1, 2, 3, 4, 2, 3, 2})); // 5
+        System.out.println(prob.longestMountain(new int[]{2, 1})); // 0
+//        System.out.println(prob.longestMountain(new int[]{1, 2})); // 0
+//        System.out.println(prob.longestMountain(new int[]{1, 2, 1})); // 3
     }
 }
