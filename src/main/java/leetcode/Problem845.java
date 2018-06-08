0e61b9d6d3ddafeefabc881b56598b046c72fb66
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-06-07 23:25:05 -0500

    Update problem 845

--- src/main/java/leetcode/Problem845.java
+++ src/main/java/leetcode/Problem845.java
@@ -11,5 +11,7 @@ public class Problem845 {
 
     public static void main(String[] args) {
         Problem845 prob = new Problem845();
+        System.out.println(prob.longestMountain(new int[]{2, 1, 4, 7, 3, 2, 5})); // 5
+        System.out.println(prob.longestMountain(new int[]{2, 2, 2})); // 0
     }
 }
