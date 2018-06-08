6c49b185df9dc06b0175963178229dffb37adb83
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-06-08 02:52:21 -0500

    Solve problem 845

--- src/main/java/leetcode/Problem845.java
+++ src/main/java/leetcode/Problem845.java
@@ -8,48 +8,32 @@ public class Problem845 {
         if (A.length == 0) {
             return 0;
         }
-        int left = -1;
-        int right = -1;
-        int i = 0;
-        int prev = A[0];
         int result = 0;
-        while (i < A.length) {
-            if (prev < A[i]) {
-                if (left >= 0 && right >= 0) {
-                    result = Math.max(result, right - left + 1);
-                    left = right;
-                    right = -1;
-                } else {
-                    if (left == -1) {
-                        left = i - 1;
-                    }
+        int left = -1;
+        int current = 0;
+        int prev = A[0];
+        boolean up = false;
+        boolean down = false;
+        while (current < A.length) {
+            if (prev < A[current]) {
+                if (left == -1 || down) {
+                    left = current - 1;
                 }
-                prev = Math.max(prev, A[i]);
-            } else if (prev > A[i]) {
-                prev = Math.min(prev, A[i]);
-                right = i;
+                prev = Math.max(prev, A[current]);
+                up = true;
+                down = false;
+            } else if (up && prev > A[current]) {
+                prev = Math.min(prev, A[current]);
+                result = Math.max(result, current - left + 1);
+                down = true;
             } else {
                 left = -1;
-                right = -1;
+                prev = A[current];
+                up = false;
+                down = false;
             }
-            i++;
-        }
-        if (left >= 0 && right >= 0) {
-            result = Math.max(result, right - left + 1);
+            current++;
         }
         return result;
-    }
-
-    public static void main(String[] args) {
-        Problem845 prob = new Problem845();
-//        System.out.println(prob.longestMountain(new int[]{2, 1, 4, 7, 3, 2, 5})); // 5
-//        System.out.println(prob.longestMountain(new int[]{2, 2, 2})); // 0
-//        System.out.println(prob.longestMountain(new int[]{1, 1, 2, 3, 4, 2, 3, 2})); // 5
-//        System.out.println(prob.longestMountain(new int[]{2, 1})); // 0
-//        System.out.println(prob.longestMountain(new int[]{1, 2})); // 0
-//        System.out.println(prob.longestMountain(new int[]{1, 2, 1})); // 3
-//        System.out.println(prob.longestMountain(new int[]{1, 2, 2, 1})); // 0
-//        System.out.println(prob.longestMountain(new int[]{1, 2, 2, 1, 5, 3, 6, 1, 0})); // 0
-        System.out.println(prob.longestMountain(new int[]{7, 4, 8})); // 0
     }
 }
