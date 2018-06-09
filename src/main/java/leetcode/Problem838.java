c570fdb653aa0aed6f935a9a8e1ce073a9aaf4d6
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-06-09 10:35:31 -0500

    Skeleton for problem 838

---
+++ src/main/java/leetcode/Problem838.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/push-dominoes/
+ */
+public class Problem838 {
+    public String pushDominoes(String dominoes) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem838 prob = new Problem838();
+    }
+}
