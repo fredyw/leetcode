17cd45824e4bd83ef44c418cad575cf10c371bea
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-06-12 00:04:48 -0500

    Skeleton for problem 849

---
+++ src/main/java/leetcode/Problem849.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/maximize-distance-to-closest-person/
+ */
+public class Problem849 {
+    public int maxDistToClosest(int[] seats) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem849 prob = new Problem849();
+    }
+}
