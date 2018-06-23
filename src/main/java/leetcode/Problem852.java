37152079b334300e160467702c8b5352a30e14c0
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-06-22 22:31:18 -0500

    Solve problem 852

--- src/main/java/leetcode/Problem852.java
+++ src/main/java/leetcode/Problem852.java
@@ -5,13 +5,14 @@ package leetcode;
  */
 public class Problem852 {
     public int peakIndexInMountainArray(int[] A) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem852 prob = new Problem852();
-        System.out.println(prob.peakIndexInMountainArray(new int[]{0, 1, 0})); // 1
-        System.out.println(prob.peakIndexInMountainArray(new int[]{0, 2, 1, 0})); // 1
+        int max = 0;
+        int maxIdx = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] > max) {
+                max = A[i];
+                maxIdx = i;
+            }
+        }
+        return maxIdx;
     }
 }
