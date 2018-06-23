c3d80db7a1deff005ac0268a89730c9181e40fcd
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-06-22 22:28:44 -0500

    Skeleton for problem 852

---
+++ src/main/java/leetcode/Problem852.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/peak-index-in-a-mountain-array/
+ */
+public class Problem852 {
+    public int peakIndexInMountainArray(int[] A) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem852 prob = new Problem852();
+        System.out.println(prob.peakIndexInMountainArray(new int[]{0, 1, 0})); // 1
+        System.out.println(prob.peakIndexInMountainArray(new int[]{0, 2, 1, 0})); // 1
+    }
+}
