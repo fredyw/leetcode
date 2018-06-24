2694223387dd1d53fda3cdf4db2c3584bed639e0
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-06-24 08:51:58 -0500

    SKeleton for problem 859

---
+++ src/main/java/leetcode/Problem859.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/buddy-strings/
+ */
+public class Problem859 {
+    public boolean buddyStrings(String A, String B) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem859 prob = new Problem859();
+    }
+}
