4645ee5ee5253403a48d95202561314dcb2a2226
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-06-24 08:57:38 -0500

    Update problem 859

--- src/main/java/leetcode/Problem859.java
+++ src/main/java/leetcode/Problem859.java
@@ -11,5 +11,11 @@ public class Problem859 {
 
     public static void main(String[] args) {
         Problem859 prob = new Problem859();
+        System.out.println(prob.buddyStrings("ab", "ba")); // true
+        System.out.println(prob.buddyStrings("ab", "ab")); // false
+        System.out.println(prob.buddyStrings("aa", "aa")); // true
+        System.out.println(prob.buddyStrings("aaaaaaabc", "aaaaaaacb")); // true
+        System.out.println(prob.buddyStrings("", "aa")); // false
+        System.out.println(prob.buddyStrings("abc", "cba")); // true
     }
 }
