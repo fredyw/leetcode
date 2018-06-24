c95ab72f5b5183d0a6cf761e47ea9135bc4d0d3a
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-06-24 10:28:28 -0500

    Solve problem 859

--- src/main/java/leetcode/Problem859.java
+++ src/main/java/leetcode/Problem859.java
@@ -11,7 +11,14 @@ public class Problem859 {
         int idx1 = 0;
         int idx2 = 0;
         int count = 0;
+        int[] charCounts = new int[26];
+        boolean hasDuplicate = false;
         for (int i = 0; i < A.length(); i++) {
+            int index = A.charAt(i) - 'a';
+            charCounts[index]++;
+            if (charCounts[index] > 1) {
+                hasDuplicate = true;
+            }
             if (A.charAt(i) != B.charAt(i)) {
                 if (count == 0) {
                     idx1 = i;
@@ -23,19 +30,10 @@ public class Problem859 {
                 count++;
             }
         }
+        if (count == 0) {
+            return hasDuplicate;
+        }
         return (A.charAt(idx1) == B.charAt(idx2) &&
             A.charAt(idx2) == B.charAt(idx1));
-    }
-
-    public static void main(String[] args) {
-        Problem859 prob = new Problem859();
-        System.out.println(prob.buddyStrings("ab", "ba")); // true
-        System.out.println(prob.buddyStrings("ab", "ab")); // false
-        System.out.println(prob.buddyStrings("aa", "aa")); // true
-        System.out.println(prob.buddyStrings("aaaaaaabc", "aaaaaaacb")); // true
-        System.out.println(prob.buddyStrings("", "aa")); // false
-        System.out.println(prob.buddyStrings("abc", "cba")); // true
-        System.out.println(prob.buddyStrings("aab", "aab")); // true
-        System.out.println(prob.buddyStrings("aba", "aba")); // true
     }
 }
