02615ce644e5c310da70884e991f7b03b5cca8f2
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-06-24 09:57:16 -0500

    Update problem 859

--- src/main/java/leetcode/Problem859.java
+++ src/main/java/leetcode/Problem859.java
@@ -5,8 +5,26 @@ package leetcode;
  */
 public class Problem859 {
     public boolean buddyStrings(String A, String B) {
-        // TODO
-        return false;
+        if (A.length() != B.length()) {
+            return false;
+        }
+        int idx1 = 0;
+        int idx2 = 0;
+        int count = 0;
+        for (int i = 0; i < A.length(); i++) {
+            if (A.charAt(i) != B.charAt(i)) {
+                if (count == 0) {
+                    idx1 = i;
+                } else if (count == 1) {
+                    idx2 = i;
+                } else {
+                    return false;
+                }
+                count++;
+            }
+        }
+        return (A.charAt(idx1) == B.charAt(idx2) &&
+            A.charAt(idx2) == B.charAt(idx1));
     }
 
     public static void main(String[] args) {
@@ -17,5 +35,7 @@ public class Problem859 {
         System.out.println(prob.buddyStrings("aaaaaaabc", "aaaaaaacb")); // true
         System.out.println(prob.buddyStrings("", "aa")); // false
         System.out.println(prob.buddyStrings("abc", "cba")); // true
+        System.out.println(prob.buddyStrings("aab", "aab")); // true
+        System.out.println(prob.buddyStrings("aba", "aba")); // true
     }
 }
