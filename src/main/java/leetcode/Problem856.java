d4ed6f81533fc512991622a3a48e0da1875ab746
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-06-27 01:09:06 -0500

    Solve problem 856

--- src/main/java/leetcode/Problem856.java
+++ src/main/java/leetcode/Problem856.java
@@ -5,35 +5,27 @@ package leetcode;
  */
 public class Problem856 {
     public int scoreOfParentheses(String S) {
-        return scoreOfParentheses(S, 0);
+        return scoreOfParentheses(S, new IntRef());
     }
 
-    private int scoreOfParentheses(String s, int i) {
-        if (i >= s.length()) {
-            return 0;
-        }
+    private static class IntRef {
+        private int index;
+    }
+
+    private int scoreOfParentheses(String s, IntRef ref) {
         int result = 0;
-        if (s.charAt(i) == '(' && s.charAt(i + 1) == ')') {
-            result = 1 + scoreOfParentheses(s, i + 2);
-        } else if (s.charAt(i) == '(') {
-            result = 2 * scoreOfParentheses(s, i + 1);
-        } else {
-//            result = scoreOfParentheses(s, i + 1);
+        while (ref.index < s.length()) {
+            if (s.charAt(ref.index) == '(' && s.charAt(ref.index + 1) == ')') {
+                result++;
+                ref.index += 2;
+            } else if (s.charAt(ref.index) == '(') {
+                ref.index++;
+                result += 2 * scoreOfParentheses(s, ref);
+            } else {
+                ref.index++;
+                return result;
+            }
         }
         return result;
-    }
-
-    public static void main(String[] args) {
-        Problem856 prob = new Problem856();
-//        System.out.println(prob.scoreOfParentheses("()")); // 1
-//        System.out.println(prob.scoreOfParentheses("(())")); // 2
-//        System.out.println(prob.scoreOfParentheses("()()()")); // 3
-//        System.out.println(prob.scoreOfParentheses("(())()")); // 3
-        System.out.println(prob.scoreOfParentheses("(()())()")); // 5
-//        System.out.println(prob.scoreOfParentheses("()()")); // 2
-//        System.out.println(prob.scoreOfParentheses("(()(()))")); // 6
-//        System.out.println(prob.scoreOfParentheses("((()))")); // 4
-//        System.out.println(prob.scoreOfParentheses("((()()))")); // 8
-//        System.out.println(prob.scoreOfParentheses("((()())())")); // 10
     }
 }
