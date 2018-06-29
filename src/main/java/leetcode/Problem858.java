d40bab159ac1f95caeacc8efe4051b8c46068d51
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-06-28 23:53:03 -0500

    Skeleton for problem 858

---
+++ src/main/java/leetcode/Problem858.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/mirror-reflection/
+ */
+public class Problem858 {
+    public int mirrorReflection(int p, int q) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem858 prob = new Problem858();
+        System.out.println(prob.mirrorReflection(2, 1)); // 2
+    }
+}
