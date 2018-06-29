781b7e5f46764abcca072d6f2ca7a606d4558950
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-06-29 01:26:04 -0500

    Update problem 858

--- src/main/java/leetcode/Problem858.java
+++ src/main/java/leetcode/Problem858.java
@@ -6,6 +6,9 @@ package leetcode;
 public class Problem858 {
     public int mirrorReflection(int p, int q) {
         // TODO
+        if (q == 0) {
+            return 0;
+        }
         return 0;
     }
 
@@ -15,5 +18,7 @@ public class Problem858 {
         System.out.println(prob.mirrorReflection(2, 0)); // 0
         System.out.println(prob.mirrorReflection(3, 1)); // 1
         System.out.println(prob.mirrorReflection(4, 1)); // 2
+        System.out.println(prob.mirrorReflection(3, 2)); // 0
+        System.out.println(prob.mirrorReflection(5, 3)); // 1
     }
 }
