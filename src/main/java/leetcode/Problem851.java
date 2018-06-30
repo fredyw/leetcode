6c3222861f015b59f3fc037e75d88d03fe7fb78c
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-06-30 13:46:54 -0500

    Skeleton for problem 851

---
+++ src/main/java/leetcode/Problem851.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/loud-and-rich/
+ */
+public class Problem851 {
+    public int[] loudAndRich(int[][] richer, int[] quiet) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem851 prob = new Problem851();
+    }
+}
