c3dee07cc038fc83c1352353673bd1e1b80f9100
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-07-01 10:52:33 -0500

    Skeleton for problem 860

---
+++ src/main/java/leetcode/Problem860.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/lemonade-change/
+ */
+public class Problem860 {
+    public boolean lemonadeChange(int[] bills) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem860 prob = new Problem860();
+    }
+}
