e5552ac8746f7f0b98e3ed8cdc7e9605802093cf
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-07-01 11:34:36 -0500

    Solve problem 860

--- src/main/java/leetcode/Problem860.java
+++ src/main/java/leetcode/Problem860.java
@@ -5,11 +5,42 @@ package leetcode;
  */
 public class Problem860 {
     public boolean lemonadeChange(int[] bills) {
-        // TODO
-        return false;
+        // 0: count of 5
+        // 1: count of 10
+        // 2: count of 20
+        int[] change = new int[3];
+        for (int bill : bills) {
+            change[index(bill)]++;
+            if (bill == 10) {
+                if (change[index(5)] == 0) {
+                    return false;
+                }
+                change[index(5)]--;
+            } else if (bill == 20) {
+                if (change[index(5)] == 0) {
+                    return false;
+                }
+                if (change[index(10)] == 0) {
+                    if (change[0] < 3) {
+                        return false;
+                    }
+                    change[index(5)] -= 3;
+                } else {
+                    change[index(10)]--;
+                    change[index(5)]--;
+                }
+            }
+        }
+        return true;
     }
 
-    public static void main(String[] args) {
-        Problem860 prob = new Problem860();
+    private static int index(int bill) {
+        if (bill == 5) {
+            return 0;
+        }
+        if (bill == 10) {
+            return 1;
+        }
+        return 2;
     }
 }
