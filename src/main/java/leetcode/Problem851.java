c24bc765d67cfbc3d2c45c3bb9da9cc0581cb800
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-07-02 00:15:06 -0500

    Update problem 851

--- src/main/java/leetcode/Problem851.java
+++ src/main/java/leetcode/Problem851.java
@@ -11,5 +11,8 @@ public class Problem851 {
 
     public static void main(String[] args) {
         Problem851 prob = new Problem851();
+        System.out.println(prob.loudAndRich(new int[][]{
+            {1, 0}, {2, 1}, {3, 1}, {3, 7}, {4, 3}, {5, 3}, {6, 3}
+        }, new int[]{3, 2, 5, 4, 6, 1, 7, 0})); // [5,5,2,5,4,5,6,7]
     }
 }
