4a96f20b8030ac387952cda4cf356a1e0991f701
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-07-04 00:51:00 -0500

    Skeleton for problem 861

---
+++ src/main/java/leetcode/Problem861.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/score-after-flipping-matrix/
+ */
+public class Problem861 {
+    public int matrixScore(int[][] A) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem861 prob = new Problem861();
+    }
+}
