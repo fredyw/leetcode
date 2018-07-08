a8f8574420e75bd30f880ffcd83d27dc9982f186
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-07-08 00:12:38 -0500

    Skeleton for problem 868

---
+++ src/main/java/leetcode/Problem868.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/transpose-matrix/
+ */
+public class Problem868 {
+    public int[][] transpose(int[][] A) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem868 prob = new Problem868();
+    }
+}
