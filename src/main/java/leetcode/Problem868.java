bb92ab823b618362585ae064a6a92297c70e2548
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-07-08 00:43:15 -0500

    Update problem 868

--- src/main/java/leetcode/Problem868.java
+++ src/main/java/leetcode/Problem868.java
@@ -27,22 +27,4 @@ public class Problem868 {
         }
         System.out.println();
     }
-    public static void main(String[] args) {
-        Problem868 prob = new Problem868();
-        // [1, 4, 7],
-        // [2, 5, 8],
-        // [3, 6, 9]
-        print(prob.transpose(new int[][]{
-            {1, 2, 3},
-            {4, 5, 6},
-            {7, 8, 9}
-        }));
-        // [1, 4],
-        // [2, 5],
-        // [3, 6]
-        print(prob.transpose(new int[][]{
-            {1, 2, 3},
-            {4, 5, 6}
-        }));
-    }
 }
