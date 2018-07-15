b8a9b43e9c56d6ad37b91cd3e5d450c70f7e0def
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-07-15 00:42:01 -0500

    Skeleton for problem 868

---
+++ src/main/java/leetcode/Problem868.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/binary-gap/
+ */
+public class Problem868 {
+    public int binaryGap(int N) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem868 prob = new Problem868();
+        System.out.println(prob.binaryGap(22)); // 2
+        System.out.println(prob.binaryGap(5)); // 2
+        System.out.println(prob.binaryGap(6)); // 1
+        System.out.println(prob.binaryGap(8)); // 0
+    }
+}
