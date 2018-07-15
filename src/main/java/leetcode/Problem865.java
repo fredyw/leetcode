26fc4da2e3a660fe04e4031856cf4d798bf3bd86
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-07-15 12:23:42 -0500

    Skeleton for problem 865

---
+++ src/main/java/leetcode/Problem865.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/smallest-subtree-with-all-the-deepest-nodes/
+ */
+public class Problem865 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode subtreeWithAllDeepest(TreeNode root) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
