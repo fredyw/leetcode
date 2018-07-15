4367c43065049490768460d296737a5f9122a8fd
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-07-14 22:15:51 -0500

    Update problem 861

--- src/main/java/leetcode/Problem861.java
+++ src/main/java/leetcode/Problem861.java
@@ -7,32 +7,25 @@ public class Problem861 {
     public int matrixScore(int[][] A) {
         int nRow = A.length;
         int nCol = A.length > 0 ? A[0].length : 0;
-        int[] score = score(A);
+        int totalScore = 0;
+        int[] score = new int[nRow];
         for (int row = 0; row < nRow; row++) {
+            int origScore = 0;
+            int flipScore = 0;
             for (int col = 0; col < nCol; col++) {
+                origScore += A[row][col] << nCol - col - 1;
+                flipScore += (A[row][col] ^ 1) << nCol - col - 1;
             }
+            score[row] = (origScore > flipScore) ? origScore : flipScore;
         }
         for (int col = 0; col < nCol; col++) {
             for (int row = 0; row < nRow; row++) {
             }
         }
-        int totalScore = 0;
         for (int s : score) {
             totalScore += s;
         }
         return totalScore;
-    }
-
-    private static int[] score(int[][] a) {
-        int[] score = new int[a.length];
-        for (int i = 0; i < a.length; i++) {
-            int s = 0;
-            for (int j = 0; j < a[i].length; j++) {
-                s += a[i][j] << a[i].length - j - 1;
-            }
-            score[i] = s;
-        }
-        return score;
     }
 
     public static void main(String[] args) {
