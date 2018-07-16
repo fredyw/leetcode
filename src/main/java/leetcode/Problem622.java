c86b5079ea65939b0f5d1aa9cb8b463a1a2ee276
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-07-15 21:45:46 -0500

    Update problem 622

--- src/main/java/leetcode/Problem622.java
+++ src/main/java/leetcode/Problem622.java
@@ -59,17 +59,4 @@ public class Problem622 {
             return queue.size() == size;
         }
     }
-
-    public static void main(String[] args) {
-        MyCircularQueue circularQueue = new MyCircularQueue(3); // set the size to be 3
-        System.out.println(circularQueue.enQueue(1));  // return true
-        System.out.println(circularQueue.enQueue(2));  // return true
-        System.out.println(circularQueue.enQueue(3));  // return true
-        System.out.println(circularQueue.enQueue(4));  // return false, the queue is full
-        System.out.println(circularQueue.Rear());  // return 3
-        System.out.println(circularQueue.isFull());  // return true
-        System.out.println(circularQueue.deQueue());  // return true
-        System.out.println(circularQueue.enQueue(4));  // return true
-        System.out.println(circularQueue.Rear());  // return 4
-    }
 }
