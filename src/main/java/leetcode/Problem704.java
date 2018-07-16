cffbcdf7e0152a4960e6d4c22c37c801e1a8790b
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-07-16 18:25:16 -0500

    Skeleton for problem 704

---
+++ src/main/java/leetcode/Problem704.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/binary-search/
+ */
+public class Problem704 {
+    public int search(int[] nums, int target) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem704 prob = new Problem704();
+    }
+}
