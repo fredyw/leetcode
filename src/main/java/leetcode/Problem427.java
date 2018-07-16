6fe83b862eb1262a962ced3692cd253910f68b7c
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-07-16 17:24:24 -0500

    Solve problem 427

--- src/main/java/leetcode/Problem427.java
+++ src/main/java/leetcode/Problem427.java
@@ -28,18 +28,6 @@ public class Problem427 {
             bottomLeft = _bottomLeft;
             bottomRight = _bottomRight;
         }
-
-        @Override
-        public String toString() {
-            return "Node{" +
-                "val=" + val +
-                ", isLeaf=" + isLeaf +
-                ", topLeft=" + topLeft +
-                ", topRight=" + topRight +
-                ", bottomLeft=" + bottomLeft +
-                ", bottomRight=" + bottomRight +
-                '}';
-        }
     }
 
     public Node construct(int[][] grid) {
@@ -50,7 +38,6 @@ public class Problem427 {
     }
 
     private static Node construct(int[][] grid, int x1, int y1, int x2, int y2) {
-//        System.out.println(String.format("(%s, %s), (%s, %s)", x1, y1, x2, y2));
         if (x1 > x2 || y1 > y2) {
             return null;
         }
@@ -75,40 +62,18 @@ public class Problem427 {
         int nCol = y2 - y1;
         int r = (nRow + 1) / 2;
         int c = (nCol + 1) / 2;
-//        System.out.println(r + ", " + c);
-//        System.out.println(String.format("top left: (%s, %s), (%s, %s)", x1, y1, x1 + r - 1, y1 + c - 1));
-//        System.out.println(String.format("top right: (%s, %s), (%s, %s)", x1, y1 + c, x1 + r - 1, y2));
-//        System.out.println(String.format("bottom left: (%s, %s), (%s, %s)", x1 + r, y1, x2, y1 + c - 1));
-//        System.out.println(String.format("bottom right: (%s, %s), (%s, %s)", x1 + r, y1 + c, x2, y2));
-//        return null;
         Node topLeft = construct(grid, x1, y1, x1 + r - 1, y1 + c - 1);
         Node topRight = construct(grid, x1, y1 + c, x1 + r - 1, y2);
         Node bottomLeft = construct(grid, x1 + r, y1, x2, y1 + c - 1);
         Node bottomRight = construct(grid, x1 + r, y1 + c, x2, y2);
         if (topLeft != null && topRight != null && bottomLeft != null && bottomRight != null) {
-            if (topLeft.val == topRight.val &&
+            if (topLeft.isLeaf && topRight.isLeaf && bottomLeft.isLeaf && bottomRight.isLeaf &&
+                topLeft.val == topRight.val &&
                 topLeft.val == bottomLeft.val &&
                 topLeft.val == bottomRight.val) {
                 return new Node(topLeft.val, true, null, null, null, null);
             }
         }
-        return new Node(false, false, topLeft, topRight, bottomLeft, bottomRight);
-    }
-
-    public static void main(String[] args) {
-        Problem427 prob = new Problem427();
-        Node root = prob.construct(new int[][]{
-            {1, 1, 0, 0},
-            {1, 1, 0, 0},
-            {1, 0, 1, 1},
-            {0, 1, 1, 1}
-        });
-        System.out.println(root.topLeft);
-        System.out.println(root.topRight);
-        System.out.println(root.bottomLeft);
-        System.out.println(root.bottomRight);
-
-        // {"$id":"1","bottomLeft":{"$id":"8","bottomLeft":null,"bottomRight":null,"isLeaf":true,"topLeft":null,"topRight":null,"val":true},"bottomRight":{"$id":"9","bottomLeft":null,"bottomRight":null,"isLeaf":true,"topLeft":null,"topRight":null,"val":false},"isLeaf":false,"topLeft":{"$id":"2","bottomLeft":null,"bottomRight":null,"isLeaf":true,"topLeft":null,"topRight":null,"val":true},"topRight":{"$id":"3","bottomLeft":{"$id":"6","bottomLeft":null,"bottomRight":null,"isLeaf":true,"topLeft":null,"topRight":null,"val":true},"bottomRight":{"$id":"7","bottomLeft":null,"bottomRight":null,"isLeaf":true,"topLeft":null,"topRight":null,"val":true},"isLeaf":false,"topLeft":{"$id":"4","bottomLeft":null,"bottomRight":null,"isLeaf":true,"topLeft":null,"topRight":null,"val":false},"topRight":{"$id":"5","bottomLeft":null,"bottomRight":null,"isLeaf":true,"topLeft":null,"topRight":null,"val":false},"val":false},"val":false}
-        // {"$id":"1","bottomLeft":{"$id":"8","bottomLeft":null,"bottomRight":null,"isLeaf":true,"topLeft":null,"topRight":null,"val":true},"bottomRight":{"$id":"9","bottomLeft":null,"bottomRight":null,"isLeaf":true,"topLeft":null,"topRight":null,"val":false},"isLeaf":false,"topLeft":{"$id":"2","bottomLeft":null,"bottomRight":null,"isLeaf":true,"topLeft":null,"topRight":null,"val":true},"topRight":{"$id":"3","bottomLeft":{"$id":"6","bottomLeft":null,"bottomRight":null,"isLeaf":true,"topLeft":null,"topRight":null,"val":true},"bottomRight":{"$id":"7","bottomLeft":null,"bottomRight":null,"isLeaf":true,"topLeft":null,"topRight":null,"val":true},"isLeaf":false,"topLeft":{"$id":"4","bottomLeft":null,"bottomRight":null,"isLeaf":true,"topLeft":null,"topRight":null,"val":false},"topRight":{"$id":"5","bottomLeft":null,"bottomRight":null,"isLeaf":true,"topLeft":null,"topRight":null,"val":false},"val":true},"val":true}
+        return new Node(true, false, topLeft, topRight, bottomLeft, bottomRight);
     }
 }
