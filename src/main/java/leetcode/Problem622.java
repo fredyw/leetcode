8e748c00b4fdb6bf1301d628c6dd582e956cf871
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-07-15 21:42:35 -0500

    Solve problem 622

--- src/main/java/leetcode/Problem622.java
+++ src/main/java/leetcode/Problem622.java
@@ -1,63 +1,75 @@
 package leetcode;
 
+import java.util.LinkedList;
+
 /**
  * https://leetcode.com/problems/design-circular-queue/
  */
 public class Problem622 {
     private static class MyCircularQueue {
+        private final int size;
+        private final LinkedList<Integer> queue = new LinkedList<>();
 
         /** Initialize your data structure here. Set the size of the queue to be k. */
         public MyCircularQueue(int k) {
-            // TODO
+            size = k;
         }
 
         /** Insert an element into the circular queue. Return true if the operation is successful. */
         public boolean enQueue(int value) {
-            // TODO
-            return false;
+            if (isFull()) {
+                return false;
+            }
+            queue.add(value);
+            return true;
         }
 
         /** Delete an element from the circular queue. Return true if the operation is successful. */
         public boolean deQueue() {
-            // TODO
-            return false;
+            if (isEmpty()) {
+                return false;
+            }
+            queue.remove();
+            return true;
         }
 
         /** Get the front item from the queue. */
         public int Front() {
-            // TODO
-            return 0;
+            if (isEmpty()) {
+                return -1;
+            }
+            return queue.getFirst();
         }
 
         /** Get the last item from the queue. */
         public int Rear() {
-            // TODO
-            return 0;
+            if (isEmpty()) {
+                return -1;
+            }
+            return queue.getLast();
         }
 
         /** Checks whether the circular queue is empty or not. */
         public boolean isEmpty() {
-            // TODO
-            return false;
+            return queue.isEmpty();
         }
 
         /** Checks whether the circular queue is full or not. */
         public boolean isFull() {
-            // TODO
-            return false;
+            return queue.size() == size;
         }
     }
 
     public static void main(String[] args) {
         MyCircularQueue circularQueue = new MyCircularQueue(3); // set the size to be 3
-        circularQueue.enQueue(1);  // return true
-        circularQueue.enQueue(2);  // return true
-        circularQueue.enQueue(3);  // return true
-        circularQueue.enQueue(4);  // return false, the queue is full
-        circularQueue.Rear();  // return 3
-        circularQueue.isFull();  // return true
-        circularQueue.deQueue();  // return true
-        circularQueue.enQueue(4);  // return true
-        circularQueue.Rear();  // return 4
+        System.out.println(circularQueue.enQueue(1));  // return true
+        System.out.println(circularQueue.enQueue(2));  // return true
+        System.out.println(circularQueue.enQueue(3));  // return true
+        System.out.println(circularQueue.enQueue(4));  // return false, the queue is full
+        System.out.println(circularQueue.Rear());  // return 3
+        System.out.println(circularQueue.isFull());  // return true
+        System.out.println(circularQueue.deQueue());  // return true
+        System.out.println(circularQueue.enQueue(4));  // return true
+        System.out.println(circularQueue.Rear());  // return 4
     }
 }
