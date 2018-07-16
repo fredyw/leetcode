27e8d9b10aaabfaecee72a92b5e93822f97a0d8b
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-07-15 22:48:05 -0500

    Skeleton for problem 700

---
+++ src/main/java/leetcode/Problem700.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/search-in-a-binary-search-tree/
+ */
+public class Problem700 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem700 prob = new Problem700();
+    }
+}
