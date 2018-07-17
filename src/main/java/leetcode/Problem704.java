710709a76813f13c5e0baeae61b850659b73a758
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-07-16 21:44:44 -0500

    Solve problem 704

--- src/main/java/leetcode/Problem704.java
+++ src/main/java/leetcode/Problem704.java
@@ -5,11 +5,18 @@ package leetcode;
  */
 public class Problem704 {
     public int search(int[] nums, int target) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem704 prob = new Problem704();
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
     }
 }
