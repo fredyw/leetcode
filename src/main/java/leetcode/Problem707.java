8288276778475104e2da4ff3ecff632900b46da0
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-07-20 00:22:53 -0500

    Update problem 707

--- src/main/java/leetcode/Problem707.java
+++ src/main/java/leetcode/Problem707.java
@@ -5,19 +5,35 @@ package leetcode;
  */
 public class Problem707 {
     private static class MyLinkedList {
+        private final static class Node {
+            private final int value;
+            private Node next;
+
+            public Node(int value) {
+                this.value = value;
+            }
+        }
+
+        private Node head;
+        private Node tail;
 
         /** Initialize your data structure here. */
         public MyLinkedList() {
-            // TODO
         }
 
-        /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
+        /**
+         * Get the value of the index-th node in the linked list. If the index is invalid,
+         * return -1.
+         */
         public int get(int index) {
             // TODO
             return 0;
         }
 
-        /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
+        /**
+         * Add a node of value val before the first element of the linked list.
+         * After the insertion, the new node will be the first node of the linked list.
+         */
         public void addAtHead(int val) {
             // TODO
         }
@@ -27,7 +43,12 @@ public class Problem707 {
             // TODO
         }
 
-        /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
+        /**
+         * Add a node of value val before the index-th node in the linked list.
+         * If index equals to the length of linked list, the node will be appended
+         * to the end of linked list. If index is greater than the length, the node
+         * will not be inserted.
+         */
         public void addAtIndex(int index, int val) {
             // TODO
         }
