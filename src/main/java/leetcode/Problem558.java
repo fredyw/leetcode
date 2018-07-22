2b7c4c86572135b6dbb83b13fed5c21c7e94130d
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-07-22 18:39:01 -0500

    Update problem 558

--- src/main/java/leetcode/Problem558.java
+++ src/main/java/leetcode/Problem558.java
@@ -28,26 +28,54 @@ public class Problem558 {
             bottomLeft = _bottomLeft;
             bottomRight = _bottomRight;
         }
+
+        @Override
+        public String toString() {
+            return "" + val;
+        }
     }
 
     public Node intersect(Node quadTree1, Node quadTree2) {
         if (quadTree1.isLeaf && quadTree2.isLeaf) {
-            Node topLeft = intersect(quadTree1.topLeft, quadTree2.topLeft);
-            Node topRight = intersect(quadTree1.topRight, quadTree2.topRight);
-            Node bottomLeft = intersect(quadTree1.bottomLeft, quadTree2.bottomLeft);
-            Node bottomRight = intersect(quadTree1.bottomRight, quadTree2.bottomRight);
-            return new Node(false, true, topLeft, topRight, bottomLeft, bottomRight);
-        } else if (!quadTree1.isLeaf) {
+            return new Node(quadTree1.val | quadTree2.val, true, null, null, null, null);
+        } else if (!quadTree1.isLeaf && quadTree2.isLeaf) {
             boolean val = quadTree1.topLeft.val | quadTree1.topRight.val |
                 quadTree1.bottomLeft.val | quadTree1.bottomRight.val;
-            return new Node(val | quadTree2.val, false, null, null, null, null);
+            return new Node(val | quadTree2.val, true, null, null, null, null);
+        } else if (quadTree1.isLeaf && !quadTree2.isLeaf) {
+            boolean val = quadTree2.topLeft.val | quadTree2.topRight.val |
+                quadTree2.bottomLeft.val | quadTree2.bottomRight.val;
+            return new Node(quadTree1.val | val, true, null, null, null, null);
         }
-        boolean val = quadTree2.topLeft.val | quadTree2.topRight.val |
-            quadTree2.bottomLeft.val | quadTree2.bottomRight.val;
-        return new Node(quadTree1.val | val, false, null, null, null, null);
+        Node topLeft = intersect(quadTree1.topLeft, quadTree2.topLeft);
+        Node topRight = intersect(quadTree1.topRight, quadTree2.topRight);
+        Node bottomLeft = intersect(quadTree1.bottomLeft, quadTree2.bottomLeft);
+        Node bottomRight = intersect(quadTree1.bottomRight, quadTree2.bottomRight);
+        return new Node(false, false, topLeft, topRight, bottomLeft, bottomRight);
     }
 
     public static void main(String[] args) {
         Problem558 prob = new Problem558();
+
+        Node tree1 = new Node(true, false,
+            new Node(true, true, null, null, null, null),
+            new Node(true, true, null, null, null, null),
+            new Node(false, true, null, null, null, null),
+            new Node(false, true, null, null, null, null));
+        Node tree2 = new Node(true, false,
+            new Node(true, true, null, null, null, null),
+            new Node(true, false,
+                new Node(false, true, null, null, null, null),
+                new Node(false, true, null, null, null, null),
+                new Node(true, true, null, null, null, null),
+                new Node(true, true, null, null, null, null)),
+            new Node(true, true, null, null, null, null),
+            new Node(false, true, null, null, null, null));
+
+        Node tree = prob.intersect(tree1, tree2);
+        System.out.println(tree.topLeft); // true
+        System.out.println(tree.topRight); // true
+        System.out.println(tree.bottomLeft); // true
+        System.out.println(tree.bottomRight); // false
     }
 }
