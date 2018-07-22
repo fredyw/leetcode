51614c04268914b7da80987472494cd7a554f338
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-07-22 00:12:37 -0500

    Skeleton for problem 872

---
+++ src/main/java/leetcode/Problem872.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/leaf-similar-trees/
+ */
+public class Problem872 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem872 prob = new Problem872();
+    }
+}
