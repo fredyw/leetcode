fb81594aabd0d23bb84a459ca2a2ef8311367ef9
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-07-22 22:14:01 -0500

    Solve problem 558

--- src/main/java/leetcode/Problem558.java
+++ src/main/java/leetcode/Problem558.java
@@ -28,54 +28,27 @@ public class Problem558 {
             bottomLeft = _bottomLeft;
             bottomRight = _bottomRight;
         }
-
-        @Override
-        public String toString() {
-            return "" + val;
-        }
     }
 
     public Node intersect(Node quadTree1, Node quadTree2) {
         if (quadTree1.isLeaf && quadTree2.isLeaf) {
             return new Node(quadTree1.val | quadTree2.val, true, null, null, null, null);
         } else if (!quadTree1.isLeaf && quadTree2.isLeaf) {
-            boolean val = quadTree1.topLeft.val | quadTree1.topRight.val |
-                quadTree1.bottomLeft.val | quadTree1.bottomRight.val;
-            return new Node(val | quadTree2.val, true, null, null, null, null);
+            return quadTree2.val ? quadTree2 : quadTree1;
         } else if (quadTree1.isLeaf && !quadTree2.isLeaf) {
-            boolean val = quadTree2.topLeft.val | quadTree2.topRight.val |
-                quadTree2.bottomLeft.val | quadTree2.bottomRight.val;
-            return new Node(quadTree1.val | val, true, null, null, null, null);
+            return quadTree1.val ? quadTree1 : quadTree2;
         }
         Node topLeft = intersect(quadTree1.topLeft, quadTree2.topLeft);
         Node topRight = intersect(quadTree1.topRight, quadTree2.topRight);
         Node bottomLeft = intersect(quadTree1.bottomLeft, quadTree2.bottomLeft);
         Node bottomRight = intersect(quadTree1.bottomRight, quadTree2.bottomRight);
-        return new Node(false, false, topLeft, topRight, bottomLeft, bottomRight);
-    }
-
-    public static void main(String[] args) {
-        Problem558 prob = new Problem558();
-
-        Node tree1 = new Node(true, false,
-            new Node(true, true, null, null, null, null),
-            new Node(true, true, null, null, null, null),
-            new Node(false, true, null, null, null, null),
-            new Node(false, true, null, null, null, null));
-        Node tree2 = new Node(true, false,
-            new Node(true, true, null, null, null, null),
-            new Node(true, false,
-                new Node(false, true, null, null, null, null),
-                new Node(false, true, null, null, null, null),
-                new Node(true, true, null, null, null, null),
-                new Node(true, true, null, null, null, null)),
-            new Node(true, true, null, null, null, null),
-            new Node(false, true, null, null, null, null));
-
-        Node tree = prob.intersect(tree1, tree2);
-        System.out.println(tree.topLeft); // true
-        System.out.println(tree.topRight); // true
-        System.out.println(tree.bottomLeft); // true
-        System.out.println(tree.bottomRight); // false
+        if (topLeft.isLeaf && topRight.isLeaf &&
+            bottomLeft.isLeaf && bottomRight.isLeaf &&
+            topLeft.val == topRight.val &&
+            topLeft.val == bottomLeft.val &&
+            topLeft.val == bottomRight.val) {
+            return new Node(topLeft.val, true, null, null, null, null);
+        }
+        return new Node(true, false, topLeft, topRight, bottomLeft, bottomRight);
     }
 }
