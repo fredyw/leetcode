baba82cf76d98f56bbcc4e020ed9873b5976f7b3
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-07-25 00:30:31 -0500

    Skeleton for problem 874

---
+++ src/main/java/leetcode/Problem874.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/walking-robot-simulation/
+ */
+public class Problem874 {
+    public int robotSim(int[] commands, int[][] obstacles) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem874 prob = new Problem874();
+    }
+}
