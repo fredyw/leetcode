84cd45652e7046ac5ae56769fe0da4dbeb846ba5
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-07-26 00:50:28 -0500

    Solve problem 874

--- src/main/java/leetcode/Problem874.java
+++ src/main/java/leetcode/Problem874.java
@@ -1,19 +1,71 @@
 package leetcode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * https://leetcode.com/problems/walking-robot-simulation/
  */
 public class Problem874 {
     public int robotSim(int[] commands, int[][] obstacles) {
-        // TODO
-        return 0;
+        int max = 0;
+        Set<String> set = new HashSet<>();
+        for (int[] obstacle : obstacles) {
+            set.add(key(obstacle[0], obstacle[1]));
+        }
+        int x = 0;
+        int y = 0;
+        Direction direction = Direction.UP;
+        for (int cmd : commands) {
+            if (cmd == -2 || cmd == -1) {
+                direction = newDirection(direction, cmd);
+            } else {
+                for (int i = 0; i < cmd; i++) {
+                    if (direction == Direction.UP) {
+                        if (set.contains(key(x, y + 1))) {
+                            break;
+                        }
+                        y++;
+                    } else if (direction == Direction.LEFT) {
+                        if (set.contains(key(x - 1, y))) {
+                            break;
+                        }
+                        x--;
+                    } else if (direction == Direction.DOWN) {
+                        if (set.contains(key(x, y - 1))) {
+                            break;
+                        }
+                        y--;
+                    } else { // Direction.RIGHT
+                        if (set.contains(key(x + 1, y))) {
+                            break;
+                        }
+                        x++;
+                    }
+                }
+                max = Math.max(max, (x * x) + (y * y));
+            }
+        }
+        return max;
     }
 
-    public static void main(String[] args) {
-        Problem874 prob = new Problem874();
-        System.out.println(prob.robotSim(new int[]{4, -1, 3}, new int[][]{})); // 25
-        System.out.println(prob.robotSim(new int[]{4, -1, 4, -2, 4}, new int[][]{
-            {2, 4}
-        })); // 65
+    private enum Direction {
+        LEFT, RIGHT, UP, DOWN
+    }
+
+    private static Direction newDirection(Direction current, int cmd) {
+        if (current == Direction.UP) {
+            return (cmd == -2) ? Direction.LEFT : Direction.RIGHT;
+        } else if (current == Direction.LEFT) {
+            return (cmd == -2) ? Direction.DOWN : Direction.UP;
+        } else if (current == Direction.DOWN) {
+            return (cmd == -2) ? Direction.RIGHT : Direction.LEFT;
+        } else { // Direction.RIGHT
+            return (cmd == -2) ? Direction.UP : Direction.DOWN;
+        }
+    }
+
+    private static String key(int x, int y) {
+        return x + "|" + y;
     }
 }
