ac7a95e13a0df22a8da488e0e8ca31f4edb1b113
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-07-29 00:51:00 -0500

    Skeleton for problem 701

---
+++ src/main/java/leetcode/Problem701.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/insert-into-a-binary-search-tree/
+ */
+public class Problem701 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem701 prob = new Problem701();
+    }
+}
