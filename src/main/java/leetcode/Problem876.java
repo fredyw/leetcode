d426be3980c727bca0ef785a7caa8f1d77d48669
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-07-29 00:37:41 -0500

    Skeleton for problem 876

---
+++ src/main/java/leetcode/Problem876.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/middle-of-the-linked-list/
+ */
+public class Problem876 {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode middleNode(ListNode head) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem876 prob = new Problem876();
+    }
+}
