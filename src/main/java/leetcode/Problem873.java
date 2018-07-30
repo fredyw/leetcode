6488e84cfb00590e64a45a71e659d556662029d0
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-07-29 21:28:16 -0500

    Skeleton for problem 873

---
+++ src/main/java/leetcode/Problem873.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/length-of-longest-fibonacci-subsequence/
+ */
+public class Problem873 {
+    public int lenLongestFibSubseq(int[] A) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem873 prob = new Problem873();
+    }
+}
