e183023282c0c55b34d9728c65f5d7cedfb0cff1
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-08-05 00:04:04 -0500

    Skeleton for problem 126

---
+++ src/main/java/leetcode/Problem126.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/word-ladder-ii/
+ */
+public class Problem126 {
+    public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem126 prob = new Problem126();
+    }
+}
