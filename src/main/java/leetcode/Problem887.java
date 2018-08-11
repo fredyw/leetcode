b0d6e7d4ca774cdb24b172feb20dfb09d7eaacad
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-08-10 21:53:49 -0500

    Skeleton for problem 887

---
+++ src/main/java/leetcode/Problem887.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/projection-area-of-3d-shapes/
+ */
+public class Problem887 {
+    public int projectionArea(int[][] grid) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem887 prob = new Problem887();
+    }
+}
