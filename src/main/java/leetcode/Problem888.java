c501a1b222679076b4576a009f2fbaceacc8d02c
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-08-18 22:35:25 -0500

    Skeleton for problem 888

---
+++ src/main/java/leetcode/Problem888.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/fair-candy-swap/
+ */
+public class Problem888 {
+    public int[] fairCandySwap(int[] A, int[] B) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem888 prob = new Problem888();
+    }
+}
