02a6c3e3ea34eacd9ab48fed0ea2be5bb1aeb328
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-08-18 23:36:02 -0500

    Solve problem 890

--- src/main/java/leetcode/Problem890.java
+++ src/main/java/leetcode/Problem890.java
@@ -1,5 +1,6 @@
 package leetcode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -7,11 +8,29 @@ import java.util.List;
  */
 public class Problem890 {
     public List<String> findAndReplacePattern(String[] words, String pattern) {
-        // TODO
-        return null;
+        List<String> result = new ArrayList<>();
+        String p = convert(pattern);
+        for (String word : words) {
+            String w = convert(word);
+            if (p.equals(w)) {
+                result.add(word);
+            }
+        }
+        return result;
     }
 
-    public static void main(String[] args) {
-        Problem890 prob = new Problem890();
+    private static String convert(String s) {
+        StringBuilder sb = new StringBuilder();
+        int i = 1;
+        int[] chars = new int[26];
+        for (char c : s.toCharArray()) {
+            if (chars[c - 'a'] == 0) {
+                sb.append(i);
+                chars[c - 'a'] = i++;
+            } else {
+                sb.append(chars[c - 'a']);
+            }
+        }
+        return sb.toString();
     }
 }
