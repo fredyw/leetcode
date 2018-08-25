ef5d41511df02a5557ecaa4d6808653a9f09722b
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-08-25 00:57:34 -0500

    Sovle problem 873

--- src/main/java/leetcode/Problem873.java
+++ src/main/java/leetcode/Problem873.java
@@ -1,15 +1,22 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * https://leetcode.com/problems/length-of-longest-fibonacci-subsequence/
  */
 public class Problem873 {
     public int lenLongestFibSubseq(int[] A) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < A.length; i++) {
+            map.put(A[i], i);
+        }
         int max = 0;
         Integer[][] memo = new Integer[A.length][A.length];
         for (int i = 0; i < A.length; i++) {
             for (int j = i + 1; j < A.length; j++) {
-                int val = lenLongestFibSubseq(A, i, j, memo);
+                int val = lenLongestFibSubseq(A, i, j, map, memo);
                 if (val > 0) {
                     val += 2;
                 }
@@ -19,27 +26,18 @@ public class Problem873 {
         return max;
     }
 
-    private int lenLongestFibSubseq(int[] a, int i, int j, Integer[][] memo) {
+    private int lenLongestFibSubseq(int[] a, int i, int j, Map<Integer, Integer> map,
+                                    Integer[][] memo) {
         if (memo[i][j] != null) {
             return memo[i][j];
         }
         int sum = a[i] + a[j];
-        int x = j + 1;
-        while (x < a.length && a[x] <= sum) {
-            if (a[x] == sum) {
-                int val = lenLongestFibSubseq(a, j, x, memo) + 1;
-                memo[i][j] = val;
-                return val;
-            }
-            x++;
+        int max = 0;
+        Integer index = map.get(sum);
+        if (index != null) {
+            max = lenLongestFibSubseq(a, j, index, map, memo) + 1;
+            memo[i][j] = max;
         }
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem873 prob = new Problem873();
-        System.out.println(prob.lenLongestFibSubseq(new int[]{1, 2, 3, 4, 5, 6, 7, 8})); // 5
-        System.out.println(prob.lenLongestFibSubseq(new int[]{1, 3, 7, 11, 12, 14, 18})); // 3
-        System.out.println(prob.lenLongestFibSubseq(new int[]{1, 3, 5})); // 0
+        return max;
     }
 }
