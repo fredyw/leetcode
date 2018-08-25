9f187af43c33360e083b68eb9c23f6a85abe32df
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-08-25 00:59:07 -0500

    Skeleton for problem 885

---
+++ src/main/java/leetcode/Problem885.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/spiral-matrix-iii/
+ */
+public class Problem885 {
+    public int[][] spiralMatrixIII(int R, int C, int r0, int c0) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem885 prob = new Problem885();
+    }
+}
