037a47aab41d8d391d8d608cb9a373bd5e70726d
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-08-24 23:18:06 -0500

    Update problem 873

--- src/main/java/leetcode/Problem873.java
+++ src/main/java/leetcode/Problem873.java
@@ -5,11 +5,12 @@ package leetcode;
  */
 public class Problem873 {
     public int lenLongestFibSubseq(int[] A) {
-        // TODO
         return 0;
     }
 
     public static void main(String[] args) {
         Problem873 prob = new Problem873();
+        System.out.println(prob.lenLongestFibSubseq(new int[]{1, 2, 3, 4, 5, 6, 7, 8})); // 5
+        System.out.println(prob.lenLongestFibSubseq(new int[]{1, 3, 7, 11, 12, 14, 18})); // 3
     }
 }
