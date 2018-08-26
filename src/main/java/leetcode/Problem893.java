2bc5955569f281c0415f11f274098955a6f85037
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-08-26 00:37:33 -0500

    Skeleton for problem 893

---
+++ src/main/java/leetcode/Problem893.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/groups-of-special-equivalent-strings/
+ */
+public class Problem893 {
+    public int numSpecialEquivGroups(String[] A) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
