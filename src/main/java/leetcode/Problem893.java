48c3f27afec6f73af1cf858041f1c4ffc1929273
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-08-26 23:23:40 -0500

    Update problem 893

--- src/main/java/leetcode/Problem893.java
+++ src/main/java/leetcode/Problem893.java
@@ -10,6 +10,10 @@ public class Problem893 {
     }
 
     public static void main(String[] args) {
-
+        Problem893 prob = new Problem893();
+        System.out.println(prob.numSpecialEquivGroups(new String[]{"a","b","c","a","c","c"})); // 3
+        System.out.println(prob.numSpecialEquivGroups(new String[]{"aa","bb","ab","ba"})); // 4
+        System.out.println(prob.numSpecialEquivGroups(new String[]{"abc","acb","bac","bca","cab","cba"})); // 3
+        System.out.println(prob.numSpecialEquivGroups(new String[]{"abcd","cdab","adcb","cbad"})); // 1
     }
 }
