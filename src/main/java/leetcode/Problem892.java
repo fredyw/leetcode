406b2e4e74751154fa86f965fad4d85ea0052078
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-08-27 00:23:32 -0500

    Skeleton for problem 892

---
+++ src/main/java/leetcode/Problem892.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/surface-area-of-3d-shapes/
+ */
+public class Problem892 {
+    public int surfaceArea(int[][] grid) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem892 prob = new Problem892();
+    }
+}
