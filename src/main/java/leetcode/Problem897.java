d2ff7bba627ba5aa57c57d0258eaaa7a1565b743
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-09-07 09:26:41 +0800

    Update prolem 897

--- src/main/java/leetcode/Problem897.java
+++ src/main/java/leetcode/Problem897.java
@@ -20,6 +20,17 @@ public class Problem897 {
     }
 
     public static void main(String[] args) {
-        
+        Problem897 prob = new Problem897();
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(3);
+        root.left.left = new TreeNode(2);
+        root.left.right = new TreeNode(4);
+        root.left.left.left = new TreeNode(1);
+        root.right = new TreeNode(6);
+        root.right.right = new TreeNode(8);
+        root.right.right.left = new TreeNode(7);
+        root.right.right.right = new TreeNode(9);
+
+        System.out.println(prob.increasingBST(root));
     }
 }
