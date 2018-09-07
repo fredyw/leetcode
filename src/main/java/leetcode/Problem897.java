f7d0dfb0468b95d3e754459a57c110ea96da74cd
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-09-07 09:19:17 +0800

    Skeleton for problem 897

---
+++ src/main/java/leetcode/Problem897.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/increasing-order-search-tree/
+ */
+public class Problem897 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode increasingBST(TreeNode root) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
