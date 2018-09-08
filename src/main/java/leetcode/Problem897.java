8ce9cc58c3fa7493aefe6004a79a1467ba558bd6
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-09-08 08:59:54 +0800

    Solve problem 897

--- src/main/java/leetcode/Problem897.java
+++ src/main/java/leetcode/Problem897.java
@@ -14,23 +14,6 @@ public class Problem897 {
         }
     }
 
-//    TreeNode cur;
-//    public TreeNode increasingBST(TreeNode root) {
-//        TreeNode ans = new TreeNode(0);
-//        cur = ans;
-//        inorder(root);
-//        return ans.right;
-//    }
-//
-//    public void inorder(TreeNode node) {
-//        if (node == null) return;
-//        inorder(node.left);
-//        node.left = null;
-//        cur = node;
-//        cur.right = cur;
-//        inorder(node.right);
-//    }
-
     public TreeNode increasingBST(TreeNode root) {
         TreeNodeRef ref = new TreeNodeRef();
         increasingBST(root, ref);
@@ -47,39 +30,14 @@ public class Problem897 {
             return;
         }
         increasingBST(node.left, ref);
+        node.left = null;
         if (ref.root == null) {
             ref.root = node;
             ref.node = node;
         } else {
-            ref.node.left = null;
             ref.node.right = node;
             ref.node = node;
         }
         increasingBST(node.right, ref);
-    }
-
-    private static void print(TreeNode node) {
-        for (TreeNode n = node; n != null; n = n.right) {
-            if (n.left != null) {
-                throw new RuntimeException("Wrong!");
-            }
-            System.out.println(n.val);
-        }
-    }
-
-    public static void main(String[] args) {
-        Problem897 prob = new Problem897();
-        TreeNode root = new TreeNode(5);
-        root.left = new TreeNode(3);
-        root.left.left = new TreeNode(2);
-        root.left.right = new TreeNode(4);
-        root.left.left.left = new TreeNode(1);
-        root.right = new TreeNode(6);
-        root.right.right = new TreeNode(8);
-        root.right.right.left = new TreeNode(7);
-        root.right.right.right = new TreeNode(9);
-
-        print(prob.increasingBST(root));
-        print(prob.increasingBST(new TreeNode(1)));
     }
 }
