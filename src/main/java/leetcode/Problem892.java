c965260bbb452b1e02a1a0d55feae8a2a1ad4e79
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-09-14 17:42:41 +0700

    Update problem 892

--- src/main/java/leetcode/Problem892.java
+++ src/main/java/leetcode/Problem892.java
@@ -5,35 +5,48 @@ package leetcode;
  */
 public class Problem892 {
     public int surfaceArea(int[][] grid) {
-        // TODO
-        return 0;
+        int result = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                result += 4 * grid[i][j];
+                if (j - 1 >= 0) {
+                    int diff = Math.abs(grid[i][j - 1] - grid[i][j]);
+                    result -= 2 * diff;
+                }
+                if (i - 1 >= 0) {
+                    int diff = Math.abs(grid[i - 1][j] - grid[i][j]);
+                    result -= 2 * diff;
+                }
+            }
+        }
+        return result;
     }
 
     public static void main(String[] args) {
         Problem892 prob = new Problem892();
-        System.out.println(prob.surfaceArea(new int[][]{
-            {1}
-        })); // 6
-        System.out.println(prob.surfaceArea(new int[][]{
-            {2}
-        })); // 10
-        System.out.println(prob.surfaceArea(new int[][]{
-            {3}
-        })); // 14
+//        System.out.println(prob.surfaceArea(new int[][]{
+//            {1}
+//        })); // 6
+//        System.out.println(prob.surfaceArea(new int[][]{
+//            {2}
+//        })); // 10
+//        System.out.println(prob.surfaceArea(new int[][]{
+//            {3}
+//        })); // 14
         System.out.println(prob.surfaceArea(new int[][]{
             {1, 2}, {3, 4}
-        })); // 10
-        System.out.println(prob.surfaceArea(new int[][]{
-            {1, 1}, {1, 1}
-        })); // 16
-        System.out.println(prob.surfaceArea(new int[][]{
-            {1, 0}, {0, 2}
-        })); // 10
-        System.out.println(prob.surfaceArea(new int[][]{
-            {1, 1, 1}, {1, 0, 1}, {1, 1, 1}
-        })); // 10
-        System.out.println(prob.surfaceArea(new int[][]{
-            {2, 2, 2}, {2, 1, 2}, {2, 2, 2}
-        })); // 10
+        })); // 34
+//        System.out.println(prob.surfaceArea(new int[][]{
+//            {1, 1}, {1, 1}
+//        })); // 16
+//        System.out.println(prob.surfaceArea(new int[][]{
+//            {1, 0}, {0, 2}
+//        })); // 16
+//        System.out.println(prob.surfaceArea(new int[][]{
+//            {1, 1, 1}, {1, 0, 1}, {1, 1, 1}
+//        })); // 32
+//        System.out.println(prob.surfaceArea(new int[][]{
+//            {2, 2, 2}, {2, 1, 2}, {2, 2, 2}
+//        })); // 46
     }
 }
