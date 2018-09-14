413aaecf291d4b8a307209c7af8fc9e5cfc04fa4
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-09-14 18:13:26 +0700

    Skeleton for problem 894

---
+++ src/main/java/leetcode/Problem894.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/all-possible-full-binary-trees/
+ */
+public class Problem894 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<TreeNode> allPossibleFBT(int N) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem894 prob = new Problem894();
+    }
+}
