447098c3f4985b3484e05d0a3ed600b98cf84578
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-09-20 11:34:59 -0700

    Skeleton for problem 905

---
+++ src/main/java/leetcode/Problem905.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/sort-array-by-parity/
+ */
+public class Problem905 {
+    public int[] sortArrayByParity(int[] A) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem905 prob = new Problem905();
+        System.out.println(Arrays.toString(prob.sortArrayByParity(new int[]{3, 1, 2, 4}))); // [2,4,3,1]
+    }
+}
