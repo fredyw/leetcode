b5a4de8bb2daf5ce4cc3b513c6db65f71acb8ce1
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-09-24 22:51:12 -0500

    Skeleton for problem 908

---
+++ src/main/java/leetcode/Problem908.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/smallest-range-i/description/
+ */
+public class Problem908 {
+    public int smallestRangeI(int[] A, int K) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem908 prob = new Problem908();
+    }
+}
