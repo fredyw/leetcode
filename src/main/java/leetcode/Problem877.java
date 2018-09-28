6211435eb4a9daa03fe7e66836a30532c772d292
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-09-27 23:18:48 -0500

    Skeleton for problem 877

---
+++ src/main/java/leetcode/Problem877.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/stone-game/
+ */
+public class Problem877 {
+    public boolean stoneGame(int[] piles) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem877 prob = new Problem877();
+    }
+}
