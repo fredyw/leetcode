0427c176d3d1aa35c76634e0d2f2328dfe7f5fa3
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-09-29 09:13:21 -0500

    Update problem 869

--- src/main/java/leetcode/Problem869.java
+++ src/main/java/leetcode/Problem869.java
@@ -11,5 +11,10 @@ public class Problem869 {
 
     public static void main(String[] args) {
         Problem869 prob = new Problem869();
+        System.out.println(prob.reorderedPowerOf2(1)); // true
+        System.out.println(prob.reorderedPowerOf2(10)); // false
+        System.out.println(prob.reorderedPowerOf2(16)); // true
+        System.out.println(prob.reorderedPowerOf2(24)); // false
+        System.out.println(prob.reorderedPowerOf2(46)); // true
     }
 }
