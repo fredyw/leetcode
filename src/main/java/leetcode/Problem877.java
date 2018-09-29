92e10af6a9f20434f7859c6320b4c06bd37f9236
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-09-28 19:34:54 -0500

    Update problem 877

--- src/main/java/leetcode/Problem877.java
+++ src/main/java/leetcode/Problem877.java
@@ -18,17 +18,18 @@ public class Problem877 {
         if (i >= j) {
             return 0;
         }
-        int val = stoneGame(piles, i + 1, j - 1);
-        int a = val + piles[i];
-        int b = val + piles[j];
-        return Math.max(a, b);
+        int a = stoneGame(piles, i + 2, j) + piles[i];
+        int b = stoneGame(piles, i, j - 2) + piles[j];
+        int c = stoneGame(piles, i + 1, j - 1) + piles[i];
+        int d = stoneGame(piles, i + 1, j - 1) + piles[j];
+        return Math.max(a, Math.max(Math.max(b, c), d));
     }
 
     public static void main(String[] args) {
         Problem877 prob = new Problem877();
         System.out.println(prob.stoneGame(new int[]{5, 3, 4, 5})); // true
-//        System.out.println(prob.stoneGame(new int[]{5, 3, 4, 6})); // true
-//        System.out.println(prob.stoneGame(new int[]{1, 1, 1, 1})); // true
-//        System.out.println(prob.stoneGame(new int[]{7, 3, 8, 2, 9, 10})); // true
+        System.out.println(prob.stoneGame(new int[]{5, 3, 4, 6})); // true
+        System.out.println(prob.stoneGame(new int[]{1, 1, 1, 1})); // true
+        System.out.println(prob.stoneGame(new int[]{7, 3, 8, 2, 9, 10})); // true
     }
 }
