35819a99ae33693c50964aa9391040ceccca07cd
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-09-29 10:34:50 -0500

    Update problem 869

--- src/main/java/leetcode/Problem869.java
+++ src/main/java/leetcode/Problem869.java
@@ -9,33 +9,34 @@ public class Problem869 {
             return true;
         }
         String s = "" + N;
-        char[] chars = s.toCharArray();
-        return reorderedPowerOf2(chars, 0);
+        return reorderedPowerOf2(s, 0);
     }
 
-    private static boolean reorderedPowerOf2(char[] chars, int idx) {
-        if (idx >= chars.length) {
+    private static boolean reorderedPowerOf2(String s, int idx) {
+        if (idx >= s.length()) {
             return false;
         }
-        for (int i = 0; i < chars.length; i++) {
+        for (int i = 0; i < s.length(); i++) {
             if (i == idx) {
                 continue;
             }
-            swap(chars, idx, i);
-            String newString = new String(chars);
+            String newString = newString(s, i);
+            if (newString.startsWith("0")) {
+                continue;
+            }
             int n = Integer.parseInt(newString);
-
+            if (isPower2(n)) {
+                return true;
+            }
             System.out.println(newString);
-            // Backtrack.
-            swap(chars, idx, i);
         }
-        return reorderedPowerOf2(chars, idx + 1);
+        return reorderedPowerOf2(s, idx + 1);
     }
 
-    private static void swap(char[] chars, int i, int j) {
-        char tmp = chars[i];
-        chars[i] = chars[j];
-        chars[j] = tmp;
+    private static String newString(String s, int i) {
+        String left = (i - 1 >= 0) ? s.substring(0, i) : "";
+        String right = (i + 1 < s.length()) ? s.substring(i + 1) : "";
+        return s.charAt(i) + left + right;
     }
 
     private static boolean isPower2(int n) {
@@ -51,6 +52,9 @@ public class Problem869 {
 //        System.out.println(prob.reorderedPowerOf2(16)); // true
 //        System.out.println(prob.reorderedPowerOf2(24)); // false
 //        System.out.println(prob.reorderedPowerOf2(46)); // true
-        System.out.println(prob.reorderedPowerOf2(562)); // true
+//        System.out.println(prob.reorderedPowerOf2(562)); // true
+//        System.out.println(prob.reorderedPowerOf2(821)); // false
+        System.out.println(prob.reorderedPowerOf2(152)); // true
+//        System.out.println(prob.reorderedPowerOf2(512)); // true
     }
 }
