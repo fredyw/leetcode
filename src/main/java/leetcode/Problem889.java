659b07662c96158660b37edac58c0196cf73a406
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-09-29 09:03:12 -0500

    Skeleton for problem 889

---
+++ src/main/java/leetcode/Problem889.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/construct-binary-tree-from-preorder-and-postorder-traversal/
+ */
+public class Problem889 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode constructFromPrePost(int[] pre, int[] post) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem889 prob = new Problem889();
+    }
+}
