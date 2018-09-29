897ef2e2f983522dea2e4aa2564c40171803242c
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-09-29 09:04:48 -0500

    Skeleton for problem 869

---
+++ src/main/java/leetcode/Problem869.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/reordered-power-of-2/
+ */
+public class Problem869 {
+    public boolean reorderedPowerOf2(int N) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem869 prob = new Problem869();
+    }
+}
