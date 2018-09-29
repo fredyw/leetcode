f41e31393e2ed7309e97f29a5a6137bcd8a242c4
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-09-29 11:50:44 -0500

    Solve problem 869

--- src/main/java/leetcode/Problem869.java
+++ src/main/java/leetcode/Problem869.java
@@ -14,12 +14,10 @@ public class Problem869 {
 
     private static boolean reorderedPowerOf2(char[] chars, int idx) {
         if (idx == chars.length) {
-            String newString = new String(chars);
-//            System.out.println(newString);
-            if (newString.startsWith("0")) {
+            if (chars[0] == '0') {
                 return false;
             }
-            int n = Integer.parseInt(newString);
+            long n = Long.parseLong(new String(chars));
             return isPower2(n);
         }
         for (int i = idx; i < chars.length; i++) {
@@ -39,26 +37,11 @@ public class Problem869 {
         chars[j] = tmp;
     }
 
-    private static double log2(int n) {
-        return Math.log(n) / Math.log(2);
+    private static double log2(long n) {
+        return Math.log10(n) / Math.log10(2);
     }
 
-    private static boolean isPower2(int n) {
-        return (Math.ceil(log2(n)) == Math.floor(log2(n)));
-    }
-
-    public static void main(String[] args) {
-        Problem869 prob = new Problem869();
-//        System.out.println(prob.reorderedPowerOf2(1)); // true
-//        System.out.println(prob.reorderedPowerOf2(10)); // false
-//        System.out.println(prob.reorderedPowerOf2(16)); // true
-//        System.out.println(prob.reorderedPowerOf2(24)); // false
-//        System.out.println(prob.reorderedPowerOf2(46)); // true
-//        System.out.println(prob.reorderedPowerOf2(562)); // true
-//        System.out.println(prob.reorderedPowerOf2(821)); // true
-//        System.out.println(prob.reorderedPowerOf2(128)); // true
-//        System.out.println(prob.reorderedPowerOf2(152)); // true
-//        System.out.println(prob.reorderedPowerOf2(512)); // true
-        System.out.println(prob.reorderedPowerOf2(679213508)); // true
+    private static boolean isPower2(long n) {
+        return Math.ceil(log2(n)) == Math.floor(log2(n));
     }
 }
