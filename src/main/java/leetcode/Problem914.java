371855a696970a72755d78cc75fa0badfef5b556
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-09-30 21:45:58 -0500

    Update problem 914

--- src/main/java/leetcode/Problem914.java
+++ src/main/java/leetcode/Problem914.java
@@ -11,5 +11,11 @@ public class Problem914 {
 
     public static void main(String[] args) {
         Problem914 prob = new Problem914();
+        System.out.println(prob.hasGroupsSizeX(new int[]{1, 2, 3, 4, 4, 3, 2, 1})); // true
+        System.out.println(prob.hasGroupsSizeX(new int[]{1, 1, 1, 2, 2, 2, 3, 3})); // false
+        System.out.println(prob.hasGroupsSizeX(new int[]{1})); // false
+        System.out.println(prob.hasGroupsSizeX(new int[]{1, 1})); // true
+        System.out.println(prob.hasGroupsSizeX(new int[]{1, 1, 2, 2, 2, 2})); // true
+        System.out.println(prob.hasGroupsSizeX(new int[]{1, 1, 1, 2, 2, 2})); // true
     }
 }
