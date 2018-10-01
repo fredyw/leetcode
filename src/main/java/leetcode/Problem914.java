2b49dce882821d829c069a27a078b83c5180ba91
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-09-30 22:42:48 -0500

    Solve problem 914

--- src/main/java/leetcode/Problem914.java
+++ src/main/java/leetcode/Problem914.java
@@ -13,32 +13,32 @@ public class Problem914 {
             int count = map.getOrDefault(val, 0) + 1;
             map.put(val, count);
         }
-        int min = Integer.MAX_VALUE;
+        int gcd = -1;
         for (int count : map.values()) {
-            min = Math.min(min, count);
-            if (min < 2) {
+            if (count < 2) {
                 return false;
+            }
+            if (gcd == -1) {
+                gcd = count;
+            } else if (gcd != count) {
+                gcd = gcd(gcd, count);
+                if (gcd == 1) {
+                    return false;
+                }
             }
         }
         for (int count : map.values()) {
-            if (count % min != 0) {
+            if (count % gcd != 0) {
                 return false;
             }
         }
         return true;
     }
 
-    public static void main(String[] args) {
-        Problem914 prob = new Problem914();
-//        System.out.println(prob.hasGroupsSizeX(new int[]{1, 2, 3, 4, 4, 3, 2, 1})); // true
-//        System.out.println(prob.hasGroupsSizeX(new int[]{1, 1, 1, 2, 2, 2, 3, 3})); // false
-//        System.out.println(prob.hasGroupsSizeX(new int[]{1})); // false
-//        System.out.println(prob.hasGroupsSizeX(new int[]{1, 1})); // true
-//        System.out.println(prob.hasGroupsSizeX(new int[]{1, 1, 2, 2, 2, 2})); // true
-//        System.out.println(prob.hasGroupsSizeX(new int[]{1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3})); // true
-//        System.out.println(prob.hasGroupsSizeX(new int[]{1, 1, 1, 2, 2, 2})); // true
-//        System.out.println(prob.hasGroupsSizeX(new int[]{1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3})); // false
-        System.out.println(prob.hasGroupsSizeX(new int[]{1, 1, 1, 1, 2, 2, 2, 2, 2, 2})); // true
-//        System.out.println(prob.hasGroupsSizeX(new int[]{1, 1, 1, 2, 2, 2, 2, 2})); // false
+    private static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
     }
 }
