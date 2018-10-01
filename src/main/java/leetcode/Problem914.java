36e5dbfa3660406f54827ceff3e911e7505b569f
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-09-30 21:40:44 -0500

    Skeleton for problem 914

---
+++ src/main/java/leetcode/Problem914.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/x-of-a-kind-in-a-deck-of-cards/
+ */
+public class Problem914 {
+    public boolean hasGroupsSizeX(int[] deck) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem914 prob = new Problem914();
+    }
+}
