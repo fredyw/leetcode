6fec5219a618ee6f4899a8bd02bed7df5402e6a3
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-10-01 22:41:38 -0500

    Update problem 889

--- src/main/java/leetcode/Problem889.java
+++ src/main/java/leetcode/Problem889.java
@@ -19,7 +19,50 @@ public class Problem889 {
         return null;
     }
 
+    private static void preOrder(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        System.out.print(node.val + " ");
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    private static void inOrder(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        inOrder(node.left);
+        System.out.print(node.val + " ");
+        inOrder(node.right);
+    }
+
+    private static void postOrder(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.print(node.val + " ");
+    }
+
     public static void main(String[] args) {
         Problem889 prob = new Problem889();
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+
+        preOrder(root);
+        System.out.println();
+
+        inOrder(root);
+        System.out.println();
+
+        postOrder(root);
+        System.out.println();
     }
 }
