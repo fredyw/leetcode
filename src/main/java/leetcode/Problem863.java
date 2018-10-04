7741458beba153b3ce508feb49591996b3e33bdb
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-10-04 00:03:37 -0500

    Skeleton for problem 863

---
+++ src/main/java/leetcode/Problem863.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree/
+ */
+public class Problem863 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int K) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem863 prob = new Problem863();
+    }
+}
