7db87bad215cf383dcefa1f793282e375f5b854f
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-10-06 23:30:21 -0500

    Update problem 863

--- src/main/java/leetcode/Problem863.java
+++ src/main/java/leetcode/Problem863.java
@@ -23,5 +23,15 @@ public class Problem863 {
 
     public static void main(String[] args) {
         Problem863 prob = new Problem863();
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(5);
+        root.left.left = new TreeNode(6);
+        root.left.right = new TreeNode(2);
+        root.left.right.left = new TreeNode(7);
+        root.left.right.right = new TreeNode(4);
+        root.right = new TreeNode(1);
+        root.right.left = new TreeNode(0);
+        root.right.right = new TreeNode(8);
+        System.out.println(prob.distanceK(root, root.left, 2)); // [7,4,1]
     }
 }
