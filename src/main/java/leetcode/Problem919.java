f191dd3e9aa6aab67086e53afb725e0b6bbec6d7
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-10-11 23:19:07 -0500

    Skeleton for problem 919

---
+++ src/main/java/leetcode/Problem919.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/complete-binary-tree-inserter/
+ */
+public class Problem919 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public static class CBTInserter {
+
+        public CBTInserter(TreeNode root) {
+
+        }
+
+        public int insert(int v) {
+            // TODO
+            return 0;
+        }
+
+        public TreeNode get_root() {
+            // TODO
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
