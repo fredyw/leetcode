9e5aeb4911a64647c8c17ec4d7a044307361cde7
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-10-12 14:24:42 -0500

    Skeleton for problem 916

---
+++ src/main/java/leetcode/Problem916.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/word-subsets/
+ */
+public class Problem916 {
+    public List<String> wordSubsets(String[] A, String[] B) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem916 prob = new Problem916();
+    }
+}
