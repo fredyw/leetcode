724edcb2007eb6af7a975bbbb64cdfc854985901
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-10-11 23:37:50 -0500

    Update problem 919

--- src/main/java/leetcode/Problem919.java
+++ src/main/java/leetcode/Problem919.java
@@ -12,12 +12,18 @@ public class Problem919 {
         TreeNode(int x) {
             val = x;
         }
+
+        @Override
+        public String toString() {
+            return "" + val;
+        }
     }
 
     public static class CBTInserter {
+        private final TreeNode root;
 
         public CBTInserter(TreeNode root) {
-
+            this.root = root;
         }
 
         public int insert(int v) {
@@ -26,12 +32,20 @@ public class Problem919 {
         }
 
         public TreeNode get_root() {
-            // TODO
-            return null;
+            return root;
         }
     }
 
     public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
 
+        CBTInserter inserter = new CBTInserter(root);
+        System.out.println(inserter.insert(4)); // 2
+        System.out.println(inserter.insert(7)); // 3
+        System.out.println(inserter.insert(8)); // 4
     }
 }
