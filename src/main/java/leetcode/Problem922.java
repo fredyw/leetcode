94288485cc9bf316cfb7851410215f906a799a73
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-10-13 23:50:58 -0500

    Skeleton for problem 922

---
+++ src/main/java/leetcode/Problem922.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/sort-array-by-parity-ii/
+ */
+public class Problem922 {
+    public int[] sortArrayByParityII(int[] A) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem922 prob = new Problem922();
+        System.out.println(prob.sortArrayByParityII(new int[]{4, 2, 5, 7})); // [4,5,2,7]
+    }
+}
