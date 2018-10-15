80113c31bd47695ce49bfb584be49c1e5c6db8a8
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-10-14 23:52:26 -0500

    Update problem 916

--- src/main/java/leetcode/Problem916.java
+++ src/main/java/leetcode/Problem916.java
@@ -1,5 +1,6 @@
 package leetcode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -7,11 +8,42 @@ import java.util.List;
  */
 public class Problem916 {
     public List<String> wordSubsets(String[] A, String[] B) {
-        // TODO
-        return null;
+        List<String> result = new ArrayList<>();
+        outer:
+        for (String a : A) {
+            for (String b : B) {
+                int[] counts = new int[26];
+                for (char c : a.toCharArray()) {
+                    counts[c - 'a']++;
+                }
+                for (char c : b.toCharArray()) {
+                    if (counts[c - 'a'] == 0) {
+                        continue outer;
+                    }
+                    counts[c - 'a']--;
+                }
+            }
+            result.add(a);
+        }
+        return result;
     }
 
     public static void main(String[] args) {
         Problem916 prob = new Problem916();
+        System.out.println(prob.wordSubsets(
+            new String[]{"amazon", "apple", "facebook", "google", "leetcode"},
+            new String[]{"e", "o"})); // ["facebook","google","leetcode"]
+        System.out.println(prob.wordSubsets(
+            new String[]{"amazon", "apple", "facebook", "google", "leetcode"},
+            new String[]{"l", "e"})); // ["apple","google","leetcode"]
+        System.out.println(prob.wordSubsets(
+            new String[]{"amazon", "apple", "facebook", "google", "leetcode"},
+            new String[]{"e", "oo"})); // ["facebook","google"]
+        System.out.println(prob.wordSubsets(
+            new String[]{"amazon", "apple", "facebook", "google", "leetcode"},
+            new String[]{"lo", "eo"})); // ["google","leetcode"]
+        System.out.println(prob.wordSubsets(
+            new String[]{"amazon", "apple", "facebook", "google", "leetcode"},
+            new String[]{"ec", "oc", "ceo"})); // ["facebook","leetcode"]
     }
 }
