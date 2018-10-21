59fb71a99e726360be94b72dfd7b585648f19e10
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-10-21 00:34:03 -0500

    Skeleton for problem 925

---
+++ src/main/java/leetcode/Problem925.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/long-pressed-name/
+ */
+public class Problem925 {
+    public boolean isLongPressedName(String name, String typed) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem925 prob = new Problem925();
+    }
+}
