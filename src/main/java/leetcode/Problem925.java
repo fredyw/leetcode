8292cfa2b490ec72f2ca1aaf6ad9bbc18d8d3f8c
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-10-21 00:49:02 -0500

    Update problem 925

--- src/main/java/leetcode/Problem925.java
+++ src/main/java/leetcode/Problem925.java
@@ -11,5 +11,9 @@ public class Problem925 {
 
     public static void main(String[] args) {
         Problem925 prob = new Problem925();
+        System.out.println(prob.isLongPressedName("alex", "aaleex")); // true
+        System.out.println(prob.isLongPressedName("saeed", "ssaaedd")); // false
+        System.out.println(prob.isLongPressedName("leelee", "lleeelee")); // true
+        System.out.println(prob.isLongPressedName("laiden", "laiden")); // true
     }
 }
