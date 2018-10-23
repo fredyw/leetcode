a4f392248b3e733f914a94e3f33b04ea61447937
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-10-22 23:09:06 -0500

    Skeleton for problem 901

---
+++ src/main/java/leetcode/Problem901.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/online-stock-span/
+ */
+public class Problem901 {
+    private static class StockSpanner {
+        public StockSpanner() {
+        }
+
+        public int next(int price) {
+            // TODO
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        StockSpanner spanner = new StockSpanner();
+    }
+}
