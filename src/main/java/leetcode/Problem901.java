6dcae07379a989523fc99802b4c19e0a046f664a
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-10-23 00:19:53 -0500

    Update problem 901

--- src/main/java/leetcode/Problem901.java
+++ src/main/java/leetcode/Problem901.java
@@ -1,16 +1,36 @@
 package leetcode;
 
+import java.util.Stack;
+
 /**
  * https://leetcode.com/problems/online-stock-span/
  */
 public class Problem901 {
     private static class StockSpanner {
+        private final Stack<Integer> stack = new Stack<>();
+
         public StockSpanner() {
         }
 
         public int next(int price) {
-            // TODO
-            return 0;
+            int count = 1;
+            Stack<Integer> tmp = new Stack<>();
+            tmp.add(price);
+            while (!stack.isEmpty()) {
+                Integer val = stack.pop();
+                if (val <= price) {
+                    tmp.add(val);
+                    count++;
+                } else {
+                    tmp.add(val);
+                    break;
+                }
+            }
+
+            while (!tmp.isEmpty()) {
+                stack.add(tmp.pop());
+            }
+            return count;
         }
     }
 
@@ -18,8 +38,8 @@ public class Problem901 {
         StockSpanner spanner = new StockSpanner();
         System.out.println(spanner.next(100)); // 1
         System.out.println(spanner.next(80)); // 1
-        System.out.println(spanner.next(60)); // 2
-        System.out.println(spanner.next(70)); // 1
+        System.out.println(spanner.next(60)); // 1
+        System.out.println(spanner.next(70)); // 2
         System.out.println(spanner.next(60)); // 1
         System.out.println(spanner.next(75)); // 4
         System.out.println(spanner.next(85)); // 6
