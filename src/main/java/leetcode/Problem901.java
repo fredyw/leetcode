4b2b3ab0a901454691d4dabf3fb4fb89c313ff54
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-10-24 00:45:53 -0500

    Update problem 901

--- src/main/java/leetcode/Problem901.java
+++ src/main/java/leetcode/Problem901.java
@@ -1,47 +1,35 @@
 package leetcode;
 
-import java.util.Stack;
-
 /**
  * https://leetcode.com/problems/online-stock-span/
  */
 public class Problem901 {
     private static class StockSpanner {
-        private final Stack<Integer> stack = new Stack<>();
-
         public StockSpanner() {
         }
 
         public int next(int price) {
-            int count = 1;
-            Stack<Integer> tmp = new Stack<>();
-            tmp.add(price);
-            while (!stack.isEmpty()) {
-                Integer val = stack.pop();
-                if (val <= price) {
-                    tmp.add(val);
-                    count++;
-                } else {
-                    tmp.add(val);
-                    break;
-                }
-            }
-
-            while (!tmp.isEmpty()) {
-                stack.add(tmp.pop());
-            }
-            return count;
+            // TODO
+            return 0;
         }
     }
 
     public static void main(String[] args) {
         StockSpanner spanner = new StockSpanner();
-        System.out.println(spanner.next(100)); // 1
-        System.out.println(spanner.next(80)); // 1
-        System.out.println(spanner.next(60)); // 1
-        System.out.println(spanner.next(70)); // 2
-        System.out.println(spanner.next(60)); // 1
-        System.out.println(spanner.next(75)); // 4
-        System.out.println(spanner.next(85)); // 6
+//        System.out.println(spanner.next(100)); // 1
+//        System.out.println(spanner.next(80)); // 1
+//        System.out.println(spanner.next(60)); // 1
+//        System.out.println(spanner.next(70)); // 2
+//        System.out.println(spanner.next(60)); // 1
+//        System.out.println(spanner.next(75)); // 4
+//        System.out.println(spanner.next(85)); // 6
+
+        System.out.println(spanner.next(1)); // 1
+        System.out.println(spanner.next(1)); // 2
+        System.out.println(spanner.next(1)); // 3
+        System.out.println(spanner.next(1)); // 4
+        System.out.println(spanner.next(10)); // 5
+        System.out.println(spanner.next(20)); // 6
+        System.out.println(spanner.next(0)); // 1
     }
 }
