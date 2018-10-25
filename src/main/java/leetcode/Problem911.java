8d02bfae5a70c1c22edd31f62e7e45dd1f953cbd
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-10-25 00:34:49 -0500

    Skeleton for problem 911

---
+++ src/main/java/leetcode/Problem911.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/online-election/
+ */
+public class Problem911 {
+    private static class TopVotedCandidate {
+        public TopVotedCandidate(int[] persons, int[] times) {
+        }
+
+        public int q(int t) {
+            // TODO
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}
