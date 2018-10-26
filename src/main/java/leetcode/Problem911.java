fb1858f463f6bbc069f217b1a664256649e31f48
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-10-25 23:31:51 -0500

    Update problem 911

--- src/main/java/leetcode/Problem911.java
+++ src/main/java/leetcode/Problem911.java
@@ -15,5 +15,13 @@ public class Problem911 {
     }
 
     public static void main(String[] args) {
+        TopVotedCandidate candidate = new TopVotedCandidate(new int[]{0, 1, 1, 0, 0, 1, 0},
+            new int[]{0, 5, 10, 15, 20, 25, 30});
+        System.out.println(candidate.q(3)); // 0
+        System.out.println(candidate.q(12)); // 1
+        System.out.println(candidate.q(25)); // 1
+        System.out.println(candidate.q(15)); // 0
+        System.out.println(candidate.q(24)); // 0
+        System.out.println(candidate.q(8)); // 1
     }
 }
