69cab0a9519ccf8fedcc0bb242c51eff3bcc455a
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-10-27 00:52:02 -0500

    Skeleton for problem 875

---
+++ src/main/java/leetcode/Problem875.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/koko-eating-bananas/
+ */
+public class Problem875 {
+    public int minEatingSpeed(int[] piles, int H) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem875 prob = new Problem875();
+    }
+}
