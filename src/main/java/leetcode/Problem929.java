8b74654877f90afad045cb75507c6b8378d4f4eb
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-10-28 23:29:36 -0500

    Skeleton for problem 929

---
+++ src/main/java/leetcode/Problem929.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/unique-email-addresses/
+ */
+public class Problem929 {
+    public int numUniqueEmails(String[] emails) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem929 prob = new Problem929();
+    }
+}
