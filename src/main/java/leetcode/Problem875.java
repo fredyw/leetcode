8cc1cb330dfcbe22936ffa1a684a104836b2ba22
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-10-31 22:15:35 -0500

    Update problem 875

--- src/main/java/leetcode/Problem875.java
+++ src/main/java/leetcode/Problem875.java
@@ -11,7 +11,12 @@ public class Problem875 {
 
     public static void main(String[] args) {
         Problem875 prob = new Problem875();
+        System.out.println(prob.minEatingSpeed(new int[]{3, 6, 7, 11}, 4)); // 11
+        System.out.println(prob.minEatingSpeed(new int[]{3, 6, 7, 11}, 5)); // 7
+        System.out.println(prob.minEatingSpeed(new int[]{3, 6, 7, 11}, 6)); // 6
+        System.out.println(prob.minEatingSpeed(new int[]{3, 6, 7, 11}, 7)); // 6
         System.out.println(prob.minEatingSpeed(new int[]{3, 6, 7, 11}, 8)); // 4
+        System.out.println(prob.minEatingSpeed(new int[]{3, 6, 7, 11}, 100)); // 1
         System.out.println(prob.minEatingSpeed(new int[]{30, 11, 23, 4, 20}, 5)); // 30
         System.out.println(prob.minEatingSpeed(new int[]{30, 11, 23, 4, 20}, 6)); // 23
     }
