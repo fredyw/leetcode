94e665dad2f6d617c350275c28367fc1469ae09f
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-11-01 23:15:44 -0500

    Skeleton for problem 931

---
+++ src/main/java/leetcode/Problem931.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/minimum-falling-path-sum/
+ */
+public class Problem931 {
+    public int minFallingPathSum(int[][] A) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem931 prob = new Problem931();
+    }
+}
