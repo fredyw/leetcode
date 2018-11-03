183ac9ee958170bd379e8b3159b74a74dfa8365e
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-11-03 10:15:22 -0500

    Update problem 931

--- src/main/java/leetcode/Problem931.java
+++ src/main/java/leetcode/Problem931.java
@@ -11,5 +11,10 @@ public class Problem931 {
 
     public static void main(String[] args) {
         Problem931 prob = new Problem931();
+        System.out.println(prob.minFallingPathSum(new int[][]{
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        })); // 12
     }
 }
