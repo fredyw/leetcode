9187ef7f40310812a52134d2b2211dc4ec018c4f
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-11-03 10:35:19 -0500

    Update problem 931

--- src/main/java/leetcode/Problem931.java
+++ src/main/java/leetcode/Problem931.java
@@ -5,8 +5,26 @@ package leetcode;
  */
 public class Problem931 {
     public int minFallingPathSum(int[][] A) {
-        // TODO
-        return 0;
+        int maxRow = A.length;
+        int maxCol = A.length > 0 ? A[0].length : 0;
+        int min = Integer.MAX_VALUE;
+//        for (int col = 0; col < maxCol; col++) {
+//            min = Math.min(min, minFaillingPathSum(A, maxRow, maxCol, 0, col));
+//        }
+        min = Math.min(min, minFaillingPathSum(A, maxRow, maxCol, 0, 0));
+        return min;
+    }
+
+    private static int minFaillingPathSum(int[][] a, int maxRow, int maxCol, int row, int col) {
+        if (row < 0 || col < 0 || row >= maxRow || col >= maxCol) {
+            return 0;
+        }
+        int value = a[row][col];
+        int min = Integer.MAX_VALUE;
+        min = Math.min(min, minFaillingPathSum(a, maxRow, maxCol, row + 1, col - 1) + value);
+        min = Math.min(min, minFaillingPathSum(a, maxRow, maxCol, row + 1, col) + value);
+        min = Math.min(min, minFaillingPathSum(a, maxRow, maxCol, row + 1, col + 1) + value);
+        return min;
     }
 
     public static void main(String[] args) {
