e1c06556f1e52210c2c02b32ea29f2be83372325
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-11-03 13:03:57 -0500

    Skeleton for problem 932

---
+++ src/main/java/leetcode/Problem932.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/beautiful-array/
+ */
+public class Problem932 {
+    public int[] beautifulArray(int N) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem932 prob = new Problem932();
+    }
+}
