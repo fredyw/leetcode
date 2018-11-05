ae686532c93da399cee8f48614612b823158c242
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-11-04 20:55:39 -0600

    Skeleton for problem 933

---
+++ src/main/java/leetcode/Problem933.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/number-of-recent-calls/
+ */
+public class Problem933 {
+    private static class RecentCounter {
+
+        public RecentCounter() {
+
+        }
+
+        public int ping(int t) {
+            // TODO
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        RecentCounter counter = new RecentCounter();
+    }
+}
