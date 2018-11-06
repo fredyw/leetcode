135aeddf2a8093c51e7190b5bd63a64cdd239d8c
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-11-05 20:48:47 -0600

    Update problem 932

--- src/main/java/leetcode/Problem932.java
+++ src/main/java/leetcode/Problem932.java
@@ -11,7 +11,11 @@ public class Problem932 {
 
     public static void main(String[] args) {
         Problem932 prob = new Problem932();
+        System.out.println(prob.beautifulArray(1)); // [1]
+        System.out.println(prob.beautifulArray(2)); // [1,2]
+        System.out.println(prob.beautifulArray(3)); // [1,3,2]
         System.out.println(prob.beautifulArray(4)); // [2,1,4,3]
         System.out.println(prob.beautifulArray(5)); // [3,1,2,5,4]
+        System.out.println(prob.beautifulArray(6)); // [1,5,3,2,6,4]
     }
 }
