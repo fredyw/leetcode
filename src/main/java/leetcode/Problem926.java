cf05e4f3e90dc6dc49165976dbe04b92f7afe89e
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-11-06 23:44:01 -0600

    Skeleton for problem 926

---
+++ src/main/java/leetcode/Problem926.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/flip-string-to-monotone-increasing/
+ */
+public class Problem926 {
+    public int minFlipsMonoIncr(String S) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem926 prob = new Problem926();
+    }
+}
