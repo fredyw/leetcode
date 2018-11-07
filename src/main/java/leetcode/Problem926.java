e434d6bc43e44bd426c20a4059a101e3b847acac
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-11-07 00:40:15 -0600

    Update problem 926

--- src/main/java/leetcode/Problem926.java
+++ src/main/java/leetcode/Problem926.java
@@ -15,5 +15,9 @@ public class Problem926 {
         System.out.println(prob.minFlipsMonoIncr("010110")); // 2
         System.out.println(prob.minFlipsMonoIncr("00011000")); // 2
         System.out.println(prob.minFlipsMonoIncr("0100111")); // 1
+        System.out.println(prob.minFlipsMonoIncr("100100111")); // 2
+        System.out.println(prob.minFlipsMonoIncr("00001")); // 0
+        System.out.println(prob.minFlipsMonoIncr("11110")); // 1
+        System.out.println(prob.minFlipsMonoIncr("11100")); // 2
     }
 }
