673bb3f71c102bc222c94babdd004cdf04431a63
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-11-12 11:37:22 -0600

    Update problem 938

--- src/main/java/leetcode/Problem938.java
+++ src/main/java/leetcode/Problem938.java
@@ -21,5 +21,24 @@ public class Problem938 {
 
     public static void main(String[] args) {
         Problem938 prob = new Problem938();
+        TreeNode root = new TreeNode(10);
+        root.left = new TreeNode(5);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(7);
+        root.right = new TreeNode(15);
+        root.right.right = new TreeNode(18);
+        System.out.println(prob.rangeSumBST(root, 7, 15)); // 32
+        System.out.println(prob.rangeSumBST(root, 6, 15)); // 32
+
+        root = new TreeNode(10);
+        root.left = new TreeNode(5);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(7);
+        root.right = new TreeNode(15);
+        root.right.left = new TreeNode(13);
+        root.right.right = new TreeNode(18);
+        root.left.left.left = new TreeNode(1);
+        root.left.right.left = new TreeNode(6);
+        System.out.println(prob.rangeSumBST(root, 6, 10)); // 23
     }
 }
