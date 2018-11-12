bb0a23a756965c94d78a35bd9fbe8b2ddb884811
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-11-12 00:07:53 -0600

    Skeleton for problem 938

---
+++ src/main/java/leetcode/Problem938.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/range-sum-of-bst/
+ */
+public class Problem938 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public int rangeSumBST(TreeNode root, int L, int R) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem938 prob = new Problem938();
+    }
+}
