b3db3d92ac323b1ab6fed44b89aae0ceef34c1b9
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-11-12 20:50:57 -0600

    Skeleton for problem 934

---
+++ src/main/java/leetcode/Problem934.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/shortest-bridge/
+ */
+public class Problem934 {
+    public int shortestBridge(int[][] A) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem934 prob = new Problem934();
+    }
+}
