0c9cf8821736b89663fb5d9e3ca7abf08b157d5a
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-11-14 00:18:23 -0600

    Update problem 934

--- src/main/java/leetcode/Problem934.java
+++ src/main/java/leetcode/Problem934.java
@@ -11,5 +11,21 @@ public class Problem934 {
 
     public static void main(String[] args) {
         Problem934 prob = new Problem934();
+        System.out.println(prob.shortestBridge(new int[][]{
+            {0, 1},
+            {1, 0}
+        })); // 1
+        System.out.println(prob.shortestBridge(new int[][]{
+            {0, 1, 0},
+            {0, 0, 0},
+            {0, 0, 1}
+        })); // 2
+        System.out.println(prob.shortestBridge(new int[][]{
+            {1, 1, 1, 1, 1},
+            {1, 0, 0, 0, 1},
+            {1, 0, 1, 0, 1},
+            {1, 0, 0, 0, 1},
+            {1, 1, 1, 1, 1}
+        })); // 1
     }
 }
