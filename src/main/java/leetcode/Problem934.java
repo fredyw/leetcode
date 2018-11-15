45561d26f80da10a06e3173ad46294a57eef36e1
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-11-15 00:54:47 -0600

    Update problem 934

--- src/main/java/leetcode/Problem934.java
+++ src/main/java/leetcode/Problem934.java
@@ -5,8 +5,37 @@ package leetcode;
  */
 public class Problem934 {
     public int shortestBridge(int[][] A) {
-        // TODO
+        int maxRow = A.length;
+        int maxCol = maxRow > 0 ? A[0][0] : 0;
+        boolean[][] visited = new boolean[maxRow][maxCol];
+        for (int row = 0; row < maxRow; row++) {
+            for (int col = 0; col < maxCol; col++) {
+                if (visited[row][col]) {
+                    continue;
+                }
+                // TODO
+            }
+        }
         return 0;
+    }
+
+    private static void shortest(int[][] a, int maxRow, int maxCol, int row, int col) {
+        // top
+        if (row - 1 >= 0) {
+
+        }
+        // right
+        if (col + 1 < maxCol) {
+
+        }
+        // bottom
+        if (row + 1 < maxRow) {
+
+        }
+        // left
+        if (col - 1 >= 0) {
+
+        }
     }
 
     public static void main(String[] args) {
@@ -20,6 +49,11 @@ public class Problem934 {
             {0, 0, 0},
             {0, 0, 1}
         })); // 2
+        System.out.println(prob.shortestBridge(new int[][]{
+            {0, 1, 1},
+            {0, 0, 0},
+            {1, 1, 0}
+        })); // 1
         System.out.println(prob.shortestBridge(new int[][]{
             {1, 1, 1, 1, 1},
             {1, 0, 0, 0, 1},
