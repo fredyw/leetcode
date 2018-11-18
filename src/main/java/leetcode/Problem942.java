2d4b74deba94c4a7aba83bbc9f9f15f6e4dc4234
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-11-18 12:52:08 -0600

    Solve problem 942

--- src/main/java/leetcode/Problem942.java
+++ src/main/java/leetcode/Problem942.java
@@ -5,11 +5,26 @@ package leetcode;
  */
 public class Problem942 {
     public int[] diStringMatch(String S) {
-        // TODO
-        return null;
-    }
-
-    public static void main(String[] args) {
-        Problem942 prob = new Problem942();
+        int start = 0;
+        int end = S.length();
+        int[] result = new int[S.length() + 1];
+        int index = 0;
+        boolean increase = false;
+        for (int i = 0; i < S.length(); i++) {
+            if (S.charAt(i) == 'I') {
+                result[index] = start++;
+                increase = true;
+            } else { // S.charAt(i) == 'D'
+                result[index] = end--;
+                increase = false;
+            }
+            index++;
+        }
+        if (increase) {
+            result[index] = start;
+        } else {
+            result[index] = end;
+        }
+        return result;
     }
 }
