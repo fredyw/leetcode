b876eef37c7a7f88d9b197a37ffecbb1f26f46eb
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-11-18 14:38:24 -0600

    Skeleton for problem 944

---
+++ src/main/java/leetcode/Problem944.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/delete-columns-to-make-sorted/
+ */
+public class Problem944 {
+    public int minDeletionSize(String[] A) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem944 prob = new Problem944();
+    }
+}
