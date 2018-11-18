07025bc520895e4335c20b811e7b288fa423a226
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-11-18 11:56:08 -0600

    Skeleton for problem 942

---
+++ src/main/java/leetcode/Problem942.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/di-string-match/
+ */
+public class Problem942 {
+    public int[] diStringMatch(String S) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem942 prob = new Problem942();
+    }
+}
