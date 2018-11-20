6f1ecf194d1a973e9ad7e6ebdf6566c8f8fe460e
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-11-19 23:48:06 -0600

    Skeleton for problem 528

---
+++ src/main/java/leetcode/Problem528.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/random-pick-with-weight/
+ */
+public class Problem528 {
+    public static class Solution {
+        public Solution(int[] w) {
+        }
+
+        public int pickIndex() {
+            // TODO
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}
