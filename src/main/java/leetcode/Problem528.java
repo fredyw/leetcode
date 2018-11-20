a02955e17dbac428688b7b2e47eb1c881cc405c8
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-11-20 01:05:49 -0600

    Update problem 528

--- src/main/java/leetcode/Problem528.java
+++ src/main/java/leetcode/Problem528.java
@@ -1,15 +1,23 @@
 package leetcode;
 
+import java.util.Random;
+
 /**
  * https://leetcode.com/problems/random-pick-with-weight/
  */
 public class Problem528 {
     public static class Solution {
+        private final Random random = new Random();
+        private int sum;
+
         public Solution(int[] w) {
+            for (int i : w) {
+                sum += i;
+            }
         }
 
         public int pickIndex() {
-            // TODO
+            int index = random.nextInt(sum);
             return 0;
         }
     }
