2e06e519b4f074daed2085caa02c5e410cf19f7f
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-11-21 02:33:00 -0600

    Solve problem 528

--- src/main/java/leetcode/Problem528.java
+++ src/main/java/leetcode/Problem528.java
@@ -1,27 +1,31 @@
 package leetcode;
 
 import java.util.Random;
+import java.util.TreeMap;
 
 /**
  * https://leetcode.com/problems/random-pick-with-weight/
  */
 public class Problem528 {
     public static class Solution {
+        private final TreeMap<Integer, Integer> map = new TreeMap<>();
         private final Random random = new Random();
         private int sum;
 
         public Solution(int[] w) {
+            // Example:
+            // [1, 3, 5] -> [0, 1, 1, 1, 2, 2, 2, 2, 2]
+            // map -> [0 -> 0, 1 -> 1, 4 -> 2]
+            int index = 0;
             for (int i : w) {
+                map.put(sum, index++);
                 sum += i;
             }
+            System.out.println(map.toString());
         }
 
         public int pickIndex() {
-            int index = random.nextInt(sum);
-            return 0;
+            return map.floorEntry(random.nextInt(sum)).getValue();
         }
-    }
-
-    public static void main(String[] args) {
     }
 }
