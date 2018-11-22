a1232b6cb642b5425a0df06eda801a06639e355b
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-11-22 00:14:56 -0600

    Skeleton for problem 939

---
+++ src/main/java/leetcode/Problem939.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/minimum-area-rectangle/
+ */
+public class Problem939 {
+    public int minAreaRect(int[][] points) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem939 prob = new Problem939();
+    }
+}
