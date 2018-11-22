777739c873d87e2c1c2fbc0b83ddefaae16cf48f
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-11-22 16:10:44 -0600

    Update problem 939

--- src/main/java/leetcode/Problem939.java
+++ src/main/java/leetcode/Problem939.java
@@ -11,5 +11,11 @@ public class Problem939 {
 
     public static void main(String[] args) {
         Problem939 prob = new Problem939();
+        System.out.println(prob.minAreaRect(new int[][]{
+            {1, 1}, {1, 3}, {3, 1}, {3, 3}, {2, 2}
+        })); // 4
+        System.out.println(prob.minAreaRect(new int[][]{
+            {1, 1}, {1, 3}, {3, 1}, {3, 3}, {4, 1}, {4, 3}
+        })); // 2
     }
 }
