38405525883efa56f2548906e3a7256640905411
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-11-25 00:05:14 -0600

    Skeleton for problem 946

---
+++ src/main/java/leetcode/Problem946.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/validate-stack-sequences/
+ */
+public class Problem946 {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem946 prob = new Problem946();
+    }
+}
