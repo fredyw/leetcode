531288f4db9f974b4fc347825d6f7a0750719496
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-11-25 16:39:31 -0600

    Update problem 881

--- src/main/java/leetcode/Problem881.java
+++ src/main/java/leetcode/Problem881.java
@@ -11,5 +11,9 @@ public class Problem881 {
 
     public static void main(String[] args) {
         Problem881 prob = new Problem881();
+        System.out.println(prob.numRescueBoats(new int[]{1, 2}, 3)); // 1
+        System.out.println(prob.numRescueBoats(new int[]{3, 2, 2, 1}, 3)); // 3
+        System.out.println(prob.numRescueBoats(new int[]{3, 5, 3, 4}, 5)); // 4
+        System.out.println(prob.numRescueBoats(new int[]{3, 5, 3, 4, 2}, 5)); // 4
     }
 }
