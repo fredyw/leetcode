efd106fccf97a7e6c6ad4780cba906bb2cf31099
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-11-25 01:00:06 -0600

    Solve problem 946

--- src/main/java/leetcode/Problem946.java
+++ src/main/java/leetcode/Problem946.java
@@ -1,21 +1,34 @@
 package leetcode;
 
+import java.util.Stack;
+
 /**
  * https://leetcode.com/problems/validate-stack-sequences/
  */
 public class Problem946 {
     public boolean validateStackSequences(int[] pushed, int[] popped) {
-        // TODO
-        return false;
-    }
-
-    public static void main(String[] args) {
-        Problem946 prob = new Problem946();
-        System.out.println(prob.validateStackSequences(
-            new int[]{1, 2, 3, 4, 5}, new int[]{4, 5, 3, 2, 1}
-        )); // true
-        System.out.println(prob.validateStackSequences(
-            new int[]{1, 2, 3, 4, 5}, new int[]{4, 3, 5, 1, 2}
-        )); // false
+        if (pushed.length == 0 || popped.length == 0) {
+            return true;
+        }
+        Stack<Integer> stack = new Stack<>();
+        int pushIdx = 0;
+        int popIdx = 0;
+        stack.push(pushed[pushIdx++]);
+        while (pushIdx < pushed.length && popIdx < popped.length) {
+            if (!stack.isEmpty() && stack.peek() == popped[popIdx]) {
+                popIdx++;
+                stack.pop();
+            } else {
+                stack.push(pushed[pushIdx++]);
+            }
+        }
+        while (!stack.isEmpty()) {
+            if (stack.peek() != popped[popIdx]) {
+                return false;
+            }
+            popIdx++;
+            stack.pop();
+        }
+        return true;
     }
 }
