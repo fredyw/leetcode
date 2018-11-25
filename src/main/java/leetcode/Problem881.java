0f0af507e26c7c01736c5213ddb8c89757a4ea1d
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-11-25 12:24:03 -0600

    Skeleton for problem 881

---
+++ src/main/java/leetcode/Problem881.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/boats-to-save-people/
+ */
+public class Problem881 {
+    public int numRescueBoats(int[] people, int limit) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem881 prob = new Problem881();
+    }
+}
