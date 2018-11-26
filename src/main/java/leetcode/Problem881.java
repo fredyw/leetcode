625a7f5fe2ccb91ad4db3d81b2e42d5846cba22f
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-11-25 23:55:18 -0600

    Update problem 881

--- src/main/java/leetcode/Problem881.java
+++ src/main/java/leetcode/Problem881.java
@@ -5,15 +5,37 @@ package leetcode;
  */
 public class Problem881 {
     public int numRescueBoats(int[] people, int limit) {
-        // TODO
-        return 0;
+        int count = 0;
+        boolean[] visited = new boolean[people.length];
+        for (int i = 0; i < people.length; i++) {
+            if (visited[i]) {
+                continue;
+            }
+            int l = limit - people[i];
+            for (int j = i + 1; j < people.length; j++) {
+                if (visited[j]) {
+                    continue;
+                }
+                if (l - people[j] >= 0) {
+                    l -= people[j];
+                    visited[j] = true;
+                    if (l == 0) {
+                        break;
+                    }
+                }
+            }
+            count++;
+        }
+        return count;
     }
 
     public static void main(String[] args) {
         Problem881 prob = new Problem881();
-        System.out.println(prob.numRescueBoats(new int[]{1, 2}, 3)); // 1
-        System.out.println(prob.numRescueBoats(new int[]{3, 2, 2, 1}, 3)); // 3
-        System.out.println(prob.numRescueBoats(new int[]{3, 5, 3, 4}, 5)); // 4
-        System.out.println(prob.numRescueBoats(new int[]{3, 5, 3, 4, 2}, 5)); // 4
+//        System.out.println(prob.numRescueBoats(new int[]{1, 2}, 3)); // 1
+//        System.out.println(prob.numRescueBoats(new int[]{3, 2, 2, 1}, 3)); // 3
+//        System.out.println(prob.numRescueBoats(new int[]{3, 5, 3, 4}, 5)); // 4
+//        System.out.println(prob.numRescueBoats(new int[]{3, 5, 3, 4, 2}, 5)); // 4
+//        System.out.println(prob.numRescueBoats(new int[]{3, 5, 3, 2, 4}, 5)); // 4
+        System.out.println(prob.numRescueBoats(new int[]{21, 40, 16, 24, 30}, 50)); // 3
     }
 }
