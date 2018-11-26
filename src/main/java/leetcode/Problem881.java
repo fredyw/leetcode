69218c5f78d59888384b1f1c6298adb6afb52ded
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-11-26 02:11:05 -0600

    Solve problem 881

--- src/main/java/leetcode/Problem881.java
+++ src/main/java/leetcode/Problem881.java
@@ -40,20 +40,4 @@ public class Problem881 {
         }
         return count;
     }
-
-    public static void main(String[] args) {
-        Problem881 prob = new Problem881();
-        System.out.println(prob.numRescueBoats(new int[]{2, 2}, 6)); // 1
-        System.out.println(prob.numRescueBoats(new int[]{2, 4}, 5)); // 2
-        System.out.println(prob.numRescueBoats(new int[]{1, 2}, 3)); // 1
-        System.out.println(prob.numRescueBoats(new int[]{3, 2, 2, 1}, 3)); // 3
-        System.out.println(prob.numRescueBoats(new int[]{3, 5, 3, 4}, 5)); // 4
-        System.out.println(prob.numRescueBoats(new int[]{3, 5, 3, 4, 2}, 5)); // 4
-        System.out.println(prob.numRescueBoats(new int[]{3, 5, 3, 2, 4}, 5)); // 4
-        System.out.println(prob.numRescueBoats(new int[]{21, 40, 16, 24, 30}, 50)); // 3
-        System.out.println(prob.numRescueBoats(new int[]{
-            2, 49, 10, 7, 11, 41, 47, 2, 22, 6, 13, 12, 33, 18, 10, 26, 2, 6, 50, 10}, 50)); // 11
-        System.out.println(prob.numRescueBoats(
-            new int[]{3, 8, 4, 9, 2, 2, 7, 1, 6, 10, 6, 7, 1, 7, 7, 6, 4, 4, 10, 1}, 10)); // 11
-    }
 }
