f3d3c47937f60f2351427bc524991137ffb2cc5d
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-11-27 04:10:45 -0600

    Skeleton for problem 947

---
+++ src/main/java/leetcode/Problem947.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/most-stones-removed-with-same-row-or-column/
+ */
+public class Problem947 {
+    public int removeStones(int[][] stones) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem947 prob = new Problem947();
+    }
+}
