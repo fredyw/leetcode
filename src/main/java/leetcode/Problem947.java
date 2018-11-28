357e2534884ce4022c3b62595112243159be7985
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-11-28 03:09:05 -0600

    Update problem 947

--- src/main/java/leetcode/Problem947.java
+++ src/main/java/leetcode/Problem947.java
@@ -11,5 +11,26 @@ public class Problem947 {
 
     public static void main(String[] args) {
         Problem947 prob = new Problem947();
+        System.out.println(prob.removeStones(new int[][]{
+            {0, 0}, {0, 1}, {1, 0}, {1, 2}, {2, 1}, {2, 2}
+        })); // 5
+        System.out.println(prob.removeStones(new int[][]{
+            {0, 0}, {0, 2}, {1, 1}, {2, 0}, {2, 2}
+        })); // 3
+        System.out.println(prob.removeStones(new int[][]{
+            {0, 0}, {0, 2}, {1, 1}, {2, 0}, {2, 2}, {3, 3}
+        })); // 3
+        System.out.println(prob.removeStones(new int[][]{
+            {0, 0}
+        })); // 0
+        System.out.println(prob.removeStones(new int[][]{
+            {0, 0}, {0, 1}, {1, 0}, {1, 2}, {2, 1}, {2, 2}, {3, 0}
+        })); // 6
+        System.out.println(prob.removeStones(new int[][]{
+            {0, 0}, {3, 3}
+        })); // 0
+        System.out.println(prob.removeStones(new int[][]{
+            {0, 0}, {0, 2}, {1, 1}, {2, 0}, {2, 2}
+        })); // 3
     }
 }
