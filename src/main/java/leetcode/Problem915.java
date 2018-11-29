9d9d148a376618b39daad0bcc1d96546aa02fa87
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-11-29 01:12:25 -0600

    Skeleton for problem 915

---
+++ src/main/java/leetcode/Problem915.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/partition-array-into-disjoint-intervals/
+ */
+public class Problem915 {
+    public int partitionDisjoint(int[] A) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem915 prob = new Problem915();
+    }
+}
