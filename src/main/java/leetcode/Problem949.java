319f9a1b43cbe855120da4435a1ffb8c6b115cbd
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-12-02 20:04:35 -0600

    Skeleton for problem 949

---
+++ src/main/java/leetcode/Problem949.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/largest-time-for-given-digits/
+ */
+public class Problem949 {
+    public String largestTimeFromDigits(int[] A) {
+        // TODO
+        return "";
+    }
+
+    public static void main(String[] args) {
+        Problem949 prob = new Problem949();
+    }
+}
