132587b76c617d5da05803c03dccff6a68ab0582
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-12-02 20:00:03 -0600

    Solve problem 915

--- src/main/java/leetcode/Problem915.java
+++ src/main/java/leetcode/Problem915.java
@@ -5,50 +5,25 @@ package leetcode;
  */
 public class Problem915 {
     public int partitionDisjoint(int[] A) {
-        int minIdx = 0;
-        int maxIdx = 0;
-        boolean smaller = true;
+        int prevMaxIdx = 0;
+        int curMaxIdx = 0;
         int i = 1;
         int answer = 1;
         while (i < A.length) {
-            if (smaller) {
-                while (i < A.length && A[i] >= A[minIdx]) {
-                    if (A[minIdx] > A[i]) {
-                        minIdx = i;
-                    }
-                    if (A[maxIdx] < A[i]) {
-                        maxIdx = i;
-                    }
-                    i++;
-                }
-            } else {
-                while (i < A.length && A[i] < A[maxIdx]) {
-                    if (A[minIdx] > A[i]) {
-                        minIdx = i;
-                    }
-                    if (A[maxIdx] < A[i]) {
-                        maxIdx = i;
-                    }
-                    i++;
-                }
-                answer = i;
+            while (i < A.length && A[i] < A[curMaxIdx]) {
+                i++;
             }
-            smaller = !smaller;
+            answer = i;
+            prevMaxIdx = curMaxIdx;
+            curMaxIdx = i;
+            while (i < A.length && A[i] >= A[prevMaxIdx]) {
+                if (A[i] > A[curMaxIdx]) {
+                    curMaxIdx = i;
+                }
+                i++;
+            }
             i++;
         }
         return answer;
-    }
-
-    public static void main(String[] args) {
-        Problem915 prob = new Problem915();
-//        System.out.println(prob.partitionDisjoint(new int[]{5, 0, 3, 8, 6})); // 3
-//        System.out.println(prob.partitionDisjoint(new int[]{5, 0, 3, 6, 8})); // 3
-//        System.out.println(prob.partitionDisjoint(new int[]{1, 1, 1, 0, 6, 12})); // 4
-//        System.out.println(prob.partitionDisjoint(new int[]{1, 1, 1, 0, 6, 12, 3, 13})); // 4
-//        System.out.println(prob.partitionDisjoint(new int[]{4, 10, 6, 2, 8, 12})); // 5
-//        System.out.println(prob.partitionDisjoint(new int[]{5, 5, 3, 8, 2, 10, 9})); // 5
-//        System.out.println(prob.partitionDisjoint(new int[]{1, 1})); // 1
-//        System.out.println(prob.partitionDisjoint(new int[]{1, 1, 1, 2, 2, 2})); // 1
-        System.out.println(prob.partitionDisjoint(new int[]{90, 47, 69, 10, 43, 92, 31, 73, 61, 97})); // 9
     }
 }
