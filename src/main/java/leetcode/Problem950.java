06a3bab0a4ea0d3a4ecd5f14431d141d36c60ed2
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-12-03 21:15:34 -0600

    Skeleton for problem 950

---
+++ src/main/java/leetcode/Problem950.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/reveal-cards-in-increasing-order/
+ */
+public class Problem950 {
+    public int[] deckRevealedIncreasing(int[] deck) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem950 prob = new Problem950();
+    }
+}
