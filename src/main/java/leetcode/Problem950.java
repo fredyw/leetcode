3abdb0f3df6fbdba02f429ce07ad19546015bf95
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-12-03 23:37:07 -0600

    Update problem 950

--- src/main/java/leetcode/Problem950.java
+++ src/main/java/leetcode/Problem950.java
@@ -9,26 +9,27 @@ public class Problem950 {
     public int[] deckRevealedIncreasing(int[] deck) {
         Arrays.sort(deck);
         int[] answer = new int[deck.length];
-        System.out.println("sorted: " + Arrays.toString(deck));
-        int n = deck.length / 2;
-        if (deck.length % 2 != 0) {
-            n++;
-        }
-        for (int i = 0, j = 0; i < n; i++, j += 2) {
-            answer[j] = deck[i];
-        }
-        for (int i = n, j = 1; i < deck.length; i++, j += 2) {
-            answer[j] = deck[i];
+        int i = 0;
+        int j = 2;
+        while (i < deck.length) {
+            int k = 0;
+            while (answer[k] != 0) {
+                k++;
+            }
+            for (; i < deck.length && k < answer.length; k += j, i++) {
+                answer[k] = deck[i];
+            }
+            j *= 2;
         }
         return answer;
     }
 
     public static void main(String[] args) {
         Problem950 prob = new Problem950();
-        System.out.println(Arrays.toString(
-            prob.deckRevealedIncreasing(new int[]{17, 13, 11, 2, 3, 5, 7}))); // [2,13,3,11,5,17,7]
-        System.out.println(Arrays.toString(
-            prob.deckRevealedIncreasing(new int[]{17, 13, 11, 2, 3, 5, 7, 20}))); // [2,11,3,17,5,13,7,20]
+//        System.out.println(Arrays.toString(
+//            prob.deckRevealedIncreasing(new int[]{17, 13, 11, 2, 3, 5, 7}))); // [2,13,3,11,5,17,7]
+//        System.out.println(Arrays.toString(
+//            prob.deckRevealedIncreasing(new int[]{17, 13, 11, 2, 3, 5, 7, 20}))); // [2,11,3,17,5,13,7,20]
 //        System.out.println(Arrays.toString(
 //            prob.deckRevealedIncreasing(new int[]{1, 2, 3}))); // [1,3,2]
 //        System.out.println(Arrays.toString(
@@ -43,5 +44,18 @@ public class Problem950 {
 //            prob.deckRevealedIncreasing(new int[]{1, 2, 3, 4, 5, 6, 7}))); // [1,6,2,5,3,7,4]
 //        System.out.println(Arrays.toString(
 //            prob.deckRevealedIncreasing(new int[]{1, 2, 3, 4, 5, 6, 7, 8}))); // [1,5,2,7,3,6,4,8]
+//        System.out.println(Arrays.toString(
+//            prob.deckRevealedIncreasing(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}))); // [1,9,2,6,3,8,4,7,5]
+//        System.out.println(Arrays.toString(
+//            prob.deckRevealedIncreasing(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}))); // [1,6,2,10,3,7,4,9,5,8]
+//        System.out.println(Arrays.toString(
+//            prob.deckRevealedIncreasing(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14})));
+//            // [1,8,2,13,3,9,4,12,5,10,6,14,7,11]
+//        System.out.println(Arrays.toString(
+//            prob.deckRevealedIncreasing(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16})));
+            // [1,9,2,13,3,10,4,15,5,11,6,14,7,12,8,16]
+        System.out.println(Arrays.toString(
+            prob.deckRevealedIncreasing(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18})));
+            // [1,10,2,18,3,11,4,15,5,12,6,17,7,13,8,16,9,14]
     }
 }
