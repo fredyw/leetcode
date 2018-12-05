33be3588d5e8c27decd2de627cf527c6735df585
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-12-05 12:01:18 -0600

    Skeleton for problem 951

---
+++ src/main/java/leetcode/Problem951.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/flip-equivalent-binary-trees/
+ */
+public class Problem951 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem951 prob = new Problem951();
+    }
+}
