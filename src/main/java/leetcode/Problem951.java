3e4bf9c66791d2d393c2a4a0165b71e9d63881ab
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-12-05 12:17:13 -0600

    Update problem 951

--- src/main/java/leetcode/Problem951.java
+++ src/main/java/leetcode/Problem951.java
@@ -21,5 +21,24 @@ public class Problem951 {
 
     public static void main(String[] args) {
         Problem951 prob = new Problem951();
+        TreeNode root1 = new TreeNode(1);
+        root1.left = new TreeNode(2);
+        root1.left.left = new TreeNode(4);
+        root1.left.right = new TreeNode(5);
+        root1.left.right.left = new TreeNode(7);
+        root1.left.right.right = new TreeNode(8);
+        root1.right = new TreeNode(3);
+        root1.right.left = new TreeNode(6);
+
+        TreeNode root2 = new TreeNode(1);
+        root2.left = new TreeNode(3);
+        root2.left.right = new TreeNode(6);
+        root2.right = new TreeNode(2);
+        root2.right.left = new TreeNode(4);
+        root2.right.right = new TreeNode(5);
+        root2.right.right.left = new TreeNode(8);
+        root2.right.right.right = new TreeNode(7);
+
+        System.out.println(prob.flipEquiv(root1, root2)); // true
     }
 }
