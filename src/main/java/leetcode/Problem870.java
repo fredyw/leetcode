7a91ae1a7b7b55b76323b9b4942c7ee5dab7629b
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-12-08 12:29:35 -0600

    Skeleton for problem 870

---
+++ src/main/java/leetcode/Problem870.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/advantage-shuffle/
+ */
+public class Problem870 {
+    public int[] advantageCount(int[] A, int[] B) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem870 prob = new Problem870();
+    }
+}
