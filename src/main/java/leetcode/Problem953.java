6caff4f4a35df0f0234b989985b616c835fb54cf
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-12-09 18:54:16 -0600

    Solve problem 953

--- src/main/java/leetcode/Problem953.java
+++ src/main/java/leetcode/Problem953.java
@@ -1,18 +1,37 @@
 package leetcode;
 
+import java.util.Arrays;
+
 /**
  * https://leetcode.com/problems/verifying-an-alien-dictionary/
  */
 public class Problem953 {
     public boolean isAlienSorted(String[] words, String order) {
-        // TODO
-        return false;
+        String[] sorted = Arrays.copyOf(words, words.length);
+        int[] o = order(order);
+        Arrays.sort(sorted, (a, b) -> {
+            int min = Math.min(a.length(), b.length());
+            for (int i = 0; i < min; i++) {
+                int diff = o[a.charAt(i) - 'a'] - o[b.charAt(i) - 'a'];
+                if (diff != 0) {
+                    return diff;
+                }
+            }
+            if (a.length() < b.length()) {
+                return -1;
+            } else if (a.length() > b.length()) {
+                return 1;
+            }
+            return 0;
+        });
+        return Arrays.equals(words, sorted);
     }
 
-    public static void main(String[] args) {
-        Problem953 prob = new Problem953();
-        System.out.println(prob.isAlienSorted(new String[]{"hello", "leetcode"}, "hlabcdefgijkmnopqrstuvwxyz")); // true
-        System.out.println(prob.isAlienSorted(new String[]{"word", "world", "row"}, "worldabcefghijkmnpqstuvxyz")); // false
-        System.out.println(prob.isAlienSorted(new String[]{"apple", "app"}, "abcdefghijklmnopqrstuvwxyz")); // false
+    private static int[] order(String s) {
+        int[] order = new int[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            order[s.charAt(i) - 'a'] = i;
+        }
+        return order;
     }
 }
