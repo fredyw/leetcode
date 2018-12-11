626037af0d1688feea5347beeac02dafea7753ce
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-12-11 11:54:39 -0600

    Solve problem 870

--- src/main/java/leetcode/Problem870.java
+++ src/main/java/leetcode/Problem870.java
@@ -1,6 +1,7 @@
 package leetcode;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 /**
  * https://leetcode.com/problems/advantage-shuffle/
@@ -14,27 +15,20 @@ public class Problem870 {
             b[i][0] = i; // the index
             b[i][1] = B[i]; // the value
         }
+        Arrays.sort(b, Comparator.comparing(x -> x[1]));
         int right = A.length - 1;
         int left = 0;
         int j = B.length - 1;
         while (left <= right) {
-            if (A[right] > b[right][1]) {
+            if (A[right] > b[j][1]) {
                 answer[b[j][0]] = A[right];
                 right--;
-                j--;
             } else {
                 answer[b[j][0]] = A[left];
                 left++;
-                j--;
             }
+            j--;
         }
         return answer;
-    }
-
-    public static void main(String[] args) {
-        Problem870 prob = new Problem870();
-        System.out.println(Arrays.toString(prob.advantageCount(new int[]{2, 7, 11, 15}, new int[]{1, 10, 4, 11}))); // [2,11,7,15]
-        System.out.println(Arrays.toString(prob.advantageCount(new int[]{12, 24, 8, 32}, new int[]{13, 25, 32, 11}))); // [24,32,8,12]
-        System.out.println(Arrays.toString(prob.advantageCount(new int[]{12, 19, 29, 46}, new int[]{10, 20, 30, 40}))); // [12,29,46,19]
     }
 }
