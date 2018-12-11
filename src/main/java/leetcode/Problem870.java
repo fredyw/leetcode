83f471dce30912635d16352d0d9bfd15c3f728bb
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-12-11 00:51:43 -0600

    Update problem 870

--- src/main/java/leetcode/Problem870.java
+++ src/main/java/leetcode/Problem870.java
@@ -11,5 +11,7 @@ public class Problem870 {
 
     public static void main(String[] args) {
         Problem870 prob = new Problem870();
+        System.out.println(prob.advantageCount(new int[]{2, 7, 11, 15}, new int[]{1, 10, 4, 11})); // [2,11,7,15]
+        System.out.println(prob.advantageCount(new int[]{12, 24, 8, 32}, new int[]{13, 25, 32, 11})); // [24,32,8,12]
     }
 }
