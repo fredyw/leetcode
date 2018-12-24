785067fce495181ec852e75d048a0bf1eada15b4
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-12-24 10:08:14 -0600

    Solve problem 961

--- src/main/java/leetcode/Problem961.java
+++ src/main/java/leetcode/Problem961.java
@@ -1,15 +1,27 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * https://leetcode.com/problems/n-repeated-element-in-size-2n-array/
  */
 public class Problem961 {
     public int repeatedNTimes(int[] A) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem961 prob = new Problem961();
+        int n = A.length / 2;
+        Map<Integer, Integer> map = new HashMap<>();
+        int answer = 0;
+        for (int a : A) {
+            if (!map.containsKey(a)) {
+                map.put(a, 1);
+            } else {
+                map.put(a, map.get(a) + 1);
+                if (map.get(a) == n) {
+                    answer = a;
+                    break;
+                }
+            }
+        }
+        return answer;
     }
 }
