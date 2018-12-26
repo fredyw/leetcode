0044b33960e64df671d9213bc85b4afb1af8a00a
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-12-26 17:54:50 -0600

    Skeleton for problem 44

---
+++ src/main/java/leetcode/Problem44.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/wildcard-matching/
+ */
+public class Problem44 {
+    public boolean isMatch(String s, String p) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem44 prob = new Problem44();
+    }
+}
