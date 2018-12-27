1496038d296a5cb28a964e955a24d1ba37c57a4e
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-12-27 17:34:54 -0600

    Update problem 44

--- src/main/java/leetcode/Problem44.java
+++ src/main/java/leetcode/Problem44.java
@@ -11,5 +11,12 @@ public class Problem44 {
 
     public static void main(String[] args) {
         Problem44 prob = new Problem44();
+        System.out.println(prob.isMatch("aa", "a")); // false
+        System.out.println(prob.isMatch("aa", "*")); // true
+        System.out.println(prob.isMatch("cb", "?a")); // false
+        System.out.println(prob.isMatch("adceb", "*a*b")); // true
+        System.out.println(prob.isMatch("acdcb", "a*c?b")); // false
+        System.out.println(prob.isMatch("acdcb", "a*c?b")); // false
+        System.out.println(prob.isMatch("aabc", "a*?c")); // true
     }
 }
