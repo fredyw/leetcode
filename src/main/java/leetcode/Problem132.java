f8b43e1c674cf55833601f97666cbfa7dcd3248c
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-12-28 11:15:06 -0600

    Skeleton for problem 132

---
+++ src/main/java/leetcode/Problem132.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/palindrome-partitioning-ii/
+ */
+public class Problem132 {
+    public int minCut(String s) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem132 prob = new Problem132();
+    }
+}
