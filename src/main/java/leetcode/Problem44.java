9124c8dcd1cfdcd8befc00d806d4b0f94cd80338
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-12-27 18:23:26 -0600

    Update problem 44

--- src/main/java/leetcode/Problem44.java
+++ src/main/java/leetcode/Problem44.java
@@ -5,18 +5,37 @@ package leetcode;
  */
 public class Problem44 {
     public boolean isMatch(String s, String p) {
-        // TODO
-        return false;
+        return isMatch(s, p, 0, 0);
+    }
+
+    private static boolean isMatch(String s, String p, int i, int j) {
+        if (i == s.length() && j == p.length()) {
+            return true;
+        } else if (i == s.length()) {
+            return false;
+        } else if (j == p.length()) {
+            return false;
+        }
+        boolean match = false;
+        if (p.charAt(j) == '?' || s.charAt(i) == p.charAt(j)) {
+            match = isMatch(s, p, i + 1, j + 1);
+        } else if (p.charAt(j) == '*') {
+            match |= isMatch(s, p, i + 1, j);
+            match |= isMatch(s, p, i, j + 1);
+            match |= isMatch(s, p, i + 1, j + 1);
+        }
+        return match;
     }
 
     public static void main(String[] args) {
         Problem44 prob = new Problem44();
-        System.out.println(prob.isMatch("aa", "a")); // false
-        System.out.println(prob.isMatch("aa", "*")); // true
-        System.out.println(prob.isMatch("cb", "?a")); // false
-        System.out.println(prob.isMatch("adceb", "*a*b")); // true
-        System.out.println(prob.isMatch("acdcb", "a*c?b")); // false
-        System.out.println(prob.isMatch("acdcb", "a*c?b")); // false
-        System.out.println(prob.isMatch("aabc", "a*?c")); // true
+//        System.out.println(prob.isMatch("aa", "a")); // false
+//        System.out.println(prob.isMatch("aa", "*")); // true
+//        System.out.println(prob.isMatch("cb", "?a")); // false
+//        System.out.println(prob.isMatch("adceb", "*a*b")); // true
+//        System.out.println(prob.isMatch("acdcb", "a*c?b")); // false
+//        System.out.println(prob.isMatch("acdcb", "a*c?b")); // false
+//        System.out.println(prob.isMatch("aabc", "a*?c")); // true
+        System.out.println(prob.isMatch("abc", "*abc*")); // true
     }
 }
