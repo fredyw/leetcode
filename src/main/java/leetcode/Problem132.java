d26ab431d03f0796b930b43ffc2bc68f60feba47
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-12-29 01:52:47 -0600

    Update problem 132

--- src/main/java/leetcode/Problem132.java
+++ src/main/java/leetcode/Problem132.java
@@ -11,5 +11,9 @@ public class Problem132 {
 
     public static void main(String[] args) {
         Problem132 prob = new Problem132();
+        System.out.println(prob.minCut("aab")); // 1
+        System.out.println(prob.minCut("aaa")); // 0
+        System.out.println(prob.minCut("bbaaaaabb")); // 0
+        System.out.println(prob.minCut("baaaaabb")); // 1
     }
 }
