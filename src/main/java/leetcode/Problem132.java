d8e00cf9f8b529caa624ba1103d8950d1a8f37fc
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-01-02 00:21:07 -0600

    Update problem 132

--- src/main/java/leetcode/Problem132.java
+++ src/main/java/leetcode/Problem132.java
@@ -5,8 +5,29 @@ package leetcode;
  */
 public class Problem132 {
     public int minCut(String s) {
-        // TODO
-        return 0;
+        return minCut(s, 0, 1, s.length());
+    }
+
+    private static int minCut(String s, int i, int j, int max) {
+        if (i == s.length() || j == s.length()) {
+            return max;
+        }
+        String sub = s.substring(i, j);
+        int min = max;
+        if (isPalindrome(sub)) {
+            min = Math.min(min, minCut(s, j, j + 1, max) - 1);
+        }
+        min = Math.min(min, minCut(s, i, j + 1, max));
+        return min;
+    }
+
+    private static boolean isPalindrome(String s) {
+        for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
