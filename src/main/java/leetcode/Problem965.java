7405307f89037e9a7b73e373602b5df0f099a4bd
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-01-02 00:23:17 -0600

    Skeleton for problem 965

---
+++ src/main/java/leetcode/Problem965.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/univalued-binary-tree/
+ */
+public class Problem965 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean isUnivalTree(TreeNode root) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem965 prob = new Problem965();
+    }
+}
