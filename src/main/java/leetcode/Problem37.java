0d8dc95332fa072a493e4dd0e845b4410f2b32a2
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-01-03 00:10:49 -0600

    Skeleton for problem 37

---
+++ src/main/java/leetcode/Problem37.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/sudoku-solver/
+ */
+public class Problem37 {
+    public void solveSudoku(char[][] board) {
+        // TODO
+    }
+
+    public static void main(String[] args) {
+        Problem37 prob = new Problem37();
+    }
+}
