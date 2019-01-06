d51622f081c7da2eb7013bda25e5a41d3814f6b5
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-01-06 00:36:49 -0600

    Skeleton for problem 970

---
+++ src/main/java/leetcode/Problem970.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/powerful-integers/
+ */
+public class Problem970 {
+    public List<Integer> powerfulIntegers(int x, int y, int bound) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem970 prob = new Problem970();
+    }
+}
