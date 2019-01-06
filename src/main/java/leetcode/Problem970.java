454df83ee488b49a983565862f69b71dd324729c
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-01-06 01:23:25 -0600

    Solve problem 970

--- src/main/java/leetcode/Problem970.java
+++ src/main/java/leetcode/Problem970.java
@@ -1,17 +1,31 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * https://leetcode.com/problems/powerful-integers/
  */
 public class Problem970 {
     public List<Integer> powerfulIntegers(int x, int y, int bound) {
-        // TODO
-        return null;
-    }
-
-    public static void main(String[] args) {
-        Problem970 prob = new Problem970();
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i <= bound; i++) {
+            int a = (int) Math.pow(x, i);
+            if (a > bound) {
+                break;
+            }
+            for (int j = 0; j <= bound; j++) {
+                int b = (int) Math.pow(y, j);
+                int c = a + b;
+                if (c > bound) {
+                    break;
+                } else {
+                    set.add(c);
+                }
+            }
+        }
+        return new ArrayList<>(set);
     }
 }
