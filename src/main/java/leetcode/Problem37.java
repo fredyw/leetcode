c8c01d943706e1bad46e2c7d75a5a3ef2996f9cf
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-01-07 00:00:19 -0600

    Update problem 37

--- src/main/java/leetcode/Problem37.java
+++ src/main/java/leetcode/Problem37.java
@@ -10,6 +10,55 @@ public class Problem37 {
         // TODO
     }
 
+    private static boolean isValid(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            boolean[] cols = new boolean[9];
+            boolean[] rows = new boolean[9];
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
+                    if (cols[board[i][j] - '0' - 1]) {
+                        return false;
+                    }
+                    cols[board[i][j] - '0' - 1] = true;
+                }
+
+                if (board[j][i] != '.') {
+                    if (rows[board[j][i] - '0' - 1]) {
+                        return false;
+                    }
+                    rows[board[j][i] - '0' - 1] = true;
+                }
+            }
+        }
+        for (int i = 0; i < 9; i += 3) {
+            for (int j = 0; j < 9; j += 3) {
+                if (!isValid(board, i, i + 3, j, j + 3)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static boolean isValid(char[][] board, int startRow, int endRow,
+                                   int startCol, int endCol) {
+        boolean[] b = new boolean[9];
+        for (int i = startRow; i < endRow; i++) {
+            for (int j = startCol; j < endCol; j++) {
+                if (board[i][j] == '.') {
+                    continue;
+                }
+                if (b[board[i][j] - '0' - 1]) {
+                    return false;
+                }
+                if (board[i][j] != '.') {
+                    b[board[i][j] - '0' - 1] = true;
+                }
+            }
+        }
+        return true;
+    }
+
     private static void print(char[][] board) {
         for (char[] c : board) {
             System.out.println(Arrays.toString(c));
