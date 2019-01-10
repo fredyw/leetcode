5348dcaccd0a6117759121fa9bd02e027d180b78
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-01-10 00:50:49 -0600

    Solve problem 969

--- src/main/java/leetcode/Problem969.java
+++ src/main/java/leetcode/Problem969.java
@@ -1,5 +1,6 @@
 package leetcode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -7,11 +8,31 @@ import java.util.List;
  */
 public class Problem969 {
     public List<Integer> pancakeSort(int[] A) {
-        // TODO
-        return null;
+        List<Integer> answer = new ArrayList<>();
+        int right = A.length - 1;
+        while (right >= 0) {
+            int max = 0;
+            int idx = -1;
+            for (int j = 0; j <= right; j++) {
+                if (max < A[j]) {
+                    max = A[j];
+                    idx = j;
+                }
+            }
+            reverse(A, 0, idx);
+            answer.add(idx + 1);
+            reverse(A, 0, right);
+            answer.add(right + 1);
+            right--;
+        }
+        return answer;
     }
 
-    public static void main(String[] args) {
-        Problem969 prob = new Problem969();
+    private static void reverse(int[] a, int i, int j) {
+        for (int x = i, y = j; x < y; x++, y--) {
+            int tmp = a[x];
+            a[x] = a[y];
+            a[y] = tmp;
+        }
     }
 }
