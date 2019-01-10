e10159bc0027a8a08d4e8e2a43cce9e1a7febff6
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-01-09 23:10:11 -0600

    Skeleton for problem 969

---
+++ src/main/java/leetcode/Problem969.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/pancake-sorting/
+ */
+public class Problem969 {
+    public List<Integer> pancakeSort(int[] A) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem969 prob = new Problem969();
+    }
+}
