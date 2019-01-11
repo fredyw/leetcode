d6f86250a12f5421f81d04af1c338c8d0db98ac9
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-01-11 00:12:41 -0600

    Skeleton for problem 904

---
+++ src/main/java/leetcode/Problem904.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/fruit-into-baskets/
+ */
+public class Problem904 {
+    public int totalFruit(int[] tree) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem904 prob = new Problem904();
+    }
+}
