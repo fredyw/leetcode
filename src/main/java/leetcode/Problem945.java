e05f2690a10b4095b537f557da53bc68db14d08d
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-01-11 22:23:19 -0600

    Skeleton for problem 945

---
+++ src/main/java/leetcode/Problem945.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/minimum-increment-to-make-array-unique/
+ */
+public class Problem945 {
+    public int minIncrementForUnique(int[] A) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem945 prob = new Problem945();
+    }
+}
