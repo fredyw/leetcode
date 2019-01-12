5e5a4021150f5c0b146570608fa89988ca8e4b82
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-01-11 22:42:12 -0600

    Solve problem 945

--- src/main/java/leetcode/Problem945.java
+++ src/main/java/leetcode/Problem945.java
@@ -10,17 +10,12 @@ public class Problem945 {
         Arrays.sort(A);
         int answer = 0;
         for (int i = 0, j = 1; j < A.length; i++, j++) {
-            while (A[j] <= A[i]) {
-                A[j]++;
-                answer++;
+            if (A[j] <= A[i]) {
+                int count = A[i] - A[j] + 1;
+                A[j] += count;
+                answer += count;
             }
         }
         return answer;
-    }
-
-    public static void main(String[] args) {
-        Problem945 prob = new Problem945();
-        System.out.println(prob.minIncrementForUnique(new int[]{1, 2, 2})); // 1
-        System.out.println(prob.minIncrementForUnique(new int[]{3, 2, 1, 2, 1, 7})); // 6
     }
 }
