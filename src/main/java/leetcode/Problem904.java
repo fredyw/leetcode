3d310d79df31510ed8bd4306b8c32a35c5aefaa7
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-01-11 23:07:09 -0600

    Update problem 904

--- src/main/java/leetcode/Problem904.java
+++ src/main/java/leetcode/Problem904.java
@@ -11,5 +11,9 @@ public class Problem904 {
 
     public static void main(String[] args) {
         Problem904 prob = new Problem904();
+        System.out.println(prob.totalFruit(new int[]{1, 2, 1})); // 3
+        System.out.println(prob.totalFruit(new int[]{0, 1, 2, 2})); // 3
+        System.out.println(prob.totalFruit(new int[]{1, 2, 3, 2, 2})); // 4
+        System.out.println(prob.totalFruit(new int[]{3, 3, 3, 1, 2, 1, 1, 2, 3, 3, 4})); // 5
     }
 }
