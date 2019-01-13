1d33bc258e29e79e6634d2090179306893edcd59
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-01-13 00:27:25 -0600

    Skeleton for problem 973

---
+++ src/main/java/leetcode/Problem973.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/k-closest-points-to-origin/
+ */
+public class Problem973 {
+    public int[][] kClosest(int[][] points, int K) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem973 prob = new Problem973();
+    }
+}
