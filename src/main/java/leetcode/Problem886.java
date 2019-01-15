3448ef1eaf4c2a0d6d69092e42ad587111f94d17
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-01-15 01:18:46 -0600

    Update problem 886

--- src/main/java/leetcode/Problem886.java
+++ src/main/java/leetcode/Problem886.java
@@ -11,5 +11,8 @@ public class Problem886 {
 
     public static void main(String[] args) {
         Problem886 prob = new Problem886();
+        System.out.println(prob.possibleBipartition(4, new int[][]{{1, 2}, {1, 3}, {2, 4}})); // true
+        System.out.println(prob.possibleBipartition(3, new int[][]{{1, 2}, {1, 3}, {2, 3}})); // false
+        System.out.println(prob.possibleBipartition(5, new int[][]{{1, 2}, {2, 3}, {3, 4}, {4, 5}, {1, 5}})); // false
     }
 }
