010016316766dd8da799e36e00388d5811fd4bcc
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-01-14 21:07:27 -0600

    Skeleton for problem 886

---
+++ src/main/java/leetcode/Problem886.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/possible-bipartition/
+ */
+public class Problem886 {
+    public boolean possibleBipartition(int N, int[][] dislikes) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem886 prob = new Problem886();
+    }
+}
