579209d96c70477b6af3a01903f5cdc3fb628a8d
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-01-18 00:35:58 -0600

    Update problem 848

--- src/main/java/leetcode/Problem848.java
+++ src/main/java/leetcode/Problem848.java
@@ -5,11 +5,19 @@ package leetcode;
  */
 public class Problem848 {
     public String shiftingLetters(String S, int[] shifts) {
-        // TODO
-        return null;
+        char[] chars = S.toCharArray();
+        for (int i = 0; i < shifts.length; i++) {
+            for (int j = 0; j <= i; j++) {
+                int val = (chars[j] - 'a' + shifts[i]) % 26;
+                chars[j] = (char) ('a' + val);
+            }
+        }
+        return new String(chars);
     }
 
     public static void main(String[] args) {
         Problem848 prob = new Problem848();
+        System.out.println(prob.shiftingLetters("abc", new int[]{3, 5, 9})); // rpl
+        System.out.println(prob.shiftingLetters("bad", new int[]{10, 20, 30})); // jyh
     }
 }
