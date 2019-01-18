2aabcaf1818ae968eb6eef13370027c405ad8fa7
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-01-17 18:25:19 -0600

    Skeleton for problem 948

---
+++ src/main/java/leetcode/Problem948.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/bag-of-tokens/
+ */
+public class Problem948 {
+    public int bagOfTokensScore(int[] tokens, int P) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem948 prob = new Problem948();
+    }
+}
