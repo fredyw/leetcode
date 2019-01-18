630041ca09f5d5178896856cb8723d31d4cf4e15
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-01-18 01:01:05 -0600

    Skeleton for problem 853

---
+++ src/main/java/leetcode/Problem853.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/car-fleet/
+ */
+public class Problem853 {
+    public int carFleet(int target, int[] position, int[] speed) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem853 prob = new Problem853();
+    }
+}
