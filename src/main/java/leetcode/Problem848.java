6a5337de047c7f1a302cf6448e1140bcb1736884
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-01-17 23:05:10 -0600

    Skeleton for problem 848

---
+++ src/main/java/leetcode/Problem848.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/shifting-letters/
+ */
+public class Problem848 {
+    public String shiftingLetters(String S, int[] shifts) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem848 prob = new Problem848();
+    }
+}
