d3d35c0082bc5bf1ad3336b49258052607a88ea7
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-01-20 01:23:32 -0600

    Update problem 853

--- src/main/java/leetcode/Problem853.java
+++ src/main/java/leetcode/Problem853.java
@@ -11,5 +11,6 @@ public class Problem853 {
 
     public static void main(String[] args) {
         Problem853 prob = new Problem853();
+        System.out.println(prob.carFleet(12, new int[]{10, 8, 0, 5, 3}, new int[]{2, 4, 1, 1, 3})); // 3
     }
 }
