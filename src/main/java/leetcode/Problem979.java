157510e8d329df62f7b13c88bba1fec53ffb65fb
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-01-20 01:05:36 -0600

    Skeleton for problem 979

---
+++ src/main/java/leetcode/Problem979.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/distribute-coins-in-binary-tree/
+ */
+public class Problem979 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public int distributeCoins(TreeNode root) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem979 prob = new Problem979();
+    }
+}
