3f1ba2201f1eb0ac02163065748c9186dc2f9134
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-01-21 10:13:42 -0600

    Update problem 979

--- src/main/java/leetcode/Problem979.java
+++ src/main/java/leetcode/Problem979.java
@@ -4,7 +4,7 @@ package leetcode;
  * https://leetcode.com/problems/distribute-coins-in-binary-tree/
  */
 public class Problem979 {
-    public class TreeNode {
+    public static class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
@@ -21,5 +21,26 @@ public class Problem979 {
 
     public static void main(String[] args) {
         Problem979 prob = new Problem979();
+
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(0);
+        root.right = new TreeNode(0);
+        System.out.println(prob.distributeCoins(root)); // 2
+
+        root = new TreeNode(0);
+        root.left = new TreeNode(3);
+        root.right = new TreeNode(0);
+        System.out.println(prob.distributeCoins(root)); // 3
+
+        root = new TreeNode(1);
+        root.left = new TreeNode(0);
+        root.right = new TreeNode(2);
+        System.out.println(prob.distributeCoins(root)); // 2
+
+        root = new TreeNode(1);
+        root.left = new TreeNode(0);
+        root.left.right = new TreeNode(3);
+        root.right = new TreeNode(0);
+        System.out.println(prob.distributeCoins(root)); // 4
     }
 }
