b55190429e17acaf0003e67de44e5475cf06caf3
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-01-21 13:18:21 -0600

    Skeleton for problem 978

---
+++ src/main/java/leetcode/Problem978.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/longest-turbulent-subarray/
+ */
+public class Problem978 {
+    public int maxTurbulenceSize(int[] A) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem978 prob = new Problem978();
+    }
+}
