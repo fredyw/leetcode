3d9e5d5e31f2d0379a53ebcbe8505f0a9f093b8c
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-01-21 15:32:58 -0600

    Update problem 966

--- src/main/java/leetcode/Problem966.java
+++ src/main/java/leetcode/Problem966.java
@@ -1,15 +1,22 @@
 package leetcode;
 
+import java.util.Arrays;
+
 /**
  * https://leetcode.com/problems/vowel-spellchecker/
  */
 public class Problem966 {
     public String[] spellchecker(String[] wordlist, String[] queries) {
+        String[] answer = new String[queries.length];
         // TODO
-        return null;
+        return queries;
     }
 
     public static void main(String[] args) {
         Problem966 prob = new Problem966();
+        System.out.println(Arrays.toString(prob.spellchecker(
+            new String[]{"KiTe", "kite", "hare", "Hare"},
+            new String[]{"kite", "Kite", "KiTe", "Hare", "HARE", "Hear", "hear", "keti", "keet", "keto"})));
+        // ["kite","KiTe","KiTe","Hare","hare","","","KiTe","","KiTe"]
     }
 }
