c20c7a490a2f98e313a0607cf72b624d7319bf36
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-01-21 15:28:59 -0600

    Skeleton for problem 966

---
+++ src/main/java/leetcode/Problem966.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/vowel-spellchecker/
+ */
+public class Problem966 {
+    public String[] spellchecker(String[] wordlist, String[] queries) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem966 prob = new Problem966();
+    }
+}
