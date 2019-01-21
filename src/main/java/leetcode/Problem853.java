22acb9831c1b48d6c58fb8c268cd7ab386ded741
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-01-21 01:37:21 -0600

    Update problem 853

--- src/main/java/leetcode/Problem853.java
+++ src/main/java/leetcode/Problem853.java
@@ -1,27 +1,39 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.TreeMap;
+import java.util.Arrays;
+import java.util.Comparator;
 
 /**
  * https://leetcode.com/problems/car-fleet/
  */
 public class Problem853 {
     public int carFleet(int target, int[] position, int[] speed) {
-        TreeMap<Integer, Double> map = new TreeMap<>();
-        for (int i = 0; i < position.length; ++i) {
-            map.put(position[i], (double) (target - position[i]) / speed[i]);
+        if (position.length == 0) {
+            return 0;
         }
-        double maxSoFar = 0;
-        int answer = 0;
-        List<Double> timeNeeded = new ArrayList<>(map.values());
-        for (int i = position.length - 1; i >= 0; i--) {
-            if (timeNeeded.get(i) > maxSoFar) {
-                maxSoFar = timeNeeded.get(i);
+        Car[] cars = new Car[position.length];
+        for (int i = 0; i < position.length; ++i) {
+            cars[i] = new Car(position[i], (double) (target - position[i]) / speed[i]);
+        }
+        Arrays.sort(cars, Comparator.comparingInt(a -> a.position));
+        int answer = 1;
+        for (int i = cars.length - 1; i > 0; i--) {
+            if (cars[i].time < cars[i - 1].time) {
                 answer++;
+            } else {
+                cars[i - 1] = cars[i];
             }
         }
         return answer;
+    }
+
+    private static class Car {
+        private final int position;
+        private final double time;
+
+        public Car(int position, double time) {
+            this.position = position;
+            this.time = time;
+        }
     }
 }
