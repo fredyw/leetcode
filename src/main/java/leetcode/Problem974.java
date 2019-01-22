b07705c25ab7391b73aa2b1e44c3711c1cfeec8c
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-01-22 16:40:03 -0600

    Solve problem 974

--- src/main/java/leetcode/Problem974.java
+++ src/main/java/leetcode/Problem974.java
@@ -5,29 +5,20 @@ package leetcode;
  */
 public class Problem974 {
     public int subarraysDivByK(int[] A, int K) {
-        int[] sums = new int[A.length];
-        int sum = 0;
-        for (int i = 0; i < A.length; i++) {
-            sum += A[i];
-            sums[i] += sum;
-        }
+        int total = 0;
+        int[] count = new int[K];
+        count[0] = 1;
         int answer = 0;
-        for (int i = 0; i < sums.length; i++) {
-            if (sums[i] % K == 0) {
-                answer++;
+        for (int i = 0; i < A.length; i++) {
+            total += A[i];
+            int mod = total % K;
+            // Make the mod positive.
+            if (mod < 0) {
+                mod += K;
             }
-            for (int j = i - 1; j >= 0; j--) {
-                if ((sums[i] - sums[j]) % K == 0) {
-                    answer++;
-                }
-            }
+            answer += count[mod];
+            count[mod]++;
         }
         return answer;
-    }
-
-    public static void main(String[] args) {
-        Problem974 prob = new Problem974();
-        System.out.println(prob.subarraysDivByK(new int[]{4, 5, 0, -2, -3, 1}, 5)); // 7
-        // [4, 5, 0, -2, -3, 1], [5], [5, 0], [5, 0, -2, -3], [0], [0, -2, -3], [-2, -3]
     }
 }
