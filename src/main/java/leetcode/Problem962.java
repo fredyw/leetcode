56ba4cc056545fa13f1da9c5f03716810f44d3ea
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-01-23 14:51:50 -0600

    Skeleton for problem 962

---
+++ src/main/java/leetcode/Problem962.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/maximum-width-ramp/
+ */
+public class Problem962 {
+    public int maxWidthRamp(int[] A) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem962 prob = new Problem962();
+    }
+}
