6e5286f1f409f7723d85ab85bced9233570f99c6
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-01-23 15:03:58 -0600

    Update problem 962

--- src/main/java/leetcode/Problem962.java
+++ src/main/java/leetcode/Problem962.java
@@ -11,5 +11,7 @@ public class Problem962 {
 
     public static void main(String[] args) {
         Problem962 prob = new Problem962();
+        System.out.println(prob.maxWidthRamp(new int[]{6, 0, 8, 2, 1, 5})); // 4
+        System.out.println(prob.maxWidthRamp(new int[]{9, 8, 1, 0, 1, 9, 4, 0, 4, 1})); // 7
     }
 }
