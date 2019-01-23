a9897faa81027e91ea2bcfef5ef3b85fc648997e
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-01-23 14:00:17 -0600

    Update problem 935

--- src/main/java/leetcode/Problem935.java
+++ src/main/java/leetcode/Problem935.java
@@ -6,6 +6,16 @@ package leetcode;
 public class Problem935 {
     public int knightDialer(int N) {
         // TODO
+        // 1 -> [6, 8]
+        // 2 -> [7, 9]
+        // 3 -> [4, 8]
+        // 4 -> [0, 3, 9]
+        // 5 -> []
+        // 6 -> [0, 1, 7]
+        // 7 -> [2, 6]
+        // 8 -> [1, 3]
+        // 9 -> [2, 4]
+        // 0 -> [4, 6]
         return 0;
     }
 
