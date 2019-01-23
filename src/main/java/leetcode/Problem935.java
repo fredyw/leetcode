fe721354dfd34c374762e1f9d9f763ecb9e9d59f
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-01-23 00:19:20 -0600

    Update problem 935

--- src/main/java/leetcode/Problem935.java
+++ src/main/java/leetcode/Problem935.java
@@ -11,5 +11,9 @@ public class Problem935 {
 
     public static void main(String[] args) {
         Problem935 prob = new Problem935();
+        System.out.println(prob.knightDialer(1)); // 10
+        System.out.println(prob.knightDialer(2)); // 20
+        System.out.println(prob.knightDialer(3)); // 46
+        System.out.println(prob.knightDialer(5000)); // 406880451
     }
 }
