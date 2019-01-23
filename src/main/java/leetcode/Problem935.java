0cc3909acb67f4be178664922fad2d5e43eac61a
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-01-22 22:50:18 -0600

    Skeleton for probelm 935

---
+++ src/main/java/leetcode/Problem935.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/knight-dialer/
+ */
+public class Problem935 {
+    public int knightDialer(int N) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem935 prob = new Problem935();
+    }
+}
