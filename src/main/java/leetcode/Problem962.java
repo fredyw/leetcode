fb65af8ef7015d2cebb6353c854787305b3f043b
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-01-23 22:03:02 -0600

    Update problem 962

--- src/main/java/leetcode/Problem962.java
+++ src/main/java/leetcode/Problem962.java
@@ -5,13 +5,22 @@ package leetcode;
  */
 public class Problem962 {
     public int maxWidthRamp(int[] A) {
-        // TODO
-        return 0;
+        int max = 0;
+        for (int i = 0; i < A.length; i++) {
+            for (int j = A.length - 1; j >= i; j--) {
+                if (A[i] <= A[j]) {
+                    max = Math.max(max, j - i);
+                    break;
+                }
+            }
+        }
+        return max;
     }
 
     public static void main(String[] args) {
         Problem962 prob = new Problem962();
         System.out.println(prob.maxWidthRamp(new int[]{6, 0, 8, 2, 1, 5})); // 4
         System.out.println(prob.maxWidthRamp(new int[]{9, 8, 1, 0, 1, 9, 4, 0, 4, 1})); // 7
+        System.out.println(prob.maxWidthRamp(new int[]{5, 4, 3, 2, 1})); // 0
     }
 }
