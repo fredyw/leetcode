b0c072f9247e556e25e91480f51224105858eee3
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-01-27 00:48:55 -0600

    Skeleton for problem 984

---
+++ src/main/java/leetcode/Problem984.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/string-without-aaa-or-bbb/
+ */
+public class Problem984 {
+    public String strWithout3a3b(int A, int B) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem984 prob = new Problem984();
+    }
+}
