9880815e03be2c3a0fcbedbae775d404e71e215d
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-01-27 02:00:23 -0600

    Solve problem 962

--- src/main/java/leetcode/Problem962.java
+++ src/main/java/leetcode/Problem962.java
@@ -1,26 +1,24 @@
 package leetcode;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
  * https://leetcode.com/problems/maximum-width-ramp/
  */
 public class Problem962 {
     public int maxWidthRamp(int[] A) {
-        int max = 0;
+        Integer[] indexes = new Integer[A.length];
         for (int i = 0; i < A.length; i++) {
-            for (int j = A.length - 1; j >= i; j--) {
-                if (A[i] <= A[j]) {
-                    max = Math.max(max, j - i);
-                    break;
-                }
-            }
+            indexes[i] = i;
         }
-        return max;
-    }
-
-    public static void main(String[] args) {
-        Problem962 prob = new Problem962();
-        System.out.println(prob.maxWidthRamp(new int[]{6, 0, 8, 2, 1, 5})); // 4
-        System.out.println(prob.maxWidthRamp(new int[]{9, 8, 1, 0, 1, 9, 4, 0, 4, 1})); // 7
-        System.out.println(prob.maxWidthRamp(new int[]{5, 4, 3, 2, 1})); // 0
+        Arrays.sort(indexes, Comparator.comparingInt(index -> A[index]));
+        int answer = 0;
+        int minIndex = A.length;
+        for (int index : indexes) {
+            answer = Math.max(answer, index - minIndex);
+            minIndex = Math.min(minIndex, index);
+        }
+        return answer;
     }
 }
