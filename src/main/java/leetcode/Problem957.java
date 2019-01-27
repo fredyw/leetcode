43742cc3a806db7a6f69a772334bd87bb44b477c
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-01-26 20:41:37 -0600

    Skeleton for problem 957

---
+++ src/main/java/leetcode/Problem957.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/prison-cells-after-n-days/
+ */
+public class Problem957 {
+    public int[] prisonAfterNDays(int[] cells, int N) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem957 prob = new Problem957();
+    }
+}
