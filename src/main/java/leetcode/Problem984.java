4bd3bfc341f72f0a4064d44dbed8519964bf4144
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-01-27 02:18:28 -0600

    Update problem 984

--- src/main/java/leetcode/Problem984.java
+++ src/main/java/leetcode/Problem984.java
@@ -5,11 +5,41 @@ package leetcode;
  */
 public class Problem984 {
     public String strWithout3a3b(int A, int B) {
-        // TODO
-        return null;
+        StringBuilder answer = new StringBuilder();
+        int a = A;
+        int b = B;
+        while (a + b > 0) {
+            if (A >= B) {
+                if (a > 0) {
+                    answer.append("a");
+                    a--;
+                }
+                if (b > 0) {
+                    answer.append("b");
+                    b--;
+                }
+            } else {
+                if (b > 0) {
+                    answer.append("b");
+                    b--;
+                }
+                if (a > 0) {
+                    answer.append("a");
+                    a--;
+                }
+            }
+        }
+        return answer.toString();
     }
 
     public static void main(String[] args) {
         Problem984 prob = new Problem984();
+        System.out.println(prob.strWithout3a3b(1, 2));
+        System.out.println(prob.strWithout3a3b(4, 1));
+        System.out.println(prob.strWithout3a3b(3, 1));
+        System.out.println(prob.strWithout3a3b(1, 3));
+        System.out.println(prob.strWithout3a3b(3, 3));
+        System.out.println(prob.strWithout3a3b(4, 4));
+        System.out.println(prob.strWithout3a3b(4, 5));
     }
 }
