c531ac3efc04ba910482128e3b2bfcc6516258a5
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-01-28 22:55:30 -0600

    Update problem 957

--- src/main/java/leetcode/Problem957.java
+++ src/main/java/leetcode/Problem957.java
@@ -11,5 +11,7 @@ public class Problem957 {
 
     public static void main(String[] args) {
         Problem957 prob = new Problem957();
+        System.out.println(prob.prisonAfterNDays(new int[]{0, 1, 0, 1, 1, 0, 0, 1}, 7)); // [0,0,1,1,0,0,0,0]
+        System.out.println(prob.prisonAfterNDays(new int[]{1, 0, 0, 1, 0, 0, 1, 0}, 1000000000)); // [0,0,1,1,1,1,1,0]
     }
 }
