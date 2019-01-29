2c069e2ad16f04a6269db2b283f94bd0c4045dfe
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-01-29 00:22:30 -0600

    Skeleton for problem 983

---
+++ src/main/java/leetcode/Problem983.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/minimum-cost-for-tickets/
+ */
+public class Problem983 {
+    public int mincostTickets(int[] days, int[] costs) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem983 prob = new Problem983();
+        System.out.println(prob.mincostTickets(new int[]{1, 4, 6, 7, 8, 20}, new int[]{2, 7, 15})); // 11
+        System.out.println(prob.mincostTickets(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 30, 31}, new int[]{2, 7, 15})); // 17
+    }
+}
