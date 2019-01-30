f8a559c5c50a5a0b59b074fbcf18bb0df9ebe78f
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-01-30 11:12:59 -0600

    SKeleton for problem 855

---
+++ src/main/java/leetcode/Problem855.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/exam-room/
+ */
+public class Problem855 {
+    private static class ExamRoom {
+        public ExamRoom(int N) {
+        }
+
+        public int seat() {
+            // TODO
+            return 0;
+        }
+
+        public void leave(int p) {
+            // TODO
+        }
+    }
+
+    public static void main(String[] args) {
+        Problem855 prob = new Problem855();
+    }
+}
