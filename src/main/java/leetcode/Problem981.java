2fa968c0be9d148b4cf8cabaa69e9fd65478f9fe
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-01-31 13:32:47 -0600

    Solve problem 981

--- src/main/java/leetcode/Problem981.java
+++ src/main/java/leetcode/Problem981.java
@@ -1,23 +1,51 @@
 package leetcode;
 
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeSet;
+
 /**
  * https://leetcode.com/problems/time-based-key-value-store/
  */
 public class Problem981 {
+    private static final Map<String, TreeSet<ValueTimestamp>> map = new HashMap<>();
+
     private static class TimeMap {
         public TimeMap() {
         }
 
         public void set(String key, String value, int timestamp) {
-            // TODO
+            if (!map.containsKey(key)) {
+                TreeSet<ValueTimestamp> set = new TreeSet<>(Comparator.comparingInt(a -> a.timestamp));
+                set.add(new ValueTimestamp(value, timestamp));
+                map.put(key, set);
+            } else {
+                map.get(key).add(new ValueTimestamp(value, timestamp));
+            }
         }
 
         public String get(String key, int timestamp) {
-            // TODO
-            return null;
+            TreeSet<ValueTimestamp> set = map.get(key);
+            if (set == null) {
+                return "";
+            }
+            ValueTimestamp value = set.floor(new ValueTimestamp("", timestamp));
+            if (value == null) {
+                return "";
+            }
+            return value.value;
         }
     }
 
-    public static void main(String[] args) {
+    private static class ValueTimestamp {
+        private final String value;
+        private final int timestamp;
+
+
+        public ValueTimestamp(String value, int timestamp) {
+            this.value = value;
+            this.timestamp = timestamp;
+        }
     }
 }
