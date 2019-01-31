b6c9fe0b78ac3e22a19a1538691d4ad567e4e9b3
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-01-31 01:46:14 -0600

    Skeleton for problem 981

---
+++ src/main/java/leetcode/Problem981.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/time-based-key-value-store/
+ */
+public class Problem981 {
+    private static class TimeMap {
+        public TimeMap() {
+        }
+
+        public void set(String key, String value, int timestamp) {
+            // TODO
+        }
+
+        public String get(String key, int timestamp) {
+            // TODO
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}
