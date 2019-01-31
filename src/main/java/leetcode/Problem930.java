8c0327f79397ad428a5b67030bb3de613ca6a444
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-01-31 13:33:50 -0600

    Skeleton for problem 930

---
+++ src/main/java/leetcode/Problem930.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/binary-subarrays-with-sum/
+ */
+public class Problem930 {
+    public int numSubarraysWithSum(int[] A, int S) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem930 prob = new Problem930();
+    }
+}
