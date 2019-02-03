4a72a3c84d6e177367fa864aaa91e1bc18c590de
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-02-03 15:59:05 -0600

    Skeleton for problem 967

---
+++ src/main/java/leetcode/Problem967.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/numbers-with-same-consecutive-differences/
+ */
+public class Problem967 {
+    public int[] numsSameConsecDiff(int N, int K) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem967 prob = new Problem967();
+    }
+}
