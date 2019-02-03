f150c1b821ee4d4f3a8ccad935de68534b46f7d7
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-02-03 01:05:28 -0600

    Solve problem 985

--- src/main/java/leetcode/Problem985.java
+++ src/main/java/leetcode/Problem985.java
@@ -1,32 +1,29 @@
 package leetcode;
 
-import java.util.Arrays;
-
 /**
  * https://leetcode.com/problems/sum-of-even-numbers-after-queries/
  */
 public class Problem985 {
     public int[] sumEvenAfterQueries(int[] A, int[][] queries) {
+        int sum = 0;
+        for (int a : A) {
+            if (a % 2 == 0) {
+                sum += a;
+            }
+        }
         int[] answer = new int[A.length];
         for (int i = 0; i < queries.length; i++) {
             int val = queries[i][0];
             int index = queries[i][1];
+            if (A[index] % 2 == 0) {
+                sum -= A[index];
+            }
             A[index] += val;
-            int sum = 0;
-            for (int a : A) {
-                if (a % 2 == 0) {
-                    sum += a;
-                }
+            if (A[index] %2 == 0) {
+                sum += A[index];
             }
             answer[i] = sum;
         }
         return answer;
-    }
-
-    public static void main(String[] args) {
-        Problem985 prob = new Problem985();
-        System.out.println(Arrays.toString(prob.sumEvenAfterQueries(new int[]{1, 2, 3, 4}, new int[][]{
-            {1, 0}, {-3, 1}, {-4, 0}, {2, 3}
-        }))); // [8,6,2,4]
     }
 }
