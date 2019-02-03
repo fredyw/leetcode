0ab665b0cbab51f304b2a04aa2ae3a59b40be2df
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-02-03 01:21:06 -0600

    Skeleton for problem 988

---
+++ src/main/java/leetcode/Problem988.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/smallest-string-starting-from-leaf/
+ */
+public class Problem988 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public String smallestFromLeaf(TreeNode root) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem988 prob = new Problem988();
+    }
+}
