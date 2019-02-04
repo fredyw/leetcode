c9d20214b5638f08e2fe1ef0ab781e902d700f94
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-02-04 12:04:23 -0600

    Update problem 842

--- src/main/java/leetcode/Problem842.java
+++ src/main/java/leetcode/Problem842.java
@@ -13,5 +13,10 @@ public class Problem842 {
 
     public static void main(String[] args) {
         Problem842 prob = new Problem842();
+        System.out.println(prob.splitIntoFibonacci("123456579")); // [123,456,579]
+        System.out.println(prob.splitIntoFibonacci("11235813")); // [1,1,2,3,5,8,13]
+        System.out.println(prob.splitIntoFibonacci("112358130")); // []
+        System.out.println(prob.splitIntoFibonacci("0123")); // []
+        System.out.println(prob.splitIntoFibonacci("1101111")); // [110, 1, 111]
     }
 }
