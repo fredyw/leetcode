1acfea4606fbdf373a0b1dedc460d31b18788d67
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-02-04 00:29:56 -0600

    Skeleton for problem 842

---
+++ src/main/java/leetcode/Problem842.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/split-array-into-fibonacci-sequence/
+ */
+public class Problem842 {
+    public List<Integer> splitIntoFibonacci(String S) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem842 prob = new Problem842();
+    }
+}
