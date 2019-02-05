6ede734ccd18ad0255409aeac4acd82b99183f74
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-02-04 23:55:05 -0600

    Skeleton for problem 987

---
+++ src/main/java/leetcode/Problem987.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/
+ */
+public class Problem987 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem987 prob = new Problem987();
+    }
+}
