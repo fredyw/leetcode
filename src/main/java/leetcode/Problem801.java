e012c388c14332761249f0bba63d42518383e235
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-02-05 01:12:37 -0600

    Skeleton for problem 801

---
+++ src/main/java/leetcode/Problem801.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/minimum-swaps-to-make-sequences-increasing/
+ */
+public class Problem801 {
+    public int minSwap(int[] A, int[] B) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem801 prob = new Problem801();
+        System.out.println(prob.minSwap(new int[]{1, 3, 5, 4}, new int[]{1, 2, 3, 7})); // 1
+    }
+}
