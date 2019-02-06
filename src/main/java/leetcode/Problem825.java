a9a22ccb5348bf789c6a4021033ce798ba2d8cc4
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-02-06 01:14:57 -0600

    Update problem 825

--- src/main/java/leetcode/Problem825.java
+++ src/main/java/leetcode/Problem825.java
@@ -5,8 +5,22 @@ package leetcode;
  */
 public class Problem825 {
     public int numFriendRequests(int[] ages) {
-        // TODO
-        return 0;
+        int answer = 0;
+        for (int i = 0; i < ages.length; i++) {
+            for (int j = i + 1; j < ages.length; j++) {
+                if (accept(ages[i], ages[j])) {
+                    answer++;
+                }
+                if (accept(ages[j], ages[i])) {
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
+
+    private static boolean accept(int a, int b) {
+        return (!((a <= 0.5 * b + 7) || (a > b) || (a > 100 && b < 100)));
     }
 
     public static void main(String[] args) {
