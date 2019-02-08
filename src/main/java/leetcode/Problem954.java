e8fb27a8814c4aec05c4947cefd343d7b480cabd
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-02-08 01:19:01 -0600

    Update problem 954

--- src/main/java/leetcode/Problem954.java
+++ src/main/java/leetcode/Problem954.java
@@ -6,14 +6,18 @@ package leetcode;
 public class Problem954 {
     public boolean canReorderDoubled(int[] A) {
         // TODO
+        for (int i = 0; i < A.length / 2; i++) {
+            System.out.println(i + ": " + A[2 * i + 1] + " --> " + (2 * A[2 * i]));
+        }
         return false;
     }
 
     public static void main(String[] args) {
         Problem954 prob = new Problem954();
-        System.out.println(prob.canReorderDoubled(new int[]{3, 1, 3, 6})); // false
-        System.out.println(prob.canReorderDoubled(new int[]{2, 1, 2, 6})); // false
-        System.out.println(prob.canReorderDoubled(new int[]{4, -2, 2, -4})); // true
-        System.out.println(prob.canReorderDoubled(new int[]{1, 2, 4, 16, 8, 4})); // false
+//        System.out.println(prob.canReorderDoubled(new int[]{3, 1, 3, 6})); // false
+//        System.out.println(prob.canReorderDoubled(new int[]{2, 1, 2, 6})); // false
+//        System.out.println(prob.canReorderDoubled(new int[]{4, -2, 2, -4})); // true
+        System.out.println(prob.canReorderDoubled(new int[]{-2, -4, 2, 4})); // true
+//        System.out.println(prob.canReorderDoubled(new int[]{1, 2, 4, 16, 8, 4})); // false
     }
 }
