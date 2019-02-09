a613070ed3baba368b8fc93ef3e7648d72f5b2f4
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-02-08 23:38:20 -0600

    Skeleton for problem 799

---
+++ src/main/java/leetcode/Problem799.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/champagne-tower/
+ */
+public class Problem799 {
+    public double champagneTower(int poured, int query_row, int query_glass) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem799 prob = new Problem799();
+        System.out.println(prob.champagneTower(1, 1, 1)); // 0.0
+        System.out.println(prob.champagneTower(2, 1, 1)); // 0.5
+    }
+}
