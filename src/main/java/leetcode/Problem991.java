2dcfe0594226afb26986e178516e6776e02d508e
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-02-11 17:42:48 -0600

    Update problem 991

--- src/main/java/leetcode/Problem991.java
+++ src/main/java/leetcode/Problem991.java
@@ -5,15 +5,26 @@ package leetcode;
  */
 public class Problem991 {
     public int brokenCalc(int X, int Y) {
-        // TODO
-        return 0;
+        if (X == Y) {
+            return 0;
+        }
+        int a = Integer.MAX_VALUE;
+        int b = Integer.MAX_VALUE;
+        if (X < Y) {
+            a = brokenCalc(X * 2, Y) + 1;
+        }
+        if (X > Y) {
+            b = brokenCalc(X - 1, Y) + 1;
+        }
+        int min = Math.min(a, b);
+        return min;
     }
 
     public static void main(String[] args) {
         Problem991 prob = new Problem991();
-        System.out.println(prob.brokenCalc(2, 3)); // 2
-        System.out.println(prob.brokenCalc(5, 8)); // 2
+//        System.out.println(prob.brokenCalc(2, 3)); // 2
+//        System.out.println(prob.brokenCalc(5, 8)); // 2
         System.out.println(prob.brokenCalc(3, 10)); // 3
-        System.out.println(prob.brokenCalc(1024, 1)); // 1023
+//        System.out.println(prob.brokenCalc(1024, 1)); // 1023
     }
 }
