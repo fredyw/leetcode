88bb1ec4381026d0f43ce3c9a3dc45d9f43ab407
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-02-11 01:41:24 -0600

    Solve problem 990

--- src/main/java/leetcode/Problem990.java
+++ src/main/java/leetcode/Problem990.java
@@ -1,51 +1,60 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * https://leetcode.com/problems/satisfiability-of-equality-equations/
  */
 public class Problem990 {
     public boolean equationsPossible(String[] equations) {
-        int[] nums = new int[26];
-        int num = 1;
+        UnionFind uf = new UnionFind();
+        List<String> notEquals = new ArrayList<>();
         for (String equation : equations) {
             char left = equation.charAt(0);
             char right = equation.charAt(3);
             if (equation.substring(1, 3).equals("==")) {
-                if (nums[left - 'a'] == 0 && nums[right - 'a'] == 0) {
-                    nums[left - 'a'] = num;
-                    nums[right - 'a'] = num++;
-                } else if (nums[left - 'a'] == 0) {
-                    nums[left - 'a'] = nums[right - 'a'];
-                } else if (nums[right - 'a'] == 0) {
-                    nums[right - 'a'] = nums[left - 'a'];
-                }
-                if (nums[left - 'a'] != nums[right - 'a']) {
-                    return false;
-                }
+                uf.union(left, right);
             } else {
-                if (nums[left - 'a'] == 0 && nums[right - 'a'] == 0) {
-                    nums[left - 'a'] = num++;
-                    nums[right - 'a'] = num++;
-                } else if (nums[left - 'a'] == 0) {
-                    nums[left - 'a'] = num++;
-                } else if (nums[right - 'a'] == 0) {
-                    nums[right - 'a'] = num++;
-                }
-                if (nums[left - 'a'] == nums[right - 'a']) {
+                if (left == right) {
                     return false;
                 }
+                notEquals.add(equation);
+            }
+        }
+        for (String notEqual : notEquals) {
+            char left = notEqual.charAt(0);
+            char right = notEqual.charAt(3);
+            if (uf.array[left - 'a'] == null || uf.array[right - 'a'] == null) {
+                continue;
+            }
+            if (uf.array[left - 'a'] == uf.array[right - 'a']) {
+                return false;
             }
         }
         return true;
     }
 
-    public static void main(String[] args) {
-        Problem990 prob = new Problem990();
-//        System.out.println(prob.equationsPossible(new String[]{"a==b", "b!=a"})); // false
-//        System.out.println(prob.equationsPossible(new String[]{"b==a", "a==b"})); // true
-//        System.out.println(prob.equationsPossible(new String[]{"a==b", "b==c", "a==c"})); // true
-//        System.out.println(prob.equationsPossible(new String[]{"a==b", "b!=c", "c==a"})); // false
-//        System.out.println(prob.equationsPossible(new String[]{"c==c", "b==d", "x!=z"})); // true
-        System.out.println(prob.equationsPossible(new String[]{"c==c", "f!=a", "f==b", "b==c"})); // true
+    private static class UnionFind {
+        private final Character[] array = new Character[26];
+
+        private void union(char a, char b) {
+            if (array[a - 'a'] == null) {
+                array[a - 'a'] = a;
+            }
+            if (array[b - 'a'] == null) {
+                array[b - 'a'] = b;
+            }
+            if (array[a - 'a'] != null && array[b - 'a'] != null &&
+                array[a - 'a'] == array[b - 'a']) {
+                return;
+            }
+            Character val = array[a - 'a'];
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] == val) {
+                    array[i] = array[b - 'a'];
+                }
+            }
+        }
     }
 }
