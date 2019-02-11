77b442aa49aa8b084660a9c26e91afd1c673c4a9
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-02-11 01:49:47 -0600

    Skeleton for problem 991

---
+++ src/main/java/leetcode/Problem991.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/broken-calculator/
+ */
+public class Problem991 {
+    public int brokenCalc(int X, int Y) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem991 prob = new Problem991();
+        System.out.println(prob.brokenCalc(2, 3)); // 2
+        System.out.println(prob.brokenCalc(5, 8)); // 2
+        System.out.println(prob.brokenCalc(3, 10)); // 3
+        System.out.println(prob.brokenCalc(1024, 1)); // 1023
+    }
+}
