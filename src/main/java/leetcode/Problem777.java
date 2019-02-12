fbc1dcf118181057725e63aa81fa23865d0adb35
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-02-11 22:46:04 -0600

    Skeleton for problem 777

---
+++ src/main/java/leetcode/Problem777.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/swap-adjacent-in-lr-string/
+ */
+public class Problem777 {
+    public boolean canTransform(String start, String end) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem777 prob = new Problem777();
+        System.out.println(prob.canTransform("RXXLRXRXL", "XRLXXRRLX")); // true
+    }
+}
