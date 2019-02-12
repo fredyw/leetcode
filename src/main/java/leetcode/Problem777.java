2b7c836f70ed4f1b7ef4d7e699bc6a381fd004a6
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-02-11 23:01:28 -0600

    Update problem 777

--- src/main/java/leetcode/Problem777.java
+++ src/main/java/leetcode/Problem777.java
@@ -5,12 +5,38 @@ package leetcode;
  */
 public class Problem777 {
     public boolean canTransform(String start, String end) {
-        // TODO
-        return false;
+        char[] s = start.toCharArray();
+        char[] e = end.toCharArray();
+        for (int i = 0; i < s.length; i++) {
+            if (s[i] != e[i]) {
+                if (i + 1 == s.length) {
+                    return false;
+                }
+                if ((s[i] == 'X' && s[i + 1] == 'L') ||
+                    (s[i] == 'R' && s[i + 1] == 'X')) {
+                    swap(e, i, i + 1);
+                    if (s[i] != e[i]) {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static void swap(char[] chars, int i, int j) {
+        char tmp = chars[i];
+        chars[i] = chars[j];
+        chars[j] = tmp;
     }
 
     public static void main(String[] args) {
         Problem777 prob = new Problem777();
-        System.out.println(prob.canTransform("RXXLRXRXL", "XRLXXRRLX")); // true
+//        System.out.println(prob.canTransform("RXXLRXRXL", "XRLXXRRLX")); // true
+//        System.out.println(prob.canTransform("RXXLRXRXL", "XRLXXRRLL")); // false
+//        System.out.println(prob.canTransform("LR", "RL")); // false
+        System.out.println(prob.canTransform("XXXXXLXXXX", "LXXXXXXXXX")); // true
     }
 }
