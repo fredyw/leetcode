2df88183b555417816d9dceb5543247a6285bfa0
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-02-13 00:16:29 -0600

    Skeleton for problem 823

---
+++ src/main/java/leetcode/Problem823.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/binary-trees-with-factors/
+ */
+public class Problem823 {
+    public int numFactoredBinaryTrees(int[] A) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem823 prob = new Problem823();
+        System.out.println(prob.numFactoredBinaryTrees(new int[]{2, 4})); // 3
+        System.out.println(prob.numFactoredBinaryTrees(new int[]{2, 4, 5, 10})); // 7
+    }
+}
