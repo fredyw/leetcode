b3be994e4f756a0066950cad5e374f54fe666144
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-02-14 00:20:36 -0600

    Skeleton for problem 955

---
+++ src/main/java/leetcode/Problem955.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/delete-columns-to-make-sorted-ii/
+ */
+public class Problem955 {
+    public int minDeletionSize(String[] A) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem955 prob = new Problem955();
+        System.out.println(prob.minDeletionSize(new String[]{"ca", "bb", "ac"})); // 1
+        System.out.println(prob.minDeletionSize(new String[]{"xc", "yb", "za"})); // 0
+    }
+}
