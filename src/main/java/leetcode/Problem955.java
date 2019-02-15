bc31c6471a0adb417310c04413f76b4b08b3caad
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-02-14 22:11:58 -0600

    Update problem 955

--- src/main/java/leetcode/Problem955.java
+++ src/main/java/leetcode/Problem955.java
@@ -5,15 +5,44 @@ package leetcode;
  */
 public class Problem955 {
     public int minDeletionSize(String[] A) {
-        // TODO
-        return 0;
+        int n = A[0].length();
+        char[][] chars = new char[A.length][];
+        for (int i = 0; i < A.length; i++) {
+            chars[i] = A[i].toCharArray();
+        }
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            char[] prev = null;
+            boolean remove = false;
+            for (int j = 0; j < chars.length; j++) {
+                if (j == 0) {
+                    prev = chars[j];
+                } else {
+                    if (new String(prev).compareTo(new String(chars[j])) > 0) {
+                        remove = true;
+                        break;
+                    }
+                }
+            }
+            if (remove) {
+                answer++;
+                for (int j = 0; j < chars.length; j++) {
+                    chars[j][i] = ' ';
+                }
+            }
+        }
+        for (int i = 0; i < chars.length; i++) {
+            System.out.println(new String(chars[i]));
+        }
+        return answer;
     }
 
     public static void main(String[] args) {
         Problem955 prob = new Problem955();
-        System.out.println(prob.minDeletionSize(new String[]{"ca", "bb", "ac"})); // 1
-        System.out.println(prob.minDeletionSize(new String[]{"xc", "yb", "za"})); // 0
-        System.out.println(prob.minDeletionSize(new String[]{"zyx","wvu","tsr"})); // 3
-        System.out.println(prob.minDeletionSize(new String[]{"abcdef", "uvwxyz"})); // 0
+//        System.out.println(prob.minDeletionSize(new String[]{"ca", "bb", "ac"})); // 1
+//        System.out.println(prob.minDeletionSize(new String[]{"xc", "yb", "za"})); // 0
+//        System.out.println(prob.minDeletionSize(new String[]{"zyx","wvu","tsr"})); // 3
+//        System.out.println(prob.minDeletionSize(new String[]{"abcdef", "uvwxyz"})); // 0
+        System.out.println(prob.minDeletionSize(new String[]{"xga", "xfb", "yfa"})); // 1
     }
 }
