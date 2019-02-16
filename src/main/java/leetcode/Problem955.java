87c775d03f24c7270141bce856b1f15ac6d29f34
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-02-16 11:47:33 -0600

    Solve problem 955

--- src/main/java/leetcode/Problem955.java
+++ src/main/java/leetcode/Problem955.java
@@ -1,9 +1,6 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.Arrays;
 
 /**
  * https://leetcode.com/problems/delete-columns-to-make-sorted-ii/
@@ -12,92 +9,28 @@ public class Problem955 {
     public int minDeletionSize(String[] A) {
         int n = A[0].length();
         int answer = 0;
-        Map<Character, List<Integer>> map = new HashMap<>();
+        String[] strings = new String[A.length];
+        Arrays.fill(strings, "");
         for (int i = 0; i < n; i++) {
-            boolean done = true;
-            boolean remove = false;
-            if (map.isEmpty()) {
-                int prevIdx = 0;
-                char prevValue = ' ';
-                boolean first = true;
-                for (int j = 0; j < A.length; j++) {
-                    if (first) {
-                        first = false;
-                    } else {
-                        if (prevValue > A[j].charAt(i)) {
-                            remove = true;
-                            map.clear();
-                            done = false;
-                            break;
-                        } else if (prevValue == A[j].charAt(i)) {
-                            List<Integer> list = map.get(prevValue);
-                            if (list == null) {
-                                list = new ArrayList<>();
-                                map.put(prevValue, list);
-                            }
-                            // TODO
-                            list.add(prevIdx);
-                            list.add(j);
-                            done = false;
-                        }
-                    }
-                    prevIdx = j;
-                    prevValue = A[j].charAt(i);
-                }
-                System.out.println(map);
-            } else {
-                Map<Character, List<Integer>> tmpMap = new HashMap<>();
-                outer:
-                for (List<Integer> indexes : map.values()) {
-                    int prevIdx = 0;
-                    char prevValue = ' ';
-                    boolean first = true;
-                    for (int idx : indexes) {
-                        if (first) {
-                            first = false;
-                        } else {
-                            if (prevValue > A[idx].charAt(i)) {
-                                remove = true;
-                                done = false;
-                                break outer;
-                            } else if (prevValue == A[idx].charAt(i)) {
-                                List<Integer> list = tmpMap.get(prevValue);
-                                if (list == null) {
-                                    list = new ArrayList<>();
-                                    tmpMap.put(prevValue, list);
-                                }
-                                // TODO
-                                list.add(prevIdx);
-                                list.add(idx);
-                                done = false;
-                            }
-                        }
-                        prevIdx = idx;
-                        prevValue = A[idx].charAt(i);
-                    }
-                }
-                map = tmpMap;
-                System.out.println(map);
+            String[] tmp = Arrays.copyOf(strings, A.length);
+            for (int j = 0; j < A.length; j++) {
+                tmp[j] += A[j].charAt(i);
             }
-            if (remove) {
+            if (!isSorted(tmp)) {
                 answer++;
-            }
-            if (done) {
-                break;
+            } else {
+                strings = tmp;
             }
         }
         return answer;
     }
 
-    public static void main(String[] args) {
-        Problem955 prob = new Problem955();
-//        System.out.println(prob.minDeletionSize(new String[]{"ca", "bb", "ac"})); // 1
-//        System.out.println(prob.minDeletionSize(new String[]{"xc", "yb", "za"})); // 0
-//        System.out.println(prob.minDeletionSize(new String[]{"zyx","wvu","tsr"})); // 3
-//        System.out.println(prob.minDeletionSize(new String[]{"abcdef", "uvwxyz"})); // 0
-//        System.out.println(prob.minDeletionSize(new String[]{"xga", "xfb", "yfa"})); // 1
-//        System.out.println(prob.minDeletionSize(new String[]{"xac", "xbb", "yax"})); // 0
-        System.out.println(prob.minDeletionSize(new String[]{"abx", "agz", "bgc", "bfc"})); // 1
-//        System.out.println(prob.minDeletionSize(new String[]{"aby", "abx", "bcy", "bcx"})); // 1
+    private static boolean isSorted(String[] strings) {
+        for (int i = 0; i < strings.length - 1; i++) {
+            if (strings[i].compareTo(strings[i + 1]) > 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
