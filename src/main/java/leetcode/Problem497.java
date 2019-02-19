d324b0d59aeaec1b73940da8ebae5a6d7856574e
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-02-18 23:08:16 -0600

    Skeleton for problem 497

---
+++ src/main/java/leetcode/Problem497.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/random-point-in-non-overlapping-rectangles/
+ */
+public class Problem497 {
+    private static class Solution {
+        public Solution(int[][] rects) {
+        }
+
+        public int[] pick() {
+            // TODO
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}
