4c7bea8b5bd22e838649d51eea4f6247f5f1e1ff
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-02-20 01:03:41 -0600

    Skeleton for problem 923

---
+++ src/main/java/leetcode/Problem923.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/3sum-with-multiplicity/
+ */
+public class Problem923 {
+    public int threeSumMulti(int[] A, int target) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem923 prob = new Problem923();
+        System.out.println(prob.threeSumMulti(new int[]{1, 1, 2, 2, 3, 3, 4, 4, 5, 5}, 8)); // 20
+        System.out.println(prob.threeSumMulti(new int[]{1, 1, 2, 2, 2, 2}, 5)); // 12
+    }
+}
