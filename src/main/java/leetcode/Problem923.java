ed378956943d072b259a56988decad041d9ca729
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-02-21 00:49:33 -0600

    Solve problem 923

--- src/main/java/leetcode/Problem923.java
+++ src/main/java/leetcode/Problem923.java
@@ -1,17 +1,39 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * https://leetcode.com/problems/3sum-with-multiplicity/
  */
 public class Problem923 {
     public int threeSumMulti(int[] A, int target) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem923 prob = new Problem923();
-        System.out.println(prob.threeSumMulti(new int[]{1, 1, 2, 2, 3, 3, 4, 4, 5, 5}, 8)); // 20
-        System.out.println(prob.threeSumMulti(new int[]{1, 1, 2, 2, 2, 2}, 5)); // 12
+        Arrays.sort(A);
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < A.length; i++) {
+            if (!map.containsKey(A[i])) {
+                map.put(A[i], new ArrayList<>());
+            }
+            map.get(A[i]).add(i);
+        }
+        long answer = 0;
+        for (int i = 0; i < A.length; i++) {
+            for (int j = i + 1; j < A.length; j++) {
+                int k = target - (A[i] + A[j]);
+                List<Integer> list = map.get(k);
+                if (list != null) {
+                    int index = Collections.binarySearch(list, j + 1);
+                    if (index < 0) {
+                        index = -index - 1;
+                    }
+                    answer += list.size() - index;
+                }
+            }
+        }
+        return (int) (answer % (1e9 + 7));
     }
 }
