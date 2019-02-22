a0a5267e710902cee1b391729cc4ebedf4327fac
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-02-22 01:14:54 -0600

    Update problem 898

--- src/main/java/leetcode/Problem898.java
+++ src/main/java/leetcode/Problem898.java
@@ -14,5 +14,6 @@ public class Problem898 {
         System.out.println(prob.subarrayBitwiseORs(new int[]{0})); // 1
         System.out.println(prob.subarrayBitwiseORs(new int[]{1, 1, 2})); // 3
         System.out.println(prob.subarrayBitwiseORs(new int[]{1, 2, 4})); // 6
+        System.out.println(prob.subarrayBitwiseORs(new int[]{1, 2, 4, 5})); // 7
     }
 }
