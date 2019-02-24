3c57cba6c7d96d86c5544b8b6b5e450ee363fc8f
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-02-23 22:48:49 -0600

    Solve problem 999

--- src/main/java/leetcode/Problem999.java
+++ src/main/java/leetcode/Problem999.java
@@ -5,11 +5,57 @@ package leetcode;
  */
 public class Problem999 {
     public int numRookCaptures(char[][] board) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem999 prob = new Problem999();
+        int answer = 0;
+        int maxRow = board.length;
+        int maxCol = maxRow > 0 ? board[0].length : 0;
+        outer:
+        for (int i = 0; i < maxRow; i++) {
+            for (int j = 0; j < maxCol; j++) {
+                if (board[i][j] == 'R') {
+                    // up
+                    for (int k = i - 1; k >= 0; k--) {
+                        if (board[k][j] == '.') {
+                            continue;
+                        }
+                        if (board[k][j] == 'p') {
+                            answer++;
+                        }
+                        break;
+                    }
+                    // right
+                    for (int k = j + 1; k < maxCol; k++) {
+                        if (board[i][k] == '.') {
+                            continue;
+                        }
+                        if (board[i][k] == 'p') {
+                            answer++;
+                        }
+                        break;
+                    }
+                    // down
+                    for (int k = i + 1; k < maxRow; k++) {
+                        if (board[k][j] == '.') {
+                            continue;
+                        }
+                        if (board[k][j] == 'p') {
+                            answer++;
+                        }
+                        break;
+                    }
+                    // left
+                    for (int k = j - 1; k >= 0; k--) {
+                        if (board[i][k] == '.') {
+                            continue;
+                        }
+                        if (board[i][k] == 'p') {
+                            answer++;
+                        }
+                        break;
+                    }
+                    break outer;
+                }
+            }
+        }
+        return answer;
     }
 }
