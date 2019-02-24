321de5d1cf9c8d6644aeea37161b4d09a7459779
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-02-23 22:34:29 -0600

    Skeleton for problem 999

---
+++ src/main/java/leetcode/Problem999.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/available-captures-for-rook/
+ */
+public class Problem999 {
+    public int numRookCaptures(char[][] board) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem999 prob = new Problem999();
+    }
+}
