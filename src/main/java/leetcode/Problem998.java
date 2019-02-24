ab92051be78dffade5b8f8e3276872f69cf8b7e6
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-02-23 23:25:43 -0600

    Skeleton for problem 998

---
+++ src/main/java/leetcode/Problem998.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/maximum-binary-tree-ii/
+ */
+public class Problem998 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode insertIntoMaxTree(TreeNode root, int val) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem998 prob = new Problem998();
+    }
+}
