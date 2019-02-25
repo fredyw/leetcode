c44c42e7b16da486887ce7e8ae997661d9b0476c
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-02-24 22:35:50 -0600

    Skeleton for problem 837

---
+++ src/main/java/leetcode/Problem837.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/new-21-game/
+ */
+public class Problem837 {
+    public double new21Game(int N, int K, int W) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem837 prob = new Problem837();
+        System.out.println(prob.new21Game(10, 1, 10)); // 1.00000
+        System.out.println(prob.new21Game(6, 1, 10)); // 0.60000
+        System.out.println(prob.new21Game(21, 17, 10)); // 0.73278
+    }
+}
