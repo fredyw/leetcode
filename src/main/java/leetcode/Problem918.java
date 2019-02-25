4e637fea0093281677b2473953a2612c219eb9b7
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-02-24 21:28:21 -0600

    Update problem 918

--- src/main/java/leetcode/Problem918.java
+++ src/main/java/leetcode/Problem918.java
@@ -5,8 +5,18 @@ package leetcode;
  */
 public class Problem918 {
     public int maxSubarraySumCircular(int[] A) {
-        // TODO
-        return 0;
+        int answer = A[0];
+        for (int i = 0; i < A.length; i++) {
+            int sum = A[i];
+            answer = Math.max(answer, sum);
+            int j = (i + 1) % A.length;
+            while (j != i) {
+                sum += A[j];
+                answer = Math.max(answer, sum);
+                j = (j + 1) % A.length;
+            }
+        }
+        return answer;
     }
 
     public static void main(String[] args) {
