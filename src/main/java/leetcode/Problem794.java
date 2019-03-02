7ae511ed738003eec026b25a5d8667b516c4ffe3
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-03-02 17:04:08 -0600

    Skeleton for problem 794

---
+++ src/main/java/leetcode/Problem794.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/valid-tic-tac-toe-state/
+ */
+public class Problem794 {
+    public boolean validTicTacToe(String[] board) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem794 prob = new Problem794();
+    }
+}
