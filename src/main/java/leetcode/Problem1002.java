b05fbd407191e1ed0e22781629aff27cc7852e7f
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-03-03 12:06:38 -0600

    Skeleton for problem 1002

---
+++ src/main/java/leetcode/Problem1002.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/find-common-characters/
+ */
+public class Problem1002 {
+    public List<String> commonChars(String[] A) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem1002 prob = new Problem1002();
+        System.out.println(prob.commonChars(new String[]{"bella", "label", "roller"})); // ["e","l","l"]
+        System.out.println(prob.commonChars(new String[]{"cool", "lock", "cook"})); // ["c","o"]
+    }
+}
