8f1bd6dfded2e8cc4ec5c8f8f1746950597bf6ca
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-03-04 00:32:40 -0600

    Update problem 837

--- src/main/java/leetcode/Problem837.java
+++ src/main/java/leetcode/Problem837.java
@@ -5,8 +5,22 @@ package leetcode;
  */
 public class Problem837 {
     public double new21Game(int N, int K, int W) {
-        // TODO
-        return 0;
+        return new21Game(N, K, W, 0);
+    }
+
+    private static double new21Game(int n, int k, int w, int val) {
+        if (val >= k) {
+            if (val <= n) {
+                return 1;
+            }
+            return 0;
+        }
+        double sum = 0;
+        for (int i = 1; i <= w; i++) {
+            double p = new21Game(n, k, w, val + i) * (1.0 / w);
+            sum += p;
+        }
+        return sum;
     }
 
     public static void main(String[] args) {
