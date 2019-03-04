f4bbd56dcd4788b01105ed44a4912accac354feb
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-03-04 00:36:01 -0600

    Solve problem 837

--- src/main/java/leetcode/Problem837.java
+++ src/main/java/leetcode/Problem837.java
@@ -5,30 +5,26 @@ package leetcode;
  */
 public class Problem837 {
     public double new21Game(int N, int K, int W) {
-        return new21Game(N, K, W, 0);
+        Double[] memo = new Double[N + 1];
+        return new21Game(N, K, W, 0, memo);
     }
 
-    private static double new21Game(int n, int k, int w, int val) {
+    private static double new21Game(int n, int k, int w, int val, Double[] memo) {
         if (val >= k) {
             if (val <= n) {
                 return 1;
             }
             return 0;
         }
+        if (memo[val] != null) {
+            return memo[val];
+        }
         double sum = 0;
         for (int i = 1; i <= w; i++) {
-            double p = new21Game(n, k, w, val + i) * (1.0 / w);
+            double p = new21Game(n, k, w, val + i, memo) * (1.0 / w);
             sum += p;
         }
+        memo[val] = sum;
         return sum;
-    }
-
-    public static void main(String[] args) {
-        Problem837 prob = new Problem837();
-        System.out.println(prob.new21Game(10, 1, 10)); // 1.00000
-        System.out.println(prob.new21Game(6, 1, 10)); // 0.60000
-        System.out.println(prob.new21Game(21, 17, 10)); // 0.73278
-        System.out.println(prob.new21Game(2, 2, 4)); // 0.3125
-        System.out.println(prob.new21Game(10, 2, 10)); // 0.99
     }
 }
