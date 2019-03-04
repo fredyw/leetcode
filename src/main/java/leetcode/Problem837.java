0606bccad0cc477e954fe052e7ee62d13d82b691
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-03-04 00:08:52 -0600

    Update problem 837

--- src/main/java/leetcode/Problem837.java
+++ src/main/java/leetcode/Problem837.java
@@ -14,5 +14,7 @@ public class Problem837 {
         System.out.println(prob.new21Game(10, 1, 10)); // 1.00000
         System.out.println(prob.new21Game(6, 1, 10)); // 0.60000
         System.out.println(prob.new21Game(21, 17, 10)); // 0.73278
+        System.out.println(prob.new21Game(2, 2, 4)); // 0.3125
+        System.out.println(prob.new21Game(10, 2, 10)); // 0.99
     }
 }
