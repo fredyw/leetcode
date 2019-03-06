0224a53a9a09da240d95da6be8cee9216175c790
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-03-06 00:02:19 -0600

    Skeleton for problem 907

---
+++ src/main/java/leetcode/Problem907.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/sum-of-subarray-minimums/
+ */
+public class Problem907 {
+    public int sumSubarrayMins(int[] A) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem907 prob = new Problem907();
+        System.out.println(prob.sumSubarrayMins(new int[]{3, 1, 2, 4})); // 17
+    }
+}
