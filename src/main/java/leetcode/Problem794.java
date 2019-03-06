fe4e8482460a15758359d542391a525468163b1b
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-03-05 23:01:06 -0600

    Update problem 794

--- src/main/java/leetcode/Problem794.java
+++ src/main/java/leetcode/Problem794.java
@@ -11,5 +11,9 @@ public class Problem794 {
 
     public static void main(String[] args) {
         Problem794 prob = new Problem794();
+        System.out.println(prob.validTicTacToe(new String[]{"O  ", "   ", "   "})); // false
+        System.out.println(prob.validTicTacToe(new String[]{"XOX", " X ", "   "})); // false
+        System.out.println(prob.validTicTacToe(new String[]{"XXX", "   ", "OOO"})); // false
+        System.out.println(prob.validTicTacToe(new String[]{"XOX", "O O", "XOX"})); // true
     }
 }
