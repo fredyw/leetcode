d6ba20d7b5acf4ab9eb26ddf20b4684865911429
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-03-08 23:36:10 -0600

    Update problem 907

--- src/main/java/leetcode/Problem907.java
+++ src/main/java/leetcode/Problem907.java
@@ -5,12 +5,21 @@ package leetcode;
  */
 public class Problem907 {
     public int sumSubarrayMins(int[] A) {
-        // TODO
-        return 0;
+        long answer = 0;
+        for (int i = 0; i < A.length; i++) {
+            int min = Integer.MAX_VALUE;
+            for (int j = i; j < A.length; j++) {
+                min = Math.min(min, A[j]);
+                answer += min;
+                answer %= 1000_000_007;
+            }
+        }
+        return (int) answer % 1000_000_007;
     }
 
     public static void main(String[] args) {
         Problem907 prob = new Problem907();
         System.out.println(prob.sumSubarrayMins(new int[]{3, 1, 2, 4})); // 17
+        System.out.println(prob.sumSubarrayMins(new int[]{3, 1, 2, 4, 10, 23})); // 76
     }
 }
