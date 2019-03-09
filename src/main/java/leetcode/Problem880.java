39f10c6158c8de26a7948d5cdc6ecd928700493e
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-03-09 14:49:25 -0600

    Update problem 880

--- src/main/java/leetcode/Problem880.java
+++ src/main/java/leetcode/Problem880.java
@@ -5,15 +5,27 @@ package leetcode;
  */
 public class Problem880 {
     public String decodeAtIndex(String S, int K) {
-        // TODO
-        return null;
+        String answer = "";
+        for (int i = 0; i < S.length(); i++) {
+            if (S.charAt(i) >= 'a' && S.charAt(i) <= 'z') {
+                answer += S.charAt(i);
+            } else {
+                int repeat = S.charAt(i) - '0';
+                String tmp = "";
+                for (int j = 0; j < repeat; j++) {
+                    tmp += answer;
+                }
+                answer = tmp;
+            }
+        }
+        return "" + answer.charAt(K - 1);
     }
 
     public static void main(String[] args) {
         Problem880 prob = new Problem880();
-        System.out.println(prob.decodeAtIndex("leet2code3", 10)); // o
-        System.out.println(prob.decodeAtIndex("ha22", 5)); // h
+//        System.out.println(prob.decodeAtIndex("leet2code3", 10)); // o
+//        System.out.println(prob.decodeAtIndex("ha22", 5)); // h
+//        System.out.println(prob.decodeAtIndex("hello23", 10)); // o
         System.out.println(prob.decodeAtIndex("a2345678999999999999999", 1)); // a
-        System.out.println(prob.decodeAtIndex("hello23", 10)); // o
     }
 }
