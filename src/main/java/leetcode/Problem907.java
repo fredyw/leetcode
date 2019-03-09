65b58fc91420e86625b0a1b9144fda5825c1d9c6
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-03-09 02:25:04 -0600

    Solve problem 907

--- src/main/java/leetcode/Problem907.java
+++ src/main/java/leetcode/Problem907.java
@@ -1,25 +1,57 @@
 package leetcode;
 
+import java.util.Stack;
+
 /**
  * https://leetcode.com/problems/sum-of-subarray-minimums/
  */
 public class Problem907 {
     public int sumSubarrayMins(int[] A) {
+        IndexValue[] iv = new IndexValue[A.length];
+        for (int i = 0; i < A.length; i++) {
+            iv[i] = new IndexValue(i, A[i]);
+        }
+        Stack<IndexValue> left = new Stack<>();
+        int[] leftCounts = new int[A.length];
+        for (int i = 0; i < iv.length; i++) {
+            while (!left.isEmpty() && left.peek().value > iv[i].value) {
+                left.pop();
+            }
+            if (!left.isEmpty()) {
+                leftCounts[i] = i - left.peek().index;
+            } else {
+                leftCounts[i] = i + 1;
+            }
+            left.push(iv[i]);
+        }
+
+        Stack<IndexValue> right = new Stack<>();
+        int[] rightCounts = new int[A.length];
+        for (int i = iv.length - 1, j = 0; i >= 0; i--, j++) {
+            while (!right.isEmpty() && right.peek().value >= iv[i].value) {
+                right.pop();
+            }
+            if (!right.isEmpty()) {
+                rightCounts[i] = right.peek().index - i;
+            } else {
+                rightCounts[i] = j + 1;
+            }
+            right.push(iv[i]);
+        }
         long answer = 0;
         for (int i = 0; i < A.length; i++) {
-            int min = Integer.MAX_VALUE;
-            for (int j = i; j < A.length; j++) {
-                min = Math.min(min, A[j]);
-                answer += min;
-                answer %= 1000_000_007;
-            }
+            answer += (A[i] * leftCounts[i] * rightCounts[i]) % 1000_000_007;
         }
-        return (int) answer % 1000_000_007;
+        return (int) answer;
     }
 
-    public static void main(String[] args) {
-        Problem907 prob = new Problem907();
-        System.out.println(prob.sumSubarrayMins(new int[]{3, 1, 2, 4})); // 17
-        System.out.println(prob.sumSubarrayMins(new int[]{3, 1, 2, 4, 10, 23})); // 76
+    private static class IndexValue {
+        private final int index;
+        private final int value;
+
+        public IndexValue(int index, int value) {
+            this.index = index;
+            this.value = value;
+        }
     }
 }
