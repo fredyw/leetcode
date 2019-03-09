9bb50672f8d2db0d6ad77aeef14a1310d95a7f34
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-03-09 10:34:47 -0600

    Skeleton for problem 880

---
+++ src/main/java/leetcode/Problem880.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/decoded-string-at-index/
+ */
+public class Problem880 {
+    public String decodeAtIndex(String S, int K) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem880 prob = new Problem880();
+        System.out.println(prob.decodeAtIndex("leet2code3", 10)); // o
+        System.out.println(prob.decodeAtIndex("ha22", 5)); // h
+        System.out.println(prob.decodeAtIndex("a2345678999999999999999", 1)); // a
+    }
+}
