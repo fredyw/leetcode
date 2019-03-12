7a67cecc04c587c7f31cb0ca7f931d42c9626c05
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-03-12 18:08:48 -0500

    Update problem 131

--- src/main/java/leetcode/Problem131.java
+++ src/main/java/leetcode/Problem131.java
@@ -29,9 +29,6 @@ public class Problem131 {
     }
 
     private static boolean isPalindrome(String s) {
-        if (s.isEmpty()) {
-            return false;
-        }
         for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
             if (s.charAt(i) != s.charAt(j)) {
                 return false;
