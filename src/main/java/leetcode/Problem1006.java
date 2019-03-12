189328ec29d902af1cd270d2627c4b4d1c2fd7ca
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-03-11 23:06:18 -0500

    Skeleton for problem 1006

---
+++ src/main/java/leetcode/Problem1006.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/clumsy-factorial/
+ */
+public class Problem1006 {
+    public int clumsy(int N) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1006 prob = new Problem1006();
+        System.out.println(prob.clumsy(4)); // 7
+        System.out.println(prob.clumsy(10)); // 12
+    }
+}
