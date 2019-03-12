f9e9aebe38044a747a6ff2481c2e987425e02c99
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-03-12 00:57:15 -0500

    Update problem 1006

--- src/main/java/leetcode/Problem1006.java
+++ src/main/java/leetcode/Problem1006.java
@@ -1,12 +1,45 @@
 package leetcode;
 
+import java.util.Stack;
+
 /**
  * https://leetcode.com/problems/clumsy-factorial/
  */
 public class Problem1006 {
     public int clumsy(int N) {
-        // TODO
-        return 0;
+        char[] ops = new char[]{'*', '/', '+', '-'};
+        int opIdx = 0;
+        Stack<Integer> numbers = new Stack<>();
+        Stack<Character> operators = new Stack<>();
+        for (int i = N; i >= 1; i--) {
+            if (i == N) {
+                numbers.add(i);
+                continue;
+            }
+            char op = ops[opIdx];
+            while (!operators.isEmpty() &&
+                ((op != '*' && op != '/') || (operators.peek() != '+' && operators.peek() != '-'))) {
+                numbers.push(apply(numbers.pop(), numbers.pop(), operators.pop()));
+            }
+            numbers.add(i);
+            operators.add(op);
+            opIdx = (opIdx + 1) % ops.length;
+        }
+        while (!operators.isEmpty()) {
+            numbers.push(apply(numbers.pop(), numbers.pop(), operators.pop()));
+        }
+        return numbers.pop();
+    }
+
+    private static int apply(int b, int a, char op) {
+        if (op == '*') {
+            return a * b;
+        } else if (op == '/') {
+            return a / b;
+        } else if (op == '+') {
+            return a + b;
+        }
+        return a - b;
     }
 
     public static void main(String[] args) {
