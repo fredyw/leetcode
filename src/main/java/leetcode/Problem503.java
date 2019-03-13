11f425dbdf6cfd8ce5f8deacc0cce3a681b7ec48
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-03-12 19:58:58 -0500

    Update problem 496 & 503

--- src/main/java/leetcode/Problem503.java
+++ src/main/java/leetcode/Problem503.java
@@ -1,32 +1,32 @@
 package leetcode;
 
+import java.util.Arrays;
+import java.util.Stack;
+
 /**
  * https://leetcode.com/problems/next-greater-element-ii/
  */
 public class Problem503 {
     public int[] nextGreaterElements(int[] nums) {
-        int[] result = new int[nums.length];
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < nums.length; i++) {
-            max = Math.max(nums[i], max);
+        int[] answer = new int[nums.length];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = nums.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && nums[i] > stack.peek()) {
+                stack.pop();
+            }
+            if (!stack.isEmpty()) {
+                answer[i] = stack.peek();
+            } else {
+                answer[i] = -1;
+            }
+            stack.push(nums[i]);
         }
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == max) {
-                result[i] = -1;
-                continue;
-            }
-            int j = i + 1;
-            if (j == nums.length) {
-                j = 0;
-            }
-            while (nums[i] >= nums[j]) {
-                j++;
-                if (j == nums.length) {
-                    j = 0;
-                }
-            }
-            result[i] = nums[j];
-        }
-        return result;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Problem503 prob = new Problem503();
+        System.out.println(Arrays.toString(prob.nextGreaterElements(new int[]{1, 2, 1}))); // [2,-1,2]
+        System.out.println(Arrays.toString(prob.nextGreaterElements(new int[]{1, 2, 1, 1}))); // [2,-1,2,2]
     }
 }
