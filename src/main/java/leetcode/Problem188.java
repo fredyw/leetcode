e7a0676d2ac3e954aeb1183292c11e89d8ac89da
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-03-13 23:14:19 -0500

    Update proble 188

--- src/main/java/leetcode/Problem188.java
+++ src/main/java/leetcode/Problem188.java
@@ -5,13 +5,34 @@ package leetcode;
  */
 public class Problem188 {
     public int maxProfit(int k, int[] prices) {
-        // TODO
-        return 0;
+        Integer[][][] memo = new Integer[k + 1][prices.length + 1][2];
+        return maxProfit(k, prices, 0, true, memo);
+    }
+
+    private static int maxProfit(int k, int[] prices, int index, boolean buy,
+                                 Integer[][][] memo) {
+        if (k == 0 || prices.length == index) {
+            return 0;
+        }
+        if (memo[k][index][buy ? 0 : 1] != null) {
+            return memo[k][index][buy ? 0 : 1];
+        }
+        int max = 0;
+        int a = 0;
+        if (buy) {
+            a = maxProfit(k, prices, index + 1, !buy, memo) - prices[index]; // buy
+        } else {
+            a = maxProfit(k - 1, prices, index + 1, !buy, memo) + prices[index]; // sell
+        }
+        int b = maxProfit(k, prices, index + 1, buy, memo); // skip
+        max = Math.max(max, Math.max(a, b));
+        memo[k][index][buy ? 0 : 1] = max;
+        return max;
     }
 
     public static void main(String[] args) {
         Problem188 prob = new Problem188();
         System.out.println(prob.maxProfit(2, new int[]{2, 4, 1})); // 2
         System.out.println(prob.maxProfit(2, new int[]{3, 2, 6, 5, 0, 3})); // 7
-    }
+   }
 }
