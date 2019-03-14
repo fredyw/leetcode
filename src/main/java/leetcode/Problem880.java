01260adf57f1e4ae01a270f82d92461661d4f9ae
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-03-13 21:56:34 -0500

    Solve problem 880

--- src/main/java/leetcode/Problem880.java
+++ src/main/java/leetcode/Problem880.java
@@ -5,40 +5,27 @@ package leetcode;
  */
 public class Problem880 {
     public String decodeAtIndex(String S, int K) {
-        String answer = "";
-        int size = 0;
-        outer:
+        long size = 0;
         for (int i = 0; i < S.length(); i++) {
             if (S.charAt(i) >= 'a' && S.charAt(i) <= 'z') {
-                answer += S.charAt(i);
                 size++;
             } else {
-                size = 0;
-                int repeat = S.charAt(i) - '0';
-                String tmp = "";
-                for (int j = 0; j < repeat; j++) {
-                    tmp += answer;
-                    size += answer.length();
-                    if (size > K) {
-                        answer = tmp;
-                        break outer;
-                    }
-                }
-                answer = tmp;
-            }
-            if (size > K) {
-                break;
+                size *= S.charAt(i) - '0';
             }
         }
-        return "" + answer.charAt(K - 1);
-    }
+        int k = K;
+        for (int i = S.length() - 1; i >= 0; i--) {
+            k %= size;
+            if (k == 0 && S.charAt(i) >= 'a' && S.charAt(i) <= 'z') {
+                return "" + S.charAt(i);
+            }
 
-    public static void main(String[] args) {
-        Problem880 prob = new Problem880();
-//        System.out.println(prob.decodeAtIndex("leet2code3", 10)); // o
-//        System.out.println(prob.decodeAtIndex("ha22", 5)); // h
-//        System.out.println(prob.decodeAtIndex("hello23", 10)); // o
-//        System.out.println(prob.decodeAtIndex("a2345678999999999999999", 1)); // a
-        System.out.println(prob.decodeAtIndex("a2345678999999999999999", 1000000000)); // a
+            if (S.charAt(i) >= 'a' && S.charAt(i) <= 'z') {
+                size--;
+            } else {
+                size /= S.charAt(i) - '0';
+            }
+        }
+        return "";
     }
 }
