4da922a7fe9a634a933ab50bb4327169a3af5cd3
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-03-14 01:21:58 -0500

    Skeleton for problem 866

---
+++ src/main/java/leetcode/Problem866.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/prime-palindrome/
+ */
+public class Problem866 {
+    public int primePalindrome(int N) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem866 prob = new Problem866();
+        System.out.println(prob.primePalindrome(6)); // 7
+        System.out.println(prob.primePalindrome(8)); // 11
+        System.out.println(prob.primePalindrome(13)); // 101
+    }
+}
