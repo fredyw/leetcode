1883c9b918353557bbd17826bbfe4c2fa6f0b506
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-03-13 22:18:06 -0500

    Skeleton for problem 188

---
+++ src/main/java/leetcode/Problem188.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iv/
+ */
+public class Problem188 {
+    public int maxProfit(int k, int[] prices) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem188 prob = new Problem188();
+        System.out.println(prob.maxProfit(2, new int[]{2, 4, 1})); // 2
+        System.out.println(prob.maxProfit(2, new int[]{3, 2, 6, 5, 0, 3})); // 7
+    }
+}
