92a5df3277641442df31288ba2c3274b3642232d
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-03-16 23:12:18 -0500

    Skeleton for problem 1012

---
+++ src/main/java/leetcode/Problem1012.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/complement-of-base-10-integer/
+ */
+public class Problem1012 {
+    public int bitwiseComplement(int N) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1012 prob = new Problem1012();
+        System.out.println(prob.bitwiseComplement(5)); // 2
+        System.out.println(prob.bitwiseComplement(7)); // 0
+        System.out.println(prob.bitwiseComplement(10)); // 5
+    }
+}
