42ab728cbff0ea00b66967a6cdced6f584cb48af
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-03-16 23:25:41 -0500

    Skeleton for problem 1013

---
+++ src/main/java/leetcode/Problem1013.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/pairs-of-songs-with-total-durations-divisible-by-60/
+ */
+public class Problem1013 {
+    public int numPairsDivisibleBy60(int[] time) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1013 prob = new Problem1013();
+        System.out.println(prob.numPairsDivisibleBy60(new int[]{30, 20, 150, 100, 40})); // 3
+        System.out.println(prob.numPairsDivisibleBy60(new int[]{60, 60, 60})); // 3
+    }
+}
