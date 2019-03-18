c4d5181555b8596f4b7b58e7d8f32bea821de4d1
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-03-18 00:02:43 -0500

    Skeleton for problem 1014

---
+++ src/main/java/leetcode/Problem1014.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/capacity-to-ship-packages-within-d-days/
+ */
+public class Problem1014 {
+    public int shipWithinDays(int[] weights, int D) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1014 prob = new Problem1014();
+        System.out.println(prob.shipWithinDays(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 5)); // 15
+        System.out.println(prob.shipWithinDays(new int[]{3, 2, 2, 4, 1, 4}, 3)); // 6
+        System.out.println(prob.shipWithinDays(new int[]{1, 2, 3, 1, 1}, 4)); // 3
+    }
+}
