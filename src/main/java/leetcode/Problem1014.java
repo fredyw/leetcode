c062b98c887fa17877dc46cb648223264a577032
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-03-19 00:32:53 -0500

    Update problem 1014

--- src/main/java/leetcode/Problem1014.java
+++ src/main/java/leetcode/Problem1014.java
@@ -12,7 +12,9 @@ public class Problem1014 {
     public static void main(String[] args) {
         Problem1014 prob = new Problem1014();
         System.out.println(prob.shipWithinDays(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 5)); // 15
-        System.out.println(prob.shipWithinDays(new int[]{3, 2, 2, 4, 1, 4}, 3)); // 6
-        System.out.println(prob.shipWithinDays(new int[]{1, 2, 3, 1, 1}, 4)); // 3
+//        System.out.println(prob.shipWithinDays(new int[]{1, 2, 3, 10, 5, 6, 7, 8, 9}, 5)); // 15
+//        System.out.println(prob.shipWithinDays(new int[]{1, 2, 10, 3, 5, 6, 7, 8, 9}, 5)); // 13
+//        System.out.println(prob.shipWithinDays(new int[]{3, 2, 2, 4, 1, 4}, 3)); // 6
+//        System.out.println(prob.shipWithinDays(new int[]{1, 2, 3, 1, 1}, 4)); // 3
     }
 }
