7b16ea7a5eba014b31d3c95100192b5a3fe93412
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-03-20 21:16:25 -0500

    Skeleton for problem 135

---
+++ src/main/java/leetcode/Problem135.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/candy/
+ */
+public class Problem135 {
+    public int candy(int[] ratings) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem135 prob = new Problem135();
+        System.out.println(prob.candy(new int[]{1, 0, 2})); // 5
+        System.out.println(prob.candy(new int[]{1, 2, 2})); // 4
+    }
+}
