bd6dc5eb3dd266af28120e9e5d860d7a2426ebe8
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-03-21 23:42:31 -0500

    Skeleton for problem 910

---
+++ src/main/java/leetcode/Problem910.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/smallest-range-ii/
+ */
+public class Problem910 {
+    public int smallestRangeII(int[] A, int K) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem910 prob = new Problem910();
+        System.out.println(prob.smallestRangeII(new int[]{1}, 0)); // 0
+        System.out.println(prob.smallestRangeII(new int[]{0, 10}, 2)); // 6
+        System.out.println(prob.smallestRangeII(new int[]{1, 3, 6}, 3)); // 3
+    }
+}
