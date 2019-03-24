c90c1748d6f58b36ddc23e665cbeae0a21183351
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-03-24 11:41:15 -0500

    Solve problem 1020

--- src/main/java/leetcode/Problem1020.java
+++ src/main/java/leetcode/Problem1020.java
@@ -9,22 +9,20 @@ public class Problem1020 {
         for (int a : A) {
             sum += a;
         }
-        int part = 0;
-        for (int a : A) {
-            part += a;
-            if (part != 0 && sum % part == 0 && sum / part == 3) {
-                return true;
+        int tmpSum = 0;
+        for (int i = 0; i < A.length; i++) {
+            tmpSum += A[i];
+            int rest = sum - tmpSum;
+            if (rest % 2 == 0 && rest / 2 == tmpSum) {
+                int s = 0;
+                for (int j = i + 1; j < A.length; j++) {
+                    s += A[j];
+                    if (s == tmpSum && rest - s == tmpSum) {
+                        return true;
+                    }
+                }
             }
         }
         return false;
-    }
-
-    public static void main(String[] args) {
-        Problem1020 prob = new Problem1020();
-//        System.out.println(prob.canThreePartsEqualSum(new int[]{0, 2, 1, -6, 6, -7, 9, 1, 2, 0, 1})); // true
-//        System.out.println(prob.canThreePartsEqualSum(new int[]{0, 2, 1, -6, 6, 7, 9, -1, 2, 0, 1})); // false
-//        System.out.println(prob.canThreePartsEqualSum(new int[]{3, 3, 6, 5, -2, 2, 5, 1, -9, 4})); // true
-//        System.out.println(prob.canThreePartsEqualSum(new int[]{0, 0, 0, 0, 0})); // true
-        System.out.println(prob.canThreePartsEqualSum(new int[]{0, 0, -3, 0, 3, 0, 0})); // true
     }
 }
