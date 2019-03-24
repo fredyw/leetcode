b613a5e4fbf6f965e82f7ccf1c415431129f5de5
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-03-24 18:39:24 -0500

    Update problem 1021

--- src/main/java/leetcode/Problem1021.java
+++ src/main/java/leetcode/Problem1021.java
@@ -5,8 +5,14 @@ package leetcode;
  */
 public class Problem1021 {
     public int maxScoreSightseeingPair(int[] A) {
-        // TODO
-        return 0;
+        int answer = 0;
+        for (int i = 0; i < A.length; i++) {
+            for (int j = i + 1; j < A.length; j++) {
+                int val = A[i] + A[j] + i - j;
+                answer = Math.max(answer, val);
+            }
+        }
+        return answer;
     }
 
     public static void main(String[] args) {
