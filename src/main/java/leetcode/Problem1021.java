2633b79966d7efc229bb41833ce570b40ac09770
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-03-24 18:31:56 -0500

    Skeleton for problem 1021

---
+++ src/main/java/leetcode/Problem1021.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/best-sightseeing-pair/
+ */
+public class Problem1021 {
+    public int maxScoreSightseeingPair(int[] A) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1021 prob = new Problem1021();
+        System.out.println(prob.maxScoreSightseeingPair(new int[]{8, 1, 5, 2, 6})); // 11
+    }
+}
