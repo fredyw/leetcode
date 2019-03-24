67be6617be0d802370290a0f8aac1481011d41a2
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-03-24 11:45:28 -0500

    Skeleton for problem 1023

---
+++ src/main/java/leetcode/Problem1023.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/binary-string-with-substrings-representing-1-to-n/
+ */
+public class Problem1023 {
+    public boolean queryString(String S, int N) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem1023 prob = new Problem1023();
+        System.out.println(prob.queryString("0110", 3)); // true
+        System.out.println(prob.queryString("0110", 4)); // false
+    }
+}
