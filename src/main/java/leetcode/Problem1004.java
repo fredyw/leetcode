29c325571c0bdd27939e76eddcdaa06b6d276788
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-03-28 01:26:05 -0500

    Update problem 1004

--- src/main/java/leetcode/Problem1004.java
+++ src/main/java/leetcode/Problem1004.java
@@ -1,92 +1,26 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * https://leetcode.com/problems/max-consecutive-ones-iii/
  */
 public class Problem1004 {
     public int longestOnes(int[] A, int K) {
-        List<NumberCount> list = new ArrayList<>();
-        for (int i = 0; i < A.length; i++) {
-            if (A[i] == 0) {
-                if (list.isEmpty()) {
-                    list.add(new NumberCount(0, 1));
-                    continue;
-                }
-                NumberCount nc = list.get(list.size() - 1);
-                if (nc.number == A[i]) {
-                    nc.count++;
-                } else {
-                    list.add(new NumberCount(0, 1));
-                }
-            } else {
-                if (list.isEmpty()) {
-                    list.add(new NumberCount(1, 1));
-                    continue;
-                }
-                NumberCount nc = list.get(list.size() - 1);
-                if (nc.number == A[i]) {
-                    nc.count++;
-                } else {
-                    list.add(new NumberCount(1, 1));
-                }
-            }
-        }
         int answer = 0;
-        for (int i = 0; i < list.size(); i++) {
-            int max = 0;
-            int count = K;
-            int j = i;
-            while (j < list.size()) {
-                NumberCount nc = list.get(j);
-                if (nc.number == 0) {
-                    if (count - nc.count >= 0) {
-                        count -= nc.count;
-                        max += nc.count;
-                    } else {
-                        max += count;
-                        break;
-                    }
-                } else {
-                    max += nc.count;
-                }
-                j++;
+        int left = 0;
+        int right = 0;
+        while (right < A.length) {
+            if (A[right] == 0) {
+                K--;
             }
-            answer = Math.max(answer, max);
-        }
-        for (int i = list.size() - 1; i >= 0; i--) {
-            int max = 0;
-            int count = K;
-            int j = i;
-            while (j >= 0) {
-                NumberCount nc = list.get(j);
-                if (nc.number == 0) {
-                    if (count - nc.count >= 0) {
-                        count -= nc.count;
-                        max += nc.count;
-                    } else {
-                        max += count;
-                        break;
-                    }
-                } else {
-                    max += nc.count;
+            while (K < 0 && left <= right) {
+                if (A[left] == 0) {
+                    K++;
                 }
-                j--;
+                left++;
             }
-            answer = Math.max(answer, max);
+            right++;
+            answer = Math.max(answer, right - left);
         }
         return answer;
-    }
-
-    private static class NumberCount {
-        private final int number;
-        private int count;
-
-        public NumberCount(int number, int count) {
-            this.number = number;
-            this.count = count;
-        }
     }
 }
