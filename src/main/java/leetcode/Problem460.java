7a485301916b0c9cc28a5935dfa2c7543254ffb6
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-03-29 00:18:23 -0500

    Skeleton for problem 460

---
+++ src/main/java/leetcode/Problem460.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/lfu-cache/
+ */
+public class Problem460 {
+    static class LFUCache {
+        public LFUCache(int capacity) {
+            // TODO
+        }
+
+        public int get(int key) {
+            // TODO
+            return 0;
+        }
+
+        public void put(int key, int value) {
+            // TODO
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}
