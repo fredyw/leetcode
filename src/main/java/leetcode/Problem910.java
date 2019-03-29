ae1cd20f16c59a2ba3bb2b668a8528202e0d6776
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-03-29 15:36:21 -0500

    Solve problem 910

--- src/main/java/leetcode/Problem910.java
+++ src/main/java/leetcode/Problem910.java
@@ -11,28 +11,12 @@ public class Problem910 {
             return 0;
         }
         Arrays.sort(A);
-        int answer = Integer.MAX_VALUE;
+        int answer = A[A.length - 1] - A[0];
         for (int i = 0; i < A.length - 1; i++) {
-            if ((A[i] + K) == (A[i + 1] + K)) {
-                continue;
-            }
-            int hi = A[i] + K;
-            int lo = A[i + 1] - K;
-            answer = Math.min(answer, Math.abs(hi - lo));
+            int hi = Math.max(A[A.length - 1] - K, A[i] + K);
+            int lo = Math.min(A[0] + K, A[i + 1] - K);
+            answer = Math.min(answer, hi - lo);
         }
-        answer = Math.min(answer, Math.abs(A[A.length - 1] - A[0]));
         return answer;
-    }
-
-    public static void main(String[] args) {
-        Problem910 prob = new Problem910();
-//        System.out.println(prob.smallestRangeII(new int[]{1}, 0)); // 0
-//        System.out.println(prob.smallestRangeII(new int[]{1}, 5)); // 0
-//        System.out.println(prob.smallestRangeII(new int[]{0, 10}, 2)); // 6
-//        System.out.println(prob.smallestRangeII(new int[]{1, 3, 6}, 3)); // 3
-//        System.out.println(prob.smallestRangeII(new int[]{1, 3, 7, 4, 1, 10, 8, 11}, 4)); // 5
-//        System.out.println(prob.smallestRangeII(new int[]{2, 7, 2}, 1)); // 3
-//        System.out.println(prob.smallestRangeII(new int[]{7, 8, 8}, 5)); // 1
-        System.out.println(prob.smallestRangeII(new int[]{1, 3, 10}, 4)); // 2
     }
 }
