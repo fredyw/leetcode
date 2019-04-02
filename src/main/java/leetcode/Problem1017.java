c964eca4f659542ce21fd5a5fe6bddaf5824c9bb
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-04-02 14:14:52 -0500

    Skeleton for problem 1017

---
+++ src/main/java/leetcode/Problem1017.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/convert-to-base-2/
+ */
+public class Problem1017 {
+    public String baseNeg2(int N) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem1017 prob = new Problem1017();
+        System.out.println(prob.baseNeg2(2)); // "110"
+        System.out.println(prob.baseNeg2(3)); // "111"
+        System.out.println(prob.baseNeg2(4)); // "100"
+    }
+}
