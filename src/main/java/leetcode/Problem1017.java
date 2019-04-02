6e9de1d45c7ddc8b98862d89110935b7c3cc5384
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-04-02 15:27:13 -0500

    Solve problem 1017

--- src/main/java/leetcode/Problem1017.java
+++ src/main/java/leetcode/Problem1017.java
@@ -5,14 +5,15 @@ package leetcode;
  */
 public class Problem1017 {
     public String baseNeg2(int N) {
-        // TODO
-        return null;
-    }
-
-    public static void main(String[] args) {
-        Problem1017 prob = new Problem1017();
-        System.out.println(prob.baseNeg2(2)); // "110"
-        System.out.println(prob.baseNeg2(3)); // "111"
-        System.out.println(prob.baseNeg2(4)); // "100"
+        if (N == 0) {
+            return "0";
+        }
+        StringBuilder answer = new StringBuilder();
+        int n = N;
+        while (n != 0) {
+            answer.append((n & 1) == 1 ? 1 : 0);
+            n = -(n >> 1);
+        }
+        return answer.reverse().toString();
     }
 }
