23d57d7005b58f4713f9614d8191eeb325ade511
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-04-03 01:49:49 -0500

    Skeleton for problem 1015

---
+++ src/main/java/leetcode/Problem1015.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/smallest-integer-divisible-by-k/
+ */
+public class Problem1015 {
+    public int smallestRepunitDivByK(int K) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1015 prob = new Problem1015();
+    }
+}
