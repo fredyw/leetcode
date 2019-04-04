f92e1a32f119b8abd6c6fb36be34bbbd53aeb1f6
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-04-04 01:06:14 -0500

    Update problem 121

--- src/main/java/leetcode/Problem121.java
+++ src/main/java/leetcode/Problem121.java
@@ -5,18 +5,16 @@ package leetcode;
  */
 public class Problem121 {
     public int maxProfit(int[] prices) {
-        int[] max = new int[prices.length];
-        int m = 0;
+        int[] maxPrices = new int[prices.length];
+        int maxPrice = 0;
         for (int i = prices.length - 1; i >= 0; i--) {
-            max[i] = Math.max(m, prices[i]);
-            m = max[i];
+            maxPrice = Math.max(maxPrice, prices[i]);
+            maxPrices[i] = maxPrice;
         }
-        int profit = 0;
+        int answer = 0;
         for (int i = 0; i < prices.length - 1; i++) {
-            int buy = prices[i];
-            int sell = max[i + 1];
-            profit = Math.max(profit, sell - buy);
+            answer = Math.max(answer, maxPrices[i + 1] - prices[i]);
         }
-        return profit;
+        return answer;
     }
 }
