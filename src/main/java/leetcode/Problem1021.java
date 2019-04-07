a89f232da09f6008e0c859fe4e82c6a187b48004
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-04-07 00:41:52 -0500

    Skeleton for problem 1021

---
+++ src/main/java/leetcode/Problem1021.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/remove-outermost-parentheses/
+ */
+public class Problem1021 {
+    public String removeOuterParentheses(String S) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem1021 prob = new Problem1021();
+    }
+}
