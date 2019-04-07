0f3ad827cc3850ef8f4fb9d7c2095b1ae39f22c3
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-04-07 01:43:42 -0500

    Rename problem 1016

--- src/main/java/leetcode/Problem1016.java
+++ src/main/java/leetcode/Problem1016.java
@@ -3,7 +3,7 @@ package leetcode;
 /**
  * https://leetcode.com/problems/binary-string-with-substrings-representing-1-to-n/
  */
-public class Problem1023 {
+public class Problem1016 {
     public boolean queryString(String S, int N) {
         for (int i = N; i >= 1; i--) {
             String s = Integer.toBinaryString(i);
