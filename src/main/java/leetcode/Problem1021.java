e518baf7d1e037cb93e86140c8f4eb2f9c0aea5a
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-04-07 01:07:38 -0500

    Solve problem 1021

--- src/main/java/leetcode/Problem1021.java
+++ src/main/java/leetcode/Problem1021.java
@@ -1,15 +1,31 @@
 package leetcode;
 
+import java.util.Stack;
+
 /**
  * https://leetcode.com/problems/remove-outermost-parentheses/
  */
 public class Problem1021 {
     public String removeOuterParentheses(String S) {
-        // TODO
-        return null;
-    }
-
-    public static void main(String[] args) {
-        Problem1021 prob = new Problem1021();
+        String answer = "";
+        Stack<Character> stack = new Stack<>();
+        boolean start = true;
+        int idx = 0;
+        for (int i = 0; i < S.length(); i++) {
+            if (S.charAt(i) == '(') {
+                stack.add('(');
+                if (start) {
+                    idx = i;
+                    start = false;
+                }
+            } else {
+                stack.pop();
+            }
+            if (stack.isEmpty()) {
+                start = true;
+                answer += S.substring(idx + 1, i);
+            }
+        }
+        return answer;
     }
 }
