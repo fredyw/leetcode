2fb9ee221be9090b259165d07a3d63a43c5089ef
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-04-07 01:45:10 -0500

    Skeleton for problem 1023

---
+++ src/main/java/leetcode/Problem1023.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/camelcase-matching/
+ */
+public class Problem1023 {
+    public List<Boolean> camelMatch(String[] queries, String pattern) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem1023 prob = new Problem1023();
+    }
+}
