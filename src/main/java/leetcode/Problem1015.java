8b32af50208c0bb69761a5cf7b8439d22cd7299d
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-04-07 02:03:23 -0500

    Solve problem 1015

--- src/main/java/leetcode/Problem1015.java
+++ src/main/java/leetcode/Problem1015.java
@@ -5,11 +5,16 @@ package leetcode;
  */
 public class Problem1015 {
     public int smallestRepunitDivByK(int K) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem1015 prob = new Problem1015();
+        if (K % 2 == 0 || K % 5 == 0) {
+            return -1;
+        }
+        int r = 1;
+        for (int i = 1; i <= K; i++) {
+            if (r % K == 0) {
+                return i;
+            }
+            r = (r * 10 + 1) % K;
+        }
+        return -1;
     }
 }
