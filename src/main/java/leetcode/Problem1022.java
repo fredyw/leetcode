f68d0003de3e4923da390f9c76ac208add04d58e
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-04-07 01:39:24 -0500

    Solve problem 1022

--- src/main/java/leetcode/Problem1022.java
+++ src/main/java/leetcode/Problem1022.java
@@ -15,20 +15,24 @@ public class Problem1022 {
     }
 
     public int sumRootToLeaf(TreeNode root) {
-        // TODO
-        return 0;
+        IntRef sum = new IntRef();
+        sumRootToLeaf(root, "", sum);
+        return sum.val;
     }
 
-    public static void main(String[] args) {
-        Problem1022 prob = new Problem1022();
+    private static class IntRef {
+        private int val;
+    }
 
-        TreeNode root = new TreeNode(1);
-        root.left = new TreeNode(0);
-        root.left.left = new TreeNode(0);
-        root.left.right = new TreeNode(1);
-        root.right = new TreeNode(1);
-        root.right.left = new TreeNode(0);
-        root.right.right = new TreeNode(1);
-        System.out.println(prob.sumRootToLeaf(root)); // 22
+    private static void sumRootToLeaf(TreeNode root, String s, IntRef sum) {
+        if (root == null) {
+            return;
+        }
+        if (root.left == null && root.right == null) {
+            int n = Integer.parseInt(s + root.val, 2);
+            sum.val += n;
+        }
+        sumRootToLeaf(root.left, s + root.val, sum);
+        sumRootToLeaf(root.right, s + root.val, sum);
     }
 }
