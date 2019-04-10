53309f67bfab71d9a223d4e4559e5bc6018a8c95
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-04-10 12:51:47 -0500

    Update problem 91

--- src/main/java/leetcode/Problem91.java
+++ src/main/java/leetcode/Problem91.java
@@ -1,43 +1,35 @@
 package leetcode;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * https://leetcode.com/problems/decode-ways/
  */
 public class Problem91 {
     public int numDecodings(String s) {
-        if (s.length() == 0) {
-            return 0;
-        }
-        Map<Integer, Integer> memo = new HashMap<>();
-        return decode(s, 0, memo);
+        Integer[] memo = new Integer[s.length()];
+        return numDecodings(s, 0, memo);
     }
 
-    private int decode(String s, int idx, Map<Integer, Integer> memo) {
-        if (memo.containsKey(idx)) {
-            return memo.get(idx);
-        }
+    private static int numDecodings(String s, int idx, Integer[] memo) {
         if (idx == s.length()) {
             return 1;
         }
-        int result = 0;
+        if (memo[idx] != null) {
+            return memo[idx];
+        }
+        int answer = 0;
         if (idx + 1 <= s.length()) {
-            String one = s.substring(idx, idx + 1);
-            int num = Integer.parseInt(one);
-            if (num >= 1 && num <= 9) {
-                result += decode(s, idx + 1, memo);
+            int n1 = Integer.parseInt(s.substring(idx, idx + 1));
+            if (1 <= n1 && n1 <= 9) {
+                answer += numDecodings(s, idx + 1, memo);
             }
         }
         if (idx + 2 <= s.length()) {
-            String two = s.substring(idx, idx + 2);
-            int num = Integer.parseInt(two);
-            if (num >= 10 && num <= 26) {
-                result += decode(s, idx + 2, memo);
+            int n2 = Integer.parseInt(s.substring(idx, idx + 2));
+            if (10 <= n2 && n2 <= 26) {
+                answer += numDecodings(s, idx + 2, memo);
             }
         }
-        memo.put(idx, result);
-        return result;
+        memo[idx] = answer;
+        return answer;
     }
 }
