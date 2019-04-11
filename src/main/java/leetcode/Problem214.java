5c68a7ba265912317e9812237388d5566aa934c3
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-04-11 00:59:45 -0500

    Update problem 214

--- src/main/java/leetcode/Problem214.java
+++ src/main/java/leetcode/Problem214.java
@@ -5,13 +5,22 @@ package leetcode;
  */
 public class Problem214 {
     public String shortestPalindrome(String s) {
-        // TODO
-        return null;
+        int i = 0;
+        int j = s.length() - 1;
+        String answer = s;
+        while (i < j) {
+            // TODO
+        }
+        return answer;
     }
 
     public static void main(String[] args) {
         Problem214 prob = new Problem214();
-        System.out.println(prob.shortestPalindrome("aacecaaa")); // "aaacecaaa"
+//        System.out.println(prob.shortestPalindrome("aacecaaa")); // "aaacecaaa"
         System.out.println(prob.shortestPalindrome("abcd")); // "dcbabcd"
+//        System.out.println(prob.shortestPalindrome("aabaa")); // "aabaa"
+//        System.out.println(prob.shortestPalindrome("aaaa")); // "aaaa"
+//        System.out.println(prob.shortestPalindrome("aaba")); // "abaaba"
+//        System.out.println(prob.shortestPalindrome("aababa")); // "ababaababa"
     }
 }
