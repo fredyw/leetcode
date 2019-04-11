8d0429c88f496fe9ec3694798843f5138f0a3f6b
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-04-10 23:20:58 -0500

    Skeleton for problem 214

---
+++ src/main/java/leetcode/Problem214.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/shortest-palindrome/
+ */
+public class Problem214 {
+    public String shortestPalindrome(String s) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem214 prob = new Problem214();
+        System.out.println(prob.shortestPalindrome("aacecaaa")); // "aaacecaaa"
+        System.out.println(prob.shortestPalindrome("abcd")); // "dcbabcd"
+    }
+}
