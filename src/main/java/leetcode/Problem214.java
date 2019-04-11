cb71053f83ab0fdc312f926cde3846f8ed2a4774
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-04-11 15:06:29 -0500

    Update problem 214

--- src/main/java/leetcode/Problem214.java
+++ src/main/java/leetcode/Problem214.java
@@ -7,20 +7,43 @@ public class Problem214 {
     public String shortestPalindrome(String s) {
         int i = 0;
         int j = s.length() - 1;
-        String answer = s;
-        while (i < j) {
-            // TODO
+        int k = j;
+        String prefix = "";
+        outer: while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                for (int x = k; x >= j; x--) {
+                    prefix += s.charAt(x);
+                    if (isPalindrome(prefix + s)) {
+                        break outer;
+                    }
+                }
+                i = 0;
+                j--;
+                k = j;
+            } else {
+                i++;
+                j--;
+            }
         }
-        return answer;
+        return prefix + s;
+    }
+
+    private static boolean isPalindrome(String s) {
+        for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
         Problem214 prob = new Problem214();
-//        System.out.println(prob.shortestPalindrome("aacecaaa")); // "aaacecaaa"
+        System.out.println(prob.shortestPalindrome("aacecaaa")); // "aaacecaaa"
         System.out.println(prob.shortestPalindrome("abcd")); // "dcbabcd"
-//        System.out.println(prob.shortestPalindrome("aabaa")); // "aabaa"
-//        System.out.println(prob.shortestPalindrome("aaaa")); // "aaaa"
-//        System.out.println(prob.shortestPalindrome("aaba")); // "abaaba"
-//        System.out.println(prob.shortestPalindrome("aababa")); // "ababaababa"
+        System.out.println(prob.shortestPalindrome("aabaa")); // "aabaa"
+        System.out.println(prob.shortestPalindrome("aaaa")); // "aaaa"
+        System.out.println(prob.shortestPalindrome("aaba")); // "abaaba"
+        System.out.println(prob.shortestPalindrome("aababa")); // "ababaababa"
     }
 }
