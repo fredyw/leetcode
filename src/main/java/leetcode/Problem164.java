791a251d475ba75af635c72247a1ec42a626d2a7
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-04-10 22:30:02 -0500

    Skeleton for problem 164

---
+++ src/main/java/leetcode/Problem164.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/maximum-gap/
+ */
+public class Problem164 {
+    public int maximumGap(int[] nums) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem164 prob = new Problem164();
+        System.out.println(prob.maximumGap(new int[]{3, 6, 9, 1})); // 3
+        System.out.println(prob.maximumGap(new int[]{10})); // 0
+    }
+}
