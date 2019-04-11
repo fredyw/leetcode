22e7d83d2a476e2c2a953e3897bd866c455e7c34
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-04-11 15:41:59 -0500

    Solve problem 214

--- src/main/java/leetcode/Problem214.java
+++ src/main/java/leetcode/Problem214.java
@@ -5,45 +5,14 @@ package leetcode;
  */
 public class Problem214 {
     public String shortestPalindrome(String s) {
-        int i = 0;
-        int j = s.length() - 1;
-        int k = j;
+        String reversed = new StringBuilder(s).reverse().toString();
         String prefix = "";
-        outer: while (i < j) {
-            if (s.charAt(i) != s.charAt(j)) {
-                for (int x = k; x >= j; x--) {
-                    prefix += s.charAt(x);
-                    if (isPalindrome(prefix + s)) {
-                        break outer;
-                    }
-                }
-                i = 0;
-                j--;
-                k = j;
-            } else {
-                i++;
-                j--;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.substring(0, s.length() - i).equals(reversed.substring(i))) {
+                break;
             }
+            prefix += reversed.charAt(i);
         }
         return prefix + s;
-    }
-
-    private static boolean isPalindrome(String s) {
-        for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
-            if (s.charAt(i) != s.charAt(j)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public static void main(String[] args) {
-        Problem214 prob = new Problem214();
-        System.out.println(prob.shortestPalindrome("aacecaaa")); // "aaacecaaa"
-        System.out.println(prob.shortestPalindrome("abcd")); // "dcbabcd"
-        System.out.println(prob.shortestPalindrome("aabaa")); // "aabaa"
-        System.out.println(prob.shortestPalindrome("aaaa")); // "aaaa"
-        System.out.println(prob.shortestPalindrome("aaba")); // "abaaba"
-        System.out.println(prob.shortestPalindrome("aababa")); // "ababaababa"
     }
 }
