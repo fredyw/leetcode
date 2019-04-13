2bab97598e9361a3feeaffdc1deb7243e45baa08
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-04-12 23:28:25 -0500

    Skeleton for problem 239

---
+++ src/main/java/leetcode/Problem239.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/sliding-window-maximum/
+ */
+public class Problem239 {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem239 prob = new Problem239();
+        System.out.println(Arrays.toString(prob.maxSlidingWindow(new int[]{1, 3, -1, -3, 5, 3, 6, 7}, 3))); // [3,3,5,5,6,7]
+    }
+}
