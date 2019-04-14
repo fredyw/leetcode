59d5186ddb3d61368deb1a7d62a8374af03cf347
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-04-14 00:17:49 -0500

    Skeleton for problem 1025

---
+++ src/main/java/leetcode/Problem1025.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/divisor-game/
+ */
+public class Problem1025 {
+    public boolean divisorGame(int N) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem1025 prob = new Problem1025();
+        System.out.println(prob.divisorGame(2)); // true
+        System.out.println(prob.divisorGame(3)); // false
+    }
+}
