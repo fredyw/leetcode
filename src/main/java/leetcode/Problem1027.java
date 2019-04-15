8a71a7456f35cbe47026231fe53303d892600b9c
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-04-14 23:30:13 -0500

    Skeleton for problem 1027

---
+++ src/main/java/leetcode/Problem1027.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/longest-arithmetic-sequence/
+ */
+public class Problem1027 {
+    public int longestArithSeqLength(int[] A) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1027 prob = new Problem1027();
+        System.out.println(prob.longestArithSeqLength(new int[]{3, 6, 9, 12})); // 4
+        System.out.println(prob.longestArithSeqLength(new int[]{9, 4, 7, 2, 10})); // 3
+        System.out.println(prob.longestArithSeqLength(new int[]{20, 1, 15, 3, 10, 5, 8})); // 4
+    }
+}
