812da93412f59989746f158c95523eac682ee0b4
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-04-19 02:03:36 -0500

    Update problem 227

--- src/main/java/leetcode/Problem227.java
+++ src/main/java/leetcode/Problem227.java
@@ -1,7 +1,6 @@
 package leetcode;
 
-import java.util.Collections;
-import java.util.Stack;
+import java.util.ArrayDeque;
 
 /**
  * https://leetcode.com/problems/basic-calculator-ii/
@@ -9,50 +8,48 @@ import java.util.Stack;
 public class Problem227 {
     public int calculate(String s) {
         String expr = s.replaceAll("\\s", "");
-        Stack<Character> operators = new Stack<>();
-        Stack<Long> operands = new Stack<>();
+        ArrayDeque<Character> operators = new ArrayDeque<>();
+        ArrayDeque<Long> operands = new ArrayDeque<>();
         String num = "";
         for (int i = 0; i < expr.length(); i++) {
             char c = expr.charAt(i);
             if (Character.isDigit(c)) {
                 num += c;
             } else { // the operator
-                operands.add(Long.valueOf(num));
+                operands.addLast(Long.valueOf(num));
                 if (!operators.isEmpty()) {
-                    char op = operators.peek();
+                    char op = operators.peekLast();
                     if (op == '*' || op == '/') {
-                        long a = operands.pop();
-                        long b = operands.pop();
-                        op = operators.pop();
-                        operands.add(evaluate(b, a, op));
+                        long a = operands.removeLast();
+                        long b = operands.removeLast();
+                        op = operators.removeLast();
+                        operands.addLast(evaluate(b, a, op));
                     }
                 }
-                operators.add(c);
+                operators.addLast(c);
                 num = "";
             }
         }
         if (!num.isEmpty()) {
-            operands.add(Long.valueOf(num));
+            operands.addLast(Long.valueOf(num));
         }
         if (!operators.isEmpty()) {
-            char op = operators.peek();
+            char op = operators.peekLast();
             if (op == '*' || op == '/') {
-                long a = operands.pop();
-                long b = operands.pop();
-                op = operators.pop();
-                operands.add(evaluate(b, a, op));
+                long a = operands.removeLast();
+                long b = operands.removeLast();
+                op = operators.removeLast();
+                operands.addLast(evaluate(b, a, op));
             }
         }
         // Evaluate from left to right.
-        Collections.reverse(operands);
-        Collections.reverse(operators);
         while (!operators.isEmpty()) {
-            long a = operands.pop();
-            long b = operands.pop();
-            char op = operators.pop();
-            operands.add(evaluate(a, b, op));
+            long a = operands.removeFirst();
+            long b = operands.removeFirst();
+            char op = operators.removeFirst();
+            operands.addFirst(evaluate(a, b, op));
         }
-        return operands.pop().intValue();
+        return operands.removeLast().intValue();
     }
 
     private static long evaluate(long a, long b, char op) {
