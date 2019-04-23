f8b13d79fd0cfa9c2f23aa17191c7f3e88b34f96
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-04-22 20:16:11 -0700

    Solve problem 912

---
+++ src/main/java/leetcode/Problem912.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/sort-an-array/
+ */
+public class Problem912 {
+    public int[] sortArray(int[] nums) {
+        Arrays.sort(nums);
+        return nums;
+    }
+}
