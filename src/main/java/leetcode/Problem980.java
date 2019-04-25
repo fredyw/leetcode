3628eedc073d2ef34d83dc35523c0ea1e6d68ec2
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-04-24 22:54:23 -0500

    Update problem 980

--- src/main/java/leetcode/Problem980.java
+++ src/main/java/leetcode/Problem980.java
@@ -11,5 +11,19 @@ public class Problem980 {
 
     public static void main(String[] args) {
         Problem980 prob = new Problem980();
+        System.out.println(prob.uniquePathsIII(new int[][]{
+            {1, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 2, -1}
+        })); // 2
+        System.out.println(prob.uniquePathsIII(new int[][]{
+            {1, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 2}
+        })); // 4
+        System.out.println(prob.uniquePathsIII(new int[][]{
+            {0, 1},
+            {2, 0}
+        })); // 0
     }
 }
