268242c0b48e2dac7a2b788b38642dc79c684639
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-04-28 00:53:07 -0500

    Update problem 336

--- src/main/java/leetcode/Problem336.java
+++ src/main/java/leetcode/Problem336.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -7,8 +9,27 @@ import java.util.List;
  */
 public class Problem336 {
     public List<List<Integer>> palindromePairs(String[] words) {
-        // TODO
-        return null;
+        List<List<Integer>> answer = new ArrayList<>();
+        for (int i = 0; i < words.length; i++) {
+            for (int j = i + 1; j < words.length; j++) {
+                if (isPalindrome(words[i] + words[j])) {
+                    answer.add(Arrays.asList(i, j));
+                }
+                if (isPalindrome(words[j] + words[i])) {
+                    answer.add(Arrays.asList(j, i));
+                }
+            }
+        }
+        return answer;
+    }
+
+    private static boolean isPalindrome(String s) {
+        for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
