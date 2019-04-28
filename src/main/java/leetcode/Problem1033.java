32d2966a73480155e644b47a4ba0a4f6e0e748fa
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-04-28 00:05:45 -0500

    Skeleton for problem 1033

---
+++ src/main/java/leetcode/Problem1033.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/moving-stones-until-consecutive/
+ */
+public class Problem1033 {
+    public int[] numMovesStones(int a, int b, int c) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem1033 prob = new Problem1033();
+        System.out.println(Arrays.toString(prob.numMovesStones(1, 2, 5))); // [1, 2]
+        System.out.println(Arrays.toString(prob.numMovesStones(4, 3, 2))); // [0, 0]
+    }
+}
