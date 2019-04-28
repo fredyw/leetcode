0284f5b0181106d407b032ff4c388dbf8fb0ff92
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-04-28 14:45:01 -0500

    Update problem 1035

--- src/main/java/leetcode/Problem1035.java
+++ src/main/java/leetcode/Problem1035.java
@@ -13,6 +13,7 @@ public class Problem1035 {
         Problem1035 prob = new Problem1035();
         System.out.println(prob.maxUncrossedLines(new int[]{1, 4, 2}, new int[]{1, 2, 4})); // 2
         System.out.println(prob.maxUncrossedLines(new int[]{2, 5, 1, 2, 5}, new int[]{10, 5, 2, 1, 5, 2})); // 3
+        System.out.println(prob.maxUncrossedLines(new int[]{2, 5, 1, 2, 5, 8}, new int[]{10, 5, 2, 1, 5, 2, 8})); // 4
         System.out.println(prob.maxUncrossedLines(new int[]{1, 3, 7, 1, 7, 5}, new int[]{1, 9, 2, 5, 1})); // 2
     }
 }
