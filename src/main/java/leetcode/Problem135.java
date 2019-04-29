bd35437787e3a7b0278a8ccafa6cccac4620a3bf
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-04-28 23:09:35 -0500

    Update problem 135

--- src/main/java/leetcode/Problem135.java
+++ src/main/java/leetcode/Problem135.java
@@ -15,5 +15,9 @@ public class Problem135 {
         System.out.println(prob.candy(new int[]{1, 2, 2})); // 4
         System.out.println(prob.candy(new int[]{2, 2, 2})); // 3
         System.out.println(prob.candy(new int[]{2, 3, 2})); // 4
+        System.out.println(prob.candy(new int[]{3, 2, 2, 3})); // 6
+        System.out.println(prob.candy(new int[]{3, 2, 2, 2, 3})); // 7
+        System.out.println(prob.candy(new int[]{1, 2, 2, 1})); // 6
+        System.out.println(prob.candy(new int[]{1, 2, 2, 2, 1})); // 7
     }
 }
