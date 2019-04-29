6e9b1ae5fadad0f3eb57075a02f28b3c92c05005
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-04-29 01:37:47 -0500

    Solve problem 135

--- src/main/java/leetcode/Problem135.java
+++ src/main/java/leetcode/Problem135.java
@@ -5,49 +5,39 @@ package leetcode;
  */
 public class Problem135 {
     public int candy(int[] ratings) {
-        int answer = 0;
+        int[] candies = new int[ratings.length];
+        // forward
         int i = 0;
-        while (i + 1 < ratings.length) {
-            // increasing
-            int count = 1;
+        while (i < ratings.length) {
             while (i + 1 < ratings.length && ratings[i] < ratings[i + 1]) {
+                if (candies[i] == 0) {
+                    candies[i] = 1;
+                }
+                candies[i + 1] = candies[i] + 1;
                 i++;
-                answer += ++count;
             }
-            // equal
-            while (i + 1 < ratings.length && ratings[i] == ratings[i + 1]) {
-                i++;
+            i++;
+        }
+        // backward
+        i = ratings.length - 1;
+        while (i >= 0) {
+            while (i - 1 >= 0 && ratings[i - 1] > ratings[i]) {
+                if (candies[i] == 0) {
+                    candies[i] = 1;
+                }
+                candies[i - 1] = Math.max(candies[i - 1], candies[i] + 1);
+                i--;
+            }
+            i--;
+        }
+        int answer = 0;
+        for (int candy : candies) {
+            if (candy == 0) {
                 answer++;
-            }
-            // decreasing
-            count = 1;
-            while (i + 1 < ratings.length && ratings[i] > ratings[i + 1]) {
-                i++;
-                answer += ++count;
+            } else {
+                answer += candy;
             }
         }
-        return answer + 1;
-    }
-
-    public static void main(String[] args) {
-        Problem135 prob = new Problem135();
-//        System.out.println(prob.candy(new int[]{1, 0, 2})); // 5
-//        System.out.println(prob.candy(new int[]{1, 0, 2, 2})); // 6
-//        System.out.println(prob.candy(new int[]{1, 1, 1, 1, 1})); // 5
-//        System.out.println(prob.candy(new int[]{1, 2, 2})); // 4
-//        System.out.println(prob.candy(new int[]{2, 2, 2})); // 3
-//        System.out.println(prob.candy(new int[]{2, 3, 2})); // 4
-//        System.out.println(prob.candy(new int[]{3, 2, 1, 2, 3})); // 11
-//        System.out.println(prob.candy(new int[]{3, 2, 2, 3})); // 6
-//        System.out.println(prob.candy(new int[]{3, 2, 2, 2, 3})); // 7
-//        System.out.println(prob.candy(new int[]{1, 2, 2, 1})); // 6
-//        System.out.println(prob.candy(new int[]{1, 2, 2, 2, 1})); // 7 
-//        System.out.println(prob.candy(new int[]{4, 3, 2, 1})); // 10
-//        System.out.println(prob.candy(new int[]{1, 2, 3, 4})); // 10
-//        System.out.println(prob.candy(new int[]{1, 2, 3, 4, 4, 4, 3, 2, 1})); // 21
-//        System.out.println(prob.candy(new int[]{1, 2, 3, 4, 3, 2, 1})); // 16
-        System.out.println(prob.candy(new int[]{2, 3, 2, 1})); // 7
-//        System.out.println(prob.candy(new int[]{2, 3, 3, 2, 1})); // 9
-//        System.out.println(prob.candy(new int[]{2, 3, 3, 4, 5})); // 9
+        return answer;
     }
 }
