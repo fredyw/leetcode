02cd6439c7cf552731ec707a5a23339b6de82fd1
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-05-02 21:55:45 -0500

    Skeleton for problem 1036

---
+++ src/main/java/leetcode/Problem1036.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/escape-a-large-maze/
+ */
+public class Problem1036 {
+    public boolean isEscapePossible(int[][] blocked, int[] source, int[] target) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem1036 prob = new Problem1036();
+    }
+}
