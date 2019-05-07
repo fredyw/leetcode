49ebce8931407b896accc042eecfee83d5e87e65
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-05-07 00:02:11 -0500

    Skeleton for problem 1040

---
+++ src/main/java/leetcode/Problem1040.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/moving-stones-until-consecutive-ii/
+ */
+public class Problem1040 {
+    public int[] numMovesStonesII(int[] stones) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem1040 prob = new Problem1040();
+        System.out.println(Arrays.toString(prob.numMovesStonesII(new int[]{7, 4, 9}))); // [1,2]
+        System.out.println(Arrays.toString(prob.numMovesStonesII(new int[]{6, 5, 4, 3, 10}))); // [2,3]
+        System.out.println(Arrays.toString(prob.numMovesStonesII(new int[]{100, 101, 104, 102, 103}))); // [0,0]
+    }
+}
