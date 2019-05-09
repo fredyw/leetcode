68827e3e9bc0f380e4a6c4ab64d21e6af1939239
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-05-08 20:27:05 -0500

    Update problem 1040

--- src/main/java/leetcode/Problem1040.java
+++ src/main/java/leetcode/Problem1040.java
@@ -16,5 +16,7 @@ public class Problem1040 {
         System.out.println(Arrays.toString(prob.numMovesStonesII(new int[]{7, 4, 9}))); // [1,2]
         System.out.println(Arrays.toString(prob.numMovesStonesII(new int[]{6, 5, 4, 3, 10}))); // [2,3]
         System.out.println(Arrays.toString(prob.numMovesStonesII(new int[]{100, 101, 104, 102, 103}))); // [0,0]
+        System.out.println(Arrays.toString(prob.numMovesStonesII(new int[]{1, 2, 5}))); // [2,2]
+        System.out.println(Arrays.toString(prob.numMovesStonesII(new int[]{1, 3, 5, 7, 10}))); // [2,4]
     }
 }
