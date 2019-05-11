28b12864c0e2d570d6dffb29fce8a849e441bd13
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-05-10 22:42:01 -0500

    Skeleton for roblem 1039

---
+++ src/main/java/leetcode/Problem1039.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/minimum-score-triangulation-of-polygon/
+ */
+public class Problem1039 {
+    public int minScoreTriangulation(int[] A) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1039 prob = new Problem1039();
+    }
+}
