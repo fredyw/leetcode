83ec5b73d4f958ec5be3c5a856ee49b73e3124b0
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-05-21 00:14:06 -0500

    Update problem 1047

--- src/main/java/leetcode/Problem1047.java
+++ src/main/java/leetcode/Problem1047.java
@@ -5,12 +5,26 @@ package leetcode;
  */
 public class Problem1047 {
     public String removeDuplicates(String S) {
-        // TODO
-        return null;
+        String answer = S;
+        int i = 0;
+        while (i < answer.length() - 1) {
+            if (answer.charAt(i) == answer.charAt(i + 1)) {
+                answer = answer.substring(0, i) + (i + 2 < answer.length() ? answer.substring(i + 2) : "");
+                i--;
+                if (i < 0) {
+                    i = 0;
+                }
+            } else {
+                i++;
+            }
+        }
+        return answer;
     }
 
     public static void main(String[] args) {
         Problem1047 prob = new Problem1047();
         System.out.println(prob.removeDuplicates("abbaca")); // ca
+        System.out.println(prob.removeDuplicates("aabbaca")); // aca
+        System.out.println(prob.removeDuplicates("cabbac")); //
     }
 }
