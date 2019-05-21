4d0db6beaf97e76f309fae1a0599ad20dfc35c4c
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-05-20 23:39:07 -0500

    Solve problem 1046

--- src/main/java/leetcode/Problem1046.java
+++ src/main/java/leetcode/Problem1046.java
@@ -1,16 +1,28 @@
 package leetcode;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 /**
  * https://leetcode.com/problems/last-stone-weight/
  */
 public class Problem1046 {
     public int lastStoneWeight(int[] stones) {
-        // TODO
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Comparator.reverseOrder());
+        for (int stone : stones) {
+            queue.add(stone);
+        }
+        while (!queue.isEmpty()) {
+            Integer x = queue.poll();
+            if (queue.isEmpty()) {
+                return x;
+            }
+            Integer y = queue.poll();
+            int diff = Math.max(x, y) - Math.min(x, y);
+            if (diff > 0) {
+                queue.add(diff);
+            }
+        }
         return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem1046 prob = new Problem1046();
-        System.out.println(prob.lastStoneWeight(new int[]{2, 7, 4, 1, 8, 1})); // 1
     }
 }
