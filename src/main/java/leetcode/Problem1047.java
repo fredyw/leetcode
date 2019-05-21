0f763203a68a9b33083a7ae2da0f264964a83ffd
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-05-20 23:52:34 -0500

    Skeleton for problem 1047

---
+++ src/main/java/leetcode/Problem1047.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/
+ */
+public class Problem1047 {
+    public String removeDuplicates(String S) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem1047 prob = new Problem1047();
+        System.out.println(prob.removeDuplicates("abbaca")); // ca
+    }
+}
