f559d9cd7c9378c7b087367e740ca8aad67b36c8
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-05-22 00:09:47 -0500

    Update problem 1039

--- src/main/java/leetcode/Problem1039.java
+++ src/main/java/leetcode/Problem1039.java
@@ -11,5 +11,10 @@ public class Problem1039 {
 
     public static void main(String[] args) {
         Problem1039 prob = new Problem1039();
+        System.out.println(prob.minScoreTriangulation(new int[]{1, 2, 3})); // 6
+        System.out.println(prob.minScoreTriangulation(new int[]{3, 7, 4, 5})); // 144
+        System.out.println(prob.minScoreTriangulation(new int[]{1, 3, 1, 4, 1, 5})); // 13
+        System.out.println(prob.minScoreTriangulation(new int[]{1, 3, 1, 4, 1, 5, 6})); // 43
+        System.out.println(prob.minScoreTriangulation(new int[]{1, 3, 1, 4, 1, 5, 6, 8})); // 91
     }
 }
