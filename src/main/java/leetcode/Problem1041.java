ed0111be92273b461e8dff9fec68635c62635985
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-05-21 21:50:06 -0500

    Solve problem 1041

--- src/main/java/leetcode/Problem1041.java
+++ src/main/java/leetcode/Problem1041.java
@@ -5,14 +5,72 @@ package leetcode;
  */
 public class Problem1041 {
     public boolean isRobotBounded(String instructions) {
-        // TODO
-        return false;
+        Coord coord = new Coord();
+        // try at least 4 times to make see if we can go back to the original position
+        for (int i = 0; i < 4; i++) {
+            for (char c : instructions.toCharArray()) {
+                isRobotBounded(coord, c);
+            }
+        }
+        return coord.x == 0 && coord.y == 0;
     }
 
-    public static void main(String[] args) {
-        Problem1041 prob = new Problem1041();
-        System.out.println(prob.isRobotBounded("GGLLGG")); // true
-        System.out.println(prob.isRobotBounded("GG")); // false
-        System.out.println(prob.isRobotBounded("GL")); // true
+    private static class Coord {
+        private int x = 0;
+        private int y = 0;
+        private Direction direction = Direction.UP;
+    }
+
+    private static void isRobotBounded(Coord coord, char c) {
+        if (c == 'G') {
+            switch (coord.direction) {
+                case UP:
+                    coord.y++;
+                    break;
+                case RIGHT:
+                    coord.x++;
+                    break;
+                case DOWN:
+                    coord.y--;
+                    break;
+                case LEFT:
+                    coord.x--;
+                    break;
+            }
+        } else if (c == 'L') {
+            switch (coord.direction) {
+                case UP:
+                    coord.direction = Direction.LEFT;
+                    break;
+                case RIGHT:
+                    coord.direction = Direction.UP;
+                    break;
+                case DOWN:
+                    coord.direction = Direction.RIGHT;
+                    break;
+                case LEFT:
+                    coord.direction = Direction.DOWN;
+                    break;
+            }
+        } else { // c == 'R'
+            switch (coord.direction) {
+                case UP:
+                    coord.direction = Direction.RIGHT;
+                    break;
+                case RIGHT:
+                    coord.direction = Direction.DOWN;
+                    break;
+                case DOWN:
+                    coord.direction = Direction.LEFT;
+                    break;
+                case LEFT:
+                    coord.direction = Direction.UP;
+                    break;
+            }
+        }
+    }
+
+    private enum Direction {
+        UP, DOWN, LEFT, RIGHT
     }
 }
