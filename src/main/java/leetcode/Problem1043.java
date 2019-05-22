ff400f215b5ee5e4b6a72cf132703c37e8da5e55
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-05-22 00:20:33 -0500

    Skeleton for problem 1043

---
+++ src/main/java/leetcode/Problem1043.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/partition-array-for-maximum-sum/
+ */
+public class Problem1043 {
+    public int maxSumAfterPartitioning(int[] A, int K) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1043 prob = new Problem1043();
+        System.out.println(prob.maxSumAfterPartitioning(new int[]{1, 15, 7, 9, 2, 5, 10}, 3)); // 84
+    }
+}
