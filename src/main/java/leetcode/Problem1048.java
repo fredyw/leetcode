43a7413732f135e394df9314101f8b470d5a2691
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-05-22 23:53:05 -0500

    Skeleton for problem 1048

---
+++ src/main/java/leetcode/Problem1048.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/longest-string-chain/
+ */
+public class Problem1048 {
+    public int longestStrChain(String[] words) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1048 prob = new Problem1048();
+        System.out.println(prob.longestStrChain(new String[]{"a", "b", "ba", "bca", "bda", "bdca"})); // 4
+    }
+}
