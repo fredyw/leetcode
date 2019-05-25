29367c909c2fd71f2309c8dd98a898b194739dc0
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-05-25 14:47:05 -0500

    Update problem 494

--- src/main/java/leetcode/Problem494.java
+++ src/main/java/leetcode/Problem494.java
@@ -1,18 +1,27 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * https://leetcode.com/problems/target-sum/
  */
 public class Problem494 {
     public int findTargetSumWays(int[] nums, int S) {
-        return findTargetSumWays(nums, S, 0);
+        return findTargetSumWays(nums, S, 0, new HashMap<>());
     }
 
-    private static int findTargetSumWays(int[] nums, int s, int idx) {
+    private static int findTargetSumWays(int[] nums, int s, int idx, Map<String, Integer> memo) {
         if (idx == nums.length) {
             return (s == 0) ? 1 : 0;
         }
-        return findTargetSumWays(nums, s - nums[idx], idx + 1) +
-            findTargetSumWays(nums, s + nums[idx], idx + 1);
+        String key = s + "|" + idx;
+        if (memo.containsKey(key)) {
+            return memo.get(key);
+        }
+        int answer = findTargetSumWays(nums, s - nums[idx], idx + 1, memo) +
+            findTargetSumWays(nums, s + nums[idx], idx + 1, memo);
+        memo.put(key, answer);
+        return answer;
     }
 }
