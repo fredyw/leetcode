42092f8454e0b866c63aeb01a978a0e40fd23123
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-05-25 01:14:55 -0500

    Update problem 1049

--- src/main/java/leetcode/Problem1049.java
+++ src/main/java/leetcode/Problem1049.java
@@ -1,12 +1,42 @@
 package leetcode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * https://leetcode.com/problems/last-stone-weight-ii/
  */
 public class Problem1049 {
     public int lastStoneWeightII(int[] stones) {
-        // TODO
-        return 0;
+        int answer = Integer.MAX_VALUE;
+        Set<Integer> indexes = new HashSet<>();
+        for (int i = 0; i < stones.length; i++) {
+            indexes.add(i);
+            answer = Math.min(answer, lastStoneWeight(stones, i, indexes, 0));
+            indexes.remove(i);
+        }
+        return answer;
+    }
+
+    private static int lastStoneWeight(int[] stones, int index, Set<Integer> indexes,
+                                       int result) {
+        if (indexes.size() == stones.length) {
+            return result;
+        }
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < stones.length; i++) {
+            if (indexes.contains(i)) {
+                continue;
+            }
+            indexes.add(i);
+            int val = Math.max(stones[index], stones[i]) - Math.min(stones[index], stones[i]);
+            if (val > 0) {
+                // TODO:
+            }
+            min = Math.min(min, lastStoneWeight(stones, i, indexes, val));
+            indexes.remove(i);
+        }
+        return min;
     }
 
     public static void main(String[] args) {
