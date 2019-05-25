7428851011c41e61d7e7c33204efcc2257ac0f6f
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-05-25 00:28:42 -0500

    Skeleton for problem 1049

---
+++ src/main/java/leetcode/Problem1049.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/last-stone-weight-ii/
+ */
+public class Problem1049 {
+    public int lastStoneWeightII(int[] stones) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1049 prob = new Problem1049();
+        System.out.println(prob.lastStoneWeightII(new int[]{2, 7, 4, 1, 8, 1})); // 1
+    }
+}
