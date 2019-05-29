18c7f0e93ad2d773c6fdeda6f1237004f423fd43
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-05-29 01:32:49 -0500

    Skeleton for problem 1052

---
+++ src/main/java/leetcode/Problem1052.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/grumpy-bookstore-owner/
+ */
+public class Problem1052 {
+    public int maxSatisfied(int[] customers, int[] grumpy, int X) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1052 prob = new Problem1052();
+        System.out.println(prob.maxSatisfied(new int[]{1, 0, 1, 2, 1, 1, 7, 5}, new int[]{0, 1, 0, 1, 0, 1, 0, 1}, 3)); // 16
+    }
+}
