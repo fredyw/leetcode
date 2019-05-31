f760cb000df26ab3e9b93eaf630d7a8204690dbc
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-05-31 00:06:54 -0500

    Solve problem 1052

--- src/main/java/leetcode/Problem1052.java
+++ src/main/java/leetcode/Problem1052.java
@@ -5,12 +5,35 @@ package leetcode;
  */
 public class Problem1052 {
     public int maxSatisfied(int[] customers, int[] grumpy, int X) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem1052 prob = new Problem1052();
-        System.out.println(prob.maxSatisfied(new int[]{1, 0, 1, 2, 1, 1, 7, 5}, new int[]{0, 1, 0, 1, 0, 1, 0, 1}, 3)); // 16
+        int satisfiedSum = 0;
+        for (int i = 0; i < customers.length; i++) {
+            satisfiedSum += customers[i];
+        }
+        int[] grumpySums = new int[grumpy.length];
+        for (int i = 0; i < grumpy.length; i++) {
+            if (i == 0) {
+                if (grumpy[i] == 1) {
+                    grumpySums[i] = customers[i];
+                }
+            } else {
+                if (grumpy[i] == 1) {
+                    grumpySums[i] = grumpySums[i - 1] + customers[i];
+                } else {
+                    grumpySums[i] = grumpySums[i - 1];
+                }
+            }
+        }
+        int grumpySum = grumpySums[grumpySums.length - 1];
+        int answer = satisfiedSum - grumpySum;
+        for (int i = X - 1; i < grumpySums.length; i++) {
+            int val = 0;
+            if (i - X < 0) {
+                val = (satisfiedSum - grumpySum) + grumpySums[i];
+            } else {
+                val = (satisfiedSum - grumpySum) + (grumpySums[i] - grumpySums[i - X]);
+            }
+            answer = Math.max(answer, val);
+        }
+        return answer;
     }
 }
