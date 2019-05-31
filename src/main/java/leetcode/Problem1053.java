1be588c8e3892a527ed18b595211ea1f2088c661
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-05-31 00:09:40 -0500

    Skeleton for problem 1053

---
+++ src/main/java/leetcode/Problem1053.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/previous-permutation-with-one-swap/
+ */
+public class Problem1053 {
+    public int[] prevPermOpt1(int[] A) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem1053 prob = new Problem1053();
+    }
+}
