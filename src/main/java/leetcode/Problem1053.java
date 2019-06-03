83eb8ee17da4f038ec67d3bbc3bb52748ec368d0
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-06-03 00:23:47 -0500

    Update problem 1053

--- src/main/java/leetcode/Problem1053.java
+++ src/main/java/leetcode/Problem1053.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.Arrays;
+
 /**
  * https://leetcode.com/problems/previous-permutation-with-one-swap/
  */
@@ -11,5 +13,9 @@ public class Problem1053 {
 
     public static void main(String[] args) {
         Problem1053 prob = new Problem1053();
+        System.out.println(Arrays.toString(prob.prevPermOpt1(new int[]{3, 2, 1}))); // [3,1,2]
+        System.out.println(Arrays.toString(prob.prevPermOpt1(new int[]{1, 1, 5}))); // [1,1,5]
+        System.out.println(Arrays.toString(prob.prevPermOpt1(new int[]{1, 9, 4, 6, 7}))); // [1,7,4,6,9]
+        System.out.println(Arrays.toString(prob.prevPermOpt1(new int[]{3, 1, 1, 3}))); // [1,3,1,3]
     }
 }
