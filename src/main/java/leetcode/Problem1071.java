996924fbba222b328853746b3823f2b2b40c807d
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-06-02 23:30:27 -0500

    Skeleton for problem 1071

---
+++ src/main/java/leetcode/Problem1071.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/greatest-common-divisor-of-strings/
+ */
+public class Problem1071 {
+    public String gcdOfStrings(String str1, String str2) {
+        // TODO
+        return "";
+    }
+
+    public static void main(String[] args) {
+        Problem1071 prob = new Problem1071();
+        System.out.println(prob.gcdOfStrings("ABCABC", "ABC")); // ABC
+        System.out.println(prob.gcdOfStrings("ABABAB", "ABAB")); // AB
+        System.out.println(prob.gcdOfStrings("LEET", "CODE")); // 
+    }
+}
