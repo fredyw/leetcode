1b580e4afba8d4fab96ebd6cfb9282dc302e9f50
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-06-04 00:20:49 -0500

    Skeleton for problem 1072

---
+++ src/main/java/leetcode/Problem1072.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/flip-columns-for-maximum-number-of-equal-rows/
+ */
+public class Problem1072 {
+    public int maxEqualRowsAfterFlips(int[][] matrix) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1072 prob = new Problem1072();
+        System.out.println(prob.maxEqualRowsAfterFlips(new int[][]{
+            {0, 1}, {1, 1}
+        })); // 1
+        System.out.println(prob.maxEqualRowsAfterFlips(new int[][]{
+            {0, 1}, {1, 0}
+        })); // 2
+        System.out.println(prob.maxEqualRowsAfterFlips(new int[][]{
+            {0, 0, 0}, {0, 0, 1}, {1, 1, 0}
+        })); // 2
+    }
+}
