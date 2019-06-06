998a32eda6195bc7610f07fd6533407fe2e9c55e
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-06-06 01:06:22 -0500

    Update problem 1072

--- src/main/java/leetcode/Problem1072.java
+++ src/main/java/leetcode/Problem1072.java
@@ -1,16 +1,34 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * https://leetcode.com/problems/flip-columns-for-maximum-number-of-equal-rows/
  */
 public class Problem1072 {
     public int maxEqualRowsAfterFlips(int[][] matrix) {
-        // TODO
+        Map<String, Integer> map = new HashMap<>();
         for (int i = 0; i < matrix.length; i++) {
+            String original = "";
+            String flipped = "";
             for (int j = 0; j < matrix[i].length; j++) {
+                original += matrix[i][j];
+                flipped += matrix[i][j] == 0 ? "1" : "0";
+            }
+            if (!map.containsKey(original) && !map.containsKey(flipped)) {
+                map.put(original, 1);
+            } else if (map.containsKey(original)) {
+                map.put(original, map.get(original) + 1);
+            } else {
+                map.put(flipped, map.get(flipped) + 1);
             }
         }
-        return 0;
+        int answer = 0;
+        for (int count : map.values()) {
+            answer = Math.max(answer, count);
+        }
+        return answer;
     }
 
     public static void main(String[] args) {
