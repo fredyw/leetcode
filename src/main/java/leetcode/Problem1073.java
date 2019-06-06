7c29f0f2085a38382f2392ee642e6166a25a78dc
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-06-06 14:33:48 -0500

    Skeleton for problem 1073

---
+++ src/main/java/leetcode/Problem1073.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/adding-two-negabinary-numbers/
+ */
+public class Problem1073 {
+    public int[] addNegabinary(int[] arr1, int[] arr2) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem1073 prob = new Problem1073();
+        System.out.println(Arrays.toString(prob.addNegabinary(new int[]{1, 1, 1, 1, 1}, new int[]{1, 0, 1}))); // [1,0,0,0,0]
+    }
+}
