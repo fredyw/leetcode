939c6b9f367fe9332e61fdb1580314bcb8292d48
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-06-08 02:03:57 -0500

    Solve problem 1036

--- src/main/java/leetcode/Problem1036.java
+++ src/main/java/leetcode/Problem1036.java
@@ -13,7 +13,8 @@ public class Problem1036 {
         for (int[] b : blocked) {
             blockedSet.add(toString(b));
         }
-        return isEscapePossible(blockedSet, source, target, 1_000_000, 1_000_000);
+        return isEscapePossible(blockedSet, source, target, 1_000_000, 1_000_000) &&
+            isEscapePossible(blockedSet, target, source, 1_000_000, 1_000_000);
     }
 
     private static boolean isEscapePossible(Set<String> blocked, int[] source,
@@ -26,9 +27,6 @@ public class Problem1036 {
             if (pop[0] == target[0] && pop[1] == target[1]) {
                 return true;
             }
-            if (coordinates.size() >= 1000) {
-                return true;
-            }
             if ((pop[0] < 0 || pop[0] == maxRow) || (pop[1] < 0 || pop[1] == maxCol)) {
                 continue;
             }
@@ -37,6 +35,9 @@ public class Problem1036 {
                 continue;
             }
             visited.add(key);
+            if (visited.size() > 20000) {
+                return true;
+            }
             for (int[] a : new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}}) {
                 coordinates.add(new int[]{pop[0] + a[0], pop[1] + a[1]});
             }
@@ -46,13 +47,5 @@ public class Problem1036 {
 
     private static String toString(int[] a) {
         return a[0] + "," + a[1];
-    }
-
-    public static void main(String[] args) {
-        Problem1036 prob = new Problem1036();
-        System.out.println(prob.isEscapePossible(
-            new int[][]{{0, 1}, {1, 0}}, new int[]{0, 0}, new int[]{0, 2})); // false
-        System.out.println(prob.isEscapePossible(
-            new int[][]{}, new int[]{0, 0}, new int[]{999999,999999})); // true
     }
 }
