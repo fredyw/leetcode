3769daec03769e9121087ad5a868154aca05d1df
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-06-09 22:39:20 -0500

    Skeleton for problem 1078

---
+++ src/main/java/leetcode/Problem1078.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/occurrences-after-bigram/
+ */
+public class Problem1078 {
+    public String[] findOcurrences(String text, String first, String second) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem1078 prob = new Problem1078();
+    }
+}
