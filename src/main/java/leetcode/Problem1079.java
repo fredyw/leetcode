37c77299a0a8dac4816e1f2fd69142abd2c09a48
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-06-10 16:46:50 -0500

    Skeleton for problem 1079

---
+++ src/main/java/leetcode/Problem1079.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/letter-tile-possibilities/
+ */
+public class Problem1079 {
+    public int numTilePossibilities(String tiles) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1079 prob = new Problem1079();
+        System.out.println(prob.numTilePossibilities("AAB")); // 8
+        System.out.println(prob.numTilePossibilities("AAABBC")); // 188
+    }
+}
