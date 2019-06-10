b48b6000f6bd0ce679460c59a36177fd0c4efcf2
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-06-09 23:13:31 -0500

    Solve problem 1078

--- src/main/java/leetcode/Problem1078.java
+++ src/main/java/leetcode/Problem1078.java
@@ -1,15 +1,24 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * https://leetcode.com/problems/occurrences-after-bigram/
  */
 public class Problem1078 {
     public String[] findOcurrences(String text, String first, String second) {
-        // TODO
-        return null;
-    }
-
-    public static void main(String[] args) {
-        Problem1078 prob = new Problem1078();
+        List<String> answer = new ArrayList<>();
+        String[] words = text.split("\\s");
+        int i = 0;
+        while (i < words.length - 1) {
+            if (words[i].equals(first) && words[i + 1].equals(second)) {
+                if (i + 2 < words.length) {
+                    answer.add(words[i + 2]);
+                }
+            }
+            i++;
+        }
+        return answer.toArray(new String[0]);
     }
 }
