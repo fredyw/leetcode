6d7f02d30814b941e402895549de78919f6e8430
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-06-11 00:02:10 -0500

    Skeleton for problem 1080

---
+++ src/main/java/leetcode/Problem1080.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/insufficient-nodes-in-root-to-leaf-paths/
+ */
+public class Problem1080 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode sufficientSubset(TreeNode root, int limit) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem1080 prob = new Problem1080();
+        
+    }
+}
