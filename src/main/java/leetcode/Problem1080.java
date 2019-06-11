d76649cc0e3722247598d809f1aaa3095ca6992d
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-06-11 00:07:00 -0500

    Update problem 1080

--- src/main/java/leetcode/Problem1080.java
+++ src/main/java/leetcode/Problem1080.java
@@ -21,6 +21,36 @@ public class Problem1080 {
 
     public static void main(String[] args) {
         Problem1080 prob = new Problem1080();
-        
+
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(4);
+        root.left.left.left = new TreeNode(8);
+        root.left.left.right = new TreeNode(9);
+        root.left.right = new TreeNode(-99);
+        root.left.right.left = new TreeNode(-99);
+        root.left.right.right = new TreeNode(-99);
+        root.right = new TreeNode(3);
+        root.right.left = new TreeNode(-99);
+        root.right.left.left = new TreeNode(12);
+        root.right.left.right = new TreeNode(13);
+        root.right.right = new TreeNode(7);
+        root.right.right.left = new TreeNode(-99);
+        root.right.right.right = new TreeNode(14);
+
+        root = prob.sufficientSubset(root, 1);
+
+        root = new TreeNode(5);
+        root.left = new TreeNode(4);
+        root.left.left = new TreeNode(11);
+        root.left.left.left = new TreeNode(7);
+        root.left.left.right = new TreeNode(1);
+        root.right = new TreeNode(8);
+        root.right.left = new TreeNode(17);
+        root.right.right = new TreeNode(4);
+        root.right.right.left = new TreeNode(5);
+        root.right.right.right = new TreeNode(3);
+
+        root = prob.sufficientSubset(root, 22);
     }
 }
