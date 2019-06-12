1b74158204f5bc1bc7c5d200764b9508408bfba5
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-06-12 00:42:52 -0500

    Skeleton for problem 1081

---
+++ src/main/java/leetcode/Problem1081.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/smallest-subsequence-of-distinct-characters/
+ */
+public class Problem1081 {
+    public String smallestSubsequence(String text) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem1081 prob = new Problem1081();
+        System.out.println(prob.smallestSubsequence("cdadabcc")); // "adbc"
+        System.out.println(prob.smallestSubsequence("abcd")); // "abcd"
+        System.out.println(prob.smallestSubsequence("ecbacba")); // "eacb"
+        System.out.println(prob.smallestSubsequence("leetcode")); // "letcod"
+    }
+}
