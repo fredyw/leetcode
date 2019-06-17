08c3d4bda0b21e0ddd1cd65752b46e315564926f
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-06-16 23:52:43 -0500

    Solve problem 1089

--- src/main/java/leetcode/Problem1089.java
+++ src/main/java/leetcode/Problem1089.java
@@ -1,22 +1,22 @@
 package leetcode;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * https://leetcode.com/problems/duplicate-zeros/
  */
 public class Problem1089 {
     public void duplicateZeros(int[] arr) {
-        // TODO
-    }
-
-    public static void main(String[] args) {
-        Problem1089 prob = new Problem1089();
-        int[] a = new int[]{1, 0, 2, 3, 0, 4, 5, 0};
-        prob.duplicateZeros(a);
-        System.out.println(Arrays.toString(a));
-        a = new int[]{1, 2, 3};
-        prob.duplicateZeros(a);
-        System.out.println(Arrays.toString(a));
+        List<Integer> answer = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                answer.add(arr[i]);
+            }
+            answer.add(arr[i]);
+        }
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = answer.get(i);
+        }
     }
 }
