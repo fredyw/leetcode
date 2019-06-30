758d7e142150953ffc00ee4993035134ad27a32a
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-06-30 16:29:41 -0500

    Skeleton for problem 1104

---
+++ src/main/java/leetcode/Problem1104.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/path-in-zigzag-labelled-binary-tree/
+ */
+public class Problem1104 {
+    public List<Integer> pathInZigZagTree(int label) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem1104 prob = new Problem1104();
+        System.out.println(prob.pathInZigZagTree(14)); // [1,3,4,14]
+        System.out.println(prob.pathInZigZagTree(26)); // [1,2,6,10,26]
+    }
+}
