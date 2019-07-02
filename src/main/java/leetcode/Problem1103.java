3184ec56e38c3250e8d3a70e7453243e8550b8e7
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-07-01 23:38:59 -0500

    Skeleton for problem 1103

---
+++ src/main/java/leetcode/Problem1103.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/distribute-candies-to-people/
+ */
+public class Problem1103 {
+    public int[] distributeCandies(int candies, int num_people) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem1103 prob = new Problem1103();
+        System.out.println(Arrays.toString(prob.distributeCandies(7, 4))); // [1,2,3,1]
+        System.out.println(Arrays.toString(prob.distributeCandies(10, 3))); // [5,2,3]
+    }
+}
