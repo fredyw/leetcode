2b2ce48ab5db0e02fbb0c9ec4ebaf8ecd1df1c94
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-07-04 00:27:38 -0500

    Skeleton for problem 1105

---
+++ src/main/java/leetcode/Problem1105.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/filling-bookcase-shelves/
+ */
+public class Problem1105 {
+    public int minHeightShelves(int[][] books, int shelf_width) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1105 prob = new Problem1105();
+    }
+}
