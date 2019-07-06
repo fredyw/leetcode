2ee4ef5ee69e2375e3067f1f67ddccf4216c0acc
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-07-06 00:38:58 -0500

    Update problem 1096

--- src/main/java/leetcode/Problem1096.java
+++ src/main/java/leetcode/Problem1096.java
@@ -1,14 +1,56 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * https://leetcode.com/problems/brace-expansion-ii/
  */
 public class Problem1096 {
     public List<String> braceExpansionII(String expression) {
-        // TODO
-        return null;
+        List<List<List<String>>> groups = new ArrayList<>();
+        groups.add(new ArrayList<>());
+        int level = 0;
+        int start = -1;
+        for (int i = 0; i < expression.length(); i++) {
+            char c = expression.charAt(i);
+            if (c == '{') {
+                if (level == 0) {
+                    start = i + 1;
+                }
+                level++;
+            } else if (c == '}') {
+                level--;
+                if (level == 0) {
+                    groups.get(groups.size() - 1).add(
+                        braceExpansionII(expression.substring(start, i)));
+                }
+            } else if (c == ',' && level == 0) {
+                groups.add(new ArrayList<>());
+            } else if (level == 0) {
+                groups.get(groups.size() - 1).add(Arrays.asList(String.valueOf(c)));
+            }
+        }
+
+        Set<String> set = new TreeSet<>();
+        for (List<List<String>> group : groups) {
+            List<String> pre = new ArrayList<>();
+            pre.add("");
+            for (List<String> g : group) {
+                List<String> tmp = new ArrayList<>();
+                for (String pStr : pre) {
+                    for (String gStr : g) {
+                        tmp.add(pStr + gStr);
+                    }
+                }
+                pre = tmp;
+            }
+            set.addAll(pre);
+        }
+        return new ArrayList<>(set);
     }
 
     public static void main(String[] args) {
