1c0979245c954138b7bc69a46ed0af6eec70a3bf
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-07-06 00:49:40 -0500

    Solve problem 1096

--- src/main/java/leetcode/Problem1096.java
+++ src/main/java/leetcode/Problem1096.java
@@ -52,17 +52,4 @@ public class Problem1096 {
         }
         return new ArrayList<>(set);
     }
-
-    public static void main(String[] args) {
-        Problem1096 prob = new Problem1096();
-        System.out.println(prob.braceExpansionII("{a{b,c}d{e,f}}")); // ["abde",abdf","acde","acdf"]
-//        System.out.println(prob.braceExpansionII("a{b,c}")); // ["ab","ac"]
-//        System.out.println(prob.braceExpansionII("{c{d,e}f}")); // ["cdf","cef"]
-//        System.out.println(prob.braceExpansionII("{a,b}{c{d,e}}")); // ["acd","ace","bcd","bce"]
-//        System.out.println(prob.braceExpansionII("{{a,z},a{b,c},{ab,z}}")); // ["a","ab","ac","z"]
-//        System.out.println(prob.braceExpansionII("{a,b,c}")); // ["a","b","c"]
-//        System.out.println(prob.braceExpansionII("{{a,b},{b,c}}")); // ["a","b","c"]
-//        System.out.println(prob.braceExpansionII("{a,b}{c,d}")); // ["ac","ad","bc","bd"]
-//        System.out.println(prob.braceExpansionII("{a{b,c}}{{d,e}f{g,h}}")); // ["abdfg", "abdfh", "abefg", "abefh", "acdfg", "acdfh", "acefg", "acefh"]
-    }
 }
