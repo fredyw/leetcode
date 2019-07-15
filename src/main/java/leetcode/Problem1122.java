10a9e443596fed47809039c8143cb427d5f4e2c5
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-07-14 23:53:15 -0500

    Skeleton for problem 1122

---
+++ src/main/java/leetcode/Problem1122.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/relative-sort-array/
+ */
+public class Problem1122 {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem1122 prob = new Problem1122();
+    }
+}
