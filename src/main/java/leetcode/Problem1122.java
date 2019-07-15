5a9da93b89daa8a61ce2785f6d7d0d06ebb7e280
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-07-15 00:05:45 -0500

    Update problem 1122

--- src/main/java/leetcode/Problem1122.java
+++ src/main/java/leetcode/Problem1122.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.Arrays;
+
 /**
  * https://leetcode.com/problems/relative-sort-array/
  */
@@ -11,5 +13,8 @@ public class Problem1122 {
 
     public static void main(String[] args) {
         Problem1122 prob = new Problem1122();
+        System.out.println(Arrays.toString(prob.relativeSortArray(
+            new int[]{2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19},
+            new int[]{2, 1, 4, 3, 9, 6}))); // [2,2,2,1,4,3,3,9,6,7,19]
     }
 }
