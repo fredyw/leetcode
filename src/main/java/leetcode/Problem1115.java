1cbdfeb1f21d8bc396660d850c61be3470745e7e
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-07-17 00:56:14 -0500

    Solve problem 1115

--- src/main/java/leetcode/Problem1115.java
+++ src/main/java/leetcode/Problem1115.java
@@ -1,31 +1,35 @@
 package leetcode;
 
+import java.util.concurrent.Semaphore;
+
 /**
  * https://leetcode.com/problems/print-foobar-alternately/
  */
 public class Problem1115 {
     private static class FooBar {
         private int n;
+        private final Semaphore fooSemaphore = new Semaphore(0);
+        private final Semaphore barSemaphore = new Semaphore(2);
 
         public FooBar(int n) {
             this.n = n;
         }
 
         public void foo(Runnable printFoo) throws InterruptedException {
-
             for (int i = 0; i < n; i++) {
-
+                barSemaphore.acquire();
                 // printFoo.run() outputs "foo". Do not change or remove this line.
                 printFoo.run();
+                fooSemaphore.release();
             }
         }
 
         public void bar(Runnable printBar) throws InterruptedException {
-
             for (int i = 0; i < n; i++) {
-
+                fooSemaphore.acquire();
                 // printBar.run() outputs "bar". Do not change or remove this line.
                 printBar.run();
+                barSemaphore.release();
             }
         }
     }
