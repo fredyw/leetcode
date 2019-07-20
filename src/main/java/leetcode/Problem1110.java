4c458335c39b5d22d2f94d61d8557f6c190d13d0
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-07-20 00:24:00 -0500

    Skeleton for problem 1110

---
+++ src/main/java/leetcode/Problem1110.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/delete-nodes-and-return-forest/
+ */
+public class Problem1110 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem1110 prob = new Problem1110();
+    }
+}
