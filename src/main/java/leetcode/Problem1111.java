c10312ccd0e869be44e67f2b40519cedbc94e61b
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-07-21 00:14:14 -0500

    Skeleton for problem 1111

---
+++ src/main/java/leetcode/Problem1111.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/maximum-nesting-depth-of-two-valid-parentheses-strings/
+ */
+public class Problem1111 {
+    public int[] maxDepthAfterSplit(String seq) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem1111 prob = new Problem1111();
+        System.out.println(Arrays.toString(prob.maxDepthAfterSplit("(()())"))); // [0,1,1,1,1,0]
+        System.out.println(Arrays.toString(prob.maxDepthAfterSplit("()(())()"))); // [0,0,0,1,1,0,1,1]
+    }
+}
