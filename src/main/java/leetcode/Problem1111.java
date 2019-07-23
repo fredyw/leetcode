da25df6b34e5927fdf2679a11eabe5a31f6af6ab
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-07-23 00:08:58 -0500

    Update problem 1111

--- src/main/java/leetcode/Problem1111.java
+++ src/main/java/leetcode/Problem1111.java
@@ -15,5 +15,6 @@ public class Problem1111 {
         Problem1111 prob = new Problem1111();
         System.out.println(Arrays.toString(prob.maxDepthAfterSplit("(()())"))); // [0,1,1,1,1,0]
         System.out.println(Arrays.toString(prob.maxDepthAfterSplit("()(())()"))); // [0,0,0,1,1,0,1,1]
+        System.out.println(Arrays.toString(prob.maxDepthAfterSplit("()(()())"))); // [0,0,0,1,0,0,0,1]
     }
 }
