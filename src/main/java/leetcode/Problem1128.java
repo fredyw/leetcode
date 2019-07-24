eb151931c411c375bd9df616dd086b96c82a8de0
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-07-24 00:14:56 -0500

    Skeleton for problem 1128

---
+++ src/main/java/leetcode/Problem1128.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/number-of-equivalent-domino-pairs/
+ */
+public class Problem1128 {
+    public int numEquivDominoPairs(int[][] dominoes) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1128 prob = new Problem1128();
+        System.out.println(prob.numEquivDominoPairs(new int[][]{
+            {1, 2}, {2, 1}, {3, 4}, {5, 6}
+        })); // 1
+    }
+}
