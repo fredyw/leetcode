6a967dfb4b021cc026cfc09cd8f9966678098d9d
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-07-24 00:23:07 -0500

    Update problem 1128

--- src/main/java/leetcode/Problem1128.java
+++ src/main/java/leetcode/Problem1128.java
@@ -5,8 +5,18 @@ package leetcode;
  */
 public class Problem1128 {
     public int numEquivDominoPairs(int[][] dominoes) {
-        // TODO
-        return 0;
+        int answer = 0;
+        for (int i = 0; i < dominoes.length; i++) {
+            for (int j = i + 1; j < dominoes.length; j++) {
+                if ((dominoes[i][0] == dominoes[j][0] &&
+                    dominoes[i][1] == dominoes[j][1]) ||
+                    (dominoes[i][0] == dominoes[j][1] &&
+                    dominoes[i][1] == dominoes[j][0])) {
+                    answer++;
+                }
+            }
+        }
+        return answer;
     }
 
     public static void main(String[] args) {
@@ -14,5 +24,17 @@ public class Problem1128 {
         System.out.println(prob.numEquivDominoPairs(new int[][]{
             {1, 2}, {2, 1}, {3, 4}, {5, 6}
         })); // 1
+        System.out.println(prob.numEquivDominoPairs(new int[][]{
+            {1, 2}, {2, 1}, {1, 2}
+        })); // 3
+        System.out.println(prob.numEquivDominoPairs(new int[][]{
+            {1, 2}, {2, 1}, {1, 2}, {1, 2}
+        })); // 6
+        System.out.println(prob.numEquivDominoPairs(new int[][]{
+            {1, 2}, {2, 1}, {1, 2}, {1, 2}, {1, 2}
+        })); // 10
+        System.out.println(prob.numEquivDominoPairs(new int[][]{
+            {1, 2}, {2, 1}, {1, 2}, {1, 2}, {1, 2}, {1, 2}
+        })); // 15
     }
 }
