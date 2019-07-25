69f4ffe4647f615c5631a6294e65d477fbfd4705
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-07-24 23:04:16 -0500

    Skeleton for problem 1116

---
+++ src/main/java/leetcode/Problem1116.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+import java.util.function.IntConsumer;
+
+/**
+ * https://leetcode.com/problems/print-zero-even-odd/
+ */
+public class Problem1116 {
+    class ZeroEvenOdd {
+        private int n;
+
+        public ZeroEvenOdd(int n) {
+            this.n = n;
+        }
+
+        // printNumber.accept(x) outputs "x", where x is an integer.
+        public void zero(IntConsumer printNumber) throws InterruptedException {
+
+        }
+
+        public void even(IntConsumer printNumber) throws InterruptedException {
+
+        }
+
+        public void odd(IntConsumer printNumber) throws InterruptedException {
+
+        }
+    }
+
+    public static void main(String[] args) {
+        Problem1116 prob = new Problem1116();
+    }
+}
