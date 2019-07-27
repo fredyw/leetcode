1c4631d9486c7729f018cf585cc4a200bf8d4254
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-07-26 23:53:13 -0500

    Skeleton for problem 1123

---
+++ src/main/java/leetcode/Problem1123.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/lowest-common-ancestor-of-deepest-leaves/
+ */
+public class Problem1123 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode lcaDeepestLeaves(TreeNode root) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+    }
+}
