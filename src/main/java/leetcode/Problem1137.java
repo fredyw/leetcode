cb712b604b462906681f2e447f2878c23a55895a
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-07-27 23:32:45 -0500

    Skeleton for problem 1137

---
+++ src/main/java/leetcode/Problem1137.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/n-th-tribonacci-number/
+ */
+public class Problem1137 {
+    public int tribonacci(int n) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1137 prob = new Problem1137();
+        System.out.println(prob.tribonacci(4)); // 4
+    }
+}
