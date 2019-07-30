7438194d4d8fd59ced7689b890a242a18e4c4bad
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-07-29 20:16:24 -0500

    Update problem 1123

--- src/main/java/leetcode/Problem1123.java
+++ src/main/java/leetcode/Problem1123.java
@@ -20,5 +20,31 @@ public class Problem1123 {
     }
 
     public static void main(String[] args) {
+        Problem1123 prob = new Problem1123();
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root = prob.lcaDeepestLeaves(root);
+        System.out.println(root); // 1
+        System.out.println(root.left); // 2
+        System.out.println(root.right); // 3
+
+
+        root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root = prob.lcaDeepestLeaves(root);
+        System.out.println(root); // 4
+
+        root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root = prob.lcaDeepestLeaves(root);
+        System.out.println(root); // 2
+        System.out.println(root.left); // 4
+        System.out.println(root.right); // 5
     }
 }
