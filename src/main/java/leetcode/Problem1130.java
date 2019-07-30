943fc2ac29573e7006d81e6ca37f5bd6c63b482a
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-07-29 23:03:54 -0500

    Skeleton for problem 1130

---
+++ src/main/java/leetcode/Problem1130.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/minimum-cost-tree-from-leaf-values/
+ */
+public class Problem1130 {
+    public int mctFromLeafValues(int[] arr) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1130 prob = new Problem1130();
+    }
+}
