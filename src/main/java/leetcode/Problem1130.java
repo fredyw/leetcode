cc11a2edd4033bb68b0338ab8657edef9e858ddd
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-07-30 23:10:50 -0500

    Update problem 1130

--- src/main/java/leetcode/Problem1130.java
+++ src/main/java/leetcode/Problem1130.java
@@ -13,5 +13,6 @@ public class Problem1130 {
         Problem1130 prob = new Problem1130();
         System.out.println(prob.mctFromLeafValues(new int[]{6, 2, 4})); // 32
         System.out.println(prob.mctFromLeafValues(new int[]{6, 2, 4, 5})); // 58
+        System.out.println(prob.mctFromLeafValues(new int[]{6, 2})); // 12
     }
 }
