f75c335807cf7a11ee482b4411f0bf766eafd5a9
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-07-31 23:47:24 -0500

    Skeleton for problem 1140

---
+++ src/main/java/leetcode/Problem1140.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/stone-game-ii/
+ */
+public class Problem1140 {
+    public int stoneGameII(int[] piles) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1140 prob = new Problem1140();
+    }
+}
