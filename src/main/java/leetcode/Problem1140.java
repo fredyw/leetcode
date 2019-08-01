6f1f8fc6a8c11418126490888dc3eff45d0a2092
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-08-01 00:22:17 -0500

    Update problem 1140

--- src/main/java/leetcode/Problem1140.java
+++ src/main/java/leetcode/Problem1140.java
@@ -11,5 +11,8 @@ public class Problem1140 {
 
     public static void main(String[] args) {
         Problem1140 prob = new Problem1140();
+        System.out.println(prob.stoneGameII(new int[]{2, 7, 9, 4, 4})); // 10
+        System.out.println(prob.stoneGameII(new int[]{2})); // 2
+        System.out.println(prob.stoneGameII(new int[]{2, 3})); // 5
     }
 }
