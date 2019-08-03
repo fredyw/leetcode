99825d584228744955869d9e15ec0dac2bc52f5f
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-08-03 18:59:45 -0500

    Skeleton for problem 1143

---
+++ src/main/java/leetcode/Problem1143.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/longest-common-subsequence/
+ */
+public class Problem1143 {
+    public int longestCommonSubsequence(String text1, String text2) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1143 prob = new Problem1143();
+        System.out.println(prob.longestCommonSubsequence("abcde", "ace")); // 3
+        System.out.println(prob.longestCommonSubsequence("abc", "abc")); // 3
+        System.out.println(prob.longestCommonSubsequence("abc", "def")); // 0
+    }
+}
