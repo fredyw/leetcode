1d86eb5213a32e6ef2bafa217a8009e5c734b66a
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-08-03 21:48:05 -0500

    Update problem 1143

--- src/main/java/leetcode/Problem1143.java
+++ src/main/java/leetcode/Problem1143.java
@@ -5,15 +5,31 @@ package leetcode;
  */
 public class Problem1143 {
     public int longestCommonSubsequence(String text1, String text2) {
-        // TODO
-        return 0;
+        return longestCommonSubsequence(text1, text2, 0, 0);
+    }
+
+    private static int longestCommonSubsequence(String text1, String text2, int i, int j) {
+        if (i == text1.length()) {
+            return 0;
+        }
+        if (j == text2.length()) {
+            return 0;
+        }
+        int value;
+        if (text1.charAt(i) == text2.charAt(j)) {
+            value = longestCommonSubsequence(text1, text2, i + 1, j + 1) + 1;
+        } else {
+            value = longestCommonSubsequence(text1, text2, i + 1, j) +
+                longestCommonSubsequence(text1, text2, i, j + 1);
+        }
+        return value;
     }
 
     public static void main(String[] args) {
         Problem1143 prob = new Problem1143();
         System.out.println(prob.longestCommonSubsequence("abcde", "ace")); // 3
-        System.out.println(prob.longestCommonSubsequence("ace", "abcde")); // 3
-        System.out.println(prob.longestCommonSubsequence("abc", "abc")); // 3
-        System.out.println(prob.longestCommonSubsequence("abc", "def")); // 0
+//        System.out.println(prob.longestCommonSubsequence("ace", "abcde")); // 3
+//        System.out.println(prob.longestCommonSubsequence("abc", "abc")); // 3
+//        System.out.println(prob.longestCommonSubsequence("abc", "def")); // 0
     }
 }
