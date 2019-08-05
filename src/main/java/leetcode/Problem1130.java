225dc98c998dbca983e0dabf8f329daab908bb56
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-08-05 00:32:23 -0500

    Update problem 1130

--- src/main/java/leetcode/Problem1130.java
+++ src/main/java/leetcode/Problem1130.java
@@ -5,8 +5,32 @@ package leetcode;
  */
 public class Problem1130 {
     public int mctFromLeafValues(int[] arr) {
-        // TODO
-        return 0;
+        return mctFromLeafValues(arr, 0, arr.length - 1).sum;
+    }
+
+    private static MaxSum mctFromLeafValues(int[] array, int start, int end) {
+        if (start == end) {
+            return new MaxSum(array[start], 0);
+        }
+        int max = 0;
+        int sum = Integer.MAX_VALUE;
+        for (int i = start; i < end; i++) {
+            MaxSum left = mctFromLeafValues(array, start, i);
+            MaxSum right = mctFromLeafValues(array, i + 1, end);
+            max = Math.max(max, Math.max(left.max, right.max));
+            sum = Math.min(sum, left.sum + right.sum + left.max * right.max);
+        }
+        return new MaxSum(max, sum);
+    }
+
+    private static class MaxSum {
+        private final int max;
+        private final int sum;
+
+        public MaxSum(int max, int sum) {
+            this.max = max;
+            this.sum = sum;
+        }
     }
 
     public static void main(String[] args) {
