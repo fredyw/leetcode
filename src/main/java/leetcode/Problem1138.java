0149322424fddf193181ad9912ecf19be02a3926
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-08-10 12:19:58 -0500

    Update problem 1138

--- src/main/java/leetcode/Problem1138.java
+++ src/main/java/leetcode/Problem1138.java
@@ -1,4 +1,17 @@
 package leetcode;
 
+/**
+ * https://leetcode.com/problems/alphabet-board-path/
+ */
 public class Problem1138 {
+    public String alphabetBoardPath(String target) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem1138 prob = new Problem1138();
+        System.out.println(prob.alphabetBoardPath("leet")); // "DDR!UURRR!!DDD!"
+        System.out.println(prob.alphabetBoardPath("code")); // "RR!DDRR!UUL!R!"
+    }
 }
