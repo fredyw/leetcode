2eebefe188e6acdc1ac98d52b1a0a8f245139903
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-08-10 01:05:53 -0500

    Solve problem 1131

--- src/main/java/leetcode/Problem1131.java
+++ src/main/java/leetcode/Problem1131.java
@@ -1,18 +1,53 @@
 package leetcode;
 
+import java.util.Arrays;
+
 /**
  * https://leetcode.com/problems/maximum-of-absolute-value-expression/
  */
 public class Problem1131 {
     public int maxAbsValExpr(int[] arr1, int[] arr2) {
+        int size = arr1.length;
+        int values[][] = new int[8][size];
+        for (int i = 0; i < size; i++) {
+            values[0][i] += arr1[i] + arr2[i] + i;
+        }
+        for (int i = 0; i < size; i++) {
+            values[1][i] += arr1[i] + arr2[i] - i;
+        }
+        for (int i = 0; i < size; i++) {
+            values[2][i] += arr1[i] - arr2[i] + i;
+        }
+        for (int i = 0; i < size; i++) {
+            values[3][i] += arr1[i] - arr2[i] - i;
+        }
+        for (int i = 0; i < size; i++) {
+            values[4][i] += -arr1[i] + arr2[i] + i;
+        }
+        for (int i = 0; i < size; i++) {
+            values[5][i] += -arr1[i] + arr2[i] - i;
+        }
+        for (int i = 0; i < size; i++) {
+            values[6][i] += -arr1[i] - arr2[i] + i;
+        }
+        for (int i = 0; i < size; i++) {
+            values[7][i] += -arr1[i] - arr2[i] - i;
+        }
         int answer = 0;
-        for (int i = 0; i < arr1.length; i++) {
-            for (int j = i + 1; j < arr2.length; j++) {
-                int value = Math.abs(arr1[i] - arr1[j]) + Math.abs(arr2[i] - arr2[j]) + Math.abs(i - j);
-                answer = Math.max(answer, value);
-            }
+        for (int[] nums : values) {
+            answer = Math.max(answer, getValue(nums));
         }
         return answer;
+    }
+
+    private static int getValue(int[] nums) {
+        int min = nums[0];
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            min = Math.min(min, nums[i]);
+            max = Math.max(max, nums[i]);
+        }
+        return max - min;
     }
 
     public static void main(String[] args) {
