9f2b504c88499cb22286e836cd24479710861ebc
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-08-12 00:00:25 -0500

    Update problem 1154

--- src/main/java/leetcode/Problem1154.java
+++ src/main/java/leetcode/Problem1154.java
@@ -7,15 +7,6 @@ import java.time.LocalDate;
  */
 public class Problem1154 {
     public int dayOfYear(String date) {
-        LocalDate localDate = LocalDate.parse(date);
-        return localDate.getDayOfYear();
-    }
-
-    public static void main(String[] args) {
-        Problem1154 prob = new Problem1154();
-        System.out.println(prob.dayOfYear("2019-01-09")); // 9
-        System.out.println(prob.dayOfYear("2019-02-10")); // 41
-        System.out.println(prob.dayOfYear("2003-03-01")); // 60
-        System.out.println(prob.dayOfYear("2004-03-01")); // 61
+        return LocalDate.parse(date).getDayOfYear();
     }
 }
