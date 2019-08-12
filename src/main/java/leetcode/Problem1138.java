fef902f8cbb22d4999c3cf94868d8dfd54cee24f
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-08-12 00:28:14 -0500

    Solve probelm 1138

--- src/main/java/leetcode/Problem1138.java
+++ src/main/java/leetcode/Problem1138.java
@@ -10,26 +10,30 @@ public class Problem1138 {
         for (int i = 0; i < target.length() - 1; i++) {
             char src = target.charAt(i);
             char dest = target.charAt(i + 1);
-            int srcRow = (src - 'a') / 5;
-            int srcCol = (src - 'a') % 5;
-            int destRow = (dest - 'a') / 5;
-            int destCol = (dest - 'a') % 5;
+            if (src == dest) {
+                answer += "!";
+            } else {
+                int srcRow = (src - 'a') / 5;
+                int srcCol = (src - 'a') % 5;
+                int destRow = (dest - 'a') / 5;
+                int destCol = (dest - 'a') % 5;
 
-            int row = destRow - srcRow;
-            int col = destCol - srcCol;
-            answer += buildPath(row, col);
+                int row = destRow - srcRow;
+                int col = destCol - srcCol;
+                answer += buildPath(row, col, destRow == 5 /* is z */);
+            }
         }
         return answer;
     }
 
-    private static String buildPath(int row, int col) {
+    private static String buildPath(int row, int col, boolean isZ) {
         String path  = "";
         if (row < 0) {
             for (int i = row; i < 0; i++) {
                 path += "U";
             }
         } else if (row > 0) {
-            for (int i = 0; i < row; i++) {
+            for (int i = 0; i < (isZ ? row - 1 : row); i++) {
                 path += "D";
             }
         }
@@ -43,13 +47,9 @@ public class Problem1138 {
                 path += "R";
             }
         }
+        if (isZ) {
+            path += "D";
+        }
         return path + "!";
-    }
-
-    public static void main(String[] args) {
-        Problem1138 prob = new Problem1138();
-//        System.out.println(prob.alphabetBoardPath("leet")); // "DDR!UURRR!!DDD!"
-//        System.out.println(prob.alphabetBoardPath("code")); // "RR!DDRR!UUL!R!"
-        System.out.println(prob.alphabetBoardPath("zdz")); // "DDDDD!UUUUURRR!DDDDLLLD!"
     }
 }
