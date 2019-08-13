0b1b42546159c46b02880d8bbadda26be1ff7da2
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-08-13 02:16:45 -0500

    Update problem 1117

--- src/main/java/leetcode/Problem1117.java
+++ src/main/java/leetcode/Problem1117.java
@@ -7,8 +7,8 @@ import java.util.concurrent.Semaphore;
  */
 public class Problem1117 {
     private static class H2O {
-        private final Semaphore hSemaphore = new Semaphore(2, true);
-        private final Semaphore oSempahore = new Semaphore(0, true);
+        private final Semaphore hSemaphore = new Semaphore(2);
+        private final Semaphore oSempahore = new Semaphore(0);
 
         public H2O() {
         }
@@ -26,32 +26,5 @@ public class Problem1117 {
             releaseOxygen.run();
             hSemaphore.release(2);
         }
-    }
-
-    public static void main(String[] args) throws Exception {
-        H2O h2o = new H2O();
-        h2o.oxygen(() -> System.out.print("O"));
-        Thread t1 = new Thread(() -> {
-            try {
-                h2o.hydrogen(() -> {
-                    for (int i = 0; i < 5; i++) {
-                        System.out.print("H");
-                    }
-                });
-            } catch (Exception e) {}
-        });
-        Thread t2 = new Thread(() -> {
-            try {
-                h2o.oxygen(() -> {
-                    for (int i = 0; i < 5; i++) {
-                        System.out.print("O");
-                    }
-                });
-            } catch (Exception e) {}
-        });
-        t1.start();
-        t2.start();
-        t1.join();
-        t2.join();
     }
 }
