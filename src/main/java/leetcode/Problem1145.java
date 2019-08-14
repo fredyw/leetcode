7e81c6366cdf4bfb32edf15538647d0f9b98e802
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-08-13 23:23:24 -0500

    Update problem 1145

--- src/main/java/leetcode/Problem1145.java
+++ src/main/java/leetcode/Problem1145.java
@@ -21,5 +21,20 @@ public class Problem1145 {
 
     public static void main(String[] args) {
         Problem1145 prob = new Problem1145();
+
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+        root.left.left.left = new TreeNode(8);
+        root.left.left.right = new TreeNode(9);
+        root.left.right.left = new TreeNode(10);
+        root.left.right.right = new TreeNode(11);
+
+        System.out.println(prob.btreeGameWinningMove(root, 11, 3)); // true
+        System.out.println(prob.btreeGameWinningMove(root, 11, 2)); // false
     }
 }
