7401184a9f17b41916819afdf8313bcf97ca7e8d
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-08-13 23:02:23 -0500

    Skeleton for problem 1145

---
+++ src/main/java/leetcode/Problem1145.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/binary-tree-coloring-game/
+ */
+public class Problem1145 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean btreeGameWinningMove(TreeNode root, int n, int x) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem1145 prob = new Problem1145();
+    }
+}
