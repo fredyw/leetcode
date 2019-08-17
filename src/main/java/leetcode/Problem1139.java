b5645e7ec89cf4a037e040d4829eea71d3670dac
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-08-16 23:39:12 -0500

    Skeleton for problem 1139

---
+++ src/main/java/leetcode/Problem1139.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/largest-1-bordered-square/
+ */
+public class Problem1139 {
+    public int largest1BorderedSquare(int[][] grid) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1139 prob = new Problem1139();
+        System.out.println(prob.largest1BorderedSquare(new int[][]{
+            {1, 1, 1},
+            {1, 0, 1},
+            {1, 1, 1}
+        })); // 9
+        System.out.println(prob.largest1BorderedSquare(new int[][]{
+            {1, 1, 0, 0},
+        })); // 1
+    }
+}
