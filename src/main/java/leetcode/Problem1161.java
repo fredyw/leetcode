666298eb0560203941f9309ee9c341d9519b2e11
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-08-19 00:08:08 -0500

    Skeleton for problem 1161

---
+++ src/main/java/leetcode/Problem1161.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/maximum-level-sum-of-a-binary-tree/
+ */
+public class Problem1161 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public int maxLevelSum(TreeNode root) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
