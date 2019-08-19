1977b2554aa7452c86882e71a5db8227ea756dd1
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-08-18 23:51:26 -0500

    Skeleton for problem 1160

---
+++ src/main/java/leetcode/Problem1160.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/find-words-that-can-be-formed-by-characters/
+ */
+public class Problem1160 {
+    public int countCharacters(String[] words, String chars) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1160 prob = new Problem1160();
+    }
+}
