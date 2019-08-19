887b32e075fe1c1c6272f70dfc951ef2e512a816
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-08-19 00:02:40 -0500

    Solve problem 1160

--- src/main/java/leetcode/Problem1160.java
+++ src/main/java/leetcode/Problem1160.java
@@ -5,13 +5,33 @@ package leetcode;
  */
 public class Problem1160 {
     public int countCharacters(String[] words, String chars) {
-        // TODO
-        return 0;
+        int answer = 0;
+        for (String word : words) {
+            if (word.length() > chars.length()) {
+                continue;
+            }
+            int[] counts = counts(chars);
+            boolean found = true;
+            for (char c : word.toCharArray()) {
+                if (counts[c - 'a'] == 0) {
+                    found = false;
+                    break;
+                } else {
+                    counts[c - 'a']--;
+                }
+            }
+            if (found) {
+                answer += word.length();
+            }
+        }
+        return answer;
     }
 
-    public static void main(String[] args) {
-        Problem1160 prob = new Problem1160();
-        System.out.println(prob.countCharacters(new String[]{"cat","bt","hat","tree"}, "atach")); // 6
-        System.out.println(prob.countCharacters(new String[]{"hello","world","leetcode"}, "welldonehoneyr")); // 10
+    private static int[] counts(String chars) {
+        int[] counts = new int[26];
+        for (char c : chars.toCharArray()) {
+           counts[c - 'a']++;
+        }
+        return counts;
     }
 }
