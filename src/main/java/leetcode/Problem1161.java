2fdd8e8834e7fb983db41139ea761649bbe2f79e
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-08-19 00:13:56 -0500

    Update problem 1161

--- src/main/java/leetcode/Problem1161.java
+++ src/main/java/leetcode/Problem1161.java
@@ -20,6 +20,20 @@ public class Problem1161 {
     }
 
     public static void main(String[] args) {
+        Problem1161 prob = new Problem1161();
 
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(7);
+        root.right = new TreeNode(0);
+        root.left.left = new TreeNode(7);
+        root.left.right = new TreeNode(-8);
+        System.out.println(prob.maxLevelSum(root)); // 2
+
+        root = new TreeNode(1);
+        root.left = new TreeNode(7);
+        root.right = new TreeNode(0);
+        root.left.left = new TreeNode(7);
+        root.left.right = new TreeNode(100);
+        System.out.println(prob.maxLevelSum(root)); // 3
     }
 }
