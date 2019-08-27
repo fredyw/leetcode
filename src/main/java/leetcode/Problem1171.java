5b81930a1afcc5c6e6e7c5c68f941772cc27f8e4
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-08-26 23:49:43 -0500

    Skeleton for problem 1171

---
+++ src/main/java/leetcode/Problem1171.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/remove-zero-sum-consecutive-nodes-from-linked-list/
+ */
+public class Problem1171 {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode removeZeroSumSublists(ListNode head) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem1171 prob = new Problem1171();
+    }
+}
