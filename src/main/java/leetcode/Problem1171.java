dbf0a7efbb0a008aacc11b7becfa085b9677caf1
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-08-27 23:29:16 -0500

    Update problem 1171

--- src/main/java/leetcode/Problem1171.java
+++ src/main/java/leetcode/Problem1171.java
@@ -1,5 +1,10 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  * https://leetcode.com/problems/remove-zero-sum-consecutive-nodes-from-linked-list/
  */
@@ -11,18 +16,45 @@ public class Problem1171 {
         ListNode(int x) {
             val = x;
         }
+
+        @Override
+        public String toString() {
+            return "" + val;
+        }
     }
 
     public ListNode removeZeroSumSublists(ListNode head) {
+        List<ListNode> nodes = new ArrayList<>();
+        for (ListNode n = head; n != null; n = n.next) {
+            nodes.add(n);
+        }
+        Set<Integer> removed = new HashSet<>();
         // TODO
-        return null;
+        int i = 0;
+        while (i < nodes.size() && removed.contains(i)) {
+            i++;
+        }
+        ListNode answer = null;
+        if (i < nodes.size()) {
+            answer = nodes.get(i);
+        }
+        ListNode next = answer;
+        for (; i < nodes.size() - 1; i++) {
+            if (!removed.contains(i + 1)) {
+                next.next = nodes.get(i + 1);
+                next = next.next;
+            }
+            next.next = null;
+        }
+        return answer;
     }
 
     private static void print(ListNode head) {
+        System.out.print("[ ");
         for (ListNode n = head; n != null; n = n.next) {
             System.out.print(n.val + " ");
         }
-        System.out.println();
+        System.out.println("]");
     }
 
     public static void main(String[] args) {
@@ -35,27 +67,27 @@ public class Problem1171 {
         head.next.next.next.next = new ListNode(1);
         print(prob.removeZeroSumSublists(head)); // [3,1]
 
-        head = new ListNode(1);
-        head.next = new ListNode(2);
-        head.next.next = new ListNode(3);
-        head.next.next.next = new ListNode(-3);
-        head.next.next.next.next = new ListNode(4);
-        print(prob.removeZeroSumSublists(head)); // [1,2,4]
-
-        head = new ListNode(1);
-        head.next = new ListNode(2);
-        head.next.next = new ListNode(3);
-        head.next.next.next = new ListNode(-3);
-        head.next.next.next.next = new ListNode(-2);
-        print(prob.removeZeroSumSublists(head)); // [1]
-
-        head = new ListNode(3);
-        head.next = new ListNode(4);
-        head.next.next = new ListNode(5);
-        head.next.next.next = new ListNode(-2);
-        head.next.next.next.next = new ListNode(-3);
-        head.next.next.next.next.next = new ListNode(2);
-        head.next.next.next.next.next.next = new ListNode(6);
-        print(prob.removeZeroSumSublists(head)); // [3, 2]
+//        head = new ListNode(1);
+//        head.next = new ListNode(2);
+//        head.next.next = new ListNode(3);
+//        head.next.next.next = new ListNode(-3);
+//        head.next.next.next.next = new ListNode(4);
+//        print(prob.removeZeroSumSublists(head)); // [1,2,4]
+//
+//        head = new ListNode(1);
+//        head.next = new ListNode(2);
+//        head.next.next = new ListNode(3);
+//        head.next.next.next = new ListNode(-3);
+//        head.next.next.next.next = new ListNode(-2);
+//        print(prob.removeZeroSumSublists(head)); // [1]
+//
+//        head = new ListNode(3);
+//        head.next = new ListNode(4);
+//        head.next.next = new ListNode(5);
+//        head.next.next.next = new ListNode(-2);
+//        head.next.next.next.next = new ListNode(-3);
+//        head.next.next.next.next.next = new ListNode(2);
+//        head.next.next.next.next.next.next = new ListNode(6);
+//        print(prob.removeZeroSumSublists(head)); // [3, 2]
     }
 }
