0bb1aeb7aabb49520b5751fc6763bcaa3fd0884d
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-09-05 23:13:56 -0500

    Skeleton for problem 1175

---
+++ src/main/java/leetcode/Problem1175.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/prime-arrangements/
+ */
+public class Problem1175 {
+    public int numPrimeArrangements(int n) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1175 prob = new Problem1175();
+        System.out.println(prob.numPrimeArrangements(5)); // 12
+        System.out.println(prob.numPrimeArrangements(100)); // 682289015
+    }
+}
