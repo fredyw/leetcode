717aabbdb8a86da677e5ca6a835c956cab27085f
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-09-16 13:26:18 -0500

    Update problem 1155

--- src/main/java/leetcode/Problem1155.java
+++ src/main/java/leetcode/Problem1155.java
@@ -5,12 +5,22 @@ package leetcode;
  */
 public class Problem1155 {
     public int numRollsToTarget(int d, int f, int target) {
-        for (int i = 0; i < d; i++) {
-            for (int j = 1; j <= f; j++) {
+        return numRollsToTarget(d, f, target, 0, new Integer[/*dice*/ 31][/*sum*/ 1001]) % 1_000_000_007;
+    }
 
-            }
+    private static int numRollsToTarget(int dice, int faces, int target, int sum, Integer[][] memo) {
+        if (dice == 0) {
+            return sum == target ? 1 : 0;
         }
-        return 0;
+        if (memo[dice][sum] != null) {
+            return memo[dice][sum];
+        }
+        int count = 0;
+        for (int i = 1; i <= faces; i++) {
+            count += (numRollsToTarget(dice - 1, faces, target, sum + i, memo)) % 1_000_000_007;
+        }
+        memo[dice][sum] = count % 1_000_000_007;
+        return count % 1_000_000_007;
     }
 
     public static void main(String[] args) {
