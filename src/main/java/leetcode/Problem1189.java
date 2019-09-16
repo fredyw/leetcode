db69ddcf1229465e83fdf01163b9fcb13a9a71d8
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-09-16 18:19:37 -0500

    Skeleton for problem 1189

---
+++ src/main/java/leetcode/Problem1189.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/maximum-number-of-balloons/
+ */
+public class Problem1189 {
+    public int maxNumberOfBalloons(String text) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1189 prob = new Problem1189();
+        System.out.println(prob.maxNumberOfBalloons("nlaebolko")); // 1
+        System.out.println(prob.maxNumberOfBalloons("loonbalxballpoon")); // 2
+        System.out.println(prob.maxNumberOfBalloons("leetcode")); // 0
+    }
+}
