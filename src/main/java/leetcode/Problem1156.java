6d4fe7dc0fe48ba9e148e45152f3e09f48b98372
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-09-19 00:00:59 -0500

    Update problem 1156

--- src/main/java/leetcode/Problem1156.java
+++ src/main/java/leetcode/Problem1156.java
@@ -9,15 +9,17 @@ import java.util.Set;
  */
 public class Problem1156 {
     public int maxRepOpt1(String text) {
-        int[][] counts = new int[text.length()][26];
-        for (int i = text.length() - 1; i >= 0; i--) {
-            int[] newArray = (i < text.length() - 1) ? Arrays.copyOf(counts[i + 1], 26) : new int[26];
-            newArray[text.charAt(i) - 'a']++;
-            counts[i] = newArray;
+        int[][] rightCounts = new int[text.length()][26];
+        int[][] leftCounts = new int[text.length()][26];
+        for (int i = text.length() - 1, j = 0; i >= 0; i--, j++) {
+            int[] forwardArray = (i < text.length() - 1) ? Arrays.copyOf(rightCounts[i + 1], 26) : new int[26];
+            forwardArray[text.charAt(i) - 'a']++;
+            rightCounts[i] = forwardArray;
+
+            int[] backwardArray = (j > 0) ? Arrays.copyOf(leftCounts[j - 1], 26) : new int[26];
+            backwardArray[text.charAt(j) - 'a']++;
+            leftCounts[j] = backwardArray;
         }
-//        for (int[] c : counts) {
-//            System.out.println(Arrays.toString(c));
-//        }
         Set<Integer> cuts = new HashSet<>();
         for (int i = 0; i < text.length() - 1; i++) {
             if (text.charAt(i) != text.charAt(i + 1)) {
@@ -28,32 +30,32 @@ public class Problem1156 {
         if (cuts.isEmpty()) {
             return text.length();
         }
-//        System.out.println(cuts);
         int answer = 0;
-        for (int i = 0; i < cuts.size(); i++) {
-            boolean swapped = false;
+        for (int cut : cuts) {
+            SwapType type = SwapType.NONE;
             int length = 1;
-            int j = i;
-            char current = text.charAt(j);
-            while (j < text.length() - 1) {
+            int i = cut;
+            char current = text.charAt(i);
+            while (i < text.length() - 1) {
                 if (current == ' ') {
-                    current = text.charAt(j);
+                    current = text.charAt(i);
                 }
-                char next = text.charAt(j + 1);
+                char next = text.charAt(i + 1);
                 if (current != next) {
                     // Check if it's swappable and there is a character to swap.
-                    if (swapped || counts[j + 1][current - 'a'] == 0) {
+                    int leftCount = i - 1 >= 0 ? leftCounts[i - 1][current - 'a'] : 0;
+                    int rightCount = rightCounts[i + 1][current - 'a'];
+                    if (type != SwapType.NONE || (leftCount == 0 && rightCount == 0)) {
                         break;
                     }
-                    swapped = true;
-                    // If we swap, the next char will be the same as current char.
+                    type = leftCount > 0 ? SwapType.LEFT : SwapType.RIGHT;
                 } else {
-                    if (swapped && counts[j + 1][current - 'a'] == 1) {
+                    if (type == SwapType.RIGHT && rightCounts[i + 1][current - 'a'] == 1) {
                         break;
                     }
                     current = ' ';
                 }
-                j++;
+                i++;
                 length++;
             }
             answer = Math.max(answer, length);
@@ -61,17 +63,21 @@ public class Problem1156 {
         return answer;
     }
 
+    private enum SwapType {
+        NONE, LEFT, RIGHT
+    }
+
     public static void main(String[] args) {
         Problem1156 prob = new Problem1156();
-//        System.out.println(prob.maxRepOpt1("ababa")); // 3
-//        System.out.println(prob.maxRepOpt1("aaabaaa")); // 6
-//        System.out.println(prob.maxRepOpt1("aaabbaaa")); // 4
-//        System.out.println(prob.maxRepOpt1("aaabaaaca")); // 7
-//        System.out.println(prob.maxRepOpt1("aaaaa")); // 5
-//        System.out.println(prob.maxRepOpt1("abcdef")); // 1
-//        System.out.println(prob.maxRepOpt1("abcdefa")); // 2
-//        System.out.println(prob.maxRepOpt1("abcaaa")); // 3
-//        System.out.println(prob.maxRepOpt1("aaa")); // 3
+        System.out.println(prob.maxRepOpt1("ababa")); // 3
+        System.out.println(prob.maxRepOpt1("aaabaaa")); // 6
+        System.out.println(prob.maxRepOpt1("aaabbaaa")); // 4
+        System.out.println(prob.maxRepOpt1("aaabaaaca")); // 7
+        System.out.println(prob.maxRepOpt1("aaaaa")); // 5
+        System.out.println(prob.maxRepOpt1("abcdef")); // 1
+        System.out.println(prob.maxRepOpt1("abcdefa")); // 2
+        System.out.println(prob.maxRepOpt1("abcaaa")); // 3
+        System.out.println(prob.maxRepOpt1("aaa")); // 3
         System.out.println(prob.maxRepOpt1("bbababaaaa")); // 6
     }
 }
