3b41d30f9c55f463eeb210fa06f50f35343b146c
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-09-22 20:15:23 -0500

    Skeleton for problem 1195

---
+++ src/main/java/leetcode/Problem1195.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+import java.util.function.IntConsumer;
+
+/**
+ * https://leetcode.com/problems/fizz-buzz-multithreaded/
+ */
+public class Problem1195 {
+    private static class FizzBuzz {
+        private int n;
+
+        public FizzBuzz(int n) {
+            this.n = n;
+        }
+
+        // printFizz.run() outputs "fizz".
+        public void fizz(Runnable printFizz) throws InterruptedException {
+
+        }
+
+        // printBuzz.run() outputs "buzz".
+        public void buzz(Runnable printBuzz) throws InterruptedException {
+
+        }
+
+        // printFizzBuzz.run() outputs "fizzbuzz".
+        public void fizzbuzz(Runnable printFizzBuzz) throws InterruptedException {
+
+        }
+
+        // printNumber.accept(x) outputs "x", where x is an integer.
+        public void number(IntConsumer printNumber) throws InterruptedException {
+
+        }
+    }
+}
