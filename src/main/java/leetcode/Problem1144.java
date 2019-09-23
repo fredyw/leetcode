1ada9e8c8b34029175c8b1ed595c09b9ab18f4ca
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-09-22 22:53:57 -0500

    Skeleton for problem 1144

---
+++ src/main/java/leetcode/Problem1144.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/decrease-elements-to-make-array-zigzag/
+ */
+public class Problem1144 {
+    public int movesToMakeZigzag(int[] nums) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1144 prob = new Problem1144();
+        System.out.println(prob.movesToMakeZigzag(new int[]{1,2,3})); // 2
+        System.out.println(prob.movesToMakeZigzag(new int[]{9,6,1,6,2})); // 4
+    }
+}
