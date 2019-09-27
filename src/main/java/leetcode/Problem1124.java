8019387240f7c3fad46a90cf4396b04b50ef0d76
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-09-27 00:00:49 -0500

    Skeleton for problem 1124

---
+++ src/main/java/leetcode/Problem1124.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/longest-well-performing-interval/
+ */
+public class Problem1124 {
+    public int longestWPI(int[] hours) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1124 prob = new Problem1124();
+        System.out.println(prob.longestWPI(new int[]{9,9,6,0,6,6,9})); // 3
+    }
+}
