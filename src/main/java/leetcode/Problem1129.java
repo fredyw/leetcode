da60d10e3e7fdce7554a4d18b52f1e99e9b0ced3
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-09-26 23:24:01 -0500

    Update problem 1129

--- src/main/java/leetcode/Problem1129.java
+++ src/main/java/leetcode/Problem1129.java
@@ -2,9 +2,12 @@ package leetcode;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Queue;
+import java.util.Set;
 
 /**
  * https://leetcode.com/problems/shortest-path-with-alternating-colors/
@@ -32,21 +35,23 @@ public class Problem1129 {
                                        List<Integer>[] blueAdjList,
                                        boolean red) {
         Integer[] counts = new Integer[n];
-        boolean[] visited = new boolean[n];
+        Set<Node> visited = new HashSet<>();
         Queue<Node> queue = new LinkedList<>();
         queue.add(new Node(0, red, 0));
         while (!queue.isEmpty()) {
             Node current = queue.remove();
             List<Integer>[] adjList = current.red ? blueAdjList : redAdjList; // alternate
             List<Integer> neighbors = adjList[current.node];
-            counts[current.node] = current.count;
-            visited[current.node] = true;
+            counts[current.node] = counts[current.node] == null ?
+                current.count : Math.min(counts[current.node], current.count);
+            visited.add(current);
             if (neighbors != null) {
                 for (int neighbor : neighbors) {
-                    if (visited[neighbor]) {
+                    Node node = new Node(neighbor, !current.red, current.count + 1);
+                    if (visited.contains(node)) {
                         continue;
                     }
-                    queue.add(new Node(neighbor, !current.red, current.count + 1));
+                    queue.add(node);
                 }
             }
         }
@@ -62,6 +67,20 @@ public class Problem1129 {
             this.node = node;
             this.red = red;
             this.count = count;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Node node1 = (Node) o;
+            return node == node1.node &&
+                red == node1.red;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(node, red);
         }
     }
 
@@ -80,20 +99,23 @@ public class Problem1129 {
         return graph;
     }
 
+    //8
+    //[[7,1],[4,2],[2,1],[5,0],[3,1],[1,0],[7,5],[4,3],[2,0],[0,7],[2,3],[1,7],[7,4],[2,2],[1,6],[6,2],[4,5],[2,4],[1,5],[1,2],[5,2],[0,4],[3,3]]
+    //[[1,5],[3,1],[6,3],[4,5],[7,1],[1,4],[5,5],[2,7],[7,3],[3,4],[0,3],[7,0],[4,6]]
     public static void main(String[] args) {
         Problem1129 prob = new Problem1129();
-//        System.out.println(Arrays.toString(prob.shortestAlternatingPaths(3,
-//            new int[][]{{0, 1}, {1, 2}}, new int[][]{}))); // [0,1,-1]
-//        System.out.println(Arrays.toString(prob.shortestAlternatingPaths(3,
-//            new int[][]{{0, 1}}, new int[][]{{2, 1}}))); // [0,1,-1]
-//        System.out.println(Arrays.toString(prob.shortestAlternatingPaths(3,
-//            new int[][]{{1, 0}}, new int[][]{{2, 1}}))); // [0,-1,-1]
-//        System.out.println(Arrays.toString(prob.shortestAlternatingPaths(3,
-//            new int[][]{{0, 1}}, new int[][]{{1, 2}}))); // [0,1,2]
-//        System.out.println(Arrays.toString(prob.shortestAlternatingPaths(3,
-//            new int[][]{{0, 1}, {0, 2}}, new int[][]{{1, 0}}))); // [0,1,1]
-//        System.out.println(Arrays.toString(prob.shortestAlternatingPaths(4,
-//            new int[][]{{0, 1}, {1, 2}, {3, 2}}, new int[][]{{1, 3}}))); // [0,1,3,2]
+        System.out.println(Arrays.toString(prob.shortestAlternatingPaths(3,
+            new int[][]{{0, 1}, {1, 2}}, new int[][]{}))); // [0,1,-1]
+        System.out.println(Arrays.toString(prob.shortestAlternatingPaths(3,
+            new int[][]{{0, 1}}, new int[][]{{2, 1}}))); // [0,1,-1]
+        System.out.println(Arrays.toString(prob.shortestAlternatingPaths(3,
+            new int[][]{{1, 0}}, new int[][]{{2, 1}}))); // [0,-1,-1]
+        System.out.println(Arrays.toString(prob.shortestAlternatingPaths(3,
+            new int[][]{{0, 1}}, new int[][]{{1, 2}}))); // [0,1,2]
+        System.out.println(Arrays.toString(prob.shortestAlternatingPaths(3,
+            new int[][]{{0, 1}, {0, 2}}, new int[][]{{1, 0}}))); // [0,1,1]
+        System.out.println(Arrays.toString(prob.shortestAlternatingPaths(4,
+            new int[][]{{0, 1}, {1, 2}, {3, 2}}, new int[][]{{1, 3}}))); // [0,1,3,2]
         System.out.println(Arrays.toString(prob.shortestAlternatingPaths(5,
             new int[][]{{0, 1}, {1, 2}, {2, 3}, {3, 4}}, new int[][]{{1, 2}, {2, 3}, {3, 1}}))); // [0,1,2,3,7]
     }
