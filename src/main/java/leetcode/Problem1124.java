cd244cbb42a055aa421dbf51c50ed754cb99eef5
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-09-27 18:52:28 -0500

    Update problem 1124

--- src/main/java/leetcode/Problem1124.java
+++ src/main/java/leetcode/Problem1124.java
@@ -12,5 +12,6 @@ public class Problem1124 {
     public static void main(String[] args) {
         Problem1124 prob = new Problem1124();
         System.out.println(prob.longestWPI(new int[]{9,9,6,0,6,6,9})); // 3
+        System.out.println(prob.longestWPI(new int[]{8,6,9,9,10,9,0,6,6,9})); // 9
     }
 }
