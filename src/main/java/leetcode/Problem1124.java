7bbe1cb3149bffac898b7625006469ffb9a71d67
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-10-01 00:37:08 -0500

    Update problem 1124

--- src/main/java/leetcode/Problem1124.java
+++ src/main/java/leetcode/Problem1124.java
@@ -1,6 +1,5 @@
 package leetcode;
 
-import java.util.Arrays;
 import java.util.Stack;
 
 /**
@@ -12,7 +11,6 @@ public class Problem1124 {
         for (int i = 1; i <= hours.length; i++) {
             sums[i] = sums[i - 1] + (hours[i - 1] <= 8 ? -1 : 1);
         }
-        System.out.println(Arrays.toString(sums));
         // Monotone decreasing stack.
         Stack<Integer> stack = new Stack<>();
         for (int i = 0; i <= hours.length; i++) {
@@ -20,7 +18,6 @@ public class Problem1124 {
                 stack.push(i);
             }
         }
-        System.out.println(stack);
         int answer = 0;
         for (int i = hours.length; i >= 0; i--) {
             while (!stack.empty() && sums[stack.peek()] < sums[i]) {
@@ -29,11 +26,5 @@ public class Problem1124 {
             }
         }
         return answer;
-    }
-
-    public static void main(String[] args) {
-        Problem1124 prob = new Problem1124();
-//        System.out.println(prob.longestWPI(new int[]{9,9,6,0,6,6,9})); // 3
-        System.out.println(prob.longestWPI(new int[]{8,6,9,9,10,9,0,6,6,9})); // 9
     }
 }
