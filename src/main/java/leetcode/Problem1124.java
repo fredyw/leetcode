6360e67f9a827802d85196a6e62088220d859a74
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-09-30 23:12:21 -0500

    Update problem 1124

--- src/main/java/leetcode/Problem1124.java
+++ src/main/java/leetcode/Problem1124.java
@@ -1,23 +1,31 @@
 package leetcode;
 
+import java.util.Arrays;
+import java.util.Stack;
+
 /**
  * https://leetcode.com/problems/longest-well-performing-interval/
  */
 public class Problem1124 {
     public int longestWPI(int[] hours) {
+        int[] sums = new int[hours.length + 1];
+        for (int i = 1; i <= hours.length; i++) {
+            sums[i] = sums[i - 1] + (hours[i - 1] <= 8 ? -1 : 1);
+        }
+        System.out.println(Arrays.toString(sums));
+        // Monotone decreasing stack.
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i <= hours.length; i++) {
+            if (stack.isEmpty() || sums[stack.peek()] > sums[i]) {
+                stack.push(i);
+            }
+        }
+        System.out.println(stack);
         int answer = 0;
-        for (int i = 0; i < hours.length; i++) {
-            int tiring = 0;
-            int nonTiring = 0;
-            for (int j = i; j < hours.length; j++) {
-                if (hours[j] <= 8) {
-                    nonTiring++;
-                } else {
-                    tiring++;
-                }
-                if (tiring > nonTiring) {
-                    answer = Math.max(answer, j - i + 1);
-                }
+        for (int i = hours.length; i >= 0; i--) {
+            while (!stack.empty() && sums[stack.peek()] < sums[i]) {
+                answer = Math.max(answer, i - stack.peek());
+                stack.pop();
             }
         }
         return answer;
@@ -25,7 +33,7 @@ public class Problem1124 {
 
     public static void main(String[] args) {
         Problem1124 prob = new Problem1124();
-        System.out.println(prob.longestWPI(new int[]{9,9,6,0,6,6,9})); // 3
+//        System.out.println(prob.longestWPI(new int[]{9,9,6,0,6,6,9})); // 3
         System.out.println(prob.longestWPI(new int[]{8,6,9,9,10,9,0,6,6,9})); // 9
     }
 }
