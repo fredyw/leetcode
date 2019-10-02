a2a60f546df90f63b854877e474c88267084f4ac
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-10-01 23:14:12 -0500

    Update problem 1209

--- src/main/java/leetcode/Problem1209.java
+++ src/main/java/leetcode/Problem1209.java
@@ -5,11 +5,37 @@ package leetcode;
  */
 public class Problem1209 {
     public String removeDuplicates(String s, int k) {
-        // TODO
-        return null;
+        String answer = s;
+        while (true) {
+            boolean found = false;
+            int count = 1;
+            String tmp = "";
+            int i = 0;
+            while (i < answer.length() - 1) {
+                if (answer.charAt(i) == answer.charAt(i + 1)) {
+                    count++;
+                } else {
+                    count = 1;
+                }
+                tmp += answer.charAt(i);
+                if (count == k) {
+                    tmp += answer.charAt(i + 1);
+                    tmp = tmp.substring(0, tmp.length() - k);
+                    found = true;
+                    i++;
+                    count = 1;
+                }
+                i++;
+            }
+            if (!found) {
+                break;
+            }
+            answer = tmp + (i < answer.length() ? answer.charAt(i) : "");
+        }
+        return answer;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Problem1209 prob = new Problem1209();
         System.out.println(prob.removeDuplicates("abcd", 2)); // "abcd"
         System.out.println(prob.removeDuplicates("deeedbbcccbdaa", 3)); // "aa"
