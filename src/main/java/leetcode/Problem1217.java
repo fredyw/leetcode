e0256c92f5b9ea5a18434d2e4b9be5511f094ad3
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-10-09 06:45:09 +0700

    Update problem 1217

---
+++ src/main/java/leetcode/Problem1217.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/play-with-chips/
+ */
+public class Problem1217 {
+    public int minCostToMoveChips(int[] chips) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1217 prob = new Problem1217();
+        System.out.println(prob.minCostToMoveChips(new int[]{1,2,3})); // 1
+        System.out.println(prob.minCostToMoveChips(new int[]{2,2,2,3,3})); // 2
+        System.out.println(prob.minCostToMoveChips(new int[]{1,1,2,3,3})); // 1
+    }
+}
