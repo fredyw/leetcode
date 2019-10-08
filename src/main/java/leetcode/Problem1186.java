c99626540b8bbcbd833338e784db20aca70c641f
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-10-09 06:45:23 +0700

    Update problem 1186

--- src/main/java/leetcode/Problem1186.java
+++ src/main/java/leetcode/Problem1186.java
@@ -1,4 +1,18 @@
 package leetcode;
 
+/**
+ * https://leetcode.com/problems/maximum-subarray-sum-with-one-deletion/
+ */
 public class Problem1186 {
+    public int maximumSum(int[] arr) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1186 prob = new Problem1186();
+        System.out.println(prob.maximumSum(new int[]{1, -2, 0, 3})); // 4
+        System.out.println(prob.maximumSum(new int[]{1, -2, -2, 3})); // 3
+        System.out.println(prob.maximumSum(new int[]{-1, -1, -1, -1})); // -1
+    }
 }
