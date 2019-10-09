221e94db3736e1da6f4c3f3905b43afc214ec6bf
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-10-09 07:13:09 +0700

    Solve problem 1217

--- src/main/java/leetcode/Problem1217.java
+++ src/main/java/leetcode/Problem1217.java
@@ -5,14 +5,15 @@ package leetcode;
  */
 public class Problem1217 {
     public int minCostToMoveChips(int[] chips) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem1217 prob = new Problem1217();
-        System.out.println(prob.minCostToMoveChips(new int[]{1,2,3})); // 1
-        System.out.println(prob.minCostToMoveChips(new int[]{2,2,2,3,3})); // 2
-        System.out.println(prob.minCostToMoveChips(new int[]{1,1,2,3,3})); // 1
+        int even = 0;
+        int odd = 0;
+        for (int i = 0; i < chips.length; i++) {
+            if (chips[i] % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+        }
+        return Math.min(even, odd);
     }
 }
