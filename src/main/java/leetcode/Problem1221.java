9f3c6e58ac82391bf38d47925b5d38aedefdab4c
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-10-19 08:27:27 -0700

    Solve problem 1221

--- src/main/java/leetcode/Problem1221.java
+++ src/main/java/leetcode/Problem1221.java
@@ -5,14 +5,21 @@ package leetcode;
  */
 public class Problem1221 {
     public int balancedStringSplit(String s) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem1221 prob = new Problem1221();
-        System.out.println(prob.balancedStringSplit("RLRRLLRLRL")); // 4
-        System.out.println(prob.balancedStringSplit("RLLLLRRRLR")); // 3
-        System.out.println(prob.balancedStringSplit("LLLLRRRR")); // 1
+        int answer = 0;
+        int lCount = 0;
+        int rCount = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'L') {
+                lCount++;
+            } else if (s.charAt(i) == 'R') {
+                rCount++;
+            }
+            if (lCount == rCount) {
+                answer++;
+                lCount = 0;
+                rCount = 0;
+            }
+        }
+        return answer;
     }
 }
