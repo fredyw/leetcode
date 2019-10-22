aca74264e45acd10346012230ee0a9a536e22efb
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-10-21 22:12:09 -0500

    Update problem 1186

--- src/main/java/leetcode/Problem1186.java
+++ src/main/java/leetcode/Problem1186.java
@@ -5,21 +5,27 @@ package leetcode;
  */
 public class Problem1186 {
     public int maximumSum(int[] arr) {
-        int answer = Integer.MIN_VALUE;
-        for (int i = 0; i < arr.length; i++) {
-            boolean negative = false;
-            int sum = arr[i];
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[j] < 0) {
-                    if (negative) {
-                        break;
-                    }
-                    negative = true;
-                } else {
-                    sum += arr[j];
-                }
-            }
-            answer = Math.max(answer, sum);
+        int answer = arr[0];
+        int maxSoFar = arr[0];
+        int[] forward = new int[arr.length];
+        forward[0] = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            maxSoFar = Math.max(arr[i], maxSoFar + arr[i]);
+            forward[i] = maxSoFar;
+            answer = Math.max(answer, maxSoFar);
+        }
+        maxSoFar = arr[arr.length - 1];
+        int[] backward = new int[arr.length];
+        backward[backward.length - 1] = arr[arr.length - 1];
+        for (int i = arr.length - 2; i >= 0; i--) {
+            maxSoFar = Math.max(arr[i], maxSoFar + arr[i]);
+            backward[i] = maxSoFar;
+            answer = Math.max(answer, maxSoFar);
+        }
+//        System.out.println(Arrays.toString(forward));
+//        System.out.println(Arrays.toString(backward));
+        for (int i = 1; i < arr.length - 1; i++) {
+            answer = Math.max(answer, forward[i - 1] + backward[i + 1]);
         }
         return answer;
     }
@@ -32,5 +38,6 @@ public class Problem1186 {
         System.out.println(prob.maximumSum(new int[]{1, -2, -1, 0, 3})); // 3
         System.out.println(prob.maximumSum(new int[]{1, 2, 3})); // 6
         System.out.println(prob.maximumSum(new int[]{-4, 1, 2, -3, 3, -4, 2})); // 6
+        System.out.println(prob.maximumSum(new int[]{-2, -3, 4, -1, -2, 1, 5, -3})); // 9
     }
 }
