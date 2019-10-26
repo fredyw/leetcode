a2bfe43c1b40464830d4d6b975c585721c1a96b8
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-10-26 09:07:51 -0500

    Update problem 1177

--- src/main/java/leetcode/Problem1177.java
+++ src/main/java/leetcode/Problem1177.java
@@ -1,5 +1,6 @@
 package leetcode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -7,14 +8,30 @@ import java.util.List;
  */
 public class Problem1177 {
     public List<Boolean> canMakePaliQueries(String s, int[][] queries) {
-        // TODO
-        return null;
+        List<Boolean> answer = new ArrayList<>();
+        for (int[] query : queries) {
+            String sub = s.substring(query[0], query[1] + 1);
+            int count = 0;
+            for (int i = 0, j = sub.length() - 1; i < j; i++, j--) {
+                if (sub.charAt(i) != sub.charAt(j)) {
+                    count++;
+                }
+                if (count > query[2]) {
+                    break;
+                }
+            }
+            answer.add(count <= query[2]);
+        }
+        return answer;
     }
 
     public static void main(String[] args) {
         Problem1177 prob = new Problem1177();
-        System.out.println(prob.canMakePaliQueries("abcda", new int[][]{
-            {3,3,0},{1,2,0},{0,3,1},{0,3,2},{0,4,1}
-        })); // [true,false,false,true,true]
+//        System.out.println(prob.canMakePaliQueries("abcda", new int[][]{
+//            {3,3,0},{1,2,0},{0,3,1},{0,3,2},{0,4,1}
+//        })); // [true,false,false,true,true]
+        System.out.println(prob.canMakePaliQueries("hunu", new int[][]{
+            {1,1,1},{2,3,0},{3,3,1},{0,3,2},{1,3,3},{2,3,1},{3,3,1},{0,3,0},{1,1,1},{2,3,0},{3,3,1},{0,3,1},{1,1,1}
+        })); // [true,false,true,true,true,true,true,false,true,false,true,true,true]
     }
 }
