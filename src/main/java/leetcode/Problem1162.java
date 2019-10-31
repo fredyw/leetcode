559d9fd573a418118bdd6c20683d6fd200567d4a
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-10-30 23:30:27 -0500

    Update problem 1162

--- src/main/java/leetcode/Problem1162.java
+++ src/main/java/leetcode/Problem1162.java
@@ -1,4 +1,25 @@
 package leetcode;
 
+/**
+ * https://leetcode.com/problems/as-far-from-land-as-possible/
+ */
 public class Problem1162 {
+    public int maxDistance(int[][] grid) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1162 prob = new Problem1162();
+        System.out.println(prob.maxDistance(new int[][]{
+            {1,0,1},
+            {0,0,0},
+            {1,0,1}
+        })); //
+        System.out.println(prob.maxDistance(new int[][]{
+            {1,0,0},
+            {0,0,0},
+            {0,0,0}
+        })); // 4
+    }
 }
