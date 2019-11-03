3e5a423025bdedb4b80e1a3d8491f31bcda593df
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-11-02 19:19:29 -0500

    Skeleton for problem 1243

---
+++ src/main/java/leetcode/Problem1243.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/array-transformation/
+ */
+public class Problem1243 {
+    public List<Integer> transformArray(int[] arr) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem1243 prob = new Problem1243();
+        System.out.println(prob.transformArray(new int[]{6,2,3,4})); // [6,3,3,4]
+        System.out.println(prob.transformArray(new int[]{1,6,3,4,3,5})); // [1,4,4,4,4,5]
+    }
+}
