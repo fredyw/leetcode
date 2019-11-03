040f39a252fef3345a0a55484ed3c8cb2a0f5b68
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-11-02 19:41:30 -0500

    Update problem 1243

--- src/main/java/leetcode/Problem1243.java
+++ src/main/java/leetcode/Problem1243.java
@@ -1,5 +1,6 @@
 package leetcode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -7,8 +8,26 @@ import java.util.List;
  */
 public class Problem1243 {
     public List<Integer> transformArray(int[] arr) {
-        // TODO
-        return null;
+        while (true) {
+            boolean found = false;
+            for (int i = 1; i < arr.length - 2; i++) {
+                if (arr[i - 1] < arr[i] && arr[i] > arr[i + 1]) {
+                    found = true;
+                    arr[i]--;
+                } else if (arr[i - 1] > arr[i] && arr[i] < arr[i + 1]) {
+                    found = true;
+                    arr[i]++;
+                }
+            }
+            if (!found) {
+                break;
+            }
+        }
+        List<Integer> answer = new ArrayList<>();
+        for (int a : arr) {
+            answer.add(a);
+        }
+        return answer;
     }
 
     public static void main(String[] args) {
