c6b67d2b43dffd1f49b6266c35779928a7852929
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-11-04 23:35:01 -0600

    Solve problem 1249

--- src/main/java/leetcode/Problem1249.java
+++ src/main/java/leetcode/Problem1249.java
@@ -1,19 +1,36 @@
 package leetcode;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
+
 /**
  * https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/
  */
 public class Problem1249 {
     public String minRemoveToMakeValid(String s) {
-        // TODO
-        return null;
-    }
-
-    public static void main(String[] args) {
-        Problem1249 prob = new Problem1249();
-        System.out.println(prob.minRemoveToMakeValid("lee(t(c)o)de)")); //  "lee(t(c)o)de"
-        System.out.println(prob.minRemoveToMakeValid("a)b(c)d")); //  "ab(c)d"
-        System.out.println(prob.minRemoveToMakeValid("))((")); //  "ab(c)d"
-        System.out.println(prob.minRemoveToMakeValid("(a(b(c)d)")); //  "a(b(c)d)"
+        Stack<Integer> stack = new Stack<>();
+        Set<Integer> removed = new HashSet<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else if (s.charAt(i) == ')') {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                } else {
+                    removed.add(i);
+                }
+            }
+        }
+        for (int index : stack) {
+            removed.add(index);
+        }
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (!removed.contains(i)) {
+                answer.append(s.charAt(i));
+            }
+        }
+        return answer.toString();
     }
 }
