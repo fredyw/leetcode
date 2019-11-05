8fe1535f399f5d0e55f67780b1100a3980be1763
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-11-04 23:19:20 -0600

    Update problem 1162

--- src/main/java/leetcode/Problem1162.java
+++ src/main/java/leetcode/Problem1162.java
@@ -33,23 +33,4 @@ public class Problem1162 {
     private static int getDistance(int[] x, int[] y) {
         return Math.abs(x[0] - y[0]) + Math.abs(x[1] - y[1]);
     }
-
-    public static void main(String[] args) {
-        Problem1162 prob = new Problem1162();
-        System.out.println(prob.maxDistance(new int[][]{
-            {1,0,1},
-            {0,0,0},
-            {1,0,1}
-        })); // 2
-        System.out.println(prob.maxDistance(new int[][]{
-            {1,0,0},
-            {0,0,0},
-            {0,0,0}
-        })); // 4
-        System.out.println(prob.maxDistance(new int[][]{
-            {1,0,1},
-            {0,0,0},
-            {0,0,0}
-        })); // 3
-    }
 }
