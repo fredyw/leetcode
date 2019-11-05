3c1c874c9202a3ec07b07398e0ff3ea786d43e0b
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-11-04 23:12:50 -0600

    Solve problem 1162

--- src/main/java/leetcode/Problem1162.java
+++ src/main/java/leetcode/Problem1162.java
@@ -1,12 +1,37 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * https://leetcode.com/problems/as-far-from-land-as-possible/
  */
 public class Problem1162 {
     public int maxDistance(int[][] grid) {
-        // TODO
-        return 0;
+        List<int[]> waters = new ArrayList<>();
+        List<int[]> lands = new ArrayList<>();
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 0) {
+                    waters.add(new int[]{i, j});
+                } else {
+                    lands.add(new int[]{i, j});
+                }
+            }
+        }
+        int answer = Integer.MIN_VALUE;
+        for (int[] water : waters) {
+            int min = Integer.MAX_VALUE;
+            for (int[] land : lands) {
+                min = Math.min(min, getDistance(water, land));
+            }
+            answer = Math.max(answer, min == Integer.MAX_VALUE ? -1 : min);
+        }
+        return answer == Integer.MIN_VALUE ? -1 : answer;
+    }
+
+    private static int getDistance(int[] x, int[] y) {
+        return Math.abs(x[0] - y[0]) + Math.abs(x[1] - y[1]);
     }
 
     public static void main(String[] args) {
@@ -15,7 +40,7 @@ public class Problem1162 {
             {1,0,1},
             {0,0,0},
             {1,0,1}
-        })); //
+        })); // 2
         System.out.println(prob.maxDistance(new int[][]{
             {1,0,0},
             {0,0,0},
