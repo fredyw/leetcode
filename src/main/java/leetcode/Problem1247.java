1c7cc578d33ee9b196da56a29a23c4a3092df6a4
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-11-05 22:59:01 -0600

    Update problem 1247

--- src/main/java/leetcode/Problem1247.java
+++ src/main/java/leetcode/Problem1247.java
@@ -8,14 +8,18 @@ import java.util.Set;
  */
 public class Problem1247 {
     public int minimumSwap(String s1, String s2) {
-        Set<Integer> xSet = new HashSet<>();
-        Set<Integer> ySet = new HashSet<>();
+        Set<Integer> x1Set = new HashSet<>();
+        Set<Integer> y1Set = new HashSet<>();
+        Set<Integer> x2Set = new HashSet<>();
+        Set<Integer> y2Set = new HashSet<>();
         for (int i = 0; i < s1.length(); i++) {
             if (s1.charAt(i) != s2.charAt(i)) {
                 if (s1.charAt(i) == 'x') {
-                    xSet.add(i);
+                    x1Set.add(i);
+                    y2Set.add(i);
                 } else {
-                    ySet.add(i);
+                    y1Set.add(i);
+                    x2Set.add(i);
                 }
             }
         }
@@ -23,27 +27,49 @@ public class Problem1247 {
         for (int i = 0; i < s1.length(); i++) {
             if (s1.charAt(i) != s2.charAt(i)) {
                 if (s1.charAt(i) == 'x') {
-                    if (xSet.size() > 1) {
-                        xSet.remove(i);
-                        ySet.remove(i);
-                        int index = xSet.iterator().next();
-                        xSet.remove(index);
-                        ySet.remove(index);
-                        answer++;
+                    if (x1Set.remove(i) && y2Set.remove(i)) {
+                        if (!x1Set.isEmpty()) {
+                            int index = x1Set.iterator().next();
+                            x1Set.remove(index);
+                            x2Set.remove(index);
+                            y1Set.remove(index);
+                            y2Set.remove(index);
+                            answer++;
+                        } else if (!x2Set.isEmpty()) {
+                            int index = x2Set.iterator().next();
+                            x1Set.remove(index);
+                            x2Set.remove(index);
+                            y1Set.remove(index);
+                            y2Set.remove(index);
+                            answer += 2;
+                        } else {
+                            return -1;
+                        }
                     }
                 } else {
-                    if (ySet.size() > 1) {
-                        xSet.remove(i);
-                        ySet.remove(i);
-                        int index = ySet.iterator().next();
-                        xSet.remove(index);
-                        ySet.remove(index);
-                        answer++;
+                    if (y1Set.remove(i) && x2Set.remove(i)) {
+                        if (!y1Set.isEmpty()) {
+                            int index = y1Set.iterator().next();
+                            x1Set.remove(index);
+                            x2Set.remove(index);
+                            y1Set.remove(index);
+                            y2Set.remove(index);
+                            answer++;
+                        } else if (!y2Set.isEmpty()) {
+                            int index = y2Set.iterator().next();
+                            x1Set.remove(index);
+                            x2Set.remove(index);
+                            y1Set.remove(index);
+                            y2Set.remove(index);
+                            answer += 2;
+                        } else {
+                            return -1;
+                        }
                     }
                 }
             }
         }
-        return xSet.isEmpty() && ySet.isEmpty() ? answer : -1;
+        return answer;
     }
 
     public static void main(String[] args) {
@@ -51,6 +77,7 @@ public class Problem1247 {
         System.out.println(prob.minimumSwap("xx", "yy")); // 1
         System.out.println(prob.minimumSwap("xy", "yx")); // 2
         System.out.println(prob.minimumSwap("xx", "xy")); // -1
+        System.out.println(prob.minimumSwap("xx", "xx")); // 0
         System.out.println(prob.minimumSwap("xxyyxyxyxx", "xyyxyxxxyx")); // 4
     }
 }
