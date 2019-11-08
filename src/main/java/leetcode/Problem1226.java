ccc94369050aa97c0249801fa62e6c8f6823d15f
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-11-08 11:03:59 -0600

    Update problem 1226

--- src/main/java/leetcode/Problem1226.java
+++ src/main/java/leetcode/Problem1226.java
@@ -1,4 +1,21 @@
 package leetcode;
 
+/**
+ * https://leetcode.com/problems/the-dining-philosophers/
+ */
 public class Problem1226 {
+    private static class DiningPhilosophers {
+        public DiningPhilosophers() {
+        }
+
+        // call the run() method of any runnable to execute its code
+        public void wantsToEat(int philosopher,
+                               Runnable pickLeftFork,
+                               Runnable pickRightFork,
+                               Runnable eat,
+                               Runnable putLeftFork,
+                               Runnable putRightFork) throws InterruptedException {
+
+        }
+    }
 }
