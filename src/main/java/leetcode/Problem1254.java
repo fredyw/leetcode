169d016c793f50875cad5fd12b01a1d68de94bde
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-11-13 00:09:12 -0600

    Skeleton for problem 1254

---
+++ src/main/java/leetcode/Problem1254.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/number-of-closed-islands/
+ */
+public class Problem1254 {
+    public int closedIsland(int[][] grid) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1254 prob = new Problem1254();
+        System.out.println(prob.closedIsland(new int[][]{
+            {1,1,1,1,1,1,1,0},
+            {1,0,0,0,0,1,1,0},
+            {1,0,1,0,1,1,1,0},
+            {1,0,0,0,0,1,0,1},
+            {1,1,1,1,1,1,1,0}
+        })); // 2
+        System.out.println(prob.closedIsland(new int[][]{
+            {0,0,1,0,0},
+            {0,1,0,1,0},
+            {0,1,1,1,0}
+        })); // 1
+        System.out.println(prob.closedIsland(new int[][]{
+            {1,1,1,1,1,1,1},
+            {1,0,0,0,0,0,1},
+            {1,0,1,1,1,0,1},
+            {1,0,1,0,1,0,1},
+            {1,0,1,1,1,0,1},
+            {1,0,0,0,0,0,1},
+            {1,1,1,1,1,1,1}
+        })); // 2
+    }
+}
