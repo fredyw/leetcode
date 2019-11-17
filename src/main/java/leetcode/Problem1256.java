a99afaeddf6154bec1b361701a05b47dc9888488
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-11-16 18:57:02 -0600

    Skeleton for problem 1256

---
+++ src/main/java/leetcode/Problem1256.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/encode-number/
+ */
+public class Problem1256 {
+    public String encode(int num) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem1256 prob = new Problem1256();
+        System.out.println(prob.encode(23)); // "1000"
+        System.out.println(prob.encode(107)); // "101100"
+    }
+}
