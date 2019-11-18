c431780f1e0d3dcc7cc77c293a9a1cfbed8c8337
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-11-17 23:08:55 -0600

    Skeleton for problem 1260

---
+++ src/main/java/leetcode/Problem1260.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/shift-2d-grid/
+ */
+public class Problem1260 {
+    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem1260 prob = new Problem1260();
+    }
+}
