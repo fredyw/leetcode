c687e05e8b6bc785d03bb724d7d1c494e44f3944
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-11-18 13:25:48 -0600

    Update problem 1260

--- src/main/java/leetcode/Problem1260.java
+++ src/main/java/leetcode/Problem1260.java
@@ -13,5 +13,21 @@ public class Problem1260 {
 
     public static void main(String[] args) {
         Problem1260 prob = new Problem1260();
+        System.out.println(prob.shiftGrid(new int[][]{
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        }, 1)); // [[9,1,2],[3,4,5],[6,7,8]]
+        System.out.println(prob.shiftGrid(new int[][]{
+            {3, 8, 1, 9},
+            {19, 7, 2, 5},
+            {4, 6, 11, 10},
+            {12, 0, 21, 13}
+        }, 4)); // [[12,0,21,13],[3,8,1,9],[19,7,2,5],[4,6,11,10]]
+        System.out.println(prob.shiftGrid(new int[][]{
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        }, 9)); // [[1,2,3],[4,5,6],[7,8,9]]
     }
 }
