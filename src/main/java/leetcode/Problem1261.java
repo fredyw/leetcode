f980667f328fabdf086f8e8d39a4b2d0752300f0
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-11-19 00:04:01 -0600

    Skeleton for problem 1261

---
+++ src/main/java/leetcode/Problem1261.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/find-elements-in-a-contaminated-binary-tree/
+ */
+public class Problem1261 {
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    private static class FindElements {
+        public FindElements(TreeNode root) {
+        }
+
+        public boolean find(int target) {
+            // TODO
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}
