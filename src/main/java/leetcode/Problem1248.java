2c9792a393c5884a35210bd96bb2e0c7c172a3e1
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-11-19 22:22:11 -0600

    Skeleton for problem 1248

---
+++ src/main/java/leetcode/Problem1248.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/count-number-of-nice-subarrays/
+ */
+public class Problem1248 {
+    public int numberOfSubarrays(int[] nums, int k) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1248 prob = new Problem1248();
+        System.out.println(prob.numberOfSubarrays(new int[]{1,1,2,1,1}, 3)); // 2
+        System.out.println(prob.numberOfSubarrays(new int[]{2,4,6}, 1)); // 0
+        System.out.println(prob.numberOfSubarrays(new int[]{2,2,2,1,2,2,1,2,2,2}, 2)); // 16
+    }
+}
