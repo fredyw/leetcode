6938519f6e738fbbaa048b20b5233da37ee0a71b
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-11-24 02:09:31 -0600

    Skeleton for problem 1266

---
+++ src/main/java/leetcode/Problem1266.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/minimum-time-visiting-all-points/
+ */
+public class Problem1266 {
+    public int minTimeToVisitAllPoints(int[][] points) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1266 prob = new Problem1266();
+        System.out.println(prob.minTimeToVisitAllPoints(new int[][]{{1,1},{3,4},{-1,0}})); // 7
+        System.out.println(prob.minTimeToVisitAllPoints(new int[][]{{3,2},{-2,2}})); // 5
+    }
+}
