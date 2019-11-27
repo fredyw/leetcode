09f87f70b32f84de19edb8728f1c3d1091f90107
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-11-26 23:37:45 -0600

    Update problem 1268

--- src/main/java/leetcode/Problem1268.java
+++ src/main/java/leetcode/Problem1268.java
@@ -7,8 +7,42 @@ import java.util.List;
  */
 public class Problem1268 {
     public List<List<String>> suggestedProducts(String[] products, String searchWord) {
-        // TODO
+        Node root = new Node(' ');
+        for (String product : products) {
+            root = build(root, product, 0);
+        }
         return null;
+    }
+
+    private static class Node {
+        private final char c;
+        private final Node[] children = new Node[26];
+        private boolean isWord;
+
+        private Node(char c) {
+            this.c = c;
+        }
+
+        @Override
+        public String toString() {
+            return "" + c;
+        }
+    }
+
+    private static Node build(Node node, String product, int index) {
+        if (index == product.length()) {
+            return node;
+        }
+        char c = product.charAt(index);
+        Node child = node.children[c - 'a'];
+        if (child == null) {
+            child = new Node(c);
+        }
+        node.children[c - 'a'] = build(child, product, index + 1);
+        if (index == product.length() - 1) {
+            node.children[c - 'a'].isWord = true;
+        }
+        return node;
     }
 
     public static void main(String[] args) {
@@ -20,11 +54,11 @@ public class Problem1268 {
         //  ["mouse","mousepad"]]
         System.out.println(prob.suggestedProducts(
             new String[]{"mobile","mouse","moneypot","monitor","mousepad"}, "mouse"));
-        System.out.println(prob.suggestedProducts(
-            new String[]{"havana"}, "havana")); // [["havana"],["havana"],["havana"],["havana"],["havana"],["havana"]]
-        System.out.println(prob.suggestedProducts(
-            new String[]{"bags","baggage","banner","box","cloths"}, "bags")); // [["baggage","bags","banner"],["baggage","bags","banner"],["baggage","bags"],["bags"]]
-        System.out.println(prob.suggestedProducts(
-            new String[]{"havana"}, "tatiana")); // [[],[],[],[],[],[],[]]
+//        System.out.println(prob.suggestedProducts(
+//            new String[]{"havana"}, "havana")); // [["havana"],["havana"],["havana"],["havana"],["havana"],["havana"]]
+//        System.out.println(prob.suggestedProducts(
+//            new String[]{"bags","baggage","banner","box","cloths"}, "bags")); // [["baggage","bags","banner"],["baggage","bags","banner"],["baggage","bags"],["bags"]]
+//        System.out.println(prob.suggestedProducts(
+//            new String[]{"havana"}, "tatiana")); // [[],[],[],[],[],[],[]]
     }
 }
