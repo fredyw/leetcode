a95604835c8d025b834f6cb2d117b5a1abc60226
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-11-30 00:25:49 -0600

    Skeleton for problem 1262

---
+++ src/main/java/leetcode/Problem1262.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/greatest-sum-divisible-by-three/
+ */
+public class Problem1262 {
+    public int maxSumDivThree(int[] nums) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1262 prob = new Problem1262();
+        System.out.println(prob.maxSumDivThree(new int[]{3,6,5,1,8})); // 18
+        System.out.println(prob.maxSumDivThree(new int[]{4})); // 0
+        System.out.println(prob.maxSumDivThree(new int[]{1,2,3,4,4})); // 12
+    }
+}
