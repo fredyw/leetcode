c8e64d6ab1f58cfaaa05c6e8d5f85f5d665ae388
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-12-01 23:19:12 -0600

    Solve problem 1275

--- src/main/java/leetcode/Problem1275.java
+++ src/main/java/leetcode/Problem1275.java
@@ -5,23 +5,50 @@ package leetcode;
  */
 public class Problem1275 {
     public String tictactoe(int[][] moves) {
-        // TODO
-        return null;
+        char[][] board = new char[3][3];
+        boolean x = true;
+        for (int[] move : moves) {
+            board[move[0]][move[1]] = x ? 'X' : 'O';
+            x = !x;
+        }
+        if (isWinner(board, 'X')) {
+            return "A";
+        } else if (isWinner(board, 'O')) {
+            return "B";
+        } else if (moves.length == 9) {
+            return "Draw";
+        }
+        return "Pending";
     }
 
-    public static void main(String[] args) {
-        Problem1275 prob = new Problem1275();
-        System.out.println(prob.tictactoe(new int[][]{
-            {0,0},{2,0},{1,1},{2,1},{2,2}
-        })); // "A"
-        System.out.println(prob.tictactoe(new int[][]{
-            {0,0},{1,1},{0,1},{0,2},{1,0},{2,0}
-        })); // "B"
-        System.out.println(prob.tictactoe(new int[][]{
-            {0,0},{1,1},{2,0},{1,0},{1,2},{2,1},{0,1},{0,2},{2,2}
-        })); // "Draw"
-        System.out.println(prob.tictactoe(new int[][]{
-            {0,0},{1,1}
-        })); // "Pending"
+    private static boolean isWinner(char[][] board, char c) {
+        // horizontal and vertical
+        for (int i = 0; i < 3; i++) {
+            int rowCount = 0;
+            int colCount = 0;
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == c) {
+                    colCount++;
+                }
+                if (board[j][i] == c) {
+                    rowCount++;
+                }
+            }
+            if (rowCount == 3 || colCount == 3) {
+                return true;
+            }
+        }
+        // diagonal
+        int leftDiagonal = 0;
+        int rightDiagonal = 0;
+        for (int i = 0, j = 2; i < 3; i++, j--) {
+            if (board[i][i] == c) {
+                leftDiagonal++;
+            }
+            if (board[i][j] == c) {
+                rightDiagonal++;
+            }
+        }
+        return leftDiagonal == 3 || rightDiagonal == 3;
     }
 }
