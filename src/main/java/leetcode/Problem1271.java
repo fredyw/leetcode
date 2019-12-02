8f6c8e7bd7df02715e32aa47b5698afb1dc9fdf2
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-12-01 23:21:18 -0600

    Skeleton for problem 1271

---
+++ src/main/java/leetcode/Problem1271.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/hexspeak/
+ */
+public class Problem1271 {
+    public String toHexspeak(String num) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem1271 prob = new Problem1271();
+        System.out.println(prob.toHexspeak("257")); // "IOI"
+        System.out.println(prob.toHexspeak("3")); // "ERROR"
+    }
+}
