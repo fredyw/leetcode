7271b62046ff31a252d82ea2e4329809d4f4b546
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-12-08 22:41:13 -0600

    Skeleton for problem 1281

---
+++ src/main/java/leetcode/Problem1281.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
+ */
+public class Problem1281 {
+    public int subtractProductAndSum(int n) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1281 prob = new Problem1281();
+        System.out.println(prob.subtractProductAndSum(234)); // 15
+        System.out.println(prob.subtractProductAndSum(4421)); // 21
+    }
+}
