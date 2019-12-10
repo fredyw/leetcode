b80738ea188b085ecca6025cb6fd4b7bbac82594
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-12-10 00:28:47 -0600

    Update problem 1191

--- src/main/java/leetcode/Problem1191.java
+++ src/main/java/leetcode/Problem1191.java
@@ -5,8 +5,13 @@ package leetcode;
  */
 public class Problem1191 {
     public int kConcatenationMaxSum(int[] arr, int k) {
-        // TODO
-        return 0;
+        int answer = 0;
+        int maxSoFar = 0;
+        for (int i = 0; i < arr.length; i++) {
+            maxSoFar = Math.max(maxSoFar, maxSoFar + arr[i]);
+            answer = Math.max(answer, maxSoFar);
+        }
+        return answer;
     }
 
     public static void main(String[] args) {
