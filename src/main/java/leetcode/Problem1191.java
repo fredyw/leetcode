53b92c34217e40e039a8a54416af57c80f3bb503
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-12-13 11:39:27 -0600

    Update problem 1191

--- src/main/java/leetcode/Problem1191.java
+++ src/main/java/leetcode/Problem1191.java
@@ -5,11 +5,15 @@ package leetcode;
  */
 public class Problem1191 {
     public int kConcatenationMaxSum(int[] arr, int k) {
+        int sum = 0;
+        for (int a : arr) {
+            sum += a;
+        }
         long answer = 0;
         long maxSoFar = 0;
         for (int j = 0; j < k; j++) {
             for (int i = 0; i < arr.length; i++) {
-                maxSoFar = Math.max(arr[i], (maxSoFar + arr[i]) % 1_000_000_007) ;
+                maxSoFar = Math.max(arr[i], (maxSoFar + arr[i]) % 1_000_000_007);
                 answer = Math.max(answer, maxSoFar);
             }
         }
@@ -20,7 +24,9 @@ public class Problem1191 {
         Problem1191 prob = new Problem1191();
 //        System.out.println(prob.kConcatenationMaxSum(new int[]{1,2}, 3)); // 9
 //        System.out.println(prob.kConcatenationMaxSum(new int[]{1,-2,1}, 5)); // 2
+//        System.out.println(prob.kConcatenationMaxSum(new int[]{1,-2,4}, 2)); // 7
         System.out.println(prob.kConcatenationMaxSum(new int[]{1,3-2,5,8}, 2)); // 30
+        System.out.println(prob.kConcatenationMaxSum(new int[]{1,3-2,5,8}, 3)); // 45
 //        System.out.println(prob.kConcatenationMaxSum(new int[]{-1,-2}, 7)); // 0
 //        System.out.println(prob.kConcatenationMaxSum(new int[]{1,2,-3}, 3)); // 3
 //        System.out.println(prob.kConcatenationMaxSum(new int[]{1,2,-3,4}, 3)); // 12
