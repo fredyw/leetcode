5b4d5d8a27ea3865b40332251a7739796ff242a5
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-12-13 12:07:57 -0600

    Update problem 1191

--- src/main/java/leetcode/Problem1191.java
+++ src/main/java/leetcode/Problem1191.java
@@ -5,19 +5,25 @@ package leetcode;
  */
 public class Problem1191 {
     public int kConcatenationMaxSum(int[] arr, int k) {
-        int sum = 0;
+        long totalSum = 0;
+        int prefixSum = 0;
         for (int a : arr) {
-            sum += a;
+            totalSum += a;
         }
-        long answer = 0;
+        // Kadene's algorithm.
+        long max = 0;
         long maxSoFar = 0;
-        for (int j = 0; j < k; j++) {
-            for (int i = 0; i < arr.length; i++) {
-                maxSoFar = Math.max(arr[i], (maxSoFar + arr[i]) % 1_000_000_007);
-                answer = Math.max(answer, maxSoFar);
-            }
+        for (int i = 0; i < arr.length; i++) {
+            maxSoFar = Math.max(arr[i], (maxSoFar + arr[i]));
+            max = Math.max(max, maxSoFar);
         }
-        return (int) answer;
+        long answer = max;
+        if (totalSum > 0) {
+            answer += totalSum * (k - 1);
+        } else {
+            answer += prefixSum;
+        }
+        return (int) answer % 1_000_000_007;
     }
 
     public static void main(String[] args) {
@@ -25,8 +31,10 @@ public class Problem1191 {
 //        System.out.println(prob.kConcatenationMaxSum(new int[]{1,2}, 3)); // 9
 //        System.out.println(prob.kConcatenationMaxSum(new int[]{1,-2,1}, 5)); // 2
 //        System.out.println(prob.kConcatenationMaxSum(new int[]{1,-2,4}, 2)); // 7
-        System.out.println(prob.kConcatenationMaxSum(new int[]{1,3-2,5,8}, 2)); // 30
-        System.out.println(prob.kConcatenationMaxSum(new int[]{1,3-2,5,8}, 3)); // 45
+//        System.out.println(prob.kConcatenationMaxSum(new int[]{5,-10,6}, 4)); // 13
+        System.out.println(prob.kConcatenationMaxSum(new int[]{5,-10,4}, 3)); // 9
+//        System.out.println(prob.kConcatenationMaxSum(new int[]{1,3-2,5,8}, 2)); // 30
+//        System.out.println(prob.kConcatenationMaxSum(new int[]{1,3-2,5,8}, 3)); // 45
 //        System.out.println(prob.kConcatenationMaxSum(new int[]{-1,-2}, 7)); // 0
 //        System.out.println(prob.kConcatenationMaxSum(new int[]{1,2,-3}, 3)); // 3
 //        System.out.println(prob.kConcatenationMaxSum(new int[]{1,2,-3,4}, 3)); // 12
