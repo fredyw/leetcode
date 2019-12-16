f1c16c16f50c7b94bd18545fb884cac1bfaf1fb3
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-12-15 22:19:52 -0600

    Solve problem 1287

--- src/main/java/leetcode/Problem1287.java
+++ src/main/java/leetcode/Problem1287.java
@@ -5,12 +5,19 @@ package leetcode;
  */
 public class Problem1287 {
     public int findSpecialInteger(int[] arr) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem1287 prob = new Problem1287();
-        System.out.println(prob.findSpecialInteger(new int[]{1,2,2,6,6,6,6,7,10})); // 6
+        int n = (int) Math.ceil(arr.length * 0.25);
+        int answer = 0;
+        int count = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] != arr[i + 1]) {
+                count = 0;
+            } else {
+                count++;
+            }
+            if (count >= n) {
+                return arr[i];
+            }
+        }
+        return arr[arr.length - 1];
     }
 }
