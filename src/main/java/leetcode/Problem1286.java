10d31e4eff066f682d9a8c6f3b875c4efacac9d7
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-12-16 13:36:46 -0600

    Update problem 1286

--- src/main/java/leetcode/Problem1286.java
+++ src/main/java/leetcode/Problem1286.java
@@ -20,13 +20,26 @@ public class Problem1286 {
         }
     }
 
+    private static void nextCombination(String str, int length, int index, String accu) {
+        if (accu.length() == length) {
+            System.out.println(accu);
+            return;
+        }
+        for (int i = index; i < str.length(); i++) {
+            char c = str.charAt(i);
+            nextCombination(str, length, i + 1, accu + c);
+        }
+    }
+
     public static void main(String[] args) {
-        CombinationIterator iter = new CombinationIterator("abc", 2);
-        System.out.println(iter.next()); // returns "ab"
-        System.out.println(iter.hasNext()); // returns true
-        System.out.println(iter.next()); // returns "ac"
-        System.out.println(iter.hasNext()); // returns true
-        System.out.println(iter.next()); // returns "bc"
-        System.out.println(iter.hasNext()); // returns false
+//        CombinationIterator iter = new CombinationIterator("abc", 2);
+//        System.out.println(iter.next()); // returns "ab"
+//        System.out.println(iter.hasNext()); // returns true
+//        System.out.println(iter.next()); // returns "ac"
+//        System.out.println(iter.hasNext()); // returns true
+//        System.out.println(iter.next()); // returns "bc"
+//        System.out.println(iter.hasNext()); // returns false
+
+        nextCombination("12345", 3, 0, "");
     }
 }
