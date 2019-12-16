4a0e935224994c6830718d68d70e182082bfb462
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-12-15 22:09:22 -0600

    Skeleton for problem 1287

---
+++ src/main/java/leetcode/Problem1287.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/element-appearing-more-than-25-in-sorted-array/
+ */
+public class Problem1287 {
+    public int findSpecialInteger(int[] arr) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1287 prob = new Problem1287();
+        System.out.println(prob.findSpecialInteger(new int[]{1,2,2,6,6,6,6,7,10})); // 6
+    }
+}
