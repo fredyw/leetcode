e0b035b27ade41660f0c0c5b007d6fff2c028982
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-12-16 22:40:29 -0600

    Solve problem 1286

--- src/main/java/leetcode/Problem1286.java
+++ src/main/java/leetcode/Problem1286.java
@@ -1,45 +1,38 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * https://leetcode.com/problems/iterator-for-combination/
  */
 public class Problem1286 {
     private static class CombinationIterator {
+        private final List<String> combinations = new ArrayList<>();
+        private int index;
+
         public CombinationIterator(String characters, int combinationLength) {
-            // TODO
+            generateCombinations(characters, combinationLength, 0, "", combinations);
         }
 
         public String next() {
-            // TODO
-            return null;
+            return combinations.get(index++);
         }
 
         public boolean hasNext() {
-            // TODO
-            return false;
+            return index < combinations.size();
         }
-    }
 
-    private static void nextCombination(String str, int length, int index, String accu) {
-        if (accu.length() == length) {
-            System.out.println(accu);
-            return;
+        private static void generateCombinations(String str, int length, int index, String accu,
+                                                 List<String> combinations) {
+            if (accu.length() == length) {
+                combinations.add(accu);
+                return;
+            }
+            for (int i = index; i < str.length(); i++) {
+                char c = str.charAt(i);
+                generateCombinations(str, length, i + 1, accu + c, combinations);
+            }
         }
-        for (int i = index; i < str.length(); i++) {
-            char c = str.charAt(i);
-            nextCombination(str, length, i + 1, accu + c);
-        }
-    }
-
-    public static void main(String[] args) {
-//        CombinationIterator iter = new CombinationIterator("abc", 2);
-//        System.out.println(iter.next()); // returns "ab"
-//        System.out.println(iter.hasNext()); // returns true
-//        System.out.println(iter.next()); // returns "ac"
-//        System.out.println(iter.hasNext()); // returns true
-//        System.out.println(iter.next()); // returns "bc"
-//        System.out.println(iter.hasNext()); // returns false
-
-        nextCombination("12345", 3, 0, "");
     }
 }
