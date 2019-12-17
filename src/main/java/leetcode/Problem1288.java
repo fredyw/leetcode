0f8a906b41350a68965c203d267eea0216dc0322
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-12-16 22:45:35 -0600

    Skeleton for problem 1288

---
+++ src/main/java/leetcode/Problem1288.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/remove-covered-intervals/
+ */
+public class Problem1288 {
+    public int removeCoveredIntervals(int[][] intervals) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1288 prob = new Problem1288();
+        System.out.println(prob.removeCoveredIntervals(new int[][]{{1,4},{3,6},{2,8}})); // 2
+    }
+}
