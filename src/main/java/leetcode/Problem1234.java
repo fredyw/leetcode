8789104da26c49628f2042b6846e988b3ece281a
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-12-23 11:22:46 -0600

    Update problem 1234

--- src/main/java/leetcode/Problem1234.java
+++ src/main/java/leetcode/Problem1234.java
@@ -1,76 +1,54 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * https://leetcode.com/problems/replace-the-substring-for-balanced-string/
  */
 public class Problem1234 {
     public int balancedString(String s) {
-        int q = 0;
-        int w = 0;
-        int e = 0;
-        int r = 0;
-        int n = s.length() / 4;
-        int startIndex = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == 'Q') {
-                q++;
-            } else if (s.charAt(i) == 'W') {
-                w++;
-            } else if (s.charAt(i) == 'E') {
-                e++;
-            } else {
-                r++;
-            }
-            startIndex = i;
-            if (q > n || w > n || e > n || r > n) {
-                if (q > n) {
-                    q--;
-                } else if (w > n) {
-                    w--;
-                } else if (e > n) {
-                    e--;
-                } else {
-                    r--;
+        Map<Character, Integer> count = new HashMap<>();
+        count.put('Q', 0);
+        count.put('W', 0);
+        count.put('E', 0);
+        count.put('R', 0);
+        char[] arr = s.toCharArray();
+        for (char c: arr) {
+            count.put(c, count.get(c) + 1);
+        }
+        int need = arr.length / 4;
+        int left = 0;
+        int right = 0;
+        int min = arr.length;
+        while (right <= arr.length) {
+            if (count.get('Q') > need || count.get('W') > need ||
+                count.get('E') > need || count.get('R') > need) {
+                if (right >= arr.length) {
+                    break;
                 }
+                count.put(arr[right], count.get(arr[right]) - 1);
+                right++;
+                continue;
+            }
+            min = Math.min(min, right-left);
+            if (min == 0) {
                 break;
             }
+            count.put(arr[left], count.get(arr[left]) + 1);
+            left++;
         }
-        int endIndex = 0;
-        for (int i = s.length() - 1; i >= startIndex; i--) {
-            if (s.charAt(i) == 'Q') {
-                q++;
-            } else if (s.charAt(i) == 'W') {
-                w++;
-            } else if (s.charAt(i) == 'E') {
-                e++;
-            } else {
-                r++;
-            }
-            endIndex = i;
-            if (q > n || w > n || e > n || r > n) {
-                if (q > n) {
-                    q--;
-                } else if (w > n) {
-                    w--;
-                } else if (e > n) {
-                    e--;
-                } else {
-                    r--;
-                }
-                break;
-            }
-        }
-        return endIndex - startIndex + 1;
+        return min;
     }
 
     public static void main(String[] args) throws Exception {
         Problem1234 prob = new Problem1234();
-        System.out.println(prob.balancedString("QWER")); // 0
-        System.out.println(prob.balancedString("QQWE")); // 1
-        System.out.println(prob.balancedString("QQQW")); // 2
-        System.out.println(prob.balancedString("QQQQ")); // 3
-        System.out.println(prob.balancedString("WQWRQQQW")); // 3
+//        System.out.println(prob.balancedString("QWER")); // 0
+//        System.out.println(prob.balancedString("QQWE")); // 1
+//        System.out.println(prob.balancedString("QQQW")); // 2
+//        System.out.println(prob.balancedString("QQQQ")); // 3
+//        System.out.println(prob.balancedString("WQWRQQQW")); // 3
         System.out.println(prob.balancedString("WWEQERQWQWWRWWERQWEQ")); // 4
-        System.out.println(prob.balancedString("QQQWEEER")); // 3
+//        System.out.println(prob.balancedString("QQQWEEER")); // 3
     }
 }
