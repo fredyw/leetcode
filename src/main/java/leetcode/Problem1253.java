6076e3a42bd6e40346644918a61472d77b265177
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-12-27 12:29:47 -0600

    Solve problem 1253

--- src/main/java/leetcode/Problem1253.java
+++ src/main/java/leetcode/Problem1253.java
@@ -12,19 +12,22 @@ public class Problem1253 {
         List<List<Integer>> answer = new ArrayList<>();
         answer.add(new ArrayList<>());
         answer.add(new ArrayList<>());
-        int twoCounts = 0;
+        int up = upper;
+        int lo = lower;
         for (int i = 0; i < colsum.length; i++) {
             if (colsum[i] == 2) {
                 answer.get(0).add(1);
                 answer.get(1).add(1);
-                twoCounts++;
+                up--;
+                lo--;
+                if (up < 0 || lo < 0) {
+                    return Collections.emptyList();
+                }
             } else {
                 answer.get(0).add(0);
                 answer.get(1).add(0);
             }
         }
-        int up = upper - twoCounts;
-        int lo = lower - twoCounts;
         for (int i = 0; i < colsum.length; i++) {
             if (colsum[i] == 1) {
                 if (up > 0) {
@@ -38,14 +41,6 @@ public class Problem1253 {
                 }
             }
         }
-        return answer;
-    }
-
-    public static void main(String[] args) {
-        Problem1253 prob = new Problem1253();
-//        System.out.println(prob.reconstructMatrix(2, 1, new int[]{1, 1, 1})); // [[1,1,0],[0,0,1]]
-//        System.out.println(prob.reconstructMatrix(2, 3, new int[]{2,2,1,1})); // []
-//        System.out.println(prob.reconstructMatrix(5, 5, new int[]{2,1,2,0,1,0,1,2,0,1})); // [[1,1,1,0,1,0,0,1,0,0],[1,0,1,0,0,0,1,1,0,1]]
-        System.out.println(prob.reconstructMatrix(1, 4, new int[]{2,1,2,0,0,2})); // [[1,0,1,0,0,1],[1,1,1,0,0,1]]
+        return up == 0 && lo == 0 ? answer : Collections.emptyList();
     }
 }
