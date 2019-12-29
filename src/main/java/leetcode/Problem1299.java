3a34fb324c9eb7f78cc1db450c2c444eef8e15b3
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-12-29 00:35:09 -0600

    Skeleton for problem 1299

---
+++ src/main/java/leetcode/Problem1299.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side/
+ */
+public class Problem1299 {
+    public int[] replaceElements(int[] arr) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem1299 prob = new Problem1299();
+        System.out.println(Arrays.toString(prob.replaceElements(new int[]{17, 18, 5, 4, 6, 1}))); // [18,6,6,6,1,-1]
+    }
+}
