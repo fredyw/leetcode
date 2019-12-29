d9520cf89d3a9510491a809ea9b1ecf6e524aeed
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-12-29 00:27:55 -0600

    Skeleton for problem 5295

---
+++ src/main/java/leetcode/Problem5295.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/find-n-unique-integers-sum-up-to-zero
+ */
+public class Problem5295 {
+    public int[] sumZero(int n) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem5295 prob = new Problem5295();
+        System.out.println(prob.sumZero(5)); // [-7,-1,1,3,4]
+        System.out.println(prob.sumZero(3)); // [-1,0,1]
+        System.out.println(prob.sumZero(1)); // [0]
+    }
+}
