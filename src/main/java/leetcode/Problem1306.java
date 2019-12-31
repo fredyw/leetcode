94638b96c859b62734f75f38e16251bc7e863239
Fredy Wijaya <fredy.wijaya@gmail.com>
2019-12-30 19:18:46 -0600

    Solve problem 1306

--- src/main/java/leetcode/Problem1306.java
+++ src/main/java/leetcode/Problem1306.java
@@ -5,14 +5,19 @@ package leetcode;
  */
 public class Problem1306 {
     public boolean canReach(int[] arr, int start) {
-        // TODO
-        return false;
+        return canReach(arr, start, new boolean[arr.length]);
     }
 
-    public static void main(String[] args) {
-        Problem1306 prob = new Problem1306();
-        System.out.println(prob.canReach(new int[]{4,2,3,0,3,1,2}, 5)); // true
-        System.out.println(prob.canReach(new int[]{4,2,3,0,3,1,2}, 0)); // true
-        System.out.println(prob.canReach(new int[]{3,0,2,1,2}, 2)); // false
+    private static boolean canReach(int[] arr, int index, boolean[] visited) {
+        if (visited[index]) {
+            return false;
+        }
+        if (arr[index] == 0) {
+            return true;
+        }
+        visited[index] = true;
+        boolean a = index + arr[index] < arr.length ? canReach(arr, index + arr[index], visited) : false;
+        boolean b = index - arr[index] >= 0 ? canReach(arr, index - arr[index], visited) : false;
+        return a || b;
     }
 }
