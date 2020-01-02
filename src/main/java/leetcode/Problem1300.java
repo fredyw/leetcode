2a69f99a16f920fc0f2e0756823c38a23f11b153
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-01-01 23:11:48 -0600

    Update problem 1300

--- src/main/java/leetcode/Problem1300.java
+++ src/main/java/leetcode/Problem1300.java
@@ -17,11 +17,12 @@ public class Problem1300 {
 
     public static void main(String[] args) {
         Problem1300 prob = new Problem1300();
-        System.out.println(prob.findBestValue(new int[]{4,9,3}, 10)); // 3
-        System.out.println(prob.findBestValue(new int[]{2,3,5}, 10)); // 5
-        System.out.println(prob.findBestValue(new int[]{60864,25176,27249,21296,20204}, 56803)); // 11361
-        System.out.println(prob.findBestValue(new int[]{10,10,10}, 10)); // 3
-        System.out.println(prob.findBestValue(new int[]{1,1,1}, 10)); // 1
-        System.out.println(prob.findBestValue(new int[]{1,2,1}, 10)); // 2
+//        System.out.println(prob.findBestValue(new int[]{4,9,3}, 10)); // 3
+//        System.out.println(prob.findBestValue(new int[]{2,3,5}, 10)); // 5
+//        System.out.println(prob.findBestValue(new int[]{60864,25176,27249,21296,20204}, 56803)); // 11361
+//        System.out.println(prob.findBestValue(new int[]{10,10,10}, 10)); // 3
+//        System.out.println(prob.findBestValue(new int[]{1,1,1}, 10)); // 1
+//        System.out.println(prob.findBestValue(new int[]{1,2,1}, 10)); // 2
+        System.out.println(prob.findBestValue(new int[]{1,2,3}, 7)); // 3
     }
 }
