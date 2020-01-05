5ee1941689c6b6ad6790d8a84d756d4d3cc53233
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-01-05 12:25:30 -0600

    Update problem 1310

--- src/main/java/leetcode/Problem1310.java
+++ src/main/java/leetcode/Problem1310.java
@@ -7,8 +7,15 @@ import java.util.Arrays;
  */
 public class Problem1310 {
     public int[] xorQueries(int[] arr, int[][] queries) {
-        // TODO
-        return null;
+        int[] answer = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            int xor = arr[queries[i][0]];
+            for (int j = queries[i][0] + 1; j <= queries[i][1]; j++) {
+                xor ^= arr[j];
+            }
+            answer[i] = xor;
+        }
+        return answer;
     }
 
     public static void main(String[] args) {
@@ -19,5 +26,8 @@ public class Problem1310 {
         System.out.println(Arrays.toString(prob.xorQueries(new int[]{4,8,2,10}, new int[][]{
             {2,3},{1,3},{0,0},{0,3}
         }))); // [8,0,4,4]
+        System.out.println(Arrays.toString(prob.xorQueries(new int[]{16}, new int[][]{
+            {0,0},{0,0},{0,0}
+        }))); // [16,16,16]
     }
 }
