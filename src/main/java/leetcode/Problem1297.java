0c395274f9033a96f04a0c08d4ff78f8d94fecc2
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-01-05 12:06:33 -0600

    Solve problem 1297

--- src/main/java/leetcode/Problem1297.java
+++ src/main/java/leetcode/Problem1297.java
@@ -9,47 +9,54 @@ import java.util.Map;
 public class Problem1297 {
     public int maxFreq(String s, int maxLetters, int minSize, int maxSize) {
         int answer = 0;
-        for (int i = minSize; i < maxSize; i++) {
-            String sub = s.substring(0, i);
-            Map<Character, Integer> charCount = new HashMap<>();
-            for (char c : sub.toCharArray()) {
-                int count = charCount.getOrDefault(c, 0);
-                if (count == 0 && charCount.size() + 1 > maxLetters) {
-                    // TODO
-                }
-                if (!charCount.containsKey(c)) {
-                    charCount.put(c, 1);
-                } else {
-                    charCount.put(c, charCount.get(c) + 1);
-                }
+        String sub = s.substring(0, minSize);
+        Map<String, Integer> strCount = new HashMap<>();
+        Map<Character, Integer> charCount = new HashMap<>();
+        for (char c : sub.toCharArray()) {
+            if (!charCount.containsKey(c)) {
+                charCount.put(c, 1);
+            } else {
+                charCount.put(c, charCount.get(c) + 1);
             }
-            Map<String, Integer> strCount = new HashMap<>();
-            strCount.put(sub, 1);
-            for (int j = minSize + 1; j < s.length(); j++) {
-                char c = s.charAt(j);
-                int count = charCount.getOrDefault(c, 0);
-                if (count == 0 && charCount.size() + 1 > maxLetters) {
-                    // TODO
-                }
-                if (!charCount.containsKey(c)) {
-                    charCount.put(c, 1);
+        }
+        if (charCount.size() <= maxLetters) {
+            int newCount;
+            if (!strCount.containsKey(sub)) {
+                newCount = 1;
+                strCount.put(sub, newCount);
+            } else {
+                newCount = strCount.get(sub) + 1;
+                strCount.put(sub, newCount);
+
+            }
+            answer = Math.max(answer, newCount);
+        }
+        for (int j = minSize; j < s.length(); j++) {
+            int count = charCount.get(sub.charAt(0)) - 1;
+            if (count == 0) {
+                charCount.remove(sub.charAt(0));
+            } else {
+                charCount.put(sub.charAt(0), count);
+            }
+            char c = s.charAt(j);
+            if (!charCount.containsKey(c)) {
+                charCount.put(c, 1);
+            } else {
+                charCount.put(c, charCount.get(c) + 1);
+            }
+            sub = sub.substring(1) + s.charAt(j);
+            if (charCount.size() <= maxLetters) {
+                int newCount;
+                if (!strCount.containsKey(sub)) {
+                    newCount = 1;
+                    strCount.put(sub, 1);
                 } else {
-                    charCount.put(c, charCount.get(c) + 1);
+                    newCount = strCount.get(sub) + 1;
+                    strCount.put(sub, newCount);
                 }
-                sub = sub.substring(1) + s.charAt(j);
+                answer = Math.max(answer, newCount);
             }
         }
         return answer;
-    }
-
-    public static void main(String[] args) {
-        Problem1297 prob = new Problem1297();
-        System.out.println(prob.maxFreq("aababcaab", 2, 3, 4)); // 2
-//        System.out.println(prob.maxFreq("aababcaab", 1, 1, 4)); // 5
-//        System.out.println(prob.maxFreq("aababcaab", 2, 2, 4)); // 3
-//        System.out.println(prob.maxFreq("aababcaabaa", 2, 2, 4)); // 3
-//        System.out.println(prob.maxFreq("aaaa", 1, 3, 3)); // 2
-//        System.out.println(prob.maxFreq("aabcabcab", 2, 2, 3)); // 3
-//        System.out.println(prob.maxFreq("abcde", 2, 3, 3)); // 0
     }
 }
