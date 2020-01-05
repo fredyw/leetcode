d7d4273301d04560ebd9f1f310e89bea2bb79c3c
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-01-04 23:18:30 -0600

    Solve problem 5303

--- src/main/java/leetcode/Problem5303.java
+++ src/main/java/leetcode/Problem5303.java
@@ -5,15 +5,21 @@ package leetcode;
  */
 public class Problem5303 {
     public String freqAlphabets(String s) {
-        // TODO
-        return null;
+        String answer = "";
+        int i = 0;
+        while (i < s.length()) {
+            if (i + 2 < s.length() && s.charAt(i + 2) == '#') {
+                answer += toChar(Integer.parseInt(s.substring(i, i + 2)));
+                i += 3;
+            } else {
+                answer += toChar(Integer.parseInt("" + s.charAt(i)));
+                i++;
+            }
+        }
+        return answer;
     }
 
-    public static void main(String[] args) {
-        Problem5303 prob = new Problem5303();
-        System.out.println(prob.freqAlphabets("10#11#12")); // "jkab"
-        System.out.println(prob.freqAlphabets("1326#")); // "acz"
-        System.out.println(prob.freqAlphabets("25#")); // "y"
-        System.out.println(prob.freqAlphabets("12345678910#11#12#13#14#15#16#17#18#19#20#21#22#23#24#25#26#")); // "abcdefghijklmnopqrstuvwxyz"
+    private static char toChar(int n) {
+        return (char) (n + 'a' - 1);
     }
 }
