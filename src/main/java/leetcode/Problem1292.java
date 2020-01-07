d9f8e94869fb917eace2f14bf3333bbc8ef7d3f1
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-01-06 23:55:21 -0600

    Update problem 1292

--- src/main/java/leetcode/Problem1292.java
+++ src/main/java/leetcode/Problem1292.java
@@ -12,22 +12,38 @@ public class Problem1292 {
     public static void main(String[] args) {
         Problem1292 prob = new Problem1292();
         System.out.println(prob.maxSideLength(new int[][]{
-            {1,1,3,2,4,3,2},{1,1,3,2,4,3,2},{1,1,3,2,4,3,2}
+            {1,1,3,2,4,3,2},
+            {1,1,3,2,4,3,2},
+            {1,1,3,2,4,3,2}
         }, 4)); // 2
         System.out.println(prob.maxSideLength(new int[][]{
-            {2,2,2,2,2},{2,2,2,2,2},{2,2,2,2,2},{2,2,2,2,2}
+            {2,2,2,2,2},
+            {2,2,2,2,2},
+            {2,2,2,2,2},
+            {2,2,2,2,2}
         }, 1)); // 0
         System.out.println(prob.maxSideLength(new int[][]{
-            {1,1,1,1},{1,0,0,0},{1,0,0,0},{1,0,0,0}
+            {1,1,1,1},
+            {1,0,0,0},
+            {1,0,0,0},
+            {1,0,0,0}
         }, 6)); // 3
         System.out.println(prob.maxSideLength(new int[][]{
-            {18,70},{61,1},{25,85},{14,40},{11,96},{97,96},{63,45}
+            {18,70},
+            {61,1},
+            {25,85},
+            {14,40},
+            {11,96},
+            {97,96},
+            {63,45}
         }, 40184)); // 2
         System.out.println(prob.maxSideLength(new int[][]{
-            {18,70},{61,1}
+            {18,70},
+            {61,1}
         }, 4)); // 1
         System.out.println(prob.maxSideLength(new int[][]{
-            {18,70},{61,5}
+            {18,70},
+            {61,5}
         }, 4)); // 0
     }
 }
