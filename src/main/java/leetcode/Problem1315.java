30c796167ce39dbab59f0c3b7972e803b8d75a5d
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-01-12 23:19:23 -0600

    Skeleton of problem 1315

---
+++ src/main/java/leetcode/Problem1315.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/sum-of-nodes-with-even-valued-grandparent/
+ */
+public class Problem1315 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public int sumEvenGrandparent(TreeNode root) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1315 prob = new Problem1315();
+    }
+}
