5444c9eb90e25b3fd9d66168b3d2b12c78ff7d49
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-01-12 23:05:17 -0600

    Skeleton of problem 1317

---
+++ src/main/java/leetcode/Problem1317.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/convert-integer-to-the-sum-of-two-no-zero-integers/
+ */
+public class Problem1317 {
+    public int[] getNoZeroIntegers(int n) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem1317 prob = new Problem1317();
+        System.out.println(Arrays.toString(prob.getNoZeroIntegers(2))); // [1,1]
+        System.out.println(Arrays.toString(prob.getNoZeroIntegers(11))); // [2,9]
+        System.out.println(Arrays.toString(prob.getNoZeroIntegers(10000))); // [1,9999]
+        System.out.println(Arrays.toString(prob.getNoZeroIntegers(69))); // [1,68]
+        System.out.println(Arrays.toString(prob.getNoZeroIntegers(1010))); // [11,999]
+    }
+}
