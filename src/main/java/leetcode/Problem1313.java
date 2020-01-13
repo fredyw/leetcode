decc146bf5fba91a053df4db7cf6fe580cb11901
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-01-12 22:59:19 -0600

    Skeleton of problem 1313

---
+++ src/main/java/leetcode/Problem1313.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/decompress-run-length-encoded-list/
+ */
+public class Problem1313 {
+    public int[] decompressRLElist(int[] nums) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem1313 prob = new Problem1313();
+    }
+}
