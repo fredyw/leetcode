929b72fcf40777690cd9f54beaa9940b44dea412
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-01-14 23:51:47 -0600

    Skeleton of problem 1318

---
+++ src/main/java/leetcode/Problem1318.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/minimum-flips-to-make-a-or-b-equal-to-c/
+ */
+public class Problem1318 {
+    public int minFlips(int a, int b, int c) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1318 prob = new Problem1318();
+        System.out.println(prob.minFlips(2, 6, 5)); // 3
+        System.out.println(prob.minFlips(4, 2, 7)); // 1
+        System.out.println(prob.minFlips(1, 2, 3)); // 0
+    }
+}
