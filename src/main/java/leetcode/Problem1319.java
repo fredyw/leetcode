01e87a00dc8ed464c62a82205b719f21437929fd
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-01-18 21:37:20 -0600

    Update problem 1319

--- src/main/java/leetcode/Problem1319.java
+++ src/main/java/leetcode/Problem1319.java
@@ -44,13 +44,4 @@ public class Problem1319 {
             count--;
         }
     }
-
-    public static void main(String[] args) {
-        Problem1319 prob = new Problem1319();
-        System.out.println(prob.makeConnected(4, new int[][]{{0,1},{0,2},{1,2}})); // 1
-        System.out.println(prob.makeConnected(6, new int[][]{{0,1},{0,2},{0,3},{1,2},{1,3}})); // 2
-        System.out.println(prob.makeConnected(6, new int[][]{{0,1},{0,2},{0,3},{1,2},{1,3},{4,5}})); // 1
-        System.out.println(prob.makeConnected(6, new int[][]{{0,1},{0,2},{0,3},{1,2}})); // -1
-        System.out.println(prob.makeConnected(5, new int[][]{{0,1},{0,2},{3,4},{2,3}})); // 0
-    }
 }
