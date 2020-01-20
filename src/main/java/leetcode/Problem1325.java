a9c6b1cfb4d9027f3438d60381d19f5edab2c2c9
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-01-20 00:00:34 -0600

    Skeleton of problem 1325

---
+++ src/main/java/leetcode/Problem1325.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/delete-leaves-with-a-given-value/
+ */
+public class Problem1325 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode removeLeafNodes(TreeNode root, int target) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem1325 prob = new Problem1325();
+    }
+}
