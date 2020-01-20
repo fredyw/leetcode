d5d3c770058ed9a7dbade801a16e0d2e19008e32
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-01-19 23:58:50 -0600

    Solve problem 1323

--- src/main/java/leetcode/Problem1323.java
+++ src/main/java/leetcode/Problem1323.java
@@ -5,13 +5,14 @@ package leetcode;
  */
 public class Problem1323 {
     public int maximum69Number (int num) {
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem1323 prob = new Problem1323();
-        System.out.println(prob.maximum69Number(9669)); // 9969
-        System.out.println(prob.maximum69Number(9996)); // 9999
-        System.out.println(prob.maximum69Number(9999)); // 9999
+        String answer = Integer.toString(num);
+        String s = Integer.toString(num);
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '6') {
+                answer = answer.substring(0, i) + '9' + answer.substring(i + 1);
+                break;
+            }
+        }
+        return Integer.parseInt(answer);
     }
 }
