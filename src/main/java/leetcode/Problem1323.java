ba5c33b6f3e4d11e3358a958a458576f3bb62db1
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-01-19 23:55:50 -0600

    Skeleton of problem 1323

---
+++ src/main/java/leetcode/Problem1323.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/maximum-69-number/
+ */
+public class Problem1323 {
+    public int maximum69Number (int num) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1323 prob = new Problem1323();
+        System.out.println(prob.maximum69Number(9669)); // 9969
+        System.out.println(prob.maximum69Number(9996)); // 9999
+        System.out.println(prob.maximum69Number(9999)); // 9999
+    }
+}
