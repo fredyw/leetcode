3bc50e44e7eae2179f1233572cc5aa71042f479b
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-01-21 23:07:51 -0600

    Update problem 1311

--- src/main/java/leetcode/Problem1311.java
+++ src/main/java/leetcode/Problem1311.java
@@ -23,6 +23,10 @@ public class Problem1311 {
             ), new int[][]{{1, 2}, {0, 3}, {0, 3}, {1, 2}}, 0, 1)); // ["B","C"]
         System.out.println(prob.watchedVideosByFriends(
             Arrays.asList(
+                Arrays.asList("A", "B"), Arrays.asList("A", "B", "C"), Arrays.asList("B", "C"), Arrays.asList("D")
+            ), new int[][]{{1, 2}, {0, 3}, {0, 3}, {1, 2}}, 0, 1)); // ["B","A","C"]
+        System.out.println(prob.watchedVideosByFriends(
+            Arrays.asList(
                 Arrays.asList("A", "B"), Arrays.asList("C"), Arrays.asList("B", "C"), Arrays.asList("D")
             ), new int[][]{{1, 2}, {0, 3}, {0, 3}, {1, 2}}, 0, 2)); // ["D"]
     }
