9bb113449238f9927618915ce2d7402f3a94509a
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-02-10 00:02:01 -0600

    Solve problem 1346

--- src/main/java/leetcode/Problem1346.java
+++ src/main/java/leetcode/Problem1346.java
@@ -1,18 +1,32 @@
 package leetcode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * https://leetcode.com/problems/check-if-n-and-its-double-exist/
  */
 public class Problem1346 {
     public boolean checkIfExist(int[] arr) {
-        // TODO
+        Set<Integer> set = new HashSet<>();
+        int nZero = 0;
+        for (int n : arr) {
+            if (n == 0) {
+                nZero++;
+            }
+            set.add(n);
+        }
+        if (nZero > 1) {
+            return true;
+        }
+        for (int n : arr) {
+            if (n == 0) {
+                continue;
+            }
+            if (n % 2 == 0 && set.contains(n / 2)) {
+                return true;
+            }
+        }
         return false;
-    }
-
-    public static void main(String[] args) {
-        Problem1346 prob = new Problem1346();
-        System.out.println(prob.checkIfExist(new int[]{10,2,5,3})); // true
-        System.out.println(prob.checkIfExist(new int[]{7,1,14,11})); // true
-        System.out.println(prob.checkIfExist(new int[]{3,1,7,11})); // false
     }
 }
