3549f36586e62d5aca957dc8faa78dc82ed0395c
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-02-10 00:24:51 -0600

    Solve problem 1342

--- src/main/java/leetcode/Problem1342.java
+++ src/main/java/leetcode/Problem1342.java
@@ -5,14 +5,16 @@ package leetcode;
  */
 public class Problem1342 {
     public int numberOfSteps (int num) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem1342 prob = new Problem1342();
-        System.out.println(prob.numberOfSteps(14)); // 16
-        System.out.println(prob.numberOfSteps(8)); // 4
-        System.out.println(prob.numberOfSteps(123)); // 12
+        int answer = 0;
+        int n = num;
+        while (n > 0) {
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                n--;
+            }
+            answer++;
+        }
+        return answer;
     }
 }
