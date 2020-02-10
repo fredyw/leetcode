baa08c5cbcaafa268dc1167b8d949b974558e83a
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-02-09 23:50:09 -0600

    Skeleton of problem 1346

---
+++ src/main/java/leetcode/Problem1346.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/check-if-n-and-its-double-exist/
+ */
+public class Problem1346 {
+    public boolean checkIfExist(int[] arr) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem1346 prob = new Problem1346();
+        System.out.println(prob.checkIfExist(new int[]{10,2,5,3})); // true
+        System.out.println(prob.checkIfExist(new int[]{7,1,14,11})); // true
+        System.out.println(prob.checkIfExist(new int[]{3,1,7,11})); // false
+    }
+}
