a12101b13bf16b6f270efbc0bea65cb8f47e74ab
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-02-10 00:22:55 -0600

    Skeleton of problem 1342

---
+++ src/main/java/leetcode/Problem1342.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+ */
+public class Problem1342 {
+    public int numberOfSteps (int num) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1342 prob = new Problem1342();
+        System.out.println(prob.numberOfSteps(14)); // 16
+        System.out.println(prob.numberOfSteps(8)); // 4
+        System.out.println(prob.numberOfSteps(123)); // 12
+    }
+}
