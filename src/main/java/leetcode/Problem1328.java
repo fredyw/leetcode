bbca1e1d30f259e39c8de1d24e181cc36acaa9e4
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-02-17 13:44:48 -0600

    Skeleton of problem 1328

---
+++ src/main/java/leetcode/Problem1328.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/break-a-palindrome/
+ */
+public class Problem1328 {
+    public String breakPalindrome(String palindrome) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem1328 prob = new Problem1328();
+        System.out.println(prob.breakPalindrome("abccba")); // "aaccba"
+        System.out.println(prob.breakPalindrome("a")); // ""
+    }
+}
