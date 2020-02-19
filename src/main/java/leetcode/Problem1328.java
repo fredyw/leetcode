fa36b09d7cb8d776bd086ee3554c7eea0f69b0e7
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-02-18 23:22:30 -0600

    Solve problem 1328

--- src/main/java/leetcode/Problem1328.java
+++ src/main/java/leetcode/Problem1328.java
@@ -5,15 +5,39 @@ package leetcode;
  */
 public class Problem1328 {
     public String breakPalindrome(String palindrome) {
-        // TODO
-        return null;
+        if (!isPalindrome(palindrome) || palindrome.length() == 1) {
+            return "";
+        }
+        char[] answer = palindrome.toCharArray();
+        boolean replaced = false;
+        for (int i = 0; i < palindrome.length(); i++) {
+            if (palindrome.length() % 2 != 0) {
+                int mid = palindrome.length() / 2;
+                if (palindrome.charAt(i) != 'a' && i != mid) {
+                    answer[i] = 'a';
+                    replaced = true;
+                    break;
+                }
+            } else {
+                if (palindrome.charAt(i) != 'a') {
+                    answer[i] = 'a';
+                    replaced = true;
+                    break;
+                }
+            }
+        }
+        if (!replaced) {
+            answer[answer.length - 1] = (char) (answer[answer.length - 1] + 1); // must be the last one
+        }
+        return new String(answer);
     }
 
-    public static void main(String[] args) {
-        Problem1328 prob = new Problem1328();
-        System.out.println(prob.breakPalindrome("abccba")); // "aaccba"
-        System.out.println(prob.breakPalindrome("a")); // ""
-        System.out.println(prob.breakPalindrome("aba")); // "abb"
-        System.out.println(prob.breakPalindrome("bab")); // "aab"
+    private static boolean isPalindrome(String str) {
+        for (int i = 0, j = str.length() - 1; i < j; i++, j--) {
+            if (str.charAt(i) != str.charAt(j)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
