12cfd7ef00e31727b2b74b5cffd81659b604e7a8
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-02-22 11:50:24 -0600

    Update problem 1208

--- src/main/java/leetcode/Problem1208.java
+++ src/main/java/leetcode/Problem1208.java
@@ -5,15 +5,31 @@ package leetcode;
  */
 public class Problem1208 {
     public int equalSubstring(String s, String t, int maxCost) {
-        // TODO
-        return 0;
+        int size = s.length();
+        int prev = 0;
+        int answer = 0;
+        int cost = maxCost;
+        for (int cur = 0; cur < size; cur++) {
+            int diff = Math.abs(s.charAt(cur) - t.charAt(cur));
+            cost -= diff;
+            if (cost < 0) {
+                cost = maxCost;
+                answer = Math.max(answer, cur - prev);
+                prev = cur;
+            }
+        }
+        if (cost >= 0) {
+            answer = Math.max(answer, size - 1 - prev);
+        }
+        return answer;
     }
 
     public static void main(String[] args) {
         Problem1208 prob = new Problem1208();
-        System.out.println(prob.equalSubstring("abcd", "bcdf", 3)); // 3
+//        System.out.println(prob.equalSubstring("abcd", "bcdf", 3)); // 3
+//        System.out.println(prob.equalSubstring("abaace", "bcxbdf", 3)); // 3
         System.out.println(prob.equalSubstring("abcd", "cdef", 3)); // 1
-        System.out.println(prob.equalSubstring("abcd", "acde", 0)); // 1
-        System.out.println(prob.equalSubstring("abcd", "bxdf", 2)); // 1
+//        System.out.println(prob.equalSubstring("abcd", "acde", 0)); // 1
+//        System.out.println(prob.equalSubstring("abcd", "bxdf", 2)); // 1
     }
 }
