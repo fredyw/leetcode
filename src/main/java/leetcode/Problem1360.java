89b30e5fdbbf83cc2e8f995a18b5e66320827158
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-02-23 22:53:29 -0600

    Skeleton of problem 1360

---
+++ src/main/java/leetcode/Problem1360.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/number-of-days-between-two-dates/
+ */
+public class Problem1360 {
+    public int daysBetweenDates(String date1, String date2) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1360 prob = new Problem1360();
+        System.out.println(prob.daysBetweenDates("2019-06-29", "2019-06-30")); // 1
+        System.out.println(prob.daysBetweenDates("2020-01-15", "2019-12-31")); // 15
+    }
+}
