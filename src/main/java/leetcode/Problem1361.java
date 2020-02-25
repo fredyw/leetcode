0c856ee97b1a7c91cfeca0cf24348ab39aee648b
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-02-24 21:16:58 -0600

    Solve problem 1361

--- src/main/java/leetcode/Problem1361.java
+++ src/main/java/leetcode/Problem1361.java
@@ -8,7 +8,7 @@ public class Problem1361 {
         int[] inCount = new int[n];
         int[] outCount = new int[n];
         for (int i = 0; i < n; i++) {
-            // there should not be any edge going into the root
+            // There should not be any edge going into the root.
             if (leftChild[i] == 0 || rightChild[i] == 0) {
                 return false;
             }
@@ -27,16 +27,12 @@ public class Problem1361 {
                 }
             }
         }
+        // Other than root, there should be exactly one edge going into every node.
+        for (int i = 1; i < n; i++) {
+            if (inCount[i] != 1) {
+                return false;
+            }
+        }
         return true;
-    }
-
-    public static void main(String[] args) {
-        Problem1361 prob = new Problem1361();
-//        System.out.println(prob.validateBinaryTreeNodes(4, new int[]{1,-1,3,-1}, new int[]{2,-1,-1,-1})); // true
-//        System.out.println(prob.validateBinaryTreeNodes(4, new int[]{1,-1,3,-1}, new int[]{2,3,-1,-1})); // false
-//        System.out.println(prob.validateBinaryTreeNodes(2, new int[]{1,0}, new int[]{-1,-1})); // false
-//        System.out.println(prob.validateBinaryTreeNodes(3, new int[]{1,2,1}, new int[]{-1,-1,-1})); // false
-        System.out.println(prob.validateBinaryTreeNodes(6, new int[]{1,-1,-1,4,-1,-1}, new int[]{2,-1,-1,5,-1,-1})); // false
-//        System.out.println(prob.validateBinaryTreeNodes(5, new int[]{3,-1,-1,1,-1}, new int[]{4,-1,-1,2,-1})); // true
     }
 }
