aea0f8a038c95a0c50052c024bd120c20c3daef4
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-03-01 00:35:36 -0600

    Solve problem 1357

--- src/main/java/leetcode/Problem1357.java
+++ src/main/java/leetcode/Problem1357.java
@@ -26,25 +26,12 @@ public class Problem1357 {
             double total = 0;
             for (int i = 0; i < product.length; i++) {
                 double price = prices.get(product[i]);
-                if (count % 3 == 0) {
-                    price = (prices.get(product[i]) * discount) / 100;
+                if (count % n == 0) {
+                    price = price - ((price * discount) / 100);
                 }
                 total += price * amount[i];
             }
             return total;
         }
-    }
-
-    public static void main(String[] args) {
-        Cashier cashier = new Cashier(
-            3, 50, new int[]{1, 2, 3, 4, 5, 6, 7}, new int[]{100, 200, 300, 400, 300, 200, 100});
-        System.out.println(cashier.getBill(new int[]{1, 2}, new int[]{1, 2})); // 500
-        System.out.println(cashier.getBill(new int[]{3, 7}, new int[]{10, 10})); // 4000
-        System.out.println(cashier.getBill(new int[]{1, 2, 3, 4, 5, 6, 7}, new int[]{1, 1, 1, 1, 1, 1, 1})); // 800
-        System.out.println(cashier.getBill(new int[]{4}, new int[]{10})); // 4000
-        System.out.println(cashier.getBill(new int[]{7, 3}, new int[]{10, 10})); // 4000
-        System.out.println(cashier.getBill(new int[]{7, 5, 3, 1, 6, 4, 2}, new int[]{10, 10, 10, 9, 9, 9, 7})); // 7350
-        System.out.println(cashier.getBill(new int[]{2, 3, 5}, new int[]{5, 3, 2})); // 2500
-
     }
 }
