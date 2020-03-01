7563e15f05af095c8100bad8e828140944f19d31
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-02-29 20:14:31 -0600

    Update problem 1357

--- src/main/java/leetcode/Problem1357.java
+++ src/main/java/leetcode/Problem1357.java
@@ -1,17 +1,37 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * https://leetcode.com/problems/apply-discount-every-n-orders/
  */
 public class Problem1357 {
     private static class Cashier {
+        private final int n;
+        private final int discount;
+        private Map</* product= */ Integer, /* price= */ Integer> prices = new HashMap<>();
+        private int count = 0;
+
         public Cashier(int n, int discount, int[] products, int[] prices) {
-            // TODO
+            this.n = n;
+            this.discount = discount;
+            for (int i = 0; i < products.length; i++) {
+                this.prices.put(products[i], prices[i]);
+            }
         }
 
         public double getBill(int[] product, int[] amount) {
-            // TODO
-            return 0;
+            count++;
+            double total = 0;
+            for (int i = 0; i < product.length; i++) {
+                double price = prices.get(product[i]);
+                if (count % 3 == 0) {
+                    price = (prices.get(product[i]) * discount) / 100;
+                }
+                total += price * amount[i];
+            }
+            return total;
         }
     }
 
