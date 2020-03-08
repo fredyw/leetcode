c39d69eb66c5d3072a193721ec1bd9a0139a076e
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-03-07 23:34:03 -0600

    Solve problem 1370

--- src/main/java/leetcode/Problem1370.java
+++ src/main/java/leetcode/Problem1370.java
@@ -31,14 +31,4 @@ public class Problem1370 {
         }
         return answer;
     }
-
-    public static void main(String[] args) {
-        Problem1370 prob = new Problem1370();
-        System.out.println(prob.sortString("aaaabbbbcccc")); // "abccbaabccba"
-        System.out.println(prob.sortString("rat")); // "art"
-        System.out.println(prob.sortString("leetcode")); // "cdelotee"
-        System.out.println(prob.sortString("ggggggg")); // "ggggggg"
-        System.out.println(prob.sortString("ggggggg")); // "ggggggg"
-        System.out.println(prob.sortString("spo")); // "ops"
-    }
 }
