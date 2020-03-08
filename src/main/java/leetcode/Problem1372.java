c10af5fc0511e75c13bf60acd0c245fa4806acf6
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-03-08 00:21:52 -0600

    Skeleton of problem 1372

---
+++ src/main/java/leetcode/Problem1372.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/longest-zigzag-path-in-a-binary-tree/
+ */
+public class Problem1372 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public int longestZigZag(TreeNode root) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+    }
+}
