bd214f3a664bb5a5b99dce88637a36d51f52ace5
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-03-07 23:37:25 -0600

    Skeleton of problem 1374

---
+++ src/main/java/leetcode/Problem1374.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/generate-a-string-with-characters-that-have-odd-counts/
+ */
+public class Problem1374 {
+    public String generateTheString(int n) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem1374 prob = new Problem1374();
+        System.out.println(prob.generateTheString(4)); // "pppz"
+        System.out.println(prob.generateTheString(2)); // "xy"
+        System.out.println(prob.generateTheString(7)); // "holasss"
+    }
+}
