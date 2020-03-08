8897500fc97c60c75364481d0af3bb0f5ae763ac
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-03-08 12:26:47 -0500

    Update problem 1372

--- src/main/java/leetcode/Problem1372.java
+++ src/main/java/leetcode/Problem1372.java
@@ -15,10 +15,30 @@ public class Problem1372 {
     }
 
     public int longestZigZag(TreeNode root) {
-        // TODO
-        return 0;
+        return Math.max(longestZigZag(root, true), longestZigZag(root, false)) - 1;
+    }
+
+    private static int longestZigZag(TreeNode root, boolean left) {
+        if (root == null) {
+            return 0;
+        }
+        if (left) {
+            return longestZigZag(root.left, !left) + 1;
+        }
+        return longestZigZag(root.right, !left) + 1;
     }
 
     public static void main(String[] args) {
+        Problem1372 prob = new Problem1372();
+
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(1);
+        root.right.left = new TreeNode(1);
+        root.right.right = new TreeNode(1);
+        root.right.right.left = new TreeNode(1);
+        root.right.right.right = new TreeNode(1);
+        root.right.right.left.right = new TreeNode(1);
+        root.right.right.left.right.right = new TreeNode(1);
+        System.out.println(prob.longestZigZag(root)); // 3
     }
 }
