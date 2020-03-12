76434655f4a42bb8fac861be6fcfed99e4676ae9
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-03-12 00:34:46 -0500

    Skeleton of problem 1379

---
+++ src/main/java/leetcode/Problem1379.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/
+ */
+public class Problem1379 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+    }
+}
