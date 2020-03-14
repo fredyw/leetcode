4d71810c3f26288f358a87f0b10531cd5a78566d
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-03-13 23:11:57 -0500

    Skeleton of problem 1371

---
+++ src/main/java/leetcode/Problem1371.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/find-the-longest-substring-containing-vowels-in-even-counts/
+ */
+public class Problem1371 {
+    public int findTheLongestSubstring(String s) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1371 prob = new Problem1371();
+        System.out.println(prob.findTheLongestSubstring("eleetminicoworoep")); // 13
+        System.out.println(prob.findTheLongestSubstring("leetcodeisgreat")); // 5
+        System.out.println(prob.findTheLongestSubstring("bcbcbc")); // 6
+    }
+}
