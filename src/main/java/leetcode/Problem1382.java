df40da2d8ee5cea6f501b90747c2343604080335
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-03-15 14:04:04 -0500

    Skeleton of problem 1382

---
+++ src/main/java/leetcode/Problem1382.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/balance-a-binary-search-tree/
+ */
+public class Problem1382 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode balanceBST(TreeNode root) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem1382 prob = new Problem1382();
+    }
+}
