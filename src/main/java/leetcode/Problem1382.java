a5b34f2a5fb09c0349207958949d0dee3d62356c
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-03-15 14:44:50 -0500

    Solve problem 1382

--- src/main/java/leetcode/Problem1382.java
+++ src/main/java/leetcode/Problem1382.java
@@ -1,7 +1,6 @@
 package leetcode;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -16,38 +15,36 @@ public class Problem1382 {
         TreeNode(int x) {
             val = x;
         }
+
+        @Override
+        public String toString() {
+            return "" + val;
+        }
     }
 
     public TreeNode balanceBST(TreeNode root) {
         List<TreeNode> nodes = new ArrayList<>();
         collectNodes(root, nodes);
-        LinkedList<TreeNode> list = new LinkedList<>();
-        int i = 0;
-        list.add(nodes.get(i++));
-        while (i < nodes.size()) {
-            TreeNode current = list.getFirst();
-            if (i < nodes.size()) {
-                current.left = nodes.get(i++);
-                list.add(current.left);
-            }
-            if (i < nodes.size()) {
-                current.right = nodes.get(i++);
-                list.add(current.right);
-            }
+        return buildTree(nodes, 0, nodes.size() - 1);
+    }
+
+    private static TreeNode buildTree(List<TreeNode> nodes, int left, int right) {
+        if (left > right) {
+            return null;
         }
-        return nodes.get(0);
+        int mid = (left + right) / 2;
+        TreeNode node = new TreeNode(nodes.get(mid).val);
+        node.left = buildTree(nodes, left, mid - 1);
+        node.right = buildTree(nodes, mid + 1, right);
+        return node;
     }
 
     private static void collectNodes(TreeNode root, List<TreeNode> nodes) {
         if (root == null) {
             return;
         }
-        nodes.add(root);
         collectNodes(root.left, nodes);
+        nodes.add(root);
         collectNodes(root.right, nodes);
-    }
-
-    public static void main(String[] args) {
-        Problem1382 prob = new Problem1382();
     }
 }
