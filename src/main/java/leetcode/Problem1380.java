f858cc01121a9c8e91f23155c8b638a3537fc81d
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-03-15 13:46:36 -0500

    Solve problem 1380

--- src/main/java/leetcode/Problem1380.java
+++ src/main/java/leetcode/Problem1380.java
@@ -1,31 +1,47 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * https://leetcode.com/problems/lucky-numbers-in-a-matrix/
  */
 public class Problem1380 {
     public List<Integer> luckyNumbers(int[][] matrix) {
-        // TODO
-        return null;
-    }
-
-    public static void main(String[] args) {
-        Problem1380 prob = new Problem1380();
-        System.out.println(prob.luckyNumbers(new int[][]{
-            {3, 7, 8},
-            {9, 11, 13},
-            {15, 16, 17}
-        })); // [15]
-        System.out.println(prob.luckyNumbers(new int[][]{
-            {1, 10, 4, 2},
-            {9, 3, 8, 7},
-            {15, 16, 17, 12}
-        })); // [12]
-        System.out.println(prob.luckyNumbers(new int[][]{
-            {7, 8},
-            {1, 2},
-        })); // [7]
+        List<Integer> answer = new ArrayList<>();
+        Set<String> set = new HashSet<>();
+        int maxRow = matrix.length;
+        int maxCol = matrix.length > 0 ? matrix[0].length : 0;
+        for (int row = 0; row < maxRow; row++) {
+            int min = Integer.MAX_VALUE;
+            int minRowIndex = 0;
+            int minColIdx = 0;
+            for (int col = 0; col < maxCol; col++) {
+                if (matrix[row][col] < min) {
+                    min = matrix[row][col];
+                    minRowIndex = row;
+                    minColIdx = col;
+                }
+            }
+            set.add(minRowIndex + "," + minColIdx);
+        }
+        for (int col = 0; col < maxCol; col++) {
+            int max = 0;
+            int maxRowIndex = 0;
+            int maxColIndex = 0;
+            for (int row = 0; row < maxRow; row++) {
+                if (matrix[row][col] > max) {
+                    max = matrix[row][col];
+                    maxRowIndex = row;
+                    maxColIndex = col;
+                }
+            }
+            if (set.contains(maxRowIndex + "," + maxColIndex)) {
+                answer.add(max);
+            }
+        }
+        return answer;
     }
 }
