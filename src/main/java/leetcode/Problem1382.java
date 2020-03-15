e6a3dddb55f7eddbb36a10161d3e95782c480c8e
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-03-15 14:18:03 -0500

    Update problem 1382

--- src/main/java/leetcode/Problem1382.java
+++ src/main/java/leetcode/Problem1382.java
@@ -1,5 +1,9 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * https://leetcode.com/problems/balance-a-binary-search-tree/
  */
@@ -15,8 +19,32 @@ public class Problem1382 {
     }
 
     public TreeNode balanceBST(TreeNode root) {
-        // TODO
-        return null;
+        List<TreeNode> nodes = new ArrayList<>();
+        collectNodes(root, nodes);
+        LinkedList<TreeNode> list = new LinkedList<>();
+        int i = 0;
+        list.add(nodes.get(i++));
+        while (i < nodes.size()) {
+            TreeNode current = list.getFirst();
+            if (i < nodes.size()) {
+                current.left = nodes.get(i++);
+                list.add(current.left);
+            }
+            if (i < nodes.size()) {
+                current.right = nodes.get(i++);
+                list.add(current.right);
+            }
+        }
+        return nodes.get(0);
+    }
+
+    private static void collectNodes(TreeNode root, List<TreeNode> nodes) {
+        if (root == null) {
+            return;
+        }
+        nodes.add(root);
+        collectNodes(root.left, nodes);
+        collectNodes(root.right, nodes);
     }
 
     public static void main(String[] args) {
