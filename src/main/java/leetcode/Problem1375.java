7a97236f36631d45b9126078403f59c931dda894
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-03-21 00:57:48 -0500

    Update problem 1375

--- src/main/java/leetcode/Problem1375.java
+++ src/main/java/leetcode/Problem1375.java
@@ -12,8 +12,14 @@ public class Problem1375 {
         }
         int yellowCount = 0;
         for (int l : light) {
-            colors[l - 1] = Color.YELLOW;
-            yellowCount++;
+            if (l - 2 >= 0 && colors[l - 2] == Color.BLUE && yellowCount == 0) {
+                colors[l - 1] = Color.BLUE;
+                answer++;
+                continue;
+            } else {
+                colors[l - 1] = Color.YELLOW;
+                yellowCount++;
+            }
             int i = 0;
             while (i < light.length && colors[i].on) {
                 if (colors[i] == Color.YELLOW) {
@@ -43,12 +49,12 @@ public class Problem1375 {
 
     public static void main(String[] args) {
         Problem1375 prob = new Problem1375();
-        System.out.println(prob.numTimesAllBlue(new int[]{2,1,3,5,4})); // 3
-        System.out.println(prob.numTimesAllBlue(new int[]{3,2,4,1,5})); // 2
-        System.out.println(prob.numTimesAllBlue(new int[]{4, 1, 2, 3})); // 1
-        System.out.println(prob.numTimesAllBlue(new int[]{2,1,4,3,6,5})); // 3
-        System.out.println(prob.numTimesAllBlue(new int[]{1,2,3,4,5,6})); // 6
-        System.out.println(prob.numTimesAllBlue(new int[]{3,2,1})); // 1
-        System.out.println(prob.numTimesAllBlue(new int[]{3,1,2})); // 1
+//        System.out.println(prob.numTimesAllBlue(new int[]{2,1,3,5,4})); // 3
+//        System.out.println(prob.numTimesAllBlue(new int[]{3,2,4,1,5})); // 2
+        System.out.println(prob.numTimesAllBlue(new int[]{4,1,2,3})); // 1
+//        System.out.println(prob.numTimesAllBlue(new int[]{2,1,4,3,6,5})); // 3
+//        System.out.println(prob.numTimesAllBlue(new int[]{1,2,3,4,5,6})); // 6
+//        System.out.println(prob.numTimesAllBlue(new int[]{3,2,1})); // 1
+//        System.out.println(prob.numTimesAllBlue(new int[]{3,1,2})); // 1
     }
 }
