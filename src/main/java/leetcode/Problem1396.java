bb0be07a3b2a11bc5c117fcf3f22fc224f7258d4
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-03-31 19:09:14 -0500

    Skeleton of problem 1396

---
+++ src/main/java/leetcode/Problem1396.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/design-underground-system/
+ */
+public class Problem1396 {
+    static class UndergroundSystem {
+        public UndergroundSystem() {
+        }
+
+        public void checkIn(int id, String stationName, int t) {
+            // TODO
+        }
+
+        public void checkOut(int id, String stationName, int t) {
+            // TODO
+        }
+
+        public double getAverageTime(String startStation, String endStation) {
+            // TODO
+            return 0;
+        }
+    }
+}
