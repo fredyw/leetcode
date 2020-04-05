fdf84da75f5359d0a9ce4db17135d7fa97ea4b84
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-04-04 22:54:49 -0500

    Skeleton of problem 1399

---
+++ src/main/java/leetcode/Problem1399.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/count-largest-group/
+ */
+public class Problem1399 {
+    public int countLargestGroup(int n) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1399 prob = new Problem1399();
+        System.out.println(prob.countLargestGroup(13)); // 4
+        System.out.println(prob.countLargestGroup(2)); // 2
+        System.out.println(prob.countLargestGroup(15)); // 6
+        System.out.println(prob.countLargestGroup(24)); // 5
+    }
+}
