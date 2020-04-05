a50119f27109cbf259647c30fb0eb93848c0692e
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-04-04 23:12:12 -0500

    Update problem 1399

--- src/main/java/leetcode/Problem1399.java
+++ src/main/java/leetcode/Problem1399.java
@@ -12,6 +12,8 @@ public class Problem1399 {
     public static void main(String[] args) {
         Problem1399 prob = new Problem1399();
         System.out.println(prob.countLargestGroup(13)); // 4
+        System.out.println(prob.countLargestGroup(14)); // 5
+        System.out.println(prob.countLargestGroup(20)); // 1
         System.out.println(prob.countLargestGroup(2)); // 2
         System.out.println(prob.countLargestGroup(15)); // 6
         System.out.println(prob.countLargestGroup(24)); // 5
