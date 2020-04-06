7f5b703e0966a341050bc442bf28aa5e915dfd07
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-04-06 11:26:57 -0500

    Update problem 1339

--- src/main/java/leetcode/Problem1339.java
+++ src/main/java/leetcode/Problem1339.java
@@ -15,8 +15,53 @@ public class Problem1339 {
     }
 
     public int maxProduct(TreeNode root) {
-        // TODO
-        return 0;
+        SumTreeNode rootSumTreeNode = build(root);
+        LongRef answer = new LongRef();
+        maxProduct(rootSumTreeNode, rootSumTreeNode.sum, answer);
+        return (int) answer.val;
+    }
+
+    private static class SumTreeNode {
+        private final long sum;
+        private SumTreeNode left;
+        private SumTreeNode right;
+
+        public SumTreeNode(int sum) {
+            this.sum = sum;
+        }
+    }
+
+    private static class LongRef {
+        private long val;
+    }
+
+    private static void maxProduct(SumTreeNode root, long totalSum, LongRef max) {
+        if (root == null) {
+            return;
+        }
+        long product = (root.sum * (totalSum - root.sum)) % 1_000_000_007;
+        max.val = Math.max(max.val, product);
+        maxProduct(root.left, totalSum, max);
+        maxProduct(root.right, totalSum, max);
+    }
+
+    private static SumTreeNode build(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        int sum = 0;
+        SumTreeNode left = build(root.left);
+        if (left != null) {
+            sum += left.sum;
+        }
+        SumTreeNode right = build(root.right);
+        if (right != null) {
+            sum += right.sum;
+        }
+        SumTreeNode current = new SumTreeNode(sum + root.val);
+        current.left = left;
+        current.right = right;
+        return current;
     }
 
     public static void main(String[] args) {
