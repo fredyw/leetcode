b4a9f8f951f87e80f66a18537859a2bb5374651a
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-04-19 00:21:12 -0500

    Solve problem 1400

--- src/main/java/leetcode/Problem1400.java
+++ src/main/java/leetcode/Problem1400.java
@@ -1,20 +1,26 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * https://leetcode.com/problems/construct-k-palindrome-strings/
  */
 public class Problem1400 {
     public boolean canConstruct(String s, int k) {
-        // TODO
-        return false;
-    }
-
-    public static void main(String[] args) {
-        Problem1400 prob = new Problem1400();
-        System.out.println(prob.canConstruct("annabelle", 2)); // true
-        System.out.println(prob.canConstruct("leetcode", 3)); // false
-        System.out.println(prob.canConstruct("true", 4)); // true
-        System.out.println(prob.canConstruct("yzyzyzyzyzyzyzy", 2)); // true
-        System.out.println(prob.canConstruct("cr", 7)); // false
+        if (k > s.length()) {
+            return false;
+        }
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        int oddCount = 0;
+        for (Map.Entry<Character, Integer> e : map.entrySet()) {
+            if (e.getValue() % 2 != 0) {
+                oddCount++;
+            }
+        }
+        return oddCount <= k;
     }
 }
