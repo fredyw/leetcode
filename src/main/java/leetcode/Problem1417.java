5fd808d181400668d59935515313cf6a8b8c39b7
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-04-20 00:03:14 -0500

    Solve problem 1417

--- src/main/java/leetcode/Problem1417.java
+++ src/main/java/leetcode/Problem1417.java
@@ -1,20 +1,40 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * https://leetcode.com/problems/reformat-the-string/
  */
 public class Problem1417 {
     public String reformat(String s) {
-        // TODO
-        return null;
-    }
-
-    public static void main(String[] args) {
-        Problem1417 prob = new Problem1417();
-        System.out.println(prob.reformat("a0b1c2")); // "0a1b2c"
-        System.out.println(prob.reformat("leetcode")); // ""
-        System.out.println(prob.reformat("1229857369")); // ""
-        System.out.println(prob.reformat("covid2019")); // "c2o0v1i9d"
-        System.out.println(prob.reformat("ab123")); // "1a2b3"
+        List<Character> chars = new ArrayList<>();
+        List<Character> numbers = new ArrayList<>();
+        for (char c : s.toCharArray()) {
+            if (c >= '0' && c <= '9') {
+                numbers.add(c);
+            } else {
+                chars.add(c);
+            }
+        }
+        if (Math.abs(chars.size() - numbers.size()) > 1) {
+            return "";
+        }
+        String answer = "";
+        boolean isNumber = chars.size() <= numbers.size();
+        int i = 0;
+        int j = 0;
+        while (i < chars.size() && j < numbers.size()) {
+            answer += isNumber ? numbers.get(i++) : chars.get(j++);
+            isNumber = !isNumber;
+        }
+        if (isNumber) {
+            answer += i < numbers.size() ? numbers.get(i) : "";
+            answer += j < chars.size() ? chars.get(j) : "";
+        } else {
+            answer += j < chars.size() ? chars.get(j) : "";
+            answer += i < numbers.size() ? numbers.get(i) : "";
+        }
+        return answer;
     }
 }
