64c798418ba49df2c804fb116b15a1a00aa2947e
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-04-20 00:06:38 -0500

    Skeleton for problem 1415

---
+++ src/main/java/leetcode/Problem1415.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/
+ */
+public class Problem1415 {
+    public String getHappyString(int n, int k) {
+        // TODO
+        return "";
+    }
+
+    public static void main(String[] args) {
+        Problem1415 prob = new Problem1415();
+        System.out.println(prob.getHappyString(1, 3)); // "c"
+        System.out.println(prob.getHappyString(1, 4)); // ""
+        System.out.println(prob.getHappyString(3, 9)); // "cab"
+        System.out.println(prob.getHappyString(2, 7)); // ""
+        System.out.println(prob.getHappyString(10, 100)); // "abacbabacb"
+    }
+}
