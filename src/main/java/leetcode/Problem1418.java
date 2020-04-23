f8d2868e186a813e0f8faf4fd05129e97e6291ce
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-04-23 01:08:14 -0500

    Solve problem 1418

--- src/main/java/leetcode/Problem1418.java
+++ src/main/java/leetcode/Problem1418.java
@@ -1,41 +1,43 @@
 package leetcode;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 /**
  * https://leetcode.com/problems/display-table-of-food-orders-in-a-restaurant/
  */
 public class Problem1418 {
     public List<List<String>> displayTable(List<List<String>> orders) {
-        // TODO
-        return null;
-    }
-
-    public static void main(String[] args) {
-        Problem1418 prob = new Problem1418();
-        System.out.println(prob.displayTable(
-            Arrays.asList(
-                Arrays.asList("David","3","Ceviche"),
-                Arrays.asList("Corina","10","Beef Burrito"),
-                Arrays.asList("David","3","Fried Chicken"),
-                Arrays.asList("Carla","5","Water"),
-                Arrays.asList("Carla","5","Ceviche"),
-                Arrays.asList("Rous","3","Ceviche")
-            ))); // [["Table","Beef Burrito","Ceviche","Fried Chicken","Water"],["3","0","2","1","0"],["5","0","1","0","1"],["10","1","0","0","0"]]
-        System.out.println(prob.displayTable(
-            Arrays.asList(
-                Arrays.asList("James","12","Fried Chicken"),
-                Arrays.asList("Ratesh","12","Fried Chicken"),
-                Arrays.asList("Amadeus","12","Fried Chicken"),
-                Arrays.asList("Adam","1","Canadian Waffles"),
-                Arrays.asList("Brianna","1","Canadian Waffles")
-            ))); // [["Table","Canadian Waffles","Fried Chicken"],["1","2","0"],["12","0","3"]]
-        System.out.println(prob.displayTable(
-            Arrays.asList(
-                Arrays.asList("Laura","2","Bean Burrito"),
-                Arrays.asList("Jhon","2","Beef Burrito"),
-                Arrays.asList("Melissa","2","Soda")
-            ))); // [["Table","Bean Burrito","Beef Burrito","Soda"],["2","1","1","1"]]
+        TreeMap</* table */ Integer,
+            TreeMap</* food */ String, /* count */ Integer>> map = new TreeMap<>();
+        TreeSet<String> foodItems = new TreeSet<>();
+        for (List<String> order : orders) {
+            Integer table = Integer.valueOf(order.get(1));
+            String food = order.get(2);
+            foodItems.add(food);
+            if (!map.containsKey(table)) {
+                map.put(table, new TreeMap<>());
+            }
+            map.get(table).put(food, map.get(table).getOrDefault(food, 0) + 1);
+        }
+        List<List<String>> answer = new ArrayList<>();
+        List<String> header = new ArrayList<>();
+        header.add("Table");
+        for (String food : foodItems) {
+            header.add(food);
+        }
+        answer.add(header);
+        for (Map.Entry<Integer, TreeMap<String, Integer>> e : map.entrySet()) {
+            List<String> row = new ArrayList<>();
+            row.add("" + e.getKey()); // table
+            for (int i = 1; i < header.size(); i++) {
+                row.add("" + e.getValue().getOrDefault(header.get(i), 0)); // count
+            }
+            answer.add(row);
+        }
+        return answer;
     }
 }
