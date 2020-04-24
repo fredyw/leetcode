6b071bf8ed96d787b9ef463e7cdfbc9df9b17700
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-04-23 23:51:29 -0500

    Skeleton for problem 1405

---
+++ src/main/java/leetcode/Problem1405.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/longest-happy-string/
+ */
+public class Problem1405 {
+    public String longestDiverseString(int a, int b, int c) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem1405 prob = new Problem1405();
+        System.out.println(prob.longestDiverseString(1, 1, 7)); // "ccaccbcc"
+        System.out.println(prob.longestDiverseString(2, 2, 1)); // "aabbc"
+        System.out.println(prob.longestDiverseString(7, 1, 0)); // "aabaa"
+    }
+}
