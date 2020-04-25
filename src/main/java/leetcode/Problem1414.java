c7186aabd9fc3b003a6328b94066be2c14f4cc36
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-04-25 12:08:38 -0500

    Update problem 1414

--- src/main/java/leetcode/Problem1414.java
+++ src/main/java/leetcode/Problem1414.java
@@ -5,8 +5,24 @@ package leetcode;
  */
 public class Problem1414 {
     public int findMinFibonacciNumbers(int k) {
-        // TODO
-        return 0;
+        Integer[] memo = new Integer[k + 1];
+        memo[1] = 1;
+        memo[2] = 1;
+        fibonacci(k, memo);
+        int answer = Integer.MAX_VALUE;
+        // TODO:
+        return answer;
+    }
+
+    private static int fibonacci(int n, Integer[] memo) {
+        if (n <= 2) {
+            return 1;
+        }
+        if (memo[n] != null) {
+            return memo[n];
+        }
+        memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo);
+        return memo[n];
     }
 
     public static void main(String[] args) {
