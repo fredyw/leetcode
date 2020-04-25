8218aa8fbdf058db996a1796a5b8f379d99d03f1
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-04-24 22:21:37 -0500

    Solve problem 1405

--- src/main/java/leetcode/Problem1405.java
+++ src/main/java/leetcode/Problem1405.java
@@ -1,20 +1,56 @@
 package leetcode;
 
+import java.util.PriorityQueue;
+
 /**
  * https://leetcode.com/problems/longest-happy-string/
  */
 public class Problem1405 {
     public String longestDiverseString(int a, int b, int c) {
-        // TODO
-        return null;
+        PriorityQueue<CharCount> queue = new PriorityQueue<>(
+            (e1, e2) -> Integer.compare(e2.count, e1.count));
+        if (a > 0) {
+            queue.add(new CharCount('a', a));
+        }
+        if (b > 0) {
+            queue.add(new CharCount('b', b));
+        }
+        if (c > 0) {
+            queue.add(new CharCount('c', c));
+        }
+        StringBuilder answer = new StringBuilder();
+        CharCount prev = null;
+        while (!queue.isEmpty()) {
+            CharCount current = queue.poll();
+            if (prev != null && current.ch == prev.ch && prev.count == 2) {
+                if (queue.isEmpty()) {
+                    break;
+                }
+                CharCount next = queue.poll();
+                queue.add(current);
+                current = next;
+            }
+            answer.append(current.ch);
+            current.count--;
+            if (current.count > 0) {
+                queue.add(current);
+            }
+            if (prev != null && prev.ch == current.ch) {
+                prev.count++;
+            } else {
+                prev = new CharCount(current.ch, 1);
+            }
+        }
+        return answer.toString();
     }
 
-    public static void main(String[] args) {
-        Problem1405 prob = new Problem1405();
-        System.out.println(prob.longestDiverseString(1, 1, 7)); // "ccaccbcc"
-        System.out.println(prob.longestDiverseString(1, 1, 10)); // "ccaccbcc"
-        System.out.println(prob.longestDiverseString(2, 2, 1)); // "aabbc"
-        System.out.println(prob.longestDiverseString(7, 1, 0)); // "aabaa"
-        System.out.println(prob.longestDiverseString(5, 6, 7)); // "ccbbaaccbbaaccbbac"
+    private static class CharCount {
+        private final char ch;
+        private int count;
+
+        public CharCount(char ch, int count) {
+            this.ch = ch;
+            this.count = count;
+        }
     }
 }
