716398f123e060a6c29e628f80c7dd977245d756
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-04-25 11:27:27 -0500

    Skeleton for problem 1414

---
+++ src/main/java/leetcode/Problem1414.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k/
+ */
+public class Problem1414 {
+    public int findMinFibonacciNumbers(int k) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1414 prob = new Problem1414();
+        System.out.println(prob.findMinFibonacciNumbers(7)); // 2
+        System.out.println(prob.findMinFibonacciNumbers(10)); // 2
+        System.out.println(prob.findMinFibonacciNumbers(19)); // 3
+    }
+}
