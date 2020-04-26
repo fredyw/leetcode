7402d325ebab41f2fdfbc800aff34a62977fbe9c
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-04-26 00:36:27 -0500

    Skeleton for problem 1422

---
+++ src/main/java/leetcode/Problem1422.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/maximum-score-after-splitting-a-string/
+ */
+public class Problem1422 {
+    public int maxScore(String s) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1422 prob = new Problem1422();
+        System.out.println(prob.maxScore("011101")); // 5
+        System.out.println(prob.maxScore("00111")); // 5
+        System.out.println(prob.maxScore("1111")); // 3
+    }
+}
