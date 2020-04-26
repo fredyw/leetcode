372dbb612cba9ddcc691ed7711e45c525cde7fd4
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-04-26 01:09:03 -0500

    Solve problem 1422

--- src/main/java/leetcode/Problem1422.java
+++ src/main/java/leetcode/Problem1422.java
@@ -5,14 +5,26 @@ package leetcode;
  */
 public class Problem1422 {
     public int maxScore(String s) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem1422 prob = new Problem1422();
-        System.out.println(prob.maxScore("011101")); // 5
-        System.out.println(prob.maxScore("00111")); // 5
-        System.out.println(prob.maxScore("1111")); // 3
+        int ones = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '1') {
+                ones++;
+            }
+        }
+        int answer = 0;
+        int left = 0;
+        int right = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '0' && i == s.length() - 1) {
+                continue;
+            }
+            if (s.charAt(i) == '0') {
+                left++;
+            } else if (s.charAt(i) == '1') {
+                right++;
+            }
+            answer = Math.max(answer, Math.abs(left + (ones - right)));
+        }
+        return answer;
     }
 }
