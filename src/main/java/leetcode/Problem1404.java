430dd37eeae2e95513d3a4ddc1ce89a110cd783d
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-04-29 01:56:19 -0500

    Skeleton for problem 1404

---
+++ src/main/java/leetcode/Problem1404.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/number-of-steps-to-reduce-a-number-in-binary-representation-to-one/
+ */
+public class Problem1404 {
+    public int numSteps(String s) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1404 prob = new Problem1404();
+        System.out.println(prob.numSteps("1101")); // 6
+        System.out.println(prob.numSteps("10")); // 1
+        System.out.println(prob.numSteps("1")); // 0
+    }
+}
