fc455e27040c6160cb4d3d2f28a4471810e77ce4
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-04-30 01:07:54 -0500

    Solve problem 1404

--- src/main/java/leetcode/Problem1404.java
+++ src/main/java/leetcode/Problem1404.java
@@ -1,18 +1,22 @@
 package leetcode;
 
+import java.math.BigInteger;
+
 /**
  * https://leetcode.com/problems/number-of-steps-to-reduce-a-number-in-binary-representation-to-one/
  */
 public class Problem1404 {
     public int numSteps(String s) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem1404 prob = new Problem1404();
-        System.out.println(prob.numSteps("1101")); // 6
-        System.out.println(prob.numSteps("10")); // 1
-        System.out.println(prob.numSteps("1")); // 0
+        int answer = 0;
+        BigInteger n = new BigInteger(s, 2);
+        while (!n.equals(BigInteger.ONE)) {
+            if (n.mod(BigInteger.valueOf(2)).equals(BigInteger.ZERO)) {
+                n = n.divide(BigInteger.valueOf(2));
+            } else {
+                n = n.add(BigInteger.ONE);
+            }
+            answer++;
+        }
+        return answer;
     }
 }
