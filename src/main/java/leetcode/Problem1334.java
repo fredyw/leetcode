559542e874f2f2523ba97f9042c6f85a25ef2283
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-05-03 01:07:36 -0500

    Skeleton for problem 1334

--- src/main/java/leetcode/Problem1334.java
+++ src/main/java/leetcode/Problem1334.java
@@ -1,4 +1,15 @@
 package leetcode;
 
+/**
+ * https://leetcode.com/problems/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/
+ */
 public class Problem1334 {
+    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1334 prob = new Problem1334();
+    }
 }
