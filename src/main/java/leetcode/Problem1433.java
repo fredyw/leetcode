1e8e623429fba399fc22d6615042e004028d9c72
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-05-03 00:37:06 -0500

    Solve problem 1433

--- src/main/java/leetcode/Problem1433.java
+++ src/main/java/leetcode/Problem1433.java
@@ -1,18 +1,26 @@
 package leetcode;
 
+import java.util.Arrays;
+
 /**
  * https://leetcode.com/problems/check-if-a-string-can-break-another-string/
  */
 public class Problem1433 {
     public boolean checkIfCanBreak(String s1, String s2) {
-        // TODO
-        return false;
-    }
-
-    public static void main(String[] args) {
-        Problem1433 prob = new Problem1433();
-        System.out.println(prob.checkIfCanBreak("abc", "xya")); // true
-        System.out.println(prob.checkIfCanBreak("abe", "acd")); // false
-        System.out.println(prob.checkIfCanBreak("leetcodee", "interview")); // true
+        char[] c1 = s1.toCharArray();
+        Arrays.sort(c1);
+        char[] c2 = s2.toCharArray();
+        Arrays.sort(c2);
+        boolean b1 = true;
+        boolean b2 = true;
+        for (int i = 0; i < c1.length; i++) {
+            if (c1[i] > c2[i]) {
+                b1 = false;
+            }
+            if (c2[i] > c1[i]) {
+                b2 = false;
+            }
+        }
+        return b1 | b2;
     }
 }
