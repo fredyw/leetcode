fbee1f78d65ea64fb98d5fb37c4a822a483aa762
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-05-06 17:04:42 -0500

    Solve problem 1334

--- src/main/java/leetcode/Problem1334.java
+++ src/main/java/leetcode/Problem1334.java
@@ -1,7 +1,5 @@
 package leetcode;
 
-import java.util.Arrays;
-
 /**
  * https://leetcode.com/problems/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/
  */
@@ -23,7 +21,6 @@ public class Problem1334 {
             graph[from][to] = weight;
             graph[to][from] = weight;
         }
-        int answer = 0;
         for (int k = 0; k < n; k++) {
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
@@ -35,19 +32,25 @@ public class Problem1334 {
                 }
             }
         }
-        for (int[] edge : graph) {
-            System.out.println(Arrays.toString(edge));
+        int answer = 0;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+            for (int j = 0; j < n; j++) {
+                if (i == j) {
+                    continue;
+                }
+                if (graph[i][j] <= distanceThreshold) {
+                    count++;
+                }
+            }
+            if (min > count) {
+                min = count;
+                answer = i;
+            } else if (min == count) {
+                answer = Math.max(answer, i);
+            }
         }
         return answer;
-    }
-
-    public static void main(String[] args) {
-        Problem1334 prob = new Problem1334();
-        System.out.println(prob.findTheCity(4, new int[][]{
-            {0, 1, 3}, {1, 2, 1}, {1, 3, 4}, {2, 3, 1},
-        }, 4)); // 3
-//        System.out.println(prob.findTheCity(5, new int[][]{
-//            {0, 1, 2}, {0, 4, 8}, {1, 2, 3}, {1, 4, 2}, {2, 3, 1}, {3, 4, 1},
-//        }, 2)); // 0
     }
 }
