bb5c1f36cac294cc066dc975a1b44c5453f5d168
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-05-16 12:17:56 -0500

    Solve problem 1446

--- src/main/java/leetcode/Problem1446.java
+++ src/main/java/leetcode/Problem1446.java
@@ -5,16 +5,16 @@ package leetcode;
  */
 public class Problem1446 {
     public int maxPower(String s) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem1446 prob = new Problem1446();
-        System.out.println(prob.maxPower("leetcode")); // 2
-        System.out.println(prob.maxPower("abbcccddddeeeeedcba")); // 5
-        System.out.println(prob.maxPower("triplepillooooow")); // 5
-        System.out.println(prob.maxPower("hooraaaaaaaaaaay")); // 11
-        System.out.println(prob.maxPower("tourist")); // 1
+        int answer = 1;
+        int tmp = 1;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i - 1) == s.charAt(i)) {
+                tmp++;
+            } else {
+                tmp = 1;
+            }
+            answer = Math.max(answer, tmp);
+        }
+        return answer;
     }
 }
