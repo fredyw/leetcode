464859245bd1539cc27fea8555e6c2d4b9c799c9
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-05-16 20:20:17 -0500

    Skeleton for problem 1352

---
+++ src/main/java/leetcode/Problem1352.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/product-of-the-last-k-numbers/
+ */
+public class Problem1352 {
+    static class ProductOfNumbers {
+        public ProductOfNumbers() {
+        }
+
+        public void add(int num) {
+            // TODO
+        }
+
+        public int getProduct(int k) {
+            // TODO
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}
