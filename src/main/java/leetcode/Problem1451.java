7e654b046b0e90348567adbb678ca877346f9473
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-05-19 09:32:19 -0500

    Skeleton for problem 1451

---
+++ src/main/java/leetcode/Problem1451.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/rearrange-words-in-a-sentence/
+ */
+public class Problem1451 {
+    public String arrangeWords(String text) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem1451 prob = new Problem1451();
+        System.out.println(prob.arrangeWords("Leetcode is cool")); // "Is cool leetcode"
+        System.out.println(prob.arrangeWords("Keep calm and code on")); // "On and keep calm code"
+        System.out.println(prob.arrangeWords("To be or not to be")); // "To be or to be not"
+    }
+}
