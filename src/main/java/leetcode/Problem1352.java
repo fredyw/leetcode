025cddd92356103fabcfaeaa7ff67b2808d02294
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-05-18 23:37:37 -0500

    Update problem 1352

--- src/main/java/leetcode/Problem1352.java
+++ src/main/java/leetcode/Problem1352.java
@@ -19,5 +19,16 @@ public class Problem1352 {
     }
 
     public static void main(String[] args) {
+        ProductOfNumbers productOfNumbers = new ProductOfNumbers();
+        productOfNumbers.add(3); // [3]
+        productOfNumbers.add(0); // [3,0]
+        productOfNumbers.add(2); // [3,0,2]
+        productOfNumbers.add(5); // [3,0,2,5]
+        productOfNumbers.add(4); // [3,0,2,5,4]
+        System.out.println(productOfNumbers.getProduct(2)); // return 20. The product of the last 2 numbers is 5 * 4 = 20
+        System.out.println(productOfNumbers.getProduct(3)); // return 40. The product of the last 3 numbers is 2 * 5 * 4 = 40
+        System.out.println(productOfNumbers.getProduct(4)); // return 0. The product of the last 4 numbers is 0 * 2 * 5 * 4 = 0
+        productOfNumbers.add(8); // [3,0,2,5,4,8]
+        System.out.println(productOfNumbers.getProduct(2)); // return 32. The product of the last 2 numbers is 4 * 8 = 32
     }
 }
