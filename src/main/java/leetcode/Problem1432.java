2c33068563deb18eca504c730bdd960520a84c79
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-05-23 01:32:20 -0500

    Skeleton for problem 1432

---
+++ src/main/java/leetcode/Problem1432.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/max-difference-you-can-get-from-changing-an-integer/
+ */
+public class Problem1432 {
+    public int maxDiff(int num) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1432 prob = new Problem1432();
+        System.out.println(prob.maxDiff(555)); // 888
+        System.out.println(prob.maxDiff(9)); // 8
+        System.out.println(prob.maxDiff(123456)); // 820000
+        System.out.println(prob.maxDiff(10000)); // 80000
+        System.out.println(prob.maxDiff(9288)); // 8700
+    }
+}
