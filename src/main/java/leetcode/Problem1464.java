2ef6b687f9cd75f1b461a0dad26f6239e8079239
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-05-31 20:05:18 -0500

    Skeleton for problem 1464

---
+++ src/main/java/leetcode/Problem1464.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/maximum-product-of-two-elements-in-an-array/
+ */
+public class Problem1464 {
+    public int maxProduct(int[] nums) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1464 prob = new Problem1464();
+        System.out.println(prob.maxProduct(new int[]{3,4,5,2})); // 12
+        System.out.println(prob.maxProduct(new int[]{1,5,4,5})); // 16
+        System.out.println(prob.maxProduct(new int[]{3,7})); // 12
+    }
+}
