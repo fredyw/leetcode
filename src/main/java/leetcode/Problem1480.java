89deaacb143c721b8b209e7674acfa99bf706756
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-06-14 22:47:36 -0500

    Solve problem 1480

--- src/main/java/leetcode/Problem1480.java
+++ src/main/java/leetcode/Problem1480.java
@@ -7,14 +7,12 @@ import java.util.Arrays;
  */
 public class Problem1480 {
     public int[] runningSum(int[] nums) {
-        // TODO
-        return null;
-    }
-
-    public static void main(String[] args) {
-        Problem1480 prob = new Problem1480();
-        System.out.println(Arrays.toString(prob.runningSum(new int[]{1,2,3,4}))); // [1,3,6,10]
-        System.out.println(Arrays.toString(prob.runningSum(new int[]{1,1,1,1,1}))); // [1,2,3,4,5]
-        System.out.println(Arrays.toString(prob.runningSum(new int[]{3,1,2,10,1}))); // [3,4,6,16,17]
+        int[] answer = new int[nums.length];
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            answer[i] = sum;
+        }
+        return answer;
     }
 }
