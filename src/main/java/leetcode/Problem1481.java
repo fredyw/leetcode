8559efe9d710c633f526ab588e51b914017f52a0
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-06-20 18:55:41 -0500

    Skeleton for problem 1481

---
+++ src/main/java/leetcode/Problem1481.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/least-number-of-unique-integers-after-k-removals/
+ */
+public class Problem1481 {
+    public int findLeastNumOfUniqueInts(int[] arr, int k) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1481 prob = new Problem1481();
+        System.out.println(prob.findLeastNumOfUniqueInts(new int[]{5,5,4}, 1)); // 1
+        System.out.println(prob.findLeastNumOfUniqueInts(new int[]{4,3,1,1,3,3,2}, 3)); // 2
+    }
+}
