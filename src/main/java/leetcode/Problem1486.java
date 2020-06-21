c86d84007e696e6fef49b86b59d2a2670c872d4a
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-06-21 11:47:33 -0500

    Solve problem 1486

--- src/main/java/leetcode/Problem1486.java
+++ src/main/java/leetcode/Problem1486.java
@@ -5,15 +5,10 @@ package leetcode;
  */
 public class Problem1486 {
     public int xorOperation(int n, int start) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem1486 prob = new Problem1486();
-        System.out.println(prob.xorOperation(5, 0)); // 8
-        System.out.println(prob.xorOperation(4, 3)); // 8
-        System.out.println(prob.xorOperation(1, 7)); // 7
-        System.out.println(prob.xorOperation(10, 5)); // 2
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            answer ^= start + (2 * i);
+        }
+        return answer;
     }
 }
