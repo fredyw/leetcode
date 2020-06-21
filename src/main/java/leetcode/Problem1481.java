7a2cd3acafad37ef0f6b4949f7559850ba353f9e
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-06-20 19:06:36 -0500

    Update problem 1481

--- src/main/java/leetcode/Problem1481.java
+++ src/main/java/leetcode/Problem1481.java
@@ -12,6 +12,10 @@ public class Problem1481 {
     public static void main(String[] args) {
         Problem1481 prob = new Problem1481();
         System.out.println(prob.findLeastNumOfUniqueInts(new int[]{5,5,4}, 1)); // 1
+        System.out.println(prob.findLeastNumOfUniqueInts(new int[]{5,5,4}, 2)); // 1
+        System.out.println(prob.findLeastNumOfUniqueInts(new int[]{5,5,4,6}, 2)); // 1
+        System.out.println(prob.findLeastNumOfUniqueInts(new int[]{5,5,4,6}, 3)); // 1
+        System.out.println(prob.findLeastNumOfUniqueInts(new int[]{5,5,4,4,6}, 4)); // 1
         System.out.println(prob.findLeastNumOfUniqueInts(new int[]{4,3,1,1,3,3,2}, 3)); // 2
     }
 }
