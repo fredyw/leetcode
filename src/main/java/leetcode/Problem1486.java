462262c8f498e42fe845219cc3d03f676f9bd0e4
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-06-21 11:45:10 -0500

    Skeleton for problem 1486

---
+++ src/main/java/leetcode/Problem1486.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/xor-operation-in-an-array/
+ */
+public class Problem1486 {
+    public int xorOperation(int n, int start) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1486 prob = new Problem1486();
+        System.out.println(prob.xorOperation(5, 0)); // 8
+        System.out.println(prob.xorOperation(4, 3)); // 8
+        System.out.println(prob.xorOperation(1, 7)); // 7
+        System.out.println(prob.xorOperation(10, 5)); // 2
+    }
+}
