12e52892717bdf1cf304614f36d668f2749b06da
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-06-27 20:58:40 -0500

    Solve problem 1492

--- src/main/java/leetcode/Problem1492.java
+++ src/main/java/leetcode/Problem1492.java
@@ -5,16 +5,15 @@ package leetcode;
  */
 public class Problem1492 {
     public int kthFactor(int n, int k) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem1492 prob = new Problem1492();
-        System.out.println(prob.kthFactor(12, 3)); // 3
-        System.out.println(prob.kthFactor(7, 2)); // 7
-        System.out.println(prob.kthFactor(4, 4)); // -1
-        System.out.println(prob.kthFactor(1, 1)); // 1
-        System.out.println(prob.kthFactor(1000, 3)); // 4
+        int j = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                j++;
+                if (j == k) {
+                    return i;
+                }
+            }
+        }
+        return -1;
     }
 }
