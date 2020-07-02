202fc61f76bb8ba9d4c17adf62258f55d0dd3eec
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-07-02 10:17:02 -0500

    Skeleton for problem 1496

---
+++ src/main/java/leetcode/Problem1496.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/path-crossing/
+ */
+public class Problem1496 {
+    public boolean isPathCrossing(String path) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem1496 prob = new Problem1496();
+        System.out.println(prob.isPathCrossing("NES")); // false
+        System.out.println(prob.isPathCrossing("NESWW")); // true
+    }
+}
