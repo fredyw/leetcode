d826e1026854009afef87a72064e05afa2bf1d50
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-07-05 12:40:40 -0500

    Skeleton for problem 1502

---
+++ src/main/java/leetcode/Problem1502.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/can-make-arithmetic-progression-from-sequence/
+ */
+public class Problem1502 {
+    public boolean canMakeArithmeticProgression(int[] arr) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem1502 prob = new Problem1502();
+        System.out.println(prob.canMakeArithmeticProgression(new int[]{3,5,1})); // true
+        System.out.println(prob.canMakeArithmeticProgression(new int[]{1,2,4})); // false
+    }
+}
