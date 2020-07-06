41eb1d553c561b44af6eb490d19034703ff296d6
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-07-05 23:12:27 -0500

    Solve problem 1493

--- src/main/java/leetcode/Problem1493.java
+++ src/main/java/leetcode/Problem1493.java
@@ -6,20 +6,24 @@ package leetcode;
 public class Problem1493 {
     public int longestSubarray(int[] nums) {
         int answer = 0;
+        int prevLen = 0;
+        int curLen = 0;
+        int nZeros = 0;
+        boolean addPrev = false;
         for (int i = 0; i < nums.length; i++) {
-            // TODO
+            if (nums[i] == 1) {
+                curLen++;
+                nZeros = 0;
+            } else {
+                if (i - 1 >= 0 && nums[i - 1] == 1) {
+                    prevLen = curLen;
+                    curLen = 0;
+                }
+                nZeros++;
+                addPrev = nZeros == 1;
+            }
+            answer = Math.max(answer, addPrev ? prevLen + curLen : curLen);
         }
-        return answer;
-    }
-
-    public static void main(String[] args) {
-        Problem1493 prob = new Problem1493();
-        System.out.println(prob.longestSubarray(new int[]{1,1,0,1})); // 3
-        System.out.println(prob.longestSubarray(new int[]{0,1,1,1,0,1,1,0,1})); // 5
-        System.out.println(prob.longestSubarray(new int[]{0,1,1,0,1,1,1,0,1})); // 5
-        System.out.println(prob.longestSubarray(new int[]{0,1,1,0,1,1,1,0,1,1,1})); // 6
-        System.out.println(prob.longestSubarray(new int[]{1,1,1})); // 2
-        System.out.println(prob.longestSubarray(new int[]{1,1,0,0,1,1,1,0,1})); // 4
-        System.out.println(prob.longestSubarray(new int[]{0,0,0})); // 0
+        return answer == nums.length ? answer - 1 : answer;
     }
 }
