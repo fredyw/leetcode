5098e5e6e1781d5c0d5080f97a066507491c8e0c
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-07-05 20:55:10 -0500

    Update problem 1493

--- src/main/java/leetcode/Problem1493.java
+++ src/main/java/leetcode/Problem1493.java
@@ -5,14 +5,18 @@ package leetcode;
  */
 public class Problem1493 {
     public int longestSubarray(int[] nums) {
-        // TODO
-        return 0;
+        int answer = 0;
+        for (int i = 0; i < nums.length; i++) {
+            // TODO
+        }
+        return answer;
     }
 
     public static void main(String[] args) {
         Problem1493 prob = new Problem1493();
         System.out.println(prob.longestSubarray(new int[]{1,1,0,1})); // 3
         System.out.println(prob.longestSubarray(new int[]{0,1,1,1,0,1,1,0,1})); // 5
+        System.out.println(prob.longestSubarray(new int[]{0,1,1,0,1,1,1,0,1})); // 5
         System.out.println(prob.longestSubarray(new int[]{1,1,1})); // 2
         System.out.println(prob.longestSubarray(new int[]{1,1,0,0,1,1,1,0,1})); // 4
         System.out.println(prob.longestSubarray(new int[]{0,0,0})); // 0
