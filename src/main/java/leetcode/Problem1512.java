8a485ba53c11ebb696d0cb02e7a74cfd52cfc985
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-07-12 11:00:23 -0500

    Solve problem 1512

--- src/main/java/leetcode/Problem1512.java
+++ src/main/java/leetcode/Problem1512.java
@@ -1,19 +1,23 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * https://leetcode.com/problems/number-of-good-pairs/
  */
 public class Problem1512 {
     public int numIdenticalPairs(int[] nums) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem1512 prob = new Problem1512();
-        System.out.println(prob.numIdenticalPairs(new int[]{1,2,3,1,1,3})); // 4
-        System.out.println(prob.numIdenticalPairs(new int[]{1,1,1,1})); // 6
-        System.out.println(prob.numIdenticalPairs(new int[]{1,1,1,1,2,2})); // 7
-        System.out.println(prob.numIdenticalPairs(new int[]{1,2,3})); // 0
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        int answer = 0;
+        for (int count : map.values()) {
+            for (int i = 1; i < count; i++) {
+                answer += i;
+            }
+        }
+        return answer;
     }
 }
