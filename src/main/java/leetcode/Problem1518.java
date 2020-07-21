2e211cb59eee0aba71dc77a39e233f2c4b2f9abe
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-07-20 23:57:56 -0500

    Skeleton for problem 1518

---
+++ src/main/java/leetcode/Problem1518.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/water-bottles/
+ */
+public class Problem1518 {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1518 prob = new Problem1518();
+        System.out.println(prob.numWaterBottles(9, 3)); // 13
+        System.out.println(prob.numWaterBottles(15, 4)); // 19
+        System.out.println(prob.numWaterBottles(5, 5)); // 6
+        System.out.println(prob.numWaterBottles(2, 3)); // 2
+    }
+}
