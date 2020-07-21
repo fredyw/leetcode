07c010581177cc421a8d5da2cbac954fe4dc7968
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-07-21 00:12:07 -0500

    Solve problem 1518

--- src/main/java/leetcode/Problem1518.java
+++ src/main/java/leetcode/Problem1518.java
@@ -5,15 +5,16 @@ package leetcode;
  */
 public class Problem1518 {
     public int numWaterBottles(int numBottles, int numExchange) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem1518 prob = new Problem1518();
-        System.out.println(prob.numWaterBottles(9, 3)); // 13
-        System.out.println(prob.numWaterBottles(15, 4)); // 19
-        System.out.println(prob.numWaterBottles(5, 5)); // 6
-        System.out.println(prob.numWaterBottles(2, 3)); // 2
+        int answer = 0;
+        while (numBottles > 0) {
+            numBottles -= numExchange;
+            if (numBottles < 0) {
+                answer += numExchange + numBottles;
+            } else {
+                answer += numExchange;
+            }
+            numBottles++;
+        }
+        return answer;
     }
 }
