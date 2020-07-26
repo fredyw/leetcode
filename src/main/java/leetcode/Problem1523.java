364659f5f09d89e5a9eccbcaa5a99f6b073caa44
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-07-26 12:55:55 -0500

    Solve problem 1523

---
+++ src/main/java/leetcode/Problem1523.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/
+ */
+public class Problem1523 {
+    public int countOdds(int low, int high) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1523 prob = new Problem1523();
+        System.out.println(prob.countOdds(3, 7)); // 3
+        System.out.println(prob.countOdds(8, 10)); // 1
+    }
+}
