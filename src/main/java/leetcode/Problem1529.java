700558357a6797c25d8dffc254497a39ca1dd4b0
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-07-26 21:21:29 -0500

    Skeleton for problem 1529

---
+++ src/main/java/leetcode/Problem1529.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/bulb-switcher-iv/
+ */
+public class Problem1529 {
+    public int minFlips(String target) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1529 prob = new Problem1529();
+        System.out.println(prob.minFlips("10111")); // 3
+        System.out.println(prob.minFlips("101")); // 3
+        System.out.println(prob.minFlips("00000")); // 0
+        System.out.println(prob.minFlips("001011101")); // 5
+    }
+}
