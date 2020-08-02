c7b59bc8ac939e29bf7c96aa195f64ddd5bb5017
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-08-02 14:37:00 -0500

    Skeleton for problem 1534

---
+++ src/main/java/leetcode/Problem1534.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/count-good-triplets/
+ */
+public class Problem1534 {
+    public int countGoodTriplets(int[] arr, int a, int b, int c) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1534 prob = new Problem1534();
+        System.out.println(prob.countGoodTriplets(new int[]{3,0,1,1,9,7}, 7, 2, 3)); // 4
+        System.out.println(prob.countGoodTriplets(new int[]{1,1,2,2,3}, 0, 0, 1)); // 0
+    }
+}
