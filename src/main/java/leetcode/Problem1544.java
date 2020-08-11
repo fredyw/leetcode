8a288353b9ae8eaf637ae103fa18b48df0505146
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-08-10 20:18:34 -0500

    Skeleton for problem 1544

---
+++ src/main/java/leetcode/Problem1544.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/make-the-string-great/
+ */
+public class Problem1544 {
+    public String makeGood(String s) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem1544 prob = new Problem1544();
+        System.out.println(prob.makeGood("leEeetcode")); // "leetcode"
+        System.out.println(prob.makeGood("abBAcC")); // ""
+        System.out.println(prob.makeGood("s")); // "s"
+    }
+}
