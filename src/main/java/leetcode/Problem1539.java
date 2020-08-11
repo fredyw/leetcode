f9ae2fb00857fd415c7a5b76632b112f8545b30d
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-08-10 20:39:05 -0500

    Skeleton for problem 1539

---
+++ src/main/java/leetcode/Problem1539.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/kth-missing-positive-number/
+ */
+public class Problem1539 {
+    public int findKthPositive(int[] arr, int k) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1539 prob = new Problem1539();
+        System.out.println(prob.findKthPositive(new int[]{2,3,4,7,11}, 5)); // 9
+        System.out.println(prob.findKthPositive(new int[]{1,2,3,4}, 2)); // 6
+    }
+}
