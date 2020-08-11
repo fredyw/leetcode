e9baa894aa9b4b21ca78c9794af0b62ebc441e70
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-08-10 20:36:56 -0500

    Update problem 1544

--- src/main/java/leetcode/Problem1544.java
+++ src/main/java/leetcode/Problem1544.java
@@ -10,33 +10,27 @@ public class Problem1544 {
         Stack<Character> stack = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
             stack.push(s.charAt(i));
-            if (stack.size() >= 2) {
-                char a = stack.pop();
-                char b = stack.pop();
-                String aStr = "" + a;
-                String bStr = "" + b;
-                if ((!bStr.equals(bStr.toLowerCase()) || !aStr.equals(bStr.toUpperCase())) &&
-                    (!bStr.equals(bStr.toUpperCase()) || !aStr.equals(bStr.toLowerCase()))) {
-                    stack.push(b);
-                    stack.push(a);
-                }
-            }
+            remove(stack);
         }
         if (stack.size() >= 2) {
-            char a = stack.pop();
-            char b = stack.pop();
-            String aStr = "" + a;
-            String bStr = "" + b;
-            if ((!bStr.equals(bStr.toLowerCase()) || !aStr.equals(bStr.toUpperCase())) &&
-                (!bStr.equals(bStr.toUpperCase()) || !aStr.equals(bStr.toLowerCase()))) {
-                stack.push(b);
-                stack.push(a);
-            }
+            remove(stack);
         }
         String answer = "";
         for (char c : stack) {
             answer += c;
         }
         return answer;
+    }
+
+    private static void remove(Stack<Character> stack) {
+        char a = stack.pop();
+        char b = stack.pop();
+        String aStr = "" + a;
+        String bStr = "" + b;
+        if ((!bStr.equals(bStr.toLowerCase()) || !aStr.equals(bStr.toUpperCase())) &&
+            (!bStr.equals(bStr.toUpperCase()) || !aStr.equals(bStr.toLowerCase()))) {
+            stack.push(b);
+            stack.push(a);
+        }
     }
 }
