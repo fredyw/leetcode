ba4d2b7ad169a12f0da094df4147fbab9a7be456
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-08-10 20:34:04 -0500

    Solve problem 1544

--- src/main/java/leetcode/Problem1544.java
+++ src/main/java/leetcode/Problem1544.java
@@ -1,18 +1,42 @@
 package leetcode;
 
+import java.util.Stack;
+
 /**
  * https://leetcode.com/problems/make-the-string-great/
  */
 public class Problem1544 {
     public String makeGood(String s) {
-        // TODO
-        return null;
-    }
-
-    public static void main(String[] args) {
-        Problem1544 prob = new Problem1544();
-        System.out.println(prob.makeGood("leEeetcode")); // "leetcode"
-        System.out.println(prob.makeGood("abBAcC")); // ""
-        System.out.println(prob.makeGood("s")); // "s"
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            stack.push(s.charAt(i));
+            if (stack.size() >= 2) {
+                char a = stack.pop();
+                char b = stack.pop();
+                String aStr = "" + a;
+                String bStr = "" + b;
+                if ((!bStr.equals(bStr.toLowerCase()) || !aStr.equals(bStr.toUpperCase())) &&
+                    (!bStr.equals(bStr.toUpperCase()) || !aStr.equals(bStr.toLowerCase()))) {
+                    stack.push(b);
+                    stack.push(a);
+                }
+            }
+        }
+        if (stack.size() >= 2) {
+            char a = stack.pop();
+            char b = stack.pop();
+            String aStr = "" + a;
+            String bStr = "" + b;
+            if ((!bStr.equals(bStr.toLowerCase()) || !aStr.equals(bStr.toUpperCase())) &&
+                (!bStr.equals(bStr.toUpperCase()) || !aStr.equals(bStr.toLowerCase()))) {
+                stack.push(b);
+                stack.push(a);
+            }
+        }
+        String answer = "";
+        for (char c : stack) {
+            answer += c;
+        }
+        return answer;
     }
 }
