5adfb40a7c988951449d54c1c866c4093fb2ecaf
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-08-10 23:39:48 -0500

    Solve problem 1539

--- src/main/java/leetcode/Problem1539.java
+++ src/main/java/leetcode/Problem1539.java
@@ -5,13 +5,19 @@ package leetcode;
  */
 public class Problem1539 {
     public int findKthPositive(int[] arr, int k) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem1539 prob = new Problem1539();
-        System.out.println(prob.findKthPositive(new int[]{2,3,4,7,11}, 5)); // 9
-        System.out.println(prob.findKthPositive(new int[]{1,2,3,4}, 2)); // 6
+        int count = 0;
+        int i = 1;
+        int j = 0;
+        while (true) {
+            if (j < arr.length && arr[j] == i) {
+                j++;
+            } else {
+                count++;
+                if (count == k) {
+                    return i;
+                }
+            }
+            i++;
+        }
     }
 }
