b42dd63d6594aa0e44bdcd5b757e0f3b59152c4a
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-08-18 01:01:54 -0500

    Solve problem 1550

--- src/main/java/leetcode/Problem1550.java
+++ src/main/java/leetcode/Problem1550.java
@@ -5,14 +5,17 @@ package leetcode;
  */
 public class Problem1550 {
     public boolean threeConsecutiveOdds(int[] arr) {
-        // TODO
-        return false;
-    }
-
-    public static void main(String[] args) {
-        Problem1550 prob = new Problem1550();
-        System.out.println(prob.threeConsecutiveOdds(new int[]{2,6,4,1})); // false
-        System.out.println(prob.threeConsecutiveOdds(new int[]{1,2,34,3,4,5,7,23,12})); // true
-        System.out.println(prob.threeConsecutiveOdds(new int[]{1,2,3,4,5})); // false
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 != 0) {
+                count++;
+            } else {
+                if (count >= 3) {
+                    return true;
+                }
+                count = 0;
+            }
+        }
+        return count >= 3;
     }
 }
