ea5726e1190b3aa2de2eb1793e62e3ab72eca11b
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-08-27 00:21:25 -0500

    Skeleton for problem 1551

---
+++ src/main/java/leetcode/Problem1551.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/minimum-operations-to-make-array-equal/
+ */
+public class Problem1551 {
+    public int minOperations(int n) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1551 prob = new Problem1551();
+        System.out.println(prob.minOperations(3)); // 2
+        System.out.println(prob.minOperations(6)); // 9
+    }
+}
