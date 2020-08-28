c2225c2605131df6c86fd78229eb6e922825be16
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-08-27 23:54:05 -0500

    Solve problem 1551

--- src/main/java/leetcode/Problem1551.java
+++ src/main/java/leetcode/Problem1551.java
@@ -5,13 +5,20 @@ package leetcode;
  */
 public class Problem1551 {
     public int minOperations(int n) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem1551 prob = new Problem1551();
-        System.out.println(prob.minOperations(3)); // 2
-        System.out.println(prob.minOperations(6)); // 9
+        int answer = 0;
+        int m = n / 2;
+        int[] array = new int[n];
+        array[0] = 1;
+        for (int i = 1; i <= m; i++) {
+            array[i] = array[i - 1] + 2;
+        }
+        int mid = array[m];
+        if (n % 2 == 0) {
+            mid--;
+        }
+        for (int i = 0; i < m; i++) {
+            answer += mid - array[i];
+        }
+        return answer;
     }
 }
