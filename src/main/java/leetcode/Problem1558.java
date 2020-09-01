f778f506a0ffbe4a28bb26b7f6ccb57ad7d92997
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-08-31 23:48:36 -0500

    Update problem 1558

--- src/main/java/leetcode/Problem1558.java
+++ src/main/java/leetcode/Problem1558.java
@@ -1,12 +1,30 @@
 package leetcode;
 
+import java.util.Arrays;
+
 /**
  * https://leetcode.com/problems/minimum-numbers-of-function-calls-to-make-target-array/
  */
 public class Problem1558 {
     public int minOperations(int[] nums) {
-        // TODO
-        return 0;
+        int answer = nums.length;
+        int max = Arrays.stream(nums).max().getAsInt();
+        int val = (int) (Math.log(max) / Math.log(2));
+        answer += val;
+        answer += max % val;
+        for (int num : nums) {
+            if (num == 0) {
+                answer--;
+                continue;
+            }
+            if (num == 1 || num == max) {
+                continue;
+            }
+            if (num % 2 != 0) {
+                answer++;
+            }
+        }
+        return answer;
     }
 
     public static void main(String[] args) {
@@ -17,5 +35,6 @@ public class Problem1558 {
         System.out.println(prob.minOperations(new int[]{3,2,2,4})); // 7
         System.out.println(prob.minOperations(new int[]{2,4,8,16})); // 8
         System.out.println(prob.minOperations(new int[]{2,3,10})); // 8
+        System.out.println(prob.minOperations(new int[]{2,3,10,89})); // 15
     }
 }
