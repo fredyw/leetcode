567cf8423e2411e563dbcccc634f8789bb9f5450
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-09-04 00:42:37 -0500

    Update problem 1545

--- src/main/java/leetcode/Problem1545.java
+++ src/main/java/leetcode/Problem1545.java
@@ -5,8 +5,26 @@ package leetcode;
  */
 public class Problem1545 {
     public char findKthBit(int n, int k) {
-        // TODO
-        return 0;
+        String s = "0";
+        for (int i = 0; i < n - 1; i++) {
+            s = s + "1" + reverse(inverse(s));
+            if (s.length() >= k) {
+                return s.charAt(k - 1);
+            }
+        }
+        return '0';
+    }
+
+    private static String reverse(String s) {
+        return new StringBuilder(s).reverse().toString();
+    }
+
+    private static String inverse(String s) {
+        String str = "";
+        for (char c : s.toCharArray()) {
+            str += c == '0' ? '1' : '0';
+        }
+        return str;
     }
 
     public static void main(String[] args) {
@@ -15,5 +33,6 @@ public class Problem1545 {
         System.out.println(prob.findKthBit(4, 11)); // "1"
         System.out.println(prob.findKthBit(1, 1)); // "0"
         System.out.println(prob.findKthBit(2, 3)); // "1"
+        System.out.println(prob.findKthBit(20, 1048575)); // "1"
     }
 }
