53f73711a2e50d8ed4606f61e4f6d87fd7421629
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-09-07 23:03:18 -0500

    Skeleton for problem 1578

---
+++ src/main/java/leetcode/Problem1578.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/minimum-deletion-cost-to-avoid-repeating-letters/
+ */
+public class Problem1578 {
+    public int minCost(String s, int[] cost) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1578 prob = new Problem1578();
+        System.out.println(prob.minCost("abaac", new int[]{1,2,3,4,5})); // 3
+        System.out.println(prob.minCost("abc", new int[]{1,2,3})); // 0
+        System.out.println(prob.minCost("aabaa", new int[]{1,2,3,4,1})); // 3
+    }
+}
