897df4736007f2a32e1ce7e49174302d7015f33c
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-09-08 22:19:13 -0500

    Update problem 1578

--- src/main/java/leetcode/Problem1578.java
+++ src/main/java/leetcode/Problem1578.java
@@ -13,6 +13,7 @@ public class Problem1578 {
         Problem1578 prob = new Problem1578();
         System.out.println(prob.minCost("abaac", new int[]{1,2,3,4,5})); // 3
         System.out.println(prob.minCost("abc", new int[]{1,2,3})); // 0
-        System.out.println(prob.minCost("aabaa", new int[]{1,2,3,4,1})); // 3
+        System.out.println(prob.minCost("aabaa", new int[]{1,2,3,4,1})); // 2
+        System.out.println(prob.minCost("aabaaa", new int[]{1,2,3,4,1,5})); // 6
     }
 }
