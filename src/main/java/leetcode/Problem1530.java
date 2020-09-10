ff092e58f7f78bfe732597a86bb53b447241ba3e
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-09-10 00:27:38 -0500

    Solve problem 1530

--- src/main/java/leetcode/Problem1530.java
+++ src/main/java/leetcode/Problem1530.java
@@ -1,5 +1,9 @@
 package leetcode;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * https://leetcode.com/problems/number-of-good-leaf-nodes-pairs/
  */
@@ -24,11 +28,36 @@ public class Problem1530 {
     }
 
     public int countPairs(TreeNode root, int distance) {
-        // TODO
-        return 0;
+        IntRef answer = new IntRef();
+        countPairs(root, distance, answer);
+        return answer.val;
     }
 
-    public static void main(String[] args) {
-        Problem1530 prob = new Problem1530();
+    private static class IntRef {
+        private int val;
+    }
+
+    private static List<Integer> countPairs(TreeNode root, int distance, IntRef answer) {
+        if (root == null) {
+            return List.of();
+        }
+        if (root.left == null && root.right == null) {
+            return List.of(1);
+        }
+        List<Integer> left = countPairs(root.left, distance, answer);
+        List<Integer> right = countPairs(root.right, distance, answer);
+        if (!left.isEmpty() && !right.isEmpty()) {
+            answer.val += left.stream()
+                .flatMap(e1 -> right.stream().filter(e2 -> e1 + e2 <= distance))
+                .count();
+            return Stream.concat(
+                left.stream().map(e -> e + 1),
+                right.stream().map(e -> e + 1))
+                .collect(Collectors.toList());
+        }
+        if (!left.isEmpty()) {
+            return left.stream().map(e -> e + 1).collect(Collectors.toList());
+        }
+        return right.stream().map(e -> e + 1).collect(Collectors.toList());
     }
 }
