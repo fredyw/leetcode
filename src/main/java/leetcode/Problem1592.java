27830ec086842a0dbfd496545802309fad3f0dee
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-09-21 12:16:28 -0500

    Solve exercise 1592

--- src/main/java/leetcode/Problem1592.java
+++ src/main/java/leetcode/Problem1592.java
@@ -5,16 +5,24 @@ package leetcode;
  */
 public class Problem1592 {
     public String reorderSpaces(String text) {
-        // TODO
-        return null;
-    }
-
-    public static void main(String[] args) {
-        Problem1592 prob = new Problem1592();
-        System.out.println(prob.reorderSpaces("  this   is  a sentence ")); // "this   is   a   sentence"
-        System.out.println(prob.reorderSpaces(" practice   makes   perfect")); // "practice   makes   perfect "
-        System.out.println(prob.reorderSpaces("hello   world")); // "hello   world"
-        System.out.println(prob.reorderSpaces("  walks  udp package   into  bar a")); // "walks  udp  package  into  bar  a "
-        System.out.println(prob.reorderSpaces("a")); // "a"
+        int totalSpaces = 0;
+        for (char c : text.toCharArray()) {
+            if (c == ' ') {
+                totalSpaces++;
+            }
+        }
+        String[] words = text.trim().split("\\s+");
+        int spaces = words.length - 1 > 0 ? totalSpaces / (words.length - 1) : 0;
+        int extraSpaces = words.length - 1 > 0 ? totalSpaces % (words.length - 1) : totalSpaces;
+        StringBuilder answer = new StringBuilder();
+        for (String word : words) {
+            if (answer.length() == 0) {
+                answer.append(word);
+            } else {
+                answer.append(" ".repeat(spaces)).append(word);
+            }
+        }
+        answer.append(" ".repeat(extraSpaces));
+        return answer.toString();
     }
 }
