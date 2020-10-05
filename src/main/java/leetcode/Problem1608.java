18bd1105fddae4d04d14cb2419e756bd4792090e
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-10-04 23:28:15 -0500

    Solve problem 1608

--- src/main/java/leetcode/Problem1608.java
+++ src/main/java/leetcode/Problem1608.java
@@ -1,19 +1,27 @@
 package leetcode;
 
+import java.util.Arrays;
+
 /**
  * https://leetcode.com/problems/special-array-with-x-elements-greater-than-or-equal-x/
  */
 public class Problem1608 {
     public int specialArray(int[] nums) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem1608 prob = new Problem1608();
-        System.out.println(prob.specialArray(new int[]{3, 5})); // 2
-        System.out.println(prob.specialArray(new int[]{0, 0})); // -1
-        System.out.println(prob.specialArray(new int[]{0, 4, 3, 0, 4})); // 3
-        System.out.println(prob.specialArray(new int[]{3, 6, 7, 7, 0})); // -1
+        Arrays.sort(nums);
+        for (int i = 1; i <= nums.length; i++) {
+            int index = Arrays.binarySearch(nums, i);
+            if (index < 0) {
+                index = -(index) - 1;
+            } else {
+                while (index >= 0 && nums[index] == i) {
+                    index--;
+                }
+                index++;
+            }
+            if (nums.length - index == i) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
