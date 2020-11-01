2db88b9d9f1c1311208c0d40ce0d3af25269c4e6
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-11-01 12:45:37 -0600

    Skeleton for problem 1641

---
+++ src/main/java/leetcode/Problem1641.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/count-sorted-vowel-strings/
+ */
+public class Problem1641 {
+    public int countVowelStrings(int n) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1641 prob = new Problem1641();
+        System.out.println(prob.countVowelStrings(1)); // 5
+        System.out.println(prob.countVowelStrings(2)); // 15
+    }
+}
