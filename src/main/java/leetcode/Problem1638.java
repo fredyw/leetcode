43459ece6269bb7419875e17e357b6324f617e2e
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-11-03 14:30:23 -0600

    Skeleton for problem 1638

---
+++ src/main/java/leetcode/Problem1638.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/count-substrings-that-differ-by-one-character/
+ */
+public class Problem1638 {
+    public int countSubstrings(String s, String t) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1638 prob = new Problem1638();
+        System.out.println(prob.countSubstrings("aba", "baba")); // 6
+        System.out.println(prob.countSubstrings("ab", "bb")); // 3
+        System.out.println(prob.countSubstrings("a", "a")); // 0
+        System.out.println(prob.countSubstrings("abe", "bbc")); // 10
+    }
+}
