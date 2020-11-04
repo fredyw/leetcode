ebb2e56eedf6165210a110b2486b6c4007c01e39
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-11-04 15:43:33 -0600

    Solve problem 1638

--- src/main/java/leetcode/Problem1638.java
+++ src/main/java/leetcode/Problem1638.java
@@ -6,36 +6,20 @@ package leetcode;
 public class Problem1638 {
     public int countSubstrings(String s, String t) {
         int answer = 0;
-        int minLength = Math.max(s.length(), t.length());
-        for (int length = 1; length <= minLength; length++) {
-            for (int i = 0; i <= s.length() - length; i++) {
-                String sub1 = s.substring(i, i + length);
-                for (int j = 0; j <= t.length() - length; j++) {
-                    String sub2 = t.substring(j, j + length);
-                    int count = 0;
-                    for (int k = 0; k < length; k++) {
-                        if (sub1.charAt(k) != sub2.charAt(k)) {
-                            count++;
-                        }
-                        if (count > 1) {
-                            break;
-                        }
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 0; j < t.length(); j++) {
+                int count = 0;
+                for (int k = i, l = j; k < s.length() && l < t.length(); k++, l++) {
+                    if (s.charAt(k) != t.charAt(l)) {
+                        count++;
                     }
-                    if (count == 1) {
-                        answer++;
+                    if (count > 1) {
+                        break;
                     }
+                    answer += count;
                 }
             }
         }
         return answer;
-    }
-
-    public static void main(String[] args) {
-        Problem1638 prob = new Problem1638();
-        System.out.println(prob.countSubstrings("aba", "baba")); // 6
-        System.out.println(prob.countSubstrings("ab", "bb")); // 3
-        System.out.println(prob.countSubstrings("a", "a")); // 0
-        System.out.println(prob.countSubstrings("abe", "bbc")); // 10
-        System.out.println(prob.countSubstrings("computer", "computation")); // 90
     }
 }
