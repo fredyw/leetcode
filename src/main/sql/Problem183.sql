c787e396da096f92cab6a0f1e2f74a7bac823020
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-11-07 17:00:26 -0600

    Update problem 183

--- src/main/sql/Problem183.sql
+++ src/main/sql/Problem183.sql
@@ -1,4 +1,9 @@
 -- https://leetcode.com/problems/customers-who-never-order/
-select c.Name Customers
-from Customers c
-where c.Id not in (select o.CustomerId from Orders o);
+
+-- select c.Name Customers
+-- from Customers c
+-- where c.Id not in (select o.CustomerId from Orders o);
+
+select c.Name as Customers
+from Customers c left join Orders o on c.Id = o.CustomerId
+where o.CustomerId is null;
