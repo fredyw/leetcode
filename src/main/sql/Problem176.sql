e3627989a1aaef54951a1a8049721b59ff365533
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-11-07 16:58:08 -0600

    Update problem 176

--- src/main/sql/Problem176.sql
+++ src/main/sql/Problem176.sql
@@ -1,4 +1,4 @@
 -- https://leetcode.com/problems/second-highest-salary/
-select max(e.Salary) as SecondHighestSalary
-from (select e1.Salary from Employee e1
-where e1.Salary < (select max(e2.Salary) from Employee e2)) as e;
+select max(Salary) as SecondHighestSalary
+from Employee
+where Salary < (select max(Salary) from Employee);
