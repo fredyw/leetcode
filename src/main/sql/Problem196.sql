d539179b0f3b0afb36f874dbe645e5d4af9bd946
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-11-07 17:01:05 -0600

    Update problem 196

--- src/main/sql/Problem196.sql
+++ src/main/sql/Problem196.sql
@@ -1,4 +1,14 @@
 -- https://leetcode.com/problems/delete-duplicate-emails/
-delete p1
-from Person p1, Person p2
-where p1.Email = p2.Email and p1.Id > p2.Id;
+
+-- delete p1
+-- from Person p1, Person p2
+-- where p1.Email = p2.Email and p1.Id > p2.Id;
+
+delete from Person
+where Id not in (
+    select t.Id from (
+        select min(Id) as Id
+        from Person
+        group by Email
+    ) as t
+);
