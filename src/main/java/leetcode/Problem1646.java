fbdab8bde9105e7b86d1c2502bf6a0f39481182d
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-11-07 23:35:02 -0600

    Skeleton for problem 1646

---
+++ src/main/java/leetcode/Problem1646.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/get-maximum-in-generated-array/
+ */
+public class Problem1646 {
+    public int getMaximumGenerated(int n) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1646 prob = new Problem1646();
+        System.out.println(prob.getMaximumGenerated(7)); // 3
+        System.out.println(prob.getMaximumGenerated(2)); // 1
+        System.out.println(prob.getMaximumGenerated(3)); // 2
+    }
+}
