b36a3706b6bf510e7a2a395bb3f14a97eabfb1cf
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-11-23 13:18:39 -0600

    Skeleton for problem 1663

---
+++ src/main/java/leetcode/Problem1663.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/smallest-string-with-a-given-numeric-value/
+ */
+public class Problem1663 {
+    public String getSmallestString(int n, int k) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem1663 prob = new Problem1663();
+        System.out.println(prob.getSmallestString(3, 27)); // "aay"
+        System.out.println(prob.getSmallestString(5, 73)); // "aaszz"
+    }
+}
