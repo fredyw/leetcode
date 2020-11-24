9129ed54057f6289eeee2e89b6c484d5d0b3dd08
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-11-24 12:00:48 -0600

    Skeleton for problem 1657

---
+++ src/main/java/leetcode/Problem1657.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/determine-if-two-strings-are-close/
+ */
+public class Problem1657 {
+    public boolean closeStrings(String word1, String word2) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem1657 prob = new Problem1657();
+        System.out.println(prob.closeStrings("abc", "bca")); // true
+        System.out.println(prob.closeStrings("a", "aa")); // false
+        System.out.println(prob.closeStrings("cabbba", "abbccc")); // true
+        System.out.println(prob.closeStrings("cabbba", "aabbss")); // false
+    }
+}
