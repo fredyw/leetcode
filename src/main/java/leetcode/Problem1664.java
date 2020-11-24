4e66abb1a39557600517b6e4022979fb9a70005f
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-11-24 11:56:58 -0600

    Solve problem 1664

--- src/main/java/leetcode/Problem1664.java
+++ src/main/java/leetcode/Problem1664.java
@@ -5,14 +5,45 @@ package leetcode;
  */
 public class Problem1664 {
     public int waysToMakeFair(int[] nums) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem1664 prob = new Problem1664();
-        System.out.println(prob.waysToMakeFair(new int[]{2,1,6,4})); // 1
-        System.out.println(prob.waysToMakeFair(new int[]{1,1,1})); // 3
-        System.out.println(prob.waysToMakeFair(new int[]{1,2,3})); // 0
+        int[] oddPrefixSums = new int[nums.length];
+        int[] evenPrefixSums = new int[nums.length];
+        int oddSum = 0;
+        int evenSum = 0;
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (i % 2 == 0) {
+                if (i - 2 >= 0) {
+                    evenPrefixSums[i] = evenPrefixSums[i - 2] + nums[i];
+                } else {
+                    evenPrefixSums[i] = nums[i];
+                }
+                evenSum = evenPrefixSums[i];
+            } else {
+                if (i - 2 >= 0) {
+                    oddPrefixSums[i] = oddPrefixSums[i - 2] + nums[i];
+                } else {
+                    oddPrefixSums[i] = nums[i];
+                }
+                oddSum = oddPrefixSums[i];
+            }
+        }
+        int answer = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int newSum = sum - nums[i];
+            int left = 0;
+            int right = 0;
+            if (i % 2 == 0) {
+                left = i - 2 >= 0 ? evenPrefixSums[i - 2] : 0;
+                right = i - 1 >= 0 ? oddSum - oddPrefixSums[i - 1] : oddSum;
+            } else {
+                left = i - 2 >= 0 ? oddPrefixSums[i - 2] : 0;
+                right = i - 1 >=  0 ? evenSum - evenPrefixSums[i - 1] : evenSum;
+            }
+            if (left + right == newSum - (left + right)) {
+                answer++;
+            }
+        }
+        return answer;
     }
 }
