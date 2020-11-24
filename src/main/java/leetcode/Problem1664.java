605d4f9d70677c62cb01a05b3bc4b0d100a42c4d
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-11-23 23:34:21 -0600

    Skeleton for problem 1664

---
+++ src/main/java/leetcode/Problem1664.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/ways-to-make-a-fair-array/
+ */
+public class Problem1664 {
+    public int waysToMakeFair(int[] nums) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1664 prob = new Problem1664();
+        System.out.println(prob.waysToMakeFair(new int[]{2,1,6,4})); // 1
+        System.out.println(prob.waysToMakeFair(new int[]{1,1,1})); // 3
+        System.out.println(prob.waysToMakeFair(new int[]{1,2,3})); // 0
+    }
+}
