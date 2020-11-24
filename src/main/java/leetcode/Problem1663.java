0bbe2340de5b6d14b97f3abb045dfe5f575e4afb
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-11-23 19:04:05 -0600

    Update problem 1663

--- src/main/java/leetcode/Problem1663.java
+++ src/main/java/leetcode/Problem1663.java
@@ -5,13 +5,18 @@ package leetcode;
  */
 public class Problem1663 {
     public String getSmallestString(int n, int k) {
-        // TODO
-        return null;
+        char[] chars = new char[n];
+        for (int i = chars.length - 1, j = 0; i >= 0; i--, j--) {
+            // TODO
+        }
+        return new String(chars);
     }
 
     public static void main(String[] args) {
         Problem1663 prob = new Problem1663();
         System.out.println(prob.getSmallestString(3, 27)); // "aay"
         System.out.println(prob.getSmallestString(5, 73)); // "aaszz"
+        System.out.println(prob.getSmallestString(27, 43)); // "aaaaaaaaaaaaaaaaaaaaaaaaaaq"
+        System.out.println(prob.getSmallestString(28, 43)); // "aaaaaaaaaaaaaaaaaaaaaaaaaap"
     }
 }
