463b24e1fa2643c57cb232ac66bdfe13a6d4de9a
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-11-25 16:51:54 -0600

    Solve problem 1657

--- src/main/java/leetcode/Problem1657.java
+++ src/main/java/leetcode/Problem1657.java
@@ -1,10 +1,6 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.Arrays;
 
 /**
  * https://leetcode.com/problems/determine-if-two-strings-are-close/
@@ -14,31 +10,21 @@ public class Problem1657 {
         if (word1.length() != word2.length()) {
             return false;
         }
-        Map<Character, Integer> map1 = new HashMap<>();
-        Map<Character, Integer> map2 = new HashMap<>();
+        boolean[] chars1 = new boolean[26];
+        boolean[] chars2 = new boolean[26];
+        int[] counts1 = new int[26];
+        int[] counts2 = new int[26];
         for (int i = 0; i < word1.length(); i++) {
-            map1.put(word1.charAt(i), map1.getOrDefault(word1.charAt(i), 0) + 1);
-            map2.put(word2.charAt(i), map2.getOrDefault(word2.charAt(i), 0) + 1);
+            chars1[word1.charAt(i) - 'a'] = true;
+            chars2[word2.charAt(i) - 'a'] = true;
+            counts1[word1.charAt(i) - 'a']++;
+            counts2[word2.charAt(i) - 'a']++;
         }
-        if (!map1.keySet().equals(map2.keySet())) {
+        if (!Arrays.equals(chars1, chars2)) {
             return false;
         }
-        List<Integer> counts1 = new ArrayList<>(map1.values());
-        List<Integer> counts2 = new ArrayList<>(map2.values());
-        Collections.sort(counts1);
-        Collections.sort(counts2);
-        return counts1.equals(counts2);
-    }
-
-    public static void main(String[] args) {
-        Problem1657 prob = new Problem1657();
-        System.out.println(prob.closeStrings("abc", "bca")); // true
-        System.out.println(prob.closeStrings("a", "aa")); // false
-        System.out.println(prob.closeStrings("cabbba", "abbccc")); // true
-        System.out.println(prob.closeStrings("cabbba", "aabbss")); // false
-        System.out.println(prob.closeStrings("ababc", "aaabc")); // false
-        System.out.println(prob.closeStrings("cbbaa", "ccbba")); // true
-        System.out.println(prob.closeStrings("caabbb", "abbccc")); // true
-        System.out.println(prob.closeStrings("uau", "ssx")); // false
+        Arrays.sort(counts1);
+        Arrays.sort(counts2);
+        return Arrays.equals(counts1, counts2);
     }
 }
