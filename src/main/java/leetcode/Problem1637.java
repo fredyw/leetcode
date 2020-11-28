cc7f56600dd65faf489051d9128aeed2b5cdead9
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-11-27 18:01:58 -0600

    Update problem 1637

--- src/main/java/leetcode/Problem1637.java
+++ src/main/java/leetcode/Problem1637.java
@@ -15,6 +15,9 @@ public class Problem1637 {
             {8,7},{9,9},{7,4},{9,7}
         })); // 1
         System.out.println(prob.maxWidthOfVerticalArea(new int[][]{
+            {9,9},{7,4},{9,7}
+        })); // 2
+        System.out.println(prob.maxWidthOfVerticalArea(new int[][]{
             {3,1},{9,0},{1,0},{1,4},{5,3},{8,8}
         })); // 3
     }
