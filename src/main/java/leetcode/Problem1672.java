5a138585e75c8337cd7817f89afbddd9f3a0634d
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-11-29 16:34:13 -0600

    Solve problem 1672

--- src/main/java/leetcode/Problem1672.java
+++ src/main/java/leetcode/Problem1672.java
@@ -1,18 +1,16 @@
 package leetcode;
 
+import java.util.Arrays;
+
 /**
  * https://leetcode.com/problems/richest-customer-wealth/
  */
 public class Problem1672 {
     public int maximumWealth(int[][] accounts) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem1672 prob = new Problem1672();
-        System.out.println(prob.maximumWealth(new int[][]{{1,2,3},{3,2,1}})); // 6
-        System.out.println(prob.maximumWealth(new int[][]{{1,5},{7,3},{3,5}})); // 10
-        System.out.println(prob.maximumWealth(new int[][]{{2,8,7},{7,1,3},{1,9,5}})); // 17
+        int answer = 0;
+        for (int[] account : accounts) {
+            answer = Math.max(answer, Arrays.stream(account).sum());
+        }
+        return answer;
     }
 }
