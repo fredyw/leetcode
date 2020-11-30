51c6521e3116705b24a1e89b1ec05bc9962dcf8c
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-11-30 10:21:36 -0600

    Solve problem 1669

--- src/main/java/leetcode/Problem1669.java
+++ src/main/java/leetcode/Problem1669.java
@@ -22,46 +22,25 @@ public class Problem1669 {
     }
 
     public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
-        // TODO
-        return null;
-    }
-
-    private static ListNode build(int[] array) {
-        ListNode root = null;
-        ListNode current = null;
-        for (int i = 0; i < array.length; i++) {
-            if (i == 0) {
-                root = new ListNode(array[i]);
-                current = root;
-            } else {
-                current.next = new ListNode(array[i]);
-                current = current.next;
+        ListNode tailList2 = null;
+        for (ListNode n = list2; n != null; n = n.next) {
+            if (n.next == null) {
+                tailList2 = n;
+                break;
             }
         }
-        return root;
-    }
-
-    private static String toString(ListNode node) {
-        StringBuilder sb = new StringBuilder();
-        for (ListNode n = node; n != null; n = node.next) {
-            if (sb.length() == 0) {
-                sb.append(node.val);
-            } else {
-                sb.append(",").append(node.val);
+        int i = 0;
+        ListNode prev = null;
+        for (ListNode cur = list1; cur != null; cur = cur.next, i++) {
+            if (i == a) {
+                prev.next = list2;
             }
+            if (i == b) {
+                tailList2.next = cur.next;
+                break;
+            }
+            prev = cur;
         }
-        return sb.toString();
-    }
-
-    public static void main(String[] args) {
-        Problem1669 prob = new Problem1669();
-        System.out.println(toString(prob.mergeInBetween(
-            build(new int[]{0, 1, 2, 3, 4, 5}),
-            3, 4,
-            build(new int[]{1000000, 1000001, 1000002})))); // [0,1,2,1000000,1000001,1000002,5]
-        System.out.println(toString(prob.mergeInBetween(
-            build(new int[]{0, 1, 2, 3, 4, 5, 6}),
-            2, 5,
-            build(new int[]{0, 1, 1000000, 1000001, 1000002, 1000003, 1000004, 6})))); // [1000000,1000001,1000002,1000003,1000004]
+        return list1;
     }
 }
