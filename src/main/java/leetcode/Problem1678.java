23b7de221b271379d941bd08c5152546f0e4ff28
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-12-06 12:15:37 -0600

    Solve problem 1678

--- src/main/java/leetcode/Problem1678.java
+++ src/main/java/leetcode/Problem1678.java
@@ -1,18 +1,13 @@
 package leetcode;
 
+import java.util.regex.Pattern;
+
 /**
  * https://leetcode.com/problems/goal-parser-interpretation/
  */
 public class Problem1678 {
     public String interpret(String command) {
-        // TODO
-        return null;
-    }
-
-    public static void main(String[] args) {
-        Problem1678 prob = new Problem1678();
-        System.out.println(prob.interpret("G()(al)")); // "Goal"
-        System.out.println(prob.interpret("G()()()()(al)")); // "Gooooal"
-        System.out.println(prob.interpret("(al)G(al)()()G")); // "alGalooG"
+        return command.replaceAll(Pattern.quote("(al)"), "al")
+            .replaceAll(Pattern.quote("()"), "o");
     }
 }
