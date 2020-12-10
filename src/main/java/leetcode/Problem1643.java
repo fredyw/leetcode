5c22e16927ba9a41b78059f915c564acb088a357
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-12-10 09:47:25 -0600

    Skeleton for problem 1643

---
+++ src/main/java/leetcode/Problem1643.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/minimum-deletions-to-make-string-balanced/
+ */
+public class Problem1643 {
+    public int minimumDeletions(String s) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1643 prob = new Problem1643();
+        System.out.println(prob.minimumDeletions("aababbab")); // 2
+        System.out.println(prob.minimumDeletions("bbaaaaabb")); // 2
+    }
+}
