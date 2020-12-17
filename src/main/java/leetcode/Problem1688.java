445331a368edb2a120630a3679255d05d54752d0
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-12-16 20:15:50 -0600

    Skeleton for problem 1688

---
+++ src/main/java/leetcode/Problem1688.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+public class Problem1688 {
+    public int numberOfMatches(int n) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1688 prob = new Problem1688();
+        System.out.println(prob.numberOfMatches(7)); // 6
+        System.out.println(prob.numberOfMatches(14)); // 13
+    }
+}
