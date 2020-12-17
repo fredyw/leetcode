2c6a0c6d22abb9a16a4b122a5d9b93d8fcda0cee
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-12-16 22:08:57 -0600

    Update problem 1643

--- src/main/java/leetcode/Problem1643.java
+++ src/main/java/leetcode/Problem1643.java
@@ -11,17 +11,28 @@ public class Problem1643 {
                 countA++;
             }
         }
+        // In the worst case, delete all As, so this is the initial value.
+        // (not the optimal answer obviously).
         int answer = countA;
         int countB = 0;
         for (char c : s.toCharArray()) {
             if (c == 'a') {
+                // countA-- here means we want to keep A at this position, but delete
+                // all other As after this position (to the right).
                 countA--;
             } else {
+                // countB++ here means B we want to delete at this position.
                 countB++;
             }
-            if (countA + countB < answer) {
-                answer = countA + countB;
-            }
+            // countA + countB here means As that we didn't delete + Bs we have deleted
+            // so far.
+            // Example:
+            //         a b a a a b b b
+            // countA: 3 3 2 1 0 0 0 0
+            // countB: 0 1 1 1 1 2 3 4
+            // ----------------------- +
+            // answer: 3 4 3 2 1 2 3 4 --> get the min, i.e. 1
+            answer = Math.min(answer, countA + countB);
         }
         return answer;
     }
