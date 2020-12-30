dc609f6e98bed0feecce0a48da5477d6192324b2
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-12-29 21:51:43 -0600

    Update problem 1685

--- src/main/java/leetcode/Problem1685.java
+++ src/main/java/leetcode/Problem1685.java
@@ -1,22 +1,24 @@
 package leetcode;
 
-import java.util.Arrays;
-
 /**
  * https://leetcode.com/problems/sum-of-absolute-differences-in-a-sorted-array/
  */
 public class Problem1685 {
     public int[] getSumAbsoluteDifferences(int[] nums) {
+        int[] prefixSums = new int[nums.length];
+        int totalSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            totalSum += nums[i];
+            prefixSums[i] = (i > 0 ? prefixSums[i - 1] : 0) + nums[i];
+        }
         int[] answer = new int[nums.length];
-        // TODO
+        for (int i = 0; i < nums.length; i++) {
+            int sumLeft = (i - 1 >= 0 ? prefixSums[i - 1] : 0);
+            int sumAbsLeft = (i * nums[i]) - sumLeft;
+            int sumRight = totalSum - prefixSums[i];
+            int sumAbsRight = sumRight - ((nums.length - i - 1) * nums[i]);
+            answer[i] = sumAbsLeft + sumAbsRight;
+        }
         return answer;
-    }
-
-    public static void main(String[] args) {
-        Problem1685 prob = new Problem1685();
-        System.out.println(Arrays.toString(prob.getSumAbsoluteDifferences(new int[]{1,2,3}))); // [3,2,3]
-        System.out.println(Arrays.toString(prob.getSumAbsoluteDifferences(new int[]{2,3,5}))); // [4,3,5]
-        System.out.println(Arrays.toString(prob.getSumAbsoluteDifferences(new int[]{1,2,3,4,5}))); // [10,7,6,7,10]
-        System.out.println(Arrays.toString(prob.getSumAbsoluteDifferences(new int[]{1,4,6,8,10}))); // [24,15,13,15,21]
     }
 }
