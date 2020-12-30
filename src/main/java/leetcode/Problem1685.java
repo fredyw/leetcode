9a7ad6cffe46643a94332c3ce6c2602a1e693e3b
Fredy Wijaya <fredy.wijaya@gmail.com>
2020-12-29 20:10:28 -0600

    Update problem 1685

--- src/main/java/leetcode/Problem1685.java
+++ src/main/java/leetcode/Problem1685.java
@@ -16,6 +16,7 @@ public class Problem1685 {
         Problem1685 prob = new Problem1685();
         System.out.println(Arrays.toString(prob.getSumAbsoluteDifferences(new int[]{1,2,3}))); // [3,2,3]
         System.out.println(Arrays.toString(prob.getSumAbsoluteDifferences(new int[]{2,3,5}))); // [4,3,5]
+        System.out.println(Arrays.toString(prob.getSumAbsoluteDifferences(new int[]{1,2,3,4,5}))); // [10,7,6,7,10]
         System.out.println(Arrays.toString(prob.getSumAbsoluteDifferences(new int[]{1,4,6,8,10}))); // [24,15,13,15,21]
     }
 }
