1b7d67c028aed8a1551cf33e2ee59d1f141ec68d
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-01-04 13:53:16 -0600

    Skelettooon for problem 1702

---
+++ src/main/java/leetcode/Problem1702.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/maximum-binary-string-after-change/
+ */
+public class Problem1702 {
+    public String maximumBinaryString(String binary) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem1702 prob = new Problem1702();
+        System.out.println(prob.maximumBinaryString("000110")); // "111011"
+        System.out.println(prob.maximumBinaryString("01")); // "01"
+    }
+}
+
