1447a51d17908462396dfcc5bd489baa645ba742
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-01-05 12:34:08 -0600

    Update problem 1702

--- src/main/java/leetcode/Problem1702.java
+++ src/main/java/leetcode/Problem1702.java
@@ -12,7 +12,14 @@ public class Problem1702 {
     public static void main(String[] args) {
         Problem1702 prob = new Problem1702();
         System.out.println(prob.maximumBinaryString("000110")); // "111011"
+        System.out.println(prob.maximumBinaryString("0000110")); // "1111011"
         System.out.println(prob.maximumBinaryString("01")); // "01"
+        System.out.println(prob.maximumBinaryString("100011001010101001")); // "111111111101111111"
+        System.out.println(prob.maximumBinaryString("1000111100101011101")); // "1111111101111111111"
+        System.out.println(prob.maximumBinaryString("01010101010101")); // 11111101111111
+        System.out.println(prob.maximumBinaryString("0101")); // 1011
+        System.out.println(prob.maximumBinaryString("1111")); // 1111
+        System.out.println(prob.maximumBinaryString("0000")); // 1110
     }
 }
 
