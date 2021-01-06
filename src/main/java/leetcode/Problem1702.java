ce195725c3a1078a34be7839992cf3602c02b864
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-01-05 20:13:33 -0600

    Update problem 1702

--- src/main/java/leetcode/Problem1702.java
+++ src/main/java/leetcode/Problem1702.java
@@ -15,11 +15,22 @@ public class Problem1702 {
         System.out.println(prob.maximumBinaryString("0000110")); // "1111011"
         System.out.println(prob.maximumBinaryString("01")); // "01"
         System.out.println(prob.maximumBinaryString("100011001010101001")); // "111111111101111111"
+        System.out.println(prob.maximumBinaryString("000011001010101001")); // "111111111101111111"
+        System.out.println(prob.maximumBinaryString("1000011001010101001")); // "1111111111101111111"
         System.out.println(prob.maximumBinaryString("1000111100101011101")); // "1111111101111111111"
+        System.out.println(prob.maximumBinaryString("0100111100101011101")); // "1111111011111111111"
+        System.out.println(prob.maximumBinaryString("0010111100101011101")); // "1111111011111111111"
         System.out.println(prob.maximumBinaryString("01010101010101")); // 11111101111111
-        System.out.println(prob.maximumBinaryString("0101")); // 1011
-        System.out.println(prob.maximumBinaryString("1111")); // 1111
-        System.out.println(prob.maximumBinaryString("0000")); // 1110
+        System.out.println(prob.maximumBinaryString("0101")); // "1011"
+        System.out.println(prob.maximumBinaryString("1010")); // "1101"
+        System.out.println(prob.maximumBinaryString("1111")); // "1111"
+        System.out.println(prob.maximumBinaryString("0000")); // "1110"
+        System.out.println(prob.maximumBinaryString("00010101")); // "11110111"
+        System.out.println(prob.maximumBinaryString("0100000101")); // "1111110111"
+        System.out.println(prob.maximumBinaryString("0000000101")); // "1111111011"
+        System.out.println(prob.maximumBinaryString("1000000101")); // "1111111011"
+        System.out.println(prob.maximumBinaryString("1111000101")); // "1111111011"
+        System.out.println(prob.maximumBinaryString("1011000101")); // "1111101111"
     }
 }
 
