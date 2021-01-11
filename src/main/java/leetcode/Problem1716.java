523821978f7ac28a18fc828a5d521b59f0f965a6
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-01-11 10:34:25 -0600

    Solve problem 1716

--- src/main/java/leetcode/Problem1716.java
+++ src/main/java/leetcode/Problem1716.java
@@ -5,14 +5,14 @@ package leetcode;
  */
 public class Problem1716 {
     public int totalMoney(int n) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem1716 prob = new Problem1716();
-        System.out.println(prob.totalMoney(4)); // 10
-        System.out.println(prob.totalMoney(10)); // 37
-        System.out.println(prob.totalMoney(20)); // 96
+        int answer = 0;
+        int week = (int) Math.ceil(n / 7.0);
+        for (int i = 1; i <= week && n > 0; i++) {
+            for (int j = 0; j < 7 && n > 0; j++) {
+                answer += i + j;
+                n--;
+            }
+        }
+        return answer;
     }
 }
