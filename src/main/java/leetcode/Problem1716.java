14746403ddf448bf74e55d0b72253f33e3205657
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-01-11 10:10:26 -0600

    Skeleton for problem 1716

---
+++ src/main/java/leetcode/Problem1716.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/calculate-money-in-leetcode-bank/
+ */
+public class Problem1716 {
+    public int totalMoney(int n) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1716 prob = new Problem1716();
+        System.out.println(prob.totalMoney(4)); // 10
+        System.out.println(prob.totalMoney(10)); // 37
+        System.out.println(prob.totalMoney(20)); // 96
+    }
+}
