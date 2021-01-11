b3b92dbcc70c36d2770245f8663391181c0e106f
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-01-11 10:52:03 -0600

    Solve problem 1720

--- src/main/java/leetcode/Problem1720.java
+++ src/main/java/leetcode/Problem1720.java
@@ -1,19 +1,15 @@
 package leetcode;
 
-import java.util.Arrays;
-
 /**
  * https://leetcode.com/problems/decode-xored-array/
  */
 public class Problem1720 {
     public int[] decode(int[] encoded, int first) {
-        // TODO
-        return null;
-    }
-
-    public static void main(String[] args) {
-        Problem1720 prob = new Problem1720();
-        System.out.println(Arrays.toString(prob.decode(new int[]{1,2,3}, 1))); // [1,0,2,1]
-        System.out.println(Arrays.toString(prob.decode(new int[]{6,2,7,3}, 4))); // [4,2,0,7,4]
+        int[] answer = new int[encoded.length + 1];
+        answer[0] = first;
+        for (int i = 0; i < encoded.length; i++) {
+            answer[i + 1] = answer[i] ^ encoded[i];
+        }
+        return answer;
     }
 }
