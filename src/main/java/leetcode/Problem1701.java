8c275c558d63422fea3d0524f45c57bab3d02e5e
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-01-12 14:09:00 -0600

    Skeleton for problem 1701

---
+++ src/main/java/leetcode/Problem1701.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/average-waiting-time/
+ */
+public class Problem1701 {
+    public double averageWaitingTime(int[][] customers) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1701 prob = new Problem1701();
+        System.out.println(prob.averageWaitingTime(new int[][]{{1,2},{2,5},{4,3}})); // 5.00000
+        System.out.println(prob.averageWaitingTime(new int[][]{{5,2},{5,4},{10,3},{20,1}})); // 3.25000
+    }
+}
+
