2918289d8c00017a52912c4b84670c10d7893bfe
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-01-18 09:31:11 -0600

    Solve problem 1689

--- src/main/java/leetcode/Problem1689.java
+++ src/main/java/leetcode/Problem1689.java
@@ -5,14 +5,10 @@ package leetcode;
  */
 public class Problem1689 {
     public int minPartitions(String n) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem1689 prob = new Problem1689();
-        System.out.println(prob.minPartitions("32")); // 3
-        System.out.println(prob.minPartitions("82734")); // 8
-        System.out.println(prob.minPartitions("27346209830709182346")); // 9
+        int answer = 0;
+        for (char c : n.toCharArray()) {
+            answer = Math.max(answer, c - '0');
+        }
+        return answer;
     }
 }
