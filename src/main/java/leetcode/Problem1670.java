9c9f644c89b71138e2a5ecfcb6e4e430563eb755
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-01-20 10:45:17 -0600

    Solve problem 1670

--- src/main/java/leetcode/Problem1670.java
+++ src/main/java/leetcode/Problem1670.java
@@ -1,51 +1,74 @@
 package leetcode;
 
+import java.util.LinkedList;
+
 /**
  * https://leetcode.com/problems/design-front-middle-back-queue/
  */
 public class Problem1670 {
     static class FrontMiddleBackQueue {
+        private final LinkedList<Integer> front = new LinkedList<>();
+        private final LinkedList<Integer> back = new LinkedList<>();
+
         public FrontMiddleBackQueue() {
         }
 
         public void pushFront(int val) {
-            // TODO
+            front.addFirst(val);
+            rebalance(front, back);
         }
 
         public void pushMiddle(int val) {
-            // TODO
+            if (front.size() > back.size()) {
+                back.addFirst(front.pollLast());
+            }
+            front.addLast(val);
         }
 
         public void pushBack(int val) {
-            // TODO
+            back.addLast(val);
+            rebalance(front, back);
         }
 
         public int popFront() {
-            // TODO
-            return 0;
+            if (front.isEmpty() && back.isEmpty()) {
+                return -1;
+            }
+            if (front.isEmpty()) {
+                return popBack();
+            }
+            int val = front.pollFirst();
+            rebalance(front, back);
+            return val;
         }
 
         public int popMiddle() {
-            // TODO
-            return 0;
+            if (front.isEmpty() && back.isEmpty()) {
+                return -1;
+            }
+            int val = front.pollLast();
+            rebalance(front, back);
+            return val;
         }
 
         public int popBack() {
-            // TODO
-            return 0;
+            if (front.isEmpty() && back.isEmpty()) {
+                return -1;
+            }
+            if (back.isEmpty()) {
+                return popFront();
+            }
+            int val = back.pollLast();
+            rebalance(front, back);
+            return val;
         }
-    }
 
-    public static void main(String[] args) {
-        FrontMiddleBackQueue q = new FrontMiddleBackQueue();
-        q.pushFront(1);  // [1]
-        q.pushBack(2);   // [1, 2]
-        q.pushMiddle(3); // [1, 3, 2]
-        q.pushMiddle(4); // [1, 4, 3, 2]
-        System.out.println(q.popFront());  // return 1 -> [4, 3, 2]
-        System.out.println(q.popMiddle()); // return 3 -> [4, 2]
-        System.out.println(q.popMiddle()); // return 4 -> [2]
-        System.out.println(q.popBack());   // return 2 -> []
-        System.out.println(q.popFront());  // return -1 -> [] (The queue is empty)
+        private static void rebalance(LinkedList<Integer> front, LinkedList<Integer> back) {
+            if (front.size() - 2 >= back.size()) {
+                back.addFirst(front.pollLast());
+            } else if (back.size() > front.size()) {
+                front.addLast(back.pollFirst());
+            }
+        }
     }
 }
