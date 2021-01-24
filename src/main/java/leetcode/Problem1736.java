efe511772da7e5451fcc1c72319abed2ddb00b72
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-01-24 13:05:11 -0600

    Solve problem 1736

--- src/main/java/leetcode/Problem1736.java
+++ src/main/java/leetcode/Problem1736.java
@@ -5,14 +5,22 @@ package leetcode;
  */
 public class Problem1736 {
     public String maximumTime(String time) {
-        // TODO
-        return "";
-    }
-
-    public static void main(String[] args) {
-        Problem1736 prob = new Problem1736();
-        System.out.println(prob.maximumTime("2?:?0")); // "23:50"
-        System.out.println(prob.maximumTime("0?:3?")); // "09:39"
-        System.out.println(prob.maximumTime("1?:22")); // "19:22"
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < time.length(); i++) {
+            if (time.charAt(i) != '?') {
+                answer.append(time.charAt(i));
+                continue;
+            }
+            if (i == 0) {
+                answer.append(time.charAt(1) == '?' || time.charAt(1) - '0' <= 3 ? "2" : "1");
+            } else if (i == 1) {
+                answer.append(answer.charAt(0) == '2' ? "3" : "9");
+            } else if (i == 3) {
+                answer.append("5");
+            } else if (i == 4) {
+                answer.append("9");
+            }
+        }
+        return answer.toString();
     }
 }
