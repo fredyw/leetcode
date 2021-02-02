37c825646aa27b531d4ef59d0eedbdf86ef5ebe0
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-02-01 19:48:14 -0600

    Solve problem 1742

--- src/main/java/leetcode/Problem1742.java
+++ src/main/java/leetcode/Problem1742.java
@@ -1,18 +1,24 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * https://leetcode.com/problems/maximum-number-of-balls-in-a-box/
  */
 public class Problem1742 {
     public int countBalls(int lowLimit, int highLimit) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem1742 prob = new Problem1742();
-        System.out.println(prob.countBalls(1, 10)); // 2
-        System.out.println(prob.countBalls(5, 15)); // 2
-        System.out.println(prob.countBalls(19, 28)); // 2
+        int answer = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = lowLimit; i <= highLimit; i++) {
+            int sum = 0;
+            for (char c : ("" + i + lowLimit).toCharArray()) {
+                sum += c - '0';
+            }
+            int count = map.getOrDefault(sum, 0) + 1;
+            map.put(sum, count);
+            answer = Math.max(answer, count);
+        }
+        return answer;
     }
 }
