a0637f21c9b738ce8019cc4eb4398948a41ed127
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-02-01 19:38:12 -0600

    Skeleton for problem 1742

---
+++ src/main/java/leetcode/Problem1742.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/maximum-number-of-balls-in-a-box/
+ */
+public class Problem1742 {
+    public int countBalls(int lowLimit, int highLimit) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1742 prob = new Problem1742();
+        System.out.println(prob.countBalls(1, 10)); // 2
+        System.out.println(prob.countBalls(5, 15)); // 2
+        System.out.println(prob.countBalls(19, 28)); // 2
+    }
+}
