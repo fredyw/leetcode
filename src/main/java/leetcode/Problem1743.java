87a7ff248d84a459319e0dc967d95be02a5b883b
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-02-02 22:51:59 -0600

    Solve problem 1743

--- src/main/java/leetcode/Problem1743.java
+++ src/main/java/leetcode/Problem1743.java
@@ -1,7 +1,6 @@
 package leetcode;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -13,24 +12,21 @@ import java.util.Set;
  */
 public class Problem1743 {
     public int[] restoreArray(int[][] adjacentPairs) {
+        int first = 0;
         Map<Integer, List<Integer>> pairs = new HashMap<>();
-        Set<Integer> set = new HashSet<>();
         for (int[] pair : adjacentPairs) {
             pairs.putIfAbsent(pair[0], new ArrayList<>());
             pairs.get(pair[0]).add(pair[1]);
             pairs.putIfAbsent(pair[1], new ArrayList<>());
             pairs.getOrDefault(pair[1], new ArrayList<>()).add(pair[0]);
-            set.add(pair[0]);
-            set.add(pair[1]);
         }
-        int first = 0;
         for (Map.Entry<Integer, List<Integer>> entry : pairs.entrySet()) {
             if (entry.getValue().size() == 1) {
                 first = entry.getKey();
                 break;
             }
         }
-        int[] answer = new int[set.size()];
+        int[] answer = new int[adjacentPairs.length + 1];
         answer[0] = first;
         Set<Integer> previous = new HashSet<>();
         previous.add(first);
@@ -46,18 +42,5 @@ public class Problem1743 {
             answer[i] = next;
         }
         return answer;
-    }
-
-    public static void main(String[] args) {
-        Problem1743 prob = new Problem1743();
-        System.out.println(Arrays.toString(prob.restoreArray(new int[][]{
-            {2,1},{3,4},{3,2}
-        }))); // [1,2,3,4]
-        System.out.println(Arrays.toString(prob.restoreArray(new int[][]{
-            {4,-2},{1,4},{-3,1}
-        }))); // [-2,4,1,-3]
-        System.out.println(Arrays.toString(prob.restoreArray(new int[][]{
-            {100000,-100000}
-        }))); // [100000,-100000]
     }
 }
