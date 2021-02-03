20d9c1ce3d67ba6df702b48d7e33b672032de4f4
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-02-02 22:44:14 -0600

    Update problem 1743

--- src/main/java/leetcode/Problem1743.java
+++ src/main/java/leetcode/Problem1743.java
@@ -1,14 +1,51 @@
 package leetcode;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * https://leetcode.com/problems/restore-the-array-from-adjacent-pairs/
  */
 public class Problem1743 {
     public int[] restoreArray(int[][] adjacentPairs) {
-        // TODO
-        return null;
+        Map<Integer, List<Integer>> pairs = new HashMap<>();
+        Set<Integer> set = new HashSet<>();
+        for (int[] pair : adjacentPairs) {
+            pairs.putIfAbsent(pair[0], new ArrayList<>());
+            pairs.get(pair[0]).add(pair[1]);
+            pairs.putIfAbsent(pair[1], new ArrayList<>());
+            pairs.getOrDefault(pair[1], new ArrayList<>()).add(pair[0]);
+            set.add(pair[0]);
+            set.add(pair[1]);
+        }
+        int first = 0;
+        for (Map.Entry<Integer, List<Integer>> entry : pairs.entrySet()) {
+            if (entry.getValue().size() == 1) {
+                first = entry.getKey();
+                break;
+            }
+        }
+        int[] answer = new int[set.size()];
+        answer[0] = first;
+        Set<Integer> previous = new HashSet<>();
+        previous.add(first);
+        for (int i = 1; i < answer.length; i++) {
+            int next = 0;
+            for (int adj : pairs.get(answer[i - 1])) {
+                if (!previous.contains(adj)) {
+                    next = adj;
+                    break;
+                }
+            }
+            previous.add(next);
+            answer[i] = next;
+        }
+        return answer;
     }
 
     public static void main(String[] args) {
