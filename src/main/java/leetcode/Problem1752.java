00467e690398efac0599f73c2e4ac81e9720805a
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-02-09 23:54:12 -0600

    Update problem 1752

--- src/main/java/leetcode/Problem1752.java
+++ src/main/java/leetcode/Problem1752.java
@@ -5,16 +5,37 @@ package leetcode;
  */
 public class Problem1752 {
     public boolean check(int[] nums) {
-        // TODO
-        return false;
+        int min = 0;
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i == 0) {
+                min = nums[i];
+            } else {
+                if (nums[i] < min) {
+                    index = i;
+                    min = nums[i];
+                }
+            }
+        }
+        int current = index;
+        int next = (index + 1) % nums.length;
+        while (next != index) {
+            if (nums[current] > nums[next]) {
+                return false;
+            }
+            current = (current + 1) % nums.length;
+            next = (next + 1) % nums.length;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
         Problem1752 prob = new Problem1752();
-        System.out.println(prob.check(new int[]{3,4,5,1,2})); // true
-        System.out.println(prob.check(new int[]{2,1,3,4})); // false
-        System.out.println(prob.check(new int[]{1,2,3})); // true
-        System.out.println(prob.check(new int[]{1,1,1})); // true
-        System.out.println(prob.check(new int[]{2,1})); // true
+//        System.out.println(prob.check(new int[]{3,4,5,1,2})); // true
+//        System.out.println(prob.check(new int[]{2,1,3,4})); // false
+//        System.out.println(prob.check(new int[]{1,2,3})); // true
+//        System.out.println(prob.check(new int[]{1,1,1})); // true
+//        System.out.println(prob.check(new int[]{2,1})); // true
+        System.out.println(prob.check(new int[]{6,10,6})); // true
     }
 }
