82847bd89d5b3e9f80a1cc277c13777137d85b49
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-02-11 20:39:31 -0600

    Skeleton for problem 1753

---
+++ src/main/java/leetcode/Problem1753.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/maximum-score-from-removing-stones/
+ */
+public class Problem1753 {
+    public int maximumScore(int a, int b, int c) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1753 prob = new Problem1753();
+        System.out.println(prob.maximumScore(2, 4, 6)); // 6
+        System.out.println(prob.maximumScore(4, 4, 6)); // 7
+        System.out.println(prob.maximumScore(1, 8, 8)); // 8
+    }
+}
