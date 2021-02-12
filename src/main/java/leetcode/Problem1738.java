fbd3f521f134d12c97b3d7723297cc2288e8a3bc
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-02-12 17:03:15 -0600

    Update problem 1738

--- src/main/java/leetcode/Problem1738.java
+++ src/main/java/leetcode/Problem1738.java
@@ -5,7 +5,40 @@ package leetcode;
  */
 public class Problem1738 {
     public int kthLargestValue(int[][] matrix, int k) {
-        // TODO
+        int maxRow = matrix.length;
+        int maxCol = maxRow > 0 ? matrix[0].length : 0;
+        int xor1 = 0;
+        for (int row = 0; row < maxRow; row++) {
+            int xor2 = 0;
+            for (int col = 0; col < maxCol; col++) {
+                if (col == 0) {
+                    xor2 = matrix[row][col];
+                } else {
+                    xor2 ^= matrix[row][col];
+                }
+            }
+            if (row == 0) {
+                xor1 = xor2;
+            } else {
+                xor1 ^= xor2;
+            }
+        }
+        xor1 = 0;
+        for (int col = 0; col < maxCol; col++) {
+            int xor2 = 0;
+            for (int row = 0; row < maxRow; row++) {
+                if (col == 0) {
+                    xor2 = matrix[row][col];
+                } else {
+                    xor2 ^= matrix[row][col];
+                }
+            }
+            if (col == 0) {
+                xor1 = xor2;
+            } else {
+                xor1 ^= xor2;
+            }
+        }
         return 0;
     }
 
