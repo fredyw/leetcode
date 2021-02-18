5fa3ae49e016b5140cd07f238865c272e358543e
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-02-18 14:30:05 -0600

    Skeleton for problem 1749

---
+++ src/main/java/leetcode/Problem1749.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/maximum-absolute-sum-of-any-subarray/
+ */
+public class Problem1749 {
+    public int maxAbsoluteSum(int[] nums) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1749 prob = new Problem1749();
+        System.out.println(prob.maxAbsoluteSum(new int[]{1,-3,2,3,-4})); // 5
+        System.out.println(prob.maxAbsoluteSum(new int[]{2,-5,1,-4,3,-2})); // 8
+    }
+}
