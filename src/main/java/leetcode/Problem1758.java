c150bdfcb06ab7bfef7e43484eecbd009fb6e72f
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-02-18 13:49:44 -0600

    Solve problem 1758

--- src/main/java/leetcode/Problem1758.java
+++ src/main/java/leetcode/Problem1758.java
@@ -5,14 +5,18 @@ package leetcode;
  */
 public class Problem1758 {
     public int minOperations(String s) {
-        // TODO
-        return 0;
+        int answer = Integer.MAX_VALUE;
+        return Math.min(answer, Math.min(getCount(s, false), getCount(s, true)));
     }
 
-    public static void main(String[] args) {
-        Problem1758 prob = new Problem1758();
-        System.out.println(prob.minOperations("0100")); // 1
-        System.out.println(prob.minOperations("10")); // 0
-        System.out.println(prob.minOperations("1111")); // 2
+    private static int getCount(String s, boolean zero) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if ((zero && s.charAt(i) == '1') || (!zero && s.charAt(i) == '0')) {
+                count++;
+            }
+            zero = !zero;
+        }
+        return count;
     }
 }
