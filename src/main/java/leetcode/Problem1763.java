e244490a436b16504eca1001ed907d864a1a00be
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-02-22 09:50:40 -0600

    Skeleton for problem 1763

---
+++ src/main/java/leetcode/Problem1763.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/longest-nice-substring/
+ */
+public class Problem1763 {
+    public String longestNiceSubstring(String s) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem1763 prob = new Problem1763();
+        System.out.println(prob.longestNiceSubstring("YazaAay")); // "aAa"
+        System.out.println(prob.longestNiceSubstring("Bb")); // "Bb"
+        System.out.println(prob.longestNiceSubstring("c")); // ""
+        System.out.println(prob.longestNiceSubstring("dDzeE")); // "dD"
+    }
+}
