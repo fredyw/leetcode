7cdb41045c90554d05461a2f5fb527de7dbf1866
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-02-22 11:29:01 -0600

    Update problem 1763

--- src/main/java/leetcode/Problem1763.java
+++ src/main/java/leetcode/Problem1763.java
@@ -5,8 +5,9 @@ package leetcode;
  */
 public class Problem1763 {
     public String longestNiceSubstring(String s) {
+        StringBuilder answer = new StringBuilder();
         // TODO
-        return null;
+        return answer.toString();
     }
 
     public static void main(String[] args) {
@@ -15,5 +16,7 @@ public class Problem1763 {
         System.out.println(prob.longestNiceSubstring("Bb")); // "Bb"
         System.out.println(prob.longestNiceSubstring("c")); // ""
         System.out.println(prob.longestNiceSubstring("dDzeE")); // "dD"
+        System.out.println(prob.longestNiceSubstring("aBbA")); // "aBbA"
+        System.out.println(prob.longestNiceSubstring("aBbAa")); // "aBbAa"
     }
 }
