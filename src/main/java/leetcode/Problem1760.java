08ab807822d8af61ecf83bf1fd21a333410c6ac2
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-02-24 16:50:57 -0600

    Skeleton for problem 1760

---
+++ src/main/java/leetcode/Problem1760.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/minimum-limit-of-balls-in-a-bag/
+ */
+public class Problem1760 {
+    public int minimumSize(int[] nums, int maxOperations) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1760 prob = new Problem1760();
+        System.out.println(prob.minimumSize(new int[]{9}, 2)); // 3
+        System.out.println(prob.minimumSize(new int[]{2,4,8,2}, 4)); // 2
+        System.out.println(prob.minimumSize(new int[]{7,17}, 2)); // 7
+    }
+}
