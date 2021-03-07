eedf78c4216e636920b98f2dfb6d914e7f8eaaef
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-03-07 12:47:38 -0600

    Skeleton for problem 1784

---
+++ src/main/java/leetcode/Problem1784.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/check-if-binary-string-has-at-most-one-segment-of-ones/
+ */
+public class Problem1784 {
+    public boolean checkOnesSegment(String s) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem1784 prob = new Problem1784();
+        System.out.println(prob.checkOnesSegment("1001")); // false
+        System.out.println(prob.checkOnesSegment("110")); // true
+    }
+}
