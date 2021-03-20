51280350ca0d31a5cca03b2cb8a52b4d0f43b419
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-03-20 18:43:52 -0500

    Skeleton for problem 1796

---
+++ src/main/java/leetcode/Problem1796.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/second-largest-digit-in-a-string/
+ */
+public class Problem1796 {
+    public int secondHighest(String s) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1796 prob = new Problem1796();
+        System.out.println(prob.secondHighest("dfa12321afd")); // 2
+        System.out.println(prob.secondHighest("abc1111")); // -1
+    }
+}
