872b37b92ad14c5ead0fbed54108c9faca76d107
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-03-20 19:00:17 -0500

    Solve problem 1796

--- src/main/java/leetcode/Problem1796.java
+++ src/main/java/leetcode/Problem1796.java
@@ -5,13 +5,27 @@ package leetcode;
  */
 public class Problem1796 {
     public int secondHighest(String s) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem1796 prob = new Problem1796();
-        System.out.println(prob.secondHighest("dfa12321afd")); // 2
-        System.out.println(prob.secondHighest("abc1111")); // -1
+        int max = -1;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c < '0' || c > '9') {
+                continue;
+            }
+            int num = c - '0';
+            max = Math.max(max, num);
+        }
+        int answer = -1;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c < '0' || c > '9') {
+                continue;
+            }
+            int num = c - '0';
+            if (max == num) {
+                continue;
+            }
+            answer = Math.max(answer, num);
+        }
+        return answer;
     }
 }
