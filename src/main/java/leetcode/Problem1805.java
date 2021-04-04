821fe7d275349a28521f69eeed1fe9a28b4637b5
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-04-04 13:13:35 -0500

    Solve problem 1805

--- src/main/java/leetcode/Problem1805.java
+++ src/main/java/leetcode/Problem1805.java
@@ -1,18 +1,22 @@
 package leetcode;
 
+import java.math.BigInteger;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * https://leetcode.com/problems/number-of-different-integers-in-a-string/
  */
 public class Problem1805 {
     public int numDifferentIntegers(String word) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem1805 prob = new Problem1805();
-        System.out.println(prob.numDifferentIntegers("a123bc34d8ef34")); // 3
-        System.out.println(prob.numDifferentIntegers("leet1234code234")); // 2
-        System.out.println(prob.numDifferentIntegers("a1b01c001")); // 1
+        String[] digits = word.replaceAll("[a-z]+", " ").split(" ");
+        Set<BigInteger> set = new HashSet<>();
+        for (String digit : digits) {
+            if (digit.isEmpty()) {
+                continue;
+            }
+            set.add(new BigInteger(digit));
+        }
+        return set.size();
     }
 }
