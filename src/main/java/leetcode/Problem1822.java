ebae949ce16957f755c71e0b3d23e6d27acef8c6
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-04-12 23:13:05 -0500

    Solve problem 1822

--- src/main/java/leetcode/Problem1822.java
+++ src/main/java/leetcode/Problem1822.java
@@ -5,14 +5,21 @@ package leetcode;
  */
 public class Problem1822 {
     public int arraySign(int[] nums) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem1822 prob = new Problem1822();
-        System.out.println(prob.arraySign(new int[]{-1,-2,-3,-4,3,2,1})); // 1
-        System.out.println(prob.arraySign(new int[]{1,5,0,2,-3})); // 0
-        System.out.println(prob.arraySign(new int[]{-1,1,-1,1,-1})); // -1
+        int answer;
+        if (nums[0] < 0) {
+            answer = -1;
+        } else if (nums[0] == 0) {
+            return 0;
+        } else {
+            answer = 1;
+        }
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < 0) {
+                answer *= -1;
+            } else if (nums[i] == 0) {
+                return 0;
+            }
+        }
+        return answer;
     }
 }
