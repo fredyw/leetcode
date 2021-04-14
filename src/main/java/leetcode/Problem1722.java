f27cfb81e6788719d3271578a65e9151d6afef51
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-04-14 00:09:33 -0500

    Solve problem 1722

--- src/main/java/leetcode/Problem1722.java
+++ src/main/java/leetcode/Problem1722.java
@@ -8,7 +8,7 @@ import java.util.Map;
  */
 public class Problem1722 {
     public int minimumHammingDistance(int[] source, int[] target, int[][] allowedSwaps) {
-        UnionFind uf = new UnionFind();
+        UnionFind uf = new UnionFind(source.length);
         for (int[] swap : allowedSwaps) {
             uf.union(swap[0], swap[1]);
         }
@@ -38,48 +38,34 @@ public class Problem1722 {
     }
 
     private static class UnionFind {
-        private final Map</* child */ Integer, /* parent */ Integer> map = new HashMap<>();
+        private final Integer[] parents; // child to parent
+
+        private UnionFind(int n) {
+            parents = new Integer[n];
+        }
 
         private void union(int a, int b) {
-            if (!map.containsKey(a)) {
-                map.put(a, a);
+            if (parents[a] == null) {
+                parents[a] = a;
             }
-            if (!map.containsKey(b)) {
-                map.put(b, b);
+            if (parents[b] == null) {
+                parents[b] = b;
             }
             int rootA = find(a);
-            int rootY = find(b);
-            if (rootA != rootY) {
-                map.put(rootA, rootY);
+            int rootB = find(b);
+            if (rootA != rootB) {
+                parents[rootA] = rootB;
             }
         }
 
         private int find(int a) {
             Integer child = a;
-            Integer parent = map.get(a);
-            while (child != null && !child.equals(parent)) {
+            Integer parent = parents[child];
+            while (parent != null && !child.equals(parent)) {
                 child = parent;
-                parent = map.get(child);
+                parent = parents[child];
             }
             return parent == null ? a : parent;
         }
-    }
-
-    public static void main(String[] args) {
-        Problem1722 prob = new Problem1722();
-        System.out.println(prob.minimumHammingDistance(
-            new int[]{1,2,3,4}, new int[]{2,1,4,5}, new int[][]{{0,1},{2,3}})); // 1
-        System.out.println(prob.minimumHammingDistance(
-            new int[]{1,2,3,4}, new int[]{1,3,2,4}, new int[][]{})); // 2
-        System.out.println(prob.minimumHammingDistance(
-            new int[]{5,1,2,4,3}, new int[]{1,5,4,2,3}, new int[][]{{0,4},{4,2},{1,3},{1,4}})); // 0
-        System.out.println(prob.minimumHammingDistance(
-            new int[]{5,1,2,4,3}, new int[]{1,5,4,2,3}, new int[][]{{0,3},{3,2},{3,1}})); // 0
-        System.out.println(prob.minimumHammingDistance(
-            new int[]{5,1,2,4,3}, new int[]{1,5,4,2,4}, new int[][]{{0,3},{3,2},{3,1}})); // 1
-        System.out.println(prob.minimumHammingDistance(
-            new int[]{2,3,1}, new int[]{1,2,2}, new int[][]{{0,2},{1,2}})); // 1
-        System.out.println(prob.minimumHammingDistance(
-            new int[]{2,3,1,1}, new int[]{1,1,2,2}, new int[][]{{0,2},{1,2}})); // 2
     }
 }
