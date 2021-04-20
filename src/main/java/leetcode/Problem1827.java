1c9f81216c695c87d9c2b4553aef76fcdc0ddd6d
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-04-20 00:36:35 -0500

    Solve problem 1827

--- src/main/java/leetcode/Problem1827.java
+++ src/main/java/leetcode/Problem1827.java
@@ -5,14 +5,13 @@ package leetcode;
  */
 public class Problem1827 {
     public int minOperations(int[] nums) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem1827 prob = new Problem1827();
-        System.out.println(prob.minOperations(new int[]{1,1,1})); // 3
-        System.out.println(prob.minOperations(new int[]{1,5,2,4,1})); // 14
-        System.out.println(prob.minOperations(new int[]{8})); // 0
+        int answer = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] >= nums[i]) {
+                answer += nums[i - 1] - nums[i] + 1;
+                nums[i] = nums[i - 1] + 1;
+            }
+        }
+        return answer;
     }
 }
