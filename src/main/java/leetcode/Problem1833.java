81270e9769af9a3a79993fdb8992a6809300b03e
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-04-24 15:59:15 -0500

    Skeleton for problem 1833

---
+++ src/main/java/leetcode/Problem1833.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/maximum-ice-cream-bars/
+ */
+public class Problem1833 {
+    public int maxIceCream(int[] costs, int coins) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1833 prob = new Problem1833();
+        System.out.println(prob.maxIceCream(new int[]{1,3,2,4,1}, 7)); // 4
+        System.out.println(prob.maxIceCream(new int[]{10,6,8,7,7,8}, 5)); // 0
+        System.out.println(prob.maxIceCream(new int[]{1,6,3,1,2,5}, 20)); // 6
+    }
+}
