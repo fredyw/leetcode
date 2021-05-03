eccd41949feb06f0e859ef404d7982d00d8751d0
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-05-02 19:03:49 -0500

    Skeleton for problem 1844

---
+++ src/main/java/leetcode/Problem1844.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/replace-all-digits-with-characters/
+ */
+public class Problem1844 {
+    public String replaceDigits(String s) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem1844 prob = new Problem1844();
+        System.out.println(prob.replaceDigits("a1c1e1")); // "abcdef"
+        System.out.println(prob.replaceDigits("a1b2c3d4e")); // "abbdcfdhe"
+    }
+}
