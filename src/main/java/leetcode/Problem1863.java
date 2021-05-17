4e4edc7190e3313704d649d961c4d3854aa88f3b
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-05-16 23:03:21 -0500

    Solve problem 1863

--- src/main/java/leetcode/Problem1863.java
+++ src/main/java/leetcode/Problem1863.java
@@ -1,18 +1,34 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * https://leetcode.com/problems/sum-of-all-subset-xor-totals/
  */
 public class Problem1863 {
     public int subsetXORSum(int[] nums) {
-        // TODO
-        return 0;
+        IntRef ref = new IntRef();
+        subSetXORSum(nums, 0, new ArrayList<>(), ref);
+        return ref.val;
     }
 
-    public static void main(String[] args) {
-        Problem1863 prob = new Problem1863();
-        System.out.println(prob.subsetXORSum(new int[]{1,3})); // 6
-        System.out.println(prob.subsetXORSum(new int[]{5,1,6})); // 28
-        System.out.println(prob.subsetXORSum(new int[]{3,4,5,6,7,8})); // 480
+    private static class IntRef {
+        private int val;
+    }
+
+    private static void subSetXORSum(int[] nums, int index, List<Integer> subset, IntRef ref) {
+        if (!subset.isEmpty()) {
+            int xor = subset.get(0);
+            for (int i = 1; i < subset.size(); i++) {
+                xor ^= subset.get(i);
+            }
+            ref.val += xor;
+        }
+        for (int i = index; i < nums.length; i++) {
+            subset.add(nums[i]);
+            subSetXORSum(nums, i + 1, subset, ref);
+            subset.remove(subset.size() - 1);
+        }
     }
 }
