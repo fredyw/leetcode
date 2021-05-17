012630bfdb34b2c8de9172c3f37d3ccf09eef0dd
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-05-16 22:23:06 -0500

    Skeleton for problem 1863

---
+++ src/main/java/leetcode/Problem1863.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/sum-of-all-subset-xor-totals/
+ */
+public class Problem1863 {
+    public int subsetXORSum(int[] nums) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1863 prob = new Problem1863();
+        System.out.println(prob.subsetXORSum(new int[]{1,3})); // 6
+        System.out.println(prob.subsetXORSum(new int[]{5,1,6})); // 28
+        System.out.println(prob.subsetXORSum(new int[]{3,4,5,6,7,8})); // 480
+    }
+}
