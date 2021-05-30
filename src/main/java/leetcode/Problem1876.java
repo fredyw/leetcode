40d00f6f431ab2d7d7e98f747638cacbd86d4700
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-05-29 21:17:02 -0500

    Skeleton for problem 1876

---
+++ src/main/java/leetcode/Problem1876.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/substrings-of-size-three-with-distinct-characters/
+ */
+public class Problem1876 {
+    public int countGoodSubstrings(String s) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1876 prob = new Problem1876();
+        System.out.println(prob.countGoodSubstrings("xyzzaz")); // 1
+        System.out.println(prob.countGoodSubstrings("aababcabc")); // 4
+    }
+}
