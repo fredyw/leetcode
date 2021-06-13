117229977fbb17119449bcf69d938c76c184f707
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-06-12 20:32:27 -0500

    Skeleton for problem 1877

---
+++ src/main/java/leetcode/Problem1877.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/minimize-maximum-pair-sum-in-array/
+ */
+public class Problem1877 {
+    public int minPairSum(int[] nums) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1877 prob = new Problem1877();
+        System.out.println(prob.minPairSum(new int[]{3,5,2,3})); // 7
+        System.out.println(prob.minPairSum(new int[]{3,5,4,2,4,6})); // 8
+    }
+}
