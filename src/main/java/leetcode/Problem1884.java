9c5eb558330881523757718a885ae62f06a81d86
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-06-12 20:45:51 -0500

    Skeleton for problem 1884

---
+++ src/main/java/leetcode/Problem1884.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/egg-drop-with-2-eggs-and-n-floors/
+ */
+public class Problem1884 {
+    public int twoEggDrop(int n) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1884 prob = new Problem1884();
+        System.out.println(prob.twoEggDrop(2)); // 2
+        System.out.println(prob.twoEggDrop(100)); // 14
+    }
+}
