66c6c0be3503f9d7465b691d8284edeaf5617adc
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-06-17 00:57:47 -0500

    Update problem 1884

--- src/main/java/leetcode/Problem1884.java
+++ src/main/java/leetcode/Problem1884.java
@@ -12,6 +12,9 @@ public class Problem1884 {
     public static void main(String[] args) {
         Problem1884 prob = new Problem1884();
         System.out.println(prob.twoEggDrop(2)); // 2
+        System.out.println(prob.twoEggDrop(3)); // 2
+        System.out.println(prob.twoEggDrop(4)); // 3
+        System.out.println(prob.twoEggDrop(5)); // 3
         System.out.println(prob.twoEggDrop(100)); // 14
     }
 }
