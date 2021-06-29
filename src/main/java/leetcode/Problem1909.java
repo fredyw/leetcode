67634da38e1dc96787ab0aefd1484e71889399ea
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-06-28 23:53:24 -0500

    Skeleton for problem 1909

---
+++ src/main/java/leetcode/Problem1909.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/remove-one-element-to-make-the-array-strictly-increasing/
+ */
+public class Problem1909 {
+    public boolean canBeIncreasing(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem1909 prob = new Problem1909();
+        System.out.println(prob.canBeIncreasing(new int[]{1,2,10,5,7})); // true
+        System.out.println(prob.canBeIncreasing(new int[]{2,3,1,2})); // false
+        System.out.println(prob.canBeIncreasing(new int[]{1,1,1})); // false
+        System.out.println(prob.canBeIncreasing(new int[]{1,2,3})); // true
+    }
+}
