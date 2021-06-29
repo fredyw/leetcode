3259d70df80de3f11ea2accc0ff39bad4e01d524
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-06-28 23:55:13 -0500

    Skeleton for problem 1913

--- src/main/java/leetcode/Problem1909.java
+++ src/main/java/leetcode/Problem1909.java
@@ -5,11 +5,7 @@ package leetcode;
  */
 public class Problem1909 {
     public boolean canBeIncreasing(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums.length; j++) {
-
-            }
-        }
+        // TODO
         return false;
     }
 
---
+++ src/main/java/leetcode/Problem1913.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/maximum-product-difference-between-two-pairs/
+ */
+public class Problem1913 {
+    public int maxProductDifference(int[] nums) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1913 prob = new Problem1913();
+        System.out.println(prob.maxProductDifference(new int[]{5,6,2,7,4})); // 34
+        System.out.println(prob.maxProductDifference(new int[]{4,2,5,9,7,4,8})); // 64
+    }
+}
