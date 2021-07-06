052d247040fcdac6cd9055f6428841c7b4d7b809
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-07-06 15:06:41 -0500

    Solve problem 1910

--- src/main/java/leetcode/Problem1910.java
+++ src/main/java/leetcode/Problem1910.java
@@ -5,13 +5,14 @@ package leetcode;
  */
 public class Problem1910 {
     public String removeOccurrences(String s, String part) {
-        // TODO
-        return null;
-    }
-
-    public static void main(String[] args) {
-        Problem1910 prob = new Problem1910();
-        System.out.println(prob.removeOccurrences("daabcbaabcbc", "abc")); // "dab"
-        System.out.println(prob.removeOccurrences("axxxxyyyyb", "xy")); // "ab"
+        String answer = s;
+        while (true) {
+            String tmp = answer;
+            answer = answer.replaceFirst(part, "");
+            if (tmp.equals(answer)) {
+                break;
+            }
+        }
+        return answer;
     }
 }
