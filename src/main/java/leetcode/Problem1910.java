ca958d13a1d270703f1ca31fb3b6d7a0e6fd4a01
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-07-06 14:24:14 -0500

    Skeleton for problem 1910

---
+++ src/main/java/leetcode/Problem1910.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/remove-all-occurrences-of-a-substring/
+ */
+public class Problem1910 {
+    public String removeOccurrences(String s, String part) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem1910 prob = new Problem1910();
+        System.out.println(prob.removeOccurrences("daabcbaabcbc", "abc")); // "dab"
+        System.out.println(prob.removeOccurrences("axxxxyyyyb", "xy")); // "ab"
+    }
+}
