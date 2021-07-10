0ebf28d4c69b83740e0778be0dc9fdfdb3984fbb
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-07-10 14:39:25 -0500

    Skeleton for problem 1925

---
+++ src/main/java/leetcode/Problem1925.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/count-square-sum-triples/
+ */
+public class Problem1925 {
+    public int countTriples(int n) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1925 prob = new Problem1925();
+        System.out.println(prob.countTriples(5)); // 2
+        System.out.println(prob.countTriples(10)); // 4
+    }
+}
