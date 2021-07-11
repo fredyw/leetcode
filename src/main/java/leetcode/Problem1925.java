57fb280bc37bfdcc5a30f6bb69ad71e4813a2c5f
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-07-10 22:10:30 -0500

    Solve problem 1925

--- src/main/java/leetcode/Problem1925.java
+++ src/main/java/leetcode/Problem1925.java
@@ -1,17 +1,27 @@
 package leetcode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * https://leetcode.com/problems/count-square-sum-triples/
  */
 public class Problem1925 {
     public int countTriples(int n) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem1925 prob = new Problem1925();
-        System.out.println(prob.countTriples(5)); // 2
-        System.out.println(prob.countTriples(10)); // 4
+        Set</* square */ Integer> squares = new HashSet<>();
+        for (int i = 1; i <= n; i++) {
+            squares.add(i * i);
+        }
+        int answer = 0;
+        for (int a = 1; a <= n; a++) {
+            for (int b = 2; b <= n; b++) {
+                // a^2 + b^2 = c^2
+                int c = (a * a) + (b * b);
+                if (squares.contains(c)) {
+                    answer++;
+                }
+            }
+        }
+        return answer;
     }
 }
