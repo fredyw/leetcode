609a670acf0773fc15e3b768a22f25d215aed27c
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-07-12 09:20:19 -0500

    Update problem 1775

--- src/main/java/leetcode/Problem1775.java
+++ src/main/java/leetcode/Problem1775.java
@@ -1,12 +1,46 @@
 package leetcode;
 
+import java.util.Arrays;
+
 /**
  * https://leetcode.com/problems/equal-sum-arrays-with-minimum-number-of-operations/
  */
 public class Problem1775 {
     public int minOperations(int[] nums1, int[] nums2) {
-        // TODO
-        return 0;
+        if (nums1.length < nums2.length && 6 * nums1.length < nums2.length) {
+            return -1;
+        } else if (nums1.length > nums2.length && nums1.length > 6 * nums2.length) {
+            return -1;
+        }
+        int sum1 = 0;
+        for (int num : nums1) {
+            sum1 += num;
+        }
+        int sum2 = 0;
+        for (int num : nums2) {
+            sum2 += num;
+        }
+        int answer = 0;
+        if (sum1 < sum2) {
+            Arrays.sort(nums1);
+            for (int num : nums1) {
+                sum1 += 6 - num;
+                answer++;
+                if (sum1 >= sum2) {
+                    break;
+                }
+            }
+        } else if (sum1 > sum2) {
+            Arrays.sort(nums2);
+            for (int num : nums2) {
+                sum2 += 6 - num;
+                answer++;
+                if (sum2 >= sum1) {
+                    break;
+                }
+            }
+        }
+        return answer;
     }
 
     public static void main(String[] args) {
