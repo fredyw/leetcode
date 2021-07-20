c66642d90393762f3aaa3c3c95ceecb843d55bba
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-07-20 00:12:27 -0500

    Skeleton for problem 1823

---
+++ src/main/java/leetcode/Problem1823.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/find-the-winner-of-the-circular-game/
+ */
+public class Problem1823 {
+    public int findTheWinner(int n, int k) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1823 prob = new Problem1823();
+        System.out.println(prob.findTheWinner(5, 2)); // 3
+        System.out.println(prob.findTheWinner(6, 5)); // 1
+    }
+}
