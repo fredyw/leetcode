4a54fbb2981db275612f84912d2073fd6e5d75df
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-07-20 21:22:47 -0500

    Solve problem 1823

--- src/main/java/leetcode/Problem1823.java
+++ src/main/java/leetcode/Problem1823.java
@@ -1,17 +1,22 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * https://leetcode.com/problems/find-the-winner-of-the-circular-game/
  */
 public class Problem1823 {
     public int findTheWinner(int n, int k) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem1823 prob = new Problem1823();
-        System.out.println(prob.findTheWinner(5, 2)); // 3
-        System.out.println(prob.findTheWinner(6, 5)); // 1
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            list.add(i);
+        }
+        int i = 0;
+        while (list.size() > 1) {
+            i = (i + k - 1) % list.size();
+            list.remove(i);
+        }
+        return list.get(0);
     }
 }
