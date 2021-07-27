dbbcfcb074385be42496f24bf6167158e51247f9
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-07-27 00:09:16 -0500

    Skeleton for problem 1806

---
+++ src/main/java/leetcode/Problem1806.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/minimum-number-of-operations-to-reinitialize-a-permutation/
+ */
+public class Problem1806 {
+    public int reinitializePermutation(int n) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1806 prob = new Problem1806();
+        System.out.println(prob.reinitializePermutation(2)); // 1
+        System.out.println(prob.reinitializePermutation(4)); // 2
+        System.out.println(prob.reinitializePermutation(6)); // 4
+    }
+}
