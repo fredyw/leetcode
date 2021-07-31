053a141d90e8e135930acd9aa984a48037ca799f
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-07-30 19:30:04 -0500

    Skeleton for problem 1945

---
+++ src/main/java/leetcode/Problem1945.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/sum-of-digits-of-string-after-convert/
+ */
+public class Problem1945 {
+    public int getLucky(String s, int k) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1945 prob = new Problem1945();
+        System.out.println(prob.getLucky("iiii", 1)); // 36
+        System.out.println(prob.getLucky("leetcode", 2)); // 6
+        System.out.println(prob.getLucky("zbax", 2)); // 8
+    }
+}
