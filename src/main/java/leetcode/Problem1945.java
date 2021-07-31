52be437953705d749c34d264043c9db66dd3afab
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-07-30 20:06:13 -0500

    Solve problem 1945

--- src/main/java/leetcode/Problem1945.java
+++ src/main/java/leetcode/Problem1945.java
@@ -5,14 +5,30 @@ package leetcode;
  */
 public class Problem1945 {
     public int getLucky(String s, int k) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem1945 prob = new Problem1945();
-        System.out.println(prob.getLucky("iiii", 1)); // 36
-        System.out.println(prob.getLucky("leetcode", 2)); // 6
-        System.out.println(prob.getLucky("zbax", 2)); // 8
+        int sum = 0;
+        for (char c : s.toCharArray()) {
+            int n = c - 'a' + 1;
+            if (n / 10 == 2) {
+                sum += 2 + (n % 10);
+            } else if (n / 10 == 1) {
+                sum += 1 + (n % 10);
+            } else {
+                sum += n % 10;
+            }
+        }
+        int answer = sum;
+        String str = Integer.toString(sum);
+        for (int i = 0; i < k - 1; i++) {
+            sum = 0;
+            for (char c : str.toCharArray()) {
+                sum += c - '0';
+            }
+            answer = sum;
+            str = Integer.toString(sum);
+            if (str.length() == 1) {
+                break;
+            }
+        }
+        return answer;
     }
 }
