041db2ba6e13fbc4e0a63689a1492d2826ea53b3
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-08-03 14:29:36 -0500

    Skeleton for problem 1780

---
+++ src/main/java/leetcode/Problem1780.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/check-if-number-is-a-sum-of-powers-of-three/
+ */
+public class Problem1780 {
+    public boolean checkPowersOfThree(int n) {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Problem1780 prob = new Problem1780();
+        System.out.println(prob.checkPowersOfThree(12)); // true
+        System.out.println(prob.checkPowersOfThree(91)); // true
+        System.out.println(prob.checkPowersOfThree(21)); // false
+    }
+}
