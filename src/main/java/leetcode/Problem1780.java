980625a3c7f96e85b3288e153967b293c4676756
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-08-06 01:01:17 -0500

    Update problem 1780

--- src/main/java/leetcode/Problem1780.java
+++ src/main/java/leetcode/Problem1780.java
@@ -5,14 +5,20 @@ package leetcode;
  */
 public class Problem1780 {
     public boolean checkPowersOfThree(int n) {
+        long sum = 0;
         // TODO
         return false;
     }
 
     public static void main(String[] args) {
         Problem1780 prob = new Problem1780();
+        System.out.println(prob.checkPowersOfThree(1)); // false
+        System.out.println(prob.checkPowersOfThree(2)); // false
+        System.out.println(prob.checkPowersOfThree(3)); // true
+        System.out.println(prob.checkPowersOfThree(4)); // true
         System.out.println(prob.checkPowersOfThree(12)); // true
-        System.out.println(prob.checkPowersOfThree(91)); // true
         System.out.println(prob.checkPowersOfThree(21)); // false
+        System.out.println(prob.checkPowersOfThree(30)); // true
+        System.out.println(prob.checkPowersOfThree(91)); // true
     }
 }
