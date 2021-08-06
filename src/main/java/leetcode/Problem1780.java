90ac266c430f607284d4d2a292dc3a8061aff1e0
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-08-06 01:10:35 -0500

    Update problem 1780

--- src/main/java/leetcode/Problem1780.java
+++ src/main/java/leetcode/Problem1780.java
@@ -16,8 +16,31 @@ public class Problem1780 {
         System.out.println(prob.checkPowersOfThree(2)); // false
         System.out.println(prob.checkPowersOfThree(3)); // true
         System.out.println(prob.checkPowersOfThree(4)); // true
+        System.out.println(prob.checkPowersOfThree(5)); // false
+        System.out.println(prob.checkPowersOfThree(6)); // false
+        System.out.println(prob.checkPowersOfThree(7)); // false
+        System.out.println(prob.checkPowersOfThree(8)); // false
+        System.out.println(prob.checkPowersOfThree(9)); // true
+        System.out.println(prob.checkPowersOfThree(10)); // true
+        System.out.println(prob.checkPowersOfThree(11)); // false
         System.out.println(prob.checkPowersOfThree(12)); // true
+        System.out.println(prob.checkPowersOfThree(13)); // true
+        System.out.println(prob.checkPowersOfThree(14)); // false
+        System.out.println(prob.checkPowersOfThree(15)); // false
+        System.out.println(prob.checkPowersOfThree(16)); // false
+        System.out.println(prob.checkPowersOfThree(17)); // false
+        System.out.println(prob.checkPowersOfThree(18)); // false
+        System.out.println(prob.checkPowersOfThree(19)); // false
+        System.out.println(prob.checkPowersOfThree(20)); // false
         System.out.println(prob.checkPowersOfThree(21)); // false
+        System.out.println(prob.checkPowersOfThree(22)); // false
+        System.out.println(prob.checkPowersOfThree(23)); // false
+        System.out.println(prob.checkPowersOfThree(24)); // false
+        System.out.println(prob.checkPowersOfThree(25)); // false
+        System.out.println(prob.checkPowersOfThree(26)); // false
+        System.out.println(prob.checkPowersOfThree(27)); // true
+        System.out.println(prob.checkPowersOfThree(28)); // true
+        System.out.println(prob.checkPowersOfThree(29)); // false
         System.out.println(prob.checkPowersOfThree(30)); // true
         System.out.println(prob.checkPowersOfThree(91)); // true
     }
