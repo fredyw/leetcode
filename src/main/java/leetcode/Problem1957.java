43a103858d1238fd1d9b725167647abe5cc800e1
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-08-08 09:12:34 -0500

    Skeleton for problem 1957

---
+++ src/main/java/leetcode/Problem1957.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/delete-characters-to-make-fancy-string/
+ */
+public class Problem1957 {
+    public String makeFancyString(String s) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem1957 prob = new Problem1957();
+        System.out.println(prob.makeFancyString("leeetcode")); // leetcode
+        System.out.println(prob.makeFancyString("aaabaaaa")); // aabaa
+        System.out.println(prob.makeFancyString("aab")); // aab
+    }
+}
