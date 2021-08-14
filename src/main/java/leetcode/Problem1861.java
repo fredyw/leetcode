9b7649e0affde89b9ab44d0aa828ed0e0863296d
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-08-14 15:27:42 -0500

    Solve problem 1861

--- src/main/java/leetcode/Problem1861.java
+++ src/main/java/leetcode/Problem1861.java
@@ -1,48 +1,33 @@
 package leetcode;
 
-import java.util.Arrays;
-
 /**
  * https://leetcode.com/problems/rotating-the-box/
  */
 public class Problem1861 {
     public char[][] rotateTheBox(char[][] box) {
-        // TODO
-        return null;
-    }
-
-    private static void print(char[][] chars) {
-        for (char[] c : chars) {
-            System.out.println(Arrays.toString(c));
+        int maxRow = box.length;
+        int maxCol = box.length > 0 ? box[0].length : 0;
+        char[][] answer = new char[maxCol][maxRow];
+        for (int col = 0; col < maxCol; col++) {
+            for (int row = 0; row < maxRow; row++) {
+                answer[col][maxRow - row - 1] = box[row][col];
+            }
         }
-    }
-
-    public static void main(String[] args) {
-        Problem1861 prob = new Problem1861();
-        // [["."],
-        //  ["#"],`
-        //  ["#"]]
-        print(prob.rotateTheBox(new char[][]{
-            {'#','.','#'}
-        }));
-        // [["#","."],
-        //  ["#","#"],
-        //  ["*","*"],
-        //  [".","."]]
-        print(prob.rotateTheBox(new char[][]{
-            {'#', '.', '*', '.'},
-            {'#', '#', '*', '.'}
-        }));
-        // [[".","#","#"],
-        //  [".","#","#"],
-        //  ["#","#","*"],
-        //  ["#","*","."],
-        //  ["#",".","*"],
-        //  ["#",".","."]]
-        print(prob.rotateTheBox(new char[][]{
-            {'#', '#', '*', '.', '*', '.'},
-            {'#', '#', '#', '*', '.', '.'},
-            {'#', '#', '#', '.', '#', '.'}
-        }));
+        maxRow = answer.length;
+        maxCol = answer.length > 0 ? answer[0].length : 0;
+        for (int col = 0; col < maxCol; col++) {
+            for (int row = maxRow - 1; row >= 0; row--) {
+                if (answer[row][col] == '#') {
+                    int r = row;
+                    while (r + 1 < maxRow && answer[r + 1][col] == '.') {
+                        char tmp = answer[r][col];
+                        answer[r][col] = answer[r + 1][col];
+                        answer[r + 1][col] = tmp;
+                        r++;
+                    }
+                }
+            }
+        }
+        return answer;
     }
 }
