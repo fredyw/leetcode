d411d63f3c5072c58eb5ef7f42ef3aa50e868b65
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-08-15 14:16:21 -0500

    Solve problem 1905

--- src/main/java/leetcode/Problem1905.java
+++ src/main/java/leetcode/Problem1905.java
@@ -1,21 +1,27 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 /**
  * https://leetcode.com/problems/count-sub-islands/
  */
 public class Problem1905 {
     public int countSubIslands(int[][] grid1, int[][] grid2) {
+        int maxRow = grid1.length;
+        int maxCol = grid1.length > 0 ? grid1[0].length : 0;
+        boolean[][] visited = new boolean[maxRow][maxCol];
+        int islandNumber = 1;
+        for (int i = 0; i < maxRow; i++) {
+            for (int j = 0; j < maxCol; j++) {
+                markIslands(grid1, maxRow, maxCol, visited, i, j, islandNumber++);
+            }
+        }
         int answer = 0;
-        List<Set<String>> islands1 = getIslands(grid1);
-        List<Set<String>> islands2 = getIslands(grid2);
-        for (Set<String> island1 : islands1) {
-            for (Set<String> island2 : islands2) {
-                if (island1.containsAll(island2)) {
+        visited = new boolean[maxRow][maxCol];
+        for (int i = 0; i < maxRow; i++) {
+            for (int j = 0; j < maxCol; j++) {
+                if (grid1[i][j] == 0 || grid2[i][j] == 0 || visited[i][j]) {
+                    continue;
+                }
+                if (isSubIsland(grid1, grid2, maxRow, maxCol, visited, i, j, grid1[i][j])) {
                     answer++;
                 }
             }
@@ -23,69 +29,41 @@ public class Problem1905 {
         return answer;
     }
 
-    private static List<Set<String>> getIslands(int[][] grid) {
-        List<Set<String>> islands = new ArrayList<>();
-        int maxRow = grid.length;
-        int maxCol = grid.length > 0 ? grid[0].length : 0;
-        boolean[][] visited = new boolean[maxRow][maxCol];
-        for (int i = 0; i < maxRow; i++) {
-            for (int j = 0; j < maxCol; j++) {
-                Set<String> island = new HashSet<>();
-                getIsland(grid, maxRow, maxCol, visited, island, i, j);
-                if (!island.isEmpty()) {
-                    islands.add(island);
-                }
-            }
-        }
-        return islands;
-    }
-
-    private static void getIsland(int[][] grid, int maxRow, int maxCol, boolean[][] visited,
-                                  Set<String> island, int row, int col) {
+    private static void markIslands(int[][] grid, int maxRow, int maxCol, boolean[][] visited,
+                                    int row, int col, int islandNumber) {
         if (row < 0 || row == maxRow || col < 0 || col == maxCol) {
-            return;
-        }
-        if (grid[row][col] == 0) {
             return;
         }
         if (visited[row][col]) {
             return;
         }
         visited[row][col] = true;
-        island.add(row + "," + col);
-        getIsland(grid, maxRow, maxCol, visited, island, row - 1, col);
-        getIsland(grid, maxRow, maxCol, visited, island, row, col + 1);
-        getIsland(grid, maxRow, maxCol, visited, island, row + 1, col);
-        getIsland(grid, maxRow, maxCol, visited, island, row, col - 1);
+        if (grid[row][col] == 0) {
+            return;
+        }
+        grid[row][col] = islandNumber;
+        markIslands(grid, maxRow, maxCol, visited, row - 1, col, islandNumber);
+        markIslands(grid, maxRow, maxCol, visited, row, col + 1, islandNumber);
+        markIslands(grid, maxRow, maxCol, visited, row + 1, col, islandNumber);
+        markIslands(grid, maxRow, maxCol, visited, row, col - 1, islandNumber);
     }
 
-    public static void main(String[] args) {
-        Problem1905 prob = new Problem1905();
-        System.out.println(prob.countSubIslands(new int[][]{
-            {1,1,1,0,0},
-            {0,1,1,1,1},
-            {0,0,0,0,0},
-            {1,0,0,0,0},
-            {1,1,0,1,1}
-        }, new int[][]{
-            {1,1,1,0,0},
-            {0,0,1,1,1},
-            {0,1,0,0,0},
-            {1,0,1,1,0},
-            {0,1,0,1,0}
-        })); // 3
-        System.out.println(prob.countSubIslands(new int[][]{
-            {1,0,1,0,1},
-            {1,1,1,1,1},
-            {0,0,0,0,0},
-            {1,1,1,1,1},
-            {1,0,1,0,1}
-        }, new int[][]{
-            {0,0,0,0,0},
-            {1,1,1,1,1},
-            {0,1,0,1,0},
-            {0,1,0,1,0},
-            {1,0,0,0,1}
-        })); // 2
+    private static boolean isSubIsland(int[][] grid1, int[][] grid2, int maxRow, int maxCol,
+                                       boolean[][] visited, int row, int col, int islandNumber) {
+        if (row < 0 || row == maxRow || col < 0 || col == maxCol) {
+            return true;
+        }
+        if (visited[row][col]) {
+            return true;
+        }
+        visited[row][col] = true;
+        if (grid2[row][col] == 0) {
+            return true;
+        }
+        boolean up = isSubIsland(grid1, grid2, maxRow, maxCol, visited, row - 1, col, islandNumber);
+        boolean right = isSubIsland(grid1, grid2, maxRow, maxCol, visited, row, col + 1, islandNumber);
+        boolean down = isSubIsland(grid1, grid2, maxRow, maxCol, visited, row + 1, col, islandNumber);
+        boolean left = isSubIsland(grid1, grid2, maxRow, maxCol, visited, row, col - 1, islandNumber);
+        return up && right && down && left && grid1[row][col] == islandNumber;
     }
 }
