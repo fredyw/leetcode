95d752db60bd48d43d1d8fc9d6615a8e64e6aeb1
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-08-21 12:06:33 -0500

    Skeleton for problem 1974

---
+++ src/main/java/leetcode/Problem1974.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/minimum-time-to-type-word-using-special-typewriter/
+ */
+public class Problem1974 {
+    public int minTimeToType(String word) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1974 prob = new Problem1974();
+        System.out.println(prob.minTimeToType("abc")); // 5
+        System.out.println(prob.minTimeToType("bza")); // 7
+        System.out.println(prob.minTimeToType("zjpc")); // 34
+    }
+}
