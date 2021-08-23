a03759c45ec270e3f9521e9a56ef99457259e954
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-08-22 23:16:32 -0500

    Skeleton for problem 1979

---
+++ src/main/java/leetcode/Problem1979.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/find-greatest-common-divisor-of-array/
+ */
+public class Problem1979 {
+    public int findGCD(int[] nums) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1979 prob = new Problem1979();
+        System.out.println(prob.findGCD(new int[]{2,5,6,9,10})); // 2
+        System.out.println(prob.findGCD(new int[]{7,5,6,8,3})); // 1
+        System.out.println(prob.findGCD(new int[]{3,3})); // 3
+    }
+}
