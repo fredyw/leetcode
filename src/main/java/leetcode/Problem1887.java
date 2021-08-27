8ea062873401e8d72f19b46f08e9e949ccae09fc
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-08-27 00:25:57 -0500

    Update problem 1887

--- src/main/java/leetcode/Problem1887.java
+++ src/main/java/leetcode/Problem1887.java
@@ -1,12 +1,29 @@
 package leetcode;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  * https://leetcode.com/problems/reduction-operations-to-make-the-array-elements-equal/
  */
 public class Problem1887 {
     public int reductionOperations(int[] nums) {
-        // TODO
-        return 0;
+        TreeMap<Integer, Integer> map = new TreeMap<>((a, b) -> Integer.compare(b, a));
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+        int answer = 0;
+        while (map.size() > 1) {
+            Map.Entry<Integer, Integer> largestEntry = map.pollFirstEntry();
+            Map.Entry<Integer, Integer> nextLargest = map.firstEntry();
+            map.put(nextLargest.getKey(), nextLargest.getValue() + 1);
+            int largestEntryCount = largestEntry.getValue() - 1;
+            if (largestEntryCount > 0) {
+                map.put(largestEntry.getKey(), largestEntryCount);
+            }
+            answer++;
+        }
+        return answer;
     }
 
     public static void main(String[] args) {
