af451f8215379425de5134023a572180d1e36408
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-09-05 15:06:10 -0500

    Skeleton for problem 1995

---
+++ src/main/java/leetcode/Problem1995.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/count-special-quadruplets/
+ */
+public class Problem1995 {
+    public int countQuadruplets(int[] nums) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1995 prob = new Problem1995();
+        System.out.println(prob.countQuadruplets(new int[]{1,2,3,6})); // 1
+        System.out.println(prob.countQuadruplets(new int[]{3,3,6,4,5})); // 0
+        System.out.println(prob.countQuadruplets(new int[]{1,1,1,3,5})); // 4
+    }
+}
