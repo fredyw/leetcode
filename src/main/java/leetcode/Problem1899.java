c48e373ecfdcdd12cb590333d686d1e328c96795
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-09-05 15:01:22 -0500

    Solve problem 1899

--- src/main/java/leetcode/Problem1899.java
+++ src/main/java/leetcode/Problem1899.java
@@ -5,60 +5,28 @@ package leetcode;
  */
 public class Problem1899 {
     public boolean mergeTriplets(int[][] triplets, int[] target) {
-        // TODO
+        boolean[] valid = new boolean[3];
+        outer:
+        for (int[] triplet : triplets) {
+            boolean[] tmp = new boolean[3];
+            for (int i = 0; i < triplet.length; i++) {
+                if (triplet[i] > target[i]) {
+                    continue outer;
+                } else if (triplet[i] == target[i]) {
+                    tmp[i] = true;
+                }
+            }
+            int validCount = 0;
+            for (int i = 0; i < tmp.length; i++) {
+                valid[i] |= tmp[i];
+                if (valid[i]) {
+                    validCount++;
+                }
+            }
+            if (validCount == 3) {
+                return true;
+            }
+        }
         return false;
-    }
-
-    public static void main(String[] args) {
-        Problem1899 prob = new Problem1899();
-        System.out.println(prob.mergeTriplets(new int[][]{
-            {2,5,3},
-            {1,8,4},
-            {1,7,5}
-        }, new int[]{2,7,5})); // true
-        System.out.println(prob.mergeTriplets(new int[][]{
-            {1,3,4},
-            {2,5,8}
-        }, new int[]{2,5,8})); // true
-        System.out.println(prob.mergeTriplets(new int[][]{
-            {2,5,3},
-            {2,3,4},
-            {1,2,5},
-            {5,2,3}
-        }, new int[]{5,5,5})); // true
-        System.out.println(prob.mergeTriplets(new int[][]{
-            {2,5,3},
-            {2,3,4},
-            {1,2,5},
-            {5,2,3}
-        }, new int[]{5,5,3})); // true
-        System.out.println(prob.mergeTriplets(new int[][]{
-            {2,5,3},
-            {2,3,4},
-            {1,2,5},
-            {5,2,3}
-        }, new int[]{5,3,3})); // false
-        System.out.println(prob.mergeTriplets(new int[][]{
-            {2,5,3},
-            {2,3,4},
-            {1,2,5},
-            {5,2,3}
-        }, new int[]{1,5,3})); // false
-        System.out.println(prob.mergeTriplets(new int[][]{
-            {2,5,3},
-            {2,3,4},
-            {1,2,5},
-            {5,2,3}
-        }, new int[]{2,2,3})); // false
-        System.out.println(prob.mergeTriplets(new int[][]{
-            {2,5,3},
-            {2,3,4},
-            {1,2,5},
-            {5,2,3}
-        }, new int[]{2,3,3})); // false
-        System.out.println(prob.mergeTriplets(new int[][]{
-            {3,4,5},
-            {4,5,6}
-        }, new int[]{3,2,5})); // false
     }
 }
