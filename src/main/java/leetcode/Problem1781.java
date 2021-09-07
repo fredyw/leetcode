315fd1e63ad9b236fae68dbcf825dd0790b42997
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-09-06 20:15:12 -0500

    Skeleton for problem 1781

---
+++ src/main/java/leetcode/Problem1781.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/sum-of-beauty-of-all-substrings/
+ */
+public class Problem1781 {
+    public int beautySum(String s) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1781 prob = new Problem1781();
+        System.out.println(prob.beautySum("aabcb")); // 5
+        System.out.println(prob.beautySum("aabcbaa")); // 17
+    }
+}
