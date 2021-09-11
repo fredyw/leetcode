a595482d47bbe1aad27287b962596f23a6724f1e
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-09-11 13:17:42 -0500

    Skeleton for problem 1781

---
+++ src/main/java/leetcode/Problem1690.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/stone-game-vii/
+ */
+public class Problem1690 {
+    public int stoneGameVII(int[] stones) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1690 prob = new Problem1690();
+        System.out.println(prob.stoneGameVII(new int[]{5,3,1,4,2})); // 6
+        System.out.println(prob.stoneGameVII(new int[]{7,90,5,1,100,10,10,2})); // 122
+    }
+}
