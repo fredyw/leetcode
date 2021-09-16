edd64f4fcae7aaa9265e8edc91a5a301f0237363
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-09-15 21:40:55 -0500

    Skeleton for problem 1734

---
+++ src/main/java/leetcode/Problem1734.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/decode-xored-permutation/
+ */
+public class Problem1734 {
+    public int[] decode(int[] encoded) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Problem1734 prob = new Problem1734();
+        System.out.println(prob.decode(new int[]{3,1})); // [1,2,3]
+        System.out.println(prob.decode(new int[]{6,5,4,6})); // [2,4,1,5,3]
+    }
+}
