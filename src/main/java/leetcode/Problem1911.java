28f508336e653c0acea99e5866d240c7cb1204e0
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-09-18 22:11:10 -0500

    Skeleton for problem 1911

---
+++ src/main/java/leetcode/Problem1911.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/maximum-alternating-subsequence-sum/
+ */
+public class Problem1911 {
+    public long maxAlternatingSum(int[] nums) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1911 prob = new Problem1911();
+        System.out.println(prob.maxAlternatingSum(new int[]{4,2,5,3})); // 7
+        System.out.println(prob.maxAlternatingSum(new int[]{5,6,7,8})); // 8
+        System.out.println(prob.maxAlternatingSum(new int[]{6,2,1,2,4,5})); // 10
+    }
+}
