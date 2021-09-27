8adf5e071b10da198a10b6f1fa90f9738db8d9f8
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-09-27 18:46:42 -0500

    Update problem 1947

--- src/main/java/leetcode/Problem1947.java
+++ src/main/java/leetcode/Problem1947.java
@@ -5,8 +5,21 @@ package leetcode;
  */
 public class Problem1947 {
     public int maxCompatibilitySum(int[][] students, int[][] mentors) {
-        // TODO
+        int[] studentArray = toArray(students);
+        int[] mentorArray = toArray(mentors);
         return 0;
+    }
+
+    private static int[] toArray(int[][] twoDArray) {
+        int[] array = new int[twoDArray.length];
+        for (int i = 0; i < twoDArray.length; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int j : array) {
+                sb.append(j);
+            }
+            array[i] = Integer.parseInt(sb.toString(), 2);
+        }
+        return array;
     }
 
     public static void main(String[] args) {
@@ -16,10 +29,10 @@ public class Problem1947 {
         }, new int[][]{
             {1,0,0},{0,0,1},{1,1,0}
         })); // 8
-        System.out.println(prob.maxCompatibilitySum(new int[][]{
-            {0,0},{0,0},{0,0}
-        }, new int[][]{
-            {1,1},{1,1},{1,1}
-        })); // 0
+//        System.out.println(prob.maxCompatibilitySum(new int[][]{
+//            {0,0},{0,0},{0,0}
+//        }, new int[][]{
+//            {1,1},{1,1},{1,1}
+//        })); // 0
     }
 }
