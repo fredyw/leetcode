4b87b536d35d254e9c51c12a2880f88ec36bf4e8
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-09-27 21:27:26 -0500

    Solve problem 1947

--- src/main/java/leetcode/Problem1947.java
+++ src/main/java/leetcode/Problem1947.java
@@ -7,14 +7,15 @@ public class Problem1947 {
     public int maxCompatibilitySum(int[][] students, int[][] mentors) {
         int[] studentArray = toArray(students);
         int[] mentorArray = toArray(mentors);
-        return 0;
+        return maxCompatibilitySum(studentArray, mentorArray, students[0].length, 0,
+            new boolean[studentArray.length]);
     }
 
     private static int[] toArray(int[][] twoDArray) {
         int[] array = new int[twoDArray.length];
         for (int i = 0; i < twoDArray.length; i++) {
             StringBuilder sb = new StringBuilder();
-            for (int j : array) {
+            for (int j : twoDArray[i]) {
                 sb.append(j);
             }
             array[i] = Integer.parseInt(sb.toString(), 2);
@@ -22,17 +23,23 @@ public class Problem1947 {
         return array;
     }
 
-    public static void main(String[] args) {
-        Problem1947 prob = new Problem1947();
-        System.out.println(prob.maxCompatibilitySum(new int[][]{
-            {1,1,0},{1,0,1},{0,0,1}
-        }, new int[][]{
-            {1,0,0},{0,0,1},{1,1,0}
-        })); // 8
-//        System.out.println(prob.maxCompatibilitySum(new int[][]{
-//            {0,0},{0,0},{0,0}
-//        }, new int[][]{
-//            {1,1},{1,1},{1,1}
-//        })); // 0
+    private static int maxCompatibilitySum(int[] students, int[] mentors, int bitLength,
+                                           int index, boolean[] visited) {
+        if (index == students.length) {
+            return 0;
+        }
+        int max = 0;
+        for (int i = 0; i < mentors.length; i++) {
+            if (visited[i]) {
+                continue;
+            }
+            visited[i] = true;
+            int xor = students[index] ^ mentors[i];
+            int sum = maxCompatibilitySum(students, mentors, bitLength, index + 1, visited)
+                + bitLength - Integer.bitCount(xor);
+            max = Math.max(max, sum);
+            visited[i] = false;
+        }
+        return max;
     }
 }
