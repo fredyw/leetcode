5aef30be3906e33ce278de001facf3c0d483bb70
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-09-28 14:41:12 -0500

    Skeleton for problem 1846

---
+++ src/main/java/leetcode/Problem1846.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/maximum-element-after-decreasing-and-rearranging/
+ */
+public class Problem1846 {
+    public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Problem1846 prob = new Problem1846();
+        System.out.println(prob.maximumElementAfterDecrementingAndRearranging(new int[]{2,2,1,2,1})); // 2
+        System.out.println(prob.maximumElementAfterDecrementingAndRearranging(new int[]{100,1,1000})); // 3
+        System.out.println(prob.maximumElementAfterDecrementingAndRearranging(new int[]{1,2,3,4,5})); // 5
+    }
+}
