85962b43e7391a50aca15f5e829acf808e8fc104
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-09-29 18:16:43 -0500

    Update problem 1846

--- src/main/java/leetcode/Problem1846.java
+++ src/main/java/leetcode/Problem1846.java
@@ -13,6 +13,9 @@ public class Problem1846 {
         Problem1846 prob = new Problem1846();
         System.out.println(prob.maximumElementAfterDecrementingAndRearranging(new int[]{2,2,1,2,1})); // 2
         System.out.println(prob.maximumElementAfterDecrementingAndRearranging(new int[]{100,1,1000})); // 3
+        System.out.println(prob.maximumElementAfterDecrementingAndRearranging(new int[]{100,100,100,1000})); // 4
+        System.out.println(prob.maximumElementAfterDecrementingAndRearranging(new int[]{1,1,1,1})); // 1
         System.out.println(prob.maximumElementAfterDecrementingAndRearranging(new int[]{1,2,3,4,5})); // 5
+        System.out.println(prob.maximumElementAfterDecrementingAndRearranging(new int[]{4,3,2,1})); // 4
     }
 }
