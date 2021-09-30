dbaab8217bd5958d876361dfa2985411a891693b
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-09-30 00:00:29 -0500

    Solve problem 1846

--- src/main/java/leetcode/Problem1846.java
+++ src/main/java/leetcode/Problem1846.java
@@ -1,21 +1,24 @@
 package leetcode;
 
+import java.util.Arrays;
+
 /**
  * https://leetcode.com/problems/maximum-element-after-decreasing-and-rearranging/
  */
 public class Problem1846 {
     public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
-        // TODO
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        Problem1846 prob = new Problem1846();
-        System.out.println(prob.maximumElementAfterDecrementingAndRearranging(new int[]{2,2,1,2,1})); // 2
-        System.out.println(prob.maximumElementAfterDecrementingAndRearranging(new int[]{100,1,1000})); // 3
-        System.out.println(prob.maximumElementAfterDecrementingAndRearranging(new int[]{100,100,100,1000})); // 4
-        System.out.println(prob.maximumElementAfterDecrementingAndRearranging(new int[]{1,1,1,1})); // 1
-        System.out.println(prob.maximumElementAfterDecrementingAndRearranging(new int[]{1,2,3,4,5})); // 5
-        System.out.println(prob.maximumElementAfterDecrementingAndRearranging(new int[]{4,3,2,1})); // 4
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            if (i == 0) {
+                if (arr[i] != 1) {
+                    arr[i] = 1;
+                }
+            } else {
+                if (arr[i - 1] != arr[i] && arr[i - 1] != arr[i] + 1) {
+                    arr[i] = arr[i - 1] + 1;
+                }
+            }
+        }
+        return arr[arr.length - 1];
     }
 }
