5bee28006bbc3da8baf3d9d55d4953fdee70f0bf
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-10-02 20:15:09 -0500

    Update problem 1503

--- src/main/kotlin/leetcode/Problem1503.kt
+++ src/main/kotlin/leetcode/Problem1503.kt
@@ -13,6 +13,7 @@ class Problem1503 {
 fun main() {
     val prob = Problem1503()
     println(prob.getLastMoment(4, intArrayOf(4, 3), intArrayOf(0, 1))) // 4
+    println(prob.getLastMoment(5, intArrayOf(4, 3), intArrayOf(0, 1))) // 5
     println(prob.getLastMoment(7, intArrayOf(), intArrayOf(0, 1, 2, 3, 4, 5, 6, 7))) // 7
     println(prob.getLastMoment(7, intArrayOf(0, 1, 2, 3, 4, 5, 6, 7), intArrayOf())) // 7
     println(prob.getLastMoment(9, intArrayOf(5), intArrayOf(4))) // 5
