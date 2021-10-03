a367c403170032cd8ac6acae64460ced15b09cba
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-10-02 20:14:13 -0500

    Convert problem 1503 to Kotlin

---
+++ src/main/kotlin/leetcode/Problem1503.kt
@@ -0,0 +1,20 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/last-moment-before-all-ants-fall-out-of-a-plank/
+ */
+class Problem1503 {
+    fun getLastMoment(n: Int, left: IntArray?, right: IntArray?): Int {
+        // TODO
+        return 0
+    }
+}
+
+fun main() {
+    val prob = Problem1503()
+    println(prob.getLastMoment(4, intArrayOf(4, 3), intArrayOf(0, 1))) // 4
+    println(prob.getLastMoment(7, intArrayOf(), intArrayOf(0, 1, 2, 3, 4, 5, 6, 7))) // 7
+    println(prob.getLastMoment(7, intArrayOf(0, 1, 2, 3, 4, 5, 6, 7), intArrayOf())) // 7
+    println(prob.getLastMoment(9, intArrayOf(5), intArrayOf(4))) // 5
+    println(prob.getLastMoment(6, intArrayOf(6), intArrayOf(0))) // 6
+}
