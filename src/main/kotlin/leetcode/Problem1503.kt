667ec0d819d5a5d9eeec68e6d6fd2a82aff204fe
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-10-03 01:37:31 -0500

    Solve problem 1503

--- src/main/kotlin/leetcode/Problem1503.kt
+++ src/main/kotlin/leetcode/Problem1503.kt
@@ -1,25 +1,19 @@
 package leetcode
 
+import kotlin.math.max
+
 /**
  * https://leetcode.com/problems/last-moment-before-all-ants-fall-out-of-a-plank/
  */
 class Problem1503 {
     fun getLastMoment(n: Int, left: IntArray, right: IntArray): Int {
-        // TODO
-        return 0
+        var answer = 0
+        for (i in left) {
+           answer = max(answer, i)
+        }
+        for (i in right) {
+            answer = max(answer, n - i)
+        }
+        return answer
     }
-}
-
-fun main() {
-    val prob = Problem1503()
-    println(prob.getLastMoment(4, intArrayOf(4, 3), intArrayOf(0, 1))) // 4
-    println(prob.getLastMoment(5, intArrayOf(4, 3), intArrayOf(0, 1))) // 5
-    println(prob.getLastMoment(5, intArrayOf(9), intArrayOf(2, 7))) // 5
-    println(prob.getLastMoment(9, intArrayOf(9), intArrayOf(2, 7))) // 9
-    println(prob.getLastMoment(9, intArrayOf(4, 9), intArrayOf(2, 7))) // 9
-    println(prob.getLastMoment(7, intArrayOf(), intArrayOf(0, 1, 2, 3, 4, 5, 6, 7))) // 7
-    println(prob.getLastMoment(7, intArrayOf(0, 1, 2, 3, 4, 5, 6, 7), intArrayOf())) // 7
-    println(prob.getLastMoment(9, intArrayOf(5), intArrayOf(4))) // 5
-    println(prob.getLastMoment(6, intArrayOf(6), intArrayOf(0))) // 6
-    println(prob.getLastMoment(9, intArrayOf(1, 3), intArrayOf(5, 9))) // 4
 }
