420c1e01fe23e4c8cb04d2c473ab7c7c3823a4cc
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-10-03 10:43:28 -0500

    Skeleton for problem 1962

---
+++ src/main/kotlin/leetcode/Problem1962.kt
@@ -0,0 +1,16 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/remove-stones-to-minimize-the-total/
+ */
+class Problem1962 {
+    fun minStoneSum(piles: IntArray, k: Int): Int {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem1962()
+    println(prob.minStoneSum(intArrayOf(5,4,9), 2)) // 12
+    println(prob.minStoneSum(intArrayOf(4,3,6,7), 12)) // 12
+}
