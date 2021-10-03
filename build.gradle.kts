a000b310d410bb18295727fac30766c7560c434e
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-10-02 19:38:23 -0500

    Add Kotlin support

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,16 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     java
+    kotlin("jvm") version "1.5.31"
 }
 
 java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(11))
     }
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "11"
 }
