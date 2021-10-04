e6a15e0fef5079a0d6052dc6f7f8e976b49ec524
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-10-03 19:25:15 -0500

    Solve problem 1962

--- src/main/kotlin/leetcode/Problem1962.kt
+++ src/main/kotlin/leetcode/Problem1962.kt
@@ -1,16 +1,22 @@
 package leetcode
 
+import java.util.PriorityQueue
+import java.util.Comparator
+import kotlin.math.ceil
+
 /**
  * https://leetcode.com/problems/remove-stones-to-minimize-the-total/
  */
 class Problem1962 {
     fun minStoneSum(piles: IntArray, k: Int): Int {
-        TODO()
+        val queue = PriorityQueue(Comparator<Int> { a, b -> b.compareTo(a) })
+        for (pile in piles) {
+            queue.add(pile)
+        }
+        for (i in 1..k) {
+            val max = ceil(queue.poll() / 2.0).toInt()
+            queue.add(max)
+        }
+        return queue.sum()
     }
-}
-
-fun main() {
-    val prob = Problem1962()
-    println(prob.minStoneSum(intArrayOf(5,4,9), 2)) // 12
-    println(prob.minStoneSum(intArrayOf(4,3,6,7), 12)) // 12
 }
