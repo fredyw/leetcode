46b427040867c973e9c2e747d770bf35ab7add0b
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-10-06 17:08:44 -0500

    Solve problem 1482

--- src/main/kotlin/leetcode/Problem1482.kt
+++ src/main/kotlin/leetcode/Problem1482.kt
@@ -1,5 +1,6 @@
 package leetcode
 
+import kotlin.math.max
 import kotlin.math.min
 
 /**
@@ -10,37 +11,40 @@ class Problem1482 {
         if (bloomDay.size < m * k) {
             return -1
         }
-        var answer = Int.MAX_VALUE
+        var min = Int.MAX_VALUE
+        var max = Int.MIN_VALUE
         for (day in bloomDay) {
-            var bouquets = m
-            var flowers = k
-            var i = 0
-            while (i < bloomDay.size) {
-                while (i < bloomDay.size && bloomDay[i] <= day) {
-                    if (flowers - 1 == 0) {
-                        bouquets--
-                        flowers = k
-                    } else {
-                        flowers--
-                    }
-                    if (bouquets == 0) {
-                        answer = min(answer, day)
-                    }
-                    i++
-                }
-                flowers = k
-                i++
+            min = min(min, day)
+            max = max(max, day)
+        }
+        while (min <= max) {
+            val mid = min + ((max - min) / 2)
+            if (isValid(bloomDay, m, k, mid)) {
+                max--
+            } else {
+                min++
             }
         }
-        return if (answer == Int.MAX_VALUE) -1 else answer
+        return min
     }
-}
 
-fun main() {
-    val prob = Problem1482()
-    println(prob.minDays(intArrayOf(1,10,3,10,2), 3, 1)) // 3
-    println(prob.minDays(intArrayOf(1,10,3,10,2), 3, 2)) // -1
-    println(prob.minDays(intArrayOf(7,7,7,7,12,7,7), 2, 3)) // 12
-    println(prob.minDays(intArrayOf(1000000000,1000000000), 1, 1)) // 1000000000
-    println(prob.minDays(intArrayOf(1,10,2,9,3,8,4,7,5,6), 4, 2)) // 9
+    private fun isValid(bloomDay: IntArray, m: Int, k: Int, day: Int): Boolean {
+        var bouquets = 0
+        var flowers = 0
+        for (d in bloomDay) {
+            if (d > day) {
+                flowers = 0
+            } else {
+                flowers++
+            }
+            if (flowers == k) {
+                flowers = 0
+                bouquets++
+            }
+            if (bouquets == m) {
+                return true
+            }
+        }
+        return false
+    }
 }
