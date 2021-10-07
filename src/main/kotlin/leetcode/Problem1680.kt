16b39e5674574e9409b5a9301f0071dd17117167
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-10-07 17:09:42 -0500

    Skeleton for problem 1680

---
+++ src/main/kotlin/leetcode/Problem1680.kt
@@ -0,0 +1,17 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/concatenation-of-consecutive-binary-numbers/
+ */
+class Problem1680 {
+    fun concatenatedBinary(n: Int): Int {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem1680()
+    println(prob.concatenatedBinary(1)) // 1
+    println(prob.concatenatedBinary(3)) // 27
+    println(prob.concatenatedBinary(12)) // 505379714
+}
