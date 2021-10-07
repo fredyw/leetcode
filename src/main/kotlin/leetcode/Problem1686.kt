ff3f92ea90ccc343283038b08341150949e5a8b3
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-10-06 23:50:21 -0500

    Add skeleton for problem 1686

---
+++ src/main/kotlin/leetcode/Problem1686.kt
@@ -0,0 +1,17 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/stone-game-vi/
+ */
+class Problem1686 {
+    fun stoneGameVI(aliceValues: IntArray, bobValues: IntArray): Int {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem1686()
+    println(prob.stoneGameVI(intArrayOf(1,3), intArrayOf(2,1))) // 1
+    println(prob.stoneGameVI(intArrayOf(1,2), intArrayOf(3,1))) // 0
+    println(prob.stoneGameVI(intArrayOf(2,4,3), intArrayOf(1,6,7))) // -1
+}
