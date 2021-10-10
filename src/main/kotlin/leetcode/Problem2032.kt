7ab63d0855ed3308aa6f2fa4aec75c0cf9880e40
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-10-10 15:03:50 -0500

    Solve problem 2032

--- src/main/kotlin/leetcode/Problem2032.kt
+++ src/main/kotlin/leetcode/Problem2032.kt
@@ -5,13 +5,40 @@ package leetcode
  */
 class Problem2032 {
     fun twoOutOfThree(nums1: IntArray, nums2: IntArray, nums3: IntArray): List<Int> {
-        TODO()
+        val set1 = mutableSetOf<Int>()
+        nums1.forEach {
+            if (it !in set1) {
+                set1 += it
+            }
+        }
+        val set2 = mutableSetOf<Int>()
+        nums2.forEach {
+            if (it !in set2) {
+                set2 += it
+            }
+        }
+        val set3 = mutableSetOf<Int>()
+        nums3.forEach {
+            if (it !in set3) {
+                set3 += it
+            }
+        }
+        val map = mutableMapOf<Int, Int>()
+        set1.forEach {
+            map[it] = (map[it] ?: 0) + 1
+        }
+        set2.forEach {
+            map[it] = (map[it] ?: 0) + 1
+        }
+        set3.forEach {
+            map[it] = (map[it] ?: 0) + 1
+        }
+        val answer = mutableListOf<Int>()
+        map.forEach {
+            if (it.value >= 2) {
+                answer += it.key
+            }
+        }
+        return answer
     }
-}
-
-fun main() {
-    val prob = Problem2032()
-    println(prob.twoOutOfThree(intArrayOf(1,1,3,2), intArrayOf(2,3), intArrayOf(3))) // [3,2]
-    println(prob.twoOutOfThree(intArrayOf(3,1), intArrayOf(2,3), intArrayOf(1,2))) // [2,3,1]
-    println(prob.twoOutOfThree(intArrayOf(1,2,2), intArrayOf(4,3,3), intArrayOf(5))) // []
 }
