980654787a0b46eb3408526f873322adf3532835
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-10-10 20:47:51 -0500

    Skeleton for problem 1695

---
+++ src/main/kotlin/leetcode/Problem1695.kt
@@ -0,0 +1,16 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/maximum-erasure-value/
+ */
+class Problem1695 {
+    fun maximumUniqueSubarray(nums: IntArray): Int {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem1695()
+    println(prob.maximumUniqueSubarray(intArrayOf(4,2,4,5,6))) // 17
+    println(prob.maximumUniqueSubarray(intArrayOf(5,2,1,2,5,2,1,2,5))) // 8
+}
