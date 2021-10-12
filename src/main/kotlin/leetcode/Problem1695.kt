7c614cf5dec2a68c0a67419978e8acb798753989
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-10-11 20:09:22 -0500

    Update problem 1695

--- src/main/kotlin/leetcode/Problem1695.kt
+++ src/main/kotlin/leetcode/Problem1695.kt
@@ -13,4 +13,5 @@ fun main() {
     val prob = Problem1695()
     println(prob.maximumUniqueSubarray(intArrayOf(4,2,4,5,6))) // 17
     println(prob.maximumUniqueSubarray(intArrayOf(5,2,1,2,5,2,1,2,5))) // 8
+    println(prob.maximumUniqueSubarray(intArrayOf(2,4,4,4,5,6,3,4,3))) // 18
 }
