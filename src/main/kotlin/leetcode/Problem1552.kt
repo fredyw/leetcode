6d330a1b0445d95f9c9b3e6d681fa292ee3b7f49
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-10-12 17:28:45 -0500

    Skeleton for problem 1552

---
+++ src/main/kotlin/leetcode/Problem1552.kt
@@ -0,0 +1,16 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/magnetic-force-between-two-balls/
+ */
+class Problem1552 {
+    fun maxDistance(position: IntArray, m: Int): Int {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem1552()
+    println(prob.maxDistance(intArrayOf(1,2,3,4,7), 3)) // 3
+    println(prob.maxDistance(intArrayOf(5,4,3,2,1,1000000000), 2)) // 999999999
+}
