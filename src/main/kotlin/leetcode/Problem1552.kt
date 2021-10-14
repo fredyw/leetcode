72b92218a2226f361eb735c643639ecbfbe3fbd6
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-10-14 00:20:18 -0500

    Update problem 1552

--- src/main/kotlin/leetcode/Problem1552.kt
+++ src/main/kotlin/leetcode/Problem1552.kt
@@ -13,4 +13,6 @@ fun main() {
     val prob = Problem1552()
     println(prob.maxDistance(intArrayOf(1,2,3,4,7), 3)) // 3
     println(prob.maxDistance(intArrayOf(5,4,3,2,1,1000000000), 2)) // 999999999
+    println(prob.maxDistance(intArrayOf(5,4,3,2,1,1000000000), 3)) // 4
+    println(prob.maxDistance(intArrayOf(5,4,3,2,1,1000000000), 4)) // 2
 }
