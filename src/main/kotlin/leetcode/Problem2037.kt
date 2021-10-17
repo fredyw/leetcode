c76936b1bbfe9a6047bc60d7558ccb150a4140b1
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-10-17 09:22:48 -0500

    Solve problem 2037

--- src/main/kotlin/leetcode/Problem2037.kt
+++ src/main/kotlin/leetcode/Problem2037.kt
@@ -1,17 +1,18 @@
 package leetcode
 
+import kotlin.math.abs
+
 /**
  * https://leetcode.com/problems/minimum-number-of-moves-to-seat-everyone/
  */
 class Problem2037 {
     fun minMovesToSeat(seats: IntArray, students: IntArray): Int {
-        TODO()
+        var answer = 0
+        seats.sort()
+        students.sort()
+        for (i in seats.indices) {
+            answer += abs(seats[i] - students[i])
+        }
+        return answer
     }
-}
-
-fun main() {
-    val prob = Problem2037()
-    println(prob.minMovesToSeat(intArrayOf(3,1,5), intArrayOf(2,7,4))) // 4
-    println(prob.minMovesToSeat(intArrayOf(4,1,5,9), intArrayOf(1,3,2,6))) // 7
-    println(prob.minMovesToSeat(intArrayOf(2,2,6,6), intArrayOf(1,3,2,6))) // 4
 }
