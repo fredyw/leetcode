9e33534efedc5126c19f30e54bb5686cbd72f604
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-10-19 13:21:44 -0500

    Solve problem 2024

--- src/main/kotlin/leetcode/Problem2024.kt
+++ src/main/kotlin/leetcode/Problem2024.kt
@@ -1,17 +1,37 @@
 package leetcode
 
+import java.util.LinkedList
+import kotlin.math.max
+
 /**
  * https://leetcode.com/problems/maximize-the-confusion-of-an-exam/
  */
 class Problem2024 {
     fun maxConsecutiveAnswers(answerKey: String, k: Int): Int {
-        TODO()
+        return max(consecutiveAnswers(answerKey, k, 'T'), consecutiveAnswers(answerKey, k, 'F'))
     }
-}
 
-fun main() {
-    val prob = Problem2024()
-    println(prob.maxConsecutiveAnswers("TTFF", 2)) // 4
-    println(prob.maxConsecutiveAnswers("TFFT", 1)) // 3
-    println(prob.maxConsecutiveAnswers("TTFTTFTT", 1)) // 5
+    private fun consecutiveAnswers(answerKey: String, k: Int, c: Char): Int {
+        var max = 0
+        var indexes = LinkedList<Int>()
+        var count = k
+        var length = 0
+        for (i in answerKey.indices) {
+            if (answerKey[i] == c) {
+                if (count - 1 < 0) {
+                    count++
+                    length = if (indexes.isEmpty()) {
+                        0
+                    } else {
+                        i - indexes.pollFirst() - 1
+                    }
+                }
+                count--
+                indexes.add(i)
+            }
+            length++
+            max = max(max, length)
+        }
+        return max
+    }
 }
