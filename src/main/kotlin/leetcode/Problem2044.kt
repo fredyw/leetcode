f9a464ff9f0040c030babc554803e4ff963002e9
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-10-19 13:28:12 -0500

    Skeleton for problem 2044

---
+++ src/main/kotlin/leetcode/Problem2044.kt
@@ -0,0 +1,17 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/count-number-of-maximum-bitwise-or-subsets/
+ */
+class Problem2044 {
+    fun countMaxOrSubsets(nums: IntArray): Int {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2044()
+    println(prob.countMaxOrSubsets(intArrayOf(3,1))) // 2
+    println(prob.countMaxOrSubsets(intArrayOf(2,2,2))) // 7
+    println(prob.countMaxOrSubsets(intArrayOf(3,2,1,5))) // 6
+}
