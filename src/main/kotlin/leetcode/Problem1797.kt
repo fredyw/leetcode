3ae6794488d77265546fa161388e58776656f182
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-10-21 11:58:02 -0500

    Update problem 1797

--- src/main/kotlin/leetcode/Problem1797.kt
+++ src/main/kotlin/leetcode/Problem1797.kt
@@ -21,11 +21,11 @@ class Problem1797 {
 
 fun main() {
     val authenticationManager = Problem1797.AuthenticationManager(5)
-    println(authenticationManager.renew("aaa", 1)) // No token exists with tokenId "aaa" at time 1, so nothing happens.
+    authenticationManager.renew("aaa", 1) // No token exists with tokenId "aaa" at time 1, so nothing happens.
     authenticationManager.generate("aaa", 2) // Generates a new token with tokenId "aaa" at time 2.
     println(authenticationManager.countUnexpiredTokens(6)) // The token with tokenId "aaa" is the only unexpired one at time 6, so return 1.
     authenticationManager.generate("bbb", 7) // Generates a new token with tokenId "bbb" at time 7.
-    println(authenticationManager.renew("aaa", 8)) // The token with tokenId "aaa" expired at time 7, and 8 >= 7, so at time 8 the renew request is ignored, and nothing happens.
-    println(authenticationManager.renew("bbb", 10)) // The token with tokenId "bbb" is unexpired at time 10, so the renew request is fulfilled and now the token will expire at time 15.
+    authenticationManager.renew("aaa", 8) // The token with tokenId "aaa" expired at time 7, and 8 >= 7, so at time 8 the renew request is ignored, and nothing happens.
+    authenticationManager.renew("bbb", 10) // The token with tokenId "bbb" is unexpired at time 10, so the renew request is fulfilled and now the token will expire at time 15.
     println(authenticationManager.countUnexpiredTokens(15)) // The token with tokenId "bbb" expires at time 15, and the token with tokenId "aaa" expired at time 7, so currently no token is unexpired, so return 0.
 }
