2963586d6004d960b344d22b04d73b7966f7c8c3
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-10-21 11:49:36 -0500

    Solve problem 2038

--- src/main/kotlin/leetcode/Problem2038.kt
+++ src/main/kotlin/leetcode/Problem2038.kt
@@ -5,13 +5,42 @@ package leetcode
  */
 class Problem2038 {
     fun winnerOfGame(colors: String): Boolean {
-        TODO()
+        var alice = true
+        var aliceIndex = 0
+        var bobIndex = 0
+        var answer = false
+        while (true) {
+            if (alice) {
+                val result = find(colors, 'A', aliceIndex)
+                if (!result.found) {
+                    answer = false // Bob wins
+                    break
+                }
+                aliceIndex = result.nextIndex
+            } else {
+                val result = find(colors, 'B', bobIndex)
+                if (!result.found) {
+                    answer = true // Alice wins
+                    break
+                }
+                bobIndex = result.nextIndex
+            }
+            alice = !alice
+        }
+        return answer
     }
-}
 
-fun main() {
-    val prob = Problem2038()
-    println(prob.winnerOfGame("AAABABB")) // true
-    println(prob.winnerOfGame("AA")) // false
-    println(prob.winnerOfGame("ABBBBBBBAAA")) // false
+    private data class Result(val nextIndex: Int, val found: Boolean)
+
+    private fun find(colors: String, color: Char, index: Int): Result {
+        var i = if (index == 0) 1 else index
+        while (i + 1 < colors.length) {
+            if (colors[i - 1] == color && colors[i] == color && colors[i + 1] == color) {
+                return Result(i + 1, true)
+            } else {
+                i++
+            }
+        }
+        return Result(-1, false)
+    }
 }
