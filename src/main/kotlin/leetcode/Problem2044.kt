96c1d7559e31e90deadaf05039418f70bb768d95
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-10-20 19:04:40 -0500

    Solve problem 2044

--- src/main/kotlin/leetcode/Problem2044.kt
+++ src/main/kotlin/leetcode/Problem2044.kt
@@ -1,17 +1,30 @@
 package leetcode
 
+import kotlin.math.max
+
 /**
  * https://leetcode.com/problems/count-number-of-maximum-bitwise-or-subsets/
  */
 class Problem2044 {
     fun countMaxOrSubsets(nums: IntArray): Int {
-        TODO()
+        var max = 0
+        for (num in nums) {
+            max = max or num
+        }
+        var answer = 0
+        for (i in nums.indices) {
+            answer += countMaxOrSubsets(nums, max, i + 1, false, nums[i])
+        }
+        return answer
     }
-}
 
-fun main() {
-    val prob = Problem2044()
-    println(prob.countMaxOrSubsets(intArrayOf(3,1))) // 2
-    println(prob.countMaxOrSubsets(intArrayOf(2,2,2))) // 7
-    println(prob.countMaxOrSubsets(intArrayOf(3,2,1,5))) // 6
+    private fun countMaxOrSubsets(nums: IntArray, max: Int, index: Int, skipped: Boolean, accu: Int): Int {
+        if (index == nums.size) {
+            return if (!skipped && accu == max) 1 else 0
+        }
+        val count = if (!skipped && accu == max) 1 else 0
+        val a = countMaxOrSubsets(nums, max, index + 1, false, accu or nums[index])
+        val b = countMaxOrSubsets(nums, max, index + 1, true, accu)
+        return a + b + count
+    }
 }
