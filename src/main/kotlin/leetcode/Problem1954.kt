beac85a9d4cf5c58eddacdfd6833f2bdfbf6079d
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-10-23 00:49:14 -0500

    Skeleton for problem 1954

---
+++ src/main/kotlin/leetcode/Problem1954.kt
@@ -0,0 +1,17 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/minimum-garden-perimeter-to-collect-enough-apples/
+ */
+class Problem1954 {
+    fun minimumPerimeter(neededApples: Long): Long {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem1954()
+    println(prob.minimumPerimeter(1)) // 8
+    println(prob.minimumPerimeter(13)) // 16
+    println(prob.minimumPerimeter(1000000000)) // 5040
+}
