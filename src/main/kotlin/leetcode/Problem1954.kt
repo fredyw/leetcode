978455bd5e17529f8bf019c0ec92ffbf3e09df61
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-10-23 15:58:02 -0500

    Update problem 1954

--- src/main/kotlin/leetcode/Problem1954.kt
+++ src/main/kotlin/leetcode/Problem1954.kt
@@ -7,6 +7,28 @@ class Problem1954 {
     fun minimumPerimeter(neededApples: Long): Long {
         TODO()
     }
+
+    private fun numApples(n: Long): Long {
+        var totalApples = 0L
+        for (i in 1..n) {
+            // *-----*
+            // |     |
+            // *     *
+            // |     |
+            // *-----*
+            var apples = i * 4L + ((i + i) * 4L)
+            for (j in 1 until i) {
+                // -**-**-
+                // *     *
+                // |     |
+                // *     *
+                // -**-**-
+                apples += (i + j) * 8L
+            }
+            totalApples += apples
+        }
+        return totalApples
+    }
 }
 
 fun main() {
