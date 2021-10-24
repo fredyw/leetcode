5a72d2965dc7a34edab02fbf9699d92b1bbdacf7
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-10-23 20:28:24 -0500

    Skeleton for problem 2002

---
+++ src/main/kotlin/leetcode/Problem2002.kt
@@ -0,0 +1,17 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/maximum-product-of-the-length-of-two-palindromic-subsequences/
+ */
+class Problem2002 {
+    fun maxProduct(s: String): Int {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2002()
+    println(prob.maxProduct("leetcodecom")) // 9
+    println(prob.maxProduct("bb")) // 1
+    println(prob.maxProduct("accbcaxxcxx")) // 25
+}
