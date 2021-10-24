df2828540c87b9a8376201b892125c9c273fb6f4
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-10-24 14:13:03 -0500

    Solve problem 2047

--- src/main/kotlin/leetcode/Problem2047.kt
+++ src/main/kotlin/leetcode/Problem2047.kt
@@ -5,14 +5,14 @@ package leetcode
  */
 class Problem2047 {
     fun countValidWords(sentence: String): Int {
-        TODO()
+        var answer = 0
+        val words = sentence.split("""\s+""".toRegex())
+        val regex = """(?:[a-z]+(?:-[a-z]+|[a-z]+)?)?[!.,]?""".toRegex()
+        for (word in words) {
+            if (word.trim().isNotEmpty() && regex.matches(word)) {
+                answer++
+            }
+        }
+        return answer
     }
-}
-
-fun main() {
-    val prob = Problem2047()
-    println(prob.countValidWords("cat and  dog")) // 3
-    println(prob.countValidWords("!this  1-s b8d!")) // 0
-    println(prob.countValidWords("alice and  bob are playing stone-game10")) // 5
-    println(prob.countValidWords("he bought 2 pencils, 3 erasers, and 1  pencil-sharpener.")) // 6
 }
