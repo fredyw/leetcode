1e6b4bd9696855637b2270177175b24b4b6c18a7
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-10-27 11:38:34 -0500

    Update problem 2002

--- src/main/kotlin/leetcode/Problem2002.kt
+++ src/main/kotlin/leetcode/Problem2002.kt
@@ -1,11 +1,38 @@
 package leetcode
 
+import kotlin.math.max
+
 /**
  * https://leetcode.com/problems/maximum-product-of-the-length-of-two-palindromic-subsequences/
  */
 class Problem2002 {
     fun maxProduct(s: String): Int {
-        TODO()
+        return longestPalindrome(s,
+            i = 0,
+            j = s.length - 1,
+            memo = Array(s.length) { IntArray(s.length) { -1 } })
+    }
+
+    private fun longestPalindrome(s: String, i: Int, j: Int, memo: Array<IntArray>): Int {
+        if (i > j) {
+            return 0
+        }
+        if (i == j) {
+            return 1
+        }
+        if (memo[i][j] != -1) {
+            return memo[i][j]
+        }
+        val max = if (s[i] == s[j]) {
+            2 + longestPalindrome(s, i + 1, j - 1, memo)
+        } else {
+            max(
+                longestPalindrome(s, i + 1, j, memo),
+                longestPalindrome(s, i, j - 1, memo)
+            )
+        }
+        memo[i][j] = max
+        return max
     }
 }
 
@@ -14,4 +41,6 @@ fun main() {
     println(prob.maxProduct("leetcodecom")) // 9
     println(prob.maxProduct("bb")) // 1
     println(prob.maxProduct("accbcaxxcxx")) // 25
+    println(prob.maxProduct("abcba")) // 6
+    println(prob.maxProduct("aaa")) // 2
 }
