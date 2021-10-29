e667098f4455a32dcb192ed6adcaec68aebce55f
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-10-29 00:47:16 -0500

    Update problem 1895

--- src/main/kotlin/leetcode/Problem1895.kt
+++ src/main/kotlin/leetcode/Problem1895.kt
@@ -86,16 +86,13 @@ class Problem1895 {
         var answer = 1
         for (row in 0 until maxRow) {
             for (col in 0 until maxCol) {
-                var r = row
-                var c = col
                 var k = 1
-                while (r < maxRow && c < maxCol) {
-                    if (isMagicSquare(rowSums, colSums, leftDiagonalSums, rightDiagonalSums, r, c, k)) {
+                while (row + k <= maxRow && col + k <= maxCol) {
+                    if (isMagicSquare(rowSums, colSums, leftDiagonalSums, rightDiagonalSums,
+                            row, col, k)) {
                         answer = max(answer, k)
                     }
                     k++
-                    r++
-                    c++
                 }
             }
         }
@@ -107,7 +104,36 @@ class Problem1895 {
                               leftDiagonalSums: Array<IntArray>,
                               rightDiagonalSums: Array<IntArray>,
                               row: Int, col: Int, size: Int): Boolean {
-        TODO()
+        val maxRow = rowSums.size
+        val maxCol = if (maxRow > 0) rowSums[0].size else 0
+        var sum = 0
+        for (r in row until row + size) {
+            val rowSum = rowSums[r][col + size - 1] - (if (col - 1 >= 0) rowSums[r][col - 1] else 0)
+            if (sum == 0) {
+                sum = rowSum
+            } else {
+                if (sum != rowSum) {
+                    return false
+                }
+            }
+        }
+        for (c in col until col + size) {
+            val colSum = colSums[row + size - 1][c] - (if (row - 1 >= 0) colSums[row - 1][c] else 0)
+            if (sum != colSum) {
+                return false
+            }
+        }
+        val rightDiagonalSum = rightDiagonalSums[row + size - 1][col + size - 1] -
+                (if (row - 1 >= 0 && col - 1 >= 0) rightDiagonalSums[row - 1][col - 1] else 0)
+        if (sum != rightDiagonalSum) {
+            return false
+        }
+        val leftDiagonalSum = leftDiagonalSums[row + size - 1][col] -
+                (if (row - 1 >= 0 && col + size < maxCol) leftDiagonalSums[row - 1][col + size] else 0)
+        if (sum != leftDiagonalSum) {
+            return false
+        }
+        return true
     }
 }
 
@@ -119,9 +145,9 @@ fun main() {
         intArrayOf(1,5,4,3,2),
         intArrayOf(1,2,7,3,4),
     ))) // 3
-//    println(prob.largestMagicSquare(arrayOf(
-//        intArrayOf(5,1,3,1),
-//        intArrayOf(9,3,3,1),
-//        intArrayOf(1,3,3,8),
-//    ))) // 2
+    println(prob.largestMagicSquare(arrayOf(
+        intArrayOf(5,1,3,1),
+        intArrayOf(9,3,3,1),
+        intArrayOf(1,3,3,8),
+    ))) // 2
 }
