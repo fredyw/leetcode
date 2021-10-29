5d06851591f635a243c20cc98490e7e3c7d08282
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-10-29 01:32:44 -0500

    Skeleton for problem 1718

---
+++ src/main/kotlin/leetcode/Problem1718.kt
@@ -0,0 +1,16 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/construct-the-lexicographically-largest-valid-sequence/
+ */
+class Problem1718 {
+    fun constructDistancedSequence(n: Int): IntArray {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem1718()
+    println(prob.constructDistancedSequence(3)) // [3,1,2,3,2]
+    println(prob.constructDistancedSequence(5)) // [5,3,1,4,3,5,2,4,2]
+}
