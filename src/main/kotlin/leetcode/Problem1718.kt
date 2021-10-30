083d9dee8baaf31a65189ef4194fb6011569c0e6
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-10-30 10:17:02 -0500

    Solve problem 1718

--- src/main/kotlin/leetcode/Problem1718.kt
+++ src/main/kotlin/leetcode/Problem1718.kt
@@ -5,12 +5,43 @@ package leetcode
  */
 class Problem1718 {
     fun constructDistancedSequence(n: Int): IntArray {
-        TODO()
+        return constructDistancedSequence(n, 0, HashSet(), IntArray(n * 2 - 1)).array
     }
-}
 
-fun main() {
-    val prob = Problem1718()
-    println(prob.constructDistancedSequence(3)) // [3,1,2,3,2]
-    println(prob.constructDistancedSequence(5)) // [5,3,1,4,3,5,2,4,2]
+    private class Answer(val found: Boolean, val array: IntArray)
+
+    private fun constructDistancedSequence(n: Int, index: Int,
+                                           visited: MutableSet<Int>, array: IntArray): Answer {
+        if (visited.size == n) {
+            return Answer(true, array)
+        }
+        if (array[index] != 0) {
+            return constructDistancedSequence(n, index + 1, visited, array)
+        }
+        for (i in n downTo 1) {
+            if (i in visited) {
+                continue
+            }
+            if (i == 1) {
+                array[index] = i
+            } else {
+                if (index + i >= array.size || array[index + i] != 0) {
+                    continue
+                }
+                array[index] = i
+                array[index + i] = i
+            }
+            visited += i
+            val answer = constructDistancedSequence(n, index + 1, visited, array)
+            if (answer.found) {
+                return answer
+            }
+            visited -= i
+            array[index] = 0
+            if (i != 1) {
+                array[index + i] = 0
+            }
+        }
+        return Answer(false, array)
+    }
 }
