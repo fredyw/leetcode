4cb31431a4ffa828a65fa0a5813cd1e421c2c9b0
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-10-30 10:19:26 -0500

    Skeleton for problem 1930

---
+++ src/main/kotlin/leetcode/Problem1930.kt
@@ -0,0 +1,17 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/unique-length-3-palindromic-subsequences/
+ */
+class Problem1930 {
+    fun countPalindromicSubsequence(s: String): Int {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem1930()
+    println(prob.countPalindromicSubsequence("aabca")) // 3
+    println(prob.countPalindromicSubsequence("adc")) // 0
+    println(prob.countPalindromicSubsequence("bbcbaba")) // 4
+}
