ecc20365c6f52a933b6e6d967f45b469736ddf87
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-10-31 09:08:46 -0500

    Solve problem 2057

--- src/main/kotlin/leetcode/Problem2057.kt
+++ src/main/kotlin/leetcode/Problem2057.kt
@@ -5,14 +5,11 @@ package leetcode
  */
 class Problem2057 {
     fun smallestEqual(nums: IntArray): Int {
-        TODO()
+        for ((index, num) in nums.withIndex()) {
+            if (index % 10 == num) {
+                return index
+            }
+        }
+        return -1
     }
-}
-
-fun main() {
-    val prob = Problem2057()
-    println(prob.smallestEqual(intArrayOf(0,1,2))) // 0
-    println(prob.smallestEqual(intArrayOf(4,3,2,1))) // 2
-    println(prob.smallestEqual(intArrayOf(1,2,3,4,5,6,7,8,9,0))) // -1
-    println(prob.smallestEqual(intArrayOf(2,1,3,5,2))) // 1
 }
