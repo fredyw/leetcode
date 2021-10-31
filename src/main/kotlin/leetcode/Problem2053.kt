26d47d092d393731a4d2ce9925d680a8adfd4cdf
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-10-31 09:03:31 -0500

    Solve problem 2053

--- src/main/kotlin/leetcode/Problem2053.kt
+++ src/main/kotlin/leetcode/Problem2053.kt
@@ -5,13 +5,22 @@ package leetcode
  */
 class Problem2053 {
     fun kthDistinct(arr: Array<String>, k: Int): String {
-        TODO()
+        val map = mutableMapOf<String, Int>()
+        for (s in arr) {
+            map[s] = (map[s] ?: 0) + 1
+        }
+        val distinctMap = map.filter { it.value == 1 }
+        var count = k
+        for (s in arr) {
+            var distinct = s
+            if (s in distinctMap) {
+                count--
+                if (count == 0) {
+                    return distinct
+                }
+            }
+        }
+        return ""
     }
 }
 
-fun main() {
-    val prob = Problem2053()
-    println(prob.kthDistinct(arrayOf("d","b","c","b","c","a"), 2)) // "a"
-    println(prob.kthDistinct(arrayOf("aaa","aa","a"), 1)) // "aaa"
-    println(prob.kthDistinct(arrayOf("a","b","a"), 3)) // ""
-}
