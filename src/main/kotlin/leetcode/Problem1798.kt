52ef3c58807b6f08f72e2c06c0f6b0bf37771e03
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-11-03 00:27:30 -0500

    Skeleton for problem 1798

---
+++ src/main/kotlin/leetcode/Problem1798.kt
@@ -0,0 +1,17 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/maximum-number-of-consecutive-values-you-can-make/
+ */
+class Problem1798 {
+    fun getMaximumConsecutive(coins: IntArray): Int {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem1798()
+    println(prob.getMaximumConsecutive(intArrayOf(1,3))) // 2
+    println(prob.getMaximumConsecutive(intArrayOf(1,1,1,4))) // 8
+    println(prob.getMaximumConsecutive(intArrayOf(1,4,10,3,1))) // 20
+}
