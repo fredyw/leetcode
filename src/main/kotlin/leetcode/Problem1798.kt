238bd9f190a6e18e8f13a2b02ebd87a9b406505f
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-11-05 22:16:17 -0500

    Solve problem 1798

--- src/main/kotlin/leetcode/Problem1798.kt
+++ src/main/kotlin/leetcode/Problem1798.kt
@@ -5,17 +5,13 @@ package leetcode
  */
 class Problem1798 {
     fun getMaximumConsecutive(coins: IntArray): Int {
-        TODO()
+        coins.sort()
+        var answer = 0
+        for (coin in coins) {
+            if (coin <= answer + 1) {
+                answer += coin
+            }
+        }
+        return answer + 1 // + 1 for 0
     }
-}
-
-fun main() {
-    val prob = Problem1798()
-    println(prob.getMaximumConsecutive(intArrayOf(1,3))) // 2
-    println(prob.getMaximumConsecutive(intArrayOf(1,1,1,4))) // 8
-    println(prob.getMaximumConsecutive(intArrayOf(1,4,10,3,1))) // 20
-    println(prob.getMaximumConsecutive(intArrayOf(1,2,3,5))) // 12
-    println(prob.getMaximumConsecutive(intArrayOf(1,1,9))) // 3
-    println(prob.getMaximumConsecutive(intArrayOf(1,3,4,10))) // 2
-    println(prob.getMaximumConsecutive(intArrayOf(1,1,1,10))) // 3
 }
