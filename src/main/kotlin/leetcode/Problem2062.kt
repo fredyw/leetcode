8e291bc8e2e692e12beb826d3d12c1d43da3b3c3
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-11-07 12:10:22 -0600

    Skeleton for problem 2062

---
+++ src/main/kotlin/leetcode/Problem2062.kt
@@ -0,0 +1,17 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/count-vowel-substrings-of-a-string/
+ */
+class Problem2062 {
+    fun countVowelSubstrings(word: String): Int {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2062()
+    println(prob.countVowelSubstrings("aeiouu")) // 2
+    println(prob.countVowelSubstrings("unicornarihan")) // 0
+    println(prob.countVowelSubstrings("cuaieuouac")) // 7
+}
