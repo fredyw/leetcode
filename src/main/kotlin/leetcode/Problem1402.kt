9b9c847819463dd8a467034b801e3c38e11720c0
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-11-07 12:36:10 -0600

    Skeleton for problem 1402

---
+++ src/main/kotlin/leetcode/Problem1402.kt
@@ -0,0 +1,18 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/reducing-dishes/
+ */
+class Problem1402 {
+    fun maxSatisfaction(satisfaction: IntArray): Int {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem1402()
+    println(prob.maxSatisfaction(intArrayOf(-1,-8,0,5,-9))) // 14
+    println(prob.maxSatisfaction(intArrayOf(4,3,2))) // 20
+    println(prob.maxSatisfaction(intArrayOf(-1,-4,-5))) // 0
+    println(prob.maxSatisfaction(intArrayOf(-2,5,-1,0,3,-3))) // 35
+}
