fcfdf0115d02f35b33d3bc7973f7f3430afdad4c
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-11-10 11:11:57 -0600

    Skeleton for problem 2063

---
+++ src/main/kotlin/leetcode/Problem2063.kt
@@ -0,0 +1,18 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/vowels-of-all-substrings/
+ */
+class Problem2063 {
+    fun countVowels(word: String): Long {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2063()
+    println(prob.countVowels("aba")) // 6
+    println(prob.countVowels("abc")) // 3
+    println(prob.countVowels("ltcd")) // 0
+    println(prob.countVowels("noosabasboosa")) // 237
+}
