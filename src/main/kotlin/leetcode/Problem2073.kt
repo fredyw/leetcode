ddb1635928c19cd49ab41fc1e6b45e94a98af8d6
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-11-14 09:25:04 -0600

    Skeleton for problem 2073

---
+++ src/main/kotlin/leetcode/Problem2073.kt
@@ -0,0 +1,16 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/time-needed-to-buy-tickets/
+ */
+class Problem2073 {
+    fun timeRequiredToBuy(tickets: IntArray, k: Int): Int {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2073()
+    println(prob.timeRequiredToBuy(intArrayOf(2,3,2), 2)) // 6
+    println(prob.timeRequiredToBuy(intArrayOf(5,1,1,1), 0)) // 8
+}
