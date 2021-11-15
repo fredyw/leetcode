63551e40ae5dd26751863ea2c223292521dc0cc5
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-11-15 14:44:03 -0600

    Solve problem 2073

--- src/main/kotlin/leetcode/Problem2073.kt
+++ src/main/kotlin/leetcode/Problem2073.kt
@@ -5,20 +5,19 @@ package leetcode
  */
 class Problem2073 {
     fun timeRequiredToBuy(tickets: IntArray, k: Int): Int {
-        TODO()
+        var answer = 0
+        while (tickets[k] > 0) {
+            for (i in tickets.indices) {
+                if (tickets[i] == 0) {
+                    continue
+                }
+                tickets[i]--
+                answer++
+                if (i == k && tickets[i] == 0) {
+                    break
+                }
+            }
+        }
+        return answer
     }
-}
-
-fun main() {
-    val prob = Problem2073()
-    println(prob.timeRequiredToBuy(intArrayOf(2,3,2), 2)) // 6
-    println(prob.timeRequiredToBuy(intArrayOf(5,1,1,1), 0)) // 8
-    println(prob.timeRequiredToBuy(intArrayOf(6,4,1,2,3), 4)) // 12
-    println(prob.timeRequiredToBuy(intArrayOf(6,4,1,2,3), 3)) // 8
-    println(prob.timeRequiredToBuy(intArrayOf(6,4,1,2,3), 3)) // 12
-    println(prob.timeRequiredToBuy(intArrayOf(6,4,7,8,3), 4)) // 15
-    println(prob.timeRequiredToBuy(intArrayOf(6,4,1,1,2,3,3), 6)) // 16
-    println(prob.timeRequiredToBuy(intArrayOf(6,4,1,1,2,3,3,7), 6)) // 18
-    println(prob.timeRequiredToBuy(intArrayOf(6,4,5,5,5,3,3,7), 6)) // 23
-    println(prob.timeRequiredToBuy(intArrayOf(6,4,5,5,5,4,3,7), 6)) // 23
 }
