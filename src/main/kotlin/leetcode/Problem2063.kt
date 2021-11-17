4dbe89f5f8add7440e6ae45115870d41355de318
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-11-17 14:38:07 -0600

    Solve problem 2063

--- src/main/kotlin/leetcode/Problem2063.kt
+++ src/main/kotlin/leetcode/Problem2063.kt
@@ -5,19 +5,16 @@ package leetcode
  */
 class Problem2063 {
     fun countVowels(word: String): Long {
-        TODO()
+        var answer = 0L
+        for (i in word.indices) {
+            if (isVowel(word[i])) {
+                answer += (i.toLong() + 1) * (word.length - i)
+            }
+        }
+        return answer
     }
 
     private fun isVowel(char: Char): Boolean {
         return char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u'
     }
-}
-
-fun main() {
-    val prob = Problem2063()
-    println(prob.countVowels("aba")) // 6
-    println(prob.countVowels("abc")) // 3
-    println(prob.countVowels("ltcd")) // 0
-    println(prob.countVowels("noosabasboosa")) // 237
-    println(prob.countVowels("bac")) // 4
 }
