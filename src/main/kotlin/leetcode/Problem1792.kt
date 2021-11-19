27cdf3f06834c18fd5e08d1a7c7c447c1c1e8d42
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-11-19 12:03:42 -0600

    Solve problem 1792

--- src/main/kotlin/leetcode/Problem1792.kt
+++ src/main/kotlin/leetcode/Problem1792.kt
@@ -1,16 +1,32 @@
 package leetcode
 
+import java.util.PriorityQueue
+
 /**
  * https://leetcode.com/problems/maximum-average-pass-ratio/
  */
 class Problem1792 {
     fun maxAverageRatio(classes: Array<IntArray>, extraStudents: Int): Double {
-        TODO()
+        val queue = PriorityQueue<IntArray> { a, b ->
+            val diff1 = ((a[0] + 1).toDouble() / (a[1] + 1).toDouble()) - (a[0].toDouble() / a[1].toDouble())
+            val diff2 = ((b[0] + 1).toDouble() / (b[1] + 1).toDouble()) - (b[0].toDouble() / b[1].toDouble())
+            diff2.compareTo(diff1)
+        }
+        for (c in classes) {
+            queue += c
+        }
+        var students = extraStudents
+        while (students > 0) {
+            val c = queue.poll()
+            c[0]++
+            c[1]++
+            queue += c
+            students--
+        }
+        var sum = 0.0
+        for (c in classes) {
+            sum += c[0].toDouble() / c[1].toDouble()
+        }
+        return sum / classes.size
     }
-}
-
-fun main() {
-    val prob = Problem1792()
-    println(prob.maxAverageRatio(arrayOf(intArrayOf(1,2), intArrayOf(3,5), intArrayOf(2,2)), 2)) // 0.78333
-    println(prob.maxAverageRatio(arrayOf(intArrayOf(2,4), intArrayOf(3,9), intArrayOf(4,5), intArrayOf(2,10)), 4)) // 0.53485
 }
