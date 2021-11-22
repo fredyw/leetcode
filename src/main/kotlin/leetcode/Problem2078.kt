7f8340ab6ae1ef62537eb990d883a71ce40111f5
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-11-21 20:45:47 -0600

    Skeleton for problem 2078

---
+++ src/main/kotlin/leetcode/Problem2078.kt
@@ -0,0 +1,17 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/two-furthest-houses-with-different-colors/
+ */
+class Problem2078 {
+    fun maxDistance(colors: IntArray): Int {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2078()
+    println(prob.maxDistance(intArrayOf(1,1,1,6,1,1,1))) // 3
+    println(prob.maxDistance(intArrayOf(1,8,3,8,3))) // 4
+    println(prob.maxDistance(intArrayOf(0,1))) // 1
+}
