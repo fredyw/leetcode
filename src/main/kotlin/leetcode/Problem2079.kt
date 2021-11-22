9dd86b0c1a7e9ae1e14ea63aaccbcc8c6a7687b0
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-11-22 13:58:24 -0600

    Skeleton for problem 2079

---
+++ src/main/kotlin/leetcode/Problem2079.kt
@@ -0,0 +1,17 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/watering-plants/
+ */
+class Problem2079 {
+    fun wateringPlants(plants: IntArray, capacity: Int): Int {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2079()
+    println(prob.wateringPlants(intArrayOf(2,2,3,3), 5)) // 14
+    println(prob.wateringPlants(intArrayOf(1,1,1,4,2,3), 4)) // 30
+    println(prob.wateringPlants(intArrayOf(7,7,7,7,7,7,7), 8)) // 49
+}
