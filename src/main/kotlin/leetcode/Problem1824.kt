9be58bd2936145fddc5c0feeb1cf292e77f4c657
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-11-23 22:08:26 -0600

    Update problem 1824

--- src/main/kotlin/leetcode/Problem1824.kt
+++ src/main/kotlin/leetcode/Problem1824.kt
@@ -12,6 +12,7 @@ class Problem1824 {
 fun main() {
     val prob = Problem1824()
     println(prob.minSideJumps(intArrayOf(0,1,2,3,0))) // 2
+    println(prob.minSideJumps(intArrayOf(0,0,2,3,0))) // 1
     println(prob.minSideJumps(intArrayOf(0,1,1,3,3,0))) // 0
     println(prob.minSideJumps(intArrayOf(0,2,1,0,3,0))) // 2
 }
