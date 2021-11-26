2d07269a7fd49cbbe23c911d3b98889c7a8856d0
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-11-26 16:53:54 -0600

    Solve problem 1424

--- src/main/kotlin/leetcode/Problem1424.kt
+++ src/main/kotlin/leetcode/Problem1424.kt
@@ -1,62 +1,24 @@
 package leetcode
 
-import kotlin.math.max
+import java.util.TreeMap
 
 /**
  * https://leetcode.com/problems/diagonal-traverse-ii/
  */
 class Problem1424 {
     fun findDiagonalOrder(nums: List<List<Int>>): IntArray {
-        val maxRow = nums.size
-        var maxCol = 0
-        for (num in nums) {
-            maxCol = max(maxCol, num.size)
+        val map = TreeMap<Int, List<Int>>()
+        for ((row, num) in nums.withIndex()) {
+            for ((col, n) in num.withIndex()) {
+                map[row + col] = (map[row + col] ?: listOf()) + n
+            }
         }
         val answer = mutableListOf<Int>()
-        for (row in 0 until maxRow) {
-            var r = row
-            var c = 0
-            while (r >= 0) {
-                if (c < nums[r].size) {
-                    answer += nums[r][c]
-                }
-                r--
-                c++
-            }
-        }
-        for (col in 1 until maxCol) {
-            var r = maxRow - 1
-            var c = col
-            while (r >= 0) {
-                if (c < nums[r].size) {
-                    answer += nums[r][c]
-                }
-                r--
-                c++
-            }
+        for (list in map.values) {
+           for (num in list.asReversed()) {
+               answer += num
+           }
         }
         return answer.toIntArray()
     }
-}
-
-fun main() {
-    val prob = Problem1424()
-    println(prob.findDiagonalOrder(listOf(
-        listOf(1,2,3),
-        listOf(4,5,6),
-        listOf(7,8,9))).contentToString()) // [1,4,2,7,5,3,8,6,9]
-    println(prob.findDiagonalOrder(listOf(
-        listOf(1,2,3,4,5),
-        listOf(6,7),
-        listOf(8),
-        listOf(9,10,11),
-        listOf(12,13,14,15,16))).contentToString()) // [1,6,2,8,7,3,9,4,12,10,5,13,11,14,15,16]
-    println(prob.findDiagonalOrder(listOf(
-        listOf(1,2,3),
-        listOf(4),
-        listOf(5,6,7),
-        listOf(8),
-        listOf(9,10,11))).contentToString()) // [1,4,2,5,3,8,6,9,7,10,11]
-    println(prob.findDiagonalOrder(listOf(
-        listOf(1,2,3,4,5,6))).contentToString()) // [1,2,3,4,5,6]
 }
