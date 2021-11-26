c11382c7024a6fa170dcc1b866bdc118675fbf44
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-11-26 15:24:52 -0600

    Solve problem 1824

--- src/main/kotlin/leetcode/Problem1824.kt
+++ src/main/kotlin/leetcode/Problem1824.kt
@@ -1,18 +1,40 @@
 package leetcode
 
+import kotlin.math.min
+
 /**
  * https://leetcode.com/problems/minimum-sideway-jumps/
  */
 class Problem1824 {
     fun minSideJumps(obstacles: IntArray): Int {
-        TODO()
+        return minSideJumps(obstacles, lane = 2, index = 0,
+            memo = Array(obstacles.size) { IntArray(4) { -1 } })
     }
-}
 
-fun main() {
-    val prob = Problem1824()
-    println(prob.minSideJumps(intArrayOf(0,1,2,3,0))) // 2
-    println(prob.minSideJumps(intArrayOf(0,0,2,3,0))) // 1
-    println(prob.minSideJumps(intArrayOf(0,1,1,3,3,0))) // 0
-    println(prob.minSideJumps(intArrayOf(0,2,1,0,3,0))) // 2
+    private fun minSideJumps(obstacles: IntArray, lane: Int, index: Int, memo: Array<IntArray>): Int {
+        if (obstacles.size == index) {
+            return 0
+        }
+        if (obstacles[index] == lane) {
+            return Int.MAX_VALUE
+        }
+        if (memo[index][lane] != -1) {
+            return memo[index][lane]
+        }
+        val noSideJump = minSideJumps(obstacles, lane, index + 1, memo)
+        var hasSideJump = Int.MAX_VALUE
+        for (l in 1..3) {
+            if (l == lane || obstacles[index] == l) {
+                continue
+            }
+            var sideJump = minSideJumps(obstacles, l, index + 1, memo)
+            if (sideJump != Int.MAX_VALUE) {
+                sideJump++
+            }
+            hasSideJump = min(hasSideJump, sideJump)
+        }
+        val min = min(noSideJump, hasSideJump)
+        memo[index][lane] = min
+        return min
+    }
 }
