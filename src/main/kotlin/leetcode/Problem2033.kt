5876e38027ace39371752b41987e685977ca4093
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-11-29 14:17:20 -0600

    Update problem 2033

--- src/main/kotlin/leetcode/Problem2033.kt
+++ src/main/kotlin/leetcode/Problem2033.kt
@@ -14,4 +14,6 @@ fun main() {
     println(prob.minOperations(arrayOf(intArrayOf(2,4), intArrayOf(6,8)), 2)) // 4
     println(prob.minOperations(arrayOf(intArrayOf(1,5), intArrayOf(2,3)), 1)) // 5
     println(prob.minOperations(arrayOf(intArrayOf(1,2), intArrayOf(3,4)), 2)) // -1
+    println(prob.minOperations(arrayOf(intArrayOf(1,3), intArrayOf(5,1000)), 4)) // -1
+    println(prob.minOperations(arrayOf(intArrayOf(2,6), intArrayOf(8,1000)), 2)) // 500
 }
