c1c828e43d98938d97091a5f7d6f71fa81749dca
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-11-29 13:53:15 -0600

    Skeleton for problem 2033

---
+++ src/main/kotlin/leetcode/Problem2033.kt
@@ -0,0 +1,17 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/minimum-operations-to-make-a-uni-value-grid/
+ */
+class Problem2033 {
+    fun minOperations(grid: Array<IntArray>, x: Int): Int {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2033()
+    println(prob.minOperations(arrayOf(intArrayOf(2,4), intArrayOf(6,8)), 2)) // 4
+    println(prob.minOperations(arrayOf(intArrayOf(1,5), intArrayOf(2,3)), 1)) // 5
+    println(prob.minOperations(arrayOf(intArrayOf(1,2), intArrayOf(3,4)), 2)) // -1
+}
