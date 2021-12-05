58782b8055494a2e07db291ba49fcd1a2d415749
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-12-05 09:50:02 -0600

    Skeleton for problem 2094

---
+++ src/main/kotlin/leetcode/Problem2094.kt
@@ -0,0 +1,17 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/finding-3-digit-even-numbers/
+ */
+class Problem2094 {
+    fun findEvenNumbers(digits: IntArray): IntArray {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2094()
+    println(prob.findEvenNumbers(intArrayOf(2,1,3,0))) // [102,120,130,132,210,230,302,310,312,320]
+    println(prob.findEvenNumbers(intArrayOf(2,2,8,8,2))) // [222,228,282,288,822,828,882]
+    println(prob.findEvenNumbers(intArrayOf(3,7,5))) // []
+}
