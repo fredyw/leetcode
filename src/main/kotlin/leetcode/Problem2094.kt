8e2d12bc0cfa08b8efff431bcb3e91b1f8d5c00a
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-12-05 09:51:12 -0600

    Update problem 2094

--- src/main/kotlin/leetcode/Problem2094.kt
+++ src/main/kotlin/leetcode/Problem2094.kt
@@ -14,4 +14,6 @@ fun main() {
     println(prob.findEvenNumbers(intArrayOf(2,1,3,0))) // [102,120,130,132,210,230,302,310,312,320]
     println(prob.findEvenNumbers(intArrayOf(2,2,8,8,2))) // [222,228,282,288,822,828,882]
     println(prob.findEvenNumbers(intArrayOf(3,7,5))) // []
+    println(prob.findEvenNumbers(intArrayOf(0,2,0,0))) // [200]
+    println(prob.findEvenNumbers(intArrayOf(0,0,0))) // []
 }
