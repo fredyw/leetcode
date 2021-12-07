24de1c7a4f9a4e979486d8648930ad8623592d12
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-12-06 23:28:44 -0600

    Solve problem 2033

--- src/main/kotlin/leetcode/Problem2033.kt
+++ src/main/kotlin/leetcode/Problem2033.kt
@@ -1,19 +1,29 @@
 package leetcode
 
+import kotlin.math.abs
+
 /**
  * https://leetcode.com/problems/minimum-operations-to-make-a-uni-value-grid/
  */
 class Problem2033 {
     fun minOperations(grid: Array<IntArray>, x: Int): Int {
-        TODO()
+        val array = IntArray(grid.size * grid[0].size)
+        var index = 0
+        for (i in grid.indices) {
+            for (j in grid[i].indices) {
+                array[index++] = grid[i][j]
+            }
+        }
+        array.sort()
+        val median = array[array.size / 2]
+        var answer = 0
+        for (i in array) {
+            val diff = abs(i - median)
+            if (diff % x != 0) {
+                return -1
+            }
+            answer += diff / x
+        }
+        return answer
     }
-}
-
-fun main() {
-    val prob = Problem2033()
-    println(prob.minOperations(arrayOf(intArrayOf(2,4), intArrayOf(6,8)), 2)) // 4
-    println(prob.minOperations(arrayOf(intArrayOf(1,5), intArrayOf(2,3)), 1)) // 5
-    println(prob.minOperations(arrayOf(intArrayOf(1,2), intArrayOf(3,4)), 2)) // -1
-    println(prob.minOperations(arrayOf(intArrayOf(1,3), intArrayOf(5,1000)), 4)) // -1
-    println(prob.minOperations(arrayOf(intArrayOf(2,6), intArrayOf(8,1000)), 2)) // 500
 }
