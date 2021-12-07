aeace9195dde294a24be8ec98c64a34aeedd2bae
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-12-07 15:47:11 -0600

    Solve problem 2091

--- src/main/kotlin/leetcode/Problem2091.kt
+++ src/main/kotlin/leetcode/Problem2091.kt
@@ -1,17 +1,35 @@
 package leetcode
 
+import kotlin.math.min
+
 /**
  * https://leetcode.com/problems/removing-minimum-and-maximum-from-array/
  */
 class Problem2091 {
     fun minimumDeletions(nums: IntArray): Int {
-        TODO()
+        var minIndex = 0
+        var maxIndex = 0
+        for ((index, num) in nums.withIndex()) {
+            if (num < nums[minIndex]) {
+                minIndex = index
+            }
+            if (num > nums[maxIndex]) {
+                maxIndex = index
+            }
+        }
+        if (minIndex == maxIndex) {
+            return 1
+        } else if (minIndex < maxIndex) {
+            // 0----minIndex----maxIndex----(size-1)
+            val a = minIndex + 1
+            val b = nums.size - maxIndex
+            val c = maxIndex - minIndex
+            return min(a + b, min(a + c, b + c))
+        }
+        // 0----maxIndex----minIndex----(size-1)
+        val a = maxIndex + 1
+        val b = nums.size - minIndex
+        val c = minIndex - maxIndex
+        return min(a + b, min(a + c, b + c))
     }
-}
-
-fun main() {
-    val prob = Problem2091()
-    println(prob.minimumDeletions(intArrayOf(2,10,7,5,4,1,8,6))) // 5
-    println(prob.minimumDeletions(intArrayOf(0,-4,19,1,8,-2,-3,5))) // 3
-    println(prob.minimumDeletions(intArrayOf(101))) // -1
 }
