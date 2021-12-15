1184d829c9db3a074fdeb5421586187c963f1718
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-12-15 14:30:05 -0600

    Skeleton for problem 2103

---
+++ src/main/kotlin/leetcode/Problem2103.kt
@@ -0,0 +1,17 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/rings-and-rods/
+ */
+class Problem2103 {
+    fun countPoints(rings: String): Int {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2103()
+    println(prob.countPoints("B0B6G0R6R0R6G9")) // 1
+    println(prob.countPoints("B0R0G0R9R0B0G0")) // 1
+    println(prob.countPoints("G4")) // 0
+}
