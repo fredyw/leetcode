a248227c9e234e49f45edf3b85a8204313c77f1e
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-12-15 20:19:27 -0600

    Update problem 2099

--- src/main/kotlin/leetcode/Problem2099.kt
+++ src/main/kotlin/leetcode/Problem2099.kt
@@ -13,5 +13,6 @@ fun main() {
     val prob = Problem2099()
     println(prob.maxSubsequence(intArrayOf(2,1,3,3), 2).contentToString()) // [3,3]
     println(prob.maxSubsequence(intArrayOf(-1,-2,3,4), 3).contentToString()) // [-1,3,4]
+    println(prob.maxSubsequence(intArrayOf(-1,-2,4,3), 3).contentToString()) // [-1,4,3]
     println(prob.maxSubsequence(intArrayOf(3,4,3,3), 2).contentToString()) // [3,4]
 }
