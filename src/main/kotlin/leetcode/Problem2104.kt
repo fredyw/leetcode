3eeafe05630b745298b49581f607fb09a114e573
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-12-25 22:23:28 -0600

    Solve problem 2104

--- src/main/kotlin/leetcode/Problem2104.kt
+++ src/main/kotlin/leetcode/Problem2104.kt
@@ -1,17 +1,23 @@
 package leetcode
 
+import kotlin.math.max
+import kotlin.math.min
+
 /**
  * https://leetcode.com/problems/sum-of-subarray-ranges/
  */
 class Problem2104 {
     fun subArrayRanges(nums: IntArray): Long {
-        TODO()
+        var answer = 0L
+        for (i in nums.indices) {
+            var min = nums[i]
+            var max = nums[i]
+            for (j in i until nums.size) {
+                min = min(min, nums[j])
+                max = max(max, nums[j])
+                answer += max - min
+            }
+        }
+        return answer
     }
-}
-
-fun main() {
-    val prob = Problem2104()
-    println(prob.subArrayRanges(intArrayOf(1,2,3))) // 4
-    println(prob.subArrayRanges(intArrayOf(1,3,3))) // 4
-    println(prob.subArrayRanges(intArrayOf(4,-2,-3,4,1))) // 59
 }
