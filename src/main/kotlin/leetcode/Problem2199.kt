61f6b39dcdb2c851077856cbab2b9fe6440e02f4
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-12-25 22:35:33 -0600

    Skeleton for problem 2199

---
+++ src/main/kotlin/leetcode/Problem2199.kt
@@ -0,0 +1,17 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/a-number-after-a-double-reversal/
+ */
+class Problem2199 {
+    fun isSameAfterReversals(num: Int): Boolean {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2199()
+    println(prob.isSameAfterReversals(526)) // true
+    println(prob.isSameAfterReversals(1800)) // false
+    println(prob.isSameAfterReversals(0)) // true
+}
