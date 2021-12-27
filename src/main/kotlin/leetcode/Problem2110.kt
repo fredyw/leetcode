81d375cd8e598e064a8b8d242449db0085c06446
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-12-27 16:06:59 -0600

    Solve problem 2110

--- src/main/kotlin/leetcode/Problem2110.kt
+++ src/main/kotlin/leetcode/Problem2110.kt
@@ -5,14 +5,20 @@ package leetcode
  */
 class Problem2110 {
     fun getDescentPeriods(prices: IntArray): Long {
-        TODO()
+        var answer = 0L
+        var i = 1
+        var count = 1L
+        while (i < prices.size) {
+            if (prices[i - 1] == prices[i] + 1) {
+                count++
+            } else {
+                // https://math.stackexchange.com/questions/1603502/how-to-count-the-number-of-substrings-that-can-be-generated-from-a-given-string
+                answer += (count * (count + 1)) / 2
+                count = 1
+            }
+            i++
+        }
+        answer += (count * (count + 1)) / 2
+        return answer
     }
-}
-
-fun main() {
-    val prob = Problem2110()
-    println(prob.getDescentPeriods(intArrayOf(3,2,1,4))) // 7
-    println(prob.getDescentPeriods(intArrayOf(8,6,7,7))) // 4
-    println(prob.getDescentPeriods(intArrayOf(1))) // 1
-    println(prob.getDescentPeriods(intArrayOf(5,4,3,2,1))) // 15
 }
