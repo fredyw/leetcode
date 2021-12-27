792dce91c2aa387ea666c05975c0c9aa2890b5a0
Fredy Wijaya <fredy.wijaya@gmail.com>
2021-12-27 16:09:26 -0600

    Skeleton for problem 2105

---
+++ src/main/kotlin/leetcode/Problem2105.kt
@@ -0,0 +1,17 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/watering-plants-ii/
+ */
+class Problem2105 {
+    fun minimumRefill(plants: IntArray, capacityA: Int, capacityB: Int): Int {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2105()
+    println(prob.minimumRefill(intArrayOf(2,2,3,3), 5, 5)) // 1
+    println(prob.minimumRefill(intArrayOf(2,2,3,3), 3, 4)) // 2
+    println(prob.minimumRefill(intArrayOf(5), 10, 8)) // 0
+}
