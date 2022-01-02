0d5fff36a676d80b45e58b521b4663285afbbe32
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-01-02 15:06:36 -0600

    Skeleton for problem 2124

---
+++ src/main/kotlin/leetcode/Problem2124.kt
@@ -0,0 +1,17 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/check-if-all-as-appears-before-all-bs/
+ */
+class Problem2124 {
+    fun checkString(s: String): Boolean {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2124()
+    println(prob.checkString("aaabbb")) // true
+    println(prob.checkString("abab")) // false
+    println(prob.checkString("bbb")) // true
+}
