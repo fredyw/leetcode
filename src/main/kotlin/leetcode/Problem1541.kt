05cbd7d93c9cb232766207183bdfdbc62c996a2a
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-01-06 23:06:06 -0600

    Solve problem 1541

--- src/main/kotlin/leetcode/Problem1541.kt
+++ src/main/kotlin/leetcode/Problem1541.kt
@@ -1,17 +1,41 @@
 package leetcode
 
+import java.util.*
+
 /**
  * https://leetcode.com/problems/minimum-insertions-to-balance-a-parentheses-string/
  */
 class Problem1541 {
     fun minInsertions(s: String): Int {
-        TODO()
+        var answer = 0
+        val stack = Stack<Char>()
+        var i = 0
+        while (i < s.length) {
+            if (s[i] == '(') {
+                stack.add(s[i])
+                i++
+            } else if (s[i] == ')' && (i + 1 == s.length || s[i + 1] != ')')) {
+                if (stack.isEmpty()) {
+                    answer += 2
+                } else {
+                    stack.pop()
+                    answer++
+                }
+                i++
+            } else if (s[i] == ')' && s[i + 1] == ')') {
+                if (stack.isEmpty()) {
+                    answer++
+                } else {
+                    stack.pop()
+                }
+                i += 2
+            }
+        }
+        while (!stack.isEmpty()) {
+            answer += 2
+            stack.pop()
+        }
+        return answer
     }
 }
 
-fun main() {
-    val prob = Problem1541()
-    println(prob.minInsertions("(()))")) // 1
-    println(prob.minInsertions("())")) // 0
-    println(prob.minInsertions("))())(")) // 3
-}
