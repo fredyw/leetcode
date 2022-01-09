ef51f29d8401f267ec9a0a565bd4db7e0148b713
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-01-08 22:44:25 -0600

    Skeleton for problem 2130

---
+++ src/main/kotlin/leetcode/Problem2130.kt
@@ -0,0 +1,14 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/maximum-twin-sum-of-a-linked-list/
+ */
+class Problem2130 {
+    class ListNode(var `val`: Int) {
+        var next: ListNode? = null
+    }
+
+    fun pairSum(head: ListNode?): Int {
+        TODO()
+    }
+}
