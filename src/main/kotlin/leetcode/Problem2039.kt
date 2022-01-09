5ad039451d721814e3dd4f089fad144ffe59c15f
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-01-08 22:36:24 -0600

    Skeleton for problem 2039

---
+++ src/main/kotlin/leetcode/Problem2039.kt
@@ -0,0 +1,16 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/the-time-when-the-network-becomes-idle/
+ */
+class Problem2039 {
+    fun networkBecomesIdle(edges: Array<IntArray>, patience: IntArray): Int {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2039()
+    println(prob.networkBecomesIdle(arrayOf(intArrayOf(0,1), intArrayOf(1,2)), intArrayOf(0,2,1))) // 8
+    println(prob.networkBecomesIdle(arrayOf(intArrayOf(0,1), intArrayOf(0,2), intArrayOf(1,2)), intArrayOf(0,10,10))) // 3
+}
