b0b176d3b8c559c62e9207e7f7bc0f1cb7e5ee27
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-01-12 21:26:27 -0600

    Update problem 2039

--- src/main/kotlin/leetcode/Problem2039.kt
+++ src/main/kotlin/leetcode/Problem2039.kt
@@ -12,5 +12,10 @@ class Problem2039 {
 fun main() {
     val prob = Problem2039()
     println(prob.networkBecomesIdle(arrayOf(intArrayOf(0,1), intArrayOf(1,2)), intArrayOf(0,2,1))) // 8
+    println(prob.networkBecomesIdle(arrayOf(intArrayOf(0,1), intArrayOf(1,2)), intArrayOf(0,3,1))) // 8
+    println(prob.networkBecomesIdle(arrayOf(intArrayOf(0,1), intArrayOf(1,2)), intArrayOf(0,10,1))) // 8
+    println(prob.networkBecomesIdle(arrayOf(intArrayOf(0,1), intArrayOf(1,2)), intArrayOf(0,10,3))) // 8
+    println(prob.networkBecomesIdle(arrayOf(intArrayOf(0,1), intArrayOf(1,2)), intArrayOf(0,10,30))) // 5
+    println(prob.networkBecomesIdle(arrayOf(intArrayOf(0,1), intArrayOf(1,2)), intArrayOf(0,1,1))) // 8
     println(prob.networkBecomesIdle(arrayOf(intArrayOf(0,1), intArrayOf(0,2), intArrayOf(1,2)), intArrayOf(0,10,10))) // 3
 }
