e464cc53024323ce33309f2f40dd9bd5c90fa25f
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-01-16 00:47:44 -0600

    Skeleton for problem 1673

---
+++ src/main/kotlin/leetcode/Problem1673.kt
@@ -0,0 +1,16 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/find-the-most-competitive-subsequence/
+ */
+class Problem1673 {
+    fun mostCompetitive(nums: IntArray, k: Int): IntArray {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem1673()
+    println(prob.mostCompetitive(intArrayOf(3,5,2,6), 2)) // [2,6]
+    println(prob.mostCompetitive(intArrayOf(2,4,3,3,5,4,9,6), 4)) // [2,3,3,4]
+}
