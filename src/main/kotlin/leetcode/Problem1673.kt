199ad728a160ac4b63261593e97ee84b9d5a7a27
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-01-16 21:28:41 -0600

    Update problem 1673

--- src/main/kotlin/leetcode/Problem1673.kt
+++ src/main/kotlin/leetcode/Problem1673.kt
@@ -13,4 +13,5 @@ fun main() {
     val prob = Problem1673()
     println(prob.mostCompetitive(intArrayOf(3,5,2,6), 2)) // [2,6]
     println(prob.mostCompetitive(intArrayOf(2,4,3,3,5,4,9,6), 4)) // [2,3,3,4]
+    println(prob.mostCompetitive(intArrayOf(4,3,3,5,4,9,6,2), 4)) // [3,3,4,2]
 }
