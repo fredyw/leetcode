63d6d4b68f767522633cb59cb308fc8d54349c43
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-01-18 14:50:09 -0600

    Skeleton for problem 2139

---
+++ src/main/kotlin/leetcode/Problem2139.kt
@@ -0,0 +1,17 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/minimum-moves-to-reach-target-score/
+ */
+class Problem2139 {
+    fun minMoves(target: Int, maxDoubles: Int): Int {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2139()
+    println(prob.minMoves(5, 0)) // 4
+    println(prob.minMoves(19, 2)) // 7
+    println(prob.minMoves(10, 4)) // 4
+}
