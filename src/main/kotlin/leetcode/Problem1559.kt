f6d77430d24a9cba0131645650f09c53e3286a76
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-01-19 11:04:35 -0600

    Skeleton for problem 1559

---
+++ src/main/kotlin/leetcode/Problem1559.kt
@@ -0,0 +1,14 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/detect-cycles-in-2d-grid/
+ */
+class Problem1559 {
+    fun containsCycle(grid: Array<CharArray>): Boolean {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem1559()
+}
