4a1bc74dc0198ff543af6b6aecba749b70e94aca
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-01-19 17:45:42 -0600

    Update problem 1559

--- src/main/kotlin/leetcode/Problem1559.kt
+++ src/main/kotlin/leetcode/Problem1559.kt
@@ -11,4 +11,18 @@ class Problem1559 {
 
 fun main() {
     val prob = Problem1559()
+    println(prob.containsCycle(arrayOf(
+        charArrayOf('a','a','a','a'),
+        charArrayOf('a','b','b','a'),
+        charArrayOf('a','b','b','a'),
+        charArrayOf('a','a','a','a')))) // true
+    println(prob.containsCycle(arrayOf(
+        charArrayOf('c','c','c','a'),
+        charArrayOf('c','d','c','c'),
+        charArrayOf('c','c','e','c'),
+        charArrayOf('f','c','c','c')))) // true
+    println(prob.containsCycle(arrayOf(
+        charArrayOf('a','b','b'),
+        charArrayOf('b','z','b'),
+        charArrayOf('b','b','a')))) // false
 }
