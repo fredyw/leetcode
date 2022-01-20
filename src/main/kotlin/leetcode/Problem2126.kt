9b54d90ad8e0ae6529c0b897e752f684bee4afba
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-01-19 21:58:51 -0600

    Skeleton for problem 2126

---
+++ src/main/kotlin/leetcode/Problem2126.kt
@@ -0,0 +1,16 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/destroying-asteroids/
+ */
+class Problem2126 {
+    fun asteroidsDestroyed(mass: Int, asteroids: IntArray): Boolean {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2126()
+    println(prob.asteroidsDestroyed(10, intArrayOf(3,9,19,5,21))) // true
+    println(prob.asteroidsDestroyed(5, intArrayOf(4,9,23,4))) // false
+}
