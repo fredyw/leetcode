6e1624398852dfd94dfea8c3bbf5013d8ecf7e43
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-01-22 15:17:07 -0600

    Rename problem 2144 to 2114

--- src/main/kotlin/leetcode/Problem2114.kt
+++ src/main/kotlin/leetcode/Problem2114.kt
@@ -5,7 +5,7 @@ import kotlin.math.max
 /**
  * https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/
  */
-class Problem2144 {
+class Problem2114 {
     fun mostWordsFound(sentences: Array<String>): Int {
         var answer = 0
         for (sentence in sentences) {
