11ce542ee32471f23a4e42c2f5aba349e47b1227
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-01-22 15:18:27 -0600

    Skeleton for problem 2144

---
+++ src/main/kotlin/leetcode/Problem2144.kt
@@ -0,0 +1,17 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/
+ */
+class Problem2144 {
+    fun minimumCost(cost: IntArray): Int {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2144()
+    println(prob.minimumCost(intArrayOf(1,2,3))) // 5
+    println(prob.minimumCost(intArrayOf(6,5,7,9,2,2))) // 23
+    println(prob.minimumCost(intArrayOf(5,5))) // 10
+}
