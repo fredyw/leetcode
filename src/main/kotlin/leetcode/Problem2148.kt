4e3a1fe43cb6763b69e34826de07a49fc9640143
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-01-23 15:06:11 -0600

    Skeleton for problem 2148

---
+++ src/main/kotlin/leetcode/Problem2148.kt
@@ -0,0 +1,16 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/count-elements-with-strictly-smaller-and-greater-elements/
+ */
+class Problem2148 {
+    fun countElements(nums: IntArray): Int {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2148()
+    println(prob.countElements(intArrayOf(11,7,2,15))) // 2
+    println(prob.countElements(intArrayOf(-3,3,3,90))) // 2
+}
