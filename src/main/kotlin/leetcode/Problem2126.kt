478e9c8c62a6d6664e7a9893ce0d44340238a175
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-01-23 17:24:31 -0600

    Solve problem 2126

--- src/main/kotlin/leetcode/Problem2126.kt
+++ src/main/kotlin/leetcode/Problem2126.kt
@@ -5,12 +5,15 @@ package leetcode
  */
 class Problem2126 {
     fun asteroidsDestroyed(mass: Int, asteroids: IntArray): Boolean {
-        TODO()
+        asteroids.sort()
+        var m = mass.toLong()
+        for (asteroid in asteroids) {
+            if (asteroid <= m) {
+                m += asteroid
+            } else {
+                return false
+            }
+        }
+        return true
     }
-}
-
-fun main() {
-    val prob = Problem2126()
-    println(prob.asteroidsDestroyed(10, intArrayOf(3,9,19,5,21))) // true
-    println(prob.asteroidsDestroyed(5, intArrayOf(4,9,23,4))) // false
 }
