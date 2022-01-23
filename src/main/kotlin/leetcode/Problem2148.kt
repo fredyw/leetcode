2430aa71e6d7ab8d86024b34d62de7ca7efede71
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-01-23 15:26:49 -0600

    Solve problem 2148

--- src/main/kotlin/leetcode/Problem2148.kt
+++ src/main/kotlin/leetcode/Problem2148.kt
@@ -1,16 +1,28 @@
 package leetcode
 
+import kotlin.math.max
+import kotlin.math.min
+
 /**
  * https://leetcode.com/problems/count-elements-with-strictly-smaller-and-greater-elements/
  */
 class Problem2148 {
     fun countElements(nums: IntArray): Int {
-        TODO()
+        var min = nums[0]
+        var max = nums[0]
+        for (num in nums) {
+            min = min(min, num)
+            max = max(max, num)
+        }
+        var minCount = 0
+        var maxCount = 0
+        for (num in nums) {
+            if (num == min) {
+                minCount++
+            } else if (num == max) {
+                maxCount++
+            }
+        }
+        return nums.size - (minCount + maxCount)
     }
-}
-
-fun main() {
-    val prob = Problem2148()
-    println(prob.countElements(intArrayOf(11,7,2,15))) // 2
-    println(prob.countElements(intArrayOf(-3,3,3,90))) // 2
 }
