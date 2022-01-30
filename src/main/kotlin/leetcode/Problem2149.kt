02ec31d7b5bccdc9db8d6bcf7d5f55a77e6f5202
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-01-30 00:23:08 -0600

    Solve problem 2149

--- src/main/kotlin/leetcode/Problem2149.kt
+++ src/main/kotlin/leetcode/Problem2149.kt
@@ -5,12 +5,29 @@ package leetcode
  */
 class Problem2149 {
     fun rearrangeArray(nums: IntArray): IntArray {
-        TODO()
+        val answer = IntArray(nums.size)
+        val positives = IntArray(nums.size / 2)
+        val negatives = IntArray(nums.size / 2)
+        var pi = 0
+        var ni = 0
+        for (num in nums) {
+            if (num < 0) {
+                negatives[ni++] = num
+            } else {
+                positives[pi++] = num
+            }
+        }
+        var i = 0
+        pi = 0
+        ni = 0
+        while (i < nums.size) {
+            if (i % 2 == 0) {
+                answer[i] = positives[pi++]
+            } else {
+                answer[i] = negatives[ni++]
+            }
+            i++
+        }
+        return answer
     }
-}
-
-fun main() {
-    val prob = Problem2149()
-    println(prob.rearrangeArray(intArrayOf(3,1,-2,-5,2,-4)).contentToString()) // [3,-2,1,-5,2,-4]
-    println(prob.rearrangeArray(intArrayOf(-1,1)).contentToString()) // [1,-1]
 }
