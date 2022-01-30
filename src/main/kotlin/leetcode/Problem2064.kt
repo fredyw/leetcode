b442df6c896bf4983b21b4d761df5d5c04a83e80
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-01-30 00:25:01 -0600

    Skeleton for problem 2064

---
+++ src/main/kotlin/leetcode/Problem2064.kt
@@ -0,0 +1,17 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/minimized-maximum-of-products-distributed-to-any-store/
+ */
+class Problem2064 {
+    fun minimizedMaximum(n: Int, quantities: IntArray): Int {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2064()
+    println(prob.minimizedMaximum(6, intArrayOf(11,6))) // 3
+    println(prob.minimizedMaximum(7, intArrayOf(15,10,10))) // 5
+    println(prob.minimizedMaximum(1, intArrayOf(100000))) // 100000
+}
