b48ecc57d888dbb3ebc04c93afa1e6da2eaa47ad
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-01-30 00:09:38 -0600

    Solve problem 1462

--- src/main/kotlin/leetcode/Problem1462.kt
+++ src/main/kotlin/leetcode/Problem1462.kt
@@ -4,23 +4,42 @@ package leetcode
  * https://leetcode.com/problems/course-schedule-iv/
  */
 class Problem1462 {
-    fun checkIfPrerequisite(numCourses: Int, prerequisites: Array<IntArray>, queries: Array<IntArray>): List<Boolean> {
-        TODO()
+    fun checkIfPrerequisite(numCourses: Int, prerequisites: Array<IntArray>,
+                            queries: Array<IntArray>): List<Boolean> {
+        val adjList = buildAdjList(prerequisites)
+        val dependencyMap = mutableMapOf<Int, MutableSet<Int>>()
+        for (course in 0 until numCourses) {
+            val dependencies = mutableSetOf<Int>()
+            dfs(adjList, BooleanArray(numCourses), dependencies, course)
+            dependencyMap[course] = dependencies
+        }
+        val answer = mutableListOf<Boolean>()
+        for (query in queries) {
+            answer += query[1] in dependencyMap[query[0]]!!
+        }
+        return answer
     }
-}
 
-fun main() {
-    val prob = Problem1462()
-    println(prob.checkIfPrerequisite(
-        2,
-        arrayOf(intArrayOf(1,0)),
-        arrayOf(intArrayOf(0,1), intArrayOf(1,0)))) // [false,true]
-    println(prob.checkIfPrerequisite(
-        2,
-        arrayOf(),
-        arrayOf(intArrayOf(1,0), intArrayOf(0,1)))) // [false,true]
-    println(prob.checkIfPrerequisite(
-        3,
-        arrayOf(intArrayOf(1,2), intArrayOf(1,0), intArrayOf(2,0)),
-        arrayOf(intArrayOf(1,0), intArrayOf(1,2)))) // [true,true]
+    private fun buildAdjList(prerequisites: Array<IntArray>): Map<Int, List<Int>> {
+        val adjList = mutableMapOf<Int, MutableList<Int>>()
+        for (prerequisite in prerequisites) {
+            val from = prerequisite[0]
+            val to = prerequisite[1]
+            val list = adjList[from] ?: mutableListOf()
+            list += to
+            adjList[from] = list
+        }
+        return adjList
+    }
+
+    private fun dfs(adjList: Map<Int, List<Int>>, visited: BooleanArray,
+                    dependencies: MutableSet<Int>, course: Int) {
+        visited[course] = true
+        for (adj in (adjList[course] ?: listOf())) {
+            if (!visited[adj]) {
+                dfs(adjList, visited, dependencies, adj)
+            }
+        }
+        dependencies += course
+    }
 }
