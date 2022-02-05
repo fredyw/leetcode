5f8c4192999e0dec0a9edf78ddaf54e664a643a1
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-02-05 14:46:22 -0600

    Skeleton for problem 2160

---
+++ src/main/kotlin/leetcode/Problem2160.kt
@@ -0,0 +1,16 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/minimum-sum-of-four-digit-number-after-splitting-digits/
+ */
+class Problem2160 {
+    fun minimumSum(num: Int): Int {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2160()
+    println(prob.minimumSum(2932)) // 52
+    println(prob.minimumSum(4009)) // 13
+}
