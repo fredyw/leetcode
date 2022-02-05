d314076a3433ff1e3d986d29a223498711d4882a
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-02-05 14:41:15 -0600

    Solve problem 2155

--- src/main/kotlin/leetcode/Problem2155.kt
+++ src/main/kotlin/leetcode/Problem2155.kt
@@ -2,13 +2,42 @@ package leetcode
 
 class Problem2155 {
     fun maxScoreIndices(nums: IntArray): List<Int> {
-        TODO()
+        var left = IntArray(nums.size)
+        var count = 0
+        for (i in 0 until nums.size) {
+            if (nums[i] == 0) {
+                count++
+            }
+            left[i] = count
+        }
+        var right = IntArray(nums.size)
+        count = 0
+        for (i in nums.size - 1 downTo 0) {
+            if (nums[i] == 1) {
+                count++
+            }
+            right[i] = count
+        }
+        val answer = mutableListOf<Int>()
+        var max = 0
+        for (i in 0 until nums.size) {
+            val score = (if (i - 1 < 0) 0 else left[i - 1]) + right[i]
+            if (score > max) {
+                max = score
+                answer.clear()
+                answer += i
+            } else if (score == max) {
+                answer += i
+            }
+        }
+        val score = left[nums.size - 1]
+        if (score > max) {
+            max = score
+            answer.clear()
+            answer += nums.size
+        } else if (score == max) {
+            answer += nums.size
+        }
+        return answer
     }
-}
-
-fun main() {
-    val prob = Problem2155()
-    println(prob.maxScoreIndices(intArrayOf(0,0,1,0))) // [2,4]
-    println(prob.maxScoreIndices(intArrayOf(0,0,0))) // [3]
-    println(prob.maxScoreIndices(intArrayOf(1,1))) // [0]
 }
