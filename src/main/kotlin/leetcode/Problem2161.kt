a1d68405485471f6fb7fb774d76e3fc85a9af071
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-02-06 14:31:51 -0600

    Solve problem 2161

--- src/main/kotlin/leetcode/Problem2161.kt
+++ src/main/kotlin/leetcode/Problem2161.kt
@@ -5,12 +5,20 @@ package leetcode
  */
 class Problem2161 {
     fun pivotArray(nums: IntArray, pivot: Int): IntArray {
-        TODO()
+        val left = mutableListOf<Int>()
+        val center = mutableListOf<Int>()
+        val right = mutableListOf<Int>()
+        for (num in nums) {
+            if (num < pivot) {
+                left += num
+            } else if (num == pivot) {
+                center += num
+            } else {
+                right += num
+            }
+        }
+        left += center
+        left += right
+        return left.toIntArray()
     }
-}
-
-fun main() {
-    val prob = Problem2161()
-    println(prob.pivotArray(intArrayOf(9,12,5,10,14,3,10), 10).contentToString()) // [9,5,3,10,10,12,14]
-    println(prob.pivotArray(intArrayOf(-3,4,3,2), 2).contentToString()) // [-3,2,4,3]
 }
