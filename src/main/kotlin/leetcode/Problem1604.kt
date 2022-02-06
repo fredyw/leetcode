e4585634a8c3a247d0ff3a3be1536d17cbb9a578
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-02-05 21:56:57 -0600

    Solve problem 1604

--- src/main/kotlin/leetcode/Problem1604.kt
+++ src/main/kotlin/leetcode/Problem1604.kt
@@ -13,7 +13,16 @@ class Problem1604 {
         }
         val answer = mutableListOf<String>()
         for ((key, value) in map) {
-            TODO()
+            var i = 0
+            value.sort()
+            while (i < value.size - 2) {
+                val t = difference(value[i], value[i + 1]) + difference(value[i + 1], value[i + 2])
+                if (t <= 60) {
+                    answer += key
+                    break
+                }
+                i++
+            }
         }
         answer.sort()
         return answer
@@ -26,14 +35,4 @@ class Problem1604 {
         val hour = (toHour.toInt() - fromHour.toInt()) * 60
         return minute + hour
     }
-}
-
-fun main() {
-    val prob = Problem1604()
-    println(prob.alertNames(
-        arrayOf("daniel","daniel","daniel","luis","luis","luis","luis"),
-        arrayOf("10:00","10:40","11:00","09:00","11:00","13:00","15:00"))) // ["daniel"]
-    println(prob.alertNames(
-        arrayOf("alice","alice","alice","bob","bob","bob","bob"),
-        arrayOf("12:01","12:00","18:00","21:00","21:20","21:30","23:00"))) // ["bob"]
 }
