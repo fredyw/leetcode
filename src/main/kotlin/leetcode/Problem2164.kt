f72c95583d531324ed8c450956421aae69131e6e
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-02-06 14:16:42 -0600

    Skeleton for problem 2164

---
+++ src/main/kotlin/leetcode/Problem2164.kt
@@ -0,0 +1,16 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/sort-even-and-odd-indices-independently/
+ */
+class Problem2164 {
+    fun sortEvenOdd(nums: IntArray): IntArray {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2164()
+    println(prob.sortEvenOdd(intArrayOf(4,1,2,3))) // [2,3,4,1]
+    println(prob.sortEvenOdd(intArrayOf(2,1))) // [2,1]
+}
