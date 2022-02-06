83258c9c9a5685fb52779fd13be82de8acd66027
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-02-06 14:57:12 -0600

    Solve problem 2165

--- src/main/kotlin/leetcode/Problem2165.kt
+++ src/main/kotlin/leetcode/Problem2165.kt
@@ -5,12 +5,26 @@ package leetcode
  */
 class Problem2165 {
     fun smallestNumber(num: Long): Long {
-        TODO()
+        if (num > 0) {
+            val chars = num.toString().toCharArray()
+            chars.sort()
+            var count = 0
+            var i = 0
+            while (chars[i] == '0') {
+                i++
+                count++
+            }
+            var answer = chars[i++] + "0".repeat(count)
+            while (i < chars.size) {
+                answer += chars[i++]
+            }
+            return answer.toLong()
+        } else if (num < 0) {
+            val chars = num.toString().substring(1).toCharArray()
+            chars.sortDescending()
+            val answer = String(chars)
+            return -1 * answer.toLong()
+        }
+        return 0
     }
-}
-
-fun main() {
-    val prob = Problem2165()
-    println(prob.smallestNumber(310)) // 103
-    println(prob.smallestNumber(-7605)) // -7605
 }
