a5777a57aef771074cd370933c9b5272523ee003
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-02-06 14:32:49 -0600

    Skeleton for problem 2165

---
+++ src/main/kotlin/leetcode/Problem2165.kt
@@ -0,0 +1,16 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/smallest-value-of-the-rearranged-number/
+ */
+class Problem2165 {
+    fun smallestNumber(num: Long): Long {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2165()
+    println(prob.smallestNumber(310)) // 103
+    println(prob.smallestNumber(-7605)) // -7605
+}
