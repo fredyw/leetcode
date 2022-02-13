c2741668861b9e0ab6dcdaeb813d912c6ab756e8
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-02-13 10:33:01 -0600

    Solve problem 2169

--- src/main/kotlin/leetcode/Problem2169.kt
+++ src/main/kotlin/leetcode/Problem2169.kt
@@ -5,12 +5,17 @@ package leetcode
  */
 class Problem2169 {
     fun countOperations(num1: Int, num2: Int): Int {
-        TODO()
+        var answer = 0
+        var n1 = num1
+        var n2 = num2
+        while (n1 > 0 && n2 > 0) {
+            if (n1 >= n2) {
+                n1 -= n2
+            } else {
+                n2 -= n1
+            }
+            answer++
+        }
+        return answer
     }
-}
-
-fun main() {
-    val prob = Problem2169()
-    println(prob.countOperations(2, 3)) // 3
-    println(prob.countOperations(10, 10)) // 1
 }
