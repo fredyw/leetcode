a9a87003d313290454c79c30fc0bf044eec847cc
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-02-13 10:26:31 -0600

    Skeleton for problem 2169

---
+++ src/main/kotlin/leetcode/Problem2169.kt
@@ -0,0 +1,16 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/count-operations-to-obtain-zero/
+ */
+class Problem2169 {
+    fun countOperations(num1: Int, num2: Int): Int {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2169()
+    println(prob.countOperations(2, 3)) // 3
+    println(prob.countOperations(10, 10)) // 1
+}
