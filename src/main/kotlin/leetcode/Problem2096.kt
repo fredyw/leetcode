ab1818f320b17141a3c8e6045d8f2d2bfb8876d0
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-02-15 23:22:16 -0600

    Skeleton for problem 2096

---
+++ src/main/kotlin/leetcode/Problem2096.kt
@@ -0,0 +1,15 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/step-by-step-directions-from-a-binary-tree-node-to-another/
+ */
+class Problem2096 {
+    class TreeNode(var `val`: Int) {
+        var left: TreeNode? = null
+        var right: TreeNode? = null
+    }
+
+    fun getDirections(root: TreeNode?, startValue: Int, destValue: Int): String {
+        TODO()
+    }
+}
