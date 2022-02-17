70086c004b21c4ad0f61b9db7db36fd39a5ebca5
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-02-17 00:44:13 -0600

    Update problem 2096

--- src/main/kotlin/leetcode/Problem2096.kt
+++ src/main/kotlin/leetcode/Problem2096.kt
@@ -15,14 +15,13 @@ class Problem2096 {
         val start = getDirection(root, startValue, "")
         val dest = getDirection(root, destValue, "")
         val minLength = min(start.length, dest.length)
-        println("$start - $dest")
         var i = 0
-        var j = 0
-        while (i < minLength && start[i++] == dest[j++]) {}
-        val subStart = start.substring(i - 1)
-        val subDest = dest.substring(j - 1)
-        println("$subStart - $subDest")
-        return ""
+        while (i < minLength && start[i] == dest[i]) {
+            i++
+        }
+        val subStart = start.substring(i)
+        val subDest = dest.substring(i)
+        return "U".repeat(subStart.length) + subDest
     }
 
     private fun getDirection(root: TreeNode?, `value`: Int, direction: String): String {
@@ -43,25 +42,25 @@ class Problem2096 {
 fun main() {
     val prob = Problem2096()
 
-//    var root1 = Problem2096.TreeNode(5)
-//    root1.left = Problem2096.TreeNode(1)
-//    root1.left?.left = Problem2096.TreeNode(3)
-//    root1.right = Problem2096.TreeNode(2)
-//    root1.right?.left = Problem2096.TreeNode(6)
-//    root1.right?.right = Problem2096.TreeNode(4)
-//    println(prob.getDirections(root1, 3, 6)) // "UURL"
-//
-//    var root2 = Problem2096.TreeNode(5)
-//    root2.left = Problem2096.TreeNode(1)
-//    root2.left?.left = Problem2096.TreeNode(3)
-//    root2.right = Problem2096.TreeNode(2)
-//    root2.right?.left = Problem2096.TreeNode(6)
-//    root2.right?.right = Problem2096.TreeNode(4)
-//    println(prob.getDirections(root2, 6, 4)) // "UR"
+    var root1 = Problem2096.TreeNode(5)
+    root1.left = Problem2096.TreeNode(1)
+    root1.left?.left = Problem2096.TreeNode(3)
+    root1.right = Problem2096.TreeNode(2)
+    root1.right?.left = Problem2096.TreeNode(6)
+    root1.right?.right = Problem2096.TreeNode(4)
+    println(prob.getDirections(root1, 3, 6)) // "UURL"
 
-//    var root3 = Problem2096.TreeNode(2)
-//    root3.left = Problem2096.TreeNode(1)
-//    println(prob.getDirections(root3, 2, 1)) // "L"
+    var root2 = Problem2096.TreeNode(5)
+    root2.left = Problem2096.TreeNode(1)
+    root2.left?.left = Problem2096.TreeNode(3)
+    root2.right = Problem2096.TreeNode(2)
+    root2.right?.left = Problem2096.TreeNode(6)
+    root2.right?.right = Problem2096.TreeNode(4)
+    println(prob.getDirections(root2, 6, 4)) // "UR"
+
+    var root3 = Problem2096.TreeNode(2)
+    root3.left = Problem2096.TreeNode(1)
+    println(prob.getDirections(root3, 2, 1)) // "L"
 
     var root4 = Problem2096.TreeNode(5)
     root4.left = Problem2096.TreeNode(1)
@@ -69,5 +68,13 @@ fun main() {
     root4.right = Problem2096.TreeNode(2)
     root4.right?.left = Problem2096.TreeNode(6)
     root4.right?.right = Problem2096.TreeNode(4)
-    println(prob.getDirections(root4, 1, 3)) // "R"
+    println(prob.getDirections(root4, 1, 3)) // "L"
+
+    var root5 = Problem2096.TreeNode(5)
+    root5.left = Problem2096.TreeNode(1)
+    root5.left?.left = Problem2096.TreeNode(3)
+    root5.right = Problem2096.TreeNode(2)
+    root5.right?.left = Problem2096.TreeNode(6)
+    root5.right?.right = Problem2096.TreeNode(4)
+    println(prob.getDirections(root5, 3, 1)) // "U"
 }
