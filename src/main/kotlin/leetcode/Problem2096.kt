132210892062ca6b4d368c746fb1e210ca2c01d3
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-02-16 23:19:01 -0600

    Update problem 2096

--- src/main/kotlin/leetcode/Problem2096.kt
+++ src/main/kotlin/leetcode/Problem2096.kt
@@ -13,3 +13,18 @@ class Problem2096 {
         TODO()
     }
 }
+
+fun main() {
+    val prob = Problem2096()
+    var root = Problem2096.TreeNode(5)
+    root.left = Problem2096.TreeNode(1)
+    root.left?.left = Problem2096.TreeNode(3)
+    root.right = Problem2096.TreeNode(2)
+    root.right?.left = Problem2096.TreeNode(6)
+    root.right?.right = Problem2096.TreeNode(4)
+    println(prob.getDirections(root, 3, 6))
+
+    root = Problem2096.TreeNode(2)
+    root.left = Problem2096.TreeNode(1)
+    println(prob.getDirections(root, 2, 1))
+}
