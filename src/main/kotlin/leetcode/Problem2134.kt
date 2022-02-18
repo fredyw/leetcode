99b56adab0b0f64a292c35c0fdf3b7e7bd292829
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-02-17 22:47:52 -0600

    Skeleton for problem 2134

---
+++ src/main/kotlin/leetcode/Problem2134.kt
@@ -0,0 +1,18 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/minimum-swaps-to-group-all-1s-together-ii/
+ */
+class Problem2134 {
+    fun minSwaps(nums: IntArray): Int {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2134()
+    println(prob.minSwaps(intArrayOf(0,1,0,1,1,0,0))) // 1
+    println(prob.minSwaps(intArrayOf(0,1,1,1,0,0,1,1,0))) // 2
+    println(prob.minSwaps(intArrayOf(1,1,0,0,1))) // 0
+
+}
