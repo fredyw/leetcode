bcf063f9933f5db44da249f16b54cb4f9011f100
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-02-20 13:29:08 -0600

    Skeleton for problem 2180

---
+++ src/main/kotlin/leetcode/Problem2180.kt
@@ -0,0 +1,16 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/count-integers-with-even-digit-sum/
+ */
+class Problem2180 {
+    fun countEven(num: Int): Int {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2180()
+    println(prob.countEven(4)) // 2
+    println(prob.countEven(30)) // 14
+}
