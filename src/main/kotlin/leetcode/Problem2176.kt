b7b789901e6098ea2ec65fa427f1c71dd6c64994
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-02-19 21:30:07 -0600

    Skeleton for problem 2176

---
+++ src/main/kotlin/leetcode/Problem2176.kt
@@ -0,0 +1,16 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/count-equal-and-divisible-pairs-in-an-array/
+ */
+class Problem2176 {
+    fun countPairs(nums: IntArray, k: Int): Int {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2176()
+    println(prob.countPairs(intArrayOf(3,1,2,2,2,1,3), 2)) // 4
+    println(prob.countPairs(intArrayOf(1,2,3,4), 1)) // 0
+}
