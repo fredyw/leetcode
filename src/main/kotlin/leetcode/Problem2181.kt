cba29d4fe1ad8c6297ab043a0279f86fad5018b2
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-02-22 14:10:43 -0600

    Skeleton for problem 2181

---
+++ src/main/kotlin/leetcode/Problem2181.kt
@@ -0,0 +1,18 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/merge-nodes-in-between-zeros/
+ */
+class Problem2181 {
+    class ListNode(var `val`: Int) {
+        var next: ListNode? = null
+    }
+
+    fun mergeNodes(head: ListNode?): ListNode? {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2181()
+}
