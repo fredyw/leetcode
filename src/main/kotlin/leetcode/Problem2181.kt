d4f5a147e3a585132bd6268e3789515b228827c6
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-02-22 14:49:32 -0600

    Solve problem 2181

--- src/main/kotlin/leetcode/Problem2181.kt
+++ src/main/kotlin/leetcode/Problem2181.kt
@@ -9,10 +9,27 @@ class Problem2181 {
     }
 
     fun mergeNodes(head: ListNode?): ListNode? {
-        TODO()
+        var node: ListNode? = head
+        var newHead: ListNode? = null
+        var newNode: ListNode? = newHead
+        var sum = 0
+        while (node != null) {
+            if (node.`val` == 0) {
+                if (sum > 0) {
+                    if (newNode == null) {
+                        newHead = ListNode(sum)
+                        newNode = newHead
+                    } else {
+                        newNode.next = ListNode(sum)
+                        newNode = newNode.next
+                    }
+                }
+                sum = 0
+            } else {
+                sum += node.`val`
+            }
+            node = node.next
+        }
+        return newHead
     }
-}
-
-fun main() {
-    val prob = Problem2181()
 }
