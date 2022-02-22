406bd9b6b764e938f97e568560687fc8f9f93db6
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-02-22 17:23:03 -0600

    Skeleton for problem 2178

---
+++ src/main/kotlin/leetcode/Problem2178.kt
@@ -0,0 +1,17 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/maximum-split-of-positive-even-integers/
+ */
+class Problem2178 {
+    fun maximumEvenSplit(finalSum: Long): List<Long> {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2178()
+    println(prob.maximumEvenSplit(12)) // [2,4,6]
+    println(prob.maximumEvenSplit(7)) // []
+    println(prob.maximumEvenSplit(28)) // [6,8,2,12]
+}
