1800bbd590656e0f9c3efd674a7af50132996ac1
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-02-22 19:24:58 -0600

    Skeleton for problem 2177

---
+++ src/main/kotlin/leetcode/Problem2177.kt
@@ -0,0 +1,16 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/find-three-consecutive-integers-that-sum-to-a-given-number/
+ */
+class Problem2177 {
+    fun sumOfThree(num: Long): LongArray {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2177()
+    println(prob.sumOfThree(33)) // [10,11,12]
+    println(prob.sumOfThree(4)) // []
+}
