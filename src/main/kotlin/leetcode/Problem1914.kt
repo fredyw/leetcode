bc8be6f0d92e084605b583675994ccb3014e0164
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-02-24 00:41:15 -0600

    Skeleton for problem 1914

---
+++ src/main/kotlin/leetcode/Problem1914.kt
@@ -0,0 +1,14 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/cyclically-rotating-a-grid/
+ */
+class Problem1914 {
+    fun rotateGrid(grid: Array<IntArray>, k: Int): Array<IntArray> {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem1914()
+}
