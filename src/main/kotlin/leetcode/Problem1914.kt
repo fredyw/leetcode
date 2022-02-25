24a221b7bdd885249edddef8ce48dd1916a77b2a
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-02-24 22:58:48 -0600

    Update problem 1914

--- src/main/kotlin/leetcode/Problem1914.kt
+++ src/main/kotlin/leetcode/Problem1914.kt
@@ -12,12 +12,12 @@ class Problem1914 {
 
 fun main() {
     val prob = Problem1914()
-    println(arrayOf(
+    println(prob.rotateGrid(arrayOf(
         intArrayOf(40,10),
-        intArrayOf(30,20), 1).contentDeepToString()) // [[10,20],[40,30]]
-    println(arrayOf(
+        intArrayOf(30,20)), 1).contentDeepToString()) // [[10,20],[40,30]]
+    println(prob.rotateGrid(arrayOf(
         intArrayOf(1,2,3,4),
         intArrayOf(5,6,7,8),
         intArrayOf(9,10,11,12),
-        intArrayOf(13,14,15,16), 1).contentDeepToString()) // [[3,4,8,12],[2,11,10,16],[1,7,6,15],[5,9,13,14]]
+        intArrayOf(13,14,15,16)), 1).contentDeepToString()) // [[3,4,8,12],[2,11,10,16],[1,7,6,15],[5,9,13,14]]
 }
