66004550d0bb50594208cd340893231b19aeba85
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-02-24 22:46:08 -0600

    Update problem 1914

--- src/main/kotlin/leetcode/Problem1914.kt
+++ src/main/kotlin/leetcode/Problem1914.kt
@@ -9,6 +9,15 @@ class Problem1914 {
     }
 }
 
+
 fun main() {
     val prob = Problem1914()
+    println(arrayOf(
+        intArrayOf(40,10),
+        intArrayOf(30,20), 1).contentDeepToString()) // [[10,20],[40,30]]
+    println(arrayOf(
+        intArrayOf(1,2,3,4),
+        intArrayOf(5,6,7,8),
+        intArrayOf(9,10,11,12),
+        intArrayOf(13,14,15,16), 1).contentDeepToString()) // [[3,4,8,12],[2,11,10,16],[1,7,6,15],[5,9,13,14]]
 }
