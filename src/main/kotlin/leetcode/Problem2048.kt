715e5644d76da69dba610823b161f04e76b3ef54
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-02-26 21:38:23 -0600

    Skeleton for problem 2048

---
+++ src/main/kotlin/leetcode/Problem2048.kt
@@ -0,0 +1,17 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/next-greater-numerically-balanced-number/
+ */
+class Problem2048 {
+    fun nextBeautifulNumber(n: Int): Int {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2048()
+    println(prob.nextBeautifulNumber(1)) // 22
+    println(prob.nextBeautifulNumber(1000)) // 1333
+    println(prob.nextBeautifulNumber(3000)) // 3133
+}
