46caed66f417256dcb47f4bfcb1f46e69547d333
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-02-27 09:45:00 -0600

    Update problem 2185

--- src/main/kotlin/leetcode/Problem2185.kt
+++ src/main/kotlin/leetcode/Problem2185.kt
@@ -8,9 +8,3 @@ class Problem2185 {
         return words.count { it.startsWith(pref) }
     }
 }
-
-fun main() {
-    val prob = Problem2185()
-    println(prob.prefixCount(arrayOf("pay","attention","practice","attend"), "at")) // 2
-    println(prob.prefixCount(arrayOf("leetcode","win","loops","success"), "code")) // 0
-}
