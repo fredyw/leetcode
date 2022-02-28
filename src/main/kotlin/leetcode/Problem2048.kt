69c9ee2ee0c6ac8f5522a361c3e3ad55e57e81b8
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-02-27 23:11:54 -0600

    Update problem 2048

--- src/main/kotlin/leetcode/Problem2048.kt
+++ src/main/kotlin/leetcode/Problem2048.kt
@@ -5,21 +5,24 @@ package leetcode
  */
 class Problem2048 {
     fun nextBeautifulNumber(n: Int): Int {
-        TODO()
+        val s = n.toString()
+        return 0
     }
 }
 
 fun main() {
     val prob = Problem2048()
-    println(prob.nextBeautifulNumber(1)) // 22
-    println(prob.nextBeautifulNumber(1000)) // 1333
-    println(prob.nextBeautifulNumber(3000)) // 3133
-    println(prob.nextBeautifulNumber(22)) // 122
-    println(prob.nextBeautifulNumber(20)) // 22
-    println(prob.nextBeautifulNumber(30)) // 122
-    println(prob.nextBeautifulNumber(4278)) // 4444
-    println(prob.nextBeautifulNumber(4578)) // 14444
-    println(prob.nextBeautifulNumber(3200)) // 3313
-    println(prob.nextBeautifulNumber(3330)) // 3331
-    println(prob.nextBeautifulNumber(3430)) // 4444
+//    println(prob.nextBeautifulNumber(1)) // 22
+//    println(prob.nextBeautifulNumber(1000)) // 1333
+//    println(prob.nextBeautifulNumber(3000)) // 3133
+//    println(prob.nextBeautifulNumber(22)) // 122
+//    println(prob.nextBeautifulNumber(20)) // 22
+//    println(prob.nextBeautifulNumber(30)) // 122
+//    println(prob.nextBeautifulNumber(4278)) // 4444
+//    println(prob.nextBeautifulNumber(4578)) // 14444
+//    println(prob.nextBeautifulNumber(3200)) // 3313
+//    println(prob.nextBeautifulNumber(3330)) // 3331
+//    println(prob.nextBeautifulNumber(3430)) // 4444
+//    println(prob.nextBeautifulNumber(412897)) // 422444
+    println(prob.nextBeautifulNumber(41289)) // 41444
 }
