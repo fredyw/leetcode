a14751443a8683fc377d8cf6dead9bb82df44ea6
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-02-28 22:40:59 -0600

    Solve problem 2048

--- src/main/kotlin/leetcode/Problem2048.kt
+++ src/main/kotlin/leetcode/Problem2048.kt
@@ -1,28 +1,42 @@
 package leetcode
 
+import kotlin.math.min
+
 /**
  * https://leetcode.com/problems/next-greater-numerically-balanced-number/
  */
 class Problem2048 {
     fun nextBeautifulNumber(n: Int): Int {
+        var answer = Answer()
         val s = n.toString()
-        return 0
+        val numbers = arrayOf("1", "22", "122", "333", "1333", "4444", "14444", "22333",
+            "55555", "122333", "155555", "224444", "666666")
+        for (num in numbers) {
+            if (s.length > num.length) {
+                continue
+            }
+            permutations(num.toCharArray(), n = n, answer = answer)
+        }
+        return answer.value
     }
-}
 
-fun main() {
-    val prob = Problem2048()
-//    println(prob.nextBeautifulNumber(1)) // 22
-//    println(prob.nextBeautifulNumber(1000)) // 1333
-//    println(prob.nextBeautifulNumber(3000)) // 3133
-//    println(prob.nextBeautifulNumber(22)) // 122
-//    println(prob.nextBeautifulNumber(20)) // 22
-//    println(prob.nextBeautifulNumber(30)) // 122
-//    println(prob.nextBeautifulNumber(4278)) // 4444
-//    println(prob.nextBeautifulNumber(4578)) // 14444
-//    println(prob.nextBeautifulNumber(3200)) // 3313
-//    println(prob.nextBeautifulNumber(3330)) // 3331
-//    println(prob.nextBeautifulNumber(3430)) // 4444
-//    println(prob.nextBeautifulNumber(412897)) // 422444
-    println(prob.nextBeautifulNumber(41289)) // 41444
+    private data class Answer(var value: Int = 1224444 /* maximum possible */)
+
+    private fun permutations(chars: CharArray, index: Int = 0, n: Int, answer: Answer) {
+        if (index == chars.size) {
+            val next = String(chars).toInt()
+            if (next > n) {
+                answer.value = min(answer.value, next)
+            }
+        }
+        for (i in index until chars.size) {
+            val tmp1 = chars[i]
+            val tmp2 = chars[index]
+            chars[i] = tmp2
+            chars[index] = tmp1
+            permutations(chars, index + 1, n, answer)
+            chars[i] = tmp1
+            chars[index] = tmp2
+        }
+    }
 }
