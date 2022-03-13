1ffecd1b41a0fa40a311ca7e42cb7cf3c340be40
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-03-12 23:05:06 -0600

    Solve problem 2200

--- src/main/kotlin/leetcode/Problem2200.kt
+++ src/main/kotlin/leetcode/Problem2200.kt
@@ -5,12 +5,16 @@ package leetcode
  */
 class Problem2200 {
     fun findKDistantIndices(nums: IntArray, key: Int, k: Int): List<Int> {
-        TODO()
+        val answer = mutableSetOf<Int>()
+        for (i in nums.indices) {
+            if (nums[i] == key) {
+                var j = if (i - k < 0) 0 else i - k
+                while (j < nums.size && j <= i + k) {
+                    answer += j
+                    j++
+                }
+            }
+        }
+        return answer.toList()
     }
-}
-
-fun main() {
-    val prob = Problem2200()
-    println(prob.findKDistantIndices(intArrayOf(3,4,9,1,3,9,5), 9, 1)) // [1,2,3,4,5,6]
-    println(prob.findKDistantIndices(intArrayOf(2,2,2,2,2), 2, 2)) // [0,1,2,3,4]
 }
