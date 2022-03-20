7de3ec9c5ebc38bcd95b2032410b9b472cc72916
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-03-19 20:48:49 -0500

    Solve problem 2186

--- src/main/kotlin/leetcode/Problem2186.kt
+++ src/main/kotlin/leetcode/Problem2186.kt
@@ -1,16 +1,24 @@
 package leetcode
 
+import kotlin.math.abs
+
 /**
  * https://leetcode.com/problems/minimum-number-of-steps-to-make-two-strings-anagram-ii/
  */
 class Problem2186 {
     fun minSteps(s: String, t: String): Int {
-        TODO()
+        val sCounts = IntArray(26)
+        for (c in s) {
+            sCounts[c - 'a']++
+        }
+        var tCounts = IntArray(26)
+        for (c in t) {
+            tCounts[c - 'a']++
+        }
+        var answer = 0
+        for (i in sCounts.indices) {
+            answer += abs(sCounts[i] - tCounts[i])
+        }
+        return answer
     }
-}
-
-fun main() {
-    val prob = Problem2186()
-    println(prob.minSteps("leetcode", "coats")) // 7
-    println(prob.minSteps("night", "thing")) // 0
 }
