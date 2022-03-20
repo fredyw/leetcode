5116087a5b45bae4132930b6b451b8fe3b94a8f7
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-03-19 20:49:57 -0500

    Skeleton for problem 2196

---
+++ src/main/kotlin/leetcode/Problem2196.kt
@@ -0,0 +1,18 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/create-binary-tree-from-descriptions/
+ */
+class Problem2196 {
+    class TreeNode(var `val`: Int) {
+        var left: TreeNode? = null
+        var right: TreeNode? = null
+    }
+
+    fun createBinaryTree(descriptions: Array<IntArray>): TreeNode? {
+        TODO()
+    }
+}
+
+fun main() {
+}
