707cbe0c3b7aa562a3e514fc585f2861f9313806
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-03-19 20:41:32 -0500

    Skeleton for problem 2186

---
+++ src/main/kotlin/leetcode/Problem2186.kt
@@ -0,0 +1,16 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/minimum-number-of-steps-to-make-two-strings-anagram-ii/
+ */
+class Problem2186 {
+    fun minSteps(s: String, t: String): Int {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2186()
+    println(prob.minSteps("leetcode", "coats")) // 7
+    println(prob.minSteps("night", "thing")) // 0
+}
