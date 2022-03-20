d687b1529c320b41e3dfa755d9bf82136a396e6d
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-03-19 23:12:43 -0500

    Solve problem 2196

--- src/main/kotlin/leetcode/Problem2196.kt
+++ src/main/kotlin/leetcode/Problem2196.kt
@@ -10,9 +10,30 @@ class Problem2196 {
     }
 
     fun createBinaryTree(descriptions: Array<IntArray>): TreeNode? {
-        TODO()
+        val map = mutableMapOf<Int, TreeNode>()
+        val parents = mutableSetOf<Int>()
+        val children = mutableSetOf<Int>()
+        for (desc in descriptions) {
+            val (parent, child) = desc
+            parents += parent
+            children += child
+            if (map[parent] == null) {
+                map[parent] = TreeNode(parent)
+            }
+            if (map[child] == null) {
+                map[child] = TreeNode(child)
+            }
+        }
+        for (desc in descriptions) {
+            val (parent, child, isLeft) = desc
+            val childNode = map[child]
+            if (isLeft == 1) {
+                map[parent]?.left = if (childNode == null) TreeNode(child) else map[child]
+            } else {
+                map[parent]?.right = if (childNode == null) TreeNode(child) else map[child]
+            }
+        }
+        parents.removeAll(children)
+        return map[parents.elementAtOrNull(0)]
     }
-}
-
-fun main() {
 }
