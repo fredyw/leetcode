0c219752b5c4366457cbe972cf9b948c6f8a1e8f
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-03-25 00:14:03 -0500

    Solve problem 2210

---
+++ src/main/kotlin/leetcode/Problem2210.kt
@@ -0,0 +1,33 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/count-hills-and-valleys-in-an-array/
+ */
+class Problem2210 {
+    fun countHillValley(nums: IntArray): Int {
+        val list = mutableListOf<Int>()
+        for (i in nums.indices) {
+            list += if (i == 0) {
+                nums[i]
+            } else {
+                if (nums[i - 1] == nums[i]) {
+                    continue
+                } else {
+                    nums[i]
+                }
+            }
+        }
+        println(list)
+        if (list.size < 3) {
+            return 0
+        }
+        var answer = 0
+        for (i in 1..list.size - 2) {
+            if ((list[i] < list[i - 1] && list[i] < list[i + 1]) ||
+                (list[i] > list[i - 1] && list[i] > list[i + 1])) {
+                answer++
+            }
+        }
+        return answer
+    }
+}
