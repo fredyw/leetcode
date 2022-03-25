2e6f5980ee332689c0aa7b8578efdea5490772e8
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-03-25 00:50:55 -0500

    Solve problem 2201

--- src/main/kotlin/leetcode/Problem2201.kt
+++ src/main/kotlin/leetcode/Problem2201.kt
@@ -5,20 +5,30 @@ package leetcode
  */
 class Problem2201 {
     fun digArtifacts(n: Int, artifacts: Array<IntArray>, dig: Array<IntArray>): Int {
-        TODO()
+        data class Cell(val row: Int, val col: Int)
+        val digs = mutableSetOf<Cell>()
+        for (d in dig) {
+            digs += Cell(d[0], d[1])
+        }
+        var answer = 0
+        for (artifact in artifacts) {
+            val r1 = artifact[0]
+            val c1 = artifact[1]
+            val r2 = artifact[2]
+            val c2 = artifact[3]
+            var found = true
+            outer@ for (r in r1..r2) {
+                for (c in c1..c2) {
+                    if (Cell(r, c) !in digs) {
+                        found = false
+                        break@outer
+                    }
+                }
+            }
+            if (found) {
+                answer++
+            }
+        }
+        return answer
     }
-}
-
-fun main() {
-    val prob = Problem2201()
-    println(prob.digArtifacts(
-        2,
-        arrayOf(intArrayOf(0,0,0,0), intArrayOf(0,1,1,1)),
-        arrayOf(intArrayOf(0,0), intArrayOf(0,1))
-    )) // 1
-    println(prob.digArtifacts(
-        2,
-        arrayOf(intArrayOf(0,0,0,0), intArrayOf(0,1,1,1)),
-        arrayOf(intArrayOf(0,0), intArrayOf(0,1), intArrayOf(1,1))
-    )) // 2
 }
