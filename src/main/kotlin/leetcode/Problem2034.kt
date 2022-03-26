c040fb68d35002ddc5a68bfc036247b02c710f6f
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-03-25 23:07:35 -0500

    Solve problem 2034

--- src/main/kotlin/leetcode/Problem2034.kt
+++ src/main/kotlin/leetcode/Problem2034.kt
@@ -1,38 +1,39 @@
 package leetcode
 
+import java.util.TreeMap
+
 /**
  * https://leetcode.com/problems/stock-price-fluctuation/
  */
 class Problem2034 {
     class StockPrice() {
+        private val timestampToPriceMap = TreeMap<Int, Int>()
+        private val priceToCountMap = TreeMap<Int, Int>()
+
         fun update(timestamp: Int, price: Int) {
-            TODO()
+            val existingPrice = timestampToPriceMap[timestamp]
+            if (existingPrice != null) {
+                val newCount = (priceToCountMap[existingPrice] ?: 0) - 1
+                if (newCount > 0) {
+                    priceToCountMap[existingPrice] = newCount
+                } else if (newCount == 0) {
+                    priceToCountMap -= existingPrice
+                }
+            }
+            priceToCountMap[price] = (priceToCountMap[price] ?: 0) + 1
+            timestampToPriceMap[timestamp] = price
         }
 
         fun current(): Int {
-            TODO()
+            return timestampToPriceMap[timestampToPriceMap.lastKey()]!!
         }
 
         fun maximum(): Int {
-            TODO()
+            return priceToCountMap.lastKey()
         }
 
         fun minimum(): Int {
-            TODO()
+            return priceToCountMap.firstKey()
         }
     }
-}
-
-fun main() {
-    val stockPrice = Problem2034.StockPrice()
-    stockPrice.update(1, 10) // Timestamps are [1] with corresponding prices [10].
-    stockPrice.update(2, 5) // Timestamps are [1,2] with corresponding prices [10,5].
-    println(stockPrice.current()) // return 5, the latest timestamp is 2 with the price being 5.
-    println(stockPrice.maximum()) // return 10, the maximum price is 10 at timestamp 1.
-    println(stockPrice.update(1, 3)) // The previous timestamp 1 had the wrong price, so it is updated to 3.
-    // Timestamps are [1,2] with corresponding prices [3,5].
-    // Timestamps are [1,2] with corresponding prices [3,5].
-    println(stockPrice.maximum()) // return 5, the maximum price is 5 after the correction.
-    stockPrice.update(4, 2) // Timestamps are [1,2,4] with corresponding prices [3,5,2].
-    println(stockPrice.minimum()) // return 2, the minimum price is 2 at timestamp 4.
 }
