b0b16611cb9eb2324ac2f7d700beda041436f3a8
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-03-26 20:30:48 -0500

    Skeleton for problem 2049

---
+++ src/main/kotlin/leetcode/Problem2049.kt
@@ -0,0 +1,16 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/count-nodes-with-the-highest-score/
+ */
+class Problem2049 {
+    fun countHighestScoreNodes(parents: IntArray): Int {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2049()
+    println(prob.countHighestScoreNodes(intArrayOf(-1,2,0,2,0))) // 3
+    println(prob.countHighestScoreNodes(intArrayOf(-1,2,0))) // 2
+}
