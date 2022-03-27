c53d33b7b0afa172d13d4005995429207cf0193a
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-03-27 11:03:07 -0500

    Solve problem 1795

---
+++ src/main/sql/Problem1795.sql
@@ -0,0 +1,8 @@
+-- https://leetcode.com/problems/rearrange-products-table/
+select p.product_id, p.store, p.price
+from (select product_id, 'store1' as store, store1 as price from products
+      union
+      select product_id, 'store2' as store, store2 as price from products
+      union
+      select product_id, 'store3' as store, store3 as price from products) as p
+where p.price is not null;
