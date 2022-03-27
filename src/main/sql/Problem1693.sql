4b09decac6f515a8dc37773fea4506bc41ae2da9
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-03-26 22:09:02 -0500

    Solve problem 1693

---
+++ src/main/sql/Problem1693.sql
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/daily-leads-and-partners/
+select date_id,
+       make_name,
+       count(distinct lead_id) as unique_leads,
+       count(distinct partner_id) as unique_partners
+from dailysales
+group by date_id, make_name;
