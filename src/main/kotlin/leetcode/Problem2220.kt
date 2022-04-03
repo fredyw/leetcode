4415f2db314329dccc7c70ac638ebf623d002048
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-04-02 23:13:46 -0500

    Solve problem 2220

--- src/main/kotlin/leetcode/Problem2220.kt
+++ src/main/kotlin/leetcode/Problem2220.kt
@@ -5,12 +5,19 @@ package leetcode
  */
 class Problem2220 {
     fun minBitFlips(start: Int, goal: Int): Int {
-        TODO()
+        var answer = 0
+        var s = start.toString(2)
+        var g = goal.toString(2)
+        if (s.length > g.length) {
+            g = g.padStart(s.length, '0')
+        } else if (s.length < g.length) {
+            s = s.padStart(g.length, '0')
+        }
+        for (i in (s.length - 1)downTo 0) {
+            if (s[i] != g[i]) {
+                answer++
+            }
+        }
+        return answer
     }
-}
-
-fun main() {
-    val prob = Problem2220()
-    println(prob.minBitFlips(10, 7)) // 3
-    println(prob.minBitFlips(3, 4)) // 3
 }
