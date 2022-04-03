5376d427edabebafb00888a250c4b1214463bee5
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-04-02 23:01:31 -0500

    Skeleton for problem 2220

---
+++ src/main/kotlin/leetcode/Problem2220.kt
@@ -0,0 +1,16 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/minimum-bit-flips-to-convert-number/
+ */
+class Problem2220 {
+    fun minBitFlips(start: Int, goal: Int): Int {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2220()
+    println(prob.minBitFlips(10, 7)) // 3
+    println(prob.minBitFlips(3, 4)) // 3
+}
