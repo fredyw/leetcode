b489e051361a58884baa1eea34992ae4d0dea588
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-04-02 22:58:46 -0500

    Update problem 2212

--- src/main/kotlin/leetcode/Problem2212.kt
+++ src/main/kotlin/leetcode/Problem2212.kt
@@ -14,14 +14,12 @@ class Problem2212 {
 
     private fun maximumBobPoints(numArrows: Int, aliceArrows: IntArray,
                                  bobArrows: IntArray, index: Int, sum: Int, answer: Answer) {
-        if (numArrows == 0) {
+        if (index == aliceArrows.size) {
             if (sum > answer.max) {
                 answer.max = sum
                 answer.bobArrows = bobArrows.copyOf()
+                answer.bobArrows[0] = numArrows
             }
-            return
-        }
-        if (index == aliceArrows.size) {
             return
         }
         maximumBobPoints(numArrows, aliceArrows, bobArrows, index + 1, sum, answer)
@@ -36,7 +34,7 @@ class Problem2212 {
 
 fun main() {
     val prob = Problem2212()
-//    println(prob.maximumBobPoints(9, intArrayOf(1,1,0,1,0,0,2,1,0,1,2,0)).contentToString()) // [0,0,0,0,1,1,0,0,1,2,3,1]
-//    println(prob.maximumBobPoints(3, intArrayOf(0,0,1,0,0,0,0,0,0,0,0,2)).contentToString()) // [0,0,0,0,0,0,0,0,1,1,1,0]
+    println(prob.maximumBobPoints(9, intArrayOf(1,1,0,1,0,0,2,1,0,1,2,0)).contentToString()) // [0,0,0,0,1,1,0,0,1,2,3,1]
+    println(prob.maximumBobPoints(3, intArrayOf(0,0,1,0,0,0,0,0,0,0,0,2)).contentToString()) // [0,0,0,0,0,0,0,0,1,1,1,0]
     println(prob.maximumBobPoints(89, intArrayOf(3,2,28,1,7,1,16,7,3,13,3,5)).contentToString()) // [21,3,0,2,8,2,17,8,4,14,4,6]
 }
