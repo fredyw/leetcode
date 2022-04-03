2408f8ba67a6e8b86bd23d2ffe021980fc793d2b
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-04-03 09:29:45 -0500

    Skeleton for problem 2221

---
+++ src/main/kotlin/leetcode/Problem2221.kt
@@ -0,0 +1,16 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/find-triangular-sum-of-an-array/
+ */
+class Problem2221 {
+    fun triangularSum(nums: IntArray): Int {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2221()
+    println(prob.triangularSum(intArrayOf(1,2,3,4,5))) // 8
+    println(prob.triangularSum(intArrayOf(5))) // 5
+}
