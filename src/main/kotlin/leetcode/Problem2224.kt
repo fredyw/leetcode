97e0907b3c250eff01db0e8dba6b6269f4addbf9
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-04-09 21:20:38 -0500

    Skeleton for problem 2224

---
+++ src/main/kotlin/leetcode/Problem2224.kt
@@ -0,0 +1,16 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/minimum-number-of-operations-to-convert-time/
+ */
+class Problem2224 {
+    fun convertTime(current: String, correct: String): Int {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2224()
+    println(prob.convertTime("02:30", "04:35")) // 3
+    println(prob.convertTime("11:00", "11:01")) // 1
+}
