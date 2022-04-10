ed637e4c897d4b57a64dc73eb490e879c1229c06
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-04-10 14:12:44 -0500

    Solve problem 2231

--- src/main/kotlin/leetcode/Problem2231.kt
+++ src/main/kotlin/leetcode/Problem2231.kt
@@ -1,16 +1,42 @@
 package leetcode
 
+import java.util.Collections
+
 /**
  * https://leetcode.com/problems/largest-number-after-digit-swaps-by-parity/
  */
 class Problem2231 {
     fun largestInteger(num: Int): Int {
-        TODO()
+        val oddIndexes = mutableListOf<Int>()
+        val evenIndexes = mutableListOf<Int>()
+        val numString = num.toString()
+        for ((i, n) in numString.withIndex()) {
+            if (n.toInt() % 2 != 0) {
+                oddIndexes += i
+            } else {
+                evenIndexes += i
+            }
+        }
+        var answer = CharArray(numString.length)
+        val sortedOddIndexes = oddIndexes.toMutableList()
+        val sortedEvenIndexes = evenIndexes.toMutableList()
+        Collections.sort(sortedOddIndexes, {a, b -> numString[b].compareTo(numString[a])})
+        Collections.sort(sortedEvenIndexes, {a, b -> numString[b].compareTo(numString[a])})
+        if (numString[0].toInt() % 2 != 0) {
+            for ((i, j) in oddIndexes.withIndex()) {
+                answer[j] = numString[sortedOddIndexes[i]]
+            }
+            for ((i, j) in evenIndexes.withIndex()) {
+                answer[j] = numString[sortedEvenIndexes[i]]
+            }
+        } else {
+            for ((i, j) in evenIndexes.withIndex()) {
+                answer[j] = numString[sortedEvenIndexes[i]]
+            }
+            for ((i, j) in oddIndexes.withIndex()) {
+                answer[j] = numString[sortedOddIndexes[i]]
+            }
+        }
+        return String(answer).toInt()
     }
-}
-
-fun main() {
-    val prob = Problem2231()
-    println(prob.largestInteger(1234)) // 3412
-    println(prob.largestInteger(65875)) // 87655
 }
