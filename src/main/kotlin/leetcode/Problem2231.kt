c2912d5d0641aebb799e3b8fdd5c3fab797bb2b4
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-04-10 11:43:54 -0500

    Skeleton for problem 2231

---
+++ src/main/kotlin/leetcode/Problem2231.kt
@@ -0,0 +1,16 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/largest-number-after-digit-swaps-by-parity/
+ */
+class Problem2231 {
+    fun largestInteger(num: Int): Int {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2231()
+    println(prob.largestInteger(1234)) // 3412
+    println(prob.largestInteger(65875)) // 87655
+}
