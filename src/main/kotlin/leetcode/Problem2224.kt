74b89d6229644de2028b3dbebd97fc5c28df021d
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-04-09 21:51:48 -0500

    Solve problem 2224

--- src/main/kotlin/leetcode/Problem2224.kt
+++ src/main/kotlin/leetcode/Problem2224.kt
@@ -5,12 +5,23 @@ package leetcode
  */
 class Problem2224 {
     fun convertTime(current: String, correct: String): Int {
-        TODO()
+        var diff = toMinutes(correct) - toMinutes(current)
+        var answer = 0
+        val minutes = mutableListOf(60, 15, 5, 1)
+        var index = 0
+        while (diff > 0) {
+            if (diff - minutes[index] < 0) {
+                index++
+                continue
+            }
+            diff -= minutes[index]
+            answer++
+        }
+        return answer
     }
-}
 
-fun main() {
-    val prob = Problem2224()
-    println(prob.convertTime("02:30", "04:35")) // 3
-    println(prob.convertTime("11:00", "11:01")) // 1
+    private fun toMinutes(time: String): Int {
+        val (h, m) = time.split(":")
+        return (h.toInt() * 60) + m.toInt()
+    }
 }
