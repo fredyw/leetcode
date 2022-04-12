f3f109532744368ec3c4d6d96b030d5d61cecd19
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-04-11 22:04:53 -0500

    Solve problem 1407

--- src/main/sql/Problem1407.sql
+++ src/main/sql/Problem1407.sql
@@ -1,1 +1,5 @@
 -- https://leetcode.com/problems/top-travellers/
+select u.name, ifnull(sum(r.distance), 0) as travelled_distance
+from users u left join rides r on u.id = r.user_id
+group by u.name
+order by travelled_distance desc, u.name asc;
