4c63b8e30520930d3b8bbeb79df1c7e92a13fec7
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-04-11 21:42:10 -0500

    Solve problem 2235

---
+++ src/main/kotlin/leetcode/Problem2235.kt
@@ -0,0 +1,10 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/add-two-integers/
+ */
+class Problem2235 {
+    fun sum(num1: Int, num2: Int): Int {
+        return num1 + num2
+    }
+}
