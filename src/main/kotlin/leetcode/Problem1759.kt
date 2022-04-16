f7dc84cb684eed190f2a66a7630cb6e70b1a7587
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-04-16 14:38:30 -0500

    Skeleton for problem 1759

---
+++ src/main/kotlin/leetcode/Problem1759.kt
@@ -0,0 +1,17 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/count-number-of-homogenous-substrings/
+ */
+class Problem1759 {
+    fun countHomogenous(s: String): Int {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem1759()
+    println(prob.countHomogenous("abbcccaa")) // 13
+    println(prob.countHomogenous("xy")) // 2
+    println(prob.countHomogenous("zzzzz")) // 15
+}
