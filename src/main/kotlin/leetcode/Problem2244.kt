ed0e32c60f8a2a50ec28d5643a601de0ba7844ae
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-04-22 21:27:33 -0500

    Skeleton for problem 2244

---
+++ src/main/kotlin/leetcode/Problem2244.kt
@@ -0,0 +1,16 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/minimum-rounds-to-complete-all-tasks/
+ */
+class Problem2244 {
+    fun minimumRounds(tasks: IntArray): Int {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2244()
+    println(prob.minimumRounds(intArrayOf(2,2,3,3,2,4,4,4,4,4))) // 4
+    println(prob.minimumRounds(intArrayOf(2,3,3))) // -1
+}
