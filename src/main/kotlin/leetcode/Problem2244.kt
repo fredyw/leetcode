b0327d7901d93db70c2ded3b8e68a9ef80a791f9
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-04-24 15:32:38 -0500

    Solve problem 2244

--- src/main/kotlin/leetcode/Problem2244.kt
+++ src/main/kotlin/leetcode/Problem2244.kt
@@ -5,13 +5,23 @@ package leetcode
  */
 class Problem2244 {
     fun minimumRounds(tasks: IntArray): Int {
-        TODO()
+        val map = mutableMapOf<Int, Int>()
+        for (task in tasks) {
+            map[task] = (map[task] ?: 0) + 1
+        }
+        var answer = 0
+        for (count in map.values) {
+            if (count < 2) {
+                return -1
+            }
+            answer += if (count % 3 == 0) {
+                count / 3
+            } else if (count % 3 == 1) {
+                (count / 3) - 1 + 2
+            } else { // count % 3 == 2
+                (count / 3) + 1
+            }
+        }
+        return answer
     }
-}
-
-fun main() {
-    val prob = Problem2244()
-    println(prob.minimumRounds(intArrayOf(2,2,3,3,2,4,4,4,4,4))) // 4
-    println(prob.minimumRounds(intArrayOf(4,4,2,3,3,2,4,4,4,2))) // 4
-    println(prob.minimumRounds(intArrayOf(2,3,3))) // -1
 }
