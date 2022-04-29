2ffbee2f049a2790bce970f2c65c011d8d49697a
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-04-29 06:46:50 -0500

    Skeleton for problem 2240

---
+++ src/main/kotlin/leetcode/Problem2240.kt
@@ -0,0 +1,16 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/number-of-ways-to-buy-pens-and-pencils/
+ */
+class Problem2240 {
+    fun waysToBuyPensPencils(total: Int, cost1: Int, cost2: Int): Long {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2240()
+    println(prob.waysToBuyPensPencils(20, 10, 5)) // 9
+    println(prob.waysToBuyPensPencils(5, 10, 10)) // 1
+}
