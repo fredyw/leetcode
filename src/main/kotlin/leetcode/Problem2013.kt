5153c0741cc48ac1375695ed2a6405a7f805c4f6
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-04-30 09:18:32 -0500

    Skeleton for problem 2013

---
+++ src/main/kotlin/leetcode/Problem2013.kt
@@ -0,0 +1,27 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/detect-squares/
+ */
+class Problem2013 {
+    class DetectSquares() {
+        fun add(point: IntArray) {
+            TODO()
+        }
+
+        fun count(point: IntArray): Int {
+            TODO()
+        }
+    }
+}
+
+fun main() {
+    val detectSquares = Problem2013.DetectSquares()
+    detectSquares.add(intArrayOf(3, 10))
+    detectSquares.add(intArrayOf(11, 2))
+    detectSquares.add(intArrayOf(3, 2))
+    println(detectSquares.count(intArrayOf(11, 10))) // 1
+    println(detectSquares.count(intArrayOf(14, 8)))  // 0
+    detectSquares.add(intArrayOf(11, 2))
+    println(detectSquares.count(intArrayOf(11, 10))) // 2
+}
