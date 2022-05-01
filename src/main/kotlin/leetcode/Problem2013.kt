9042895c8c4c0fa880352856a68e9cb89cd1cb83
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-05-01 13:46:58 -0500

    Update problem 2013

--- src/main/kotlin/leetcode/Problem2013.kt
+++ src/main/kotlin/leetcode/Problem2013.kt
@@ -24,4 +24,7 @@ fun main() {
     println(detectSquares.count(intArrayOf(14, 8)))  // 0
     detectSquares.add(intArrayOf(11, 2))
     println(detectSquares.count(intArrayOf(11, 10))) // 2
+    detectSquares.add(intArrayOf(3, 10))
+    detectSquares.add(intArrayOf(3, 2))
+    println(detectSquares.count(intArrayOf(11, 10))) // 8
 }
