7f9bb11e7f3b0b1530a193e5a55be4a0bc42bfef
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-05-02 23:28:59 -0500

    Skeleton for problem 2249

---
+++ src/main/kotlin/leetcode/Problem2249.kt
@@ -0,0 +1,16 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/count-lattice-points-inside-a-circle/
+ */
+class Problem2249 {
+    fun countLatticePoints(circles: Array<IntArray>): Int {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2249()
+    println(prob.countLatticePoints(arrayOf(intArrayOf(2,2,1)))) // 5
+    println(prob.countLatticePoints(arrayOf(intArrayOf(2,2,2), intArrayOf(3,4,1)))) // 16
+}
