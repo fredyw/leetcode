caecb4ec64331b4745acec6a9cba8fefb15497f6
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-05-03 14:59:10 -0500

    Solve problem 2249

--- src/main/kotlin/leetcode/Problem2249.kt
+++ src/main/kotlin/leetcode/Problem2249.kt
@@ -5,54 +5,18 @@ package leetcode
  */
 class Problem2249 {
     fun countLatticePoints(circles: Array<IntArray>): Int {
+        data class Point(val x: Int, val y: Int)
         val points = mutableSetOf<Point>()
         for (circle in circles) {
-            latticePoints(circle, points)
+            val (x, y, r) = circle
+            for (i in -r..r) {
+                for (j in -r..r) {
+                    if (i * i + j * j <= r * r) {
+                        points += Point(x + i, y + j)
+                    }
+                }
+            }
         }
         return points.size
     }
-
-    private data class Point(val x: Int, val y: Int)
-
-    private fun latticePoints(circle: IntArray, points: MutableSet<Point>) {
-        val (x, y, r) = circle
-        // Center
-        for (i in x - r..x + r) {
-            points += Point(i, y)
-        }
-        // Top
-        var k = r - 1
-        for (j in y + 1..y + r) {
-            for (i in x - k..x + k) {
-                points += Point(i, j)
-            }
-            k--
-        }
-        // Bottom
-        k = r - 1
-        for (j in y - 1 downTo y - r) {
-            for (i in x - k..x + k) {
-                points += Point(i, j)
-            }
-            k--
-        }
-    }
-}
-
-fun main() {
-    val prob = Problem2249()
-    println(prob.countLatticePoints(arrayOf(intArrayOf(2,2,1)))) // 5
-    println(prob.countLatticePoints(arrayOf(intArrayOf(2,2,2), intArrayOf(3,4,1)))) // 16
-    println(prob.countLatticePoints(arrayOf(intArrayOf(2,2,2), intArrayOf(3,5,1)))) // 18
-    println(prob.countLatticePoints(arrayOf(
-        intArrayOf(8,9,6),
-        intArrayOf(9,8,4),
-        intArrayOf(4,1,1),
-        intArrayOf(8,5,1),
-        intArrayOf(7,1,1),
-        intArrayOf(6,7,5),
-        intArrayOf(7,1,1),
-        intArrayOf(7,1,1),
-        intArrayOf(5,5,3)
-    ))) // 141
 }
