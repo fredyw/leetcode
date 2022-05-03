981dc727c2c22f7fdc188cec2ef3f0fc575b2c06
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-05-02 20:02:13 -0500

    Update problem 2192

--- src/main/kotlin/leetcode/Problem2192.kt
+++ src/main/kotlin/leetcode/Problem2192.kt
@@ -1,11 +1,38 @@
 package leetcode
 
+import java.util.*
+
 /**
  * https://leetcode.com/problems/all-ancestors-of-a-node-in-a-directed-acyclic-graph/
  */
 class Problem2192 {
     fun getAncestors(n: Int, edges: Array<IntArray>): List<List<Int>> {
-        TODO()
+        val adjList = reverse(n, edges)
+        val answer = mutableListOf<List<Int>>()
+        for (i in 0 until n) {
+            val ancestors = TreeSet<Int>()
+            getAncestors(i, adjList, ancestors)
+            answer += ancestors.toList()
+        }
+        return answer
+    }
+
+    private fun reverse(n: Int, edges: Array<IntArray>): Array<MutableList<Int>> {
+        val adjList = Array<MutableList<Int>>(n) { mutableListOf() }
+        for (edge in edges) {
+            val from = edge[0]
+            val to = edge[1]
+            adjList[to].add(from)
+        }
+        return adjList
+    }
+
+    private fun getAncestors(i: Int, adjList: Array<MutableList<Int>>,
+                             ancestors: TreeSet<Int>) {
+        for (adjacent in adjList[i]) {
+            ancestors += adjacent
+            getAncestors(adjacent, adjList, ancestors)
+        }
     }
 }
 
