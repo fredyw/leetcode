7a5de91d342aa9d2bae45fb23d76dda1b8a822b3
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-05-06 00:23:13 -0500

    Skeleton for problem 1514

---
+++ src/main/kotlin/leetcode/Problem1514.kt
@@ -0,0 +1,10 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/path-with-maximum-probability/
+ */
+class Problem1514 {
+    fun maxProbability(n: Int, edges: Array<IntArray>, succProb: DoubleArray, start: Int, end: Int): Double {
+        TODO()
+    }
+}
