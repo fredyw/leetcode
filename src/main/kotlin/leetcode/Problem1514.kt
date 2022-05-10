a4f24f442f5775fd88d1618c15a25e7570b43e5a
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-05-10 15:22:42 -0500

    Solve problem 1514

--- src/main/kotlin/leetcode/Problem1514.kt
+++ src/main/kotlin/leetcode/Problem1514.kt
@@ -1,42 +1,46 @@
 package leetcode
 
+import java.util.PriorityQueue
+
 /**
  * https://leetcode.com/problems/path-with-maximum-probability/
  */
 class Problem1514 {
-    fun maxProbability(n: Int, edges: Array<IntArray>, succProb: DoubleArray, start: Int, end: Int): Double {
-        TODO()
+    fun maxProbability(n: Int, edges: Array<IntArray>, succProb: DoubleArray, start: Int,
+                       end: Int): Double {
+        val adjList = buildAdjList(n, edges, succProb)
+        val dist = DoubleArray(n) { Double.NEGATIVE_INFINITY }
+        val queue = PriorityQueue<Node>()
+        queue += Node(start, dist[start])
+        while (queue.isNotEmpty()) {
+            val node = queue.remove()
+            for (edge in adjList[node.n]) {
+                val prob = if (dist[edge.from] == Double.NEGATIVE_INFINITY) edge.prob
+                    else dist[edge.from] * edge.prob
+                if (prob > dist[edge.to]) {
+                    dist[edge.to] = prob
+                    queue += Node(edge.to, prob)
+                }
+            }
+        }
+        return if (dist[end] == Double.NEGATIVE_INFINITY) 0.0 else dist[end]
     }
-}
 
-fun main() {
-    val prob = Problem1514()
-    println(
-        prob.maxProbability(
-            n = 3,
-            arrayOf(intArrayOf(0, 1), intArrayOf(1, 2), intArrayOf(0, 2)),
-            doubleArrayOf(0.5, 0.5, 0.2),
-            start = 0,
-            end = 2
-        )
-    ) // 0.25000
-    println(
-        prob.maxProbability(
-            n = 3,
-            arrayOf(intArrayOf(0, 1), intArrayOf(1, 2), intArrayOf(0, 2)),
-            doubleArrayOf(0.5, 0.5, 0.3),
-            start = 0,
-            end = 2
-        )
-    ) // 0.30000
-    println(
-        prob.maxProbability(
-            n = 3,
-            arrayOf(intArrayOf(0, 1)),
-            doubleArrayOf(0.5),
-            start = 0,
-            end = 2
-        )
-    ) // 0.00000
+    private fun buildAdjList(n: Int, edges: Array<IntArray>, succProb: DoubleArray): Array<MutableList<Edge>> {
+        val adjList = Array(n) { mutableListOf<Edge>() }
+        for ((i, e) in edges.withIndex()) {
+            val (from, to) = e
+            adjList[from].add(Edge(from, to, succProb[i]))
+            adjList[to].add(Edge(to, from, succProb[i]))
+        }
+        return adjList
+    }
 
+    private data class Node(val n: Int, val prob: Double) : Comparable<Node> {
+        override fun compareTo(other: Node): Int {
+            return other.prob.compareTo(prob)
+        }
+    }
+
+    private data class Edge(val from: Int, val to: Int, val prob: Double)
 }
