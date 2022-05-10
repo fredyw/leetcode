8a69830a9df841ed04567dd9baf8dbc23555730f
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-05-10 14:20:51 -0500

    Update problem 1514

--- src/main/kotlin/leetcode/Problem1514.kt
+++ src/main/kotlin/leetcode/Problem1514.kt
@@ -8,3 +8,35 @@ class Problem1514 {
         TODO()
     }
 }
+
+fun main() {
+    val prob = Problem1514()
+    println(
+        prob.maxProbability(
+            n = 3,
+            arrayOf(intArrayOf(0, 1), intArrayOf(1, 2), intArrayOf(0, 2)),
+            doubleArrayOf(0.5, 0.5, 0.2),
+            start = 0,
+            end = 2
+        )
+    ) // 0.25000
+    println(
+        prob.maxProbability(
+            n = 3,
+            arrayOf(intArrayOf(0, 1), intArrayOf(1, 2), intArrayOf(0, 2)),
+            doubleArrayOf(0.5, 0.5, 0.3),
+            start = 0,
+            end = 2
+        )
+    ) // 0.30000
+    println(
+        prob.maxProbability(
+            n = 3,
+            arrayOf(intArrayOf(0, 1)),
+            doubleArrayOf(0.5),
+            start = 0,
+            end = 2
+        )
+    ) // 0.00000
+
+}
