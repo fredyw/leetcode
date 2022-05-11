7a18559438b377a5f411d2891b7a990f4bd8573d
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-05-11 01:30:18 -0500

    Update problem 2266

--- src/main/kotlin/leetcode/Problem2266.kt
+++ src/main/kotlin/leetcode/Problem2266.kt
@@ -12,5 +12,10 @@ class Problem2266 {
 fun main() {
     val prob = Problem2266()
     println(prob.countTexts("22233")) // 8
+    println(prob.countTexts("222")) // 4
+    println(prob.countTexts("2222")) // 7
+    println(prob.countTexts("22222")) // 13
+    println(prob.countTexts("222222")) // 24
+    println(prob.countTexts("2222222")) // 44
     println(prob.countTexts("222222222222222222222222222222222222")) // 82876089
 }
