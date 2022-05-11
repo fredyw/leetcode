8f9b2c925a672c1ae9da799731a11fec371621f0
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-05-11 00:52:38 -0500

    Solve problem 2265

--- src/main/kotlin/leetcode/Problem2265.kt
+++ src/main/kotlin/leetcode/Problem2265.kt
@@ -10,6 +10,25 @@ class Problem2265 {
     }
 
     fun averageOfSubtree(root: TreeNode?): Int {
-        TODO()
+        val answer = IntRef()
+        averageOfSubtree(root, answer)
+        return answer.value
+    }
+
+    private data class SumCount(val sum: Int = 0, val count: Int = 0)
+    private data class IntRef(var value: Int = 0)
+
+    private fun averageOfSubtree(root: TreeNode?, answer: IntRef): SumCount {
+        if (root == null) {
+            return SumCount()
+        }
+        val left = averageOfSubtree(root.left, answer)
+        val right = averageOfSubtree(root.right, answer)
+        val newSum = (left.sum + right.sum + root.`val`)
+        val newCount = left.count + right.count + 1
+        if (newSum / newCount == root.`val`) {
+            answer.value++
+        }
+        return SumCount(newSum, newCount)
     }
 }
