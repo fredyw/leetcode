0a24b816f23731d5956eaa1cf5c1e99aefe6f0b0
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-05-11 00:54:10 -0500

    Skeleton for problem 2266

---
+++ src/main/kotlin/leetcode/Problem2266.kt
@@ -0,0 +1,16 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/count-number-of-texts/
+ */
+class Problem2266 {
+    fun countTexts(pressedKeys: String): Int {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2266()
+    println(prob.countTexts("22233")) // 8
+    println(prob.countTexts("222222222222222222222222222222222222")) // 82876089
+}
