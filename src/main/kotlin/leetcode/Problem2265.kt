06425e0f684486a33bf767502904306b21b120d0
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-05-11 00:24:27 -0500

    Skeleton for problem 2265

---
+++ src/main/kotlin/leetcode/Problem2265.kt
@@ -0,0 +1,15 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/count-nodes-equal-to-average-of-subtree/
+ */
+class Problem2265 {
+    class TreeNode(var `val`: Int) {
+        var left: TreeNode? = null
+        var right: TreeNode? = null
+    }
+
+    fun averageOfSubtree(root: TreeNode?): Int {
+        TODO()
+    }
+}
