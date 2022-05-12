e6b45d26a74ead710d3c0211a8d049b30f91c44d
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-05-11 19:00:19 -0500

    Solve problem 2266

--- src/main/kotlin/leetcode/Problem2266.kt
+++ src/main/kotlin/leetcode/Problem2266.kt
@@ -5,17 +5,56 @@ package leetcode
  */
 class Problem2266 {
     fun countTexts(pressedKeys: String): Int {
-        TODO()
+        if (pressedKeys.length == 1) {
+            return 1
+        }
+        var answer = 1L
+        var i = 0
+        var length = 1
+        val memo3 = IntArray(pressedKeys.length + 1) { -1 }
+        val memo4 = IntArray(pressedKeys.length + 1) { -1 }
+        while (i < pressedKeys.length - 1) {
+            if (pressedKeys[i] != pressedKeys[i + 1]) {
+                val numDigits = numDigits(pressedKeys[i])
+                answer = (answer * countTexts(
+                    numDigits,
+                    length,
+                    if (numDigits == 3) memo3 else memo4
+                )) % 1_000_000_007
+                length = 1
+            } else {
+                length++
+            }
+            i++
+        }
+        val numDigits = numDigits(pressedKeys[i])
+        answer = (answer * countTexts(
+            numDigits,
+            length,
+            if (numDigits == 3) memo3 else memo4
+        )) % 1_000_000_007
+        return answer.toInt()
     }
-}
 
-fun main() {
-    val prob = Problem2266()
-    println(prob.countTexts("22233")) // 8
-    println(prob.countTexts("222")) // 4
-    println(prob.countTexts("2222")) // 7
-    println(prob.countTexts("22222")) // 13
-    println(prob.countTexts("222222")) // 24
-    println(prob.countTexts("2222222")) // 44
-    println(prob.countTexts("222222222222222222222222222222222222")) // 82876089
+    private fun numDigits(key: Char): Int {
+        return if (key == '7' || key == '9') 4 else 3
+    }
+
+    private fun countTexts(numDigits: Int, length: Int, memo: IntArray): Int {
+        if (length < 0) {
+            return 0
+        }
+        if (length == 0) {
+            return 1
+        }
+        if (memo[length] != -1) {
+            return memo[length]
+        }
+        var count = 0
+        for (digit in 1..numDigits) {
+            count = (count + countTexts(numDigits, length - digit, memo)) % 1_000_000_007
+        }
+        memo[length] = count
+        return count
+    }
 }
