cf9059ca990ee754731eb1147f2c2caeeaef921c
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-05-13 16:24:34 -0500

    Solve problem 1438

--- src/main/kotlin/leetcode/Problem1438.kt
+++ src/main/kotlin/leetcode/Problem1438.kt
@@ -1,17 +1,37 @@
 package leetcode
 
+import java.util.*
+import kotlin.math.max
+
 /**
  * https://leetcode.com/problems/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/
  */
 class Problem1438 {
     fun longestSubarray(nums: IntArray, limit: Int): Int {
-        TODO()
+        var answer = 1
+        val minDeque = LinkedList<Int>()
+        val maxDeque = LinkedList<Int>()
+        var left = 0
+        for (right in nums.indices) {
+            while (minDeque.isNotEmpty() && nums[right] < minDeque.peekLast()) {
+                minDeque.pollLast()
+            }
+            minDeque.addLast(nums[right])
+            while (maxDeque.isNotEmpty() && nums[right] > maxDeque.peekLast()) {
+                maxDeque.pollLast()
+            }
+            maxDeque.addLast(nums[right])
+            while (maxDeque.peekFirst() - minDeque.peekFirst() > limit) {
+                if (minDeque.peekFirst() == nums[left]) {
+                    minDeque.pollFirst()
+                }
+                if (maxDeque.peekFirst() == nums[left]) {
+                    maxDeque.pollFirst()
+                }
+                left++
+            }
+            answer = max(answer, right - left + 1)
+        }
+        return answer
     }
-}
-
-fun main() {
-    val prob = Problem1438()
-    println(prob.longestSubarray(intArrayOf(8,2,4,7), 4)) // 2
-    println(prob.longestSubarray(intArrayOf(10,1,2,4,7,2), 5)) // 4
-    println(prob.longestSubarray(intArrayOf(4,2,2,2,4,4,2,2), 0)) // 3
 }
