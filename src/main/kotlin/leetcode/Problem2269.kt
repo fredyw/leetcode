6cd3f19dd3238b2f17cda98af0f79203c5766ecd
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-05-15 01:13:08 -0500

    Skeleton for problem 2269

---
+++ src/main/kotlin/leetcode/Problem2269.kt
@@ -0,0 +1,16 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/find-the-k-beauty-of-a-number/
+ */
+class Problem2269 {
+    fun divisorSubstrings(num: Int, k: Int): Int {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2269()
+    println(prob.divisorSubstrings(240, 2)) // 2
+    println(prob.divisorSubstrings(430043, 2)) // 2
+}
