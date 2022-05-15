9568277e16beffce0fb28e1f5cbbc8617a345f1d
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-05-15 10:05:21 -0500

    Skeleton for problem 2275

---
+++ src/main/kotlin/leetcode/Problem2275.kt
@@ -0,0 +1,16 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/largest-combination-with-bitwise-and-greater-than-zero/
+ */
+class Problem2275 {
+    fun largestCombination(candidates: IntArray): Int {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2275()
+    println(prob.largestCombination(intArrayOf(16,17,71,62,12,24,14))) // 4
+    println(prob.largestCombination(intArrayOf(8,8))) // 2
+}
