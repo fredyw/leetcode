57a79d94220f73a10ba60a96a1038d06b3cd0650
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-05-17 10:44:02 -0500

    Solve problem 2274

--- src/main/kotlin/leetcode/Problem2274.kt
+++ src/main/kotlin/leetcode/Problem2274.kt
@@ -1,16 +1,21 @@
 package leetcode
 
+import kotlin.math.max
+
 /**
  * https://leetcode.com/problems/maximum-consecutive-floors-without-special-floors/
  */
 class Problem2274 {
     fun maxConsecutive(bottom: Int, top: Int, special: IntArray): Int {
-        TODO()
+        var answer = 0
+        val list = mutableListOf<Int>()
+        list += bottom - 1
+        list += special.sorted()
+        list += top + 1
+        for (i in 1 until list.size) {
+            val diff = list[i] - list[i - 1] - 1
+            answer = max(answer, if (diff > 1) diff else 0)
+        }
+        return answer
     }
-}
-
-fun main() {
-    val prob = Problem2274()
-    println(prob.maxConsecutive(2, 9, intArrayOf(4,6))) // 3
-    println(prob.maxConsecutive(6, 8, intArrayOf(7,6,8))) // 0
 }
