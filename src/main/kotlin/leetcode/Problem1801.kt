ff3e0d295f880f43accae6111fe8ac9c9663c4ef
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-05-17 15:57:22 -0500

    Solve problem 1801

--- src/main/kotlin/leetcode/Problem1801.kt
+++ src/main/kotlin/leetcode/Problem1801.kt
@@ -24,7 +24,7 @@ class Problem1801 {
                     }
                 }
                 if (remainingAmount > 0) {
-                    buyMap[price] = remainingAmount
+                    buyMap[price] = (buyMap[price] ?: 0) + remainingAmount
                 }
             } else { // sell
                 var remainingAmount = amount.toLong()
@@ -40,7 +40,7 @@ class Problem1801 {
                     }
                 }
                 if (remainingAmount > 0) {
-                    sellMap[price] = remainingAmount
+                    sellMap[price] = (sellMap[price] ?: 0) + remainingAmount
                 }
             }
         }
@@ -53,19 +53,4 @@ class Problem1801 {
         }
         return answer.toInt() % 1_000_000_007
     }
-}
-
-fun main() {
-    val prob = Problem1801()
-//    println(prob.getNumberOfBacklogOrders(arrayOf(
-//        intArrayOf(10,5,0), intArrayOf(15,2,1), intArrayOf(25,1,1), intArrayOf(30,4,0)
-//    ))) // 6
-//    println(prob.getNumberOfBacklogOrders(arrayOf(
-//        intArrayOf(7,1000000000,1), intArrayOf(15,3,0), intArrayOf(5,999999995,0), intArrayOf(5,1,1)
-//    ))) // 999999984
-    println(prob.getNumberOfBacklogOrders(arrayOf(
-        intArrayOf(1,29,1), intArrayOf(22,7,1), intArrayOf(24,1,0), intArrayOf(25,15,1),
-        intArrayOf(18,8,1), intArrayOf(8,22,0), intArrayOf(25,15,1), intArrayOf(30,1,1),
-        intArrayOf(27,30,0)
-    ))) // 22
 }
