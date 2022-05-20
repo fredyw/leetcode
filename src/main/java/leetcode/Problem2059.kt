2bcc8533a31d4b5ff778445c50446e5a10497cb4
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-05-20 00:24:52 -0500

    Solve problem 2059

--- src/main/java/leetcode/Problem2059.kt
+++ src/main/java/leetcode/Problem2059.kt
@@ -1,21 +1,36 @@
 package leetcode
 
+import java.util.LinkedList
+import kotlin.math.min
+
 /**
  * https://leetcode.com/problems/minimum-operations-to-convert-number/
  */
 class Problem2059 {
     fun minimumOperations(nums: IntArray, start: Int, goal: Int): Int {
-        TODO()
+        var answer = 0
+        val queue = LinkedList<Int>()
+        val visited = BooleanArray(1001)
+        queue.add(start)
+        while (queue.isNotEmpty()) {
+            val size = queue.size
+            for (i in 0 until size) {
+                val n = queue.remove()
+                if (n == goal) {
+                    return answer
+                }
+                if (n < 0 || n > 1000 || visited[n]) {
+                    continue
+                }
+                visited[n] = true
+                for (num in nums) {
+                    for (m in intArrayOf(n + num, n - num, n xor num)) {
+                        queue.add(m)
+                    }
+                }
+            }
+            answer++
+        }
+        return -1
     }
-}
-
-fun main() {
-    val prob = Problem2059()
-    println(prob.minimumOperations(intArrayOf(2,4,12), 2, 12)) // 2
-    println(prob.minimumOperations(intArrayOf(3,5,7), 0, -4)) // 2
-    println(prob.minimumOperations(intArrayOf(2,8,16), 0, 1)) // -1
-    println(prob.minimumOperations(intArrayOf(2,3,9), 0, -4)) // 3
-    println(prob.minimumOperations(intArrayOf(2), 0, -2)) // -1
-    println(prob.minimumOperations(intArrayOf(2,2), 0, 4)) // 3
-    println(prob.minimumOperations(intArrayOf(2,4), 0, -6)) // -1
 }
