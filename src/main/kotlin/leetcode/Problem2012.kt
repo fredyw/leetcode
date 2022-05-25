8cee5091860ef950ace7a5f654e4a87040919551
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-05-24 23:29:12 -0500

    Skeleton for problem 2012

---
+++ src/main/kotlin/leetcode/Problem2012.kt
@@ -0,0 +1,17 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/sum-of-beauty-in-the-array/
+ */
+class Problem2012 {
+    fun sumOfBeauties(nums: IntArray): Int {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2012()
+    println(prob.sumOfBeauties(intArrayOf(1,2,3))) // 2
+    println(prob.sumOfBeauties(intArrayOf(2,4,6,4))) // 1
+    println(prob.sumOfBeauties(intArrayOf(3,2,1))) // 0
+}
