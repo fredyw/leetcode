dad6781d9036896cad9aca22d4df1ef2cabf5179
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-05-25 22:22:31 -0500

    Update problem 2012

--- src/main/kotlin/leetcode/Problem2012.kt
+++ src/main/kotlin/leetcode/Problem2012.kt
@@ -13,5 +13,10 @@ fun main() {
     val prob = Problem2012()
     println(prob.sumOfBeauties(intArrayOf(1,2,3))) // 2
     println(prob.sumOfBeauties(intArrayOf(2,4,6,4))) // 1
+    println(prob.sumOfBeauties(intArrayOf(2,4,6,5))) // 2
+    println(prob.sumOfBeauties(intArrayOf(2,4,6,6))) // 2
+    println(prob.sumOfBeauties(intArrayOf(2,4,6,7))) // 4
+    println(prob.sumOfBeauties(intArrayOf(2,4,6,7,8,6))) // 4
+    println(prob.sumOfBeauties(intArrayOf(2,4,6,7,8,4))) // 3
     println(prob.sumOfBeauties(intArrayOf(3,2,1))) // 0
 }
