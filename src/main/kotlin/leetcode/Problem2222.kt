a9d179a8a0e4def9a89ee0fd95d62624aaa7b21f
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-05-26 02:17:17 -0500

    Skeleton for problem 2222

---
+++ src/main/kotlin/leetcode/Problem2222.kt
@@ -0,0 +1,16 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/number-of-ways-to-select-buildings/
+ */
+class Problem2222 {
+    fun numberOfWays(s: String): Long {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2222()
+    println(prob.numberOfWays("001101")) // 6
+    println(prob.numberOfWays("11100")) // 0
+}
