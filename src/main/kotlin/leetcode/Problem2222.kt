b429933f86d5033e61fce9738e494ab61c220c79
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-05-26 02:53:00 -0500

    Solve problem 2222

--- src/main/kotlin/leetcode/Problem2222.kt
+++ src/main/kotlin/leetcode/Problem2222.kt
@@ -5,12 +5,35 @@ package leetcode
  */
 class Problem2222 {
     fun numberOfWays(s: String): Long {
-        TODO()
+        data class Binary(val zero: Int = 0, val one: Int = 0)
+        val left = Array(s.length) { Binary() }
+        for (i in s.indices) {
+            val zero = if (s[i] == '0') 1 else 0
+            val one = if (s[i] == '1') 1 else 0
+            left[i] = if (i == 0) {
+                Binary(zero, one)
+            } else {
+                Binary(zero + left[i - 1].zero, one + left[i - 1].one)
+            }
+        }
+        val right = Array(s.length) { Binary() }
+        for (i in s.length - 1 downTo 0) {
+            val zero = if (s[i] == '0') 1 else 0
+            val one = if (s[i] == '1') 1 else 0
+            right[i] = if (i == s.length - 1) {
+                Binary(zero, one)
+            } else {
+                Binary(zero + right[i + 1].zero, one + right[i + 1].one)
+            }
+        }
+        var answer = 0L
+        for (i in 1..s.length - 2) {
+            if (s[i] == '0') { // look for 101
+                answer += left[i - 1].one * right[i + 1].one
+            } else { // look for 010
+                answer += left[i - 1].zero * right[i + 1].zero
+            }
+        }
+        return answer
     }
-}
-
-fun main() {
-    val prob = Problem2222()
-    println(prob.numberOfWays("001101")) // 6
-    println(prob.numberOfWays("11100")) // 0
 }
