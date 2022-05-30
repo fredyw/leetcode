08b6066899b662d1e3fc647085e7de2d70317651
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-05-30 00:36:46 -0500

    Solve problem 2287

--- src/main/kotlin/leetcode/Problem2287.kt
+++ src/main/kotlin/leetcode/Problem2287.kt
@@ -1,17 +1,34 @@
 package leetcode
 
+import kotlin.math.min
+
 /**
  * https://leetcode.com/problems/rearrange-characters-to-make-target-string/
  */
 class Problem2287 {
     fun rearrangeCharacters(s: String, target: String): Int {
-        TODO()
+        val sCounts = IntArray(26)
+        for (c in s) {
+            sCounts[c - 'a']++
+        }
+        val tCounts = IntArray(26)
+        for (c in target) {
+            tCounts[c - 'a']++
+        }
+        var answer = Int.MAX_VALUE
+        for ((i, t) in tCounts.withIndex()) {
+            if (t == 0) {
+                continue
+            }
+            if (sCounts[i] == 0) {
+                return 0
+            }
+            val c = sCounts[i] / t
+            if (c == 0) {
+                return 0
+            }
+            answer = min(answer, c)
+        }
+        return answer
     }
-}
-
-fun main() {
-    val prob = Problem2287()
-    println(prob.rearrangeCharacters("ilovecodingonleetcode", "code")) // 2
-    println(prob.rearrangeCharacters("abcba", "abc")) // 1
-    println(prob.rearrangeCharacters("abbaccaddaeea", "aaaaa")) // 1
 }
