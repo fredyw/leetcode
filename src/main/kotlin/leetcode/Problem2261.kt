16ed0b36520ef7519e77cefdaef2d0b1e41ba499
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-05-31 14:59:43 -0500

    Update problem 2261

--- src/main/kotlin/leetcode/Problem2261.kt
+++ src/main/kotlin/leetcode/Problem2261.kt
@@ -12,5 +12,7 @@ class Problem2261 {
 fun main() {
     val prob = Problem2261()
     println(prob.countDistinct(intArrayOf(2,3,3,2,2), 2, 2)) // 11
+    println(prob.countDistinct(intArrayOf(2,3,4,2,2), 2, 2)) // 9
+    println(prob.countDistinct(intArrayOf(2,3,5,2,2), 2, 2)) // 12
     println(prob.countDistinct(intArrayOf(1,2,3,4), 4, 1)) // 10
 }
