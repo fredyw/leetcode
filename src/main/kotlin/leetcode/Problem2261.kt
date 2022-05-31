8ecbf591ccf5efa717d013cdc356ab0ac5b48543
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-05-31 14:41:57 -0500

    Skeleton for problem 2261

---
+++ src/main/kotlin/leetcode/Problem2261.kt
@@ -0,0 +1,16 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/k-divisible-elements-subarrays/
+ */
+class Problem2261 {
+    fun countDistinct(nums: IntArray, k: Int, p: Int): Int {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2261()
+    println(prob.countDistinct(intArrayOf(2,3,3,2,2), 2, 2)) // 11
+    println(prob.countDistinct(intArrayOf(1,2,3,4), 4, 1)) // 10
+}
