c843908b9fb7ab5fa5b7e4f449dc912b5acaee56
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-06-05 15:52:36 -0500

    Solve problem 2115

--- src/main/kotlin/leetcode/Problem2115.kt
+++ src/main/kotlin/leetcode/Problem2115.kt
@@ -5,22 +5,43 @@ package leetcode
  */
 class Problem2115 {
     fun findAllRecipes(recipes: Array<String>, ingredients: List<List<String>>, supplies: Array<String>): List<String> {
-        TODO()
+        val recipeToIngredients = mutableMapOf<String, List<String>>()
+        for ((index, recipe) in recipes.withIndex()) {
+            recipeToIngredients[recipe] = ingredients[index]
+        }
+        val answer = mutableListOf<String>()
+        val memo = mutableMapOf<String, Boolean>()
+        val visited = mutableSetOf<String>()
+        val supplySet = supplies.toSet()
+        for (recipe in recipes) {
+            if (findAllRecipes(recipeToIngredients, supplySet, recipe, visited, memo)) {
+                answer += recipe
+            }
+        }
+        return answer
     }
-}
 
-fun main() {
-    val prob = Problem2115()
-    println(prob.findAllRecipes(
-        arrayOf("bread"),
-        listOf(listOf("yeast","flour")),
-        arrayOf("yeast","flour","corn"))) // ["bread"]
-    println(prob.findAllRecipes(
-        arrayOf("bread","sandwich"),
-        listOf(listOf("yeast","flour"), listOf("bread","meat")),
-        arrayOf("yeast","flour","meat"))) // ["bread","sandwich"]
-    println(prob.findAllRecipes(
-        arrayOf("bread","sandwich","burger"),
-        listOf(listOf("yeast","flour"), listOf("bread","meat"), listOf("sandwich","meat","bread")),
-        arrayOf("yeast","flour","meat"))) // ["bread","sandwich","burger"]
+    private fun findAllRecipes(recipeToIngredients: MutableMap<String, List<String>>,
+                               supplies: Set<String>,
+                               recipe: String,
+                               visited: MutableSet<String>,
+                               memo: MutableMap<String, Boolean>): Boolean {
+        if (memo[recipe] != null) {
+            return memo[recipe]!!
+        }
+        if (recipe in visited) {
+            return false
+        }
+        visited += recipe
+        var found = true
+        for (ingredient in recipeToIngredients[recipe] ?: listOf()) {
+            found = if (ingredient in recipeToIngredients) {
+                found && findAllRecipes(recipeToIngredients, supplies, ingredient, visited, memo)
+            } else {
+                found && ingredient in supplies
+            }
+        }
+        memo[recipe] = found
+        return found
+    }
 }
