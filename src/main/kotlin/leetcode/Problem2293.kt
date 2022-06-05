0eaeb930641e44017c73f7c5e0b00ca22eb46cba
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-06-05 11:13:32 -0500

    Skeleton for problem 2293

---
+++ src/main/kotlin/leetcode/Problem2293.kt
@@ -0,0 +1,16 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/min-max-game/
+ */
+class Problem2293 {
+    fun minMaxGame(nums: IntArray): Int {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem2293()
+    println(prob.minMaxGame(intArrayOf(1,3,5,2,4,8,2,2))) // 1
+    println(prob.minMaxGame(intArrayOf(3))) // 3
+}
