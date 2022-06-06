fd2817abf2a18f2e1640e78686158845bba77f0e
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-06-06 15:05:23 -0500

    Solve problem 2295

--- src/main/kotlin/leetcode/Problem2295.kt
+++ src/main/kotlin/leetcode/Problem2295.kt
@@ -5,16 +5,17 @@ package leetcode
  */
 class Problem2295 {
     fun arrayChange(nums: IntArray, operations: Array<IntArray>): IntArray {
-        TODO()
+        val map = mutableMapOf<Int, Int>()
+        for ((index, num) in nums.withIndex()) {
+            map[num] = index
+        }
+        for ((from, to) in operations) {
+            val index = map[from]
+            if (index != null) {
+                nums[index] = to
+                map[to] = index
+            }
+        }
+        return nums
     }
-}
-
-fun main() {
-    val prob = Problem2295()
-    println(prob.arrayChange(
-        intArrayOf(1,2,4,6),
-        arrayOf(intArrayOf(1,3), intArrayOf(4,7), intArrayOf(6,1)))) // [3,2,7,1]
-    println(prob.arrayChange(
-        intArrayOf(1,2),
-        arrayOf(intArrayOf(1,3), intArrayOf(2,1), intArrayOf(3,2)))) // [2,1]
 }
