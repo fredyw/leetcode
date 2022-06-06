17ca274abb86bf7c5ff47e3a2ec1182331c8dc1a
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-06-06 15:06:46 -0500

    Skeleton for problem 1717

---
+++ src/main/kotlin/leetcode/Problem1717.kt
@@ -0,0 +1,16 @@
+package leetcode
+
+/**
+ * https://leetcode.com/problems/maximum-score-from-removing-substrings/
+ */
+class Problem1717 {
+    fun maximumGain(s: String, x: Int, y: Int): Int {
+        TODO()
+    }
+}
+
+fun main() {
+    val prob = Problem1717()
+    println(prob.maximumGain("cdbcbbaaabab", 4, 5)) // 19
+    println(prob.maximumGain("aabbaaxybbaabb", 5, 4)) // 20
+}
