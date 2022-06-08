b4942c074257953e6f709fd15de14264cf8ddd3f
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-06-07 22:00:38 -0500

    Update problem 1717

--- src/main/kotlin/leetcode/Problem1717.kt
+++ src/main/kotlin/leetcode/Problem1717.kt
@@ -5,7 +5,36 @@ package leetcode
  */
 class Problem1717 {
     fun maximumGain(s: String, x: Int, y: Int): Int {
-        TODO()
+        var answer = 0
+        var tmp = s
+        while (true) {
+            if (x > y) {
+                var i = tmp.indexOf("ab")
+                if (i < 0) {
+                    i = tmp.indexOf("ba")
+                    if (i < 0) {
+                        break
+                    }
+                    answer += y
+                } else {
+                    answer += x
+                }
+                tmp = tmp.substring(0, i) + tmp.substring(i + 2)
+            } else {
+                var i = tmp.indexOf("ba")
+                if (i < 0) {
+                    i = tmp.indexOf("ab")
+                    if (i < 0) {
+                        break
+                    }
+                    answer += x
+                } else {
+                    answer += y
+                }
+                tmp = tmp.substring(0, i) + tmp.substring(i + 2)
+            }
+        }
+        return answer
     }
 }
 
