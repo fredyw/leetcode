e48c0b079a0ce92bad162eaa4d5c4ca22deff1bb
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-06-12 03:11:39 -0500

    Skeleton for problem 2305

---
+++ src/main/rust/src/bin/problem2305.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/fair-distribution-of-cookies/
+pub fn distribute_cookies(cookies: Vec<i32>, k: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", distribute_cookies(vec![8, 15, 10, 20, 8], 2)); // 31
+    println!("{}", distribute_cookies(vec![6, 1, 3, 2, 2, 4, 1, 3], 2)); // 7
+}
