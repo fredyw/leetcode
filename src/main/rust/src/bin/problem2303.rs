92c610b40d8159f0d28fd58e34d096ea39035d2b
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-06-12 00:56:01 -0500

    Solve problem 2303

--- src/main/rust/src/bin/problem2303.rs
+++ src/main/rust/src/bin/problem2303.rs
@@ -1,6 +1,21 @@
 // https://leetcode.com/problems/calculate-amount-paid-in-taxes/
 pub fn calculate_tax(brackets: Vec<Vec<i32>>, income: i32) -> f64 {
-    todo!()
+    let mut answer = 0.0;
+    for (i, e) in brackets.iter().enumerate() {
+        let upper = if income < e[0] { income } else { e[0] };
+        let percent = &e[1];
+        let mut prev = 0;
+        if i > 0 {
+            let p = &brackets[i - 1];
+            prev = p[0];
+        }
+        let tax = (upper - prev) as f64 * (*percent as f64 / 100.0) as f64;
+        answer += tax;
+        if income < e[0] {
+            break;
+        }
+    }
+    answer
 }
 
 fn main() {
