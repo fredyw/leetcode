14f72030740c946f54a92ecaebfff3d70de3658b
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-06-13 17:51:44 -0500

    Solve problem 1975

--- src/main/rust/src/bin/problem1975.rs
+++ src/main/rust/src/bin/problem1975.rs
@@ -1,5 +1,21 @@
+use std::cmp;
+
 pub fn max_matrix_sum(matrix: Vec<Vec<i32>>) -> i64 {
-    todo!()
+    let mut sum: i64 = 0;
+    let mut negative_count = 0;
+    let mut smallest = i32::MAX;
+    for i in 0..matrix.len() {
+        for j in 0..matrix[0].len() {
+            sum += i64::abs(matrix[i][j] as i64);
+            negative_count += if matrix[i][j] < 0 { 1 } else { 0 };
+            smallest = cmp::min(smallest, i32::abs(matrix[i][j]));
+        }
+    }
+    if negative_count % 2 == 0 {
+        sum
+    } else {
+        sum - (smallest as i64 * 2)
+    }
 }
 
 fn main() {
