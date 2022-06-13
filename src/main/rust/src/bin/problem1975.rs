7f01ea77c780221984c63eaa2383b12f04971b90
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-06-13 00:26:20 -0500

    Convert problem 1975 skeleton from Kotlin to Rust

---
+++ src/main/rust/src/bin/problem1975.rs
@@ -0,0 +1,29 @@
+pub fn max_matrix_sum(matrix: Vec<Vec<i32>>) -> i64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", max_matrix_sum(vec![vec![1, -1], vec![-1, 1]])); // 4
+    println!(
+        "{}",
+        max_matrix_sum(vec![vec![1, 2, 3], vec![-1, -2, -3], vec![1, 2, 3]])
+    ); // 16
+    println!(
+        "{}",
+        max_matrix_sum(vec![
+            vec![1, 2, 3, 4],
+            vec![-1, -2, -3, 4],
+            vec![1, 2, 3, 4],
+            vec![1, 2, 3, 4]
+        ])
+    ); // 38
+    println!(
+        "{}",
+        max_matrix_sum(vec![
+            vec![1, 2, 3, 4],
+            vec![-1, -2, -3, -4],
+            vec![1, 2, 3, 4],
+            vec![1, 2, 3, 4]
+        ])
+    ); // 40
+}
