766408ace07b01a5a6fa4e1e99b9f4c63f392c2a
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-06-14 00:25:33 -0500

    Skeleton for problem 2086

---
+++ src/main/rust/src/bin/problem2086.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/minimum-number-of-buckets-required-to-collect-rainwater-from-houses/
+pub fn minimum_buckets(street: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", minimum_buckets(String::from("H..H"))); // 2
+    println!("{}", minimum_buckets(String::from(".H.H."))); // 1
+    println!("{}", minimum_buckets(String::from(".HHH."))); // -1
+}
