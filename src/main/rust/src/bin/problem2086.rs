46dad8f2059137bf5822f9caf7afa10dcbddd61a
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-06-14 10:12:12 -0500

    Update problem 2086

--- src/main/rust/src/bin/problem2086.rs
+++ src/main/rust/src/bin/problem2086.rs
@@ -7,4 +7,7 @@ fn main() {
     println!("{}", minimum_buckets(String::from("H..H"))); // 2
     println!("{}", minimum_buckets(String::from(".H.H."))); // 1
     println!("{}", minimum_buckets(String::from(".HHH."))); // -1
+    println!("{}", minimum_buckets(String::from(".H..H..H."))); // 3
+    println!("{}", minimum_buckets(String::from(".H.H..H."))); // 2
+    println!("{}", minimum_buckets(String::from(".H.H.H."))); // 2
 }
