d2a1e8a8b1f7469c4a6f9a383d8052e2cb8592b4
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-06-14 16:40:45 -0500

    Skeleton for problem 2140

---
+++ src/main/rust/src/bin/problem2140.rs
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/solving-questions-with-brainpower/
+pub fn most_points(questions: Vec<Vec<i32>>) -> i64 {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        most_points(vec![vec![3, 2], vec![4, 3], vec![4, 4], vec![2, 5]])
+    ); // 5
+    println!(
+        "{}",
+        most_points(vec![
+            vec![1, 1],
+            vec![2, 2],
+            vec![3, 3],
+            vec![4, 4],
+            vec![5, 5]
+        ])
+    ); // 7
+}
