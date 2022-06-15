5d092853066453171d75e347249f527c6250c7f7
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-06-15 10:51:28 -0500

    Solve problem 2140

--- src/main/rust/src/bin/problem2140.rs
+++ src/main/rust/src/bin/problem2140.rs
@@ -1,6 +1,23 @@
+use std::cmp;
+
 // https://leetcode.com/problems/solving-questions-with-brainpower/
 pub fn most_points(questions: Vec<Vec<i32>>) -> i64 {
-    todo!()
+    fn f(questions: &Vec<Vec<i32>>, index: i32, memo: &mut Vec<i64>) -> i64 {
+        if (index as usize) >= questions.len() {
+            return 0;
+        }
+        if memo[index as usize] != -1 {
+            return memo[index as usize];
+        }
+        let points = &questions[index as usize][0];
+        let brainpower = &questions[index as usize][1];
+        let solve = f(questions, index + brainpower + 1, memo) + *points as i64;
+        let skip = f(questions, index + 1, memo);
+        let max: i64 = cmp::max(solve, skip);
+        memo[index as usize] = max;
+        max
+    }
+    f(&questions, 0, &mut vec![-1; questions.len() as usize])
 }
 
 fn main() {
