31456935cc72aa26d814b528d917076ed6040305
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-06-15 11:02:53 -0500

    Skeleton for problem 2216

---
+++ src/main/rust/src/bin/problem2216.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/minimum-deletions-to-make-array-beautiful/
+pub fn min_deletion(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", min_deletion(vec![1, 1, 2, 3, 5])); // 1
+    println!("{}", min_deletion(vec![1, 1, 2, 2, 3, 3])); // 2
+}
