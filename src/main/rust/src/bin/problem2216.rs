6b6c3adbf56d31190cadfb7fd00f000789b19d59
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-06-16 14:39:44 -0500

    Update problem 2216

--- src/main/rust/src/bin/problem2216.rs
+++ src/main/rust/src/bin/problem2216.rs
@@ -6,4 +6,6 @@ pub fn min_deletion(nums: Vec<i32>) -> i32 {
 fn main() {
     println!("{}", min_deletion(vec![1, 1, 2, 3, 5])); // 1
     println!("{}", min_deletion(vec![1, 1, 2, 2, 3, 3])); // 2
+    println!("{}", min_deletion(vec![1, 1, 2, 2, 3])); // 1
+    println!("{}", min_deletion(vec![1, 1, 1, 1, 1])); // 5
 }
