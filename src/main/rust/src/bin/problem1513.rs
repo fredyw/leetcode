d0b859b3fc907b2b1092fa56d2a767324ba9e96b
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-06-17 14:35:09 -0500

    Skeleton for problem 1513

---
+++ src/main/rust/src/bin/problem1513.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/number-of-substrings-with-only-1s/
+pub fn num_sub(s: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", num_sub(String::from("0110111"))); // 9
+    println!("{}", num_sub(String::from("101"))); // 2
+    println!("{}", num_sub(String::from("111111"))); // 21
+}
