3bb1c4c44319498dc761bfe7bc1296975899af6a
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-06-16 19:25:07 -0500

    Solve problem 2216

--- src/main/rust/src/bin/problem2216.rs
+++ src/main/rust/src/bin/problem2216.rs
@@ -1,6 +1,23 @@
 // https://leetcode.com/problems/minimum-deletions-to-make-array-beautiful/
 pub fn min_deletion(nums: Vec<i32>) -> i32 {
-    todo!()
+    let mut answer = 0;
+    let mut count = 0;
+    let mut even = true;
+    let mut i = 0;
+    while i < nums.len() {
+        if even && i + 1 < nums.len() && nums[i] == nums[i + 1] {
+            answer += 1;
+        } else {
+            even = !even;
+            count += 1;
+        }
+        i += 1;
+    }
+    if count % 2 == 0 {
+        answer
+    } else {
+        answer + 1
+    }
 }
 
 fn main() {
