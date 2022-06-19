c270065db982041118de2079b5bdc17873af073c
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-06-19 13:35:30 -0500

    Solve problem 786

--- src/main/rust/src/bin/problem786.rs
+++ src/main/rust/src/bin/problem786.rs
@@ -1,6 +1,20 @@
 // https://leetcode.com/problems/k-th-smallest-prime-fraction/
 pub fn kth_smallest_prime_fraction(arr: Vec<i32>, k: i32) -> Vec<i32> {
-    todo!()
+    struct Fraction(i32, i32);
+    let mut fractions: Vec<Fraction> = vec![];
+    for i in 0..arr.len() {
+        for j in i + 1..arr.len() {
+            // println!("{} / {} = {}", arr[i], arr[j], i as f64 / j as f64);
+            fractions.push(Fraction(arr[i], arr[j]));
+        }
+    }
+    fractions.sort_by(|a, b| {
+        (a.0 as f64 / a.1 as f64)
+            .partial_cmp(&(b.0 as f64 / b.1 as f64))
+            .unwrap()
+    });
+    let answer = &fractions[(k - 1) as usize];
+    vec![answer.0, answer.1]
 }
 
 fn main() {
