910e6b8a60516252ea2f6bf6cb12c2645a300f8c
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-06-19 13:03:22 -0500

    Skeleton for problem 786

---
+++ src/main/rust/src/bin/problem786.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/k-th-smallest-prime-fraction/
+pub fn kth_smallest_prime_fraction(arr: Vec<i32>, k: i32) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", kth_smallest_prime_fraction(vec![1, 2, 3, 5], 3)); // [2,5]
+    println!("{:?}", kth_smallest_prime_fraction(vec![1, 7], 1)); // [1,7]
+}
