46a170cdc3506be4d3938dd1345278e1cdf265af
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-06-19 17:05:29 -0500

    Skeleton for problem 1754

---
+++ src/main/rust/src/bin/problem1754.rs
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/largest-merge-of-two-strings/
+pub fn largest_merge(word1: String, word2: String) -> String {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        largest_merge(String::from("cabaa"), String::from("bcaaa"))
+    ); // cbcabaaaaa
+    println!(
+        "{}",
+        largest_merge(String::from("abcabc"), String::from("abdcaba"))
+    ); // abdcabcabcaba
+}
