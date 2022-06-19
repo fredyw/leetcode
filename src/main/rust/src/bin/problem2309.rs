681d08befa36e6b6b974113166be72c368d9a332
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-06-19 01:15:23 -0500

    Skeleton for problem 2309

---
+++ src/main/rust/src/bin/problem2309.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/greatest-english-letter-in-upper-and-lower-case/
+pub fn greatest_letter(s: String) -> String {
+    todo!();
+}
+
+fn main() {
+    println!("{}", greatest_letter(String::from("lEeTcOdE"))); // "E"
+    println!("{}", greatest_letter(String::from("arRAzFif"))); // "R"
+    println!("{}", greatest_letter(String::from("AbCdEfGhIjK"))); // ""
+}
