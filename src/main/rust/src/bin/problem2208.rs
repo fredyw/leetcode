26f714cab00a9ce52fb7ef81b6da84cdb378817d
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-06-20 17:31:27 -0500

    Skeleton for problem 2208

---
+++ src/main/rust/src/bin/problem2208.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/minimum-operations-to-halve-array-sum/
+pub fn halve_array(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", halve_array(vec![5, 19, 8, 1])); // 3
+    println!("{}", halve_array(vec![3, 8, 20])); // 3
+}
