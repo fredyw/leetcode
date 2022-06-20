d17b99cc91e44287fc650c78d8ab32ecac6740b7
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-06-20 16:58:40 -0500

    Solve problem 1754

--- src/main/rust/src/bin/problem1754.rs
+++ src/main/rust/src/bin/problem1754.rs
@@ -1,6 +1,32 @@
 // https://leetcode.com/problems/largest-merge-of-two-strings/
 pub fn largest_merge(word1: String, word2: String) -> String {
-    todo!()
+    let mut answer = String::from("");
+    let chars1: Vec<char> = word1.chars().collect();
+    let chars2: Vec<char> = word2.chars().collect();
+    let mut i = 0;
+    let mut j = 0;
+    while i < chars1.len() && j < chars2.len() {
+        if chars1[i] > chars2[j] {
+            answer.push(chars1[i]);
+            i += 1;
+        } else if chars1[i] < chars2[j] {
+            answer.push(chars2[j]);
+            j += 1;
+        } else {
+            answer.push(chars1[i]);
+            if word1[i..] >= word2[j..] {
+                i += 1;
+            } else {
+                j += 1;
+            }
+        }
+    }
+    if i < chars1.len() {
+        answer.push_str(&word1[i..]);
+    } else {
+        answer.push_str(&word2[j..]);
+    }
+    answer
 }
 
 fn main() {
@@ -16,4 +42,22 @@ fn main() {
         "{}",
         largest_merge(String::from("abb"), String::from("acde"))
     ); // "acdeabb"
+    println!(
+        "{}",
+        largest_merge(String::from("bbbaba"), String::from("bbbd"))
+    ); // "bbbdbbbaba"
+    println!(
+        "{}",
+        largest_merge(
+            String::from("qqqqqqqqqeqeqqeeqqq"),
+            String::from("qqqqqqqqeqqqeeqqeeqqqqqeq")
+        )
+    ); // "qqqqqqqqqqqqqqqqqeqqqeqeqqeeqqqeeqqeeqqqqqeq"
+    println!(
+        "{}",
+        largest_merge(
+            String::from("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"),
+            String::from("bbbbbbbbbbbbbbbbbbbb")
+        )
+    ); // "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"
 }
