a1e9af298aa2f8dbefba89b92739921dbe5c442d
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-06-20 19:04:28 -0500

    Solve problem 2208

--- src/main/rust/src/bin/problem2208.rs
+++ src/main/rust/src/bin/problem2208.rs
@@ -1,6 +1,31 @@
+use std::cmp::Ordering;
+use std::collections::BinaryHeap;
+
 // https://leetcode.com/problems/minimum-operations-to-halve-array-sum/
 pub fn halve_array(nums: Vec<i32>) -> i32 {
-    todo!()
+    #[derive(PartialEq, PartialOrd)]
+    struct Wrapper(f64);
+    impl Eq for Wrapper {}
+    impl Ord for Wrapper {
+        fn cmp(&self, other: &Self) -> Ordering {
+            self.0.partial_cmp(&other.0).unwrap()
+        }
+    }
+    let mut answer = 0;
+    let mut heap: BinaryHeap<Wrapper> = BinaryHeap::new();
+    let mut total_sum: f64 = 0.0;
+    for n in nums {
+        total_sum += n as f64;
+        heap.push(Wrapper(n as f64));
+    }
+    let mut s = 0.0;
+    while s < total_sum / 2.0 && !heap.is_empty() {
+        let half = heap.pop().unwrap().0 / 2.0;
+        s += half;
+        heap.push(Wrapper(half));
+        answer += 1;
+    }
+    answer
 }
 
 fn main() {
