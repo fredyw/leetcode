aed5959f53a619e678df0cf0ce9b7722097bfb44
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-06-21 00:01:58 -0500

    Skeleton for problem 1915

---
+++ src/main/rust/src/bin/problem1915.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/number-of-wonderful-substrings/
+pub fn wonderful_substrings(word: String) -> i64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", wonderful_substrings(String::from("aba"))); // 4
+    println!("{}", wonderful_substrings(String::from("aabb"))); // 9
+    println!("{}", wonderful_substrings(String::from("he"))); // 2
+}
