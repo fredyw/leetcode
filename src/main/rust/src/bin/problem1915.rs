abfa7fd206751c0aa3ccead5d51ae8ea144220cb
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-06-21 15:34:04 -0500

    Solve problem 1915

--- src/main/rust/src/bin/problem1915.rs
+++ src/main/rust/src/bin/problem1915.rs
@@ -1,6 +1,20 @@
 // https://leetcode.com/problems/number-of-wonderful-substrings/
 pub fn wonderful_substrings(word: String) -> i64 {
-    todo!()
+    let mut answer = 0;
+    let mut counts = vec![0; 1024]; // 1111111111 = 1023
+    counts[0] = 1;
+    let chars: Vec<char> = word.chars().collect();
+    let mut current = 0;
+    for c in chars {
+        current ^= 1 << c as i32 - 'a' as i32;
+        answer += counts[current];
+        for j in 0..10 {
+            let n = counts[current ^ (1 << j)];
+            answer += n;
+        }
+        counts[current] += 1;
+    }
+    answer
 }
 
 fn main() {
