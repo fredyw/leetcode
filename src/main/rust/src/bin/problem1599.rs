3716aaa71f2c9e003acbbff9f3b3f2b7754b7560
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-06-21 23:30:02 -0500

    Skeleton for problem 1599

---
+++ src/main/rust/src/bin/problem1599.rs
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/maximum-profit-of-operating-a-centennial-wheel/
+pub fn min_operations_max_profit(
+    customers: Vec<i32>,
+    boarding_cost: i32,
+    running_cost: i32,
+) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", min_operations_max_profit(vec![8, 3], 5, 6)); // 3
+    println!("{}", min_operations_max_profit(vec![10, 9, 6], 6, 4)); // 7
+    println!("{}", min_operations_max_profit(vec![3, 4, 0, 5, 1], 1, 92)); // -1
+}
