b9557c196237f5ace65e5c57392ba84465b8818e
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-06-22 17:53:02 -0500

    Solve problem 1599

--- src/main/rust/src/bin/problem1599.rs
+++ src/main/rust/src/bin/problem1599.rs
@@ -4,22 +4,48 @@ pub fn min_operations_max_profit(
     boarding_cost: i32,
     running_cost: i32,
 ) -> i32 {
-    let mut answer = 0;
+    let mut answer = -1;
+    let mut max_profit = 0;
     let mut i = 1;
     let mut wait = 0;
+    let mut num_customer = 0;
     for customer in customers {
-        todo!();
+        let tmp = wait;
+        wait -= 4;
+        wait += customer;
+        if wait < 0 {
+            wait = 0;
+            num_customer += tmp + customer;
+        } else {
+            num_customer += 4;
+        }
+        let profit = (num_customer * boarding_cost) - (i * running_cost);
+        if max_profit < profit {
+            answer = i;
+            max_profit = profit;
+        }
         i += 1;
     }
-    if answer < 0 {
-        -1
-    } else {
-        answer
+    while wait > 0 {
+        if wait - 4 < 0 {
+            num_customer += wait;
+        } else {
+            num_customer += 4;
+        }
+        let profit = (num_customer * boarding_cost) - (i * running_cost);
+        if max_profit < profit {
+            answer = i;
+            max_profit = profit;
+        }
+        i += 1;
+        wait -= 4;
     }
+    answer
 }
 
 fn main() {
     println!("{}", min_operations_max_profit(vec![8, 3], 5, 6)); // 3
     println!("{}", min_operations_max_profit(vec![10, 9, 6], 6, 4)); // 7
     println!("{}", min_operations_max_profit(vec![3, 4, 0, 5, 1], 1, 92)); // -1
+    println!("{}", min_operations_max_profit(vec![2], 2, 4)); // -1
 }
