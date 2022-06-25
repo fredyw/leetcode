891584b572b2ab1d19807dddf91df9b218f365ba
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-06-25 14:09:46 -0500

    Update problem 2054

--- src/main/rust/src/bin/problem2054.rs
+++ src/main/rust/src/bin/problem2054.rs
@@ -2,26 +2,6 @@ use std::cmp;
 
 // https://leetcode.com/problems/two-best-non-overlapping-events/
 pub fn max_two_events(events: Vec<Vec<i32>>) -> i32 {
-    fn f(events: &Vec<Vec<i32>>, index: usize, memo: &mut Vec<i32>) -> i32 {
-        if index == events.len() {
-            return 0;
-        }
-        if memo[index] != -1 {
-            return memo[index];
-        }
-        let mut max = 0;
-        let current_end_time = events[index][1];
-        for i in (index + 1)..events.len() {
-            let start_time = &events[i][0];
-            let value = &events[i][2];
-            if current_end_time < *start_time {
-                max = cmp::max(max, f(events, i, memo) + value);
-            }
-        }
-        memo[index] = max;
-        max
-    }
-
     let mut events = events.clone();
     events.sort_by(|a, b| {
         let start_time1 = &a[0];
@@ -36,27 +16,34 @@ pub fn max_two_events(events: Vec<Vec<i32>>) -> i32 {
         }
     });
     let mut answer = 0;
-    let mut memo = vec![-1; events.len()];
     for i in 0..events.len() {
-        let value = &events[i][2];
-        answer = cmp::max(answer, f(&events, i, &mut memo) + value);
+        let end_time1 = &events[i][1];
+        let value1 = &events[i][2];
+        answer = cmp::max(answer, *value1);
+        for j in (i + 1)..events.len() {
+            let start_time2 = &events[j][0];
+            let value2 = &events[j][2];
+            if end_time1 < start_time2 {
+                answer = cmp::max(answer, value1 + value2);
+            }
+        }
     }
     answer
 }
 
 fn main() {
-    // println!(
-    //     "{}",
-    //     max_two_events(vec![vec![1, 3, 2], vec![4, 5, 2], vec![2, 4, 3]])
-    // ); // 4
-    // println!(
-    //     "{}",
-    //     max_two_events(vec![vec![1, 3, 2], vec![4, 5, 2], vec![1, 5, 5]])
-    // ); // 5
-    // println!(
-    //     "{}",
-    //     max_two_events(vec![vec![1, 5, 3], vec![1, 5, 1], vec![6, 6, 5]])
-    // ); // 8
+    println!(
+        "{}",
+        max_two_events(vec![vec![1, 3, 2], vec![4, 5, 2], vec![2, 4, 3]])
+    ); // 4
+    println!(
+        "{}",
+        max_two_events(vec![vec![1, 3, 2], vec![4, 5, 2], vec![1, 5, 5]])
+    ); // 5
+    println!(
+        "{}",
+        max_two_events(vec![vec![1, 5, 3], vec![1, 5, 1], vec![6, 6, 5]])
+    ); // 8
     println!(
         "{}",
         max_two_events(vec![
