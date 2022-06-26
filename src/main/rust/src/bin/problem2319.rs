46a4067c2479851e6be483c28c1395eb8f45170b
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-06-26 11:30:04 -0500

    Solve problem 2319

--- src/main/rust/src/bin/problem2319.rs
+++ src/main/rust/src/bin/problem2319.rs
@@ -1,6 +1,43 @@
+use std::collections::HashSet;
+
 // https://leetcode.com/problems/check-if-matrix-is-x-matrix/
 pub fn check_x_matrix(grid: Vec<Vec<i32>>) -> bool {
-    todo!()
+    let n = grid.len();
+    let mut set: HashSet<(i32, i32)> = HashSet::new();
+    let mut row: i32 = 0;
+    let mut col: i32 = 0;
+    while row < n as i32 {
+        if grid[row as usize][col as usize] == 0 {
+            return false;
+        }
+        set.insert((row, col));
+        row += 1;
+        col += 1;
+    }
+    let mut row: i32 = 0;
+    let mut col: i32 = n as i32 - 1;
+    while row < n as i32 {
+        if grid[row as usize][col as usize] == 0 {
+            return false;
+        }
+        set.insert((row, col));
+        row += 1;
+        col -= 1;
+    }
+    let mut row: i32 = 0;
+    while row < n as i32 {
+        let mut col: i32 = 0;
+        while col < n as i32 {
+            if !set.contains(&(row, col)) {
+                if grid[row as usize][col as usize] != 0 {
+                    return false;
+                }
+            }
+            col += 1;
+        }
+        row += 1;
+    }
+    true
 }
 
 fn main() {
@@ -16,5 +53,5 @@ fn main() {
     println!(
         "{}",
         check_x_matrix(vec![vec![5, 7, 0], vec![0, 3, 1], vec![0, 5, 0]])
-    ); // true
+    ); // false
 }
