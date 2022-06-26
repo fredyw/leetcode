419513a605c257631ec32099f05c8ff6a84902e9
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-06-25 19:13:35 -0500

    Solve problem 2315

--- src/main/rust/src/bin/problem2315.rs
+++ src/main/rust/src/bin/problem2315.rs
@@ -1,6 +1,16 @@
 // https://leetcode.com/problems/count-asterisks/
 pub fn count_asterisks(s: String) -> i32 {
-    todo!()
+    let mut answer = 0;
+    let mut j = 0;
+    for i in s.split("|") {
+        if j % 2 != 0 {
+            j += 1;
+            continue;
+        }
+        answer += (i.chars().filter(|a| *a == '*').count()) as i32;
+        j += 1;
+    }
+    answer
 }
 
 fn main() {
