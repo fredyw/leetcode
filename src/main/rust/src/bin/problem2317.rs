aa4d7b328556e38893a7e777c9beee037c9e8b1e
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-06-26 11:32:11 -0500

    Skeleton for problem 2317

---
+++ src/main/rust/src/bin/problem2317.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/maximum-xor-after-operations/
+pub fn maximum_xor(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", maximum_xor(vec![3, 2, 4, 6])); // 7
+    println!("{}", maximum_xor(vec![1, 2, 3, 9, 2])); // 11
+}
