256c86a5feeb9a12d7f3c4db4cf73dfecea19b0a
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-06-27 18:23:41 -0500

    Update problem 2317

--- src/main/rust/src/bin/problem2317.rs
+++ src/main/rust/src/bin/problem2317.rs
@@ -6,4 +6,6 @@ pub fn maximum_xor(nums: Vec<i32>) -> i32 {
 fn main() {
     println!("{}", maximum_xor(vec![3, 2, 4, 6])); // 7
     println!("{}", maximum_xor(vec![1, 2, 3, 9, 2])); // 11
+    println!("{}", maximum_xor(vec![1, 2, 3, 9])); // 11
+    println!("{}", maximum_xor(vec![1, 2, 3, 9, 9, 9])); // 11
 }
