f1e6734d9fed85f58fee2daa8d6a08a5f2cd7a12
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-06-27 21:37:46 -0500

    Solve problem 2317

--- src/main/rust/src/bin/problem2317.rs
+++ src/main/rust/src/bin/problem2317.rs
@@ -1,6 +1,28 @@
 // https://leetcode.com/problems/maximum-xor-after-operations/
 pub fn maximum_xor(nums: Vec<i32>) -> i32 {
-    todo!()
+    fn get_nth_bit(i: i32, n: i32) -> i32 {
+        (i >> n) & 1
+    }
+
+    fn set_nth_bit(i: i32, n: i32) -> i32 {
+        (1 << n) | i
+    }
+
+    let mut xor = 0;
+    for num in nums.iter() {
+        xor ^= *num;
+    }
+    let mut answer = xor;
+    for n in 0..32 {
+        let mut sum = 0;
+        for num in nums.iter() {
+            sum += get_nth_bit(*num, n);
+        }
+        if sum != 0 && sum % 2 == 0 {
+            answer = set_nth_bit(answer, n);
+        }
+    }
+    answer
 }
 
 fn main() {
@@ -8,4 +30,10 @@ fn main() {
     println!("{}", maximum_xor(vec![1, 2, 3, 9, 2])); // 11
     println!("{}", maximum_xor(vec![1, 2, 3, 9])); // 11
     println!("{}", maximum_xor(vec![1, 2, 3, 9, 9, 9])); // 11
+    println!(
+        "{}",
+        maximum_xor(vec![
+            1, 2, 3, 9, 2, 8784, 3223, 456, 234, 587, 4535, 12353, 456
+        ])
+    ); // 16383
 }
