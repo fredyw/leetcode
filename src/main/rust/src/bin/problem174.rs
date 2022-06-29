8c3c62b79d169bda8664ae1a65324c122ac2bd61
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-06-28 23:48:35 -0500

    Skeleton for problem 174

---
+++ src/main/rust/src/bin/problem174.rs
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/dungeon-game/
+pub fn calculate_minimum_hp(dungeon: Vec<Vec<i32>>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        calculate_minimum_hp(vec![vec![-2, -3, 3], vec![-5, -10, 1], vec![10, 30, -5]])
+    ); // 7
+    println!("{}", calculate_minimum_hp(vec![vec![0]])); // 1
+}
