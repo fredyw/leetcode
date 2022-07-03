f22a532a08d0249e1619ba8847ddd3cd8653546f
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-07-03 12:06:39 -0500

    Solve problem 174

--- src/main/rust/src/bin/problem174.rs
+++ src/main/rust/src/bin/problem174.rs
@@ -1,30 +1,34 @@
 // https://leetcode.com/problems/dungeon-game/
 pub fn calculate_minimum_hp(dungeon: Vec<Vec<i32>>) -> i32 {
-    fn f(dungeon: &Vec<Vec<i32>>, row: i32, col: i32, sum: i32, health: i32) -> i32 {
+    fn f(dungeon: &Vec<Vec<i32>>, row: i32, col: i32, memo: &mut Vec<Vec<i32>>) -> i32 {
         let max_rows = dungeon.len() as i32;
         let max_cols = if max_rows > 0 { dungeon[0].len() } else { 0 } as i32;
+        if row == max_rows || col == max_cols {
+            return i32::MAX;
+        }
+        if memo[row as usize][col as usize] != -1 {
+            return memo[row as usize][col as usize];
+        }
         let value = &dungeon[row as usize][col as usize];
-        let health = if sum + value <= 0 {
-            (sum + value).abs() + 1
-        } else {
-            health
-        };
         if row == max_rows - 1 && col == max_cols - 1 {
-            return health;
+            return if *value <= 0 { -value + 1 } else { 1 };
         }
-        let mut min = i32::MAX;
-        if col + 1 < max_cols {
-            let right = f(dungeon, row, col + 1, sum + value, health);
-            min = min.min(right);
-        }
-        if row + 1 < max_rows {
-            let down = f(dungeon, row + 1, col, sum + value, health);
-            min = min.min(down);
-        }
+        let right = f(dungeon, row, col + 1, memo);
+        let down = f(dungeon, row + 1, col, memo);
+        let min = right.min(down) - value;
+        let min = if min <= 0 { 1 } else { min };
+        memo[row as usize][col as usize] = min;
         min
     }
 
-    f(&dungeon, 0, 0, 0, 1)
+    let max_rows = dungeon.len() as i32;
+    let max_cols = if max_rows > 0 { dungeon[0].len() } else { 0 } as i32;
+    f(
+        &dungeon,
+        0,
+        0,
+        &mut vec![vec![-1; max_cols as usize]; max_rows as usize],
+    )
 }
 
 fn main() {
