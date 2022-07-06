a962f38be4d654b41f5d755cdb4b945bf345edd9
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-07-06 16:34:58 -0500

    Update problem 1906

--- src/main/rust/src/bin/problem1906.rs
+++ src/main/rust/src/bin/problem1906.rs
@@ -1,6 +1,41 @@
 // https://leetcode.com/problems/minimum-absolute-difference-queries/
 pub fn min_difference(nums: Vec<i32>, queries: Vec<Vec<i32>>) -> Vec<i32> {
-    todo!()
+    let mut prefix = vec![vec![0; 101]; 100001];
+    for i in 0..nums.len() {
+        let j = nums[i] as usize;
+        prefix[i][j] += 1;
+        if i > 0 {
+            for j in 1..101 {
+                prefix[i][j] += prefix[i - 1][j];
+            }
+        }
+    }
+    let mut answer: Vec<i32> = vec![];
+    for query in queries.iter() {
+        let left = query[0] as usize;
+        let right = query[1] as usize;
+        let mut counts = vec![0; 101];
+        for i in 0..101 {
+            if left == 0 {
+                counts[i] = prefix[right][i];
+            } else {
+                counts[i] = prefix[right][i] - prefix[left - 1][i];
+            }
+        }
+        let mut min = i32::MAX;
+        let mut prev = 0;
+        for i in 0..101 {
+            if counts[i] == 0 {
+                continue;
+            }
+            if prev > 0 {
+                min = min.min((i as i32 - prev).abs());
+            }
+            prev = i as i32;
+        }
+        answer.push(if min == i32::MAX { -1 } else { min });
+    }
+    answer
 }
 
 fn main() {
