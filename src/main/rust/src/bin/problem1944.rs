3b5a35d59c39137dfe539e75b531e186906bb741
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-07-09 13:50:20 -0500

    Skeleton for problem 1944

---
+++ src/main/rust/src/bin/problem1944.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/number-of-visible-people-in-a-queue/
+pub fn can_see_persons_count(heights: Vec<i32>) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", can_see_persons_count(vec![10, 6, 8, 5, 11, 9])); // [3,1,2,1,1,0]
+    println!("{:?}", can_see_persons_count(vec![5, 1, 2, 3, 10])); // [4,1,1,1,0]
+}
