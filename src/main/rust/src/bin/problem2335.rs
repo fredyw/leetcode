7c4a9e34089b725f9432ba5ccd30c2408a28dea7
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-07-10 00:15:42 -0500

    Skeleton for problem 2335

---
+++ src/main/rust/src/bin/problem2335.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/minimum-amount-of-time-to-fill-cups/
+pub fn fill_cups(amount: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", fill_cups(vec![1, 4, 2])); // 4
+    println!("{}", fill_cups(vec![5, 4, 4])); // 7
+    println!("{}", fill_cups(vec![5, 0, 0])); // 5
+}
