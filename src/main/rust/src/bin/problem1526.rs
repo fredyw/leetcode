95a606cc69342cd32f16514504c5dc18512dc5a8
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-07-13 00:28:45 -0500

    Skeleton for problem 1526

---
+++ src/main/rust/src/bin/problem1526.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/minimum-number-of-increments-on-subarrays-to-form-a-target-array/
+pub fn min_number_operations(target: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", min_number_operations(vec![1, 2, 3, 2, 1])); // 3
+    println!("{}", min_number_operations(vec![3, 1, 1, 2])); // 4
+    println!("{}", min_number_operations(vec![3, 1, 5, 4, 2])); // 7
+}
