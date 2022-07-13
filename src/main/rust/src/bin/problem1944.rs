68212a5e24850e4de1fd0d8d8a7360d2066aaba0
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-07-12 20:39:17 -0500

    Solve problem 1944

--- src/main/rust/src/bin/problem1944.rs
+++ src/main/rust/src/bin/problem1944.rs
@@ -1,6 +1,31 @@
+use std::collections::VecDeque;
+
 // https://leetcode.com/problems/number-of-visible-people-in-a-queue/
 pub fn can_see_persons_count(heights: Vec<i32>) -> Vec<i32> {
-    todo!()
+    let mut answer: Vec<i32> = vec![0; heights.len()];
+    let mut deque: VecDeque<i32> = VecDeque::new();
+    for i in (0..heights.len()).rev() {
+        let height = &heights[i];
+        let mut count = 0;
+        loop {
+            let front = deque.front();
+            match front {
+                Some(value) => {
+                    if height > value {
+                        deque.pop_front();
+                        count += 1;
+                    } else {
+                        count += 1;
+                        break;
+                    }
+                }
+                None => break,
+            }
+        }
+        answer[i] = count;
+        deque.push_front(*height);
+    }
+    answer
 }
 
 fn main() {
