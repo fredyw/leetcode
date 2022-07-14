afb3e3248c78d43bb7f35dbe3b984b6c6d20d467
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-07-14 16:37:42 -0500

    Update problem 2337

--- src/main/rust/src/bin/problem2337.rs
+++ src/main/rust/src/bin/problem2337.rs
@@ -11,4 +11,12 @@ fn main() {
     println!("{}", can_change(String::from("R_L_"), String::from("__LR"))); // true
     println!("{}", can_change(String::from("_R"), String::from("R_"))); // true
     println!("{}", can_change(String::from("LLLR"), String::from("LRRR"))); // false
+    println!(
+        "{}",
+        can_change(String::from("__L_R_L_R__"), String::from("L___RL_R___"))
+    ); // false
+    println!(
+        "{}",
+        can_change(String::from("__L_RL__R__"), String::from("L___RL_R___"))
+    ); // true
 }
