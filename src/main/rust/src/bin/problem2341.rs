0e9c0e6c262ff59dbbece0caf4ed8d532ab4e5b5
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-07-17 07:50:46 -0500

    Skeleton for problem 2341

---
+++ src/main/rust/src/bin/problem2341.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/maximum-number-of-pairs-in-array/
+pub fn number_of_pairs(nums: Vec<i32>) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", number_of_pairs(vec![1, 3, 2, 1, 3, 2, 2])); // [3,1]
+    println!("{:?}", number_of_pairs(vec![1, 1])); // [1,0]
+    println!("{:?}", number_of_pairs(vec![0])); // [0,1]
+}
