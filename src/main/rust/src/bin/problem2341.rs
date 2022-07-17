093d914b43d46439d4bb4bffce4ab809ed381f74
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-07-17 08:06:39 -0500

    Solve problem 2341

--- src/main/rust/src/bin/problem2341.rs
+++ src/main/rust/src/bin/problem2341.rs
@@ -1,6 +1,25 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/maximum-number-of-pairs-in-array/
 pub fn number_of_pairs(nums: Vec<i32>) -> Vec<i32> {
-    todo!()
+    let mut answer = vec![0; 2];
+    let mut map: HashMap<i32, i32> = HashMap::new();
+    for i in nums {
+        if !map.contains_key(&i) {
+            map.insert(i, 1);
+        } else {
+            map.insert(i, map.get(&i).unwrap() + 1);
+        }
+    }
+    let mut num_pairs = 0;
+    let mut num_leftover = 0;
+    for (_, count) in map.iter() {
+        num_pairs += count / 2;
+        num_leftover += count % 2;
+    }
+    answer[0] = num_pairs;
+    answer[1] = num_leftover;
+    answer
 }
 
 fn main() {
