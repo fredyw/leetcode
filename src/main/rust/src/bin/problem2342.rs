090334b23e0a2b6edecf5c1060fc3a71858a23ee
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-07-17 11:37:15 -0500

    Skeleton for problem 2342

---
+++ src/main/rust/src/bin/problem2342.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/max-sum-of-a-pair-with-equal-sum-of-digits/
+pub fn maximum_sum(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", maximum_sum(vec![18, 43, 36, 13, 7])); // 54
+    println!("{}", maximum_sum(vec![10, 12, 19, 14])); // -1
+}
