08ae6dad542771d11444b33d12ff2b2e5eec2200
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-07-18 23:56:23 -0500

    Skeleton for problem 1696

---
+++ src/main/rust/src/bin/problem1696.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/jump-game-vi/
+pub fn max_result(nums: Vec<i32>, k: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", max_result(vec![1, -1, -2, 4, -7, 3], 2)); // 7
+    println!("{}", max_result(vec![10, -5, -2, 4, 0, 3], 3)); // 17
+    println!("{}", max_result(vec![1, -5, -20, 4, -1, 3, -6, -3], 2)); // 0
+}
