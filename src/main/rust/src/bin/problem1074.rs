3926b2dde9ee8697b2f577908ad79e8b1fb4722e
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-07-20 16:44:36 -0500

    Update problem 1074

--- src/main/rust/src/bin/problem1074.rs
+++ src/main/rust/src/bin/problem1074.rs
@@ -1,6 +1,9 @@
 // https://leetcode.com/problems/number-of-submatrices-that-sum-to-target/
 pub fn num_submatrix_sum_target(matrix: Vec<Vec<i32>>, target: i32) -> i32 {
-    todo!()
+    let mut answer = 0;
+    let max_rows = matrix.len();
+    let max_cols = if max_rows > 0 { matrix[0].len() } else { 0 };
+    answer
 }
 
 fn main() {
