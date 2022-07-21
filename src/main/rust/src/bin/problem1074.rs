222cf82220eb35ac3b8f28146151fdc2291b6991
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-07-20 23:09:35 -0500

    Solve problem 1074

--- src/main/rust/src/bin/problem1074.rs
+++ src/main/rust/src/bin/problem1074.rs
@@ -5,26 +5,37 @@ pub fn num_submatrix_sum_target(matrix: Vec<Vec<i32>>, target: i32) -> i32 {
     let max_cols = if max_rows > 0 { matrix[0].len() } else { 0 };
     let mut prefix_sums = vec![vec![0; max_cols]; max_rows];
     for row in 0..max_rows {
+        let mut prefix_sum = 0;
         for col in 0..max_cols {
-            prefix_sums[row][col] = if col == 0 {
-                matrix[row][col]
-            } else {
-                prefix_sums[row][col - 1] + matrix[row][col]
-            };
+            prefix_sum += matrix[row][col];
+            prefix_sums[row][col] = prefix_sum
+                + if row > 0 {
+                    prefix_sums[row - 1][col]
+                } else {
+                    0
+                };
         }
     }
     for row1 in 0..max_rows {
         for col1 in 0..max_cols {
             for row2 in row1..max_rows {
                 for col2 in col1..max_cols {
-                    let mut sum = 0;
-                    for i in row1..=row2 {
-                        sum += if col1 == 0 {
-                            prefix_sums[i][col2]
-                        } else {
-                            prefix_sums[i][col2] - prefix_sums[i][col1 - 1]
-                        };
-                    }
+                    let a = if row1 > 0 {
+                        prefix_sums[row1 - 1][col2]
+                    } else {
+                        0
+                    };
+                    let b = if col1 > 0 {
+                        prefix_sums[row2][col1 - 1]
+                            - if row1 > 0 {
+                                prefix_sums[row1 - 1][col1 - 1]
+                            } else {
+                                0
+                            }
+                    } else {
+                        0
+                    };
+                    let sum = prefix_sums[row2][col2] - a - b;
                     if sum == target {
                         answer += 1;
                     }
