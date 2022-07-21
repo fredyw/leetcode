861127f1ef34971991defa4e18eb8a3b5d0521a8
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-07-20 22:20:50 -0500

    Update problem 1074

--- src/main/rust/src/bin/problem1074.rs
+++ src/main/rust/src/bin/problem1074.rs
@@ -3,7 +3,7 @@ pub fn num_submatrix_sum_target(matrix: Vec<Vec<i32>>, target: i32) -> i32 {
     let mut answer = 0;
     let max_rows = matrix.len();
     let max_cols = if max_rows > 0 { matrix[0].len() } else { 0 };
-    let mut prefix_sums = vec![vec![0; max_cols]; max_cols];
+    let mut prefix_sums = vec![vec![0; max_cols]; max_rows];
     for row in 0..max_rows {
         for col in 0..max_cols {
             prefix_sums[row][col] = if col == 0 {
@@ -17,7 +17,17 @@ pub fn num_submatrix_sum_target(matrix: Vec<Vec<i32>>, target: i32) -> i32 {
         for col1 in 0..max_cols {
             for row2 in row1..max_rows {
                 for col2 in col1..max_cols {
-                    todo!()
+                    let mut sum = 0;
+                    for i in row1..=row2 {
+                        sum += if col1 == 0 {
+                            prefix_sums[i][col2]
+                        } else {
+                            prefix_sums[i][col2] - prefix_sums[i][col1 - 1]
+                        };
+                    }
+                    if sum == target {
+                        answer += 1;
+                    }
                 }
             }
         }
@@ -26,19 +36,19 @@ pub fn num_submatrix_sum_target(matrix: Vec<Vec<i32>>, target: i32) -> i32 {
 }
 
 fn main() {
-    // println!(
-    //     "{}",
-    //     num_submatrix_sum_target(vec![vec![0, 0, 0], vec![1, -1, 1], vec![0, 1, 0]], 0)
-    // ); // 14
-    // println!(
-    //     "{}",
-    //     num_submatrix_sum_target(vec![vec![0, 1, 0], vec![1, 1, 1], vec![0, 1, 0]], 0)
-    // ); // 4
-    // println!(
-    //     "{}",
-    //     num_submatrix_sum_target(vec![vec![1, -1], vec![-1, 1]], 0)
-    // ); // 5
-    // println!("{}", num_submatrix_sum_target(vec![vec![904]], 0)); // 0
+    println!(
+        "{}",
+        num_submatrix_sum_target(vec![vec![0, 0, 0], vec![1, -1, 1], vec![0, 1, 0]], 0)
+    ); // 14
+    println!(
+        "{}",
+        num_submatrix_sum_target(vec![vec![0, 1, 0], vec![1, 1, 1], vec![0, 1, 0]], 0)
+    ); // 4
+    println!(
+        "{}",
+        num_submatrix_sum_target(vec![vec![1, -1], vec![-1, 1]], 0)
+    ); // 5
+    println!("{}", num_submatrix_sum_target(vec![vec![904]], 0)); // 0
     println!(
         "{}",
         num_submatrix_sum_target(
