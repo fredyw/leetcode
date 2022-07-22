3ee9dc3b8bc4185fd78e8d789fb83d9f4114810b
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-07-22 14:24:36 -0500

    Skeleton for problem 2327

---
+++ src/main/rust/src/bin/problem2327.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/number-of-people-aware-of-a-secret/
+pub fn people_aware_of_secret(n: i32, delay: i32, forget: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", people_aware_of_secret(6, 2, 4)); // 5
+    println!("{}", people_aware_of_secret(4, 1, 3)); // 6
+}
