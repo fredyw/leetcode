8a98bb9dbfb3ce328b0539680af7938f2f8b4e43
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-07-22 00:04:45 -0500

    Solve problem 2136

--- src/main/rust/src/bin/problem2136.rs
+++ src/main/rust/src/bin/problem2136.rs
@@ -1,6 +1,17 @@
 // https://leetcode.com/problems/earliest-possible-day-of-full-bloom/
 pub fn earliest_full_bloom(plant_time: Vec<i32>, grow_time: Vec<i32>) -> i32 {
-    todo!()
+    let mut answer = 0;
+    let mut v: Vec<(i32, i32)> = vec![];
+    for i in 0..plant_time.len() {
+        v.push((grow_time[i], plant_time[i]));
+    }
+    v.sort_by(|a, b| b.cmp(a));
+    let mut day = 0;
+    for (grow, plant) in v.iter() {
+        day += plant;
+        answer = answer.max(day + grow);
+    }
+    answer
 }
 
 fn main() {
