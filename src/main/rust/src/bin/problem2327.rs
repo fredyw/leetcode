47a5bfc5a60dc92f5d4d5d0b68392708a13b59a2
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-07-23 11:57:18 -0500

    Update problem 2327

--- src/main/rust/src/bin/problem2327.rs
+++ src/main/rust/src/bin/problem2327.rs
@@ -1,22 +1,22 @@
 // https://leetcode.com/problems/number-of-people-aware-of-a-secret/
 pub fn people_aware_of_secret(n: i32, delay: i32, forget: i32) -> i32 {
-    fn f(n: i32, delay: i32, forget: i32, day: i32, memo: &mut Vec<Vec<i64>>) -> i64 {
+    fn f(n: i32, delay: i32, forget: i32, day: i32, memo: &mut Vec<i64>) -> i64 {
         if day > n {
             return 0;
         }
         let secret_day = day + delay;
-        if memo[n as usize][secret_day as usize] != -1 {
-            return memo[n as usize][secret_day as usize];
+        if memo[secret_day as usize] != -1 {
+            return memo[secret_day as usize];
         }
         let mut sum: i64 = if day + forget <= n { 0 } else { 1 };
         for d in secret_day..(secret_day + (forget - delay)) {
             sum += f(n, delay, forget, d, memo) % 1_000_000_007;
         }
-        memo[n as usize][secret_day as usize] = sum;
+        memo[secret_day as usize] = sum;
         sum
     }
 
-    let mut memo = vec![vec![-1i64; (n + delay + 1) as usize]; (n + 1) as usize];
+    let mut memo = vec![-1i64; (n + delay + 1) as usize];
     (f(n, delay, forget, 1, &mut memo) % 1_000_000_007) as i32
 }
 
