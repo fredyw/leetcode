9743dcc6c470f27d19da0c613e4662fad360556b
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-07-23 11:44:49 -0500

    Solve problem 2327

--- src/main/rust/src/bin/problem2327.rs
+++ src/main/rust/src/bin/problem2327.rs
@@ -1,38 +1,30 @@
-use std::collections::LinkedList;
-
 // https://leetcode.com/problems/number-of-people-aware-of-a-secret/
 pub fn people_aware_of_secret(n: i32, delay: i32, forget: i32) -> i32 {
-    let mut answer: i64 = if 1 + forget > n { 1 } else { 0 };
-    let mut list: LinkedList<i32> = LinkedList::new();
-    list.push_back(1);
-    loop {
-        let front = list.pop_front();
-        match front {
-            Some(day) => {
-                if day > n {
-                    break;
-                }
-                let secret_day = day + delay;
-                for d in secret_day..(secret_day + (forget - delay)) {
-                    if d > n {
-                        break;
-                    }
-                    list.push_back(d);
-                    if d + forget > n {
-                        answer += 1;
-                    }
-                }
-            }
-            None => break,
+    fn f(n: i32, delay: i32, forget: i32, day: i32, memo: &mut Vec<Vec<i64>>) -> i64 {
+        if day > n {
+            return 0;
         }
+        let secret_day = day + delay;
+        if memo[n as usize][secret_day as usize] != -1 {
+            return memo[n as usize][secret_day as usize];
+        }
+        let mut sum: i64 = if day + forget <= n { 0 } else { 1 };
+        for d in secret_day..(secret_day + (forget - delay)) {
+            sum += f(n, delay, forget, d, memo) % 1_000_000_007;
+        }
+        memo[n as usize][secret_day as usize] = sum;
+        sum
     }
-    (answer % 1_000_000_007) as i32
+
+    let mut memo = vec![vec![-1i64; (n + delay + 1) as usize]; (n + delay + 1) as usize];
+    (f(n, delay, forget, 1, &mut memo) % 1_000_000_007) as i32
 }
 
 fn main() {
     println!("{}", people_aware_of_secret(6, 2, 4)); // 5
     println!("{}", people_aware_of_secret(4, 1, 3)); // 6
     println!("{}", people_aware_of_secret(10, 3, 5)); // 5
+    println!("{}", people_aware_of_secret(10, 2, 4)); // 14
     println!("{}", people_aware_of_secret(1000, 500, 600)); // 100
     println!("{}", people_aware_of_secret(100, 20, 30)); // 6775
     println!("{}", people_aware_of_secret(1000, 2, 1000)); // 517691607
