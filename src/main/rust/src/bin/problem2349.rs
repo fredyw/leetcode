15d9d94be18025e3014192ea48c6438c0ea62bd3
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-07-24 16:37:41 -0500

    Skeleton for problem 2349

---
+++ src/main/rust/src/bin/problem2349.rs
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/design-a-number-container-system/
+struct NumberContainers {}
+
+impl NumberContainers {
+    fn new() -> Self {
+        todo!()
+    }
+
+    fn change(&self, index: i32, number: i32) {
+        todo!()
+    }
+
+    fn find(&self, number: i32) -> i32 {
+        todo!()
+    }
+}
+
+fn main() {
+    let nc = NumberContainers::new();
+    println!("{}", nc.find(10)); // -1
+    nc.change(2, 10);
+    nc.change(1, 10);
+    nc.change(3, 10);
+    nc.change(5, 10);
+    println!("{}", nc.find(10)); // 1
+    nc.change(1, 20);
+    println!("{}", nc.find(10)); // 2.
+}
