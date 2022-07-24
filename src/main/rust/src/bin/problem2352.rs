d299ce37beb6c007e3ed31ab0269858ff4fd1b5b
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-07-23 23:51:30 -0500

    Solve problem 2352

--- src/main/rust/src/bin/problem2352.rs
+++ src/main/rust/src/bin/problem2352.rs
@@ -1,6 +1,33 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/equal-row-and-column-pairs/
 pub fn equal_pairs(grid: Vec<Vec<i32>>) -> i32 {
-    todo!()
+    let mut answer = 0;
+    let mut map: HashMap<String, i32> = HashMap::new();
+    let n = grid.len();
+    for col in 0..n {
+        let mut v: Vec<String> = vec![];
+        for row in 0..n {
+            v.push(grid[row][col].to_string());
+        }
+        let string = v.join(",");
+        match map.get(&string) {
+            Some(count) => map.insert(string, count + 1),
+            None => map.insert(string, 1),
+        };
+    }
+    for row in 0..n {
+        let string = grid[row]
+            .iter()
+            .map(|a| a.to_string())
+            .collect::<Vec<String>>()
+            .join(",");
+        match map.get(&string) {
+            Some(count) => answer += count,
+            None => (),
+        }
+    }
+    answer
 }
 
 fn main() {
@@ -17,4 +44,5 @@ fn main() {
             vec![2, 4, 2, 2]
         ])
     ); // 3
+    println!("{}", equal_pairs(vec![vec![11, 1], vec![1, 11]])); // 2
 }
