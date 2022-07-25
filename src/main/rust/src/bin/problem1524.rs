eb8be1706075668deae3b0f32a138723ce641c3d
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-07-24 22:57:48 -0500

    Skeleton for problem 1524

---
+++ src/main/rust/src/bin/problem1524.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/number-of-sub-arrays-with-odd-sum/
+pub fn num_of_subarrays(arr: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", num_of_subarrays(vec![1, 3, 5])); // 4
+    println!("{}", num_of_subarrays(vec![2, 4, 6])); // 0
+    println!("{}", num_of_subarrays(vec![1, 2, 3, 4, 5, 6, 7])); // 16
+}
