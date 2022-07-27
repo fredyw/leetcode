70a5eee4dd585fb98260ad5c8a3ddfcf8af89e68
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-07-26 23:48:57 -0500

    Skeleton for problem 2348

---
+++ src/main/rust/src/bin/problem2348.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/number-of-zero-filled-subarrays/
+pub fn zero_filled_subarray(nums: Vec<i32>) -> i64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", zero_filled_subarray(vec![1, 3, 0, 0, 2, 0, 0, 4])); // 6
+    println!("{}", zero_filled_subarray(vec![0, 0, 0, 2, 0, 0])); // 9
+    println!("{}", zero_filled_subarray(vec![2, 10, 2019])); // 0
+}
