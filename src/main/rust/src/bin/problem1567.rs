e5787e0e831b1c6f5e04cea991a46e6980c6fed7
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-07-27 18:03:30 -0500

    Skeleton for problem 1567

---
+++ src/main/rust/src/bin/problem1567.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/maximum-length-of-subarray-with-positive-product/
+pub fn get_max_len(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", get_max_len(vec![1, -2, -3, 4])); // 4
+    println!("{}", get_max_len(vec![0, 1, -2, -3, -4])); // 3
+    println!("{}", get_max_len(vec![-1, -2, -3, 0, 1])); // 2
+}
