211620783da50c43c93eae99720671566afa1c60
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-07-26 20:40:30 -0500

    Solve problem 1524

--- src/main/rust/src/bin/problem1524.rs
+++ src/main/rust/src/bin/problem1524.rs
@@ -1,13 +1,17 @@
 // https://leetcode.com/problems/number-of-sub-arrays-with-odd-sum/
 pub fn num_of_subarrays(arr: Vec<i32>) -> i32 {
     let mut answer: i64 = 0;
+    let mut odd_sum: i64 = 0;
+    let mut even_sum: i64 = 1;
+    let mut current_sum: i64 = 0;
     for i in 0..arr.len() {
-        let mut sum = 0;
-        for j in i..arr.len() {
-            sum += arr[j];
-            if sum % 2 != 0 {
-                answer += 1;
-            }
+        current_sum += arr[i] as i64;
+        if current_sum % 2 == 0 {
+            even_sum += 1;
+            answer += odd_sum;
+        } else {
+            odd_sum += 1;
+            answer += even_sum;
         }
     }
     (answer % 1_000_000_007) as i32
