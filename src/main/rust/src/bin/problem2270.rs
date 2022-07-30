2c302166e13ff5cee73247bcb91527384d63a045
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-07-30 18:25:45 -0500

    Solve problem 2270

--- src/main/rust/src/bin/problem2270.rs
+++ src/main/rust/src/bin/problem2270.rs
@@ -1,6 +1,22 @@
 // https://leetcode.com/problems/number-of-ways-to-split-array/
 pub fn ways_to_split_array(nums: Vec<i32>) -> i32 {
-    todo!()
+    let mut prefix_sums: Vec<i64> = vec![0; nums.len()];
+    for (i, num) in nums.iter().enumerate() {
+        prefix_sums[i] = if i == 0 {
+            *num as i64
+        } else {
+            prefix_sums[i - 1] + *num as i64
+        };
+    }
+    let mut answer = 0;
+    for i in 0..(nums.len() - 1) {
+        let left = prefix_sums[i];
+        let right = prefix_sums[nums.len() - 1] - prefix_sums[i];
+        if left >= right {
+            answer += 1;
+        }
+    }
+    answer
 }
 
 fn main() {
