79317b8acc9037ecc0a2d95cad269b53b81d27f8
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-07-30 10:57:16 -0500

    Skeleton for problem 1750

---
+++ src/main/rust/src/bin/problem1750.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/minimum-length-of-string-after-deleting-similar-ends/
+pub fn minimum_length(s: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", minimum_length("ca".to_string())); // 2
+    println!("{}", minimum_length("cabaabac".to_string())); // 0
+    println!("{}", minimum_length("aabccabba".to_string())); // 3
+}
