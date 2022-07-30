8ab529a1ea88fea1fdad973c9b9b168aa28a8e2f
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-07-30 15:44:04 -0500

    Skeleton for problem 2270

---
+++ src/main/rust/src/bin/problem2270.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/number-of-ways-to-split-array/
+pub fn ways_to_split_array(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", ways_to_split_array(vec![10, 4, -8, 7])); // 2
+    println!("{}", ways_to_split_array(vec![2, 3, 1, 0])); // 2
+}
