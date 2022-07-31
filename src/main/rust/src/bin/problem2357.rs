24d4e0442c1ad79f5e1cdf84c79931c7cd4c8e12
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-07-31 00:14:51 -0500

    Skeleton for problem 2357

---
+++ src/main/rust/src/bin/problem2357.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/make-array-zero-by-subtracting-equal-amounts/
+pub fn minimum_operations(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", minimum_operations(vec![1, 5, 0, 3, 5])); // 3
+    println!("{}", minimum_operations(vec![0])); // 0
+}
