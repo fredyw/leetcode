e5c0eb1e06ced49e0c3107f1aebbc7cfd6fd76d6
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-07-31 10:37:51 -0500

    Solve problem 2357

--- src/main/rust/src/bin/problem2357.rs
+++ src/main/rust/src/bin/problem2357.rs
@@ -1,6 +1,14 @@
+use std::collections::HashSet;
+
 // https://leetcode.com/problems/make-array-zero-by-subtracting-equal-amounts/
 pub fn minimum_operations(nums: Vec<i32>) -> i32 {
-    todo!()
+    let mut set: HashSet<i32> = HashSet::new();
+    for num in nums.iter() {
+        if *num != 0 {
+            set.insert(*num);
+        }
+    }
+    set.len() as i32
 }
 
 fn main() {
