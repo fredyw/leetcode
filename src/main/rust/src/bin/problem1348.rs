1796eb6f0bad491a256f0305959a707fbbb0173f
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-08-01 12:43:19 -0500

    Skeleton for problem 1348

---
+++ src/main/rust/src/bin/problem1348.rs
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/tweet-counts-per-frequency/
+struct TweetCounts {}
+
+impl TweetCounts {
+    fn new() -> Self {
+        todo!()
+    }
+
+    fn record_tweet(&mut self, tweet_name: String, time: i32) {
+        todo!()
+    }
+
+    fn get_tweet_counts_per_frequency(
+        &self,
+        freq: String,
+        tweet_name: String,
+        start_time: i32,
+        end_time: i32,
+    ) -> Vec<i32> {
+        todo!()
+    }
+}
+
+fn main() {
+    let mut tweet_counts = TweetCounts::new();
+    tweet_counts.record_tweet("tweet3".to_string(), 0);
+    tweet_counts.record_tweet("tweet3".to_string(), 60);
+    tweet_counts.record_tweet("tweet3".to_string(), 10);
+    tweet_counts.get_tweet_counts_per_frequency("minute".to_string(), "tweet3".to_string(), 0, 59); // [2]
+    tweet_counts.get_tweet_counts_per_frequency("minute".to_string(), "tweet3".to_string(), 0, 60); // [2,1]
+    tweet_counts.record_tweet("tweet3".to_string(), 120);
+    tweet_counts.get_tweet_counts_per_frequency("hour".to_string(), "tweet3".to_string(), 0, 210);
+    // [4]
+}
