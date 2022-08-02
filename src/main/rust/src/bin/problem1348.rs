be27d36808dbd8d7d4bf58c5feff45b5f2e27e68
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-08-01 23:11:58 -0500

    Update problem 1348

--- src/main/rust/src/bin/problem1348.rs
+++ src/main/rust/src/bin/problem1348.rs
@@ -1,9 +1,11 @@
+use std::collections::{BTreeMap, BTreeSet};
+
 // https://leetcode.com/problems/tweet-counts-per-frequency/
 struct TweetCounts {}
 
 impl TweetCounts {
     fn new() -> Self {
-        todo!()
+        TweetCounts {}
     }
 
     fn record_tweet(&mut self, tweet_name: String, time: i32) {
@@ -17,6 +19,11 @@ impl TweetCounts {
         start_time: i32,
         end_time: i32,
     ) -> Vec<i32> {
+        match tweet_name.as_str() {
+            "minute" => (),
+            "hour" => (),
+            _ => (),
+        };
         todo!()
     }
 }
@@ -25,10 +32,12 @@ fn main() {
     let mut tweet_counts = TweetCounts::new();
     tweet_counts.record_tweet("tweet3".to_string(), 0);
     tweet_counts.record_tweet("tweet3".to_string(), 60);
+    tweet_counts.record_tweet("tweet3".to_string(), 60);
     tweet_counts.record_tweet("tweet3".to_string(), 10);
-    tweet_counts.get_tweet_counts_per_frequency("minute".to_string(), "tweet3".to_string(), 0, 59); // [2]
-    tweet_counts.get_tweet_counts_per_frequency("minute".to_string(), "tweet3".to_string(), 0, 60); // [2,1]
+    tweet_counts.record_tweet("tweet3".to_string(), 10);
+    tweet_counts.get_tweet_counts_per_frequency("minute".to_string(), "tweet3".to_string(), 0, 59); // [3]
+    tweet_counts.get_tweet_counts_per_frequency("minute".to_string(), "tweet3".to_string(), 0, 60); // [3,2]
     tweet_counts.record_tweet("tweet3".to_string(), 120);
     tweet_counts.get_tweet_counts_per_frequency("hour".to_string(), "tweet3".to_string(), 0, 210);
-    // [4]
+    // [6]
 }
