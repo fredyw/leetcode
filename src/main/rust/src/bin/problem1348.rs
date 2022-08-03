55ecdbc527a60a476130b190a94622e857635f46
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-08-03 01:33:41 -0500

    Solve problem 1348

--- src/main/rust/src/bin/problem1348.rs
+++ src/main/rust/src/bin/problem1348.rs
@@ -1,4 +1,5 @@
-use std::collections::{BTreeMap, BTreeSet, HashMap};
+use std::collections::{BTreeMap, HashMap};
+use std::ops::Bound::{Excluded, Included};
 
 // https://leetcode.com/problems/tweet-counts-per-frequency/
 struct TweetCounts {
@@ -37,17 +38,38 @@ impl TweetCounts {
         start_time: i32,
         end_time: i32,
     ) -> Vec<i32> {
-        match tweet_name.as_str() {
-            "minute" => {
-                let mut time = start_time;
-                while time < end_time {
-                    time += 60;
+        fn f(
+            map: &HashMap<String, BTreeMap<i32, i32>>,
+            tweet_name: &String,
+            start_time: i32,
+            end_time: i32,
+            chunk: i32,
+            v: &mut Vec<i32>,
+        ) {
+            let mut time = start_time;
+            while time <= end_time {
+                match map.get(tweet_name) {
+                    Some(m) => {
+                        let count: i32 = m
+                            .range((Included(time), Excluded((end_time + 1).min(time + chunk))))
+                            .into_iter()
+                            .map(|a| a.1)
+                            .sum();
+                        v.push(count);
+                    }
+                    None => v.push(0),
                 }
+                time += chunk;
             }
-            "hour" => (),
-            _ => (),
+        }
+
+        let mut v = vec![];
+        match freq.as_str() {
+            "minute" => f(&self.map, &tweet_name, start_time, end_time, 60, &mut v),
+            "hour" => f(&self.map, &tweet_name, start_time, end_time, 3600, &mut v),
+            _ => f(&self.map, &tweet_name, start_time, end_time, 86400, &mut v),
         };
-        vec![]
+        v
     }
 }
 
@@ -58,9 +80,50 @@ fn main() {
     tweet_counts.record_tweet("tweet3".to_string(), 60);
     tweet_counts.record_tweet("tweet3".to_string(), 10);
     tweet_counts.record_tweet("tweet3".to_string(), 10);
-    tweet_counts.get_tweet_counts_per_frequency("minute".to_string(), "tweet3".to_string(), 0, 59); // [3]
-    tweet_counts.get_tweet_counts_per_frequency("minute".to_string(), "tweet3".to_string(), 0, 60); // [3,2]
+    println!(
+        "{:?}",
+        tweet_counts.get_tweet_counts_per_frequency(
+            "minute".to_string(),
+            "tweet3".to_string(),
+            0,
+            59
+        )
+    ); // [3]
+    println!(
+        "{:?}",
+        tweet_counts.get_tweet_counts_per_frequency(
+            "minute".to_string(),
+            "tweet3".to_string(),
+            0,
+            60
+        )
+    ); // [3,2]
     tweet_counts.record_tweet("tweet3".to_string(), 120);
-    tweet_counts.get_tweet_counts_per_frequency("hour".to_string(), "tweet3".to_string(), 0, 210);
-    // [6]
+    println!(
+        "{:?}",
+        tweet_counts.get_tweet_counts_per_frequency(
+            "hour".to_string(),
+            "tweet3".to_string(),
+            0,
+            210
+        )
+    ); // [6]
+    println!(
+        "{:?}",
+        tweet_counts.get_tweet_counts_per_frequency(
+            "hour".to_string(),
+            "tweet4".to_string(),
+            0,
+            210
+        )
+    ); // [0]
+    println!(
+        "{:?}",
+        tweet_counts.get_tweet_counts_per_frequency(
+            "hour".to_string(),
+            "tweet3".to_string(),
+            70,
+            80
+        )
+    ); // [0]
 }
