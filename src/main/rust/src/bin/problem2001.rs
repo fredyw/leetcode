73f3b0b98d655a4be24194a09073b5480c2bfcd7
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-08-03 01:35:59 -0500

    Skeleton for problem 2001

---
+++ src/main/rust/src/bin/problem2001.rs
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/number-of-pairs-of-interchangeable-rectangles/
+pub fn interchangeable_rectangles(rectangles: Vec<Vec<i32>>) -> i64 {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        interchangeable_rectangles(vec![vec![4, 8], vec![3, 6], vec![10, 20], vec![15, 30]])
+    ); // 6
+    println!(
+        "{}",
+        interchangeable_rectangles(vec![vec![4, 5], vec![7, 8]])
+    ); // 0
+}
