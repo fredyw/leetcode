db7b29bfad035e275ceb76bc5957d938c7d78ee6
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-08-03 22:56:23 -0500

    Skeleton for problem 2358

---
+++ src/main/rust/src/bin/problem2358.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/maximum-number-of-groups-entering-a-competition/
+pub fn maximum_groups(grades: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", maximum_groups(vec![10, 6, 12, 7, 3, 5])); // 3
+    println!("{}", maximum_groups(vec![8, 8])); // 1
+}
