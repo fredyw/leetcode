4370e33a497599019b2bc82c4cefcc0b557de313
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-08-04 10:36:15 -0500

    Solve problem 2358

--- src/main/rust/src/bin/problem2358.rs
+++ src/main/rust/src/bin/problem2358.rs
@@ -1,9 +1,20 @@
 // https://leetcode.com/problems/maximum-number-of-groups-entering-a-competition/
 pub fn maximum_groups(grades: Vec<i32>) -> i32 {
-    todo!()
+    let mut answer = 0;
+    let mut i = 1;
+    let mut j = i;
+    while j <= grades.len() {
+        i += 1;
+        j += i;
+        answer += 1;
+    }
+    answer
 }
 
 fn main() {
     println!("{}", maximum_groups(vec![10, 6, 12, 7, 3, 5])); // 3
     println!("{}", maximum_groups(vec![8, 8])); // 1
+    println!("{}", maximum_groups(vec![1, 1, 1, 1, 1])); // 2
+    println!("{}", maximum_groups(vec![1, 3, 5, 6, 1])); // 2
+    println!("{}", maximum_groups(vec![1, 3, 5, 6, 1, 1])); // 3
 }
