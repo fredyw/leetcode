6e6b67236bb57576026f30c0f5506e1de96221f4
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-08-07 16:24:57 -0500

    Skeleton for problem 2365

---
+++ src/main/rust/src/bin/problem2365.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/task-scheduler-ii/
+pub fn task_scheduler_ii(tasks: Vec<i32>, space: i32) -> i64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", task_scheduler_ii(vec![1, 2, 1, 2, 3, 1], 3)); // 9
+    println!("{}", task_scheduler_ii(vec![5, 8, 8, 5], 2)); // 6
+}
