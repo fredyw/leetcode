2e28deaaf94f206a52c3fbb5ac2553cdeee8a62d
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-08-07 09:13:17 -0500

    Skeleton for problem 2367

---
+++ src/main/rust/src/bin/problem2367.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/number-of-arithmetic-triplets/
+pub fn arithmetic_triplets(nums: Vec<i32>, diff: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", arithmetic_triplets(vec![0, 1, 4, 6, 7, 10], 3)); // 2
+    println!("{}", arithmetic_triplets(vec![4, 5, 6, 7, 8, 9], 2)); // 2
+}
