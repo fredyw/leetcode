9d0cf301c9574edf8b56ce4f5fb59e5c9c9fde0d
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-08-07 09:29:07 -0500

    Solve problem 2367

--- src/main/rust/src/bin/problem2367.rs
+++ src/main/rust/src/bin/problem2367.rs
@@ -1,6 +1,20 @@
+use std::collections::HashSet;
+
 // https://leetcode.com/problems/number-of-arithmetic-triplets/
 pub fn arithmetic_triplets(nums: Vec<i32>, diff: i32) -> i32 {
-    todo!()
+    let mut set: HashSet<i32> = HashSet::new();
+    for num in nums.iter() {
+        set.insert(*num);
+    }
+    let mut answer = 0;
+    for num in nums.into_iter() {
+        let a = num + diff;
+        let b = a + diff;
+        if set.get(&a).is_some() && set.get(&b).is_some() {
+            answer += 1;
+        }
+    }
+    answer
 }
 
 fn main() {
