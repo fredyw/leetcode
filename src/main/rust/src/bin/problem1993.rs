d74f1d62d8b9062df20c8ad29c4b55babda25b65
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-08-07 11:11:07 -0500

    Update problem 1993

--- src/main/rust/src/bin/problem1993.rs
+++ src/main/rust/src/bin/problem1993.rs
@@ -1,5 +1,10 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/operations-on-tree/
-struct LockingTree {}
+struct LockingTree {
+    tree: HashMap<i32, Vec<i32>>,
+    locks: HashMap<i32, i32>,
+}
 
 /**
  * `&self` means the method takes an immutable reference
@@ -7,24 +12,52 @@ struct LockingTree {}
  */
 impl LockingTree {
     fn new(parent: Vec<i32>) -> Self {
-        LockingTree {}
+        let mut tree = HashMap::new();
+        for (num, parent) in parent.into_iter().enumerate() {
+            let children = tree.entry(parent).or_insert(vec![]);
+            (*children).push(num as i32);
+        }
+        LockingTree {
+            tree,
+            locks: HashMap::new(),
+        }
     }
 
-    fn lock(&self, num: i32, user: i32) -> bool {
-        todo!()
+    fn lock(&mut self, num: i32, user: i32) -> bool {
+        match self.locks.get(&num) {
+            Some(_) => false,
+            None => {
+                self.locks.insert(num, user);
+                true
+            }
+        }
     }
 
-    fn unlock(&self, num: i32, user: i32) -> bool {
-        todo!()
+    fn unlock(&mut self, num: i32, user: i32) -> bool {
+        match self.locks.get(&num) {
+            Some(u) => {
+                if user != *u {
+                    return false;
+                }
+                self.locks.insert(num, user);
+                true
+            }
+            None => false,
+        }
     }
 
-    fn upgrade(&self, num: i32, user: i32) -> bool {
-        todo!()
+    fn upgrade(&mut self, num: i32, user: i32) -> bool {
+        match self.locks.get(&num) {
+            Some(_) => false,
+            None => {
+                todo!()
+            }
+        }
     }
 }
 
 fn main() {
-    let tree = LockingTree::new(vec![-1, 0, 0, 1, 1, 2, 2]);
+    let mut tree = LockingTree::new(vec![-1, 0, 0, 1, 1, 2, 2]);
     println!("{}", tree.lock(2, 2)); // true
     println!("{}", tree.unlock(2, 3)); // false
     println!("{}", tree.unlock(2, 2)); // true
