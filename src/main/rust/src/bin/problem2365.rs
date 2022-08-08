737e39025f4ba220a3322281470b08b08855106c
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-08-07 23:35:19 -0500

    Update problem 2365

--- src/main/rust/src/bin/problem2365.rs
+++ src/main/rust/src/bin/problem2365.rs
@@ -6,4 +6,5 @@ pub fn task_scheduler_ii(tasks: Vec<i32>, space: i32) -> i64 {
 fn main() {
     println!("{}", task_scheduler_ii(vec![1, 2, 1, 2, 3, 1], 3)); // 9
     println!("{}", task_scheduler_ii(vec![5, 8, 8, 5], 2)); // 6
+    println!("{}", task_scheduler_ii(vec![1, 2, 3], 3)); // 3
 }
