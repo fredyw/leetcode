98a186e0034b526a40fbc69f7d065e2f245aa9cc
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-08-09 12:20:33 -0500

    Skeleton for problem 1894

---
+++ src/main/rust/src/bin/problem1894.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/find-the-student-that-will-replace-the-chalk/
+pub fn chalk_replacer(chalk: Vec<i32>, k: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", chalk_replacer(vec![5, 1, 5], 22)); // 0
+    println!("{}", chalk_replacer(vec![3, 4, 1, 2], 25)); // 1
+}
