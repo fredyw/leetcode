48bb0dacc06307a7e36bd98d8e6d05883ce19770
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-08-11 00:45:28 -0500

    Skeleton for problem 2028

---
+++ src/main/rust/src/bin/problem2028.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/find-missing-observations/
+pub fn missing_rolls(rolls: Vec<i32>, mean: i32, n: i32) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", missing_rolls(vec![3, 2, 4, 3], 4, 2)); // [6,6]
+    println!("{:?}", missing_rolls(vec![1, 5, 6], 3, 4)); // [2,3,2,2]
+    println!("{:?}", missing_rolls(vec![1, 2, 3, 4], 6, 4)); // []
+}
