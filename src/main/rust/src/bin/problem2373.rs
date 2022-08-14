8cc1223c38ccbfb1af4e1c3a3dc062ac9894df17
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-08-13 23:38:58 -0500

    Solve problem 2373

--- src/main/rust/src/bin/problem2373.rs
+++ src/main/rust/src/bin/problem2373.rs
@@ -1,6 +1,19 @@
 // https://leetcode.com/problems/largest-local-values-in-a-matrix/
 pub fn largest_local(grid: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-    todo!()
+    let n = grid.len();
+    let mut answer = vec![vec![0; n - 2]; n - 2];
+    for i in 0..(n - 2) {
+        for j in 0..(n - 2) {
+            let mut max = 0;
+            for row in i..(i + 3) {
+                for col in j..(j + 3) {
+                    max = max.max(grid[row][col]);
+                }
+            }
+            answer[i][j] = max;
+        }
+    }
+    answer
 }
 
 fn main() {
