ecfeb40f2d013b4ea250f7b5378dc4d28c257203
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-08-18 00:26:20 -0500

    Solve problem 2028

--- src/main/rust/src/bin/problem2028.rs
+++ src/main/rust/src/bin/problem2028.rs
@@ -1,30 +1,15 @@
 // https://leetcode.com/problems/find-missing-observations/
 pub fn missing_rolls(rolls: Vec<i32>, mean: i32, n: i32) -> Vec<i32> {
-    fn f(n: i32, sum: i32, acc: &mut Vec<i32>) -> bool {
-        if n == 0 {
-            if sum == 0 {
-                return true;
-            }
-            return false;
-        }
-        for i in 1..=6 {
-            acc.push(i);
-            if f(n - 1, sum - i, acc) {
-                return true;
-            }
-            acc.pop();
-        }
-        false
-    }
-
     let m = rolls.len() as i32;
     let sum: i32 = rolls.into_iter().sum();
     let missing = (mean * (n + m)) - sum;
-    if missing > 6 * n {
+    if missing > 6 * n || missing <= 0 || missing < n {
         return vec![];
     }
-    let mut answer: Vec<i32> = vec![];
-    f(n, missing, &mut answer);
+    let mut answer: Vec<i32> = vec![missing / n; n as usize];
+    for i in 0..(missing % n) {
+        answer[i as usize] += 1;
+    }
     answer
 }
 
@@ -32,6 +17,7 @@ fn main() {
     println!("{:?}", missing_rolls(vec![3, 2, 4, 3], 4, 2)); // [6,6]
     println!("{:?}", missing_rolls(vec![1, 5, 6], 3, 4)); // [2,3,2,2]
     println!("{:?}", missing_rolls(vec![1, 2, 3, 4], 6, 4)); // []
+    println!("{:?}", missing_rolls(vec![6, 3, 4, 3, 5, 3], 1, 6)); // []
     println!(
         "{:?}",
         missing_rolls(
