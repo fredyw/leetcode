3d638ea8c0fc43400605182c1c7dd57a9abd8a1f
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-08-18 00:28:04 -0500

    Skeleton for problem 2375

---
+++ src/main/rust/src/bin/problem2375.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/construct-smallest-number-from-di-string/
+pub fn smallest_number(pattern: String) -> String {
+    todo!()
+}
+
+fn main() {
+    println!("{}", smallest_number("IIIDIDDD".to_string())); // "123549876"
+    println!("{}", smallest_number("DDD".to_string())); // "4321"
+}
