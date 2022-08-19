9dd1e5ef74888fbe96f5b899c8fcf127d243bce4
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-08-19 00:34:59 -0500

    Skeleton for problem 2374

---
+++ src/main/rust/src/bin/problem2374.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/node-with-highest-edge-score/
+pub fn edge_score(edges: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", edge_score(vec![1, 0, 0, 0, 0, 7, 7, 5])); // 7
+    println!("{}", edge_score(vec![2, 0, 0, 2])); // 0
+}
