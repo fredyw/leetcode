4a3d64219fb320dac58e9553fb8fc42f5f35516c
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-08-18 23:30:58 -0500

    Update problem 2375

--- src/main/rust/src/bin/problem2375.rs
+++ src/main/rust/src/bin/problem2375.rs
@@ -8,4 +8,7 @@ fn main() {
     println!("{}", smallest_number("DDD".to_string())); // "4321"
     println!("{}", smallest_number("IDIDIDID".to_string())); // "132547698"
     println!("{}", smallest_number("DIDIDIDI".to_string())); // "214365879"
+    println!("{}", smallest_number("IDIIDDII".to_string())); // "132476589"
+    println!("{}", smallest_number("IIIDDDII".to_string())); // "123765489"
+    println!("{}", smallest_number("DDDIIIDD".to_string())); // "432156987"
 }
