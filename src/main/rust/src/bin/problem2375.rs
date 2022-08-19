f18859536945d139da199bcf61bf138d9f79f2d1
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-08-18 23:12:06 -0500

    Update problem 2375

--- src/main/rust/src/bin/problem2375.rs
+++ src/main/rust/src/bin/problem2375.rs
@@ -6,4 +6,6 @@ pub fn smallest_number(pattern: String) -> String {
 fn main() {
     println!("{}", smallest_number("IIIDIDDD".to_string())); // "123549876"
     println!("{}", smallest_number("DDD".to_string())); // "4321"
+    println!("{}", smallest_number("IDIDIDID".to_string())); // "132547698"
+    println!("{}", smallest_number("DIDIDIDI".to_string())); // "214365879"
 }
