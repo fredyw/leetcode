bd2f4cce8ddde4937b010eb17879bfbc01068867
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-08-20 01:18:04 -0500

    Solve problem 2374

--- src/main/rust/src/bin/problem2374.rs
+++ src/main/rust/src/bin/problem2374.rs
@@ -1,6 +1,20 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/node-with-highest-edge-score/
 pub fn edge_score(edges: Vec<i32>) -> i32 {
-    todo!()
+    let mut map: HashMap<i32, i64> = HashMap::new();
+    for (i, edge) in edges.into_iter().enumerate() {
+        *map.entry(edge).or_insert(0) += i as i64;
+    }
+    *map.iter()
+        .max_by(|a, b| {
+            if a.1 == b.1 {
+                return b.0.cmp(a.0);
+            }
+            a.1.cmp(b.1)
+        })
+        .unwrap()
+        .0
 }
 
 fn main() {
