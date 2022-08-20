785e038d9ca8a4c49a7c88d775df67a020945010
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-08-20 01:20:03 -0500

    Skeleton for problem 2121

---
+++ src/main/rust/src/bin/problem2121.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/intervals-between-identical-elements/
+pub fn get_distances(arr: Vec<i32>) -> Vec<i64> {
+    todo!()
+}
+
+fn main() {
+    println!("{}", get_distances(vec![2, 1, 3, 1, 2, 3, 3])); // [4,2,7,2,4,4,5]
+    println!("{}", get_distances(vec![10, 5, 10, 10])); // [5,0,3,4]
+}
