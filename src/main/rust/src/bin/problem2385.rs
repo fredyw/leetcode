3b5b4c8cb34e15ec5943307ca7484e0c29fb11d8
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-08-21 16:24:01 -0500

    Update problem 2385

--- src/main/rust/src/bin/problem2385.rs
+++ src/main/rust/src/bin/problem2385.rs
@@ -25,6 +25,33 @@ pub fn amount_of_time(root: Option<Rc<RefCell<TreeNode>>>, start: i32) -> i32 {
 }
 
 fn main() {
+    fn add_left(
+        parent: &Option<Rc<RefCell<TreeNode>>>,
+        value: i32,
+    ) -> Option<Rc<RefCell<TreeNode>>> {
+        parent.as_ref().unwrap().as_ref().borrow_mut().left =
+            Some(Rc::new(RefCell::new(TreeNode::new(value))));
+        parent.as_ref().unwrap().as_ref().borrow().left.clone()
+    }
+    fn add_right(
+        parent: &Option<Rc<RefCell<TreeNode>>>,
+        value: i32,
+    ) -> Option<Rc<RefCell<TreeNode>>> {
+        parent.as_ref().unwrap().as_ref().borrow_mut().right =
+            Some(Rc::new(RefCell::new(TreeNode::new(value))));
+        parent.as_ref().unwrap().as_ref().borrow().right.clone()
+    }
+
+    let root = Some(Rc::new(RefCell::new(TreeNode::new(1))));
+    let parent = add_left(&root, 5);
+    let parent = add_right(&parent, 4);
+    add_left(&parent, 9);
+    add_right(&parent, 2);
+    let parent = add_right(&root, 3);
+    add_left(&parent, 10);
+    add_right(&parent, 6);
+    println!("{}", amount_of_time(root, 3)); // 4
+
     let root = Some(Rc::new(RefCell::new(TreeNode::new(1))));
     println!("{}", amount_of_time(root, 1)); // 0
 }
