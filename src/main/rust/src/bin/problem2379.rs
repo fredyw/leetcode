a8126a2147d95b71e62c7e0db02b99249ff41bbc
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-08-21 01:12:13 -0500

    Skeleton for problem 2379

---
+++ src/main/rust/src/bin/problem2379.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/minimum-recolors-to-get-k-consecutive-black-blocks/
+pub fn minimum_recolors(blocks: String, k: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", minimum_recolors("WBBWWBBWBW".to_string(), 7)); // 3
+    println!("{}", minimum_recolors("WBWBBBW".to_string(), 2)); // 0
+}
