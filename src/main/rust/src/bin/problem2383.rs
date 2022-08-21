df211e488ab3095b4b22fd57d73b512f13f29699
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-08-21 12:47:32 -0500

    Solve problem 2383

--- src/main/rust/src/bin/problem2383.rs
+++ src/main/rust/src/bin/problem2383.rs
@@ -6,7 +6,11 @@ pub fn min_number_of_hours(
     experience: Vec<i32>,
 ) -> i32 {
     let total_energy: i32 = energy.into_iter().sum();
-    let energy_required = total_energy - initial_energy + 1;
+    let energy_required = if total_energy >= initial_energy {
+        total_energy - initial_energy + 1
+    } else {
+        0
+    };
     let mut experience_required = 0;
     let mut total_experience = initial_experience;
     for exp in experience.into_iter() {
@@ -14,23 +18,22 @@ pub fn min_number_of_hours(
             let training = exp - total_experience + 1;
             experience_required += training;
             total_experience += training;
-        } else {
-            total_experience += exp;
         }
+        total_experience += exp;
     }
     energy_required + experience_required
 }
 
 fn main() {
-    // println!(
-    //     "{}",
-    //     min_number_of_hours(5, 3, vec![1, 4, 3, 2], vec![2, 6, 3, 1])
-    // ); // 8
-    // println!("{}", min_number_of_hours(2, 4, vec![1], vec![3])); // 0
-    // println!(
-    //     "{}",
-    //     min_number_of_hours(1, 1, vec![1, 1, 1, 1], vec![1, 1, 1, 50])
-    // ); // 51
+    println!(
+        "{}",
+        min_number_of_hours(5, 3, vec![1, 4, 3, 2], vec![2, 6, 3, 1])
+    ); // 8
+    println!("{}", min_number_of_hours(2, 4, vec![1], vec![3])); // 0
+    println!(
+        "{}",
+        min_number_of_hours(1, 1, vec![1, 1, 1, 1], vec![1, 1, 1, 50])
+    ); // 51
     println!("{}", min_number_of_hours(1, 1, vec![1, 1], vec![1, 50])); // 51
-                                                                        // println!("{}", min_number_of_hours(5, 3, vec![1, 4], vec![2, 5])); // 2
+    println!("{}", min_number_of_hours(5, 3, vec![1, 4], vec![2, 5])); // 2
 }
