b9d084f577cdaf09e449ba98ae8ae112c782a0c7
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-08-21 22:50:40 -0500

    Update problem 2380

--- src/main/rust/src/bin/problem2380.rs
+++ src/main/rust/src/bin/problem2380.rs
@@ -2,13 +2,20 @@
 pub fn seconds_to_remove_occurrences(s: String) -> i32 {
     let mut answer = 0;
     let mut chars: Vec<char> = s.chars().collect();
-    let mut i = 0;
-    while i < chars.len() - 1 {
-        if chars[i] == '0' && chars[i + 1] == '1' {
-            chars.swap(i, i + 1);
-            answer += 1;
+    loop {
+        let mut i = 0;
+        let mut count = 0;
+        while i < chars.len() - 1 {
+            if chars[i] == '0' && chars[i + 1] == '1' {
+                chars.swap(i, i + 1);
+                count += 1;
+            }
+            i += 1;
         }
-        i += 1;
+        if count == 0 {
+            break;
+        }
+        answer += 1;
     }
     answer
 }
