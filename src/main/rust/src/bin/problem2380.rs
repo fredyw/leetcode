f5cbc2fe0d867f316ff65d45fe8ecd8e68ebba22
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-08-21 22:44:01 -0500

    Update problem 2380

--- src/main/rust/src/bin/problem2380.rs
+++ src/main/rust/src/bin/problem2380.rs
@@ -1,9 +1,23 @@
 // https://leetcode.com/problems/time-needed-to-rearrange-a-binary-string/
 pub fn seconds_to_remove_occurrences(s: String) -> i32 {
-    todo!()
+    let mut answer = 0;
+    let mut chars: Vec<char> = s.chars().collect();
+    let mut i = 0;
+    while i < chars.len() - 1 {
+        if chars[i] == '0' && chars[i + 1] == '1' {
+            chars.swap(i, i + 1);
+            answer += 1;
+        }
+        i += 1;
+    }
+    answer
 }
 
 fn main() {
     println!("{}", seconds_to_remove_occurrences("0110101".to_string())); // 4
     println!("{}", seconds_to_remove_occurrences("11100".to_string())); // 0
+    println!(
+        "{}",
+        seconds_to_remove_occurrences("1000111001010010010101010101".to_string())
+    ); // 15
 }
