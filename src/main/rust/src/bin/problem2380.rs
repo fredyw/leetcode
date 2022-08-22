94cca3e4e06ccde42ff1bcf2bb562cbde9edfe80
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-08-21 19:45:56 -0500

    Skeleton for problem 2380

---
+++ src/main/rust/src/bin/problem2380.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/time-needed-to-rearrange-a-binary-string/
+pub fn seconds_to_remove_occurrences(s: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", seconds_to_remove_occurrences("0110101".to_string())); // 4
+    println!("{}", seconds_to_remove_occurrences("11100".to_string())); // 0
+}
