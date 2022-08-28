2edd7de81b70814891050f4aa9d41832382c644d
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-08-28 09:47:56 -0500

    Solve problem 2389

--- src/main/rust/src/bin/problem2389.rs
+++ src/main/rust/src/bin/problem2389.rs
@@ -1,6 +1,22 @@
 // https://leetcode.com/problems/longest-subsequence-with-limited-sum/
-pub fn answer_queries(nums: Vec<i32>, queries: Vec<i32>) -> Vec<i32> {
-    todo!()
+pub fn answer_queries(mut nums: Vec<i32>, queries: Vec<i32>) -> Vec<i32> {
+    let mut answer: Vec<i32> = vec![];
+    nums.sort();
+    let mut prefix_sums: Vec<i32> = vec![0; nums.len()];
+    for (i, num) in nums.iter().enumerate() {
+        if i == 0 {
+            prefix_sums[i] = *num;
+        } else {
+            prefix_sums[i] = prefix_sums[i - 1] + num;
+        }
+    }
+    for query in queries.into_iter() {
+        match prefix_sums.binary_search(&query) {
+            Ok(i) => answer.push((i + 1) as i32),
+            Err(i) => answer.push(i as i32),
+        }
+    }
+    answer
 }
 
 fn main() {
