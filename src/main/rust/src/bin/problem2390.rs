6a6b247f9e038e9afcfdc478565bd61fcfa53e8b
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-08-28 13:00:36 -0500

    Solve problem 2390

--- src/main/rust/src/bin/problem2390.rs
+++ src/main/rust/src/bin/problem2390.rs
@@ -1,6 +1,17 @@
+use std::collections::VecDeque;
+
 // https://leetcode.com/problems/removing-stars-from-a-string/
 pub fn remove_stars(s: String) -> String {
-    todo!()
+    let mut deque: VecDeque<char> = VecDeque::new();
+    let chars: Vec<char> = s.chars().collect();
+    for c in chars.into_iter() {
+        if c == '*' {
+            deque.pop_back();
+        } else {
+            deque.push_back(c);
+        }
+    }
+    deque.iter().collect::<String>()
 }
 
 fn main() {
