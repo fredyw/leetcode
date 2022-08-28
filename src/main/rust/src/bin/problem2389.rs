80b75a0e29f12d1a1200f533d38346a7d3dd8e5c
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-08-28 00:08:56 -0500

    Skeleton for problem 2389

---
+++ src/main/rust/src/bin/problem2389.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/longest-subsequence-with-limited-sum/
+pub fn answer_queries(nums: Vec<i32>, queries: Vec<i32>) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", answer_queries(vec![4, 5, 2, 1], vec![3, 10, 21])); // [2,3,4]
+    println!("{:?}", answer_queries(vec![2, 3, 4, 5], vec![1])); // [0]
+}
