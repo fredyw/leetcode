d2a168c1c8bcbce4965afd80f1690436f27cacf1
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-08-28 12:07:00 -0500

    Solve problem 2391

--- src/main/rust/src/bin/problem2391.rs
+++ src/main/rust/src/bin/problem2391.rs
@@ -1,6 +1,54 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/minimum-amount-of-time-to-collect-garbage/
 pub fn garbage_collection(garbage: Vec<String>, travel: Vec<i32>) -> i32 {
-    todo!()
+    fn garbage_map(garbage: String) -> HashMap<char, i32> {
+        let mut map: HashMap<char, i32> = HashMap::new();
+        let chars: Vec<char> = garbage.chars().collect();
+        for c in chars.into_iter() {
+            *map.entry(c).or_insert(0) += 1;
+        }
+        map
+    }
+
+    let mut garbage_vec: Vec<HashMap<char, i32>> = vec![];
+    let mut metal_time = 0;
+    let mut paper_time = 0;
+    let mut glass_time = 0;
+    let mut metal_last_index = 0;
+    let mut paper_last_index = 0;
+    let mut glass_last_index = 0;
+    for (i, g) in garbage.into_iter().enumerate() {
+        let map = garbage_map(g);
+        if let Some(count) = map.get(&'M') {
+            metal_time += count;
+            metal_last_index = i;
+        }
+        if let Some(count) = map.get(&'P') {
+            paper_time += count;
+            paper_last_index = i;
+        }
+        if let Some(count) = map.get(&'G') {
+            glass_time += count;
+            glass_last_index = i;
+        }
+        garbage_vec.push(map);
+    }
+    for i in 0..garbage_vec.len() {
+        if i == 0 {
+            continue;
+        }
+        if metal_last_index >= i {
+            metal_time += travel[i - 1];
+        }
+        if paper_last_index >= i {
+            paper_time += travel[i - 1];
+        }
+        if glass_last_index >= i {
+            glass_time += travel[i - 1];
+        }
+    }
+    metal_time + paper_time + glass_time
 }
 
 fn main() {
