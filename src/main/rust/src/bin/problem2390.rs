2d24731736340d01c7ccb7538da166e6a2b12c65
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-08-28 12:09:08 -0500

    Skeleton for problem 2390

---
+++ src/main/rust/src/bin/problem2390.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/removing-stars-from-a-string/
+pub fn remove_stars(s: String) -> String {
+    todo!()
+}
+
+fn main() {
+    println!("{}", remove_stars("leet**cod*e".to_string())); // "lecoe"
+    println!("{}", remove_stars("erase*****".to_string())); // ""
+}
