23ea61ad12fd373a8eddbfb2c72c21b644806c06
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-09-02 17:34:00 -0500

    Solve problem 2008

--- src/main/rust/src/bin/problem2008.rs
+++ src/main/rust/src/bin/problem2008.rs
@@ -1,6 +1,9 @@
 // https://leetcode.com/problems/maximum-earnings-from-taxi/
 pub fn max_taxi_earnings(n: i32, mut rides: Vec<Vec<i32>>) -> i64 {
     fn f(rides: &Vec<Vec<i32>>, index: usize, memo: &mut Vec<i64>) -> i64 {
+        if index >= rides.len() {
+            return 0;
+        }
         if memo[index] != -1 {
             return memo[index];
         }
@@ -8,37 +11,24 @@ pub fn max_taxi_earnings(n: i32, mut rides: Vec<Vec<i32>>) -> i64 {
         let start = v[0];
         let end = v[1];
         let tip = v[2];
-        let earn = (end - start + tip) as i64;
+        let profit = (end - start + tip) as i64;
         let mut i: i32 = match rides.binary_search_by(|a| a.get(0).unwrap().cmp(&end)) {
             Ok(i) => i as i32,
             Err(i) => i as i32,
         };
         if let Some(v) = rides.get(i as usize) {
             let s = v.get(0).unwrap();
-            let mut found = false;
-            while i >= 0 && rides.get(i as usize).unwrap().get(0).unwrap() == s {
-                found = true;
+            while i - 1 >= 0 && rides.get(i as usize - 1).unwrap().get(0).unwrap() == s {
                 i -= 1;
             }
-            if found {
-                i += 1;
-            }
         }
-        let mut max = earn;
-        for j in (i as usize)..rides.len() {
-            max = max.max(f(rides, j, memo) + earn);
-        }
+        let max = f(rides, index as usize + 1, memo).max(f(rides, i as usize, memo) + profit);
         memo[index] = max;
         max
     }
 
     rides.sort_by(|a, b| a.get(0).unwrap().cmp(b.get(0).unwrap()));
-    let mut answer = 0;
-    let mut memo = vec![-1; n as usize];
-    for i in 0..rides.len() {
-        answer = answer.max(f(&rides, i, &mut memo));
-    }
-    answer
+    f(&rides, 0, &mut vec![-1; rides.len()])
 }
 
 fn main() {
@@ -78,4 +68,18 @@ fn main() {
             ]
         )
     ); // 32
+    println!(
+        "{}",
+        max_taxi_earnings(
+            4,
+            vec![
+                vec![2, 3, 5],
+                vec![1, 3, 2],
+                vec![2, 4, 3],
+                vec![1, 4, 1],
+                vec![3, 4, 10],
+                vec![1, 3, 4]
+            ]
+        )
+    ); // 17
 }
