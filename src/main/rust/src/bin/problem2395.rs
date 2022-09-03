3286b6bba16f2f01075f6cd98086f74e03efc918
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-09-03 18:24:08 -0500

    Skeleton for problem 2395

---
+++ src/main/rust/src/bin/problem2395.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/find-subarrays-with-equal-sum/
+pub fn find_subarrays(nums: Vec<i32>) -> bool {
+    todo!()
+}
+
+fn main() {
+    println!("{}", find_subarrays(vec![4, 2, 4])); // true
+    println!("{}", find_subarrays(vec![1, 2, 3, 4, 5])); // false
+    println!("{}", find_subarrays(vec![0, 0, 0])); // true
+}
