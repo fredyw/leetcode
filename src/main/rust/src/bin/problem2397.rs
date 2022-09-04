9cbc86209a1614c8d923c8175ded487f87f8afb1
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-09-04 12:06:30 -0500

    Skeleton for problem 2397

---
+++ src/main/rust/src/bin/problem2397.rs
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/maximum-rows-covered-by-columns/
+pub fn maximum_rows(mat: Vec<Vec<i32>>, cols: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        maximum_rows(
+            vec![vec![0, 0, 0], vec![1, 0, 1], vec![0, 1, 1], vec![0, 0, 1]],
+            2
+        )
+    ); // 3
+    println!("{}", maximum_rows(vec![vec![1], vec![0]], 1)); // 2
+}
