afbbb075045b7ddff6ea915af5d7e7407e4f6590
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-09-03 19:01:23 -0500

    Skeleton for problem 1936

---
+++ src/main/rust/src/bin/problem1936.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/add-minimum-number-of-rungs/
+pub fn add_rungs(rungs: Vec<i32>, dist: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", add_rungs(vec![1, 3, 5, 10], 2)); // 2
+    println!("{}", add_rungs(vec![3, 6, 8, 10], 3)); // 0
+    println!("{}", add_rungs(vec![3, 4, 6, 7], 2)); // 1
+}
