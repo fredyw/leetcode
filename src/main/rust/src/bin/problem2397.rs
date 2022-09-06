7036a7a227d463b1ccb77dd77ab9a7e65648b024
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-09-06 17:04:15 -0500

    Update problem 2397

--- src/main/rust/src/bin/problem2397.rs
+++ src/main/rust/src/bin/problem2397.rs
@@ -11,5 +11,31 @@ fn main() {
             2
         )
     ); // 3
+    println!(
+        "{}",
+        maximum_rows(
+            vec![
+                vec![0, 0, 0],
+                vec![1, 0, 1],
+                vec![0, 1, 1],
+                vec![0, 0, 1],
+                vec![1, 1, 1]
+            ],
+            2
+        )
+    ); // 3
+    println!(
+        "{}",
+        maximum_rows(
+            vec![
+                vec![0, 0, 0],
+                vec![1, 0, 1],
+                vec![0, 1, 1],
+                vec![0, 0, 1],
+                vec![1, 1, 1]
+            ],
+            3
+        )
+    ); // 5
     println!("{}", maximum_rows(vec![vec![1], vec![0]], 1)); // 2
 }
