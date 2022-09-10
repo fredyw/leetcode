f28b5dca33fcc5b38e15e907ffccd7bd7dd2d4b7
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-09-10 17:28:53 -0500

    Skeleton for problem 2401

---
+++ src/main/rust/src/bin/problem2401.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/longest-nice-subarray/
+pub fn longest_nice_subarray(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", longest_nice_subarray(vec![1, 3, 8, 48, 10])); // 3
+    println!("{}", longest_nice_subarray(vec![3, 1, 5, 11, 13])); // 1
+}
