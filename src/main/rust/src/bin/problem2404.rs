4ea65ec2bf9aedd4d2da671e90cfc31213c84347
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-09-11 11:17:03 -0500

    Solve problem 2404

--- src/main/rust/src/bin/problem2404.rs
+++ src/main/rust/src/bin/problem2404.rs
@@ -1,6 +1,25 @@
+use std::cmp::Ordering;
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/most-frequent-even-element/
 pub fn most_frequent_even(nums: Vec<i32>) -> i32 {
-    todo!()
+    let mut map: HashMap<i32, i32> = HashMap::new();
+    for num in nums.into_iter() {
+        if num % 2 == 0 {
+            *map.entry(num).or_insert(0) += 1;
+        }
+    }
+    if map.is_empty() {
+        -1
+    } else {
+        *map.iter()
+            .max_by(|a, b| match a.1.cmp(b.1) {
+                Ordering::Equal => b.0.cmp(a.0),
+                _ => a.1.cmp(b.1),
+            })
+            .unwrap()
+            .0
+    }
 }
 
 fn main() {
