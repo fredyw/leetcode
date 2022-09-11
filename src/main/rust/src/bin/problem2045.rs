6b3ab967ae3dc663b1cf29bc0cb56899ca79f198
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-09-11 15:19:02 -0500

    Skeleton for problem 2045

---
+++ src/main/rust/src/bin/problem2045.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/optimal-partition-of-string/
+pub fn partition_string(s: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", partition_string("abacaba".to_string())); // 4
+    println!("{}", partition_string("ssssss".to_string())); // 6
+}
