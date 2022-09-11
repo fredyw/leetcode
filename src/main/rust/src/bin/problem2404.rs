ef6f4306d306721dd2922908bac9c6b10191c464
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-09-11 10:51:10 -0500

    Skeleton for problem 2404

---
+++ src/main/rust/src/bin/problem2404.rs
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/most-frequent-even-element/
+pub fn most_frequent_even(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", most_frequent_even(vec![0, 1, 2, 2, 4, 4, 1])); // 2
+    println!("{}", most_frequent_even(vec![4, 4, 4, 9, 2, 4])); // 4
+    println!(
+        "{}",
+        most_frequent_even(vec![29, 47, 21, 41, 13, 37, 25, 7])
+    ); // -1
+}
