ea8e2960aa820b2888dd4707f8461fc3ef810145
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-09-16 00:03:20 -0500

    Skeleton for problem 1996

---
+++ src/main/rust/src/bin/problem1996.rs
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/the-number-of-weak-characters-in-the-game/
+pub fn number_of_weak_characters(properties: Vec<Vec<i32>>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        number_of_weak_characters(vec![vec![5, 5], vec![6, 3], vec![3, 6]])
+    ); // 0
+    println!(
+        "{}",
+        number_of_weak_characters(vec![vec![2, 2], vec![3, 3]])
+    ); // 1
+    println!(
+        "{}",
+        number_of_weak_characters(vec![vec![1, 5], vec![10, 4], vec![4, 3]])
+    ); // 1
+}
