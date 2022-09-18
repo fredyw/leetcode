7dedea359853ca4d6821bd4588edd2b10e414578
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-09-18 11:16:37 -0500

    Update problem 2415

--- src/main/rust/src/bin/problem2415.rs
+++ src/main/rust/src/bin/problem2415.rs
@@ -24,4 +24,38 @@ pub fn reverse_odd_levels(root: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefC
     todo!()
 }
 
-fn main() {}
+fn main() {
+    fn create_node(value: i32) -> Option<Rc<RefCell<TreeNode>>> {
+        Some(Rc::new(RefCell::new(TreeNode::new(value))))
+    }
+
+    fn add_left(
+        parent: &Option<Rc<RefCell<TreeNode>>>,
+        value: i32,
+    ) -> Option<Rc<RefCell<TreeNode>>> {
+        parent.as_ref().unwrap().as_ref().borrow_mut().left = create_node(value);
+        parent.as_ref().unwrap().as_ref().borrow().left.clone()
+    }
+
+    fn add_right(
+        parent: &Option<Rc<RefCell<TreeNode>>>,
+        value: i32,
+    ) -> Option<Rc<RefCell<TreeNode>>> {
+        parent.as_ref().unwrap().as_ref().borrow_mut().right = create_node(value);
+        parent.as_ref().unwrap().as_ref().borrow().right.clone()
+    }
+
+    let root = create_node(2);
+    let left = add_left(&root, 3);
+    add_left(&left, 8);
+    add_right(&left, 13);
+    let right = add_right(&root, 5);
+    add_left(&right, 21);
+    add_right(&right, 34);
+    println!("{:?}", reverse_odd_levels(root)); // [2,5,3,8,13,21,34]
+
+    let root = create_node(7);
+    add_left(&root, 13);
+    add_right(&root, 11);
+    println!("{:?}", reverse_odd_levels(root)); // [7,11,13]
+}
