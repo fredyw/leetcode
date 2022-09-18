eee649c183e08425e353f601ace6d5ba1123466f
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-09-18 09:57:14 -0500

    Skeleton for problem 2413

---
+++ src/main/rust/src/bin/problem2413.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/smallest-even-multiple/
+pub fn smallest_even_multiple(n: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", smallest_even_multiple(5)); // 10
+    println!("{}", smallest_even_multiple(6)); // 6
+}
