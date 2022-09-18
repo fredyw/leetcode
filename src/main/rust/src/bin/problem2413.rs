d25310cf7c70ecc3ab1469b2b5435e230e538d61
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-09-18 10:02:03 -0500

    Solve problem 2413

--- src/main/rust/src/bin/problem2413.rs
+++ src/main/rust/src/bin/problem2413.rs
@@ -1,9 +1,16 @@
 // https://leetcode.com/problems/smallest-even-multiple/
 pub fn smallest_even_multiple(n: i32) -> i32 {
-    todo!()
+    if n % 2 == 0 {
+        n
+    } else {
+        n * 2
+    }
 }
 
 fn main() {
     println!("{}", smallest_even_multiple(5)); // 10
     println!("{}", smallest_even_multiple(6)); // 6
+    println!("{}", smallest_even_multiple(7)); // 14
+    println!("{}", smallest_even_multiple(1)); // 2
+    println!("{}", smallest_even_multiple(4)); // 4
 }
