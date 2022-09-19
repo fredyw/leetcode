3408d76aaa269f5e3d2a437c3f873f5662a80e69
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-09-18 19:35:41 -0500

    Skeleton for problem 2410

---
+++ src/main/rust/src/bin/problem2410.rs
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/maximum-matching-of-players-with-trainers/
+pub fn match_players_and_trainers(players: Vec<i32>, trainers: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        match_players_and_trainers(vec![4, 7, 9], vec![8, 2, 5, 8])
+    ); // 2
+    println!("{}", match_players_and_trainers(vec![1, 1, 1], vec![10])); // 1
+}
