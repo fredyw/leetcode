f5dd9716b98f71cbf23e55916cb25b75fb6df75b
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-09-18 23:59:23 -0500

    Skeleton for problem 2414

---
+++ src/main/rust/src/bin/problem2414.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/length-of-the-longest-alphabetical-continuous-substring/
+pub fn longest_continuous_substring(s: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", longest_continuous_substring("abacaba".to_string())); // 2
+    println!("{}", longest_continuous_substring("abcde".to_string())); // 5
+}
