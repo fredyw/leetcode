bc5b4f63be3637656399cfaf9d7ad8f8301de62a
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-09-20 23:53:12 -0500

    Skeleton for problem 2406

---
+++ src/main/rust/src/bin/problem2406.rs
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/divide-intervals-into-minimum-number-of-groups/
+pub fn min_groups(intervals: Vec<Vec<i32>>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        min_groups(vec![
+            vec![5, 10],
+            vec![6, 8],
+            vec![1, 5],
+            vec![2, 3],
+            vec![1, 10]
+        ])
+    ); // 3
+    println!(
+        "{}",
+        min_groups(vec![vec![1, 3], vec![5, 6], vec![8, 10], vec![11, 13]])
+    ); // 1
+}
