3cc95e1555c9a627f3259ee4f9d76c2bb2d02a2f
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-10-01 09:56:47 -0500

    Skeleton for problem 2419

---
+++ src/main/rust/src/bin/problem2419.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/longest-subarray-with-maximum-bitwise-and/
+pub fn longest_subarray(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", longest_subarray(vec![1, 2, 3, 3, 2, 2])); // 2
+    println!("{}", longest_subarray(vec![1, 2, 3, 4])); // 1
+}
