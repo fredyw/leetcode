f3257f1b4aa324181df1452de4fe146725d7a66e
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-10-02 00:08:29 -0500

    Skeleton for problem 2423

---
+++ src/main/rust/src/bin/problem2423.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/remove-letter-to-equalize-frequency/
+pub fn equal_frequency(word: String) -> bool {
+    todo!()
+}
+
+fn main() {
+    println!("{}", equal_frequency("abcc".to_string())); // true
+    println!("{}", equal_frequency("aazz".to_string())); // false
+}
