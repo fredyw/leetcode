c381fd330a3378a7a1145cb53d7a25e89c18b36f
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-10-02 00:02:02 -0500

    Skeleton for problem 2427

---
+++ src/main/rust/src/bin/problem2427.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/number-of-common-factors/
+pub fn common_factors(a: i32, b: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", common_factors(12, 6)); // 4
+    println!("{}", common_factors(25, 30)); // 2
+}
