6d1ad67edf0ff9703cbbbc3fe81c022325b684df
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-10-02 16:12:36 -0500

    Solve problem 2428

--- src/main/rust/src/bin/problem2428.rs
+++ src/main/rust/src/bin/problem2428.rs
@@ -1,6 +1,27 @@
 // https://leetcode.com/problems/maximum-sum-of-an-hourglass/
 pub fn max_sum(grid: Vec<Vec<i32>>) -> i32 {
-    todo!()
+    fn hourglass_sum(grid: &Vec<Vec<i32>>, row: usize, col: usize) -> i32 {
+        let max_row = grid.len();
+        let max_col = if max_row > 0 { grid[0].len() } else { 0 };
+        if row + 2 >= max_row || col + 2 >= max_col {
+            return 0;
+        }
+        grid[row][col]
+            + grid[row][col + 1]
+            + grid[row][col + 2]
+            + grid[row + 1][col + 1]
+            + grid[row + 2][col]
+            + grid[row + 2][col + 1]
+            + grid[row + 2][col + 2]
+    }
+
+    let mut answer = 0;
+    for i in 0..grid.len() {
+        for j in 0..grid[i].len() {
+            answer = answer.max(hourglass_sum(&grid, i, j));
+        }
+    }
+    answer
 }
 
 fn main() {
