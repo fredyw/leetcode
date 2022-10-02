b786ae8470fbc5ee4fe553e1f275bbc30eb138d6
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-10-02 15:10:47 -0500

    Solve problem 2419

--- src/main/rust/src/bin/problem2419.rs
+++ src/main/rust/src/bin/problem2419.rs
@@ -1,9 +1,32 @@
 // https://leetcode.com/problems/longest-subarray-with-maximum-bitwise-and/
 pub fn longest_subarray(nums: Vec<i32>) -> i32 {
-    todo!()
+    let mut answer = 0;
+    let max = nums.iter().max().unwrap();
+    let mut count = 0;
+    for num in nums.iter() {
+        if num == max {
+            count += 1;
+        } else {
+            count = 0;
+        }
+        answer = answer.max(count);
+    }
+    answer
 }
 
 fn main() {
     println!("{}", longest_subarray(vec![1, 2, 3, 3, 2, 2])); // 2
     println!("{}", longest_subarray(vec![1, 2, 3, 4])); // 1
+    println!(
+        "{}",
+        longest_subarray(vec![
+            311155, 311155, 311155, 311155, 311155, 311155, 311155, 311155, 201191, 311155
+        ])
+    ); // 8
+    println!(
+        "{}",
+        longest_subarray(vec![
+            96317, 96317, 96317, 96317, 96317, 96317, 96317, 96317, 96317, 279979
+        ])
+    ); // 1
 }
