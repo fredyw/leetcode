e57a9357ddb3dbacf31b3b19dd5d7becc6fc9d6b
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-10-02 16:14:15 -0500

    Skeleton for problem 2425

---
+++ src/main/rust/src/bin/problem2425.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/bitwise-xor-of-all-pairings/
+pub fn xor_all_nums(nums1: Vec<i32>, nums2: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", xor_all_nums(vec![2, 1, 3], vec![10, 2, 5, 0])); // 13
+    println!("{}", xor_all_nums(vec![1, 2], vec![3, 4])); // 0
+}
