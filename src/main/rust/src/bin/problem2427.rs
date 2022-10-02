2d9a18333162dd3e02e92af8e38b701d94b4f203
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-10-02 00:13:54 -0500

    Update problem 2427

--- src/main/rust/src/bin/problem2427.rs
+++ src/main/rust/src/bin/problem2427.rs
@@ -6,4 +6,8 @@ pub fn common_factors(a: i32, b: i32) -> i32 {
 fn main() {
     println!("{}", common_factors(12, 6)); // 4
     println!("{}", common_factors(25, 30)); // 2
+    println!("{}", common_factors(8, 12)); // 3
+    println!("{}", common_factors(50, 48)); // 2
+    println!("{}", common_factors(50, 45)); // 2
+    println!("{}", common_factors(12, 24)); // 6
 }
