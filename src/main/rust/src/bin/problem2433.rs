4394855da1a77c6034cfefd7949af920e1821d2e
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-10-10 00:09:23 -0500

    Skeleton for problem 2433

---
+++ src/main/rust/src/bin/problem2433.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/find-the-original-array-of-prefix-xor/
+pub fn find_array(pref: Vec<i32>) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", find_array(vec![5, 2, 0, 3, 1])); // [5,7,2,3,2]
+    println!("{:?}", find_array(vec![13])); // [13]
+}
