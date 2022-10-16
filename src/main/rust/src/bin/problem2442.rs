e8f234108d3e55f70e7ce058f6816a60365b8637
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-10-16 14:01:28 -0500

    Solve problem 2442

--- src/main/rust/src/bin/problem2442.rs
+++ src/main/rust/src/bin/problem2442.rs
@@ -1,6 +1,18 @@
+use std::collections::HashSet;
+
 // https://leetcode.com/problems/count-number-of-distinct-integers-after-reverse-operations/
 pub fn count_distinct_integers(nums: Vec<i32>) -> i32 {
-    todo!()
+    let mut set: HashSet<i32> = nums.iter().cloned().collect();
+    for num in nums.into_iter() {
+        let n: i32 = format!("{}", num)
+            .chars()
+            .rev()
+            .collect::<String>()
+            .parse()
+            .unwrap();
+        set.insert(n);
+    }
+    set.len() as i32
 }
 
 fn main() {
