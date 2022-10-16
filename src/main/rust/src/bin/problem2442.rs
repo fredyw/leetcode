3050f9a9b69e97efd2be4997c1753a60ee2f83d3
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-10-16 13:54:29 -0500

    Skeleton for problem 2442

---
+++ src/main/rust/src/bin/problem2442.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/count-number-of-distinct-integers-after-reverse-operations/
+pub fn count_distinct_integers(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", count_distinct_integers(vec![1, 13, 10, 12, 31])); // 6
+    println!("{}", count_distinct_integers(vec![2, 2, 2])); // 1
+}
