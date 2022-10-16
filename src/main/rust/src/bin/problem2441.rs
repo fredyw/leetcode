10352f67d2ae4b5552deda22f4de6f6fab7e9318
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-10-16 10:25:58 -0500

    Solve problem 2441

--- src/main/rust/src/bin/problem2441.rs
+++ src/main/rust/src/bin/problem2441.rs
@@ -1,6 +1,16 @@
+use std::collections::HashSet;
+
 // https://leetcode.com/problems/largest-positive-integer-that-exists-with-its-negative/
 pub fn find_max_k(nums: Vec<i32>) -> i32 {
-    todo!()
+    let mut answer = -1;
+    let positive_nums: HashSet<&i32> = nums.iter().filter(|a| **a > 0).collect();
+    let negative_nums: HashSet<&i32> = nums.iter().filter(|a| **a < 0).collect();
+    for positive_num in positive_nums.into_iter() {
+        if negative_nums.contains(&-positive_num) {
+            answer = answer.max(*positive_num);
+        }
+    }
+    answer
 }
 
 fn main() {
