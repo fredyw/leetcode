08de5f4480d8c1f948dad09e61574462985ef237
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-10-16 10:21:05 -0500

    Skeleton for problem 2441

---
+++ src/main/rust/src/bin/problem2441.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/largest-positive-integer-that-exists-with-its-negative/
+pub fn find_max_k(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", find_max_k(vec![-1, 2, -3, 3])); // 3
+    println!("{}", find_max_k(vec![-1, 10, 6, 7, -7, 1])); // 7
+    println!("{}", find_max_k(vec![-10, 8, 6, 7, -2, -3])); // -1
+}
