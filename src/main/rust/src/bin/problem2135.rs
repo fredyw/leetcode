edccbee6d6bdfa056b4ad93570c0df02cc11afed
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-10-22 01:36:46 -0500

    Solve problem 2135

--- src/main/rust/src/bin/problem2135.rs
+++ src/main/rust/src/bin/problem2135.rs
@@ -1,6 +1,29 @@
+use std::collections::HashSet;
+
 // https://leetcode.com/problems/count-words-obtained-after-adding-a-letter/
 pub fn word_count(start_words: Vec<String>, target_words: Vec<String>) -> i32 {
-    todo!()
+    let start_set: HashSet<String> = start_words
+        .into_iter()
+        .map(|w| {
+            let mut chars: Vec<char> = w.chars().collect();
+            chars.sort();
+            chars.into_iter().collect::<String>()
+        })
+        .collect();
+    let mut answer = 0;
+    for target in target_words.into_iter() {
+        let mut chars: Vec<char> = target.chars().collect();
+        chars.sort();
+        for i in 0..chars.len() {
+            let mut s: String = (&chars[0..i]).into_iter().collect();
+            s.push_str(&(&chars[i + 1..]).into_iter().collect::<String>());
+            if start_set.contains(&s) {
+                answer += 1;
+                break;
+            }
+        }
+    }
+    answer
 }
 
 fn main() {
