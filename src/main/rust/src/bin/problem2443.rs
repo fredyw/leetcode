af354167189fe1d8b32cec8a645df485f017fdea
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-10-22 21:09:26 -0500

    Skeleton for problem 2443

---
+++ src/main/rust/src/bin/problem2443.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/sum-of-number-and-its-reverse/
+pub fn sum_of_number_and_reverse(num: i32) -> bool {
+    todo!()
+}
+
+fn main() {
+    println!("{}", sum_of_number_and_reverse(443)); // true
+    println!("{}", sum_of_number_and_reverse(63)); // false
+    println!("{}", sum_of_number_and_reverse(181)); // true
+}
