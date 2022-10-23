579e84b667bd4d65101b815a90124b7f732aca70
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-10-22 21:18:38 -0500

    Solve problem 2443

--- src/main/rust/src/bin/problem2443.rs
+++ src/main/rust/src/bin/problem2443.rs
@@ -1,10 +1,22 @@
 // https://leetcode.com/problems/sum-of-number-and-its-reverse/
 pub fn sum_of_number_and_reverse(num: i32) -> bool {
-    todo!()
+    for i in num / 2..=num {
+        let reversed: i32 = format!("{}", i)
+            .chars()
+            .rev()
+            .collect::<String>()
+            .parse()
+            .unwrap();
+        if i + reversed == num {
+            return true;
+        }
+    }
+    false
 }
 
 fn main() {
     println!("{}", sum_of_number_and_reverse(443)); // true
     println!("{}", sum_of_number_and_reverse(63)); // false
     println!("{}", sum_of_number_and_reverse(181)); // true
+    println!("{}", sum_of_number_and_reverse(0)); // true
 }
