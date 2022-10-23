9736e6233a5bb016a63085ed12cef4450c813bb8
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-10-23 11:18:54 -0500

    Solve problem 2446

--- src/main/rust/src/bin/problem2446.rs
+++ src/main/rust/src/bin/problem2446.rs
@@ -1,6 +1,19 @@
 // https://leetcode.com/problems/determine-if-two-events-have-conflict/
 pub fn have_conflict(event1: Vec<String>, event2: Vec<String>) -> bool {
-    todo!()
+    fn to_minutes(time: String) -> i32 {
+        let mut split = time.split(":");
+        let hours: i32 = split.next().unwrap().parse().unwrap();
+        let minutes: i32 = split.next().unwrap().parse().unwrap();
+        (hours * 60) + minutes
+    }
+
+    let from_event1 = to_minutes(event1[0].to_string());
+    let to_event1 = to_minutes(event1[1].to_string());
+    let from_event2 = to_minutes(event2[0].to_string());
+    let to_event2 = to_minutes(event2[1].to_string());
+
+    from_event1 <= from_event2 && from_event2 <= to_event1
+        || from_event2 <= from_event1 && from_event1 <= to_event2
 }
 
 fn main() {
@@ -9,6 +22,13 @@ fn main() {
         have_conflict(
             vec!["01:15".to_string(), "02:00".to_string()],
             vec!["02:00".to_string(), "03:00".to_string()]
+        )
+    ); // true
+    println!(
+        "{}",
+        have_conflict(
+            vec!["02:00".to_string(), "03:00".to_string()],
+            vec!["01:15".to_string(), "02:00".to_string()]
         )
     ); // true
     println!(
