129b929e2a79ea36446d5cbe10324330c679d4fa
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-10-23 11:23:12 -0500

    Skeleton for problem 2017

---
+++ src/main/rust/src/bin/problem2017.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/grid-game/
+pub fn grid_game(grid: Vec<Vec<i32>>) -> i64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", grid_game(vec![vec![2, 5, 4], vec![1, 5, 1]])); // 4
+    println!("{}", grid_game(vec![vec![3, 3, 1], vec![8, 5, 2]])); // 4
+    println!("{}", grid_game(vec![vec![1, 3, 1, 15], vec![1, 3, 3, 1]])); // 7
+}
