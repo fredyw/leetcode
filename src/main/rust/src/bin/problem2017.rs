b8dbc957643192aba0aac6d18ad4bc73d38c3e59
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-10-30 01:01:19 -0500

    Solve problem 2017

--- src/main/rust/src/bin/problem2017.rs
+++ src/main/rust/src/bin/problem2017.rs
@@ -1,5 +1,5 @@
 // https://leetcode.com/problems/grid-game/
-pub fn grid_game(mut grid: Vec<Vec<i32>>) -> i64 {
+pub fn grid_game(grid: Vec<Vec<i32>>) -> i64 {
     fn prefix_sums(v: &Vec<i32>) -> Vec<i64> {
         let num_cols = v.len();
         let mut prefix_sums = vec![0i64; num_cols];
@@ -13,57 +13,30 @@ pub fn grid_game(mut grid: Vec<Vec<i32>>) -> i64 {
         return prefix_sums;
     }
 
-    let row1_prefix_sums = prefix_sums(&grid[0]);
-    let row2_prefix_sums = prefix_sums(&grid[1]);
-    let mut max = 0;
-    let mut max_index = 0;
-    let mut is_right = true;
     let num_cols = grid[0].len();
-    for i in 0..num_cols {
-        let right_sum = row1_prefix_sums[num_cols - 1] + grid[1][num_cols - 1] as i64;
-        let down_sum = row1_prefix_sums[i]
-            + (row2_prefix_sums[num_cols - 1] - if i == 0 { 0 } else { row2_prefix_sums[i - 1] });
-        if max < right_sum {
-            max = right_sum;
-            max_index = i;
-            is_right = true;
-        }
-        if max < down_sum {
-            max = down_sum;
-            max_index = i;
-            is_right = false;
-        }
-    }
-    if is_right {
-        for i in 0..num_cols {
-            grid[0][i] = 0;
-            grid[1][num_cols - 1] = 0;
-        }
-    } else {
-        for i in 0..=max_index {
-            grid[0][i] = 0;
-        }
-        for i in max_index..num_cols {
-            grid[1][i] = 0;
-        }
-    }
-    let row1_prefix_sums = prefix_sums(&grid[0]);
-    let row2_prefix_sums = prefix_sums(&grid[1]);
-    let mut answer = 0;
-    let num_cols = grid[0].len();
-    for i in 0..num_cols {
-        let right_sum = row1_prefix_sums[num_cols - 1] + grid[1][num_cols - 1] as i64;
-        let down_sum = row1_prefix_sums[i]
-            + (row2_prefix_sums[num_cols - 1] - if i == 0 { 0 } else { row2_prefix_sums[i - 1] });
-        answer = answer.max(right_sum.max(down_sum));
+    let row0_prefix_sums = prefix_sums(&grid[0]);
+    let row1_prefix_sums = prefix_sums(&grid[1]);
+    // row0: -xxxxxx
+    // row1: |------
+    let sum1 = row0_prefix_sums[num_cols - 1] - grid[0][0] as i64;
+    // row0: -------
+    // row1: xxxxxx|
+    let sum2 = row1_prefix_sums[num_cols - 1] - grid[1][num_cols - 1] as i64;
+    let mut answer = sum1.min(sum2);
+    for i in 1..num_cols - 1 {
+        // row0: ---|xxx
+        // row1: xxx|---
+        let sum3 = row0_prefix_sums[num_cols - 1] - row0_prefix_sums[i];
+        let sum4 = row1_prefix_sums[i] - grid[1][i] as i64;
+        answer = answer.min(sum3.max(sum4));
     }
     answer
 }
 
 fn main() {
-    // println!("{}", grid_game(vec![vec![2, 5, 4], vec![1, 5, 1]])); // 4
-    // println!("{}", grid_game(vec![vec![3, 3, 1], vec![8, 5, 2]])); // 4
-    // println!("{}", grid_game(vec![vec![1, 3, 1, 15], vec![1, 3, 3, 1]])); // 7
+    println!("{}", grid_game(vec![vec![2, 5, 4], vec![1, 5, 1]])); // 4
+    println!("{}", grid_game(vec![vec![3, 3, 1], vec![8, 5, 2]])); // 4
+    println!("{}", grid_game(vec![vec![1, 3, 1, 15], vec![1, 3, 3, 1]])); // 7
     println!(
         "{}",
         grid_game(vec![
