7a7cbf2e2f940d387ec9aa0561697c45816ae539
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-10-29 23:59:28 -0500

    Skeleton for problem 2451

---
+++ src/main/rust/src/bin/problem2451.rs
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/odd-string-difference/
+pub fn odd_string(words: Vec<String>) -> String {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        odd_string(vec![
+            "adc".to_string(),
+            "wzy".to_string(),
+            "abc".to_string()
+        ])
+    ); // "abc"
+    println!(
+        "{}",
+        odd_string(vec![
+            "aaa".to_string(),
+            "bob".to_string(),
+            "ccc".to_string(),
+            "ddd".to_string()
+        ])
+    ); // "bob"
+}
