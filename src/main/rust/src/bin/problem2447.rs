8dcca08b4cd6f1a0030ed1e0f2d68d0064b6614a
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-10-30 23:41:57 -0500

    Skeleton for problem 2447

---
+++ src/main/rust/src/bin/problem2447.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/number-of-subarrays-with-gcd-equal-to-k/
+pub fn subarray_gcd(nums: Vec<i32>, k: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", subarray_gcd(vec![9, 3, 1, 2, 6, 3], 3)); // 4
+    println!("{}", subarray_gcd(vec![4], 7)); // 0
+}
