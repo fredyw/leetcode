84c97c9a9fd737572e5c373ff4c7c64e8b6af770
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-10-30 23:40:11 -0500

    Solve problem 2452

--- src/main/rust/src/bin/problem2452.rs
+++ src/main/rust/src/bin/problem2452.rs
@@ -1,38 +1,30 @@
-use std::collections::HashSet;
-
 // https://leetcode.com/problems/words-within-two-edits-of-dictionary/
 pub fn two_edit_words(queries: Vec<String>, dictionary: Vec<String>) -> Vec<String> {
-    let mut answer: Vec<String> = vec![];
-    let dictionary: HashSet<String> = dictionary.into_iter().collect();
-    'outer: for query in queries.iter() {
-        if dictionary.contains(query) {
-            answer.push(query.clone());
-            continue;
+    fn is_two_edit_distance(s1: &str, s2: &str) -> bool {
+        if s1.len() != s2.len() {
+            return false;
         }
-        let mut bytes: Vec<u8> = query.as_bytes().into_iter().map(|a| *a).collect();
-        for i in 0..bytes.len() {
-            let tmp1 = bytes[i];
-            for j in 0..26 {
-                bytes[i] = j as u8 + 'a' as u8;
-                let s = String::from_utf8(bytes.clone()).unwrap();
-                if dictionary.contains(&s) {
-                    answer.push(query.clone());
-                    continue 'outer;
-                }
-                for k in i + 1..bytes.len() {
-                    let tmp2 = bytes[k];
-                    for l in 0..26 {
-                        bytes[k] = l as u8 + 'a' as u8;
-                        let s = String::from_utf8(bytes.clone()).unwrap();
-                        if dictionary.contains(&s) {
-                            answer.push(query.clone());
-                            continue 'outer;
-                        }
-                    }
-                    bytes[k] = tmp2;
-                }
+        let mut count = 0;
+        let bytes1 = s1.as_bytes();
+        let bytes2 = s2.as_bytes();
+        for i in 0..s1.len() {
+            if bytes1[i] != bytes2[i] {
+                count += 1;
             }
-            bytes[i] = tmp1;
+            if count > 2 {
+                return false;
+            }
+        }
+        true
+    }
+
+    let mut answer: Vec<String> = vec![];
+    for query in queries.iter() {
+        for word in dictionary.iter() {
+            if is_two_edit_distance(query, word) {
+                answer.push(query.clone());
+                break;
+            }
         }
     }
     answer
