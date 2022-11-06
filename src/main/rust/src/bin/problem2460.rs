bd90291843aa91761cf85180cb342566abc79412
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-11-06 00:04:41 -0500

    Skeleton for problem 2460

---
+++ src/main/rust/src/bin/problem2460.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/apply-operations-to-an-array/
+pub fn apply_operations(nums: Vec<i32>) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", apply_operations(vec![1, 2, 2, 1, 1, 0])); // [1,4,2,0,0,0]
+    println!("{:?}", apply_operations(vec![0, 1])); // [1,0]
+}
