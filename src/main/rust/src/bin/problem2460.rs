238118e38edcfbef3c89fcc143418897c18753e9
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-11-06 00:13:41 -0500

    Solve problem 2460

--- src/main/rust/src/bin/problem2460.rs
+++ src/main/rust/src/bin/problem2460.rs
@@ -1,6 +1,23 @@
 // https://leetcode.com/problems/apply-operations-to-an-array/
-pub fn apply_operations(nums: Vec<i32>) -> Vec<i32> {
-    todo!()
+pub fn apply_operations(mut nums: Vec<i32>) -> Vec<i32> {
+    let mut answer = vec![0; nums.len()];
+    let mut j = 0;
+    for i in 0..nums.len() {
+        if i + 1 < nums.len() && nums[i] == nums[i + 1] {
+            nums[i] = nums[i] * 2;
+            nums[i + 1] = 0;
+            if nums[i] > 0 {
+                answer[j] = nums[i];
+                j += 1;
+            }
+        } else {
+            if nums[i] > 0 {
+                answer[j] = nums[i];
+                j += 1;
+            }
+        }
+    }
+    answer
 }
 
 fn main() {
