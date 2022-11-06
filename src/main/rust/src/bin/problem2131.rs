de45fa0151565ab64ae22be984f3f833dfbb91f3
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-11-06 10:51:18 -0600

    Solve problem 2131

--- src/main/rust/src/bin/problem2131.rs
+++ src/main/rust/src/bin/problem2131.rs
@@ -7,10 +7,18 @@ pub fn longest_palindrome(words: Vec<String>) -> i32 {
         *map.entry(word).or_insert(0) += 1;
     }
     let mut answer = 0;
+    let mut has_odd = false;
     while let Some((word, count)) = map.iter().next() {
         let bytes = word.as_bytes();
         if bytes[0] == bytes[1] {
-            answer += *count * 2;
+            if *count % 2 != 0 {
+                has_odd = true;
+            }
+            answer += if *count % 2 == 0 {
+                *count * 2
+            } else {
+                (*count - 1) * 2
+            };
             map.remove(&word.clone());
         } else {
             let reversed_word: String = word.chars().rev().collect();
@@ -23,41 +31,45 @@ pub fn longest_palindrome(words: Vec<String>) -> i32 {
             }
         }
     }
-    answer
+    if has_odd {
+        answer + 2
+    } else {
+        answer
+    }
 }
 
 fn main() {
-    // println!(
-    //     "{}",
-    //     longest_palindrome(vec!["lc".to_string(), "cl".to_string(), "gg".to_string()])
-    // ); // 6
-    // println!(
-    //     "{}",
-    //     longest_palindrome(vec![
-    //         "ab".to_string(),
-    //         "ty".to_string(),
-    //         "yt".to_string(),
-    //         "lc".to_string(),
-    //         "cl".to_string(),
-    //         "ab".to_string()
-    //     ])
-    // ); // 8
-    // println!(
-    //     "{}",
-    //     longest_palindrome(vec!["cc".to_string(), "ll".to_string(), "xx".to_string()])
-    // ); // 2
-    // println!(
-    //     "{}",
-    //     longest_palindrome(vec![
-    //         "lc".to_string(),
-    //         "cl".to_string(),
-    //         "gg".to_string(),
-    //         "gg".to_string(),
-    //         "gg".to_string(),
-    //         "aa".to_string(),
-    //         "aa".to_string()
-    //     ])
-    // ); // 14
+    println!(
+        "{}",
+        longest_palindrome(vec!["lc".to_string(), "cl".to_string(), "gg".to_string()])
+    ); // 6
+    println!(
+        "{}",
+        longest_palindrome(vec![
+            "ab".to_string(),
+            "ty".to_string(),
+            "yt".to_string(),
+            "lc".to_string(),
+            "cl".to_string(),
+            "ab".to_string()
+        ])
+    ); // 8
+    println!(
+        "{}",
+        longest_palindrome(vec!["cc".to_string(), "ll".to_string(), "xx".to_string()])
+    ); // 2
+    println!(
+        "{}",
+        longest_palindrome(vec![
+            "lc".to_string(),
+            "cl".to_string(),
+            "gg".to_string(),
+            "gg".to_string(),
+            "gg".to_string(),
+            "aa".to_string(),
+            "aa".to_string()
+        ])
+    ); // 14
     println!(
         "{}",
         longest_palindrome(vec![
