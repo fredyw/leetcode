f25a1f8f4d2a5e6b0daf93e51d286d0d66aa5c68
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-11-13 00:05:16 -0600

    Skeleton for problem 2469

---
+++ src/main/rust/src/bin/problem2469.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/convert-the-temperature/
+pub fn convert_temperature(celsius: f64) -> Vec<f64> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", convert_temperature(36.50)); // [309.65000,97.70000]
+    println!("{:?}", convert_temperature(122.11)); // [395.26000,251.79800]
+}
