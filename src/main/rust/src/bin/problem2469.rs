647babf44226f073aace03622b377235cef63e1b
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-11-13 00:08:19 -0600

    Solve problem 2469

--- src/main/rust/src/bin/problem2469.rs
+++ src/main/rust/src/bin/problem2469.rs
@@ -1,6 +1,6 @@
 // https://leetcode.com/problems/convert-the-temperature/
 pub fn convert_temperature(celsius: f64) -> Vec<f64> {
-    todo!()
+    vec![celsius + 273.15, (celsius * 1.80) + 32.00]
 }
 
 fn main() {
