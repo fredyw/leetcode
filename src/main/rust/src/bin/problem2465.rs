295dc78eaddbb98713b23758c989b440161cca6f
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-11-13 00:14:51 -0600

    Skeleton for problem 2465

---
+++ src/main/rust/src/bin/problem2465.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/number-of-distinct-averages/
+pub fn distinct_averages(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", distinct_averages(vec![4, 1, 4, 0, 3, 5])); // 2
+    println!("{}", distinct_averages(vec![1, 100])); // 1
+}
