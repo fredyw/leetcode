f16c843a99b53733d71056cce4bb76555cc9b0c7
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-11-13 00:50:47 -0600

    Solve problem 2465

--- src/main/rust/src/bin/problem2465.rs
+++ src/main/rust/src/bin/problem2465.rs
@@ -1,6 +1,18 @@
+use std::collections::HashSet;
+
 // https://leetcode.com/problems/number-of-distinct-averages/
-pub fn distinct_averages(nums: Vec<i32>) -> i32 {
-    todo!()
+pub fn distinct_averages(mut nums: Vec<i32>) -> i32 {
+    nums.sort();
+    let mut set: HashSet<String> = HashSet::new();
+    let mut i = 0;
+    let mut j = nums.len() - 1;
+    while i < j {
+        let average = (nums[i] + nums[j]) as f32 / 2 as f32;
+        set.insert(format!("{}", average));
+        i += 1;
+        j -= 1;
+    }
+    set.len() as i32
 }
 
 fn main() {
