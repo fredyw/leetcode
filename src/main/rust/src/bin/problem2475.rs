a66f88fa1ad7220d7a2e1ee37635c6a07a2af8d8
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-11-20 00:21:28 -0600

    Update problem 2475

--- src/main/rust/src/bin/problem2475.rs
+++ src/main/rust/src/bin/problem2475.rs
@@ -5,5 +5,10 @@ pub fn unequal_triplets(nums: Vec<i32>) -> i32 {
 
 fn main() {
     println!("{}", unequal_triplets(vec![4, 4, 2, 4, 3])); // 3
+    println!("{}", unequal_triplets(vec![4, 4, 2, 4, 3, 4])); // 4
+    println!("{}", unequal_triplets(vec![4, 4, 2, 4, 3, 4, 5])); // 13
+    println!("{}", unequal_triplets(vec![4, 4, 2, 4, 3, 4, 5, 5])); // 22
     println!("{}", unequal_triplets(vec![1, 1, 1, 1, 1])); // 0
+    println!("{}", unequal_triplets(vec![1, 2, 3, 4])); // 4
+    println!("{}", unequal_triplets(vec![1, 2, 3, 4, 5])); // 10
 }
