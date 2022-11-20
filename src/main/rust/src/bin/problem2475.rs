722064aa326d003bb88feaa499b222a0364b4be2
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-11-20 00:40:39 -0600

    Solve problem 2475

--- src/main/rust/src/bin/problem2475.rs
+++ src/main/rust/src/bin/problem2475.rs
@@ -1,6 +1,16 @@
 // https://leetcode.com/problems/number-of-unequal-triplets-in-array/
 pub fn unequal_triplets(nums: Vec<i32>) -> i32 {
-    todo!()
+    let mut answer = 0;
+    for i in 0..nums.len() {
+        for j in i + 1..nums.len() {
+            for k in j + 1..nums.len() {
+                if nums[i] != nums[j] && nums[i] != nums[k] && nums[j] != nums[k] {
+                    answer += 1;
+                }
+            }
+        }
+    }
+    answer
 }
 
 fn main() {
