abde229cc31506707eff9c50978eee2b9f48e421
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-11-20 00:05:59 -0600

    Skeleton for problem 2475

---
+++ src/main/rust/src/bin/problem2475.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/number-of-unequal-triplets-in-array/
+pub fn unequal_triplets(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", unequal_triplets(vec![4, 4, 2, 4, 3])); // 3
+    println!("{}", unequal_triplets(vec![1, 1, 1, 1, 1])); // 0
+}
