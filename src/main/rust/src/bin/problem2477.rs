19e5239accfd00b654cc0c3dc45ff8d3b9e424cc
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-11-20 10:59:59 -0600

    Skeleton for problem 2477

---
+++ src/main/rust/src/bin/problem2477.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/minimum-fuel-cost-to-report-to-the-capital/
+pub fn minimum_fuel_cost(roads: Vec<Vec<i32>>, seats: i32) -> i64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", minimum_fuel_cost(vec![vec![0,1],vec![0,2],vec![0,3]], 5)); // 3
+    println!("{}", minimum_fuel_cost(vec![vec![3,1],vec![3,2],vec![1,0],vec![0,4],vec![0,5],vec![4,6]], 2)); // 7
+    println!("{}", minimum_fuel_cost(vec![], 1)); // 0
+}
