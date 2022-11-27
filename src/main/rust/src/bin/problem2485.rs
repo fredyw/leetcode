dfdc0ba8afae6415bebf8bb74c969922246235ba
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-11-26 23:44:28 -0600

    Skeleton for proble 2485

---
+++ src/main/rust/src/bin/problem2485.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/find-the-pivot-integer/
+pub fn pivot_integer(n: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", pivot_integer(8)); // 6
+    println!("{}", pivot_integer(1)); // 1
+    println!("{}", pivot_integer(4)); // -1
+}
