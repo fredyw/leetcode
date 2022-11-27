2e6f0d68e5b6215e1d0c356eece80c531d129c50
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-11-27 00:02:35 -0600

    Solve problem 2481

--- src/main/rust/src/bin/problem2481.rs
+++ src/main/rust/src/bin/problem2481.rs
@@ -1,9 +1,19 @@
 // https://leetcode.com/problems/minimum-cuts-to-divide-a-circle/
 pub fn number_of_cuts(n: i32) -> i32 {
-    todo!()
+    if n == 1 {
+        0
+    } else if n % 2 == 0 {
+        n / 2
+    } else {
+        n
+    }
 }
 
 fn main() {
     println!("{}", number_of_cuts(4)); // 2
     println!("{}", number_of_cuts(3)); // 3
+    println!("{}", number_of_cuts(2)); // 1
+    println!("{}", number_of_cuts(1)); // 0
+    println!("{}", number_of_cuts(5)); // 5
+    println!("{}", number_of_cuts(6)); // 3
 }
