f657c6c123534f63ba4f613aafb64df6b5f74fe9
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-11-27 00:24:46 -0600

    Update problem 2477

--- src/main/rust/src/bin/problem2477.rs
+++ src/main/rust/src/bin/problem2477.rs
@@ -4,7 +4,31 @@ pub fn minimum_fuel_cost(roads: Vec<Vec<i32>>, seats: i32) -> i64 {
 }
 
 fn main() {
-    println!("{}", minimum_fuel_cost(vec![vec![0,1],vec![0,2],vec![0,3]], 5)); // 3
-    println!("{}", minimum_fuel_cost(vec![vec![3,1],vec![3,2],vec![1,0],vec![0,4],vec![0,5],vec![4,6]], 2)); // 7
+    println!(
+        "{}",
+        minimum_fuel_cost(vec![vec![0, 1], vec![0, 2], vec![0, 3]], 5)
+    ); // 3
+    println!(
+        "{}",
+        minimum_fuel_cost(
+            vec![
+                vec![3, 1],
+                vec![3, 2],
+                vec![1, 0],
+                vec![0, 4],
+                vec![0, 5],
+                vec![4, 6]
+            ],
+            2
+        )
+    ); // 7
     println!("{}", minimum_fuel_cost(vec![], 1)); // 0
+    println!(
+        "{}",
+        minimum_fuel_cost(vec![vec![0, 1], vec![1, 2], vec![2, 3], vec![3, 4]], 2)
+    ); // 6
+    println!(
+        "{}",
+        minimum_fuel_cost(vec![vec![0, 1], vec![1, 2], vec![2, 3], vec![3, 4]], 1)
+    ); // 10
 }
