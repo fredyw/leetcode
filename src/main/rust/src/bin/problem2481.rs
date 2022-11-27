d3d0ab2acab1a4c822b27bb3511363ae16de4c99
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-11-26 23:55:28 -0600

    Skeleton for problem 2481

---
+++ src/main/rust/src/bin/problem2481.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/minimum-cuts-to-divide-a-circle/
+pub fn number_of_cuts(n: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", number_of_cuts(4)); // 2
+    println!("{}", number_of_cuts(3)); // 3
+}
