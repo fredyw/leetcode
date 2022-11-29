01432ab66c454d8729f308db64ec400e5ee6e9c6
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-11-29 15:01:19 -0600

    Solve problem 2482

--- src/main/rust/src/bin/problem2482.rs
+++ src/main/rust/src/bin/problem2482.rs
@@ -1,6 +1,27 @@
 // https://leetcode.com/problems/difference-between-ones-and-zeros-in-row-and-column/
 pub fn ones_minus_zeros(grid: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-    todo!()
+    let max_rows = grid.len();
+    let max_cols = if max_rows > 0 { grid[0].len() } else { 0 };
+    let mut rows: Vec<i32> = vec![0; max_rows];
+    let mut cols: Vec<i32> = vec![0; max_cols];
+    for row in 0..max_rows {
+        for col in 0..max_cols {
+            rows[row] += grid[row][col];
+            cols[col] += grid[row][col];
+        }
+    }
+
+    let mut answer = vec![vec![0; max_cols]; max_rows];
+    for row in 0..max_rows {
+        for col in 0..max_cols {
+            let ones_row = rows[row];
+            let zeros_row = max_rows as i32 - ones_row;
+            let ones_col = cols[col];
+            let zeros_col = max_cols as i32 - ones_col;
+            answer[row][col] = ones_row + ones_col - zeros_row - zeros_col;
+        }
+    }
+    answer
 }
 
 fn main() {
