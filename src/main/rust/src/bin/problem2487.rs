0972199c696e43de41a48389faf6717377711846
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-11-30 00:45:24 -0600

    Solve problem 2487

--- src/main/rust/src/bin/problem2487.rs
+++ src/main/rust/src/bin/problem2487.rs
@@ -13,38 +13,66 @@ impl ListNode {
 }
 
 pub fn remove_nodes(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-    todo!()
+    let vec = to_vec(&head);
+    let mut answer: Vec<i32> = vec![];
+    for i in (0..vec.len()).rev() {
+        if i == vec.len() - 1 {
+            answer.push(vec[i]);
+        } else {
+            let last = answer[answer.len() - 1];
+            let max = last.max(vec[i]);
+            if max != last || vec[i] == max {
+                answer.push(max);
+            }
+        }
+    }
+    answer.reverse();
+    to_linked_list(answer)
+}
+
+fn to_linked_list(v: Vec<i32>) -> Option<Box<ListNode>> {
+    let mut head: Option<Box<ListNode>> = None;
+    let mut current = &mut head;
+    for i in v.iter() {
+        match current {
+            Some(a) => {
+                a.next = Some(Box::new(ListNode::new(*i)));
+                current = &mut a.next;
+            }
+            None => {
+                head = Some(Box::new(ListNode::new(*i)));
+                current = &mut head;
+            }
+        }
+    }
+    head
+}
+
+fn to_vec(node: &Option<Box<ListNode>>) -> Vec<i32> {
+    let mut node = node;
+    let mut v: Vec<i32> = vec![];
+    while let Some(n) = node {
+        v.push(n.val);
+        node = &n.next
+    }
+    v
 }
 
 fn main() {
-    fn to_linked_list(v: Vec<i32>) -> Option<Box<ListNode>> {
-        let mut head: Option<Box<ListNode>> = None;
-        let mut current = &mut head;
-        for i in v.iter() {
-            match current {
-                Some(a) => {
-                    a.next = Some(Box::new(ListNode::new(*i)));
-                    current = &mut a.next;
-                }
-                None => {
-                    head = Some(Box::new(ListNode::new(*i)));
-                    current = &mut head;
-                }
-            }
-        }
-        head
-    }
-
-    fn to_vec(node: &Option<Box<ListNode>>) -> Vec<i32> {
-        let mut node = node;
-        let mut v: Vec<i32> = vec![];
-        while let Some(n) = node {
-            v.push(n.val);
-            node = &n.next
-        }
-        v
-    }
-
-    println!("{:?}", to_vec(&to_linked_list(vec![5, 2, 13, 3, 8]))); // [13,8]
-    println!("{:?}", to_vec(&to_linked_list(vec![1, 1, 1, 1]))); // [1,1,1,1]
+    println!(
+        "{:?}",
+        to_vec(&remove_nodes(to_linked_list(vec![5, 2, 13, 3, 8])))
+    ); // [13,8]
+    println!(
+        "{:?}",
+        to_vec(&remove_nodes(to_linked_list(vec![5, 2, 8, 3, 13])))
+    ); // [13]
+    println!(
+        "{:?}",
+        to_vec(&remove_nodes(to_linked_list(vec![5, 2, 13, 3, 8, 1, 4])))
+    ); // [13,8,4]
+    println!(
+        "{:?}",
+        to_vec(&remove_nodes(to_linked_list(vec![1, 1, 1, 1])))
+    ); // [1,1,1,1]
 }
