b4b69968dd1622b91e60e9840d8e26ceb2a20d16
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-12-04 14:24:38 -0600

    Skeleton for problem 2490

---
+++ src/main/rust/src/bin/problem2490.rs
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/circular-sentence/
+pub fn is_circular_sentence(sentence: String) -> bool {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        is_circular_sentence("leetcode exercises sound delightful".to_string())
+    ); // true
+    println!("{}", is_circular_sentence("eetcode".to_string())); // true
+    println!("{}", is_circular_sentence("Leetcode is cool".to_string())); // false
+}
