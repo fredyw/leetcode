fd4dd58abf5aacdd5f506131c45ce9a16c7bfa6d
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-12-03 21:46:52 -0600

    Skeleton for problem 2483

---
+++ src/main/rust/src/bin/problem2483.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/minimum-penalty-for-a-shop/
+pub fn best_closing_time(customers: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", best_closing_time("YYNY".to_string())); // 2
+    println!("{}", best_closing_time("NNNNN".to_string())); // 0
+    println!("{}", best_closing_time("YYYY".to_string())); // 4
+}
