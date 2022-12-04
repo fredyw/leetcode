a3d5bd035cbc90657264ab4768faa2629a9e4ff3
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-12-03 21:44:06 -0600

    Solve problem 2486

--- src/main/rust/src/bin/problem2486.rs
+++ src/main/rust/src/bin/problem2486.rs
@@ -1,6 +1,20 @@
 // https://leetcode.com/problems/append-characters-to-string-to-make-subsequence/
 pub fn append_characters(s: String, t: String) -> i32 {
-    todo!()
+    let mut i = 0;
+    let mut j = 0;
+    let s: Vec<char> = s.chars().collect();
+    let t: Vec<char> = t.chars().collect();
+    while j < t.len() {
+        while i < s.len() && s[i] != t[j] {
+            i += 1;
+        }
+        if i == s.len() {
+            break;
+        }
+        i += 1;
+        j += 1;
+    }
+    (t.len() - j) as i32
 }
 
 fn main() {
