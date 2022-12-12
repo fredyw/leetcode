a4438042cbd188a5c960ec7bc6b77e47a05e2e44
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-12-12 08:03:47 -0600

    Solve problem 2491

--- src/main/rust/src/bin/problem2491.rs
+++ src/main/rust/src/bin/problem2491.rs
@@ -1,6 +1,22 @@
 // https://leetcode.com/problems/divide-players-into-teams-of-equal-skill/
-pub fn divide_players(skill: Vec<i32>) -> i64 {
-    todo!()
+pub fn divide_players(mut skill: Vec<i32>) -> i64 {
+    skill.sort();
+    let mut answer: i64 = 0;
+    let mut i = 0;
+    let mut j = skill.len() - 1;
+    let mut equal_skill = 0;
+    while i < j {
+        let sum = skill[i] + skill[j];
+        if equal_skill == 0 || equal_skill == sum {
+            answer += skill[i] as i64 * skill[j] as i64;
+            equal_skill = sum;
+        } else {
+            return -1;
+        }
+        i += 1;
+        j -= 1;
+    }
+    answer
 }
 
 fn main() {
