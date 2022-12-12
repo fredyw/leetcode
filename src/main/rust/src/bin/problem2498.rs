4ff1e24862fedd28c63816414e2cd9bb1d3ee3db
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-12-12 12:17:18 -0600

    Solve problem 2498

--- src/main/rust/src/bin/problem2498.rs
+++ src/main/rust/src/bin/problem2498.rs
@@ -1,6 +1,26 @@
 // https://leetcode.com/problems/frog-jump-ii/
 pub fn max_jump(stones: Vec<i32>) -> i32 {
-    todo!()
+    if stones.len() == 2 {
+        return stones[1] - stones[0];
+    }
+    let mut answer = 0;
+    let mut i = 0;
+    while i < stones.len() - 2 {
+        let diff = stones[i + 2] - stones[i];
+        answer = answer.max(diff);
+        i += 2;
+    }
+    let mut i = if stones.len() % 2 == 0 {
+        stones.len() - 1
+    } else {
+        stones.len() - 2
+    };
+    while i as i32 - 2 >= 0 {
+        let diff = stones[i] - stones[i - 2];
+        answer = answer.max(diff);
+        i -= 2;
+    }
+    answer
 }
 
 fn main() {
