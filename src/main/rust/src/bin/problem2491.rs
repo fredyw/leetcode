3cde3cbca019513f1d644498cc1810e9c3f1ff40
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-12-11 23:50:48 -0600

    Skeleton for problem 2491

---
+++ src/main/rust/src/bin/problem2491.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/divide-players-into-teams-of-equal-skill/
+pub fn divide_players(skill: Vec<i32>) -> i64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", divide_players(vec![3, 2, 5, 1, 3, 4])); // 22
+    println!("{}", divide_players(vec![3, 4])); // 12
+    println!("{}", divide_players(vec![1, 1, 2, 3])); // -1
+}
