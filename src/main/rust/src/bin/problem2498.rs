e7233548a095b06f2f1929cd71fe67f23020905d
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-12-12 08:09:45 -0600

    Skeleton for problem 2498

---
+++ src/main/rust/src/bin/problem2498.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/frog-jump-ii/
+pub fn max_jump(stones: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", max_jump(vec![0, 2, 5, 6, 7])); // 5
+    println!("{}", max_jump(vec![0, 3, 9])); // 9
+}
