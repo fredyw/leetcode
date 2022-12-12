02990a1ad3d1300e04466a5bf76d955ed42e6b10
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-12-11 23:29:18 -0600

    Solve problem 2500

--- src/main/rust/src/bin/problem2500.rs
+++ src/main/rust/src/bin/problem2500.rs
@@ -1,6 +1,18 @@
 // https://leetcode.com/problems/delete-greatest-value-in-each-row/
-pub fn delete_greatest_value(grid: Vec<Vec<i32>>) -> i32 {
-    todo!()
+pub fn delete_greatest_value(mut grid: Vec<Vec<i32>>) -> i32 {
+    for row in grid.iter_mut() {
+        row.sort_by(|a, b| b.cmp(a));
+    }
+    let mut answer = 0;
+    let num_cols = grid[0].len();
+    for col in 0..num_cols {
+        let mut max = 0;
+        for row in grid.iter() {
+            max = max.max(row[col]);
+        }
+        answer += max;
+    }
+    answer
 }
 
 fn main() {
