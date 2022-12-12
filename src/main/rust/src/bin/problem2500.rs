04aad563e23513058159c17a6064dfff1d9b952d
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-12-11 23:16:25 -0600

    Skeleton for problem 2500

---
+++ src/main/rust/src/bin/problem2500.rs
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/delete-greatest-value-in-each-row/
+pub fn delete_greatest_value(grid: Vec<Vec<i32>>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        delete_greatest_value(vec![vec![1, 2, 4], vec![3, 3, 1]])
+    ); // 8
+    println!("{}", delete_greatest_value(vec![vec![10]])); // 10
+}
