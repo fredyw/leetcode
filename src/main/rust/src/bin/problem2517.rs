eb47987d47f5beebcff0ecd5ef0d48111ea0c509
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-12-26 14:56:50 -0600

    Skeleton for problem 2517

---
+++ src/main/rust/src/bin/problem2517.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/maximum-tastiness-of-candy-basket/
+pub fn maximum_tastiness(price: Vec<i32>, k: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", maximum_tastiness(vec![13, 5, 1, 8, 21, 2], 3)); // 8
+    println!("{}", maximum_tastiness(vec![1, 3, 1], 2)); // 2
+    println!("{}", maximum_tastiness(vec![7, 7, 7, 7], 2)); // 0
+}
