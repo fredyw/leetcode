4a9f9c5ff12ce806f15ce7a14e36159796768bb4
Fredy Wijaya <fredy.wijaya@gmail.com>
2022-12-26 00:55:39 -0600

    Skeleton for problem 2511

---
+++ src/main/rust/src/bin/problem2511.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/maximum-enemy-forts-that-can-be-captured/
+pub fn capture_forts(forts: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", capture_forts(vec![1, 0, 0, -1, 0, 0, 0, 0, 1])); // 4
+    println!("{}", capture_forts(vec![0, 0, 1, -1])); // 0
+}
