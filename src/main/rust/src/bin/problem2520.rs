f628fe5e5106ca69abe636a89a3bd87559c2b840
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-01-01 13:56:23 -0600

    Skeleton for problem 2520

---
+++ src/main/rust/src/bin/problem2520.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/count-the-digits-that-divide-a-number/
+pub fn count_digits(num: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", count_digits(7)); // 1
+    println!("{}", count_digits(121)); // 2
+    println!("{}", count_digits(1248)); // 4
+}
