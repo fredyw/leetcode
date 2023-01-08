3000476f50d027ae8435af420333cd7d61df9067
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-01-08 01:44:27 -0600

    Skeleton for problem 2525

---
+++ src/main/rust/src/bin/problem2525.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/categorize-box-according-to-criteria/
+pub fn categorize_box(length: i32, width: i32, height: i32, mass: i32) -> String {
+    todo!()
+}
+
+fn main() {
+    println!("{}", categorize_box(1000, 35, 700, 300)); // "Heavy"
+    println!("{}", categorize_box(200, 50, 800, 50)); // "Neither"
+}
