ef3dfc15980f466323e98fc4dd3161077ffe2f20
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-01-08 01:44:19 -0600

    Solve problem 2529

--- src/main/rust/src/bin/problem2529.rs
+++ src/main/rust/src/bin/problem2529.rs
@@ -1,6 +1,15 @@
 // https://leetcode.com/problems/maximum-count-of-positive-integer-and-negative-integer/
 pub fn maximum_count(nums: Vec<i32>) -> i32 {
-    todo!()
+    let mut neg_count = 0;
+    let mut pos_count = 0;
+    for num in nums.into_iter() {
+        if num < 0 {
+            neg_count += 1;
+        } else if num > 0 {
+            pos_count += 1;
+        }
+    }
+    neg_count.max(pos_count)
 }
 
 fn main() {
