8101df65f8ee64f8465f260d8f51ed03f7e9928a
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-01-08 01:39:04 -0600

    Skeleton for problem 2529

---
+++ src/main/rust/src/bin/problem2529.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/maximum-count-of-positive-integer-and-negative-integer/
+pub fn maximum_count(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", maximum_count(vec![-2, -1, -1, 1, 2, 3])); // 3
+    println!("{}", maximum_count(vec![-3, -2, -1, 0, 0, 1, 2])); // 3
+    println!("{}", maximum_count(vec![5, 20, 66, 1314])); // 4
+}
