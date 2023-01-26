b4a9c7d2a1f300c169bd4e7c436aa5a8968bf940
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-01-25 23:44:53 -0600

    Skeleton for problem 2544

---
+++ src/main/rust/src/bin/problem2544.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/alternating-digit-sum/
+pub fn alternate_digit_sum(n: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", alternate_digit_sum(521)); // 4
+    println!("{}", alternate_digit_sum(111)); // 1
+    println!("{}", alternate_digit_sum(886996)); // 0
+}
