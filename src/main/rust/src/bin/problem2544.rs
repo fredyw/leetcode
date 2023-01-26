37e6fb1ce9322e7decee4365b219b1ec125195ea
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-01-25 23:52:03 -0600

    Solve problem 2544

--- src/main/rust/src/bin/problem2544.rs
+++ src/main/rust/src/bin/problem2544.rs
@@ -1,6 +1,19 @@
 // https://leetcode.com/problems/alternating-digit-sum/
 pub fn alternate_digit_sum(n: i32) -> i32 {
-    todo!()
+    let mut answer: i32 = 0;
+    let s = format!("{}", n);
+    let bytes = s.as_bytes();
+    let mut positive = true;
+    for b in bytes.iter() {
+        let num = *b - '0' as u8;
+        if positive {
+            answer += num as i32;
+        } else {
+            answer += -(num as i32);
+        }
+        positive = !positive;
+    }
+    answer
 }
 
 fn main() {
