982cd983c7048c082f2af0029eddfe4cf587a5bd
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-01-29 00:26:11 -0600

    Solve problem 2549

--- src/main/rust/src/bin/problem2549.rs
+++ src/main/rust/src/bin/problem2549.rs
@@ -1,6 +1,10 @@
 // https://leetcode.com/problems/count-distinct-numbers-on-board/
 pub fn distinct_integers(n: i32) -> i32 {
-    todo!()
+    if n == 1 {
+        1
+    } else {
+        n - 1
+    }
 }
 
 fn main() {
