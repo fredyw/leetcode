b0e76e531d096689f06f815ee48332ef009b3ac9
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-01-29 00:16:15 -0600

    Skeleton for problem 2549

---
+++ src/main/rust/src/bin/problem2549.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/count-distinct-numbers-on-board/
+pub fn distinct_integers(n: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", distinct_integers(5)); // 4
+    println!("{}", distinct_integers(3)); // 2
+}
