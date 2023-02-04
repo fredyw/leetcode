aad1f0df89fa780463a56f25f9c40c4c22bf3322
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-02-04 14:27:03 -0600

    Skeleton for problem 2553

---
+++ src/main/rust/src/bin/problem2553.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/separate-the-digits-in-an-array/
+pub fn separate_digits(nums: Vec<i32>) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", separate_digits(vec![13, 25, 83, 77])); // [1,3,2,5,8,3,7,7]
+    println!("{:?}", separate_digits(vec![7, 1, 3, 9])); // [7,1,3,9]
+}
