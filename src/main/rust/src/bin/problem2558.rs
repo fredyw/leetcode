a3b278e18cec2543be12ed16362380c7c47df3e5
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-02-04 22:42:36 -0600

    Skeleton for problem 2558

---
+++ src/main/rust/src/bin/problem2558.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/take-gifts-from-the-richest-pile/
+pub fn pick_gifts(gifts: Vec<i32>, k: i32) -> i64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", pick_gifts(vec![25, 64, 9, 4, 100], 4)); // 29
+    println!("{}", pick_gifts(vec![1, 1, 1, 1], 4)); // 4
+}
