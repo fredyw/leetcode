df37a5b96fb0624a6a05b9ae642a198414836395
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-02-09 23:53:19 -0600

    Skeleton for problem 2527

---
+++ src/main/rust/src/bin/problem2527.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/find-xor-beauty-of-array/
+pub fn xor_beauty(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", xor_beauty(vec![1, 4])); // 5
+    println!("{}", xor_beauty(vec![15, 45, 20, 2, 34, 35, 5, 44, 32, 30])); // 34
+}
