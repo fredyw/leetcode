0f8d7c15a9954bb92df3d5ba4731341b3698cb53
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-02-11 23:15:31 -0600

    Skeleton for problem 2562

---
+++ src/main/rust/src/bin/problem2562.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/find-the-array-concatenation-value/
+pub fn find_the_array_conc_val(nums: Vec<i32>) -> i64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", find_the_array_conc_val(vec![7, 52, 2, 4])); // 596
+    println!("{}", find_the_array_conc_val(vec![5, 14, 13, 8, 12])); // 673
+}
