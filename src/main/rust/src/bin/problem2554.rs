6869e7812ff01dd751912716d49f11d35d7f30bf
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-02-11 23:28:51 -0600

    Skeleton for problem 2554

---
+++ src/main/rust/src/bin/problem2554.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/maximum-number-of-integers-to-choose-from-a-range-i/
+pub fn max_count(banned: Vec<i32>, n: i32, max_sum: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", max_count(vec![1, 6, 5], 5, 6)); // 2
+    println!("{}", max_count(vec![1, 2, 3, 4, 5, 6, 7], 8, 1)); // 0
+    println!("{}", max_count(vec![11], 7, 50)); // 7
+}
