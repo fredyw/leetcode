1dcdd059aaddc3ee3172210efe8cd20e5af20580
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-02-12 11:06:21 -0600

    Solve problem 2559

--- src/main/rust/src/bin/problem2559.rs
+++ src/main/rust/src/bin/problem2559.rs
@@ -1,6 +1,28 @@
 // https://leetcode.com/problems/count-vowel-strings-in-ranges/
 pub fn vowel_strings(words: Vec<String>, queries: Vec<Vec<i32>>) -> Vec<i32> {
-    todo!()
+    fn is_vowel(byte: u8) -> bool {
+        byte == 'a' as u8
+            || byte == 'e' as u8
+            || byte == 'i' as u8
+            || byte == 'o' as u8
+            || byte == 'u' as u8
+    }
+
+    let mut prefix: Vec<i32> = vec![0; words.len()];
+    for (i, word) in words.iter().enumerate() {
+        let bytes = word.as_bytes();
+        prefix[i] += if i == 0 { 0 } else { prefix[i - 1] };
+        if is_vowel(bytes[0]) && is_vowel(bytes[bytes.len() - 1]) {
+            prefix[i] += 1;
+        }
+    }
+    let mut answer = vec![];
+    for query in queries.into_iter() {
+        let from = query[0] as usize;
+        let to = query[1] as usize;
+        answer.push(prefix[to] - if from == 0 { 0 } else { prefix[from - 1] });
+    }
+    answer
 }
 
 fn main() {
