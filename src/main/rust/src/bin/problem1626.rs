e5923a30ca57b4e12fbbf4122a5d8e1ee49b09c8
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-02-13 23:04:39 -0600

    Solve problem 1626

--- src/main/rust/src/bin/problem1626.rs
+++ src/main/rust/src/bin/problem1626.rs
@@ -1,6 +1,37 @@
 // https://leetcode.com/problems/best-team-with-no-conflicts/
 pub fn best_team_score(scores: Vec<i32>, ages: Vec<i32>) -> i32 {
-    todo!()
+    fn best_team_score(
+        ages_and_scores: &Vec<(i32, i32)>,
+        prev_index: usize,
+        index: usize,
+        memo: &mut Vec<Vec<i32>>,
+    ) -> i32 {
+        if index == ages_and_scores.len() {
+            return 0;
+        }
+        if memo[prev_index][index] != -1 {
+            return memo[prev_index][index];
+        }
+        let (prev_age, prev_score) = ages_and_scores[prev_index];
+        let (age, score) = ages_and_scores[index];
+        if prev_age < age && score < prev_score {
+            return best_team_score(ages_and_scores, prev_index, index + 1, memo);
+        }
+        let take = best_team_score(ages_and_scores, index, index + 1, memo) + score;
+        let skip = best_team_score(ages_and_scores, prev_index, index + 1, memo);
+        let max = take.max(skip);
+        memo[prev_index][index] = max;
+        max
+    }
+
+    let mut ages_and_scores: Vec<(i32, i32)> = ages.into_iter().zip(scores).collect();
+    ages_and_scores.sort();
+    let mut memo: Vec<Vec<i32>> = vec![vec![-1; 1001]; 1001];
+    let mut answer = 0;
+    for i in 0..ages_and_scores.len() {
+        answer = answer.max(best_team_score(&ages_and_scores, i, i, &mut memo));
+    }
+    answer
 }
 
 fn main() {
