037bff379df637fd7a472ec02739223a8dcca12e
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-02-19 09:43:25 -0600

    Skeleton for problem 2566

---
+++ src/main/rust/src/bin/problem2566.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/maximum-difference-by-remapping-a-digit/
+pub fn min_max_difference(num: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", min_max_difference(11891)); // 99009
+    println!("{}", min_max_difference(90)); // 99
+}
