c392851ade1b7b9006fbbc37d42f3cb468c7600e
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-02-20 14:29:39 -0600

    Update problem 2568

--- src/main/rust/src/bin/problem2568.rs
+++ src/main/rust/src/bin/problem2568.rs
@@ -6,4 +6,5 @@ pub fn min_impossible_or(nums: Vec<i32>) -> i32 {
 fn main() {
     println!("{}", min_impossible_or(vec![2, 1])); // 4
     println!("{}", min_impossible_or(vec![5, 3, 2])); // 1
+    println!("{}", min_impossible_or(vec![5, 3, 2, 1])); // 4
 }
