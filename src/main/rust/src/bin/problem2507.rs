a695e200aa6d9fc11b3d5481c470e283ee78cf1c
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-02-20 13:22:43 -0600

    Skeleton for problem 2507

---
+++ src/main/rust/src/bin/problem2507.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/smallest-value-after-replacing-with-sum-of-prime-factors/
+pub fn smallest_value(n: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", smallest_value(15)); // 5
+    println!("{}", smallest_value(3)); // 3
+}
