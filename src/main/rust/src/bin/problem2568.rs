0f77bf872050c5e53aed77042ab705681bf7be4f
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-02-20 13:55:07 -0600

    Skeleton for problem 2568

---
+++ src/main/rust/src/bin/problem2568.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/minimum-impossible-or/
+pub fn min_impossible_or(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", min_impossible_or(vec![2, 1])); // 4
+    println!("{}", min_impossible_or(vec![5, 3, 2])); // 1
+}
