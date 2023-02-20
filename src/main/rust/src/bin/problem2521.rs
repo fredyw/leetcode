51b9a72848facc43a1e2ae4efa268e02176a4dcf
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-02-20 15:42:19 -0600

    Skeleton for problem 2521

---
+++ src/main/rust/src/bin/problem2521.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/distinct-prime-factors-of-product-of-array/
+pub fn distinct_prime_factors(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", distinct_prime_factors(vec![2, 4, 3, 7, 10, 6])); // 4
+    println!("{}", distinct_prime_factors(vec![2, 4, 8, 16])); // 1
+}
