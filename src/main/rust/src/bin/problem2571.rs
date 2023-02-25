8c9ab8786d2da4e8bbbd4b8606a45e3cbf406a64
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-02-25 13:58:01 -0600

    Skeleton for problem 2571

---
+++ src/main/rust/src/bin/problem2571.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/minimum-operations-to-reduce-an-integer-to-0/
+pub fn min_operations(n: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", min_operations(39)); // 3
+    println!("{}", min_operations(54)); // 3
+}
