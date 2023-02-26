8b85d13a3617d16b66068c6b5dc9996d14ec0279
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-02-26 09:17:25 -0600

    Skeleton for problem 2574

---
+++ src/main/rust/src/bin/problem2574.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/left-and-right-sum-differences/
+pub fn left_rigth_difference(nums: Vec<i32>) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", left_rigth_difference(vec![10, 4, 8, 3])); // [15,1,11,22]
+    println!("{:?}", left_rigth_difference(vec![1])); // [0]
+}
