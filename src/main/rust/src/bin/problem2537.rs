c1ea0ab3e552e76366679e08d87f7041735de888
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-03-01 23:08:04 -0600

    Skeleton for problem 2537

---
+++ src/main/rust/src/bin/problem2537.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/count-the-number-of-good-subarrays/
+pub fn count_good(nums: Vec<i32>, k: i32) -> i64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", count_good(vec![1, 1, 1, 1, 1], 10)); // 1
+    println!("{}", count_good(vec![3, 1, 4, 3, 2, 2, 4], 2)); // 4
+}
