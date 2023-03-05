78f1bf59314f18c520f6ae7560d4b42ddbc426ce
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-03-05 09:36:15 -0600

    Skeleton for problem 2579

---
+++ src/main/rust/src/bin/problem2579.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/count-total-number-of-colored-cells/
+pub fn colored_cells(n: i32) -> i64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", colored_cells(1)); // 1
+    println!("{}", colored_cells(2)); // 5
+}
