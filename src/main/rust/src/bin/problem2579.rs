d251d17741c9e72f9ea4ff018f557bf39ca5e582
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-03-05 09:44:32 -0600

    Solve problem 2579

--- src/main/rust/src/bin/problem2579.rs
+++ src/main/rust/src/bin/problem2579.rs
@@ -1,9 +1,20 @@
 // https://leetcode.com/problems/count-total-number-of-colored-cells/
 pub fn colored_cells(n: i32) -> i64 {
-    todo!()
+    let mut answer: i64 = 1;
+    let multiplier = 4;
+    for i in 1..n {
+        answer += i as i64 * multiplier;
+    }
+    answer
 }
 
 fn main() {
     println!("{}", colored_cells(1)); // 1
     println!("{}", colored_cells(2)); // 5
+    println!("{}", colored_cells(3)); // 13
+    println!("{}", colored_cells(4)); // 25
+    println!("{}", colored_cells(5)); // 41
+    println!("{}", colored_cells(6)); // 61
+    println!("{}", colored_cells(7)); // 85
+    println!("{}", colored_cells(8)); // 113
 }
