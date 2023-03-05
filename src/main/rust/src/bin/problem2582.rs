cd14a982dc32327f26794d6ae7324bb9a2e2494e
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-03-05 00:38:03 -0600

    Update problem 2582

--- src/main/rust/src/bin/problem2582.rs
+++ src/main/rust/src/bin/problem2582.rs
@@ -6,4 +6,7 @@ pub fn pass_the_pillow(n: i32, time: i32) -> i32 {
 fn main() {
     println!("{}", pass_the_pillow(4, 5)); // 2
     println!("{}", pass_the_pillow(3, 2)); // 3
+    println!("{}", pass_the_pillow(4, 1000)); // 3
+    println!("{}", pass_the_pillow(4, 9)); // 4
+    println!("{}", pass_the_pillow(4, 8)); // 3
 }
