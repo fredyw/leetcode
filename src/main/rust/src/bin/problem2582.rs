842705b23c61629dcab7f4620e69570c79f25cc8
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-03-05 09:33:23 -0600

    Solve problem 2582

--- src/main/rust/src/bin/problem2582.rs
+++ src/main/rust/src/bin/problem2582.rs
@@ -1,6 +1,27 @@
 // https://leetcode.com/problems/pass-the-pillow/
 pub fn pass_the_pillow(n: i32, time: i32) -> i32 {
-    todo!()
+    let mut answer = 0;
+    let mut count = 0;
+    let mut t = 1;
+    let mut forward = true;
+    while count <= time {
+        if count == time {
+            answer = t;
+            break;
+        }
+        if t == n {
+            forward = false;
+        } else if t == 1 {
+            forward = true;
+        }
+        if forward {
+            t += 1;
+        } else {
+            t -= 1;
+        }
+        count += 1;
+    }
+    answer
 }
 
 fn main() {
