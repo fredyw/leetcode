29786ac30667bc370f69718bc6dde0c3a5f95fc6
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-03-05 00:31:37 -0600

    Solve problem 2582

---
+++ src/main/rust/src/bin/problem2582.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/pass-the-pillow/
+pub fn pass_the_pillow(n: i32, time: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", pass_the_pillow(4, 5)); // 2
+    println!("{}", pass_the_pillow(3, 2)); // 3
+}
