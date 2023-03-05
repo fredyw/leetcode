f18e8ebd314b65f6ed854f10179199845bc6c31b
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-03-05 00:06:36 -0600

    Skeleton for problem 2578

---
+++ src/main/rust/src/bin/problem2578.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/split-with-minimum-sum/
+pub fn split_num(num: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", split_num(4325)); // 49
+    println!("{}", split_num(687)); // 75
+}
