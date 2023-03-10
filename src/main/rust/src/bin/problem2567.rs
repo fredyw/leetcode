c1ab641aaa0d92d9d86a6224a7944cc746557c6c
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-03-09 23:55:06 -0600

    Update problem 2567

--- src/main/rust/src/bin/problem2567.rs
+++ src/main/rust/src/bin/problem2567.rs
@@ -6,4 +6,8 @@ pub fn minimize_sum(nums: Vec<i32>) -> i32 {
 fn main() {
     println!("{}", minimize_sum(vec![1, 4, 3])); // 0
     println!("{}", minimize_sum(vec![1, 4, 7, 8, 5])); // 3
+    println!("{}", minimize_sum(vec![7, 7, 7, 8, 5])); // 0
+    println!("{}", minimize_sum(vec![6, 6, 7, 8, 5])); // 1
+    println!("{}", minimize_sum(vec![6, 6, 7, 8, 5])); // 3
+    println!("{}", minimize_sum(vec![6, 6, 7, 8, 80, 99, 100])); // 74
 }
