ec434045360f8ec4deaa307adae0b9746fb33e59
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-03-09 23:08:07 -0600

    Skeleton for problem 2567

---
+++ src/main/rust/src/bin/problem2567.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/minimum-score-by-changing-two-elements/
+pub fn minimize_sum(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", minimize_sum(vec![1, 4, 3])); // 0
+    println!("{}", minimize_sum(vec![1, 4, 7, 8, 5])); // 3
+}
