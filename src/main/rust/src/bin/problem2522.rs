9a7a57ac1699b00dc665ca6b0774954b73d4b8e5
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-03-11 12:37:43 -0600

    Skeleton for problem 2522

---
+++ src/main/rust/src/bin/problem2522.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/partition-string-into-substrings-with-values-at-most-k/
+pub fn minimum_partition(s: String, k: i32) -> i32 {
+    0
+}
+
+fn main() {
+    println!("{}", minimum_partition("165462".to_string(), 60)); // 4
+    println!("{}", minimum_partition("238182".to_string(), 5)); // -1
+}
