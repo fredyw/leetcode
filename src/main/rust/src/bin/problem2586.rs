051e59fb27a892fb0dad21c4097ea440ccfd3814
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-03-12 00:35:51 -0600

    Solve problem 2686

--- src/main/rust/src/bin/problem2586.rs
+++ src/main/rust/src/bin/problem2586.rs
@@ -1,6 +1,17 @@
 // https://leetcode.com/problems/count-the-number-of-vowel-strings-in-range/
 pub fn vowel_strings(words: Vec<String>, left: i32, right: i32) -> i32 {
-    todo!()
+    fn is_vowel(c: char) -> bool {
+        c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
+    }
+
+    let mut answer = 0;
+    for i in left..=right {
+        let chars: Vec<char> = words[i as usize].chars().collect();
+        if is_vowel(chars[0]) && is_vowel(chars[chars.len() - 1]) {
+            answer += 1;
+        }
+    }
+    answer
 }
 
 fn main() {
