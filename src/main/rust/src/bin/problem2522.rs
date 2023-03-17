0ca802d4d0620047fa9b0308d5091df87529851d
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-03-16 23:48:39 -0500

    Update problem 2522

--- src/main/rust/src/bin/problem2522.rs
+++ src/main/rust/src/bin/problem2522.rs
@@ -6,4 +6,7 @@ pub fn minimum_partition(s: String, k: i32) -> i32 {
 fn main() {
     println!("{}", minimum_partition("165462".to_string(), 60)); // 4
     println!("{}", minimum_partition("238182".to_string(), 5)); // -1
+    println!("{}", minimum_partition("1356".to_string(), 500)); // 2
+    println!("{}", minimum_partition("1356".to_string(), 500)); // 2
+    println!("{}", minimum_partition("8974735375948735".to_string(), 69)); // 10
 }
