8f761e062a8bd0f46d91fb28d7de6ea6150206dd
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-03-18 15:38:38 -0500

    Skeleton for problem 2588

---
+++ src/main/rust/src/bin/problem2588.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/count-the-number-of-beautiful-subarrays/
+pub fn beautiful_subarrays(nums: Vec<i32>) -> i64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", beautiful_subarrays(vec![4, 3, 1, 2, 4])); // 2
+    println!("{}", beautiful_subarrays(vec![1, 10, 4])); // 0
+}
