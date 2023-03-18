3402904437e141a3181f6368bb9aa7e1839c1740
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-03-18 14:53:46 -0500

    Skeleton for problem 2592

---
+++ src/main/rust/src/bin/problem2592.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/maximize-greatness-of-an-array/
+pub fn maximize_greatness(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", maximize_greatness(vec![1, 3, 5, 2, 1, 3, 1])); // 4
+    println!("{}", maximize_greatness(vec![1, 2, 3, 4])); // 3
+}
