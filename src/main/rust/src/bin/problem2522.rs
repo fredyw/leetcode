e69f16d6841517cf069a569a2f76314c5672450c
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-03-18 14:50:45 -0500

    Solve problem 2522

--- src/main/rust/src/bin/problem2522.rs
+++ src/main/rust/src/bin/problem2522.rs
@@ -1,6 +1,28 @@
 // https://leetcode.com/problems/partition-string-into-substrings-with-values-at-most-k/
 pub fn minimum_partition(s: String, k: i32) -> i32 {
-    0
+    let mut answer = 0;
+    let chars: Vec<char> = s.chars().collect();
+    let mut s = String::new();
+    let mut i = 0;
+    while i < chars.len() {
+        s.push(chars[i]);
+        let mut n = s.parse::<i64>().unwrap();
+        let mut found = false;
+        while n <= k as i64 && i < chars.len() {
+            found = true;
+            i += 1;
+            if i < chars.len() {
+                s.push(chars[i]);
+                n = s.parse::<i64>().unwrap();
+            }
+        }
+        s.clear();
+        if !found {
+            return -1;
+        }
+        answer += 1;
+    }
+    answer
 }
 
 fn main() {
