c81889f6276cc052a2a55ec1e9b0265fa59daa09
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-03-19 14:09:13 -0500

    Solve problem 2591

--- src/main/rust/src/bin/problem2591.rs
+++ src/main/rust/src/bin/problem2591.rs
@@ -8,28 +8,31 @@ pub fn dist_money(money: i32, children: i32) -> i32 {
     let mut money = money - children;
     for i in 0..children {
         if i == children - 1 {
-            if 7 == money {
+            if money == 7 {
                 answer += 1;
-                money -= 7;
+            } else if answer > 0 && money == 3 {
+                // Avoid giving 4 to a child.
+                answer -= 1;
             }
-        } else if 7 <= money {
+            break;
+        }
+        if money < 7 {
+            break;
+        }
+        if money >= 7 {
             answer += 1;
             money -= 7;
         }
-    }
-    // We need to avoid giving 4.
-    if money == 3 && answer > 0 {
-        answer -= 1;
     }
     answer
 }
 
 fn main() {
-    // println!("{}", dist_money(20, 3)); // 1
-    // println!("{}", dist_money(16, 2)); // 2
-    // println!("{}", dist_money(200, 30)); // 24
-    // println!("{}", dist_money(200, 11)); // 10
-    // println!("{}", dist_money(2, 2)); // 0
-    // println!("{}", dist_money(5, 2)); // 0
+    println!("{}", dist_money(20, 3)); // 1
+    println!("{}", dist_money(16, 2)); // 2
+    println!("{}", dist_money(200, 30)); // 24
+    println!("{}", dist_money(200, 11)); // 10
+    println!("{}", dist_money(2, 2)); // 0
+    println!("{}", dist_money(5, 2)); // 0
     println!("{}", dist_money(13, 3)); // 1
 }
