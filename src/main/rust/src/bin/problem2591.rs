8b272b749cf88e2e8ed67ddbc3abeba08b00d7a2
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-03-19 13:58:35 -0500

    Update problem 2591

--- src/main/rust/src/bin/problem2591.rs
+++ src/main/rust/src/bin/problem2591.rs
@@ -1,9 +1,35 @@
 // https://leetcode.com/problems/distribute-money-to-maximum-children/
 pub fn dist_money(money: i32, children: i32) -> i32 {
-    todo!()
+    // Each child must get 1.
+    if money < children {
+        return -1;
+    }
+    let mut answer = 0;
+    let mut money = money - children;
+    for i in 0..children {
+        if i == children - 1 {
+            if 7 == money {
+                answer += 1;
+                money -= 7;
+            }
+        } else if 7 <= money {
+            answer += 1;
+            money -= 7;
+        }
+    }
+    // We need to avoid giving 4.
+    if money == 3 && answer > 0 {
+        answer -= 1;
+    }
+    answer
 }
 
 fn main() {
-    println!("{}", dist_money(20, 3)); // 1
-    println!("{}", dist_money(16, 2)); // 2
+    // println!("{}", dist_money(20, 3)); // 1
+    // println!("{}", dist_money(16, 2)); // 2
+    // println!("{}", dist_money(200, 30)); // 24
+    // println!("{}", dist_money(200, 11)); // 10
+    // println!("{}", dist_money(2, 2)); // 0
+    // println!("{}", dist_money(5, 2)); // 0
+    println!("{}", dist_money(13, 3)); // 1
 }
