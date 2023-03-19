ff16c5618834bd8d4159be7f2925fa318f69dfef
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-03-19 10:37:32 -0500

    Skeleton for problem 2591

---
+++ src/main/rust/src/bin/problem2591.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/distribute-money-to-maximum-children/
+pub fn dist_money(money: i32, children: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", dist_money(20, 3)); // 1
+    println!("{}", dist_money(16, 2)); // 2
+}
