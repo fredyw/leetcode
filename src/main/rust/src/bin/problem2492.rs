067ae5f51d09cf459e06e8c0983ae8563259990b
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-03-26 09:56:45 -0500

    Update problem 2492

--- src/main/rust/src/bin/problem2492.rs
+++ src/main/rust/src/bin/problem2492.rs
@@ -13,6 +13,13 @@ fn main() {
     ); // 5
     println!(
         "{}",
+        min_score(
+            4,
+            vec![vec![1, 2, 9], vec![2, 3, 3], vec![2, 4, 6], vec![1, 4, 7]]
+        )
+    ); // 3
+    println!(
+        "{}",
         min_score(4, vec![vec![1, 2, 2], vec![1, 3, 4], vec![3, 4, 7]])
     ); // 5
 }
