8b3a4f09509d4f2cddf79f870b6f83646fb8385d
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-03-26 12:55:36 -0500

    Skeleton for problem 2593

---
+++ src/main/rust/src/bin/problem2593.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/find-score-of-an-array-after-marking-all-elements/
+pub fn find_score(nums: Vec<i32>) -> i64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", find_score(vec![2, 1, 3, 4, 5, 2])); // 7
+    println!("{}", find_score(vec![2, 3, 5, 1, 3, 2])); // 5
+}
