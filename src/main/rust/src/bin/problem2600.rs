8999b7fde9bfac6153d6221254b426fe54d19273
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-03-26 09:21:30 -0500

    Skeleton for problem 2600

---
+++ src/main/rust/src/bin/problem2600.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/k-items-with-the-maximum-sum/
+pub fn k_items_with_maximum_sum(num_ones: i32, num_zeros: i32, num_neg_ones: i32, k: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", k_items_with_maximum_sum(3, 2, 0, 2)); // 2
+    println!("{}", k_items_with_maximum_sum(3, 2, 0, 4)); // 3
+}
