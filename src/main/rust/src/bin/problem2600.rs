f4c5e8d4bbce910fea59778cf571b9902fb2b7c8
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-03-26 09:44:16 -0500

    Solve problem 2600

--- src/main/rust/src/bin/problem2600.rs
+++ src/main/rust/src/bin/problem2600.rs
@@ -1,6 +1,26 @@
 // https://leetcode.com/problems/k-items-with-the-maximum-sum/
 pub fn k_items_with_maximum_sum(num_ones: i32, num_zeros: i32, num_neg_ones: i32, k: i32) -> i32 {
-    todo!()
+    let mut answer = 0;
+    let mut count = k;
+    if count <= num_ones {
+        return answer + count;
+    }
+    if count > num_ones {
+        answer += num_ones;
+        count -= num_ones;
+    }
+
+    if count <= num_zeros {
+        return answer;
+    }
+    if count > num_zeros {
+        count -= num_zeros;
+    }
+
+    if count <= num_neg_ones {
+        return answer - count;
+    }
+    answer
 }
 
 fn main() {
