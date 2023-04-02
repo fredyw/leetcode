adbbf2a0b19b5c3e3f394a005bb2c8fd015399d7
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-04-02 08:42:37 -0500

    Skeleton for problem 2605

---
+++ src/main/rust/src/bin/problem2605.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/form-smallest-number-from-two-digit-arrays/
+pub fn min_number(nums1: Vec<i32>, nums2: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", min_number(vec![4, 1, 3], vec![5, 7])); // 15
+    println!("{}", min_number(vec![3, 5, 2, 6], vec![3, 1, 7])); // 3
+}
