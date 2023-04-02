afd9636662782be2bc311208f429edea671d87d3
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-04-02 10:32:24 -0500

    Solve problem 2609

--- src/main/rust/src/bin/problem2609.rs
+++ src/main/rust/src/bin/problem2609.rs
@@ -1,6 +1,22 @@
 // https://leetcode.com/problems/find-the-longest-balanced-substring-of-a-binary-string/
 pub fn find_the_longest_balanced_substring(s: String) -> i32 {
-    todo!()
+    let mut answer = 0;
+    let mut i = 0;
+    let chars: Vec<char> = s.chars().collect();
+    while i < chars.len() {
+        let mut zero_count = 0;
+        while i < chars.len() && chars[i] == '0' {
+            zero_count += 1;
+            i += 1;
+        }
+        let mut one_count = 0;
+        while i < chars.len() && chars[i] == '1' {
+            one_count += 1;
+            i += 1;
+        }
+        answer = answer.max(zero_count.min(one_count) * 2);
+    }
+    answer
 }
 
 fn main() {
