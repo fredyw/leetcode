8c4774e862beab60b04eda5408ef1381bcfb265e
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-04-02 17:49:27 -0500

    Solve problem 1934

--- src/main/sql/Problem1934.sql
+++ src/main/sql/Problem1934.sql
@@ -1,1 +1,5 @@
 -- https://leetcode.com/problems/confirmation-rate/
+select s.user_id,
+       ifnull(round(sum(c.action='confirmed') / count(c.action), 2), 0) as confirmation_rate
+from signups s left join confirmations c on s.user_id = c.user_id
+group by s.user_id;
