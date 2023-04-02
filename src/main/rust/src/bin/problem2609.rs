b411c4ed499e7aed3878c9c17aefeef2e3faa39c
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-04-02 08:44:55 -0500

    Skeleton for problem 2609

---
+++ src/main/rust/src/bin/problem2609.rs
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/find-the-longest-balanced-substring-of-a-binary-string/
+pub fn find_the_longest_balanced_substring(s: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        find_the_longest_balanced_substring("01000111".to_string())
+    ); // 6
+    println!(
+        "{}",
+        find_the_longest_balanced_substring("00111".to_string())
+    ); // 4
+}
