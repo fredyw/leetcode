0e100b351cbe9e0fbaa348ceeadec190384f94a8
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-04-02 10:38:40 -0500

    Update problem 2610

--- src/main/rust/src/bin/problem2610.rs
+++ src/main/rust/src/bin/problem2610.rs
@@ -6,4 +6,5 @@ pub fn find_matrix(nums: Vec<i32>) -> Vec<Vec<i32>> {
 fn main() {
     println!("{:?}", find_matrix(vec![1, 3, 4, 1, 2, 3, 1])); // [[1,3,4,2],[1,3],[1]]
     println!("{:?}", find_matrix(vec![1, 2, 3, 4])); // [[4,3,2,1]]
+    println!("{:?}", find_matrix(vec![2, 1, 1])); // [[1,2],[1]]
 }
