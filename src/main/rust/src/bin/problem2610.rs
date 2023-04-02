84e2de40c1b4f259442dffe89ec87dbb168cba16
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-04-02 10:35:05 -0500

    Skeleton for problem 2610

---
+++ src/main/rust/src/bin/problem2610.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/convert-an-array-into-a-2d-array-with-conditions/
+pub fn find_matrix(nums: Vec<i32>) -> Vec<Vec<i32>> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", find_matrix(vec![1, 3, 4, 1, 2, 3, 1])); // [[1,3,4,2],[1,3],[1]]
+    println!("{:?}", find_matrix(vec![1, 2, 3, 4])); // [[4,3,2,1]]
+}
