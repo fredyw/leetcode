baa9c7c4b99a409662428bf8d739d611c4b090bf
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-04-02 11:15:10 -0500

    Solve problem 2610

--- src/main/rust/src/bin/problem2610.rs
+++ src/main/rust/src/bin/problem2610.rs
@@ -1,6 +1,20 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/convert-an-array-into-a-2d-array-with-conditions/
 pub fn find_matrix(nums: Vec<i32>) -> Vec<Vec<i32>> {
-    todo!()
+    let mut map: HashMap<i32, Vec<i32>> = HashMap::new();
+    let mut max_size = 0;
+    for num in nums.into_iter() {
+        map.entry(num).or_insert(vec![]).push(num);
+        max_size = max_size.max(map.get(&num).unwrap().len());
+    }
+    let mut answer: Vec<Vec<i32>> = vec![vec![]; max_size];
+    for (num, v) in map.into_iter() {
+        for row in 0..v.len() {
+            answer[row].push(num);
+        }
+    }
+    answer
 }
 
 fn main() {
