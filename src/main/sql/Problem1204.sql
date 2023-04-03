baa8f8b30a16330ac8a3872366dfde41b9269574
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-04-02 21:00:40 -0500

    Solve problem 1204

--- src/main/sql/Problem1204.sql
+++ src/main/sql/Problem1204.sql
@@ -1,1 +1,10 @@
 -- https://leetcode.com/problems/last-person-to-fit-in-the-bus/
+with cummulative_weight as (
+    select turn, person_name, sum(weight) over (order by turn) as total_weight
+    from queue
+    order by turn
+)
+select person_name
+from cummulative_weight
+where total_weight <= 1000
+order by turn desc limit 1;
