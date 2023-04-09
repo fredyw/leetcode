3aeaab405f53c50a1aaff1af062ec82d3b04f2e6
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-04-09 12:17:02 -0500

    Update problem 1251

--- src/main/sql/Problem1251.sql
+++ src/main/sql/Problem1251.sql
@@ -1,6 +1,6 @@
 -- https://leetcode.com/problems/average-selling-price/
 select p.product_id,
        round(sum(p.price * u.units) / sum(u.units), 2) as average_price
-from Prices p join UnitsSold u on (p.product_id = u.product_id)
+from Prices p join UnitsSold u on p.product_id = u.product_id
 where u.purchase_date between p.start_date and p.end_date
 group by p.product_id;
