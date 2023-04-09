84c303641a888e0ae58fee46e104412e7de7eb4a
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-04-09 13:32:55 -0500

    Solve problem 1321

--- src/main/sql/problem1321.sql
+++ src/main/sql/problem1321.sql
@@ -1,1 +1,10 @@
 -- https://leetcode.com/problems/restaurant-growth/
+select employee_id, department_id
+from Employee
+where primary_flag = 'Y'
+group by employee_id
+union
+select employee_id, department_id
+from Employee
+group by employee_id
+having count(employee_id) = 1;
