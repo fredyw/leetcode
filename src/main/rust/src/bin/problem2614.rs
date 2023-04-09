0b9964a7d955a1a443d87bdbbc16e97c464e7d7f
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-04-09 10:46:55 -0500

    Skeleton for problem 2614

---
+++ src/main/rust/src/bin/problem2614.rs
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/prime-in-diagonal/
+pub fn diagonal_prime(nums: Vec<Vec<i32>>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        diagonal_prime(vec![vec![1, 2, 3], vec![5, 6, 7], vec![9, 10, 11]])
+    ); // 1
+    println!(
+        "{}",
+        diagonal_prime(vec![vec![1, 2, 3], vec![5, 17, 7], vec![9, 11, 10]])
+    ); // 17
+}
