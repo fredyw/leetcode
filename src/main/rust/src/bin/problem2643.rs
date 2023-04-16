c8e1197fa68e2f249914ce8f100795c3111be2bc
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-04-16 09:44:43 -0500

    Solve problem 2643

--- src/main/rust/src/bin/problem2643.rs
+++ src/main/rust/src/bin/problem2643.rs
@@ -1,6 +1,23 @@
 // https://leetcode.com/problems/row-with-maximum-ones/
 pub fn row_and_maximum_ones(mat: Vec<Vec<i32>>) -> Vec<i32> {
-    todo!()
+    let mut answer = vec![0; 2];
+    let mut max_row = 0;
+    let mut max_count = 0;
+    for i in 0..mat.len() {
+        let mut count = 0;
+        for j in 0..mat[i].len() {
+            if mat[i][j] == 1 {
+                count += 1;
+            }
+        }
+        if max_count < count {
+            max_count = count;
+            max_row = i as i32;
+        }
+    }
+    answer[0] = max_row;
+    answer[1] = max_count;
+    answer
 }
 
 fn main() {
