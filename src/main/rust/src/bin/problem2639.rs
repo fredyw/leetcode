c4abc4948bfedea565c2fdddd80e2e3623e71cd2
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-04-16 10:14:32 -0500

    Solve problem 2639

--- src/main/rust/src/bin/problem2639.rs
+++ src/main/rust/src/bin/problem2639.rs
@@ -1,6 +1,16 @@
 // https://leetcode.com/problems/find-the-width-of-columns-of-a-grid/
 pub fn find_column_width(grid: Vec<Vec<i32>>) -> Vec<i32> {
-    todo!()
+    let num_rows = grid.len();
+    let num_cols = if num_rows > 0 { grid[0].len() } else { 0 };
+    let mut answer = vec![0; num_cols];
+    for col in 0..num_cols {
+        let mut max_length = 0;
+        for row in 0..num_rows {
+            max_length = max_length.max(format!("{}", grid[row][col]).len() as i32);
+        }
+        answer[col] = max_length;
+    }
+    answer
 }
 
 fn main() {
