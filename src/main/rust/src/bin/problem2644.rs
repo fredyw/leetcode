3715ff992b334f2e8fe739ec6501e2190b41c410
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-04-16 10:16:16 -0500

    Skeleton for problem 2644

---
+++ src/main/rust/src/bin/problem2644.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/find-the-maximum-divisibility-score/
+pub fn max_div_score(nums: Vec<i32>, divisors: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", max_div_score(vec![4, 7, 9, 3, 9], vec![5, 2, 3])); // 3
+    println!("{}", max_div_score(vec![20, 14, 21, 10], vec![5, 7, 5])); // 5
+    println!("{}", max_div_score(vec![12], vec![10, 16])); // 10
+}
