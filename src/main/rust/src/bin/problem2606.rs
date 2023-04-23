18d9bf79af6a1f0492593947a2a561f56c134805
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-04-23 09:57:46 -0500

    Update problem 2606

--- src/main/rust/src/bin/problem2606.rs
+++ src/main/rust/src/bin/problem2606.rs
@@ -1,6 +1,27 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/find-the-substring-with-maximum-cost/
 pub fn maximum_cost_substring(s: String, chars: String, vals: Vec<i32>) -> i32 {
-    todo!()
+    let mut map: HashMap<char, i32> = chars
+        .chars()
+        .enumerate()
+        .map(|(i, c)| (c, vals[i]))
+        .collect();
+    for (i, c) in ('a'..='z').enumerate() {
+        if !map.contains_key(&c) {
+            map.insert(c, i as i32 + 1);
+        }
+    }
+    let mut answer = 0;
+    let s: Vec<char> = s.chars().collect();
+    for i in 0..s.len() {
+        let mut sum = 0;
+        for j in i..s.len() {
+            sum += map.get(&s[j]).unwrap();
+            answer = answer.max(sum);
+        }
+    }
+    answer
 }
 
 fn main() {
