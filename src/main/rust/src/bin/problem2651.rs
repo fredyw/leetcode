87e94a48f1244c756dbe4e72f24eb534a33c238f
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-04-23 00:36:18 -0500

    Skeleton for problem 2651

---
+++ src/main/rust/src/bin/problem2651.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/calculate-delayed-arrival-time/
+pub fn find_delayed_arrival_time(arrival_time: i32, delayed_time: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", find_delayed_arrival_time(15, 5)); // 20
+    println!("{}", find_delayed_arrival_time(13, 11)); // 0
+}
