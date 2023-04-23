0cf46b6e455d7cff70d774ee41c9115d41cf7419
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-04-23 09:20:31 -0500

    Skeleton for problem 2640

---
+++ src/main/rust/src/problem2640.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/find-the-score-of-all-prefixes-of-an-array/
+pub fn find_prefix_score(nums: Vec<i32>) -> Vec<i64> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", find_prefix_score(vec![2, 3, 7, 5, 10])); // [4,10,24,36,56]
+    println!("{:?}", find_prefix_score(vec![1, 1, 2, 4, 8, 16])); // [2,4,8,16,32,64]
+}
