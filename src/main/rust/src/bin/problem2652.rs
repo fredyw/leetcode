cd4794c587007e4e5e3435f9a9cd63c00936792d
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-04-23 00:27:37 -0500

    Skeleton for problem 2652

---
+++ src/main/rust/src/bin/problem2652.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/sum-multiples/
+pub fn sum_of_multiples(n: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", sum_of_multiples(7)); // 21
+    println!("{}", sum_of_multiples(10)); // 40
+    println!("{}", sum_of_multiples(9)); // 30
+}
