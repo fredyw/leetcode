dbcf202c63f7e727878ffd37d307f986248ac8d8
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-04-23 09:30:11 -0500

    Solve problem 2640

--- src/main/rust/src/bin/problem2640.rs
+++ src/main/rust/src/bin/problem2640.rs
@@ -1,6 +1,12 @@
 // https://leetcode.com/problems/find-the-score-of-all-prefixes-of-an-array/
 pub fn find_prefix_score(nums: Vec<i32>) -> Vec<i64> {
-    todo!()
+    let mut answer: Vec<i64> = vec![0; nums.len()];
+    let mut max = 0;
+    for (i, num) in nums.into_iter().enumerate() {
+        max = max.max(num);
+        answer[i] += if i == 0 { 0 } else { answer[i - 1] } + num as i64 + max as i64;
+    }
+    answer
 }
 
 fn main() {
