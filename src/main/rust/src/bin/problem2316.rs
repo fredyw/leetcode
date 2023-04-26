b874169510e033ccc412428eeaa11f2358868216
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-04-25 23:56:04 -0500

    Update problem 2316

--- src/main/rust/src/bin/problem2316.rs
+++ src/main/rust/src/bin/problem2316.rs
@@ -1,6 +1,42 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/count-unreachable-pairs-of-nodes-in-an-undirected-graph/
 pub fn count_pairs(n: i32, edges: Vec<Vec<i32>>) -> i64 {
-    todo!()
+    fn dfs(map: &HashMap<i32, Vec<i32>>, visited: &mut Vec<bool>, node: i32) -> i32 {
+        let mut count = 1;
+        visited[node as usize] = true;
+        for neighbor in map.get(&node).unwrap_or(&vec![]).iter() {
+            if visited[*neighbor as usize] {
+                continue;
+            }
+            count += dfs(map, visited, *neighbor);
+        }
+        count
+    }
+
+    let mut map: HashMap<i32, Vec<i32>> = HashMap::new();
+    for edge in edges.into_iter() {
+        let from = edge[0];
+        let to = edge[1];
+        map.entry(from).or_insert(vec![]).push(to);
+        map.entry(to).or_insert(vec![]).push(from);
+    }
+
+    let mut visited = vec![false; n as usize];
+    let mut counts = vec![];
+    for i in 0..n {
+        if visited[i as usize] {
+            continue;
+        }
+        counts.push(dfs(&map, &mut visited, i));
+    }
+    let mut answer = 0;
+    for i in 0..counts.len() {
+        for j in i + 1..counts.len() {
+            answer += counts[i] as i64 * counts[j] as i64;
+        }
+    }
+    answer
 }
 
 fn main() {
