966a6b127fe9105ca28422ae19fc0b8bfb043f52
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-04-30 16:54:02 -0500

    Skeleton for problem 2660

---
+++ src/main/rust/src/bin/problem2660.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/determine-the-winner-of-a-bowling-game/
+pub fn is_winner(player1: Vec<i32>, player2: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", is_winner(vec![4, 10, 7, 9], vec![6, 5, 2, 3])); // 1
+    println!("{}", is_winner(vec![3, 5, 7, 6], vec![8, 10, 10, 2])); // 2
+    println!("{}", is_winner(vec![2, 3], vec![4, 1])); // 0
+}
