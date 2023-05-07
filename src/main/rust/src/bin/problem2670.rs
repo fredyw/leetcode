c27636418f32963018b1dae93c14b74f32420984
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-05-07 11:56:12 -0500

    Skeleton for problem 2670

---
+++ src/main/rust/src/bin/problem2670.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/find-the-distinct-difference-array/
+pub fn distinct_difference_array(nums: Vec<i32>) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", distinct_difference_array(vec![1, 2, 3, 4, 5])); // [-3,-1,1,3,5]
+    println!("{:?}", distinct_difference_array(vec![3, 2, 3, 4, 2])); // [-2,-1,0,2,3]
+}
