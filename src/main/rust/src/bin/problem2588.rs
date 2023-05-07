0cc6df03f9c83ae9e4643eb56594957ac3ead1a1
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-05-06 23:13:56 -0500

    Update problem 2588

--- src/main/rust/src/bin/problem2588.rs
+++ src/main/rust/src/bin/problem2588.rs
@@ -6,4 +6,5 @@ pub fn beautiful_subarrays(nums: Vec<i32>) -> i64 {
 fn main() {
     println!("{}", beautiful_subarrays(vec![4, 3, 1, 2, 4])); // 2
     println!("{}", beautiful_subarrays(vec![1, 10, 4])); // 0
+    println!("{}", beautiful_subarrays(vec![3, 4])); // 0
 }
