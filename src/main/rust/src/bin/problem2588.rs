9eee0ded1c581bb95b15db6d5c3129041942ea28
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-05-08 23:23:20 -0500

    Update problem 2588

--- src/main/rust/src/bin/problem2588.rs
+++ src/main/rust/src/bin/problem2588.rs
@@ -1,10 +1,30 @@
 // https://leetcode.com/problems/count-the-number-of-beautiful-subarrays/
 pub fn beautiful_subarrays(nums: Vec<i32>) -> i64 {
-    todo!()
+    let mut prefix: Vec<i32> = vec![0; nums.len()];
+    for i in 0..nums.len() {
+        if i == 0 {
+            prefix[i] = nums[i];
+        } else {
+            prefix[i] = prefix[i - 1] ^ nums[i];
+        }
+    }
+    let mut answer = 0;
+    for i in 0..nums.len() {
+        let mut xor = nums[i];
+        for j in i + 1..nums.len() {
+            xor ^= nums[j];
+            if xor == 0 {
+                answer += 1;
+            }
+        }
+    }
+    answer
 }
 
 fn main() {
     println!("{}", beautiful_subarrays(vec![4, 3, 1, 2, 4])); // 2
+    println!("{}", beautiful_subarrays(vec![4, 3, 1, 2, 4, 4])); // 4
     println!("{}", beautiful_subarrays(vec![1, 10, 4])); // 0
     println!("{}", beautiful_subarrays(vec![3, 4])); // 0
+    println!("{}", beautiful_subarrays(vec![1])); // 0
 }
