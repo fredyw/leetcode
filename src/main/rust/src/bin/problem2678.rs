5ac3f1831c71bf9020367d61d5329c1c8a256f88
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-05-13 18:56:39 -0500

    Skeleton for problem 2678

---
+++ src/main/rust/src/bin/problem2678.rs
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/number-of-senior-citizens/
+pub fn count_seniors(details: Vec<String>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        count_seniors(vec![
+            "7868190130M7522".to_string(),
+            "5303914400F9211".to_string(),
+            "9273338290F4010".to_string()
+        ])
+    ); //
+    println!(
+        "{}",
+        count_seniors(vec![
+            "1313579440F2036".to_string(),
+            "2921522980M5644".to_string()
+        ])
+    ); // 0
+}
