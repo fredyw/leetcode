4d6a94112bbed27432faacd28814ef4ffe67ae0e
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-05-14 13:18:09 -0500

    Solve problem 2682

--- src/main/rust/src/bin/problem2682.rs
+++ src/main/rust/src/bin/problem2682.rs
@@ -1,9 +1,27 @@
 // https://leetcode.com/problems/find-the-losers-of-the-circular-game/
 pub fn circular_game_losers(n: i32, k: i32) -> Vec<i32> {
-    todo!()
+    let mut visited: Vec<bool> = vec![false; n as usize];
+    let mut friend = 0;
+    let mut i: i32 = 1;
+    visited[friend as usize] = true;
+    loop {
+        friend = (friend + (i * k)) % n;
+        if visited[friend as usize] {
+            break;
+        }
+        visited[friend as usize] = true;
+        i += 1;
+    }
+    visited
+        .into_iter()
+        .enumerate()
+        .filter(|(i, v)| *i > 0 && !*v)
+        .map(|(i, _)| i as i32 + 1)
+        .collect()
 }
 
 fn main() {
     println!("{:?}", circular_game_losers(5, 2)); // [4,5]
     println!("{:?}", circular_game_losers(4, 4)); // [2,3,4]
+    println!("{:?}", circular_game_losers(2, 1)); // []
 }
