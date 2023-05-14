98485cd3f71eaea57c9e0aecb8091e51b5bb4abd
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-05-14 12:48:20 -0500

    Skeleton for problem 2682

---
+++ src/main/rust/src/bin/problem2682.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/find-the-losers-of-the-circular-game/
+pub fn circular_game_losers(n: i32, k: i32) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", circular_game_losers(5, 2)); // [4,5]
+    println!("{:?}", circular_game_losers(4, 4)); // [2,3,4]
+}
