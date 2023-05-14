423f9c0363535d4cf20c530c294a2227163fe4d6
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-05-13 19:05:02 -0500

    Solve problem 2678

--- src/main/rust/src/bin/problem2678.rs
+++ src/main/rust/src/bin/problem2678.rs
@@ -1,6 +1,13 @@
 // https://leetcode.com/problems/number-of-senior-citizens/
 pub fn count_seniors(details: Vec<String>) -> i32 {
-    todo!()
+    details
+        .into_iter()
+        .filter(|detail| {
+            let chars: Vec<char> = detail.chars().collect();
+            let age: i32 = format!("{}{}", chars[11], chars[12]).parse().unwrap();
+            age > 60
+        })
+        .count() as i32
 }
 
 fn main() {
@@ -11,7 +18,7 @@ fn main() {
             "5303914400F9211".to_string(),
             "9273338290F4010".to_string()
         ])
-    ); //
+    ); // 2
     println!(
         "{}",
         count_seniors(vec![
