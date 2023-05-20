606b7052f7d4a392521f203a6ff114a2875ba671
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-05-20 09:58:24 -0500

    Solve problem 619

--- src/main/sql/problem619.sql
+++ src/main/sql/problem619.sql
@@ -1,1 +1,10 @@
 -- https://leetcode.com/problems/biggest-single-number/
+with single_numbers as (
+    select num
+    from MyNumbers
+    group by num
+    having count(num) = 1
+    order by num desc
+)
+select max(num) as num
+from single_numbers;
