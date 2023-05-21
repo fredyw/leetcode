802ca556c089e2e0df8e6d69daa847044f8a51c7
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-05-21 14:27:25 -0500

    Solve problem 2696

--- src/main/rust/src/bin/problem2696.rs
+++ src/main/rust/src/bin/problem2696.rs
@@ -1,6 +1,28 @@
+use std::collections::VecDeque;
+
 // https://leetcode.com/problems/minimum-string-length-after-removing-substrings/
 pub fn min_length(s: String) -> i32 {
-    todo!()
+    fn is_ab_or_cd(chars: &Vec<char>, deque: &VecDeque<char>, i: usize) -> bool {
+        if deque.is_empty() {
+            return false;
+        }
+        let first = deque.back().unwrap();
+        let second = chars[i];
+        (*first == 'A' && second == 'B') || (*first == 'C' && second == 'D')
+    }
+
+    let chars: Vec<char> = s.chars().collect();
+    let mut deque: VecDeque<char> = VecDeque::new();
+    let mut i = 0;
+    while i < chars.len() {
+        if is_ab_or_cd(&chars, &deque, i) {
+            deque.pop_back();
+        } else {
+            deque.push_back(chars[i]);
+        }
+        i += 1;
+    }
+    deque.len() as i32
 }
 
 fn main() {
