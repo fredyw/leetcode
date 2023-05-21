3778046683efcdf5399395856b13d95b67b52268
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-05-21 14:31:28 -0500

    Skeleton for problem 2685

---
+++ src/main/rust/src/bin/problem2685.rs
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/count-the-number-of-complete-components/
+pub fn count_complete_components(n: i32, edges: Vec<Vec<i32>>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        count_complete_components(6, vec![vec![0, 1], vec![0, 2], vec![1, 2], vec![3, 4]])
+    ); // 3
+    println!(
+        "{}",
+        count_complete_components(
+            6,
+            vec![vec![0, 1], vec![0, 2], vec![1, 2], vec![3, 4], vec![3, 5]]
+        )
+    ); // 1
+}
