f21da72611cef36dd2322d8ed3e2395f4ff2006b
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-05-21 13:49:52 -0500

    Skeleton for problem 2697

---
+++ src/main/rust/src/bin/problem2697.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/lexicographically-smallest-palindrome/
+pub fn make_smallest_palindrome(s: String) -> String {
+    todo!()
+}
+
+fn main() {
+    println!("{}", make_smallest_palindrome("egcfe".to_string())); // "efcfe"
+    println!("{}", make_smallest_palindrome("abcd".to_string())); // "abba"
+    println!("{}", make_smallest_palindrome("seven".to_string())); // "neven"
+}
