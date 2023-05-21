40f80f0d637a74aa1386b8678dea2e006932a5f6
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-05-21 14:03:20 -0500

    Skeleton for problem 2696

---
+++ src/main/rust/src/bin/problem2696.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/minimum-string-length-after-removing-substrings/
+pub fn min_length(s: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", min_length("ABFCACDB".to_string())); // 2
+    println!("{}", min_length("ACBBD".to_string())); // 5
+}
