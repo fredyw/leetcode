208c50d2e4506dd389255fa1bcf54af2b7393cf8
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-05-21 13:58:12 -0500

    Solve problem 2697

--- src/main/rust/src/bin/problem2697.rs
+++ src/main/rust/src/bin/problem2697.rs
@@ -1,10 +1,29 @@
 // https://leetcode.com/problems/lexicographically-smallest-palindrome/
 pub fn make_smallest_palindrome(s: String) -> String {
-    todo!()
+    let mut answer: Vec<char> = vec![' '; s.len()];
+    let chars: Vec<char> = s.chars().collect();
+    let mut i = 0;
+    let mut j = s.len() as i32 - 1;
+    while i <= j {
+        if chars[i as usize] < chars[j as usize] {
+            answer[i as usize] = chars[i as usize];
+            answer[j as usize] = chars[i as usize];
+        } else if chars[i as usize] > chars[j as usize] {
+            answer[i as usize] = chars[j as usize];
+            answer[j as usize] = chars[j as usize];
+        } else {
+            answer[i as usize] = chars[i as usize];
+            answer[j as usize] = chars[j as usize];
+        }
+        i += 1;
+        j -= 1;
+    }
+    answer.into_iter().collect()
 }
 
 fn main() {
     println!("{}", make_smallest_palindrome("egcfe".to_string())); // "efcfe"
     println!("{}", make_smallest_palindrome("abcd".to_string())); // "abba"
     println!("{}", make_smallest_palindrome("seven".to_string())); // "neven"
+    println!("{}", make_smallest_palindrome("s".to_string())); // "s"
 }
