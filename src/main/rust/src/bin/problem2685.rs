c4c42a35748c2f9df4fbe84446e7dcec9404f5a2
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-05-25 12:18:02 -0500

    Update problem 2685

--- src/main/rust/src/bin/problem2685.rs
+++ src/main/rust/src/bin/problem2685.rs
@@ -15,4 +15,12 @@ fn main() {
             vec![vec![0, 1], vec![0, 2], vec![1, 2], vec![3, 4], vec![3, 5]]
         )
     ); // 1
+    println!(
+        "{}",
+        count_complete_components(4, vec![vec![0, 1], vec![1, 2], vec![2, 3], vec![3, 0]])
+    ); // 0
+    println!(
+        "{}",
+        count_complete_components(4, vec![vec![0, 1], vec![1, 2], vec![2, 3], vec![3, 0]])
+    ); // 1
 }
