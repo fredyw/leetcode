6ee3edab34295372d56b0dea0a054c031b8700bf
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-05-26 01:08:27 -0500

    Solve problem 2685

--- src/main/rust/src/bin/problem2685.rs
+++ src/main/rust/src/bin/problem2685.rs
@@ -1,6 +1,50 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/count-the-number-of-complete-components/
 pub fn count_complete_components(n: i32, edges: Vec<Vec<i32>>) -> i32 {
-    todo!()
+    fn extract_subgraph(
+        graph: &HashMap<i32, Vec<i32>>,
+        parent: i32,
+        visited: &mut Vec<bool>,
+        subgraph: &mut HashMap<i32, i32>,
+    ) {
+        if visited[parent as usize] {
+            return;
+        }
+        visited[parent as usize] = true;
+        for child in graph.get(&parent).unwrap_or(&vec![]) {
+            *subgraph.entry(parent).or_insert(0) += 1;
+            extract_subgraph(graph, *child, visited, subgraph);
+        }
+    }
+
+    fn is_complete(graph: &HashMap<i32, i32>) -> bool {
+        for count in graph.values() {
+            if *count != graph.keys().len() as i32 - 1 {
+                return false;
+            }
+        }
+        true
+    }
+
+    let mut graph: HashMap<i32, Vec<i32>> = HashMap::new();
+    for edge in edges {
+        graph.entry(edge[0]).or_insert(vec![]).push(edge[1]);
+        graph.entry(edge[1]).or_insert(vec![]).push(edge[0]);
+    }
+    let mut visited: Vec<bool> = vec![false; n as usize];
+    let mut answer = 0;
+    for i in 0..n {
+        if visited[i as usize] {
+            continue;
+        }
+        let mut subgraph: HashMap<i32, i32> = HashMap::new();
+        extract_subgraph(&graph, i, &mut visited, &mut subgraph);
+        if is_complete(&subgraph) {
+            answer += 1;
+        }
+    }
+    answer
 }
 
 fn main() {
