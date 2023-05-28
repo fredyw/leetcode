3e87ea88b286c868df3562fe19dd9df5d00da1a9
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-05-28 15:10:31 -0500

    Solve problem 2710

--- src/main/rust/src/bin/problem2710.rs
+++ src/main/rust/src/bin/problem2710.rs
@@ -1,6 +1,14 @@
 // https://leetcode.com/problems/remove-trailing-zeros-from-a-string/
 pub fn remove_trailing_zeros(num: String) -> String {
-    todo!()
+    let chars: Vec<char> = num.chars().collect();
+    let mut i = num.len() as i32 - 1;
+    while i >= 0 {
+        if chars[i as usize] != '0' {
+            break;
+        }
+        i -= 1;
+    }
+    (&chars[0..i as usize + 1]).iter().collect::<String>()
 }
 
 fn main() {
