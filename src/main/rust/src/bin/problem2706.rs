13d040d6360b5bbe595c201d0f9c386771164638
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-05-28 15:00:05 -0500

    Skeleton for problem 2706

---
+++ src/main/rust/src/bin/problem2706.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/buy-two-chocolates/
+pub fn buy_choco(prices: Vec<i32>, money: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", buy_choco(vec![1, 2, 2], 3)); // 0
+    println!("{}", buy_choco(vec![3, 2, 3], 3)); // 3
+}
