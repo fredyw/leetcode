960f1c2d531771ead9de8a5063d99ffe36eb8960
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-05-28 15:02:16 -0500

    Skeleton for problem 2710

---
+++ src/main/rust/src/bin/problem2710.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/remove-trailing-zeros-from-a-string/
+pub fn remove_trailing_zeros(num: String) -> String {
+    todo!()
+}
+
+fn main() {
+    println!("{}", remove_trailing_zeros("51230100".to_string())); // "512301"
+    println!("{}", remove_trailing_zeros("123".to_string())); // "123"
+}
