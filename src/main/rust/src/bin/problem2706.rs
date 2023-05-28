25488dfab37855bb69f7de7d5e71775dc7a142f9
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-05-28 15:13:36 -0500

    Solve problem 2706

--- src/main/rust/src/bin/problem2706.rs
+++ src/main/rust/src/bin/problem2706.rs
@@ -1,6 +1,11 @@
 // https://leetcode.com/problems/buy-two-chocolates/
-pub fn buy_choco(prices: Vec<i32>, money: i32) -> i32 {
-    todo!()
+pub fn buy_choco(mut prices: Vec<i32>, money: i32) -> i32 {
+    prices.sort();
+    if prices[0] + prices[1] <= money {
+        money - (prices[0] + prices[1])
+    } else {
+        money
+    }
 }
 
 fn main() {
