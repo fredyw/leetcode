7cfe8cb06c4f6dd4ecf3200c7b265921c55640e4
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-05-29 20:06:48 -0500

    Skeleton for problem 2683

---
+++ src/main/rust/src/bin/problem2683.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/neighboring-bitwise-xor/
+pub fn does_valid_array_exist(derived: Vec<i32>) -> bool {
+    todo!()
+}
+
+fn main() {
+    println!("{}", does_valid_array_exist(vec![1, 1, 0])); // true
+    println!("{}", does_valid_array_exist(vec![1, 1])); // true
+    println!("{}", does_valid_array_exist(vec![1, 0])); // false
+}
