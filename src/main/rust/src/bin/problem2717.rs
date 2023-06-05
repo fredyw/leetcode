5c1af2d23aee834d60bdf00ad32badbee4d57543
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-06-04 20:35:41 -0500

    Skeleton for problem 2717

---
+++ src/main/rust/src/bin/problem2717.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/semi-ordered-permutation/
+pub fn semi_ordered_permutation(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", semi_ordered_permutation(vec![2, 1, 4, 3])); // 2
+    println!("{}", semi_ordered_permutation(vec![2, 4, 1, 3])); // 3
+    println!("{}", semi_ordered_permutation(vec![1, 3, 4, 2, 5])); // 0
+}
