54a475dc7f5997e7ef2197fda8ab1878f23acf66
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-06-04 20:24:35 -0500

    Skeleton for problem 2716

---
+++ src/main/rust/src/bin/problem2716.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/minimize-string-length/
+pub fn minimized_string_length(s: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", minimized_string_length("aaabc".to_string())); // 3
+    println!("{}", minimized_string_length("cbbd".to_string())); // 3
+    println!("{}", minimized_string_length("dddaaa".to_string())); // 2
+}
