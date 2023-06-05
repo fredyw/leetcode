eee847f14b02e975e81373dd73497a86ea693344
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-06-04 20:27:18 -0500

    Solve problem 2716

--- src/main/rust/src/bin/problem2716.rs
+++ src/main/rust/src/bin/problem2716.rs
@@ -1,10 +1,13 @@
+use std::collections::HashSet;
+
 // https://leetcode.com/problems/minimize-string-length/
 pub fn minimized_string_length(s: String) -> i32 {
-    todo!()
+    s.chars().collect::<HashSet<char>>().len() as i32
 }
 
 fn main() {
     println!("{}", minimized_string_length("aaabc".to_string())); // 3
     println!("{}", minimized_string_length("cbbd".to_string())); // 3
     println!("{}", minimized_string_length("dddaaa".to_string())); // 2
+    println!("{}", minimized_string_length("dddaaadd".to_string())); // 2
 }
