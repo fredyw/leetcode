d9c81af4ed2a133276c652d4c5fd8cd50537de48
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-06-05 20:48:30 -0500

    Skeleton for problem 2698

---
+++ src/main/rust/src/bin/problem2698.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/find-the-punishment-number-of-an-integer/
+pub fn punishment_number(n: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", punishment_number(10)); // 182
+    println!("{}", punishment_number(37)); // 1478
+}
