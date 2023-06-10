df9bdfe86da79793bb0ce6d148983b1ec3b52265
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-06-09 22:53:43 -0500

    Solve problem 2698

--- src/main/rust/src/bin/problem2698.rs
+++ src/main/rust/src/bin/problem2698.rs
@@ -1,7 +1,6 @@
 // https://leetcode.com/problems/find-the-punishment-number-of-an-integer/
 pub fn punishment_number(n: i32) -> i32 {
-    fn is_punishment_number(num: i32, chars: &Vec<char>, i: usize, acc: i32, s: String) -> bool {
-        let sum = s.parse::<i32>().unwrap_or(0);
+    fn is_punishment_number(num: i32, chars: &Vec<char>, i: usize, acc: i32, sum: i32) -> bool {
         if sum > num {
             return false;
         }
@@ -9,10 +8,10 @@ pub fn punishment_number(n: i32) -> i32 {
             return acc + sum == num;
         }
         let n = chars[i] as i32 - '0' as i32;
-        if is_punishment_number(num, chars, i + 1, acc + sum, n.to_string()) {
+        if is_punishment_number(num, chars, i + 1, acc + sum, n) {
             return true;
         }
-        if is_punishment_number(num, chars, i + 1, acc, s + &n.to_string()) {
+        if is_punishment_number(num, chars, i + 1, acc, sum * 10 + n) {
             return true;
         }
         false
@@ -21,7 +20,7 @@ pub fn punishment_number(n: i32) -> i32 {
     let mut answer = 0;
     for i in 1..=n {
         let chars: Vec<char> = (i * i).to_string().chars().collect();
-        if is_punishment_number(i, &chars, 0, 0, String::new()) {
+        if is_punishment_number(i, &chars, 0, 0, 0) {
             answer += i * i;
         }
     }
