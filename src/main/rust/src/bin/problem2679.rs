d01b50c018969bcbd1dc51ae7eb2710f2d6bcfec
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-06-09 22:55:46 -0500

    Skeleton for problem 2679

---
+++ src/main/rust/src/bin/problem2679.rs
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/sum-in-a-matrix/
+pub fn matrix_sum(nums: Vec<Vec<i32>>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        matrix_sum(vec![
+            vec![7, 2, 1],
+            vec![6, 4, 2],
+            vec![6, 5, 3],
+            vec![3, 2, 1]
+        ])
+    ); // 15
+    println!("{}", matrix_sum(vec![vec![1]])); // 1
+}
