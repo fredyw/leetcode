cfdc0cda73bdb51f0940204e5903492bf5cf62cd
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-06-10 12:19:20 -0500

    Skeleton for problem 2673

---
+++ src/main/rust/src/bin/problem2673.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/make-costs-of-paths-equal-in-a-binary-tree/
+pub fn min_increments(n: i32, cost: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", min_increments(7, vec![1, 5, 2, 2, 3, 3, 1])); // 6
+    println!("{}", min_increments(3, vec![5, 3, 3])); // 0
+}
