cda0f7fec2b9c38d8a006512a663885e6fa586e8
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-06-10 20:41:41 -0500

    Skeleton for problem 2729

---
+++ src/main/rust/src/bin/problem2729.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/check-if-the-number-is-fascinating/
+pub fn is_fascinating(n: i32) -> bool {
+    todo!()
+}
+
+fn main() {
+    println!("{}", is_fascinating(192)); // true
+    println!("{}", is_fascinating(100)); // false
+}
