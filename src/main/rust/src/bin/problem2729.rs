c479a1b479c1552441786ee09da6db3f56cca1a4
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-06-10 20:51:23 -0500

    Solve problem 2729

--- src/main/rust/src/bin/problem2729.rs
+++ src/main/rust/src/bin/problem2729.rs
@@ -1,9 +1,23 @@
+use std::collections::HashSet;
+
 // https://leetcode.com/problems/check-if-the-number-is-fascinating/
 pub fn is_fascinating(n: i32) -> bool {
-    todo!()
+    let chars = format!("{}{}{}", n, n * 2, n * 3)
+        .chars()
+        .collect::<Vec<char>>();
+    if chars.len() != 9 {
+        return false;
+    }
+    chars
+        .into_iter()
+        .filter(|c| *c != '0')
+        .collect::<HashSet<char>>()
+        .len()
+        == 9
 }
 
 fn main() {
     println!("{}", is_fascinating(192)); // true
     println!("{}", is_fascinating(100)); // false
+    println!("{}", is_fascinating(783)); // false
 }
