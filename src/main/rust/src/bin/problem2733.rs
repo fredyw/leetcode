7967d30cb7ce45d8ac61a61ca2b66165198baeea
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-06-11 12:37:27 -0500

    Skeleton for problem 2733

---
+++ src/main/rust/src/bin/problem2733.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/neither-minimum-nor-maximum/
+pub fn find_non_min_or_max(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", find_non_min_or_max(vec![3, 2, 1, 4])); // 2
+    println!("{}", find_non_min_or_max(vec![1, 2])); // -1
+    println!("{}", find_non_min_or_max(vec![2, 1, 3])); // 2
+}
