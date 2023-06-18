7067753272b77f8c4a77b99d636bdf4dd2b2430d
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-06-18 12:25:50 -0500

    Update problem 2739

---
+++ src/main/rust/src/bin/problem2739.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/total-distance-traveled/
+pub fn distance_traveled(main_tank: i32, additional_tank: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", distance_traveled(5, 10)); // 60
+    println!("{}", distance_traveled(1, 2)); // 10
+}
