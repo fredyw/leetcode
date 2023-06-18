8d1ea2a3cc595fa46830b6e1c4d3c5b74dd85f35
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-06-18 13:08:11 -0500

    Solve problem 2739

--- src/main/rust/src/bin/problem2739.rs
+++ src/main/rust/src/bin/problem2739.rs
@@ -1,9 +1,26 @@
+use std::cmp::min;
+
 // https://leetcode.com/problems/total-distance-traveled/
-pub fn distance_traveled(main_tank: i32, additional_tank: i32) -> i32 {
-    todo!()
+pub fn distance_traveled(mut main_tank: i32, mut additional_tank: i32) -> i32 {
+    let mut answer = 0;
+    while main_tank > 0 {
+        if main_tank >= 5 {
+            if additional_tank > 0 {
+                main_tank += 1;
+                additional_tank -= 1;
+            }
+        }
+        let min_tank = min(main_tank, 5);
+        main_tank -= min(min_tank, 5);
+        answer += min_tank * 10;
+    }
+    answer
 }
 
 fn main() {
     println!("{}", distance_traveled(5, 10)); // 60
     println!("{}", distance_traveled(1, 2)); // 10
+    println!("{}", distance_traveled(9, 2)); // 110
+    println!("{}", distance_traveled(100, 4)); // 1040
+    println!("{}", distance_traveled(100, 68)); // 1240
 }
