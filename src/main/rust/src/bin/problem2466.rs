daa0d7fd0c1d2030dccb72a0e89a6bb443afb284
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-06-18 21:15:44 -0500

    Skeleton for problem 2466

---
+++ src/main/rust/src/bin/problem2466.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/count-ways-to-build-good-strings/
+pub fn count_good_strings(low: i32, high: i32, zero: i32, one: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", count_good_strings(3, 3, 1, 1)); // 8
+    println!("{}", count_good_strings(2, 3, 1, 2)); // 5
+}
