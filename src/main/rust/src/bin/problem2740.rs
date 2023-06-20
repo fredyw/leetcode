34b20c7c5fd55143fa7ad71508457fc48472b3b1
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-06-20 11:09:36 -0500

    Skeleton for problem 2740

---
+++ src/main/rust/src/bin/problem2740.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/find-the-value-of-the-partition/
+pub fn find_value_of_partition(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", find_value_of_partition(vec![1, 3, 2, 4])); // 1
+    println!("{}", find_value_of_partition(vec![100, 1, 10])); // 9
+}
