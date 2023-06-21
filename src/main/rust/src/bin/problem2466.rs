65111fde30297e7771df4cd2b00f777fd2244a04
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-06-20 23:54:57 -0500

    Update problem 2466

--- src/main/rust/src/bin/problem2466.rs
+++ src/main/rust/src/bin/problem2466.rs
@@ -1,21 +1,43 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/count-ways-to-build-good-strings/
 pub fn count_good_strings(low: i32, high: i32, zero: i32, one: i32) -> i32 {
-    fn count_good_strings(low: i32, high: i32, zero: i32, one: i32, length: i32) -> i64 {
+    fn count_good_strings(
+        low: i64,
+        high: i64,
+        zero: i64,
+        one: i64,
+        length: i64,
+        memo: &mut HashMap<i64, i64>,
+    ) -> i64 {
         if length > high {
             return 0;
+        }
+        if let Some(m) = memo.get(&length) {
+            return *m;
         }
         let good_string = if length >= low && length <= high {
             1
         } else {
             0
         };
-        let count = count_good_strings(low, high, zero, one, length + zero)
-            + count_good_strings(low, high, zero, one, length + one)
-            + good_string;
-        count % 10000000007
+        let count = (count_good_strings(low, high, zero, one, length + zero, memo)
+            + count_good_strings(low, high, zero, one, length + one, memo)
+            + good_string)
+            % 1000000007;
+        memo.insert(length, count);
+        count
     }
 
-    count_good_strings(low, high, zero, one, 0) as i32
+    let mut memo: HashMap<i64, i64> = HashMap::new();
+    count_good_strings(
+        low as i64,
+        high as i64,
+        zero as i64,
+        one as i64,
+        0,
+        &mut memo,
+    ) as i32
 }
 
 fn main() {
