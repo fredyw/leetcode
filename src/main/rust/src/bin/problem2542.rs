6466e1c33ce43e52c89b3c8ed6636444689173b7
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-06-21 12:43:45 -0500

    Skeleton for problem 2542

---
+++ src/main/rust/src/bin/problem2542.rs
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/maximum-subsequence-score/
+pub fn max_score(nums1: Vec<i32>, nums2: Vec<i32>, k: i32) -> i64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", max_score(vec![1, 3, 3, 2], vec![2, 1, 3, 4], 3)); // 12
+    println!(
+        "{}",
+        max_score(vec![4, 2, 3, 1, 1], vec![7, 5, 10, 9, 6], 1)
+    ); // 30
+}
