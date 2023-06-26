c0b3ea7342377375c4967a4d03c88ad6ddfa67cb
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-06-26 10:23:52 -0500

    Skeleton for problem 2748

---
+++ src/main/rust/src/bin/problem2748.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/number-of-beautiful-pairs/
+pub fn count_beautiful_pairs(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", count_beautiful_pairs(vec![2, 5, 1, 4])); // 5
+    println!("{}", count_beautiful_pairs(vec![11, 21, 12])); // 2
+}
