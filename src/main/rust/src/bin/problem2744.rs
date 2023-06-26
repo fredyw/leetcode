1f6c65491e04cc589bd4538dde33af1320b1d73a
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-06-26 10:22:34 -0500

    Solve problem 2744

--- src/main/rust/src/bin/problem2744.rs
+++ src/main/rust/src/bin/problem2744.rs
@@ -1,6 +1,23 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/find-maximum-number-of-string-pairs/
 pub fn maximum_number_of_string_pairs(words: Vec<String>) -> i32 {
-    todo!()
+    let mut word_map: HashMap<String, i32> = HashMap::new();
+    for word in words.iter() {
+        *word_map.entry(word.clone()).or_insert(0) += 1;
+    }
+    let mut answer = 0;
+    for word in words.iter() {
+        let reversed_word: String = word.chars().rev().collect();
+        if word == &reversed_word {
+            continue;
+        }
+        let count = word_map.get(&reversed_word).unwrap_or(&0);
+        if count > &0 {
+            answer += 1;
+        }
+    }
+    answer / 2
 }
 
 fn main() {
@@ -20,6 +37,21 @@ fn main() {
     ); // 1
     println!(
         "{}",
-        maximum_number_of_string_pairs(vec!["aa".to_string(), "ab".to_string(),])
+        maximum_number_of_string_pairs(vec!["aa".to_string(), "ab".to_string()])
+    ); // 0
+    println!(
+        "{}",
+        maximum_number_of_string_pairs(vec![
+            "ff".to_string(),
+            "tx".to_string(),
+            "qr".to_string(),
+            "zw".to_string(),
+            "wr".to_string(),
+            "jr".to_string(),
+            "zt".to_string(),
+            "jk".to_string(),
+            "sq".to_string(),
+            "xx".to_string()
+        ])
     ); // 0
 }
