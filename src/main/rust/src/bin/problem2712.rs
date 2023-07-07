eb7222a7847fd4bd79071954ff1bc65540b5ed98
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-07-06 21:00:38 -0500

    Skeleton for problem 2712

---
+++ src/main/rust/src/bin/problem2712.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/minimum-cost-to-make-all-characters-equal/
+pub fn minimum_cost(s: String) -> i64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", minimum_cost("0011".to_string())); // 2
+    println!("{}", minimum_cost("010101".to_string())); // 9
+}
