e0a8d0c1cf76b4a02a562ce7a26b9e55c030fc01
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-07-06 20:55:51 -0500

    Solve problem 2672

--- src/main/rust/src/bin/problem2672.rs
+++ src/main/rust/src/bin/problem2672.rs
@@ -1,6 +1,36 @@
 // https://leetcode.com/problems/number-of-adjacent-elements-with-the-same-color/
 pub fn color_the_array(n: i32, queries: Vec<Vec<i32>>) -> Vec<i32> {
     let mut answer = vec![0; queries.len()];
+    let mut nums = vec![0; n as usize];
+    let mut total_count = 0;
+    for (i, query) in queries.into_iter().enumerate() {
+        let index = query[0];
+        let color = query[1];
+        let mut count = 0;
+        if nums[index as usize] != 0 && index as usize + 1 < nums.len() {
+            if nums[index as usize] == nums[index as usize + 1] {
+                count += 1;
+            }
+        }
+        if nums[index as usize] != 0 && index - 1 >= 0 {
+            if nums[index as usize] == nums[index as usize - 1] {
+                count += 1;
+            }
+        }
+        total_count -= count;
+        nums[index as usize] = color;
+        if index as usize + 1 < nums.len() {
+            if nums[index as usize] == nums[index as usize + 1] {
+                total_count += 1;
+            }
+        }
+        if index - 1 >= 0 {
+            if nums[index as usize] == nums[index as usize - 1] {
+                total_count += 1;
+            }
+        }
+        answer[i] = total_count;
+    }
     answer
 }
 
