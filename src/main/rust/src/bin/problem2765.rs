92e60c458edae7738e63992ca97279e487f70c2f
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-07-09 12:58:23 -0500

    Skeleton for problem 2765

---
+++ src/main/rust/src/bin/problem2765.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/longest-alternating-subarray/
+pub fn alternating_subarray(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", alternating_subarray(vec![2, 3, 4, 3, 4])); // 4
+    println!("{}", alternating_subarray(vec![4, 5, 6])); // 2
+}
