2e3448af8202c1e0e2e5e88c64bfcb1f2c4a035e
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-07-09 12:41:26 -0500

    Skeleton for problem 2769

---
+++ src/main/rust/src/bin/problem2769.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/find-the-maximum-achievable-number/
+pub fn the_maximum_achievable_x(num: i32, t: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", the_maximum_achievable_x(4, 1)); // 6
+    println!("{}", the_maximum_achievable_x(3, 2)); // 7
+}
