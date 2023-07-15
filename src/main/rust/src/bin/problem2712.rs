71782815c83efd0717ce7babd3232140205e6edf
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-07-15 12:41:58 -0500

    Update problem 2712

--- src/main/rust/src/bin/problem2712.rs
+++ src/main/rust/src/bin/problem2712.rs
@@ -6,4 +6,7 @@ pub fn minimum_cost(s: String) -> i64 {
 fn main() {
     println!("{}", minimum_cost("0011".to_string())); // 2
     println!("{}", minimum_cost("010101".to_string())); // 9
+    println!("{}", minimum_cost("101101".to_string())); // 6
+    println!("{}", minimum_cost("10010110".to_string())); // 12
+    println!("{}", minimum_cost("1010101101110001".to_string())); // 41
 }
