a13bac6825fd772beb3fea4908438b9828abb422
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-07-15 13:46:21 -0500

    Skeleton for problem 2767

---
+++ src/main/rust/src/bin/problem2767.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/partition-string-into-minimum-beautiful-substrings/
+pub fn minimum_beautiful_substrings(s: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", minimum_beautiful_substrings("1011".to_string())); // 2
+    println!("{}", minimum_beautiful_substrings("111".to_string())); // 3
+    println!("{}", minimum_beautiful_substrings("0".to_string())); // -1
+}
