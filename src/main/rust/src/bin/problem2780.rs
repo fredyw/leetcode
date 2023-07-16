e0e11d0bcc713682090949bc90faf84f16ee859c
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-07-16 13:34:30 -0500

    Solve problem 2780

--- src/main/rust/src/bin/problem2780.rs
+++ src/main/rust/src/bin/problem2780.rs
@@ -1,6 +1,32 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/minimum-index-of-a-valid-split/
 pub fn minimum_index(nums: Vec<i32>) -> i32 {
-    todo!()
+    let mut dominant_num = 0;
+    let mut dominant_count = 0;
+    let mut map: HashMap<i32, i32> = HashMap::new();
+    for num in nums.iter() {
+        *map.entry(*num).or_insert(0) += 1;
+        let count = map.get(&num).unwrap();
+        if *count > dominant_count {
+            dominant_num = *num;
+            dominant_count = *count;
+        }
+    }
+    let mut current_count = 0;
+    for (i, num) in nums.iter().enumerate() {
+        if *num == dominant_num {
+            current_count += 1;
+            let left_length: i32 = i as i32 + 1;
+            let right_length: i32 = nums.len() as i32 - i as i32 - 1;
+            if current_count * 2 > left_length
+                && (dominant_count - current_count) * 2 > right_length
+            {
+                return i as i32;
+            }
+        }
+    }
+    -1
 }
 
 fn main() {
