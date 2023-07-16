8487216105717b64b8271249b6bba5f25721f9f1
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-07-16 13:02:22 -0500

    Update problem 2780

--- src/main/rust/src/bin/problem2780.rs
+++ src/main/rust/src/bin/problem2780.rs
@@ -6,4 +6,5 @@ pub fn minimum_index(nums: Vec<i32>) -> i32 {
 fn main() {
     println!("{}", minimum_index(vec![1, 2, 2, 2])); // 2
     println!("{}", minimum_index(vec![2, 1, 3, 1, 1, 1, 7, 1, 2, 1])); // 4
+    println!("{}", minimum_index(vec![3, 3, 3, 3, 7, 2, 2])); // -1
 }
