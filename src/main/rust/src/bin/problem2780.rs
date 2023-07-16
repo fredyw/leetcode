20fd0bbae2b9b29b57be3c93d7e04c4d0bb8fe36
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-07-16 12:56:55 -0500

    Skeleton for problem 2780

---
+++ src/main/rust/src/bin/problem2780.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/minimum-index-of-a-valid-split/
+pub fn minimum_index(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", minimum_index(vec![1, 2, 2, 2])); // 2
+    println!("{}", minimum_index(vec![2, 1, 3, 1, 1, 1, 7, 1, 2, 1])); // 4
+}
