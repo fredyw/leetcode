80706f4053fa5ad0470cd243b70d0054408eeead
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-07-22 13:57:50 -0500

    Solve problem 2784

--- src/main/rust/src/bin/problem2784.rs
+++ src/main/rust/src/bin/problem2784.rs
@@ -1,6 +1,18 @@
 // https://leetcode.com/problems/check-if-array-is-good/
-pub fn is_good(nums: Vec<i32>) -> bool {
-    todo!()
+pub fn is_good(mut nums: Vec<i32>) -> bool {
+    nums.sort();
+    if nums.len() != nums[nums.len() - 1] as usize + 1 {
+        return false;
+    }
+    if nums[0] != 1 {
+        return false;
+    }
+    for i in 0..nums.len() - 2 {
+        if nums[i] + 1 != nums[i + 1] {
+            return false;
+        }
+    }
+    nums[nums.len() - 2] == nums[nums.len() - 1]
 }
 
 fn main() {
