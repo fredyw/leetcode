4f83b9073d37236d3e6ffe66b370823833da8cf7
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-07-22 13:27:51 -0500

    Solve problem 2767

--- src/main/rust/src/bin/problem2767.rs
+++ src/main/rust/src/bin/problem2767.rs
@@ -1,21 +1,63 @@
 // https://leetcode.com/problems/partition-string-into-minimum-beautiful-substrings/
 pub fn minimum_beautiful_substrings(s: String) -> i32 {
-    fn is_power_of_5(n: u32) -> bool {
-        if n == 0 {
-            return false;
+    fn is_power_of_5(n: i32) -> bool {
+        let mut x = n;
+        while x > 1 && x % 5 == 0 {
+            x /= 5;
         }
-        if n == 1 {
-            return true;
-        }
-        let p = f32::powf(n as f32, 1.0 / 5.0);
-        5u32.pow(p as u32) == n
+        x == 1
     }
 
-    todo!()
+    fn minimum_beautiful_substrings(chars: &Vec<char>, i: usize, num: i32) -> i32 {
+        if i == chars.len() {
+            if is_power_of_5(num) {
+                return 1;
+            }
+            return i32::MAX;
+        }
+        let n = if chars[i] == '0' { 0 } else { 1 };
+        let new_num = (num << 1) ^ n;
+        if new_num == 0 {
+            return i32::MAX;
+        }
+        let mut min = i32::MAX;
+        if is_power_of_5(new_num) {
+            let m = minimum_beautiful_substrings(chars, i + 1, 0);
+            if m != i32::MAX {
+                min = min.min(m + 1);
+            }
+        }
+        let m = minimum_beautiful_substrings(chars, i + 1, new_num);
+        if m != i32::MAX {
+            min = min.min(m);
+        }
+        min
+    }
+
+    let chars: Vec<char> = s.chars().into_iter().collect();
+    let min = minimum_beautiful_substrings(&chars, 0, 0);
+    if min == i32::MAX {
+        -1
+    } else {
+        min
+    }
 }
 
 fn main() {
-    // println!("{}", minimum_beautiful_substrings("1011".to_string())); // 2
-    // println!("{}", minimum_beautiful_substrings("111".to_string())); // 3
-    // println!("{}", minimum_beautiful_substrings("0".to_string())); // -1
+    println!("{}", minimum_beautiful_substrings("1011".to_string())); // 2
+    println!("{}", minimum_beautiful_substrings("111".to_string())); // 3
+    println!("{}", minimum_beautiful_substrings("0".to_string())); // -1
+    println!(
+        "{}",
+        minimum_beautiful_substrings("11010101010010".to_string())
+    ); // -1
+    println!("{}", minimum_beautiful_substrings("0111".to_string())); // -1
+    println!(
+        "{}",
+        minimum_beautiful_substrings("100111000110111".to_string())
+    ); // 4
+    println!(
+        "{}",
+        minimum_beautiful_substrings("11110100001001".to_string())
+    ); // 1
 }
