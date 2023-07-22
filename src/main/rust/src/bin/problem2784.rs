e817e79408d19fc36ef39c6afcf6cc57a9737eb1
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-07-22 13:47:47 -0500

    Skeleton for problem 2784

---
+++ src/main/rust/src/bin/problem2784.rs
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/check-if-array-is-good/
+pub fn is_good(nums: Vec<i32>) -> bool {
+    todo!()
+}
+
+fn main() {
+    println!("{}", is_good(vec![2, 1, 3])); // false
+    println!("{}", is_good(vec![1, 3, 3, 2])); // true
+    println!("{}", is_good(vec![1, 1])); // true
+    println!("{}", is_good(vec![3, 4, 4, 1, 2, 1])); // false
+}
