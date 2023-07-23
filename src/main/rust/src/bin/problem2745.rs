3216d279f4ab61c3776abeaad2b8afd09d2bb012
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-07-23 13:08:56 -0500

    Skeleton for problem 2745

---
+++ src/main/rust/src/bin/problem2745.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/construct-the-longest-new-string/
+pub fn longest_string(x: i32, y: i32, z: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", longest_string(2, 5, 1)); // 12
+    println!("{}", longest_string(3, 2, 2)); // 14
+}
