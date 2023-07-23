0675f55e175d6121136e94595406ba57a263dc3f
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-07-23 13:03:53 -0500

    Solve problem 2785

--- src/main/rust/src/bin/problem2785.rs
+++ src/main/rust/src/bin/problem2785.rs
@@ -1,6 +1,29 @@
 // https://leetcode.com/problems/sort-vowels-in-a-string/
 pub fn sort_vowels(s: String) -> String {
-    todo!()
+    fn is_vowel(c: char) -> bool {
+        c == 'a'
+            || c == 'e'
+            || c == 'i'
+            || c == 'o'
+            || c == 'u'
+            || c == 'A'
+            || c == 'E'
+            || c == 'I'
+            || c == 'O'
+            || c == 'U'
+    }
+
+    let mut chars: Vec<char> = s.chars().collect();
+    let mut vowels: Vec<char> = chars.iter().copied().filter(|c| is_vowel(*c)).collect();
+    vowels.sort_by(|a, b| a.cmp(b));
+    let mut j = 0;
+    for i in 0..chars.len() {
+        if is_vowel(chars[i]) {
+            chars[i] = vowels[j];
+            j += 1;
+        }
+    }
+    chars.into_iter().collect()
 }
 
 fn main() {
