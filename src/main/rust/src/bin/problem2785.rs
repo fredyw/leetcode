b3cdb99ea2da0ab77e158ef333cebaa1a1fd14a0
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-07-23 12:41:04 -0500

    Skeleton for problem 2785

---
+++ src/main/rust/src/bin/problem2785.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/sort-vowels-in-a-string/
+pub fn sort_vowels(s: String) -> String {
+    todo!()
+}
+
+fn main() {
+    println!("{}", sort_vowels("lEetcOde".to_string())); // "lEOtcede"
+    println!("{}", sort_vowels("lYmpH".to_string())); // "lYmpH"
+}
