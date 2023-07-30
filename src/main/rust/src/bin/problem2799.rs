adb48e017439d841a65aca76d2df514f2c774ffb
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-07-30 12:49:15 -0500

    Skeleton for problem 2799

---
+++ src/main/rust/src/bin/problem2799.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/construct-the-longest-new-string/
+pub fn count_complete_subarrays(nums: Vec<i32>) -> i32 {
+    todo!();
+}
+
+fn main() {
+    println!("{}", count_complete_subarrays(vec![1, 3, 1, 2, 2])); // 4
+    println!("{}", count_complete_subarrays(vec![5, 5, 5, 5])); // 10
+}
