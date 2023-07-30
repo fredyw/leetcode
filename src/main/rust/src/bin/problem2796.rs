bedd9173cb5b1f9602fd9bce528d6d010b2bf849
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-07-29 23:36:53 -0500

    Solve problem 2796

--- src/main/rust/src/bin/problem2796.rs
+++ src/main/rust/src/bin/problem2796.rs
@@ -1,6 +1,6 @@
 // https://leetcode.com/problems/number-of-employees-who-met-the-target/
 pub fn number_of_employees_who_met_target(hours: Vec<i32>, target: i32) -> i32 {
-    todo!();
+    hours.into_iter().filter(|h| *h as i32 >= target).count() as i32
 }
 
 fn main() {
@@ -10,6 +10,6 @@ fn main() {
     ); // 3
     println!(
         "{}",
-        number_of_employees_who_met_target(vec![5,1,4,2,2], 6)
+        number_of_employees_who_met_target(vec![5, 1, 4, 2, 2], 6)
     ); // 0
 }
