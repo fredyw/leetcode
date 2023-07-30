3801d0fbf0c2b31a4fddef270ba46b7f29b888a3
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-07-29 23:32:58 -0500

    Skeleton for problem 2796

---
+++ src/main/rust/src/bin/problem2796.rs
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/number-of-employees-who-met-the-target/
+pub fn number_of_employees_who_met_target(hours: Vec<i32>, target: i32) -> i32 {
+    todo!();
+}
+
+fn main() {
+    println!(
+        "{}",
+        number_of_employees_who_met_target(vec![0, 1, 2, 3, 4], 2)
+    ); // 3
+    println!(
+        "{}",
+        number_of_employees_who_met_target(vec![5,1,4,2,2], 6)
+    ); // 0
+}
