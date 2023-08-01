c7554fb77a0fa7d0a2e075709c2f07ef58531ec2
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-07-31 20:16:20 -0500

    Solve problem 2745

--- src/main/rust/src/bin/problem2745.rs
+++ src/main/rust/src/bin/problem2745.rs
@@ -1,9 +1,15 @@
 // https://leetcode.com/problems/construct-the-longest-new-string/
 pub fn longest_string(x: i32, y: i32, z: i32) -> i32 {
-    todo!()
+    if x == y {
+        (x * 2) + (y * 2) + (z * 2)
+    } else {
+        let min = x.min(y);
+        (min * 2) + ((min + 1) * 2) + (z * 2)
+    }
 }
 
 fn main() {
     println!("{}", longest_string(2, 5, 1)); // 12
     println!("{}", longest_string(3, 2, 2)); // 14
+    println!("{}", longest_string(40, 50, 24)); // 210
 }
