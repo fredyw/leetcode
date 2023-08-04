a00a254fe6b3b88656862c3316f8339d6c340c6b
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-08-04 00:26:29 -0500

    Skeleton for problem 2645

---
+++ src/main/rust/src/bin/problem2645.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/minimum-additions-to-make-valid-string/
+pub fn add_minimum(word: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", add_minimum("b".to_string())); // 2
+    println!("{}", add_minimum("aaa".to_string())); // 6
+    println!("{}", add_minimum("abc".to_string())); // 0
+}
