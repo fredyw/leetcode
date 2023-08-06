d65c9efbe1293e31ebe015a065880640ed0f2898
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-08-05 21:10:18 -0500

    Skeleton for problem 2806

---
+++ src/main/rust/src/bin/problem2806.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/account-balance-after-rounded-purchase/
+pub fn account_balance_after_purchase(purchase_amount: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", account_balance_after_purchase(9)); // 90
+    println!("{}", account_balance_after_purchase(15)); // 80
+}
