aa8cd2d73b2cab25bc3fb82bc77491c754a6d9b7
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-08-06 10:53:07 -0500

    Skeleton for problem 2810

---
+++ src/main/rust/src/bin/problem2810.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/faulty-keyboard/
+pub fn final_string(s: String) -> String {
+    todo!()
+}
+
+fn main() {
+    println!("{}", final_string("string".to_string())); // "rtsng"
+    println!("{}", final_string("poiinter".to_string())); // "ponter"
+}
