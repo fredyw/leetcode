94455dd12405345bc23f6dfd1ffeb7291b638f6a
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-08-06 11:07:07 -0500

    Update problem 2810

--- src/main/rust/src/bin/problem2810.rs
+++ src/main/rust/src/bin/problem2810.rs
@@ -1,6 +1,31 @@
 // https://leetcode.com/problems/faulty-keyboard/
 pub fn final_string(s: String) -> String {
-    todo!()
+    let mut answer: Vec<char> = vec![];
+    let chars: Vec<char> = s.chars().collect();
+    let mut i = 0;
+    let mut j = 0;
+    while j < chars.len() {
+        if chars[j] == 'i' {
+            let mut count = 0;
+            let mut k = j;
+            while k < chars.len() && chars[k] == 'i' {
+                count += 1;
+                k += 1;
+            }
+            let sub = &mut chars[i..j].to_vec();
+            if count % 2 != 0 {
+                sub.reverse();
+                answer.append(sub);
+            } else {
+                answer.append(sub);
+            }
+            j = k;
+            i = k;
+        } else {
+            j += 1;
+        }
+    }
+    answer.into_iter().collect()
 }
 
 fn main() {
