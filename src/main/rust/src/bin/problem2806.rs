9f5ef3e72f2179beddde9658a37f340b82fea652
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-08-05 21:37:23 -0500

    Solve problem 2806

--- src/main/rust/src/bin/problem2806.rs
+++ src/main/rust/src/bin/problem2806.rs
@@ -1,9 +1,21 @@
 // https://leetcode.com/problems/account-balance-after-rounded-purchase/
 pub fn account_balance_after_purchase(purchase_amount: i32) -> i32 {
-    todo!()
+    if purchase_amount % 10 == 0 {
+        return 100 - purchase_amount;
+    }
+    let d = purchase_amount / 10;
+    let r = purchase_amount % 10;
+    if r < 5 {
+        100 - (d * 10)
+    } else {
+        100 - ((d + 1) * 10)
+    }
 }
 
 fn main() {
     println!("{}", account_balance_after_purchase(9)); // 90
     println!("{}", account_balance_after_purchase(15)); // 80
+    println!("{}", account_balance_after_purchase(4)); // 100
+    println!("{}", account_balance_after_purchase(50)); // 50
+    println!("{}", account_balance_after_purchase(11)); // 90
 }
