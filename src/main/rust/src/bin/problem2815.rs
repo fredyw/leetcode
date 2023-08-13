4046a4313220c113f708e03517f6a80c34a12a16
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-08-12 23:58:06 -0500

    Skeleton for problem 2815

---
+++ src/main/rust/src/bin/problem2815.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/max-pair-sum-in-an-array/
+pub fn max_sum(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", max_sum(vec![51, 71, 17, 24, 42])); // 88
+    println!("{}", max_sum(vec![1, 2, 3, 4])); // -1
+}
