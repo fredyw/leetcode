c0bd3ab83c3d20dcdd47d84791dac97ff96b2eee
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-08-17 14:41:02 -0500

    Skeleton for problem 2369

---
+++ src/main/rust/src/bin/problem2369.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/check-if-there-is-a-valid-partition-for-the-array/
+pub fn valid_partition(nums: Vec<i32>) -> bool {
+    todo!()
+}
+
+fn main() {
+    println!("{}", valid_partition(vec![4, 4, 4, 5, 6])); // true
+    println!("{}", valid_partition(vec![1, 1, 1, 2])); // false
+}
