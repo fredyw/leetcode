acd06b3b9c0b87b4b9b1807f7b756d41c0b37769
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-08-19 16:51:51 -0500

    Update problem 2369

--- src/main/rust/src/bin/problem2369.rs
+++ src/main/rust/src/bin/problem2369.rs
@@ -1,6 +1,38 @@
 // https://leetcode.com/problems/check-if-there-is-a-valid-partition-for-the-array/
 pub fn valid_partition(nums: Vec<i32>) -> bool {
-    todo!()
+    #[derive(Debug, PartialEq, Eq)]
+    enum Type {
+        Initial,
+        Equal,
+        Increasing,
+    }
+
+    fn valid_partition(nums: &Vec<i32>, index: usize, count: i32, t: Type) -> bool {
+        if index == nums.len() {
+            return (t == Type::Equal && count == 2) || count == 3;
+        }
+        if count == 1 {
+            if nums[index - 1] == nums[index] {
+                return valid_partition(nums, index + 1, count + 1, Type::Equal);
+            }
+            if nums[index - 1] + 1 == nums[index] {
+                return valid_partition(nums, index + 1, count + 1, Type::Increasing);
+            }
+        }
+        if count == 2 {
+            if t == Type::Equal && nums[index - 1] == nums[index] {
+                return valid_partition(nums, index + 1, count + 1, Type::Equal)
+                    || valid_partition(nums, index + 1, 1, Type::Initial);
+            }
+            if t == Type::Increasing && nums[index - 1] + 1 == nums[index] {
+                return valid_partition(nums, index + 1, count + 1, Type::Increasing);
+            }
+        }
+        // count == 3
+        valid_partition(nums, index + 1, 1, Type::Initial)
+    }
+
+    valid_partition(&nums, 1, 1, Type::Initial)
 }
 
 fn main() {
