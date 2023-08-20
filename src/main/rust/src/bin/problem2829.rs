ad6317d8c5d206544eb986231928f4943034b5b0
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-08-20 10:11:24 -0500

    Skeleton for problem 2829

---
+++ src/main/rust/src/bin/problem2829.rs
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/determine-the-minimum-sum-of-a-k-avoiding-array/
+pub fn minimum_sum(n: i32, k: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", minimum_sum(5, 4)); // 18
+    println!("{}", minimum_sum(2, 6)); // 3
+    println!("{}", minimum_sum(3, 3)); // 8
+    println!("{}", minimum_sum(10, 6)); // 69
+}
