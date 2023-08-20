308ba48b688d2e7fb9aadd9fabcd927730e76a92
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-08-19 23:09:39 -0500

    Skeleton for problem 2824

---
+++ src/main/rust/src/bin/problem2824.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/count-pairs-whose-sum-is-less-than-target/
+pub fn count_pairs(nums: Vec<i32>, target: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", count_pairs(vec![-1, 1, 2, 3, 1], 2)); // 3
+    println!("{}", count_pairs(vec![-6, 2, 5, -2, -7, -1, 3], -2)); // 10
+}
