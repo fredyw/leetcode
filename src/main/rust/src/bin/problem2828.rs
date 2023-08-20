3fc024349c4709451eb5fc939e7405d755983054
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-08-19 23:12:59 -0500

    Skeleton for problem 2828

---
+++ src/main/rust/src/bin/problem2828.rs
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/check-if-a-string-is-an-acronym-of-words/
+pub fn is_acronym(words: Vec<String>, s: String) -> bool {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        is_acronym(
+            vec![
+                "alice".to_string(),
+                "bob".to_string(),
+                "charlie".to_string()
+            ],
+            "abc".to_string()
+        )
+    ); // true
+    println!(
+        "{}",
+        is_acronym(vec!["an".to_string(), "apple".to_string()], "a".to_string())
+    ); // false
+    println!(
+        "{}",
+        is_acronym(
+            vec![
+                "never".to_string(),
+                "gonna".to_string(),
+                "give".to_string(),
+                "up".to_string(),
+                "on".to_string(),
+                "you".to_string()
+            ],
+            "ngguoy".to_string()
+        )
+    ); // true
+}
