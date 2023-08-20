5f231a10cdf2390e1ffdc37ffdb1fecb54fd64ad
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-08-20 10:23:07 -0500

    Solve problem 2829

--- src/main/rust/src/bin/problem2829.rs
+++ src/main/rust/src/bin/problem2829.rs
@@ -1,6 +1,24 @@
+use std::collections::HashSet;
+
 // https://leetcode.com/problems/determine-the-minimum-sum-of-a-k-avoiding-array/
 pub fn minimum_sum(n: i32, k: i32) -> i32 {
-    todo!()
+    let mut answer = 0;
+    let mut count = 0;
+    let mut set: HashSet<i32> = HashSet::new();
+    let mut i = 1;
+    while count < n {
+        if set.contains(&i) {
+            i += 1;
+            continue;
+        }
+        if k - i > 0 {
+            set.insert(k - i);
+        }
+        answer += i;
+        i += 1;
+        count += 1;
+    }
+    answer
 }
 
 fn main() {
