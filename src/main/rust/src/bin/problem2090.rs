6fae8e72565864ba73b73974b3c8c9ff389a8d30
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-08-21 11:33:31 -0500

    Solve problem 2090

--- src/main/rust/src/bin/problem2090.rs
+++ src/main/rust/src/bin/problem2090.rs
@@ -1,6 +1,24 @@
 // https://leetcode.com/problems/k-radius-subarray-averages/
 pub fn get_averages(nums: Vec<i32>, k: i32) -> Vec<i32> {
-    todo!()
+    let mut answer = vec![-1; nums.len()];
+    let length = (k as usize * 2) + 1;
+    if nums.len() < length {
+        return answer;
+    }
+    let mut sum: i64 = 0;
+    for i in 0..length {
+        sum += nums[i] as i64;
+    }
+    let mut index = k as usize;
+    answer[index] = (sum / length as i64) as i32;
+    index += 1;
+    for i in length..nums.len() {
+        sum += nums[i] as i64;
+        sum -= nums[i - length] as i64;
+        answer[index] = (sum / length as i64) as i32;
+        index += 1;
+    }
+    answer
 }
 
 fn main() {
