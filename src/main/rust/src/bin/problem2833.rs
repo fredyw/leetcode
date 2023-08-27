77816c16e288d739cee0a244f42c84a6032ecd86
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-08-26 23:27:56 -0500

    Skeleton for problem 2833

---
+++ src/main/rust/src/bin/problem2833.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/furthest-point-from-origin/
+pub fn furthest_distance_from_origin(moves: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", furthest_distance_from_origin("L_RL__R".to_string())); // 3
+    println!("{}", furthest_distance_from_origin("_R__LL_".to_string())); // 5
+    println!("{}", furthest_distance_from_origin("_______".to_string())); // 7
+}
