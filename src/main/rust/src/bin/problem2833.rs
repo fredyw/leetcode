dccd8af18e16ab3dff678d691d796bbdfc09333b
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-08-26 23:53:24 -0500

    Solve problem 2833

--- src/main/rust/src/bin/problem2833.rs
+++ src/main/rust/src/bin/problem2833.rs
@@ -1,10 +1,36 @@
 // https://leetcode.com/problems/furthest-point-from-origin/
 pub fn furthest_distance_from_origin(moves: String) -> i32 {
-    todo!()
+    let mut answer: i32 = 0;
+    let chars: Vec<char> = moves.chars().collect();
+    let mut left_count = 0;
+    let mut right_count = 0;
+    for c in chars.iter() {
+        if *c == 'L' {
+            left_count += 1;
+        } else if *c == 'R' {
+            right_count += 1;
+        }
+    }
+    for c in chars.into_iter() {
+        // Always choose 'L' for '_'.
+        if c == 'L' {
+            answer -= 1;
+        } else if c == 'R' {
+            answer += 1;
+        } else {
+            if left_count < right_count {
+                answer += 1;
+            } else {
+                answer -= 1;
+            }
+        }
+    }
+    answer.abs()
 }
 
 fn main() {
     println!("{}", furthest_distance_from_origin("L_RL__R".to_string())); // 3
     println!("{}", furthest_distance_from_origin("_R__LL_".to_string())); // 5
     println!("{}", furthest_distance_from_origin("_______".to_string())); // 7
+    println!("{}", furthest_distance_from_origin("R_".to_string())); // 2
 }
