3dae006f4a38c25834f545e390acebf6794dc28f
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-08-30 00:45:58 -0500

    Skeleton for problem 1870

---
+++ src/main/rust/src/bin/problem1870.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/minimum-speed-to-arrive-on-time/
+pub fn min_speed_on_time(dist: Vec<i32>, hour: f64) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", min_speed_on_time(vec![1, 3, 2], 6.0)); // 1
+    println!("{}", min_speed_on_time(vec![1, 3, 2], 2.7)); // 3
+    println!("{}", min_speed_on_time(vec![1, 3, 2], 1.9)); // -1
+}
