56246072d4a0c6e71e2b71eaea3116b8d54a1543
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-08-29 21:22:02 -0500

    Skeleton for problem 2834

---
+++ src/main/rust/src/bin/problem2834.rs
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/find-the-minimum-possible-sum-of-a-beautiful-array/
+pub fn minimum_possible_sum(n: i32, target: i32) -> i64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", minimum_possible_sum(2, 3)); // 4
+    println!("{}", minimum_possible_sum(3, 3)); // 8
+    println!("{}", minimum_possible_sum(1, 1)); // 1
+    println!("{}", minimum_possible_sum(4, 4)); // 12
+}
