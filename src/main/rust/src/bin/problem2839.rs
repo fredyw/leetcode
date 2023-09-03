aecc3e32fbae4a418345984fa7b724a05670477c
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-09-02 19:10:46 -0500

    Skeleton for problem 2839

---
+++ src/main/rust/src/bin/problem2839.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/check-if-strings-can-be-made-equal-with-operations-i/
+pub fn can_be_equal(s1: String, s2: String) -> bool {
+    todo!()
+}
+
+fn main() {
+    println!("{}", can_be_equal("abcd".to_string(), "cdab".to_string())); // true
+    println!("{}", can_be_equal("abcd".to_string(), "dacb".to_string())); // false
+}
