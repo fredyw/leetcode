bb6578142cc74a38364ff383f7e8473e543f9751
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-09-03 12:47:58 -0500

    Skeleton for problem 2843

---
+++ src/main/rust/src/bin/problem2843.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/count-symmetric-integers/
+pub fn count_symmetric_integers(low: i32, high: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", count_symmetric_integers(1, 100)); // 9
+    println!("{}", count_symmetric_integers(1200, 1230)); // 4
+}
