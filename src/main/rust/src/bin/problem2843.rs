59f946d591388a0a3693442b8ff7e93012a396de
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-09-03 13:07:56 -0500

    Solve problem 2843

--- src/main/rust/src/bin/problem2843.rs
+++ src/main/rust/src/bin/problem2843.rs
@@ -1,6 +1,23 @@
 // https://leetcode.com/problems/count-symmetric-integers/
 pub fn count_symmetric_integers(low: i32, high: i32) -> i32 {
-    todo!()
+    let mut answer = 0;
+    for i in low..=high {
+        let chars: Vec<char> = format!("{}", i).chars().collect();
+        if chars.len() % 2 == 0 {
+            let left_sum: i32 = chars[0..chars.len() / 2]
+                .iter()
+                .map(|c| *c as i32 - '0' as i32)
+                .sum();
+            let right_sum: i32 = chars[chars.len() / 2..chars.len()]
+                .iter()
+                .map(|c| *c as i32 - '0' as i32)
+                .sum();
+            if left_sum == right_sum {
+                answer += 1;
+            }
+        }
+    }
+    answer
 }
 
 fn main() {
