3dc6bb5b06899fe0f4b10e0edde38c2b77d92635
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-09-09 23:25:58 -0500

    Skeleton for problem 2848

---
+++ src/main/rust/src/bin/problem2848.rs
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/points-that-intersect-with-cars/
+pub fn number_of_points(nums: Vec<Vec<i32>>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        number_of_points(vec![vec![3, 6], vec![1, 5], vec![4, 7]])
+    ); // 7
+    println!("{}", number_of_points(vec![vec![1, 3], vec![5, 8]])); // 7
+}
