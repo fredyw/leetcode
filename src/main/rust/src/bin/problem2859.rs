b2902327fc6d7c2aaed71e448f6c9f83ce2f4e81
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-09-16 23:58:05 -0500

    Solve problem 2859

--- src/main/rust/src/bin/problem2859.rs
+++ src/main/rust/src/bin/problem2859.rs
@@ -1,6 +1,24 @@
 // https://leetcode.com/problems/sum-of-values-at-indices-with-k-set-bits/description/
 pub fn sum_indices_with_k_set_bits(nums: Vec<i32>, k: i32) -> i32 {
-    todo!()
+    fn count_bits(n: usize) -> i32 {
+        let mut count = 0;
+        let mut i = n;
+        while i > 0 {
+            if i & 1 == 1 {
+                count += 1;
+            }
+            i >>= 1;
+        }
+        count
+    }
+
+    let mut answer = 0;
+    for (i, n) in nums.into_iter().enumerate() {
+        if count_bits(i) == k {
+            answer += n;
+        }
+    }
+    answer
 }
 
 fn main() {
