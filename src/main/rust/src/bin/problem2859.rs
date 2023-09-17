8a738fcb1770dc04d6fa13047e78325016c8ae5d
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-09-16 23:44:05 -0500

    Skeleton for problem 2859

---
+++ src/main/rust/src/bin/problem2859.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/sum-of-values-at-indices-with-k-set-bits/description/
+pub fn sum_indices_with_k_set_bits(nums: Vec<i32>, k: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", sum_indices_with_k_set_bits(vec![5, 10, 1, 5, 2], 1)); // 13
+    println!("{}", sum_indices_with_k_set_bits(vec![4, 3, 2, 1], 2)); // 1
+}
