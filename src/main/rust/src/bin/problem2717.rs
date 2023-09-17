599206f578ea1c3655f4386ab239248f64504bcd
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-09-17 00:27:43 -0500

    Solve problem 2717

--- src/main/rust/src/bin/problem2717.rs
+++ src/main/rust/src/bin/problem2717.rs
@@ -1,6 +1,33 @@
 // https://leetcode.com/problems/semi-ordered-permutation/
-pub fn semi_ordered_permutation(nums: Vec<i32>) -> i32 {
-    todo!()
+pub fn semi_ordered_permutation(mut nums: Vec<i32>) -> i32 {
+    let mut answer = 0;
+    let min = 1;
+    let max = nums.len() as i32;
+    let mut i: i32 = 0;
+    while i < nums.len() as i32 {
+        if nums[i as usize] == min {
+            break;
+        }
+        i += 1;
+    }
+    while i - 1 >= 0 {
+        nums.swap(i as usize, i as usize - 1);
+        i -= 1;
+        answer += 1;
+    }
+    let mut i = 0;
+    while i < nums.len() {
+        if nums[i] == max {
+            break;
+        }
+        i += 1;
+    }
+    while i + 1 < nums.len() {
+        nums.swap(i, i + 1);
+        i += 1;
+        answer += 1;
+    }
+    answer
 }
 
 fn main() {
