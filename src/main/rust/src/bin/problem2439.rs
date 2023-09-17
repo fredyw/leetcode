3a02f8c54862d93de99fefad3a48c2edc2c3a1cd
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-09-17 00:29:27 -0500

    Skeleton for problem 2439

---
+++ src/main/rust/src/bin/problem2439.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/minimize-maximum-of-array/description/
+pub fn minimize_array_value(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", minimize_array_value(vec![3, 7, 1, 6])); // 5
+    println!("{}", minimize_array_value(vec![10, 1])); // 10
+}
