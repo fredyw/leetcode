b526195dacf94fdcc7be4478157d0cbac166330a
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-09-24 12:16:55 -0500

    Skeleton for problem 2864

---
+++ src/main/rust/src/bin/problem2864.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/maximum-odd-binary-number/description/
+pub fn maximum_odd_binary_number(s: String) -> String {
+    todo!()
+}
+
+fn main() {
+    println!("{}", maximum_odd_binary_number("010".to_string())); // "001"
+    println!("{}", maximum_odd_binary_number("0101".to_string())); // "1001"
+}
