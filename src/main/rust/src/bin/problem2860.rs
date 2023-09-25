144f020b226bce7346dfbcdfa3a9a009c87c3cdc
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-09-24 20:37:10 -0500

    Skeleton for problem 2860

---
+++ src/main/rust/src/bin/problem2860.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/happy-students/description/
+pub fn count_ways(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", count_ways(vec![1, 1])); // 2
+    println!("{}", count_ways(vec![6, 0, 3, 3, 6, 7, 2, 7])); // 3
+}
