7a352ded4af3b5587c8a072fbc2d606a4e50b0aa
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-10-01 00:48:18 -0500

    Skeleton for problem 2869

---
+++ src/main/rust/src/bin/problem2869.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/minimum-operations-to-collect-elements/description/
+pub fn min_operations(nums: Vec<i32>, k: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", min_operations(vec![3, 1, 5, 4, 2], 2)); // 4
+    println!("{}", min_operations(vec![3, 1, 5, 4, 2], 5)); // 5
+    println!("{}", min_operations(vec![3, 2, 5, 3, 1], 3)); // 4
+}
