4265929c27fd9a0d6df18f4d64a49543514546f3
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-10-01 01:07:13 -0500

    Skeleton for problem 2873

---
+++ src/main/rust/src/bin/problem2873.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/maximum-value-of-an-ordered-triplet-i/description/
+pub fn maximum_triplet_value(nums: Vec<i32>) -> i64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", maximum_triplet_value(vec![12, 6, 1, 2, 7])); // 77
+    println!("{}", maximum_triplet_value(vec![1, 10, 3, 4, 19])); // 133
+    println!("{}", maximum_triplet_value(vec![1, 2, 3])); // 0
+}
