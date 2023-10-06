ff76d6a5f6ffaac1bff30df0530cd1cc5282d0c2
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-10-06 00:50:49 -0500

    Update problem 2860

--- src/main/rust/src/bin/problem2860.rs
+++ src/main/rust/src/bin/problem2860.rs
@@ -1,9 +1,18 @@
 // https://leetcode.com/problems/happy-students/description/
-pub fn count_ways(nums: Vec<i32>) -> i32 {
-    todo!()
+pub fn count_ways(mut nums: Vec<i32>) -> i32 {
+    nums.sort();
+    let mut answer = 0;
+    for (i, n) in nums.iter().enumerate() {
+        if *n < (i + 1) as i32 {
+            answer += 1;
+        }
+    }
+    answer
 }
 
 fn main() {
     println!("{}", count_ways(vec![1, 1])); // 2
     println!("{}", count_ways(vec![6, 0, 3, 3, 6, 7, 2, 7])); // 3
+    println!("{}", count_ways(vec![1, 1, 0, 1])); // 1
+    println!("{}", count_ways(vec![2, 3, 3, 3, 3, 3])); // 2
 }
