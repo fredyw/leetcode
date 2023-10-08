6807c226bcabcdee1506b58fd5a2942c7a6b24f8
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-10-08 00:42:50 -0500

    Solve problem 2894

--- src/main/rust/src/bin/problem2894.rs
+++ src/main/rust/src/bin/problem2894.rs
@@ -1,6 +1,15 @@
 // https://leetcode.com/problems/divisible-and-non-divisible-sums-difference/description/
 pub fn difference_of_sums(n: i32, m: i32) -> i32 {
-    todo!()
+    let mut num1 = 0;
+    let mut num2 = 0;
+    for i in 1..=n {
+        if i % m != 0 {
+            num1 += i;
+        } else {
+            num2 += i;
+        }
+    }
+    num1 - num2
 }
 
 fn main() {
