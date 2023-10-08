d88d81b8af93bb4f94edc8b9dcc26b5a32d5f567
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-10-08 00:39:49 -0500

    Skeleton for problem 2894

---
+++ src/main/rust/src/bin/problem2894.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/divisible-and-non-divisible-sums-difference/description/
+pub fn difference_of_sums(n: i32, m: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", difference_of_sums(10, 3)); // 19
+    println!("{}", difference_of_sums(5, 6)); // 15
+    println!("{}", difference_of_sums(5, 1)); // -15
+}
