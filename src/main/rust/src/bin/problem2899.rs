8212be7815c38c59b195a9246a3406c5a0be8a47
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-10-15 00:05:56 -0500

    Skeleton for problem 2899

---
+++ src/main/rust/src/bin/problem2899.rs
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/last-visited-integers/description/
+pub fn last_visited_integers(words: Vec<String>) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{:?}",
+        last_visited_integers(vec![
+            "1".to_string(),
+            "2".to_string(),
+            "prev".to_string(),
+            "prev".to_string(),
+            "prev".to_string()
+        ])
+    ); // [2,1,-1]
+    println!(
+        "{:?}",
+        last_visited_integers(vec![
+            "1".to_string(),
+            "prev".to_string(),
+            "2".to_string(),
+            "prev".to_string(),
+            "prev".to_string()
+        ])
+    ); // [1,2,1]
+}
