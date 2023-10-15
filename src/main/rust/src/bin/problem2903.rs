9641cc37e9f020d14c7f03203c24c5c50a966b39
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-10-15 00:03:38 -0500

    Solve problem 2903

--- src/main/rust/src/bin/problem2903.rs
+++ src/main/rust/src/bin/problem2903.rs
@@ -1,6 +1,15 @@
 // https://leetcode.com/problems/find-indices-with-index-and-value-difference-i/description/
 pub fn find_indices(nums: Vec<i32>, index_difference: i32, value_difference: i32) -> Vec<i32> {
-    todo!()
+    for i in 0..nums.len() {
+        for j in 0..nums.len() {
+            if (i as i32 - j as i32).abs() >= index_difference
+                && (nums[i] - nums[j]).abs() >= value_difference
+            {
+                return vec![i as i32, j as i32];
+            }
+        }
+    }
+    vec![-1, -1]
 }
 
 fn main() {
