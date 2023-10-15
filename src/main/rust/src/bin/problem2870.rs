b273f76f9a0d6eef73c067ef0a36b776b4d39c55
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-10-14 23:57:43 -0500

    Skeleton for problem 2870

---
+++ src/main/rust/src/bin/problem2870.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/minimum-number-of-operations-to-make-array-empty/description/
+pub fn min_operations(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", min_operations(vec![2, 3, 3, 2, 2, 4, 2, 3, 4])); // 4
+    println!("{}", min_operations(vec![2, 1, 2, 2, 3, 3])); // -1
+}
