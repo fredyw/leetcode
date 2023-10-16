66f21cc54aac0be6c05c6a8eb92470aca2291c84
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-10-15 23:50:54 -0500

    Solve problem 2899

--- src/main/rust/src/bin/problem2899.rs
+++ src/main/rust/src/bin/problem2899.rs
@@ -1,6 +1,22 @@
 // https://leetcode.com/problems/last-visited-integers/description/
 pub fn last_visited_integers(words: Vec<String>) -> Vec<i32> {
-    todo!()
+    let mut answer = vec![];
+    let mut nums: Vec<&str> = vec![];
+    let mut i: i32 = 0;
+    for word in words.iter() {
+        if word != "prev" {
+            nums.push(word);
+            i = nums.len() as i32 - 1;
+        } else {
+            if i < 0 || nums.is_empty() {
+                answer.push(-1);
+            } else {
+                answer.push(nums[i as usize].parse().unwrap());
+                i -= 1;
+            }
+        }
+    }
+    answer
 }
 
 fn main() {
@@ -38,4 +54,13 @@ fn main() {
             "prev".to_string()
         ])
     ); // [2,1,-1,-1,-1,3]
+    println!(
+        "{:?}",
+        last_visited_integers(vec![
+            "prev".to_string(),
+            "prev".to_string(),
+            "prev".to_string(),
+            "27".to_string()
+        ])
+    ); // [-1,-1,-1]
 }
