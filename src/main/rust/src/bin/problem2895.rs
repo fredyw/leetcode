221506587ecfbf6508703b0779bd8d0c54b4f01a
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-10-22 12:44:57 -0500

    Skeleton for problem 2895

---
+++ src/main/rust/src/bin/problem2895.rs
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/minimum-processing-time/description/
+pub fn min_processing_time(processor_time: Vec<i32>, tasks: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        min_processing_time(vec![8, 10], vec![2, 2, 3, 1, 8, 7, 4, 5])
+    ); // 16
+    println!(
+        "{}",
+        min_processing_time(vec![10, 20], vec![2, 3, 1, 2, 5, 8, 4, 3])
+    ); // 23
+}
