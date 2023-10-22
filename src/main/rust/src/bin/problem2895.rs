322d736ddd2022732f7864c6702e736ff215cdf2
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-10-22 13:04:17 -0500

    Solve problem 2895

--- src/main/rust/src/bin/problem2895.rs
+++ src/main/rust/src/bin/problem2895.rs
@@ -1,6 +1,18 @@
 // https://leetcode.com/problems/minimum-processing-time/description/
-pub fn min_processing_time(processor_time: Vec<i32>, tasks: Vec<i32>) -> i32 {
-    todo!()
+pub fn min_processing_time(mut processor_time: Vec<i32>, mut tasks: Vec<i32>) -> i32 {
+    processor_time.sort();
+    tasks.sort();
+    let mut answer = 0;
+    let mut j = 0;
+    for start in processor_time.iter().rev() {
+        let mut max = 0;
+        for _i in 0..4 {
+            max = max.max(start + tasks[j]);
+            j += 1;
+        }
+        answer = answer.max(max);
+    }
+    answer
 }
 
 fn main() {
