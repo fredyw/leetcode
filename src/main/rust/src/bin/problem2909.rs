4de539c1b0e1bd522d9425e017c1d081b15c35bd
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-10-28 01:11:19 -0500

    Skeleton for problem 2909

---
+++ src/main/rust/src/bin/problem2909.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/minimum-sum-of-mountain-triplets-ii/description/
+pub fn minimum_sum(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", minimum_sum(vec![8, 6, 1, 5, 3])); // 9
+    println!("{}", minimum_sum(vec![5, 4, 8, 7, 10, 2])); // 13
+    println!("{}", minimum_sum(vec![6, 5, 4, 3, 4, 5])); // -1
+}
