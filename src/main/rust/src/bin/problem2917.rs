193c7a20bc6642caa0ee7cd90f53b435ebc65a8e
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-10-29 13:11:55 -0500

    Solve problem 2917

--- src/main/rust/src/bin/problem2917.rs
+++ src/main/rust/src/bin/problem2917.rs
@@ -1,6 +1,18 @@
 // https://leetcode.com/problems/find-the-k-or-of-an-array/description/
 pub fn find_k_or(nums: Vec<i32>, k: i32) -> i32 {
-    todo!()
+    let mut answer = 0;
+    for i in 0..32 {
+        let mut count = 0;
+        for num in nums.iter() {
+            if *num >> i & 1 == 1 {
+                count += 1;
+            }
+        }
+        if count >= k {
+            answer += 2_i32.pow(i);
+        }
+    }
+    answer
 }
 
 fn main() {
