97fe31c2f016295fb0bbbfb452c4be344b6cf779
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-10-29 00:27:43 -0500

    Skeleton for problem 2913

---
+++ src/main/rust/src/bin/problem2913.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/subarrays-distinct-element-sum-of-squares-i/description/
+pub fn sum_counts(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", sum_counts(vec![1, 2, 1])); // 15
+    println!("{}", sum_counts(vec![1, 1])); // 3
+}
