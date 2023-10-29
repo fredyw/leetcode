4f3538ef3cf2104a1769ffdce78c6a0fef32a820
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-10-29 00:38:55 -0500

    Skeleton for problem 2917

---
+++ src/main/rust/src/bin/problem2917.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/find-the-k-or-of-an-array/description/
+pub fn find_k_or(nums: Vec<i32>, k: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", find_k_or(vec![7, 12, 9, 8, 9, 15], 4)); // 9
+    println!("{}", find_k_or(vec![2, 12, 1, 11, 4, 5], 6)); // 0
+    println!("{}", find_k_or(vec![10, 8, 5, 9, 11, 6, 8], 1)); // 15
+}
