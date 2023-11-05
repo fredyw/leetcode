3affdf074c007245268feea0aaad61040f0e3d9b
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-11-04 23:32:12 -0500

    Skeleton for problem 2923

---
+++ src/main/rust/src/bin/problem2923.rs
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/find-champion-i/description/
+pub fn find_champion(grid: Vec<Vec<i32>>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", find_champion(vec![vec![0, 1], vec![0, 0]])); // 0
+    println!(
+        "{}",
+        find_champion(vec![vec![0, 0, 1], vec![1, 0, 1], vec![0, 0, 0]])
+    ); // 1
+}
