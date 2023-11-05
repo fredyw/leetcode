03648695141ebff75233885866decf26c000ce5d
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-11-04 23:50:55 -0500

    Solve problem 2923

--- src/main/rust/src/bin/problem2923.rs
+++ src/main/rust/src/bin/problem2923.rs
@@ -1,6 +1,18 @@
 // https://leetcode.com/problems/find-champion-i/description/
 pub fn find_champion(grid: Vec<Vec<i32>>) -> i32 {
-    todo!()
+    let mut answer = 0;
+    let mut max = 0;
+    for i in 0..grid.len() {
+        let mut count = 0;
+        for j in 0..grid[i].len() {
+            count += grid[i][j];
+        }
+        if count >= max {
+            max = count;
+            answer = i as i32;
+        }
+    }
+    answer
 }
 
 fn main() {
