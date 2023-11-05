b5715c7e85ce21a2edc97697a6d7d6b40f55d4b0
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-11-05 09:10:01 -0600

    Skeleton for problem 2914

---
+++ src/main/rust/src/bin/problem2914.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/minimum-number-of-changes-to-make-binary-string-beautiful/description/
+pub fn min_changes(s: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", min_changes("1001".to_string())); // 2
+    println!("{}", min_changes("10".to_string())); // 1
+}
