dfddf3bd2e208b925ada57bb73bc6d9536d6d083
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-11-07 00:17:41 -0600

    Solve problem 2914

--- src/main/rust/src/bin/problem2914.rs
+++ src/main/rust/src/bin/problem2914.rs
@@ -1,6 +1,15 @@
 // https://leetcode.com/problems/minimum-number-of-changes-to-make-binary-string-beautiful/description/
 pub fn min_changes(s: String) -> i32 {
-    todo!()
+    let mut answer = 0;
+    let bytes = s.as_bytes();
+    let mut i = 0;
+    while i < bytes.len() {
+        if bytes[i] != bytes[i + 1] {
+            answer += 1;
+        }
+        i += 2;
+    }
+    answer
 }
 
 fn main() {
@@ -8,4 +17,5 @@ fn main() {
     println!("{}", min_changes("10".to_string())); // 1
     println!("{}", min_changes("0000".to_string())); // 0
     println!("{}", min_changes("10000010".to_string())); // 2
+    println!("{}", min_changes("11010110101010".to_string())); // 6
 }
