da898fe017bc75f202f50d92b142ee2b60142bc7
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-11-12 09:07:06 -0600

    Update problem 2928

--- src/main/rust/src/bin/problem2928.rs
+++ src/main/rust/src/bin/problem2928.rs
@@ -6,4 +6,5 @@ pub fn distribute_candies(n: i32, limit: i32) -> i32 {
 fn main() {
     println!("{}", distribute_candies(5, 2)); // 3
     println!("{}", distribute_candies(3, 3)); // 10
+    println!("{}", distribute_candies(10, 3)); // 0
 }
