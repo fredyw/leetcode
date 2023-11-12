4e71667245a0fc9cb15f66d5064778dd4f3a49c1
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-11-12 09:02:04 -0600

    Skeleton for problem 2928

---
+++ src/main/rust/src/bin/problem2928.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/distribute-candies-among-children-i/description/
+pub fn distribute_candies(n: i32, limit: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", distribute_candies(5, 2)); // 3
+    println!("{}", distribute_candies(3, 3)); // 10
+}
