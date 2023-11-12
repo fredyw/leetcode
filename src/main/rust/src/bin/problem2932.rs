50b63b701d6df6934a53e78c4276b41369464a97
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-11-12 13:04:02 -0600

    Skeleton for problem 2932

---
+++ src/main/rust/src/bin/problem2932.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/maximum-strong-pair-xor-i/description/
+pub fn maximum_strong_pair_xor(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", maximum_strong_pair_xor(vec![1, 2, 3, 4, 5])); // 7
+    println!("{}", maximum_strong_pair_xor(vec![10, 100])); // 0
+    println!("{}", maximum_strong_pair_xor(vec![5, 6, 25, 30])); // 7
+}
