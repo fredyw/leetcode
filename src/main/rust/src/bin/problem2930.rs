d42bb46ad1d36e766d67198e8ea06806b7261319
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-11-20 10:29:53 -0600

    Skeleton for problem 2930

---
+++ src/main/rust/src/bin/problem2930.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/number-of-strings-which-can-be-rearranged-to-contain-substring/description/
+pub fn string_count(n: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", string_count(4)); // 12
+    println!("{}", string_count(10)); // 83943898
+}
