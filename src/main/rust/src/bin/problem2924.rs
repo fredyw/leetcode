c4ff56d1323702f415559c7aaab188b2d8028383
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-11-20 10:28:07 -0600

    Solve problem 2924

--- src/main/rust/src/bin/problem2924.rs
+++ src/main/rust/src/bin/problem2924.rs
@@ -1,6 +1,23 @@
 // https://leetcode.com/problems/find-champion-ii/description/
 pub fn find_champion(n: i32, edges: Vec<Vec<i32>>) -> i32 {
-    todo!()
+    let mut v = vec![false; n as usize];
+    for edge in edges.iter() {
+        let from = edge[0];
+        let to = edge[1];
+        v[to as usize] = true;
+    }
+    let mut answer = 0;
+    let mut count = 0;
+    for (i, &b) in v.iter().enumerate() {
+        if !b {
+            answer = i as i32;
+            count += 1;
+        }
+        if count > 1 {
+            return -1;
+        }
+    }
+    answer
 }
 
 fn main() {
@@ -13,4 +30,5 @@ fn main() {
         "{}",
         find_champion(3, vec![vec![0, 1], vec![1, 2], vec![0, 2]])
     ); // 0
+    println!("{}", find_champion(3, vec![vec![1, 0], vec![2, 1]])); // 2
 }
