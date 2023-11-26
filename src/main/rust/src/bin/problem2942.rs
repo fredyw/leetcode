5bc3b2a7f8dc0d77b59c303bb7a5c99e40ecef6d
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-11-25 19:13:15 -0600

    Skeleton for problem 2942

---
+++ src/main/rust/src/bin/problem2942.rs
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/find-words-containing-character/description/
+pub fn find_words_containing(words: Vec<String>, x: char) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{:?}",
+        find_words_containing(vec!["leet".to_string(), "code".to_string()], 'e')
+    ); // [0,1]
+    println!(
+        "{:?}",
+        find_words_containing(
+            vec![
+                "abc".to_string(),
+                "bcd".to_string(),
+                "aaaa".to_string(),
+                "cbc".to_string()
+            ],
+            'a'
+        )
+    ); // [0,2]
+    println!(
+        "{:?}",
+        find_words_containing(
+            vec![
+                "abc".to_string(),
+                "bcd".to_string(),
+                "aaaa".to_string(),
+                "cbc".to_string()
+            ],
+            'z'
+        )
+    ); // []
+}
