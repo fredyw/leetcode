1a1816281fdbdbb0ec5da41733e50591b6242696
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-12-03 00:17:07 -0600

    Solve problem 2951

--- src/main/rust/src/bin/problem2951.rs
+++ src/main/rust/src/bin/problem2951.rs
@@ -1,6 +1,12 @@
 // https://leetcode.com/problems/find-the-peaks/description/
 pub fn find_peaks(mountain: Vec<i32>) -> Vec<i32> {
-    todo!()
+    let mut answer = vec![];
+    for i in 1..mountain.len() - 1 {
+        if mountain[i - 1] < mountain[i] && mountain[i] > mountain[i + 1] {
+            answer.push(i as i32);
+        }
+    }
+    answer
 }
 
 fn main() {
