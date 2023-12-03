5895e186df2cdae519a84ca314de3604f374e1c8
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-12-03 00:14:07 -0600

    Skeleton for problem 2951

---
+++ src/main/rust/src/bin/problem2951.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/find-the-peaks/description/
+pub fn find_peaks(mountain: Vec<i32>) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", find_peaks(vec![2, 4, 4])); // []
+    println!("{:?}", find_peaks(vec![1, 4, 3, 8, 5])); // [1,3]
+}
