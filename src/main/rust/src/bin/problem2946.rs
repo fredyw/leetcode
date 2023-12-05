96046b7f18641040608278b8855991262bb7f454
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-12-04 19:33:02 -0600

    Update problem 2946

--- src/main/rust/src/bin/problem2946.rs
+++ src/main/rust/src/bin/problem2946.rs
@@ -1,6 +1,25 @@
 // https://leetcode.com/problems/matrix-similarity-after-cyclic-shifts/description/
 pub fn are_similar(mat: Vec<Vec<i32>>, k: i32) -> bool {
-    todo!()
+    let m = mat.len();
+    let n = if m > 0 { mat[0].len() } else { 0 };
+    let k = k % n as i32;
+    let mut shifted_mat = mat.clone();
+    for row in 0..m {
+        for col in 0..n {
+            if row % 2 == 0 {
+                // left
+                // println!("new_col: {}", n + ((col as i32 - 1) % k as usize));
+                shifted_mat[row][col] = mat[row][col + (col as i32 - k) as usize % n];
+            } else {
+                // right
+                shifted_mat[row][col] = mat[row][(col + k as usize) % n];
+            }
+        }
+    }
+    for v in shifted_mat.iter() {
+        println!("{:?}", v);
+    }
+    mat == shifted_mat
 }
 
 fn main() {
@@ -11,6 +30,6 @@ fn main() {
             2
         )
     ); // true
-    println!("{}", are_similar(vec![vec![2, 2], vec![2, 2]], 3)); // true
-    println!("{}", are_similar(vec![vec![1, 2]], 1)); // false
+       // println!("{}", are_similar(vec![vec![2, 2], vec![2, 2]], 3)); // true
+       // println!("{}", are_similar(vec![vec![1, 2]], 1)); // false
 }
