be6063f48b6e30869dcb38af3c17c96b2a53f28c
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-12-10 23:53:42 -0600

    Skeleton for problem 2947

---
+++ src/main/rust/src/bin/problem2947.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/count-beautiful-substrings-i/description/
+pub fn beautiful_substrings(s: String, k: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", beautiful_substrings("baeyh".to_string(), 2)); // 2
+    println!("{}", beautiful_substrings("abba".to_string(), 1)); // 3
+    println!("{}", beautiful_substrings("bcdf".to_string(), 1)); // 0
+}
