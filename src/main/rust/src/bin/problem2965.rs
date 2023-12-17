521fede0e459ccd0c0371499f1629bbd79cd4019
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-12-17 12:34:46 -0600

    Solve problem 2965

--- src/main/rust/src/bin/problem2965.rs
+++ src/main/rust/src/bin/problem2965.rs
@@ -1,6 +1,20 @@
 // https://leetcode.com/problems/find-missing-and-repeated-values/description/
 pub fn find_missing_and_repeated_values(grid: Vec<Vec<i32>>) -> Vec<i32> {
-    todo!()
+    let mut answer = vec![0; 2];
+    let mut nums = vec![0; (grid.len() * grid.len()) + 1];
+    for i in 0..grid.len() {
+        for j in 0..grid[i].len() {
+            nums[grid[i][j] as usize] += 1;
+        }
+    }
+    for i in 1..nums.len() {
+        if nums[i] > 1 {
+            answer[0] = i as i32;
+        } else if nums[i] == 0 {
+            answer[1] = i as i32;
+        }
+    }
+    answer
 }
 
 fn main() {
