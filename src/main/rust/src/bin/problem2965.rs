3a185bf5fcda1f03e4930acea57b44170fa9c175
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-12-17 12:24:52 -0600

    Skeleton for problem 2965

---
+++ src/main/rust/src/bin/problem2965.rs
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/find-missing-and-repeated-values/description/
+pub fn find_missing_and_repeated_values(grid: Vec<Vec<i32>>) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{:?}",
+        find_missing_and_repeated_values(vec![vec![1, 3], vec![2, 2]])
+    ); // [2,4]
+    println!(
+        "{:?}",
+        find_missing_and_repeated_values(vec![vec![9, 1, 7], vec![8, 9, 2], vec![3, 4, 6]])
+    ); // [9,5]
+}
