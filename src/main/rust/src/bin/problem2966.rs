5640cc544170ed33e54c76cb319e798a5851641e
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-12-19 10:18:30 -0600

    Update problem 2966

--- src/main/rust/src/bin/problem2966.rs
+++ src/main/rust/src/bin/problem2966.rs
@@ -8,4 +8,5 @@ fn main() {
     println!("{:?}", divide_array(vec![1, 3, 3, 2, 7, 3], 3)); // []
     println!("{:?}", divide_array(vec![1, 2, 3], 1)); // []
     println!("{:?}", divide_array(vec![1, 2, 2], 1)); // [[1],[2],[2]]]
+    println!("{:?}", divide_array(vec![1, 2, 2, 3, 3, 4], 1)); // [[1,2,2],[3,3,4]]
 }
