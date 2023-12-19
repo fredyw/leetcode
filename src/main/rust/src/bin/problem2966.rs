780ede43a8183768e8913c13538b3424159f2eeb
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-12-19 09:57:04 -0600

    Skeleton for problem 2966

---
+++ src/main/rust/src/bin/problem2966.rs
@@ -0,0 +1,9 @@
+//https://leetcode.com/problems/divide-array-into-arrays-with-max-difference/description/
+pub fn divide_array(nums: Vec<i32>, k: i32) -> Vec<Vec<i32>> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", divide_array(vec![1, 3, 4, 8, 7, 9, 3, 5, 1], 2)); // [[1,1,3],[3,4,5],[7,8,9]]
+    println!("{:?}", divide_array(vec![1, 3, 3, 2, 7, 3], 3)); // []
+}
