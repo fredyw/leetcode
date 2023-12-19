7324d8cfb42b3970de527f609dea4f688e343269
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-12-19 00:09:13 -0600

    Update problem 2947

--- src/main/rust/src/bin/problem2947.rs
+++ src/main/rust/src/bin/problem2947.rs
@@ -8,4 +8,5 @@ fn main() {
     println!("{}", beautiful_substrings("abba".to_string(), 1)); // 3
     println!("{}", beautiful_substrings("bcdf".to_string(), 1)); // 0
     println!("{}", beautiful_substrings("abbaa".to_string(), 1)); // 4
+    println!("{}", beautiful_substrings("aaabb".to_string(), 1)); // 2
 }
