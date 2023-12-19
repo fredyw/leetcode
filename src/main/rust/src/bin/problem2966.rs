3a18d1ff67c5b817b2666ff11321886a80435bbf
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-12-19 10:12:00 -0600

    Update problem 2966

--- src/main/rust/src/bin/problem2966.rs
+++ src/main/rust/src/bin/problem2966.rs
@@ -6,4 +6,6 @@ pub fn divide_array(nums: Vec<i32>, k: i32) -> Vec<Vec<i32>> {
 fn main() {
     println!("{:?}", divide_array(vec![1, 3, 4, 8, 7, 9, 3, 5, 1], 2)); // [[1,1,3],[3,4,5],[7,8,9]]
     println!("{:?}", divide_array(vec![1, 3, 3, 2, 7, 3], 3)); // []
+    println!("{:?}", divide_array(vec![1, 2, 3], 1)); // []
+    println!("{:?}", divide_array(vec![1, 2, 2], 1)); // [[1],[2],[2]]]
 }
