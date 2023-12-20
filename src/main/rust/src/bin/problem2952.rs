55ef1d6a0701a903bbd00315d928b3624cea5a41
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-12-20 13:44:09 -0600

    Skeleton for problem 2952

---
+++ src/main/rust/src/bin/problem2952.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/minimum-number-of-coins-to-be-added/description/
+pub fn minimum_added_coins(coins: Vec<i32>, target: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", minimum_added_coins(vec![1, 4, 10], 19)); // 2
+    println!("{}", minimum_added_coins(vec![1, 4, 10, 5, 7, 19], 19)); // 1
+    println!("{}", minimum_added_coins(vec![1, 1, 1], 20)); // 3
+}
