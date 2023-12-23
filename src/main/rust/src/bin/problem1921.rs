5b59b657d269323533044dfe944360be296c569d
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-12-23 00:39:58 -0600

    Skeleton for problem 1921

---
+++ src/main/rust/src/bin/problem1921.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/eliminate-maximum-number-of-monsters/description/
+pub fn eliminate_maximum(dist: Vec<i32>, speed: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", eliminate_maximum(vec![1, 3, 4], vec![1, 1, 1])); // 3
+    println!("{}", eliminate_maximum(vec![1, 1, 2, 3], vec![1, 1, 1, 1])); // 1
+    println!("{}", eliminate_maximum(vec![3, 2, 4], vec![5, 3, 2])); // 1
+}
