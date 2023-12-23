af718f9130b07167ba12c8df6cd88b79a0c16b82
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-12-23 12:56:05 -0600

    Skeleton for problem 10031

---
+++ src/main/rust/src/bin/problem10031.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/count-the-number-of-incremovable-subarrays-i/description/
+pub fn incremovable_subarray_count(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", incremovable_subarray_count(vec![1, 2, 3, 4])); // 10
+    println!("{}", incremovable_subarray_count(vec![6, 5, 7, 8])); // 7
+    println!("{}", incremovable_subarray_count(vec![8, 7, 6, 6])); // 3
+}
