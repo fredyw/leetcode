3eb02706a9c4bee7fa350dc02e5c6cf833867e65
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-12-22 23:49:43 -0600

    Skeleton for problem 2938

---
+++ src/main/rust/src/bin/problem2938.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/separate-black-and-white-balls/description/
+pub fn minimum_steps(s: String) -> i64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", minimum_steps("101".to_string())); // 1
+    println!("{}", minimum_steps("100".to_string())); // 2
+    println!("{}", minimum_steps("0111".to_string())); // 0
+}
