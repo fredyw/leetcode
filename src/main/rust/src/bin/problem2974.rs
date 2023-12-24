f09835f000991f2d815c3500fe72fa883b5b60fb
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-12-23 23:44:36 -0600

    Skeleton for problem 2974

---
+++ src/main/rust/src/bin/problem2974.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/minimum-number-game/description/
+pub fn number_game(nums: Vec<i32>) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", number_game(vec![5, 4, 2, 3])); // [3,2,5,4]
+    println!("{:?}", number_game(vec![2, 5])); // [5,2]
+}
