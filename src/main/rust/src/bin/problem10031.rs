79fea534176864bfd7329aa440ae1f4161241445
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-12-24 02:00:07 -0600

    Solve problem 10031

--- src/main/rust/src/bin/problem10031.rs
+++ src/main/rust/src/bin/problem10031.rs
@@ -1,6 +1,28 @@
 // https://leetcode.com/problems/count-the-number-of-incremovable-subarrays-i/description/
 pub fn incremovable_subarray_count(nums: Vec<i32>) -> i32 {
-    todo!()
+    fn check(nums: &Vec<i32>, i: usize, j: usize) -> bool {
+        let mut prev = 0;
+        for x in 0..nums.len() {
+            if x >= i && x <= j {
+                continue;
+            }
+            if prev > 0 && prev >= nums[x] {
+                return false;
+            }
+            prev = nums[x];
+        }
+        true
+    }
+
+    let mut answer = 0;
+    for i in 0..nums.len() {
+        for j in i..nums.len() {
+            if check(&nums, i, j) {
+                answer += 1;
+            }
+        }
+    }
+    answer
 }
 
 fn main() {
@@ -11,4 +33,9 @@ fn main() {
         "{}",
         incremovable_subarray_count(vec![1, 2, 3, 1, 4, 5, 2, 6, 7, 8, 1, 2, 9])
     ); // 11
+    println!(
+        "{}",
+        incremovable_subarray_count(vec![1, 2, 3, 1, 4, 5, 2, 6, 7, 8, 1, 2, 9, 10])
+    ); // 15
+    println!("{}", incremovable_subarray_count(vec![1, 2, 3, 4, 5])); // 15
 }
