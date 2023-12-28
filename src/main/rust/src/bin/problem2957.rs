12d7e6addf12a89ed5049ef8c3807a2294b3cb5d
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-12-28 12:43:17 -0600

    Skeleton for problem 2957

---
+++ src/main/rust/src/bin/problem2957.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/remove-adjacent-almost-equal-characters/description/
+pub fn remove_almost_equal_characters(word: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", remove_almost_equal_characters("aaaaa".to_string())); // 2
+    println!("{}", remove_almost_equal_characters("zyxyxyz".to_string())); // 3
+}
