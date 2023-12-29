515ce54fe7f4f390943a33c15dbaae9f10de838e
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-12-29 11:08:42 -0600

    Solve problem 2957

--- src/main/rust/src/bin/problem2957.rs
+++ src/main/rust/src/bin/problem2957.rs
@@ -1,6 +1,25 @@
 // https://leetcode.com/problems/remove-adjacent-almost-equal-characters/description/
 pub fn remove_almost_equal_characters(word: String) -> i32 {
-    todo!()
+    fn diff(a: u8, b: u8) -> u8 {
+        if a > b {
+            a - b
+        } else {
+            b - a
+        }
+    }
+
+    let mut answer = 0;
+    let mut i = 1;
+    let bytes = word.as_bytes();
+    while i < bytes.len() {
+        if diff(bytes[i], bytes[i - 1]) <= 1 {
+            i += 2;
+            answer += 1;
+        } else {
+            i += 1;
+        }
+    }
+    answer
 }
 
 fn main() {
