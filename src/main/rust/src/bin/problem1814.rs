1928e184dc2fddf6e40b9dbab9bc239325f45431
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-12-29 11:17:16 -0600

    Skeleton for problem 1814

---
+++ src/main/rust/src/bin/problem1814.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/count-nice-pairs-in-an-array/description/
+pub fn count_nice_pairs(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", count_nice_pairs(vec![42, 11, 1, 97])); // 2
+    println!("{:?}", count_nice_pairs(vec![13, 10, 35, 24, 76])); // 4
+}
