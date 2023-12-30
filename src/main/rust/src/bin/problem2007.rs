f2179932e64d05d81bd59784214a08f51db8918e
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-12-30 13:57:35 -0600

    Skeleton for problem 2007

---
+++ src/main/rust/src/bin/problem2007.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/find-original-array-from-doubled-array/description/
+pub fn find_original_array(changed: Vec<i32>) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", find_original_array(vec![1, 3, 4, 2, 6, 8])); // [1,3,4]
+    println!("{:?}", find_original_array(vec![6, 3, 0, 1])); // []
+    println!("{:?}", find_original_array(vec![1])); // []
+}
