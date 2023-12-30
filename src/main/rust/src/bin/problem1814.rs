1054a97508e96699a1a8039d11044d263d2d5f4c
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-12-30 13:44:53 -0600

    Update problem 1814

--- src/main/rust/src/bin/problem1814.rs
+++ src/main/rust/src/bin/problem1814.rs
@@ -1,6 +1,28 @@
 // https://leetcode.com/problems/count-nice-pairs-in-an-array/description/
 pub fn count_nice_pairs(nums: Vec<i32>) -> i32 {
-    todo!()
+    fn reverse(mut n: i32) -> i32 {
+        let mut reversed = 0;
+        while n > 0 {
+            let m = n % 10;
+            reversed = reversed * 10 + m;
+            n /= 10;
+        }
+        reversed
+    }
+
+    let mut reversed = vec![];
+    for &num in nums.iter() {
+        reversed.push(reverse(num));
+    }
+    let mut answer: i64 = 0;
+    for i in 0..nums.len() {
+        for j in i + 1..nums.len() {
+            if nums[i] + reversed[j] == reversed[i] + nums[j] {
+                answer = (answer + 1) % 1_000_000_007;
+            }
+        }
+    }
+    answer as i32
 }
 
 fn main() {
