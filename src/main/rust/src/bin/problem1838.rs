402d5cab6b69bf3818adca513ef988a071482f29
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-12-31 00:42:44 -0600

    Skeleton for problem 1838

---
+++ src/main/rust/src/bin/problem1838.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/frequency-of-the-most-frequent-element/description/
+pub fn max_frequency(nums: Vec<i32>, k: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", max_frequency(vec![1, 2, 4], 5)); // 3
+    println!("{}", max_frequency(vec![1, 4, 8, 13], 5)); // 2
+    println!("{}", max_frequency(vec![3, 9, 6], 2)); // 1
+}
