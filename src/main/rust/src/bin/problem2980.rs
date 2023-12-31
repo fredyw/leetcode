b7c10eb5c6cd4dd2f8c1dba36ed9442c57d05ef0
Fredy Wijaya <fredy.wijaya@gmail.com>
2023-12-31 14:33:34 -0600

    Skeleton for problem 2980

---
+++ src/main/rust/src/bin/problem2980.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/check-if-bitwise-or-has-trailing-zeros/description/
+pub fn has_trailing_zeros(nums: Vec<i32>) -> bool {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", has_trailing_zeros(vec![1, 2, 3, 4, 5])); // true
+    println!("{:?}", has_trailing_zeros(vec![2, 4, 8, 16])); // true
+    println!("{:?}", has_trailing_zeros(vec![1, 3, 5, 7, 9])); // false
+}
