9104cbf3a020de8ad92f219a8d9921907023e04a
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-01-01 11:31:57 -0600

    Update problem 1838

--- src/main/rust/src/bin/problem1838.rs
+++ src/main/rust/src/bin/problem1838.rs
@@ -1,6 +1,21 @@
 // https://leetcode.com/problems/frequency-of-the-most-frequent-element/description/
-pub fn max_frequency(nums: Vec<i32>, k: i32) -> i32 {
-    todo!()
+pub fn max_frequency(mut nums: Vec<i32>, k: i32) -> i32 {
+    nums.sort();
+    let mut answer = 0;
+    for i in (0..nums.len()).rev() {
+        let mut count = 1;
+        let mut j = i as isize - 1;
+        let mut n = k;
+        while j >= 0 {
+            n -= nums[i] - nums[j as usize];
+            if n >= 0 {
+                count += 1;
+            }
+            j -= 1;
+        }
+        answer = answer.max(count);
+    }
+    answer
 }
 
 fn main() {
@@ -8,4 +23,5 @@ fn main() {
     println!("{}", max_frequency(vec![1, 4, 8, 13], 5)); // 2
     println!("{}", max_frequency(vec![3, 9, 6], 2)); // 1
     println!("{}", max_frequency(vec![1, 4, 6, 10, 12, 17], 10)); // 3
+    println!("{}", max_frequency(vec![1, 2, 3, 9, 10], 3)); // 3
 }
