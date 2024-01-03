f12888289b05d26ffc063b0b4b2ea9e7ff859c07
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-01-02 18:48:54 -0600

    Update problem 1838

--- src/main/rust/src/bin/problem1838.rs
+++ src/main/rust/src/bin/problem1838.rs
@@ -4,7 +4,6 @@ pub fn max_frequency(mut nums: Vec<i32>, k: i32) -> i32 {
     let mut answer = 1;
     let mut right = nums.len() as isize - 1;
     let mut left = right - 1;
-    let mut count = 1;
     let mut n = k;
     while left >= 0 {
         let diff = nums[right as usize] - nums[left as usize];
@@ -13,13 +12,11 @@ pub fn max_frequency(mut nums: Vec<i32>, k: i32) -> i32 {
                 n += nums[right as usize] - nums[right as usize - 1];
             }
             right -= 1;
-            count -= 1;
         } else {
-            count += 1;
             left -= 1;
             n -= diff;
         }
-        answer = answer.max(count);
+        answer = answer.max(right as i32 - left as i32);
     }
     answer
 }
@@ -30,4 +27,8 @@ fn main() {
     println!("{}", max_frequency(vec![3, 9, 6], 2)); // 1
     println!("{}", max_frequency(vec![1, 4, 6, 10, 12, 17], 10)); // 3
     println!("{}", max_frequency(vec![1, 2, 3, 9, 10], 3)); // 3
+    println!(
+        "{}",
+        max_frequency(vec![1, 39, 399, 320, 239, 9887, 8743, 7123], 1000)
+    ); // 5
 }
