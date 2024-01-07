934145d7eebbac19cda90a65295e637752e7f36c
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-01-07 15:39:52 -0600

    Update problem 10031

--- src/main/rust/src/bin/problem10031.rs
+++ src/main/rust/src/bin/problem10031.rs
@@ -8,5 +8,5 @@ fn main() {
     println!("{}", missing_integer(vec![3, 4, 5, 1, 12, 14, 13])); // 15
     println!("{}", missing_integer(vec![1, 2, 3, 9, 10, 11])); // 6
     println!("{}", missing_integer(vec![1, 2, 3, 6, 9, 10, 11])); // 7
-    println!("{}", missing_integer(vec![1, 2, 3, 6, 9, 10, 11])); // 7
+    println!("{}", missing_integer(vec![1, 9, 10])); // 2
 }
