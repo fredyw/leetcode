47bc417ecfdf1bc40c6af7ec184df58e49426b45
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-01-07 14:56:38 -0600

    Skeleton for problem 10031

---
+++ src/main/rust/src/bin/problem10031.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/smallest-missing-integer-greater-than-sequential-prefix-sum/description/
+pub fn missing_integer(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", missing_integer(vec![1, 2, 3, 2, 5])); // 6
+    println!("{}", missing_integer(vec![3, 4, 5, 1, 12, 14, 13])); // 15
+}
