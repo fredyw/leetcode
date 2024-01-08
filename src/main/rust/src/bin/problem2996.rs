c44ca796e53c563d344bb5011fa9c7d101946e3b
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-01-08 13:03:35 -0600

    Solve problem 2996

--- src/main/rust/src/bin/problem2996.rs
+++ src/main/rust/src/bin/problem2996.rs
@@ -1,6 +1,28 @@
+use std::collections::HashSet;
+
 // https://leetcode.com/problems/smallest-missing-integer-greater-than-sequential-prefix-sum/description/
 pub fn missing_integer(nums: Vec<i32>) -> i32 {
-    todo!()
+    fn next_greater(mut n: i32, set: &HashSet<i32>) -> i32 {
+        while set.contains(&n) {
+            n += 1;
+        }
+        n
+    }
+
+    let mut set: HashSet<i32> = HashSet::new();
+    for &num in nums.iter() {
+        set.insert(num);
+    }
+    let mut sum = 0;
+    for i in 0..nums.len() {
+        if i + 1 < nums.len() && nums[i] + 1 == nums[i + 1] {
+            sum += nums[i];
+        } else {
+            sum += nums[i];
+            break;
+        }
+    }
+    next_greater(sum, &set)
 }
 
 fn main() {
@@ -9,4 +31,7 @@ fn main() {
     println!("{}", missing_integer(vec![1, 2, 3, 9, 10, 11])); // 6
     println!("{}", missing_integer(vec![1, 2, 3, 6, 9, 10, 11])); // 7
     println!("{}", missing_integer(vec![1, 9, 10])); // 2
+    println!("{}", missing_integer(vec![5, 10, 1])); // 6
+    println!("{}", missing_integer(vec![1, 5, 10])); // 2
+    println!("{}", missing_integer(vec![1, 2, 3])); // 6
 }
