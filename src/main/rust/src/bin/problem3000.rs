1790bff4de8a48abbdc017882590e77c4232a88d
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-01-08 13:06:58 -0600

    Skeleton for problem 3000

---
+++ src/main/rust/src/bin/problem3000.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/maximum-area-of-longest-diagonal-rectangle/description/
+pub fn area_of_max_diagonal(dimensions: Vec<Vec<i32>>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", area_of_max_diagonal(vec![vec![9, 3], vec![8, 6]])); // 48
+    println!("{}", area_of_max_diagonal(vec![vec![3, 4], vec![4, 3]])); // 12
+}
