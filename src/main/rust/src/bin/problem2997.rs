3879ce3a3a80634a547b900e58ef77a0a1da9ce6
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-01-08 21:00:45 -0600

    Skeleton for problem 2997

---
+++ src/main/rust/src/bin/problem2997.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/minimum-number-of-operations-to-make-array-xor-equal-to-k/description/
+pub fn min_operations(nums: Vec<i32>, k: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", min_operations(vec![2, 1, 3, 4], 1)); // 2
+    println!("{}", min_operations(vec![2, 0, 2, 0], 0)); // 0
+}
