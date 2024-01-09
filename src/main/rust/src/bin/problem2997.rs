f59bdf9f1e8da08eca7b0255b9f39896745662a0
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-01-08 21:14:56 -0600

    Update problem 2997

--- src/main/rust/src/bin/problem2997.rs
+++ src/main/rust/src/bin/problem2997.rs
@@ -6,4 +6,6 @@ pub fn min_operations(nums: Vec<i32>, k: i32) -> i32 {
 fn main() {
     println!("{}", min_operations(vec![2, 1, 3, 4], 1)); // 2
     println!("{}", min_operations(vec![2, 0, 2, 0], 0)); // 0
+    println!("{}", min_operations(vec![2, 1, 3, 4], 1)); // 1
+    println!("{}", min_operations(vec![2, 1, 3, 4], 7)); // 2
 }
