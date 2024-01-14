b4534a456d5645bdc2d41a2f53ed3a87861e3224
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-01-14 10:44:30 -0600

    Solve problem 3005

--- src/main/rust/src/bin/problem3005.rs
+++ src/main/rust/src/bin/problem3005.rs
@@ -1,6 +1,22 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/count-elements-with-maximum-frequency/
 pub fn max_frequency_elements(nums: Vec<i32>) -> i32 {
-    todo!()
+    let mut map: HashMap<i32, i32> = HashMap::new();
+    for num in nums.into_iter() {
+        *map.entry(num).or_insert(0) += 1;
+    }
+    let mut answer = 0;
+    let mut max = 0;
+    for &count in map.values() {
+        if count > max {
+            max = count;
+            answer = count;
+        } else if count == max {
+            answer += count;
+        }
+    }
+    answer
 }
 
 fn main() {
