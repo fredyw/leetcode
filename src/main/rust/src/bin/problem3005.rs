ebc35c8128a4e7408937285f21335f71737eb63c
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-01-14 10:34:19 -0600

    Skeleton for problem 3005

---
+++ src/main/rust/src/bin/problem3005.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/count-elements-with-maximum-frequency/
+pub fn max_frequency_elements(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", max_frequency_elements(vec![1, 2, 2, 3, 1, 4])); // 4
+    println!("{}", max_frequency_elements(vec![1, 2, 3, 4, 5])); // 5
+}
