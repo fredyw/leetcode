63391ed919ba2a6eb0c1562439b2f744ae6f5597
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-01-20 15:54:40 -0600

    Solve problem 2583

--- src/main/rust/src/bin/problem2583.rs
+++ src/main/rust/src/bin/problem2583.rs
@@ -1,5 +1,6 @@
 // https://leetcode.com/problems/kth-largest-sum-in-a-binary-tree/description/
 use std::cell::RefCell;
+use std::collections::VecDeque;
 use std::rc::Rc;
 
 #[derive(Debug, PartialEq, Eq)]
@@ -21,7 +22,32 @@ impl TreeNode {
 }
 
 pub fn kth_largest_level_sum(root: Option<Rc<RefCell<TreeNode>>>, k: i32) -> i64 {
-    todo!()
+    let mut sums = vec![];
+    let mut deque: VecDeque<Rc<RefCell<TreeNode>>> = VecDeque::new();
+    deque.push_back(root.unwrap());
+    while !deque.is_empty() {
+        let size = deque.len();
+        let mut sum: i64 = 0;
+        for _ in 0..size {
+            let node = deque.pop_front();
+            if let Some(node) = node {
+                sum += node.as_ref().borrow().val as i64;
+                if let Some(left) = node.as_ref().borrow().left.clone() {
+                    deque.push_back(left);
+                }
+                if let Some(right) = node.as_ref().borrow().right.clone() {
+                    deque.push_back(right);
+                }
+            }
+        }
+        sums.push(sum);
+    }
+    sums.sort_by(|a, b| b.cmp(a));
+    if sums.len() < k as usize {
+        -1
+    } else {
+        sums[k as usize - 1]
+    }
 }
 
 fn main() {
@@ -47,14 +73,13 @@ fn main() {
     let right = add_right(&root, 9);
     add_left(&right, 3);
     add_right(&right, 7);
-    let left = add_left(&left, 2);
     add_right(&left, 1);
+    let left = add_left(&left, 2);
     add_left(&left, 4);
     add_right(&left, 6);
-
     println!("{}", kth_largest_level_sum(root, 2)); // 13
 
-    let root = Some(Rc::new(RefCell::new(TreeNode::new(5))));
+    let root = Some(Rc::new(RefCell::new(TreeNode::new(1))));
     let left = add_left(&root, 2);
     add_left(&left, 3);
     println!("{}", kth_largest_level_sum(root, 1)); // 3
