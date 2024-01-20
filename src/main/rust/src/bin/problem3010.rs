5eebc1891b54967c41ece7f94ad537f1b08df35e
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-01-20 15:56:26 -0600

    Skeleton for problem 3010

---
+++ src/main/rust/src/bin/problem3010.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/divide-an-array-into-subarrays-with-minimum-cost-i/description/
+pub fn minimum_cost(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", minimum_cost(vec![1, 2, 3, 12])); // 6
+    println!("{}", minimum_cost(vec![5, 4, 3])); // 12
+    println!("{}", minimum_cost(vec![10, 3, 1, 1])); // 12
+}
