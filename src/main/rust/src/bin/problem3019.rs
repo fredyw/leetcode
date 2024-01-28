72433fb136540ea7a09340e744adc7a5b0f2d02f
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-01-28 01:28:19 -0600

    Skeleton for problem 3019

---
+++ src/main/rust/src/bin/problem3019.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/number-of-changing-keys/description/
+pub fn count_key_changes(s: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", count_key_changes("aAbBcC".to_string())); // 2
+    println!("{}", count_key_changes("AaAaAaaA".to_string())); // 0
+    println!("{}", count_key_changes("mDVD".to_string())); // 3
+}
