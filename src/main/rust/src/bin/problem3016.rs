6984b065d4fddc65e1a52fa833dab9241057ee42
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-01-28 11:14:49 -0600

    Skeleton for problem 3016

---
+++ src/main/rust/src/bin/problem3016.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/minimum-number-of-pushes-to-type-word-ii/description/
+pub fn minimum_pushes(word: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", minimum_pushes("abcde".to_string())); // 5
+    println!("{}", minimum_pushes("xyzxyzxyzxyz".to_string())); // 12
+    println!("{}", minimum_pushes("aabbccddeeffgghhiiiiii".to_string())); // 24
+}
