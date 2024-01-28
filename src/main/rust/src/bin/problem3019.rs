d7c22f73261669cab8467d9a70e5c0ea681f9903
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-01-28 01:32:53 -0600

    Solve problem 3019

--- src/main/rust/src/bin/problem3019.rs
+++ src/main/rust/src/bin/problem3019.rs
@@ -1,6 +1,13 @@
 // https://leetcode.com/problems/number-of-changing-keys/description/
 pub fn count_key_changes(s: String) -> i32 {
-    todo!()
+    let mut answer = 0;
+    let chars: Vec<char> = s.chars().collect();
+    for i in 0..s.len() - 1 {
+        if chars[i].to_lowercase().to_string() != chars[i + 1].to_lowercase().to_string() {
+            answer += 1;
+        }
+    }
+    answer
 }
 
 fn main() {
