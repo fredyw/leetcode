1480f6a54c1802a55c146de358a99afe2d0f9aaa
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-01-29 01:18:13 -0600

    Skeleton for problem 3011

---
+++ src/main/rust/src/bin/problem3011.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/find-if-array-can-be-sorted/description/
+pub fn can_sort_array(nums: Vec<i32>) -> bool {
+    todo!()
+}
+
+fn main() {
+    println!("{}", can_sort_array(vec![8, 4, 2, 30, 15])); // true
+    println!("{}", can_sort_array(vec![1, 2, 3, 4, 5])); // true
+    println!("{}", can_sort_array(vec![3, 16, 8, 4, 2])); // false
+}
