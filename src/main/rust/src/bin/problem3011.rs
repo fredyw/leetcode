ce6db88b370d417d25027a8ad96161063a3ed96f
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-01-31 00:57:39 -0600

    Update problem 3011

--- src/main/rust/src/bin/problem3011.rs
+++ src/main/rust/src/bin/problem3011.rs
@@ -1,10 +1,14 @@
 // https://leetcode.com/problems/find-if-array-can-be-sorted/description/
 pub fn can_sort_array(nums: Vec<i32>) -> bool {
-    todo!()
+    let mut sorted_nums = nums.clone();
+    sorted_nums.sort();
+    false
 }
 
 fn main() {
     println!("{}", can_sort_array(vec![8, 4, 2, 30, 15])); // true
     println!("{}", can_sort_array(vec![1, 2, 3, 4, 5])); // true
     println!("{}", can_sort_array(vec![3, 16, 8, 4, 2])); // false
+    println!("{}", can_sort_array(vec![16, 8, 4, 2])); // true
+    println!("{}", can_sort_array(vec![16, 3])); // false
 }
