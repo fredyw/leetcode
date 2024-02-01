cd7373b5f9ecc3fbe6cc23b1354a9f7175718098
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-02-01 00:51:30 -0600

    Solve problem 3011

--- src/main/rust/src/bin/problem3011.rs
+++ src/main/rust/src/bin/problem3011.rs
@@ -2,7 +2,18 @@
 pub fn can_sort_array(nums: Vec<i32>) -> bool {
     let mut sorted_nums = nums.clone();
     sorted_nums.sort();
-    false
+    let sorted_nums: Vec<(i32, u32)> = sorted_nums.iter().map(|n| (*n, n.count_ones())).collect();
+    let mut nums: Vec<(i32, u32)> = nums.iter().map(|n| (*n, n.count_ones())).collect();
+    let mut i = 0;
+    while i < nums.len() {
+        let mut j = i;
+        while j < nums.len() && nums[i].1 == nums[j].1 {
+            j += 1;
+        }
+        nums[i..j].sort();
+        i += 1;
+    }
+    sorted_nums == nums
 }
 
 fn main() {
@@ -11,4 +22,5 @@ fn main() {
     println!("{}", can_sort_array(vec![3, 16, 8, 4, 2])); // false
     println!("{}", can_sort_array(vec![16, 8, 4, 2])); // true
     println!("{}", can_sort_array(vec![16, 3])); // false
+    println!("{}", can_sort_array(vec![16])); // true
 }
