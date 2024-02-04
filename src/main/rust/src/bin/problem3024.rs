1747a6aebe2ba03aefa5dd2f188ec0b633e21a92
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-02-04 01:35:16 -0600

    Skeleton for problem 3024

---
+++ src/main/rust/src/bin/problem3024.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/type-of-triangle-ii/description/
+pub fn triangle_type(nums: Vec<i32>) -> String {
+    todo!()
+}
+
+fn main() {
+    println!("{}", triangle_type(vec![3, 3, 3])); // "equilateral"
+    println!("{}", triangle_type(vec![3, 4, 5])); // "scalene"
+}
