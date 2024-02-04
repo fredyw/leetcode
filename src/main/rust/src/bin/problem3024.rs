cbdb9a690d09decca0b314684c2c01dcbccf9b2e
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-02-04 01:51:19 -0600

    Solve problem 3024

--- src/main/rust/src/bin/problem3024.rs
+++ src/main/rust/src/bin/problem3024.rs
@@ -1,9 +1,24 @@
 // https://leetcode.com/problems/type-of-triangle-ii/description/
 pub fn triangle_type(nums: Vec<i32>) -> String {
-    todo!()
+    fn is_triangle(nums: &Vec<i32>) -> bool {
+        nums[0] + nums[1] > nums[2] && nums[1] + nums[2] > nums[0] && nums[0] + nums[2] > nums[1]
+    }
+
+    if !is_triangle(&nums) {
+        "none".to_string()
+    } else if nums[0] == nums[1] && nums[1] == nums[2] {
+        "equilateral".to_string()
+    } else if nums[0] != nums[1] && nums[1] != nums[2] && nums[2] != nums[0] {
+        "scalene".to_string()
+    } else {
+        "isosceles".to_string()
+    }
 }
 
 fn main() {
     println!("{}", triangle_type(vec![3, 3, 3])); // "equilateral"
     println!("{}", triangle_type(vec![3, 4, 5])); // "scalene"
+    println!("{}", triangle_type(vec![3, 5, 5])); // "isosceles"
+    println!("{}", triangle_type(vec![1, 2, 3])); // "nonw"
+    println!("{}", triangle_type(vec![8, 4, 2])); // "nonw"
 }
