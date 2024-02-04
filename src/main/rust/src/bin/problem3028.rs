8c6268cafaed9d6c5226570dbc1bb714aa589138
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-02-04 01:30:54 -0600

    Skeleton for problem 3028

---
+++ src/main/rust/src/bin/problem3028.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/ant-on-the-boundary/
+pub fn return_to_boundary_count(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", return_to_boundary_count(vec![2, 3, -5])); // 1
+    println!("{}", return_to_boundary_count(vec![3, 2, -3, -4])); // 0
+    println!("{}", return_to_boundary_count(vec![2, 3, -5, 5, -5])); // 2
+}
