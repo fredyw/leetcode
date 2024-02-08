a59d77c4fd00674426a5f4f7b9b009d9b8321d6d
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-02-07 23:51:41 -0600

    Skeleton for problem 3015

---
+++ src/main/rust/src/bin/problem3015.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/count-the-number-of-houses-at-a-certain-distance-i/description/
+pub fn count_of_pairs(n: i32, x: i32, y: i32) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", count_of_pairs(3, 1, 3)); // [6,0,0]
+    println!("{:?}", count_of_pairs(5, 2, 4)); // [10,8,2,0,0]
+    println!("{:?}", count_of_pairs(4, 1, 1)); // [6,4,2,0]
+}
