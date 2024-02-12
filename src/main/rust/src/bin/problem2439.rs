195b121868b2555ca9633dd9d0f551950dd6566e
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-02-12 01:14:33 -0600

    Update problem 2439

--- src/main/rust/src/bin/problem2439.rs
+++ src/main/rust/src/bin/problem2439.rs
@@ -6,4 +6,5 @@ pub fn minimize_array_value(nums: Vec<i32>) -> i32 {
 fn main() {
     println!("{}", minimize_array_value(vec![3, 7, 1, 6])); // 5
     println!("{}", minimize_array_value(vec![10, 1])); // 10
+    println!("{}", minimize_array_value(vec![1, 1, 1, 1])); // 1
 }
