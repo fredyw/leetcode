6222a4cf3dd62f9d577ee7456bd4f3bd1a6dcbfc
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-02-16 01:14:15 -0600

    Update problem 2439

--- src/main/rust/src/bin/problem2439.rs
+++ src/main/rust/src/bin/problem2439.rs
@@ -1,11 +1,27 @@
 // https://leetcode.com/problems/minimize-maximum-of-array/description/
 pub fn minimize_array_value(nums: Vec<i32>) -> i32 {
-    todo!()
+    let mut answer = nums[0];
+    for i in 1..nums.len() {
+        if nums[i - 1] < nums[i] {
+            let n = (nums[i] - nums[i - 1]) / 2;
+            let mid = if (nums[i] - nums[i - 1] + 1) % 2 == 0 {
+                (nums[i - 1] + n) + 1
+            } else {
+                nums[i - 1] + n
+            };
+            answer = answer.max(mid);
+        } else {
+            answer = answer.max(nums[i]);
+        }
+    }
+    answer
 }
 
 fn main() {
-    println!("{}", minimize_array_value(vec![3, 7, 1, 6])); // 5
-    println!("{}", minimize_array_value(vec![10, 1])); // 10
-    println!("{}", minimize_array_value(vec![1, 1, 1, 1])); // 1
-    println!("{}", minimize_array_value(vec![1, 10])); // 6
+    // println!("{}", minimize_array_value(vec![3, 7, 1, 6])); // 5
+    // println!("{}", minimize_array_value(vec![10, 1])); // 10
+    // println!("{}", minimize_array_value(vec![1, 1, 1, 1])); // 1
+    // println!("{}", minimize_array_value(vec![1, 10])); // 6
+    println!("{}", minimize_array_value(vec![1, 2, 3, 4, 5])); // 3
+                                                               // println!("{}", minimize_array_value(vec![1, 2, 4, 3, 4, 10, 2])); // 4
 }
