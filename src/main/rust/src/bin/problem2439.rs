09c3dbb7700fe8d0f23e3aae4e4c1ddbc4eae120
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-02-16 16:34:52 -0600

    Solve problem 2439

--- src/main/rust/src/bin/problem2439.rs
+++ src/main/rust/src/bin/problem2439.rs
@@ -1,29 +1,21 @@
 // https://leetcode.com/problems/minimize-maximum-of-array/description/
-pub fn minimize_array_value(mut nums: Vec<i32>) -> i32 {
-    let mut answer = nums[0];
-    for i in 1..nums.len() {
-        if nums[i - 1] < nums[i] {
-            let n = (nums[i] - nums[i - 1]) / 2;
-            let mid = if (nums[i] - nums[i - 1] + 1) % 2 == 0 {
-                (nums[i - 1] + n) + 1
-            } else {
-                nums[i - 1] + n
-            };
-            nums[i] = nums[i] - mid;
-            answer = answer.max(mid);
-        } else {
-            answer = answer.max(nums[i]);
-        }
+pub fn minimize_array_value(nums: Vec<i32>) -> i32 {
+    let mut answer = 0;
+    let mut sum: i64 = 0;
+    for i in 0..nums.len() {
+        sum += nums[i] as i64;
+        let average = (sum as f64 / (i as i64 + 1) as f64).ceil() as i64;
+        answer = answer.max(average);
     }
-    answer
+    answer as i32
 }
 
 fn main() {
-    // println!("{}", minimize_array_value(vec![3, 7, 1, 6])); // 5
-    // println!("{}", minimize_array_value(vec![10, 1])); // 10
-    // println!("{}", minimize_array_value(vec![1, 1, 1, 1])); // 1
-    // println!("{}", minimize_array_value(vec![1, 10])); // 6
-    // println!("{}", minimize_array_value(vec![5, 4, 3, 2, 1])); // 5
-    // println!("{}", minimize_array_value(vec![1, 2, 3, 4, 5])); // 3
+    println!("{}", minimize_array_value(vec![3, 7, 1, 6])); // 5
+    println!("{}", minimize_array_value(vec![10, 1])); // 10
+    println!("{}", minimize_array_value(vec![1, 1, 1, 1])); // 1
+    println!("{}", minimize_array_value(vec![1, 10])); // 6
+    println!("{}", minimize_array_value(vec![5, 4, 3, 2, 1])); // 5
+    println!("{}", minimize_array_value(vec![1, 2, 3, 4, 5])); // 3
     println!("{}", minimize_array_value(vec![1, 2, 4, 3, 4, 10])); // 4
 }
