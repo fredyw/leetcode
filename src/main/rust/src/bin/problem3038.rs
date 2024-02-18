7d3469e4807508d774200a73477bbfa91a9d9735
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-02-18 13:09:16 -0600

    Skeleton for problem 3038

---
+++ src/main/rust/src/bin/problem3038.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/maximum-number-of-operations-with-the-same-score-i/description/
+pub fn max_operations(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", max_operations(vec![3, 2, 1, 4, 5])); // 2
+    println!("{}", max_operations(vec![3, 2, 6, 1, 4])); // 1
+}
