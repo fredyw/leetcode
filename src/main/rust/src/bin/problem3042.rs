daa60027818f3128ba7007eb79d79d28eed76f22
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-02-18 13:07:13 -0600

    Solve problem 3042

--- src/main/rust/src/bin/problem3042.rs
+++ src/main/rust/src/bin/problem3042.rs
@@ -1,6 +1,14 @@
-// https://leetcode.com/problems/count-prefix-and-suffix-pairs-i/description/
+// https://leetcode.com/problems/   count-prefix-and-suffix-pairs-i/description/
 pub fn count_prefix_suffix_pairs(words: Vec<String>) -> i32 {
-    todo!()
+    let mut answer = 0;
+    for i in 0..words.len() {
+        for j in i + 1..words.len() {
+            if words[j].starts_with(&words[i]) && words[j].ends_with(&words[i]) {
+                answer += 1;
+            }
+        }
+    }
+    answer
 }
 
 fn main() {
