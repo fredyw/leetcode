1b70397361f33ba3c500a4ce64e6b39fce954337
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-02-19 14:57:24 -0600

    Solve problem 3038

--- src/main/rust/src/bin/problem3038.rs
+++ src/main/rust/src/bin/problem3038.rs
@@ -1,9 +1,17 @@
 // https://leetcode.com/problems/maximum-number-of-operations-with-the-same-score-i/description/
 pub fn max_operations(nums: Vec<i32>) -> i32 {
-    todo!()
+    let mut answer = 1;
+    let sum = nums[0] + nums[1];
+    let mut i = 2;
+    while i < nums.len() - 1 && sum == nums[i] + nums[i + 1] {
+        answer += 1;
+        i += 2;
+    }
+    answer
 }
 
 fn main() {
     println!("{}", max_operations(vec![3, 2, 1, 4, 5])); // 2
     println!("{}", max_operations(vec![3, 2, 6, 1, 4])); // 1
+    println!("{}", max_operations(vec![2, 2, 3, 2, 4, 2, 3, 3, 1, 3])); // 1
 }
