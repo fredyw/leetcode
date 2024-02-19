fa066e57c2434c770516c996302a0db19729f8ba
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-02-19 17:53:40 -0600

    Update problem 3039

--- src/main/rust/src/bin/problem3039.rs
+++ src/main/rust/src/bin/problem3039.rs
@@ -1,6 +1,18 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/apply-operations-to-make-string-empty/description/
 pub fn last_non_empty_string(s: String) -> String {
-    todo!()
+    let mut map: HashMap<char, u32> = HashMap::new();
+    for c in s.chars().into_iter() {
+        *map.entry(c).or_insert(0) += 1;
+    }
+    let mut max_count = 0;
+    for (_, count) in map.iter() {
+        max_count = max_count.max(*count);
+    }
+    let mut answer = String::new();
+    // TODO
+    answer
 }
 
 fn main() {
