000e976e747842ac0920728222a2b3dcfb6d6bc4
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-02-19 15:06:06 -0600

    Skeleton for problem 3039

---
+++ src/main/rust/src/bin/problem3039.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/apply-operations-to-make-string-empty/description/
+pub fn last_non_empty_string(s: String) -> String {
+    todo!()
+}
+
+fn main() {
+    println!("{}", last_non_empty_string("aabcbbca".to_string())); // "ba"
+    println!("{}", last_non_empty_string("abcd".to_string())); // "abcd"
+}
