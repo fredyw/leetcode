45fe44b1100f8a019dad2232c717134f1e286634
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-02-24 13:24:03 -0600

    Skeleton for problem 2971

---
+++ src/main/rust/src/bin/problem2971.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/find-polygon-with-the-largest-perimeter/description/
+pub fn largest_perimeter(nums: Vec<i32>) -> i64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", largest_perimeter(vec![5, 5, 5])); // 15
+    println!("{}", largest_perimeter(vec![1, 12, 1, 2, 5, 50, 3])); // 12
+    println!("{}", largest_perimeter(vec![5, 5, 50])); // -1
+}
