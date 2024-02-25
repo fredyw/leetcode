b67aed54b6fcd713d22bf8d4b15b4cc3df73649f
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-02-25 00:51:59 -0600

    Skeleton for problem 3046

---
+++ src/main/rust/src/bin/problem3046.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/split-the-array/description/
+pub fn is_possible_to_split(nums: Vec<i32>) -> bool {
+    todo!()
+}
+
+fn main() {
+    println!("{}", is_possible_to_split(vec![1, 1, 2, 2, 3, 4])); // true
+    println!("{}", is_possible_to_split(vec![1, 1, 1, 1])); // false
+}
