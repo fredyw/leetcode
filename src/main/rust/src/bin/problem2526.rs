5642b6235fb92abff73bffcdccc99b0497c857cd
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-02-24 21:52:30 -0600

    Solve probelm 2526

--- src/main/rust/src/bin/problem2526.rs
+++ src/main/rust/src/bin/problem2526.rs
@@ -1,19 +1,39 @@
-struct DataStream {}
+struct DataStream {
+    value: i32,
+    k: i32,
+    count: i32,
+}
 
 impl DataStream {
     fn new(value: i32, k: i32) -> Self {
-        todo!()
+        DataStream {
+            value,
+            k,
+            count: 0,
+        }
     }
 
-    fn consec(&self, num: i32) -> bool {
-        todo!()
+    fn consec(&mut self, num: i32) -> bool {
+        if self.value != num {
+            self.count = 0;
+        } else {
+            self.count += 1;
+        }
+        self.count >= self.k
     }
 }
 
 fn main() {
-    let data_stream = DataStream::new(4, 3);
-    println!("{}", data_stream.consec(4)); // false
-    println!("{}", data_stream.consec(4)); // false
-    println!("{}", data_stream.consec(4)); // true
-    println!("{}", data_stream.consec(3)); // false
+    // let mut data_stream = DataStream::new(4, 3);
+    // println!("{}", data_stream.consec(4)); // false
+    // println!("{}", data_stream.consec(4)); // false
+    // println!("{}", data_stream.consec(4)); // true
+    // println!("{}", data_stream.consec(3)); // false
+
+    let mut data_stream  = DataStream::new(1, 2);
+    println!("{}", data_stream.consec(1)); // false
+    println!("{}", data_stream.consec(2)); // false
+    println!("{}", data_stream.consec(1)); // false
+    println!("{}", data_stream.consec(1)); // true
+    println!("{}", data_stream.consec(1)); // true
 }
