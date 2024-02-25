5ef6f1c5f99620fa5f8a2f2ddb544dadcf6fc14e
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-02-25 01:00:10 -0600

    Solve problem 3046

--- src/main/rust/src/bin/problem3046.rs
+++ src/main/rust/src/bin/problem3046.rs
@@ -1,6 +1,17 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/split-the-array/description/
 pub fn is_possible_to_split(nums: Vec<i32>) -> bool {
-    todo!()
+    let mut map: HashMap<i32, i32> = HashMap::new();
+    for num in nums {
+        *map.entry(num).or_insert(0) += 1;
+    }
+    for (_, count) in map.into_iter() {
+        if count > 2 {
+            return false;
+        }
+    }
+    true
 }
 
 fn main() {
