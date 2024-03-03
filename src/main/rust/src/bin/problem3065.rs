12c7a1e055026b19400fb56f9b59918a60e0596a
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-03-02 23:56:11 -0600

    Skeleton for problem 3065

---
+++ src/main/rust/src/bin/problem3065.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/minimum-operations-to-exceed-threshold-value-i/description/
+pub fn min_operations(nums: Vec<i32>, k: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", min_operations(vec![2, 11, 10, 1, 3], 10)); // 3
+    println!("{}", min_operations(vec![1, 1, 2, 4, 9], 1)); // 0
+    println!("{}", min_operations(vec![1, 1, 2, 4, 9], 9)); // 4
+}
