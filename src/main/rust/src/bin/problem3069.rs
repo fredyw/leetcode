e03bfb5f59e0b51ed1db209ae1fa0fac21421eb2
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-03-03 00:11:15 -0600

    Solve problem 3069

--- src/main/rust/src/bin/problem3069.rs
+++ src/main/rust/src/bin/problem3069.rs
@@ -1,6 +1,20 @@
 // https://leetcode.com/problems/distribute-elements-into-two-arrays-i/description/
 pub fn result_array(nums: Vec<i32>) -> Vec<i32> {
-    todo!()
+    let mut arr1 = vec![];
+    let mut arr2 = vec![];
+    arr1.push(nums[0]);
+    arr2.push(nums[1]);
+    let mut i = 2;
+    while i < nums.len() {
+        if arr1[arr1.len() - 1] > arr2[arr2.len() - 1] {
+            arr1.push(nums[i]);
+        } else {
+            arr2.push(nums[i]);
+        }
+        i += 1;
+    }
+    arr1.append(&mut arr2);
+    arr1
 }
 
 fn main() {
