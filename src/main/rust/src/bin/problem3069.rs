281d3e1950b7d66a59720a7d2fdbf51583ebfe75
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-03-02 23:59:54 -0600

    Skeleton for problem 3069

---
+++ src/main/rust/src/bin/problem3069.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/distribute-elements-into-two-arrays-i/description/
+pub fn result_array(nums: Vec<i32>) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", result_array(vec![2, 1, 3])); // [2,3,1]
+    println!("{:?}", result_array(vec![5, 4, 3, 8])); // [5,3,4,8]
+}
