2b43c251ebc0ea16d495ad5286aeb91ca06562af
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-03-10 00:08:51 -0600

    Skeleton for problem 3074

---
+++ src/main/rust/src/bin/problem3074.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/apple-redistribution-into-boxes/description/
+pub fn minimum_boxes(apple: Vec<i32>, capacity: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", minimum_boxes(vec![1, 3, 2], vec![4, 3, 1, 5, 2])); // 2
+    println!("{}", minimum_boxes(vec![5, 5, 5], vec![2, 4, 2, 7])); // 4
+}
