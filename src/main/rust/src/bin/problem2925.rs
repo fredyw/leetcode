aec9347d1c772cd3b8296f98baac7a39076830e0
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-03-11 13:05:49 -0500

    Update problem 2925

--- src/main/rust/src/bin/problem2925.rs
+++ src/main/rust/src/bin/problem2925.rs
@@ -2,7 +2,7 @@
 pub fn maximum_score_after_operations(edges: Vec<Vec<i32>>, values: Vec<i32>) -> i64 {
     fn dfs(tree: &Vec<Vec<usize>>, values: &Vec<i32>, root: usize) -> (i64, i64) {
         if tree[root].is_empty() {
-            return (0, 0);
+            return (0, values[root] as i64);
         }
         let mut total_score = 0;
         let mut total_sum = 0;
@@ -26,25 +26,40 @@ pub fn maximum_score_after_operations(edges: Vec<Vec<i32>>, values: Vec<i32>) ->
 }
 
 fn main() {
-    println!(
-        "{}",
-        maximum_score_after_operations(
-            vec![vec![0, 1], vec![0, 2], vec![0, 3], vec![2, 4], vec![4, 5]],
-            vec![5, 2, 5, 2, 1, 1]
-        )
-    ); // 11
+    // println!(
+    //     "{}",
+    //     maximum_score_after_operations(
+    //         vec![vec![0, 1], vec![0, 2], vec![0, 3], vec![2, 4], vec![4, 5]],
+    //         vec![5, 2, 5, 2, 1, 1]
+    //     )
+    // ); // 11
+    // println!(
+    //     "{}",
+    //     maximum_score_after_operations(
+    //         vec![
+    //             vec![0, 1],
+    //             vec![0, 2],
+    //             vec![1, 3],
+    //             vec![1, 4],
+    //             vec![2, 5],
+    //             vec![2, 6]
+    //         ],
+    //         vec![20, 10, 9, 7, 4, 3, 5]
+    //     )
+    // ); // 40
     println!(
         "{}",
         maximum_score_after_operations(
             vec![
-                vec![0, 1],
-                vec![0, 2],
-                vec![1, 3],
-                vec![1, 4],
-                vec![2, 5],
-                vec![2, 6]
+                vec![7, 0],
+                vec![3, 1],
+                vec![6, 2],
+                vec![4, 3],
+                vec![4, 5],
+                vec![4, 6],
+                vec![4, 7]
             ],
-            vec![20, 10, 9, 7, 4, 3, 5]
+            vec![2, 16, 23, 17, 22, 21, 8, 6]
         )
-    ); // 40
+    ); // 113
 }
