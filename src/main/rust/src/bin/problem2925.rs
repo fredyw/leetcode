b4a5ecb475e56bfb794cb03afc0737531a42fb09
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-03-11 12:08:20 -0500

    Update problem 2925

--- src/main/rust/src/bin/problem2925.rs
+++ src/main/rust/src/bin/problem2925.rs
@@ -1,11 +1,18 @@
 // https://leetcode.com/problems/maximum-score-after-applying-operations-on-a-tree/
 pub fn maximum_score_after_operations(edges: Vec<Vec<i32>>, values: Vec<i32>) -> i64 {
-    fn dfs(tree: &Vec<Vec<usize>>, values: &Vec<i32>, root: usize, score: i64, sum: i64) -> i64 {
+    fn dfs(tree: &Vec<Vec<usize>>, values: &Vec<i32>, root: usize) -> (i64, i64) {
         if tree[root].is_empty() {
-            return if sum > 0 { score } else { 0 };
+            return (0, 0);
         }
-        for child in tree[root].iter() {}
-        0
+        let mut total_score = 0;
+        let mut total_sum = 0;
+        for child in tree[root].iter() {
+            let (score, sum) = dfs(tree, values, *child);
+            total_score += score;
+            total_sum += sum;
+        }
+        let max_score = total_sum.max(total_score + values[root] as i64);
+        return (max_score, values[root] as i64 + total_sum);
     }
 
     let mut tree: Vec<Vec<usize>> = vec![vec![]; values.len()];
@@ -14,7 +21,8 @@ pub fn maximum_score_after_operations(edges: Vec<Vec<i32>>, values: Vec<i32>) ->
         let to = edge[1] as usize;
         tree[from].push(to);
     }
-    dfs(&tree, &values, 0, 0, 0)
+    let (score, sum) = dfs(&tree, &values, 0);
+    sum.max(score)
 }
 
 fn main() {
@@ -25,18 +33,18 @@ fn main() {
             vec![5, 2, 5, 2, 1, 1]
         )
     ); // 11
-       // println!(
-       //     "{}",
-       //     maximum_score_after_operations(
-       //         vec![
-       //             vec![0, 1],
-       //             vec![0, 2],
-       //             vec![1, 3],
-       //             vec![1, 4],
-       //             vec![2, 5],
-       //             vec![2, 6]
-       //         ],
-       //         vec![20, 10, 9, 7, 4, 3, 5]
-       //     )
-       // ); // 40
+    println!(
+        "{}",
+        maximum_score_after_operations(
+            vec![
+                vec![0, 1],
+                vec![0, 2],
+                vec![1, 3],
+                vec![1, 4],
+                vec![2, 5],
+                vec![2, 6]
+            ],
+            vec![20, 10, 9, 7, 4, 3, 5]
+        )
+    ); // 40
 }
