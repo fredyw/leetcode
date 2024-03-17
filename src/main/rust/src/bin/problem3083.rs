6c3e7ec40b56f8c82a5165e6a94452858d4cc414
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-03-17 16:28:29 -0500

    Skeleton for problem 3083

---
+++ src/main/rust/src/bin/problem3083.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/existence-of-a-substring-in-a-string-and-its-reverse/description/
+pub fn is_substring_present(s: String) -> bool {
+    todo!()
+}
+
+fn main() {
+    println!("{}", is_substring_present("leetcode".to_string())); // true
+    println!("{}", is_substring_present("abcba".to_string())); // true
+    println!("{}", is_substring_present("abcd".to_string())); // false
+}
