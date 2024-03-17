6621aaf2927c79535aed70020c330b8d0ca9ff98
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-03-17 16:27:11 -0500

    Solve problem 3079

--- src/main/rust/src/bin/problem3079.rs
+++ src/main/rust/src/bin/problem3079.rs
@@ -1,9 +1,22 @@
 // https://leetcode.com/problems/find-the-sum-of-encrypted-integers/description/
 pub fn sum_of_encrypted_int(nums: Vec<i32>) -> i32 {
-    todo!()
+    fn max_number(n: i32) -> i32 {
+        n.to_string().chars().max().unwrap() as i32 - '0' as i32
+    }
+
+    nums.iter()
+        .map(|n| {
+            let len = n.to_string().len();
+            max_number(*n)
+                .to_string()
+                .repeat(len)
+                .parse::<i32>()
+                .unwrap()
+        })
+        .sum()
 }
 
 fn main() {
-    println!("{}", sum_of_encrypted_int(vec![1, 2, 3])); // 5
+    println!("{}", sum_of_encrypted_int(vec![1, 2, 3])); // 6
     println!("{}", sum_of_encrypted_int(vec![10, 21, 31])); // 66
 }
