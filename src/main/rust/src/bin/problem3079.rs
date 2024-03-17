282ea57cb1baf142e19cdc2ac5f154aaaffd2569
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-03-17 15:02:12 -0500

    Skeleton for problem 3079

---
+++ src/main/rust/src/bin/problem3079.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/find-the-sum-of-encrypted-integers/description/
+pub fn sum_of_encrypted_int(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", sum_of_encrypted_int(vec![1, 2, 3])); // 5
+    println!("{}", sum_of_encrypted_int(vec![10, 21, 31])); // 66
+}
