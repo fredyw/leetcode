199df759c559a699f8e479b683a9e4543e8b1d7d
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-03-17 21:52:20 -0500

    Solve problem 3083

--- src/main/rust/src/bin/problem3083.rs
+++ src/main/rust/src/bin/problem3083.rs
@@ -1,6 +1,11 @@
 // https://leetcode.com/problems/existence-of-a-substring-in-a-string-and-its-reverse/description/
 pub fn is_substring_present(s: String) -> bool {
-    todo!()
+    s.chars()
+        .rev()
+        .collect::<Vec<char>>()
+        .windows(2)
+        .map(|w| w.iter().collect::<String>())
+        .any(|sub| s.contains(&sub))
 }
 
 fn main() {
