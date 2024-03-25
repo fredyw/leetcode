d0a89b81d007a5d030a92f9303084a55cea5967c
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-03-24 22:10:30 -0500

    Skeleton for problem 3090

---
+++ src/main/rust/src/bin/problem3090.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/maximum-length-substring-with-two-occurrences/description/
+pub fn maximum_length_substring(s: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", maximum_length_substring("bcbbbcba".to_string())); // 4
+    println!("{}", maximum_length_substring("aaaa".to_string())); // 2
+}
