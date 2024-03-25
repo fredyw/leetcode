b854c63ee7942d9d1da31a2fdefe120e949ca440
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-03-24 22:19:43 -0500

    Solve problem 3090

--- src/main/rust/src/bin/problem3090.rs
+++ src/main/rust/src/bin/problem3090.rs
@@ -1,6 +1,24 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/maximum-length-substring-with-two-occurrences/description/
 pub fn maximum_length_substring(s: String) -> i32 {
-    todo!()
+    let mut answer = 0;
+    let bytes = s.as_bytes();
+    for i in 0..bytes.len() {
+        let mut map: HashMap<u8, i32> = HashMap::new();
+        let mut length = 0;
+        for j in i..bytes.len() {
+            *map.entry(bytes[j]).or_insert(0) += 1;
+            if let Some(c) = map.get(&bytes[j]) {
+                if *c == 3 {
+                    break;
+                }
+            }
+            length += 1;
+            answer = answer.max(length);
+        }
+    }
+    answer
 }
 
 fn main() {
