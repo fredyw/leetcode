da693552ea7c4a92fab6c69c0c5314b448a1c86b
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-03-30 15:08:05 -0500

    Skeleton for problem 3095

---
+++ src/main/rust/src/bin/problem3095.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/shortest-subarray-with-or-at-least-k-i/description/
+pub fn minimum_subarray_length(nums: Vec<i32>, k: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", minimum_subarray_length(vec![1, 2, 3], 2)); // 1
+    println!("{}", minimum_subarray_length(vec![2, 1, 8], 10)); // 3
+    println!("{}", minimum_subarray_length(vec![1, 2], 0)); // 1
+}
