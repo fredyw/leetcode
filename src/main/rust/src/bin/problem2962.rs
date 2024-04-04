032e03b54894c5188b0686a06284c63228eb7385
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-04-04 17:06:12 -0500

    Skeleton for problem 2962

---
+++ src/main/rust/src/bin/problem2962.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/count-subarrays-where-max-element-appears-at-least-k-times/description/
+pub fn count_subarrays(nums: Vec<i32>, k: i32) -> i64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", count_subarrays(vec![1, 3, 2, 3, 3], 2)); // 6
+    println!("{}", count_subarrays(vec![1, 4, 2, 1], 3)); // 0
+}
