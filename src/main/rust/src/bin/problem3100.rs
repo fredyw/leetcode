4671de297463c1a181930c9b56cc95abe447cb81
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-04-04 17:04:15 -0500

    Solve problem 3100

--- src/main/rust/src/bin/problem3100.rs
+++ src/main/rust/src/bin/problem3100.rs
@@ -1,9 +1,19 @@
 // https://leetcode.com/problems/water-bottles-ii/description/
-pub fn max_bottles_drunk(num_bottles: i32, num_exchange: i32) -> i32 {
-    todo!()
+pub fn max_bottles_drunk(num_bottles: i32, mut num_exchange: i32) -> i32 {
+    let mut answer = num_bottles;
+    let mut empty_bottles = num_bottles;
+    while empty_bottles - num_exchange >= 0 {
+        empty_bottles -= num_exchange;
+        num_exchange += 1;
+        answer += 1;
+        empty_bottles += 1;
+    }
+    answer
 }
 
 fn main() {
     println!("{}", max_bottles_drunk(13, 6)); // 15
     println!("{}", max_bottles_drunk(10, 3)); // 13
+    println!("{}", max_bottles_drunk(20, 1)); // 26
+    println!("{}", max_bottles_drunk(3, 1)); // 5
 }
