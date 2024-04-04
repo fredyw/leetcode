2aee1e3779ac8dac4d4755e903c8f4e8f2663bdb
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-04-04 16:17:08 -0500

    Skeleton for problem 3100

---
+++ src/main/rust/src/bin/problem3100.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/water-bottles-ii/description/
+pub fn max_bottles_drunk(num_bottles: i32, num_exchange: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", max_bottles_drunk(13, 6)); // 15
+    println!("{}", max_bottles_drunk(10, 3)); // 13
+}
