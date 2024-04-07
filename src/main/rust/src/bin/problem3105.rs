77bf521fd8c0afd03d5b51795cb9cbf05150e10d
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-04-07 00:54:55 -0500

    Skeleton for problem 3105

---
+++ src/main/rust/src/bin/problem3105.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/longest-strictly-increasing-or-strictly-decreasing-subarray/description/
+pub fn longest_monotonic_subarray(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", longest_monotonic_subarray(vec![1, 4, 3, 3, 2])); // 2
+    println!("{}", longest_monotonic_subarray(vec![3, 3, 3, 3])); // 1
+    println!("{}", longest_monotonic_subarray(vec![3, 2, 1])); // 3
+}
