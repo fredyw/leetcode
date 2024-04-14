05cb40ab37f70ab773fb175b684c7b53484b1472
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-04-14 00:22:06 -0500

    Skeleton for problem 3110

---
+++ src/main/rust/src/bin/problem3110.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/score-of-a-string/description/
+pub fn score_of_string(s: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", score_of_string("hello".to_string())); // 13
+    println!("{}", score_of_string("zaz".to_string())); // 50
+}
