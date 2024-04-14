2354cc385f2e094d48f0ec62eedb8390db9459aa
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-04-14 00:38:32 -0500

    Update problem 3114

--- src/main/rust/src/bin/problem3114.rs
+++ src/main/rust/src/bin/problem3114.rs
@@ -6,4 +6,5 @@ pub fn find_latest_time(s: String) -> String {
 fn main() {
     println!("{}", find_latest_time("1?:?4".to_string())); // "11:54"
     println!("{}", find_latest_time("0?:5?".to_string())); // "09:59"
+    println!("{}", find_latest_time("??:??".to_string())); // "11:59"
 }
