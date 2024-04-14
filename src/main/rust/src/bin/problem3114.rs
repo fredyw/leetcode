c9ac9129850ebf56ae2bb6c1b2600722743f647b
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-04-14 00:27:09 -0500

    Skeleton for problem 3114

---
+++ src/main/rust/src/bin/problem3114.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/latest-time-you-can-obtain-after-replacing-characters/description/
+pub fn find_latest_time(s: String) -> String {
+    todo!()
+}
+
+fn main() {
+    println!("{}", find_latest_time("1?:?4".to_string())); // "11:54"
+    println!("{}", find_latest_time("0?:5?".to_string())); // "09:59"
+}
