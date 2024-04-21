21bb782fb85e74771eebac32bb6b0d8f9f0150e0
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-04-21 02:25:48 -0500

    Skeleton for problem 3120

---
+++ src/main/rust/src/bin/problem3120.rs
@@ -0,0 +1,10 @@
+//https://leetcode.com/problems/count-the-number-of-special-characters-i/description/
+pub fn number_of_special_chars(word: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", number_of_special_chars("aaAbcBC".to_string())); // 3
+    println!("{}", number_of_special_chars("abc".to_string())); // 0
+    println!("{}", number_of_special_chars("abBCab".to_string())); // 1
+}
