7d8e445cd4f9a4e17eccd60f4d582386728de308
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-04-27 18:09:21 -0500

    Solve problem 3127

--- src/main/rust/src/bin/problem3127.rs
+++ src/main/rust/src/bin/problem3127.rs
@@ -1,6 +1,25 @@
 // https://leetcode.com/problems/make-a-square-with-the-same-color/description/
 pub fn can_make_square(grid: Vec<Vec<char>>) -> bool {
-    todo!()
+    fn count_white(grid: &Vec<Vec<char>>, row: usize, col: usize) -> u32 {
+        if grid[row][col] == 'W' {
+            1
+        } else {
+            0
+        }
+    }
+
+    fn can_make_square(grid: &Vec<Vec<char>>, row: usize, col: usize) -> bool {
+        let total_white_count = count_white(&grid, row, col)
+            + count_white(&grid, row, col + 1)
+            + count_white(&grid, row + 1, col)
+            + count_white(&grid, row + 1, col + 1);
+        total_white_count <= 1 || total_white_count >= 3
+    }
+
+    can_make_square(&grid, 0, 0)
+        || can_make_square(&grid, 0, 1)
+        || can_make_square(&grid, 1, 0)
+        || can_make_square(&grid, 1, 1)
 }
 
 fn main() {
