c24e99f60127cd5de42c5527d7548aa235518f48
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-04-28 01:23:06 -0500

    Skeleton for problem 3131

---
+++ src/main/rust/src/bin/problem3131.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/find-the-integer-added-to-array-i/description/
+pub fn added_integer(nums1: Vec<i32>, nums2: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", added_integer(vec![2, 6, 4], vec![9, 7, 5])); // 3
+    println!("{}", added_integer(vec![10], vec![5])); // -5
+    println!("{}", added_integer(vec![1, 1, 1, 1], vec![1, 1, 1, 1])); // 3
+}
