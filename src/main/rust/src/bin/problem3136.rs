e5002c07f29ecb7860e8b7d59b71e86fa484e0ea
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-05-05 00:35:41 -0500

    Skeleton for problem 3136

---
+++ src/main/rust/src/bin/problem3136.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/valid-word/
+pub fn is_valid(word: String) -> bool {
+    todo!()
+}
+
+fn main() {
+    println!("{}", is_valid("234Adas".to_string())); // true
+    println!("{}", is_valid("b3".to_string())); // false
+    println!("{}", is_valid("a3$e".to_string())); // false
+}
