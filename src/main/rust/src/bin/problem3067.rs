bdb0f585969831a48cd1e936332fcbd9e6bee6a4
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-05-06 00:49:45 -0500

    Update problem 3067

--- src/main/rust/src/bin/problem3067.rs
+++ src/main/rust/src/bin/problem3067.rs
@@ -1,6 +1,72 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/count-pairs-of-connectable-servers-in-a-weighted-tree-network/description/
 pub fn count_pairs_of_connectable_servers(edges: Vec<Vec<i32>>, signal_speed: i32) -> Vec<i32> {
-    todo!()
+    #[derive(Debug)]
+    struct Edge {
+        to: usize,
+        weight: i32,
+    }
+
+    fn dfs(
+        graph: &HashMap<usize, Vec<Edge>>,
+        signal_speed: i32,
+        node: usize,
+        from: usize,
+        has_connectable: bool,
+        sum: i32,
+        visited: &mut Vec<bool>,
+        answer: &mut Vec<i32>,
+    ) {
+        if sum % signal_speed == 0 {}
+        visited[from] = true;
+        for edge in graph.get(&from).unwrap_or(&vec![]) {
+            if visited[edge.to] {
+                continue;
+            }
+            dfs(
+                graph,
+                signal_speed,
+                node,
+                edge.to,
+                has_connectable,
+                sum + edge.weight,
+                visited,
+                answer,
+            );
+        }
+    }
+
+    let mut graph: HashMap<usize, Vec<Edge>> = HashMap::new();
+    for edge in edges.iter() {
+        let from = edge[0] as usize;
+        let to = edge[1] as usize;
+        let weight = edge[2];
+        graph
+            .entry(from)
+            .or_insert(vec![])
+            .push(Edge { to, weight });
+    }
+
+    let mut answer = vec![0; graph.len()];
+    for node in 0..graph.len() {
+        for from in 0..graph.len() {
+            if node == from {
+                continue;
+            }
+            dfs(
+                &graph,
+                signal_speed,
+                node,
+                from,
+                false,
+                0,
+                &mut vec![false; graph.len()],
+                &mut answer,
+            );
+        }
+    }
+    answer
 }
 
 fn main() {
