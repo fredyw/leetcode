f6afadc062d5aab49918d043cd368da69050b8d9
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-05-09 00:25:17 -0500

    Skeleton for problem 3106

---
+++ src/main/rust/src/bin/problem3106.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/lexicographically-smallest-string-after-operations-with-constraint/description/
+pub fn get_smallest_string(s: String, k: i32) -> String {
+    todo!()
+}
+
+fn main() {
+    println!("{}", get_smallest_string("zbbz".to_string(), 3)); // "aaaz"
+    println!("{}", get_smallest_string("xaxcd".to_string(), 4)); // "aawcd"
+    println!("{}", get_smallest_string("lol".to_string(), 0)); // "lol"
+}
