8381aec7e9a5b20b8cd10a9951b8ce0fcb2e4f27
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-05-11 19:04:50 -0500

    Solve problem 3142

--- src/main/rust/src/bin/problem3142.rs
+++ src/main/rust/src/bin/problem3142.rs
@@ -1,6 +1,16 @@
 // https://leetcode.com/problems/check-if-grid-satisfies-conditions/description/
 pub fn satisfies_conditions(grid: Vec<Vec<i32>>) -> bool {
-    todo!()
+    for i in 0..grid.len() {
+        for j in 0..grid[0].len() {
+            if i + 1 < grid.len() && grid[i][j] != grid[i + 1][j] {
+                return false;
+            }
+            if j + 1 < grid[i].len() && grid[i][j] == grid[i][j + 1] {
+                return false;
+            }
+        }
+    }
+    true
 }
 
 fn main() {
