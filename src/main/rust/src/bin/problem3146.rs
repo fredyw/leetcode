249ed111efd2ddc5686ef7c4e27d405a12d38295
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-05-12 00:52:14 -0500

    Skeleton for probelem 3146

---
+++ src/main/rust/src/bin/problem3146.rs
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/permutation-difference-between-two-strings/
+pub fn find_permutation_difference(s: String, t: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        find_permutation_difference("abc".to_string(), "bac".to_string())
+    ); // 2
+    println!(
+        "{}"
+        find_permutation_difference("abcde".to_string(), "edbac".to_string())
+    ); // 12
+}
