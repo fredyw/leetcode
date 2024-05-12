f80c15778794f1596ea0eba7be05f773523840e0
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-05-12 01:01:08 -0500

    Solve problem 3146

--- src/main/rust/src/bin/problem3146.rs
+++ src/main/rust/src/bin/problem3146.rs
@@ -1,6 +1,16 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/permutation-difference-between-two-strings/
 pub fn find_permutation_difference(s: String, t: String) -> i32 {
-    todo!()
+    let mut map: HashMap<char, usize> = HashMap::new();
+    for (i, char) in t.chars().enumerate() {
+        map.insert(char, i);
+    }
+    let mut answer = 0;
+    for (i, char) in s.chars().enumerate() {
+        answer += (*map.get(&char).unwrap() as i32 - i as i32).abs();
+    }
+    answer
 }
 
 fn main() {
@@ -9,7 +19,7 @@ fn main() {
         find_permutation_difference("abc".to_string(), "bac".to_string())
     ); // 2
     println!(
-        "{}"
+        "{}",
         find_permutation_difference("abcde".to_string(), "edbac".to_string())
     ); // 12
 }
