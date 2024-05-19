a28e993dd2b48f0839b7c045d834422d69107b32
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-05-19 09:04:26 -0500

    Solve problem 3151

--- src/main/rust/src/bin/problem3151.rs
+++ src/main/rust/src/bin/problem3151.rs
@@ -1,6 +1,11 @@
 // https://leetcode.com/problems/special-array-i/description/
 pub fn is_array_special(nums: Vec<i32>) -> bool {
-    todo!()
+    for i in 0..nums.len() - 1 {
+        if nums[i] % 2 == 0 && nums[i + 1] % 2 == 0 || nums[i] % 2 != 0 && nums[i + 1] % 2 != 0 {
+            return false;
+        }
+    }
+    true
 }
 
 fn main() {
