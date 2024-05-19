05e0905a756beefbf11c5a624a3f40bce02344e1
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-05-19 08:55:06 -0500

    Skeleton for problem 3151

---
+++ src/main/rust/src/bin/problem3151.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/special-array-i/description/
+pub fn is_array_special(nums: Vec<i32>) -> bool {
+    todo!()
+}
+
+fn main() {
+    println!("{}", is_array_special(vec![1])); // true
+    println!("{}", is_array_special(vec![2, 1, 4])); // true
+    println!("{}", is_array_special(vec![4, 3, 1, 6])); // false
+}
