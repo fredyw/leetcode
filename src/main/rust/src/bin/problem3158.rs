d9633769a63ba7e5e3b06cd3eb04f89e745a40a7
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-05-25 16:41:04 -0500

    Skeleton for problem 3158

---
+++ src/main/rust/src/bin/problem3158.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/find-the-xor-of-numbers-which-appear-twice/description/
+pub fn duplicate_numbers_xor(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", duplicate_numbers_xor(vec![1, 2, 1, 3])); // 1
+    println!("{}", duplicate_numbers_xor(vec![1, 2, 3])); // 0
+    println!("{}", duplicate_numbers_xor(vec![1, 2, 2, 1])); // 3
+}
