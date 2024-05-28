7939a850deefeef1e45d70babc2408d83b8aa51f
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-05-28 16:50:17 -0500

    Skeleton for problem 3162

---
+++ src/main/rust/src/bin/problem3162.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/find-the-number-of-good-pairs-i/description/
+pub fn number_of_pairs(nums1: Vec<i32>, nums2: Vec<i32>, k: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", number_of_pairs(vec![1, 3, 4], vec![1, 3, 4], 1)); // 5
+    println!("{}", number_of_pairs(vec![1, 2, 4, 12], vec![2, 4], 3)); // 2
+}
