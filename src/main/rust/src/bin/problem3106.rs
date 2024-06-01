fbe7b89de25c3c7993931be86a6f806d6292ea94
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-01 14:22:07 -0500

    Solve problem 3106

--- src/main/rust/src/bin/problem3106.rs
+++ src/main/rust/src/bin/problem3106.rs
@@ -1,6 +1,23 @@
 // https://leetcode.com/problems/lexicographically-smallest-string-after-operations-with-constraint/description/
 pub fn get_smallest_string(s: String, k: i32) -> String {
-    todo!()
+    let mut answer: Vec<char> = s.chars().collect();
+    let mut n = k;
+    for (i, c) in s.chars().enumerate() {
+        if n == 0 {
+            break;
+        }
+        let a = (c as i32 - 'a' as i32).abs();
+        let b = (c as i32 - ('a' as i32 + 26)).abs();
+        let min = a.min(b);
+        if min <= n {
+            answer[i] = 'a';
+            n -= min;
+        } else {
+            answer[i] = (c as u8 - n as u8) as char;
+            n = 0;
+        }
+    }
+    answer.into_iter().collect()
 }
 
 fn main() {
