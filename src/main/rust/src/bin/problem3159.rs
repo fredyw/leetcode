8b156275f6974239387b47799047d506cacd0622
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-01 14:43:22 -0500

    Solve problem 3159

--- src/main/rust/src/bin/problem3159.rs
+++ src/main/rust/src/bin/problem3159.rs
@@ -1,6 +1,20 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/find-occurrences-of-an-element-in-an-array/description/
 pub fn occurrences_of_element(nums: Vec<i32>, queries: Vec<i32>, x: i32) -> Vec<i32> {
-    todo!()
+    let mut map: HashMap<i32, Vec<usize>> = HashMap::new();
+    for (i, num) in nums.into_iter().enumerate() {
+        map.entry(num).or_insert(vec![]).push(i);
+    }
+    let mut answer: Vec<i32> = vec![-1; queries.len()];
+    for (i, query) in queries.iter().enumerate() {
+        if let Some(v) = map.get(&x) {
+            if *query as usize - 1 < v.len() {
+                answer[i] = v[*query as usize - 1] as i32;
+            }
+        }
+    }
+    answer
 }
 
 fn main() {
