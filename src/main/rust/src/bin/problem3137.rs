cc16b741e62f73f30770976fdd5093b2338bd063
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-01 14:55:26 -0500

    Skeleton for problem 3137

---
+++ src/main/rust/src/bin/problem3137.rs
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/minimum-number-of-operations-to-make-word-k-periodic/description/
+pub fn minimum_operations_to_make_k_periodic(word: String, k: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        minimum_operations_to_make_k_periodic("leetcodeleet".to_string(), 4)
+    ); // 1
+    println!(
+        "{}",
+        minimum_operations_to_make_k_periodic("leetcoleet".to_string(), 2)
+    ); // 3
+}
