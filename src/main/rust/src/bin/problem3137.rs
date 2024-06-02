57ebea4a3f1050139ef751cf00bd91052bfa7ab0
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-02 00:18:56 -0500

    Solve problem 3137

--- src/main/rust/src/bin/problem3137.rs
+++ src/main/rust/src/bin/problem3137.rs
@@ -1,6 +1,19 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/minimum-number-of-operations-to-make-word-k-periodic/description/
 pub fn minimum_operations_to_make_k_periodic(word: String, k: i32) -> i32 {
-    todo!()
+    let mut map: HashMap<&str, i32> = HashMap::new();
+    let mut i = 0;
+    while i < word.len() {
+        let sub = &word[i..i + k as usize];
+        *map.entry(sub).or_insert(0) += 1;
+        i += k as usize;
+    }
+    let mut max = 0;
+    for (_, count) in map.iter() {
+        max = max.max(*count);
+    }
+    (word.len() as i32 - (k * max)) / k
 }
 
 fn main() {
@@ -12,4 +25,8 @@ fn main() {
         "{}",
         minimum_operations_to_make_k_periodic("leetcoleet".to_string(), 2)
     ); // 3
+    println!(
+        "{}",
+        minimum_operations_to_make_k_periodic("leetcoleet".to_string(), 2)
+    ); // 2
 }
