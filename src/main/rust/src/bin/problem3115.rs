9126132c85de58f9cf40661a1cbda092d7e9f451
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-03 13:38:58 -0500

    Skeleton for problem 3115

---
+++ src/main/rust/src/bin/problem3115.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/maximum-prime-difference/description/
+pub fn maximum_prime_difference(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", maximum_prime_difference(vec![4, 2, 9, 5, 3])); // 3
+    println!("{}", maximum_prime_difference(vec![4, 8, 2, 8])); // 0
+}
