203b42467f02804018e7b3064d4d3161ea6209d9
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-03 13:37:39 -0500

    Solve problem 3111

--- src/main/rust/src/bin/problem3111.rs
+++ src/main/rust/src/bin/problem3111.rs
@@ -1,6 +1,15 @@
 // https://leetcode.com/problems/minimum-rectangles-to-cover-points/description/
-pub fn min_rectangles_to_cover_points(points: Vec<Vec<i32>>, w: i32) -> i32 {
-    todo!()
+pub fn min_rectangles_to_cover_points(mut points: Vec<Vec<i32>>, w: i32) -> i32 {
+    points.sort_by(|a, b| a[0].cmp(&b[0]));
+    let mut answer = 1;
+    let mut x = points[0][0];
+    for point in points {
+        if x + w < point[0] {
+            x = point[0];
+            answer += 1;
+        }
+    }
+    answer
 }
 
 fn main() {
