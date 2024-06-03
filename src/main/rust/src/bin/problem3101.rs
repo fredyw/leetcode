e9424e90ab1ed88d13b9759c3a3cd32a1f53b004
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-03 14:09:38 -0500

    Skeleton for problem 3101

---
+++ src/main/rust/src/bin/problem3101.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/count-alternating-subarrays/description/
+pub fn count_alternating_subarrays(nums: Vec<i32>) -> i64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", count_alternating_subarrays(vec![0, 1, 1, 1])); // 5
+    println!("{}", count_alternating_subarrays(vec![1, 0, 1, 0])); // 10
+}
