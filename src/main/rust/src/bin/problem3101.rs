eb92d28ffdeb0e4d36673b588a96202fc59bbabb
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-04 13:57:54 -0500

    Solve problem 3101

--- src/main/rust/src/bin/problem3101.rs
+++ src/main/rust/src/bin/problem3101.rs
@@ -1,6 +1,23 @@
 // https://leetcode.com/problems/count-alternating-subarrays/description/
 pub fn count_alternating_subarrays(nums: Vec<i32>) -> i64 {
-    todo!()
+    fn sum(n: i64) -> i64 {
+        (n * (1 + n)) / 2
+    }
+
+    let mut answer: i64 = 0;
+    let mut i = 0;
+    let mut length: i64 = 1;
+    while i < nums.len() - 1 {
+        if nums[i] != nums[i + 1] {
+            length += 1;
+        } else {
+            answer += sum(length);
+            length = 1;
+        }
+        i += 1;
+    }
+    answer += sum(length);
+    answer
 }
 
 fn main() {
