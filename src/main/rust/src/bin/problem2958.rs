239966e9019b529f6c92c6403f8ca6646b89e797
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-05 11:24:04 -0500

    Solve problem 2958

--- src/main/rust/src/bin/problem2958.rs
+++ src/main/rust/src/bin/problem2958.rs
@@ -1,6 +1,28 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/length-of-longest-subarray-with-at-most-k-frequency/description/
 pub fn max_subarray_length(nums: Vec<i32>, k: i32) -> i32 {
-    todo!()
+    let mut answer = 0;
+    let mut map: HashMap<i32, i32> = HashMap::new();
+    let mut i = 0;
+    let mut j = 0;
+    while j < nums.len() {
+        let count = map.get(&nums[j]).unwrap_or(&0);
+        if count + 1 <= k {
+            *map.entry(nums[j]).or_insert(0) += 1;
+            j += 1;
+        } else {
+            while i < j {
+                *map.entry(nums[i]).or_insert(0) -= 1;
+                i += 1;
+                if nums[i - 1] == nums[j] {
+                    break;
+                }
+            }
+        }
+        answer = answer.max(j as i32 - i as i32);
+    }
+    answer
 }
 
 fn main() {
