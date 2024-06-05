197c1babd334c619fd34bb54b935cbeb3757ac64
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-05 11:25:23 -0500

    Skeleton for problem 3163

---
+++ src/main/rust/src/bin/problem3163.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/string-compression-iii/description/
+pub fn compressed_string(word: String) -> String {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", compressed_string("abcde".to_string())); // "1a1b1c1d1e"
+    println!("{:?}", compressed_string("aaaaaaaaaaaaaabb".to_string())); // "9a5a2b"
+}
