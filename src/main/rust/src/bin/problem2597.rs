b0fa70e6e88a7f7997640be0f495f50d518901db
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-05 17:04:37 -0500

    Update problem 2597

--- src/main/rust/src/bin/problem2597.rs
+++ src/main/rust/src/bin/problem2597.rs
@@ -6,4 +6,5 @@ pub fn beautiful_subsets(nums: Vec<i32>, k: i32) -> i32 {
 fn main() {
     println!("{}", beautiful_subsets(vec![2, 4, 6], 2)); // 4
     println!("{}", beautiful_subsets(vec![1], 1)); // 1
+    println!("{}", beautiful_subsets(vec![2, 4, 6, 9], 2)); // 9
 }
