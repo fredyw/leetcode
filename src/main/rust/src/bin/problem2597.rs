5d9650cc4d5b6229bd380442e78aab18a77fd0fb
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-05 14:50:42 -0500

    Skeleton for problem 2597

---
+++ src/main/rust/src/bin/problem2597.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/the-number-of-beautiful-subsets/description/
+pub fn beautiful_subsets(nums: Vec<i32>, k: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", beautiful_subsets(vec![2, 4, 6], 2)); // 4
+    println!("{}", beautiful_subsets(vec![1], 1)); // 1
+}
