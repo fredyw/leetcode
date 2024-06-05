ce515f75894dae508c34581de658ca73782bc306
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-05 11:54:36 -0500

    Skeleton for problem 3075

---
+++ src/main/rust/src/bin/problem3075.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/maximize-happiness-of-selected-children/description/
+pub fn maximum_happiness_sum(happiness: Vec<i32>, k: i32) -> i64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", maximum_happiness_sum(vec![1, 2, 3], 2)); // 4
+    println!("{}", maximum_happiness_sum(vec![1, 1, 1, 1], 2)); // 1
+    println!("{}", maximum_happiness_sum(vec![2, 3, 4, 5], 1)); // 5
+}
