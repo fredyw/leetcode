50ee0a5c26425b43b1ada027d56fa11c89fa0d0b
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-06 15:27:04 -0500

    Update problem 2597

--- src/main/rust/src/bin/problem2597.rs
+++ src/main/rust/src/bin/problem2597.rs
@@ -1,6 +1,22 @@
 // https://leetcode.com/problems/the-number-of-beautiful-subsets/description/
-pub fn beautiful_subsets(nums: Vec<i32>, k: i32) -> i32 {
-    todo!()
+pub fn beautiful_subsets(mut nums: Vec<i32>, k: i32) -> i32 {
+    fn beautiful_subsets(nums: &Vec<i32>, k: i32, i: usize) -> i32 {
+        let mut count = 0;
+        for j in i + 1..nums.len() {
+            if nums[j] - nums[i] == k {
+                continue;
+            }
+            count += beautiful_subsets(nums, k, j) + 1;
+        }
+        count
+    }
+
+    nums.sort();
+    let mut answer = 0;
+    for i in 0..nums.len() {
+        answer += beautiful_subsets(&nums, k, i);
+    }
+    answer + nums.len() as i32
 }
 
 fn main() {
@@ -8,4 +24,5 @@ fn main() {
     println!("{}", beautiful_subsets(vec![1], 1)); // 1
     println!("{}", beautiful_subsets(vec![2, 4, 6, 9], 2)); // 9
     println!("{}", beautiful_subsets(vec![2, 4, 6, 8], 2)); // 7
+    println!("{}", beautiful_subsets(vec![10, 4, 5, 7, 2, 1], 3)); // 23
 }
