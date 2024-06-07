b371841d1c2d01ee73c7d5a5915340bd9bf7acdf
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-06 19:53:04 -0500

    Skeleton for problem 2597

--- src/main/rust/src/bin/problem2597.rs
+++ src/main/rust/src/bin/problem2597.rs
@@ -1,30 +1,34 @@
 // https://leetcode.com/problems/the-number-of-beautiful-subsets/description/
 pub fn beautiful_subsets(mut nums: Vec<i32>, k: i32) -> i32 {
-    fn beautiful_subsets(nums: &Vec<i32>, k: i32, i: usize) -> i32 {
-        println!("[{}]: {}", i, nums[i]);
+    fn beautiful_subsets(nums: &Vec<i32>, k: i32, i: usize, memo: &mut Vec<i32>) -> i32 {
+        if memo[i] != -1 {
+            return memo[i];
+        }
         let mut count = 0;
         for j in i + 1..nums.len() {
-            println!("-- trying {} with {}", nums[i], nums[j]);
             if nums[j] - nums[i] == k {
-                println!("-- nope");
-                count += beautiful_subsets(nums, k, j);
-            } else {
-                println!("-- ok");
-                count += beautiful_subsets(nums, k, j) + 1;
+                continue;
             }
+            count += beautiful_subsets(nums, k, j, memo) + 1;
         }
+        memo[i] = count;
         count
     }
 
     nums.sort();
-    println!("{:?}", nums);
-    beautiful_subsets(&nums, k, 0) + nums.len() as i32
+    let mut answer = 0;
+    let mut memo: Vec<i32> = vec![-1; nums.len()];
+    for i in 0..nums.len() {
+        answer += beautiful_subsets(&nums, k, i, &mut memo) + 1;
+    }
+    answer
 }
 
 fn main() {
-    // println!("{}", beautiful_subsets(vec![2, 4, 6], 2)); // 4
-    // println!("{}", beautiful_subsets(vec![1], 1)); // 1
-    // println!("{}", beautiful_subsets(vec![2, 4, 6, 9], 2)); // 9
-    // println!("{}", beautiful_subsets(vec![2, 4, 6, 8], 2)); // 7
+    println!("{}", beautiful_subsets(vec![2, 4, 6], 2)); // 4
+    println!("{}", beautiful_subsets(vec![1], 1)); // 1
+    println!("{}", beautiful_subsets(vec![2, 4, 6, 9], 2)); // 9
+    println!("{}", beautiful_subsets(vec![2, 4, 6, 8], 2)); // 7
+                                                            // [1, 2, 4, 5, 7, 10]
     println!("{}", beautiful_subsets(vec![10, 4, 5, 7, 2, 1], 3)); // 23
 }
