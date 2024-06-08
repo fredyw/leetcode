3590e951670b6d4d2fd0f763b4e1a4677c0af164
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-08 01:33:53 -0500

    Skeleton for problem 3168

---
+++ src/main/rust/src/bin/problem3168.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/minimum-number-of-chairs-in-a-waiting-room/description/
+pub fn minimum_chairs(s: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", minimum_chairs("EEEEEEE".to_string())); // 7
+    println!("{}", minimum_chairs("ELELEEL".to_string())); // 2
+    println!("{}", minimum_chairs("ELEELEELLL".to_string())); // 3
+}
