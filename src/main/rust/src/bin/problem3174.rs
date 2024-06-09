f07dae8569177b85b667bcb919fe800ba8f50a9f
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-09 17:18:41 -0500

    Skeleton for problem 3174

---
+++ src/main/rust/src/bin/problem3174.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/clear-digits/description/
+pub fn clear_digits(s: String) -> String {
+    todo!()
+}
+
+fn main() {
+    println!("{}", clear_digits("abc".to_string())); // "abc"
+    println!("{}", clear_digits("cb34".to_string())); // ""
+}
