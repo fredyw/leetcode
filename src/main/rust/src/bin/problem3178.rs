32fa72f1a5692c2bb32d9370af5b689c7d8f619a
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-09 17:32:03 -0500

    Skeleton for problem 3178

---
+++ src/main/rust/src/bin/problem3178.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/find-the-child-who-has-the-ball-after-k-seconds/description/
+pub fn number_of_child(n: i32, k: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", number_of_child(3, 5)); // 1
+    println!("{}", number_of_child(5, 6)); // 2
+    println!("{}", number_of_child(4, 2)); // 2
+}
