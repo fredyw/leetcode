8824715d9f9984b8f4271b9b52aedaec5d16b01e
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-09 17:39:09 -0500

    Solve problem 3178

--- src/main/rust/src/bin/problem3178.rs
+++ src/main/rust/src/bin/problem3178.rs
@@ -1,6 +1,21 @@
 // https://leetcode.com/problems/find-the-child-who-has-the-ball-after-k-seconds/description/
-pub fn number_of_child(n: i32, k: i32) -> i32 {
-    todo!()
+pub fn number_of_child(n: i32, mut k: i32) -> i32 {
+    let mut answer = 0;
+    let mut increment = true;
+    while k > 0 {
+        if answer == 0 {
+            increment = true;
+        } else if answer == n - 1 {
+            increment = false;
+        }
+        if increment {
+            answer += 1;
+        } else {
+            answer -= 1;
+        }
+        k -= 1;
+    }
+    answer
 }
 
 fn main() {
