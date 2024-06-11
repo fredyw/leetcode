07bc3f133b671e924d2f9e1b1acc50d160b8458a
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-11 00:48:56 -0500

    Skeleton for problem 3179

---
+++ src/main/rust/src/bin/problem3179.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/find-the-n-th-value-after-k-seconds/
+pub fn value_after_k_seconds(n: i32, k: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", value_after_k_seconds(4, 5)); // 56
+    println!("{}", value_after_k_seconds(5, 3)); // 35
+}
