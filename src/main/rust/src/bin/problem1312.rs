19c3f1735b9e1420d81e7777c6c19100a42c6378
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-12 13:33:31 -0500

    Skeleton for problem 1312

---
+++ src/main/rust/src/bin/problem1312.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/minimum-insertion-steps-to-make-a-string-palindrome/description/
+pub fn min_insertions(s: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", min_insertions("zzazz".to_string())); // 0
+    println!("{}", min_insertions("mbadm".to_string())); // 2
+    println!("{}", min_insertions("leetcode".to_string())); // 5
+}
