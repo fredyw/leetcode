0aa3fb40db29b9a6922b331d7a13cd69d3b19093
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-13 11:25:17 -0500

    Skeleton for problem 408

---
+++ src/main/rust/src/bin/problem408.rs
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/valid-word-abbreviation/description/
+pub fn valid_word_abbreviation(word: String, abbr: String) -> bool {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        valid_word_abbreviation("internationalization".to_string(), "i12iz4n".to_string())
+    ); // true
+    println!(
+        "{}",
+        valid_word_abbreviation("apple".to_string(), "a2e".to_string())
+    ); // false
+}
