643c4468b434c06225d648fbece7be715c604959
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-13 16:43:10 -0500

    Skeleton for problem 314

---
+++ src/main/rust/src/bin/problem314.rs
@@ -0,0 +1,117 @@
+use std::cell::RefCell;
+use std::collections::VecDeque;
+use std::rc::Rc;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+// https://leetcode.com/problems/binary-tree-vertical-order-traversal/description/
+pub fn vertical_order(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<Vec<i32>> {
+    todo!()
+}
+
+fn main() {
+    fn create_node(value: i32) -> Option<Rc<RefCell<TreeNode>>> {
+        Some(Rc::new(RefCell::new(TreeNode::new(value))))
+    }
+
+    fn add_left(
+        parent: &Option<Rc<RefCell<TreeNode>>>,
+        value: i32,
+    ) -> Option<Rc<RefCell<TreeNode>>> {
+        parent.as_ref().unwrap().as_ref().borrow_mut().left = create_node(value);
+        parent.as_ref().unwrap().as_ref().borrow().left.clone()
+    }
+
+    fn add_right(
+        parent: &Option<Rc<RefCell<TreeNode>>>,
+        value: i32,
+    ) -> Option<Rc<RefCell<TreeNode>>> {
+        parent.as_ref().unwrap().as_ref().borrow_mut().right = create_node(value);
+        parent.as_ref().unwrap().as_ref().borrow().right.clone()
+    }
+
+    fn to_tree(nodes: Vec<Option<i32>>) -> Option<Rc<RefCell<TreeNode>>> {
+        let mut deque: VecDeque<Option<Rc<RefCell<TreeNode>>>> = VecDeque::new();
+        let root = create_node(nodes[0].unwrap());
+        deque.push_back(root.clone());
+        let mut i = 1;
+        while i < nodes.len() {
+            let parent = deque.pop_front().unwrap();
+            let mut has_left = false;
+            if let Some(left) = nodes[i] {
+                deque.push_back(add_left(&parent, left));
+                has_left = true;
+            }
+            let mut has_right = false;
+            if i + 1 < nodes.len() {
+                if let Some(right) = nodes[i + 1] {
+                    deque.push_back(add_right(&parent, right));
+                    has_right = true;
+                }
+            }
+            if !has_left && !has_right {
+                deque.push_back(parent);
+            }
+            i += 2;
+        }
+        root
+    }
+
+    println!(
+        "{:?}",
+        vertical_order(to_tree(vec![
+            Some(3),
+            Some(9),
+            Some(20),
+            None,
+            None,
+            Some(15),
+            Some(7),
+        ]))
+    ); // [[9],[3,15],[20],[7]]
+    println!(
+        "{:?}",
+        vertical_order(to_tree(vec![
+            Some(3),
+            Some(9),
+            Some(8),
+            Some(4),
+            Some(0),
+            Some(1),
+            Some(7),
+        ]))
+    ); // [[4],[9],[3,0,1],[8],[7]]
+    println!(
+        "{:?}",
+        vertical_order(to_tree(vec![
+            Some(3),
+            Some(9),
+            Some(8),
+            Some(4),
+            Some(0),
+            Some(1),
+            Some(7),
+            None,
+            None,
+            None,
+            Some(2),
+            Some(5),
+        ]))
+    ); // [[4],[9,5],[3,0,1],[8,2],[7]]
+}
