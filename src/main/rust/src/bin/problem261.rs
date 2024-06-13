2c9a5cd5bc058d9d2fddc0e2d5d0a4a15e1389a0
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-13 11:19:03 -0500

    Solve problem 261

--- src/main/rust/src/bin/problem261.rs
+++ src/main/rust/src/bin/problem261.rs
@@ -1,14 +1,38 @@
 // https://leetcode.com/problems/graph-valid-tree/description/
 pub fn valid_tree(n: i32, edges: Vec<Vec<i32>>) -> bool {
-    fn has_cycle(edges: &Vec<Vec<i32>>) -> bool {
-        todo!()
+    fn build_graph(edges: &Vec<Vec<i32>>, n: i32) -> Vec<Vec<i32>> {
+        let mut adj_list = vec![vec![]; n as usize];
+        for edge in edges.iter() {
+            adj_list[edge[0] as usize].push(edge[1]);
+            adj_list[edge[1] as usize].push(edge[0]);
+        }
+        adj_list
     }
 
-    fn connected(edges: &Vec<Vec<i32>>) -> bool {
-        todo!()
+    fn has_cycle(graph: &Vec<Vec<i32>>, node: i32, parent: i32, visited: &mut Vec<bool>) -> bool {
+        visited[node as usize] = true;
+        for adjacent in graph[node as usize].iter() {
+            if !visited[*adjacent as usize] {
+                if has_cycle(graph, *adjacent, node, visited) {
+                    return true;
+                }
+            } else {
+                // Has cycle.
+                if *adjacent != parent {
+                    return true;
+                }
+            }
+        }
+        false
     }
 
-    !has_cycle(&edges) && connected(&edges)
+    let graph = build_graph(&edges, n);
+    let mut visited = vec![false; n as usize];
+    if has_cycle(&graph, 0, 0, &mut visited) {
+        return false;
+    }
+    // Check if all the nodes are connected.
+    visited.iter().all(|b| *b)
 }
 
 fn main() {
