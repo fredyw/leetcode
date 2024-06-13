44ca51e9c1864e04a1a76660fe6dd973a679d536
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-13 00:44:18 -0500

    Solve problem 13312

--- src/main/rust/src/bin/problem1312.rs
+++ src/main/rust/src/bin/problem1312.rs
@@ -1,19 +1,31 @@
 // https://leetcode.com/problems/minimum-insertion-steps-to-make-a-string-palindrome/description/
 pub fn min_insertions(s: String) -> i32 {
-    let mut answer = 0;
-    let bytes = s.as_bytes();
-    let mut i = 0;
-    let mut j = s.len() - 1;
-    while i < j {
-        if bytes[i] != bytes[j] {
-            i += 1;
-            answer += 1;
-        } else {
-            i += 1;
-            j -= 1;
+    fn min_insertions(chars: &Vec<char>, i: usize, j: usize, memo: &mut Vec<Vec<i32>>) -> i32 {
+        if i >= j {
+            return 0;
         }
+        if memo[i][j] != -1 {
+            return memo[i][j];
+        }
+        let mut min = i32::MAX;
+        if chars[i] == chars[j] {
+            min = min.min(min_insertions(chars, i + 1, j - 1, memo));
+        } else {
+            let left = min_insertions(chars, i + 1, j, memo) + 1;
+            let right = min_insertions(chars, i, j - 1, memo) + 1;
+            min = min.min(left.min(right));
+        }
+        memo[i][j] = min;
+        min
     }
-    answer
+
+    let chars: Vec<char> = s.chars().collect();
+    min_insertions(
+        &chars,
+        0,
+        chars.len() - 1,
+        &mut vec![vec![-1; chars.len()]; chars.len()],
+    )
 }
 
 fn main() {
