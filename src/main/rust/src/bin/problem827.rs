98c323ba830cd7fccac0518c12e3e7c1f4cce815
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-14 11:22:23 -0500

    Skeleton for problem 827

---
+++ src/main/rust/src/bin/problem827.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/making-a-large-island/description/
+pub fn largest_island(grid: Vec<Vec<i32>>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", largest_island(vec![vec![1, 0], vec![0, 1]])); // 3
+    println!("{}", largest_island(vec![vec![1, 0], vec![0, 1]])); // 4
+    println!("{}", largest_island(vec![vec![1, 0], vec![0, 1]])); // 4
+}
