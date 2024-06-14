81dbada61b7a74016f65fe68f47a916641d53adf
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-14 11:45:50 -0500

    Update problem 827

--- src/main/rust/src/bin/problem827.rs
+++ src/main/rust/src/bin/problem827.rs
@@ -5,6 +5,6 @@ pub fn largest_island(grid: Vec<Vec<i32>>) -> i32 {
 
 fn main() {
     println!("{}", largest_island(vec![vec![1, 0], vec![0, 1]])); // 3
-    println!("{}", largest_island(vec![vec![1, 0], vec![0, 1]])); // 4
-    println!("{}", largest_island(vec![vec![1, 0], vec![0, 1]])); // 4
+    println!("{}", largest_island(vec![vec![1, 1], vec![1, 0]])); // 4
+    println!("{}", largest_island(vec![vec![1, 1], vec![1, 1]])); // 4
 }
