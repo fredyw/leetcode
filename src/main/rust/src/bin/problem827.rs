d6b54ce497ba23d381c9dc17763dcafe3402061e
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-15 01:40:49 -0500

    Update probem 827

--- src/main/rust/src/bin/problem827.rs
+++ src/main/rust/src/bin/problem827.rs
@@ -4,32 +4,27 @@ pub fn largest_island(grid: Vec<Vec<i32>>) -> i32 {
         grid: &Vec<Vec<i32>>,
         row: i32,
         col: i32,
-        change: bool,
+        mut convert: bool,
         visited: &mut Vec<Vec<bool>>,
     ) -> i32 {
         let n = grid.len() as i32;
         let mut max = 0;
+        if row < 0 || col < 0 || row == n || col == n {
+            return 0;
+        }
+        if visited[row as usize][col as usize] {
+            return 0;
+        }
         visited[row as usize][col as usize] = true;
+        if grid[row as usize][col as usize] == 0 {
+            if convert {
+                return 0;
+            }
+            convert = true;
+        }
+        let island = 1;
         for (r, c) in [(0, 1), (0, -1), (1, 0), (-1, 0)] {
-            let next_row = row + r;
-            let next_col = col + c;
-            if next_row < 0
-                || next_col < 0
-                || next_row == n
-                || next_col == n
-                || visited[next_row as usize][next_col as usize]
-            {
-                continue;
-            }
-            if grid[next_row as usize][next_col as usize] == 0 {
-                if !change {
-                    let update = largest_island(grid, next_row, next_col, !change, visited) + 1;
-                    let skip = largest_island(grid, next_row, next_col, change, visited);
-                    max = max.max(update.max(skip));
-                }
-            } else {
-                max = max.max(largest_island(grid, next_row, next_col, change, visited) + 1);
-            }
+            max = max.max(largest_island(grid, row + r, col + c, convert, visited) + island);
         }
         max
     }
@@ -39,14 +34,13 @@ pub fn largest_island(grid: Vec<Vec<i32>>) -> i32 {
     for row in 0..n {
         for col in 0..n {
             let mut visited = vec![vec![false; n]; n];
-            if grid[row][col] == 0 {
-                let update = largest_island(&grid, row as i32, col as i32, true, &mut visited) + 1;
-                let skip = largest_island(&grid, row as i32, col as i32, false, &mut visited);
-                answer = answer.max(update.max(skip));
-            } else {
-                answer = answer
-                    .max(largest_island(&grid, row as i32, col as i32, false, &mut visited) + 1);
-            }
+            answer = answer.max(largest_island(
+                &grid,
+                row as i32,
+                col as i32,
+                false,
+                &mut visited,
+            ));
         }
     }
     answer
