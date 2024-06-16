6339bc5148c39346288440212dfc94086b56b3da
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-16 00:41:51 -0500

    Solve problem 266

--- src/main/rust/src/bin/problem266.rs
+++ src/main/rust/src/bin/problem266.rs
@@ -1,6 +1,18 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/palindrome-permutation/description/
 pub fn can_permute_palindrome(s: String) -> bool {
-    todo!()
+    let mut map: HashMap<char, i32> = HashMap::new();
+    for char in s.chars() {
+        *map.entry(char).or_insert(0) += 1;
+    }
+    let mut num_odd = 0;
+    for (_, count) in map.into_iter() {
+        if count % 2 != 0 {
+            num_odd += 1;
+        }
+    }
+    num_odd <= 1
 }
 
 fn main() {
