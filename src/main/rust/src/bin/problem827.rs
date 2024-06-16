e9c5600a0366fd3ade88ea01f13391ea17706437
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-16 00:09:37 -0500

    Solve problem 827

--- src/main/rust/src/bin/problem827.rs
+++ src/main/rust/src/bin/problem827.rs
@@ -1,5 +1,5 @@
 use std::cell::RefCell;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::rc::Rc;
 
 // https://leetcode.com/problems/making-a-large-island/description/
@@ -9,8 +9,8 @@ pub fn largest_island(grid: Vec<Vec<i32>>) -> i32 {
         row: i32,
         col: i32,
         visited: &mut Vec<Vec<bool>>,
-        island: &mut HashMap<(i32, i32), Rc<RefCell<i32>>>,
-        rc_size: Rc<RefCell<i32>>,
+        island: &mut HashMap<(i32, i32), Rc<RefCell<(i32, i32)>>>,
+        rc_size: Rc<RefCell<(i32, i32)>>,
     ) -> i32 {
         let n = grid.len() as i32;
         if row < 0 || col < 0 || row == n || col == n {
@@ -34,10 +34,14 @@ pub fn largest_island(grid: Vec<Vec<i32>>) -> i32 {
     let mut answer = 0;
     let n = grid.len();
     let mut visited = vec![vec![false; n]; n];
-    let mut island: HashMap<(i32, i32), Rc<RefCell<i32>>> = HashMap::new();
+    let mut island: HashMap<(i32, i32), Rc<RefCell<(i32, i32)>>> = HashMap::new();
+    let mut i = 0;
     for row in 0..n {
         for col in 0..n {
-            let mut rc_size = Rc::new(RefCell::new(0));
+            if visited[row][col] {
+                continue;
+            }
+            let rc_size = Rc::new(RefCell::new((0, i)));
             let size = island_size(
                 &grid,
                 row as i32,
@@ -46,8 +50,9 @@ pub fn largest_island(grid: Vec<Vec<i32>>) -> i32 {
                 &mut island,
                 rc_size.clone(),
             );
-            *rc_size.borrow_mut() = size;
+            *rc_size.borrow_mut() = (size, i);
             answer = answer.max(size);
+            i += 1;
         }
     }
     for row in 0..n as i32 {
@@ -55,11 +60,17 @@ pub fn largest_island(grid: Vec<Vec<i32>>) -> i32 {
             // Attempt to make an island.
             if grid[row as usize][col as usize] == 0 {
                 let mut total_size = 0;
+                let mut visited: HashSet<i32> = HashSet::new();
                 for (r, c) in [(0, 1), (0, -1), (1, 0), (-1, 0)] {
                     let next_row = row + r;
                     let next_col = col + c;
                     if let Some(size) = island.get(&(next_row, next_col)) {
-                        total_size += *size.borrow();
+                        let (size, i) = *size.borrow();
+                        if visited.contains(&i) {
+                            continue;
+                        }
+                        total_size += size;
+                        visited.insert(i);
                     }
                 }
                 answer = answer.max(total_size + 1);
