90e20d9e50f718ae4b0c10b8180f8484979bdcc3
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-16 01:03:46 -0500

    Skeleton for problem 1762

---
+++ src/main/rust/src/bin/problem1762.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/buildings-with-an-ocean-view/description/
+pub fn find_buildings(heights: Vec<i32>) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", find_buildings(vec![4, 2, 3, 1])); // [0,2,3]
+    println!("{:?}", find_buildings(vec![4, 3, 2, 1])); // [0,1,2,3]
+    println!("{:?}", find_buildings(vec![1, 3, 2, 4])); // [3]
+}
