21b521d5c85d0b502ce1bdc3fb2b08bfb0ed9db2
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-16 00:38:14 -0500

    Skeleton for problem 266

---
+++ src/main/rust/src/bin/problem266.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/palindrome-permutation/description/
+pub fn can_permute_palindrome(s: String) -> bool {
+    todo!()
+}
+
+fn main() {
+    println!("{}", can_permute_palindrome("code".to_string())); // false
+    println!("{}", can_permute_palindrome("aab".to_string())); // true
+    println!("{}", can_permute_palindrome("carerac".to_string())); // true
+}
