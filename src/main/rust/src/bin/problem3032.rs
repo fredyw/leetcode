c6d57efa1da6bbf06e2b08c19958f0c8457182cc
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-17 00:01:45 -0500

    Solve problem 3032

--- src/main/rust/src/bin/problem3032.rs
+++ src/main/rust/src/bin/problem3032.rs
@@ -1,6 +1,21 @@
 // https://leetcode.com/problems/count-numbers-with-unique-digits-ii/description/
 pub fn number_count(a: i32, b: i32) -> i32 {
-    todo!()
+    let mut answer = 0;
+    for i in a..=b {
+        let mut nums = vec![false; 10];
+        let mut has_duplicate = false;
+        for c in format!("{i}").chars() {
+            if nums[c as usize - '0' as usize] {
+                has_duplicate = true;
+                break;
+            }
+            nums[c as usize - '0' as usize] = true;
+        }
+        if !has_duplicate {
+            answer += 1;
+        }
+    }
+    answer
 }
 
 fn main() {
