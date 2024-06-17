7fbadf7f9c6c3f91337d750baa0fe304a5095c93
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-16 23:53:28 -0500

    Solve problem 1165

--- src/main/rust/src/bin/problem1165.rs
+++ src/main/rust/src/bin/problem1165.rs
@@ -1,6 +1,25 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/single-row-keyboard/description/
 pub fn calculate_time(keyboard: String, word: String) -> i32 {
-    todo!()
+    let keyboard = keyboard.as_bytes();
+    let mut map: HashMap<&u8, usize> = HashMap::new();
+    for (i, b) in keyboard.iter().enumerate() {
+        map.insert(b, i);
+    }
+    let mut answer: i32 = 0;
+    let mut first = true;
+    let mut prev: &u8 = &0;
+    for b in word.as_bytes() {
+        if first {
+            first = false;
+            answer += *map.get(b).unwrap() as i32;
+        } else {
+            answer += (*map.get(&prev).unwrap() as i32 - *map.get(b).unwrap() as i32).abs();
+        }
+        prev = b;
+    }
+    answer
 }
 
 fn main() {
