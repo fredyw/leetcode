d1b908b24afe49a6ff2224f5bf977abe655fe263
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-17 16:52:02 -0500

    Update problem 1762

--- src/main/rust/src/bin/problem1762.rs
+++ src/main/rust/src/bin/problem1762.rs
@@ -1,6 +1,15 @@
 // https://leetcode.com/problems/buildings-with-an-ocean-view/description/
 pub fn find_buildings(heights: Vec<i32>) -> Vec<i32> {
-    todo!()
+    let mut answer = vec![];
+    let mut i = 0;
+    while i < heights.len() {
+        let mut j = i + 1;
+        while j < heights.len() && heights[i] >= heights[j] {
+            j += 1;
+        }
+        i = j;
+    }
+    answer
 }
 
 fn main() {
@@ -8,4 +17,5 @@ fn main() {
     println!("{:?}", find_buildings(vec![4, 3, 2, 1])); // [0,1,2,3]
     println!("{:?}", find_buildings(vec![1, 3, 2, 4])); // [3]
     println!("{:?}", find_buildings(vec![1, 3, 2, 4, 3, 2, 3])); // [3,6]
+    println!("{:?}", find_buildings(vec![1, 3, 2, 4, 3, 2, 1])); // [3,4,5,6]
 }
