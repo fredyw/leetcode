08facbf2802586ee62f95dce29ffb489049cf1f6
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-16 23:37:35 -0500

    Skeleton for problem 3173

---
+++ src/main/rust/src/bin/problem3173.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/bitwise-or-of-adjacent-elements/description/
+pub fn or_array(nums: Vec<i32>) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", or_array(vec![1, 3, 7, 15])); // [3,7,15]
+    println!("{:?}", or_array(vec![8, 4, 2])); // [12,6]
+    println!("{:?}", or_array(vec![5, 4, 9, 11])); // [5,13,11]
+}
