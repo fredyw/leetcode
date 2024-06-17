09d0fdd34a0d77724ef894153606dc20991aeff2
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-16 23:55:03 -0500

    Skeleton for problem 3032

---
+++ src/main/rust/src/bin/problem3032.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/count-numbers-with-unique-digits-ii/description/
+pub fn number_count(a: i32, b: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", number_count(1, 20)); // 19
+    println!("{}", number_count(9, 19)); // 10
+    println!("{}", number_count(80, 120)); // 27
+}
