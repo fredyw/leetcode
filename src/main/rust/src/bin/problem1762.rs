846072a78916d8aba0a8524ddc6cbc8d9b28d6e7
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-17 16:25:49 -0500

    Update problem 1762

--- src/main/rust/src/bin/problem1762.rs
+++ src/main/rust/src/bin/problem1762.rs
@@ -7,4 +7,5 @@ fn main() {
     println!("{:?}", find_buildings(vec![4, 2, 3, 1])); // [0,2,3]
     println!("{:?}", find_buildings(vec![4, 3, 2, 1])); // [0,1,2,3]
     println!("{:?}", find_buildings(vec![1, 3, 2, 4])); // [3]
+    println!("{:?}", find_buildings(vec![1, 3, 2, 4, 3, 2, 3])); // [3,6]
 }
