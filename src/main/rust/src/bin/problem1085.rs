e82732e9783f1f93d1b210e3fc38a17922525a1f
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-18 00:20:39 -0500

    Solve problem 1085

---
+++ src/main/rust/src/bin/problem1085.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/sum-of-digits-in-the-minimum-number/description/
+pub fn sum_of_digits(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", sum_of_digits(vec![34, 23, 1, 24, 75, 33, 54, 8])); // 0
+    println!("{}", sum_of_digits(vec![99, 77, 33, 66, 55])); // 1
+}
