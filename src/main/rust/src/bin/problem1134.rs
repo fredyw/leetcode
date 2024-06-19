39aa08d308b9681ce292e5243a348228492ea397
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-19 01:12:50 -0500

    Skeleton for problem 1134

---
+++ src/main/rust/src/bin/problem1134.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/armstrong-number/description/
+pub fn is_armstrong(n: i32) -> bool {
+    todo!()
+}
+
+fn main() {
+    println!("{}", is_armstrong(153)); // true
+    println!("{}", is_armstrong(123)); // false
+}
