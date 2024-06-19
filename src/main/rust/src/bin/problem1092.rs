97b92852d41a70cc91eb41abccff9a1fecf2db40
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-19 16:22:57 -0500

    Skeleton for problem 1092

---
+++ src/main/rust/src/bin/problem1092.rs
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/shortest-common-supersequence/description/
+pub fn shortest_common_supersequence(str1: String, str2: String) -> String {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        shortest_common_supersequence("abac".to_string(), "cab".to_string())
+    ); // "cabac"
+    println!(
+        "{}",
+        shortest_common_supersequence("aaaaaaaa".to_string(), "aaaaaaaa".to_string())
+    ); // "aaaaaaaa"
+}
