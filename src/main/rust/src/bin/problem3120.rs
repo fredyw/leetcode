f20476e1c2506cac1ce086f83a90956643fa9792
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-19 16:24:12 -0500

    Solve problem 3120

--- src/main/rust/src/bin/problem3120.rs
+++ src/main/rust/src/bin/problem3120.rs
@@ -1,6 +1,23 @@
+use std::collections::HashSet;
+
 //https://leetcode.com/problems/count-the-number-of-special-characters-i/description/
 pub fn number_of_special_chars(word: String) -> i32 {
-    todo!()
+    let mut uppercase_chars: HashSet<char> = HashSet::new();
+    let mut lowercase_chars: HashSet<char> = HashSet::new();
+    for char in word.chars() {
+        if char.is_ascii_uppercase() {
+            uppercase_chars.insert(char);
+        } else {
+            lowercase_chars.insert(char);
+        }
+    }
+    let mut answer = 0;
+    for char in uppercase_chars.iter() {
+        if lowercase_chars.contains(&char.to_lowercase().next().unwrap()) {
+            answer += 1;
+        }
+    }
+    answer
 }
 
 fn main() {
