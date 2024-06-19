d2157cfea3ceca540826bb32aa2a0646ce82c73c
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-19 01:16:59 -0500

    Solve problem 1134

--- src/main/rust/src/bin/problem1134.rs
+++ src/main/rust/src/bin/problem1134.rs
@@ -1,9 +1,14 @@
 // https://leetcode.com/problems/armstrong-number/description/
 pub fn is_armstrong(n: i32) -> bool {
-    todo!()
+    let s = format!("{n}");
+    s.chars()
+        .map(|c| (c as i32 - '0' as i32).pow(s.len() as u32))
+        .sum::<i32>()
+        == n
 }
 
 fn main() {
     println!("{}", is_armstrong(153)); // true
     println!("{}", is_armstrong(123)); // false
+    println!("{}", is_armstrong(2)); // true
 }
