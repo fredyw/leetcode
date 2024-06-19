50ace090f1dbe8c00595ca79313da1f8a63a1fd0
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-19 17:54:27 -0500

    Skeleton for problem 1060

---
+++ src/main/rust/src/bin/problem1060.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/missing-element-in-sorted-array/description/
+pub fn missing_element(nums: Vec<i32>, k: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", missing_element(vec![4, 7, 9, 10], 1)); // 5
+    println!("{}", missing_element(vec![4, 7, 9, 10], 3)); // 8
+    println!("{}", missing_element(vec![1, 2, 4], 3)); // 6
+}
