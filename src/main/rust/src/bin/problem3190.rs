0c25ec18d90cd9f1dd21860e666981977c85d667
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-22 16:10:04 -0500

    Skeleton for problem 3190

---
+++ src/main/rust/src/bin/problem3190.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/find-minimum-operations-to-make-all-elements-divisible-by-three/description/
+pub fn minimum_operations(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", minimum_operations(vec![1, 2, 3, 4])); // 3
+    println!("{}", minimum_operations(vec![3, 6, 9])); // 0
+}
