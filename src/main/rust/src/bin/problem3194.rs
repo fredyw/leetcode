fc2c2673c8a903fe677f25d90d726a11298dccbe
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-23 11:20:23 -0500

    Update problem 3194

--- src/main/rust/src/bin/problem3194.rs
+++ src/main/rust/src/bin/problem3194.rs
@@ -1,6 +1,15 @@
 // https://leetcode.com/problems/minimum-average-of-smallest-and-largest-elements/description/
-pub fn minimum_average(nums: Vec<i32>) -> f64 {
-    todo!()
+pub fn minimum_average(mut nums: Vec<i32>) -> f64 {
+    let mut answer = f64::MAX;
+    nums.sort();
+    let mut i = 0;
+    let mut j = nums.len() - 1;
+    while i < j {
+        answer = answer.min((nums[i] as f64 + nums[j] as f64) / 2.0);
+        i += 1;
+        j -= 1;
+    }
+    answer
 }
 
 fn main() {
