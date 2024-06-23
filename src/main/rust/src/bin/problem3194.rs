bc95d3c9962494d50f185cecd71b15cb618f2826
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-23 11:15:20 -0500

    Skeleton for problem 3194

---
+++ src/main/rust/src/bin/problem3194.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/minimum-average-of-smallest-and-largest-elements/description/
+pub fn minimum_average(nums: Vec<i32>) -> f64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", minimum_average(vec![7, 8, 3, 4, 15, 13, 4, 1])); // 5.5
+    println!("{}", minimum_average(vec![1, 9, 8, 3, 10, 5])); // 5.5
+    println!("{}", minimum_average(vec![1, 2, 3, 7, 8, 9])); // 5.0
+}
