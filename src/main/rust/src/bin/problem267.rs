4aebc1fef3971e3e4b383f8e9fa93fa0c84ca314
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-25 17:12:28 -0500

    Skeleton for problem 267

---
+++ src/main/rust/src/bin/problem267.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/palindrome-permutation-ii/description/
+pub fn generate_palindromes(s: String) -> Vec<String> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", generate_palindromes("aabb".to_string())); // ["abba","baab"]
+    println!("{:?}", generate_palindromes("abc".to_string())); // []
+}
