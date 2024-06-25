60f1aa5fdde7e17b06435d0d39aeb2fb17d58a75
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-25 00:28:27 -0500

    Solve problem 3062

--- src/main/rust/src/bin/problem3062.rs
+++ src/main/rust/src/bin/problem3062.rs
@@ -13,7 +13,35 @@ impl ListNode {
 
 // https://leetcode.com/problems/winner-of-the-linked-list-game/description/
 pub fn game_result(head: Option<Box<ListNode>>) -> String {
-    todo!()
+    fn to_vec(node: Option<Box<ListNode>>) -> Vec<i32> {
+        let mut node = node;
+        let mut v: Vec<i32> = vec![];
+        while let Some(n) = node {
+            v.push(n.val);
+            node = n.next
+        }
+        v
+    }
+
+    let mut odd_win = 0;
+    let mut even_win = 0;
+    let vec: Vec<i32> = to_vec(head);
+    let mut i = 0;
+    while i < vec.len() - 1 {
+        if vec[i] < vec[i + 1] {
+            odd_win += 1;
+        } else if vec[i] > vec[i + 1] {
+            even_win += 1;
+        }
+        i += 2;
+    }
+    if even_win > odd_win {
+        "Even".to_string()
+    } else if even_win < odd_win {
+        "Odd".to_string()
+    } else {
+        "Tie".to_string()
+    }
 }
 
 fn to_linked_list(v: Vec<i32>) -> Option<Box<ListNode>> {
