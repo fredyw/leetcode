420856299e90c7d1851a2e4e233215d49fc8e71d
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-27 00:25:47 -0500

    Skeleton for problem 359

---
+++ src/main/rust/src/bin/problem359.rs
@@ -0,0 +1,13 @@
+struct Logger {}
+
+impl Logger {
+    fn new() -> Self {
+        todo!()
+    }
+
+    fn should_print_message(&self, timestamp: i32, message: String) -> bool {
+        todo!()
+    }
+}
+
+fn main() {}
