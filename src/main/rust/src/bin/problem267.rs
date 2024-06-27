6ce0137aab18ece183868547d42273de48d094be
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-27 00:53:29 -0500

    Update problem 267

--- src/main/rust/src/bin/problem267.rs
+++ src/main/rust/src/bin/problem267.rs
@@ -6,4 +6,7 @@ pub fn generate_palindromes(s: String) -> Vec<String> {
 fn main() {
     println!("{:?}", generate_palindromes("aabb".to_string())); // ["abba","baab"]
     println!("{:?}", generate_palindromes("abc".to_string())); // []
+    println!("{:?}", generate_palindromes("aaabbb".to_string())); // []
+    println!("{:?}", generate_palindromes("aabbc".to_string())); // ["abcba","bacab"]
+    println!("{:?}", generate_palindromes("aaaabbbb".to_string())); // ["aabbbbaa","ababbaba","abbaabba","baabbaab","babaabab","bbaaaabb"]
 }
