f69e562541a96d76692f70a02f2ffb12f2d5e715
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-27 00:09:30 -0500

    Skeleton for problem 1180

---
+++ src/main/rust/src/bin/problem1180.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/count-substrings-with-only-one-distinct-letter/description/
+pub fn count_letters(s: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", count_letters("aaaba".to_string())); // 8
+    println!("{}", count_letters("aaaaaaaaaa".to_string())); // 5
+}
