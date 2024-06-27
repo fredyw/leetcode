3aa71f420d2c1c6e19f7cf972111d01f32223261
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-27 00:24:11 -0500

    Solve problem 1180

--- src/main/rust/src/bin/problem1180.rs
+++ src/main/rust/src/bin/problem1180.rs
@@ -1,9 +1,21 @@
 // https://leetcode.com/problems/count-substrings-with-only-one-distinct-letter/description/
 pub fn count_letters(s: String) -> i32 {
-    todo!()
+    let mut answer = 0;
+    let bytes = s.as_bytes();
+    let mut i = 0;
+    while i < bytes.len() {
+        let mut j = i + 1;
+        while j < bytes.len() && bytes[i] == bytes[j] {
+            j += 1;
+        }
+        answer += (1..=(j - i) as i32).into_iter().sum::<i32>();
+        i = j;
+    }
+    answer
 }
 
 fn main() {
     println!("{}", count_letters("aaaba".to_string())); // 8
     println!("{}", count_letters("aaaaaaaaaa".to_string())); // 55
+    println!("{}", count_letters("aaaa".to_string())); // 10
 }
