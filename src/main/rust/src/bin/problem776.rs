2ed707c1f7cf54b10973f519de1c63c86ffb49ba
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-28 18:50:24 -0500

    Skeleton for problem 776

---
+++ src/main/rust/src/bin/problem776.rs
@@ -0,0 +1,30 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+// https://leetcode.com/problems/split-bst/
+pub fn split_bst(
+    root: Option<Rc<RefCell<TreeNode>>>,
+    target: i32,
+) -> Vec<Option<Rc<RefCell<TreeNode>>>> {
+    todo!()
+}
+
+fn main() {}
