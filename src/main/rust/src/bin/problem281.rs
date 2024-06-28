ae88bb813ecf32b149cdf0329fe92c12c5545a02
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-28 18:47:55 -0500

    Solve problem 281

--- src/main/rust/src/bin/problem281.rs
+++ src/main/rust/src/bin/problem281.rs
@@ -1,24 +1,49 @@
 // https://leetcode.com/problems/zigzag-iterator/description/
-struct ZigzagIterator {}
+struct ZigzagIterator {
+    v1: Vec<i32>,
+    v2: Vec<i32>,
+    i1: usize,
+    i2: usize,
+    first: bool,
+}
 
 impl ZigzagIterator {
-    /** initialize your data structure here. */
     fn new(v1: Vec<i32>, v2: Vec<i32>) -> Self {
-        ZigzagIterator {}
+        ZigzagIterator {
+            v1,
+            v2,
+            i1: 0,
+            i2: 0,
+            first: true,
+        }
     }
 
     fn next(&mut self) -> i32 {
-        todo!()
+        let val = if self.first && self.i1 < self.v1.len() {
+            let v = self.v1[self.i1];
+            self.i1 += 1;
+            v
+        } else {
+            let v = self.v2[self.i2];
+            self.i2 += 1;
+            v
+        };
+        if self.first && self.i2 < self.v2.len() {
+            self.first = false;
+        } else if !self.first && self.i1 < self.v1.len() {
+            self.first = true;
+        }
+        val
     }
 
     fn has_next(&self) -> bool {
-        todo!()
+        self.i1 < self.v1.len() || self.i2 < self.v2.len()
     }
 }
 
 fn to_vec(mut iterator: ZigzagIterator) -> Vec<i32> {
     let mut v = vec![];
-    while (iterator.has_next()) {
+    while iterator.has_next() {
         v.push(iterator.next());
     }
     v
@@ -33,4 +58,10 @@ fn main() {
 
     let iter = ZigzagIterator::new(vec![], vec![1]);
     println!("{:?}", to_vec(iter)); // [1]
+
+    let iter = ZigzagIterator::new(vec![3, 4, 5, 6], vec![1, 2]);
+    println!("{:?}", to_vec(iter)); // [3,1,4,2,5,6]
+
+    let iter = ZigzagIterator::new(vec![], vec![]);
+    println!("{:?}", to_vec(iter)); // []
 }
