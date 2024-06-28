c1394c1349ac5cacead6436a877aa7695be757e3
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-28 18:24:38 -0500

    Solve problem 293

--- src/main/rust/src/bin/problem293.rs
+++ src/main/rust/src/bin/problem293.rs
@@ -1,6 +1,18 @@
 // https://leetcode.com/problems/flip-game/description/
 pub fn generate_possible_next_moves(current_state: String) -> Vec<String> {
-    todo!()
+    let mut answer = vec![];
+    let bytes = current_state.as_bytes();
+    let mut i = 0;
+    while i + 1 < bytes.len() {
+        if bytes[i] == '+' as u8 && bytes[i + 1] == '+' as u8 {
+            let mut chars: Vec<char> = current_state.chars().collect();
+            chars[i] = '-';
+            chars[i + 1] = '-';
+            answer.push(chars.into_iter().collect());
+        }
+        i += 1;
+    }
+    answer
 }
 
 fn main() {
