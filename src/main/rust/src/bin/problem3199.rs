8081632762e8c7c932b790a3669466531fa001c7
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-28 00:21:57 -0500

    Solve problem 3199

---
+++ src/main/rust/src/bin/problem3199.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/count-triplets-with-even-xor-set-bits-i/description/
+pub fn triplet_count(a: Vec<i32>, b: Vec<i32>, c: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", triplet_count(vec![1], vec![2], vec![3])); // 1
+    println!("{}", triplet_count(vec![1, 1], vec![2, 3], vec![1, 5])); // 1
+}
