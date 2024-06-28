8703f6b720f5e11855379bc6914a196f66fcfebd
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-28 15:25:39 -0500

    Skeleton for problem 293

---
+++ src/main/rust/src/bin/problem293.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/flip-game/description/
+pub fn generate_possible_next_moves(current_state: String) -> Vec<String> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", generate_possible_next_moves("++++".to_string())); // ["--++","+--+","++--"]
+    println!("{:?}", generate_possible_next_moves("+".to_string())); // []
+}
