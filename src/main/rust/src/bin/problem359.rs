06a2a83592b462b5ae195f2ca4b313b53a308195
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-28 00:51:05 -0500

    Update problem 359

--- src/main/rust/src/bin/problem359.rs
+++ src/main/rust/src/bin/problem359.rs
@@ -10,4 +10,12 @@ impl Logger {
     }
 }
 
-fn main() {}
+fn main() {
+    let logger = Logger::new();
+    println!("{}", logger.should_print_message(1, "foo".to_string())); // true
+    println!("{}", logger.should_print_message(2, "bar".to_string())); // true
+    println!("{}", logger.should_print_message(3, "foo".to_string())); // false
+    println!("{}", logger.should_print_message(8, "bar".to_string())); // false
+    println!("{}", logger.should_print_message(10, "foo".to_string())); // false
+    println!("{}", logger.should_print_message(11, "foo".to_string())); // true
+}
