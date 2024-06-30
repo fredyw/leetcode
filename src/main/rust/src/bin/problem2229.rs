d84a43beb0e4307cdeabf985eef2180a404985b4
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-29 19:16:57 -0500

    Skeleton for problem 2229

---
+++ src/main/rust/src/bin/problem2229.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/check-if-an-array-is-consecutive/description/
+pub fn is_consecutive(nums: Vec<i32>) -> bool {
+    todo!()
+}
+
+fn main() {
+    println!("{}", is_consecutive(vec![1, 3, 4, 2])); // true
+    println!("{}", is_consecutive(vec![1, 3])); // false
+    println!("{}", is_consecutive(vec![3, 5, 4])); // true
+}
