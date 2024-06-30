efc5f3095794ee3fcf9be8154f0bdd595567c724
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-29 19:25:29 -0500

    Skeleton for problem 1426

---
+++ src/main/rust/src/bin/problem1426.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/counting-elements/
+pub fn count_elements(arr: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", count_elements(vec![1, 2, 3])); // 2
+    println!("{}", count_elements(vec![1, 1, 3, 3, 5, 5, 7, 7])); // 0
+    println!("{}", count_elements(vec![1, 1, 2, 2, 3, 3])); // 4
+}
