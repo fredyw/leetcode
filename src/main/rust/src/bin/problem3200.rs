668b7ee1759ec2f1e3f114c60e53855db5dbb084
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-30 01:42:23 -0500

    Skeleton for problem 3200

---
+++ src/main/rust/src/bin/problem3200.rs
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/maximum-height-of-a-triangle/description/
+pub fn max_height_of_triangle(red: i32, blue: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", max_height_of_triangle(2, 4)); // 3
+    println!("{}", max_height_of_triangle(2, 1)); // 2
+    println!("{}", max_height_of_triangle(1, 1)); // 1
+    println!("{}", max_height_of_triangle(10, 1)); // 2
+}
