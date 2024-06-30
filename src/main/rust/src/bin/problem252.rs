cfde2d82d0766eee4e7efbb186baec9d9e4dac2a
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-30 09:16:04 -0500

    Skeleton for problem 252

---
+++ src/main/rust/src/bin/problem252.rs
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/meeting-rooms/description/
+pub fn can_attend_meetings(intervals: Vec<Vec<i32>>) -> bool {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        can_attend_meetings(vec![vec![0, 30], vec![5, 10], vec![15, 20]])
+    ); // false
+    println!(
+        "{}",
+        can_attend_meetings(vec![vec![0, 30], vec![5, 10], vec![15, 20]])
+    ); // true
+}
