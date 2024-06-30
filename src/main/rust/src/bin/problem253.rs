723698cf3c3e81e88be0fb3a7dd9e6ada6e13f58
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-06-30 09:28:53 -0500

    Skeleton for problem 253

---
+++ src/main/rust/src/bin/problem253.rs
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/meeting-rooms-ii/description/
+pub fn min_meeting_rooms(intervals: Vec<Vec<i32>>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        min_meeting_rooms(vec![vec![0, 30], vec![5, 10], vec![15, 20]])
+    ); // 2
+    println!("{}", min_meeting_rooms(vec![vec![7, 10], vec![2, 4]])); // 1
+}
