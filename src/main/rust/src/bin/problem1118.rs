ff5e5f6677f826d211027bac0a7bfc5bf229a2e7
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-01 02:29:18 -0500

    Solve problem 1118

--- src/main/rust/src/bin/problem1118.rs
+++ src/main/rust/src/bin/problem1118.rs
@@ -1,6 +1,20 @@
 // https://leetcode.com/problems/number-of-days-in-a-month/description/
 pub fn number_of_days(year: i32, month: i32) -> i32 {
-    todo!()
+    pub fn is_leap_year(year: i32) -> bool {
+        year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
+    }
+
+    match month {
+        2 => {
+            if is_leap_year(year) {
+                29
+            } else {
+                28
+            }
+        }
+        4 | 6 | 9 | 11 => 30,
+        _ => 31,
+    }
 }
 
 fn main() {
