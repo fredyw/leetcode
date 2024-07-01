1b33d36a69238784f4586291045a26aa61235e36
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-01 02:24:59 -0500

    Skeleton for problem 1118

---
+++ src/main/rust/src/bin/problem1118.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/number-of-days-in-a-month/description/
+pub fn number_of_days(year: i32, month: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", number_of_days(1992, 7)); // 31
+    println!("{}", number_of_days(2000, 2)); // 29
+    println!("{}", number_of_days(1900, 2)); // 28
+}
