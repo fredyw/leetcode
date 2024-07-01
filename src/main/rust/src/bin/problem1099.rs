e4e3acf85de923a52c64f6277aa960c13c833147
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-01 02:36:57 -0500

    Skeleton for problem 1099

---
+++ src/main/rust/src/bin/problem1099.rs
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/two-sum-less-than-k/
+pub fn two_sum_less_than_k(nums: Vec<i32>, k: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        two_sum_less_than_k(vec![34, 23, 1, 24, 75, 33, 54, 8], 60)
+    ); // 58
+    println!("{}", two_sum_less_than_k(vec![10, 20, 30], 15)); // -1
+}
