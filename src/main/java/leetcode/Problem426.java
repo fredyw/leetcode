619c5bc21adbdb9fb0c4497e9985914b8e05b62d
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-02 00:28:17 -0500

    Skeleton for problem 426

---
+++ src/main/java/leetcode/Problem426.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+// https://leetcode.com/problems/convert-binary-search-tree-to-sorted-doubly-linked-list/description/
+public class Problem426 {
+    public static class Node {
+        public int val;
+        public Node left;
+        public Node right;
+
+        public Node(int val) {
+            this.val = val;
+        }
+
+        public Node(int val, Node left, Node right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    ;
+
+    public Node treeToDoublyList(Node root) {
+        // TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+    }
+}
