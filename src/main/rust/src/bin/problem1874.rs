66a47cde433aa7d419038cdf59adac6f57bd4098
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-02 18:15:04 -0500

    Skeleton for problem 1874

---
+++ src/main/rust/src/bin/problem1874.rs
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/minimize-product-sum-of-two-arrays/
+pub fn min_product_sum(nums1: Vec<i32>, nums2: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", min_product_sum(vec![5, 3, 4, 2], vec![4, 2, 2, 5])); // 40
+    println!(
+        "{}",
+        min_product_sum(vec![2, 1, 4, 5, 7], vec![3, 2, 4, 8, 6])
+    ); // 65
+}
