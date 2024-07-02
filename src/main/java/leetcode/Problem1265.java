7cbf2c2329ed91e1497348c67b53df93ccd0e829
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-02 16:59:12 -0500

    Skeleton for problem 1265

---
+++ src/main/java/leetcode/Problem1265.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+// https://leetcode.com/problems/print-immutable-linked-list-in-reverse/
+public class Problem1265 {
+    interface ImmutableListNode {
+        void printValue(); // print the value of this node.
+        ImmutableListNode getNext(); // return the next node.
+    }
+
+    public void printLinkedListInReverse(ImmutableListNode head) {
+        // TODO
+    }
+}
