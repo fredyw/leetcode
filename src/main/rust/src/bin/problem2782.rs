46dcdc2e6edf00fac64462b5f2b8965cf934b86e
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-02 19:06:42 -0500

    Skeleton for problem 2782

---
+++ src/main/rust/src/bin/problem2782.rs
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/number-of-unique-categories/description/
+struct CategoryHandler {}
+
+impl CategoryHandler {
+    pub fn new(categories: Vec<i32>) -> Self {
+        unimplemented!()
+    }
+    pub fn have_same_category(&self, a: i32, b: i32) -> bool {
+        unimplemented!()
+    }
+}
+
+pub fn number_of_categories(n: i32, category_handler: CategoryHandler) -> i32 {
+    todo!()
+}
+
+fn main() {}
