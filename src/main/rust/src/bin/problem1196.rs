98dd3755ba98cb18b65420e8b7fc6361c204dc9e
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-04 00:13:40 -0500

    Skeleton for problem 1196

---
+++ src/main/rust/src/bin/problem1196.rs
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/how-many-apples-can-you-put-into-the-basket/description/
+pub fn max_number_of_apples(weight: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", max_number_of_apples(vec![100, 200, 150, 1000])); // 4
+    println!(
+        "{}",
+        max_number_of_apples(vec![900, 950, 800, 1000, 700, 800])
+    ); // 5
+}
