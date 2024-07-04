41ceaf7ffd30c852a2f5a12e5af0c8ddf05b586d
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-04 10:06:19 -0500

    Solve problem 3205

--- src/main/rust/src/bin/problem3205.rs
+++ src/main/rust/src/bin/problem3205.rs
@@ -1,16 +1,21 @@
 // https://leetcode.com/problems/maximum-array-hopping-score-i/description/
 pub fn max_score(nums: Vec<i32>) -> i32 {
-    fn max_score(nums: &Vec<i32>, i: usize, j: usize) -> i32 {
+    fn max_score(nums: &Vec<i32>, i: usize, j: usize, memo: &mut Vec<Vec<i32>>) -> i32 {
         if j == nums.len() {
             return 0;
         }
-        let hop = max_score(nums, j, j + 1) + (j as i32 - i as i32) * nums[j];
-        let skip = max_score(nums, i, j + 1) + (j as i32 - i as i32) * nums[j];
+        if memo[i][j] != -1 {
+            return memo[i][j];
+        }
+        let score = (j as i32 - i as i32) * nums[j];
+        let hop = max_score(nums, j, j + 1, memo) + score;
+        let skip = max_score(nums, i, j + 1, memo);
         let max = hop.max(skip);
+        memo[i][j] = max;
         max
     }
 
-    max_score(&nums, 0, 0)
+    max_score(&nums, 0, 0, &mut vec![vec![-1; nums.len()]; nums.len()])
 }
 
 fn main() {
