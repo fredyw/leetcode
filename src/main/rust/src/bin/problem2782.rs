cb78f7f14d8962a0fbce65bf813838482aa576f7
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-04 00:26:01 -0500

    Solve problem 2782

--- src/main/rust/src/bin/problem2782.rs
+++ src/main/rust/src/bin/problem2782.rs
@@ -11,7 +11,22 @@ impl CategoryHandler {
 }
 
 pub fn number_of_categories(n: i32, category_handler: CategoryHandler) -> i32 {
-    todo!()
+    let mut v = vec![];
+    for i in 0..n {
+        v.push(i);
+    }
+    let mut answer = 0;
+    while !v.is_empty() {
+        let mut tmp: Vec<i32> = vec![];
+        for i in 1..v.len() {
+            if !category_handler.have_same_category(v[0], v[i]) {
+                tmp.push(v[i]);
+            }
+        }
+        v = tmp;
+        answer += 1;
+    }
+    answer
 }
 
 fn main() {}
