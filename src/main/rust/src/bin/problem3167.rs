106bca46087758832409f985a0a6938b5fc7443e
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-04 00:41:25 -0500

    Solve problem 3167

--- src/main/rust/src/bin/problem3167.rs
+++ src/main/rust/src/bin/problem3167.rs
@@ -1,6 +1,28 @@
+use std::collections::BTreeMap;
+
 // https://leetcode.com/problems/better-compression-of-string/description/
 pub fn better_compression(compressed: String) -> String {
-    todo!()
+    let mut map: BTreeMap<char, i32> = BTreeMap::new();
+    let mut char = ' ';
+    let mut num = String::new();
+    for c in compressed.chars() {
+        if c.is_numeric() {
+            num.push(c);
+        } else {
+            if !num.is_empty() {
+                *map.entry(char).or_insert(0) += num.parse::<i32>().unwrap();
+            }
+            num.clear();
+            char = c;
+        }
+    }
+    *map.entry(char).or_insert(0) += num.parse::<i32>().unwrap();
+    let mut answer = String::new();
+    for (char, count) in map.into_iter() {
+        answer.push(char);
+        answer.push_str(&count.to_string());
+    }
+    answer
 }
 
 fn main() {
