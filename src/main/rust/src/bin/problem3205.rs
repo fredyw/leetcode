4770e2e885293c2a8c5d226df196af644deaa85d
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-04 00:43:30 -0500

    Skeleton for problem 3205

---
+++ src/main/rust/src/bin/problem3205.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/maximum-array-hopping-score-i/description/
+pub fn max_score(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", max_score(vec![1, 5, 8])); // 16
+    println!("{}", max_score(vec![4, 5, 2, 8, 9, 1, 3])); // 42
+}
