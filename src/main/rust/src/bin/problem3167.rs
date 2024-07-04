33a215cadcb7305f8cfd0e684a8d887ee1d5941a
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-04 00:28:37 -0500

    Skeleton for problem 3167

---
+++ src/main/rust/src/bin/problem3167.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/better-compression-of-string/description/
+pub fn better_compression(compressed: String) -> String {
+    todo!()
+}
+
+fn main() {
+    println!("{}", better_compression("a3c9b2c1".to_string())); // "a3b2c10"
+    println!("{}", better_compression("c2b3a1".to_string())); // "a1b3c2"
+    println!("{}", better_compression("a2b4c1".to_string())); // "a2b4c1"
+}
