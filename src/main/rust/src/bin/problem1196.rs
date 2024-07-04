95a751fbb12fce6e227fd74ccb7ed431f0624613
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-04 00:18:41 -0500

    Solve problem 1196

--- src/main/rust/src/bin/problem1196.rs
+++ src/main/rust/src/bin/problem1196.rs
@@ -1,6 +1,15 @@
 // https://leetcode.com/problems/how-many-apples-can-you-put-into-the-basket/description/
-pub fn max_number_of_apples(weight: Vec<i32>) -> i32 {
-    todo!()
+pub fn max_number_of_apples(mut weight: Vec<i32>) -> i32 {
+    weight.sort();
+    let mut answer = 0;
+    let mut n = 5000;
+    let mut i = 0;
+    while i < weight.len() && n - weight[i] >= 0 {
+        n -= weight[i];
+        i += 1;
+        answer += 1;
+    }
+    answer
 }
 
 fn main() {
