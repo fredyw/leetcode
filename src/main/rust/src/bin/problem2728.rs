fbaa1d0c5556e10ccd063fcf47705d2e0336ce4c
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-04 19:01:57 -0500

    Skeleton for problem 2728

---
+++ src/main/rust/src/bin/problem2728.rs
@@ -0,0 +1,29 @@
+struct Street {}
+
+impl Street {
+    pub fn new(doors: Vec<i32>) -> Self {
+        unimplemented!()
+    }
+    pub fn open_door(&mut self) {
+        unimplemented!()
+    }
+    pub fn close_door(&mut self) {
+        unimplemented!()
+    }
+    pub fn is_door_open(&self) -> bool {
+        unimplemented!()
+    }
+    pub fn move_right(&mut self) {
+        unimplemented!()
+    }
+    pub fn move_left(&mut self) {
+        unimplemented!()
+    }
+}
+
+// https://leetcode.com/problems/count-houses-in-a-circular-street/description/
+pub fn house_count(street: Street, k: i32) -> i32 {
+    todo!()
+}
+
+fn main() {}
