3d6f7263586c148187774bd7b372347d6f9b0607
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-06 23:26:30 -0500

    Skeleton for problem 3206

---
+++ src/main/rust/src/bin/problem3206.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/alternating-groups-i/description/
+pub fn number_of_alternating_groups(colors: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", number_of_alternating_groups(vec![1, 1, 1])); // 0
+    println!("{}", number_of_alternating_groups(vec![0, 1, 0, 0, 1])); // 3
+}
