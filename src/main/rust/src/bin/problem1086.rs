6155d540da973634a7b581889c2767da7c987c41
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-07 10:58:43 -0500

    Solve problem 1086

--- src/main/rust/src/bin/problem1086.rs
+++ src/main/rust/src/bin/problem1086.rs
@@ -1,6 +1,31 @@
+use std::cmp::Ordering;
+
 // https://leetcode.com/problems/high-five/description/
-pub fn high_five(items: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-    todo!()
+pub fn high_five(mut items: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+    items.sort_by(|a, b| {
+        let cmp = a[0].cmp(&b[0]);
+        if cmp == Ordering::Equal {
+            b[1].cmp(&a[1])
+        } else {
+            cmp
+        }
+    });
+    let mut answer = vec![];
+    let mut i = 0;
+    while i < items.len() {
+        let val = items[i][0];
+        let mut sum = 0;
+        for _ in 0..5 {
+            sum += items[i][1];
+            i += 1;
+        }
+        let avg = sum / 5;
+        answer.push(vec![val, avg]);
+        while i < items.len() && val == items[i][0] {
+            i += 1;
+        }
+    }
+    answer
 }
 
 fn main() {
