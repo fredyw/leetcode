916515ce8af480fc25219aecbbd87c29c33fe0d6
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-06 23:35:07 -0500

    Solve problem 32-6

--- src/main/rust/src/bin/problem3206.rs
+++ src/main/rust/src/bin/problem3206.rs
@@ -1,6 +1,18 @@
 // https://leetcode.com/problems/alternating-groups-i/description/
 pub fn number_of_alternating_groups(colors: Vec<i32>) -> i32 {
-    todo!()
+    let mut answer = 0;
+    let mut n = 0;
+    let mut i = 0;
+    while n < colors.len() {
+        let prev_idx = if i == 0 { colors.len() - 1 - i } else { i - 1 };
+        let next_idx = (i + 1) % colors.len();
+        if colors[prev_idx] != colors[i] && colors[i] != colors[next_idx] {
+            answer += 1;
+        }
+        i = next_idx;
+        n += 1;
+    }
+    answer
 }
 
 fn main() {
