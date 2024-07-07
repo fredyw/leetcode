ab09e3f976545bf1a53b5f49e9fce1265079558c
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-06 23:21:18 -0500

    Skeleton for problem 3210

---
+++ src/main/rust/src/bin/problem3210.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/find-the-encrypted-string/description/
+pub fn get_encrypted_string(s: String, k: i32) -> String {
+    todo!()
+}
+
+fn main() {
+    println!("{}", get_encrypted_string("dart".to_string(), 3)); // "tdar"
+    println!("{}", get_encrypted_string("aaa".to_string(), 1)); // "aaa"
+}
