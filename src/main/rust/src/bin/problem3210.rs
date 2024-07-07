3b3b06e362452ac3278fe5635f69cad3597dbb6c
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-06 23:25:13 -0500

    Solve problem 3210

--- src/main/rust/src/bin/problem3210.rs
+++ src/main/rust/src/bin/problem3210.rs
@@ -1,6 +1,12 @@
 // https://leetcode.com/problems/find-the-encrypted-string/description/
 pub fn get_encrypted_string(s: String, k: i32) -> String {
-    todo!()
+    let mut answer = String::new();
+    let chars: Vec<char> = s.chars().collect();
+    for i in 0..chars.len() {
+        let j = (i + k as usize) % chars.len();
+        answer.push(chars[j]);
+    }
+    answer
 }
 
 fn main() {
