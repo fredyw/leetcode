1c02a2591c45aa64e47003b7748d619837141653
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-08 15:07:39 -0500

    Skeleton for problem 3211

---
+++ src/main/rust/src/bin/problem3211.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/generate-binary-strings-without-adjacent-zeros/description/
+pub fn valid_strings(n: i32) -> Vec<String> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", valid_strings(3)); // ["010","011","101","110","111"]
+    println!("{:?}", valid_strings(1)); // ["0","1"]
+}
