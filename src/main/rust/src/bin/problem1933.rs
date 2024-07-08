d792a55c39eea41c74fa3db4cc63b4e447151231
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-07 19:22:23 -0500

    Skeleton for problem 1933

---
+++ src/main/rust/src/bin/problem1933.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/check-if-string-is-decomposable-into-value-equal-substrings/description/
+pub fn is_decomposable(s: String) -> bool {
+    todo!()
+}
+
+fn main() {
+    println!("{}", is_decomposable("000111000".to_string())); // false
+    println!("{}", is_decomposable("00011111222".to_string())); // true
+    println!("{}", is_decomposable("011100022233".to_string())); // false
+}
