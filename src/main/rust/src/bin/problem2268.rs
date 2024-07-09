1a7c4054468470ef033dd659e27c8dd3f63cde86
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-09 15:48:17 -0500

    Skeleton for problem 2268

---
+++ src/main/rust/src/bin/problem2268.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/minimum-number-of-keypresses/
+pub fn minimum_keypresses(s: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", minimum_keypresses("apple".to_string())); // 5
+    println!("{}", minimum_keypresses("abcdefghijkl".to_string())); // 15
+}
