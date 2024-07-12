dd3599e5c40cf0aea68b73cc400bbae18d65eae0
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-12 01:19:47 -0500

    Solve problem 1777

--- src/main/sql/Problem1777.sql
+++ src/main/sql/Problem1777.sql
@@ -1,1 +1,7 @@
 -- https://leetcode.com/problems/products-price-for-each-store/description/
+select product_id,
+       max(case when store = 'store1' then price else null end) as store1,
+       max(case when store = 'store2' then price else null end) as store2,
+       max(case when store = 'store3' then price else null end) as store3
+from products
+group by product_id;
