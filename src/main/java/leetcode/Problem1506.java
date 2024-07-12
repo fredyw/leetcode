b3c8ae19a3420661c003933f912028f628be997c
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-12 17:33:54 -0500

    Solve problem 1506

--- src/main/java/leetcode/Problem1506.java
+++ src/main/java/leetcode/Problem1506.java
@@ -1,7 +1,12 @@
 package leetcode;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
 
 // https://leetcode.com/problems/find-root-of-n-ary-tree/description/
 public class Problem1506 {
@@ -26,7 +31,17 @@ public class Problem1506 {
     }
 
     public Node findRoot(List<Node> tree) {
-        // TODO
+        Set<Integer> set = new HashSet<>();
+        for (Node node : tree) {
+            for (Node child : node.children) {
+                set.add(child.val);
+            }
+        }
+        for (Node node : tree) {
+            if (!set.contains(node.val)) {
+                return node;
+            }
+        }
         return null;
     }
 }
