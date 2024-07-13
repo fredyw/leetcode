2e299e1582facd62bf45eef91bef062e4a843d0b
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-12 19:09:58 -0500

    Skeleton for problem 3064

---
+++ src/main/rust/src/bin/problem3064.rs
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/guess-the-number-using-bitwise-questions-i/description/
+
+unsafe fn common_set_bits(num: i32) -> i32 {
+    unimplemented!()
+}
+
+unsafe fn find_number() -> i32 {
+    todo!()
+}
+
+fn main() {}
