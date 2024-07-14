4f225befea173d7e0379c26fde4675982080bce7
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-14 15:09:43 -0500

    Skeleton for problem 484

---
+++ src/main/rust/src/bin/problem484.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/find-permutation/description/
+pub fn find_permutation(s: String) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", find_permutation("I".to_string())); // [1,2]
+    println!("{:?}", find_permutation("DI".to_string())); // [2,1,3]
+}
