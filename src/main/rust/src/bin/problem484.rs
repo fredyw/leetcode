66fadc4cba03cbfc216c2e45d8154b3e30d42ce6
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-14 16:21:31 -0500

    Update problem 484

--- src/main/rust/src/bin/problem484.rs
+++ src/main/rust/src/bin/problem484.rs
@@ -10,7 +10,8 @@ fn main() {
     println!("{:?}", find_permutation("IDDI".to_string())); // [1,4,3,2,5]
     println!("{:?}", find_permutation("IIII".to_string())); // [1,2,3,4,5]
     println!("{:?}", find_permutation("DDDD".to_string())); // [1,2,3,5,4]
-    println!("{:?}", find_permutation("IIID".to_string())); // [4,3,2,1,5]
+    println!("{:?}", find_permutation("IIIID".to_string())); // [1,2,3,4,6,5]
+    println!("{:?}", find_permutation("DDDDI".to_string())); // [5,4,3,2,1,6]
     println!("{:?}", find_permutation("DIDIDI".to_string())); // [2,1,4,3,6,5,7]
     println!("{:?}", find_permutation("IDIDID".to_string())); // [1,3,2,5,4,7,6]
 }
