29f5437eda8f80b0bfc7a6a0cad575899723c3a3
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-14 00:33:09 -0500

    Skeleton for problem 3216

---
+++ src/main/rust/src/bin/problem3216.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/lexicographically-smallest-string-after-a-swap/description/
+pub fn get_smallest_string(s: String) -> String {
+    todo!()
+}
+
+fn main() {
+    println!("{}", get_smallest_string("45320".to_string())); // "43520"
+    println!("{}", get_smallest_string("001".to_string())); // "001"
+}
