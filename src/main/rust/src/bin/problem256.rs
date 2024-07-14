19530d1cd5a10f5350c64d216dfc79232340075e
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-14 00:44:25 -0500

    Skeleton for problem 256

---
+++ src/main/rust/src/bin/problem256.rs
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/paint-house/description/
+pub fn min_cost(costs: Vec<Vec<i32>>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        min_cost(vec![vec![17, 2, 17], vec![16, 16, 5], vec![14, 3, 19]])
+    ); // 10
+    println!("{}", min_cost(vec![vec![7, 6, 2]])); // 2
+}
