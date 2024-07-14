6e7c83eada9ea6f95a8597b646d0c9e2e73cc7ba
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-14 15:18:10 -0500

    Skeleton for problem 1228

---
+++ src/main/rust/src/bin/problem1228.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/missing-number-in-arithmetic-progression/description/
+pub fn missing_number(arr: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", missing_number(vec![5, 7, 11, 13])); // 9
+    println!("{}", missing_number(vec![15, 13, 12])); // 14
+}
