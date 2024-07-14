2bec7207dcd9e6909c43327e1407e0c0c0f11218
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-14 00:42:28 -0500

    Solve problem 3216

--- src/main/rust/src/bin/problem3216.rs
+++ src/main/rust/src/bin/problem3216.rs
@@ -1,6 +1,22 @@
 // https://leetcode.com/problems/lexicographically-smallest-string-after-a-swap/description/
 pub fn get_smallest_string(s: String) -> String {
-    todo!()
+    let mut answer: Vec<char> = s.chars().collect();
+    for i in 0..answer.len() - 1 {
+        let current = answer[i].to_digit(10).unwrap();
+        let next = answer[i + 1].to_digit(10).unwrap();
+        if current % 2 == 0 && next % 2 == 0 {
+            if current > next {
+                answer.swap(i, i + 1);
+                break;
+            }
+        } else if current % 2 != 0 && next % 2 != 0 {
+            if current > next {
+                answer.swap(i, i + 1);
+                break;
+            }
+        }
+    }
+    answer.into_iter().collect()
 }
 
 fn main() {
