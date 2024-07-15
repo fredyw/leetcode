ec0260dd56673666c4b724aecc2a21392ca33cb0
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-15 16:28:39 -0500

    Solve problem 484

--- src/main/rust/src/bin/problem484.rs
+++ src/main/rust/src/bin/problem484.rs
@@ -13,20 +13,20 @@ pub fn find_permutation(s: String) -> Vec<i32> {
             }
         }
     }
-    println!("{v:?}");
     let mut answer = vec![];
-    answer.push(1);
     let mut i = 0;
-    let mut n = 2;
+    let mut n = if v[0].0 == 'I' {
+        answer.push(1);
+        2
+    } else {
+        answer.push(v[0].1 + 1);
+        v[0].1 + 1
+    };
     while i < v.len() {
         let (char, count) = v[i];
         if char == 'I' {
             let from = n;
             let to = n + count - 1;
-            println!("I");
-            println!("  before n: {n}");
-            // println!("I --> from: {from}");
-            // println!("I --> to: {to}");
             for j in from..to {
                 n += 1;
                 answer.push(j);
@@ -38,21 +38,13 @@ pub fn find_permutation(s: String) -> Vec<i32> {
             } else {
                 answer.push(n);
             }
-            println!("  after n: {n}");
-            println!("  answer: {:?}", answer);
         } else {
             let from = n - count;
             let to = n;
-            println!("D:");
-            println!("  before n: {n}");
-            // println!("D --> from: {from}");
-            // println!("D --> to: {to}");
             for j in (from..to).rev() {
                 answer.push(j);
             }
             n += 1;
-            println!("  after n: {n}");
-            println!("  answer: {:?}", answer);
         }
         i += 1;
     }
@@ -60,16 +52,16 @@ pub fn find_permutation(s: String) -> Vec<i32> {
 }
 
 fn main() {
-    // println!("{:?}", find_permutation("I".to_string())); // [1,2]
-    // println!("{:?}", find_permutation("DI".to_string())); // [2,1,3]
-    // println!("{:?}", find_permutation("DIDIDIIIIDIIDID".to_string())); // [2,1,4,3,6,5,7,8,9,11,10,12,14,13,16,15]
-    // println!("{:?}", find_permutation("IDDI".to_string())); // [1,4,3,2,5]
-    // println!("{:?}", find_permutation("IIII".to_string())); // [1,2,3,4,5]
-    // println!("{:?}", find_permutation("DDDD".to_string())); // [1,2,3,5,4]
-    // println!("{:?}", find_permutation("IIIID".to_string())); // [1,2,3,4,6,5]
-    // println!("{:?}", find_permutation("DDDDI".to_string())); // [5,4,3,2,1,6]
-    // println!("{:?}", find_permutation("DIDIDI".to_string())); // [2,1,4,3,6,5,7]
-    // println!("{:?}", find_permutation("IDIDID".to_string())); // [1,3,2,5,4,7,6]
-    // println!("{:?}", find_permutation("DDDIIIDDD".to_string())); // [4,3,2,1,5,6,10,9,8,7]
+    println!("{:?}", find_permutation("I".to_string())); // [1,2]
+    println!("{:?}", find_permutation("DI".to_string())); // [2,1,3]
+    println!("{:?}", find_permutation("DIDIDIIIIDIIDID".to_string())); // [2,1,4,3,6,5,7,8,9,11,10,12,14,13,16,15]
+    println!("{:?}", find_permutation("IDDI".to_string())); // [1,4,3,2,5]
+    println!("{:?}", find_permutation("IIII".to_string())); // [1,2,3,4,5]
+    println!("{:?}", find_permutation("DDDD".to_string())); // [1,2,3,5,4]
+    println!("{:?}", find_permutation("IIIID".to_string())); // [1,2,3,4,6,5]
+    println!("{:?}", find_permutation("DDDDI".to_string())); // [5,4,3,2,1,6]
+    println!("{:?}", find_permutation("DIDIDI".to_string())); // [2,1,4,3,6,5,7]
+    println!("{:?}", find_permutation("IDIDID".to_string())); // [1,3,2,5,4,7,6]
+    println!("{:?}", find_permutation("DDDIIIDDD".to_string())); // [4,3,2,1,5,6,10,9,8,7]
     println!("{:?}", find_permutation("IIIDDDIII".to_string())); // [1,2,3,7,6,5,4,8,9,10]
 }
