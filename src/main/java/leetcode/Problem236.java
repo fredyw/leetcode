d8fd8a5c69ac021e103795c9ccf9a0dded63602b
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-16 13:32:43 -0500

    Update problem 236

--- src/main/java/leetcode/Problem236.java
+++ src/main/java/leetcode/Problem236.java
@@ -29,17 +29,13 @@ public class Problem236 {
         if (root == null) {
             return false;
         }
-        boolean mid = false;
-        if (root.val == p.val || root.val == q.val) {
-            mid = true;
-        }
+        boolean parent = root.val == p.val || root.val == q.val;
         boolean left = lowestCommonAncestor(root.left, p, q, lca);
         boolean right = lowestCommonAncestor(root.right, p, q, lca);
-        if ((mid && left) || (mid && right) || (left && right)) {
-            if (lca.node == null) {
-                lca.node = root;
-            }
+        if ((parent && left) || (parent && right) || (left && right)) {
+            lca.node = root;
+            return true;
         }
-        return left | mid | right;
+        return left || parent || right;
     }
 }
