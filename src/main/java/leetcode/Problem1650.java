ad8cc10c9a405b1f65afa2328f17a4eadd693202
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-16 12:43:09 -0500

    Skeleton for problem 1650

---
+++ src/main/java/leetcode/Problem1650.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree-iii/description/
+public class Problem1650 {
+    public static class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node parent;
+    }
+
+    public Node lowestCommonAncestor(Node p, Node q) {
+        // TODO
+        return null;
+    }
+}
