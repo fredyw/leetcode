36d2f034b0baddcd126fbdcb27d78fb63c123b50
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-16 15:09:57 -0500

    Solve problem 1676

--- src/main/java/leetcode/Problem1676.java
+++ src/main/java/leetcode/Problem1676.java
@@ -1,6 +1,8 @@
 package leetcode;
 
 import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 // https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree-iv/description/
 public class Problem1676 {
@@ -16,7 +18,10 @@ public class Problem1676 {
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode[] nodes) {
         LCA lca = new LCA();
-        lowestCommonAncestor(root, nodes, lca);
+        lowestCommonAncestor(root, Arrays.stream(nodes)
+                .map(node -> node.val)
+                .collect(Collectors.toSet()),
+            lca);
         return lca.node;
     }
 
@@ -24,13 +29,17 @@ public class Problem1676 {
         private TreeNode node;
     }
 
-    private static boolean lowestCommonAncestor(TreeNode root, TreeNode[] nodes, LCA lca) {
+    private static boolean lowestCommonAncestor(TreeNode root, Set<Integer> nodes, LCA lca) {
         if (root == null) {
             return false;
         }
-        var parent = Arrays.stream(nodes).anyMatch(node -> node.val == root.val);
+        var parent = nodes.contains(root.val);
         var left = lowestCommonAncestor(root.left, nodes, lca);
         var right = lowestCommonAncestor(root.right, nodes, lca);
-        return parent || left || right;
+        if (parent || (left && right)) {
+            lca.node = root;
+            return true;
+        }
+        return left || right;
     }
 }
