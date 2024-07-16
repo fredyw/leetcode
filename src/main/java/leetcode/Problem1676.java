6f48fd20dfea45c3a5dd5ce46e4a6302ff7bab7f
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-16 14:45:45 -0500

    Update problem 1676

--- src/main/java/leetcode/Problem1676.java
+++ src/main/java/leetcode/Problem1676.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.Arrays;
+
 // https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree-iv/description/
 public class Problem1676 {
     public static class TreeNode {
@@ -13,7 +15,22 @@ public class Problem1676 {
     }
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode[] nodes) {
-        // TODO
-        return null;
+        LCA lca = new LCA();
+        lowestCommonAncestor(root, nodes, lca);
+        return lca.node;
+    }
+
+    private static class LCA {
+        private TreeNode node;
+    }
+
+    private static boolean lowestCommonAncestor(TreeNode root, TreeNode[] nodes, LCA lca) {
+        if (root == null) {
+            return false;
+        }
+        var parent = Arrays.stream(nodes).anyMatch(node -> node.val == root.val);
+        var left = lowestCommonAncestor(root.left, nodes, lca);
+        var right = lowestCommonAncestor(root.right, nodes, lca);
+        return parent || left || right;
     }
 }
