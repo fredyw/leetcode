ca09dd56a9fb6f58cec906097bad7080ad656a91
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-15 23:42:03 -0500

    Skeleton for problem 2802

---
+++ src/main/rust/src/bin/problem2802.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/find-the-k-th-lucky-number/description/
+pub fn kth_lucky_number(k: i32) -> String {
+    todo!()
+}
+
+fn main() {
+    println!("{}", kth_lucky_number(4)); // "47"
+    println!("{}", kth_lucky_number(10)); // "477"
+    println!("{}", kth_lucky_number(1000)); // "777747447"
+}
