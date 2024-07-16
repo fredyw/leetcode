aed51efb77a0a7a2681612381d47d11abba9b897
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-16 13:49:29 -0500

    Skeleton for problem 1676

---
+++ src/main/java/leetcode/Problem1676.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree-iv/description/
+public class Problem1676 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode[] nodes) {
+        // TODO
+        return null;
+    }
+}
