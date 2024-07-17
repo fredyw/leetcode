23c35f77b9d2a4f9a86c87247970baf9f96b725d
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-17 10:59:19 -0500

    Update problem 1708

--- src/main/rust/src/bin/problem1708.rs
+++ src/main/rust/src/bin/problem1708.rs
@@ -1,6 +1,12 @@
 // https://leetcode.com/problems/largest-subarray-length-k/description/
 pub fn largest_subarray(nums: Vec<i32>, k: i32) -> Vec<i32> {
-    todo!()
+    let mut answer = &nums[0..k as usize];
+    for i in 1..nums.len() - k as usize + 1 {
+        if answer < &nums[i..i + k as usize] {
+            answer = &nums[i..i + k as usize];
+        }
+    }
+    answer.iter().copied().collect()
 }
 
 fn main() {
