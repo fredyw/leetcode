a70cf43588bb964682b7c61cfa7be0ed9280a3c1
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-20 18:24:07 -0500

    Update problem 1106

--- src/main/rust/src/bin/problem1106.rs
+++ src/main/rust/src/bin/problem1106.rs
@@ -1,6 +1,17 @@
 // https://leetcode.com/problems/parsing-a-boolean-expression
 pub fn parse_bool_expr(expression: String) -> bool {
-    false
+    fn parse_bool_expr(expression: &Vec<char>, i: usize) -> bool {
+        if expression[i] == '&' {
+        } else if expression[i] == '|' {
+        } else if expression[i] == '!' {
+        } else if expression[i] == '(' {
+        } else if expression[i] == ')' {
+        }
+        todo!()
+    }
+
+    let expression: Vec<char> = expression.chars().collect();
+    parse_bool_expr(&expression, 0)
 }
 
 fn main() {
@@ -9,4 +20,5 @@ fn main() {
     println!("{}", parse_bool_expr("!(&(f,t))".to_string())); // true
     println!("{}", parse_bool_expr("|(t)".to_string())); // true
     println!("{}", parse_bool_expr("&(t)".to_string())); // true
+    println!("{}", parse_bool_expr("&(|(f,f,f,t),f)".to_string())); // false
 }
