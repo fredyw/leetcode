05241c35d6aed8d9eff64908bddfc0c9c5ea4973
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-20 17:58:34 -0500

    Skeleton for problem 2979

---
+++ src/main/rust/src/bin/problem2979.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/most-expensive-item-that-can-not-be-bought/description/
+pub fn most_expensive_item(prime_one: i32, prime_two: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", most_expensive_item(2, 5)); // 3
+    println!("{}", most_expensive_item(5, 7)); // 23
+}
