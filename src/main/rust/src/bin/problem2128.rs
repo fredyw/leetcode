374053bcc5fea686d37d8ca1025188752eade05c
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-19 23:57:18 -0500

    Solve problem 2128

--- src/main/rust/src/bin/problem2128.rs
+++ src/main/rust/src/bin/problem2128.rs
@@ -1,6 +1,24 @@
 // https://leetcode.com/problems/remove-all-ones-with-row-and-column-flips/description/
-pub fn remove_ones(grid: Vec<Vec<i32>>) -> bool {
-    todo!()
+pub fn remove_ones(mut grid: Vec<Vec<i32>>) -> bool {
+    let max_row = grid.len();
+    let max_col = if max_row > 0 { grid[0].len() } else { 0 };
+    for col in 0..max_col {
+        if grid[0][col] == 1 {
+            for row in 0..max_row {
+                grid[row][col] = if grid[row][col] == 0 { 1 } else { 0 };
+            }
+        }
+    }
+    for row in 0..max_row {
+        let mut sum = 0;
+        for col in 0..max_col {
+            sum += grid[row][col];
+        }
+        if sum != 0 && sum != max_col as i32 {
+            return false;
+        }
+    }
+    true
 }
 
 fn main() {
