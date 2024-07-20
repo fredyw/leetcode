537a9ad72e48439c6dcca0d1d66987e8a958a70b
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-19 23:58:46 -0500

    Skeleton for problem 544

---
+++ src/main/rust/src/bin/problem544.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/output-contest-matches/description/
+pub fn find_contest_match(n: i32) -> String {
+    todo!()
+}
+
+fn main() {
+    println!("{}", find_contest_match(4)); // "((1,4),(2,3))"
+    println!("{}", find_contest_match(8)); // "(((1,8),(4,5)),((2,7),(3,6)))"
+}
