995fb99272e39ff2d924cdd8c6f88ae340c3237a
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-20 00:26:15 -0500

    Update problem 544

--- src/main/rust/src/bin/problem544.rs
+++ src/main/rust/src/bin/problem544.rs
@@ -6,4 +6,5 @@ pub fn find_contest_match(n: i32) -> String {
 fn main() {
     println!("{}", find_contest_match(4)); // "((1,4),(2,3))"
     println!("{}", find_contest_match(8)); // "(((1,8),(4,5)),((2,7),(3,6)))"
+    println!("{}", find_contest_match(16)); // "((((1,16),(8,9)),((4,13),(5,12))),(((2,15),(7,10)),((3,14),(6,11))))"
 }
