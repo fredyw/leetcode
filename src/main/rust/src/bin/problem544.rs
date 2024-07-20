d2c4ab9e6d8fc1fd5f666993452f1e1650cd9eba
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-20 17:56:27 -0500

    Solve problem 544

--- src/main/rust/src/bin/problem544.rs
+++ src/main/rust/src/bin/problem544.rs
@@ -1,6 +1,25 @@
 // https://leetcode.com/problems/output-contest-matches/description/
 pub fn find_contest_match(n: i32) -> String {
-    todo!()
+    fn find_contest_match(vec: Vec<String>) -> String {
+        if vec.len() == 1 {
+            return vec.first().unwrap().clone();
+        }
+        let mut new_vec: Vec<String> = vec![];
+        let mut i = 0;
+        let mut j = vec.len() - 1;
+        while i < j {
+            new_vec.push(format!("({},{})", vec[i], vec[j]));
+            i += 1;
+            j -= 1;
+        }
+        find_contest_match(new_vec)
+    }
+
+    let mut v: Vec<String> = vec![];
+    for i in 1..=n {
+        v.push(i.to_string());
+    }
+    find_contest_match(v)
 }
 
 fn main() {
