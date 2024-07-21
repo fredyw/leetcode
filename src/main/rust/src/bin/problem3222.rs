3f55c2ce1e5689995081ef504229c2cfbe774785
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-21 01:01:14 -0500

    Skeleton for problem 3222

---
+++ src/main/rust/src/bin/problem3222.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/find-the-winning-player-in-coin-game/description/
+pub fn losing_player(x: i32, y: i32) -> String {
+    todo!()
+}
+
+fn main() {
+    println!("{}", losing_player(2, 7)); // "Alice"
+    println!("{}", losing_player(4, 11)); // "Bob"
+}
