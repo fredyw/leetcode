bfb25acf03090c15fa81df8071e8100b2cc7dba3
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-21 01:00:12 -0500

    Solve problem 3226

--- src/main/rust/src/bin/problem3226.rs
+++ src/main/rust/src/bin/problem3226.rs
@@ -1,6 +1,20 @@
 // https://leetcode.com/problems/number-of-bit-changes-to-make-two-integers-equal/description/
-pub fn min_changes(n: i32, k: i32) -> i32 {
-    todo!()
+pub fn min_changes(mut n: i32, k: i32) -> i32 {
+    let mut answer = 0;
+    let mut shift = 0;
+    while n != k {
+        if (n >> shift) & 1 != (k >> shift) & 1 {
+            if (n >> shift) & 1 == 0 {
+                return -1;
+            }
+            n = !n;
+            n |= 1 << shift;
+            n = !n;
+            answer += 1;
+        }
+        shift += 1;
+    }
+    answer
 }
 
 fn main() {
