69053604f59b39dbf2a64427a0c29f9b4ef75c2c
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-21 16:14:39 -0500

    Solve problem 2979

--- src/main/rust/src/bin/problem2979.rs
+++ src/main/rust/src/bin/problem2979.rs
@@ -1,6 +1,7 @@
 // https://leetcode.com/problems/most-expensive-item-that-can-not-be-bought/description/
 pub fn most_expensive_item(prime_one: i32, prime_two: i32) -> i32 {
-    todo!()
+    // https://en.wikipedia.org/wiki/Coin_problem
+    (prime_one * prime_two) - prime_one - prime_two
 }
 
 fn main() {
