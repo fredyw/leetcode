26976b00f140eb0b08f9c32795c0f38e123c34da
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-21 01:10:44 -0500

    Solve problem 3222

--- src/main/rust/src/bin/problem3222.rs
+++ src/main/rust/src/bin/problem3222.rs
@@ -1,9 +1,20 @@
 // https://leetcode.com/problems/find-the-winning-player-in-coin-game/description/
-pub fn losing_player(x: i32, y: i32) -> String {
-    todo!()
+pub fn losing_player(mut x: i32, mut y: i32) -> String {
+    let mut alice = false;
+    while x - 1 >= 0 && y - 4 >= 0 {
+        alice = !alice;
+        x -= 1;
+        y -= 4;
+    }
+    if alice {
+        "Alice".to_string()
+    } else {
+        "Bob".to_string()
+    }
 }
 
 fn main() {
     println!("{}", losing_player(2, 7)); // "Alice"
     println!("{}", losing_player(4, 11)); // "Bob"
+    println!("{}", losing_player(1, 4)); // "Alice"
 }
