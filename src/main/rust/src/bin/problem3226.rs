7f7bb368c30090f3a3f18e842e49106f445ccb68
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-21 00:23:44 -0500

    Skeleton for problem 3226

---
+++ src/main/rust/src/bin/problem3226.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/number-of-bit-changes-to-make-two-integers-equal/description/
+pub fn min_changes(n: i32, k: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", min_changes(13, 4)); // 2
+    println!("{}", min_changes(21, 21)); // 0
+    println!("{}", min_changes(14, 13)); // -1
+}
