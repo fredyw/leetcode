8c79b7d907498c7d23e7ae6daaaa6fd763616473
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-22 17:11:04 -0500

    Skeleton for problem 2330

---
+++ src/main/rust/src/bin/problem2330.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/valid-palindrome-iv/description/
+pub fn make_palindrome(s: String) -> bool {
+    todo!()
+}
+
+fn main() {
+    println!("{}", make_palindrome("abcdba".to_string())); // true
+    println!("{}", make_palindrome("aa".to_string())); // true
+    println!("{}", make_palindrome("abcdef".to_string())); // false
+}
