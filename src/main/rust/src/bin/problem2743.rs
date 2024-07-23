19812cc4a90f343283bea65570c0d88497ff507c
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-23 00:39:13 -0500

    Update problem 2743

--- src/main/rust/src/bin/problem2743.rs
+++ src/main/rust/src/bin/problem2743.rs
@@ -7,4 +7,7 @@ fn main() {
     println!("{}", number_of_special_substrings("abcd".to_string())); // 10
     println!("{}", number_of_special_substrings("ooo".to_string())); // 3
     println!("{}", number_of_special_substrings("abab".to_string())); // 7
+    println!("{}", number_of_special_substrings("abcde".to_string())); // 15
+    println!("{}", number_of_special_substrings("abc".to_string())); // 6
+    println!("{}", number_of_special_substrings("abcabcabc".to_string())); // 24
 }
