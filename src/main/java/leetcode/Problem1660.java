406e801098ca684355f50a4df58f13a15325b1d9
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-23 13:00:54 -0500

    Skeleton for problem 1660

---
+++ src/main/java/leetcode/Problem1660.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+// https://leetcode.com/problems/correct-a-binary-tree/description/
+public class Problem1660 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode correctBinaryTree(TreeNode root) {
+        // TODO
+        return null;
+    }
+}
