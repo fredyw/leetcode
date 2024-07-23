52661fbcccbccd335f8f66ce74dfb4596ef62461
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-23 00:16:38 -0500

    Solve problem 2330

--- src/main/rust/src/bin/problem2330.rs
+++ src/main/rust/src/bin/problem2330.rs
@@ -1,6 +1,20 @@
 // https://leetcode.com/problems/valid-palindrome-iv/description/
 pub fn make_palindrome(s: String) -> bool {
-    todo!()
+    let bytes = s.as_bytes();
+    let mut i = 0;
+    let mut j = s.len() - 1;
+    let mut n = 2;
+    while i < j {
+        if bytes[i] != bytes[j] {
+            if n == 0 {
+                return false;
+            }
+            n -= 1;
+        }
+        i += 1;
+        j -= 1;
+    }
+    true
 }
 
 fn main() {
