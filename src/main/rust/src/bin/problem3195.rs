2fb721d92d4806d51f6ce1424dab8750848e91e2
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-23 14:32:05 -0500

    Skeleton for problem 3195

---
+++ src/main/rust/src/bin/problem3195.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/find-the-minimum-area-to-cover-all-ones-i/description/
+pub fn minimum_area(grid: Vec<Vec<i32>>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", minimum_area(vec![vec![0, 1, 0], vec![1, 0, 1]])); // 6
+    println!("{}", minimum_area(vec![vec![1, 0], vec![0, 0]])); // 1
+}
