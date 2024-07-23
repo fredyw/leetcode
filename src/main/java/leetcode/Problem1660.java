98dddf104e18a6fc755338874b90be998417bb7f
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-23 14:22:19 -0500

    Update problem 1660

--- src/main/java/leetcode/Problem1660.java
+++ src/main/java/leetcode/Problem1660.java
@@ -1,5 +1,9 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 // https://leetcode.com/problems/correct-a-binary-tree/description/
 public class Problem1660 {
     public static class TreeNode {
@@ -22,7 +26,53 @@ public class Problem1660 {
     }
 
     public TreeNode correctBinaryTree(TreeNode root) {
-        // TODO
-        return null;
+        Map<Integer, Integer> map = new HashMap<>();
+        traverse(root, 0, map);
+        correctBinaryTree(root, null, Direction.CENTER, map);
+        return root;
+    }
+
+    private static void traverse(TreeNode root, int level, Map<Integer, Integer> map) {
+        if (root == null) {
+            return;
+        }
+        map.put(root.val, level);
+        traverse(root.left, level + 1, map);
+        traverse(root.right, level + 1, map);
+    }
+
+    private enum Direction {
+        CENTER, LEFT, RIGHT
+    }
+
+    private static void correctBinaryTree(TreeNode node,
+                                          TreeNode parent,
+                                          Direction direction,
+                                          Map<Integer, Integer> map) {
+        if (node == null) {
+            return;
+        }
+        if (node.left != null) {
+            if (Objects.equals(map.get(node.val), map.get(node.left.val))) {
+                if (direction == Direction.LEFT) {
+                    parent.left = null;
+                } else if (direction == Direction.RIGHT) {
+                    parent.right = null;
+                }
+            } else {
+                correctBinaryTree(node.left, node, Direction.LEFT, map);
+            }
+        }
+        if (node.right != null) {
+            if (Objects.equals(map.get(node.val), map.get(node.right.val))) {
+                if (direction == Direction.LEFT) {
+                    parent.left = null;
+                } else if (direction == Direction.RIGHT) {
+                    parent.right = null;
+                }
+            } else {
+                correctBinaryTree(node.right, node, Direction.RIGHT, map);
+            }
+        }
     }
 }
