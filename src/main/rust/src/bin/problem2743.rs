27daf84025d74076c9330c9d142edb88d78c712f
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-23 00:18:45 -0500

    Skeleton for problem 2743

---
+++ src/main/rust/src/bin/problem2743.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/count-substrings-without-repeating-character/description/
+pub fn number_of_special_substrings(s: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", number_of_special_substrings("abcd".to_string())); // 10
+    println!("{}", number_of_special_substrings("ooo".to_string())); // 3
+    println!("{}", number_of_special_substrings("abab".to_string())); // 7
+}
