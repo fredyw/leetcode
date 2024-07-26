ce20d04a71cef2454e6eec58aaeb2d928d64ae5e
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-26 11:20:00 -0500

    Solve problem 2408

--- src/main/rust/src/bin/problem2408.rs
+++ src/main/rust/src/bin/problem2408.rs
@@ -1,26 +1,47 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/design-sql/description/
-struct SQL {}
+struct SQL {
+    tables: HashMap<String, HashMap<i32, Vec<String>>>,
+    ids: HashMap<String, i32>,
+}
 
 impl SQL {
     fn new(names: Vec<String>, columns: Vec<i32>) -> Self {
-        SQL {}
+        let mut tables: HashMap<String, HashMap<i32, Vec<String>>> = HashMap::new();
+        for name in names {
+            tables.insert(name, HashMap::new());
+        }
+        SQL {
+            tables,
+            ids: HashMap::new(),
+        }
     }
 
-    fn insert_row(&self, name: String, row: Vec<String>) {
-        todo!()
+    fn insert_row(&mut self, name: String, row: Vec<String>) {
+        let id = self.ids.entry(name.clone()).or_insert(1);
+        self.tables.get_mut(&name).unwrap().insert(*id, row);
+        *id += 1;
     }
 
-    fn delete_row(&self, name: String, row_id: i32) {
-        todo!()
+    fn delete_row(&mut self, name: String, row_id: i32) {
+        self.tables.get_mut(&name).unwrap().remove(&row_id);
     }
 
     fn select_cell(&self, name: String, row_id: i32, column_id: i32) -> String {
-        todo!()
+        self.tables
+            .get(&name)
+            .unwrap_or(&HashMap::new())
+            .get(&row_id)
+            .unwrap_or(&Vec::new())
+            .get(column_id as usize - 1)
+            .unwrap_or(&String::new())
+            .clone()
     }
 }
 
 fn main() {
-    let sql = SQL::new(
+    let mut sql = SQL::new(
         vec!["one".to_string(), "two".to_string(), "three".to_string()],
         vec![2, 3, 1],
     );
