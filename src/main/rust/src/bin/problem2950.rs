77f58c4127b80f1344ee9d8648ad7af96fea1e51
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-26 11:54:23 -0500

    Solve problem 2950

--- src/main/rust/src/bin/problem2950.rs
+++ src/main/rust/src/bin/problem2950.rs
@@ -1,6 +1,32 @@
 // https://leetcode.com/problems/number-of-divisible-substrings/description/
 pub fn count_divisible_substrings(word: String) -> i32 {
-    todo!()
+    fn get_num(c: char) -> i32 {
+        match c {
+            'a' | 'b' => 1,
+            'c' | 'd' | 'e' => 2,
+            'f' | 'g' | 'h' => 3,
+            'i' | 'j' | 'k' => 4,
+            'l' | 'm' | 'n' => 5,
+            'o' | 'p' | 'q' => 6,
+            'r' | 's' | 't' => 7,
+            'u' | 'v' | 'w' => 8,
+            _ => 9,
+        }
+    }
+
+    let chars: Vec<char> = word.chars().collect();
+    let mut answer = 0;
+    for i in 0..chars.len() {
+        let mut sum = get_num(chars[i]);
+        answer += 1;
+        for j in i + 1..chars.len() {
+            sum += get_num(chars[j]);
+            if sum % (j - i + 1) as i32 == 0 {
+                answer += 1;
+            }
+        }
+    }
+    answer
 }
 
 fn main() {
