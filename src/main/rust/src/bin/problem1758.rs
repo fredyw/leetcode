1b68bfdc527ddc5fc9f57aff9d95d0048134a386
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-26 18:57:40 -0500

    Update problem 1758

--- src/main/rust/src/bin/problem1758.rs
+++ src/main/rust/src/bin/problem1758.rs
@@ -1,18 +1,25 @@
 // https://leetcode.com/problems/design-most-recently-used-queue/description/
-struct MRUQueue {}
+struct MRUQueue {
+    queue: Vec<i32>,
+}
 
 impl MRUQueue {
     fn new(n: i32) -> Self {
-        MRUQueue {}
+        let queue: Vec<i32> = (1..=n).collect();
+        MRUQueue { queue }
     }
 
-    fn fetch(&self, k: i32) -> i32 {
-        todo!()
+    fn fetch(&mut self, k: i32) -> i32 {
+        let size = self.queue.len();
+        let n = self.queue[k as usize - 1];
+        self.queue.swap(size - 1, n as usize);
+        println!("queue: {:?}", self.queue);
+        n
     }
 }
 
 fn main() {
-    let queue = MRUQueue::new(8);
+    let mut queue = MRUQueue::new(8);
     println!("{}", queue.fetch(3)); // 3
     println!("{}", queue.fetch(5)); // 6
     println!("{}", queue.fetch(2)); // 2
