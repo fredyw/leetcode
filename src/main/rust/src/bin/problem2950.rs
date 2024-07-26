cfee22053dbf520e3172727865fd98d2a321672d
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-26 11:21:53 -0500

    Skeleton for problem 2950

---
+++ src/main/rust/src/bin/problem2950.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/number-of-divisible-substrings/description/
+pub fn count_divisible_substrings(word: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", count_divisible_substrings("asdf".to_string())); // 6
+    println!("{}", count_divisible_substrings("bdh".to_string())); // 4
+    println!("{}", count_divisible_substrings("abcd".to_string())); // 6
+}
