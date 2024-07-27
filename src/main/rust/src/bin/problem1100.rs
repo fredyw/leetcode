91aa00ccd6315afe06da381557abb9d3be7b5a3d
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-27 12:50:54 -0500

    Skeleton for problem 1100

---
+++ src/main/rust/src/bin/problem1100.rs
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/find-k-length-substrings-with-no-repeated-characters/description/
+pub fn num_k_len_substr_no_repeats(s: String, k: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        num_k_len_substr_no_repeats("havefunonleetcode".to_string(), 5)
+    ); // 6
+    println!("{}", num_k_len_substr_no_repeats("home".to_string(), 5)); // 0
+}
