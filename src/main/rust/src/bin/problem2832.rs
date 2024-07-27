e18c8b9a84e25fecf1b8dd8cbd611db7fc79131e
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-27 18:34:07 -0500

    Skeleton for problem 2832

---
+++ src/main/rust/src/bin/problem2832.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/maximal-range-that-each-element-is-maximum-in-it/
+pub fn maximum_length_of_ranges(nums: Vec<i32>) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", maximum_length_of_ranges(vec![1, 5, 4, 3, 6])); // [1,4,2,1,5]
+    println!("{:?}", maximum_length_of_ranges(vec![1, 2, 3, 4, 5])); // [1,2,3,4,5]
+}
