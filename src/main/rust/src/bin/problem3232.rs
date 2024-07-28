07244cf1afea6d1281b95141039c37d71d81a67f
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-28 01:25:02 -0500

    Skeleton for problem 3232

---
+++ src/main/rust/src/bin/problem3232.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/find-if-digit-game-can-be-won/description/
+pub fn can_alice_win(nums: Vec<i32>) -> bool {
+    todo!()
+}
+
+fn main() {
+    println!("{}", can_alice_win(vec![1, 2, 3, 4, 10])); // false
+    println!("{}", can_alice_win(vec![1, 2, 3, 4, 5, 14])); // true
+    println!("{}", can_alice_win(vec![5, 5, 5, 25])); // true
+}
