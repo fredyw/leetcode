af92bcc952e45a636a2bfcd3ff91fe49db0276f5
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-30 15:01:04 -0500

    Solve problem 2832

--- src/main/rust/src/bin/problem2832.rs
+++ src/main/rust/src/bin/problem2832.rs
@@ -1,6 +1,46 @@
+use std::collections::VecDeque;
+
 // https://leetcode.com/problems/maximal-range-that-each-element-is-maximum-in-it/
 pub fn maximum_length_of_ranges(nums: Vec<i32>) -> Vec<i32> {
-    todo!()
+    let mut forward: Vec<i32> = vec![-1; nums.len()];
+    let mut deque: VecDeque<usize> = VecDeque::new();
+    for i in 0..nums.len() {
+        while let Some(index) = deque.back() {
+            if nums[*index] > nums[i] {
+                break;
+            }
+            forward[*index] = i as i32;
+            deque.pop_back();
+        }
+        deque.push_back(i);
+    }
+    let mut backward: Vec<i32> = vec![-1; nums.len()];
+    let mut deque: VecDeque<usize> = VecDeque::new();
+    for i in (0..nums.len()).rev() {
+        while let Some(index) = deque.back() {
+            if nums[*index] > nums[i] {
+                break;
+            }
+            backward[*index] = i as i32;
+            deque.pop_back();
+        }
+        deque.push_back(i);
+    }
+    let mut answer = vec![0; nums.len()];
+    for i in 0..answer.len() {
+        let right_length = if forward[i] == -1 {
+            (answer.len() - i) as i32
+        } else {
+            forward[i] - i as i32
+        };
+        let left_length = if backward[i] == -1 {
+            i as i32
+        } else {
+            i as i32 - (backward[i] + 1)
+        };
+        answer[i] = left_length + right_length;
+    }
+    answer
 }
 
 fn main() {
