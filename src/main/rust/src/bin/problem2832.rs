9677364997851ca0222612cd03d7aaff10a10b16
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-30 01:25:45 -0500

    Update problem 2832

--- src/main/rust/src/bin/problem2832.rs
+++ src/main/rust/src/bin/problem2832.rs
@@ -6,4 +6,5 @@ pub fn maximum_length_of_ranges(nums: Vec<i32>) -> Vec<i32> {
 fn main() {
     println!("{:?}", maximum_length_of_ranges(vec![1, 5, 4, 3, 6])); // [1,4,2,1,5]
     println!("{:?}", maximum_length_of_ranges(vec![1, 2, 3, 4, 5])); // [1,2,3,4,5]
+    println!("{:?}", maximum_length_of_ranges(vec![1, 5, 2, 4, 3])); // [1,5,1,3,1]
 }
