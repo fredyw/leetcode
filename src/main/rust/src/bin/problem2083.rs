64268e159afda638e73e2bdde3cc7f2c3ffa861c
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-30 15:04:21 -0500

    Skeleton for problem 2083

---
+++ src/main/rust/src/bin/problem2083.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/substrings-that-begin-and-end-with-the-same-letter/
+pub fn number_of_substrings(s: String) -> i64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", number_of_substrings("abcba".to_string())); // 7
+    println!("{}", number_of_substrings("abacad".to_string())); // 9
+    println!("{}", number_of_substrings("a".to_string())); // 1
+}
