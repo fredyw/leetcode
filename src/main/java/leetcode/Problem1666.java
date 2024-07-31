77be8040a3ac23c1b3507c09d7f37f51a903c643
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-07-31 13:16:43 -0500

    Skeleton for problem 1666

---
+++ src/main/java/leetcode/Problem1666.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+// https://leetcode.com/problems/change-the-root-of-a-binary-tree/
+public class Problem1666 {
+    public static class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node parent;
+    }
+
+    public Node flipBinaryTree(Node root, Node leaf) {
+        return root;
+    }
+}
