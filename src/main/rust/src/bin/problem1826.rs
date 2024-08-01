93fc2bfaac6185b862620f22f7c2075a9307764c
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-08-01 01:10:55 -0500

    Solve problem 1826

--- src/main/rust/src/bin/problem1826.rs
+++ src/main/rust/src/bin/problem1826.rs
@@ -1,6 +1,22 @@
 // https://leetcode.com/problems/faulty-sensor/description/
 pub fn bad_sensor(sensor1: Vec<i32>, sensor2: Vec<i32>) -> i32 {
-    todo!()
+    for i in 0..sensor1.len() {
+        if sensor1[i] != sensor2[i] {
+            if i == sensor1.len() - 1 {
+                break;
+            }
+            if sensor1[i] == sensor2[i + 1] && sensor2[i] == sensor1[i + 1] {
+                continue;
+            }
+            if sensor1[i] == sensor2[i + 1] {
+                return 1;
+            }
+            if sensor2[i] == sensor1[i + 1] {
+                return 2;
+            }
+        }
+    }
+    -1
 }
 
 fn main() {
@@ -10,4 +26,8 @@ fn main() {
         "{}",
         bad_sensor(vec![2, 3, 2, 2, 3, 2], vec![2, 3, 2, 3, 2, 7])
     ); // 2
+    println!(
+        "{}",
+        bad_sensor(vec![2, 3, 2, 3, 2, 7], vec![2, 3, 2, 2, 3, 2])
+    ); // 1
 }
