aca207cbe4757d70d3c3d5fe7e457f965e4d46b9
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-08-01 01:14:43 -0500

    Skeleton for problem 3237

---
+++ src/main/rust/src/bin/problem3237.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/alt-and-tab-simulation/description/
+pub fn simulation_result(windows: Vec<i32>, queries: Vec<i32>) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", simulation_result(vec![1, 2, 3], vec![3, 3, 2])); // [2,3,1]
+    println!("{:?}", simulation_result(vec![1, 4, 2, 3], vec![4, 1, 3])); // [3,1,4,2]
+}
