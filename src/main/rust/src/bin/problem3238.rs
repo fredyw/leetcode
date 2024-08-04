2c46f34cfc443d1d1579d4d98314d2f7a8415bb9
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-08-04 01:40:29 -0500

    Solve problem 3238

--- src/main/rust/src/bin/problem3238.rs
+++ src/main/rust/src/bin/problem3238.rs
@@ -1,6 +1,25 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/find-the-number-of-winning-players/
 pub fn winning_player_count(n: i32, pick: Vec<Vec<i32>>) -> i32 {
-    todo!()
+    let mut map: HashMap<i32, HashMap<i32, i32>> = HashMap::new();
+    let mut max: Vec<i32> = vec![0; n as usize];
+    for p in pick {
+        let player = p[0];
+        let color = p[1];
+        let m = map.entry(player).or_insert(HashMap::new());
+        let count = m.entry(color).or_insert(0);
+        *count += 1;
+        max[player as usize] = max[player as usize].max(*count);
+    }
+    let mut answer = 0;
+    for i in 0..n {
+        let count = max[i as usize];
+        if count >= i + 1 {
+            answer += 1;
+        }
+    }
+    answer
 }
 
 fn main() {
