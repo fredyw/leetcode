2d64220b16e30991b379fbb72696437817cd1fe1
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-08-06 14:36:41 -0500

    Solve problem 280

--- src/main/rust/src/bin/problem280.rs
+++ src/main/rust/src/bin/problem280.rs
@@ -1,6 +1,11 @@
 // https://leetcode.com/problems/wiggle-sort/
 pub fn wiggle_sort(nums: &mut Vec<i32>) {
-    todo!()
+    nums.sort();
+    let mut i = 1;
+    while i < nums.len() - 1 {
+        nums.swap(i, i + 1);
+        i += 2;
+    }
 }
 
 fn main() {
