9f25f6900a98faf47ed34966c0d8656474f1ed84
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-08-06 16:17:25 -0500

    Skeleton for problem 320

---
+++ src/main/rust/src/bin/problem320.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/generalized-abbreviation/
+pub fn generate_abbreviations(word: String) -> Vec<String> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", generate_abbreviations("word".to_string())); // ["4","3d","2r1","2rd","1o2","1o1d","1or1","1ord","w3","w2d","w1r1","w1rd","wo2","wo1d","wor1","word"]
+    println!("{:?}", generate_abbreviations("a".to_string())); // ["1","a"]
+}
