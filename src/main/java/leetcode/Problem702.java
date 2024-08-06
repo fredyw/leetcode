171cacb5d280238961e1fc91635876c84247fb02
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-08-06 00:48:00 -0500

    Skeleton for problem 702

---
+++ src/main/java/leetcode/Problem702.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+// https://leetcode.com/problems/search-in-a-sorted-array-of-unknown-size/description/
+public class Problem702 {
+    public interface ArrayReader {
+        int get(int index);
+    }
+
+    public int search(ArrayReader reader, int target) {
+        // TODO
+        return 0;
+    }
+}
