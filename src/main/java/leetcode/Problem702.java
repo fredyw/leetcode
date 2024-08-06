d18d9262b3d8338e052f905db75c9bb1d49acd4f
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-08-06 13:31:22 -0500

    Solve problem 702

--- src/main/java/leetcode/Problem702.java
+++ src/main/java/leetcode/Problem702.java
@@ -7,7 +7,23 @@ public class Problem702 {
     }
 
     public int search(ArrayReader reader, int target) {
-        // TODO
-        return 0;
+        int left = 0;
+        int right = 1;
+        while (reader.get(right) < target) {
+            left = right;
+            right <<= 1;
+        }
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (reader.get(mid) == target) {
+                return mid;
+            }
+            if (reader.get(mid) < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
     }
 }
