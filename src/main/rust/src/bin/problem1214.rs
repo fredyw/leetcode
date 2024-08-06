3cfafd65deb2a20b7d9ec109feceeda8c1c92f09
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-08-06 16:07:02 -0500

    Update problem 1214

--- src/main/rust/src/bin/problem1214.rs
+++ src/main/rust/src/bin/problem1214.rs
@@ -1,4 +1,5 @@
 use std::cell::RefCell;
+use std::collections::VecDeque;
 use std::rc::Rc;
 
 #[derive(Debug, PartialEq, Eq)]
@@ -28,4 +29,65 @@ pub fn two_sum_bs_ts(
     todo!()
 }
 
-fn main() {}
+fn create_node(value: i32) -> Option<Rc<RefCell<TreeNode>>> {
+    Some(Rc::new(RefCell::new(TreeNode::new(value))))
+}
+
+fn add_left(parent: &Option<Rc<RefCell<TreeNode>>>, value: i32) -> Option<Rc<RefCell<TreeNode>>> {
+    parent.as_ref().unwrap().borrow_mut().left = create_node(value);
+    parent.as_ref().unwrap().borrow().left.clone()
+}
+
+fn add_right(parent: &Option<Rc<RefCell<TreeNode>>>, value: i32) -> Option<Rc<RefCell<TreeNode>>> {
+    parent.as_ref().unwrap().borrow_mut().right = create_node(value);
+    parent.as_ref().unwrap().borrow().right.clone()
+}
+
+fn to_tree(nodes: Vec<Option<i32>>) -> Option<Rc<RefCell<TreeNode>>> {
+    if nodes.len() == 0 {
+        return None;
+    }
+    let mut deque: VecDeque<Option<Rc<RefCell<TreeNode>>>> = VecDeque::new();
+    let root = create_node(nodes[0].unwrap());
+    deque.push_back(root.clone());
+    let mut i = 1;
+    while i < nodes.len() {
+        let parent = deque.pop_front().unwrap();
+        let mut has_left = false;
+        if let Some(left) = nodes[i] {
+            deque.push_back(add_left(&parent, left));
+            has_left = true;
+        }
+        let mut has_right = false;
+        if i + 1 < nodes.len() {
+            if let Some(right) = nodes[i + 1] {
+                deque.push_back(add_right(&parent, right));
+                has_right = true;
+            }
+        }
+        if !has_left && !has_right {
+            deque.push_back(parent);
+        }
+        i += 2;
+    }
+    root
+}
+
+fn main() {
+    println!(
+        "{}",
+        two_sum_bs_ts(
+            to_tree(vec![Some(2), Some(1), Some(4)]),
+            to_tree(vec![Some(1), Some(0), Some(3)]),
+            5
+        )
+    ); // true
+    println!(
+        "{}",
+        two_sum_bs_ts(
+            to_tree(vec![Some(0), Some(-10), Some(10)]),
+            to_tree(vec![Some(5), Some(1), Some(7), Some(0), Some(2)]),
+            18
+        )
+    ); // false
+}
