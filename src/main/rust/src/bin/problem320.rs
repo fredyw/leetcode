31088c1280e44675c16dbcb8f2e2e3dadfa7053e
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-08-08 12:01:37 -0500

    Solve problem 320

--- src/main/rust/src/bin/problem320.rs
+++ src/main/rust/src/bin/problem320.rs
@@ -4,39 +4,50 @@ pub fn generate_abbreviations(word: String) -> Vec<String> {
         chars: &Vec<char>,
         index: usize,
         count: u32,
+        length: u32,
         can_use_number: bool,
         abbr: &mut Vec<char>,
+        answer: &mut Vec<String>,
     ) {
-        let s1: String = abbr.iter().collect();
-        let s2: String = if index == chars.len() {
-            "".to_string()
-        } else {
-            (&chars[index..]).iter().collect()
-        };
-        println!("{}{}", s1, s2);
         if index == chars.len() {
+            if length != chars.len() as u32 {
+                return;
+            }
+            let mut s: String = abbr.iter().collect();
+            if can_use_number && count - 1 > 0 {
+                s.push_str(&(count - 1).to_string());
+            }
+            answer.push(s);
             return;
         }
         if can_use_number {
-            abbr.push(char::from_digit(count, 10).unwrap());
-            generate_abbreviations(chars, index + 1, 1, false, abbr);
-            abbr.pop();
+            generate_abbreviations(chars, index + 1, count + 1, length, true, abbr, answer);
 
-            generate_abbreviations(chars, index + 1, count + 1, true, abbr);
+            for c in count.to_string().chars() {
+                abbr.push(c);
+            }
+            generate_abbreviations(chars, index + 1, 1, length + count, false, abbr, answer);
+            for _ in 0..count.to_string().len() {
+                abbr.pop();
+            }
+
+            abbr.push(chars[index]);
+            generate_abbreviations(chars, index + 1, 1, length + 1, true, abbr, answer);
+            abbr.pop();
         } else {
             abbr.push(chars[index]);
-            generate_abbreviations(chars, index + 1, 1, true, abbr);
+            generate_abbreviations(chars, index + 1, 1, length + 1, true, abbr, answer);
             abbr.pop();
         }
     }
 
+    let mut answer: Vec<String> = vec![];
     let chars: Vec<char> = word.chars().collect();
-    generate_abbreviations(&chars, 0, 1, false, &mut vec![]);
-    generate_abbreviations(&chars, 0, 1, true, &mut vec![]);
-    vec![]
+    generate_abbreviations(&chars, 0, 1, 0, true, &mut vec![], &mut answer);
+    answer
 }
 
 fn main() {
     println!("{:?}", generate_abbreviations("word".to_string())); // ["4","3d","2r1","2rd","1o2","1o1d","1or1","1ord","w3","w2d","w1r1","w1rd","wo2","wo1d","wor1","word"]
-                                                                  // println!("{:?}", generate_abbreviations("a".to_string())); // ["1","a"]
+    println!("{:?}", generate_abbreviations("a".to_string())); // ["1","a"]
 }
