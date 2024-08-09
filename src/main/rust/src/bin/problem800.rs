d15446b7ba31b11ddd217244fc4226c32bf0739d
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-08-09 01:04:14 -0500

    Skeleton for problem 800

---
+++ src/main/rust/src/bin/problem800.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/similar-rgb-color/description/
+pub fn similar_rgb(color: String) -> String {
+    todo!()
+}
+
+fn main() {
+    println!("{}", similar_rgb("#09f166".to_string())); // "#11ee66"
+    println!("{}", similar_rgb("#4e3fe1".to_string())); // "#5544dd"
+}
