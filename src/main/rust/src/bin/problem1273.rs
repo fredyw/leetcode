c2054ecefb42ab026e85481659575f54f24f6f76
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-08-10 09:20:11 -0500

    Update problem 1273

--- src/main/rust/src/bin/problem1273.rs
+++ src/main/rust/src/bin/problem1273.rs
@@ -1,6 +1,19 @@
 // https://leetcode.com/problems/delete-tree-nodes/description/
 pub fn delete_tree_nodes(nodes: i32, parent: Vec<i32>, value: Vec<i32>) -> i32 {
-    todo!()
+    fn delete_tree_node(graph: &Vec<Vec<usize>>, node: usize) {}
+
+    let mut graph: Vec<Vec<usize>> = vec![vec![]; nodes as usize];
+    let mut root = 0;
+    for (i, n) in parent.iter().enumerate() {
+        if *n == -1 {
+            root = i;
+        } else {
+            graph[*n as usize].push(i);
+        }
+    }
+
+    delete_tree_node(&graph, root);
+    0
 }
 
 fn main() {
@@ -8,8 +21,8 @@ fn main() {
         "{}",
         delete_tree_nodes(7, vec![-1, 0, 0, 1, 2, 2, 2], vec![1, -2, 4, 0, -2, -1, -1])
     ); // 2
-    println!(
-        "{}",
-        delete_tree_nodes(7, vec![-1, 0, 0, 1, 2, 2, 2], vec![1, -2, 4, 0, -2, -1, -2])
-    ); // 6
+       // println!(
+       //     "{}",
+       //     delete_tree_nodes(7, vec![-1, 0, 0, 1, 2, 2, 2], vec![1, -2, 4, 0, -2, -1, -2])
+       // ); // 6
 }
