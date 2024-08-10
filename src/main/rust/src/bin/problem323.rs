efd0d21c5c327a834224c9b341c68faba2a03741
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-08-10 02:00:43 -0500

    Solve problem 323

--- src/main/rust/src/bin/problem323.rs
+++ src/main/rust/src/bin/problem323.rs
@@ -1,6 +1,30 @@
 // https://leetcode.com/problems/number-of-connected-components-in-an-undirected-graph/description/
 pub fn count_components(n: i32, edges: Vec<Vec<i32>>) -> i32 {
-    todo!()
+    fn dfs(adj_list: &Vec<Vec<i32>>, node: i32, visited: &mut Vec<bool>) {
+        if visited[node as usize] {
+            return;
+        }
+        visited[node as usize] = true;
+        for adj in adj_list[node as usize].iter() {
+            dfs(adj_list, *adj, visited);
+        }
+    }
+
+    let mut adj_list: Vec<Vec<i32>> = vec![vec![]; n as usize];
+    for edge in edges {
+        adj_list[edge[0] as usize].push(edge[1]);
+        adj_list[edge[1] as usize].push(edge[0]);
+    }
+    let mut answer = 0;
+    let mut visited: Vec<bool> = vec![false; n as usize];
+    for i in 0..n {
+        if visited[i as usize] {
+            continue;
+        }
+        dfs(&adj_list, i, &mut visited);
+        answer += 1;
+    }
+    answer
 }
 
 fn main() {
