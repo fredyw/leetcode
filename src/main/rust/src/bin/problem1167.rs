47eab7cbdb09e1c3eeee890678b8ece17e8c0abd
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-08-11 01:08:41 -0500

    Solve problem 1167

--- src/main/rust/src/bin/problem1167.rs
+++ src/main/rust/src/bin/problem1167.rs
@@ -1,6 +1,23 @@
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
+
 // https://leetcode.com/problems/minimum-cost-to-connect-sticks/description/
 pub fn connect_sticks(sticks: Vec<i32>) -> i32 {
-    todo!()
+    let mut heap: BinaryHeap<Reverse<i32>> = BinaryHeap::new();
+    for stick in sticks {
+        heap.push(Reverse(stick));
+    }
+    let mut answer = 0;
+    while !heap.is_empty() {
+        let a = heap.pop().unwrap().0;
+        if heap.is_empty() {
+            break;
+        }
+        let b = heap.pop().unwrap().0;
+        answer += a + b;
+        heap.push(Reverse(a + b));
+    }
+    answer
 }
 
 fn main() {
