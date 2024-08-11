605d3b47840bfba256371b8ffd23b4178443f6e7
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-08-11 00:56:41 -0500

    Solve problem 3248

--- src/main/rust/src/bin/problem3248.rs
+++ src/main/rust/src/bin/problem3248.rs
@@ -1,6 +1,24 @@
 // https://leetcode.com/problems/snake-in-matrix/description/
 pub fn final_position_of_snake(n: i32, commands: Vec<String>) -> i32 {
-    todo!()
+    let mut grid: Vec<Vec<i32>> = vec![vec![0; n as usize]; n as usize];
+    let mut num = 0;
+    for i in 0..grid.len() {
+        for j in 0..grid[0].len() {
+            grid[i][j] = num;
+            num += 1;
+        }
+    }
+    let mut row = 0;
+    let mut col = 0;
+    for command in commands {
+        match command.as_str() {
+            "UP" => row -= 1,
+            "RIGHT" => col += 1,
+            "DOWN" => row += 1,
+            _ => col -= 1,
+        }
+    }
+    grid[row][col]
 }
 
 fn main() {
