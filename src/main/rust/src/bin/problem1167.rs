4a4982c776cfb1be3d0ecbabf6fc32a6b354573d
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-08-11 00:59:42 -0500

    Skeleton for problem 1167

---
+++ src/main/rust/src/bin/problem1167.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/minimum-cost-to-connect-sticks/description/
+pub fn connect_sticks(sticks: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", connect_sticks(vec![2, 4, 3])); // 14
+    println!("{}", connect_sticks(vec![1, 8, 3, 5])); // 30
+    println!("{}", connect_sticks(vec![5])); // 0
+}
