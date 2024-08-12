6ca7446d76ef912ea7cb310005bdcd53ef6585aa
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-08-12 10:47:11 -0500

    Skeleton for problem 1852

---
+++ src/main/rust/src/bin/problem1852.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/distinct-numbers-in-each-subarray/description/
+pub fn distinct_numbers(nums: Vec<i32>, k: i32) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", distinct_numbers(vec![1, 2, 3, 2, 2, 1, 3], 3)); // [3,2,2,2,3]
+    println!("{:?}", distinct_numbers(vec![1, 1, 1, 1, 2, 3, 4], 4)); // [1,2,3,4]
+}
