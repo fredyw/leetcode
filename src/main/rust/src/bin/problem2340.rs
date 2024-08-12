8eed5afc9d31c4f4e093cd6e0f8a62ab50535e43
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-08-12 11:09:03 -0500

    Skeleton for problem 2340

---
+++ src/main/rust/src/bin/problem2340.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/minimum-adjacent-swaps-to-make-a-valid-array/description/
+pub fn minimum_swaps(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", minimum_swaps(vec![3, 4, 5, 5, 3, 1])); // 6
+    println!("{}", minimum_swaps(vec![9])); // 0
+}
