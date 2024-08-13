600bdf112ec00a4c200918b288b43cae4832816e
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-08-13 17:05:26 -0500

    Skeleton for problem 2674

---
+++ src/main/java/leetcode/Problem2674.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+// https://leetcode.com/problems/split-a-circular-linked-list/
+public class Problem2674 {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode[] splitCircularLinkedList(ListNode list) {
+        // TODO
+        return null;
+    }
+}
