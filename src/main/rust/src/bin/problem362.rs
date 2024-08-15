ca675b1dd7c810d282029e36ae4c74862844eb00
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-08-15 01:34:08 -0500

    Solve problem 362

--- src/main/rust/src/bin/problem362.rs
+++ src/main/rust/src/bin/problem362.rs
@@ -20,19 +20,20 @@ impl HitCounter {
         self.count += 1;
         *self.map.entry(timestamp).or_insert(0) += 1;
         self.deque.push_back(timestamp);
+    }
+
+    fn get_hits(&mut self, timestamp: i32) -> i32 {
+        self.deque.push_back(timestamp);
         if timestamp - 300 >= 0 {
             while let Some(n) = self.deque.front() {
                 if *n > timestamp - 300 {
                     break;
                 }
-                self.count -= self.map.get(n).unwrap();
+                self.count -= self.map.get(n).unwrap_or(&0);
                 self.map.remove(n);
                 self.deque.pop_front();
             }
         }
-    }
-
-    fn get_hits(&self, timestamp: i32) -> i32 {
         self.count
     }
 }
@@ -40,10 +41,14 @@ impl HitCounter {
 fn main() {
     let mut hit_counter = HitCounter::new();
     hit_counter.hit(1);
+    hit_counter.hit(1);
     hit_counter.hit(2);
     hit_counter.hit(3);
-    println!("{}", hit_counter.get_hits(4)); // 3
+    println!("{}", hit_counter.get_hits(4)); // 4
     hit_counter.hit(300);
-    println!("{}", hit_counter.get_hits(300)); // 4
-    println!("{}", hit_counter.get_hits(301)); // 3
+    println!("{}", hit_counter.get_hits(300)); // 5
+    println!("{}", hit_counter.get_hits(310)); // 1
+    hit_counter.hit(320);
+    println!("{}", hit_counter.get_hits(369)); // 2
+    println!("{}", hit_counter.get_hits(1000)); // 0
 }
