70cdfe3a940821f945263acfca56b073a0d6c57d
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-08-14 23:08:07 -0500

    Update problem 362

--- src/main/rust/src/bin/problem362.rs
+++ src/main/rust/src/bin/problem362.rs
@@ -1,22 +1,44 @@
+use std::collections::{HashMap, VecDeque};
+
 // https://leetcode.com/problems/design-hit-counter/
-struct HitCounter {}
+struct HitCounter {
+    deque: VecDeque<i32>,
+    map: HashMap<i32, i32>,
+    count: i32,
+}
 
 impl HitCounter {
     fn new() -> Self {
-        HitCounter {}
+        HitCounter {
+            deque: VecDeque::new(),
+            map: HashMap::new(),
+            count: 0,
+        }
     }
 
-    fn hit(&self, timestamp: i32) {
-        todo!()
+    fn hit(&mut self, timestamp: i32) {
+        self.count += 1;
+        *self.map.entry(timestamp).or_insert(0) += 1;
+        self.deque.push_back(timestamp);
+        if timestamp - 300 >= 0 {
+            while let Some(n) = self.deque.front() {
+                if *n > timestamp - 300 {
+                    break;
+                }
+                self.count -= self.map.get(n).unwrap();
+                self.map.remove(n);
+                self.deque.pop_front();
+            }
+        }
     }
 
     fn get_hits(&self, timestamp: i32) -> i32 {
-        todo!()
+        self.count
     }
 }
 
 fn main() {
-    let hit_counter = HitCounter::new();
+    let mut hit_counter = HitCounter::new();
     hit_counter.hit(1);
     hit_counter.hit(2);
     hit_counter.hit(3);
