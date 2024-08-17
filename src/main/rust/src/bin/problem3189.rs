e4abb72c321613a22927c8680454d710f20dd956
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-08-17 18:06:17 -0500

    Skeleton for problem 3189

---
+++ src/main/rust/src/bin/problem3189.rs
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/minimum-moves-to-get-a-peaceful-board/description/
+pub fn min_moves(rooks: Vec<Vec<i32>>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", min_moves(vec![vec![0, 0], vec![1, 0], vec![1, 1]])); // 3
+    println!(
+        "{}",
+        min_moves(vec![vec![0, 0], vec![0, 1], vec![0, 2], vec![0, 3]])
+    ); // 3
+}
