10be280bd97d5e0c8feefed207c5fee5943618a8
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-08-21 01:34:23 -0500

    Update problem 3189

--- src/main/rust/src/bin/problem3189.rs
+++ src/main/rust/src/bin/problem3189.rs
@@ -11,33 +11,35 @@ pub fn min_moves(mut rooks: Vec<Vec<i32>>) -> i32 {
         }
     });
     let mut answer = 0;
+    println!("before {:?}", rooks);
     for i in (0..n).rev() {
-        answer += i as i32 - rooks[i][0];
+        answer += (i as i32 - rooks[i][0]).abs();
         rooks[i][0] = i as i32;
     }
     for i in (0..n).rev() {
-        answer += i as i32 - rooks[i][1];
+        answer += (i as i32 - rooks[i][1]).abs();
         rooks[i][1] = i as i32;
     }
+    println!("after  {:?}", rooks);
     answer
 }
 
 fn main() {
-    // println!("{}", min_moves(vec![vec![0, 0], vec![1, 0], vec![1, 1]])); // 3
-    // println!(
-    //     "{}",
-    //     min_moves(vec![vec![0, 0], vec![0, 1], vec![0, 2], vec![0, 3]])
-    // ); // 6
-    // println!(
-    //     "{}",
-    //     min_moves(vec![
-    //         vec![0, 0],
-    //         vec![0, 1],
-    //         vec![1, 0],
-    //         vec![1, 1],
-    //         vec![4, 4]
-    //     ])
-    // ); // 8
+    println!("{}", min_moves(vec![vec![0, 0], vec![1, 0], vec![1, 1]])); // 3
+    println!(
+        "{}",
+        min_moves(vec![vec![0, 0], vec![0, 1], vec![0, 2], vec![0, 3]])
+    ); // 6
+    println!(
+        "{}",
+        min_moves(vec![
+            vec![0, 0],
+            vec![0, 1],
+            vec![1, 0],
+            vec![1, 1],
+            vec![4, 4]
+        ])
+    ); // 8
     println!(
         "{}",
         min_moves(vec![
@@ -48,4 +50,14 @@ fn main() {
             vec![3, 2]
         ])
     ); // 8
+    println!(
+        "{}",
+        min_moves(vec![
+            vec![0, 0],
+            vec![1, 3],
+            vec![4, 1],
+            vec![2, 4],
+            vec![2, 0]
+        ])
+    ); // 3
 }
