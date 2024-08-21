f285fa774c1be28227ef5433068938734c0d6af0
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-08-21 17:47:22 -0500

    Convert problem 424 solution to Rust

--- src/main/rust/src/bin/problem424.rs
+++ src/main/rust/src/bin/problem424.rs
@@ -1,30 +1,25 @@
-use std::collections::{HashMap, VecDeque};
+use std::collections::{HashSet, VecDeque};
 
 // https://leetcode.com/problems/longest-repeating-character-replacement/description/
 pub fn character_replacement(s: String, k: i32) -> i32 {
     let chars: Vec<char> = s.chars().collect();
+    let set: HashSet<char> = s.chars().collect();
     let mut answer = 0;
-    let mut deque: VecDeque<usize> = VecDeque::new();
-    deque.push_back(0);
-    let mut map: HashMap<char, i32> = HashMap::new();
-    *map.entry(chars[0]).or_insert(0) += 1;
-    for i in 1..chars.len() {
-        println!("{i}:");
-        if chars[i - 1] != chars[i] {
-            deque.push_back(i);
-        }
-        println!(" deque: {:?}", deque);
-        *map.entry(chars[i]).or_insert(0) += 1;
-        println!(" map: {:?}", map);
-        while map.len() as i32 > k {
-            let index = deque.pop_front().unwrap();
-            let count = map.get_mut(&chars[index]).unwrap();
-            *count -= 1;
-            if *count == 0 {
-                map.remove(&chars[index]);
+    for char in set.iter() {
+        let mut deque: VecDeque<usize> = VecDeque::new();
+        let mut j = 0;
+        for i in 0..chars.len() {
+            if chars[i] != *char {
+                deque.push_back(i);
             }
+            let length = if deque.len() as i32 > k {
+                j = deque.pop_front().unwrap_or(0) + 1;
+                i as i32 - j as i32
+            } else {
+                i as i32 - j as i32 + 1
+            };
+            answer = answer.max(length);
         }
-        answer = answer.max(i as i32 - *deque.front().unwrap_or(&0) as i32 + 1);
     }
     answer
 }
@@ -36,4 +31,5 @@ fn main() {
     println!("{}", character_replacement("AB".to_string(), 2)); // 2
     println!("{}", character_replacement("AB".to_string(), 1)); // 2
     println!("{}", character_replacement("AABAABAAABAB".to_string(), 2)); // 9
+    println!("{}", character_replacement("AAAAA".to_string(), 2)); // 5
 }
