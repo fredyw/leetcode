cf9dc3b0b4badea987fbecb8b49f509c40e0338a
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-08-22 09:59:35 -0500

    Skeleton for problem 3215

---
+++ src/main/rust/src/bin/problem3215.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/count-triplets-with-even-xor-set-bits-ii/description/
+pub fn triplet_count(a: Vec<i32>, b: Vec<i32>, c: Vec<i32>) -> i64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", triplet_count(vec![1], vec![2], vec![3])); // 1
+    println!("{}", triplet_count(vec![1, 1], vec![2, 3], vec![1, 5])); // 4
+}
