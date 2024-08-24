d5dd9c42a59b887c181e192d8d406eba8477c6d6
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-08-24 14:49:52 -0500

    Skeleton for problem 3191

---
+++ src/main/rust/src/bin/problem3191.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/minimum-operations-to-make-binary-array-elements-equal-to-one-i/description/
+pub fn min_operations(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", min_operations(vec![0, 1, 1, 1, 0, 0])); // 3
+    println!("{}", min_operations(vec![0, 1, 1, 1])); // -1
+}
