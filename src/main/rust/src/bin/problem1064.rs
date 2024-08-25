16b531223c41eb734ead5b9dfd2c1deb1ddd9415
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-08-25 10:58:11 -0500

    Update problem 1064

--- src/main/rust/src/bin/problem1064.rs
+++ src/main/rust/src/bin/problem1064.rs
@@ -1,11 +1,20 @@
 // https://leetcode.com/problems/fixed-point/description/
 pub fn fixed_point(arr: Vec<i32>) -> i32 {
-    for (i, n) in arr.iter().enumerate() {
-        if i as i32 == *n {
-            return *n;
+    let mut answer = -1;
+    let mut left: i32 = 0;
+    let mut right: i32 = arr.len() as i32 - 1;
+    while left <= right {
+        let mid = left + (right - left) / 2;
+        if arr[mid as usize] == mid {
+            answer = arr[mid as usize];
+            right = mid - 1;
+        } else if arr[mid as usize] < mid {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
         }
     }
-    -1
+    answer
 }
 
 fn main() {
