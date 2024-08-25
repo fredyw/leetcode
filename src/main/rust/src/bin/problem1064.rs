abc672741f27bd48f55a2507922a855fad172255
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-08-25 10:47:32 -0500

    Skeleton for problem 1064

---
+++ src/main/rust/src/bin/problem1064.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/fixed-point/description/
+pub fn fixed_point(arr: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", fixed_point(vec![-10, -5, 0, 3, 7])); // 3
+    println!("{}", fixed_point(vec![0, 2, 5, 8, 17])); // 0
+    println!("{}", fixed_point(vec![-10, -5, 3, 4, 7, 9])); // -1
+}
