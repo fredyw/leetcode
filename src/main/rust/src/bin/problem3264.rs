bf3824875034a118a4307358f62f4135dac85af4
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-08-25 01:33:54 -0500

    Solve problem 3264

--- src/main/rust/src/bin/problem3264.rs
+++ src/main/rust/src/bin/problem3264.rs
@@ -1,6 +1,18 @@
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
+
 // https://leetcode.com/problems/final-array-state-after-k-multiplication-operations-i/description/
-pub fn get_final_state(nums: Vec<i32>, k: i32, multiplier: i32) -> Vec<i32> {
-    todo!()
+pub fn get_final_state(mut nums: Vec<i32>, k: i32, multiplier: i32) -> Vec<i32> {
+    let mut heap: BinaryHeap<Reverse<(i32, usize)>> = BinaryHeap::new();
+    for (i, num) in nums.iter().enumerate() {
+        heap.push(Reverse((*num, i)));
+    }
+    for _ in 0..k {
+        let (n, i) = heap.pop().unwrap().0;
+        nums[i] *= multiplier;
+        heap.push(Reverse((nums[i], i)));
+    }
+    nums
 }
 
 fn main() {
