0332ee018fd56cf670a098390b45bea7d7aae6e9
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-08-25 01:22:47 -0500

    Skeleton for problem 3264

---
+++ src/main/rust/src/bin/problem3264.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/final-array-state-after-k-multiplication-operations-i/description/
+pub fn get_final_state(nums: Vec<i32>, k: i32, multiplier: i32) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", get_final_state(vec![2, 1, 3, 5, 6], 5, 2)); // [8,4,6,5,6]
+    println!("{:?}", get_final_state(vec![1, 2], 3, 4)); // [16,8]
+}
