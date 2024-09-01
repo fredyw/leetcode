e83f9f1b35bb095e04690a4ec65549be60714e6c
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-08-31 23:30:06 -0500

    Skeleton for problem 3270

---
+++ src/main/rust/src/bin/problem3270.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/find-the-key-of-the-numbers/description/
+pub fn generate_key(num1: i32, num2: i32, num3: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", generate_key(1, 10, 1000)); // 0
+    println!("{}", generate_key(987, 879, 798)); // 777
+    println!("{}", generate_key(1, 2, 3)); // 1
+}
