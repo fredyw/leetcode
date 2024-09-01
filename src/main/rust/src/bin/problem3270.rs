7f20ae809394dce1d48a2796c6236b26d3686900
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-08-31 23:38:55 -0500

    Solve problem 3270

--- src/main/rust/src/bin/problem3270.rs
+++ src/main/rust/src/bin/problem3270.rs
@@ -1,6 +1,17 @@
 // https://leetcode.com/problems/find-the-key-of-the-numbers/description/
 pub fn generate_key(num1: i32, num2: i32, num3: i32) -> i32 {
-    todo!()
+    let mut answer = String::new();
+    let num1 = format!("{:04}", num1);
+    let num1 = num1.as_bytes();
+    let num2 = format!("{:04}", num2);
+    let num2 = num2.as_bytes();
+    let num3 = format!("{:04}", num3);
+    let num3 = num3.as_bytes();
+    for i in 0..4 {
+        let min = num1[i].min(num2[i].min(num3[i])) - '0' as u8;
+        answer.push_str(&min.to_string());
+    }
+    answer.parse().unwrap()
 }
 
 fn main() {
