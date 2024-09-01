f53553d67daa713d8ad4814557da25e221c3ccbc
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-08-31 23:40:24 -0500

    Skeleton for problem 3274

---
+++ src/main/rust/src/bin/problem3274.rs
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/check-if-two-chessboard-squares-have-the-same-color/description/
+pub fn check_two_chessboards(coordinate1: String, coordinate2: String) -> bool {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        check_two_chessboards("a1".to_string(), "c3".to_string())
+    ); // true
+    println!(
+        "{}",
+        check_two_chessboards("a1".to_string(), "h3".to_string())
+    ); // false
+}
