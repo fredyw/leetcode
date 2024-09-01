9078c42d1961ee5f21b90ab23fde2bd0bc4fa188
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-09-01 00:39:19 -0500

    Solve problem 2863

--- src/main/rust/src/bin/problem2863.rs
+++ src/main/rust/src/bin/problem2863.rs
@@ -2,32 +2,32 @@ use std::collections::VecDeque;
 
 // https://leetcode.com/problems/maximum-length-of-semi-decreasing-subarrays/description/
 pub fn max_subarray_length(nums: Vec<i32>) -> i32 {
-    let mut next_smaller_elements = vec![-1; nums.len()];
     let mut deque: VecDeque<usize> = VecDeque::new();
-    for (i, n) in nums.iter().enumerate() {
-        while !deque.is_empty() && nums[*deque.front().unwrap()] > nums[i] {
-            let index = deque.pop_front().unwrap();
-            next_smaller_elements[index] = nums[i];
+    for i in (0..nums.len()).rev() {
+        if deque.is_empty() || nums[*deque.back().unwrap()] > nums[i] {
+            deque.push_back(i);
         }
-        deque.push_back(i);
     }
-    println!("{:?}", next_smaller_elements);
-    let mut new_nums: Vec<(i32, usize)> = nums.iter().enumerate().map(|(i, n)| (*n, i)).collect();
-    new_nums.sort_by(|a, b| b.0.cmp(&a.0));
-    println!("{:?}", new_nums);
-    0
+    let mut answer = 0;
+    for i in 0..nums.len() {
+        while !deque.is_empty() && nums[*deque.back().unwrap()] < nums[i] {
+            let j = deque.pop_back().unwrap();
+            answer = answer.max(j as i32 - i as i32 + 1);
+        }
+    }
+    answer
 }
 
 fn main() {
-    // println!(
-    //     "{}",
-    //     max_subarray_length(vec![7, 6, 5, 4, 3, 2, 1, 6, 10, 11])
-    // ); // 8
-    // println!(
-    //     "{}",
-    //     max_subarray_length(vec![57, 55, 50, 60, 61, 58, 63, 59, 64, 60, 63])
-    // ); // 6
-    // println!("{}", max_subarray_length(vec![1, 2, 3, 4])); // 0
+    println!(
+        "{}",
+        max_subarray_length(vec![7, 6, 5, 4, 3, 2, 1, 6, 10, 11])
+    ); // 8
+    println!(
+        "{}",
+        max_subarray_length(vec![57, 55, 50, 60, 61, 58, 63, 59, 64, 60, 63])
+    ); // 6
+    println!("{}", max_subarray_length(vec![1, 2, 3, 4])); // 0
     println!(
         "{}",
         max_subarray_length(vec![7, 6, 5, 4, 3, 2, 1, 6, 1, 10, 11])
