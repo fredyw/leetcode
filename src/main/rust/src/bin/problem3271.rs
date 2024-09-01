98caee0862dc542c4c8683ea02951094c7c4489b
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-09-01 08:41:08 -0500

    SKeleton for problem 3271

---
+++ src/main/rust/src/bin/problem3271.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/hash-divided-string/description/
+pub fn string_hash(s: String, k: i32) -> String {
+    todo!()
+}
+
+fn main() {
+    println!("{}", string_hash("abcd".to_string(), 2)); // "bf"
+    println!("{}", string_hash("mxz".to_string(), 3)); // "i"
+}
