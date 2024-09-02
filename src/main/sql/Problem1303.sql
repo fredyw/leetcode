ef37c093218fbe721593953d26749e1b2280cd6e
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-09-02 16:18:23 -0500

    Solve problem 1303

--- src/main/sql/Problem1303.sql
+++ src/main/sql/Problem1303.sql
@@ -1,1 +1,9 @@
 -- https://leetcode.com/problems/find-the-team-size/description/
+with team_count as (
+    select team_id, count(*) as team_size
+    from Employee
+    group by team_id
+)
+select e.employee_id, c.team_size
+from Employee e  join team_count c
+     on e.team_id = c.team_id;
