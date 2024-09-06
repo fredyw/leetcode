be5cd4e2444318749872b48542e982ed3beab4b8
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-09-06 00:51:40 -0500

    Skeleton for problem 2992

---
+++ src/main/rust/src/bin/problem2992.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/number-of-self-divisible-permutations/description/
+pub fn self_divisible_permutation_count(n: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", self_divisible_permutation_count(1)); // 1
+    println!("{}", self_divisible_permutation_count(2)); // 1
+    println!("{}", self_divisible_permutation_count(3)); // 3
+}
