210d2cfc0394d670ef77371dce3729f375206ade
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-09-05 22:01:44 -0500

    Solve problem 3150

--- src/main/sql/Problem3150.sql
+++ src/main/sql/Problem3150.sql
@@ -1,1 +1,7 @@
 -- https://leetcode.com/problems/invalid-tweets-ii/description/
+select tweet_id
+from Tweets
+where length(content) > 140
+      and regexp_count(content, '@') > 3
+      and regexp_count(content, '#') > 3
+order by tweet_id;
