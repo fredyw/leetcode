7b70163defd3c5007782937a7e6a08f2c488a68e
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-09-06 01:02:15 -0500

    Update problem 2992

--- src/main/rust/src/bin/problem2992.rs
+++ src/main/rust/src/bin/problem2992.rs
@@ -1,5 +1,13 @@
 // https://leetcode.com/problems/number-of-self-divisible-permutations/description/
 pub fn self_divisible_permutation_count(n: i32) -> i32 {
+    fn gcd(a: i32, b: i32) -> i32 {
+        if b == 0 {
+            a
+        } else {
+            gcd(b, a % b)
+        }
+    }
+
     todo!()
 }
 
