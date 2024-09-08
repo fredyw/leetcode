fc24cbabe0f936c897531d409f9118f936a2bda6
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-09-08 08:49:08 -0500

    Solve problem 3192

--- src/main/rust/src/bin/problem3192.rs
+++ src/main/rust/src/bin/problem3192.rs
@@ -1,6 +1,18 @@
 // https://leetcode.com/problems/minimum-operations-to-make-binary-array-elements-equal-to-one-ii/description/
 pub fn min_operations(nums: Vec<i32>) -> i32 {
-    todo!()
+    let mut answer = 0;
+    let mut num_flips = 0;
+    for n in nums.iter() {
+        let mut n = *n;
+        if num_flips % 2 != 0 {
+            n = if n == 0 { 1 } else { 0 };
+        }
+        if n == 0 {
+            num_flips += 1;
+            answer += 1;
+        }
+    }
+    answer
 }
 
 fn main() {
