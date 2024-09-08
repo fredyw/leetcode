93b75858a5d77623ccab0d81eb52648e626c4ceb
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-09-08 00:39:34 -0500

    Skeleton for problem 3280

---
+++ src/main/rust/src/bin/problem3280.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/convert-date-to-binary/description/
+pub fn convert_date_to_binary(date: String) -> String {
+    todo!()
+}
+
+fn main() {
+    println!("{}", convert_date_to_binary("2080-02-29".to_string())); // "100000100000-10-11101"
+    println!("{}", convert_date_to_binary("1900-01-01".to_string())); // "11101101100-1-1"
+}
