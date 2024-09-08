2f6c03114864cfe6809031568593b7753fb441bb
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-09-08 08:50:48 -0500

    Skeleton for problem 666

---
+++ src/main/rust/src/bin/problem666.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/path-sum-iv/description/
+pub fn path_sum(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", path_sum(vec![113, 215, 221])); // 12
+    println!("{}", path_sum(vec![113, 221])); // 4
+}
