11520f47c8cd8d85ee47236df30112a219542047
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-09-09 09:06:13 -0500

    Skeleton for problem 510

---
+++ src/main/java/leetcode/Problem510.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+// https://leetcode.com/problems/inorder-successor-in-bst-ii/description/
+public class Problem510 {
+    public static class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node parent;
+    }
+
+    public Node inorderSuccessor(Node node) {
+        // TODO
+        return null;
+    }
+}
