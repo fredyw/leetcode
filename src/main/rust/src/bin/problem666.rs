c431726524165f712601b96178d371719265872d
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-09-09 00:16:12 -0500

    Update problem 666

--- src/main/rust/src/bin/problem666.rs
+++ src/main/rust/src/bin/problem666.rs
@@ -6,4 +6,6 @@ pub fn path_sum(nums: Vec<i32>) -> i32 {
 fn main() {
     println!("{}", path_sum(vec![113, 215, 221])); // 12
     println!("{}", path_sum(vec![113, 221])); // 4
+    println!("{}", path_sum(vec![113, 212, 221, 331, 341])); // 15
+    println!("{}", path_sum(vec![113, 212, 221, 311, 321, 331, 341])); // 22
 }
