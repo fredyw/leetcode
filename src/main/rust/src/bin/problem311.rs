14d539b5e0d347d0a8791dfbd5a0df7f20c60450
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-09-11 18:41:54 -0500

    Update problem 311

--- src/main/rust/src/bin/problem311.rs
+++ src/main/rust/src/bin/problem311.rs
@@ -1,6 +1,15 @@
 // https://leetcode.com/problems/sparse-matrix-multiplication/
 pub fn multiply(mat1: Vec<Vec<i32>>, mat2: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-    todo!()
+    let mut sparse_mat2: Vec<Vec<i32>> = vec![vec![]; mat2[0].len()];
+    for col in 0..mat2[0].len() {
+        for row in 0..mat2.len() {
+            if mat2[row][col] != 0 {
+                sparse_mat2[col].push(mat2[row][col]);
+            }
+        }
+    }
+    let mut answer = vec![vec![0; mat2[0].len()]; mat1.len()];
+    answer
 }
 
 fn main() {
@@ -12,4 +21,8 @@ fn main() {
         )
     ); // [[7,0,0],[-7,0,3]]
     println!("{:?}", multiply(vec![vec![0]], vec![vec![0]])); // [[0]]
+    println!(
+        "{:?}",
+        multiply(vec![vec![1, -5]], vec![vec![12], vec![-1]])
+    ); // [[17]]
 }
