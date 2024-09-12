bb3c293f2110996d55166b18c4692f214293605b
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-09-12 01:08:56 -0500

    Solve problem 311

--- src/main/rust/src/bin/problem311.rs
+++ src/main/rust/src/bin/problem311.rs
@@ -1,22 +1,37 @@
+use std::collections::HashSet;
+
 // https://leetcode.com/problems/sparse-matrix-multiplication/
 pub fn multiply(mat1: Vec<Vec<i32>>, mat2: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-    let mut sparse_mat1: Vec<Vec<usize>> = vec![vec![]; mat1.len()];
+    let mut sparse_mat1: Vec<HashSet<usize>> = vec![HashSet::new(); mat1.len()];
     for row in 0..mat1.len() {
         for col in 0..mat1[row].len() {
             if mat1[row][col] != 0 {
-                sparse_mat1[row].push(col);
+                sparse_mat1[row].insert(col);
             }
         }
     }
-    let mut sparse_mat2: Vec<Vec<usize>> = vec![vec![]; mat2[0].len()];
+    let mut sparse_mat2: Vec<HashSet<usize>> = vec![HashSet::new(); mat2[0].len()];
     for col in 0..mat2[0].len() {
         for row in 0..mat2.len() {
             if mat2[row][col] != 0 {
-                sparse_mat2[col].push(row);
+                sparse_mat2[col].insert(row);
             }
         }
     }
     let mut answer = vec![vec![0; mat2[0].len()]; mat1.len()];
+    for row in 0..sparse_mat1.len() {
+        let cols = &sparse_mat1[row];
+        for col in 0..sparse_mat2.len() {
+            let rows = &sparse_mat2[col];
+            let mut dot_product = 0;
+            for c in cols.iter() {
+                if rows.contains(c) {
+                    dot_product += mat1[row][*c] * mat2[*c][col];
+                }
+            }
+            answer[row][col] = dot_product;
+        }
+    }
     answer
 }
 
