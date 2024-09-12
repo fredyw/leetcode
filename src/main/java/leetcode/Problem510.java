6ed4c04520e0ef636921f56af4e048f0935916c4
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-09-12 11:10:40 -0500

    Solve problem 510

--- src/main/java/leetcode/Problem510.java
+++ src/main/java/leetcode/Problem510.java
@@ -10,7 +10,35 @@ public class Problem510 {
     }
 
     public Node inorderSuccessor(Node node) {
-        // TODO
-        return null;
+        Node root = findRoot(node);
+        var answer = new Ref<Node>();
+        inOrderSuccessor(root, node.val, answer);
+        return answer.val;
+    }
+
+    private static Node findRoot(Node node) {
+        Node n = node;
+        while (n.parent != null) {
+            n = n.parent;
+        }
+        return n;
+    }
+
+    private static class Ref<T> {
+        private T val;
+    }
+
+    private static void inOrderSuccessor(Node root, int node, Ref<Node> answer) {
+        if (root == null) {
+            return;
+        }
+        inOrderSuccessor(root.left, node, answer);
+        if (root.val > node) {
+            if (answer.val == null) {
+                answer.val = root;
+            }
+            return;
+        }
+        inOrderSuccessor(root.right, node, answer);
     }
 }
