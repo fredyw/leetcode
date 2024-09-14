d315a85165e3720fc8c03eceebda66ad94e8230f
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-09-14 08:50:02 -0500

    Skeleton for problem 2436

---
+++ src/main/rust/src/bin/problem2436.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/minimum-split-into-subarrays-with-gcd-greater-than-one/
+pub fn minimum_splits(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", minimum_splits(vec![12, 6, 3, 14, 8])); // 2
+    println!("{}", minimum_splits(vec![4, 12, 6, 14])); // 1
+}
