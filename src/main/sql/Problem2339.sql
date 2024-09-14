94e3b7df8b8f0f8ef5c70fb252454bd2d5301ad0
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-09-14 08:44:53 -0500

    Solve problem 2339

--- src/main/sql/Problem2339.sql
+++ src/main/sql/Problem2339.sql
@@ -1,1 +1,4 @@
 -- https://leetcode.com/problems/all-the-matches-of-the-league/description/
+select t1.team_name as home_team, t2.team_name as away_team
+from Teams t1, Teams t2
+where t1.team_name != t2.team_name;
