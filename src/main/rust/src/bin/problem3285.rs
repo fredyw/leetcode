0cbd4d3b6fb36bfab88acde37163d1ef826a3285
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-09-15 01:11:25 -0500

    Solve problem 3285

--- src/main/rust/src/bin/problem3285.rs
+++ src/main/rust/src/bin/problem3285.rs
@@ -1,6 +1,12 @@
 // https://leetcode.com/problems/find-indices-of-stable-mountains/description/
 pub fn stable_mountains(height: Vec<i32>, threshold: i32) -> Vec<i32> {
-    todo!()
+    let mut answer = vec![];
+    for i in 1..height.len() {
+        if height[i - 1] > threshold {
+            answer.push(i as i32);
+        }
+    }
+    answer
 }
 
 fn main() {
