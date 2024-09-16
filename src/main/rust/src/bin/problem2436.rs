3fc57977649d9be0d8628496dcb70e4726b93e59
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-09-15 19:00:31 -0500

    Solve problem 2436

--- src/main/rust/src/bin/problem2436.rs
+++ src/main/rust/src/bin/problem2436.rs
@@ -8,14 +8,21 @@ pub fn minimum_splits(nums: Vec<i32>) -> i32 {
         }
     }
 
-    let mut gcd = nums[0];
+    let mut answer = 1;
+    let mut g = nums[0];
     for i in 1..nums.len() {
-        gcd = gcd(gcd, nums[i]);
+        g = gcd(g, nums[i]);
+        if g == 1 {
+            g = nums[i];
+            answer += 1;
+        }
     }
-    gcd
+    answer
 }
 
 fn main() {
     println!("{}", minimum_splits(vec![12, 6, 3, 14, 8])); // 2
     println!("{}", minimum_splits(vec![4, 12, 6, 14])); // 1
+    println!("{}", minimum_splits(vec![3])); // 1
+    println!("{}", minimum_splits(vec![2, 3, 4, 5, 6, 7, 8])); // 7
 }
