268342000e2c3d3bc203c2173737760be5448f8f
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-09-17 09:05:48 -0500

    Solve problem 2985

--- src/main/sql/Problem2985.sql
+++ src/main/sql/Problem2985.sql
@@ -1,2 +1,4 @@
 -- https://leetcode.com/problems/calculate-compressed-mean/description/
-
+select round(sum(item_count * order_occurrences) / cast(sum(order_occurrences) as decimal), 2)
+       as average_items_per_order
+from Orders;
