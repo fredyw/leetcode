e6a6b66582550dc003e627c09a6a0e5f03365a31
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-09-19 00:58:43 -0500

    Solve problem 1571

--- src/main/sql/Problem1571.sql
+++ src/main/sql/Problem1571.sql
@@ -1,1 +1,6 @@
 -- https://leetcode.com/problems/warehouse-manager/description/
+select w.name as warehouse_name,
+       sum(w.units * p.width * p.length * p.height) as volume
+from Warehouse w join Products p
+     on (w.product_id = p.product_id)
+group by w.name;
