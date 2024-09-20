c515a66191716ac39201698be231cdc32059550d
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-09-20 00:27:04 -0500

    Update problem 2387

--- src/main/rust/src/bin/problem2387.rs
+++ src/main/rust/src/bin/problem2387.rs
@@ -9,4 +9,8 @@ fn main() {
         matrix_median(vec![vec![1, 1, 2], vec![2, 3, 3], vec![1, 3, 4]])
     ); // 2
     println!("{}", matrix_median(vec![vec![1, 1, 3, 3, 4]])); // 3
+    println!(
+        "{}",
+        matrix_median(vec![vec![1, 1, 1], vec![9, 9, 9], vec![7, 8, 8]])
+    ); // 8
 }
