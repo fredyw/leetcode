dfbc3a5a884528562ee9232ad769d3fe37f6be0a
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-09-21 08:52:32 -0500

    Skeleton for problem 1885

---
+++ src/main/rust/src/bin/problem1885.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/count-pairs-in-two-arrays/description/
+pub fn count_pairs(nums1: Vec<i32>, nums2: Vec<i32>) -> i64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", count_pairs(vec![2, 1, 2, 1], vec![1, 2, 1, 2])); // 1
+    println!("{}", count_pairs(vec![1, 10, 6, 2], vec![1, 4, 1, 5])); // 5
+}
