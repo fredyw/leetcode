576ca301242c51a8f1a06914b2c5fdd69db8d286
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-09-21 01:44:13 -0500

    Solve problem 2026

--- src/main/sql/problem2026.sql
+++ src/main/sql/problem2026.sql
@@ -1,1 +1,5 @@
 -- https://leetcode.com/problems/low-quality-problems/description/
+select problem_id
+from Problems
+where (likes / (cast(likes as float) + cast(dislikes as float))) * 100 < 60
+order by problem_id asc;
