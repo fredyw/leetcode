b6f48fdf2b278dd3d80741d6e68bbc1d8dfdab93
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-09-20 19:46:38 -0500

    Skeleton for problem 3294

---
+++ src/main/java/leetcode/Problem3294.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+// https://leetcode.com/problems/convert-doubly-linked-list-to-array-ii/
+public class Problem3294 {
+    private static class Node {
+        public int val;
+        public Node prev;
+        public Node next;
+    }
+
+    public int[] toArray(Node node) {
+        // TODO
+        return null;
+    }
+}
