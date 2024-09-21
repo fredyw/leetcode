cb0411e7a86c5f5bd5be9868bf78321cb288e758
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-09-21 09:21:30 -0500

    Update problem 2387

--- src/main/rust/src/bin/problem2387.rs
+++ src/main/rust/src/bin/problem2387.rs
@@ -1,12 +1,41 @@
 // https://leetcode.com/problems/median-of-a-row-wise-sorted-matrix/description/
 pub fn matrix_median(grid: Vec<Vec<i32>>) -> i32 {
+    fn get_element_count(v: &Vec<i32>, value: i32) -> i32 {
+        let mut left: i32 = 0;
+        let mut right = v.len() as i32 - 1;
+        while left <= right {
+            let mid = left + (right - left) / 2;
+            if v[mid as usize] <= value {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        left
+    }
+
     let mut min = i32::MAX;
     let mut max = i32::MIN;
     for i in 0..grid.len() {
         min = min.min(grid[i][0]);
         max = max.max(grid[i][grid[i].len() - 1]);
     }
-    0
+    let median = (grid.len() * grid[0].len() / 2) as i32;
+    let mut low = min;
+    let mut high = max;
+    while low <= high {
+        let mid = low + (high - low) / 2;
+        let mut count = 0;
+        for i in 0..grid.len() {
+            count += get_element_count(&grid[i], mid);
+        }
+        if count <= median {
+            low += mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    low
 }
 
 fn main() {
