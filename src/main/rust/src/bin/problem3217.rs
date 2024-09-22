ed389584e605698e46ec727d742d568ba3d759fd
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-09-22 01:00:46 -0500

    Solve problem 3217

--- src/main/rust/src/bin/problem3217.rs
+++ src/main/rust/src/bin/problem3217.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub struct ListNode {
     pub val: i32,
@@ -13,7 +15,81 @@ impl ListNode {
 
 // https://leetcode.com/problems/delete-nodes-from-linked-list-present-in-array/
 pub fn modified_list(nums: Vec<i32>, head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-    todo!()
+    fn to_linked_list(v: Vec<i32>) -> Option<Box<ListNode>> {
+        let mut head: Option<Box<ListNode>> = None;
+        let mut current = &mut head;
+        for i in v.iter() {
+            match current {
+                Some(a) => {
+                    a.next = Some(Box::new(ListNode::new(*i)));
+                    current = &mut a.next;
+                }
+                None => {
+                    head = Some(Box::new(ListNode::new(*i)));
+                    current = &mut head;
+                }
+            }
+        }
+        head
+    }
+
+    let nums: HashSet<i32> = nums.into_iter().collect();
+    let mut node = head;
+    let mut v: Vec<i32> = vec![];
+    while let Some(n) = node {
+        if !nums.contains(&n.val) {
+            v.push(n.val);
+        }
+        node = n.next
+    }
+    to_linked_list(v)
 }
 
-fn main() {}
+fn to_linked_list(v: Vec<i32>) -> Option<Box<ListNode>> {
+    let mut head: Option<Box<ListNode>> = None;
+    let mut current = &mut head;
+    for i in v.iter() {
+        match current {
+            Some(a) => {
+                a.next = Some(Box::new(ListNode::new(*i)));
+                current = &mut a.next;
+            }
+            None => {
+                head = Some(Box::new(ListNode::new(*i)));
+                current = &mut head;
+            }
+        }
+    }
+    head
+}
+
+fn to_vec(node: Option<Box<ListNode>>) -> Vec<i32> {
+    let mut node = node;
+    let mut v: Vec<i32> = vec![];
+    while let Some(n) = node {
+        v.push(n.val);
+        node = n.next
+    }
+    v
+}
+
+fn main() {
+    println!(
+        "{:?}",
+        to_vec(modified_list(
+            vec![1, 2, 3],
+            to_linked_list(vec![1, 2, 3, 4, 5])
+        ))
+    ); // [4,5]
+    println!(
+        "{:?}",
+        to_vec(modified_list(
+            vec![1],
+            to_linked_list(vec![1, 2, 1, 2, 1, 2])
+        ))
+    ); // [2,2,2]
+    println!(
+        "{:?}",
+        to_vec(modified_list(vec![5], to_linked_list(vec![1, 2, 3, 4])))
+    ); // [1,2,3,4]
+}
