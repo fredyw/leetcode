6a0f2d1886638a5aaffac57bd030354f3ac274de
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-09-22 10:16:56 -0500

    Update problem 1885

--- src/main/rust/src/bin/problem1885.rs
+++ src/main/rust/src/bin/problem1885.rs
@@ -1,9 +1,17 @@
 // https://leetcode.com/problems/count-pairs-in-two-arrays/description/
 pub fn count_pairs(nums1: Vec<i32>, nums2: Vec<i32>) -> i64 {
-    let mut answer = 0;
+    // nums1[i] + nums1[j] > nums2[i] + nums2[j]
+    // nums1[i] - nums2[i] > nums2[j] - nums1[j]
+    let mut diff1 = vec![0; nums1.len()];
+    let mut diff2 = vec![0; nums2.len()];
     for i in 0..nums1.len() {
-        for j in i + 1..nums1.len() {
-            if nums1[i] + nums1[j] > nums2[i] + nums2[j] {
+        diff1[i] = nums1[i] - nums2[i];
+        diff2[i] = nums2[i] - nums1[i];
+    }
+    let mut answer = 0;
+    for i in 0..diff1.len() {
+        for j in i + 1..diff2.len() {
+            if diff1[i] > diff2[j] {
                 answer += 1;
             }
         }
