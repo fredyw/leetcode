f6519214bce445e99b0bed63170957c16a4cdba2
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-09-23 00:34:48 -0500

    Solve problem 1885

--- src/main/rust/src/bin/problem1885.rs
+++ src/main/rust/src/bin/problem1885.rs
@@ -1,19 +1,36 @@
 // https://leetcode.com/problems/count-pairs-in-two-arrays/description/
 pub fn count_pairs(nums1: Vec<i32>, nums2: Vec<i32>) -> i64 {
-    // nums1[i] + nums1[j] > nums2[i] + nums2[j]
-    // nums1[i] - nums2[i] > nums2[j] - nums1[j]
-    let mut diff1 = vec![0; nums1.len()];
-    let mut diff2 = vec![0; nums2.len()];
-    for i in 0..nums1.len() {
-        diff1[i] = nums1[i] - nums2[i];
-        diff2[i] = nums2[i] - nums1[i];
-    }
-    let mut answer = 0;
-    for i in 0..diff1.len() {
-        for j in i + 1..diff2.len() {
-            if diff1[i] > diff2[j] {
-                answer += 1;
+    fn greater_than_count(v: &[i32], target: i32) -> i64 {
+        let mut low: isize = 0;
+        let mut high: isize = v.len() as isize;
+        while low <= high {
+            let mid = low + (high - low) / 2;
+            if mid as usize >= v.len() {
+                break;
             }
+            if v[mid as usize] <= target {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        v.len() as i64 - low as i64
+    }
+
+    // nums1[i] + nums1[j] > nums2[i] + nums2[j]
+    // (nums1[i] - nums2[i]) + (nums1[j] - nums2[j]) > 0
+    // diff[i] + diff[j] > 0
+    let mut diff = vec![0; nums1.len()];
+    for i in 0..nums1.len() {
+        diff[i] = nums1[i] - nums2[i];
+    }
+    diff.sort();
+    let mut answer = 0;
+    for i in 0..diff.len() - 1 {
+        if diff[i] > 0 {
+            answer += (diff.len() - (i + 1)) as i64;
+        } else {
+            answer += greater_than_count(&diff[i + 1..], diff[i].abs());
         }
     }
     answer
