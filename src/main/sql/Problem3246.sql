8cfd8c079906c63d5f1afde04e83b298e0882bcb
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-09-24 00:55:25 -0500

    Solve problem 3246

--- src/main/sql/Problem3246.sql
+++ src/main/sql/Problem3246.sql
@@ -2,6 +2,7 @@
 select
     team_id,
     team_name,
-    sum(wins * 3 + draws) as points
+    (wins * 3) + draws as points,
+    rank() over(order by (wins * 3) + draws desc) as position
 from TeamStats
 order by points desc, team_name asc;
