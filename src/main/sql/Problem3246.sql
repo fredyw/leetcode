b11acd81dee0e4f2995041dfaad3df669bca633b
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-09-24 00:51:49 -0500

    Update problem 3246

--- src/main/sql/Problem3246.sql
+++ src/main/sql/Problem3246.sql
@@ -1,1 +1,7 @@
 -- https://leetcode.com/problems/premier-league-table-ranking/description/
+select
+    team_id,
+    team_name,
+    sum(wins * 3 + draws) as points
+from TeamStats
+order by points desc, team_name asc;
