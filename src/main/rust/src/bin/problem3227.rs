a2911a71a21bda60e06e3ddaeb819ed0f5eb2600
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-09-24 14:53:38 -0500

    Solve problem 3227

--- src/main/rust/src/bin/problem3227.rs
+++ src/main/rust/src/bin/problem3227.rs
@@ -1,6 +1,11 @@
 // https://leetcode.com/problems/vowels-game-in-a-string/description/
 pub fn does_alice_win(s: String) -> bool {
-    todo!()
+    for c in s.chars() {
+        if c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' {
+            return true;
+        }
+    }
+    false
 }
 
 fn main() {
@@ -8,4 +13,5 @@ fn main() {
     println!("{}", does_alice_win("bbcd".to_string())); // false
     println!("{}", does_alice_win("a".to_string())); // true
     println!("{}", does_alice_win("b".to_string())); // false
+    println!("{}", does_alice_win("leetcodera".to_string())); // true
 }
