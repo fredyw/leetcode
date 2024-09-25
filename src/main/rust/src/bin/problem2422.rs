4d744ff8ee1a89002c2fe565137a970def2bd863
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-09-25 01:17:38 -0500

    Skeleton for problem 2422

---
+++ src/main/rust/src/bin/problem2422.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/merge-operations-to-turn-array-into-a-palindrome/description/
+pub fn minimum_operations(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", minimum_operations(vec![4, 3, 2, 1, 2, 3, 1])); // 2
+    println!("{}", minimum_operations(vec![1, 2, 3, 4])); // 3
+}
