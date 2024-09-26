6dab28b9273642f24c19c0c98ee48a9c07c8ea50
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-09-26 00:59:10 -0500

    Update problem 2422

--- src/main/rust/src/bin/problem2422.rs
+++ src/main/rust/src/bin/problem2422.rs
@@ -1,10 +1,36 @@
 // https://leetcode.com/problems/merge-operations-to-turn-array-into-a-palindrome/description/
-pub fn minimum_operations(nums: Vec<i32>) -> i32 {
-    todo!()
+pub fn minimum_operations(mut nums: Vec<i32>) -> i32 {
+    let mut answer = 0;
+    let mut i = 0;
+    let mut j = nums.len() - 1;
+    while i < j {
+        if nums[i] == nums[j] {
+            i += 1;
+            j -= 1;
+        } else if nums[i] == nums[j] + nums[j - 1] {
+            nums[j - 1] += nums[j];
+            i += 1;
+            j -= 2;
+            answer += 1;
+        } else if nums[i] + nums[i + 1] == nums[j] {
+            nums[i + 1] += nums[i];
+            i += 2;
+            j -= 1;
+            answer += 1;
+        } else {
+            nums[i + 1] += nums[i];
+            i += 1;
+            answer += 1;
+        }
+    }
+    answer
 }
 
 fn main() {
     println!("{}", minimum_operations(vec![4, 3, 2, 1, 2, 3, 1])); // 2
     println!("{}", minimum_operations(vec![1, 2, 3, 4])); // 3
     println!("{}", minimum_operations(vec![1, 2, 3, 4, 3])); // 2
+    println!("{}", minimum_operations(vec![1])); // 0
+    println!("{}", minimum_operations(vec![1, 1])); // 0
+    println!("{}", minimum_operations(vec![1, 2])); // 1
 }
