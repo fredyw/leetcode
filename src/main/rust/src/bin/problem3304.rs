6f564cc5744e4bbff5548cdd25eaa1d14119c919
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-09-29 01:40:43 -0500

    Skeleton for problem 3304

---
+++ src/main/rust/src/bin/problem3304.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/find-the-k-th-character-in-string-game-i/description/
+pub fn kth_character(k: i32) -> char {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", kth_character(5)); // "b"
+    println!("{:?}", kth_character(10)); // "c"
+}
