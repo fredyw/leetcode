f8b4f8c093ce4f9de1ac8d654a3ffb7cd2d781fb
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-10-04 15:26:13 -0500

    Update problem 1245

--- src/main/rust/src/bin/problem1245.rs
+++ src/main/rust/src/bin/problem1245.rs
@@ -1,6 +1,13 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/tree-diameter/description/
 pub fn tree_diameter(edges: Vec<Vec<i32>>) -> i32 {
-    todo!()
+    let mut graph: HashMap<i32, Vec<i32>> = HashMap::new();
+    for edge in edges {
+        graph.entry(edge[0]).or_insert(Vec::new()).push(edge[1]);
+        graph.entry(edge[1]).or_insert(Vec::new()).push(edge[0]);
+    }
+    0
 }
 
 fn main() {
