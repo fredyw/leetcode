c0085f7ead2f717a77fe602300cf4010ce917656
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-10-10 00:36:29 -0500

    Solve problem 3309

--- src/main/rust/src/bin/problem3309.rs
+++ src/main/rust/src/bin/problem3309.rs
@@ -1,6 +1,22 @@
 // https://leetcode.com/problems/maximum-possible-number-by-binary-concatenation/description/
 pub fn max_good_number(nums: Vec<i32>) -> i32 {
-    todo!()
+    let mut answer = 0;
+    for i in 0..nums.len() {
+        for j in 0..nums.len() {
+            if i == j {
+                continue;
+            }
+            for k in 0..nums.len() {
+                if i == k || j == k {
+                    continue;
+                }
+                let binary = format!("{:b}{:b}{:b}", nums[i], nums[j], nums[k]);
+                let n = i32::from_str_radix(&binary, 2).unwrap();
+                answer = answer.max(n);
+            }
+        }
+    }
+    answer
 }
 
 fn main() {
