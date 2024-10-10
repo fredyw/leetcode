bc821cf7404d8bcbca00a106795e92b23476a275
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-10-10 00:22:20 -0500

    Skeleton for problem 3309

---
+++ src/main/rust/src/bin/problem3309.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/maximum-possible-number-by-binary-concatenation/description/
+pub fn max_good_number(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", max_good_number(vec![1, 2, 3])); // 30
+    println!("{}", max_good_number(vec![2, 8, 16])); // 1296
+}
