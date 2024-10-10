f645751ad2f2950e534b63c86f0da4a626e87b65
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-10-09 19:51:10 -0500

    Update problem 1564

--- src/main/rust/src/bin/problem1564.rs
+++ src/main/rust/src/bin/problem1564.rs
@@ -9,20 +9,18 @@ pub fn max_boxes_in_warehouse(mut boxes: Vec<i32>, warehouse: Vec<i32>) -> i32 {
         }
     }
     boxes.sort();
-    println!("{:?}", prefix_min);
-    println!("{:?}", boxes);
     let mut answer = 0;
     let mut i: isize = prefix_min.len() as isize - 1;
     let mut j = 0;
     while i >= 0 && j < boxes.len() {
-        while j < boxes.len() {
-            if boxes[j] <= prefix_min[i as usize] {
-                println!("{} --> {}", prefix_min[i as usize], boxes[j]);
+        let mut k = j;
+        while k < boxes.len() {
+            if boxes[k] <= prefix_min[i as usize] {
                 answer += 1;
                 j += 1;
                 break;
             }
-            j += 1;
+            k += 1;
         }
         i -= 1;
     }
@@ -34,16 +32,16 @@ fn main() {
         "{}",
         max_boxes_in_warehouse(vec![4, 3, 4, 1], vec![5, 3, 3, 4, 1])
     ); // 3
-       // println!(
-       //     "{}",
-       //     max_boxes_in_warehouse(vec![1, 2, 2, 3, 4], vec![3, 4, 1, 2])
-       // ); // 3
-       // println!(
-       //     "{}",
-       //     max_boxes_in_warehouse(vec![1, 2, 3], vec![1, 2, 3, 4])
-       // ); // 1
-       // println!(
-       //     "{}",
-       //     max_boxes_in_warehouse(vec![1, 5, 3, 4, 2], vec![2, 5, 3, 3, 4, 1])
-       // ); // 2
+    println!(
+        "{}",
+        max_boxes_in_warehouse(vec![1, 2, 2, 3, 4], vec![3, 4, 1, 2])
+    ); // 3
+    println!(
+        "{}",
+        max_boxes_in_warehouse(vec![1, 2, 3], vec![1, 2, 3, 4])
+    ); // 1
+    println!(
+        "{}",
+        max_boxes_in_warehouse(vec![1, 5, 3, 4, 2], vec![2, 5, 3, 3, 4, 1])
+    ); // 2
 }
