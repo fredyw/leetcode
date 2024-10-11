66442a37242713a8716c95fd77452d802fb8fcae
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-10-11 01:06:31 -0500

    Skeleton for problem 2137

---
+++ src/main/rust/src/bin/problem2137.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/pour-water-between-buckets-to-make-water-levels-equal/description/
+pub fn equalize_water(buckets: Vec<i32>, loss: i32) -> f64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", equalize_water(vec![1, 2, 7], 80)); // 2.00000
+    println!("{}", equalize_water(vec![2, 4, 6], 50)); // 3.50000
+    println!("{}", equalize_water(vec![3, 3, 3, 3], 40)); // 3.00000
+}
