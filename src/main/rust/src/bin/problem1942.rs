e2e468d6d387289dad1551696bbb62808e9c8f95
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-10-12 08:57:24 -0500

    Skeleton for problem 1942

---
+++ src/main/rust/src/bin/problem1942.rs
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/the-number-of-the-smallest-unoccupied-chair/description/
+pub fn smallest_chair(times: Vec<Vec<i32>>, target_friend: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        smallest_chair(vec![vec![1, 4], vec![2, 3], vec![4, 6]], 1)
+    ); // 1
+    println!(
+        "{}",
+        smallest_chair(vec![vec![3, 10], vec![1, 5], vec![2, 6]], 0)
+    ); // 2
+}
