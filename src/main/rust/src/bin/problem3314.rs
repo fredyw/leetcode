cc4a222d25c060e90a0cee555fa3c0149142bb10
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-10-13 23:08:23 -0500

    Skeleton for problem 3314

---
+++ src/main/rust/src/bin/problem3314.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/construct-the-minimum-bitwise-array-i/description/
+pub fn min_bitwise_array(nums: Vec<i32>) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", min_bitwise_array(vec![2, 3, 5, 7])); // [-1,1,4,3]
+    println!("{:?}", min_bitwise_array(vec![11, 13, 31])); // [9,12,15]
+}
