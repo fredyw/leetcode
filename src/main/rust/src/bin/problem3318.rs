ef2412a9908bd12654fd43d31d14b818b3add66e
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-10-13 22:43:10 -0500

    Skeleton for problem 3318

---
+++ src/main/rust/src/bin/problem3318.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/find-x-sum-of-all-k-long-subarrays-i/description/
+pub fn find_x_sum(nums: Vec<i32>, k: i32, x: i32) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", find_x_sum(vec![1, 1, 2, 2, 3, 4, 2, 3], 6, 2)); // [6,10,12]
+    println!("{:?}", find_x_sum(vec![3, 8, 7, 8, 7, 5], 2, 2)); // [11,15,15,15,12]
+}
