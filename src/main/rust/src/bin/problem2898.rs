99ee20011c73d4edbc59489cb077f3860a9cba38
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-10-15 18:49:44 -0500

    Skeleton for problem 2898

---
+++ src/main/rust/src/bin/problem2898.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/maximum-linear-stock-score/description/
+pub fn max_score(prices: Vec<i32>) -> i64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", max_score(vec![1, 5, 3, 7, 8])); // 20
+    println!("{}", max_score(vec![5, 6, 7, 8, 9])); // 35
+}
