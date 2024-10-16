c588ea3a86c2af80224e68a8736a6c3248edaf90
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-10-16 00:09:08 -0500

    Update problem 2898

--- src/main/rust/src/bin/problem2898.rs
+++ src/main/rust/src/bin/problem2898.rs
@@ -6,4 +6,7 @@ pub fn max_score(prices: Vec<i32>) -> i64 {
 fn main() {
     println!("{}", max_score(vec![1, 5, 3, 7, 8])); // 20
     println!("{}", max_score(vec![5, 6, 7, 8, 9])); // 35
+    println!("{}", max_score(vec![1, 5, 3])); // 5
+    println!("{}", max_score(vec![1])); // 1
+    println!("{}", max_score(vec![1, 2, 3])); // 6
 }
