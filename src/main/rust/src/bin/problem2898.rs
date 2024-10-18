c7d279ab86dc22e3d7e1ca1dd733e40bd409b002
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-10-18 09:30:57 -0500

    Solve problem 2898

--- src/main/rust/src/bin/problem2898.rs
+++ src/main/rust/src/bin/problem2898.rs
@@ -1,6 +1,16 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/maximum-linear-stock-score/description/
 pub fn max_score(prices: Vec<i32>) -> i64 {
-    todo!()
+    let mut map: HashMap<i32, i64> = HashMap::new();
+    for (i, price) in prices.into_iter().enumerate() {
+        *map.entry(price - i as i32).or_insert(0) += price as i64;
+    }
+    let mut answer = 0;
+    for (_, sum) in map {
+        answer = answer.max(sum);
+    }
+    answer
 }
 
 fn main() {
