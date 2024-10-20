4344c50c8a3745fa1f535e6161d72e383d39f311
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-10-19 20:35:29 -0500

    Solve problem 2137

--- src/main/rust/src/bin/problem2137.rs
+++ src/main/rust/src/bin/problem2137.rs
@@ -1,6 +1,27 @@
 // https://leetcode.com/problems/pour-water-between-buckets-to-make-water-levels-equal/description/
-pub fn equalize_water(buckets: Vec<i32>, loss: i32) -> f64 {
-    todo!()
+pub fn equalize_water(mut buckets: Vec<i32>, loss: i32) -> f64 {
+    buckets.sort();
+    let mut left = buckets[0] as f64;
+    let mut right = buckets[buckets.len() - 1] as f64;
+    while left + 0.00000001 < right {
+        let mid = (left + right) / 2.0;
+        let mut need = 0.0;
+        for &bucket in &buckets {
+            let bucket = bucket as f64;
+            let diff = mid - bucket;
+            need += if diff >= 0.0 {
+                diff
+            } else {
+                diff - (diff * (loss as f64 / 100.0))
+            };
+        }
+        if need <= 0.0 {
+            left = mid;
+        } else {
+            right = mid;
+        }
+    }
+    left
 }
 
 fn main() {
