209716857f6269509b4fadc9514434d0ce3c58bd
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-10-21 10:58:06 -0500

    Solve problem 3125

--- src/main/rust/src/bin/problem3125.rs
+++ src/main/rust/src/bin/problem3125.rs
@@ -1,17 +1,14 @@
 // https://leetcode.com/problems/maximum-number-that-makes-result-of-bitwise-and-zero/description/
 pub fn max_number(n: i64) -> i64 {
+    let mut i = n;
+    let mut count = 0;
+    while i > 0 {
+        i = i >> 1;
+        count += 1;
+    }
     let mut answer = 0;
-    let mut and = 0;
-    for i in (0..=n).rev() {
-        if i == n {
-            and = i;
-        } else {
-            and &= i;
-        }
-        if and == 0 {
-            answer = i;
-            break;
-        }
+    for i in 0..count - 1 {
+        answer = answer | (1 << i);
     }
     answer
 }
