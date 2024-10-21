d956563373bc4ccb065acb870d5eb4f65baed6de
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-10-21 11:08:42 -0500

    Skeleton for problem 2445

---
+++ src/main/rust/src/bin/problem2445.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/number-of-nodes-with-value-one/description/
+pub fn number_of_nodes(n: i32, queries: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", number_of_nodes(5, vec![1, 2, 5])); // 3
+    println!("{}", number_of_nodes(3, vec![2, 3, 3])); // 1
+}
