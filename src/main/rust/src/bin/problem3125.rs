5af3228bb2a9f270956baf3e9998931c4a6adf80
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-10-21 10:32:27 -0500

    Skeleton for problem 3125

---
+++ src/main/rust/src/bin/problem3125.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/maximum-number-that-makes-result-of-bitwise-and-zero/description/
+pub fn max_number(n: i64) -> i64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", max_number(7)); // 3
+    println!("{}", max_number(9)); // 7
+    println!("{}", max_number(17)); // 15
+}
