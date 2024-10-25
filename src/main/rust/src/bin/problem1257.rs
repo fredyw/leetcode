13b0f647d0de37b5ada0b89a652cdda15dedd4b0
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-10-24 19:19:23 -0500

    Solve problem 1257

--- src/main/rust/src/bin/problem1257.rs
+++ src/main/rust/src/bin/problem1257.rs
@@ -1,6 +1,33 @@
+use std::collections::{HashMap, HashSet};
+
 // https://leetcode.com/problems/smallest-common-region/description/
 pub fn find_smallest_region(regions: Vec<Vec<String>>, region1: String, region2: String) -> String {
-    todo!()
+    let mut map: HashMap<String, String> = HashMap::new();
+    for region in regions.iter() {
+        for i in 1..region.len() {
+            map.insert(region[i].to_string(), region[0].to_string()); // child to parent
+        }
+    }
+    let mut path: HashSet<String> = HashSet::new();
+    let mut region1 = region1;
+    path.insert(region1.clone());
+    while let Some(region) = map.get(&region1) {
+        path.insert(region.clone());
+        region1 = region.clone();
+    }
+    let mut answer = String::new();
+    let mut region2 = region2;
+    if path.contains(&region2) {
+        return region2;
+    }
+    while let Some(region) = map.get(&region2) {
+        if path.contains(region) {
+            answer = region.clone();
+            break;
+        }
+        region2 = region.clone();
+    }
+    answer
 }
 
 fn main() {
@@ -64,4 +91,39 @@ fn main() {
             "South America".to_string()
         )
     ); // "Earth"
+    println!(
+        "{}",
+        find_smallest_region(
+            vec![
+                vec![
+                    "North America".to_string(),
+                    "United States".to_string(),
+                    "Canada".to_string()
+                ],
+                vec![
+                    "United States".to_string(),
+                    "New York".to_string(),
+                    "Boston".to_string()
+                ],
+                vec![
+                    "Canada".to_string(),
+                    "Ontario".to_string(),
+                    "Quebec".to_string()
+                ],
+                vec!["South America".to_string(), "Brazil".to_string()],
+                vec![
+                    "Earth".to_string(),
+                    "North America".to_string(),
+                    "South America".to_string()
+                ],
+                vec![
+                    "Europe".to_string(),
+                    "France".to_string(),
+                    "Italy".to_string()
+                ],
+            ],
+            "Italy".to_string(),
+            "France".to_string()
+        )
+    ); // "Europe"
 }
