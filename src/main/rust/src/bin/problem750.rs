21fc13ee3cb3e66b66cf01dc2a033264e7808a02
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-10-25 16:08:56 -0500

    Solve problem 750

--- src/main/rust/src/bin/problem750.rs
+++ src/main/rust/src/bin/problem750.rs
@@ -1,6 +1,26 @@
 // https://leetcode.com/problems/number-of-corner-rectangles/description/
 pub fn count_corner_rectangles(grid: Vec<Vec<i32>>) -> i32 {
-    todo!()
+    let mut answer = 0;
+    let num_rows = grid.len();
+    let num_cols = if num_rows > 0 { grid[0].len() } else { 0 };
+    for row1 in 0..num_rows {
+        for col1 in 0..num_cols {
+            if grid[row1][col1] != 1 {
+                continue;
+            }
+            for row2 in row1 + 1..num_rows {
+                if grid[row2][col1] != 1 {
+                    continue;
+                }
+                for col2 in col1 + 1..num_cols {
+                    if grid[row1][col2] == 1 && grid[row2][col2] == 1 {
+                        answer += 1;
+                    }
+                }
+            }
+        }
+    }
+    answer
 }
 
 fn main() {
