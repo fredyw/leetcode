1ced2f4b16555f824e44d514a6b9cd3aebd8e208
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-10-27 00:55:46 -0500

    Skeleton for problem 3330

---
+++ src/main/rust/src/bin/problem3330.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/find-the-original-typed-string-i/description/
+pub fn possible_string_count(word: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", possible_string_count("abbcccc".to_string())); // 5
+    println!("{}", possible_string_count("abcd".to_string())); // 1
+    println!("{}", possible_string_count("aaaa".to_string())); // 4
+}
