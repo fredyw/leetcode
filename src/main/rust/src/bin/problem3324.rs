53754b80f623ce623ef6c466dcdd07abd799db4e
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-10-28 14:53:36 -0500

    Update problem 3324

--- src/main/rust/src/bin/problem3324.rs
+++ src/main/rust/src/bin/problem3324.rs
@@ -1,6 +1,26 @@
 // https://leetcode.com/problems/find-the-sequence-of-strings-appeared-on-the-screen/description/
 pub fn string_sequence(target: String) -> Vec<String> {
-    todo!()
+    let mut answer: Vec<String> = vec![];
+    let target = target.as_bytes();
+    let mut i = 0;
+    answer.push("a".to_string());
+    while i < target.len() {
+        let prev = answer[answer.len() - 1].as_bytes();
+        let mut new_string = answer[answer.len() - 1].clone();
+        let mut last_char = prev[prev.len() - 1];
+        while target[i] != last_char {
+            last_char = (last_char + 1) % 'z' as u8;
+            new_string.pop();
+            new_string.push(last_char as char);
+            answer.push(new_string.clone());
+        }
+        if i + 1 < target.len() {
+            new_string.push('a');
+            answer.push(new_string);
+        }
+        i += 1;
+    }
+    answer
 }
 
 fn main() {
