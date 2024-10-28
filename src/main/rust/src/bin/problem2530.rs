61c52bc475d341f470f3f19e0b7e027ef5053d4c
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-10-28 18:09:35 -0500

    Solve problem 2530

--- src/main/rust/src/bin/problem2530.rs
+++ src/main/rust/src/bin/problem2530.rs
@@ -1,6 +1,20 @@
+use std::collections::BinaryHeap;
+
 // https://leetcode.com/problems/maximal-score-after-applying-k-operations/description/
 pub fn max_kelements(nums: Vec<i32>, k: i32) -> i64 {
-    todo!()
+    let mut heap: BinaryHeap<i32> = BinaryHeap::new();
+    for num in nums {
+        heap.push(num);
+    }
+    let mut answer = 0;
+    let mut n = k;
+    while n > 0 {
+        let e = heap.pop().unwrap();
+        answer += e as i64;
+        heap.push(if e % 3 == 0 { e / 3 } else { (e / 3) + 1 });
+        n -= 1;
+    }
+    answer
 }
 
 fn main() {
