8d80c1c5746f8c94a234bd47c1055beb8ec28d91
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-10-28 17:36:28 -0500

    Skeleton for problem 2530

---
+++ src/main/rust/src/bin/problem2530.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/maximal-score-after-applying-k-operations/description/
+pub fn max_kelements(nums: Vec<i32>, k: i32) -> i64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", max_kelements(vec![10, 10, 10, 10, 10], 5)); // 50
+    println!("{}", max_kelements(vec![1, 10, 3, 3, 3], 3)); // 17
+}
