81213fe46a08275ceb0e497db9f4de94cef44bf2
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-10-28 12:22:08 -0500

    Skeleton for problem 3324

---
+++ src/main/rust/src/bin/problem3324.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/find-the-sequence-of-strings-appeared-on-the-screen/description/
+pub fn string_sequence(target: String) -> Vec<String> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", string_sequence("abc".to_string())); // ["a","aa","ab","aba","abb","abc"]
+    println!("{:?}", string_sequence("he".to_string())); // ["a","b","c","d","e","f","g","h","ha","hb","hc","hd","he"]
+}
