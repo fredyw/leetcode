efc996be19799cd0200f721738f32fe35d23374a
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-10-29 00:56:16 -0500

    Skeleton for problem 2664

---
+++ src/main/rust/src/bin/problem2664.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/the-knights-tour/description/
+pub fn tour_of_knight(m: i32, n: i32, r: i32, c: i32) -> Vec<Vec<i32>> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", tour_of_knight(1, 1, 0, 0)); // [[0]]
+    println!("{:?}", tour_of_knight(3, 4, 0, 0)); // [[0,3,6,9],[11,8,1,4],[2,5,10,7]]
+}
