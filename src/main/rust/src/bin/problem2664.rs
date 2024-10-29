a84767029d59b0ffe729d99a02e9dcadfe093dd7
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-10-29 16:04:30 -0500

    Solve problem 2664

--- src/main/rust/src/bin/problem2664.rs
+++ src/main/rust/src/bin/problem2664.rs
@@ -9,7 +9,7 @@ pub fn tour_of_knight(m: i32, n: i32, r: i32, c: i32) -> Vec<Vec<i32>> {
             (row - 1, col - 2),
             (row + 1, col - 2),
             (row - 1, col + 2),
-            (row + 1, col - 2),
+            (row + 1, col + 2),
         ]
     }
 
@@ -19,12 +19,11 @@ pub fn tour_of_knight(m: i32, n: i32, r: i32, c: i32) -> Vec<Vec<i32>> {
         col: i32,
         visited: &mut Vec<Vec<bool>>,
         count: i32,
-    ) {
+    ) -> bool {
         let num_rows = board.len() as i32;
         let num_cols = if num_rows > 0 { board[0].len() } else { 0 } as i32;
         if count == num_rows * num_cols {
-            println!("{:?}", board);
-            return;
+            return true;
         }
         if row < 0
             || row >= num_rows
@@ -32,19 +31,19 @@ pub fn tour_of_knight(m: i32, n: i32, r: i32, c: i32) -> Vec<Vec<i32>> {
             || col >= num_cols
             || visited[row as usize][col as usize]
         {
-            return;
+            return false;
         }
         visited[row as usize][col as usize] = true;
         let prev = board[row as usize][col as usize];
         board[row as usize][col as usize] = count;
-        for v in board.iter() {
-            println!("{:?}", v);
-        }
-        println!("");
         for (r, c) in possible_moves(row, col) {
-            tour_of_knight(board, r, c, visited, count + 1);
+            if tour_of_knight(board, r, c, visited, count + 1) {
+                return true;
+            }
         }
         board[row as usize][col as usize] = prev;
+        visited[row as usize][col as usize] = false;
+        false
     }
 
     let mut answer: Vec<Vec<i32>> = vec![vec![-1; n as usize]; m as usize];
@@ -59,6 +58,6 @@ pub fn tour_of_knight(m: i32, n: i32, r: i32, c: i32) -> Vec<Vec<i32>> {
 }
 
 fn main() {
-    // println!("{:?}", tour_of_knight(1, 1, 0, 0)); // [[0]]
+    println!("{:?}", tour_of_knight(1, 1, 0, 0)); // [[0]]
     println!("{:?}", tour_of_knight(3, 4, 0, 0)); // [[0,3,6,9],[11,8,1,4],[2,5,10,7]]
 }
