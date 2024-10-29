85b5c121cdcfae5facca83a630c86390f142d95e
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-10-29 14:07:47 -0500

    Update problem 2664

--- src/main/rust/src/bin/problem2664.rs
+++ src/main/rust/src/bin/problem2664.rs
@@ -1,6 +1,25 @@
 // https://leetcode.com/problems/the-knights-tour/description/
 pub fn tour_of_knight(m: i32, n: i32, r: i32, c: i32) -> Vec<Vec<i32>> {
+    fn tour_of_knight(board: &mut Vec<Vec<i32>>, row: i32, col: i32, visited: &mut Vec<Vec<bool>>) {
+        let num_rows = board.len() as i32;
+        let num_cols = if num_rows > 0 { board[0].len() } else { 0 } as i32;
+        if row < 0
+            || row >= num_rows
+            || col < 0
+            || col >= num_cols
+            || visited[row as usize][col as usize]
+        {
+            return;
+        }
+    }
+
     let mut answer: Vec<Vec<i32>> = vec![vec![-1; n as usize]; m as usize];
+    tour_of_knight(
+        &mut answer,
+        r,
+        c,
+        &mut vec![vec![false; n as usize]; m as usize],
+    );
     answer
 }
 
