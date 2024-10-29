5bd8d71911d6cef0a5dbd46782474a222c181af8
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-10-29 16:33:39 -0500

    Update problem 3319

--- src/main/rust/src/bin/problem3319.rs
+++ src/main/rust/src/bin/problem3319.rs
@@ -1,3 +1,24 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
 // https://leetcode.com/problems/k-th-largest-perfect-subtree-size-in-binary-tree/description/
 pub fn kth_largest_perfect_subtree(root: Option<Rc<RefCell<TreeNode>>>, k: i32) -> i32 {
     todo!()
