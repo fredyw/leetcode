bae5791dcd44d7bb52632851b7417b0210ef771d
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-10-30 13:55:38 -0500

    Solve problem 439

--- src/main/rust/src/bin/problem439.rs
+++ src/main/rust/src/bin/problem439.rs
@@ -1,22 +1,37 @@
 // https://leetcode.com/problems/ternary-expression-parser/description/
 pub fn parse_ternary(expression: String) -> String {
-    fn parse_ternary(expression: &Vec<char>, index: usize) -> String {
-        if index == expression.len() {
-            return "".to_string();
-        }
-        if index + 1 < expression.len() && expression[index + 1] == '?' {
-            parse_ternary(expression, index + 2);
+    fn is_literal(expression: &Vec<char>, index: usize) -> bool {
+        index + 1 == expression.len() || expression[index + 1] == ':'
+    }
+
+    fn parse_ternary(expression: &Vec<char>, index: &mut usize) -> String {
+        let cond = expression[*index];
+        *index += 2;
+        let if_expr = if is_literal(expression, *index) {
+            expression[*index].to_string()
         } else {
+            parse_ternary(expression, index)
+        };
+        *index += 2;
+        let else_expr = if is_literal(expression, *index) {
+            expression[*index].to_string()
+        } else {
+            parse_ternary(expression, index)
+        };
+        if cond == 'T' {
+            if_expr
+        } else {
+            else_expr
         }
-        "".to_string()
     }
 
     let expression: Vec<char> = expression.chars().collect();
-    parse_ternary(&expression, 0)
+    parse_ternary(&expression, &mut 0)
 }
 
 fn main() {
     println!("{}", parse_ternary("T?2:3".to_string())); // "2"
+    println!("{}", parse_ternary("F?2:3".to_string())); // "3"
     println!("{}", parse_ternary("F?1:T?4:5".to_string())); // "4"
     println!("{}", parse_ternary("T?T?F:5:3".to_string())); // "F"
     println!("{}", parse_ternary("T?T?T:T:F".to_string())); // "T"
