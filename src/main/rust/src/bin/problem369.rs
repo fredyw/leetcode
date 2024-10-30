beb849f6abf2fc8da3efde2c07fec2cf2304c47c
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-10-30 00:41:52 -0500

    Update problem 369

--- src/main/rust/src/bin/problem369.rs
+++ src/main/rust/src/bin/problem369.rs
@@ -16,4 +16,36 @@ pub fn plus_one(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
     todo!()
 }
 
-fn main() {}
+fn to_linked_list(v: Vec<i32>) -> Option<Box<ListNode>> {
+    let mut head: Option<Box<ListNode>> = None;
+    let mut current = &mut head;
+    for i in v.iter() {
+        match current {
+            Some(a) => {
+                a.next = Some(Box::new(ListNode::new(*i)));
+                current = &mut a.next;
+            }
+            None => {
+                head = Some(Box::new(ListNode::new(*i)));
+                current = &mut head;
+            }
+        }
+    }
+    head
+}
+
+fn to_vec(node: &Option<Box<ListNode>>) -> Vec<i32> {
+    let mut node = node;
+    let mut v: Vec<i32> = vec![];
+    while let Some(n) = node {
+        v.push(n.val);
+        node = &n.next
+    }
+    v
+}
+
+fn main() {
+    println!("{:?}", to_vec(&plus_one(to_linked_list(vec!(1, 2, 3))))); // [1,2,4]
+    println!("{:?}", to_vec(&plus_one(to_linked_list(vec!(0))))); // [1]
+    println!("{:?}", to_vec(&plus_one(to_linked_list(vec!(9))))); // [1,0]
+}
