e4d3d139ae9899f739431e5bcdfec1a6960a2b13
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-10-30 00:56:19 -0500

    Solve problem 369

--- src/main/rust/src/bin/problem369.rs
+++ src/main/rust/src/bin/problem369.rs
@@ -13,7 +13,51 @@ impl ListNode {
 
 // https://leetcode.com/problems/plus-one-linked-list/
 pub fn plus_one(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-    todo!()
+    fn to_linked_list(v: Vec<i32>) -> Option<Box<ListNode>> {
+        let mut head: Option<Box<ListNode>> = None;
+        let mut current = &mut head;
+        for i in v.iter() {
+            match current {
+                Some(a) => {
+                    a.next = Some(Box::new(ListNode::new(*i)));
+                    current = &mut a.next;
+                }
+                None => {
+                    head = Some(Box::new(ListNode::new(*i)));
+                    current = &mut head;
+                }
+            }
+        }
+        head
+    }
+
+    fn to_vec(node: Option<Box<ListNode>>) -> Vec<i32> {
+        let mut node = node;
+        let mut v: Vec<i32> = vec![];
+        while let Some(n) = node {
+            v.push(n.val);
+            node = n.next
+        }
+        v
+    }
+
+    let mut v = to_vec(head);
+    let mut carry = true;
+    for i in (0..v.len()).rev() {
+        if carry {
+            if v[i] + 1 > 9 {
+                v[i] = 0;
+                carry = true;
+            } else {
+                v[i] += 1;
+                carry = false;
+            }
+        }
+    }
+    if carry {
+        v.insert(0, 1);
+    }
+    to_linked_list(v)
 }
 
 fn to_linked_list(v: Vec<i32>) -> Option<Box<ListNode>> {
@@ -34,18 +78,26 @@ fn to_linked_list(v: Vec<i32>) -> Option<Box<ListNode>> {
     head
 }
 
-fn to_vec(node: &Option<Box<ListNode>>) -> Vec<i32> {
+fn to_vec(node: Option<Box<ListNode>>) -> Vec<i32> {
     let mut node = node;
     let mut v: Vec<i32> = vec![];
     while let Some(n) = node {
         v.push(n.val);
-        node = &n.next
+        node = n.next
     }
     v
 }
 
 fn main() {
-    println!("{:?}", to_vec(&plus_one(to_linked_list(vec!(1, 2, 3))))); // [1,2,4]
-    println!("{:?}", to_vec(&plus_one(to_linked_list(vec!(0))))); // [1]
-    println!("{:?}", to_vec(&plus_one(to_linked_list(vec!(9))))); // [1,0]
+    println!("{:?}", to_vec(plus_one(to_linked_list(vec!(1, 2, 3))))); // [1,2,4]
+    println!("{:?}", to_vec(plus_one(to_linked_list(vec!(0))))); // [1]
+    println!("{:?}", to_vec(plus_one(to_linked_list(vec!(9))))); // [1,0]
+    println!(
+        "{:?}",
+        to_vec(plus_one(to_linked_list(vec!(9, 9, 9, 9, 9))))
+    ); // [1,0,0,0,0,0]
+    println!(
+        "{:?}",
+        to_vec(plus_one(to_linked_list(vec!(8, 9, 9, 9, 9))))
+    ); // [9,0,0,0,0]
 }
