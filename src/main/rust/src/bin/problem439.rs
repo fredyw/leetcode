0c17a4d752a320ad533dc9f7098fd8de6e144a89
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-10-30 10:50:49 -0500

    Update problem 439

--- src/main/rust/src/bin/problem439.rs
+++ src/main/rust/src/bin/problem439.rs
@@ -7,4 +7,5 @@ fn main() {
     println!("{}", parse_ternary("T?2:3".to_string())); // "2"
     println!("{}", parse_ternary("F?1:T?4:5".to_string())); // "4"
     println!("{}", parse_ternary("T?T?F:5:3".to_string())); // "F"
+    println!("{}", parse_ternary("T?T?T:T:F".to_string())); // "T"
 }
