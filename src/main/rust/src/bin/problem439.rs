785ac8256ced9a0f8fb95871c44452bacadee7a9
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-10-30 13:03:52 -0500

    Update problem 439

--- src/main/rust/src/bin/problem439.rs
+++ src/main/rust/src/bin/problem439.rs
@@ -1,7 +1,14 @@
 // https://leetcode.com/problems/ternary-expression-parser/description/
 pub fn parse_ternary(expression: String) -> String {
+    fn parse_ternary(expression: &Vec<char>, index: usize) -> String {
+        if index == expression.len() {
+            return "".to_string();
+        }
+        "".to_string()
+    }
+
     let expression: Vec<char> = expression.chars().collect();
-    todo!()
+    parse_ternary(&expression, 0)
 }
 
 fn main() {
@@ -9,4 +16,5 @@ fn main() {
     println!("{}", parse_ternary("F?1:T?4:5".to_string())); // "4"
     println!("{}", parse_ternary("T?T?F:5:3".to_string())); // "F"
     println!("{}", parse_ternary("T?T?T:T:F".to_string())); // "T"
+    println!("{}", parse_ternary("T?T?3:4:T?1:2".to_string())); // "3"
 }
