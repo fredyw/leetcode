d7104026ddd45db8b3ae994938b5239bbb09a33d
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-10-30 00:32:30 -0500

    Update problem 3319

--- src/main/rust/src/bin/problem3319.rs
+++ src/main/rust/src/bin/problem3319.rs
@@ -1,4 +1,5 @@
 use std::cell::RefCell;
+use std::collections::VecDeque;
 use std::rc::Rc;
 
 #[derive(Debug, PartialEq, Eq)]
@@ -21,7 +22,33 @@ impl TreeNode {
 
 // https://leetcode.com/problems/k-th-largest-perfect-subtree-size-in-binary-tree/description/
 pub fn kth_largest_perfect_subtree(root: Option<Rc<RefCell<TreeNode>>>, k: i32) -> i32 {
-    todo!()
+    fn kth_largest_perfect_subtree(
+        root: Option<Rc<RefCell<TreeNode>>>,
+        perfect_subtrees: &mut Vec<i32>,
+    ) -> (bool, i32) {
+        let node = root.unwrap();
+        let left = node.as_ref().borrow().left.clone();
+        let right = node.as_ref().borrow().right.clone();
+        if left.is_some() && right.is_some() {
+            let (left_perfect, left_count) = kth_largest_perfect_subtree(left, perfect_subtrees);
+            let (right_perfect, right_count) = kth_largest_perfect_subtree(right, perfect_subtrees);
+            if left_perfect && right_perfect && left_count == right_count {
+                perfect_subtrees.push(left_count + right_count + 1);
+                (true, left_count + right_count + 1)
+            } else {
+                (false, -1)
+            }
+        } else {
+            perfect_subtrees.push(1);
+            (true, 1)
+        }
+    }
+
+    let mut perfect_subtrees: Vec<i32> = vec![];
+    kth_largest_perfect_subtree(root, &mut perfect_subtrees);
+    perfect_subtrees.sort_by(|a, b| b.cmp(&a));
+    println!("{:?}", perfect_subtrees);
+    *perfect_subtrees.get(k as usize - 1).unwrap_or(&-1)
 }
 
 fn create_node(value: i32) -> Option<Rc<RefCell<TreeNode>>> {
