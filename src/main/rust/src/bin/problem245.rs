374946f2c0caa40ab5a054bae76a0a45f41dada5
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-10-31 13:07:14 -0500

    Update problem 245

--- src/main/rust/src/bin/problem245.rs
+++ src/main/rust/src/bin/problem245.rs
@@ -1,6 +1,25 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/shortest-word-distance-iii/
 pub fn shortest_word_distance(words_dict: Vec<String>, word1: String, word2: String) -> i32 {
-    todo!()
+    let mut map: HashMap<&str, Vec<i32>> = HashMap::new();
+    for (i, word) in words_dict.iter().enumerate() {
+        map.entry(word).or_insert(vec![]).push(i as i32);
+    }
+    let mut answer = i32::MAX;
+    let mut i = 0;
+    let mut j = 0;
+    let indexes1 = map.get(word1.as_str()).unwrap();
+    let indexes2 = map.get(word2.as_str()).unwrap();
+    while i < indexes1.len() && j < indexes2.len() {
+        answer = answer.min((indexes1[i] - indexes2[j]).abs());
+        if indexes1[i] < indexes2[j] {
+            i += 1;
+        } else {
+            j += 1;
+        }
+    }
+    answer
 }
 
 fn main() {
