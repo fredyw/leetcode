73d224198bb525f47816d65e8fcc457800df0fe4
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-10-31 12:28:05 -0500

    Solve problem 159

--- src/main/rust/src/bin/problem159.rs
+++ src/main/rust/src/bin/problem159.rs
@@ -1,6 +1,27 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/longest-substring-with-at-most-two-distinct-characters/description/
 pub fn length_of_longest_substring_two_distinct(s: String) -> i32 {
-    todo!()
+    let mut answer = 0;
+    let bytes = s.as_bytes();
+    let mut map: HashMap<u8, i32> = HashMap::new();
+    let mut i = 0;
+    let mut j = 0;
+    while i < s.len() {
+        *map.entry(bytes[i]).or_insert(0) += 1;
+        while map.len() > 2 {
+            let count = map.get_mut(&bytes[j]).unwrap();
+            if *count - 1 == 0 {
+                map.remove(&bytes[j]);
+            } else {
+                *count -= 1;
+            }
+            j += 1;
+        }
+        answer = answer.max((i - j) as i32 + 1);
+        i += 1;
+    }
+    answer
 }
 
 fn main() {
