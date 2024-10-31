cd9edf6f56accd6ca304f55de4ff384aa5a72903
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-10-31 13:13:24 -0500

    Skeleton for problem 247

---
+++ src/main/rust/src/bin/problem247.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/strobogrammatic-number-ii/description/
+pub fn find_strobogrammatic(n: i32) -> Vec<String> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", find_strobogrammatic(2)); // ["11","69","88","96"]
+    println!("{:?}", find_strobogrammatic(1)); // ["0","1","8"]
+}
