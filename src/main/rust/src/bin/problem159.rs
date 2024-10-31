5f84b1a34bc2324219f0e6996764ce0fcc3f1cb6
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-10-31 12:11:20 -0500

    Skeleton for problem 159

---
+++ src/main/rust/src/bin/problem159.rs
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/longest-substring-with-at-most-two-distinct-characters/description/
+pub fn length_of_longest_substring_two_distinct(s: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        length_of_longest_substring_two_distinct("eceba".to_string())
+    ); // 3
+    println!(
+        "{}",
+        length_of_longest_substring_two_distinct("ccaabbb".to_string())
+    ); // 5
+}
