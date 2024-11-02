332b79cb1d1bb846ceeafa62edbc7c3a5e14101d
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-11-02 12:36:39 -0500

    Skeleton for problem 255

---
+++ src/main/rust/src/bin/problem255.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/verify-preorder-sequence-in-binary-search-tree/description/
+pub fn verify_preorder(preorder: Vec<i32>) -> bool {
+    todo!()
+}
+
+fn main() {
+    println!("{}", verify_preorder(vec![5, 2, 1, 3, 6])); // true
+    println!("{}", verify_preorder(vec![5, 2, 6, 1, 3])); // false
+}
