da497725308ce26fe4b086b13208b2a3d0cd1ef7
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-11-02 12:43:41 -0500

    Update problem 255

--- src/main/rust/src/bin/problem255.rs
+++ src/main/rust/src/bin/problem255.rs
@@ -6,4 +6,5 @@ pub fn verify_preorder(preorder: Vec<i32>) -> bool {
 fn main() {
     println!("{}", verify_preorder(vec![5, 2, 1, 3, 6])); // true
     println!("{}", verify_preorder(vec![5, 2, 6, 1, 3])); // false
+    println!("{}", verify_preorder(vec![1, 2, 3, 4, 5])); // true
 }
