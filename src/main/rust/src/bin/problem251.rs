4494b96fd4ad40a36f39742e1e7278e7d969dc78
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-11-02 10:27:57 -0500

    Skeleton for problem 251

---
+++ src/main/rust/src/bin/problem251.rs
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/flatten-2d-vector/description/
+struct Vector2D {}
+
+impl Vector2D {
+    fn new(vec: Vec<Vec<i32>>) -> Self {
+        Self {}
+    }
+
+    fn next(&self) -> i32 {
+        todo!()
+    }
+
+    fn has_next(&self) -> bool {
+        todo!()
+    }
+}
+
+fn main() {}
