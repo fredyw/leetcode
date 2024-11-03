079874f71f8e98ded24efba2c9b53347aad4c5bf
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-11-03 08:44:54 -0600

    Skeleton for problem 3340

---
+++ src/main/rust/src/bin/problem3340.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/check-balanced-string/description/
+pub fn is_balanced(num: String) -> bool {
+    todo!()
+}
+
+fn main() {
+    println!("{}", is_balanced("1234".to_string())); // false
+    println!("{}", is_balanced("24123".to_string())); // true
+}
