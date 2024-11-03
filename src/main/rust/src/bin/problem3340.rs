22bbf29f9b5c187488be4b044b2c252a2e92e54b
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-11-03 08:48:39 -0600

    Solve problem 3340

--- src/main/rust/src/bin/problem3340.rs
+++ src/main/rust/src/bin/problem3340.rs
@@ -1,6 +1,19 @@
 // https://leetcode.com/problems/check-balanced-string/description/
 pub fn is_balanced(num: String) -> bool {
-    todo!()
+    let chars: Vec<char> = num.chars().collect();
+    let even_sum: i32 = chars
+        .iter()
+        .enumerate()
+        .filter(|(i, _)| i % 2 == 0)
+        .map(|(_, c)| *c as i32 - '0' as i32)
+        .sum();
+    let odd_sum: i32 = chars
+        .iter()
+        .enumerate()
+        .filter(|(i, _)| i % 2 != 0)
+        .map(|(_, c)| *c as i32 - '0' as i32)
+        .sum();
+    even_sum == odd_sum
 }
 
 fn main() {
