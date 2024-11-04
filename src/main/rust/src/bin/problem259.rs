490c3335662a6548dbe6ea68ce612b09fd825e3d
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-11-04 12:06:04 -0600

    Skeleton for problem 259

---
+++ src/main/rust/src/bin/problem259.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/3sum-smaller/description/
+pub fn three_sum_smaller(nums: Vec<i32>, target: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", three_sum_smaller(vec![-2, 0, 1, 3], 2)); // 2
+    println!("{}", three_sum_smaller(vec![], 0)); // 0
+    println!("{}", three_sum_smaller(vec![0], 0)); // 0
+}
