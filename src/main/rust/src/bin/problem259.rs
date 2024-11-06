3592e86f000edd4cf2cc9d8db13cd4a81500e536
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-11-06 09:59:09 -0600

    Solve problem 259

--- src/main/rust/src/bin/problem259.rs
+++ src/main/rust/src/bin/problem259.rs
@@ -1,20 +1,25 @@
 // https://leetcode.com/problems/3sum-smaller/description/
 pub fn three_sum_smaller(mut nums: Vec<i32>, target: i32) -> i32 {
-    fn less_than_count(nums: &Vec<i32>, i: usize, target: i32) -> i32 {
-        let mut left = i as i32;
+    fn less_than_count(nums: &Vec<i32>, start: usize, target: i32) -> i32 {
+        let mut left = start as i32;
         let mut right = nums.len() as i32;
-        while left < right {
+        while left <= right {
             let mid = left + (right - left) / 2;
+            if mid >= nums.len() as i32 {
+                break;
+            }
             if nums[mid as usize] < target {
                 left = mid + 1;
             } else {
                 right = mid - 1;
             }
         }
-        if right < i as i32 {
+        if right < start as i32 {
             0
+        } else if right >= nums.len() as i32 {
+            nums.len() as i32 - start as i32
         } else {
-            right - i as i32
+            right - start as i32 + 1
         }
     }
 
@@ -27,11 +32,7 @@ pub fn three_sum_smaller(mut nums: Vec<i32>, target: i32) -> i32 {
             }
             let sum = nums[i] + nums[j];
             let x = target - sum;
-            let count = less_than_count(&nums, j + 1, x);
-            // println!("i: {}, j: {}, x: {}, count: {}", i, j, x, count);
-            if count > 0 {
-                answer += count;
-            }
+            answer += less_than_count(&nums, j + 1, x);
         }
     }
     answer
@@ -48,4 +49,7 @@ fn main() {
     println!("{}", three_sum_smaller(vec![-2, -2, 0, 1], 2)); // 4
     println!("{}", three_sum_smaller(vec![-2, 0, 1], 2)); // 1
     println!("{}", three_sum_smaller(vec![0, 0, 0], 0)); // 0
+    println!("{}", three_sum_smaller(vec![1, -1, 2, 0, 3, -2], 2)); // 10
+    println!("{}", three_sum_smaller(vec![-2, -1, 0, 1, 2, 3], 2)); // 10
+    println!("{}", three_sum_smaller(vec![-4, -1, -1, 0, 1, 2], -5)); // 1
 }
