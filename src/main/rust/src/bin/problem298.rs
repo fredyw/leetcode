330e23fb8a4d3cff16572ad51c7721f73f8c6a1d
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-11-06 17:13:19 -0600

    Update problem 298

--- src/main/rust/src/bin/problem298.rs
+++ src/main/rust/src/bin/problem298.rs
@@ -1,4 +1,5 @@
 use std::cell::RefCell;
+use std::collections::VecDeque;
 use std::rc::Rc;
 
 #[derive(Debug, PartialEq, Eq)]
@@ -24,4 +25,67 @@ pub fn longest_consecutive(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
     todo!()
 }
 
-fn main() {}
+fn create_node(value: i32) -> Option<Rc<RefCell<TreeNode>>> {
+    Some(Rc::new(RefCell::new(TreeNode::new(value))))
+}
+
+fn add_left(parent: &Option<Rc<RefCell<TreeNode>>>, value: i32) -> Option<Rc<RefCell<TreeNode>>> {
+    parent.as_ref().unwrap().borrow_mut().left = create_node(value);
+    parent.as_ref().unwrap().borrow().left.clone()
+}
+
+fn add_right(parent: &Option<Rc<RefCell<TreeNode>>>, value: i32) -> Option<Rc<RefCell<TreeNode>>> {
+    parent.as_ref().unwrap().borrow_mut().right = create_node(value);
+    parent.as_ref().unwrap().borrow().right.clone()
+}
+
+fn to_tree(nodes: Vec<Option<i32>>) -> Option<Rc<RefCell<TreeNode>>> {
+    if nodes.len() == 0 {
+        return None;
+    }
+    let mut deque: VecDeque<Option<Rc<RefCell<TreeNode>>>> = VecDeque::new();
+    let root = create_node(nodes[0].unwrap());
+    deque.push_back(root.clone());
+    let mut i = 1;
+    while i < nodes.len() {
+        let parent = deque.pop_front().unwrap();
+        if let Some(left) = nodes[i] {
+            deque.push_back(add_left(&parent, left));
+        }
+        if i + 1 < nodes.len() {
+            if let Some(right) = nodes[i + 1] {
+                deque.push_back(add_right(&parent, right));
+            }
+        }
+        i += 2;
+    }
+    root
+}
+
+fn main() {
+    println!(
+        "{}",
+        longest_consecutive(to_tree(vec![
+            Some(1),
+            None,
+            Some(3),
+            Some(2),
+            Some(4),
+            None,
+            None,
+            None,
+            Some(5)
+        ]))
+    ); // 3
+    println!(
+        "{}",
+        longest_consecutive(to_tree(vec![
+            Some(2),
+            None,
+            Some(3),
+            Some(2),
+            None,
+            Some(1)
+        ]))
+    ); // 2
+}
