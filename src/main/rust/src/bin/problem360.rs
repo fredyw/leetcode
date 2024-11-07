f50346a1d77ab086cfb2baeba6a006f23a9d466f
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-11-06 19:09:46 -0600

    Skeleton for problem 360

---
+++ src/main/rust/src/bin/problem360.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/sort-transformed-array/description/
+pub fn sort_transformed_array(nums: Vec<i32>, a: i32, b: i32, c: i32) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", sort_transformed_array(vec![-4, -2, 2, 4], 1, 3, 5)); // [3,9,15,33]
+    println!("{:?}", sort_transformed_array(vec![-4, -2, 2, 4], -1, 3, 5)); // [-23,-5,1,7]
+}
