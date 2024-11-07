6a9709ed52ede7390dd345de540c581d52bf5616
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-11-06 23:11:13 -0600

    Skeleton for problem 651

---
+++ src/main/rust/src/bin/problem651.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/4-keys-keyboard/description/
+pub fn max_a(n: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", max_a(3)); // 3
+    println!("{}", max_a(7)); // 9
+}
