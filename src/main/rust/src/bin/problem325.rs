4f00278fae5de603a1d40340f010e35b2cbed34f
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-11-09 08:25:47 -0600

    Skeleton for problem 325

---
+++ src/main/rust/src/bin/problem325.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/maximum-size-subarray-sum-equals-k/description/
+pub fn max_sub_array_len(nums: Vec<i32>, k: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", max_sub_array_len(vec![1, -1, 5, -2, 3], 3)); // 4
+    println!("{}", max_sub_array_len(vec![-2, -1, 2, 1], 1)); // 2
+}
