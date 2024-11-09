142d0b0cb6ce59c3873c0193a0b330ac50e84db5
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-11-09 08:24:13 -0600

    Skeleton for problem 294

---
+++ src/main/rust/src/bin/problem294.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/flip-game-ii/description/
+pub fn can_win(current_state: String) -> bool {
+    todo!()
+}
+
+fn main() {
+    println!("{}", can_win("++++".to_string())); // true
+    println!("{}", can_win("+".to_string())); // false
+}
