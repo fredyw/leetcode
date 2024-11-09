760be20681f806400e936bed89175df1b2ee3b43
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-11-09 00:22:03 -0600

    Solve problem 271

--- src/main/rust/src/bin/problem271.rs
+++ src/main/rust/src/bin/problem271.rs
@@ -7,11 +7,28 @@ impl Codec {
     }
 
     fn encode(&self, strs: Vec<String>) -> String {
-        todo!()
+        // 1,2,3|4,5,6
+        strs.into_iter()
+            .map(|s| {
+                s.as_bytes()
+                    .into_iter()
+                    .map(|&b| format!("{},", b))
+                    .collect::<String>()
+            })
+            .collect::<Vec<String>>()
+            .join("|")
     }
 
     fn decode(&self, s: String) -> Vec<String> {
-        todo!()
+        s.split('|')
+            .map(|a| {
+                a.split(',')
+                    .into_iter()
+                    .filter(|b| !b.is_empty())
+                    .map(|b| b.parse::<u8>().unwrap() as char)
+                    .collect::<String>()
+            })
+            .collect()
     }
 }
 
