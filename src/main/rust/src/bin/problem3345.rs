cc7f68d1259a5f38263371d5775b8310a072fee9
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-11-09 23:40:59 -0600

    Solve problem 3345

--- src/main/rust/src/bin/problem3345.rs
+++ src/main/rust/src/bin/problem3345.rs
@@ -1,6 +1,17 @@
 // https://leetcode.com/problems/smallest-divisible-digit-product-i/description/
 pub fn smallest_number(n: i32, t: i32) -> i32 {
-    todo!()
+    fn product(n: i32) -> i32 {
+        n.to_string()
+            .chars()
+            .map(|c| c.to_digit(10).unwrap() as i32)
+            .product()
+    }
+
+    let mut answer = n;
+    while product(answer) % t != 0 {
+        answer += 1;
+    }
+    answer
 }
 
 fn main() {
