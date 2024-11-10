94ddff4523662f05067370103f7ca979696362a9
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-11-09 23:37:30 -0600

    Skeleton for problem 3345

---
+++ src/main/rust/src/bin/problem3345.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/smallest-divisible-digit-product-i/description/
+pub fn smallest_number(n: i32, t: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", smallest_number(10, 2)); // 10
+    println!("{}", smallest_number(15, 3)); // 16
+}
