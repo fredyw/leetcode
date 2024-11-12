202fe601474853bdd204878d564a09d89b43ef72
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-11-12 10:14:39 -0600

    Skeleton for problem 490

---
+++ src/main/rust/src/bin/problem490.rs
@@ -0,0 +1,49 @@
+// https://leetcode.com/problems/the-maze/description/
+pub fn has_path(maze: Vec<Vec<i32>>, start: Vec<i32>, destination: Vec<i32>) -> bool {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        has_path(
+            vec![
+                vec![0, 0, 1, 0, 0],
+                vec![0, 0, 0, 0, 0],
+                vec![0, 0, 0, 1, 0],
+                vec![1, 1, 0, 1, 1],
+                vec![0, 0, 0, 0, 0]
+            ],
+            vec![0, 4],
+            vec![4, 4]
+        )
+    ); // true
+    println!(
+        "{}",
+        has_path(
+            vec![
+                vec![0, 0, 1, 0, 0],
+                vec![0, 0, 0, 0, 0],
+                vec![0, 0, 0, 1, 0],
+                vec![1, 1, 0, 1, 1],
+                vec![0, 0, 0, 0, 0]
+            ],
+            vec![0, 4],
+            vec![3, 2]
+        )
+    ); // false
+    println!(
+        "{}",
+        has_path(
+            vec![
+                vec![0, 0, 0, 0, 0],
+                vec![1, 1, 0, 0, 1],
+                vec![0, 0, 0, 0, 0],
+                vec![0, 1, 0, 0, 1],
+                vec![0, 1, 0, 0, 0]
+            ],
+            vec![4, 3],
+            vec![0, 1]
+        )
+    ); // false
+}
