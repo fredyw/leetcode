cce01151caf473612d43eb8853c8c09a94b335a6
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-11-12 10:11:30 -0600

    Solve problem 325

--- src/main/rust/src/bin/problem325.rs
+++ src/main/rust/src/bin/problem325.rs
@@ -1,6 +1,31 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/maximum-size-subarray-sum-equals-k/description/
 pub fn max_sub_array_len(nums: Vec<i32>, k: i32) -> i32 {
-    todo!()
+    let mut map: HashMap<i32, usize> = HashMap::new();
+    let mut prefix_sums: Vec<i32> = vec![0; nums.len()];
+    for (i, num) in nums.iter().enumerate() {
+        if i == 0 {
+            prefix_sums[i] = *num;
+        } else {
+            prefix_sums[i] = prefix_sums[i - 1] + num;
+        }
+        map.insert(prefix_sums[i], i);
+    }
+    let mut answer = 0;
+    for i in 0..nums.len() {
+        if i == 0 {
+            if let Some(index) = map.get(&k) {
+                answer = answer.max(*index as i32 + 1);
+            }
+        } else {
+            let x = prefix_sums[i] + (k - (prefix_sums[i] - prefix_sums[i - 1]));
+            if let Some(index) = map.get(&x) {
+                answer = answer.max(*index as i32 - i as i32 + 1);
+            }
+        }
+    }
+    answer
 }
 
 fn main() {
