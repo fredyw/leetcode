e76ad2e9fb6559e6cd2224c245816a0d2fc47c54
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-11-16 09:01:03 -0600

    Skeleton for problem 3353

---
+++ src/main/rust/src/bin/problem3353.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/minimum-total-operations/description/
+pub fn min_operations(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", min_operations(vec![1, 4, 2])); // 2
+    println!("{}", min_operations(vec![10, 10, 10])); // 0
+}
