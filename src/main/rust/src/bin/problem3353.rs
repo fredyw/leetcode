23acf08c110836613df3210c0dd72617a7376afc
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-11-16 12:47:16 -0600

    Solve problem 3353

--- src/main/rust/src/bin/problem3353.rs
+++ src/main/rust/src/bin/problem3353.rs
@@ -1,6 +1,15 @@
 // https://leetcode.com/problems/minimum-total-operations/description/
 pub fn min_operations(nums: Vec<i32>) -> i32 {
-    todo!()
+    let mut answer = 0;
+    let mut i = nums.len() as isize - 1;
+    while i >= 0 {
+        let n = nums[i as usize];
+        while i >= 0 && nums[i as usize] == n {
+            i -= 1;
+        }
+        answer += 1;
+    }
+    answer - 1
 }
 
 fn main() {
@@ -8,4 +17,8 @@ fn main() {
     println!("{}", min_operations(vec![10, 10, 10])); // 0
     println!("{}", min_operations(vec![1, 2, 3])); // 2
     println!("{}", min_operations(vec![1, 4, 2, 4, 2])); // 4
+    println!(
+        "{}",
+        min_operations(vec![2, 2, 2, 1, 1, 1, 2, 2, 2, 3, 3, 3])
+    ); // 3
 }
