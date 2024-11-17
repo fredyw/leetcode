a4cf4ac9b041d69948de4ffdff4187b612be9b90
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-11-17 08:54:18 -0600

    Solve problem 1623

--- src/main/sql/Problem1623.sql
+++ src/main/sql/Problem1623.sql
@@ -1,1 +1,11 @@
 -- https://leetcode.com/problems/all-valid-triplets-that-can-represent-a-country/description/
+select a.student_name as member_A,
+       b.student_name as member_B,
+       c.student_name as member_C
+from SchoolA a, SchoolB b, SchoolC c
+where a.student_id != b.student_id and
+    b.student_id != c.student_id and
+    a.student_id != c.student_id and
+    a.student_name != b.student_name and
+    b.student_name != c.student_name and
+    a.student_name != c.student_name;
