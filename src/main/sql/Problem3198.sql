214039c22162e341bdee24592778dd5f988d8706
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-11-17 09:04:24 -0600

    Solve problem 3198

--- src/main/sql/Problem3198.sql
+++ src/main/sql/Problem3198.sql
@@ -1,1 +1,5 @@
 -- https://leetcode.com/problems/find-cities-in-each-state/description/
+select state, string_agg(city, ', ') within group (order by city) as cities
+from cities
+group by state
+order by state;
