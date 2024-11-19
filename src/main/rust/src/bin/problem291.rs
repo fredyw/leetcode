f074a425079cc6e9781f4c8807184d578d9a649b
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-11-19 17:31:09 -0600

    Update problem 291

--- src/main/rust/src/bin/problem291.rs
+++ src/main/rust/src/bin/problem291.rs
@@ -1,6 +1,51 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/word-pattern-ii/description/
 pub fn word_pattern_match(pattern: String, s: String) -> bool {
-    todo!()
+    fn word_pattern_match<'a>(
+        pattern: &'a str,
+        string: &'a str,
+        pattern_index: usize,
+        string_index: usize,
+        pattern_to_string_map: &mut HashMap<&'a str, &'a str>,
+        string_to_pattern_map: &mut HashMap<&'a str, &'a str>,
+    ) -> bool {
+        if pattern_index == pattern.len() {
+            return true;
+        }
+        let mut found = false;
+        for i in pattern_index + 1..=pattern.len() {
+            let p1 = &pattern[pattern_index..pattern_index + 1];
+            for j in string_index + 1..=string.len() {
+                let s2 = &string[string_index..j];
+                if let Some(s1) = pattern_to_string_map.get(p1) {
+                    if *s1 != s2 {
+                        continue;
+                    }
+                }
+                if let Some(p2) = string_to_pattern_map.get(s2) {
+                    if p1 != *p2 {
+                        continue;
+                    }
+                }
+                string_to_pattern_map.insert(s2, p1);
+                pattern_to_string_map.insert(p1, s2);
+                found |= word_pattern_match(
+                    pattern,
+                    string,
+                    i,
+                    j,
+                    pattern_to_string_map,
+                    string_to_pattern_map,
+                );
+                pattern_to_string_map.remove(p1);
+                string_to_pattern_map.insert(s2, p1);
+            }
+        }
+        found
+    }
+
+    word_pattern_match(&pattern, &s, 0, 0, &mut HashMap::new(), &mut HashMap::new())
 }
 
 fn main() {
