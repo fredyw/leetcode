8a31e528bae2424d4dbd4c73b7ec37f872537cba
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-11-21 10:27:27 -0600

    Solve problem 379

--- src/main/rust/src/bin/problem379.rs
+++ src/main/rust/src/bin/problem379.rs
@@ -1,26 +1,48 @@
+use std::collections::HashSet;
+
 // https://leetcode.com/problems/design-phone-directory/description/
-struct PhoneDirectory {}
+struct PhoneDirectory {
+    available: HashSet<i32>,
+    assigned: HashSet<i32>,
+}
 
 impl PhoneDirectory {
     fn new(max_numbers: i32) -> Self {
-        Self {}
+        let mut available = HashSet::new();
+        for i in 0..max_numbers {
+            available.insert(i);
+        }
+        Self {
+            available,
+            assigned: HashSet::new(),
+        }
     }
 
-    fn get(&self) -> i32 {
-        todo!()
+    fn get(&mut self) -> i32 {
+        let val = if let Some(n) = self.available.iter().next() {
+            *n
+        } else {
+            -1
+        };
+        if val != -1 {
+            self.assigned.insert(val);
+            self.available.remove(&val);
+        }
+        val
     }
 
     fn check(&self, number: i32) -> bool {
-        todo!()
+        self.available.contains(&number)
     }
 
-    fn release(&self, number: i32) {
-        todo!()
+    fn release(&mut self, number: i32) {
+        self.assigned.remove(&number);
+        self.available.insert(number);
     }
 }
 
 fn main() {
-    let dir = PhoneDirectory::new(3);
+    let mut dir = PhoneDirectory::new(3);
     println!("{}", dir.get()); // 0
     println!("{}", dir.get()); // 1
     println!("{}", dir.check(2)); // true
