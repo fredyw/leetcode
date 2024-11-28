f6f89859b45d059d42e1d2071afa5fe0005d0a14
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-11-29 02:46:20 +0800

    Solve problem 3364

--- src/main/rust/src/bin/problem3364.rs
+++ src/main/rust/src/bin/problem3364.rs
@@ -1,31 +1,19 @@
 // https://leetcode.com/problems/minimum-positive-sum-subarray/description/
 pub fn minimum_sum_subarray(nums: Vec<i32>, l: i32, r: i32) -> i32 {
     let mut answer = i32::MAX;
-    let mut curr = 0;
-    let mut prev = 0;
-    let mut sum = 0;
-    while curr < nums.len() {
-        let length = curr as i32 - prev as i32 + 1;
-        sum += nums[curr];
-        if length > r {
-            sum -= nums[prev];
-            if sum > 0 {
-                answer = answer.min(sum);
+    for i in 0..nums.len() {
+        let mut sum = 0;
+        for j in i..nums.len() {
+            let length = j as i32 - i as i32 + 1;
+            sum += nums[j];
+            if length >= l && length <= r {
+                if sum > 0 {
+                    answer = answer.min(sum);
+                }
             }
-            prev += 1;
-            continue;
-        }
-        if length >= l && length <= r && sum > 0 {
-            answer = answer.min(sum);
-        }
-        curr += 1;
-    }
-    while prev <= curr - 1 {
-        sum -= nums[prev];
-        prev += 1;
-        let length = curr as i32 - 1 - prev as i32 + 1;
-        if length >= l && length <= r && sum > 0 {
-            answer = answer.min(sum);
+            if length > r {
+                break;
+            }
         }
     }
     if answer == i32::MAX {
@@ -36,10 +24,10 @@ pub fn minimum_sum_subarray(nums: Vec<i32>, l: i32, r: i32) -> i32 {
 }
 
 fn main() {
-    // println!("{}", minimum_sum_subarray(vec![3, -2, 1, 4], 2, 3)); // 1
-    // println!("{}", minimum_sum_subarray(vec![-2, 2, -3, 1], 2, 3)); // -1
-    // println!("{}", minimum_sum_subarray(vec![1, 2, 3, 4], 2, 4)); // 3
-    // println!("{}", minimum_sum_subarray(vec![17, 13], 1, 2)); // 13
+    println!("{}", minimum_sum_subarray(vec![3, -2, 1, 4], 2, 3)); // 1
+    println!("{}", minimum_sum_subarray(vec![-2, 2, -3, 1], 2, 3)); // -1
+    println!("{}", minimum_sum_subarray(vec![1, 2, 3, 4], 2, 4)); // 3
+    println!("{}", minimum_sum_subarray(vec![17, 13], 1, 2)); // 13
     println!("{}", minimum_sum_subarray(vec![-23, 2, -12], 1, 2)); // 2
-                                                                   // println!("{}", minimum_sum_subarray(vec![6, 19, 4], 2, 2)); // 23
+    println!("{}", minimum_sum_subarray(vec![6, 19, 4], 2, 2)); // 23
 }
