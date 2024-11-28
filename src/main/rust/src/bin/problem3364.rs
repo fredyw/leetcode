561413255ffb553a4527740754a54c41415dbbac
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-11-29 01:51:54 +0800

    Skeleton for problem 3364

---
+++ src/main/rust/src/bin/problem3364.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/minimum-positive-sum-subarray/description/
+pub fn minimum_sum_subarray(nums: Vec<i32>, l: i32, r: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", minimum_sum_subarray(vec![3, -2, 1, 4], 2, 3)); // 1
+    println!("{}", minimum_sum_subarray(vec![-2, 2, -3, 1], 2, 3)); // -1
+    println!("{}", minimum_sum_subarray(vec![1, 2, 3, 4], 2, 4)); // 3
+}
