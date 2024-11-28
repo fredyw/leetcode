7be51e15fe21ff821062c10e904d882c3091bf25
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-11-29 05:33:02 +0800

    Update problem 1062

--- src/main/rust/src/bin/problem1062.rs
+++ src/main/rust/src/bin/problem1062.rs
@@ -7,4 +7,6 @@ fn main() {
     println!("{}", longest_repeating_substring("abcd".to_string())); // 0
     println!("{}", longest_repeating_substring("abbaba".to_string())); // 2
     println!("{}", longest_repeating_substring("aabcaabdaab".to_string())); // 3
+    println!("{}", longest_repeating_substring("aa".to_string())); // 1
+    println!("{}", longest_repeating_substring("aaaaa".to_string())); // 4
 }
