557d4eeeae3d276de0052cac3c7971e70548c82b
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-11-29 05:10:42 +0800

    Skeleton for problem 1062

---
+++ src/main/rust/src/bin/problem1062.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/longest-repeating-substring/description/
+pub fn longest_repeating_substring(s: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", longest_repeating_substring("abcd".to_string())); // 0
+    println!("{}", longest_repeating_substring("abbaba".to_string())); // 2
+    println!("{}", longest_repeating_substring("aabcaabdaab".to_string())); // 3
+}
