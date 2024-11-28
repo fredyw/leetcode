d7a2410bf2b4f2915fb2806c52d223cb5a019b0c
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-11-29 02:49:59 +0800

    Skeleton for problem 3360

---
+++ src/main/rust/src/bin/problem3360.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/stone-removal-game/description/
+pub fn can_alice_win(n: i32) -> bool {
+    todo!()
+}
+
+fn main() {
+    println!("{}", can_alice_win(12)); // true
+    println!("{}", can_alice_win(1)); // false
+}
