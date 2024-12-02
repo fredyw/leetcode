e2498ba6cec95169a04e8f6e4845999b5f7eff33
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-12-02 07:17:52 +0700

    Skeleton for problem 3370

---
+++ src/main/rust/src/bin/problem3370.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/smallest-number-with-all-set-bits/description/
+pub fn smallest_number(n: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", smallest_number(5)); // 7
+    println!("{}", smallest_number(10)); // 15
+    println!("{}", smallest_number(3)); // 3
+}
