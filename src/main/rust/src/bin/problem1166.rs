c8838b76fd20537b2ebad8c17e257e638ed22aff
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-12-07 16:00:03 +0700

    Update problem 1166

--- src/main/rust/src/bin/problem1166.rs
+++ src/main/rust/src/bin/problem1166.rs
@@ -1,22 +1,41 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/design-file-system/description/
-struct FileSystem {}
+struct FileSystem {
+    map: HashMap<String, i32>,
+}
 
 impl FileSystem {
     fn new() -> Self {
-        todo!()
+        Self {
+            map: HashMap::new(),
+        }
     }
 
-    fn create_path(&self, path: String, value: i32) -> bool {
-        todo!()
+    fn create_path(&mut self, path: String, value: i32) -> bool {
+        if self.map.is_empty() {
+            self.map.insert(path, value);
+            return true;
+        }
+        if let Some(index) = path.rfind('/') {
+            let parent = &path[0..index];
+            if !self.map.contains_key(parent) {
+                return false;
+            }
+            self.map.insert(path, value);
+            true
+        } else {
+            false
+        }
     }
 
     fn get(&self, path: String) -> i32 {
-        todo!()
+        *self.map.get(&path).unwrap_or(&-1)
     }
 }
 
 fn main() {
-    let fs = FileSystem::new();
+    let mut fs = FileSystem::new();
     println!("{}", fs.create_path("/a".to_string(), 1)); // true
     println!("{}", fs.get("/a".to_string())); // 1
     println!("{}", fs.create_path("/a/b".to_string(), 2)); // true
@@ -24,7 +43,7 @@ fn main() {
     println!("{}", fs.create_path("/a/b/c/d".to_string(), 3)); // false
     println!("{}", fs.get("/a/b/c".to_string())); // -1
 
-    let fs = FileSystem::new();
+    let mut fs = FileSystem::new();
     println!("{}", fs.create_path("/leet".to_string(), 1)); // true
     println!("{}", fs.create_path("/leet/code".to_string(), 2)); // true
     println!("{}", fs.get("/leet/code".to_string())); // 2
