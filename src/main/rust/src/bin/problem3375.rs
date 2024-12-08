67d7a7a7c0fece30fd6999f30d5c047fe52564d3
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-12-08 10:00:34 +0700

    Solve problem 3375

--- src/main/rust/src/bin/problem3375.rs
+++ src/main/rust/src/bin/problem3375.rs
@@ -1,6 +1,16 @@
+use std::collections::HashSet;
+
 // https://leetcode.com/problems/minimum-operations-to-make-array-values-equal-to-k/description/
 pub fn min_operations(nums: Vec<i32>, k: i32) -> i32 {
-    todo!()
+    let mut set: HashSet<i32> = HashSet::new();
+    for num in nums {
+        if num < k {
+            return -1;
+        } else if num > k {
+            set.insert(num);
+        }
+    }
+    set.len() as i32
 }
 
 fn main() {
