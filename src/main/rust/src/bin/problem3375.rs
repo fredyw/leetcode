7c4750d49e872149dfeb08ae77ac748d3af5995f
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-12-08 09:48:34 +0700

    Skeleton for problem 3375

---
+++ src/main/rust/src/bin/problem3375.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/minimum-operations-to-make-array-values-equal-to-k/description/
+pub fn min_operations(nums: Vec<i32>, k: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", min_operations(vec![5, 2, 5, 4, 5], 2)); // 2
+    println!("{}", min_operations(vec![2, 1, 2], 2)); // -1
+    println!("{}", min_operations(vec![9, 7, 5, 3], 1)); // 4
+}
