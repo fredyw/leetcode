7e4cb2a4a65b06441d305caed1bcccf9446c2c34
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-12-08 09:44:15 +0700

    Solve problem 1166

--- src/main/rust/src/bin/problem1166.rs
+++ src/main/rust/src/bin/problem1166.rs
@@ -14,9 +14,12 @@ impl FileSystem {
 
     fn create_path(&mut self, path: String, value: i32) -> bool {
         if let Some(index) = path.rfind('/') {
+            if self.map.contains_key(&path) {
+                return false;
+            }
             if index > 0 {
                 let parent = &path[0..index];
-                if !self.map.contains_key(parent) || self.map.contains_key(&path) {
+                if !self.map.contains_key(parent) {
                     return false;
                 }
             }
@@ -33,26 +36,26 @@ impl FileSystem {
 }
 
 fn main() {
-    // let mut fs = FileSystem::new();
-    // println!("{}", fs.create_path("/a".to_string(), 1)); // true
-    // println!("{}", fs.get("/a".to_string())); // 1
-    // println!("{}", fs.create_path("/a/b".to_string(), 2)); // true
-    // println!("{}", fs.get("/a/b".to_string())); // 2
-    // println!("{}", fs.create_path("/a/b/c/d".to_string(), 3)); // false
-    // println!("{}", fs.get("/a/b/c".to_string())); // -1
-    //
-    // let mut fs = FileSystem::new();
-    // println!("{}", fs.create_path("/leet".to_string(), 1)); // true
-    // println!("{}", fs.create_path("/leet/code".to_string(), 2)); // true
-    // println!("{}", fs.get("/leet/code".to_string())); // 2
-    // println!("{}", fs.create_path("/c/d".to_string(), 1)); // false
-    // println!("{}", fs.get("/c".to_string())); // -1
-    //
-    // let mut fs = FileSystem::new();
-    // println!("{}", fs.create_path("/leet".to_string(), 1)); // true
-    // println!("{}", fs.create_path("/leet/code".to_string(), 2)); // true
-    // println!("{}", fs.create_path("/leet/code".to_string(), 3)); // false
-    // println!("{}", fs.get("/leet/code".to_string())); // 2
+    let mut fs = FileSystem::new();
+    println!("{}", fs.create_path("/a".to_string(), 1)); // true
+    println!("{}", fs.get("/a".to_string())); // 1
+    println!("{}", fs.create_path("/a/b".to_string(), 2)); // true
+    println!("{}", fs.get("/a/b".to_string())); // 2
+    println!("{}", fs.create_path("/a/b/c/d".to_string(), 3)); // false
+    println!("{}", fs.get("/a/b/c".to_string())); // -1
+
+    let mut fs = FileSystem::new();
+    println!("{}", fs.create_path("/leet".to_string(), 1)); // true
+    println!("{}", fs.create_path("/leet/code".to_string(), 2)); // true
+    println!("{}", fs.get("/leet/code".to_string())); // 2
+    println!("{}", fs.create_path("/c/d".to_string(), 1)); // false
+    println!("{}", fs.get("/c".to_string())); // -1
+
+    let mut fs = FileSystem::new();
+    println!("{}", fs.create_path("/leet".to_string(), 1)); // true
+    println!("{}", fs.create_path("/leet/code".to_string(), 2)); // true
+    println!("{}", fs.create_path("/leet/code".to_string(), 3)); // false
+    println!("{}", fs.get("/leet/code".to_string())); // 2
 
     let mut fs = FileSystem::new();
     println!("{}", fs.create_path("/a".to_string(), 1)); // true
