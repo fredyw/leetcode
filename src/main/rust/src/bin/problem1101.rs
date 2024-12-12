520117c1e893eaf75aa6bbdc42156a641869b540
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-12-12 15:40:38 +0700

    Update problem 1101

--- src/main/rust/src/bin/problem1101.rs
+++ src/main/rust/src/bin/problem1101.rs
@@ -1,6 +1,47 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/the-earliest-moment-when-everyone-become-friends/description/
-pub fn earliest_acq(logs: Vec<Vec<i32>>, n: i32) -> i32 {
-    todo!()
+pub fn earliest_acq(mut logs: Vec<Vec<i32>>, n: i32) -> i32 {
+    struct UnionFind {
+        // Child to parent
+        v: HashMap<char, char>,
+    }
+
+    impl UnionFind {
+        fn new() -> UnionFind {
+            UnionFind { v: HashMap::new() }
+        }
+
+        fn union(&mut self, a: char, b: char) {
+            if self.v.get(&a).is_none() {
+                self.v.insert(a, a);
+            }
+            if self.v.get(&b).is_none() {
+                self.v.insert(b, b);
+            }
+            let root_a = self.find(a).unwrap();
+            let root_b = self.find(b).unwrap();
+            if root_a > root_b {
+                self.v.insert(root_a, root_b);
+            } else if root_a < root_b {
+                self.v.insert(root_b, root_a);
+            }
+        }
+
+        fn find(&self, a: char) -> Option<char> {
+            let mut child = a;
+            while let Some(parent) = self.v.get(&child) {
+                if *parent == child {
+                    return Some(*parent);
+                }
+                child = *parent;
+            }
+            None
+        }
+    }
+
+    logs.sort_by(|a, b| a[0].cmp(&b[0]));
+    0
 }
 
 fn main() {
