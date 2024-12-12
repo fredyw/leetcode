0ef310495196d3fcf820191c6a1a7ce8c9ad0d0b
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-12-12 16:04:59 +0700

    Skeleton for problem 3379

---
+++ src/main/rust/src/bin/problem3379.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/transformed-array/description/
+pub fn construct_transformed_array(nums: Vec<i32>) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", construct_transformed_array(vec![3, -2, 1, 1])); // [1,1,1,3]
+    println!("{:?}", construct_transformed_array(vec![-1, 4, -1])); // [-1,-1,4]
+}
