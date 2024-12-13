d6b7dc6f0aaa1de4f281b498586c5b67552f6829
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-12-13 10:24:44 +0700

    Update problem 573

--- src/main/rust/src/bin/problem573.rs
+++ src/main/rust/src/bin/problem573.rs
@@ -10,7 +10,16 @@ pub fn min_distance(
         (x1 - x2).abs() + (y1 - y2).abs()
     }
 
-    todo!()
+    let mut answer = 0;
+    let mut x1 = squirrel[0];
+    let mut y1 = squirrel[1];
+    for nut in nuts.iter() {
+        let x2 = nut[0];
+        let y2 = nut[1];
+        let distance = distance(x1, y1, x2, y2);
+        println!("distance: {}", distance);
+    }
+    answer
 }
 
 fn main() {
@@ -18,8 +27,8 @@ fn main() {
         "{}",
         min_distance(5, 7, vec![2, 2], vec![4, 4], vec![vec![3, 0], vec![2, 5]])
     ); // 12
-    println!(
-        "{}",
-        min_distance(1, 3, vec![0, 1], vec![0, 0], vec![vec![0, 2]])
-    ); // 3
+       // println!(
+       //     "{}",
+       //     min_distance(1, 3, vec![0, 1], vec![0, 0], vec![vec![0, 2]])
+       // ); // 3
 }
