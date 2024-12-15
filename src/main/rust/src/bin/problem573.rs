a114212a1f1205a325a23768b480fd8a5d4c90aa
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-12-16 04:38:25 +0700

    Update problem 573

--- src/main/rust/src/bin/problem573.rs
+++ src/main/rust/src/bin/problem573.rs
@@ -10,15 +10,29 @@ pub fn min_distance(
         (x1 - x2).abs() + (y1 - y2).abs()
     }
 
-    let mut answer = 0;
-    let mut x1 = squirrel[0];
-    let mut y1 = squirrel[1];
+    let mut min_distance = i32::MAX;
+    let mut min_x = 0;
+    let mut min_y = 0;
     for nut in nuts.iter() {
-        let x2 = nut[0];
-        let y2 = nut[1];
-        let distance = distance(x1, y1, x2, y2);
-        println!("distance: {}", distance);
+        let distance = distance(squirrel[0], squirrel[1], nut[0], nut[1]);
+        if distance < min_distance {
+            min_distance = distance;
+            min_x = nut[0];
+            min_y = nut[1];
+        }
     }
+    let mut answer = min_distance;
+    let mut x = min_x;
+    let mut y = min_y;
+    for nut in nuts.iter() {
+        if nut[0] == min_x && nut[1] == min_y {
+            continue;
+        }
+        answer += distance(x, y, nut[0], nut[1]);
+        x = nut[0];
+        y = nut[1];
+    }
+    answer += distance(x, y, tree[0], tree[1]);
     answer
 }
 
@@ -27,8 +41,8 @@ fn main() {
         "{}",
         min_distance(5, 7, vec![2, 2], vec![4, 4], vec![vec![3, 0], vec![2, 5]])
     ); // 12
-       // println!(
-       //     "{}",
-       //     min_distance(1, 3, vec![0, 1], vec![0, 0], vec![vec![0, 2]])
-       // ); // 3
+    println!(
+        "{}",
+        min_distance(1, 3, vec![0, 1], vec![0, 0], vec![vec![0, 2]])
+    ); // 3
 }
