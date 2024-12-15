dc70a0df5f09760b68b5faa1dc99df61c9883f31
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-12-16 05:16:41 +0700

    Update problem 573

--- src/main/rust/src/bin/problem573.rs
+++ src/main/rust/src/bin/problem573.rs
@@ -10,29 +10,24 @@ pub fn min_distance(
         (x1 - x2).abs() + (y1 - y2).abs()
     }
 
-    let mut min_distance = i32::MAX;
+    let mut answer = i32::MAX;
     let mut min_x = 0;
     let mut min_y = 0;
     for nut in nuts.iter() {
         let distance = distance(squirrel[0], squirrel[1], nut[0], nut[1]);
-        if distance < min_distance {
-            min_distance = distance;
+        if distance <= answer {
+            answer = distance;
             min_x = nut[0];
             min_y = nut[1];
         }
     }
-    let mut answer = min_distance;
-    let mut x = min_x;
-    let mut y = min_y;
+    answer += distance(min_x, min_y, tree[0], tree[1]);
     for nut in nuts.iter() {
         if nut[0] == min_x && nut[1] == min_y {
             continue;
         }
-        answer += distance(x, y, nut[0], nut[1]);
-        x = nut[0];
-        y = nut[1];
+        answer += distance(tree[0], tree[1], nut[0], nut[1]) * 2;
     }
-    answer += distance(x, y, tree[0], tree[1]);
     answer
 }
 
@@ -76,4 +71,50 @@ fn main() {
             ]
         )
     ); // 100
+    println!(
+        "{}",
+        min_distance(
+            7,
+            6,
+            vec![5, 4],
+            vec![3, 4],
+            vec![
+                vec![1, 4],
+                vec![3, 0],
+                vec![5, 0],
+                vec![6, 1],
+                vec![1, 3],
+                vec![1, 1],
+                vec![0, 3],
+                vec![2, 2],
+                vec![6, 3],
+                vec![4, 1],
+                vec![2, 4],
+                vec![2, 1],
+                vec![1, 0],
+                vec![3, 2],
+                vec![4, 2],
+                vec![2, 5],
+                vec![0, 2],
+                vec![1, 2],
+                vec![3, 3],
+                vec![6, 2],
+                vec![2, 0],
+                vec![5, 1],
+                vec![4, 5],
+                vec![5, 2],
+                vec![0, 1],
+                vec![4, 0],
+                vec![5, 3],
+                vec![3, 5],
+                vec![0, 5],
+                vec![6, 4],
+                vec![5, 5],
+                vec![2, 3],
+                vec![3, 1],
+                vec![4, 4],
+                vec![4, 3]
+            ]
+        )
+    ); // 288
 }
