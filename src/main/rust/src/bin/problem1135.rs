4a8f87c0d34c03a842e7a0854fa5b3eeb6dbafbb
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-12-21 14:41:01 +0700

    Skeleton for problem 1135

---
+++ src/main/rust/src/bin/problem1135.rs
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/connecting-cities-with-minimum-cost/description/
+pub fn minimum_cost(n: i32, connections: Vec<Vec<i32>>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        minimum_cost(3, vec![vec![1, 2, 5], vec![1, 3, 6], vec![2, 3, 1]])
+    ); // 6
+    println!("{}", minimum_cost(4, vec![vec![1, 2, 3], vec![3, 4, 4]])); // -1
+}
