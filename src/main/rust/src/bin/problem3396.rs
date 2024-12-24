7ad85176a679a7a6910b776f184cbe35191d72b0
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-12-24 09:44:36 +0700

    Skeleton for problem 3396

---
+++ src/main/rust/src/bin/problem3396.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/minimum-number-of-operations-to-make-elements-in-array-distinct/description/
+pub fn minimum_operations(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", minimum_operations(vec![1, 2, 3, 4, 2, 3, 3, 5, 7])); // 2
+    println!("{}", minimum_operations(vec![4, 5, 6, 4, 4])); // 2
+    println!("{}", minimum_operations(vec![6, 7, 8, 9])); // 0
+}
