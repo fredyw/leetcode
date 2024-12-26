9f01929007c1d25caa7d95437efd26c953adcc3d
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-12-26 23:09:12 +0700

    Update problem 3396

--- src/main/rust/src/bin/problem3396.rs
+++ src/main/rust/src/bin/problem3396.rs
@@ -1,10 +1,38 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/minimum-number-of-operations-to-make-elements-in-array-distinct/description/
 pub fn minimum_operations(nums: Vec<i32>) -> i32 {
-    todo!()
+    let mut map: HashMap<i32, i32> = HashMap::new();
+    for num in nums.iter() {
+        *map.entry(*num).or_insert(0) += 1;
+    }
+    let mut map: HashMap<i32, i32> = map.into_iter().filter(|(_, count)| *count > 1).collect();
+    if map.is_empty() {
+        return 0;
+    }
+    println!("{:?}", map);
+    let mut answer = 0;
+    let mut i = 0;
+    while i < nums.len() {
+        let mut j = i;
+        while j < nums.len() && j < i + 3 {
+            if let Some(count) = map.get_mut(&nums[j]) {
+                *count -= 1;
+                if *count == 0 {
+                    map.remove(&nums[j]);
+                }
+            }
+            j += 1;
+        }
+        answer += 1;
+        println!("{:?}", map);
+        i += 3;
+    }
+    answer
 }
 
 fn main() {
     println!("{}", minimum_operations(vec![1, 2, 3, 4, 2, 3, 3, 5, 7])); // 2
-    println!("{}", minimum_operations(vec![4, 5, 6, 4, 4])); // 2
-    println!("{}", minimum_operations(vec![6, 7, 8, 9])); // 0
+                                                                         // println!("{}", minimum_operations(vec![4, 5, 6, 4, 4])); // 2
+                                                                         // println!("{}", minimum_operations(vec![6, 7, 8, 9])); // 0
 }
