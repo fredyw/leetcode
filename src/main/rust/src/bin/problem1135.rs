98bde22b34128504bdbe400ba6742c2c3bf578d8
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-12-28 06:50:48 -0600

    Solve problem 1135

--- src/main/rust/src/bin/problem1135.rs
+++ src/main/rust/src/bin/problem1135.rs
@@ -1,4 +1,5 @@
-use std::collections::{HashMap, HashSet};
+use std::cmp::Reverse;
+use std::collections::{BinaryHeap, HashMap, HashSet};
 
 // https://leetcode.com/problems/connecting-cities-with-minimum-cost/description/
 pub fn minimum_cost(n: i32, connections: Vec<Vec<i32>>) -> i32 {
@@ -14,18 +15,42 @@ pub fn minimum_cost(n: i32, connections: Vec<Vec<i32>>) -> i32 {
         graph
     }
 
-    fn visit(graph: &HashMap<i32, Vec<i32>>, visited: &mut HashSet<i32>, node: i32) {
-        visited.insert(node);
-        for adjacent in graph.get(&node).unwrap_or(&Vec::new()).iter() {
-            if !visited.contains(adjacent) {
-                visit(&graph, visited, *adjacent);
+    fn visit(
+        graph: &HashMap<i32, Vec<(i32, i32)>>,
+        visited: &mut HashSet<i32>,
+        binary_heap: &mut BinaryHeap<Reverse<(i32, i32, i32)>>,
+        from: i32,
+    ) {
+        visited.insert(from);
+        for (to, cost) in graph.get(&from).unwrap_or(&Vec::new()).iter() {
+            if !visited.contains(to) {
+                binary_heap.push(Reverse((*cost, from, *to)));
             }
         }
     }
 
+    let mut answer = 0;
     let graph = build_graph(&connections);
-
-    todo!()
+    let mut binary_heap: BinaryHeap<Reverse<(i32, i32, i32)>> = BinaryHeap::new();
+    let mut visited: HashSet<i32> = HashSet::new();
+    visit(&graph, &mut visited, &mut binary_heap, 1);
+    while let Some(Reverse((cost, from, to))) = binary_heap.pop() {
+        if visited.contains(&from) && visited.contains(&to) {
+            continue;
+        }
+        answer += cost;
+        if !visited.contains(&from) {
+            visit(&graph, &mut visited, &mut binary_heap, from);
+        }
+        if !visited.contains(&to) {
+            visit(&graph, &mut visited, &mut binary_heap, to);
+        }
+    }
+    if visited.len() != n as usize {
+        -1
+    } else {
+        answer
+    }
 }
 
 fn main() {
