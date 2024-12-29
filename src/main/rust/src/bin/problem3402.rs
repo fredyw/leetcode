36d81e5bb2dc7867dc660c13fc87568fc9d5ae80
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-12-29 05:48:42 -0600

    Skeleton for problem 3402

---
+++ src/main/rust/src/bin/problem3402.rs
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/minimum-operations-to-make-columns-strictly-increasing/
+pub fn minimum_operations(grid: Vec<Vec<i32>>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        minimum_operations(vec![vec![3, 2], vec![1, 3], vec![3, 4], vec![0, 1]])
+    ); // 15
+    println!(
+        "{}",
+        minimum_operations(vec![vec![3, 2, 1], vec![2, 1, 0], vec![1, 2, 3]])
+    ); // 12
+}
