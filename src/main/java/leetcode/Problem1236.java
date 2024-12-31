1db61e479373b7ccde48a79e40c968ed5e8d34f1
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-12-31 03:46:28 -0600

    Solve problem 1236

--- src/main/java/leetcode/Problem1236.java
+++ src/main/java/leetcode/Problem1236.java
@@ -1,6 +1,9 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 // https://leetcode.com/problems/web-crawler/
 public class Problem1236 {
@@ -9,6 +12,29 @@ public class Problem1236 {
     }
 
     public List<String> crawl(String startUrl, HtmlParser htmlParser) {
-        return List.of();
+        Set<String> answer = new HashSet<>();
+        crawl(startUrl, htmlParser, answer);
+        return answer.stream().toList();
+    }
+
+    private static void crawl(String startUrl, HtmlParser htmlParser, Set<String> urls) {
+        if (!urls.add(startUrl)) {
+            return;
+        }
+        String hostname = getHostname(startUrl);
+        for (String url : htmlParser.getUrls(startUrl)) {
+            if (getHostname(url).equals(hostname)) {
+                crawl(url, htmlParser, urls);
+            }
+        }
+    }
+
+    private static String getHostname(String url) {
+        String sub = url.substring(7);
+        int index = sub.indexOf("/");
+        if (index == -1) {
+            return sub;
+        }
+        return sub.substring(0, index);
     }
 }
