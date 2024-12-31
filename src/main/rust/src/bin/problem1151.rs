b60b206e293ef981c3e3f39b46e8cde4c669e232
Fredy Wijaya <fredy.wijaya@gmail.com>
2024-12-31 04:58:53 -0600

    Skeleton for problem 1151

---
+++ src/main/rust/src/bin/problem1151.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/minimum-swaps-to-group-all-1s-together/description/
+pub fn min_swaps(data: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", min_swaps(vec![1, 0, 1, 0, 1])); // 1
+    println!("{}", min_swaps(vec![0, 0, 0, 1, 0])); // 0
+    println!("{}", min_swaps(vec![1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1])); // 3
+}
