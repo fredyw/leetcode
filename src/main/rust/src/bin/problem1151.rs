ce1461bf7e6fa8c51659c9caf7735d986a694ef7
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-02 13:42:42 -0600

    Solve problem 1151

--- src/main/rust/src/bin/problem1151.rs
+++ src/main/rust/src/bin/problem1151.rs
@@ -1,6 +1,31 @@
 // https://leetcode.com/problems/minimum-swaps-to-group-all-1s-together/description/
 pub fn min_swaps(data: Vec<i32>) -> i32 {
-    todo!()
+    let mut total_one_count: i32 = 0;
+    for n in data.iter() {
+        if *n == 1 {
+            total_one_count += 1;
+        }
+    }
+    let mut answer = i32::MAX;
+    let mut ones = 0;
+    let mut i: usize = 0;
+    while i < total_one_count as usize {
+        if data[i] == 1 {
+            ones += 1;
+        }
+        i += 1;
+        answer = answer.min(total_one_count - ones);
+    }
+    for j in i..data.len() {
+        if data[j - total_one_count as usize] == 1 {
+            ones -= 1;
+        }
+        if data[j] == 1 {
+            ones += 1;
+        }
+        answer = answer.min(total_one_count - ones);
+    }
+    answer
 }
 
 fn main() {
