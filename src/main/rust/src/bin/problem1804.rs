1e83645775dcb6cdac65ee8e0270333518666341
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-04 17:07:38 -0600

    Update problem 1804

--- src/main/rust/src/bin/problem1804.rs
+++ src/main/rust/src/bin/problem1804.rs
@@ -2,27 +2,68 @@ use std::collections::HashMap;
 
 // https://leetcode.com/problems/implement-trie-ii-prefix-tree/description/
 struct Trie {
-    children: HashMap<char, Trie>,
+    children: HashMap<u8, Trie>,
+    num_words: i32,
 }
 
 impl Trie {
     fn new() -> Self {
         Self {
             children: HashMap::new(),
+            num_words: 0,
         }
     }
 
-    fn insert(&mut self, word: String) {}
+    fn insert(&mut self, word: String) {
+        fn insert(trie: &mut Trie, word: &[u8], i: usize) {
+            if i == word.len() {
+                trie.num_words += 1;
+                return;
+            }
+            if let Some(child) = trie.children.get_mut(&word[i]) {
+                insert(child, word, i + 1);
+            } else {
+                let mut new_trie = Trie::new();
+                insert(&mut new_trie, word, i + 1);
+                trie.children.insert(word[i], new_trie);
+            }
+        }
+
+        insert(self, word.as_bytes(), 0)
+    }
 
     fn count_words_equal_to(&self, word: String) -> i32 {
-        todo!()
+        fn find(trie: &Trie, word: &[u8], i: usize) -> i32 {
+            if i == word.len() {
+                return trie.num_words;
+            }
+            if let Some(child) = trie.children.get(&word[i]) {
+                find(child, word, i + 1)
+            } else {
+                0
+            }
+        }
+
+        find(self, word.as_bytes(), 0)
     }
 
     fn count_words_starting_with(&self, prefix: String) -> i32 {
-        todo!()
+        0
     }
 
-    fn erase(&mut self, word: String) {}
+    fn erase(&mut self, word: String) {
+        fn erase(trie: &mut Trie, word: &[u8], i: usize) {
+            if i == word.len() {
+                trie.num_words -= 1;
+                return;
+            }
+            if let Some(child) = trie.children.get_mut(&word[i]) {
+                erase(child, word, i + 1);
+            }
+        }
+
+        erase(self, &word.as_bytes(), 0);
+    }
 }
 
 fn main() {
