df7555699b8f1d4b2520313dedcb991788ecd2fa
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-04 17:32:27 -0600

    Skeleton for problem 1746

---
+++ src/main/rust/src/bin/problem1746.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/maximum-subarray-sum-after-one-operation/
+pub fn max_sum_after_operation(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", max_sum_after_operation(vec![2, -1, -4, -3])); // 17
+    println!("{}", max_sum_after_operation(vec![1, -1, 1, 1, -1, -1, 1])); // 4
+}
