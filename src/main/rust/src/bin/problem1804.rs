ebcbccc1ab11d45b594719fbab808567279f7222
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-04 08:54:03 -0600

    Skeleton for problem 1804

---
+++ src/main/rust/src/bin/problem1804.rs
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/implement-trie-ii-prefix-tree/description/
+struct Trie {}
+
+impl Trie {
+    fn new() -> Self {
+        Self {}
+    }
+
+    fn insert(&self, word: String) {}
+
+    fn count_words_equal_to(&self, word: String) -> i32 {
+        todo!()
+    }
+
+    fn count_words_starting_with(&self, prefix: String) -> i32 {
+        todo!()
+    }
+
+    fn erase(&self, word: String) {}
+}
+
+fn main() {
+    let mut trie = Trie::new();
+    trie.insert("apple".to_string());
+    trie.insert("apple".to_string());
+    println!("{}", trie.count_words_equal_to("apple".to_string())); // 2
+    println!("{}", trie.count_words_starting_with("app".to_string())); // 2
+    trie.erase("apple".to_string());
+    println!("{}", trie.count_words_equal_to("apple".to_string())); // 1
+    println!("{}", trie.count_words_starting_with("app".to_string())); // 1
+    trie.erase("apple".to_string());
+    println!("{}", trie.count_words_starting_with("app".to_string())); // 0
+}
