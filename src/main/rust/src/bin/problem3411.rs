e78c6cdd23aaf0eb0d513b982f335990bacac1ae
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-05 13:57:51 -0600

    Skeleton for problem 3411

---
+++ src/main/rust/src/bin/problem3411.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/maximum-subarray-with-equal-products/description/
+pub fn max_length(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", max_length(vec![1, 2, 1, 2, 1, 1, 1])); // 5
+    println!("{}", max_length(vec![2, 3, 4, 5, 6])); // 3
+    println!("{}", max_length(vec![1, 2, 3, 1, 4, 5, 1])); // 5
+}
