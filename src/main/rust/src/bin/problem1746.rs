b417bfc90fe103326f35147491743ab87abc3a6a
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-06 17:43:56 -0600

    Update problem 1746

--- src/main/rust/src/bin/problem1746.rs
+++ src/main/rust/src/bin/problem1746.rs
@@ -1,22 +1,27 @@
 // https://leetcode.com/problems/maximum-subarray-sum-after-one-operation/
 pub fn max_sum_after_operation(nums: Vec<i32>) -> i32 {
-    fn f(nums: &Vec<i32>, i: usize, square: bool) -> i32 {
+    fn f(nums: &Vec<i32>, i: usize, square: bool, answer: &mut i32) -> i32 {
         if i == nums.len() {
             return 0;
         }
-        if square {
-            f(nums, i + 1, false) + nums[i]
+        let mut max = if square {
+            f(nums, i + 1, false, answer) + nums[i]
         } else {
-            let squared = 0;
-            let non_squared = f(nums, i + 1, false) + nums[i];
+            let squared = f(nums, i + 1, true, answer) + (nums[i] * nums[i]);
+            let non_squared = f(nums, i + 1, false, answer) + nums[i];
             squared.max(non_squared)
-        }
+        };
+        println!("max: {}", max);
+        *answer = *answer.max(&mut max);
+        max
     }
 
-    f(&nums, 0, false)
+    let mut answer = 0;
+    f(&nums, 0, false, &mut answer);
+    answer
 }
 
 fn main() {
     println!("{}", max_sum_after_operation(vec![2, -1, -4, -3])); // 17
-    println!("{}", max_sum_after_operation(vec![1, -1, 1, 1, -1, -1, 1])); // 4
+                                                                  // println!("{}", max_sum_after_operation(vec![1, -1, 1, 1, -1, -1, 1])); // 4
 }
