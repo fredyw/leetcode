11890ce97f38def0ac75daa5faaed76d9c9c049d
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-06 17:11:15 -0600

    Update problem 1746

--- src/main/rust/src/bin/problem1746.rs
+++ src/main/rust/src/bin/problem1746.rs
@@ -1,6 +1,19 @@
 // https://leetcode.com/problems/maximum-subarray-sum-after-one-operation/
 pub fn max_sum_after_operation(nums: Vec<i32>) -> i32 {
-    todo!()
+    fn f(nums: &Vec<i32>, i: usize, square: bool) -> i32 {
+        if i == nums.len() {
+            return 0;
+        }
+        if square {
+            f(nums, i + 1, false) + nums[i]
+        } else {
+            let squared = 0;
+            let non_squared = f(nums, i + 1, false) + nums[i];
+            squared.max(non_squared)
+        }
+    }
+
+    f(&nums, 0, false)
 }
 
 fn main() {
