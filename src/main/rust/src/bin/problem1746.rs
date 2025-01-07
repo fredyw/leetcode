63b6b167e93bcaa428c5547d50766a6c90f0c5c4
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-07 10:25:28 -0600

    Solve problem 1746

--- src/main/rust/src/bin/problem1746.rs
+++ src/main/rust/src/bin/problem1746.rs
@@ -1,27 +1,45 @@
 // https://leetcode.com/problems/maximum-subarray-sum-after-one-operation/
 pub fn max_sum_after_operation(nums: Vec<i32>) -> i32 {
-    fn f(nums: &Vec<i32>, i: usize, can_square: bool, answer: &mut i32) -> i32 {
+    fn max_sum_after_operation(
+        nums: &Vec<i32>,
+        i: usize,
+        can_square: bool,
+        memo: &mut Vec<Vec<i32>>,
+        answer: &mut i32,
+    ) -> i32 {
         if i == nums.len() {
             return 0;
         }
+        if memo[i][if can_square { 1 } else { 0 }] != -1 {
+            return memo[i][if can_square { 1 } else { 0 }];
+        }
         let mut max = if can_square {
-            let squared = f(nums, i + 1, false, answer) + (nums[i] * nums[i]);
-            let non_squared = nums[i].max(f(nums, i + 1, true, answer) + nums[i]);
+            let squared = (nums[i] * nums[i]).max(
+                max_sum_after_operation(nums, i + 1, false, memo, answer) + (nums[i] * nums[i]),
+            );
+            let non_squared =
+                nums[i].max(max_sum_after_operation(nums, i + 1, true, memo, answer) + nums[i]);
             squared.max(non_squared)
         } else {
-            nums[i].max(f(nums, i + 1, false, answer) + nums[i])
+            nums[i].max(max_sum_after_operation(nums, i + 1, false, memo, answer) + nums[i])
         };
-        println!("max: {}", max);
+        memo[i][if can_square { 1 } else { 0 }] = max;
         *answer = *answer.max(&mut max);
         max
     }
 
     let mut answer = 0;
-    f(&nums, 0, true, &mut answer);
+    max_sum_after_operation(
+        &nums,
+        0,
+        true,
+        &mut vec![vec![-1; 2]; nums.len()],
+        &mut answer,
+    );
     answer
 }
 
 fn main() {
     println!("{}", max_sum_after_operation(vec![2, -1, -4, -3])); // 17
-                                                                  // println!("{}", max_sum_after_operation(vec![1, -1, 1, 1, -1, -1, 1])); // 4
+    println!("{}", max_sum_after_operation(vec![1, -1, 1, 1, -1, -1, 1])); // 4
 }
