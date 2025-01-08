0f8de4b3fb1a631d0f3cfd83c3a0bfd0b8ba6a30
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-07 19:45:19 -0600

    Skeleton for problem 1794

---
+++ src/main/rust/src/bin/problem1794.rs
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/count-pairs-of-equal-substrings-with-minimum-difference/description/
+pub fn count_quadruples(first_string: String, second_string: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        count_quadruples("abcd".to_string(), "bccda".to_string())
+    ); // 1
+    println!("{}", count_quadruples("ab".to_string(), "cd".to_string())); // 0
+}
