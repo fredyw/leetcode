be0e5ecd0565d3f3663c47a6b054f24f1c09066f
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-07 19:31:52 -0600

    Skeleton for problem 1698

---
+++ src/main/rust/src/bin/problem1698.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/number-of-distinct-substrings-in-a-string/description/
+pub fn count_distinct(s: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", count_distinct("aabbaba".to_string())); // 21
+    println!("{}", count_distinct("abcdefg".to_string())); // 28
+}
