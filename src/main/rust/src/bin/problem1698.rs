32ca5779571417d41b93246ab9e8e9a3acee3a49
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-07 19:43:22 -0600

    Solve problem 1698

--- src/main/rust/src/bin/problem1698.rs
+++ src/main/rust/src/bin/problem1698.rs
@@ -1,6 +1,15 @@
+use std::collections::HashSet;
+
 // https://leetcode.com/problems/number-of-distinct-substrings-in-a-string/description/
 pub fn count_distinct(s: String) -> i32 {
-    todo!()
+    let mut set: HashSet<&str> = HashSet::new();
+    for i in 0..s.len() {
+        for j in i..s.len() {
+            let sub = &s[i..=j];
+            set.insert(sub);
+        }
+    }
+    set.len() as i32
 }
 
 fn main() {
