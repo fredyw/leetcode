88c4a3e4914d81686d8743b03d067ffddb563000
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-10 00:44:22 -0600

    Solve problem 1772

--- src/main/rust/src/bin/problem1772.rs
+++ src/main/rust/src/bin/problem1772.rs
@@ -1,6 +1,39 @@
+use std::collections::{HashMap, HashSet};
+
 // https://leetcode.com/problems/sort-features-by-popularity/description/
 pub fn sort_features(features: Vec<String>, responses: Vec<String>) -> Vec<String> {
-    todo!()
+    let mut feature_map: HashMap<String, usize> = HashMap::new();
+    for (i, feature) in features.into_iter().enumerate() {
+        feature_map.insert(feature, i);
+    }
+    let mut response_set: Vec<HashSet<&str>> = vec![];
+    for response in responses.iter() {
+        response_set.push(response.split(" ").collect::<HashSet<&str>>());
+    }
+    let mut map: HashMap<&str, i32> = HashMap::new();
+    for (feature, _) in feature_map.iter() {
+        map.insert(feature, 0);
+    }
+    for response in response_set.iter() {
+        for s in response {
+            if feature_map.contains_key(*s) {
+                *map.entry(s).or_insert(0) += 1;
+            }
+        }
+    }
+    let mut v: Vec<(&str, i32)> = map.into_iter().map(|(s, c)| (s, c)).collect();
+    v.sort_by(|a, b| {
+        let cmp = b.1.cmp(&a.1);
+        if cmp == std::cmp::Ordering::Equal {
+            feature_map
+                .get(a.0)
+                .unwrap()
+                .cmp(feature_map.get(b.0).unwrap())
+        } else {
+            cmp
+        }
+    });
+    v.into_iter().map(|(s, _)| s.to_owned()).collect()
 }
 
 fn main() {
