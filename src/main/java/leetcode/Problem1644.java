2dc5030992bc1e72752a71faebdb0b51e5519e42
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-10 11:06:57 -0600

    Update problem 1644

--- src/main/java/leetcode/Problem1644.java
+++ src/main/java/leetcode/Problem1644.java
@@ -13,7 +13,26 @@ public class Problem1644 {
     }
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        // TODO
-        return null;
+        LCA lca = new LCA();
+        lowestCommonAncestor(root, p, q, lca);
+        return lca.node;
+    }
+
+    private static class LCA {
+        private TreeNode node;
+    }
+
+    private boolean lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q, LCA lca) {
+        if (root == null) {
+            return false;
+        }
+        boolean parent = root.val == p.val || root.val == q.val;
+        boolean left = lowestCommonAncestor(root.left, p, q, lca);
+        boolean right = lowestCommonAncestor(root.right, p, q, lca);
+        if (parent || (left && right)) {
+            lca.node = root;
+            return true;
+        }
+        return left || right;
     }
 }
