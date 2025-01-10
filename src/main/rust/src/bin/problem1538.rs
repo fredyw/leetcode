60cea04aa2b4414224445b414cdc82f4a42d2c87
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-10 13:07:22 -0600

    Skeleton for problem 1538

---
+++ src/main/rust/src/bin/problem1538.rs
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/guess-the-majority-in-a-hidden-array/
+struct ArrayReader;
+
+impl ArrayReader {
+    // Compares 4 different elements in the array
+    // return 4 if the values of the 4 elements are the same (0 or 1).
+    // return 2 if three elements have a value equal to 0 and one element has value equal to 1 or vice versa.
+    // return 0 : if two element have a value equal to 0 and two elements have a value equal to 1.
+    pub fn query(a: i32, b: i32, c: i32, d: i32) -> i32 {
+        todo!()
+    }
+
+    // Returns the length of the array
+    pub fn length() -> i32 {
+        todo!()
+    }
+}
+
+pub fn get_majority(reader: &ArrayReader) -> i32 {
+    todo!()
+}
+
+fn main() {}
