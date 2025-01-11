b4b2f61c3534e691b16e08410cc20049a29c27fb
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-11 16:27:57 -0600

    Skeleton for problem 742

---
+++ src/main/java/leetcode/Problem742.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+// https://leetcode.com/problems/closest-leaf-in-a-binary-tree/description/
+public class Problem742 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int findClosestLeaf(TreeNode root, int k) {
+        // TODO
+        return 0;
+    }
+}
