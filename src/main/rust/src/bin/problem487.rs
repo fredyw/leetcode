95bd0bccdf998be06d99139339bebb8f01d5af39
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-15 01:19:09 -0600

    Skeleton for problem 487

---
+++ src/main/rust/src/bin/problem487.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/max-consecutive-ones-ii/description/
+pub fn find_max_consecutive_ones(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", find_max_consecutive_ones(vec![1, 0, 1, 1, 0])); // 4
+    println!("{}", find_max_consecutive_ones(vec![1, 0, 1, 1, 0, 1])); // 4
+}
