01a7381982e1b01e80ce5ac5ae6cc67f3631dfd0
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-15 19:32:53 -0600

    Solve problem 487

--- src/main/rust/src/bin/problem487.rs
+++ src/main/rust/src/bin/problem487.rs
@@ -3,13 +3,15 @@ pub fn find_max_consecutive_ones(nums: Vec<i32>) -> i32 {
     let mut answer = 0;
     let mut prev = 0;
     let mut current = 0;
+    let mut zero_index = 0;
     let mut zero_count = 0;
     while current < nums.len() {
         if nums[current] == 0 {
             if zero_count == 1 {
                 zero_count -= 1;
-                prev = current;
+                prev = zero_index + 1;
             }
+            zero_index = current;
             zero_count += 1;
         }
         answer = answer.max(current as i32 - prev as i32 + 1);
@@ -19,11 +21,11 @@ pub fn find_max_consecutive_ones(nums: Vec<i32>) -> i32 {
 }
 
 fn main() {
-    // println!("{}", find_max_consecutive_ones(vec![1, 0, 1, 1, 0])); // 4
-    // println!("{}", find_max_consecutive_ones(vec![1, 0, 1, 1, 0, 1])); // 4
-    // println!("{}", find_max_consecutive_ones(vec![1, 1, 1])); // 3
-    // println!("{}", find_max_consecutive_ones(vec![0, 1, 1, 0])); // 3
-    // println!("{}", find_max_consecutive_ones(vec![0, 0, 0])); // 1
+    println!("{}", find_max_consecutive_ones(vec![1, 0, 1, 1, 0])); // 4
+    println!("{}", find_max_consecutive_ones(vec![1, 0, 1, 1, 0, 1])); // 4
+    println!("{}", find_max_consecutive_ones(vec![1, 1, 1])); // 3
+    println!("{}", find_max_consecutive_ones(vec![0, 1, 1, 0])); // 3
+    println!("{}", find_max_consecutive_ones(vec![0, 0, 0])); // 1
     println!(
         "{}",
         find_max_consecutive_ones(vec![1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1])
