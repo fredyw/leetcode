a1ce43551591e2ade5a469c5edefe1e32749fe21
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-18 17:55:52 -0600

    Solve problem 3423

--- src/main/rust/src/bin/problem3423.rs
+++ src/main/rust/src/bin/problem3423.rs
@@ -1,6 +1,14 @@
 // https://leetcode.com/problems/maximum-difference-between-adjacent-elements-in-a-circular-array/description/
 pub fn max_adjacent_distance(nums: Vec<i32>) -> i32 {
-    todo!()
+    let mut answer = 0;
+    for i in 0..nums.len() {
+        if i == nums.len() - 1 {
+            answer = answer.max((nums[i] - nums[0]).abs());
+        } else {
+            answer = answer.max((nums[i] - nums[i + 1]).abs());
+        }
+    }
+    answer
 }
 
 fn main() {
