8a842abdb18279c81fd9ff97ba1d0c2744ccca45
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-17 23:58:02 -0600

    Update problem 616

--- src/main/rust/src/bin/problem616.rs
+++ src/main/rust/src/bin/problem616.rs
@@ -1,6 +1,18 @@
+use std::collections::HashSet;
+
 // https://leetcode.com/problems/add-bold-tag-in-string/
 pub fn add_bold_tag(s: String, words: Vec<String>) -> String {
-    todo!()
+    let mut answer = String::new();
+    let words: HashSet<String> = words.into_iter().collect();
+    for i in 0..s.len() {
+        for j in i + 1..=s.len() {
+            let sub = &s[i..j];
+            if words.contains(sub) {
+                println!("{}", sub);
+            }
+        }
+    }
+    answer
 }
 
 fn main() {
@@ -11,15 +23,26 @@ fn main() {
             vec!["abc".to_string(), "123".to_string()]
         )
     ); // "<b>abc</b>xyz<b>123</b>"
-    println!(
-        "{}",
-        add_bold_tag(
-            "aaabbb".to_string(),
-            vec!["aa".to_string(), "b".to_string()]
-        )
-    ); // "<b>aaabbb</b>"
-    println!(
-        "{}",
-        add_bold_tag("aaaaa".to_string(), vec!["aaaa".to_string()])
-    ); // "<b>aaaaa</b>"
+       // println!(
+       //     "{}",
+       //     add_bold_tag(
+       //         "aaabbb".to_string(),
+       //         vec!["aa".to_string(), "b".to_string()]
+       //     )
+       // ); // "<b>aaabbb</b>"
+       // println!(
+       //     "{}",
+       //     add_bold_tag("aaaaa".to_string(), vec!["aaaa".to_string()])
+       // ); // "<b>aaaaa</b>"
+       // println!(
+       //     "{}",
+       //     add_bold_tag(
+       //         "abcxyz123".to_string(),
+       //         vec![
+       //             "abc".to_string(),
+       //             "123".to_string(),
+       //             "abcxyz123".to_string()
+       //         ]
+       //     )
+       // ); // "<b>abcxyz123</b>"
 }
