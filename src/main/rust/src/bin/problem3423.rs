ddc39cf7300fd23453c54fbe1e7a5728cdc88497
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-18 17:53:20 -0600

    Skeleton for problem 3423

---
+++ src/main/rust/src/bin/problem3423.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/maximum-difference-between-adjacent-elements-in-a-circular-array/description/
+pub fn max_adjacent_distance(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", max_adjacent_distance(vec![1, 2, 4])); // 3
+    println!("{}", max_adjacent_distance(vec![-5, -10, -5])); // 5
+}
