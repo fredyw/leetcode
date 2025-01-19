eee3e2b5cb2a67f4013c4a728a3ed1fa361095cc
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-19 01:08:58 -0600

    Skeleton for problem 3223

---
+++ src/main/rust/src/bin/problem3223.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/minimum-length-of-string-after-operations/description/
+pub fn minimum_length(s: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", minimum_length("abaacbcbb".to_string())); // 5
+    println!("{}", minimum_length("aa".to_string())); // 2
+}
