855d6a902621fe72e63939e8a92164e4962455b7
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-19 11:05:29 -0600

    Update problem 2773

--- src/main/java/leetcode/Problem2773.java
+++ src/main/java/leetcode/Problem2773.java
@@ -1,5 +1,8 @@
 package leetcode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 // https://leetcode.com/problems/height-of-special-binary-tree/description/
 public class Problem2773 {
     public static class TreeNode {
@@ -22,7 +25,30 @@ public class Problem2773 {
     }
 
     public int heightOfTree(TreeNode root) {
-        // TODO
-        return 0;
+        Set<Integer> leaves = new HashSet<>();
+        getLeaves(root, new HashSet<>(), leaves);
+        return heightOfTree(root, leaves);
+    }
+
+    private static int heightOfTree(TreeNode root, Set<Integer> leaves) {
+        if (root == null || leaves.contains(root.val)) {
+            return 0;
+        }
+        int left = heightOfTree(root.left, leaves);
+        int right = heightOfTree(root.right, leaves);
+        return Integer.max(left, right) + 1;
+    }
+
+    private static void getLeaves(TreeNode root, Set<Integer> visited, Set<Integer> leaves) {
+        if (root == null) {
+            return;
+        }
+        if (visited.contains(root.val)) {
+            leaves.add(root.val);
+            return;
+        }
+        visited.add(root.val);
+        getLeaves(root.left, visited, leaves);
+        getLeaves(root.left, visited, leaves);
     }
 }
