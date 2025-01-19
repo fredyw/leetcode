7950de5420c31c6f1ee056337002f5881728d064
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-19 01:07:45 -0600

    Solve problem 3400

--- src/main/rust/src/bin/problem3400.rs
+++ src/main/rust/src/bin/problem3400.rs
@@ -1,6 +1,16 @@
 // https://leetcode.com/problems/maximum-number-of-matching-indices-after-right-shifts/description/
 pub fn maximum_matching_indices(nums1: Vec<i32>, nums2: Vec<i32>) -> i32 {
-    todo!()
+    let mut answer = 0;
+    for i in 0..nums1.len() {
+        let mut count = 0;
+        for j in 0..nums1.len() {
+            if nums1[(i + j) % nums1.len()] == nums2[j] {
+                count += 1;
+            }
+        }
+        answer = answer.max(count);
+    }
+    answer
 }
 
 fn main() {
