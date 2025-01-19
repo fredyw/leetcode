57bed14e8ece4a7790dea67d8030e9f33a589f04
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-19 00:46:44 -0600

    Skeleton for problem 3427

---
+++ src/main/rust/src/bin/problem3427.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/sum-of-variable-length-subarrays/description/
+pub fn subarray_sum(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", subarray_sum(vec![2, 3, 1])); // 11
+    println!("{}", subarray_sum(vec![3, 1, 1, 2])); // 13
+}
