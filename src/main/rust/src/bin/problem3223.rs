7a40c1cdfe5aac2a03d5734eeef2f57ef384919d
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-19 09:47:56 -0600

    Update problem 3233

--- src/main/rust/src/bin/problem3223.rs
+++ src/main/rust/src/bin/problem3223.rs
@@ -1,6 +1,18 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/minimum-length-of-string-after-operations/description/
 pub fn minimum_length(s: String) -> i32 {
-    todo!()
+    let mut map: HashMap<&u8, i32> = HashMap::new();
+    for b in s.as_bytes() {
+        *map.entry(b).or_insert(0) += 1;
+    }
+    let mut answer = s.len() as i32;
+    for (_, count) in map.iter() {
+        if count % 3 == 0 {
+            answer -= count;
+        }
+    }
+    answer
 }
 
 fn main() {
