90d54895f60d3067326b0703393e055184af6f0b
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-19 09:59:03 -0600

    Skeleton for problem 2773

---
+++ src/main/java/leetcode/Problem2773.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+// https://leetcode.com/problems/height-of-special-binary-tree/description/
+public class Problem2773 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int heightOfTree(TreeNode root) {
+        // TODO
+        return 0;
+    }
+}
