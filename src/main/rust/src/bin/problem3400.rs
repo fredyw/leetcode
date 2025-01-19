9c8e2cc1a3c09ed5a5611121e668e8c98614daa4
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-19 00:52:29 -0600

    Skeleton for problem 3400

---
+++ src/main/rust/src/bin/problem3400.rs
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/maximum-number-of-matching-indices-after-right-shifts/description/
+pub fn maximum_matching_indices(nums1: Vec<i32>, nums2: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        maximum_matching_indices(vec![3, 1, 2, 3, 1, 2], vec![1, 2, 3, 1, 2, 3])
+    ); // 6
+    println!(
+        "{}",
+        maximum_matching_indices(vec![1, 4, 2, 5, 3, 1], vec![2, 3, 1, 2, 4, 6])
+    ); // 3
+}
