9af05b72c869b5c10a0169a77c03d6130671e3bf
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-19 11:14:09 -0600

    Skeleton for problem 3391

---
+++ src/main/rust/src/bin/problem3391.rs
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/design-a-3d-binary-matrix-with-efficient-layer-tracking/description/
+struct Matrix3D {}
+
+impl Matrix3D {
+    fn new(n: i32) -> Self {
+        todo!()
+    }
+
+    fn set_cell(&self, x: i32, y: i32, z: i32) {
+        todo!()
+    }
+
+    fn unset_cell(&self, x: i32, y: i32, z: i32) {
+        todo!()
+    }
+
+    fn largest_matrix(&self) -> i32 {
+        todo!()
+    }
+}
+
+fn main() {
+    let matrix = Matrix3D::new(4);
+    matrix.set_cell(2, 1, 1);
+    println!("{}", matrix.largest_matrix()); // 2
+    matrix.unset_cell(2, 1, 1);
+    println!("{}", matrix.largest_matrix()); // 3
+}
