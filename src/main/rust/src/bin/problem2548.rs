a0f655495ddff6b2b45e049631efc9a568665cdf
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-19 22:00:16 -0600

    Skeleton for problem 2548

---
+++ src/main/rust/src/bin/problem2548.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/maximum-price-to-fill-a-bag/description/
+pub fn max_price(items: Vec<Vec<i32>>, capacity: i32) -> f64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", max_price(vec![vec![50, 1], vec![10, 8]], 5)); // 55.00000
+    println!("{}", max_price(vec![vec![100, 30]], 50)); // -1.00000
+}
