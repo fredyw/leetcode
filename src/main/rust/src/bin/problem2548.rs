f169a3cfbc7c711f47c8ea0c4dba4588d6a22898
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-21 20:35:27 -0600

    Update problem 2548

--- src/main/rust/src/bin/problem2548.rs
+++ src/main/rust/src/bin/problem2548.rs
@@ -7,4 +7,5 @@ fn main() {
     println!("{}", max_price(vec![vec![50, 1], vec![10, 8]], 5)); // 55.00000
     println!("{}", max_price(vec![vec![100, 30]], 50)); // -1.00000
     println!("{}", max_price(vec![vec![100, 60]], 50)); // 83.33333
+    println!("{}", max_price(vec![vec![100, 60], vec![200, 100]], 50)); // 100.00000
 }
