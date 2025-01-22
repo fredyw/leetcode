9b23d3ed0bbd3f360b7bfec5a28e086118a28fe9
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-22 14:53:45 -0600

    Solve problem 2852

--- src/main/rust/src/bin/problem2852.rs
+++ src/main/rust/src/bin/problem2852.rs
@@ -1,6 +1,16 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/sum-of-remoteness-of-all-cells/description/
 pub fn sum_remoteness(grid: Vec<Vec<i32>>) -> i64 {
-    fn traverse(grid: &Vec<Vec<i32>>, row: i32, col: i32, visited: &mut Vec<Vec<bool>>) {
+    fn traverse(
+        grid: &Vec<Vec<i32>>,
+        row: i32,
+        col: i32,
+        id: i32,
+        visited: &mut Vec<Vec<bool>>,
+        connected_components: &mut Vec<Vec<i32>>,
+        sum: &mut i64,
+    ) {
         let n = grid.len() as i32;
         if row < 0
             || row == n
@@ -12,22 +22,56 @@ pub fn sum_remoteness(grid: Vec<Vec<i32>>) -> i64 {
             return;
         }
         visited[row as usize][col as usize] = true;
+        connected_components[row as usize][col as usize] = id;
+        *sum += grid[row as usize][col as usize] as i64;
         for (r, c) in [(-1, 0), (0, 1), (1, 0), (0, -1)] {
-            traverse(grid, row + r, col + c, visited);
+            traverse(
+                grid,
+                row + r,
+                col + c,
+                id,
+                visited,
+                connected_components,
+                sum,
+            );
         }
     }
 
     let n = grid.len();
     let mut visited = vec![vec![false; n]; n];
-    let mut connected_components: Vec<Vec<i32>> = vec![vec![]; n];
+    let mut connected_components: Vec<Vec<i32>> = vec![vec![-1; n]; n];
+    let mut sums: HashMap<i32, i64> = HashMap::new();
+    let mut total_sum = 0;
+    let mut id = 0;
     for row in 0..n {
         for col in 0..n {
             if grid[row][col] == -1 || visited[row][col] {
                 continue;
             }
+            let mut sum = 0;
+            traverse(
+                &grid,
+                row as i32,
+                col as i32,
+                id,
+                &mut visited,
+                &mut connected_components,
+                &mut sum,
+            );
+            total_sum += sum;
+            sums.insert(id, sum);
+            id += 1;
         }
     }
-    todo!()
+    let mut answer = 0;
+    for connected in connected_components.iter() {
+        for id in connected.iter() {
+            if *id >= 0 {
+                answer += total_sum - sums[id];
+            }
+        }
+    }
+    answer
 }
 
 fn main() {
