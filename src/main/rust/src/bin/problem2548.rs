66496c6e6e511806e1009360a0a32737c79b5d32
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-21 20:32:44 -0600

    Update problem 2548

--- src/main/rust/src/bin/problem2548.rs
+++ src/main/rust/src/bin/problem2548.rs
@@ -6,4 +6,5 @@ pub fn max_price(items: Vec<Vec<i32>>, capacity: i32) -> f64 {
 fn main() {
     println!("{}", max_price(vec![vec![50, 1], vec![10, 8]], 5)); // 55.00000
     println!("{}", max_price(vec![vec![100, 30]], 50)); // -1.00000
+    println!("{}", max_price(vec![vec![100, 60]], 50)); // 83.33333
 }
