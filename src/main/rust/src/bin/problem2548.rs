e1812a5116025a0d00067773d5c1e0cf24395f74
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-22 11:34:46 -0600

    Solve problem 2548

--- src/main/rust/src/bin/problem2548.rs
+++ src/main/rust/src/bin/problem2548.rs
@@ -1,6 +1,37 @@
+use std::cmp::Ordering;
+
 // https://leetcode.com/problems/maximum-price-to-fill-a-bag/description/
-pub fn max_price(items: Vec<Vec<i32>>, capacity: i32) -> f64 {
-    todo!()
+pub fn max_price(mut items: Vec<Vec<i32>>, mut capacity: i32) -> f64 {
+    items.sort_by(|a, b| {
+        let price_per_item1 = a[0] as f64 / a[1] as f64;
+        let price_per_item2 = b[0] as f64 / b[1] as f64;
+        if price_per_item1 < price_per_item2 {
+            Ordering::Greater
+        } else if price_per_item1 == price_per_item2 {
+            Ordering::Equal
+        } else {
+            Ordering::Less
+        }
+    });
+    let mut answer = 0.0;
+    let mut capacity = capacity as f64;
+    for item in items {
+        let price = item[0] as f64;
+        let weight = item[1] as f64;
+        let price_per_item = price / weight;
+        if weight <= capacity {
+            answer += price_per_item * weight;
+            capacity -= weight;
+        } else {
+            answer += price_per_item * capacity;
+            capacity = 0.0;
+        }
+    }
+    if capacity > 0.0 {
+        -1.0
+    } else {
+        answer
+    }
 }
 
 fn main() {
@@ -8,4 +39,5 @@ fn main() {
     println!("{}", max_price(vec![vec![100, 30]], 50)); // -1.00000
     println!("{}", max_price(vec![vec![100, 60]], 50)); // 83.33333
     println!("{}", max_price(vec![vec![100, 60], vec![200, 100]], 50)); // 100.00000
+    println!("{}", max_price(vec![vec![500, 1000], vec![200, 100]], 50)); // 100.00000
 }
