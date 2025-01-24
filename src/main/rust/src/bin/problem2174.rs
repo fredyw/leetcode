519d23242797e559a844573f78e9156b915c2227
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-24 16:33:35 -0600

    Update problem 2174

--- src/main/rust/src/bin/problem2174.rs
+++ src/main/rust/src/bin/problem2174.rs
@@ -1,6 +1,18 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/remove-all-ones-with-row-and-column-flips-ii/description/
 pub fn remove_ones(grid: Vec<Vec<i32>>) -> i32 {
-    todo!()
+    let num_rows = grid.len();
+    let num_cols = if num_rows > 0 { grid[0].len() } else { 0 };
+    let mut rows: HashMap<usize, Vec<(usize, usize)>> = HashMap::new();
+    let mut cols: HashMap<usize, Vec<(usize, usize)>> = HashMap::new();
+    for row in 0..num_rows {
+        for col in 0..num_cols {
+            rows.entry(row).or_insert(vec![]).push((row, col));
+            cols.entry(col).or_insert(vec![]).push((row, col));
+        }
+    }
+    0
 }
 
 fn main() {
