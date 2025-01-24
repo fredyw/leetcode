8cb7ea6cc09fd95ac0e69b47b6b5d9813832fecd
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-24 17:02:16 -0600

    Solve problem 2174

--- src/main/rust/src/bin/problem2174.rs
+++ src/main/rust/src/bin/problem2174.rs
@@ -1,38 +1,42 @@
-use std::collections::HashSet;
-
 // https://leetcode.com/problems/remove-all-ones-with-row-and-column-flips-ii/description/
 pub fn remove_ones(grid: Vec<Vec<i32>>) -> i32 {
     fn remove_ones(
         grid: &Vec<Vec<i32>>,
-        changed_rows: &mut HashSet<usize>,
-        changed_cols: &mut HashSet<usize>,
+        changed_rows: &mut Vec<bool>,
+        changed_cols: &mut Vec<bool>,
     ) -> i32 {
         let num_rows = grid.len();
         let num_cols = if num_rows > 0 { grid[0].len() } else { 0 };
-        if changed_rows.len() == num_rows && changed_cols.len() == num_cols {
-            return 0;
-        }
         let mut min = i32::MAX;
         for row in 0..num_rows {
             for col in 0..num_cols {
                 if grid[row][col] == 0 {
                     continue;
                 }
-                if grid[row][col] == 1 && changed_rows.contains(&row) && changed_cols.contains(&col)
-                {
+                if grid[row][col] == 1 && (changed_rows[row] || changed_cols[col]) {
                     continue;
                 }
-                changed_rows.insert(row);
-                changed_cols.insert(col);
+                changed_rows[row] = true;
+                changed_cols[col] = true;
                 min = min.min(remove_ones(grid, changed_rows, changed_cols) + 1);
-                changed_rows.remove(&row);
-                changed_cols.remove(&col);
+                changed_rows[row] = false;
+                changed_cols[col] = false;
             }
         }
-        min
+        if min == i32::MAX {
+            0
+        } else {
+            min
+        }
     }
 
-    remove_ones(&grid, &mut HashSet::new(), &mut HashSet::new())
+    let num_rows = grid.len();
+    let num_cols = if num_rows > 0 { grid[0].len() } else { 0 };
+    remove_ones(
+        &grid,
+        &mut vec![false; num_rows],
+        &mut vec![false; num_cols],
+    )
 }
 
 fn main() {
@@ -40,9 +44,9 @@ fn main() {
         "{}",
         remove_ones(vec![vec![1, 1, 1], vec![1, 1, 1], vec![0, 1, 0]])
     ); // 2
-       // println!(
-       //     "{}",
-       //     remove_ones(vec![vec![0, 1, 0], vec![1, 0, 1], vec![0, 1, 0]])
-       // ); // 2
-       // println!("{}", remove_ones(vec![vec![0, 0], vec![0, 0]])); // 0
+    println!(
+        "{}",
+        remove_ones(vec![vec![0, 1, 0], vec![1, 0, 1], vec![0, 1, 0]])
+    ); // 2
+    println!("{}", remove_ones(vec![vec![0, 0], vec![0, 0]])); // 0
 }
