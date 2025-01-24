68d70b276bfd71e95fad52d670bf671baeabe36a
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-24 16:48:36 -0600

    Update problem 2174

--- src/main/rust/src/bin/problem2174.rs
+++ src/main/rust/src/bin/problem2174.rs
@@ -1,18 +1,38 @@
-use std::collections::HashMap;
+use std::collections::HashSet;
 
 // https://leetcode.com/problems/remove-all-ones-with-row-and-column-flips-ii/description/
 pub fn remove_ones(grid: Vec<Vec<i32>>) -> i32 {
-    let num_rows = grid.len();
-    let num_cols = if num_rows > 0 { grid[0].len() } else { 0 };
-    let mut rows: HashMap<usize, Vec<(usize, usize)>> = HashMap::new();
-    let mut cols: HashMap<usize, Vec<(usize, usize)>> = HashMap::new();
-    for row in 0..num_rows {
-        for col in 0..num_cols {
-            rows.entry(row).or_insert(vec![]).push((row, col));
-            cols.entry(col).or_insert(vec![]).push((row, col));
+    fn remove_ones(
+        grid: &Vec<Vec<i32>>,
+        changed_rows: &mut HashSet<usize>,
+        changed_cols: &mut HashSet<usize>,
+    ) -> i32 {
+        let num_rows = grid.len();
+        let num_cols = if num_rows > 0 { grid[0].len() } else { 0 };
+        if changed_rows.len() == num_rows && changed_cols.len() == num_cols {
+            return 0;
         }
+        let mut min = i32::MAX;
+        for row in 0..num_rows {
+            for col in 0..num_cols {
+                if grid[row][col] == 0 {
+                    continue;
+                }
+                if grid[row][col] == 1 && changed_rows.contains(&row) && changed_cols.contains(&col)
+                {
+                    continue;
+                }
+                changed_rows.insert(row);
+                changed_cols.insert(col);
+                min = min.min(remove_ones(grid, changed_rows, changed_cols) + 1);
+                changed_rows.remove(&row);
+                changed_cols.remove(&col);
+            }
+        }
+        min
     }
-    0
+
+    remove_ones(&grid, &mut HashSet::new(), &mut HashSet::new())
 }
 
 fn main() {
@@ -20,9 +40,9 @@ fn main() {
         "{}",
         remove_ones(vec![vec![1, 1, 1], vec![1, 1, 1], vec![0, 1, 0]])
     ); // 2
-    println!(
-        "{}",
-        remove_ones(vec![vec![0, 1, 0], vec![1, 0, 1], vec![0, 1, 0]])
-    ); // 2
-    println!("{}", remove_ones(vec![vec![0, 0], vec![0, 0]])); // 0
+       // println!(
+       //     "{}",
+       //     remove_ones(vec![vec![0, 1, 0], vec![1, 0, 1], vec![0, 1, 0]])
+       // ); // 2
+       // println!("{}", remove_ones(vec![vec![0, 0], vec![0, 0]])); // 0
 }
