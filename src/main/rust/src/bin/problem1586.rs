b9299b49f23719acbb74934bbdaf115b9b983d6b
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-25 09:24:12 -0600

    Update problem 1586

--- src/main/rust/src/bin/problem1586.rs
+++ src/main/rust/src/bin/problem1586.rs
@@ -1,4 +1,5 @@
 use std::cell::RefCell;
+use std::collections::VecDeque;
 use std::rc::Rc;
 
 // https://leetcode.com/problems/binary-search-tree-iterator-ii/description/
@@ -44,4 +45,70 @@ impl BSTIterator {
     }
 }
 
-fn main() {}
+fn create_node(value: i32) -> Option<Rc<RefCell<TreeNode>>> {
+    Some(Rc::new(RefCell::new(TreeNode::new(value))))
+}
+
+fn add_left(parent: &Option<Rc<RefCell<TreeNode>>>, value: i32) -> Option<Rc<RefCell<TreeNode>>> {
+    parent.as_ref().unwrap().borrow_mut().left = create_node(value);
+    parent.as_ref().unwrap().borrow().left.clone()
+}
+
+fn add_right(parent: &Option<Rc<RefCell<TreeNode>>>, value: i32) -> Option<Rc<RefCell<TreeNode>>> {
+    parent.as_ref().unwrap().borrow_mut().right = create_node(value);
+    parent.as_ref().unwrap().borrow().right.clone()
+}
+
+fn to_tree(nodes: Vec<Option<i32>>) -> Option<Rc<RefCell<TreeNode>>> {
+    if nodes.len() == 0 {
+        return None;
+    }
+    let mut deque: VecDeque<Option<Rc<RefCell<TreeNode>>>> = VecDeque::new();
+    let root = create_node(nodes[0].unwrap());
+    deque.push_back(root.clone());
+    let mut i = 1;
+    while i < nodes.len() {
+        let parent = deque.pop_front().unwrap();
+        let mut has_left = false;
+        if let Some(left) = nodes[i] {
+            deque.push_back(add_left(&parent, left));
+            has_left = true;
+        }
+        let mut has_right = false;
+        if i + 1 < nodes.len() {
+            if let Some(right) = nodes[i + 1] {
+                deque.push_back(add_right(&parent, right));
+                has_right = true;
+            }
+        }
+        if !has_left && !has_right {
+            deque.push_back(parent);
+        }
+        i += 2;
+    }
+    root
+}
+
+fn main() {
+    let iter = BSTIterator::new(to_tree(vec![
+        Some(7),
+        Some(3),
+        Some(15),
+        None,
+        None,
+        Some(9),
+        Some(20),
+    ]));
+    println!("{}", iter.next()); // 3
+    println!("{}", iter.next()); // 7
+    println!("{}", iter.prev()); // 3
+    println!("{}", iter.next()); // 7
+    println!("{}", iter.has_next()); // true
+    println!("{}", iter.next()); // 9
+    println!("{}", iter.next()); // 15
+    println!("{}", iter.next()); // 20
+    println!("{}", iter.has_next()); // false
+    println!("{}", iter.has_prev()); // true
+    println!("{}", iter.prev()); // 15
+    println!("{}", iter.prev()); // 9
+}
