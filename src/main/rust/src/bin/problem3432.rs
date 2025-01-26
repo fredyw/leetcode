cb9f0d8c2aa45d15dd89b5c998446ce21c7b7724
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-26 09:57:28 -0600

    Skeleton for problem 3432

---
+++ src/main/rust/src/bin/problem3432.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/count-partitions-with-even-sum-difference/description/
+pub fn count_partitions(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", count_partitions(vec![10, 10, 3, 7, 6])); // 4
+    println!("{}", count_partitions(vec![1, 2, 2])); // 0
+    println!("{}", count_partitions(vec![2, 4, 6, 8])); // 3
+}
