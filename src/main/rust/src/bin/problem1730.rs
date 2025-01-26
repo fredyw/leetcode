bf48fb9deaea27fbc59527d8f978e0e106c7696d
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-26 10:23:11 -0600

    Solve problem 1730

--- src/main/rust/src/bin/problem1730.rs
+++ src/main/rust/src/bin/problem1730.rs
@@ -13,6 +13,7 @@ pub fn get_food(grid: Vec<Vec<char>>) -> i32 {
         (-1, -1)
     }
 
+    let mut answer = i32::MAX;
     let num_rows = grid.len();
     let num_cols = if num_rows > 0 { grid[0].len() } else { 0 };
     let (row, col) = get_person_index(&grid);
@@ -21,7 +22,6 @@ pub fn get_food(grid: Vec<Vec<char>>) -> i32 {
     let mut visited = vec![vec![false; num_cols]; num_rows];
     let mut length = 0;
     while !deque.is_empty() {
-        length += 1;
         let n = deque.len();
         for _ in 0..n {
             let (row, col) = deque.pop_front().unwrap();
@@ -29,20 +29,26 @@ pub fn get_food(grid: Vec<Vec<char>>) -> i32 {
                 || row == num_rows as i32
                 || col < 0
                 || col == num_cols as i32
+                || grid[row as usize][col as usize] == 'X'
                 || visited[row as usize][col as usize]
             {
                 continue;
             }
             visited[row as usize][col as usize] = true;
             if grid[row as usize][col as usize] == '#' {
-                return length;
+                answer = answer.min(length);
             }
             for (r, c) in [(-1, 0), (0, 1), (1, 0), (0, -1)] {
                 deque.push_back((row + r, col + c));
             }
         }
+        length += 1;
     }
-    -1
+    if answer == i32::MAX {
+        -1
+    } else {
+        answer
+    }
 }
 
 fn main() {
