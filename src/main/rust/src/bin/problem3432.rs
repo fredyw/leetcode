0ea801fc2f72b54296f2854b0926c493107425ed
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-26 10:02:30 -0600

    Solve problem 3432

--- src/main/rust/src/bin/problem3432.rs
+++ src/main/rust/src/bin/problem3432.rs
@@ -1,6 +1,16 @@
 // https://leetcode.com/problems/count-partitions-with-even-sum-difference/description/
 pub fn count_partitions(nums: Vec<i32>) -> i32 {
-    todo!()
+    let mut answer = 0;
+    let total_sum: i32 = nums.iter().sum();
+    let mut left_sum = 0;
+    for i in 0..nums.len() - 1 {
+        left_sum += nums[i];
+        let right_sum = total_sum - left_sum;
+        if (left_sum - right_sum) % 2 == 0 {
+            answer += 1;
+        }
+    }
+    answer
 }
 
 fn main() {
