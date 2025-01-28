ff6c79257139c89aa88852184d74a5477d4a0a74
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-28 14:42:21 -0600

    Update problem 3431

--- src/main/rust/src/bin/problem3431.rs
+++ src/main/rust/src/bin/problem3431.rs
@@ -1,5 +1,14 @@
+use std::mem;
+
 // https://leetcode.com/problems/minimum-unlocked-indices-to-sort-nums/description/
-pub fn min_unlocked_indices(nums: Vec<i32>, locked: Vec<i32>) -> i32 {
+pub fn min_unlocked_indices(mut nums: Vec<i32>, locked: Vec<i32>) -> i32 {
+    for i in 0..nums.len() - 1 {
+        if nums[i] > nums[i + 1] {
+            if locked[i] == 0 && nums[i] - nums[i + 1] == 1 {
+                mem::swap(nums, i, i + 1);
+            }
+        }
+    }
     todo!()
 }
 
@@ -16,4 +25,6 @@ fn main() {
         "{}",
         min_unlocked_indices(vec![1, 2, 1, 2, 3, 2, 1], vec![0, 0, 0, 0, 0, 0, 0])
     ); // -1
+    println!("{}", min_unlocked_indices(vec![3, 2, 1], vec![0, 0, 0])); // -1
+    println!("{}", min_unlocked_indices(vec![3, 2, 1], vec![1, 1, 1])); // -1
 }
