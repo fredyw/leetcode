be41a367cc748655e1de472a288036cc22cea338
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-28 14:57:53 -0600

    Update problem 3431

--- src/main/rust/src/bin/problem3431.rs
+++ src/main/rust/src/bin/problem3431.rs
@@ -18,18 +18,19 @@ pub fn min_unlocked_indices(mut nums: Vec<i32>, mut locked: Vec<i32>) -> i32 {
 }
 
 fn main() {
-    println!(
-        "{}",
-        min_unlocked_indices(vec![1, 2, 1, 2, 3, 2], vec![1, 0, 1, 1, 0, 1])
-    ); // 0
-    println!(
-        "{}",
-        min_unlocked_indices(vec![1, 2, 1, 1, 3, 2, 2], vec![1, 0, 1, 1, 0, 1, 0])
-    ); // 2
-    println!(
-        "{}",
-        min_unlocked_indices(vec![1, 2, 1, 2, 3, 2, 1], vec![0, 0, 0, 0, 0, 0, 0])
-    ); // -1
-    println!("{}", min_unlocked_indices(vec![3, 2, 1], vec![0, 0, 0])); // -1
-    println!("{}", min_unlocked_indices(vec![3, 2, 1], vec![1, 1, 1])); // -1
+    // println!(
+    //     "{}",
+    //     min_unlocked_indices(vec![1, 2, 1, 2, 3, 2], vec![1, 0, 1, 1, 0, 1])
+    // ); // 0
+    // println!(
+    //     "{}",
+    //     min_unlocked_indices(vec![1, 2, 1, 1, 3, 2, 2], vec![1, 0, 1, 1, 0, 1, 0])
+    // ); // 2
+    // println!(
+    //     "{}",
+    //     min_unlocked_indices(vec![1, 2, 1, 2, 3, 2, 1], vec![0, 0, 0, 0, 0, 0, 0])
+    // ); // -1
+    // println!("{}", min_unlocked_indices(vec![3, 2, 1], vec![0, 0, 0])); // -1
+    // println!("{}", min_unlocked_indices(vec![3, 2, 1], vec![1, 1, 1])); // -1
+    println!("{}", min_unlocked_indices(vec![2, 2, 1], vec![1, 0, 0])); // 1
 }
