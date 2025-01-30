728533a179d13627f65a45f7c346672b7d9b96af
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-30 13:11:49 -0600

    Solve problem 2168

--- src/main/rust/src/bin/problem2168.rs
+++ src/main/rust/src/bin/problem2168.rs
@@ -1,6 +1,32 @@
+use std::collections::HashSet;
+
 // https://leetcode.com/problems/unique-substrings-with-equal-digit-frequency/description/
 pub fn equal_digit_frequency(s: String) -> i32 {
-    todo!()
+    let mut set: HashSet<&str> = HashSet::new();
+    for i in 0..s.len() {
+        'outer: for j in i + 1..=s.len() {
+            let sub = &s[i..j];
+            if set.contains(sub) {
+                continue;
+            }
+            let mut digits = vec![0; 10];
+            for b in sub.as_bytes() {
+                digits[*b as usize - '0' as usize] += 1;
+            }
+            let mut count = 0;
+            for i in 0..10 {
+                if digits[i] != 0 {
+                    if count == 0 {
+                        count = digits[i];
+                    } else if count != digits[i] {
+                        continue 'outer;
+                    }
+                }
+            }
+            set.insert(sub);
+        }
+    }
+    set.len() as i32
 }
 
 fn main() {
