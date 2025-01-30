df93a0d51fca052f2bd3c8fa29108af805dfc994
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-30 11:16:38 -0600

    Skeleton for problem 2219

---
+++ src/main/rust/src/bin/problem2219.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/maximum-sum-score-of-array/description/
+pub fn maximum_sum_score(nums: Vec<i32>) -> i64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", maximum_sum_score(vec![4, 3, -2, 5])); // 10
+    println!("{}", maximum_sum_score(vec![-3, -5])); // -3
+}
