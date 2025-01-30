dd7294f1104d90903f9d476ed63182e0b72a6e85
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-01-30 12:15:49 -0600

    Skeleton for problem 2168

---
+++ src/main/rust/src/bin/problem2168.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/unique-substrings-with-equal-digit-frequency/description/
+pub fn equal_digit_frequency(s: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", equal_digit_frequency("1212".to_string())); // 5
+    println!("{}", equal_digit_frequency("12321".to_string())); // 9
+}
