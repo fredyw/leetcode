28748c157a5c3fbb04cead5762ad05ebbf2276b4
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-02-01 15:21:15 -0600

    Update problem 2168

--- src/main/rust/src/bin/problem2168.rs
+++ src/main/rust/src/bin/problem2168.rs
@@ -4,26 +4,25 @@ use std::collections::HashSet;
 pub fn equal_digit_frequency(s: String) -> i32 {
     let mut set: HashSet<&str> = HashSet::new();
     for i in 0..s.len() {
-        'outer: for j in i + 1..=s.len() {
+        let mut counts: Vec<i32> = vec![0; 10];
+        let mut num_unique = 0;
+        let mut max_count = 0;
+        let mut total_count = 0;
+        for j in i + 1..=s.len() {
             let sub = &s[i..j];
+            let digit = *(&s[j - 1..j].as_bytes()[0]) as usize - '0' as usize;
+            if counts[digit] == 0 {
+                num_unique += 1;
+            }
+            counts[digit] += 1;
+            max_count = max_count.max(counts[digit]);
+            total_count += 1;
             if set.contains(sub) {
                 continue;
             }
-            let mut digits = vec![0; 10];
-            for b in sub.as_bytes() {
-                digits[*b as usize - '0' as usize] += 1;
+            if num_unique * max_count == total_count {
+                set.insert(sub);
             }
-            let mut count = 0;
-            for i in 0..10 {
-                if digits[i] != 0 {
-                    if count == 0 {
-                        count = digits[i];
-                    } else if count != digits[i] {
-                        continue 'outer;
-                    }
-                }
-            }
-            set.insert(sub);
         }
     }
     set.len() as i32
