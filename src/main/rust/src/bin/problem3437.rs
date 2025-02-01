6fb4810908b637fdf1fefd7b9395fddcb6a3015f
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-02-01 15:43:54 -0600

    Skeleton for problem 3437

---
+++ src/main/rust/src/bin/problem3437.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/permutations-iii/
+pub fn permute(n: i32) -> Vec<Vec<i32>> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", permute(4)); // [[1,2,3,4],[1,4,3,2],[2,1,4,3],[2,3,4,1],[3,2,1,4],[3,4,1,2],[4,1,2,3],[4,3,2,1]]
+    println!("{:?}", permute(2)); // [[1,2],[2,1]]
+    println!("{:?}", permute(3)); // [[1,2,3],[3,2,1]]
+}
