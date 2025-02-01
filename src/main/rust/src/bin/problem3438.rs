f00a7fea546493c00af7f6f3fe92600a71811841
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-02-01 15:39:26 -0600

    Solve problem 3438

--- src/main/rust/src/bin/problem3438.rs
+++ src/main/rust/src/bin/problem3438.rs
@@ -1,6 +1,20 @@
 // https://leetcode.com/problems/find-valid-pair-of-adjacent-digits-in-string/description/
 pub fn find_valid_pair(s: String) -> String {
-    todo!()
+    let mut counts: Vec<i32> = vec![0; 10];
+    let bytes = s.as_bytes();
+    for i in 0..bytes.len() {
+        counts[bytes[i] as usize - '0' as usize] += 1;
+    }
+    for i in 0..bytes.len() - 1 {
+        if bytes[i] != bytes[i + 1] {
+            if counts[bytes[i] as usize - '0' as usize] == (bytes[i] - '0' as u8) as i32
+                && counts[bytes[i + 1] as usize - '0' as usize] == (bytes[i + 1] - '0' as u8) as i32
+            {
+                return s[i..i + 2].to_string();
+            }
+        }
+    }
+    "".to_string()
 }
 
 fn main() {
