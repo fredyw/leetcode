d7de0b444bfe2c63d8fed9ddf5d4b7ea19eeeeb2
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-02-01 15:24:44 -0600

    Skeleton for problem 3438

---
+++ src/main/rust/src/bin/problem3438.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/find-valid-pair-of-adjacent-digits-in-string/description/
+pub fn find_valid_pair(s: String) -> String {
+    todo!()
+}
+
+fn main() {
+    println!("{}", find_valid_pair("2523533".to_string())); // "23"
+    println!("{}", find_valid_pair("221".to_string())); // "21"
+    println!("{}", find_valid_pair("22".to_string())); // ""
+}
