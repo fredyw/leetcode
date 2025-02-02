ca6036e2e795e17d3999cc07a9149b7c3589b346
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-02-02 11:11:41 -0600

    Solve problem 3437

--- src/main/rust/src/bin/problem3437.rs
+++ src/main/rust/src/bin/problem3437.rs
@@ -1,21 +1,32 @@
 // https://leetcode.com/problems/permutations-iii/
 pub fn permute(n: i32) -> Vec<Vec<i32>> {
-    fn permute(nums: &mut Vec<i32>, n: usize, i: usize, visited: &mut Vec<bool>) {
-        if i == nums.len() {
-            return;
-        }
+    fn permute(
+        nums: &mut Vec<i32>,
+        n: usize,
+        visited: &mut Vec<bool>,
+        accu: &mut Vec<i32>,
+        permutations: &mut Vec<Vec<i32>>,
+    ) {
         if n == 0 {
-            println!("{:?}", nums);
+            permutations.push(accu.clone());
             return;
         }
         for j in 0..nums.len() {
             if visited[j] {
                 continue;
             }
+            if let Some(last) = accu.last() {
+                if last % 2 == 0 && nums[j] % 2 == 0 {
+                    continue;
+                }
+                if last % 2 != 0 && nums[j] % 2 != 0 {
+                    continue;
+                }
+            }
             visited[j] = true;
-            nums.swap(i, j);
-            permute(nums, n - 1, j + 1, visited);
-            nums.swap(i, j);
+            accu.push(nums[j]);
+            permute(nums, n - 1, visited, accu, permutations);
+            accu.pop();
             visited[j] = false;
         }
     }
@@ -26,12 +37,12 @@ pub fn permute(n: i32) -> Vec<Vec<i32>> {
     }
     let mut answer = vec![];
     let n = nums.len();
-    permute(&mut nums, n, 0, &mut vec![false; n]);
+    permute(&mut nums, n, &mut vec![false; n], &mut vec![], &mut answer);
     answer
 }
 
 fn main() {
-    // println!("{:?}", permute(4)); // [[1,2,3,4],[1,4,3,2],[2,1,4,3],[2,3,4,1],[3,2,1,4],[3,4,1,2],[4,1,2,3],[4,3,2,1]]
-    // println!("{:?}", permute(2)); // [[1,2],[2,1]]
+    println!("{:?}", permute(4)); // [[1,2,3,4],[1,4,3,2],[2,1,4,3],[2,3,4,1],[3,2,1,4],[3,4,1,2],[4,1,2,3],[4,3,2,1]]
+    println!("{:?}", permute(2)); // [[1,2],[2,1]]
     println!("{:?}", permute(3)); // [[1,2,3],[3,2,1]]
 }
