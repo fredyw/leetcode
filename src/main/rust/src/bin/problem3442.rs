78f6887c83552adaad48075dbfabd308f754dce8
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-02-02 01:58:06 -0600

    Skeleton for problem 3442

---
+++ src/main/rust/src/bin/problem3442.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/maximum-difference-between-even-and-odd-frequency-i/description/
+pub fn max_difference(s: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", max_difference("aaaaabbc".to_string())); // 3
+    println!("{}", max_difference("abcabcab".to_string())); // 1
+}
