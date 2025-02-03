f21e6dedb9e43009607f0cf5fbda91f4cf2fc9e5
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-02-03 12:20:08 -0600

    Skeleton for problem 2323

---
+++ src/main/rust/src/bin/problem2323.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/find-minimum-time-to-finish-all-jobs-ii/description/
+pub fn minimum_time(jobs: Vec<i32>, workers: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", minimum_time(vec![5, 2, 4], vec![1, 7, 5])); // 2
+    println!("{}", minimum_time(vec![3, 18, 15, 9], vec![6, 5, 1, 3])); // 3
+}
