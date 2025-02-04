c3eb08b25dd6d57399d9da213054ac2ff4057db8
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-02-04 13:50:04 -0600

    Solve problem 2323

--- src/main/rust/src/bin/problem2323.rs
+++ src/main/rust/src/bin/problem2323.rs
@@ -1,6 +1,13 @@
 // https://leetcode.com/problems/find-minimum-time-to-finish-all-jobs-ii/description/
-pub fn minimum_time(jobs: Vec<i32>, workers: Vec<i32>) -> i32 {
-    todo!()
+pub fn minimum_time(mut jobs: Vec<i32>, mut workers: Vec<i32>) -> i32 {
+    jobs.sort();
+    workers.sort();
+    let mut answer = 0;
+    for i in 0..jobs.len() {
+        let num_days = (jobs[i] / workers[i]) + if jobs[i] % workers[i] == 0 { 0 } else { 1 };
+        answer = answer.max(num_days);
+    }
+    answer
 }
 
 fn main() {
