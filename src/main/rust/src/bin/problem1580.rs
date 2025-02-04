0d0ce96f63efad7a7a4e5df9552695498354eb2e
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-02-04 17:26:32 -0600

    Skeleton for problem 1580

---
+++ src/main/rust/src/bin/problem1580.rs
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/put-boxes-into-the-warehouse-ii/
+pub fn max_boxes_in_warehouse(boxes: Vec<i32>, warehouse: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        max_boxes_in_warehouse(vec![1, 2, 2, 3, 4], vec![3, 4, 1, 2])
+    ); // 4
+    println!(
+        "{}",
+        max_boxes_in_warehouse(vec![3, 5, 5, 2], vec![2, 1, 3, 4, 5])
+    ); // 3
+}
