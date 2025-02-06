36b6f31cff978d3eb49ee7367da176434fa757c3
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-02-06 12:31:15 -0600

    Update problem 1580

--- src/main/rust/src/bin/problem1580.rs
+++ src/main/rust/src/bin/problem1580.rs
@@ -12,4 +12,6 @@ fn main() {
         "{}",
         max_boxes_in_warehouse(vec![3, 5, 5, 2], vec![2, 1, 3, 4, 5])
     ); // 3
+    println!("{}", max_boxes_in_warehouse(vec![1, 2, 3], vec![3, 2, 4])); // 3
+    println!("{}", max_boxes_in_warehouse(vec![1, 2, 3], vec![3, 4, 2])); // 3
 }
