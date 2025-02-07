dac70f34860b4d121f4434ae45fcb86b4ee8fac8
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-02-07 01:50:40 -0600

    Update problem 1580

--- src/main/rust/src/bin/problem1580.rs
+++ src/main/rust/src/bin/problem1580.rs
@@ -9,11 +9,18 @@ pub fn max_boxes_in_warehouse(mut boxes: Vec<i32>, warehouse: Vec<i32>) -> i32 {
             warehouse_min_index = i;
         }
     }
-    let mut answer = 0;
     let mut box_index = 0;
+    let mut answer = if warehouse_min_value >= boxes[0] {
+        box_index += 1;
+        1
+    } else {
+        0
+    };
     let mut warehouse_left_index = warehouse_min_index as i32 - 1;
     let mut warehouse_right_index = warehouse_min_index as i32 + 1;
-    while warehouse_left_index >= 0 || warehouse_right_index < warehouse.len() as i32 {
+    while (warehouse_left_index >= 0 || warehouse_right_index < warehouse.len() as i32)
+        && box_index < boxes.len()
+    {
         let mut can_go_left = false;
         let mut can_go_right = false;
         if warehouse_left_index < 0 {
@@ -29,18 +36,18 @@ pub fn max_boxes_in_warehouse(mut boxes: Vec<i32>, warehouse: Vec<i32>) -> i32 {
             }
         }
         if can_go_left {
-            if warehouse[warehouse_left_index as usize] < boxes[box_index] {
-                break;
+            if warehouse[warehouse_left_index as usize] >= boxes[box_index] {
+                answer += 1;
+                box_index += 1;
             }
             warehouse_left_index -= 1;
         } else if can_go_right {
-            if warehouse[warehouse_right_index as usize] < boxes[box_index] {
-                break;
+            if warehouse[warehouse_right_index as usize] >= boxes[box_index] {
+                answer += 1;
+                box_index += 1;
             }
             warehouse_right_index += 1;
         }
-        box_index += 1;
-        answer += 1;
     }
     answer
 }
@@ -57,8 +64,12 @@ fn main() {
     // println!("{}", max_boxes_in_warehouse(vec![1, 2, 3], vec![3, 2, 4])); // 3
     // println!("{}", max_boxes_in_warehouse(vec![1, 2, 3], vec![3, 4, 2])); // 3
     // println!("{}", max_boxes_in_warehouse(vec![2, 2, 2], vec![1, 1, 1])); // 0
+    // println!(
+    //     "{}",
+    //     max_boxes_in_warehouse(vec![1, 2, 3], vec![1, 2, 3, 4])
+    // ); // 3
     println!(
         "{}",
-        max_boxes_in_warehouse(vec![1, 2, 3], vec![1, 2, 3, 4])
-    ); // 3
+        max_boxes_in_warehouse(vec![4, 5, 6, 2], vec![3, 2, 6, 3, 3, 7])
+    ); // 2
 }
