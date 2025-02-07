1fd14ddb2fa22f9713cf14efbaf3e553ff0132d0
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-02-07 11:18:02 -0600

    Skeleton for problem 2450

---
+++ src/main/rust/src/bin/problem2450.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/number-of-distinct-binary-strings-after-applying-operations/description/
+pub fn count_distinct_strings(s: String, k: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", count_distinct_strings("1001".to_string(), 3)); // 4
+    println!("{}", count_distinct_strings("10110".to_string(), 5)); // 2
+}
