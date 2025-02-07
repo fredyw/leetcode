55f0d56dab90052f06fb2a12e2b5f673d2d03967
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-02-07 08:54:34 -0600

    Skeleton for problem 2431

---
+++ src/main/rust/src/bin/problem2431.rs
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/maximize-total-tastiness-of-purchased-fruits/description/
+pub fn max_tastiness(
+    price: Vec<i32>,
+    tastiness: Vec<i32>,
+    max_amount: i32,
+    max_coupons: i32,
+) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", max_tastiness(vec![10, 20, 20], vec![5, 8, 8], 20, 1)); // 13
+    println!("{}", max_tastiness(vec![10, 15, 7], vec![5, 8, 20], 10, 2)); // 28
+}
