4661a519aa3a04cf17863089f5ddee6f1344d181
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-02-08 14:43:33 -0600

    Update problem 2450

--- src/main/rust/src/bin/problem2450.rs
+++ src/main/rust/src/bin/problem2450.rs
@@ -1,31 +1,7 @@
-use std::collections::HashSet;
-
 // https://leetcode.com/problems/number-of-distinct-binary-strings-after-applying-operations/description/
 pub fn count_distinct_strings(s: String, k: i32) -> i32 {
-    fn flip(chars: &mut Vec<char>, i: usize, k: i32) {
-        for j in i..i + k as usize {
-            chars[j] = if chars[j] == '1' { '0' } else { '1' };
-        }
-    }
-
-    fn count_distinct_strings(chars: &mut Vec<char>, k: i32, set: &mut HashSet<Vec<char>>) {
-        if set.contains(chars) {
-            return;
-        }
-        let mut i = 0;
-        while (i + k as usize) <= chars.len() {
-            set.insert(chars.clone());
-            flip(chars, i, k);
-            count_distinct_strings(chars, k, set);
-            flip(chars, i, k);
-            i += 1;
-        }
-    }
-
-    let mut chars: Vec<char> = s.chars().collect();
-    let mut set: HashSet<Vec<char>> = HashSet::new();
-    count_distinct_strings(&mut chars, k, &mut set);
-    set.len() as i32
+    let answer: u64 = 2u64.pow(s.len() as u32) / 2u64.pow(k as u32 - 1);
+    (answer % 1_000_000_007) as i32
 }
 
 fn main() {
