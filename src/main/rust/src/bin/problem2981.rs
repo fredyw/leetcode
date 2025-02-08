5e79e2d357177ae8d2c4c17d99fe245d15774688
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-02-08 15:12:31 -0600

    Skeleton for problem 2981

---
+++ src/main/rust/src/bin/problem2981.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/find-longest-special-substring-that-occurs-thrice-i/description/
+pub fn maximum_length(s: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", maximum_length("aaaa".to_string())); // 2
+    println!("{}", maximum_length("abcdef".to_string())); // -1
+    println!("{}", maximum_length("abcaba".to_string())); // 1
+}
