60b678e43b3f82e2eb722489d24391f12e629749
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-02-08 11:32:36 -0600

    Update problem 2450

--- src/main/rust/src/bin/problem2450.rs
+++ src/main/rust/src/bin/problem2450.rs
@@ -6,4 +6,5 @@ pub fn count_distinct_strings(s: String, k: i32) -> i32 {
 fn main() {
     println!("{}", count_distinct_strings("1001".to_string(), 3)); // 4
     println!("{}", count_distinct_strings("10110".to_string(), 5)); // 2
+    println!("{}", count_distinct_strings("1001".to_string(), 2)); // 8
 }
