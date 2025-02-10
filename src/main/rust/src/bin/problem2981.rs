e9249819c3304ba0d5ece4addc52bfbb4588afa7
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-02-09 23:19:24 -0600

    Solve problem 2981

--- src/main/rust/src/bin/problem2981.rs
+++ src/main/rust/src/bin/problem2981.rs
@@ -7,14 +7,27 @@ pub fn maximum_length(s: String) -> i32 {
     for i in 0..bytes.len() {
         for j in i..bytes.len() {
             let sub = &bytes[i..=j];
-            println!("{:?}", sub);
+            if j == i {
+                *map.entry(sub).or_insert(0) += 1;
+            } else {
+                if bytes[j - 1] != bytes[j] {
+                    break;
+                }
+                *map.entry(sub).or_insert(0) += 1;
+            }
         }
     }
-    0
+    let mut answer = -1;
+    for (bytes, count) in map.iter() {
+        if *count >= 3 {
+            answer = answer.max(bytes.len() as i32);
+        }
+    }
+    answer
 }
 
 fn main() {
     println!("{}", maximum_length("aaaa".to_string())); // 2
-                                                        // println!("{}", maximum_length("abcdef".to_string())); // -1
-                                                        // println!("{}", maximum_length("abcaba".to_string())); // 1
+    println!("{}", maximum_length("abcdef".to_string())); // -1
+    println!("{}", maximum_length("abcaba".to_string())); // 1
 }
