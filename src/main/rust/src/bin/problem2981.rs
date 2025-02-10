f93426c7fe10bb625335e496b23621b6bf835cf2
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-02-09 23:13:38 -0600

    Update problem 2981

--- src/main/rust/src/bin/problem2981.rs
+++ src/main/rust/src/bin/problem2981.rs
@@ -1,10 +1,20 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/find-longest-special-substring-that-occurs-thrice-i/description/
 pub fn maximum_length(s: String) -> i32 {
-    todo!()
+    let mut map: HashMap<&[u8], i32> = HashMap::new();
+    let bytes = s.as_bytes();
+    for i in 0..bytes.len() {
+        for j in i..bytes.len() {
+            let sub = &bytes[i..=j];
+            println!("{:?}", sub);
+        }
+    }
+    0
 }
 
 fn main() {
     println!("{}", maximum_length("aaaa".to_string())); // 2
-    println!("{}", maximum_length("abcdef".to_string())); // -1
-    println!("{}", maximum_length("abcaba".to_string())); // 1
+                                                        // println!("{}", maximum_length("abcdef".to_string())); // -1
+                                                        // println!("{}", maximum_length("abcaba".to_string())); // 1
 }
