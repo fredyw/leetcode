cf7f6f6102d807af7f6afa38d05eb8deb6e8974e
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-02-09 23:23:47 -0600

    Solve problem 2703

---
+++ src/main/typescript/problem2703.ts
@@ -0,0 +1,5 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+
+function argumentsLength(...args: JSONValue[]): number {
+    return args.length;
+}
