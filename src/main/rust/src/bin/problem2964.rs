7eb788cf7e755ada25d8c61074405e03c4558905
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-02-12 10:42:29 -0600

    Update problem 2964

--- src/main/rust/src/bin/problem2964.rs
+++ src/main/rust/src/bin/problem2964.rs
@@ -1,12 +1,14 @@
 use std::collections::HashMap;
 
 // https://leetcode.com/problems/number-of-divisible-triplet-sums/
-pub fn divisible_triplet_count(nums: Vec<i32>, d: i32) -> i32 {
+pub fn divisible_triplet_count(mut nums: Vec<i32>, d: i32) -> i32 {
     let mut map: HashMap<i32, i32> = HashMap::new();
     for i in 0..nums.len() {
-        *map.entry(nums[i] % d).or_insert(0) += 1;
+        nums[i] = nums[i] % d;
+        *map.entry(nums[i]).or_insert(0) += 1;
     }
-    println!("{:?}", map);
+    println!("{:?}", nums);
+    let mut answer = 0;
     for i in 0..nums.len() {
         for j in i + 1..nums.len() {
             // (a + b) % d = 0
@@ -14,11 +16,19 @@ pub fn divisible_triplet_count(nums: Vec<i32>, d: i32) -> i32 {
             let a = nums[i] + nums[j];
             let b = (d - (a % d)) % d;
             if let Some(count) = map.get(&b) {
-                println!("b: {b}, count: {count}");
+                answer += *count;
+                // Remove duplicates.
+                if nums[i] == b {
+                    answer -= 1;
+                }
+                if nums[j] == b {
+                    answer -= 1;
+                }
+                println!("a: {a}, b: {b}, count: {count}");
             }
         }
     }
-    0
+    answer
 }
 
 fn main() {
