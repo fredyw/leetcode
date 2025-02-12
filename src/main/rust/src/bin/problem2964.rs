6dcb381c7a735196c091147f5c6be37a4d33919e
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-02-12 09:44:32 -0600

    Skeleton for problem 2864

---
+++ src/main/rust/src/bin/problem2964.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/number-of-divisible-triplet-sums/
+pub fn divisible_triplet_count(nums: Vec<i32>, d: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", divisible_triplet_count(vec![3, 3, 4, 7, 8], 5)); // 3
+    println!("{}", divisible_triplet_count(vec![3, 3, 3, 3], 3)); // 4
+    println!("{}", divisible_triplet_count(vec![3, 3, 3, 3], 6)); // 0
+}
