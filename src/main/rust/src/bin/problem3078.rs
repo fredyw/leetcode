e807a02e41f9c5393b3014f2f81c18511fc6a510
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-02-14 00:54:44 -0600

    Update problem 3078

--- src/main/rust/src/bin/problem3078.rs
+++ src/main/rust/src/bin/problem3078.rs
@@ -1,6 +1,15 @@
 // https://leetcode.com/problems/match-alphanumerical-pattern-in-matrix-i/description/
 pub fn find_pattern(board: Vec<Vec<i32>>, pattern: Vec<String>) -> Vec<i32> {
-    todo!()
+    let mut patterns: Vec<Vec<char>> = vec![];
+    for p in pattern {
+        patterns.push(p.chars().collect());
+    }
+    let num_rows = patterns.len();
+    let num_cols = patterns[0].len();
+    for row in 0..=board.len() - num_rows {
+        for col in 0..=board[row].len() - num_rows {}
+    }
+    vec![]
 }
 
 fn main() {
@@ -11,15 +20,15 @@ fn main() {
             vec!["ab".to_string(), "bb".to_string()]
         )
     ); // [0,0]
-    println!(
-        "{:?}",
-        find_pattern(
-            vec![vec![1, 1, 2], vec![3, 3, 4], vec![6, 6, 6]],
-            vec!["ab".to_string(), "66".to_string()]
-        )
-    ); // [1,1]
-    println!(
-        "{:?}",
-        find_pattern(vec![vec![1, 2], vec![2, 1]], vec!["xx".to_string()])
-    ); // [-1,-1]
+       // println!(
+       //     "{:?}",
+       //     find_pattern(
+       //         vec![vec![1, 1, 2], vec![3, 3, 4], vec![6, 6, 6]],
+       //         vec!["ab".to_string(), "66".to_string()]
+       //     )
+       // ); // [1,1]
+       // println!(
+       //     "{:?}",
+       //     find_pattern(vec![vec![1, 2], vec![2, 1]], vec!["xx".to_string()])
+       // ); // [-1,-1]
 }
