a2401e59c5dc1297e5352743fd867c208b5dc4be
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-02-14 01:33:01 -0600

    Update problem 3078

--- src/main/rust/src/bin/problem3078.rs
+++ src/main/rust/src/bin/problem3078.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/match-alphanumerical-pattern-in-matrix-i/description/
 pub fn find_pattern(board: Vec<Vec<i32>>, pattern: Vec<String>) -> Vec<i32> {
     let mut patterns: Vec<Vec<char>> = vec![];
@@ -6,21 +8,21 @@ pub fn find_pattern(board: Vec<Vec<i32>>, pattern: Vec<String>) -> Vec<i32> {
     }
     let pat_num_rows = patterns.len();
     let pat_num_cols = patterns[0].len();
-    for board_row in 0..=board.len() - pat_num_rows {
-        for board_col in 0..=board[board_row].len() - pat_num_rows {
-            let mut map: Vec<char> = vec![' '; 10];
+    for board_row in 0..board.len() - pat_num_rows {
+        for board_col in 0..board[board_row].len() - pat_num_rows {
+            let mut map: HashMap<char, i32> = HashMap::new();
             let mut found = true;
             'outer: for pat_row in 0..pat_num_rows {
                 for pat_col in 0..pat_num_cols {
                     let b_row = board_row + pat_row;
                     let b_col = board_col + pat_col;
-                    println!("({b_row},{b_col})");
-                    let num = board[b_row][b_col] as usize;
-                    if map[num] != ' ' && map[num] != patterns[pat_row][pat_col] {
-                        found = false;
-                        break 'outer;
+                    if let Some(num) = map.get(&patterns[pat_row][pat_col]) {
+                        if *num != board[b_row][b_col] {
+                            found = false;
+                            break 'outer;
+                        }
                     }
-                    map[num] = patterns[pat_row][pat_col];
+                    map.insert(patterns[pat_row][pat_col], board[b_row][b_col]);
                 }
             }
             if found {
@@ -39,15 +41,15 @@ fn main() {
     //         vec!["ab".to_string(), "bb".to_string()]
     //     )
     // ); // [0,0]
-    // println!(
-    //     "{:?}",
-    //     find_pattern(
-    //         vec![vec![1, 1, 2], vec![3, 3, 4], vec![6, 6, 6]],
-    //         vec!["ab".to_string(), "66".to_string()]
-    //     )
-    // ); // [1,1]
     println!(
         "{:?}",
-        find_pattern(vec![vec![1, 2], vec![2, 1]], vec!["xx".to_string()])
-    ); // [-1,-1]
+        find_pattern(
+            vec![vec![1, 1, 2], vec![3, 3, 4], vec![6, 6, 6]],
+            vec!["ab".to_string(), "66".to_string()]
+        )
+    ); // [1,1]
+       // println!(
+       //     "{:?}",
+       //     find_pattern(vec![vec![1, 2], vec![2, 1]], vec!["xx".to_string()])
+       // ); // [-1,-1]
 }
