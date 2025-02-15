701a41044810b47749f71382af8a8624fae6cd3e
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-02-14 19:27:32 -0600

    Skeleton for problem 2936

---
+++ src/main/rust/src/bin/problem2936.rs
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/number-of-equal-numbers-blocks/description/
+struct BigArray {}
+impl BigArray {
+    pub fn new(elements: Vec<i32>) -> Self {
+        unimplemented!();
+    }
+
+    pub fn at(&self, size: usize) -> i32 {
+        unimplemented!();
+    }
+
+    pub fn size(&self) -> usize {
+        unimplemented!();
+    }
+}
+
+pub fn count_blocks(nums: BigArray) -> i32 {
+    todo!()
+}
+
+fn main() {}
