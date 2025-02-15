2fe91b113d35a9444a7b5db2d733ad2a4772c9fd
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-02-15 10:32:32 -0600

    Update problem 2936

--- src/main/rust/src/bin/problem2936.rs
+++ src/main/rust/src/bin/problem2936.rs
@@ -1,21 +1,24 @@
 // https://leetcode.com/problems/number-of-equal-numbers-blocks/description/
-struct BigArray {}
+pub struct BigArray {
+    elements: Vec<i32>,
+}
+
 impl BigArray {
     pub fn new(elements: Vec<i32>) -> Self {
-        unimplemented!();
+        Self { elements }
     }
 
-    pub fn at(&self, size: usize) -> i32 {
-        unimplemented!();
+    pub fn at(&self, index: usize) -> i32 {
+        self.elements[index]
     }
 
     pub fn size(&self) -> usize {
-        unimplemented!();
+        self.elements.len()
     }
 }
 
 pub fn count_blocks(nums: BigArray) -> i32 {
-    let mut answer = 0;
+    let mut answer = 1;
     for i in 0..nums.size() - 1 {
         if nums.at(i) != nums.at(i + 1) {
             answer += 1;
@@ -24,4 +27,11 @@ pub fn count_blocks(nums: BigArray) -> i32 {
     answer
 }
 
-fn main() {}
+fn main() {
+    println!("{}", count_blocks(BigArray::new(vec![3, 3, 3, 3, 3]))); // 1
+    println!(
+        "{}",
+        count_blocks(BigArray::new(vec![1, 1, 1, 3, 9, 9, 9, 2, 10, 10]))
+    ); // 5
+    println!("{}", count_blocks(BigArray::new(vec![1, 2, 3, 4, 5, 6, 7]))); // 7
+}
