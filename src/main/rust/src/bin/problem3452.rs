d70175fc089bc2c350bd860bb0da41178087a506
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-02-15 10:40:44 -0600

    Skeleton for problem 3452

---
+++ src/main/rust/src/bin/problem3452.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/sum-of-good-numbers/description/
+pub fn sum_of_good_numbers(nums: Vec<i32>, k: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", sum_of_good_numbers(vec![1, 3, 2, 1, 5, 4], 2)); // 12
+    println!("{}", sum_of_good_numbers(vec![2, 1], 1)); // 2
+}
