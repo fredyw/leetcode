a8c33db656d82a8b9792c6790841b0f817f50137
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-02-15 10:49:05 -0600

    Solve problem 3452

--- src/main/rust/src/bin/problem3452.rs
+++ src/main/rust/src/bin/problem3452.rs
@@ -1,6 +1,14 @@
 // https://leetcode.com/problems/sum-of-good-numbers/description/
 pub fn sum_of_good_numbers(nums: Vec<i32>, k: i32) -> i32 {
-    todo!()
+    let mut answer = 0;
+    for i in 0..nums.len() {
+        if (i as i32 - k < 0 || nums[i - k as usize] < nums[i])
+            && (i + k as usize >= nums.len() || nums[i] > nums[i + k as usize])
+        {
+            answer += nums[i];
+        }
+    }
+    answer
 }
 
 fn main() {
