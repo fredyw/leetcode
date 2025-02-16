5a8468d122dad859197047241530988befd186fd
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-02-16 00:44:06 -0600

    Solve problem 3456

--- src/main/rust/src/bin/problem3456.rs
+++ src/main/rust/src/bin/problem3456.rs
@@ -1,9 +1,28 @@
 // https://leetcode.com/problems/find-special-substring-of-length-k/description/
 pub fn has_special_substring(s: String, k: i32) -> bool {
-    todo!()
+    let bytes = s.as_bytes();
+    let mut i = 0;
+    let mut len = 1;
+    while i < bytes.len() - 1 {
+        if bytes[i] == bytes[i + 1] {
+            len += 1;
+        } else {
+            if len == k {
+                return true;
+            }
+            len = 1;
+        }
+        i += 1;
+    }
+    if len == k {
+        true
+    } else {
+        false
+    }
 }
 
 fn main() {
     println!("{}", has_special_substring("aaabaaa".to_string(), 3)); // true
     println!("{}", has_special_substring("abc".to_string(), 2)); // false
+    println!("{}", has_special_substring("aabaaa".to_string(), 3)); // true
 }
