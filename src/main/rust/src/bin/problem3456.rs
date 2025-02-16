f8cb6956b168133ac007dc69b4e0d6ca465392e7
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-02-16 00:39:13 -0600

    Skeleton for problem 3456

---
+++ src/main/rust/src/bin/problem3456.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/find-special-substring-of-length-k/description/
+pub fn has_special_substring(s: String, k: i32) -> bool {
+    todo!()
+}
+
+fn main() {
+    println!("{}", has_special_substring("aaabaaa".to_string(), 3)); // true
+    println!("{}", has_special_substring("abc".to_string(), 2)); // false
+}
