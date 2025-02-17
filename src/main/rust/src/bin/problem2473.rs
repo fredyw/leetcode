df06c5d3056ebec17222ec1fe3435c0e09c57915
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-02-17 09:38:52 -0600

    Update problem 2473

--- src/main/rust/src/bin/problem2473.rs
+++ src/main/rust/src/bin/problem2473.rs
@@ -1,31 +1,38 @@
 // https://leetcode.com/problems/minimum-cost-to-buy-apples/description/
 pub fn min_cost(n: i32, roads: Vec<Vec<i32>>, apple_cost: Vec<i32>, k: i32) -> Vec<i64> {
-    let mut graph: Vec<Vec<i32>> = vec![vec![0; n as usize]; n as usize];
-    for road in roads.iter() {
-        let a = road[0] - 1;
-        let b = road[1] - 1;
-        let cost = road[2];
-        graph[a as usize][b as usize] = cost;
-        graph[b as usize][a as usize] = cost;
-    }
-
+    let mut answer: Vec<i64> = vec![i64::MAX; n as usize];
+    let mut dp: Vec<Vec<i64>> = vec![vec![0; n as usize]; n as usize];
     for i in 0..n as usize {
         for j in 0..n as usize {
-            if graph[i][j] == 0 && i != j {
-                graph[i][j] = i32::MAX;
+            if i == j {
+                dp[i][j] = 0;
+            } else {
+                dp[i][j] = i64::MAX;
             }
         }
     }
-
+    for road in roads.iter() {
+        let from = road[0] as usize - 1;
+        let to = road[1] as usize - 1;
+        let cost = road[2];
+        dp[from][to] = cost as i64;
+        dp[to][from] = cost as i64;
+        answer[from] =
+            answer[from].min(dp[from][to] + apple_cost[to] as i64 + (k as i64 * dp[from][to]));
+        answer[to] =
+            answer[to].min(dp[to][from] + apple_cost[from] as i64 + (k as i64 * dp[to][from]));
+    }
     for k in 0..n as usize {
         for i in 0..n as usize {
             for j in 0..n as usize {
-                graph[i][j] = graph[i][j].min(graph[i][k].saturating_add(graph[k][j]));
+                if dp[i][k] == i64::MAX || dp[k][j] == i64::MAX {
+                    continue;
+                }
+                dp[i][j] = dp[i][j].min(dp[i][k] + dp[k][j]);
+                answer[i] = answer[i].min(dp[i][j] + apple_cost[j] as i64 + (k as i64 * dp[i][j]));
             }
         }
     }
-
-    let mut answer = vec![0; n as usize];
     answer
 }
 
