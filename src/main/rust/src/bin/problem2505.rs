27dcba9b460cc6d7074db725bf90898e55c3aa92
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-02-21 00:51:30 -0600

    Skeleton for problem 2505

---
+++ src/main/rust/src/bin/problem2505.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/bitwise-or-of-all-subsequence-sums/description/
+pub fn subsequence_sum_or(nums: Vec<i32>) -> i64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", subsequence_sum_or(vec![2, 1, 0, 3])); // 7
+    println!("{}", subsequence_sum_or(vec![0, 0, 0])); // 0
+}
