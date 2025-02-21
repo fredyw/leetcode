4b95953430c634919d11615029841841c906631c
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-02-21 12:20:06 -0600

    Solve problem 2505

--- src/main/rust/src/bin/problem2505.rs
+++ src/main/rust/src/bin/problem2505.rs
@@ -3,6 +3,7 @@ pub fn subsequence_sum_or(nums: Vec<i32>) -> i64 {
     let mut answer: i64 = 0;
     let mut sum: i64 = 0;
     for i in 0..nums.len() {
+        answer |= nums[i] as i64;
         sum += nums[i] as i64;
         answer |= sum;
     }
