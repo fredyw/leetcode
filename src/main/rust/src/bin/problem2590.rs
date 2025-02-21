2a500b83094ff433f77c333b6d768a558e22998a
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-02-21 15:12:04 -0600

    Skeleton for problem 2590

--- src/main/rust/src/bin/problem2590.rs
+++ src/main/rust/src/bin/problem2590.rs
@@ -1,3 +1,5 @@
+use std::os::unix::raw::time_t;
+
 // https://leetcode.com/problems/design-a-todo-list/description/
 struct TodoList {}
 
@@ -7,7 +9,7 @@ impl TodoList {
     }
 
     fn add_task(
-        &self,
+        &mut self,
         user_id: i32,
         task_description: String,
         due_date: i32,
@@ -24,9 +26,27 @@ impl TodoList {
         todo!()
     }
 
-    fn complete_task(&self, user_id: i32, task_id: i32) {
+    fn complete_task(&mut self, user_id: i32, task_id: i32) {
         todo!()
     }
 }
 
-fn main() {}
+fn main() {
+    let mut todo_list = TodoList::new();
+    println!("{}", todo_list.add_task(1, "Task1".to_string(), 50, vec![])); // 1
+    println!(
+        "{}",
+        todo_list.add_task(1, "Task2".to_string(), 100, vec!["P1".to_string()])
+    ); // 2
+    println!("{:?}", todo_list.get_all_tasks(1)); // ["Task1","Task2"]
+    println!("{:?}", todo_list.get_all_tasks(5)); // []
+    println!(
+        "{}",
+        todo_list.add_task(1, "Task3".to_string(), 30, vec!["P1".to_string()])
+    ); // 3
+    println!("{:?}", todo_list.get_tasks_for_tag(1, "P1".to_string())); // ["Task3","Task2"]
+    todo_list.complete_task(5, 1);
+    todo_list.complete_task(1, 2);
+    println!("{:?}", todo_list.get_tasks_for_tag(1, "P1".to_string())); // ["Task3"]
+    println!("{:?}", todo_list.get_all_tasks(1)); //["Task3","Task1"]
+}
