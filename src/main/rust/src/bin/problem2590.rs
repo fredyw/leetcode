150993e65ccd820b9002f0c591e3a8c278fb782a
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-02-22 17:03:56 -0600

    Update problem 2590

--- src/main/rust/src/bin/problem2590.rs
+++ src/main/rust/src/bin/problem2590.rs
@@ -1,16 +1,29 @@
 use std::collections::{HashMap, HashSet};
 
 // https://leetcode.com/problems/design-a-todo-list/description/
+#[derive(Debug)]
 struct TodoList {
-    tasks: HashMap<i32, Vec<Task>>,
+    tasks: HashMap<i32, HashMap<i32, Task>>,
     task_ids: HashMap<i32, i32>,
 }
 
+#[derive(Debug)]
 struct Task {
-    task_description: String,
+    description: String,
     due_date: i32,
     tags: HashSet<String>,
     completed: bool,
+}
+
+impl Task {
+    fn new(description: String, due_date: i32, tags: HashSet<String>) -> Task {
+        Self {
+            description,
+            due_date,
+            tags,
+            completed: false,
+        }
+    }
 }
 
 impl TodoList {
@@ -28,21 +41,52 @@ impl TodoList {
         due_date: i32,
         tags: Vec<String>,
     ) -> i32 {
-        let id = self.task_ids.entry(user_id).or_insert(0);
-        *id += 1;
-        *id
+        let task_id = self.task_ids.entry(user_id).or_insert(0);
+        *task_id += 1;
+        let task_id = *task_id;
+        let tasks = self.tasks.entry(user_id).or_insert(HashMap::new());
+        tasks.insert(
+            task_id,
+            Task::new(task_description, due_date, tags.into_iter().collect()),
+        );
+        task_id
     }
 
     fn get_all_tasks(&self, user_id: i32) -> Vec<String> {
-        todo!()
+        let empty_map: HashMap<i32, Task> = HashMap::new();
+        let mut tasks: Vec<&Task> = self
+            .tasks
+            .get(&user_id)
+            .unwrap_or(&empty_map)
+            .iter()
+            .filter(|(_, task)| !task.completed)
+            .map(|(_, task)| task)
+            .collect();
+        tasks.sort_by(|a, b| a.due_date.cmp(&b.due_date));
+        tasks.iter().map(|task| task.description.clone()).collect()
     }
 
     fn get_tasks_for_tag(&self, user_id: i32, tag: String) -> Vec<String> {
-        todo!()
+        let empty_map: HashMap<i32, Task> = HashMap::new();
+        let mut tasks: Vec<&Task> = self
+            .tasks
+            .get(&user_id)
+            .unwrap_or(&empty_map)
+            .iter()
+            .filter(|(_, task)| !task.completed)
+            .filter(|(_, task)| task.tags.contains(&tag))
+            .map(|(_, task)| task)
+            .collect();
+        tasks.sort_by(|a, b| a.due_date.cmp(&b.due_date));
+        tasks.iter().map(|task| task.description.clone()).collect()
     }
 
     fn complete_task(&mut self, user_id: i32, task_id: i32) {
-        todo!()
+        if let Some(tasks) = self.tasks.get_mut(&user_id) {
+            if let Some(task) = tasks.get_mut(&task_id) {
+                task.completed = true;
+            }
+        }
     }
 }
 
