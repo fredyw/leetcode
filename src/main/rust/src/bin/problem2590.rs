0b82225c62c07eb91e5fe889f697517fd9201002
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-02-22 16:17:20 -0600

    Update problem 2590

--- src/main/rust/src/bin/problem2590.rs
+++ src/main/rust/src/bin/problem2590.rs
@@ -1,11 +1,24 @@
-use std::os::unix::raw::time_t;
+use std::collections::{HashMap, HashSet};
 
 // https://leetcode.com/problems/design-a-todo-list/description/
-struct TodoList {}
+struct TodoList {
+    tasks: HashMap<i32, Vec<Task>>,
+    task_ids: HashMap<i32, i32>,
+}
+
+struct Task {
+    task_description: String,
+    due_date: i32,
+    tags: HashSet<String>,
+    completed: bool,
+}
 
 impl TodoList {
     fn new() -> Self {
-        Self {}
+        Self {
+            tasks: HashMap::new(),
+            task_ids: HashMap::new(),
+        }
     }
 
     fn add_task(
@@ -15,7 +28,9 @@ impl TodoList {
         due_date: i32,
         tags: Vec<String>,
     ) -> i32 {
-        todo!()
+        let id = self.task_ids.entry(user_id).or_insert(0);
+        *id += 1;
+        *id
     }
 
     fn get_all_tasks(&self, user_id: i32) -> Vec<String> {
