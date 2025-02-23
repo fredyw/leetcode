c7c446d9f9b1e407f9feb5b80a2f255b2cd57d8a
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-02-23 09:03:08 -0600

    Skeleton for problem 3243

---
+++ src/main/rust/src/bin/problem3243.rs
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/shortest-distance-after-road-addition-queries-i/
+pub fn shortest_distance_after_queries(n: i32, queries: Vec<Vec<i32>>) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{:?}",
+        shortest_distance_after_queries(5, vec![vec![2, 4], vec![0, 2], vec![0, 4]])
+    ); // [3,2,1]
+    println!(
+        "{:?}",
+        shortest_distance_after_queries(4, vec![vec![0, 3], vec![0, 2]])
+    ); // [1,1]
+}
