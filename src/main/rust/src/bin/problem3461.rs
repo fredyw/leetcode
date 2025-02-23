610e9ed0109e792571a31c5e2e328c4ec384c3aa
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-02-23 02:33:10 -0600

    Solve problem 3461

--- src/main/rust/src/bin/problem3461.rs
+++ src/main/rust/src/bin/problem3461.rs
@@ -1,6 +1,14 @@
 // https://leetcode.com/problems/check-if-digits-are-equal-in-string-after-operations-i/description/
 pub fn has_same_digits(s: String) -> bool {
-    todo!()
+    let mut v: Vec<u8> = s.bytes().collect();
+    while v.len() > 2 {
+        let mut tmp = vec![];
+        for i in 0..v.len() - 1 {
+            tmp.push((v[i] + v[i + 1]) % 10);
+        }
+        v = tmp;
+    }
+    v[0] == v[1]
 }
 
 fn main() {
