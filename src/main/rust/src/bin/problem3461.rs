c828fcf4fa6987f41a3fee14ce30c66c3eac0a8f
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-02-23 02:27:15 -0600

    Skeleton for problem 3461

---
+++ src/main/rust/src/bin/problem3461.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/check-if-digits-are-equal-in-string-after-operations-i/description/
+pub fn has_same_digits(s: String) -> bool {
+    todo!()
+}
+
+fn main() {
+    println!("{}", has_same_digits("3902".to_string())); // true
+    println!("{}", has_same_digits("34789".to_string())); // false
+}
