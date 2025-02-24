301ce3f9be3938bfea66adaf6ec5e45b9439c90b
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-02-24 10:44:52 -0600

    Update problem 3243

--- src/main/rust/src/bin/problem3243.rs
+++ src/main/rust/src/bin/problem3243.rs
@@ -1,6 +1,21 @@
 // https://leetcode.com/problems/shortest-distance-after-road-addition-queries-i/
 pub fn shortest_distance_after_queries(n: i32, queries: Vec<Vec<i32>>) -> Vec<i32> {
-    todo!()
+    fn shortest_path(graph: &Vec<Vec<usize>>, start: usize) -> i32 {
+        0
+    }
+
+    let mut graph: Vec<Vec<usize>> = vec![vec![]; n as usize];
+    for i in 0..n - 1 {
+        graph[i as usize].push(i as usize + 1);
+    }
+    let mut answer = vec![];
+    for query in queries {
+        let from = query[0];
+        let to = query[1];
+        graph[from as usize].push(to as usize);
+        answer.push(shortest_path(&graph, 0));
+    }
+    answer
 }
 
 fn main() {
@@ -8,8 +23,8 @@ fn main() {
         "{:?}",
         shortest_distance_after_queries(5, vec![vec![2, 4], vec![0, 2], vec![0, 4]])
     ); // [3,2,1]
-    println!(
-        "{:?}",
-        shortest_distance_after_queries(4, vec![vec![0, 3], vec![0, 2]])
-    ); // [1,1]
+       // println!(
+       //     "{:?}",
+       //     shortest_distance_after_queries(4, vec![vec![0, 3], vec![0, 2]])
+       // ); // [1,1]
 }
