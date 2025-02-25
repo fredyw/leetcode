59a9b936bf00c9c51ca7db5552b579d0c0e14241
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-02-25 11:25:09 -0600

    Update problem 3243

--- src/main/rust/src/bin/problem3243.rs
+++ src/main/rust/src/bin/problem3243.rs
@@ -2,21 +2,24 @@ use std::collections::VecDeque;
 
 // https://leetcode.com/problems/shortest-distance-after-road-addition-queries-i/
 pub fn shortest_distance_after_queries(n: i32, queries: Vec<Vec<i32>>) -> Vec<i32> {
-    fn shortest_path(graph: &Vec<Vec<usize>>, start: usize, end: usize) -> i32 {
-        let mut shortest_distance = i32::MAX;
+    fn shortest_path(graph: &Vec<Vec<usize>>, start: usize, shortest_paths: &mut Vec<i32>) {
         let mut deque: VecDeque<(usize, i32)> = VecDeque::new();
-        deque.push_back((start, 0));
+        deque.push_back((
+            start,
+            if shortest_paths[start] == i32::MAX {
+                0
+            } else {
+                shortest_paths[start]
+            },
+        ));
         while !deque.is_empty() {
             if let Some((node, distance)) = deque.pop_front() {
-                if node == end {
-                    shortest_distance = shortest_distance.min(distance);
-                }
+                shortest_paths[node] = shortest_paths[node].min(distance);
                 for adj in graph[node].iter() {
                     deque.push_back((*adj, distance + 1));
                 }
             }
         }
-        shortest_distance
     }
 
     let mut graph: Vec<Vec<usize>> = vec![vec![]; n as usize];
@@ -24,11 +27,17 @@ pub fn shortest_distance_after_queries(n: i32, queries: Vec<Vec<i32>>) -> Vec<i3
         graph[i as usize].push(i as usize + 1);
     }
     let mut answer = vec![];
-    for query in queries {
+    let mut shortest_paths: Vec<i32> = vec![i32::MAX; graph.len()];
+    for (i, query) in queries.into_iter().enumerate() {
         let from = query[0];
         let to = query[1];
         graph[from as usize].push(to as usize);
-        answer.push(shortest_path(&graph, 0, n as usize - 1));
+        if i == 0 {
+            shortest_path(&graph, 0, &mut shortest_paths);
+        } else {
+            shortest_path(&graph, from as usize, &mut shortest_paths);
+        }
+        answer.push(shortest_paths[n as usize - 1]);
     }
     answer
 }
