23a951f8a66adc8f731e51e04f4104949a251290
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-02-28 19:29:13 -0600

    Skeleton for problem 1136

---
+++ src/main/rust/src/bin/problem1136.rs
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/parallel-courses/
+pub fn minimum_semesters(n: i32, relations: Vec<Vec<i32>>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", minimum_semesters(3, vec![vec![1, 3], vec![2, 3]])); // 2
+    println!(
+        "{}",
+        minimum_semesters(3, vec![vec![1, 2], vec![2, 3], vec![3, 1]])
+    ); // -1
+}
