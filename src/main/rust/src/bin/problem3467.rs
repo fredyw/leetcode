0a7bfabf9e0a1ce7718ef42b05db1efabc0dfea0
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-03-01 22:37:25 -0600

    Solve problem 3467

--- src/main/rust/src/bin/problem3467.rs
+++ src/main/rust/src/bin/problem3467.rs
@@ -1,9 +1,15 @@
 // https://leetcode.com/problems/transform-array-by-parity/description/
 pub fn transform_array(nums: Vec<i32>) -> Vec<i32> {
-    todo!()
+    let mut answer = vec![];
+    for num in nums {
+        answer.push(if num % 2 == 0 { 0 } else { 1 });
+    }
+    answer.sort();
+    answer
 }
 
 fn main() {
     println!("{:?}", transform_array(vec![4, 3, 2, 1])); // [0,0,1,1]
-    println!("{:?}", transform_array(vec![1, 5, 1, 4, 2])); // 0,0,1,1,1]
+    println!("{:?}", transform_array(vec![1, 5, 1, 4, 2])); // [0,0,1,1,1]
+    println!("{:?}", transform_array(vec![8])); // [0]
 }
