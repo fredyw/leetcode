03f25909e5bd4669baef1899fcf35ac0eda16e99
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-03-02 08:36:31 -0600

    Skeleton for problem 3471

---
+++ src/main/rust/src/bin/problem3471.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/find-the-largest-almost-missing-integer/description/
+pub fn largest_integer(nums: Vec<i32>, k: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", largest_integer(vec![3, 9, 2, 1, 7], 3)); // 7
+    println!("{}", largest_integer(vec![3, 9, 7, 2, 1, 7], 4)); // 3
+    println!("{}", largest_integer(vec![0, 0], 1)); // -1
+}
