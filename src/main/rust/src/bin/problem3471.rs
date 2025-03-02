b50c48c5ac5780be934bd565c515fc1a51968bbd
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-03-02 14:35:53 -0600

    Solve problem 3471

--- src/main/rust/src/bin/problem3471.rs
+++ src/main/rust/src/bin/problem3471.rs
@@ -1,6 +1,24 @@
+use std::collections::{HashMap, HashSet};
+
 // https://leetcode.com/problems/find-the-largest-almost-missing-integer/description/
 pub fn largest_integer(nums: Vec<i32>, k: i32) -> i32 {
-    todo!()
+    let mut map: HashMap<i32, i32> = HashMap::new();
+    for i in 0..nums.len() - k as usize + 1 {
+        let mut set: HashSet<i32> = HashSet::new();
+        for j in i..i + k as usize {
+            set.insert(nums[j]);
+        }
+        for n in set {
+            *map.entry(n).or_insert(0) += 1;
+        }
+    }
+    let mut answer = -1;
+    for (n, count) in map {
+        if count == 1 {
+            answer = answer.max(n);
+        }
+    }
+    answer
 }
 
 fn main() {
