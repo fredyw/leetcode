8ccab6a94213c60484ea4451e8f66c177df9a76b
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-03-01 22:29:26 -0600

    Skeleton for problem 3467

---
+++ src/main/rust/src/bin/problem3467.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/transform-array-by-parity/description/
+pub fn transform_array(nums: Vec<i32>) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", transform_array(vec![4, 3, 2, 1])); // [0,0,1,1]
+    println!("{:?}", transform_array(vec![1, 5, 1, 4, 2])); // 0,0,1,1,1]
+}
