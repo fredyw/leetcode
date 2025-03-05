f006862bb9229023568ada16dd217c2f3b82b90c
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-03-05 09:48:18 -0600

    Skeleton for problem 2684

---
+++ src/main/rust/src/bin/problem2684.rs
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/maximum-number-of-moves-in-a-grid/description/
+pub fn max_moves(grid: Vec<Vec<i32>>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        max_moves(vec![
+            vec![2, 4, 3, 5],
+            vec![5, 4, 9, 3],
+            vec![3, 4, 2, 11],
+            vec![10, 9, 13, 15]
+        ])
+    ); // 3
+    println!(
+        "{}",
+        max_moves(vec![vec![3, 2, 4], vec![2, 1, 9], vec![1, 1, 7]])
+    ); // 0
+}
