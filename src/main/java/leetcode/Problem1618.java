5813e977787d13fa6dc16c6077aeba337b7daf0c
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-03-05 09:44:59 -0600

    Solve problem 1618

--- src/main/java/leetcode/Problem1618.java
+++ src/main/java/leetcode/Problem1618.java
@@ -2,7 +2,7 @@ package leetcode;
 
 // https://leetcode.com/problems/maximum-font-to-fit-a-sentence-in-a-screen/
 public class Problem1618 {
-    interface FontInfo {
+    public interface FontInfo {
         // Return the width of char ch when fontSize is used.
         int getWidth(int fontSize, char ch);
 
@@ -11,7 +11,22 @@ public class Problem1618 {
     }
 
     public int maxFont(String text, int w, int h, int[] fonts, FontInfo fontInfo) {
-        // TODO
-        return 0;
+        int left = 0;
+        int right = fonts.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int height = fontInfo.getHeight(fonts[mid]);
+            int totalWidth = getTotalWidth(text, fonts[mid], fontInfo);
+            if (height <= h && totalWidth <= w) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return right < 0 ? -1 : fonts[right];
+    }
+
+    private static int getTotalWidth(String text, int fontSize, FontInfo fontInfo) {
+        return text.chars().map(c -> fontInfo.getWidth(fontSize, (char) c)).sum();
     }
 }
