52d83b842596ad2620c2314f41ef99d7349692e2
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-03-05 10:22:54 -0600

    Update problem 2684

--- src/main/rust/src/bin/problem2684.rs
+++ src/main/rust/src/bin/problem2684.rs
@@ -17,4 +17,14 @@ fn main() {
         "{}",
         max_moves(vec![vec![3, 2, 4], vec![2, 1, 9], vec![1, 1, 7]])
     ); // 0
+    println!("{}", max_moves(vec![vec![2, 4], vec![5, 4],])); // 1
+    println!(
+        "{}",
+        max_moves(vec![
+            vec![8, 4, 3, 5],
+            vec![5, 4, 9, 3],
+            vec![3, 4, 2, 11],
+            vec![10, 9, 13, 15]
+        ])
+    ); // 3
 }
