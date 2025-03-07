e67269c870fe100a05f68625af86fd2735a28db9
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-03-07 10:55:41 -0600

    Solve problem 3465

--- src/main/sql/Problem3465.sql
+++ src/main/sql/Problem3465.sql
@@ -1,1 +1,7 @@
 -- https://leetcode.com/problems/find-products-with-valid-serial-numbers/description/
+select product_id,
+       product_name,
+       description
+from products
+where description ~ '\ySN\d{4}\-\d{4}\y'
+order by product_id;
