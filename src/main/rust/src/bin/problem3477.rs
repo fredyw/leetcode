0aa67517f83c620d9c94fd83211d87458ffb6438
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-03-09 00:35:35 -0600

    Skeleton for problem 3477

---
+++ src/main/rust/src/bin/problem3477.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/fruits-into-baskets-ii/description/
+pub fn num_of_unplaced_fruits(fruits: Vec<i32>, baskets: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", num_of_unplaced_fruits(vec![4, 2, 5], vec![3, 5, 4])); // 1
+    println!("{}", num_of_unplaced_fruits(vec![3, 6, 1], vec![6, 4, 7])); // 0
+}
