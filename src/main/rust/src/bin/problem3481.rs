95b4681f1d97d207e2a501dfdcc0417c9d7ae671
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-03-11 13:41:39 -0500

    Solve problem 3481

--- src/main/rust/src/bin/problem3481.rs
+++ src/main/rust/src/bin/problem3481.rs
@@ -2,27 +2,35 @@ use std::collections::HashMap;
 
 // https://leetcode.com/problems/apply-substitutions/
 pub fn apply_substitutions(replacements: Vec<Vec<String>>, text: String) -> String {
+    fn substitute(s: &str, map: &HashMap<String, String>) -> String {
+        let mut string = String::new();
+        let chars: Vec<char> = s.chars().collect();
+        let mut i = 0;
+        while i < chars.len() {
+            if chars[i] == '%' {
+                let mut j = i + 1;
+                let mut s = String::new();
+                while j < chars.len() && chars[j] != '%' {
+                    s.push(chars[j]);
+                    j += 1;
+                }
+                i = j;
+                string.push_str(map.get(&s).unwrap());
+            } else {
+                string.push(chars[i]);
+            }
+            i += 1;
+        }
+        string
+    }
+
     let mut map: HashMap<String, String> = HashMap::new();
     for replacement in replacements {
         map.insert(replacement[0].clone(), replacement[1].clone());
     }
-    let mut answer = String::new();
-    let chars: Vec<char> = text.chars().collect();
-    let mut i = 0;
-    while i < chars.len() {
-        if chars[i] == '%' {
-            let mut j = i + 1;
-            let mut s = String::new();
-            while j < chars.len() && chars[j] != '%' {
-                s.push(chars[j]);
-                j += 1;
-            }
-            i = j;
-            answer.push_str(map.get(&s).unwrap());
-        } else {
-            answer.push(chars[i]);
-        }
-        i += 1;
+    let mut answer = substitute(&text, &map);
+    while answer.contains(&"%") {
+        answer = substitute(&answer, &map);
     }
     answer
 }
