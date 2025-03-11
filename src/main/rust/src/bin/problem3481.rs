ff96ebde26eb24049826a5e8ecfdbdc7cf496b03
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-03-11 10:25:32 -0500

    Update problem 3481

--- src/main/rust/src/bin/problem3481.rs
+++ src/main/rust/src/bin/problem3481.rs
@@ -1,6 +1,30 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/apply-substitutions/
 pub fn apply_substitutions(replacements: Vec<Vec<String>>, text: String) -> String {
-    todo!()
+    let mut map: HashMap<String, String> = HashMap::new();
+    for replacement in replacements {
+        map.insert(replacement[0].clone(), replacement[1].clone());
+    }
+    let mut answer = String::new();
+    let chars: Vec<char> = text.chars().collect();
+    let mut i = 0;
+    while i < chars.len() {
+        if chars[i] == '%' {
+            let mut j = i + 1;
+            let mut s = String::new();
+            while j < chars.len() && chars[j] != '%' {
+                s.push(chars[j]);
+                j += 1;
+            }
+            i = j;
+            answer.push_str(map.get(&s).unwrap());
+        } else {
+            answer.push(chars[i]);
+        }
+        i += 1;
+    }
+    answer
 }
 
 fn main() {
