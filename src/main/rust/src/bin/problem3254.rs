62e3591e0be1be6bdf748c2ca55a25fe9f39ee14
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-03-12 14:07:14 -0500

    Update problem 3254

--- src/main/rust/src/bin/problem3254.rs
+++ src/main/rust/src/bin/problem3254.rs
@@ -1,6 +1,25 @@
 // https://leetcode.com/problems/find-the-power-of-k-size-subarrays-i/description/
 pub fn results_array(nums: Vec<i32>, k: i32) -> Vec<i32> {
-    todo!()
+    let mut answer = vec![];
+    let mut i = 0;
+    while i < nums.len() - k as usize + 1 {
+        let mut has_power = true;
+        let mut j = i;
+        while i + (k as usize) - 1 < nums.len() && j < i + k as usize - 1 {
+            if nums[j] + 1 != nums[j + 1] {
+                has_power = false;
+                break;
+            }
+            j += 1;
+        }
+        if has_power {
+            answer.push(nums[j]);
+        } else {
+            answer.push(-1);
+        }
+        i += 1;
+    }
+    answer
 }
 
 fn main() {
