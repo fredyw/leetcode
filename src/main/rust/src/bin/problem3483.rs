d3f95b2082792e87524025f817ad9aab09b45daa
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-03-15 11:09:22 -0500

    Skeleton for problem 3483

---
+++ src/main/rust/src/bin/problem3483.rs
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/unique-3-digit-even-numbers/description/
+pub fn total_numbers(digits: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", total_numbers(vec![1, 2, 3, 4])); // 12
+    println!("{}", total_numbers(vec![0, 2, 2])); // 2
+    println!("{}", total_numbers(vec![6, 6, 6])); // 1
+    println!("{}", total_numbers(vec![1, 3, 5])); // 0
+}
