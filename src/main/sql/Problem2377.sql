a75040df0b722697a5764a9432084ce9b33bdbbe
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-03-15 14:30:45 -0500

    Solve problem 2377

--- src/main/sql/Problem2377.sql
+++ src/main/sql/Problem2377.sql
@@ -1,1 +1,7 @@
 -- https://leetcode.com/problems/sort-the-olympic-table/description/
+select country, gold_medals, silver_medals, bronze_medals
+from Olympic
+order by gold_medals desc,
+         silver_medals desc,
+         bronze_medals desc,
+         country asc;
