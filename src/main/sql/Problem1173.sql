19cc6ef292484185dd85cf58acfd67b34b37608f
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-03-15 01:57:09 -0500

    Solve problem 1173

--- src/main/sql/Problem1173.sql
+++ src/main/sql/Problem1173.sql
@@ -1,1 +1,13 @@
 -- https://leetcode.com/problems/immediate-food-delivery-i/description/
+with immediate_orders as (
+    select cast(count(*) as decimal) as count
+    from Delivery
+    where order_date = customer_pref_delivery_date
+), total_orders as (
+    select cast(count(*) as decimal) as count
+    from Delivery
+)
+select round(
+    ((select count from immediate_orders) / (select count from total_orders) * 100),
+    2
+) as immediate_percentage;
