5984e0cfb293d759b4ea182dadb549f89ab0ba4a
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-03-15 11:20:43 -0500

    Update problem 3483

--- src/main/rust/src/bin/problem3483.rs
+++ src/main/rust/src/bin/problem3483.rs
@@ -1,11 +1,31 @@
+use std::collections::HashSet;
+
 // https://leetcode.com/problems/unique-3-digit-even-numbers/description/
 pub fn total_numbers(digits: Vec<i32>) -> i32 {
-    todo!()
+    let mut set: HashSet<String> = HashSet::new();
+    for i in 0..digits.len() {
+        if digits[i] == 0 {
+            continue;
+        }
+        for j in 0..digits.len() {
+            if i == j {
+                continue;
+            }
+            for k in 0..digits.len() {
+                if i == k || k == j {
+                    continue;
+                }
+                set.insert(format!("{}{}{}", digits[i], digits[j], digits[k]));
+            }
+        }
+    }
+    println!("{:?}", set);
+    set.len() as i32
 }
 
 fn main() {
     println!("{}", total_numbers(vec![1, 2, 3, 4])); // 12
-    println!("{}", total_numbers(vec![0, 2, 2])); // 2
-    println!("{}", total_numbers(vec![6, 6, 6])); // 1
-    println!("{}", total_numbers(vec![1, 3, 5])); // 0
+                                                     // println!("{}", total_numbers(vec![0, 2, 2])); // 2
+                                                     // println!("{}", total_numbers(vec![6, 6, 6])); // 1
+                                                     // println!("{}", total_numbers(vec![1, 3, 5])); // 0
 }
