779ad19dcb23e20d4b61d75885afea18e76f107d
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-03-16 01:11:23 -0500

    Skeleton for problem 3487

---
+++ src/main/rust/src/bin/problem3487.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/maximum-unique-subarray-sum-after-deletion/description/
+pub fn max_sum(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", max_sum(vec![1, 2, 3, 4, 5])); // 15
+    println!("{}", max_sum(vec![1, 1, 0, 1, 1])); // 1
+    println!("{}", max_sum(vec![1, 2, -1, -2, 1, 0, -1])); // 3
+}
