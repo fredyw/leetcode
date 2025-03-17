1e2fc5045c7031aa00185189e33baca7e547c9cc
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-03-17 13:44:30 -0500

    Solve problem 3094

--- src/main/rust/src/bin/problem3094.rs
+++ src/main/rust/src/bin/problem3094.rs
@@ -4,7 +4,13 @@ unsafe fn common_bits(num: i32) -> i32 {
 
 // https://leetcode.com/problems/guess-the-number-using-bitwise-questions-ii/description/
 unsafe fn find_number() -> i32 {
-    todo!()
+    let mut answer = 0;
+    for i in 0..30 {
+        if common_bits(1 << i) > common_bits(1 << i) {
+            answer |= (1 << i);
+        }
+    }
+    answer
 }
 
 fn main() {}
