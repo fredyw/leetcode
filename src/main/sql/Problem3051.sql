f02125eb9b9c3769ea4401df6f04995d950e43e9
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-03-17 01:13:56 -0500

    Solve problem 3051

--- src/main/sql/Problem3051.sql
+++ src/main/sql/Problem3051.sql
@@ -1,1 +1,7 @@
 -- https://leetcode.com/problems/find-candidates-for-data-scientist-position/description/
+select candidate_id
+from Candidates
+where skill in ('Python', 'Tableau', 'PostgreSQL')
+group by candidate_id
+having count(skill) >= 3
+order by candidate_id;
