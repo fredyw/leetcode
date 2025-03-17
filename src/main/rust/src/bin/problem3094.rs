9f9cde56c2063a5bfc12d3212dc82b1636078abd
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-03-17 09:13:15 -0500

    Skeleton for problem 3094

---
+++ src/main/rust/src/bin/problem3094.rs
@@ -0,0 +1,10 @@
+unsafe fn common_bits(num: i32) -> i32 {
+    unimplemented!()
+}
+
+// https://leetcode.com/problems/guess-the-number-using-bitwise-questions-ii/description/
+unsafe fn find_number() -> i32 {
+    todo!()
+}
+
+fn main() {}
