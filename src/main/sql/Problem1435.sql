a751460ed7e4dff8bedeeb351f2d82e66f00f9a2
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-03-18 19:38:52 -0500

    Solve problem 1435

--- src/main/sql/Problem1435.sql
+++ src/main/sql/Problem1435.sql
@@ -1,1 +1,16 @@
 -- https://leetcode.com/problems/create-a-session-bar-chart/description/
+select '[0-5>' as bin, count(*) as total
+from Sessions
+where duration < 5 * 60
+union all
+select '[5-10>' as bin, count(*) as total
+from Sessions
+where duration >= 5 * 60 and duration < 10 * 60
+union all
+select '[10-15>' as bin, count(*) as total
+from Sessions
+where duration >= 10 * 60 and duration < 15 * 60
+union all
+select '15 or more' as bin, count(*) as total
+from Sessions
+where duration >= 15 * 60;
