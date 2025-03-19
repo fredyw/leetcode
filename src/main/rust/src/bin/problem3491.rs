c31e909d20f31b6fc8346c6c760fc388de5ee64c
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-03-19 14:37:37 -0500

    Solve problem 3491

--- src/main/rust/src/bin/problem3491.rs
+++ src/main/rust/src/bin/problem3491.rs
@@ -1,6 +1,16 @@
 // https://leetcode.com/problems/phone-number-prefix/description/
 pub fn phone_prefix(numbers: Vec<String>) -> bool {
-    todo!()
+    for (i, num1) in numbers.iter().enumerate() {
+        for (j, num2) in numbers.iter().enumerate() {
+            if i == j {
+                continue;
+            }
+            if num1.starts_with(num2) {
+                return false;
+            }
+        }
+    }
+    true
 }
 
 fn main() {
@@ -22,4 +32,14 @@ fn main() {
             "00153".to_string()
         ])
     ); // false
+    println!(
+        "{}",
+        phone_prefix(vec![
+            "007".to_string(),
+            "15".to_string(),
+            "00153".to_string(),
+            "001".to_string()
+        ])
+    ); // false
+    println!("{}", phone_prefix(vec!["1".to_string(), "1".to_string()])); // false
 }
