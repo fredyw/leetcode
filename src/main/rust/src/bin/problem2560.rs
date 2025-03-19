db744b122d1432e43627a6c4f05a12911d5bcdb0
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-03-18 19:40:24 -0500

    Skeleton for problem 2560

---
+++ src/main/rust/src/bin/problem2560.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/house-robber-iv/
+pub fn min_capability(nums: Vec<i32>, k: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", min_capability(vec![2, 3, 5, 9], 2)); // 5
+    println!("{}", min_capability(vec![2, 7, 9, 3, 1], 2)); // 2
+}
