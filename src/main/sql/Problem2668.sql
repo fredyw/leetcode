5a43323cea0e05b273060010f32001365a22671c
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-03-19 20:17:28 -0500

    Solve problem 2668

--- src/main/sql/Problem2668.sql
+++ src/main/sql/Problem2668.sql
@@ -1,1 +1,5 @@
 -- https://leetcode.com/problems/find-latest-salaries/description/
+select emp_id, firstname, lastname, max(salary) as salary, department_id
+from Salary
+group by emp_id, firstname, lastname, department_id
+order by emp_id;
