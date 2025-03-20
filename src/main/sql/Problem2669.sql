f879320487b7ebd7d6769bde8301f4a9dbbc0a8b
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-03-20 12:06:58 -0500

    Solve problem 2669

--- src/main/sql/Problem2669.sql
+++ src/main/sql/Problem2669.sql
@@ -1,1 +1,5 @@
 -- https://leetcode.com/problems/count-artist-occurrences-on-spotify-ranking-list/description/
+select artist, count(*) as occurrences
+from Spotify
+group by artist
+order by occurrences desc, artist asc;
