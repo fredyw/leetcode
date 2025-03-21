a57071283a2792b75af8147bbaf8878c765c93d0
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-03-21 00:41:14 -0500

    Solve problem 2835

--- src/main/sql/Problem2835.sql
+++ src/main/sql/Problem2835.sql
@@ -1,1 +1,10 @@
 -- https://leetcode.com/problems/highest-salaries-difference/description/
+select abs((
+    select max(salary)
+    from Salaries
+    where department = 'Engineering'
+) - (
+    select max(salary)
+    from Salaries
+    where department = 'Marketing'
+)) as salary_difference;
