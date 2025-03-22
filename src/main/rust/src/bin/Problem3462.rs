4924bd2a110ecabe3a4dd5224376bceb08902842
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-03-22 11:37:02 -0500

    Skeleton for problem 3462

---
+++ src/main/rust/src/bin/Problem3462.rs
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/maximum-sum-with-at-most-k-elements/description/
+pub fn max_sum(grid: Vec<Vec<i32>>, limits: Vec<i32>, k: i32) -> i64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", max_sum(vec![vec![1, 2], vec![3, 4]], vec![1, 2], 2)); // 7
+    println!(
+        "{}",
+        max_sum(vec![vec![5, 3, 7], vec![8, 2, 6]], vec![2, 2], 3)
+    ); // 21
+}
