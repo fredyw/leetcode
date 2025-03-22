691a738587d3fdd0626596d222b2251e2f987065
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-03-22 11:28:09 -0500

    Solve problem 3059

--- src/main/sql/Problem3059.sql
+++ src/main/sql/Problem3059.sql
@@ -1,1 +1,7 @@
 -- https://leetcode.com/problems/find-all-unique-email-domains/description/
+select substring(email FROM '@(.*\.com)$') as email_domain,
+       count(*) as count
+from Emails
+where substring(email FROM '@(.*\.com)$') is not null
+group by email_domain
+order by email_domain;
