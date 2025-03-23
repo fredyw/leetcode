70bec246478fee2b7ad187cbf16617425304f075
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-03-23 09:38:08 -0500

    Solve problem 3204

--- src/main/sql/Problem3204.sql
+++ src/main/sql/Problem3204.sql
@@ -1,1 +1,4 @@
 -- https://leetcode.com/problems/bitwise-user-permissions-analysis/description/
+select bit_and(permissions) as common_perms,
+       bit_or(permissions) as any_perms
+from user_permissions;
