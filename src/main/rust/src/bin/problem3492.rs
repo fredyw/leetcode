e9d0679448a492bc80956864299bf46caa656d12
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-03-22 23:41:02 -0500

    Skeleton for problem 3492

---
+++ src/main/rust/src/bin/problem3492.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/maximum-containers-on-a-ship/description/
+pub fn max_containers(n: i32, w: i32, max_weight: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", max_containers(2, 3, 15)); // 4
+    println!("{}", max_containers(3, 5, 20)); // 4
+}
