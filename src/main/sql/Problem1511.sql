63db7570725818b5904cf0217e681b94aa0764ea
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-03-23 09:32:46 -0500

    Update problem 1511

--- src/main/sql/Problem1511.sql
+++ src/main/sql/Problem1511.sql
@@ -1,17 +1,7 @@
 -- https://leetcode.com/problems/customer-order-frequency/
-with june_orders as (
-    select c.customer_id, c.name, sum(o.quantity * p.price) as total_spending
-    from Orders o join Customers c on o.customer_id = c.customer_id
-        join Product p on o.product_id = p.product_id
-    where o.order_date between '2020-06-01' and '2020-06-30'
-    group by c.customer_id, c.name
-), july_orders as (
-    select c.customer_id, c.name, sum(o.quantity * p.price) as total_spending
-    from Orders o join Customers c on o.customer_id = c.customer_id
-        join Product p on o.product_id = p.product_id
-    where o.order_date between '2020-07-01' and '2020-07-31'
-    group by c.customer_id, c.name
-)
-select a.customer_id, a.name
-from june_orders a join july_orders b on a.customer_id = b.customer_id
-where a.total_spending >= 100 and b.total_spending >= 100;
+select c.customer_id, c.name
+from Customers c join Orders o on c.customer_id = o.customer_id
+     join Product p ON o.product_id = p.product_id
+group by c.customer_id, c.name
+having sum(case when o.order_date between '2020-06-01' and '2020-06-30' then o.quantity * p.price else 0 end) >= 100
+   and sum(case when o.order_date between '2020-07-01' and '2020-07-31' then o.quantity * p.price else 0 end) >= 100;
