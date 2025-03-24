cef7d989daf66d227059bf27b343d7ab8e97e827
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-03-24 11:41:56 -0500

    Solve problem 603

--- src/main/sql/Problem603.sql
+++ src/main/sql/Problem603.sql
@@ -1,1 +1,6 @@
 -- https://leetcode.com/problems/consecutive-available-seats/description/
+select distinct c1.seat_id
+from cinema c1 join cinema c2
+     on abs(c1.seat_id - c2.seat_id) = 1
+     and c1.free = 1 and c2.free = 1
+order by c1.seat_id;
