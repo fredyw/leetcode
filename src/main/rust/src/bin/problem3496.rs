ba8ecbf19e167857133232f88e66bb0b7217851f
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-03-25 11:09:52 -0500

    Update problem 3496

--- src/main/rust/src/bin/problem3496.rs
+++ src/main/rust/src/bin/problem3496.rs
@@ -1,6 +1,17 @@
 // https://leetcode.com/problems/maximize-score-after-pair-deletions/
 pub fn max_score(nums: Vec<i32>) -> i32 {
-    todo!()
+    fn max_score(nums: &Vec<i32>, i: usize, j: usize) -> i32 {
+        if j - i + 1 <= 2 {
+            return 0;
+        }
+        let a = max_score(nums, i + 2, j) + nums[i] + nums[i + 1];
+        let b = max_score(nums, i, j - 2) + nums[j] + nums[j - 1];
+        let c = max_score(nums, i + 1, j - 1) + nums[i] + nums[j];
+        let max = a.max(b.max(c));
+        max
+    }
+
+    max_score(&nums, 0, nums.len() - 1)
 }
 
 fn main() {
