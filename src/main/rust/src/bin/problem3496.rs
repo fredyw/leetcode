3398d0c939eea549c4aa6d9bf065d4265ebcd90a
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-03-25 01:17:43 -0500

    Skeleton for problem 3496

---
+++ src/main/rust/src/bin/problem3496.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/maximize-score-after-pair-deletions/
+pub fn max_score(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", max_score(vec![2, 4, 1])); // 6
+    println!("{}", max_score(vec![5, -1, 4, 2])); // 7
+}
