4cfe5d1ffd140c118239ddfe7eb39c0785f3b532
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-03-26 01:00:12 -0500

    Solve problem 3338

--- src/main/sql/Problem3338.sql
+++ src/main/sql/Problem3338.sql
@@ -1,1 +1,15 @@
 -- https://leetcode.com/problems/second-highest-salary-ii/description/
+with highest_salary_per_dept as (
+    select dept, max(salary) as highest_salary
+    from employees
+    group by dept
+), second_highest_salary_per_dept as (
+    select e.dept, max(e.salary) as second_highest_salary
+    from employees e join highest_salary_per_dept s
+        on e.dept = s.dept and e.salary != s.highest_salary
+    group by e.dept
+)
+select e.emp_id, e.dept
+from employees e join second_highest_salary_per_dept s
+     on e.dept = s.dept and e.salary = s.second_highest_salary
+order by e.emp_id;
