bcfb6a3a9e8fc332504d864bbec5e6ff54ec88a4
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-03-28 21:16:56 -0500

    Update problem 3394

--- src/main/rust/src/bin/problem3394.rs
+++ src/main/rust/src/bin/problem3394.rs
@@ -41,4 +41,46 @@ fn main() {
             ]
         )
     ); // false
+    println!(
+        "{}",
+        check_valid_cuts(
+            5,
+            vec![
+                vec![0, 2, 2, 4],
+                vec![1, 0, 3, 2],
+                vec![2, 2, 3, 4],
+                vec![3, 0, 4, 2],
+                vec![3, 2, 4, 4],
+                vec![3, 4, 4, 5],
+            ]
+        )
+    ); // true
+    println!(
+        "{}",
+        check_valid_cuts(
+            5,
+            vec![
+                vec![0, 2, 2, 4],
+                vec![1, 0, 3, 2],
+                vec![2, 2, 3, 4],
+                vec![3, 0, 4, 2],
+                vec![3, 2, 4, 4],
+                vec![4, 0, 5, 1],
+            ]
+        )
+    ); // true
+    println!(
+        "{}",
+        check_valid_cuts(
+            4,
+            vec![
+                vec![0, 2, 2, 4],
+                vec![1, 0, 3, 2],
+                vec![2, 2, 3, 4],
+                vec![3, 0, 4, 2],
+                vec![3, 2, 4, 4],
+                vec![0, 0, 1, 1],
+            ]
+        )
+    ); // false
 }
