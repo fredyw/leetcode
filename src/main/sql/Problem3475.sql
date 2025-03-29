645e0f68c18ab9960699f0a96c5894c3c8edd2d8
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-03-29 09:28:00 -0500

    Solve problem 3475

--- src/main/sql/Problem3475.sql
+++ src/main/sql/Problem3475.sql
@@ -1,1 +1,10 @@
 -- https://leetcode.com/problems/dna-pattern-recognition/description/
+select sample_id,
+       dna_sequence,
+       species,
+       case when dna_sequence ~ '^ATG' then 1 else 0 end as has_start,
+       case when dna_sequence ~ '(TAA|TAG|TGA)$' then 1 else 0 end as has_stop,
+       case when dna_sequence ~ '(ATAT)+' then 1 else 0 end as has_atat,
+       case when dna_sequence ~ 'GGG+' then 1 else 0 end as has_ggg
+from Samples
+order by sample_id;
