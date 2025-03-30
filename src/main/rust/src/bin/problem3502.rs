9e83b1b3e21eb752b84912a8109f8ef567c6b0a5
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-03-30 00:41:37 -0500

    Update problem 3502

--- src/main/rust/src/bin/problem3502.rs
+++ src/main/rust/src/bin/problem3502.rs
@@ -3,4 +3,7 @@ pub fn min_costs(cost: Vec<i32>) -> Vec<i32> {
     todo!()
 }
 
-fn main() {}
+fn main() {
+    println!("{:?}", min_costs(vec![5, 3, 4, 1, 3, 2])); // [5,3,3,1,1,1]
+    println!("{:?}", min_costs(vec![1, 2, 4, 6, 7])); // [1,1,1,1,1]
+}
