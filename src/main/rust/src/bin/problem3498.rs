6ba691c303627dbf3903042c187475dd374c8f0d
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-03-30 00:37:01 -0500

    Solve problem 3498

--- src/main/rust/src/bin/problem3498.rs
+++ src/main/rust/src/bin/problem3498.rs
@@ -1,6 +1,10 @@
 // https://leetcode.com/problems/reverse-degree-of-a-string/description/
 pub fn reverse_degree(s: String) -> i32 {
-    todo!()
+    s.chars()
+        .into_iter()
+        .enumerate()
+        .map(|(i, c)| (i as i32 + 1) * (26 - (c as i32 - 'a' as i32)))
+        .sum()
 }
 
 fn main() {
