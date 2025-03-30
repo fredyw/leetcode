0554a44def4a60caf81f8e15b04db229f78e7782
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-03-30 13:27:32 -0500

    Solve problem 3394

--- src/main/rust/src/bin/problem3394.rs
+++ src/main/rust/src/bin/problem3394.rs
@@ -1,5 +1,5 @@
 // https://leetcode.com/problems/check-if-grid-can-be-cut-into-sections/description/
-pub fn check_valid_cuts(n: i32, rectangles: Vec<Vec<i32>>) -> bool {
+pub fn check_valid_cuts(_n: i32, rectangles: Vec<Vec<i32>>) -> bool {
     let mut x_vec: Vec<(i32, i32)> = vec![];
     let mut y_vec: Vec<(i32, i32)> = vec![];
     for rectangle in rectangles {
@@ -9,8 +9,8 @@ pub fn check_valid_cuts(n: i32, rectangles: Vec<Vec<i32>>) -> bool {
     x_vec.sort();
     let mut count = 0;
     let mut max = 0;
-    for (start, end) in x_vec.iter() {
-        if max <= *start {
+    for (i, (start, end)) in x_vec.iter().enumerate() {
+        if i > 0 && max <= *start {
             count += 1;
         }
         max = max.max(*end);
@@ -21,8 +21,8 @@ pub fn check_valid_cuts(n: i32, rectangles: Vec<Vec<i32>>) -> bool {
     y_vec.sort();
     let mut count = 0;
     let mut max = 0;
-    for (start, end) in y_vec.iter() {
-        if max <= *start {
+    for (i, (start, end)) in y_vec.iter().enumerate() {
+        if i > 0 && max <= *start {
             count += 1;
         }
         max = max.max(*end);
@@ -31,30 +31,30 @@ pub fn check_valid_cuts(n: i32, rectangles: Vec<Vec<i32>>) -> bool {
 }
 
 fn main() {
-    // println!(
-    //     "{}",
-    //     check_valid_cuts(
-    //         5,
-    //         vec![
-    //             vec![1, 0, 5, 2],
-    //             vec![0, 2, 2, 4],
-    //             vec![3, 2, 5, 3],
-    //             vec![0, 4, 4, 5]
-    //         ]
-    //     )
-    // ); // true
-    // println!(
-    //     "{}",
-    //     check_valid_cuts(
-    //         4,
-    //         vec![
-    //             vec![0, 0, 1, 1],
-    //             vec![2, 0, 3, 4],
-    //             vec![0, 2, 2, 3],
-    //             vec![3, 0, 4, 3]
-    //         ]
-    //     )
-    // ); // true
+    println!(
+        "{}",
+        check_valid_cuts(
+            5,
+            vec![
+                vec![1, 0, 5, 2],
+                vec![0, 2, 2, 4],
+                vec![3, 2, 5, 3],
+                vec![0, 4, 4, 5]
+            ]
+        )
+    ); // true
+    println!(
+        "{}",
+        check_valid_cuts(
+            4,
+            vec![
+                vec![0, 0, 1, 1],
+                vec![2, 0, 3, 4],
+                vec![0, 2, 2, 3],
+                vec![3, 0, 4, 3]
+            ]
+        )
+    ); // true
     println!(
         "{}",
         check_valid_cuts(
@@ -68,46 +68,46 @@ fn main() {
             ]
         )
     ); // false
-       // println!(
-       //     "{}",
-       //     check_valid_cuts(
-       //         5,
-       //         vec![
-       //             vec![0, 2, 2, 4],
-       //             vec![1, 0, 3, 2],
-       //             vec![2, 2, 3, 4],
-       //             vec![3, 0, 4, 2],
-       //             vec![3, 2, 4, 4],
-       //             vec![3, 4, 4, 5],
-       //         ]
-       //     )
-       // ); // true
-       // println!(
-       //     "{}",
-       //     check_valid_cuts(
-       //         5,
-       //         vec![
-       //             vec![0, 2, 2, 4],
-       //             vec![1, 0, 3, 2],
-       //             vec![2, 2, 3, 4],
-       //             vec![3, 0, 4, 2],
-       //             vec![3, 2, 4, 4],
-       //             vec![4, 0, 5, 1],
-       //         ]
-       //     )
-       // ); // true
-       // println!(
-       //     "{}",
-       //     check_valid_cuts(
-       //         4,
-       //         vec![
-       //             vec![0, 2, 2, 4],
-       //             vec![1, 0, 3, 2],
-       //             vec![2, 2, 3, 4],
-       //             vec![3, 0, 4, 2],
-       //             vec![3, 2, 4, 4],
-       //             vec![0, 0, 1, 1],
-       //         ]
-       //     )
-       // ); // false
+    println!(
+        "{}",
+        check_valid_cuts(
+            5,
+            vec![
+                vec![0, 2, 2, 4],
+                vec![1, 0, 3, 2],
+                vec![2, 2, 3, 4],
+                vec![3, 0, 4, 2],
+                vec![3, 2, 4, 4],
+                vec![3, 4, 4, 5],
+            ]
+        )
+    ); // true
+    println!(
+        "{}",
+        check_valid_cuts(
+            5,
+            vec![
+                vec![0, 2, 2, 4],
+                vec![1, 0, 3, 2],
+                vec![2, 2, 3, 4],
+                vec![3, 0, 4, 2],
+                vec![3, 2, 4, 4],
+                vec![4, 0, 5, 1],
+            ]
+        )
+    ); // true
+    println!(
+        "{}",
+        check_valid_cuts(
+            4,
+            vec![
+                vec![0, 2, 2, 4],
+                vec![1, 0, 3, 2],
+                vec![2, 2, 3, 4],
+                vec![3, 0, 4, 2],
+                vec![3, 2, 4, 4],
+                vec![0, 0, 1, 1],
+            ]
+        )
+    ); // false
 }
