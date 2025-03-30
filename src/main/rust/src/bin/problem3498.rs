9c4cc51a64b4d3d4d117086eeebab882622e6881
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-03-30 00:31:37 -0500

    Skeleton for problem 3498

---
+++ src/main/rust/src/bin/problem3498.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/reverse-degree-of-a-string/description/
+pub fn reverse_degree(s: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", reverse_degree("abc".to_string())); // 148
+    println!("{}", reverse_degree("zaza".to_string())); // 160
+}
