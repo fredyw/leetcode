34e3f213921627b9d0072035dadc00dc6775f138
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-04-04 23:13:29 -0500

    Solve problem 1322

--- src/main/sql/problem1322.sql
+++ src/main/sql/problem1322.sql
@@ -1,21 +1,14 @@
 -- https://leetcode.com/problems/ads-performance/
-with total_clicked as (
-    select ad_id, cast(count(*) as decimal) as count
-    from Ads
-    where action = 'Clicked'
-    group by ad_id
-), total_viewed as (
-    select ad_id, cast(count(*) as decimal) as count
-    from Ads
-    where action = 'Viewed'
-    group by ad_id
-)
-select c.ad_id,
-       case
-           when c.count + v.count = 0 then 0
-           else round(c.count / (c.count + v.count) * 100, 2)
-       end as ctr
-from total_clicked c join total_viewed v
-     on c.ad_id = v.ad_id
-group by c.ad_id, ctr
-order by ctr desc, c.ad_id asc;
+select
+    ad_id,
+    case
+        when sum(case when action = 'Clicked' then 1 else 0 end) +
+             sum(case when action = 'Viewed' then 1 else 0 end) = 0 then 0
+        else round(
+            cast(sum(case when action = 'Clicked' then 1 else 0 end) as decimal) /
+            (cast(sum(case when action = 'Clicked' then 1 else 0 end) as decimal) +
+             cast(sum(case when action = 'Viewed' then 1 else 0 end) as decimal)) * 100, 2)
+    end as ctr
+from Ads
+group by ad_id
+order by ctr desc, ad_id asc;
