f683f89df06602e29649020463a3dbe21d5dda13
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-04-06 02:26:07 -0500

    Update problem 3507

--- src/main/rust/src/bin/problem3507.rs
+++ src/main/rust/src/bin/problem3507.rs
@@ -6,4 +6,6 @@ pub fn minimum_pair_removal(nums: Vec<i32>) -> i32 {
 fn main() {
     println!("{}", minimum_pair_removal(vec![5, 2, 3, 1])); // 2
     println!("{}", minimum_pair_removal(vec![1, 2, 2])); // 0
+    println!("{}", minimum_pair_removal(vec![5, 1, 3, 2])); // 2
+    println!("{}", minimum_pair_removal(vec![10, 1, 3, 2])); // 3
 }
