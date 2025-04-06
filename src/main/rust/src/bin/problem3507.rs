70a72875f60922bc7082e9eda3f3c9194b2edf82
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-04-06 16:19:08 -0500

    Solve problem 3507

--- src/main/rust/src/bin/problem3507.rs
+++ src/main/rust/src/bin/problem3507.rs
@@ -1,6 +1,39 @@
 // https://leetcode.com/problems/minimum-pair-removal-to-sort-array-i/
-pub fn minimum_pair_removal(nums: Vec<i32>) -> i32 {
-    todo!()
+pub fn minimum_pair_removal(mut nums: Vec<i32>) -> i32 {
+    fn is_sorted(nums: &Vec<i32>) -> bool {
+        for i in 0..nums.len() - 1 {
+            if nums[i] > nums[i + 1] {
+                return false;
+            }
+        }
+        true
+    }
+
+    let mut answer = 0;
+    while !is_sorted(&nums) {
+        let mut sum = i32::MAX;
+        let mut index = 0;
+        for i in 0..nums.len() - 1 {
+            if nums[i] + nums[i + 1] < sum {
+                sum = nums[i] + nums[i + 1];
+                index = i;
+            }
+        }
+        let mut tmp = vec![];
+        let mut i = 0;
+        while i < nums.len() {
+            if i == index {
+                tmp.push(nums[i] + nums[i + 1]);
+                i += 2;
+            } else {
+                tmp.push(nums[i]);
+                i += 1;
+            }
+        }
+        nums = tmp;
+        answer += 1;
+    }
+    answer
 }
 
 fn main() {
