c2e591b63a65e97e8065da798b1d8cac5f7bc2fc
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-04-06 02:15:09 -0500

    Skeleton for problem 3507

---
+++ src/main/rust/src/bin/problem3507.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/minimum-pair-removal-to-sort-array-i/
+pub fn minimum_pair_removal(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", minimum_pair_removal(vec![5, 2, 3, 1])); // 2
+    println!("{}", minimum_pair_removal(vec![1, 2, 2])); // 0
+}
