f48ad8f1078eb752253ff0e68ff0616fef42adfb
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-04-12 09:18:06 -0500

    Skeleton for problem 2205

---
+++ src/main/sql/Problem2205.sql
@@ -0,0 +1,8 @@
+-- https://leetcode.com/problems/the-number-of-users-that-are-eligible-for-discount/
+CREATE OR REPLACE FUNCTION getUserIDs(startDate DATE, endDate DATE, minAmount INT) RETURNS INT AS $$
+BEGIN
+  RETURN (
+	  
+  );
+END;
+$$ LANGUAGE plpgsql;
