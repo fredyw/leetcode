0a58b26abf4aeb57464c183efe105e254e4f8b10
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-04-12 23:55:01 -0500

    Skeleton for problem 3516

---
+++ src/main/rust/src/bin/problme3516.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/find-closest-person/description/
+pub fn find_closest(x: i32, y: i32, z: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", find_closest(2, 7, 4)); // 1
+    println!("{}", find_closest(2, 5, 6)); // 2
+    println!("{}", find_closest(1, 5, 3)); // 0
+}
