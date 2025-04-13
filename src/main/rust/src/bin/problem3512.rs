6c9ad80aae8b7eec2326277ce02bc536d503cbb4
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-04-12 23:50:41 -0500

    Skeleton for problem 3512

---
+++ src/main/rust/src/bin/problem3512.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/minimum-operations-to-make-array-sum-divisible-by-k/description/
+pub fn min_operations(nums: Vec<i32>, k: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", min_operations(vec![3, 9, 7], 5)); // 4
+    println!("{}", min_operations(vec![4, 1, 3], 4)); // 0
+    println!("{}", min_operations(vec![3, 2], 6)); // 5
+}
