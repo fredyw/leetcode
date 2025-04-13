dd1dc385e8ff18135f191ac0afbcc42241262770
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-04-13 00:38:52 -0500

    Solve problem 2948

--- src/main/rust/src/bin/problem2948.rs
+++ src/main/rust/src/bin/problem2948.rs
@@ -1,8 +1,11 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/make-lexicographically-smallest-array-by-swapping-elements/
-pub fn lexicographically_smallest_array(nums: Vec<i32>, limit: i32) -> Vec<i32> {
+pub fn lexicographically_smallest_array(mut nums: Vec<i32>, limit: i32) -> Vec<i32> {
     let mut sorted_nums = nums.clone();
     sorted_nums.sort();
     let mut groups: Vec<Vec<i32>> = vec![];
+    let mut map: HashMap<i32, usize> = HashMap::new();
     let mut same_group = false;
     for i in 0..sorted_nums.len() - 1 {
         if sorted_nums[i + 1] - sorted_nums[i] <= limit {
@@ -10,33 +13,47 @@ pub fn lexicographically_smallest_array(nums: Vec<i32>, limit: i32) -> Vec<i32> 
                 groups.push(vec![]);
             }
             groups.last_mut().unwrap().push(sorted_nums[i]);
+            map.insert(sorted_nums[i], groups.len() - 1);
             same_group = true;
         } else {
             if same_group {
                 groups.last_mut().unwrap().push(sorted_nums[i]);
+                map.insert(sorted_nums[i], groups.len() - 1);
             }
             same_group = false;
         }
     }
-    for group in groups.iter() {
-        println!("{:?}", group);
+    if same_group {
+        groups
+            .last_mut()
+            .unwrap()
+            .push(sorted_nums[sorted_nums.len() - 1]);
+        map.insert(sorted_nums[sorted_nums.len() - 1], groups.len() - 1);
+    }
+    let mut group_index: HashMap<usize, usize> = HashMap::new();
+    for i in 0..nums.len() {
+        if let Some(idx) = map.get(&nums[i]) {
+            let cur_idx = group_index.entry(*idx).or_insert(0);
+            nums[i] = groups[*idx][*cur_idx];
+            *cur_idx += 1;
+        }
     }
     nums
 }
 
 fn main() {
-    // println!(
-    //     "{:?}",
-    //     lexicographically_smallest_array(vec![1, 5, 3, 9, 8], 2)
-    // ); // [1,3,5,8,9]
-    // println!(
-    //     "{:?}",
-    //     lexicographically_smallest_array(vec![1, 7, 6, 18, 2, 1], 3)
-    // ); // [1,6,7,18,1,2]
-    // println!(
-    //     "{:?}",
-    //     lexicographically_smallest_array(vec![1, 7, 28, 19, 10], 3)
-    // ); // [1,7,28,19,10]
+    println!(
+        "{:?}",
+        lexicographically_smallest_array(vec![1, 5, 3, 9, 8], 2)
+    ); // [1,3,5,8,9]
+    println!(
+        "{:?}",
+        lexicographically_smallest_array(vec![1, 7, 6, 18, 2, 1], 3)
+    ); // [1,6,7,18,1,2]
+    println!(
+        "{:?}",
+        lexicographically_smallest_array(vec![1, 7, 28, 19, 10], 3)
+    ); // [1,7,28,19,10]
     println!(
         "{:?}",
         lexicographically_smallest_array(vec![1, 60, 34, 84, 62, 56, 39, 76, 49, 38], 4)
