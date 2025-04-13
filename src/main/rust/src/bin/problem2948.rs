315d71d96a445acad6e91dae135c522bc41997f8
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-04-13 00:23:14 -0500

    Update problem 2948

--- src/main/rust/src/bin/problem2948.rs
+++ src/main/rust/src/bin/problem2948.rs
@@ -1,30 +1,39 @@
-use std::cmp::Ordering;
-
 // https://leetcode.com/problems/make-lexicographically-smallest-array-by-swapping-elements/
-pub fn lexicographically_smallest_array(mut nums: Vec<i32>, limit: i32) -> Vec<i32> {
-    nums.sort_by(|a, b| {
-        if (a - b).abs() <= limit {
-            a.cmp(b)
+pub fn lexicographically_smallest_array(nums: Vec<i32>, limit: i32) -> Vec<i32> {
+    let mut sorted_nums = nums.clone();
+    sorted_nums.sort();
+    let mut groups: Vec<Vec<(usize, i32)>> = vec![];
+    let mut same_group = false;
+    for i in 0..sorted_nums.len() - 1 {
+        if sorted_nums[i + 1] - sorted_nums[i] <= limit {
+            if !same_group {
+                groups.push(vec![]);
+            }
+            groups.last_mut().unwrap().push((i, sorted_nums[i]));
+            same_group = true;
         } else {
-            Ordering::Equal
+            same_group = false;
         }
-    });
+    }
+    for group in groups.iter() {
+        println!("{:?}", group);
+    }
     nums
 }
 
 fn main() {
-    println!(
-        "{:?}",
-        lexicographically_smallest_array(vec![1, 5, 3, 9, 8], 2)
-    ); // [1,3,5,8,9]
-    println!(
-        "{:?}",
-        lexicographically_smallest_array(vec![1, 7, 6, 18, 2, 1], 3)
-    ); // [1,6,7,18,1,2]
-    println!(
-        "{:?}",
-        lexicographically_smallest_array(vec![1, 7, 28, 19, 10], 3)
-    ); // [1,7,28,19,10]
+    // println!(
+    //     "{:?}",
+    //     lexicographically_smallest_array(vec![1, 5, 3, 9, 8], 2)
+    // ); // [1,3,5,8,9]
+    // println!(
+    //     "{:?}",
+    //     lexicographically_smallest_array(vec![1, 7, 6, 18, 2, 1], 3)
+    // ); // [1,6,7,18,1,2]
+    // println!(
+    //     "{:?}",
+    //     lexicographically_smallest_array(vec![1, 7, 28, 19, 10], 3)
+    // ); // [1,7,28,19,10]
     println!(
         "{:?}",
         lexicographically_smallest_array(vec![1, 60, 34, 84, 62, 56, 39, 76, 49, 38], 4)
