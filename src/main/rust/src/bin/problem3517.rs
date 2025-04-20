87c1e972ce0fe59a5696a411a514486ceca2e539
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-04-19 21:41:02 -0500

    Solve problem 3517

--- src/main/rust/src/bin/problem3517.rs
+++ src/main/rust/src/bin/problem3517.rs
@@ -5,13 +5,21 @@ pub fn smallest_palindrome(s: String) -> String {
     let mut answer = vec![' '; s.len()];
     let mut i = 0;
     let mut j = 0;
+    let mut mid = ' ';
     while i < chars.len() {
+        if i + 1 < chars.len() && chars[i] != chars[i + 1] {
+            mid = chars[i];
+            i += 1;
+        }
         answer[j] = chars[i];
         if i + 1 < chars.len() {
             answer[chars.len() - j - 1] = chars[i + 1];
         }
         i += 2;
         j += 1;
+    }
+    if mid != ' ' {
+        answer[j] = mid;
     }
     answer.iter().collect()
 }
@@ -21,4 +29,5 @@ fn main() {
     println!("{}", smallest_palindrome("babab".to_string())); // "abbba"
     println!("{}", smallest_palindrome("daccad".to_string())); // "acddca"
     println!("{}", smallest_palindrome("yey".to_string())); // "yey"
+    println!("{}", smallest_palindrome("abbcc".to_string())); // "bcacb"
 }
