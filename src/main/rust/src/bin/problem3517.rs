bbbe8f2909dad9d521b6b425006338746634d763
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-04-19 20:17:24 -0500

    Update problem 3517

--- src/main/rust/src/bin/problem3517.rs
+++ src/main/rust/src/bin/problem3517.rs
@@ -1,10 +1,24 @@
 // https://leetcode.com/problems/smallest-palindromic-rearrangement-i/
 pub fn smallest_palindrome(s: String) -> String {
-    todo!()
+    let mut chars: Vec<char> = s.chars().collect();
+    chars.sort();
+    let mut answer = vec![' '; s.len()];
+    let mut i = 0;
+    let mut j = 0;
+    while i < chars.len() {
+        answer[j] = chars[i];
+        if i + 1 < chars.len() {
+            answer[chars.len() - j - 1] = chars[i + 1];
+        }
+        i += 2;
+        j += 1;
+    }
+    answer.iter().collect()
 }
 
 fn main() {
     println!("{}", smallest_palindrome("z".to_string())); // "z"
     println!("{}", smallest_palindrome("babab".to_string())); // "abbba"
     println!("{}", smallest_palindrome("daccad".to_string())); // "acddca"
+    println!("{}", smallest_palindrome("yey".to_string())); // "yey"
 }
