39465e46698e1435e9ed9905a28cc2bd6762aa29
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-04-19 20:08:31 -0500

    Skeleton for problem 3517

---
+++ src/main/rust/src/bin/problem3517.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/smallest-palindromic-rearrangement-i/
+pub fn smallest_palindrome(s: String) -> String {
+    todo!()
+}
+
+fn main() {
+    println!("{}", smallest_palindrome("z".to_string())); // "z"
+    println!("{}", smallest_palindrome("babab".to_string())); // "abbba"
+    println!("{}", smallest_palindrome("daccad".to_string())); // "acddca"
+}
