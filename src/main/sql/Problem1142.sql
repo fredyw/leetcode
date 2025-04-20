6b51b47879e985c47790a559c0c8c45d09d8dfd5
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-04-19 19:12:36 -0500

    Solve problem 1142

--- src/main/sql/Problem1142.sql
+++ src/main/sql/Problem1142.sql
@@ -1,1 +1,8 @@
 -- https://leetcode.com/problems/user-activity-for-the-past-30-days-ii/description/
+select coalesce(round(avg(count), 2), 0) as average_sessions_per_user
+    from (
+    select user_id, count(distinct session_id) as count
+    from Activity
+    where activity_date between '2019-07-28'::date - interval '30 days' and '2019-07-27'
+    group by user_id
+);
