f6116f5f9f5c4c904593e543075b50fc8fcbf441
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-04-26 11:13:37 -0500

    Update problem 3253

--- src/main/rust/src/bin/problem3253.rs
+++ src/main/rust/src/bin/problem3253.rs
@@ -1,6 +1,29 @@
 // https://leetcode.com/problems/construct-string-with-minimum-cost-easy/description/
 pub fn minimum_cost(target: String, words: Vec<String>, costs: Vec<i32>) -> i32 {
-    todo!()
+    fn minimum_cost(target: &str, words: &Vec<(String, i32)>, index: usize) -> i32 {
+        if index == target.len() {
+            return 0;
+        }
+        let mut min_cost = i32::MAX;
+        let sub = &target[index..];
+        for (word, cost) in words.iter() {
+            if sub.starts_with(word) {
+                min_cost = min_cost.min(minimum_cost(target, words, index + word.len()) + *cost);
+            }
+        }
+        min_cost
+    }
+
+    let answer = minimum_cost(
+        &target,
+        &words.into_iter().zip(costs.into_iter()).collect(),
+        0,
+    );
+    if answer == i32::MAX {
+        -1
+    } else {
+        answer
+    }
 }
 
 fn main() {
@@ -26,4 +49,18 @@ fn main() {
             vec![1, 10, 100]
         )
     ); // -1
+    println!(
+        "{}",
+        minimum_cost(
+            "abcdef".to_string(),
+            vec![
+                "abcd".to_string(),
+                "abc".to_string(),
+                "d".to_string(),
+                "def".to_string(),
+                "ef".to_string()
+            ],
+            vec![100, 1, 1, 10, 5]
+        )
+    ); // 7
 }
