aa14cfe660c92473e192fa6513d7a7e68ec2f02c
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-04-27 00:30:26 -0500

    Skeleton for problem 254

---
+++ src/main/rust/src/bin/problem254.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/factor-combinations/description/
+pub fn get_factors(n: i32) -> Vec<Vec<i32>> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", get_factors(1)); // []
+    println!("{:?}", get_factors(12)); // [[2,6],[3,4],[2,2,3]]
+    println!("{:?}", get_factors(37)); // []
+}
