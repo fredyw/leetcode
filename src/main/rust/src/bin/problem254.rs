e2202e666ebaf0e0116f7f414b8393a722a1ff86
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-04-27 23:49:34 -0500

    Update problem 254

--- src/main/rust/src/bin/problem254.rs
+++ src/main/rust/src/bin/problem254.rs
@@ -1,10 +1,24 @@
 // https://leetcode.com/problems/factor-combinations/description/
 pub fn get_factors(n: i32) -> Vec<Vec<i32>> {
-    (2..=n.isqrt()).map(|x| vec![x, n / x]).collect()
+    fn factors(n: i32) -> Vec<Vec<i32>> {
+        (2..=n.isqrt())
+            .filter(|m| n % m == 0)
+            .map(|m| vec![m, n / m])
+            .collect()
+    }
+
+    let mut answer = vec![];
+    for v in factors(n).iter() {
+        for n in v.iter() {}
+    }
+    answer
 }
 
 fn main() {
-    println!("{:?}", get_factors(1)); // []
-    println!("{:?}", get_factors(12)); // [[2,6],[3,4],[2,2,3]]
-    println!("{:?}", get_factors(37)); // []
+    // println!("{:?}", get_factors(1)); // []
+    // println!("{:?}", get_factors(12)); // [[2,6],[3,4],[2,2,3]]
+    // println!("{:?}", get_factors(37)); // []
+    // println!("{:?}", get_factors(24)); // [[2,12],[3,8],[4,6],[2,2,6],[2,3,4],[2,2,2,3]]
+    // println!("{:?}", get_factors(4)); // [[2,2]]
+    println!("{:?}", get_factors(8)); // [[2,4],[2,2,2]]
 }
