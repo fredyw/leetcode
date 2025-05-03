53ecd671ae093a1c9d3ab3109b2babe26dbe6b86
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-05-03 17:48:33 -0500

    Update problem 161

--- src/main/rust/src/bin/problem161.rs
+++ src/main/rust/src/bin/problem161.rs
@@ -1,27 +1,60 @@
 // https://leetcode.com/problems/one-edit-distance/description/
 pub fn is_one_edit_distance(s: String, t: String) -> bool {
+    fn is_one_edit_distance(
+        s: &Vec<char>,
+        t: &Vec<char>,
+        i: usize,
+        j: usize,
+        change: bool,
+        memo: &mut Vec<Vec<Vec<i32>>>,
+    ) -> bool {
+        if i >= s.len() && j >= t.len() && change {
+            return true;
+        }
+        if i >= s.len() || j >= t.len() {
+            return false;
+        }
+        if change && s[i] != t[j] {
+            return false;
+        }
+        let c = if change { 1 } else { 0 };
+        if memo[i][j][c] != -1 {
+            return memo[i][j][c] == 1;
+        }
+        let has_change = s[i] != t[j] || change;
+        let remove = is_one_edit_distance(s, t, i, j + 1, has_change, memo);
+        let add = is_one_edit_distance(s, t, i + 1, j, has_change, memo);
+        let replace = is_one_edit_distance(s, t, i + 1, j + 1, has_change, memo);
+        let found = remove || add || replace;
+        memo[i][j][c] = if found { 1 } else { 0 };
+        found
+    }
+
     let s: Vec<char> = s.chars().collect();
     let t: Vec<char> = t.chars().collect();
-    let mut i = 0;
-    let mut j = 0;
-    while i < s.len() && j < t.len() {
-        if s[i] != t[j] {}
-    }
-    todo!()
+    is_one_edit_distance(
+        &s,
+        &t,
+        0,
+        0,
+        false,
+        &mut vec![vec![vec![-1; 2]; t.len()]; s.len()],
+    )
 }
 
 fn main() {
-    println!(
-        "{}",
-        is_one_edit_distance("ab".to_string(), "acb".to_string())
-    ); // true
-    println!("{}", is_one_edit_distance("".to_string(), "".to_string())); // false
-    println!(
-        "{}",
-        is_one_edit_distance("ab".to_string(), "a".to_string())
-    ); // true
-    println!(
-        "{}",
-        is_one_edit_distance("ab".to_string(), "ar".to_string())
-    ); // true
+    // println!(
+    //     "{}",
+    //     is_one_edit_distance("ab".to_string(), "acb".to_string())
+    // ); // true
+    // println!("{}", is_one_edit_distance("".to_string(), "".to_string())); // false
+    // println!(
+    //     "{}",
+    //     is_one_edit_distance("ab".to_string(), "a".to_string())
+    // ); // true
+    // println!(
+    //     "{}",
+    //     is_one_edit_distance("ab".to_string(), "ar".to_string())
+    // ); // true
+    println!("{}", is_one_edit_distance("a".to_string(), "".to_string())); // false
 }
