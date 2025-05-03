4bac9915297be1aeecc7e2c67972c9ce6ea529c0
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-05-03 09:06:37 -0500

    Skeleton for problem 288

---
+++ src/main/rust/src/bin/problem288.rs
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/unique-word-abbreviation/
+struct ValidWordAbbr {}
+
+impl ValidWordAbbr {
+    fn new(dictionary: Vec<String>) -> Self {
+        todo!()
+    }
+
+    fn is_unique(&self, word: String) -> bool {
+        todo!()
+    }
+}
+
+fn main() {}
