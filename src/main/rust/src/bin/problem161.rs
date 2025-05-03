f9d310cda9bbb48f767e4a19b1d64fa02748910c
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-05-03 11:25:23 -0500

    Skeleton for problem 161

---
+++ src/main/rust/src/bin/problem161.rs
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/one-edit-distance/description/
+pub fn is_one_edit_distance(s: String, t: String) -> bool {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        is_one_edit_distance("ab".to_string(), "acb".to_string())
+    ); // true
+    println!("{}", is_one_edit_distance("".to_string(), "".to_string())); // false
+}
