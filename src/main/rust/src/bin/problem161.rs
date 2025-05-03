01ff46ef1c0e75bab7217e6f78ef5e5ae23b095f
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-05-03 16:50:56 -0500

    Update problem 161

--- src/main/rust/src/bin/problem161.rs
+++ src/main/rust/src/bin/problem161.rs
@@ -1,5 +1,12 @@
 // https://leetcode.com/problems/one-edit-distance/description/
 pub fn is_one_edit_distance(s: String, t: String) -> bool {
+    let s: Vec<char> = s.chars().collect();
+    let t: Vec<char> = t.chars().collect();
+    let mut i = 0;
+    let mut j = 0;
+    while i < s.len() && j < t.len() {
+        if s[i] != t[j] {}
+    }
     todo!()
 }
 
@@ -9,4 +16,12 @@ fn main() {
         is_one_edit_distance("ab".to_string(), "acb".to_string())
     ); // true
     println!("{}", is_one_edit_distance("".to_string(), "".to_string())); // false
+    println!(
+        "{}",
+        is_one_edit_distance("ab".to_string(), "a".to_string())
+    ); // true
+    println!(
+        "{}",
+        is_one_edit_distance("ab".to_string(), "ar".to_string())
+    ); // true
 }
