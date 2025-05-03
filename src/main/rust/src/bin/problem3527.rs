f619e9be0d88823a5f01f401305c4b76a4d8ea21
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-05-02 23:19:32 -0500

    Solve problem 3527

--- src/main/rust/src/bin/problem3527.rs
+++ src/main/rust/src/bin/problem3527.rs
@@ -1,6 +1,28 @@
+use std::cmp::Ordering;
+use std::collections::{HashMap, HashSet};
+
 // https://leetcode.com/problems/find-the-most-common-response/
 pub fn find_common_response(responses: Vec<Vec<String>>) -> String {
-    todo!()
+    let responses: Vec<HashSet<String>> = responses
+        .into_iter()
+        .map(|v| v.into_iter().collect::<HashSet<String>>())
+        .collect();
+    let mut map: HashMap<String, u32> = HashMap::new();
+    for response in responses {
+        for r in response {
+            *map.entry(r).or_insert(0) += 1;
+        }
+    }
+    let mut v: Vec<(String, u32)> = map.into_iter().collect();
+    v.sort_by(|a, b| {
+        let ord = b.1.cmp(&a.1);
+        if ord == Ordering::Equal {
+            a.0.cmp(&b.0)
+        } else {
+            ord
+        }
+    });
+    v[0].0.to_string()
 }
 
 fn main() {
