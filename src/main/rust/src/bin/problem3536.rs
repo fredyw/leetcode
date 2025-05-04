4a81a1161e870ab71da7cb6bc59a03ae347b843c
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-05-04 14:37:49 -0500

    Skeleton for problem 3536

---
+++ src/main/rust/src/bin/problem3536.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/maximum-product-of-two-digits/description/
+pub fn max_product(n: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", max_product(31)); // 3
+    println!("{}", max_product(22)); // 4
+    println!("{}", max_product(124)); // 8
+}
