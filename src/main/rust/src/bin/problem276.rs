c54eea2b64ed44c0383f52e0d2df274f837452e9
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-05-04 00:31:42 -0500

    Skeleton for problem 276

---
+++ src/main/rust/src/bin/problem276.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/paint-fence/
+pub fn num_ways(n: i32, k: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", num_ways(3, 2)); // 6
+    println!("{}", num_ways(1, 1)); // 1
+    println!("{}", num_ways(7, 2)); // 42
+}
