c9ba3deea0d960333b052c372ee5f32776bc4109
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-05-06 00:22:16 -0500

    Solve problem 534

--- src/main/sql/Problem534.sql
+++ src/main/sql/Problem534.sql
@@ -1,1 +1,5 @@
 -- https://leetcode.com/problems/game-play-analysis-iii/description/
+select player_id,
+       event_date,
+       sum(games_played) over (partition by player_id order by event_date) as games_played_so_far
+from Activity;
