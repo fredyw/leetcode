d1896a39d58bf9679916cce9840302317d16687a
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-05-08 21:13:24 -0500

    Update problem 276

--- src/main/rust/src/bin/problem276.rs
+++ src/main/rust/src/bin/problem276.rs
@@ -7,4 +7,5 @@ fn main() {
     println!("{}", num_ways(3, 2)); // 6
     println!("{}", num_ways(1, 1)); // 1
     println!("{}", num_ways(7, 2)); // 42
+    println!("{}", num_ways(2, 3)); // 9
 }
