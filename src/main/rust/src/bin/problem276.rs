d2a2a172043136fa37234cbe13e8e4b6fdd26ba6
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-05-10 19:10:28 -0500

    Solve problem 276

--- src/main/rust/src/bin/problem276.rs
+++ src/main/rust/src/bin/problem276.rs
@@ -1,41 +1,21 @@
-use std::collections::HashMap;
-
 // https://leetcode.com/problems/paint-fence/
 pub fn num_ways(n: i32, k: i32) -> i32 {
-    fn num_ways(
-        n: i32,
-        k: i32,
-        prev_colors: [i32; 2],
-        memo: &mut HashMap<(i32, i32, i32), i32>,
-    ) -> i32 {
-        if n == 0 {
-            return 1;
+    fn num_ways(n: i32, k: i32, memo: &mut Vec<i32>) -> i32 {
+        if n == 1 {
+            return k;
         }
-        let key = (n, prev_colors[0], prev_colors[1]);
-        if let Some(v) = memo.get(&key) {
-            return *v;
+        if n == 2 {
+            return k * k;
         }
-        let mut count = 0;
-        for color in 1..=k {
-            if prev_colors[0] == color && prev_colors[1] == color {
-                continue;
-            }
-            let mut colors = prev_colors.clone();
-            if prev_colors[0] == -1 {
-                colors[0] = color;
-            } else if prev_colors[1] == -1 {
-                colors[1] = color;
-            } else {
-                colors[0] = colors[1];
-                colors[1] = color;
-            }
-            count += num_ways(n - 1, k, colors, memo);
+        if memo[n as usize] != -1 {
+            return memo[n as usize];
         }
-        memo.insert(key, count);
+        let count = (k - 1) * (num_ways(n - 1, k, memo) + num_ways(n - 2, k, memo));
+        memo[n as usize] = count;
         count
     }
 
-    num_ways(n, k, [0, 0], &mut HashMap::new())
+    num_ways(n, k, &mut vec![-1; n as usize + 1])
 }
 
 fn main() {
@@ -43,5 +23,6 @@ fn main() {
     println!("{}", num_ways(1, 1)); // 1
     println!("{}", num_ways(7, 2)); // 42
     println!("{}", num_ways(2, 3)); // 9
+    println!("{}", num_ways(10, 6)); // 49503750
     println!("{}", num_ways(2, 46340)); // 2147395600
 }
