6bd9e6703585700f4c60c522910682dc5315e651
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-05-10 19:30:45 -0500

    Solve problem 570

--- src/main/sql/Problem570.sql
+++ src/main/sql/Problem570.sql
@@ -1,1 +1,10 @@
 -- https://leetcode.com/problems/managers-with-at-least-5-direct-reports/
+with managers as (
+    select managerId
+    from Employee
+    group by managerId
+    having count(managerId) >= 5
+)
+select e.name
+from Employee e join managers m
+     on e.id = m.managerId;
