246552c88c6ec413da97192ce1fdacf9d13c7dbe
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-05-12 00:09:15 -0500

    Skeleton for problem 3541

---
+++ src/main/rust/src/bin/problem3541.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/find-most-frequent-vowel-and-consonant/description/
+pub fn max_freq_sum(s: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", max_freq_sum("successes".to_string())); // 6
+    println!("{}", max_freq_sum("aeiaeia".to_string())); // 3
+}
