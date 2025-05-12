d445a09720a5cb09f86fd8c7b3f6ded492a25994
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-05-12 00:14:47 -0500

    Solve problem 3541

--- src/main/rust/src/bin/problem3541.rs
+++ src/main/rust/src/bin/problem3541.rs
@@ -1,6 +1,16 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/find-most-frequent-vowel-and-consonant/description/
 pub fn max_freq_sum(s: String) -> i32 {
-    todo!()
+    let mut vowels: HashMap<char, i32> = HashMap::new();
+    let mut consonants: HashMap<char, i32> = HashMap::new();
+    for c in s.chars() {
+        match c {
+            'a' | 'e' | 'i' | 'o' | 'u' => *vowels.entry(c).or_insert(0) += 1,
+            _ => *consonants.entry(c).or_insert(0) += 1,
+        }
+    }
+    vowels.values().max().unwrap_or(&0) + consonants.values().max().unwrap_or(&0)
 }
 
 fn main() {
