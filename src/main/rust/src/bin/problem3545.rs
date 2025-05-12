53cf936e696c95bcf3cc2ca2dd92a6ba2a3f60d4
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-05-12 01:30:02 -0500

    Solve problem 3545

--- src/main/rust/src/bin/problem3545.rs
+++ src/main/rust/src/bin/problem3545.rs
@@ -1,6 +1,18 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/minimum-deletions-for-at-most-k-distinct-characters/description/
 pub fn min_deletion(s: String, k: i32) -> i32 {
-    todo!()
+    let mut map: HashMap<char, i32> = HashMap::new();
+    for c in s.chars() {
+        *map.entry(c).or_insert(0) += 1;
+    }
+    let mut v: Vec<i32> = map.iter().map(|e| *e.1).collect();
+    v.sort_by(|a, b| b.cmp(&a));
+    let mut answer = 0;
+    for i in k as usize..v.len() {
+        answer += v[i];
+    }
+    answer
 }
 
 fn main() {
