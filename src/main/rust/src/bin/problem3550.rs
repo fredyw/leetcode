3db62c33abca50e6ea1a53e5be5e455779964b62
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-05-18 18:02:02 -0500

    Solve problem 3550

--- src/main/rust/src/bin/problem3550.rs
+++ src/main/rust/src/bin/problem3550.rs
@@ -1,6 +1,17 @@
 // https://leetcode.com/problems/smallest-index-with-digit-sum-equal-to-index/description/
 pub fn smallest_index(nums: Vec<i32>) -> i32 {
-    todo!()
+    let mut answer = -1;
+    for (i, n) in nums.into_iter().enumerate() {
+        let sum: i32 = n.to_string().chars().map(|c| c as i32 - '0' as i32).sum();
+        if sum == i as i32 {
+            if answer == -1 {
+                answer = i as i32;
+            } else {
+                answer = answer.min(i as i32);
+            }
+        }
+    }
+    answer
 }
 
 fn main() {
