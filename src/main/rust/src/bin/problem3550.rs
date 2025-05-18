eb5120ef89c96621d7708d90888b8e119769832c
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-05-18 17:55:08 -0500

    Skeleton for problem 3550

---
+++ src/main/rust/src/bin/problem3550.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/smallest-index-with-digit-sum-equal-to-index/description/
+pub fn smallest_index(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", smallest_index(vec![1, 3, 2])); // 2
+    println!("{}", smallest_index(vec![1, 10, 11])); // 1
+    println!("{}", smallest_index(vec![1, 2, 3])); // -1
+}
