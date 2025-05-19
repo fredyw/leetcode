9558d269eb7b6291338b794bf07aa769ce616c5d
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-05-19 17:37:45 -0500

    Skeleton for problem 340

---
+++ src/main/rust/src/bin/problem340.rs
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/longest-substring-with-at-most-k-distinct-characters/description/
+pub fn length_of_longest_substring_k_distinct(s: String, k: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        length_of_longest_substring_k_distinct("eceba".to_string(), 2)
+    ); // 3
+    println!(
+        "{}",
+        length_of_longest_substring_k_distinct("aa".to_string(), 1)
+    ); // 2
+}
