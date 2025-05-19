885330b95f46a8963d8db9ba0fd628906e1f0f3c
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-05-18 19:18:38 -0500

    Solve problem 2066

--- src/main/sql/Problem2066.sql
+++ src/main/sql/Problem2066.sql
@@ -1,1 +1,7 @@
 -- https://leetcode.com/problems/account-balance/description/
+select account_id,
+       day,
+       sum(case when type = 'Deposit' then amount else -amount end)
+           over (partition by account_id order by day) as balance
+from Transactions
+order by account_id, day;
