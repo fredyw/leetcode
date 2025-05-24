14c37f0ce8d28ae5d20c0601d69032bd29c253a0
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-05-23 23:35:23 -0500

    Update problem 340

--- src/main/rust/src/bin/problem340.rs
+++ src/main/rust/src/bin/problem340.rs
@@ -1,6 +1,12 @@
+use std::collections::{HashMap, VecDeque};
+
 // https://leetcode.com/problems/longest-substring-with-at-most-k-distinct-characters/description/
 pub fn length_of_longest_substring_k_distinct(s: String, k: i32) -> i32 {
-    todo!()
+    let bytes = s.as_bytes();
+    let mut map: HashMap<u8, i32> = HashMap::new();
+    let mut answer = 0;
+    for i in 0..bytes.len() {}
+    answer
 }
 
 fn main() {
