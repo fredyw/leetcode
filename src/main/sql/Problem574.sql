2150a625e32c1aa140af690ea56bdf9da648b18a
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-05-24 17:27:08 -0500

    Solve problem 574

--- src/main/sql/Problem574.sql
+++ src/main/sql/Problem574.sql
@@ -1,1 +1,12 @@
 -- https://leetcode.com/problems/winning-candidate/description/
+with vote_count as (
+    select c.name, count(*) as count
+    from Candidate c join Vote v on v.candidateId = c.id
+    group by c.name
+), max_count as (
+    select max(count) as max_count
+    from vote_count
+)
+select name
+from vote_count
+where count = (select max_count from max_count);
