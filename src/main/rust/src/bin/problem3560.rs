8acc91bfaefad8b7ccfbefce17ce458dbb943198
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-05-25 09:13:32 -0500

    Skeleton for problem 3560

---
+++ src/main/rust/src/bin/problem3560.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/find-minimum-log-transportation-cost/description/
+pub fn min_cutting_cost(n: i32, m: i32, k: i32) -> i64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", min_cutting_cost(6, 5, 5)); // 5
+    println!("{}", min_cutting_cost(4, 4, 6)); // 0
+}
