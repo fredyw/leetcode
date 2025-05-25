9a892e987f6e270d66fb4937057074a420f48774
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-05-24 23:27:24 -0500

    Skeleton for problem 3537

---
+++ src/main/rust/src/bin/problem3537.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/fill-a-special-grid/
+pub fn special_grid(n: i32) -> Vec<Vec<i32>> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", special_grid(0)); // [[0]]
+    println!("{:?}", special_grid(1)); // [[3,0],[2,1]]
+    println!("{:?}", special_grid(2)); // [[15,12,3,0],[14,13,2,1],[11,8,7,4],[10,9,6,5]]
+}
