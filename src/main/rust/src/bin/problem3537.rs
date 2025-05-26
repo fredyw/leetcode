967f21b2c262a69f636c0167b285691d31c8c379
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-05-26 10:42:41 -0500

    Update problem 3557

--- src/main/rust/src/bin/problem3537.rs
+++ src/main/rust/src/bin/problem3537.rs
@@ -1,12 +1,31 @@
 // https://leetcode.com/problems/fill-a-special-grid/
 pub fn special_grid(n: i32) -> Vec<Vec<i32>> {
+    fn special_grid(row: usize, col: usize, n: i32, num: &mut i32, answer: &mut Vec<Vec<i32>>) {
+        answer[row][col] = *num;
+        answer[row + 1][col] = *num + 1;
+        answer[row + 1][col - 1] = *num + 2;
+        answer[row][col - 1] = *num + 3;
+        *num += 3;
+    }
+
     let mut answer = vec![vec![0; 2u32.pow(n as u32) as usize]; 2u32.pow(n as u32) as usize];
+    let num_rows = answer.len();
+    let num_cols = if num_rows > 0 { answer[0].len() } else { 0 };
+    special_grid(0, num_cols - 1, n, &mut 0, &mut answer);
     answer
 }
 
 fn main() {
     println!("{:?}", special_grid(0)); // [[0]]
+
+    // [3,0]
+    // [2,1]
     println!("{:?}", special_grid(1)); // [[3,0],[2,1]]
+
+    // [15,12,3,0]
+    // [14,13,2,1]
+    // [11, 8,7,4]
+    // [10, 9,6,5]
     println!("{:?}", special_grid(2)); // [[15,12,3,0],[14,13,2,1],[11,8,7,4],[10,9,6,5]]
 
     // [63,60,51,48,15,12, 3, 0]
