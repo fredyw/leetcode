0c74031d563b9cc1313044195e094b2f0b646260
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-05-26 11:15:42 -0500

    Solve problem 3537

--- src/main/rust/src/bin/problem3537.rs
+++ src/main/rust/src/bin/problem3537.rs
@@ -1,11 +1,33 @@
 // https://leetcode.com/problems/fill-a-special-grid/
 pub fn special_grid(n: i32) -> Vec<Vec<i32>> {
     fn special_grid(row: usize, col: usize, n: i32, num: &mut i32, answer: &mut Vec<Vec<i32>>) {
-        answer[row][col] = *num;
-        answer[row + 1][col] = *num + 1;
-        answer[row + 1][col - 1] = *num + 2;
-        answer[row][col - 1] = *num + 3;
-        *num += 4;
+        if n == 0 {
+            answer[row][col] = *num;
+            *num += 1;
+            return;
+        }
+        special_grid(row, col, n - 1, num, answer);
+        special_grid(
+            row + 2u32.pow(n as u32 - 1) as usize,
+            col,
+            n - 1,
+            num,
+            answer,
+        );
+        special_grid(
+            row + 2u32.pow(n as u32 - 1) as usize,
+            col - 2u32.pow(n as u32 - 1) as usize,
+            n - 1,
+            num,
+            answer,
+        );
+        special_grid(
+            row,
+            col - 2u32.pow(n as u32 - 1) as usize,
+            n - 1,
+            num,
+            answer,
+        );
     }
 
     let mut answer = vec![vec![0; 2u32.pow(n as u32) as usize]; 2u32.pow(n as u32) as usize];
@@ -17,24 +39,7 @@ pub fn special_grid(n: i32) -> Vec<Vec<i32>> {
 
 fn main() {
     println!("{:?}", special_grid(0)); // [[0]]
-
-    // [3,0]
-    // [2,1]
     println!("{:?}", special_grid(1)); // [[3,0],[2,1]]
-
-    // [15,12,3,0]
-    // [14,13,2,1]
-    // [11, 8,7,4]
-    // [10, 9,6,5]
     println!("{:?}", special_grid(2)); // [[15,12,3,0],[14,13,2,1],[11,8,7,4],[10,9,6,5]]
-
-    // [63,60,51,48,15,12, 3, 0]
-    // [62,61,50,49,14,13, 2, 1]
-    // [59,56,55,52,11, 8, 7, 4]
-    // [58,57,54,53,10, 9, 6, 5]
-    // [47,44,35,32,31,28,19,16]
-    // [46,45,34,33,30,29,18,17]
-    // [43,40,39,36,27,24,23,20]
-    // [42,41,38,37,26,25,22,21]
     println!("{:?}", special_grid(3)); // [[63,60,51,48,15,12,3,0],[62,61,50,49,14,13,2,1],[59,56,55,52,11,8,7,4],[58,57,54,53,10,9,6,5],[47,44,35,32,31,28,19,16],[46,45,34,33,30,29,18,17],[43,40,39,36,27,24,23,20],[42,41,38,37,26,25,22,21]]
 }
