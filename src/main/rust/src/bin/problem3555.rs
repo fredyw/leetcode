a036ab155107a26899b354b3660a891a618aedbb
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-05-26 11:34:51 -0500

    Skeleton for problem 3555

---
+++ src/main/rust/src/bin/problem3555.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/smallest-subarray-to-sort-in-every-sliding-window/description/
+pub fn min_subarray_sort(nums: Vec<i32>, k: i32) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", min_subarray_sort(vec![1, 3, 2, 4, 5], 3)); // [2,2,0]
+    println!("{:?}", min_subarray_sort(vec![5, 4, 3, 2, 1], 4)); // [4,4]
+}
