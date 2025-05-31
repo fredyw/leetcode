2289c2a770712b59cc315b1871fed8a9aa0a50c5
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-05-31 15:08:19 -0500

    Solve problem 1285

--- src/main/sql/Problem1285.sql
+++ src/main/sql/Problem1285.sql
@@ -1,1 +1,10 @@
 -- https://leetcode.com/problems/find-the-start-and-end-number-of-continuous-ranges/description/
+with group_logs as (
+    select log_id, log_id - dense_rank() over (order by log_id) as grp
+    from Logs
+    group by log_id
+)
+select min(log_id) as start_id, max(log_id) as end_id
+from group_logs
+group by grp
+order by start_id;
