708658fa2ca9c2bfbd2f371bae4e26068587e64e
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-05-31 19:33:25 -0500

    Solve problem 2993

--- src/main/sql/Problem2993.sql
+++ src/main/sql/Problem2993.sql
@@ -1,1 +1,8 @@
 -- https://leetcode.com/problems/friday-purchases-i/description/
+select ceil(extract(day from purchase_date) / 7) as week_of_month,
+       purchase_date,
+       sum(amount_spend) as total_amount
+from Purchases
+where extract(dow from purchase_date) = 5
+group by week_of_month, purchase_date
+order by week_of_month;
