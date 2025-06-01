67c06c7979c591b2869b3d072650c46553adfe69
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-05-31 19:17:43 -0500

    Solve problem 3528

--- src/main/rust/src/bin/problem3528.rs
+++ src/main/rust/src/bin/problem3528.rs
@@ -1,6 +1,26 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/unit-conversion-i/description/
 pub fn base_unit_conversions(conversions: Vec<Vec<i32>>) -> Vec<i32> {
-    todo!()
+    fn dfs(map: &HashMap<i32, Vec<(i32, i32)>>, from: i32, product: i32, answer: &mut Vec<i32>) {
+        answer[from as usize] = product;
+        for (to, c) in map.get(&from).unwrap_or(&vec![]) {
+            dfs(
+                map,
+                *to,
+                ((product as i64 * *c as i64) % 1_000_000_007) as i32,
+                answer,
+            );
+        }
+    }
+
+    let mut map: HashMap<i32, Vec<(i32, i32)>> = HashMap::new();
+    for c in conversions.iter() {
+        map.entry(c[0]).or_insert(vec![]).push((c[1], c[2]));
+    }
+    let mut answer: Vec<i32> = vec![1; conversions.len() + 1];
+    dfs(&map, 0, 1, &mut answer);
+    answer
 }
 
 fn main() {
