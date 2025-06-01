9abe93e0fab21fde19b7db48fdcd4aeeb0f4459e
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-06-01 09:48:06 -0500

    Solve problem 2988

--- src/main/sql/Problem2988.sql
+++ src/main/sql/Problem2988.sql
@@ -1,1 +1,18 @@
 -- https://leetcode.com/problems/manager-of-the-largest-department/description/
+with largest_dep as (
+    select dep_id
+    from Employees
+    group by dep_id
+    having count(*) = (
+        select max(count) as max
+        from (
+            select dep_id, count(*)
+            from Employees
+            group by dep_id
+        )
+    )
+)
+select emp_name as manager_name, dep_id
+from Employees
+where position = 'Manager' and dep_id in (select dep_id from largest_dep)
+order by dep_id;
