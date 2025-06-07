fd8e6fedd3e09785c97bc1562e35d26e33c5db97
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-06-07 09:02:59 -0500

    Skeleton for problem 3571

---
+++ src/main/rust/src/bin/problem3571.rs
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/find-the-shortest-superstring-ii/description/
+pub fn shortest_superstring(s1: String, s2: String) -> String {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        shortest_superstring("aba".to_string(), "bab".to_string())
+    ); // "abab"
+    println!(
+        "{}",
+        shortest_superstring("aa".to_string(), "aaa".to_string())
+    ); // "aaa"
+}
