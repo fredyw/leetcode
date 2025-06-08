20041a8f0ab743bb4b699118b141342b28ac465a
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-06-08 14:02:34 -0500

    Update problem 555

--- src/main/rust/src/bin/problem555.rs
+++ src/main/rust/src/bin/problem555.rs
@@ -11,15 +11,15 @@ pub fn split_looped_string(mut strs: Vec<String>) -> String {
         let rev = strs[i].chars().rev().collect::<String>();
         for s in vec![strs[i].as_str(), rev.as_str()] {
             for j in 0..s.len() {
-                let new_string = &mut s[j..].to_string();
+                let mut new_string = (&s[j..]).to_string();
                 for k in i + 1..strs.len() {
                     new_string.push_str(&strs[k]);
                 }
                 for k in 0..i {
                     new_string.push_str(&strs[k]);
                 }
-                if *new_string > answer {
-                    answer = new_string.clone();
+                if new_string > answer {
+                    answer = new_string;
                 }
             }
         }
@@ -54,4 +54,8 @@ fn main() {
         "{}",
         split_looped_string(vec!["a".to_string(), "b".to_string(), "c".to_string()])
     ); // "cab"
+    println!(
+        "{}",
+        split_looped_string(vec!["yzy".to_string(), "aba".to_string()])
+    ); // "zyabay"
 }
