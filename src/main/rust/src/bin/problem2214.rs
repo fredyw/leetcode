32c3eb7ffc5d960f326bcc1cf89ae45b53fb6a46
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-06-14 02:40:55 -0500

    Skeleton for problem 2214

---
+++ src/main/rust/src/bin/problem2214.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/minimum-health-to-beat-game/description/
+pub fn minimum_health(damage: Vec<i32>, armor: i32) -> i64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", minimum_health(vec![2, 7, 4, 3], 4)); // 13
+    println!("{}", minimum_health(vec![2, 5, 3, 4], 7)); // 10
+    println!("{}", minimum_health(vec![3, 3, 3], 0)); // 10
+}
