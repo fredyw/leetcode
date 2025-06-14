77399e30f370cb0ee517ed1743e6660bbb48879e
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-06-14 02:37:49 -0500

    Solve problem 578

--- src/main/sql/Problem578.sql
+++ src/main/sql/Problem578.sql
@@ -1,1 +1,12 @@
 -- https://leetcode.com/problems/get-highest-answer-rate-question/description/
+select t.question_id as survey_log 
+from (
+    select question_id,
+           sum(case when answer_id is not null then 1 else 0 end) /
+           count(distinct question_id) as answer
+    from SurveyLog
+    where action = 'answer'
+    group by question_id
+) t
+order by t.answer desc
+limit 1;
