2c675c42d385e3eaa51a5c57eed3073fba26d4f1
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-06-15 09:25:28 -0500

    Skeleton for problem 3581

---
+++ src/main/rust/src/bin/problem3581.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/count-odd-letters-from-number/description/
+pub fn count_odd_letters(n: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", count_odd_letters(41)); // 5
+    println!("{}", count_odd_letters(20)); // 5
+}
