37996b99dc24510e5967ba627d38851c56c72a9f
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-06-22 11:16:03 -0500

    Skeleton for problem 3591

---
+++ src/main/rust/src/bin/problem3591.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/check-if-any-element-has-prime-frequency/description/
+pub fn check_prime_frequency(nums: Vec<i32>) -> bool {
+    todo!()
+}
+
+fn main() {
+    println!("{}", check_prime_frequency(vec![1, 2, 3, 4, 5, 4])); // true
+    println!("{}", check_prime_frequency(vec![1, 2, 3, 4, 5])); // false
+    println!("{}", check_prime_frequency(vec![2, 2, 2, 4, 4])); // true
+}
