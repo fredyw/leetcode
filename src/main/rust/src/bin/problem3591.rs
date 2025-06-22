8b7787b5f06eb15327bfe85cbc66c0d81f260a54
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-06-22 15:13:34 -0500

    Solve problem 3591

--- src/main/rust/src/bin/problem3591.rs
+++ src/main/rust/src/bin/problem3591.rs
@@ -1,6 +1,27 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/check-if-any-element-has-prime-frequency/description/
 pub fn check_prime_frequency(nums: Vec<i32>) -> bool {
-    todo!()
+    fn is_prime(n: i32) -> bool {
+        if n == 1 {
+            return false;
+        }
+        if n == 2 {
+            return true;
+        }
+        for i in 2..=(n as f64).sqrt() as i32 {
+            if n % i == 0 {
+                return false;
+            }
+        }
+        true
+    }
+
+    let mut map: HashMap<i32, i32> = HashMap::new();
+    for num in nums.iter() {
+        *map.entry(*num).or_insert(0) += 1;
+    }
+    map.values().any(|&count| is_prime(count))
 }
 
 fn main() {
