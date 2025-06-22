63129c3db381c0f82b937e52d8c122c15ba8fec1
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-06-22 15:16:27 -0500

    Skeleton for problem 3565

---
+++ src/main/rust/src/bin/problem3565.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/sequential-grid-path-cover/description/
+pub fn find_path(grid: Vec<Vec<i32>>, k: i32) -> Vec<Vec<i32>> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", find_path(vec![vec![0, 0, 0], vec![0, 1, 2]], 2)); // [[0,0],[1,0],[1,1],[1,2],[0,2],[0,1]]
+    println!("{:?}", find_path(vec![vec![1, 0, 4], vec![3, 0, 2]], 4)); // []
+}
