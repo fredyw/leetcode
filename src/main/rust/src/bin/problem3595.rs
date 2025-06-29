214c26300f8359c8bea28e2773fd08e0818af95a
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-06-29 11:06:46 -0500

    Skeleton for problem 3595

---
+++ src/main/rust/src/bin/problem3595.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/once-twice
+pub fn once_twice(nums: Vec<i32>) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", once_twice(vec![2, 2, 3, 2, 5, 5, 5, 7, 7])); // [3,7]
+    println!("{:?}", once_twice(vec![4, 4, 6, 4, 9, 9, 9, 6, 8])); // [8,6]
+}
