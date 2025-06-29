e2c85718cb88cbb633fe57ddc994bb1b029dbd24
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-06-29 01:46:43 -0500

    Update problem 3565

--- src/main/rust/src/bin/problem3565.rs
+++ src/main/rust/src/bin/problem3565.rs
@@ -1,5 +1,33 @@
 // https://leetcode.com/problems/sequential-grid-path-cover/description/
 pub fn find_path(grid: Vec<Vec<i32>>, k: i32) -> Vec<Vec<i32>> {
+    fn find_path(
+        grid: &Vec<Vec<i32>>,
+        row: isize,
+        col: isize,
+        k: i32,
+        count: &mut i32,
+        visited: &mut Vec<Vec<bool>>,
+    ) -> bool {
+        let num_rows = grid.len() as isize;
+        let num_cols = if num_rows > 0 { grid[0].len() } else { 0 } as isize;
+        if *count == grid.len() as i32 {
+            return true;
+        }
+        if row < 0
+            || row == num_rows
+            || col < 0
+            || col == num_cols
+            || visited[row as usize][col as usize]
+        {
+            return false;
+        }
+        visited[row as usize][col as usize] = true;
+        for (r, c) in [(-1, 0), (0, 1), (1, 0), (0, -1)] {
+            if find_path(grid, row + r, col + c, k, count, visited) {}
+        }
+        true
+    }
+
     todo!()
 }
 
