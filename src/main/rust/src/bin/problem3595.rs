50203036d348965d5a41182bfb619d32815ae6b3
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-06-29 11:16:38 -0500

    Solve problem 3595

--- src/main/rust/src/bin/problem3595.rs
+++ src/main/rust/src/bin/problem3595.rs
@@ -1,6 +1,20 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/once-twice
 pub fn once_twice(nums: Vec<i32>) -> Vec<i32> {
-    todo!()
+    let mut map: HashMap<i32, i32> = HashMap::new();
+    for num in nums.iter() {
+        *map.entry(*num).or_insert(0) += 1;
+    }
+    let mut answer = vec![0; 2];
+    for (num, count) in map.iter() {
+        if *count == 1 {
+            answer[0] = *num;
+        } else if *count == 2 {
+            answer[1] = *num;
+        }
+    }
+    answer
 }
 
 fn main() {
