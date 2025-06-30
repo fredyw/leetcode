506b43e4ab92668068317196ef2ea286631f6423
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-06-30 00:36:37 -0500

    Update problem 3596

--- src/main/rust/src/bin/problem3596.rs
+++ src/main/rust/src/bin/problem3596.rs
@@ -11,10 +11,10 @@ pub fn min_cost(m: i32, n: i32) -> i32 {
         if row < 0 || col < 0 || row == num_rows || col == num_cols {
             return i32::MAX;
         }
-        if row as usize == dest.0 && col as usize == dest.1 {
-            return 0;
-        }
         let val = (row + 1) * (col + 1);
+        if row as usize == dest.0 && col as usize == dest.1 {
+            return val;
+        }
         let cost = if step % 2 == 0 {
             let left = min_cost(num_rows, num_cols, row, col - 1, dest, step + 1);
             let up = min_cost(num_rows, num_cols, row - 1, col, dest, step + 1);
@@ -31,7 +31,12 @@ pub fn min_cost(m: i32, n: i32) -> i32 {
         }
     }
 
-    min_cost(m, n, 0, 0, (m as usize - 1, n as usize - 1), 1)
+    let cost = min_cost(m, n, 0, 0, (m as usize - 1, n as usize - 1), 1);
+    if cost == i32::MAX {
+        -1
+    } else {
+        cost
+    }
 }
 
 fn main() {
