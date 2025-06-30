7479a17265ca8a1ce73cff18144cab25c5fca4b4
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-06-30 00:31:19 -0500

    Update problem 3596

--- src/main/rust/src/bin/problem3596.rs
+++ src/main/rust/src/bin/problem3596.rs
@@ -1,6 +1,37 @@
 // https://leetcode.com/problems/minimum-cost-path-with-alternating-directions-i/description/
 pub fn min_cost(m: i32, n: i32) -> i32 {
-    todo!()
+    fn min_cost(
+        num_rows: i32,
+        num_cols: i32,
+        row: i32,
+        col: i32,
+        dest: (usize, usize),
+        step: i32,
+    ) -> i32 {
+        if row < 0 || col < 0 || row == num_rows || col == num_cols {
+            return i32::MAX;
+        }
+        if row as usize == dest.0 && col as usize == dest.1 {
+            return 0;
+        }
+        let val = (row + 1) * (col + 1);
+        let cost = if step % 2 == 0 {
+            let left = min_cost(num_rows, num_cols, row, col - 1, dest, step + 1);
+            let up = min_cost(num_rows, num_cols, row - 1, col, dest, step + 1);
+            left.min(up)
+        } else {
+            let right = min_cost(num_rows, num_cols, row, col + 1, dest, step + 1);
+            let down = min_cost(num_rows, num_cols, row + 1, col, dest, step + 1);
+            right.min(down)
+        };
+        if cost == i32::MAX {
+            cost
+        } else {
+            cost + val
+        }
+    }
+
+    min_cost(m, n, 0, 0, (m as usize - 1, n as usize - 1), 1)
 }
 
 fn main() {
