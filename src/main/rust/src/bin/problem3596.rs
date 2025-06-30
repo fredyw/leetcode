e3ffff78223ee7b33fcd0cc1e0383c098b345d8b
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-06-30 00:49:12 -0500

    Solve problem 3596

--- src/main/rust/src/bin/problem3596.rs
+++ src/main/rust/src/bin/problem3596.rs
@@ -1,57 +1,10 @@
 // https://leetcode.com/problems/minimum-cost-path-with-alternating-directions-i/description/
 pub fn min_cost(m: i32, n: i32) -> i32 {
-    fn min_cost(
-        num_rows: i32,
-        num_cols: i32,
-        row: i32,
-        col: i32,
-        dest: (usize, usize),
-        step: i32,
-        visited: &mut Vec<Vec<bool>>,
-    ) -> i32 {
-        if row < 0
-            || col < 0
-            || row == num_rows
-            || col == num_cols
-            || visited[row as usize][col as usize]
-        {
-            return i32::MAX;
-        }
-        let val = (row + 1) * (col + 1);
-        if row as usize == dest.0 && col as usize == dest.1 {
-            return val;
-        }
-        visited[row as usize][col as usize] = true;
-        let cost = if step % 2 == 0 {
-            let left = min_cost(num_rows, num_cols, row, col - 1, dest, step + 1, visited);
-            let up = min_cost(num_rows, num_cols, row - 1, col, dest, step + 1, visited);
-            left.min(up)
-        } else {
-            let right = min_cost(num_rows, num_cols, row, col + 1, dest, step + 1, visited);
-            let down = min_cost(num_rows, num_cols, row + 1, col, dest, step + 1, visited);
-            right.min(down)
-        };
-        visited[row as usize][col as usize] = false;
-        if cost == i32::MAX {
-            cost
-        } else {
-            cost + val
-        }
-    }
-
-    let cost = min_cost(
-        m,
-        n,
-        0,
-        0,
-        (m as usize - 1, n as usize - 1),
-        1,
-        &mut vec![vec![false; n as usize]; m as usize],
-    );
-    if cost == i32::MAX {
-        -1
-    } else {
-        cost
+    match (m, n) {
+        (1, 1) => 1,
+        (1, 2) => 3,
+        (2, 1) => 3,
+        _ => -1,
     }
 }
 
@@ -59,4 +12,5 @@ fn main() {
     println!("{}", min_cost(1, 1)); // 1
     println!("{}", min_cost(2, 1)); // 3
     println!("{}", min_cost(2, 2)); // -1
+    println!("{}", min_cost(1000000, 1000000)); // -1
 }
