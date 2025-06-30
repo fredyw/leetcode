930c550ad319d9c7156a64a2246c1651926239bf
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-06-29 19:17:51 -0500

    Skeleton for problem 3596

---
+++ src/main/rust/src/bin/problem3596.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/minimum-cost-path-with-alternating-directions-i/description/
+pub fn min_cost(m: i32, n: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", min_cost(1, 1)); // 1
+    println!("{}", min_cost(2, 1)); // 3
+}
