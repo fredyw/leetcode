673decae89abe14a1b29de0a8169d97d77858120
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-07-04 00:05:06 -0500

    Solve problem 1699

--- src/main/sql/Problem1699.sql
+++ src/main/sql/Problem1699.sql
@@ -1,1 +1,11 @@
 -- https://leetcode.com/problems/number-of-calls-between-two-persons/description/
+with t as (
+    select
+        case when from_id < to_id then from_id else to_id end as person1,
+        case when from_id > to_id then from_id else to_id end as person2,
+        duration
+    from Calls
+)
+select person1, person2, count(*) as call_count, sum(duration) as total_duration
+from t
+group by person1, person2;
