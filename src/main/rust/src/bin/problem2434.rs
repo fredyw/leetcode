bc594eb8cf9b7c924b18c0f92797f58cf889600b
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-07-05 16:23:35 -0500

    Skeleton for problem 2434

---
+++ src/main/rust/src/bin/problem2434.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/using-a-robot-to-print-the-lexicographically-smallest-string/description/
+pub fn robot_with_string(s: String) -> String {
+    todo!()
+}
+
+fn main() {
+    println!("{}", robot_with_string("zza".to_string())); // "azz"
+    println!("{}", robot_with_string("bac".to_string())); // "abc"
+    println!("{}", robot_with_string("bdda".to_string())); // "addb"
+}
