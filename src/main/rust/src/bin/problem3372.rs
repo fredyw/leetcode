f08c5e3a6852e6c157e33a63b18d5fe1a8b3611f
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-07-05 15:49:22 -0500

    Update problem 3372

--- src/main/rust/src/bin/problem3372.rs
+++ src/main/rust/src/bin/problem3372.rs
@@ -2,28 +2,7 @@ use std::collections::{HashMap, HashSet};
 
 // https://leetcode.com/problems/maximize-the-number-of-target-nodes-after-connecting-trees-i/description/
 pub fn max_target_nodes(edges1: Vec<Vec<i32>>, edges2: Vec<Vec<i32>>, k: i32) -> Vec<i32> {
-    #[derive(Eq, PartialEq, Hash, Debug)]
-    struct Node {
-        tree: i32,
-        value: i32,
-    }
-
-    impl Node {
-        fn one(value: i32) -> Node {
-            Node { tree: 1, value }
-        }
-
-        fn two(value: i32) -> Node {
-            Node { tree: 2, value }
-        }
-    }
-
-    fn count<'a>(
-        graph: &'a HashMap<Node, Vec<Node>>,
-        node: &'a Node,
-        k: i32,
-        visited: &mut HashSet<&'a Node>,
-    ) -> i32 {
+    fn count(graph: &HashMap<i32, Vec<i32>>, node: i32, k: i32, visited: &mut HashSet<i32>) -> i32 {
         if k == 0 {
             return 0;
         }
@@ -32,36 +11,40 @@ pub fn max_target_nodes(edges1: Vec<Vec<i32>>, edges2: Vec<Vec<i32>>, k: i32) ->
         if let Some(v) = graph.get(&node) {
             for adj in v.iter() {
                 if !visited.contains(adj) {
-                    c += count(graph, adj, k - 1, visited) + 1;
+                    c += count(graph, *adj, k - 1, visited) + 1;
                 }
             }
         }
         c
     }
 
-    let mut graph1: HashMap<Node, Vec<Node>> = HashMap::new();
+    let mut graph1: HashMap<i32, Vec<i32>> = HashMap::new();
     for edge in edges1 {
-        graph1
-            .entry(Node::one(edge[0]))
-            .or_insert(vec![])
-            .push(Node::one(edge[1]));
-        graph1
-            .entry(Node::one(edge[1]))
-            .or_insert(vec![])
-            .push(Node::one(edge[0]));
+        graph1.entry(edge[0]).or_insert(vec![]).push(edge[1]);
+        graph1.entry(edge[1]).or_insert(vec![]).push(edge[0]);
     }
-    let mut graph2: HashMap<Node, Vec<Node>> = HashMap::new();
+    let mut counts1: Vec<i32> = vec![0; graph1.len()];
+    for node in graph1.keys() {
+        counts1[*node as usize] = count(&graph1, *node, k, &mut HashSet::new()) + 1;
+    }
+    let mut graph2: HashMap<i32, Vec<i32>> = HashMap::new();
     for edge in edges2 {
-        graph2
-            .entry(Node::two(edge[0]))
-            .or_insert(vec![])
-            .push(Node::two(edge[1]));
-        graph2
-            .entry(Node::two(edge[1]))
-            .or_insert(vec![])
-            .push(Node::two(edge[0]));
+        graph2.entry(edge[0]).or_insert(vec![]).push(edge[1]);
+        graph2.entry(edge[1]).or_insert(vec![]).push(edge[0]);
     }
-    vec![]
+    let mut counts2: Vec<i32> = vec![0; graph2.len()];
+    for node in graph2.keys() {
+        counts2[*node as usize] = count(&graph2, *node, k - 1, &mut HashSet::new()) + 1;
+    }
+    let mut answer: Vec<i32> = vec![0; counts1.len()];
+    for (i, c1) in counts1.iter().enumerate() {
+        let mut max = 0;
+        for (_, c2) in counts2.iter().enumerate() {
+            max = max.max(*c1 + *c2);
+        }
+        answer[i] = max;
+    }
+    answer
 }
 
 fn main() {
@@ -81,12 +64,16 @@ fn main() {
             2
         )
     ); // [9,7,9,8,8]
-       // println!(
-       //     "{:?}",
-       //     max_target_nodes(
-       //         vec![vec![0, 1], vec![0, 2], vec![0, 3], vec![0, 4]],
-       //         vec![vec![0, 1], vec![1, 2], vec![2, 3]],
-       //         1
-       //     )
-       // ); // [9,7,9,8,8]
+    println!(
+        "{:?}",
+        max_target_nodes(
+            vec![vec![0, 1], vec![0, 2], vec![0, 3], vec![0, 4]],
+            vec![vec![0, 1], vec![1, 2], vec![2, 3]],
+            1
+        )
+    ); // [6,3,3,3,3]
+    println!(
+        "{:?}",
+        max_target_nodes(vec![vec![0, 1]], vec![vec![0, 1]], 1)
+    ); // [1,1]
 }
