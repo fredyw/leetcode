b03c65e676ae7b3e45719c73b338b8e478f2498c
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-07-06 08:55:32 -0500

    Skeleton for problem 3602

---
+++ src/main/rust/src/bin/problem3602.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/hexadecimal-and-hexatrigesimal-conversion/description/
+pub fn concat_hex36(n: i32) -> String {
+    todo!()
+}
+
+fn main() {
+    println!("{}", concat_hex36(13)); // "A91P1"
+    println!("{}", concat_hex36(36)); // "5101000"
+}
