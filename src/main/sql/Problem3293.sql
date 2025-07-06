a90a91f07444e030a2efe147596ebd26b9fb9274
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-07-06 11:22:01 -0500

    Solve problem 3293

--- src/main/sql/Problem3293.sql
+++ src/main/sql/Problem3293.sql
@@ -1,1 +1,9 @@
 -- https://leetcode.com/problems/calculate-product-final-price/description/
+select p.product_id,
+       case
+           when d.discount is null then p.price
+           else p.price - ((p.price * d.discount) / 100.0)
+           end as final_price,
+       p.category
+from Products p left join Discounts d on p.category = d.category
+order by p.product_id;
