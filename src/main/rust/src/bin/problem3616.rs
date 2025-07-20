b3b3f003f34f13bf1834ad8fa9946196e2ec8869
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-07-20 01:04:03 -0500

    Skeleton for problem 3616

---
+++ src/main/rust/src/bin/problem3616.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/number-of-student-replacements/description/
+pub fn total_replacements(ranks: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", total_replacements(vec![4, 1, 2])); // 1
+    println!("{}", total_replacements(vec![2, 2, 3])); // 0
+}
