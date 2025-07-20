a7d27d42e2c6d32e0a35d5952cb86cc5630399c2
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-07-20 01:08:04 -0500

    Solve problem 3616

--- src/main/rust/src/bin/problem3616.rs
+++ src/main/rust/src/bin/problem3616.rs
@@ -1,6 +1,14 @@
 // https://leetcode.com/problems/number-of-student-replacements/description/
 pub fn total_replacements(ranks: Vec<i32>) -> i32 {
-    todo!()
+    let mut answer = 0;
+    let mut min = ranks[0];
+    for i in 1..ranks.len() {
+        if min > ranks[i] {
+            min = ranks[i];
+            answer += 1;
+        }
+    }
+    answer
 }
 
 fn main() {
