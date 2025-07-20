dbb23984e9080f1efd0b7daec27dd375ad8c6922
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-07-20 10:15:06 -0500

    Skeleton for problem 3622

---
+++ src/main/rust/src/bin/problem3622.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/check-divisibility-by-digit-sum-and-product/description/
+pub fn check_divisibility(n: i32) -> bool {
+    todo!()
+}
+
+fn main() {
+    println!("{}", check_divisibility(99)); // true
+    println!("{}", check_divisibility(23)); // false
+}
