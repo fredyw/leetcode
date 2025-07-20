3a816b05087a1c54b69743d5480e5f569f293907
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-07-20 10:20:09 -0500

    Solve problem 3622

--- src/main/rust/src/bin/problem3622.rs
+++ src/main/rust/src/bin/problem3622.rs
@@ -1,6 +1,24 @@
 // https://leetcode.com/problems/check-divisibility-by-digit-sum-and-product/description/
 pub fn check_divisibility(n: i32) -> bool {
-    todo!()
+    fn digit_sum(mut n: i32) -> i32 {
+        let mut sum = 0;
+        while n > 0 {
+            sum += n % 10;
+            n /= 10;
+        }
+        sum
+    }
+
+    fn digit_product(mut n: i32) -> i32 {
+        let mut product = 1;
+        while n > 0 {
+            product *= n % 10;
+            n /= 10;
+        }
+        product
+    }
+
+    n % (digit_sum(n) + digit_product(n)) == 0
 }
 
 fn main() {
