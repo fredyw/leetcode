f53526fe1be0f9fc222ba646817567e4e451d63e
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-07-28 00:58:04 -0500

    Solve problem 3631

--- src/main/rust/src/bin/problem3631.rs
+++ src/main/rust/src/bin/problem3631.rs
@@ -1,6 +1,16 @@
 // https://leetcode.com/problems/sort-threats-by-severity-and-exploitability/description/
-pub fn sort_threats(threats: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-    todo!()
+pub fn sort_threats(mut threats: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+    threats.sort_by(|a, b| {
+        let score_a = 2 * a[1] as i64 + a[2] as i64;
+        let score_b = 2 * b[1] as i64 + b[2] as i64;
+        let cmp = score_b.cmp(&score_a);
+        if cmp == std::cmp::Ordering::Equal {
+            a[0].cmp(&b[0])
+        } else {
+            cmp
+        }
+    });
+    threats
 }
 
 fn main() {
