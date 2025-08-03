4c5746a544c549d19860c3101aa7db1f631f6d7b
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-08-03 18:46:30 -0500

    Solve problem 3633

--- src/main/rust/src/bin/problem3633.rs
+++ src/main/rust/src/bin/problem3633.rs
@@ -5,7 +5,38 @@ pub fn earliest_finish_time(
     water_start_time: Vec<i32>,
     water_duration: Vec<i32>,
 ) -> i32 {
-    todo!()
+    fn earliest_finish_time(
+        a_start_time: &Vec<i32>,
+        a_duration: &Vec<i32>,
+        b_start_time: &Vec<i32>,
+        b_duration: &Vec<i32>,
+    ) -> i32 {
+        let mut min_time = i32::MAX;
+        for i in 0..a_start_time.len() {
+            let time = a_start_time[i] + a_duration[i];
+            for j in 0..b_start_time.len() {
+                if time > b_start_time[j] {
+                    min_time = min_time.min(time + b_duration[j]);
+                } else {
+                    min_time = min_time.min(b_start_time[j] + b_duration[j]);
+                }
+            }
+        }
+        min_time
+    }
+
+    earliest_finish_time(
+        &land_start_time,
+        &land_duration,
+        &water_start_time,
+        &water_duration,
+    )
+    .min(earliest_finish_time(
+        &water_start_time,
+        &water_duration,
+        &land_start_time,
+        &land_duration,
+    ))
 }
 
 fn main() {
