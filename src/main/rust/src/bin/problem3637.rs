25bfc32098bfaace21392e169b714456aa6a435a
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-08-03 18:13:02 -0500

    Solve problem 3637

--- src/main/rust/src/bin/problem3637.rs
+++ src/main/rust/src/bin/problem3637.rs
@@ -1,29 +1,19 @@
 // https://leetcode.com/problems/trionic-array-i/description/
 pub fn is_trionic(nums: Vec<i32>) -> bool {
+    if nums[0] >= nums[1] {
+        return false;
+    }
     let mut group = 1;
-    let mut count = 0;
     let mut increase = true;
     for i in 0..nums.len() - 1 {
         if nums[i] == nums[i + 1] {
             return false;
         }
-        if increase && nums[i] > nums[i + 1] && count == 0 {
-            return false;
-        }
-        if !increase && nums[i] < nums[i + 1] && count == 0 {
-            return false;
-        }
-        if increase && nums[i] < nums[i + 1] {
-            count += 1;
-        } else if increase && nums[i] > nums[i + 1] {
+        if increase && nums[i] > nums[i + 1] {
             increase = !increase;
-            count = 0;
             group += 1;
-        } else if !increase && nums[i] > nums[i + 1] {
-            count += 1;
         } else if !increase && nums[i] < nums[i + 1] {
             increase = !increase;
-            count = 0;
             group += 1;
         }
     }
@@ -31,9 +21,9 @@ pub fn is_trionic(nums: Vec<i32>) -> bool {
 }
 
 fn main() {
-    // println!("{}", is_trionic(vec![1, 3, 5, 4, 2, 6])); // true
-    // println!("{}", is_trionic(vec![2, 1, 3])); // false
-    // println!("{}", is_trionic(vec![1, 3, 5, 4, 2, 6, 4])); // false
-    // println!("{}", is_trionic(vec![9, 1, 7])); // false
-    println!("{}", is_trionic(vec![5, 9, 1, 7])); // false
+    println!("{}", is_trionic(vec![1, 3, 5, 4, 2, 6])); // true
+    println!("{}", is_trionic(vec![2, 1, 3])); // false
+    println!("{}", is_trionic(vec![1, 3, 5, 4, 2, 6, 4])); // false
+    println!("{}", is_trionic(vec![9, 1, 7])); // false
+    println!("{}", is_trionic(vec![5, 9, 1, 7])); // true
 }
