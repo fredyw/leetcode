29d1573159c3ffd35e8b5f2726a3549460b015d2
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-08-16 00:47:13 -0500

    Skeleton for problem 3341

---
+++ src/main/rust/src/bin/problem3341.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/find-minimum-time-to-reach-last-room-i/description/
+pub fn min_time_to_reach(move_time: Vec<Vec<i32>>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", min_time_to_reach(vec![vec![0, 4], vec![4, 4]])); // 6
+    println!("{}", min_time_to_reach(vec![vec![0, 0, 0], vec![0, 0, 0]])); // 3
+    println!("{}", min_time_to_reach(vec![vec![0, 1], vec![1, 2]])); // 3
+}
