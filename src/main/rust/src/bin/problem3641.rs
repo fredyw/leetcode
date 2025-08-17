a84d0e637fa6139e5c511329e6f4c3c49a0b4acd
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-08-17 11:36:46 -0500

    Update problem 3641

--- src/main/rust/src/bin/problem3641.rs
+++ src/main/rust/src/bin/problem3641.rs
@@ -7,4 +7,8 @@ fn main() {
     println!("{}", longest_subarray(vec![1, 2, 3, 1, 2, 3, 4], 2)); // 6
     println!("{}", longest_subarray(vec![1, 1, 1, 1, 1], 4)); // 5
     println!("{}", longest_subarray(vec![1, 1, 1, 1, 1], 0)); // 1
+    println!(
+        "{}",
+        longest_subarray(vec![1, 2, 1, 1, 1, 1, 1, 1, 3, 1, 2, 3, 4], 2)
+    ); // 11
 }
