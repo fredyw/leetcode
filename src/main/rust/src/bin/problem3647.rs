76ac4c9194826085e966255da0fe5bfd553eafb1
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-08-24 01:14:53 -0500

    Skeleton for problem 3647

---
+++ src/main/rust/src/bin/problem3647.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/maximum-weight-in-two-bags/description/
+pub fn max_weight(weights: Vec<i32>, w1: i32, w2: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", max_weight(vec![1, 4, 3, 2], 5, 4)); // 9
+    println!("{}", max_weight(vec![3, 6, 4, 8], 9, 7)); // 15
+    println!("{}", max_weight(vec![5, 7], 2, 3)); // 0
+}
