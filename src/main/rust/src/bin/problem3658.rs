916fe16ec7a68a351b6e60e224ea3492712d3ad5
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-08-24 01:12:43 -0500

    Solve problem 3658

--- src/main/rust/src/bin/problem3658.rs
+++ src/main/rust/src/bin/problem3658.rs
@@ -1,6 +1,26 @@
 // https://leetcode.com/problems/gcd-of-odd-and-even-sums/description/
 pub fn gcd_of_odd_even_sums(n: i32) -> i32 {
-    todo!()
+    fn gcd(a: i32, b: i32) -> i32 {
+        if b == 0 {
+            a
+        } else {
+            gcd(b, a % b)
+        }
+    }
+
+    let mut sum_odd = 0;
+    let mut x = 1;
+    for _ in 0..n {
+        sum_odd += x;
+        x += 2;
+    }
+    let mut sum_even = 0;
+    let mut x = 2;
+    for _ in 0..n {
+        sum_even += x;
+        x += 2;
+    }
+    gcd(sum_odd, sum_even)
 }
 
 fn main() {
