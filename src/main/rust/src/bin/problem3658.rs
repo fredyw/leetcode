56cc7369d4a9c411a28977d37e09c4fb9d275c02
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-08-24 01:02:24 -0500

    Skeleton for problem 3658

---
+++ src/main/rust/src/bin/problem3658.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/gcd-of-odd-and-even-sums/description/
+pub fn gcd_of_odd_even_sums(n: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", gcd_of_odd_even_sums(4)); // 4
+    println!("{}", gcd_of_odd_even_sums(5)); // 5
+}
