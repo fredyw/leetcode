97d79547f46e36cb46e8a4f46e72eda67ab5b4fa
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-08-24 17:42:31 -0500

    Skeleton for problem 3662

---
+++ src/main/rust/src/bin/problem3662.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/filter-characters-by-frequency/description/
+pub fn filter_characters(s: String, k: i32) -> String {
+    todo!()
+}
+
+fn main() {
+    println!("{}", filter_characters("aadbbcccca".to_string(), 3)); // "dbb"
+    println!("{}", filter_characters("xyz".to_string(), 2)); // "xyz"
+}
