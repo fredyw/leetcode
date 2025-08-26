83709a66c82c04b5e9d952fb3cd5ec41d5a68a35
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-08-25 22:59:23 -0500

    Update problem 3647

--- src/main/rust/src/bin/problem3647.rs
+++ src/main/rust/src/bin/problem3647.rs
@@ -1,10 +1,25 @@
 // https://leetcode.com/problems/maximum-weight-in-two-bags/description/
 pub fn max_weight(weights: Vec<i32>, w1: i32, w2: i32) -> i32 {
-    fn max_weight(weights: &Vec<i32>, w1: i32, w2: i32) -> i32 {
-        todo!()
+    fn max_weight(weights: &Vec<i32>, w1: i32, w2: i32, index: usize) -> i32 {
+        if index == weights.len() {
+            return 0;
+        }
+        let a = max_weight(weights, w1, w2, index + 1);
+        let b = if w1 - weights[index] > 0 {
+            max_weight(weights, w1 - weights[index], w2, index + 1) + weights[index]
+        } else {
+            0
+        };
+        let c = if w2 - weights[index] > 0 {
+            max_weight(weights, w1, w2 - weights[index], index + 1) + weights[index]
+        } else {
+            0
+        };
+        let max = a.max(b).max(c);
+        max
     }
 
-    max_weight(&weights, w1, w2)
+    max_weight(&weights, w1, w2, 0)
 }
 
 fn main() {
