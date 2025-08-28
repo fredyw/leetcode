591bdfbafe8817f5ebc724fb7a23eb78f175aa98
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-08-27 21:29:15 -0500

    Skeleton for problem 3085

---
+++ src/main/rust/src/bin/problem3085.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/minimum-deletions-to-make-string-k-special/description/
+pub fn minimum_deletions(word: String, k: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", minimum_deletions("aabcaba".to_string(), 0)); // 3
+    println!("{}", minimum_deletions("dabdcbdcdcd".to_string(), 2)); // 2
+    println!("{}", minimum_deletions("aaabaaa".to_string(), 2)); // 1
+}
