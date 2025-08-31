a5e8465366b6b946b47bbcd44b0370366b40c282
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-08-31 09:51:43 -0500

    Solve problem 3663

--- src/main/rust/src/bin/problem3663.rs
+++ src/main/rust/src/bin/problem3663.rs
@@ -1,6 +1,25 @@
+use std::cmp::Ordering;
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/find-the-least-frequent-digit/description/
 pub fn get_least_frequent_digit(n: i32) -> i32 {
-    todo!()
+    let mut map: HashMap<char, i32> = HashMap::new();
+    for x in n.to_string().chars() {
+        *map.entry(x).or_insert(0) += 1;
+    }
+    n.to_string()
+        .chars()
+        .min_by(|x, y| {
+            let cmp = map.get(x).cmp(&map.get(y));
+            if cmp == Ordering::Equal {
+                x.cmp(y)
+            } else {
+                cmp
+            }
+        })
+        .unwrap()
+        .to_digit(10)
+        .unwrap() as i32
 }
 
 fn main() {
