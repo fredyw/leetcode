f881dd2cc76d041e32b43fce1059564b56abc31e
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-08-31 09:27:52 -0500

    Skeleton for problem 3668

---
+++ src/main/rust/src/bin/problem3668.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/restore-finishing-order/description/
+pub fn recover_order(order: Vec<i32>, friends: Vec<i32>) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", recover_order(vec![3, 1, 2, 5, 4], vec![1, 3, 4])); // [3,1,4]
+    println!("{:?}", recover_order(vec![1, 4, 5, 3, 2], vec![2, 5])); // [5,2]
+}
