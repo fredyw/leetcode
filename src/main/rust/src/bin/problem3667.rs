3358692b0ef9a938ab5c5802e335392f11a7e6ad
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-08-31 09:25:14 -0500

    Skeleton for problem 3667

---
+++ src/main/rust/src/bin/problem3667.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/sort-array-by-absolute-value/
+pub fn sort_by_absolute_value(nums: Vec<i32>) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", sort_by_absolute_value(vec![3, -1, -4, 1, 5])); // [-1,1,3,-4,5]
+    println!("{:?}", sort_by_absolute_value(vec![-100, 100])); // [-100,100]
+}
