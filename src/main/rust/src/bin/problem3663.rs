dc76558156b51fe5c69ac63dea531dfa9ddde7c8
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-08-31 09:39:01 -0500

    Skeleton for problem 3663

---
+++ src/main/rust/src/bin/problem3663.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/find-the-least-frequent-digit/description/
+pub fn get_least_frequent_digit(n: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", get_least_frequent_digit(1553322)); // 1
+    println!("{}", get_least_frequent_digit(723344511)); // 2
+}
