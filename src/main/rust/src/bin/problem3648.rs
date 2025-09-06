6e29c744c291c46aa691674bfea9aec86f68d52f
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-09-06 13:06:42 -0500

    Skeleton for problem 3648

---
+++ src/main/rust/src/bin/problem3648.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/minimum-sensors-to-cover-grid/description/
+pub fn min_sensors(n: i32, m: i32, k: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", min_sensors(5, 5, 1)); // 4
+    println!("{}", min_sensors(2, 2, 2)); // 1
+}
