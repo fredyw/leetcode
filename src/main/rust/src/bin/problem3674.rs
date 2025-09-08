9bc1b0128dd35ee5a53e53da40a3d7ec51dbff60
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-09-07 23:38:30 -0500

    Update problem 3674

--- src/main/rust/src/bin/problem3674.rs
+++ src/main/rust/src/bin/problem3674.rs
@@ -1,9 +1,24 @@
 // https://leetcode.com/problems/minimum-operations-to-equalize-array/description/
 pub fn min_operations(nums: Vec<i32>) -> i32 {
-    todo!()
+    let bitwise_and = nums.iter().copied().reduce(|acc, num| acc & num).unwrap();
+    let mut answer = 0;
+    let mut i = 0;
+    while i < nums.len() {
+        if nums[i] != bitwise_and {
+            answer += 1;
+            while i < nums.len() && nums[i] != bitwise_and {
+                i += 1;
+            }
+        } else {
+            i += 1;
+        }
+    }
+    answer
 }
 
 fn main() {
     println!("{}", min_operations(vec![1, 2])); // 1
     println!("{}", min_operations(vec![5, 5, 5])); // 0
+    println!("{}", min_operations(vec![8, 1, 10])); // 1
+    println!("{}", min_operations(vec![37, 4, 125])); // 1
 }
