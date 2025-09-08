79ab8153dfd3ad66cb62e1ad66f82fa2d963f62a
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-09-07 20:12:42 -0500

    Skeleton for problem 3647

---
+++ src/main/rust/src/bin/problem3674.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/minimum-operations-to-equalize-array/description/
+pub fn min_operations(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", min_operations(vec![1, 2])); // 1
+    println!("{}", min_operations(vec![5, 5, 5])); // 0
+}
