01ed984eeb5af2883c0e0281bd8fd91ce2310954
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-09-13 15:36:32 -0500

    Update problem 3648

--- src/main/rust/src/bin/problem3648.rs
+++ src/main/rust/src/bin/problem3648.rs
@@ -1,9 +1,24 @@
 // https://leetcode.com/problems/minimum-sensors-to-cover-grid/description/
 pub fn min_sensors(n: i32, m: i32, k: i32) -> i32 {
-    todo!()
+    let mut answer = 0;
+    let mut row = k;
+    while row < n {
+        let mut col = k;
+        while col < m {
+            col += k + 1;
+            answer += 1;
+        }
+        row += k + 1;
+    }
+    if answer == 0 {
+        1
+    } else {
+        answer
+    }
 }
 
 fn main() {
-    println!("{}", min_sensors(5, 5, 1)); // 4
-    println!("{}", min_sensors(2, 2, 2)); // 1
+    // println!("{}", min_sensors(5, 5, 1)); // 4
+    // println!("{}", min_sensors(2, 2, 2)); // 1
+    println!("{}", min_sensors(6, 5, 1)); // 4
 }
