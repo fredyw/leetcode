a5c1aa07f310fb3b17c15498240e9627ca906001
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-09-13 16:38:53 -0500

    Update problem 3648

--- src/main/rust/src/bin/problem3648.rs
+++ src/main/rust/src/bin/problem3648.rs
@@ -5,22 +5,26 @@ pub fn min_sensors(n: i32, m: i32, k: i32) -> i32 {
     while row < n {
         let mut col = k;
         while col < m {
-            println!("({row}, {col})");
-            col += k + 1;
+            // println!("({row}, {col})");
+            col += (k * 2) + 1;
             answer += 1;
         }
-        row += k + 1;
+        if col - k < m {
+            answer += 1;
+        }
+        row += (k * 2) + 1;
     }
-    if answer == 0 {
-        1
+    if row - k < n {
+        answer + 1
     } else {
         answer
     }
 }
 
 fn main() {
-    // println!("{}", min_sensors(5, 5, 1)); // 4
-    // println!("{}", min_sensors(2, 2, 2)); // 1
+    println!("{}", min_sensors(5, 5, 1)); // 4
+    println!("{}", min_sensors(2, 2, 2)); // 1
     println!("{}", min_sensors(6, 5, 1)); // 4
-                                          // println!("{}", min_sensors(44, 69, 8)); // 15
+    println!("{}", min_sensors(44, 69, 8)); // 15
+    println!("{}", min_sensors(88, 26, 10)); // 10
 }
