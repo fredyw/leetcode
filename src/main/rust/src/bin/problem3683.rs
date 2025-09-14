7101d125eeced0892decc381158f4fe0143fcc4e
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-09-14 17:23:53 -0500

    Skeleton for problem 3683

---
+++ src/main/rust/src/bin/problem3683.rs
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/earliest-time-to-finish-one-task/description/
+pub fn earliest_time(tasks: Vec<Vec<i32>>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", earliest_time(vec![vec![1, 6], vec![2, 3]])); // 5
+    println!(
+        "{}",
+        earliest_time(vec![vec![100, 100], vec![100, 100], vec![100, 100]])
+    ); // 200
+}
