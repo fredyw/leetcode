c93a7a6847c0253ae0381f3ec57c0f4c480a321b
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-09-14 17:22:15 -0500

    Solve problem 3684

--- src/main/rust/src/bin/problem3684.rs
+++ src/main/rust/src/bin/problem3684.rs
@@ -1,6 +1,8 @@
 // https://leetcode.com/problems/maximize-sum-of-at-most-k-distinct-elements/description/
-pub fn max_k_distinct(nums: Vec<i32>, k: i32) -> Vec<i32> {
-    todo!()
+pub fn max_k_distinct(mut nums: Vec<i32>, k: i32) -> Vec<i32> {
+    nums.sort_by(|a, b| b.cmp(a));
+    nums.dedup();
+    nums[0..(k as usize).min(nums.len())].to_vec()
 }
 
 fn main() {
