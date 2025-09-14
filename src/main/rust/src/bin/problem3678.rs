d13e158b0ef5ccfb02e5d12f55e11841063e7524
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-09-14 17:28:04 -0500

    Skeleton for problem 3678

---
+++ src/main/rust/src/bin/problem3678.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/smallest-absent-positive-greater-than-average/description/
+pub fn smallest_absent(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", smallest_absent(vec![3, 5])); // 6
+    println!("{}", smallest_absent(vec![-1, 1, 2])); // 3
+    println!("{}", smallest_absent(vec![4, -1])); // 2
+}
