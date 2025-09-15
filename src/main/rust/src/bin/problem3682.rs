b60257f6eb8e1d6264fa424c2026d1bfd5ec9acb
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-09-14 23:41:56 -0500

    Skeleton for problem 3682

---
+++ src/main/rust/src/bin/problem3682.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/minimum-index-sum-of-common-elements/description/
+pub fn minimum_sum(nums1: Vec<i32>, nums2: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", minimum_sum(vec![3, 2, 1], vec![1, 3, 1])); // 1
+    println!("{}", minimum_sum(vec![5, 1, 2], vec![2, 1, 3])); // 2
+    println!("{}", minimum_sum(vec![6, 4], vec![7, 8])); // -1
+}
