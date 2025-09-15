615baf8e6a98a6b0a562527fd087ea3403b2e1b4
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-09-14 19:53:48 -0500

    Solve problem 3678

--- src/main/rust/src/bin/problem3678.rs
+++ src/main/rust/src/bin/problem3678.rs
@@ -3,7 +3,10 @@ use std::collections::HashSet;
 // https://leetcode.com/problems/smallest-absent-positive-greater-than-average/description/
 pub fn smallest_absent(nums: Vec<i32>) -> i32 {
     let set = nums.iter().copied().collect::<HashSet<i32>>();
-    let mut avg = (nums.iter().sum::<i32>() as f32 / nums.len() as f32).ceil() as i32;
+    let mut avg = (nums.iter().sum::<i32>() / nums.len() as i32) + 1;
+    if avg <= 0 {
+        avg = 1;
+    }
     while set.contains(&avg) {
         avg += 1;
     }
@@ -14,4 +17,8 @@ fn main() {
     println!("{}", smallest_absent(vec![3, 5])); // 6
     println!("{}", smallest_absent(vec![-1, 1, 2])); // 3
     println!("{}", smallest_absent(vec![4, -1])); // 2
+    println!("{}", smallest_absent(vec![89])); // 90
+    println!("{}", smallest_absent(vec![-4, -3, -2])); // 1
+    println!("{}", smallest_absent(vec![-34])); // 1
+    println!("{}", smallest_absent(vec![1, -39, 9])); // 2
 }
