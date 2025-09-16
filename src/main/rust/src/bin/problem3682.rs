e49159ee379e68752e1774398353e08af5b48524
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-09-16 00:39:56 -0500

    Solve problem 3682

--- src/main/rust/src/bin/problem3682.rs
+++ src/main/rust/src/bin/problem3682.rs
@@ -1,6 +1,24 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/minimum-index-sum-of-common-elements/description/
 pub fn minimum_sum(nums1: Vec<i32>, nums2: Vec<i32>) -> i32 {
-    todo!()
+    let mut map: HashMap<i32, usize> = HashMap::new();
+    for (i, num) in nums2.iter().enumerate() {
+        if !map.contains_key(&num) {
+            map.insert(*num, i);
+        }
+    }
+    let mut answer = i32::MAX;
+    for (i, num) in nums1.iter().enumerate() {
+        if let Some(j) = map.get(num) {
+            answer = answer.min(i as i32 + *j as i32);
+        }
+    }
+    if answer == i32::MAX {
+        -1
+    } else {
+        answer
+    }
 }
 
 fn main() {
