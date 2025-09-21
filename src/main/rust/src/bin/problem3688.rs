968ee820f76a0a55d5d4d6773b38ac6f4da5631f
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-09-21 01:20:34 -0500

    Skeleton for problem 3688

---
+++ src/main/rust/src/bin/problem3688.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/bitwise-or-of-even-numbers-in-an-array/description/
+pub fn even_number_bitwise_o_rs(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", even_number_bitwise_o_rs(vec!(1, 2, 3, 4, 5, 6))); // 6
+    println!("{}", even_number_bitwise_o_rs(vec!(7, 9, 11))); // 6
+    println!("{}", even_number_bitwise_o_rs(vec!(1, 8, 16))); // 24
+}
