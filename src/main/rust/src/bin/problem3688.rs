1faa1c8c924a8565ca85bfddd35a4c6813c037b1
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-09-21 01:21:25 -0500

    Solve problem 3688

--- src/main/rust/src/bin/problem3688.rs
+++ src/main/rust/src/bin/problem3688.rs
@@ -1,10 +1,12 @@
 // https://leetcode.com/problems/bitwise-or-of-even-numbers-in-an-array/description/
 pub fn even_number_bitwise_o_rs(nums: Vec<i32>) -> i32 {
-    todo!()
+    nums.into_iter()
+        .filter(|&n| n & 1 == 0)
+        .fold(0, |acc, n| acc | n)
 }
 
 fn main() {
     println!("{}", even_number_bitwise_o_rs(vec!(1, 2, 3, 4, 5, 6))); // 6
-    println!("{}", even_number_bitwise_o_rs(vec!(7, 9, 11))); // 6
+    println!("{}", even_number_bitwise_o_rs(vec!(7, 9, 11))); // 0
     println!("{}", even_number_bitwise_o_rs(vec!(1, 8, 16))); // 24
 }
