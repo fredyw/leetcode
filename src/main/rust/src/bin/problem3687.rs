19a949d9fe5d7054fafd49719aad1a817d843f84
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-09-21 01:18:53 -0500

    Solve problem 3687

--- src/main/rust/src/bin/problem3687.rs
+++ src/main/rust/src/bin/problem3687.rs
@@ -1,6 +1,17 @@
 // https://leetcode.com/problems/library-late-fee-calculator/description/
 pub fn late_fee(days_late: Vec<i32>) -> i32 {
-    todo!()
+    days_late
+        .into_iter()
+        .map(|day| {
+            if day == 1 {
+                1
+            } else if day <= 5 {
+                day * 2
+            } else {
+                day * 3
+            }
+        })
+        .sum()
 }
 
 fn main() {
