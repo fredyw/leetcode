9c9aef345a3fff6fe294645b5de47e4f62383a64
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-09-21 01:15:48 -0500

    Skeleton for problem 3687

---
+++ src/main/rust/src/bin/problem3687.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/library-late-fee-calculator/description/
+pub fn late_fee(days_late: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", late_fee(vec![5, 1, 7])); // 32
+    println!("{}", late_fee(vec![1, 1])); // 2
+}
