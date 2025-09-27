c46851f2fcd8b2093be25aab0933dd9a73b7655a
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-09-27 09:52:12 -0500

    Solve problem 1596

--- src/main/sql/Problem1596.sql
+++ src/main/sql/Problem1596.sql
@@ -1,1 +1,11 @@
 -- https://leetcode.com/problems/the-most-frequently-ordered-products-for-each-customer/description/
+with ranked_orders as (
+    select customer_id,
+           product_id,
+           rank() over (partition by customer_id order by count(product_id) desc) as rank
+    from Orders
+    group by customer_id, product_id
+)
+select r.customer_id, r.product_id, p.product_name
+from ranked_orders r join Products p on r.product_id = p.product_id
+where r.rank = 1;
