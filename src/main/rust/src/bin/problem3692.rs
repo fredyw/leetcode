8191327f356c3129984fb5d0be972508268dbeee
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-09-27 18:34:42 -0500

    Skeleton for problem 3692

---
+++ src/main/rust/src/bin/problem3692.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/majority-frequency-characters/
+pub fn majority_frequency_group(s: String) -> String {
+    todo!()
+}
+
+fn main() {
+    println!("{}", majority_frequency_group("aaabbbccdddde".to_string())); // "ab"
+    println!("{}", majority_frequency_group("abcd".to_string())); // "abcd"
+    println!("{}", majority_frequency_group("pfpfgi".to_string())); // "fp"
+}
