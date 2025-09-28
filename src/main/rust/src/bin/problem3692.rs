483f2caa0c4180bd1915e9f1f859f82867837512
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-09-28 01:29:36 -0500

    Solve problem 3692

--- src/main/rust/src/bin/problem3692.rs
+++ src/main/rust/src/bin/problem3692.rs
@@ -1,12 +1,33 @@
-use std::collections::HashMap;
+use std::cmp::Ordering;
+use std::collections::{HashMap, HashSet};
 
 // https://leetcode.com/problems/majority-frequency-characters/
 pub fn majority_frequency_group(s: String) -> String {
-    let mut map: HashMap<char, i32> = HashMap::new();
+    let mut freqs: HashMap<char, i32> = HashMap::new();
     for c in s.chars() {
-        *map.entry(c).or_insert(0) += 1;
+        *freqs.entry(c).or_insert(0) += 1;
     }
-    todo!()
+    let mut group_size: HashMap<i32, HashSet<char>> = HashMap::new();
+    for (char, count) in &freqs {
+        group_size
+            .entry(*count)
+            .or_insert(HashSet::new())
+            .insert(*char);
+    }
+    group_size
+        .iter()
+        .max_by(|(freq1, set1), (freq2, set2)| {
+            let cmp = set1.len().cmp(&set2.len());
+            if cmp == Ordering::Equal {
+                freq1.cmp(freq2)
+            } else {
+                cmp
+            }
+        })
+        .unwrap()
+        .1
+        .iter()
+        .collect()
 }
 
 fn main() {
