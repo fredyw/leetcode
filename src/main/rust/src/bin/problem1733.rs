2e4adbb9ab0f6a565b090aa1d35fffe87b5d5d14
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-09-28 21:35:19 -0500

    Solve problem 1733

--- src/main/rust/src/bin/problem1733.rs
+++ src/main/rust/src/bin/problem1733.rs
@@ -7,15 +7,26 @@ pub fn minimum_teachings(n: i32, languages: Vec<Vec<i32>>, friendships: Vec<Vec<
         .into_iter()
         .map(|v| v.into_iter().collect())
         .collect();
+    let friendships: Vec<Vec<i32>> = friendships
+        .into_iter()
+        .filter(|friendship| {
+            let fidx1 = friendship[0] as usize - 1;
+            let fidx2 = friendship[1] as usize - 1;
+            languages[fidx1].is_disjoint(&languages[fidx2])
+        })
+        .collect();
     for language in 1..=n {
         let mut count = 0;
+        let mut language_learned: HashSet<usize> = HashSet::new();
         for friendship in &friendships {
             let fidx1 = friendship[0] as usize - 1;
             let fidx2 = friendship[1] as usize - 1;
-            if !languages[fidx1].contains(&language) {
+            if !language_learned.contains(&fidx1) && !languages[fidx1].contains(&language) {
+                language_learned.insert(fidx1);
                 count += 1;
             }
-            if !languages[fidx2].contains(&language) {
+            if !language_learned.contains(&fidx2) && !languages[fidx2].contains(&language) {
+                language_learned.insert(fidx2);
                 count += 1;
             }
         }
@@ -25,14 +36,14 @@ pub fn minimum_teachings(n: i32, languages: Vec<Vec<i32>>, friendships: Vec<Vec<
 }
 
 fn main() {
-    // println!(
-    //     "{}",
-    //     minimum_teachings(
-    //         2,
-    //         vec![vec![1], vec![2], vec![1, 2]],
-    //         vec![vec![1, 2], vec![1, 3], vec![2, 3]]
-    //     )
-    // ); // 1
+    println!(
+        "{}",
+        minimum_teachings(
+            2,
+            vec![vec![1], vec![2], vec![1, 2]],
+            vec![vec![1, 2], vec![1, 3], vec![2, 3]]
+        )
+    ); // 1
     println!(
         "{}",
         minimum_teachings(
