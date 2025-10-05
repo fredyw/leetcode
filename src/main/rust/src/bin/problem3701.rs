b12a9bd6e83a5d8fb0e0265c739b02b5fe2fa2c3
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-10-05 00:53:52 -0500

    Skeleton for problem 3701

---
+++ src/main/rust/src/bin/problem3701.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/compute-alternating-sum/description/
+pub fn alternating_sum(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", alternating_sum(vec![1, 3, 5, 7])); // -4
+    println!("{}", alternating_sum(vec![100])); // 100
+}
