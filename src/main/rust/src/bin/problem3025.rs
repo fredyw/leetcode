3877bafcee062d5948ffa3cabb716d45f566f6b1
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-10-05 09:35:52 -0500

    Skeleton for problem 3025

---
+++ src/main/rust/src/bin/problem3025.rs
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/find-the-number-of-ways-to-place-people-i/description/
+pub fn number_of_pairs(points: Vec<Vec<i32>>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        number_of_pairs(vec![vec![1, 1], vec![2, 2], vec![3, 3]])
+    ); // 0
+    println!(
+        "{}",
+        number_of_pairs(vec![vec![6, 2], vec![4, 4], vec![2, 6]])
+    ); // 2
+    println!(
+        "{}",
+        number_of_pairs(vec![vec![3, 1], vec![1, 3], vec![1, 1]])
+    ); // 2
+}
