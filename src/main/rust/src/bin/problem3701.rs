8c51a16a8b41219abd1ce116d1937b1a1f06db56
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-10-05 00:55:23 -0500

    Solve probelm 3701

--- src/main/rust/src/bin/problem3701.rs
+++ src/main/rust/src/bin/problem3701.rs
@@ -1,6 +1,15 @@
 // https://leetcode.com/problems/compute-alternating-sum/description/
 pub fn alternating_sum(nums: Vec<i32>) -> i32 {
-    todo!()
+    let mut even_sum = 0;
+    let mut odd_sum = 0;
+    for (i, num) in nums.iter().enumerate() {
+        if i % 2 == 0 {
+            even_sum += num;
+        } else {
+            odd_sum += num;
+        }
+    }
+    even_sum - odd_sum
 }
 
 fn main() {
