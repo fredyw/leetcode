4d97d2f81a0977be9dde1f027aa72dd5c25b67d9
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-10-11 18:00:07 -0500

    Solve problem 3706

--- src/main/rust/src/bin/problem3706.rs
+++ src/main/rust/src/bin/problem3706.rs
@@ -1,6 +1,15 @@
 // https://leetcode.com/problems/maximum-distance-between-unequal-words-in-array-ii/description/
 pub fn max_distance(words: Vec<String>) -> i32 {
-    todo!()
+    let mut answer = 0;
+    for (i, word) in words.iter().enumerate() {
+        if *word != words[0] {
+            answer = answer.max(i as i32 + 1);
+        }
+        if *word != words[words.len() - 1] {
+            answer = answer.max(words.len() as i32 - i as i32);
+        }
+    }
+    answer
 }
 
 fn main() {
@@ -26,4 +35,14 @@ fn main() {
         "{}",
         max_distance(vec!["z".to_string(), "z".to_string(), "z".to_string()])
     ); // 0
+    println!(
+        "{}",
+        max_distance(vec![
+            "a".to_string(),
+            "b".to_string(),
+            "c".to_string(),
+            "a".to_string(),
+            "b".to_string()
+        ])
+    ); // 5
 }
