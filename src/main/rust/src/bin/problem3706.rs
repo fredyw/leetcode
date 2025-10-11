11df5f48102081b2e7256c49417e5bb0b0694555
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-10-11 09:36:12 -0500

    Skeleton for problem 3706

---
+++ src/main/rust/src/bin/problem3706.rs
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/maximum-distance-between-unequal-words-in-array-ii/description/
+pub fn max_distance(words: Vec<String>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        max_distance(vec![
+            "leetcode".to_string(),
+            "leetcode".to_string(),
+            "codeforces".to_string()
+        ])
+    ); // 3
+    println!(
+        "{}",
+        max_distance(vec![
+            "a".to_string(),
+            "b".to_string(),
+            "c".to_string(),
+            "a".to_string(),
+            "a".to_string()
+        ])
+    ); // 4
+    println!(
+        "{}",
+        max_distance(vec!["z".to_string(), "z".to_string(), "z".to_string()])
+    ); // 0
+}
