f9a4341d26fd441ba3469b78c4d28a26f037712a
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-10-11 18:35:07 -0500

    Solve problem 3304

--- src/main/rust/src/bin/problem3304.rs
+++ src/main/rust/src/bin/problem3304.rs
@@ -1,6 +1,18 @@
 // https://leetcode.com/problems/find-the-k-th-character-in-string-game-i/description/
 pub fn kth_character(k: i32) -> char {
-    todo!()
+    let mut v: Vec<char> = vec![];
+    v.push('a');
+    while (v.len() as i32) < k {
+        let mut new_vec: Vec<char> = vec![];
+        for i in v.iter() {
+            new_vec.push(((*i as u8 + 1) % 'z' as u8) as char);
+            if new_vec.len() + v.len() == k as usize {
+                break;
+            }
+        }
+        v.extend(new_vec);
+    }
+    v[k as usize - 1]
 }
 
 fn main() {
