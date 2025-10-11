35b194d57228228df2b785e9eae0967e5f370acd
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-10-11 18:27:33 -0500

    Skeleton for problem 3707

---
+++ src/main/rust/src/bin/problem3707.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/equal-score-substrings/description/
+pub fn score_balance(s: String) -> bool {
+    todo!()
+}
+
+fn main() {
+    println!("{}", score_balance("adcb".to_string())); // true
+    println!("{}", score_balance("bace".to_string())); // false
+}
