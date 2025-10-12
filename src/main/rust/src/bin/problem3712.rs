eb027c373c0eed36fd3fe524ce7867a3ac3510c6
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-10-12 09:19:22 -0500

    Skeleton for problem 3712

---
+++ src/main/rust/src/bin/problem3712.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/sum-of-elements-with-frequency-divisible-by-k/
+pub fn sum_divisible_by_k(nums: Vec<i32>, k: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", sum_divisible_by_k(vec![1, 2, 2, 3, 3, 3, 3, 4], 2)); // 16
+    println!("{}", sum_divisible_by_k(vec![1, 2, 3, 4, 5], 2)); // 0
+    println!("{}", sum_divisible_by_k(vec![4, 4, 4, 1, 2, 3], 3)); // 12
+}
