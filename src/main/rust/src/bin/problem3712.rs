4787e23ec5ed2c7285f8d3911a8ea84f7b7a398f
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-10-12 09:21:27 -0500

    Solve problem 3712

--- src/main/rust/src/bin/problem3712.rs
+++ src/main/rust/src/bin/problem3712.rs
@@ -1,6 +1,15 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/sum-of-elements-with-frequency-divisible-by-k/
 pub fn sum_divisible_by_k(nums: Vec<i32>, k: i32) -> i32 {
-    todo!()
+    let mut map: HashMap<i32, i32> = HashMap::new();
+    for num in nums {
+        *map.entry(num).or_insert(0) += 1;
+    }
+    map.iter()
+        .filter(|(_, c)| *c % k == 0)
+        .map(|(n, c)| *n * *c)
+        .sum()
 }
 
 fn main() {
