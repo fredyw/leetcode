391450bb2045d9f169c2a8f599a000c1be00d50a
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-10-12 14:13:38 -0500

    Skeleton for problem 3697

---
+++ src/main/rust/src/bin/problem3697.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/compute-decimal-representation/description/
+pub fn decimal_representation(n: i32) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", decimal_representation(537)); // [500,30,7]
+    println!("{:?}", decimal_representation(102)); // [100,2]
+    println!("{:?}", decimal_representation(6)); // [6]
+}
