7519984fd4287d573236cd7552c0effc6f7f7a08
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-10-12 14:30:53 -0500

    Solve problem 3697

--- src/main/rust/src/bin/problem3697.rs
+++ src/main/rust/src/bin/problem3697.rs
@@ -1,6 +1,18 @@
 // https://leetcode.com/problems/compute-decimal-representation/description/
 pub fn decimal_representation(n: i32) -> Vec<i32> {
-    todo!()
+    let mut answer = vec![];
+    let mut n = n;
+    let mut exp = format!("{}", n).len() as i32 - 1;
+    while n > 0 {
+        let d = 10u32.pow(exp as u32);
+        let v = (n / d as i32) * d as i32;
+        if v > 0 {
+            answer.push(v);
+        }
+        n %= d as i32;
+        exp -= 1;
+    }
+    answer
 }
 
 fn main() {
