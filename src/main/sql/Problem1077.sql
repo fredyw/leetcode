17f947fcb55c9762eaaca119287d353232ec90a6
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-10-19 00:38:17 -0500

    Solve problem 1077

--- src/main/sql/Problem1077.sql
+++ src/main/sql/Problem1077.sql
@@ -1,1 +1,10 @@
 -- https://leetcode.com/problems/project-employees-iii/description/
+with max_years as (
+    select p.project_id, max(e.experience_years) as max_experience_years
+    from Project p join Employee e on p.employee_id = e.employee_id
+    group by p.project_id
+)
+select p.project_id, e.employee_id
+from Project p join Employee e on p.employee_id = e.employee_id
+               join max_years m on p.project_id = m.project_id
+    and e.experience_years = m.max_experience_years;
