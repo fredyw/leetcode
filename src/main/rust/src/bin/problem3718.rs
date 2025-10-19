31d0d3e7267bc2c8ddbf0b847d028204eddd6888
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-10-18 23:43:31 -0500

    Skeleton for problem 3718

---
+++ src/main/rust/src/bin/problem3718.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/smallest-missing-multiple-of-k/description/
+pub fn missing_multiple(nums: Vec<i32>, k: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", missing_multiple(vec![8, 2, 3, 4, 6], 2)); // 10
+    println!("{}", missing_multiple(vec![1, 4, 7, 10, 15], 5)); // 5
+}
