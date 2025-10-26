63d0ea08f26788c9235468ce3d978c434c73058e
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-10-25 23:10:00 -0500

    Skeleton for problem 3726

---
+++ src/main/rust/src/bin/problem3726.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/remove-zeros-in-decimal-representation/description/
+pub fn remove_zeros(n: i64) -> i64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", remove_zeros(1020030)); // 123
+    println!("{}", remove_zeros(1)); // 1
+}
