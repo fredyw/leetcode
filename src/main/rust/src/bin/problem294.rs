f06d574af1d0f73cee83bc4b7c796a21435ad269
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-10-26 19:02:40 -0500

    Solve problem 294

--- src/main/rust/src/bin/problem294.rs
+++ src/main/rust/src/bin/problem294.rs
@@ -1,6 +1,25 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/flip-game-ii/description/
 pub fn can_win(current_state: String) -> bool {
-    todo!()
+    fn can_win(state: String, memo: &mut HashMap<String, bool>) -> bool {
+        if let Some(&result) = memo.get(&state) {
+            return result;
+        }
+        for i in 0..state.len().saturating_sub(1) {
+            if &state[i..i + 2] == "++" {
+                let next_state = format!("{}{}{}", &state[..i], "--", &state[i + 2..]);
+                if !can_win(next_state, memo) {
+                    memo.insert(state, true);
+                    return true;
+                }
+            }
+        }
+        memo.insert(state, false);
+        false
+    }
+
+    can_win(current_state, &mut HashMap::new())
 }
 
 fn main() {
