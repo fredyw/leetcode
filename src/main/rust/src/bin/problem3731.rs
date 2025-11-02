de0cf002b40996836eb0965762dd29241f387828
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-11-02 08:38:36 -0600

    Skeleton for problem 3731

---
+++ src/main/rust/src/bin/problem3731.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/find-missing-elements/description/
+pub fn find_missing_elements(nums: Vec<i32>) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", find_missing_elements(vec![1, 4, 2, 5])); // [3]
+    println!("{:?}", find_missing_elements(vec![7, 8, 6, 9])); // []
+    println!("{:?}", find_missing_elements(vec![5, 1])); // [2,3,4]
+}
