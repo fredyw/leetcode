00629a8b32a6269b84431be0002680cb65a20b33
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-11-08 17:35:59 -0600

    Skeleton for problem 3736

---
+++ src/main/rust/src/bin/problem3736.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/minimum-moves-to-equal-array-elements-iii/description/
+pub fn min_moves(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", min_moves(vec![2, 1, 3])); // 3
+    println!("{}", min_moves(vec![4, 4, 5])); // 2
+}
