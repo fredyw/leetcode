0fea8a02c606df655e693e2e50510cdc54bd1eda
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-11-10 22:09:41 -0600

    Skeleton for problem 3740

---
+++ src/main/rust/src/bin/problem3740.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/minimum-distance-between-three-equal-elements-i/description/
+pub fn minimum_distance(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", minimum_distance(vec![1, 2, 1, 1, 3])); // 6
+    println!("{}", minimum_distance(vec![1, 1, 2, 3, 2, 1, 2])); // 8
+    println!("{}", minimum_distance(vec![1])); // -1
+}
