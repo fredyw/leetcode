f0e1b29eb46cfdd3bf42946cafdec3a8c58d6d99
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-11-15 22:39:00 -0600

    Update problem 3221

--- src/main/rust/src/bin/problem3221.rs
+++ src/main/rust/src/bin/problem3221.rs
@@ -1,6 +1,19 @@
 // https://leetcode.com/problems/maximum-array-hopping-score-ii
 pub fn max_score(nums: Vec<i32>) -> i64 {
-    todo!()
+    fn max_score(nums: &Vec<i32>, i: usize, memo: &mut Vec<i64>) -> i64 {
+        if memo[i] != -1 {
+            return memo[i];
+        }
+        let mut max = 0;
+        for j in i + 1..nums.len() {
+            let score = max_score(nums, j, memo) + ((j as i64 - i as i64) * nums[j] as i64);
+            max = max.max(score);
+        }
+        memo[i] = max;
+        max
+    }
+
+    max_score(&nums, 0, &mut vec![-1; nums.len()])
 }
 
 fn main() {
