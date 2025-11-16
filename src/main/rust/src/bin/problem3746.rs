fd6d3f62b77de8c3e568fdf518c8d04bb30d49ed
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-11-16 10:03:54 -0600

    Skeleton for problem 3746

---
+++ src/main/rust/src/bin/problem3746.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/minimum-string-length-after-balanced-removals/description/
+pub fn min_length_after_removals(s: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", min_length_after_removals("aabbab".to_string())); // 0
+    println!("{}", min_length_after_removals("aaaa".to_string())); // 4
+    println!("{}", min_length_after_removals("aaabb".to_string())); // 1
+}
