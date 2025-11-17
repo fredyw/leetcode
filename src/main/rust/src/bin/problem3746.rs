da2e3c8ce4eb3a6346f2444ce618b44873ecf2ad
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-11-16 23:47:54 -0600

    Solve problem 3746

--- src/main/rust/src/bin/problem3746.rs
+++ src/main/rust/src/bin/problem3746.rs
@@ -1,6 +1,15 @@
 // https://leetcode.com/problems/minimum-string-length-after-balanced-removals/description/
 pub fn min_length_after_removals(s: String) -> i32 {
-    todo!()
+    let mut a_count = 0;
+    let mut b_count = 0;
+    for char in s.chars() {
+        if char == 'a' {
+            a_count += 1;
+        } else {
+            b_count += 1;
+        }
+    }
+    s.len() as i32 - (a_count.min(b_count) * 2)
 }
 
 fn main() {
