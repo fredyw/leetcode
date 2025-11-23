73928b46f0377043d07218e41b90abdc846309d4
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-11-22 22:41:10 -0600

    Skeleton for problem 3750

---
+++ src/main/rust/src/bin/problem3750.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/minimum-number-of-flips-to-reverse-binary-string/description/
+pub fn minimum_flips(n: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", minimum_flips(7)); // 0
+    println!("{}", minimum_flips(10)); // 4
+}
