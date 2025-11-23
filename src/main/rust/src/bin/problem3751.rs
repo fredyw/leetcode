43cfad16be23814edb30d68f5fef526cb0ccc513
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-11-23 12:02:45 -0600

    Solve problem 3751

--- src/main/rust/src/bin/problem3751.rs
+++ src/main/rust/src/bin/problem3751.rs
@@ -1,6 +1,18 @@
 // https://leetcode.com/problems/total-waviness-of-numbers-in-range-i/description/
 pub fn total_waviness(num1: i32, num2: i32) -> i32 {
-    todo!()
+    let mut answer = 0;
+    for num in num1..=num2 {
+        let s = num.to_string();
+        let bytes = s.as_bytes();
+        for i in 1..bytes.len() - 1 {
+            if bytes[i - 1] > bytes[i] && bytes[i + 1] > bytes[i] {
+                answer += 1;
+            } else if bytes[i - 1] < bytes[i] && bytes[i + 1] < bytes[i] {
+                answer += 1;
+            }
+        }
+    }
+    answer
 }
 
 fn main() {
