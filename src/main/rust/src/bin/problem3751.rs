3e7781f053dfda2e236ef528b32be82a08a9d598
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-11-22 23:55:41 -0600

    Skeleton for problem 3751

---
+++ src/main/rust/src/bin/problem3751.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/total-waviness-of-numbers-in-range-i/description/
+pub fn total_waviness(num1: i32, num2: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", total_waviness(120, 130)); // 3
+    println!("{}", total_waviness(198, 202)); // 3
+    println!("{}", total_waviness(4848, 4848)); // 2
+}
