3445e347172d73afeca297ec80de75a62dd3b329
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-11-22 23:28:17 -0600

    Solve problem 3750

--- src/main/rust/src/bin/problem3750.rs
+++ src/main/rust/src/bin/problem3750.rs
@@ -1,6 +1,9 @@
 // https://leetcode.com/problems/minimum-number-of-flips-to-reverse-binary-string/description/
 pub fn minimum_flips(n: i32) -> i32 {
-    todo!()
+    let n = n as u32;
+    let m = n.reverse_bits() >> n.leading_zeros();
+    let x = m ^ n;
+    x.count_ones() as i32
 }
 
 fn main() {
