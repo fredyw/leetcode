d605e6df88730dd6aa05282131a95a6fa1bbbca5
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-11-25 17:16:12 -0600

    Skeleton for problem 3754

---
+++ src/main/rust/src/bin/problem3754.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/concatenate-non-zero-digits-and-multiply-by-sum-i/description/
+pub fn sum_and_multiply(n: i32) -> i64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", sum_and_multiply(10203004)); // 12340
+    println!("{}", sum_and_multiply(1000)); // 1
+}
