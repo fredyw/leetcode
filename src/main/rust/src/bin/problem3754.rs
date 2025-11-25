c600c69cb4756570d561264a5de1476a1f01f55b
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-11-25 17:20:52 -0600

    Solve problem 3754

--- src/main/rust/src/bin/problem3754.rs
+++ src/main/rust/src/bin/problem3754.rs
@@ -1,6 +1,14 @@
 // https://leetcode.com/problems/concatenate-non-zero-digits-and-multiply-by-sum-i/description/
 pub fn sum_and_multiply(n: i32) -> i64 {
-    todo!()
+    let mut sum = 0;
+    let mut s = String::new();
+    for c in n.to_string().chars() {
+        if c != '0' {
+            sum += c.to_digit(10).unwrap() as i64;
+            s.push(c);
+        }
+    }
+    s.parse::<i64>().unwrap_or(0) * sum
 }
 
 fn main() {
