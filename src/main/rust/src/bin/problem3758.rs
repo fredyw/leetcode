5060f480067e8a26eb120a2c1b3fdbbbe0a6cee1
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-11-25 17:23:52 -0600

    Skeleton for problem 3758

---
+++ src/main/rust/src/bin/problem3758.rs
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/convert-number-words-to-digits/description/
+pub fn convert_number(s: String) -> String {
+    todo!()
+}
+
+fn main() {
+    println!("{}", convert_number("onefourthree".to_string())); // "134"
+    println!("{}", convert_number("ninexsix".to_string())); // "96"
+    println!("{}", convert_number("zeero".to_string())); // "
+    println!("{}", convert_number("tw".to_string())); // ""
+}
