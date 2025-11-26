e63a0c673ebb1ed0f54780d9c5085a5b5784744d
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-11-25 23:13:53 -0600

    Update problem 3741

--- src/main/rust/src/bin/problem3741.rs
+++ src/main/rust/src/bin/problem3741.rs
@@ -1,6 +1,22 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/minimum-distance-between-three-equal-elements-ii/
 pub fn minimum_distance(nums: Vec<i32>) -> i32 {
-    todo!()
+    let mut map: HashMap<i32, Vec<usize>> = HashMap::new();
+    for (i, n) in nums.iter().enumerate() {
+        map.entry(*n).or_insert(Vec::new()).push(i);
+    }
+    let mut answer = 0;
+    for (_, v) in map.iter() {
+        if v.len() >= 3 {
+            let i = v[0] as i32;
+            let j = v[v.len() / 2] as i32;
+            let k = v[v.len() - 1] as i32;
+            let distance = (i - j).abs() + (j - k).abs() + (k - i).abs();
+            answer = answer.max(distance);
+        }
+    }
+    answer
 }
 
 fn main() {
