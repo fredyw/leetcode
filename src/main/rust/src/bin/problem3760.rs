b3767dd06af1eb8ffbd222705ae52a42b1819e6a
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-11-30 09:44:43 -0600

    Skeleton for problem 3760

---
+++ src/main/rust/src/bin/problem3760.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/maximum-substrings-with-distinct-start/
+pub fn max_distinct(s: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", max_distinct("abab".to_string())); // 2
+    println!("{}", max_distinct("abcd".to_string())); // 4
+    println!("{}", max_distinct("aaaa".to_string())); // 1
+}
