02c1a60733edf17b0dd62a1f806d4a4ff37da26d
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-12-02 01:29:29 -0600

    Solve problem 2314

--- src/main/sql/Problem2314.sql
+++ src/main/sql/Problem2314.sql
@@ -1,1 +1,12 @@
 -- https://leetcode.com/problems/the-first-day-of-the-maximum-recorded-degree-in-each-city/
+with t as (
+    select city_id,
+           day,
+           degree,
+           rank() over (partition by city_id order by degree desc, day asc) as rnk
+    from Weather
+)
+select city_id, day, degree
+from t
+where rnk = 1
+order by city_id;
