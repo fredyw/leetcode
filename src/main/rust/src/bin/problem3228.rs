508cd717dc878c3279000e95316a1a2d04f1ca7c
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-12-05 18:29:37 -0600

    Update problem 3228

--- src/main/rust/src/bin/problem3228.rs
+++ src/main/rust/src/bin/problem3228.rs
@@ -1,6 +1,17 @@
 // https://leetcode.com/problems/maximum-number-of-operations-to-move-ones-to-the-end/description/
 pub fn max_operations(s: String) -> i32 {
-    todo!()
+    let mut answer = 0;
+    let mut one_idx = s.len(); // initial value
+    for (i, char) in s.chars().rev().enumerate() {
+        println!("index: {i}");
+        if one_idx == s.len() {
+            one_idx = i;
+        } else {
+            answer += (one_idx - i) as i32 - 1;
+            one_idx = i;
+        }
+    }
+    answer
 }
 
 fn main() {
