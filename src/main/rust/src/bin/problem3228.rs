1a630b0c0fdfaced49e2fa4ff596ff0ddd0b98f5
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-12-05 18:16:38 -0600

    Skeleton for problem 3228

---
+++ src/main/rust/src/bin/problem3228.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/maximum-number-of-operations-to-move-ones-to-the-end/description/
+pub fn max_operations(s: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", max_operations("1001101".to_string())); // 4
+    println!("{}", max_operations("00111".to_string())); // 0
+}
