028fb203021f588df2d0bad17e0113158df87695
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-12-07 09:38:44 -0600

    Skeleton for problem 3769

---
+++ src/main/rust/src/bin/problem3769.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/sort-integers-by-binary-reflection/description/
+pub fn sort_by_reflection(nums: Vec<i32>) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", sort_by_reflection(vec![4, 5, 4])); // [4,4,5]
+    println!("{:?}", sort_by_reflection(vec![3, 6, 5, 8])); // [8,3,6,5]
+}
