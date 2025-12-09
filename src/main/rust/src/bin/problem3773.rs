6c2ee7ceed2cc4921df7485e6ca3a0bdfa40c327
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-12-08 22:57:11 -0600

    Skeleton for problem 3773

---
+++ src/main/rust/src/bin/problem3773.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/maximum-number-of-equal-length-runs/description/
+pub fn max_same_length_runs(s: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", max_same_length_runs("hello".to_string())); // 3
+    println!("{}", max_same_length_runs("aaabaaa".to_string())); // 2
+}
