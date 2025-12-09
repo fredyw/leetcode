4216fc17a0025a1a7e403b493dcc2ded0b82157d
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-12-08 23:04:46 -0600

    Solve problem 3773

--- src/main/rust/src/bin/problem3773.rs
+++ src/main/rust/src/bin/problem3773.rs
@@ -1,6 +1,20 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/maximum-number-of-equal-length-runs/description/
 pub fn max_same_length_runs(s: String) -> i32 {
-    todo!()
+    let bytes = s.as_bytes();
+    let mut length = 1;
+    let mut map: HashMap<i32, i32> = HashMap::new();
+    for i in 0..bytes.len() - 1 {
+        if bytes[i] == bytes[i + 1] {
+            length += 1;
+        } else {
+            *map.entry(length).or_insert(0) += 1;
+            length = 1;
+        }
+    }
+    *map.entry(length).or_insert(0) += 1;
+    map.into_iter().map(|(_, count)| count).max().unwrap()
 }
 
 fn main() {
