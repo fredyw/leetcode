d3fb111e5ae59b037d3b56139e737da4f0bd167e
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-12-13 13:36:06 -0600

    Skeleton for problem 3689

---
+++ src/main/rust/src/bin/problem3689.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/maximum-total-subarray-value-i/description/
+pub fn max_total_value(nums: Vec<i32>, k: i32) -> i64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", max_total_value(vec![1, 3, 2], 2)); // 4
+    println!("{}", max_total_value(vec![4, 2, 5, 1], 3)); // 12
+}
