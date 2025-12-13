fe334b6116b3ec9211db0279bb19b7b472773004
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-12-13 10:00:08 -0600

    Solve problem 3228

--- src/main/rust/src/bin/problem3228.rs
+++ src/main/rust/src/bin/problem3228.rs
@@ -1,16 +1,16 @@
 // https://leetcode.com/problems/maximum-number-of-operations-to-move-ones-to-the-end/description/
 pub fn max_operations(s: String) -> i32 {
+    let s = if let Some(i) = s.rfind(|c| c != '1') {
+        &s[0..=i]
+    } else {
+        ""
+    };
     let mut answer = 0;
-    let bytes = s.as_bytes();
     let mut count = 0;
-    for i in 0..s.bytes().len() - 1 {
-        if bytes[i] == b'1' && bytes[i + 1] == b'1' {
-            count += 1;
-        } else if bytes[i] == b'0' && bytes[i + 1] == b'1' {
-            count = 1;
-        } else if bytes[i] == b'1' && bytes[i + 1] == b'0' {
+    for x in s.split('0') {
+        if !x.is_empty() {
+            count += x.len() as i32;
             answer += count;
-            count = 1;
         }
     }
     answer
@@ -21,4 +21,6 @@ fn main() {
     println!("{}", max_operations("00111".to_string())); // 0
     println!("{}", max_operations("111000111000111".to_string())); // 9
     println!("{}", max_operations("10011011101".to_string())); // 10
+    println!("{}", max_operations("10".to_string())); // 1
+    println!("{}", max_operations("0000".to_string())); // 0
 }
