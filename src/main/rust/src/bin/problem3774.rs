878184a00887845f8d47ec2e280b9f221e5d8897
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-12-13 23:53:04 -0600

    Updat eproblem 3774

--- src/main/rust/src/bin/problem3774.rs
+++ src/main/rust/src/bin/problem3774.rs
@@ -1,6 +1,15 @@
 // https://leetcode.com/problems/absolute-difference-between-maximum-and-minimum-k-elements/
-pub fn abs_difference(nums: Vec<i32>, k: i32) -> i32 {
-    todo!()
+pub fn abs_difference(mut nums: Vec<i32>, k: i32) -> i32 {
+    nums.sort();
+    let mut largest = 0;
+    let mut smallest = 0;
+    for i in 0..k {
+        smallest += nums[i as usize];
+    }
+    for i in nums.len() - k as usize..nums.len() {
+        largest += nums[i as usize];
+    }
+    (smallest - largest).abs()
 }
 
 fn main() {
