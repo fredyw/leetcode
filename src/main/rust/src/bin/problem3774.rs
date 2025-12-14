952ef6e1be41f50b72b437ce9c7364d925537744
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-12-13 23:49:25 -0600

    Skeleton for probelm 3774

---
+++ src/main/rust/src/bin/problem3774.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/absolute-difference-between-maximum-and-minimum-k-elements/
+pub fn abs_difference(nums: Vec<i32>, k: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", abs_difference(vec![5, 2, 2, 4], 2)); // 5
+    println!("{}", abs_difference(vec![100], 1)); // 0
+}
