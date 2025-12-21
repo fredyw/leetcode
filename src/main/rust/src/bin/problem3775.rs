2baf39fc52eb593b695f000e2bef1c7e17e931fa
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-12-21 13:23:08 -0600

    Skeleton for problem 3775

---
+++ src/main/rust/src/bin/problem3775.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/reverse-words-with-same-vowel-count/description/
+pub fn reverse_words(s: String) -> String {
+    todo!()
+}
+
+fn main() {
+    println!("{}", reverse_words("cat and mice".to_string())); // "cat dna mice"
+    println!("{}", reverse_words("book is nice".to_string())); // "book is ecin"
+    println!("{}", reverse_words("banana healthy".to_string())); // "banana healthy"
+}
