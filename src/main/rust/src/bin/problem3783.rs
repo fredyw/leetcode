cf435bd9728889643b0de711e62bddb4edb62eed
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-12-20 22:39:55 -0600

    Solve problem 3783

--- src/main/rust/src/bin/problem3783.rs
+++ src/main/rust/src/bin/problem3783.rs
@@ -1,10 +1,17 @@
 // https://leetcode.com/problems/mirror-distance-of-an-integer/description/
 pub fn mirror_distance(n: i32) -> i32 {
-    todo!()
+    let r: i32 = n
+        .to_string()
+        .chars()
+        .rev()
+        .collect::<String>()
+        .parse()
+        .unwrap();
+    (n - r).abs()
 }
 
 fn main() {
     println!("{}", mirror_distance(25)); // 27
     println!("{}", mirror_distance(10)); // 9
-    println!("{}", mirror_distance(7)); // 10
+    println!("{}", mirror_distance(7)); // 0
 }
