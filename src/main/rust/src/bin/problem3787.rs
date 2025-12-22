01f68d622543a6e923da3e267c5c14b97e3f6cfe
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-12-22 15:46:11 -0600

    Update problem 3787

--- src/main/rust/src/bin/problem3787.rs
+++ src/main/rust/src/bin/problem3787.rs
@@ -1,6 +1,17 @@
 // https://leetcode.com/problems/find-diameter-endpoints-of-a-tree/description/
 pub fn find_special_nodes(n: i32, edges: Vec<Vec<i32>>) -> String {
-    todo!()
+    let mut graph: Vec<Vec<i32>> = vec![vec![]; n as usize];
+    for edge in edges {
+        graph[edge[0] as usize].push(edge[1]);
+        graph[edge[1] as usize].push(edge[0]);
+    }
+    let mut answer: Vec<char> = vec!['0'; n as usize];
+    for (i, v) in graph.into_iter().enumerate() {
+        if v.len() == 1 {
+            answer[i] = '1';
+        }
+    }
+    answer.into_iter().collect()
 }
 
 fn main() {
@@ -20,4 +31,11 @@ fn main() {
         )
     ); // "1000111"
     println!("{}", find_special_nodes(2, vec![vec![0, 1]])); // "11"
+    println!(
+        "{}",
+        find_special_nodes(
+            6,
+            vec![vec![1, 5], vec![3, 2], vec![2, 5], vec![4, 0], vec![0, 5]]
+        )
+    ); // "000110"
 }
