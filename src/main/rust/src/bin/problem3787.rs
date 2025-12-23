d604d0d96c8327517502eb6e156b95a522dd449b
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-12-23 14:56:38 -0600

    Solve problem 3787

--- src/main/rust/src/bin/problem3787.rs
+++ src/main/rust/src/bin/problem3787.rs
@@ -1,14 +1,51 @@
 // https://leetcode.com/problems/find-diameter-endpoints-of-a-tree/description/
 pub fn find_special_nodes(n: i32, edges: Vec<Vec<i32>>) -> String {
+    fn dfs(
+        graph: &Vec<Vec<i32>>,
+        root: usize,
+        visited: &mut Vec<bool>,
+        length: i32,
+        lengths: &mut Vec<i32>,
+    ) {
+        lengths[root] = length;
+        visited[root] = true;
+        for adj in &graph[root] {
+            if !visited[*adj as usize] {
+                dfs(graph, *adj as usize, visited, length + 1, lengths);
+            }
+        }
+    }
+
     let mut graph: Vec<Vec<i32>> = vec![vec![]; n as usize];
     for edge in edges {
         graph[edge[0] as usize].push(edge[1]);
         graph[edge[1] as usize].push(edge[0]);
     }
-    let mut answer: Vec<char> = vec!['0'; n as usize];
-    for (i, v) in graph.into_iter().enumerate() {
-        if v.len() == 1 {
-            answer[i] = '1';
+    let mut lengths: Vec<i32> = vec![0; n as usize];
+    // Find an extreme point and a diameter.
+    dfs(&graph, 0, &mut vec![false; n as usize], 0, &mut lengths);
+    let (_, i) = lengths
+        .into_iter()
+        .enumerate()
+        .map(|(i, length)| (length, i))
+        .max()
+        .unwrap();
+    let mut lengths_1: Vec<i32> = vec![0; n as usize];
+    dfs(&graph, i, &mut vec![false; n as usize], 0, &mut lengths_1);
+    let (&diameter, i) = lengths_1
+        .iter()
+        .enumerate()
+        .map(|(i, length)| (length, i))
+        .max()
+        .unwrap();
+    let mut lengths_2: Vec<i32> = vec![0; n as usize];
+    dfs(&graph, i, &mut vec![false; n as usize], 0, &mut lengths_2);
+    let mut answer: Vec<char> = Vec::new();
+    for i in 0..n as usize {
+        if lengths_1[i] == diameter || lengths_2[i] == diameter {
+            answer.push('1');
+        } else {
+            answer.push('0');
         }
     }
     answer.into_iter().collect()
