0f573b682bf9af06efb3bea204f3f606663ad7f3
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-12-29 16:30:50 -0600

    Skeleton for problem 3792

---
+++ src/main/rust/src/bin/problem3792.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/sum-of-increasing-product-blocks/description/
+pub fn sum_of_blocks(n: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", sum_of_blocks(3)); // 127
+    println!("{}", sum_of_blocks(7)); // 6997165
+}
