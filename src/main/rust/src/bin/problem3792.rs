2f2fc0c0e4a0d7d1e5b9ad1888ac4dee269cbace
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-12-29 16:44:05 -0600

    Solve problem 3792

--- src/main/rust/src/bin/problem3792.rs
+++ src/main/rust/src/bin/problem3792.rs
@@ -1,9 +1,20 @@
 // https://leetcode.com/problems/sum-of-increasing-product-blocks/description/
 pub fn sum_of_blocks(n: i32) -> i32 {
-    todo!()
+    let mut answer: i64 = 0;
+    let mut num = 0;
+    for count in 1..=n {
+        let mut product: i64 = 1;
+        for _ in 0..count {
+            num += 1;
+            product = (product * num) % 1_000_000_007;
+        }
+        answer += product;
+    }
+    (answer % 1_000_000_007) as i32
 }
 
 fn main() {
     println!("{}", sum_of_blocks(3)); // 127
     println!("{}", sum_of_blocks(7)); // 6997165
+    println!("{}", sum_of_blocks(1000)); // 587219233
 }
