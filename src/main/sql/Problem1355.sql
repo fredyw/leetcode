50fda835e3f0abb239045e7a2b8ea159f19aa641
Fredy Wijaya <fredy.wijaya@gmail.com>
2025-12-31 18:19:40 -0600

    Solve problem 1355

--- src/main/sql/Problem1355.sql
+++ src/main/sql/Problem1355.sql
@@ -1,1 +1,16 @@
 -- https://leetcode.com/problems/activity-participants/description/
+with activity_count as (
+    select activity, count(*) as count
+    from Friends
+    group by activity
+),
+min_max_stats as (
+    select activity,
+           count,
+           min(count) over() as min_count,
+           max(count) over() as max_count
+    from activity_count
+)
+select activity
+from min_max_stats
+where count > min_count and count < max_count;
