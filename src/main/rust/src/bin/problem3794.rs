c2fe5adafc13e9e6b608084900f047a4e777be30
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-01-03 19:51:00 -0600

    Skeleton for problem 3794

---
+++ src/main/rust/src/bin/problem3794.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/reverse-string-prefix/description/
+pub fn reverse_prefix(s: String, k: i32) -> String {
+    todo!()
+}
+
+fn main() {
+    println!("{}", reverse_prefix("abcd".to_string(), 2)); // "bacd"
+    println!("{}", reverse_prefix("xyz".to_string(), 3)); // "zyx"
+    println!("{}", reverse_prefix("hey".to_string(), 1)); // "hey"
+}
