0e328d3f120dbdf6bc9c50e90d9da2eefa1aee0f
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-01-08 14:00:29 -0600

    Skeleton for problem 3798

---
+++ src/main/rust/src/bin/problem3798.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/largest-even-number/description/
+pub fn largest_even(s: String) -> String {
+    todo!()
+}
+
+fn main() {
+    println!("{}", largest_even("1112".to_string())); // "1112"
+    println!("{}", largest_even("221".to_string())); // "22"
+    println!("{}", largest_even("1".to_string())); // ""
+}
