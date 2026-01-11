9d87eca872c77abe81c4304ee9ede54eda5ecb45
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-01-10 23:49:50 -0600

    Skeleton for problem 3627

---
+++ src/main/rust/src/bin/problem3627.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/maximum-median-sum-of-subsequences-of-size-3/description/
+pub fn maximum_median_sum(nums: Vec<i32>) -> i64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", maximum_median_sum(vec![2, 1, 3, 2, 1, 3])); // 5
+    println!("{}", maximum_median_sum(vec![1, 1, 10, 10, 10, 10])); // 20
+}
