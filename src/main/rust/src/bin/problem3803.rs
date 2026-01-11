3c817fd3527aa00962171c9e334a45666db6bd22
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-01-10 23:04:02 -0600

    Skeleton for problem 3803

---
+++ src/main/rust/src/bin/problem3803.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/count-residue-prefixes/description/
+pub fn residue_prefixes(s: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", residue_prefixes("abc".to_string())); // 2
+    println!("{}", residue_prefixes("dd".to_string())); // 1
+    println!("{}", residue_prefixes("bob".to_string())); // 2
+}
