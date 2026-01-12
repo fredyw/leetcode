d062390a33e28a46c778cffb922fbad7e069dbc0
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-01-11 21:16:45 -0600

    Solve problem 3627

--- src/main/rust/src/bin/problem3627.rs
+++ src/main/rust/src/bin/problem3627.rs
@@ -1,6 +1,15 @@
 // https://leetcode.com/problems/maximum-median-sum-of-subsequences-of-size-3/description/
-pub fn maximum_median_sum(nums: Vec<i32>) -> i64 {
-    todo!()
+pub fn maximum_median_sum(mut nums: Vec<i32>) -> i64 {
+    nums.sort();
+    let mut answer = 0;
+    let mut i = nums.len() as isize - 2;
+    let mut count = 0;
+    while count < nums.len() / 3 {
+        answer += nums[i as usize] as i64;
+        i -= 2;
+        count += 1;
+    }
+    answer
 }
 
 fn main() {
