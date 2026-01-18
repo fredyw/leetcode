68708ea1cb458ce90592e1e2e99cb7254ff1b9d3
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-01-18 09:14:08 -0600

    Skeleton for problem 3804

---
+++ src/main/rust/src/bin/problem3804.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/number-of-centered-subarrays/description/
+pub fn centered_subarrays(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", centered_subarrays(vec![-1, 1, 0])); // 5
+    println!("{}", centered_subarrays(vec![2, -3])); // 2
+}
