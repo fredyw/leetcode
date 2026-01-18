0d21466a2d9b120b834c459ae07ecb664796c003
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-01-18 09:20:56 -0600

    Update problem 3804

--- src/main/rust/src/bin/problem3804.rs
+++ src/main/rust/src/bin/problem3804.rs
@@ -1,6 +1,19 @@
+use std::collections::HashSet;
+
 // https://leetcode.com/problems/number-of-centered-subarrays/description/
 pub fn centered_subarrays(nums: Vec<i32>) -> i32 {
-    todo!()
+    let mut answer = 0;
+    let unique_nums: HashSet<i32> = nums.iter().copied().collect();
+    for i in 0..nums.len() {
+        let mut sum = 0;
+        for j in i..nums.len() {
+            sum += nums[j];
+            if unique_nums.contains(&sum) {
+                answer += 1;
+            }
+        }
+    }
+    answer
 }
 
 fn main() {
