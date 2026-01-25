f77d37a892c064dbafd6baf1dd4b4c0bc9d37e1d
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-01-25 10:08:00 -0600

    Skeleton for problem 3818

---
+++ src/main/rust/src/bin/problem3818.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/minimum-prefix-removal-to-make-array-strictly-increasing/description/
+pub fn minimum_prefix_length(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", minimum_prefix_length(vec![1, -1, 2, 3, 3, 4, 5])); // 4
+    println!("{}", minimum_prefix_length(vec![4, 3, -2, -5])); // 3
+    println!("{}", minimum_prefix_length(vec![1, 2, 3, 4])); // 0
+}
