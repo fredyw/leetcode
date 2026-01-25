63c31c6a648feb3eac63fd26bcfc5e442565ac2c
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-01-24 21:25:28 -0600

    Skeleton for problem 3813

---
+++ src/main/rust/src/bin/problem3813.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/vowel-consonant-score/description/
+pub fn vowel_consonant_score(s: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", vowel_consonant_score("cooear".to_string())); // 2
+    println!("{}", vowel_consonant_score("axeyizou".to_string())); // 1
+    println!("{}", vowel_consonant_score("au 123".to_string())); // 0
+}
