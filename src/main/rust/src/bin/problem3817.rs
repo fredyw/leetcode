21a8842f9ad0276c3c0e7dea864b4350395fdece
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-01-24 21:58:31 -0600

    Solve problem 3817

--- src/main/rust/src/bin/problem3817.rs
+++ src/main/rust/src/bin/problem3817.rs
@@ -1,6 +1,17 @@
 // https://leetcode.com/problems/good-indices-in-a-digit-string/description/
 pub fn good_indices(s: String) -> Vec<i32> {
-    todo!()
+    let chars: Vec<char> = s.chars().collect();
+    let mut answer = vec![];
+    for i in 0..chars.len() {
+        let digits = i.checked_ilog10().unwrap_or(0) + 1;
+        let from = (i as isize - digits as isize + 1) as usize;
+        let to = i;
+        let sub: String = chars[from..=to].iter().collect();
+        if sub == i.to_string() {
+            answer.push(i as i32);
+        }
+    }
+    answer
 }
 
 fn main() {
