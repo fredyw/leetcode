81a2876e482a585cd39fbe69cc72e18d666d0da7
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-02-01 10:14:28 -0600

    Skeleton for problem 3822

---
+++ src/main/rust/src/bin/problem3822.rs
@@ -0,0 +1,25 @@
+struct OrderManagementSystem {}
+
+impl OrderManagementSystem {
+    fn new() -> Self {
+        todo!()
+    }
+
+    fn add_order(&self, order_id: i32, order_type: String, price: i32) {
+        todo!()
+    }
+
+    fn modify_order(&self, order_id: i32, new_price: i32) {
+        todo!()
+    }
+
+    fn cancel_order(&self, order_id: i32) {
+        todo!()
+    }
+
+    fn get_orders_at_price(&self, order_type: String, price: i32) -> Vec<i32> {
+        todo!()
+    }
+}
+
+fn main() {}
