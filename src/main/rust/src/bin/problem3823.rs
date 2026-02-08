89444999b26faa2052aab2c60c423068c39996ec
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-02-08 10:36:06 -0600

    Update problem 3823

--- src/main/rust/src/bin/problem3823.rs
+++ src/main/rust/src/bin/problem3823.rs
@@ -1,5 +1,17 @@
 // https://leetcode.com/problems/reverse-letters-then-special-characters-in-a-string/description/
 pub fn reverse_by_type(s: String) -> String {
+    let chars: Vec<char> = s.chars().collect();
+    let mut i = 0;
+    let mut j = chars.len() - 1;
+    while i < j {
+        if chars[i].is_ascii_alphabetic() && chars[j].is_ascii_alphabetic() {}
+        if !chars[i].is_ascii_alphabetic() {
+            i += 1;
+        }
+        if !chars[j].is_ascii_alphabetic() {
+            j -= 1;
+        }
+    }
     todo!()
 }
 
