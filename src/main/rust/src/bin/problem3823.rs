30e794e09c23c4036b7d2f8f51c11db1412762c7
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-02-08 10:47:24 -0600

    Solve problem 3823

--- src/main/rust/src/bin/problem3823.rs
+++ src/main/rust/src/bin/problem3823.rs
@@ -1,10 +1,14 @@
 // https://leetcode.com/problems/reverse-letters-then-special-characters-in-a-string/description/
 pub fn reverse_by_type(s: String) -> String {
-    let chars: Vec<char> = s.chars().collect();
+    let mut chars: Vec<char> = s.chars().collect();
     let mut i = 0;
     let mut j = chars.len() - 1;
     while i < j {
-        if chars[i].is_ascii_alphabetic() && chars[j].is_ascii_alphabetic() {}
+        if chars[i].is_ascii_alphabetic() && chars[j].is_ascii_alphabetic() {
+            chars.swap(i, j);
+            i += 1;
+            j -= 1;
+        }
         if !chars[i].is_ascii_alphabetic() {
             i += 1;
         }
@@ -12,7 +16,22 @@ pub fn reverse_by_type(s: String) -> String {
             j -= 1;
         }
     }
-    todo!()
+    let mut i = 0;
+    let mut j = chars.len() - 1;
+    while i < j {
+        if !chars[i].is_ascii_alphabetic() && !chars[j].is_ascii_alphabetic() {
+            chars.swap(i, j);
+            i += 1;
+            j -= 1;
+        }
+        if chars[i].is_ascii_alphabetic() {
+            i += 1;
+        }
+        if chars[j].is_ascii_alphabetic() {
+            j -= 1;
+        }
+    }
+    chars.into_iter().collect()
 }
 
 fn main() {
