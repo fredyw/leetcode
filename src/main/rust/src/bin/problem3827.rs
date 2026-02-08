e6fe661efde8d00ae15f5a20e7aa65a39c25b9bb
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-02-08 11:20:33 -0600

    Solve problem 3827

--- src/main/rust/src/bin/problem3827.rs
+++ src/main/rust/src/bin/problem3827.rs
@@ -1,6 +1,13 @@
 // https://leetcode.com/problems/count-monobit-integers/description/
 pub fn count_monobit(n: i32) -> i32 {
-    todo!()
+    let mut answer = 0;
+    for i in 0..=n {
+        let length = u32::BITS - i.leading_zeros();
+        if i.count_ones() == length || i.count_zeros() == length {
+            answer += 1;
+        }
+    }
+    answer
 }
 
 fn main() {
