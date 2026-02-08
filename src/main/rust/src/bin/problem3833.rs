3f74de7fe52826366a6ebcc76ef756d1d10d2fba
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-02-08 11:21:43 -0600

    Skeleton for problem 3833

---
+++ src/main/rust/src/bin/problem3833.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/count-dominant-indices/description/
+pub fn dominant_indices(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", dominant_indices(vec![5, 4, 3])); // 2
+    println!("{}", dominant_indices(vec![4, 1, 2])); // 1
+}
