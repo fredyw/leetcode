307b57100ade6b6d6ae001bfb3dfc5efc20eb5bc
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-02-08 11:38:14 -0600

    Solve problem 3833

--- src/main/rust/src/bin/problem3833.rs
+++ src/main/rust/src/bin/problem3833.rs
@@ -1,6 +1,20 @@
 // https://leetcode.com/problems/count-dominant-indices/description/
 pub fn dominant_indices(nums: Vec<i32>) -> i32 {
-    todo!()
+    let mut averages: Vec<f64> = vec![0.0; nums.len()];
+    let mut i = nums.len() as isize - 1;
+    let mut sum = 0;
+    while i >= 0 {
+        sum += nums[i as usize];
+        averages[i as usize] = sum as f64 / (nums.len() - i as usize) as f64;
+        i -= 1;
+    }
+    let mut answer = 0;
+    for i in 0..nums.len() - 1 {
+        if nums[i] as f64 > averages[i + 1] {
+            answer += 1;
+        }
+    }
+    answer
 }
 
 fn main() {
