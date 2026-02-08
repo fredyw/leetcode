0d745b81c5067a107d355911cd23d52bb2d73dec
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-02-08 10:48:34 -0600

    Skeleton for problem 3827

---
+++ src/main/rust/src/bin/problem3827.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/count-monobit-integers/description/
+pub fn count_monobit(n: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", count_monobit(1)); // 2
+    println!("{}", count_monobit(4)); // 3
+}
