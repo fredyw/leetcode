0546d0d6c32b28b55f61636b9ad5ab1d4337f6f4
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-02-15 19:10:43 -0600

    Skeleton for problem 3842

---
+++ src/main/rust/src/bin/problem3842.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/toggle-light-bulbs/description/
+pub fn toggle_light_bulbs(bulbs: Vec<i32>) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", toggle_light_bulbs(vec![10, 30, 20, 10])); // [20,30]
+    println!("{:?}", toggle_light_bulbs(vec![100, 100])); // []
+}
