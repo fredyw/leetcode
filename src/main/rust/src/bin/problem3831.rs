d6662147482dbe999f4b17b4da0d30004e081f8a
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-02-15 19:23:05 -0600

    Skeleton for problem 3831

---
+++ src/main/rust/src/bin/problem3831.rs
@@ -0,0 +1,27 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+// https://leetcode.com/problems/median-of-a-binary-search-tree-level/
+pub fn level_median(root: Option<Rc<RefCell<TreeNode>>>, level: i32) -> i32 {
+    todo!()
+}
+
+fn main() {}
