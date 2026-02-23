62103deae8517157a241d75ee84d92235cbc23fc
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-02-22 18:31:55 -0600

    Skeleton for problem 3837

---
+++ src/main/rust/src/bin/problem3837.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/delayed-count-of-equal-elements/description/
+pub fn delayed_count(nums: Vec<i32>, k: i32) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", delayed_count(vec![1, 2, 1, 1], 1)); // [2,0,0,0]
+    println!("{:?}", delayed_count(vec![3, 1, 3, 1], 0)); // [1,1,0,0]
+}
