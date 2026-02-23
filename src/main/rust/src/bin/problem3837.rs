bcf262c0383f6c57aa78bc5ad04021f60787ab21
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-02-22 18:41:59 -0600

    Update problem 3837

--- src/main/rust/src/bin/problem3837.rs
+++ src/main/rust/src/bin/problem3837.rs
@@ -6,4 +6,6 @@ pub fn delayed_count(nums: Vec<i32>, k: i32) -> Vec<i32> {
 fn main() {
     println!("{:?}", delayed_count(vec![1, 2, 1, 1], 1)); // [2,0,0,0]
     println!("{:?}", delayed_count(vec![3, 1, 3, 1], 0)); // [1,1,0,0]
+    println!("{:?}", delayed_count(vec![1, 2, 1, 1, 1], 1)); // [3,0,1,0,0]
+    println!("{:?}", delayed_count(vec![1, 2, 1, 1, 1], 0)); // [3,0,2,1,0]
 }
