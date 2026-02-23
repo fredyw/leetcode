b90f04ccde1caa490b19bd8c5b3f3c5ae9ffc616
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-02-22 18:17:51 -0600

    Skeleton for problem 3846

---
+++ src/main/rust/src/bin/problem3846.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/total-distance-to-type-a-string-using-one-finger/description/
+pub fn total_distance(s: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", total_distance("hello".to_string())); // 17
+    println!("{}", total_distance("a".to_string())); // 0
+}
