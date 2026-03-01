b1d943712ad4bd85510f36f7ab1295f582887e2f
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-02-28 23:37:59 -0600

    Skeleton for problem 3856

---
+++ src/main/rust/src/bin/problem3856.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/trim-trailing-vowels/description/
+pub fn trim_trailing_vowels(s: String) -> String {
+    todo!()
+}
+
+fn main() {
+    println!("{]}", trim_trailing_vowels("idea".to_string())); // "id"
+    println!("{]}", trim_trailing_vowels("day".to_string())); // "day"
+    println!("{]}", trim_trailing_vowels("aeiou".to_string())); // ""
+}
