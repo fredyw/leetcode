a3b99a6960a67200b95b64b53e832b0f7357b935
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-02-28 22:00:16 -0600

    Skeleton for problem 3852

---
+++ src/main/rust/src/bin/problem3852.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/smallest-pair-with-different-frequencies/
+pub fn min_distinct_freq_pair(nums: Vec<i32>) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", min_distinct_freq_pair(vec![1, 1, 2, 2, 3, 4])); // [1,3]
+    println!("{:?}", min_distinct_freq_pair(vec![1, 5])); // [-1,-1]
+    println!("{:?}", min_distinct_freq_pair(vec![7])); // [-1,-1]
+}
