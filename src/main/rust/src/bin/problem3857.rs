5508ac9619ba83e295fe0718d1f6f9671c3d99f3
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-03-01 10:01:52 -0600

    Skeleton for problem 3857

---
+++ src/main/rust/src/bin/problem3857.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/minimum-cost-to-split-into-ones/description/
+pub fn min_cost(n: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", min_cost(3)); // 3
+    println!("{}", min_cost(4)); // 6
+}
