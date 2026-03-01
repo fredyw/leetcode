fb85a070f6b3ec1d6c306c64754f46bda74afc8a
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-02-28 22:34:00 -0600

    Skeleton for problem 3831

--- src/main/rust/src/bin/problem3831.rs
+++ src/main/rust/src/bin/problem3831.rs
@@ -1,4 +1,5 @@
 use std::cell::RefCell;
+use std::collections::VecDeque;
 use std::rc::Rc;
 
 #[derive(Debug, PartialEq, Eq)]
@@ -24,4 +25,58 @@ pub fn level_median(root: Option<Rc<RefCell<TreeNode>>>, level: i32) -> i32 {
     todo!()
 }
 
-fn main() {}
+fn create_node(value: i32) -> Option<Rc<RefCell<TreeNode>>> {
+    Some(Rc::new(RefCell::new(TreeNode::new(value))))
+}
+
+fn add_left(parent: &Option<Rc<RefCell<TreeNode>>>, value: i32) -> Option<Rc<RefCell<TreeNode>>> {
+    parent.as_ref().unwrap().borrow_mut().left = create_node(value);
+    parent.as_ref().unwrap().borrow().left.clone()
+}
+
+fn add_right(parent: &Option<Rc<RefCell<TreeNode>>>, value: i32) -> Option<Rc<RefCell<TreeNode>>> {
+    parent.as_ref().unwrap().borrow_mut().right = create_node(value);
+    parent.as_ref().unwrap().borrow().right.clone()
+}
+
+fn to_tree(nodes: Vec<Option<i32>>) -> Option<Rc<RefCell<TreeNode>>> {
+    if nodes.len() == 0 {
+        return None;
+    }
+    let mut deque: VecDeque<Option<Rc<RefCell<TreeNode>>>> = VecDeque::new();
+    let root = create_node(nodes[0].unwrap());
+    deque.push_back(root.clone());
+    let mut i = 1;
+    while i < nodes.len() {
+        let parent = deque.pop_front().unwrap();
+        let mut has_left = false;
+        if let Some(left) = nodes[i] {
+            deque.push_back(add_left(&parent, left));
+            has_left = true;
+        }
+        let mut has_right = false;
+        if i + 1 < nodes.len() {
+            if let Some(right) = nodes[i + 1] {
+                deque.push_back(add_right(&parent, right));
+                has_right = true;
+            }
+        }
+        if !has_left && !has_right {
+            deque.push_back(parent);
+        }
+        i += 2;
+    }
+    root
+}
+
+fn main() {
+    println!(
+        "{}",
+        level_median(to_tree(vec![Some(4), None, Some(5), None, Some(7)]), 2)
+    ); // 7
+    println!(
+        "{}",
+        level_median(to_tree(vec![Some(6), Some(3), Some(8)]), 1)
+    ); // 8
+    println!("{}", level_median(to_tree(vec![Some(2), Some(1)]), 2)); // -1
+}
