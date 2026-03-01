88cdab85c2bf723aeda89048b425e8bc4fd6f0cd
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-02-28 22:21:17 -0600

    Solve problem 3852

--- src/main/rust/src/bin/problem3852.rs
+++ src/main/rust/src/bin/problem3852.rs
@@ -1,10 +1,25 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/smallest-pair-with-different-frequencies/
-pub fn min_distinct_freq_pair(nums: Vec<i32>) -> Vec<i32> {
-    todo!()
+pub fn min_distinct_freq_pair(mut nums: Vec<i32>) -> Vec<i32> {
+    nums.sort_unstable();
+    let mut map: HashMap<i32, i32> = HashMap::new();
+    for i in 0..nums.len() {
+        *map.entry(nums[i]).or_insert(0) += 1;
+    }
+    let first_count = map.get(&nums[0]).unwrap();
+    for i in 0..nums.len() {
+        let count = map.get(&nums[i]).unwrap();
+        if first_count != count {
+            return vec![nums[0], nums[i]];
+        }
+    }
+    vec![-1, -1]
 }
 
 fn main() {
     println!("{:?}", min_distinct_freq_pair(vec![1, 1, 2, 2, 3, 4])); // [1,3]
     println!("{:?}", min_distinct_freq_pair(vec![1, 5])); // [-1,-1]
     println!("{:?}", min_distinct_freq_pair(vec![7])); // [-1,-1]
+    println!("{:?}", min_distinct_freq_pair(vec![1, 5, 6, 6])); // [1, 6]
 }
