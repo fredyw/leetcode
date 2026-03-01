afbe32177d10960a91d8ea4be55707227bd31bb0
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-03-01 10:19:36 -0600

    Solve problem 3857

--- src/main/rust/src/bin/problem3857.rs
+++ src/main/rust/src/bin/problem3857.rs
@@ -1,9 +1,13 @@
 // https://leetcode.com/problems/minimum-cost-to-split-into-ones/description/
 pub fn min_cost(n: i32) -> i32 {
-    todo!()
+    if n == 1 {
+        return 0;
+    }
+    min_cost(n - 1) + (n - 1)
 }
 
 fn main() {
     println!("{}", min_cost(3)); // 3
     println!("{}", min_cost(4)); // 6
+    println!("{}", min_cost(500)); // 6
 }
