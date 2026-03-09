2c82dce34ddd37caaf7f45750fc86aa070e8b66d
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-03-08 23:03:02 -0500

    Solve problem 3861

--- src/main/rust/src/bin/problem3861.rs
+++ src/main/rust/src/bin/problem3861.rs
@@ -1,6 +1,16 @@
 // https://leetcode.com/problems/minimum-capacity-box/description/
 pub fn minimum_index(capacity: Vec<i32>, item_size: i32) -> i32 {
-    todo!()
+    let mut answer = -1;
+    let mut min_capacity = i32::MAX;
+    for i in 0..capacity.len() {
+        if capacity[i] >= item_size {
+            if min_capacity == i32::MAX || min_capacity > capacity[i] {
+                answer = i as i32;
+                min_capacity = capacity[i];
+            }
+        }
+    }
+    answer
 }
 
 fn main() {
