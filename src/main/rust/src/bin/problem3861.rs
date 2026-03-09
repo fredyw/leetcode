ad519c95b5e875f3ec8795fe025c4784e4fdb66a
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-03-08 22:56:11 -0500

    Skeleton for problem 3861

---
+++ src/main/rust/src/bin/problem3861.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/minimum-capacity-box/description/
+pub fn minimum_index(capacity: Vec<i32>, item_size: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", minimum_index(vec![1, 5, 3, 7], 3)); // 2
+    println!("{}", minimum_index(vec![3, 5, 4, 3], 2)); // 0
+    println!("{}", minimum_index(vec![4], 5)); // -1
+}
