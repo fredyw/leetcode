c1445e4affdae9ccba5f04cb9ce128be80d4a160
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-03-14 00:34:26 -0500

    Skeleton for problem 3865

---
+++ src/main/rust/src/bin/problem3865.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/reverse-k-subarrays/description/
+pub fn reverse_subarrays(nums: Vec<i32>, k: i32) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", reverse_subarrays(vec![1, 2, 4, 3, 5, 6], 3)); // [2,1,3,4,6,5]
+    println!("{:?}", reverse_subarrays(vec![5, 4, 4, 2], 1)); // [2,4,4,5]
+}
