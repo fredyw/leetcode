7fb391abf45d46ab3169a65f65923a3ef8ae6154
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-03-15 10:17:58 -0500

    Solve problem 3866

--- src/main/rust/src/bin/problem3866.rs
+++ src/main/rust/src/bin/problem3866.rs
@@ -1,6 +1,21 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/first-unique-even-element/
 pub fn first_unique_even(nums: Vec<i32>) -> i32 {
-    todo!()
+    let mut map: HashMap<i32, i32> = HashMap::new();
+    for num in &nums {
+        *map.entry(*num).or_insert(0) += 1;
+    }
+    for num in &nums {
+        if num % 2 == 0 {
+            if let Some(count) = map.get(num) {
+                if *count == 1 {
+                    return *num;
+                }
+            }
+        }
+    }
+    -1
 }
 
 fn main() {
