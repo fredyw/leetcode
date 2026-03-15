186441bc9ab70fba50ca166a1d0633622934d2a5
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-03-15 00:01:46 -0500

    Skeleton for problem 3713

---
+++ src/main/rust/src/bin/problem3713.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/longest-balanced-substring-i/description/
+pub fn longest_balanced(s: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", longest_balanced("abbac".to_string())); // 4
+    println!("{}", longest_balanced("zzabccy".to_string())); // 4
+    println!("{}", longest_balanced("aba".to_string())); // 2
+}
