87cb5ab637b7c79ad55929c4d5d5eb5e0e03be5c
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-03-14 23:18:55 -0500

    Update problem 3837

--- src/main/rust/src/bin/problem3837.rs
+++ src/main/rust/src/bin/problem3837.rs
@@ -1,6 +1,26 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/delayed-count-of-equal-elements/description/
 pub fn delayed_count(nums: Vec<i32>, k: i32) -> Vec<i32> {
-    todo!()
+    let mut map_total_count: HashMap<i32, i32> = HashMap::new();
+    for num in &nums {
+        *map_total_count.entry(*num).or_insert(0) += 1;
+    }
+    let mut answer = Vec::with_capacity(nums.len());
+    let mut map_count: HashMap<i32, i32> = HashMap::new();
+    for i in 0..nums.len() {
+        *map_count.entry(nums[i]).or_insert(0) += 1;
+        let mut j = i + 1;
+        let mut count = 0;
+        while j < nums.len() && j < i + (k as usize) + 1 {
+            if nums[j] == nums[i] {
+                count += 1;
+            }
+            j += 1;
+        }
+        answer.push(map_total_count[&nums[i]] - (map_count[&nums[i]] + count));
+    }
+    answer
 }
 
 fn main() {
