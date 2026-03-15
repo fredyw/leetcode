514fa827cb6143712352f737e37dd0a95bb0d916
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-03-15 10:11:53 -0500

    Skeleton for problem 3866

---
+++ src/main/rust/src/bin/problem3866.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/first-unique-even-element/
+pub fn first_unique_even(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", first_unique_even(vec![3, 4, 2, 5, 4, 6])); // 2
+    println!("{}", first_unique_even(vec![4, 4])); // -1
+}
