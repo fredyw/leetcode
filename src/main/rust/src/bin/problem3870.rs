bba0a6e2c3279cd9ef65c78225750746b3ee39f5
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-03-15 09:53:06 -0500

    Skeleton for problem 3870

---
+++ src/main/rust/src/bin/problem3870.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/count-commas-in-range/description/
+pub fn count_commas(n: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", count_commas(1002)); // 3
+    println!("{}", count_commas(998)); // 0
+}
