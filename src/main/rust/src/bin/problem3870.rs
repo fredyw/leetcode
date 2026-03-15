65917488347089c83f8d5465a38a1a5ea6ec405a
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-03-15 10:10:40 -0500

    Solve problem 3870

--- src/main/rust/src/bin/problem3870.rs
+++ src/main/rust/src/bin/problem3870.rs
@@ -1,9 +1,14 @@
 // https://leetcode.com/problems/count-commas-in-range/description/
 pub fn count_commas(n: i32) -> i32 {
-    todo!()
+    if n < 1000 {
+        0
+    } else {
+        n - 1000 + 1
+    }
 }
 
 fn main() {
     println!("{}", count_commas(1002)); // 3
     println!("{}", count_commas(998)); // 0
+    println!("{}", count_commas(10003)); // 9004
 }
