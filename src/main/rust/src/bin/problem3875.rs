bba34610bfcbe01898651135609c66b315d6d988
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-03-24 11:26:47 -0500

    Update problem 3875

--- src/main/rust/src/bin/problem3875.rs
+++ src/main/rust/src/bin/problem3875.rs
@@ -6,4 +6,7 @@ pub fn uniform_array(nums1: Vec<i32>) -> bool {
 fn main() {
     println!("{}", uniform_array(vec![2, 3])); // true
     println!("{}", uniform_array(vec![4, 6])); // true
+    println!("{}", uniform_array(vec![5, 10, 15])); // true
+    println!("{}", uniform_array(vec![4, 8, 12])); // true
+    println!("{}", uniform_array(vec![1, 2, 4])); // true
 }
