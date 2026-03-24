3faa3fb85f16e709519cb56cc05ad65da50817d1
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-03-24 11:20:10 -0500

    Skeleton for problem 3875

---
+++ src/main/rust/src/bin/problem3875.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/construct-uniform-parity-array-i/description/
+pub fn uniform_array(nums1: Vec<i32>) -> bool {
+    todo!()
+}
+
+fn main() {
+    println!("{}", uniform_array(vec![2, 3])); // true
+    println!("{}", uniform_array(vec![4, 6])); // true
+}
