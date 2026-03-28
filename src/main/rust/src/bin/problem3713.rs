45803309bca6d011d3e55f58160a466f8efefed1
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-03-28 17:46:07 -0500

    Solve problem 3713

--- src/main/rust/src/bin/problem3713.rs
+++ src/main/rust/src/bin/problem3713.rs
@@ -1,6 +1,33 @@
 // https://leetcode.com/problems/longest-balanced-substring-i/description/
 pub fn longest_balanced(s: String) -> i32 {
-    todo!()
+    fn is_balanced(counts: &Vec<i32>) -> bool {
+        let mut c = 0;
+        for count in counts {
+            if *count != 0 {
+                if c == 0 {
+                    c = *count;
+                } else {
+                    if c != *count {
+                        return false;
+                    }
+                }
+            }
+        }
+        true
+    }
+
+    let bytes = s.as_bytes();
+    let mut answer = 0;
+    for i in 0..bytes.len() {
+        let mut counts: Vec<i32> = vec![0; 26];
+        for j in i..bytes.len() {
+            counts[bytes[j] as usize - b'a' as usize] += 1;
+            if is_balanced(&counts) {
+                answer = answer.max(j as i32 - i as i32 + 1);
+            }
+        }
+    }
+    answer
 }
 
 fn main() {
