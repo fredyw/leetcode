72dc7e231d0f7295349d997439dc58f2e6b4345f
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-03-28 17:48:18 -0500

    Skeleton for problem 3880

---
+++ src/main/rust/src/bin/problem3880.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/minimum-absolute-difference-between-two-values/description/
+pub fn min_absolute_difference(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", min_absolute_difference(vec![1, 0, 0, 2, 0, 1])); // 2
+    println!("{}", min_absolute_difference(vec![1, 0, 1, 0])); // -1
+}
