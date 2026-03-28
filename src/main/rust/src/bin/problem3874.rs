295b03b34aee384a872aa13374d5987df5119e75
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-03-28 17:56:41 -0500

    Skeleton for problem 3874

---
+++ src/main/rust/src/bin/problem3874.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/valid-subarrays-with-exactly-one-peak/description/
+pub fn valid_subarrays(nums: Vec<i32>, k: i32) -> i64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", valid_subarrays(vec![1, 3, 2], 1)); // 4
+    println!("{}", valid_subarrays(vec![7, 8, 9], 2)); // 0
+    println!("{}", valid_subarrays(vec![4, 3, 5, 1], 2)); // 6
+}
