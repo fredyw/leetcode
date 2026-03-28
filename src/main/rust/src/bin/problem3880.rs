ae480211cb1cfeaa4ea93d04f4c9dd82173be6be
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-03-28 17:54:17 -0500

    Solve problem 3880

--- src/main/rust/src/bin/problem3880.rs
+++ src/main/rust/src/bin/problem3880.rs
@@ -1,6 +1,18 @@
 // https://leetcode.com/problems/minimum-absolute-difference-between-two-values/description/
 pub fn min_absolute_difference(nums: Vec<i32>) -> i32 {
-    todo!()
+    let mut answer = i32::MAX;
+    for i in 0..nums.len() {
+        for j in i + 1..nums.len() {
+            if (nums[i] == 1 && nums[j] == 2) || (nums[i] == 2 && nums[j] == 1) {
+                answer = answer.min(j as i32 - i as i32);
+            }
+        }
+    }
+    if answer == i32::MAX {
+        -1
+    } else {
+        answer
+    }
 }
 
 fn main() {
