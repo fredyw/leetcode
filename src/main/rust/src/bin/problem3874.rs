d476ae9781b73454987b7f8bf26eee332cfab1cf
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-03-28 19:20:07 -0500

    Update problem 3874

--- src/main/rust/src/bin/problem3874.rs
+++ src/main/rust/src/bin/problem3874.rs
@@ -7,4 +7,5 @@ fn main() {
     println!("{}", valid_subarrays(vec![1, 3, 2], 1)); // 4
     println!("{}", valid_subarrays(vec![7, 8, 9], 2)); // 0
     println!("{}", valid_subarrays(vec![4, 3, 5, 1], 2)); // 6
+    println!("{}", valid_subarrays(vec![1, 4, 2, 5, 2], 5)); // 8
 }
