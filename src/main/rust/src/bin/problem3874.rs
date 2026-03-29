52677d502753336b6b5268c029d7dbc7f4bd5272
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-03-29 15:17:42 -0500

    Solve problem 3874

--- src/main/rust/src/bin/problem3874.rs
+++ src/main/rust/src/bin/problem3874.rs
@@ -8,34 +8,25 @@ pub fn valid_subarrays(nums: Vec<i32>, k: i32) -> i64 {
             peaks.push(i as i64);
         }
     }
-    let mut peak_index: isize = 0;
-    for i in 1..nums.len() - 1 {
-        if nums[i] > nums[i - 1] && nums[i] > nums[i + 1] {
-            let left = if peak_index - 1 >= 0 {
-                (i as i64).min(k + 1).min(peaks[peak_index as usize - 1])
-            } else {
-                (i as i64).min(k + 1)
-            } + 1;
-            let right = if peak_index + 1 < peaks.len() as isize {
-                (nums.len() as i64 - i as i64)
-                    .min(k + 1)
-                    .min(peaks[peak_index as usize + 1] - 1)
-            } else {
-                (nums.len() as i64 - i as i64).min(k + 1)
-            };
-            println!("i: {i}, left = {left}, right = {right}");
-            answer += left * right;
-            peak_index += 1;
-        }
+    for (idx, &i) in peaks.iter().enumerate() {
+        let prev_peak = if idx > 0 { peaks[idx - 1] + 1 } else { 0 };
+        let left = i - 0.max(i - k).max(prev_peak) + 1;
+        let next_peak = if idx + 1 < peaks.len() {
+            peaks[idx + 1] - 1
+        } else {
+            nums.len() as i64 - 1
+        };
+        let right = (nums.len() as i64 - 1).min(i + k).min(next_peak) - i + 1;
+        answer += left * right;
     }
     answer
 }
 
 fn main() {
-    // println!("{}", valid_subarrays(vec![1, 3, 2], 1)); // 4
-    // println!("{}", valid_subarrays(vec![7, 8, 9], 2)); // 0
-    // println!("{}", valid_subarrays(vec![4, 3, 5, 1], 2)); // 6
-    // println!("{}", valid_subarrays(vec![1, 4, 2, 5, 2], 5)); // 8
+    println!("{}", valid_subarrays(vec![1, 3, 2], 1)); // 4
+    println!("{}", valid_subarrays(vec![7, 8, 9], 2)); // 0
+    println!("{}", valid_subarrays(vec![4, 3, 5, 1], 2)); // 6
+    println!("{}", valid_subarrays(vec![1, 4, 2, 5, 2], 5)); // 8
     println!("{}", valid_subarrays(vec![1, 3, 2, 4, 3, 5, 1, 4, 2], 2)); // 16
-                                                                         // println!("{}", valid_subarrays(vec![1, 3, 2, 4, 3, 5, 1, 4, 2], 3)); // 16
+    println!("{}", valid_subarrays(vec![1, 3, 2, 4, 3, 5, 1, 4, 2], 3)); // 16
 }
