0bc20f079d096ea1563530f8e691179f2c4a67f1
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-03-29 15:20:22 -0500

    Skeleton for problem 3884

---
+++ src/main/rust/src/bin/problem3884.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/first-matching-character-from-both-ends/description/
+pub fn first_matching_index(s: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", first_matching_index("abcacbd".to_string())); // 1
+    println!("{}", first_matching_index("abc".to_string())); // 1
+    println!("{}", first_matching_index("abcdab".to_string())); // -1
+}
