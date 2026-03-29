0a1dec7a0ccf6a61f2f445908b840b1576b4ad03
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-03-29 15:36:03 -0500

    Solve problem 3884

--- src/main/rust/src/bin/problem3884.rs
+++ src/main/rust/src/bin/problem3884.rs
@@ -1,6 +1,12 @@
 // https://leetcode.com/problems/first-matching-character-from-both-ends/description/
 pub fn first_matching_index(s: String) -> i32 {
-    todo!()
+    let bytes = s.as_bytes();
+    for i in 0..=bytes.len() / 2 {
+        if bytes[i] == bytes[bytes.len() - i - 1] {
+            return i as i32;
+        }
+    }
+    -1
 }
 
 fn main() {
