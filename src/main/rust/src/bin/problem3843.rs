1e01ad14c74f4791017cc6bb72fd536d021eff5b
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-03-29 15:41:13 -0500

    Skeleton for problem 3843

---
+++ src/main/rust/src/bin/problem3843.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/first-element-with-unique-frequency/description/
+pub fn first_unique_freq(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", first_unique_freq(vec![20, 10, 30, 30])); // 30
+    println!("{}", first_unique_freq(vec![20, 20, 10, 30, 30, 30])); // 20
+    println!("{}", first_unique_freq(vec![10, 10, 20, 20])); // -1
+}
