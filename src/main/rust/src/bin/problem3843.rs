64c672a101ff8955fc63319123e5bdbc8f3f18c7
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-04-01 17:43:09 -0500

    Solve problem 3843

--- src/main/rust/src/bin/problem3843.rs
+++ src/main/rust/src/bin/problem3843.rs
@@ -1,6 +1,28 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/first-element-with-unique-frequency/description/
 pub fn first_unique_freq(nums: Vec<i32>) -> i32 {
-    todo!()
+    let mut freqs: HashMap<i32, i32> = HashMap::new();
+    for num in &nums {
+        *freqs.entry(*num).or_insert(0) += 1;
+    }
+    let mut unique_counts: HashMap<i32, bool> = HashMap::new();
+    for (_, count) in &freqs {
+        if unique_counts.contains_key(count) {
+            unique_counts.insert(*count, false);
+        } else {
+            unique_counts.insert(*count, true);
+        }
+    }
+    for &num in &nums {
+        let count = freqs.get(&num).unwrap();
+        if let Some(unique) = unique_counts.get(count) {
+            if *unique {
+                return num;
+            }
+        }
+    }
+    -1
 }
 
 fn main() {
