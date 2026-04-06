786742139fcb221cec01996fae187591a2a588cb
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-04-06 15:35:31 -0500

    Skeleton for problem 3849

---
+++ src/main/rust/src/bin/problem3849.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/maximum-bitwise-xor-after-rearrangement/description/
+pub fn maximum_xor(s: String, t: String) -> String {
+    todo!()
+}
+
+fn main() {
+    println!("{}", maximum_xor("101".to_string(), "011".to_string())); // "110"
+    println!("{}", maximum_xor("0110".to_string(), "1110".to_string())); // "1101"
+    println!("{}", maximum_xor("0101".to_string(), "1001".to_string())); // "1111"
+}
