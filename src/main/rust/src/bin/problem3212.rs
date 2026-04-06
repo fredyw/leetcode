12389fa5dfafdad3dba35f7eb8e78fd705bee898
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-04-06 15:32:39 -0500

    Solve problem 3212

--- src/main/rust/src/bin/problem3212.rs
+++ src/main/rust/src/bin/problem3212.rs
@@ -3,26 +3,25 @@ pub fn number_of_submatrices(grid: Vec<Vec<char>>) -> i32 {
     let mut answer = 0;
     let num_rows = grid.len();
     let num_cols = if num_rows > 0 { grid[0].len() } else { 0 };
-    let mut count_x = 0;
-    let mut count_y = 0;
+    let mut counts: Vec<Vec<(i32, i32)>> = vec![vec![(0, 0); num_cols]; num_rows];
     for row in 0..num_rows {
-        if grid[row][0] == 'X' {
-            count_x += 1;
-        } else if grid[row][0] == 'Y' {
-            count_y += 1;
-        }
-        if count_x > 0 && count_x == count_y {
-            answer += 1;
-        }
-    }
-    for col in 0..num_cols {
-        if grid[0][col] == 'X' {
-            count_x += 1;
-        } else if grid[0][col] == 'Y' {
-            count_y += 1;
-        }
-        if count_x > 0 && count_x == count_y {
-            answer += 1;
+        let mut count_x = 0;
+        let mut count_y = 0;
+        for col in 0..num_cols {
+            if grid[row][col] == 'X' {
+                count_x += 1;
+            } else if grid[row][col] == 'Y' {
+                count_y += 1;
+            }
+            if row > 0 {
+                counts[row][col].0 += counts[row - 1][col].0;
+                counts[row][col].1 += counts[row - 1][col].1;
+            }
+            counts[row][col].0 += count_x;
+            counts[row][col].1 += count_y;
+            if counts[row][col].0 > 0 && counts[row][col].0 == counts[row][col].1 {
+                answer += 1;
+            }
         }
     }
     answer
