ab28ce28017c4cd6874e9d5392b7114ab404a581
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-04-06 16:28:43 -0500

    Skeleton for problem 3719

---
+++ src/main/rust/src/bin/problem3719.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/longest-balanced-subarray-i/description/
+pub fn longest_balanced(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", longest_balanced(vec![2, 5, 4, 3])); // 4
+    println!("{}", longest_balanced(vec![3, 2, 2, 5, 4])); // 5
+    println!("{}", longest_balanced(vec![1, 2, 3, 2])); // 3
+}
