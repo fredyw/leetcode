c15a443e0d812b2f78a0bfa2a20d5616774d7a51
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-04-06 16:53:59 -0500

    Solve problem 3719

--- src/main/rust/src/bin/problem3719.rs
+++ src/main/rust/src/bin/problem3719.rs
@@ -1,6 +1,23 @@
+use std::collections::HashSet;
+
 // https://leetcode.com/problems/longest-balanced-subarray-i/description/
 pub fn longest_balanced(nums: Vec<i32>) -> i32 {
-    todo!()
+    let mut answer = 0;
+    for i in 0..nums.len() {
+        let mut odds: HashSet<i32> = HashSet::new();
+        let mut evens: HashSet<i32> = HashSet::new();
+        for j in i..nums.len() {
+            if nums[j] % 2 == 0 {
+                evens.insert(nums[j]);
+            } else {
+                odds.insert(nums[j]);
+            }
+            if evens.len() == odds.len() {
+                answer = answer.max((j - i) as i32 + 1);
+            }
+        }
+    }
+    answer
 }
 
 fn main() {
