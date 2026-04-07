da9f0157404cf225d5faa808d78da6a8e089c1d4
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-04-07 13:37:27 -0500

    Skeleton for problem 3851

---
+++ src/main/rust/src/bin/problem3851.rs
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/maximum-requests-without-violating-the-limit/description/
+pub fn max_requests(requests: Vec<Vec<i32>>, k: i32, window: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        max_requests(vec![vec![1, 1], vec![2, 1], vec![1, 7], vec![2, 8]], 1, 4)
+    ); // 4
+    println!(
+        "{}",
+        max_requests(vec![vec![1, 2], vec![1, 5], vec![1, 2], vec![1, 6]], 2, 5)
+    ); // 4
+    println!(
+        "{}",
+        max_requests(vec![vec![1, 1], vec![2, 5], vec![1, 2], vec![3, 9]], 1, 1)
+    ); // 4
+}
