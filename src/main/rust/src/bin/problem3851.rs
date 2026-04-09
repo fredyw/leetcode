dca0a73a9bf065339e50cfd26d6797af855e5eb7
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-04-09 14:57:56 -0500

    Update problem 3851

--- src/main/rust/src/bin/problem3851.rs
+++ src/main/rust/src/bin/problem3851.rs
@@ -16,4 +16,20 @@ fn main() {
         "{}",
         max_requests(vec![vec![1, 1], vec![2, 5], vec![1, 2], vec![3, 9]], 1, 1)
     ); // 4
+    println!(
+        "{}",
+        max_requests(
+            vec![vec![1, 1], vec![1, 3], vec![1, 5], vec![1, 7], vec![1, 9]],
+            2,
+            3
+        )
+    ); // 7
+    println!(
+        "{}",
+        max_requests(
+            vec![vec![1, 1], vec![1, 2], vec![1, 3], vec![1, 4], vec![1, 5]],
+            2,
+            2
+        )
+    ); // 4
 }
