d0d855f073b2c1cab9aa354d0720f3a944a03318
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-04-13 18:30:40 -0500

    Update problem 3851

--- src/main/rust/src/bin/problem3851.rs
+++ src/main/rust/src/bin/problem3851.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
 
 // https://leetcode.com/problems/maximum-requests-without-violating-the-limit/description/
 pub fn max_requests(requests: Vec<Vec<i32>>, k: i32, window: i32) -> i32 {
@@ -11,36 +11,53 @@ pub fn max_requests(requests: Vec<Vec<i32>>, k: i32, window: i32) -> i32 {
     for (_, time) in &mut map {
         time.sort_unstable();
     }
-    0
+    let mut answer = 0;
+    for (_, time) in &map {
+        let mut deque: VecDeque<i32> = VecDeque::new();
+        for t in time {
+            while let Some(front) = deque.front() {
+                if *front < *t - k {
+                    deque.pop_front();
+                } else {
+                    break;
+                }
+            }
+            if deque.len() < k as usize {
+                answer += 1;
+                deque.push_back(*t);
+            }
+        }
+    }
+    answer
 }
 
 fn main() {
-    println!(
-        "{}",
-        max_requests(vec![vec![1, 1], vec![2, 1], vec![1, 7], vec![2, 8]], 1, 4)
-    ); // 4
+    // println!(
+    //     "{}",
+    //     max_requests(vec![vec![1, 1], vec![2, 1], vec![1, 7], vec![2, 8]], 1, 4)
+    // ); // 4
     println!(
         "{}",
         max_requests(vec![vec![1, 2], vec![1, 5], vec![1, 2], vec![1, 6]], 2, 5)
-    ); // 4
-    println!(
-        "{}",
-        max_requests(vec![vec![1, 1], vec![2, 5], vec![1, 2], vec![3, 9]], 1, 1)
-    ); // 4
-    println!(
-        "{}",
-        max_requests(
-            vec![vec![1, 1], vec![1, 3], vec![1, 5], vec![1, 7], vec![1, 9]],
-            2,
-            3
-        )
-    ); // 7
-    println!(
-        "{}",
-        max_requests(
-            vec![vec![1, 1], vec![1, 2], vec![1, 3], vec![1, 4], vec![1, 5]],
-            2,
-            2
-        )
-    ); // 4
+    ); // 2
+       // println!(
+       //     "{}",
+       //     max_requests(vec![vec![1, 1], vec![2, 5], vec![1, 2], vec![3, 9]], 1, 1)
+       // ); // 3
+       // println!(
+       //     "{}",
+       //     max_requests(
+       //         vec![vec![1, 1], vec![1, 3], vec![1, 5], vec![1, 7], vec![1, 9]],
+       //         2,
+       //         3
+       //     )
+       // ); // 5
+       // println!(
+       //     "{}",
+       //     max_requests(
+       //         vec![vec![1, 1], vec![1, 2], vec![1, 3], vec![1, 4], vec![1, 5]],
+       //         2,
+       //         2
+       //     )
+       // ); // 4
 }
