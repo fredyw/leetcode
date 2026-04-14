97b6556421d656d32cb225c33c7aedce36fa2b2f
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-04-14 10:30:12 -0500

    Skeleton for problem 3894

---
+++ src/main/rust/src/bin/problem3894.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/traffic-signal-color/description/
+pub fn traffic_signal(timer: i32) -> String {
+    todo!()
+}
+
+fn main() {
+    println!("{}", traffic_signal(60)); // "Red"
+    println!("{}", traffic_signal(5)); // "Invalid"
+}
