0b208deda208cfa12dee1e74ae0a49371d93fa30
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-04-15 16:35:51 -0500

    Solve problem 3689

--- src/main/rust/src/bin/problem3689.rs
+++ src/main/rust/src/bin/problem3689.rs
@@ -1,6 +1,8 @@
 // https://leetcode.com/problems/maximum-total-subarray-value-i/description/
 pub fn max_total_value(nums: Vec<i32>, k: i32) -> i64 {
-    todo!()
+    let min = *nums.iter().min().unwrap() as i64;
+    let max = *nums.iter().max().unwrap() as i64;
+    (max - min) * k as i64
 }
 
 fn main() {
