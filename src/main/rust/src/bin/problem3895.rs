7b588c6a308a2712ccd513aa10e8431baef6c554
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-04-15 10:43:06 -0500

    Skeleton for problem 3895

---
+++ src/main/rust/src/bin/problem3895.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/count-digit-appearances/description/
+pub fn count_digit_occurrences(nums: Vec<i32>, digit: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", count_digit_occurrences(vec![12, 54, 32, 22], 2)); // 4
+    println!("{}", count_digit_occurrences(vec![1, 34, 7], 9)); // 0
+}
