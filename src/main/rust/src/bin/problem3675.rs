3e2d5ab3b9ba373357127026f00b15caceb06c47
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-04-17 13:49:18 -0500

    Update problem 3675

--- src/main/rust/src/bin/problem3675.rs
+++ src/main/rust/src/bin/problem3675.rs
@@ -1,9 +1,21 @@
 // https://leetcode.com/problems/minimum-operations-to-transform-string/description/
 pub fn min_operations(s: String) -> i32 {
-    todo!()
+    let mut answer = 0;
+    let mut chars: Vec<bool> = vec![false; 26];
+    for c in s.chars() {
+        if c != 'a' {
+            if !chars[c as usize - 'a' as usize] {
+                answer += 1;
+            }
+            chars[c as usize - 'a' as usize] = true;
+        }
+    }
+    answer
 }
 
 fn main() {
     println!("{}", min_operations(String::from("yz"))); // 2
     println!("{}", min_operations(String::from("a"))); // 0
+    println!("{}", min_operations(String::from("b"))); // 25
+    println!("{}", min_operations(String::from("bz"))); // 25
 }
