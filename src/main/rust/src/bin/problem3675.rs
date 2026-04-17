b4a60a6a576ab233833bb3904964b11fdc05aadd
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-04-17 10:10:20 -0500

    Skeleton for problem 3675

---
+++ src/main/rust/src/bin/problem3675.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/minimum-operations-to-transform-string/description/
+pub fn min_operations(s: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", min_operations(String::from("yz"))); // 2
+    println!("{}", min_operations(String::from("a"))); // 0
+}
