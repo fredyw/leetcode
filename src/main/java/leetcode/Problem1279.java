bac95731d32978fe434a15f70f8c83cea57af360
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-04-17 09:55:41 -0500

    Solve problem 1279

--- src/main/java/leetcode/Problem1279.java
+++ src/main/java/leetcode/Problem1279.java
@@ -3,16 +3,26 @@ package leetcode;
 // https://leetcode.com/problems/traffic-light-controlled-intersection/description/
 public class Problem1279 {
     public static class TrafficLight {
+        private int greenRoad;
+
         public TrafficLight() {
+            this.greenRoad = 1;
         }
 
         public void carArrived(
             int carId,           // ID of the car
-            int roadId,          // ID of the road the car travels on. Can be 1 (road A) or 2 (road B)
+            int roadId,          // ID of the road the car travels on (1 or 2)
             int direction,       // Direction of the car
-            Runnable turnGreen,  // Use turnGreen.run() to turn light to green on current road
-            Runnable crossCar    // Use crossCar.run() to make car cross the intersection
+            Runnable turnGreen,  // Turns light to green on current road
+            Runnable crossCar    // Makes car cross the intersection
         ) {
+            synchronized(this) {
+                if (greenRoad != roadId) {
+                    turnGreen.run();
+                    greenRoad = roadId;
+                }
+                crossCar.run();
+            }
         }
     }
 }
