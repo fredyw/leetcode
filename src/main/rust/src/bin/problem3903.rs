77c2fb94b6dce00040a16df667619e150164b80c
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-04-19 22:12:27 -0500

    Skeleton for problem 3903

---
+++ src/main/rust/src/bin/problem3903.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/smallest-stable-index-i/description/
+pub fn first_stable_index(nums: Vec<i32>, k: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", first_stable_index(vec![5, 0, 1, 4], 3)); // 3
+    println!("{}", first_stable_index(vec![3, 2, 1], 1)); // -1
+    println!("{}", first_stable_index(vec![0], 0)); // 0
+}
