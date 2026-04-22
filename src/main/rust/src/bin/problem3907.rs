4836b7d57a088ea17c226322594e3b77d57f1074
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-04-22 10:45:56 -0500

    Skeleton for problem 3907

---
+++ src/main/rust/src/bin/problem3907.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/count-smaller-elements-with-opposite-parity/description/
+pub fn count_smaller_opposite_parity(nums: Vec<i32>) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", count_smaller_opposite_parity(vec![5, 2, 4, 1, 3])); // [2,1,2,0,0]
+    println!("{:?}", count_smaller_opposite_parity(vec![4, 4, 1])); // [1,1,0]
+    println!("{:?}", count_smaller_opposite_parity(vec![7])); // [0]
+}
