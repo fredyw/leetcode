e4ec7b8e389c80db620e3a5d2389e805af7790e5
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-04-26 00:32:48 -0500

    Skeleton for problem 3908

---
+++ src/main/rust/src/bin/problem3908.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/valid-digit-number/description/
+pub fn valid_digit(n: i32, x: i32) -> bool {
+    todo!()
+}
+
+fn main() {
+    println!("{}", valid_digit(101, 0)); // true
+    println!("{}", valid_digit(232, 2)); // false
+    println!("{}", valid_digit(5, 1)); // false
+}
