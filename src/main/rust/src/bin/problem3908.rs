4c64a28ba0feae13db72b516fc018b8befbf04f0
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-04-26 01:11:41 -0500

    Solve problem 3908

--- src/main/rust/src/bin/problem3908.rs
+++ src/main/rust/src/bin/problem3908.rs
@@ -1,6 +1,8 @@
 // https://leetcode.com/problems/valid-digit-number/description/
 pub fn valid_digit(n: i32, x: i32) -> bool {
-    todo!()
+    let x = std::char::from_digit(x as u32, 10).unwrap();
+    let n = n.to_string();
+    n.contains(x) && !n.starts_with(x)
 }
 
 fn main() {
