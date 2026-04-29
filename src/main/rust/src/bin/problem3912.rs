670a8781c56da44550a6d3513e38477d92a5a2ef
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-04-29 10:10:45 -0500

    Skeleton for problem 3912

---
+++ src/main/rust/src/bin/problem3912.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/valid-elements-in-an-array/description/
+pub fn find_valid_elements(nums: Vec<i32>) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", find_valid_elements(vec![1, 2, 4, 2, 3, 2])); // [1,2,4,3,2]
+    println!("{:?}", find_valid_elements(vec![5, 5, 5, 5])); // [5,5]
+    println!("{:?}", find_valid_elements(vec![1])); // [1]
+}
