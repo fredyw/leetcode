fd9437b387eb01c43fca5d8d97b5d10bc6e99ab3
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-05-01 19:57:07 -0500

    Solve problem 3025

--- src/main/rust/src/bin/problem3025.rs
+++ src/main/rust/src/bin/problem3025.rs
@@ -1,6 +1,30 @@
 // https://leetcode.com/problems/find-the-number-of-ways-to-place-people-i/description/
-pub fn number_of_pairs(points: Vec<Vec<i32>>) -> i32 {
-    todo!()
+pub fn number_of_pairs(mut points: Vec<Vec<i32>>) -> i32 {
+    let mut answer = 0;
+    points.sort_by(|a, b| {
+        if a[0] == b[0] {
+            b[1].cmp(&a[1])
+        } else {
+            a[0].cmp(&b[0])
+        }
+    });
+    for i in 0..points.len() {
+        let y_a = points[i][1];
+        let mut max_y_seen = i32::MIN;
+        for j in (i + 1)..points.len() {
+            let y_b = points[j][1];
+            if y_b <= y_a {
+                if y_b > max_y_seen {
+                    answer += 1;
+                    max_y_seen = y_b;
+                }
+            }
+            if max_y_seen == y_a {
+                break;
+            }
+        }
+    }
+    answer
 }
 
 fn main() {
