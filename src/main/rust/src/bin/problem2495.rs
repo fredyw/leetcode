0ba67384f0308c21a3d4329d242c7490e46bc31d
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-05-02 20:49:04 -0500

    Skeleton for problem 2495

---
+++ src/main/rust/src/bin/problem2495.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/number-of-subarrays-having-even-product/description/
+pub fn even_product(nums: Vec<i32>) -> i64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", even_product(vec![9, 6, 7, 13])); // 6
+    println!("{}", even_product(vec![7, 3, 5])); // 0
+}
