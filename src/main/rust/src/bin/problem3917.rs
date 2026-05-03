9089d8faf82cbb67025f7c06b53971f07831cd34
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-05-03 12:42:27 -0500

    Skeleton for problem 3917

---
+++ src/main/rust/src/bin/problem3917.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/count-indices-with-opposite-parity/description/
+pub fn count_opposite_parity(nums: Vec<i32>) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", count_opposite_parity(vec![1, 2, 3, 4])); // [2,1,1,0]
+    println!("{:?}", count_opposite_parity(vec![1])); // [0]
+}
