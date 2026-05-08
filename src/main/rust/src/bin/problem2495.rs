f8e1a4d9af9c88ea04921d82d8502c40577b1c55
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-05-08 12:58:05 -0500

    Solve problem 2495

--- src/main/rust/src/bin/problem2495.rs
+++ src/main/rust/src/bin/problem2495.rs
@@ -1,6 +1,22 @@
 // https://leetcode.com/problems/number-of-subarrays-having-even-product/description/
 pub fn even_product(nums: Vec<i32>) -> i64 {
-    todo!()
+    let mut even_indexes: Vec<i64> = vec![-1; nums.len()];
+    for i in (0..nums.len()).rev() {
+        if nums[i] % 2 == 0 {
+            even_indexes[i] = i as i64;
+        } else {
+            if i + 1 < even_indexes.len() {
+                even_indexes[i] = even_indexes[i + 1];
+            }
+        }
+    }
+    let mut answer = 0;
+    for i in 0..nums.len() {
+        if even_indexes[i] != -1 {
+            answer += nums.len() as i64 - even_indexes[i];
+        }
+    }
+    answer
 }
 
 fn main() {
