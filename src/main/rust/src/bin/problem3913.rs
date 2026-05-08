792e00e796dc5075afefd6e95c6f55898b45612b
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-05-08 13:31:01 -0500

    Update problem 3913

--- src/main/rust/src/bin/problem3913.rs
+++ src/main/rust/src/bin/problem3913.rs
@@ -1,6 +1,29 @@
+use itertools::Itertools;
+use std::cmp::Ordering;
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/sort-vowels-by-frequency/description/
 pub fn sort_vowels(s: String) -> String {
-    todo!()
+    let mut vowel_counts: HashMap<char, (i32, usize)> = HashMap::new();
+    for (i, c) in s.chars().enumerate() {
+        if c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' {
+            let (count, _) = vowel_counts.entry(c).or_insert((0, i));
+            *count += 1;
+        }
+    }
+    let vowel_counts: Vec<(char, (i32, usize))> = vowel_counts
+        .into_iter()
+        .sorted_by(|(_, (c1, i1)), (_, (c2, i2))| {
+            let cmp = c2.cmp(c1);
+            if cmp == Ordering::Equal {
+                i1.cmp(i2)
+            } else {
+                cmp
+            }
+        })
+        .collect();
+    let mut answer = String::new();
+    answer
 }
 
 fn main() {
