062218ae54ddfe6bdf0457fab78960ffb23388ad
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-05-08 13:04:07 -0500

    Skeleton for problem 3913

---
+++ src/main/rust/src/bin/problem3913.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/sort-vowels-by-frequency/description/
+pub fn sort_vowels(s: String) -> String {
+    todo!()
+}
+
+fn main() {
+    println!("{}", sort_vowels("leetcode".to_string())); // "leetcedo"
+    println!("{}", sort_vowels("aeiaaioooa".to_string())); // "aaaaoooiie"
+}
