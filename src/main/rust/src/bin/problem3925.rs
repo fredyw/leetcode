f9b95b70650b5947387c47c443675f7157041819
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-05-10 08:46:08 -0500

    Skeleton for problem 3925

---
+++ src/main/rust/src/bin/problem3925.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/concatenate-array-with-reverse/description/
+pub fn concat_with_reverse(nums: Vec<i32>) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", concat_with_reverse(vec![1, 2, 3])); // [1,2,3,3,2,1]
+    println!("{:?}", concat_with_reverse(vec![1])); // [1]
+}
