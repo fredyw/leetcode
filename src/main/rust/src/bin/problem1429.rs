f51cf7727ab8d5f7c43c55381c721a71c526693c
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-05-10 18:12:16 -0500

    Skeleton for problem 1429

---
+++ src/main/rust/src/bin/problem1429.rs
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/first-unique-number/description/
+struct FirstUnique {}
+
+impl FirstUnique {
+    fn new(nums: Vec<i32>) -> Self {
+        todo!()
+    }
+
+    fn show_first_unique(&self) -> i32 {
+        todo!()
+    }
+
+    fn add(&self, value: i32) {
+        todo!()
+    }
+}
+
+fn main() {}
