51c0f1cc3a73e15ea51bd5ba475c31361728a31c
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-05-10 17:41:46 -0500

    Update problem 3488

--- src/main/rust/src/bin/problem3488.rs
+++ src/main/rust/src/bin/problem3488.rs
@@ -7,6 +7,23 @@ pub fn solve_queries(nums: Vec<i32>, queries: Vec<i32>) -> Vec<i32> {
         map.entry(num).or_insert(vec![]).push(i);
     }
     let mut answer = vec![-1; queries.len()];
+    for (i, q) in queries.iter().enumerate() {
+        let q = *q as usize;
+        let num = nums[q];
+        let indexes = map.get(&num).unwrap();
+        let index = indexes.binary_search(&i).unwrap() as isize;
+        let left = if index - 1 == 0 {
+            nums.len() - indexes[indexes.len() - 1]
+        } else {
+            index as usize - 1
+        };
+        let right = if index + 1 == nums.len() as isize {
+            indexes[0]
+        } else {
+            nums.len() - index as usize + 1
+        };
+        answer[i] = left.min(right) as i32;
+    }
     answer
 }
 
