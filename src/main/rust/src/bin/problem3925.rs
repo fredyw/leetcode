2a349cb6e271a7bb7b227093e577a1393bfbc169
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-05-10 08:50:49 -0500

    Solve problem 3925

--- src/main/rust/src/bin/problem3925.rs
+++ src/main/rust/src/bin/problem3925.rs
@@ -1,6 +1,15 @@
 // https://leetcode.com/problems/concatenate-array-with-reverse/description/
 pub fn concat_with_reverse(nums: Vec<i32>) -> Vec<i32> {
-    todo!()
+    let mut reverse = nums.clone();
+    reverse.reverse();
+    let mut answer = vec![];
+    for num in nums {
+        answer.push(num);
+    }
+    for num in reverse {
+        answer.push(num);
+    }
+    answer
 }
 
 fn main() {
