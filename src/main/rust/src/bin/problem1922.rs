64941cd126318cd259658e2b8f20d89736e4a3d8
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-05-10 19:47:44 -0500

    Skeleton for problem 1922

---
+++ src/main/rust/src/bin/problem1922.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/count-good-numbers/description/
+pub fn count_good_numbers(n: i64) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", count_good_numbers(1)); // 5
+    println!("{}", count_good_numbers(4)); // 400
+    println!("{}", count_good_numbers(50)); // 564908303
+}
