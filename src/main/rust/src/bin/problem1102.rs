cc79a32b676bf58b910983ab6983e36e72f7a4f1
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-05-12 16:43:18 -0500

    Solve problem 1102

--- src/main/rust/src/bin/problem1102.rs
+++ src/main/rust/src/bin/problem1102.rs
@@ -1,6 +1,36 @@
+use std::collections::BinaryHeap;
+
 // https://leetcode.com/problems/path-with-maximum-minimum-value/description/
 pub fn maximum_minimum_path(grid: Vec<Vec<i32>>) -> i32 {
-    todo!()
+    let num_rows = grid.len() as isize;
+    let num_cols = if num_rows > 0 { grid[0].len() } else { 0 } as isize;
+    let mut visited = vec![vec![false; num_cols as usize]; num_rows as usize];
+    let mut heap: BinaryHeap<(i32, (isize, isize))> = BinaryHeap::new();
+    heap.push((grid[0][0], (0, 0)));
+    visited[0][0] = true;
+    while let Some((score, (row, col))) = heap.pop() {
+        if row == num_rows - 1 && col == num_cols - 1 {
+            return score;
+        }
+        for (r, c) in [(-1, 0), (0, 1), (1, 0), (0, -1)] {
+            let new_row = row + r;
+            let new_col = col + c;
+            if new_row < 0
+                || new_col < 0
+                || new_row == num_rows
+                || new_col == num_cols
+                || visited[new_row as usize][new_col as usize]
+            {
+                continue;
+            }
+            visited[new_row as usize][new_col as usize] = true;
+            heap.push((
+                score.min(grid[new_row as usize][new_col as usize]),
+                (new_row, new_col),
+            ));
+        }
+    }
+    -1
 }
 
 fn main() {
