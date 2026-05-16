a44e324d668273b71685575cbc9bf6a14118d123
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-05-16 17:38:45 -0500

    Solve problem 2464

--- src/main/rust/src/bin/problem2464.rs
+++ src/main/rust/src/bin/problem2464.rs
@@ -4,9 +4,35 @@ pub fn valid_subarray_split(nums: Vec<i32>) -> i32 {
         if b == 0 {
             return a;
         }
-        gcd(b, a / b)
+        gcd(b, a % b)
     }
-    todo!()
+
+    fn valid_subarray_split(nums: &Vec<i32>, i: usize, memo: &mut Vec<i32>) -> i32 {
+        if i == nums.len() {
+            return 0;
+        }
+        if memo[i] != -1 {
+            return memo[i];
+        }
+        let mut min = i32::MAX;
+        for j in i..nums.len() {
+            if gcd(nums[i], nums[j]) > 1 {
+                let splits = valid_subarray_split(nums, j + 1, memo);
+                if splits != i32::MAX {
+                    min = min.min(splits + 1);
+                }
+            }
+        }
+        memo[i] = min;
+        min
+    }
+
+    let answer = valid_subarray_split(&nums, 0, &mut vec![-1; nums.len()]);
+    if answer == i32::MAX {
+        -1
+    } else {
+        answer
+    }
 }
 
 fn main() {
