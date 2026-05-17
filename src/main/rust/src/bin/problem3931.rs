dc6766c780bb15b34c26cce041dc6f639b9e4762
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-05-17 09:17:15 -0500

    Skeleton for problem 3931

---
+++ src/main/rust/src/bin/problem3931.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/check-adjacent-digit-differences/description/
+pub fn is_adjacent_diff_at_most_two(s: String) -> bool {
+    todo!()
+}
+
+fn main() {
+    println!("{}", is_adjacent_diff_at_most_two("132".to_string())); // true
+    println!("{}", is_adjacent_diff_at_most_two("129".to_string())); // false
+}
