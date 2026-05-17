c046eef99eb577c5bec0bd16fb62ead09382e568
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-05-17 10:41:03 -0500

    Solve problem 3918

--- src/main/rust/src/bin/problem3918.rs
+++ src/main/rust/src/bin/problem3918.rs
@@ -9,14 +9,11 @@ pub fn sum_of_primes_in_range(n: i32) -> i32 {
         .unwrap();
     let start = if n < m { n } else { m } as usize;
     let end = if n >= m { n } else { m } as usize;
-    let mut answer = 0;
     let mut is_prime: Vec<bool> = vec![true; end + 1];
     is_prime[0] = false;
-    for i in 2..=end {
+    is_prime[1] = false;
+    for i in 2..=(end as f32).sqrt() as usize {
         if is_prime[i] {
-            if i >= start && i <= end {
-                answer += i;
-            }
             let mut j = i + i;
             while j <= end {
                 is_prime[j] = false;
@@ -24,7 +21,12 @@ pub fn sum_of_primes_in_range(n: i32) -> i32 {
             }
         }
     }
-    answer as i32
+    is_prime
+        .iter()
+        .enumerate()
+        .filter(|&(i, is_prime)| *is_prime && i >= start && i <= end)
+        .map(|(i, _)| i as i32)
+        .sum()
 }
 
 fn main() {
