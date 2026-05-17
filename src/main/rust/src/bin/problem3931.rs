9f4d8ad11ba55bf5b857ff2f554822b5482b2135
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-05-17 09:43:39 -0500

    Solve problem 3931

--- src/main/rust/src/bin/problem3931.rs
+++ src/main/rust/src/bin/problem3931.rs
@@ -1,6 +1,12 @@
 // https://leetcode.com/problems/check-adjacent-digit-differences/description/
 pub fn is_adjacent_diff_at_most_two(s: String) -> bool {
-    todo!()
+    let bytes = s.as_bytes();
+    for i in 0..bytes.len() - 1 {
+        if (bytes[i] as i32 - bytes[i + 1] as i32).abs() > 2 {
+            return false;
+        }
+    }
+    true
 }
 
 fn main() {
