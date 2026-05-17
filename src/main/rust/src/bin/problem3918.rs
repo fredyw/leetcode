fb8997b4f9a2f431fead47f10e4b91f93d3fb638
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-05-17 09:46:10 -0500

    Skeleton for problem 3918

---
+++ src/main/rust/src/bin/problem3918.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/sum-of-primes-between-number-and-its-reverse/description/
+pub fn sum_of_primes_in_range(n: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", sum_of_primes_in_range(13)); // 132
+    println!("{}", sum_of_primes_in_range(10)); // 17
+    println!("{}", sum_of_primes_in_range(8)); // 0
+}
