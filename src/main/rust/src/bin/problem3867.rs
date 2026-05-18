013933ac794b9399697cba262b947cdf6ad92600
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-05-18 09:01:39 -0500

    Skeleton for problem 3867

---
+++ src/main/rust/src/bin/problem3867.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/sum-of-gcd-of-formed-pairs/description/
+pub fn gcd_sum(nums: Vec<i32>) -> i64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", gcd_sum(vec![2, 6, 4])); // 2
+    println!("{}", gcd_sum(vec![3, 6, 2, 8])); // 5
+}
