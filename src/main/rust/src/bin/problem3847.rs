5b063176ace303cffcfcb8504ed2e226ab5a86aa
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-05-18 14:19:30 -0500

    Skeleton for problem 3847

---
+++ src/main/rust/src/bin/problem3847.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/find-the-score-difference-in-a-game/
+pub fn score_difference(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", score_difference(vec![1, 2, 3])); // 0
+    println!("{}", score_difference(vec![2, 4, 2, 1, 2, 1])); // 4
+    println!("{}", score_difference(vec![1])); // -1
+}
