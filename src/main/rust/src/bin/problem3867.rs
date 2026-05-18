ac7383cabc724d29a623549fd2cd7ef41e9561da
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-05-18 14:13:13 -0500

    Solve problem 3867

--- src/main/rust/src/bin/problem3867.rs
+++ src/main/rust/src/bin/problem3867.rs
@@ -3,7 +3,19 @@ pub fn gcd_sum(nums: Vec<i32>) -> i64 {
     fn gcd(a: i32, b: i32) -> i32 {
         if b == 0 { a } else { gcd(b, a % b) }
     }
-    todo!()
+
+    let mut max = 0;
+    let mut prefix_gcd = vec![0; nums.len()];
+    for (i, n) in nums.iter().enumerate() {
+        max = max.max(*n);
+        prefix_gcd[i] = gcd(nums[i], max);
+    }
+    prefix_gcd.sort_unstable();
+    let mut answer = 0;
+    for i in 0..prefix_gcd.len() / 2 {
+        answer += gcd(prefix_gcd[i], prefix_gcd[prefix_gcd.len() - 1 - i]) as i64;
+    }
+    answer
 }
 
 fn main() {
