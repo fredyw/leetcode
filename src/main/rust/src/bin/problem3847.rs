f7c632532d368eca5d44ac41365814c071d17adc
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-05-19 11:37:20 -0500

    Solve problem 3847

--- src/main/rust/src/bin/problem3847.rs
+++ src/main/rust/src/bin/problem3847.rs
@@ -1,6 +1,22 @@
 // https://leetcode.com/problems/find-the-score-difference-in-a-game/
 pub fn score_difference(nums: Vec<i32>) -> i32 {
-    todo!()
+    let mut player1 = true;
+    let mut player1_score = 0;
+    let mut player2_score = 0;
+    for (i, num) in nums.iter().enumerate() {
+        if num % 2 != 0 {
+            player1 = !player1;
+        }
+        if (i + 1) % 6 == 0 {
+            player1 = !player1;
+        }
+        if player1 {
+            player1_score += num;
+        } else {
+            player2_score += num;
+        }
+    }
+    player1_score - player2_score
 }
 
 fn main() {
