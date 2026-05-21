505b660419841108d3b2efbff4405e03bc718e58
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-05-21 09:50:48 -0500

    Skeleton for problem 2429

---
+++ src/main/rust/src/bin/problem2429.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/minimize-xor/description/
+pub fn minimize_xor(num1: i32, num2: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", minimize_xor(3, 5)); // 3
+    println!("{}", minimize_xor(1, 12)); // 3
+}
