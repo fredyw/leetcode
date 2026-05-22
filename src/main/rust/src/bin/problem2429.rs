aeca56299309cdf32ec3eb4e84f59ac315f46cd0
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-05-22 12:57:58 -0500

    Update problem 2429

--- src/main/rust/src/bin/problem2429.rs
+++ src/main/rust/src/bin/problem2429.rs
@@ -1,9 +1,15 @@
 // https://leetcode.com/problems/minimize-xor/description/
 pub fn minimize_xor(num1: i32, num2: i32) -> i32 {
-    todo!()
+    let num1_bits = num1.count_ones();
+    let num2_bits = num2.count_ones();
+    let mut answer = num1;
+    while num1_bits > num2_bits && (num1_bits - num2_bits > 0) {
+        answer &= answer - 1;
+    }
+    answer
 }
 
 fn main() {
     println!("{}", minimize_xor(3, 5)); // 3
-    println!("{}", minimize_xor(1, 12)); // 3
+    // println!("{}", minimize_xor(1, 12)); // 3
 }
