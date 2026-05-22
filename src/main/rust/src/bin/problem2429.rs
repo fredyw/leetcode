e0caedaf8beb301e0b60a2455dec40cb3734f93e
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-05-22 15:57:16 -0500

    Solve problem 2429

--- src/main/rust/src/bin/problem2429.rs
+++ src/main/rust/src/bin/problem2429.rs
@@ -4,20 +4,21 @@ pub fn minimize_xor(num1: i32, num2: i32) -> i32 {
     let num2_bits = num2.count_ones();
     let mut answer = num1;
     if num1_bits > num2_bits {
-        for _ in 0..=num1_bits - num2_bits {
+        for _ in 0..num1_bits - num2_bits {
             answer &= answer - 1;
         }
     } else if num1_bits < num2_bits {
-        answer = 0;
-        for i in 0..=num2_bits - num1_bits {
-            answer |= 1 << i;
+        for i in 0..num2_bits - num1_bits {
+            answer |= answer + 1;
         }
     }
     answer
 }
 
 fn main() {
-    // println!("{}", minimize_xor(3, 5)); // 3
-    // println!("{}", minimize_xor(1, 12)); // 3
+    println!("{}", minimize_xor(3, 5)); // 3
+    println!("{}", minimize_xor(1, 12)); // 3
     println!("{}", minimize_xor(25, 72)); // 24
+    println!("{}", minimize_xor(65, 84)); // 24
+    println!("{}", minimize_xor(65, 7)); // 67
 }
