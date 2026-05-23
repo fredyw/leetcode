e51d1c10569db33bc2243f797777c8bed5fed329
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-05-22 19:48:47 -0500

    Skeleton for problem 1674

---
+++ src/main/rust/src/bin/problem1674.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/minimum-moves-to-make-array-complementary/description/
+pub fn min_moves(nums: Vec<i32>, limit: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", min_moves(vec![1, 2, 4, 3], 4)); // 1
+    println!("{}", min_moves(vec![1, 2, 2, 1], 2)); // 2
+    println!("{}", min_moves(vec![1, 2, 1, 2], 2)); // 0
+}
