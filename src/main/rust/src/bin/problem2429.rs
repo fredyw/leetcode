24cda6a30c091fd16ddd3b2974eb0c65563891e8
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-05-22 19:45:34 -0500

    Update problem 2429

--- src/main/rust/src/bin/problem2429.rs
+++ src/main/rust/src/bin/problem2429.rs
@@ -8,7 +8,7 @@ pub fn minimize_xor(num1: i32, num2: i32) -> i32 {
             answer &= answer - 1;
         }
     } else if num1_bits < num2_bits {
-        for i in 0..num2_bits - num1_bits {
+        for _ in 0..num2_bits - num1_bits {
             answer |= answer + 1;
         }
     }
