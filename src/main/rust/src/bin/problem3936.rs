0bb81f71bc99708ce38b69042ccc6c181aed42be
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-05-24 10:55:36 -0500

    Skeleton for problem 3936

---
+++ src/main/rust/src/bin/problem3936.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/minimum-swaps-to-move-zeros-to-end/description/
+pub fn minimum_swaps(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", minimum_swaps(vec![0, 1, 0, 3, 12])); // 2
+    println!("{}", minimum_swaps(vec![0, 1, 0, 2])); // 1
+    println!("{}", minimum_swaps(vec![1, 2, 0])); // 0
+}
