21394660629cecbf5efa0254551122b0eb132e72
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-05-24 10:01:29 -0500

    Skeleton for problem 3940

---
+++ src/main/rust/src/bin/problem3940.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/limit-occurrences-in-sorted-array/description/
+pub fn limit_occurrences(nums: Vec<i32>, k: i32) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", limit_occurrences(vec![1, 1, 1, 2, 2, 3], 2)); // [1,1,2,2,3]
+    println!("{:?}", limit_occurrences(vec![1, 2, 3], 1)); // [1,2,3]
+}
