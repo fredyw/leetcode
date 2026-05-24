b2bae8ffbe3eec3ae4eaa8a49eceb0bd041caf12
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-05-24 11:20:10 -0500

    Solve problem 3936

--- src/main/rust/src/bin/problem3936.rs
+++ src/main/rust/src/bin/problem3936.rs
@@ -1,6 +1,13 @@
 // https://leetcode.com/problems/minimum-swaps-to-move-zeros-to-end/description/
 pub fn minimum_swaps(nums: Vec<i32>) -> i32 {
-    todo!()
+    let total_zero_count = nums.iter().filter(|&n| *n == 0).count();
+    let mut zero_count = 0;
+    for i in nums.len() - total_zero_count..nums.len() {
+        if nums[i] == 0 {
+            zero_count += 1;
+        }
+    }
+    (total_zero_count - zero_count) as i32
 }
 
 fn main() {
