7634657fd1b92e3a632ba0257ad0bdfe26029bbf
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-05-27 08:53:23 -0500

    Skeleton for problem 3941

---
+++ src/main/rust/src/bin/problem3941.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/password-strength/description/
+pub fn password_strength(password: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", password_strength("aA1!".to_string())); // 11
+    println!("{}", password_strength("bbB11#".to_string())); // 11
+}
