4a57baf370aa5d41e5d42525f9b5326b8db1e404
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-05-27 09:28:56 -0500

    Solve problem 3941

--- src/main/rust/src/bin/problem3941.rs
+++ src/main/rust/src/bin/problem3941.rs
@@ -1,6 +1,26 @@
+use std::collections::HashSet;
+
 // https://leetcode.com/problems/password-strength/description/
 pub fn password_strength(password: String) -> i32 {
-    todo!()
+    let mut lowercase_chars: HashSet<char> = HashSet::new();
+    let mut uppercase_chars: HashSet<char> = HashSet::new();
+    let mut numbers: HashSet<char> = HashSet::new();
+    let mut special_chars: HashSet<char> = HashSet::new();
+    for char in password.chars() {
+        if char.is_lowercase() {
+            lowercase_chars.insert(char);
+        } else if char.is_uppercase() {
+            uppercase_chars.insert(char);
+        } else if char.is_digit(10) {
+            numbers.insert(char);
+        } else {
+            special_chars.insert(char);
+        }
+    }
+    ((lowercase_chars.len() * 1)
+        + (uppercase_chars.len() * 2)
+        + (numbers.len() * 3)
+        + (special_chars.len() * 5)) as i32
 }
 
 fn main() {
