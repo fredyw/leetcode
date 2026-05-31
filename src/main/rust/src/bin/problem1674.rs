14811d2261da1275a3ee13afefb89c20253e29e2
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-05-30 21:07:26 -0500

    Update problem 1674

--- src/main/rust/src/bin/problem1674.rs
+++ src/main/rust/src/bin/problem1674.rs
@@ -6,12 +6,17 @@ pub fn min_moves(nums: Vec<i32>, limit: i32) -> i32 {
     for i in 0..(n / 2) {
         let x = nums[i].min(nums[n - 1 - i]) as usize;
         let y = nums[i].max(nums[n - 1 - i]) as usize;
+        // 1. [2, x] needs 2 moves.
         diff[2] += 2;
         diff[x + 1] -= 2;
+        // 2. [x + 1, x + y - 1] needs 1 move.
         diff[x + 1] += 1;
         diff[x + y] -= 1;
+        // 3. [x + y, x + y] needs 0 moves.
+        // 4. [x + y + 1, y + limit] needs 1 move.
         diff[x + y + 1] += 1;
         diff[y + limit + 1] -= 1;
+        // 5. [y + limit + 1, 2 * limit] needs 2 moves.
         diff[y + limit + 1] += 2;
         diff[2 * limit + 1] -= 2;
     }
