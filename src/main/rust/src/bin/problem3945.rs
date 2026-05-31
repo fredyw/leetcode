9a88fced805a1763176742c0101640245977ee01
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-05-31 18:13:28 -0500

    Solve problem 3945

--- src/main/rust/src/bin/problem3945.rs
+++ src/main/rust/src/bin/problem3945.rs
@@ -1,6 +1,15 @@
+use itertools::Itertools;
+
 // https://leetcode.com/problems/digit-frequency-score/description/
 pub fn digit_frequency_score(n: i32) -> i32 {
-    todo!()
+    n.to_string()
+        .chars()
+        .counts()
+        .into_iter()
+        .fold(0, |acc, (char, count)| {
+            let digit = char.to_digit(10).unwrap() as i32;
+            acc + (digit * count as i32)
+        })
 }
 
 fn main() {
