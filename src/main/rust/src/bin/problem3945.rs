2d12c7ecee8ac3da7a645c3abe2a400b9a5fdedf
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-05-31 18:02:36 -0500

    Skeleton for problem 3945

---
+++ src/main/rust/src/bin/problem3945.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/digit-frequency-score/description/
+pub fn digit_frequency_score(n: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", digit_frequency_score(122)); // 5
+    println!("{}", digit_frequency_score(101)); // 2
+}
