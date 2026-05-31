7255bcef104f16348b67edd3b22248955825f3b2
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-05-30 20:23:46 -0500

    Solve problem 1674

--- src/main/rust/src/bin/problem1674.rs
+++ src/main/rust/src/bin/problem1674.rs
@@ -1,6 +1,27 @@
 // https://leetcode.com/problems/minimum-moves-to-make-array-complementary/description/
 pub fn min_moves(nums: Vec<i32>, limit: i32) -> i32 {
-    todo!()
+    let n = nums.len();
+    let limit = limit as usize;
+    let mut diff = vec![0; 2 * limit + 2];
+    for i in 0..(n / 2) {
+        let x = nums[i].min(nums[n - 1 - i]) as usize;
+        let y = nums[i].max(nums[n - 1 - i]) as usize;
+        diff[2] += 2;
+        diff[x + 1] -= 2;
+        diff[x + 1] += 1;
+        diff[x + y] -= 1;
+        diff[x + y + 1] += 1;
+        diff[y + limit + 1] -= 1;
+        diff[y + limit + 1] += 2;
+        diff[2 * limit + 1] -= 2;
+    }
+    let mut answer = n as i32;
+    let mut current_moves = 0;
+    for x in 2..=(2 * limit) {
+        current_moves += diff[x];
+        answer = answer.min(current_moves);
+    }
+    answer
 }
 
 fn main() {
