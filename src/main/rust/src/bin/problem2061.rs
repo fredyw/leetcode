8867c68dccda3cd789e3a2117f5bc24102adcff6
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-06-02 13:11:22 -0500

    Update problem 2061

--- src/main/rust/src/bin/problem2061.rs
+++ src/main/rust/src/bin/problem2061.rs
@@ -1,6 +1,71 @@
 // https://leetcode.com/problems/number-of-spaces-cleaning-robot-cleaned/description/
 pub fn number_of_clean_rooms(room: Vec<Vec<i32>>) -> i32 {
-    todo!()
+    enum Direction {
+        Left,
+        Right,
+        Up,
+        Down,
+    }
+
+    fn traverse(
+        room: &Vec<Vec<i32>>,
+        row: isize,
+        col: isize,
+        direction: Direction,
+        visited: &mut Vec<Vec<bool>>,
+    ) -> i32 {
+        let num_rows = room.len() as isize;
+        let num_cols = if num_rows > 0 { room[0].len() } else { 0 } as isize;
+        if num_rows < 0
+            || num_cols < 0
+            || num_rows == row
+            || num_cols == col
+            || visited[row as usize][col as usize]
+        {
+            return 0;
+        }
+        visited[row as usize][col as usize] = true;
+        match direction {
+            Direction::Left => {
+                if col - 1 < 0 {
+                    traverse(room, row - 1, col, Direction::Up, visited)
+                } else {
+                    traverse(room, row, col - 1, direction, visited)
+                }
+            }
+            Direction::Right => {
+                if col + 1 == num_cols {
+                    traverse(room, row + 1, col, Direction::Down, visited)
+                } else {
+                    traverse(room, row, col + 1, direction, visited)
+                }
+            }
+            Direction::Up => {
+                if row - 1 < 0 {
+                    traverse(room, row, col + 1, Direction::Right, visited)
+                } else {
+                    traverse(room, row - 1, col, direction, visited)
+                }
+            }
+            Direction::Down => {
+                if row + 1 == num_rows {
+                    traverse(room, row, col - 1, Direction::Left, visited)
+                } else {
+                    traverse(room, row + 1, col, direction, visited)
+                }
+            }
+        }
+    }
+
+    let num_rows = room.len();
+    let num_cols = if num_rows > 0 { room[0].len() } else { 0 };
+    traverse(
+        &room,
+        0,
+        0,
+        Direction::Right,
+        &mut vec![vec![false; num_cols]; num_rows],
+    )
 }
 
 fn main() {
