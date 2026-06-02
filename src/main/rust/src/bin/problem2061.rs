66a0e54edc77a0a31f4b25f80f34570168acfbd1
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-06-02 13:51:15 -0500

    Solve problem 2061

--- src/main/rust/src/bin/problem2061.rs
+++ src/main/rust/src/bin/problem2061.rs
@@ -1,7 +1,8 @@
 // https://leetcode.com/problems/number-of-spaces-cleaning-robot-cleaned/description/
 pub fn number_of_clean_rooms(room: Vec<Vec<i32>>) -> i32 {
+    #[derive(Copy, Clone)]
     enum Direction {
-        Left,
+        Left = 0,
         Right,
         Up,
         Down,
@@ -12,49 +13,51 @@ pub fn number_of_clean_rooms(room: Vec<Vec<i32>>) -> i32 {
         row: isize,
         col: isize,
         direction: Direction,
-        visited: &mut Vec<Vec<bool>>,
+        visited: &mut Vec<Vec<Vec<bool>>>,
     ) -> i32 {
         let num_rows = room.len() as isize;
         let num_cols = if num_rows > 0 { room[0].len() } else { 0 } as isize;
-        if num_rows < 0
-            || num_cols < 0
+        if row < 0
+            || col < 0
             || num_rows == row
             || num_cols == col
-            || visited[row as usize][col as usize]
+            || visited[row as usize][col as usize][direction as usize]
+            || room[row as usize][col as usize] == 1
         {
             return 0;
         }
-        visited[row as usize][col as usize] = true;
-        match direction {
+        let is_first_visit = !visited[row as usize][col as usize].iter().any(|&v| v);
+        visited[row as usize][col as usize][direction as usize] = true;
+        (match direction {
             Direction::Left => {
-                if col - 1 < 0 {
-                    traverse(room, row - 1, col, Direction::Up, visited)
+                if col - 1 < 0 || room[row as usize][(col - 1) as usize] == 1 {
+                    traverse(room, row, col, Direction::Up, visited)
                 } else {
                     traverse(room, row, col - 1, direction, visited)
                 }
             }
             Direction::Right => {
-                if col + 1 == num_cols {
-                    traverse(room, row + 1, col, Direction::Down, visited)
+                if col + 1 == num_cols || room[row as usize][(col + 1) as usize] == 1 {
+                    traverse(room, row, col, Direction::Down, visited)
                 } else {
                     traverse(room, row, col + 1, direction, visited)
                 }
             }
             Direction::Up => {
-                if row - 1 < 0 {
-                    traverse(room, row, col + 1, Direction::Right, visited)
+                if row - 1 < 0 || room[(row - 1) as usize][col as usize] == 1 {
+                    traverse(room, row, col, Direction::Right, visited)
                 } else {
                     traverse(room, row - 1, col, direction, visited)
                 }
             }
             Direction::Down => {
-                if row + 1 == num_rows {
-                    traverse(room, row, col - 1, Direction::Left, visited)
+                if row + 1 == num_rows || room[(row + 1) as usize][col as usize] == 1 {
+                    traverse(room, row, col, Direction::Left, visited)
                 } else {
                     traverse(room, row + 1, col, direction, visited)
                 }
             }
-        }
+        }) + if is_first_visit { 1 } else { 0 }
     }
 
     let num_rows = room.len();
@@ -64,7 +67,7 @@ pub fn number_of_clean_rooms(room: Vec<Vec<i32>>) -> i32 {
         0,
         0,
         Direction::Right,
-        &mut vec![vec![false; num_cols]; num_rows],
+        &mut vec![vec![vec![false; 4]; num_cols]; num_rows],
     )
 }
 
@@ -80,9 +83,20 @@ fn main() {
     println!(
         "{}",
         number_of_clean_rooms(vec![vec![0, 0, 0], vec![0, 0, 0], vec![0, 0, 0]])
-    ); // 0
+    ); // 8
     println!(
         "{}",
         number_of_clean_rooms(vec![vec![0, 0, 0], vec![1, 0, 0], vec![0, 0, 0]])
     ); // 7
+    println!("{}", number_of_clean_rooms(vec![vec![0]])); // 1
+    println!(
+        "{}",
+        number_of_clean_rooms(vec![
+            vec![0, 0, 0, 1, 0],
+            vec![1, 1, 0, 0, 1],
+            vec![1, 1, 0, 0, 1],
+            vec![0, 0, 1, 0, 1],
+            vec![1, 1, 0, 1, 0]
+        ])
+    ); // 5
 }
