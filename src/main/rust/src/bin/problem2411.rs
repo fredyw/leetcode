ff45d444d7bc918aceb3cc89582f2619c313a565
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-06-06 18:16:50 -0500

    Skeleton for problem 2411

---
+++ src/main/rust/src/bin/problem2411.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/smallest-subarrays-with-maximum-bitwise-or/description/
+pub fn smallest_subarrays(nums: Vec<i32>) -> Vec<i32> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", smallest_subarrays(vec![1, 0, 2, 1, 3])); // [3,3,2,2,1]
+    println!("{:?}", smallest_subarrays(vec![1, 2])); // [2,1]
+}
