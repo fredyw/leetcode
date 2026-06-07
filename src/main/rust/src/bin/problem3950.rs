35966d42c80740c3dbcaa324ada6e5ed3d64034b
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-06-07 11:46:33 -0500

    Skeleton for problem 3950

---
+++ src/main/rust/src/bin/problem3950.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/exactly-one-consecutive-set-bits-pair
+pub fn consecutive_set_bits(n: i32) -> bool {
+    todo!()
+}
+
+fn main() {
+    println!("{}", consecutive_set_bits(6)); // true
+    println!("{}", consecutive_set_bits(5)); // false
+}
