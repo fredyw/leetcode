51d5b76ef9c66984ad07456741cf550aee65595f
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-06-07 12:01:22 -0500

    Skeleton for problem 3954

---
+++ src/main/rust/src/bin/problem3954.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/sum-of-compatible-numbers-in-range-i
+pub fn sum_of_good_integers(n: i32, k: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", sum_of_good_integers(2, 3)); // 10
+    println!("{}", sum_of_good_integers(5, 1)); // 0
+}
