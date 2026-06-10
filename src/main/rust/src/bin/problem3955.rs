eefa7c24b331b52902b3170e0229ba9e85be2755
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-06-10 12:08:45 -0500

    Skeleton for problem 3955

---
+++ src/main/rust/src/bin/problem3955.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/valid-binary-strings-with-cost-limit/description/
+pub fn generate_valid_strings(n: i32, k: i32) -> Vec<String> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", generate_valid_strings(3, 1)); // ["000","010","100"]
+    println!("{:?}", generate_valid_strings(1, 0)); // ["0","1"]
+}
