3595990f97ac0e90578f38f01e4769843e7f1f4e
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-06-11 15:20:32 -0500

    Update problem 3955

--- src/main/rust/src/bin/problem3955.rs
+++ src/main/rust/src/bin/problem3955.rs
@@ -1,5 +1,15 @@
 // https://leetcode.com/problems/valid-binary-strings-with-cost-limit/description/
 pub fn generate_valid_strings(n: i32, k: i32) -> Vec<String> {
+    fn generate(n: i32, k: i32, prev: i32, cur: i32, sum: i32) {
+        if sum > k || n == cur {
+            return;
+        }
+        if prev + 1 != cur {
+            generate(n, k, cur, cur + 1, sum + cur);
+        }
+        generate(n, k, prev, cur + 1, sum); // skip
+    }
+
     todo!()
 }
 
