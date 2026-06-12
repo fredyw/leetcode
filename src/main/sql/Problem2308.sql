a305c3c05c45ce3fa1a2c47a6662d2674db87ca2
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-06-12 13:58:37 -0500

    Solve problem 2308

--- src/main/sql/Problem2308.sql
+++ src/main/sql/Problem2308.sql
@@ -1,1 +1,21 @@
 -- https://leetcode.com/problems/arrange-table-by-gender/description/
+WITH t AS (
+    SELECT
+        ROW_NUMBER() OVER (PARTITION BY gender ORDER BY user_id) AS row_num,
+        user_id,
+        CASE
+            WHEN gender = 'female' THEN 0
+            WHEN gender = 'other' THEN 1
+            ELSE 2
+        END AS gender_order
+    FROM genders
+)
+SELECT
+    user_id,
+    CASE
+        WHEN gender_order = 0 THEN 'female'
+        WHEN gender_order = 1 THEN 'other'
+        ELSE 'male'
+    END AS gender
+FROM t
+ORDER BY row_num, gender_order;
