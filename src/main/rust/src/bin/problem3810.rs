6a337c65e212b347d997ed36aa6df83d2a7e57e2
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-06-14 10:45:13 -0500

    Skeleton for problem 3810

---
+++ src/main/rust/src/bin/problem3810.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/minimum-operations-to-reach-target-array/description/
+pub fn min_operations(nums: Vec<i32>, target: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", min_operations(vec![1, 2, 3], vec![2, 1, 3])); // 2
+    println!("{}", min_operations(vec![4, 1, 4], vec![5, 1, 4])); // 1
+    println!("{}", min_operations(vec![7, 3, 7], vec![5, 5, 9])); // 2
+}
