87d797cb854d81d8419e52f7f4113b0f2d273863
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-06-14 09:02:05 -0500

    Solve problem 3959

--- src/main/rust/src/bin/problem3959.rs
+++ src/main/rust/src/bin/problem3959.rs
@@ -1,6 +1,13 @@
 // https://leetcode.com/problems/check-good-integer/description/
 pub fn check_good_integer(n: i32) -> bool {
-    todo!()
+    let mut digit_sum = 0;
+    let mut square_sum = 0;
+    for c in n.to_string().chars() {
+        let n = c as i32 - b'0' as i32;
+        digit_sum += n;
+        square_sum += n * n;
+    }
+    square_sum - digit_sum >= 50
 }
 
 fn main() {
