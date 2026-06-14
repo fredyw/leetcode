a014c5af9d6285188c3707cca4baec1506ab8959
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-06-14 08:53:12 -0500

    Skeleton for problem 3959

---
+++ src/main/rust/src/bin/problem3959.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/check-good-integer/description/
+pub fn check_good_integer(n: i32) -> bool {
+    todo!()
+}
+
+fn main() {
+    println!("{}", check_good_integer(1000)); // false
+    println!("{}", check_good_integer(19)); // true
+}
