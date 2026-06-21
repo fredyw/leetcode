5b9ff01f476b36f813c33bb6ae4b7aee00cd3342
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-06-20 21:07:03 -0500

    Skeleton for problem 3963

---
+++ src/main/rust/src/bin/problem3963.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/create-grid-with-exactly-one-path/description/
+pub fn create_grid(m: i32, n: i32) -> Vec<String> {
+    todo!()
+}
+
+fn main() {
+    println!("{:?}", create_grid(2, 3)); // ["..#","#.."]
+    println!("{:?}", create_grid(3, 3)); // ["..#","#..","##."]
+    println!("{:?}", create_grid(1, 4)); // ["...."]
+}
