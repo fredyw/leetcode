226a0aa8fce5d04d30296244dd383b3a9e2a133b
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-06-21 09:22:39 -0500

    Solve problem 3963

--- src/main/rust/src/bin/problem3963.rs
+++ src/main/rust/src/bin/problem3963.rs
@@ -1,6 +1,12 @@
 // https://leetcode.com/problems/create-grid-with-exactly-one-path/description/
 pub fn create_grid(m: i32, n: i32) -> Vec<String> {
-    let mut answer: Vec<Vec<char>> = vec![vec!['.'; n as usize]; m as usize];
+    let mut answer: Vec<Vec<char>> = vec![vec!['#'; n as usize]; m as usize];
+    for row in 0..m as usize {
+        answer[row][0] = '.';
+    }
+    for col in 0..n as usize {
+        answer[m as usize - 1][col] = '.';
+    }
     answer
         .into_iter()
         .map(|char_vec| char_vec.into_iter().collect::<String>())
