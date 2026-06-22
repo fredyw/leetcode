e0266b338e2add8b907510da80c40af7da080b4d
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-06-22 14:54:19 -0500

    Solve problem 3810

--- src/main/rust/src/bin/problem3810.rs
+++ src/main/rust/src/bin/problem3810.rs
@@ -1,6 +1,13 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/minimum-operations-to-reach-target-array/description/
 pub fn min_operations(nums: Vec<i32>, target: Vec<i32>) -> i32 {
-    todo!()
+    let mut map: HashMap<i32, bool> = HashMap::new();
+    for (i, num) in nums.iter().enumerate() {
+        let same = map.entry(*num).or_insert(true);
+        *same = *same && (*num == target[i]);
+    }
+    map.into_iter().filter(|(_, v)| !*v).count() as i32
 }
 
 fn main() {
