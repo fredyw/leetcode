a17e3cab19eb7458d54c57e113472753137097ee
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-06-27 08:11:09 -0500

    Skeleton for problem 3968

---
+++ src/main/rust/src/bin/problem3968.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/maximum-manhattan-distance-after-all-moves/description/
+pub fn max_distance(moves: String) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", max_distance("L_D_".to_string())); // 4
+    println!("{}", max_distance("U_R".to_string())); // 3
+}
