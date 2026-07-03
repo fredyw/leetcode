9db3d2a251f43042700f3e4352dbb4b430422f82
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-07-03 14:07:37 -0500

    Skeleton for problem 3572

---
+++ src/main/rust/src/bin/problem3572.rs
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/maximize-ysum-by-picking-a-triplet-of-distinct-xvalues/description/
+pub fn max_sum_distinct_triplet(x: Vec<i32>, y: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!(
+        "{}",
+        max_sum_distinct_triplet(vec![1, 2, 1, 3, 2], vec![5, 3, 4, 6, 2])
+    ); // 14
+    println!(
+        "{}",
+        max_sum_distinct_triplet(vec![1, 2, 1, 2], vec![4, 5, 6, 7])
+    ); // -1
+}
