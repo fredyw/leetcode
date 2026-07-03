b6604d15c0e8d8b056153f79313195ddfe50bad2
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-07-03 17:00:17 -0500

    Solve problem 3572

--- src/main/rust/src/bin/problem3572.rs
+++ src/main/rust/src/bin/problem3572.rs
@@ -1,6 +1,20 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/maximize-ysum-by-picking-a-triplet-of-distinct-xvalues/description/
 pub fn max_sum_distinct_triplet(x: Vec<i32>, y: Vec<i32>) -> i32 {
-    todo!()
+    let mut map: HashMap<i32, i32> = HashMap::new();
+    for i in 0..x.len() {
+        map.entry(x[i])
+            .and_modify(|n| *n = (*n).max(y[i]))
+            .or_insert(y[i]);
+    }
+    let mut v: Vec<i32> = map.into_iter().map(|(_, v)| v).collect();
+    v.sort_unstable();
+    if v.len() <= 2 {
+        -1
+    } else {
+        v[v.len() - 2] + v[v.len() - 1] + v[v.len() - 3]
+    }
 }
 
 fn main() {
