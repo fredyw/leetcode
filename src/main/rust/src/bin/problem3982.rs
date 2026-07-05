4a529a37f1ddceb77021332d37b77132aec0e98b
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-07-05 09:56:07 -0500

    Solve problem 3982

--- src/main/rust/src/bin/problem3982.rs
+++ src/main/rust/src/bin/problem3982.rs
@@ -1,6 +1,17 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/sum-of-integers-with-maximum-digit-range/description/
 pub fn max_digit_range(nums: Vec<i32>) -> i32 {
-    todo!()
+    let mut map: HashMap<i32, Vec<i32>> = HashMap::new();
+    for num in &nums {
+        let s = num.to_string();
+        let min = s.bytes().min().unwrap() as i32 - 'a' as i32;
+        let max = s.bytes().max().unwrap() as i32 - 'a' as i32;
+        let diff = max - min;
+        map.entry(diff).or_insert(Vec::new()).push(*num);
+    }
+    let max = map.iter().map(|(k, _)| *k).max().unwrap();
+    map.get(&max).unwrap_or(&Vec::new()).iter().sum()
 }
 
 fn main() {
