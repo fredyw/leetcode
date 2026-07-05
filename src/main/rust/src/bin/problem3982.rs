288a32cc1547c8712476ef78827059d0aebfa7ac
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-07-05 09:50:04 -0500

    Skeleton for problem 3982

---
+++ src/main/rust/src/bin/problem3982.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/sum-of-integers-with-maximum-digit-range/description/
+pub fn max_digit_range(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", max_digit_range(vec![5724, 111, 350])); // 6074
+    println!("{}", max_digit_range(vec![90, 900])); // 990
+}
