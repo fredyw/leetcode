28de34b09b955a982c5ad731015939f918b72f9b
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-07-05 08:52:46 -0500

    Skeleton for problem 3978

---
+++ src/main/rust/src/bin/problem3978.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/unique-middle-element/description/
+pub fn is_middle_element_unique(nums: Vec<i32>) -> bool {
+    todo!()
+}
+
+fn main() {
+    println!("{}", is_middle_element_unique(vec![1, 2, 3])); // true
+    println!("{}", is_middle_element_unique(vec![1, 2, 2])); // false
+}
