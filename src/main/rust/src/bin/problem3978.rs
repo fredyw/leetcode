ab2873dbcb9ab846feb56d358a962cb36df839fa
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-07-05 08:59:33 -0500

    Solve problem 3978

--- src/main/rust/src/bin/problem3978.rs
+++ src/main/rust/src/bin/problem3978.rs
@@ -1,6 +1,16 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/unique-middle-element/description/
 pub fn is_middle_element_unique(nums: Vec<i32>) -> bool {
-    todo!()
+    let mut uniques: HashMap<i32, bool> = HashMap::new();
+    for num in &nums {
+        uniques
+            .entry(*num)
+            .and_modify(|e| *e = false)
+            .or_insert(true);
+    }
+    let mid = nums[(nums.len() - 1) / 2];
+    *uniques.get(&mid).unwrap()
 }
 
 fn main() {
