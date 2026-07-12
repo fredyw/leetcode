aac5b1ab66913e4760f9a9ecde6a7016e1e19980
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-07-12 13:40:23 -0500

    Solve problem 3986

--- src/main/rust/src/bin/problem3986.rs
+++ src/main/rust/src/bin/problem3986.rs
@@ -1,6 +1,13 @@
+use itertools::Itertools;
+
 // https://leetcode.com/problems/number-of-elapsed-seconds-between-two-times/description/
 pub fn seconds_between_times(start_time: String, end_time: String) -> i32 {
-    todo!()
+    let (start_hour, start_minute, start_second) = start_time.split(":").collect_tuple().unwrap();
+    let (end_hour, end_minute, end_second) = end_time.split(":").collect_tuple().unwrap();
+    let hour = end_hour.parse::<i32>().unwrap() - start_hour.parse::<i32>().unwrap();
+    let minute = end_minute.parse::<i32>().unwrap() - start_minute.parse::<i32>().unwrap();
+    let second = end_second.parse::<i32>().unwrap() - start_second.parse::<i32>().unwrap();
+    (hour * 60 * 60) + (minute * 60) + second
 }
 
 fn main() {
