cf02e8bd90dd2b4da4d5d134fbeef5cc2db35986
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-07-17 00:01:08 -0500

    Update problem 3532

--- src/main/rust/src/bin/problem3532.rs
+++ src/main/rust/src/bin/problem3532.rs
@@ -20,12 +20,14 @@ pub fn path_existence_queries(
             set.insert(i);
             connected.push(set);
             set = HashSet::new();
+            if i + 1 == nums.len() - 1 {
+                set.insert(i + 1);
+            }
         }
     }
     if !set.is_empty() {
         connected.push(set);
     }
-    println!("{:?}", connected);
     let mut answer = Vec::new();
     for query in queries {
         let (u, v) = (query[0] as usize, query[1] as usize);
@@ -42,23 +44,23 @@ pub fn path_existence_queries(
 }
 
 fn main() {
-    // println!(
-    //     "{:?}",
-    //     path_existence_queries(2, vec![1, 3], 1, vec![vec![0, 0], vec![0, 1]])
-    // ); // [true,false]
-    // println!(
-    //     "{:?}",
-    //     path_existence_queries(
-    //         4,
-    //         vec![2, 5, 6, 8],
-    //         2,
-    //         vec![vec![0, 1], vec![0, 2], vec![1, 3], vec![2, 3]]
-    //     )
-    // ); // [false,false,true,true]
-    // println!(
-    //     "{:?}",
-    //     path_existence_queries(1, vec![10], 1, vec![vec![0, 0]])
-    // ); // [true]
+    println!(
+        "{:?}",
+        path_existence_queries(2, vec![1, 3], 1, vec![vec![0, 0], vec![0, 1]])
+    ); // [true,false]
+    println!(
+        "{:?}",
+        path_existence_queries(
+            4,
+            vec![2, 5, 6, 8],
+            2,
+            vec![vec![0, 1], vec![0, 2], vec![1, 3], vec![2, 3]]
+        )
+    ); // [false,false,true,true]
+    println!(
+        "{:?}",
+        path_existence_queries(1, vec![10], 1, vec![vec![0, 0]])
+    ); // [true]
     println!(
         "{:?}",
         path_existence_queries(2, vec![25868, 59061], 0, vec![vec![1, 1]])
