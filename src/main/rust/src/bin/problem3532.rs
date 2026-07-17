571c2f0cfe2e64d1827a39c6514c3da363727a64
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-07-16 23:53:47 -0500

    Update problem 3532

--- src/main/rust/src/bin/problem3532.rs
+++ src/main/rust/src/bin/problem3532.rs
@@ -1,25 +1,58 @@
+use std::collections::HashSet;
+
 // https://leetcode.com/problems/path-existence-queries-in-a-graph-i/description/
 pub fn path_existence_queries(
-    n: i32,
+    _n: i32,
     nums: Vec<i32>,
     max_diff: i32,
     queries: Vec<Vec<i32>>,
 ) -> Vec<bool> {
-    todo!()
+    let mut connected: Vec<HashSet<usize>> = vec![];
+    let mut set = HashSet::new();
+    for i in 0..nums.len() - 1 {
+        if (nums[i] - nums[i + 1]).abs() <= max_diff {
+            set.insert(i);
+            set.insert(i + 1);
+        } else {
+            set.insert(i);
+            connected.push(set);
+            set = HashSet::new();
+        }
+    }
+    if !set.is_empty() {
+        connected.push(set);
+    }
+    let mut answer = Vec::new();
+    for query in queries {
+        let (u, v) = (query[0] as usize, query[1] as usize);
+        let mut is_connected = false;
+        for conn in &connected {
+            is_connected = conn.contains(&u) && conn.contains(&v);
+            if is_connected {
+                break;
+            }
+        }
+        answer.push(is_connected);
+    }
+    answer
 }
 
 fn main() {
+    // println!(
+    //     "{:?}",
+    //     path_existence_queries(2, vec![1, 3], 1, vec![vec![0, 0], vec![0, 1]])
+    // ); // [true,false]
+    // println!(
+    //     "{:?}",
+    //     path_existence_queries(
+    //         4,
+    //         vec![2, 5, 6, 8],
+    //         2,
+    //         vec![vec![0, 1], vec![0, 2], vec![1, 3], vec![2, 3]]
+    //     )
+    // ); // [false,false,true,true]
     println!(
         "{:?}",
-        path_existence_queries(2, vec![1, 3], 1, vec![vec![0, 0], vec![0, 1]])
-    ); // [true,false]
-    println!(
-        "{:?}",
-        path_existence_queries(
-            4,
-            vec![2, 5, 6, 8],
-            2,
-            vec![vec![0, 1], vec![0, 2], vec![1, 3], vec![2, 3]]
-        )
-    ); // [false,false,true,true]
+        path_existence_queries(1, vec![10], 1, vec![vec![0, 0]])
+    ); // [true]
 }
