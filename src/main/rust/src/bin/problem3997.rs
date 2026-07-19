6a210af3d7e7ba048a4e71771eeafcae8276165d
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-07-19 16:11:23 -0500

    Skeleton for problem 3997

---
+++ src/main/rust/src/bin/problem3997.rs
@@ -0,0 +1,27 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+// https://leetcode.com/problems/count-dominant-nodes-in-a-binary-tree/description/
+pub fn count_dominant_nodes(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+    todo!()
+}
+
+fn main() {}
