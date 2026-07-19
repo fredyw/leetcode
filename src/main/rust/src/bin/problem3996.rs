5895b7514a78a98a054b04fd1da1dec77f6a2432
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-07-19 10:41:03 -0500

    Skeleton for problem 3996

---
+++ src/main/rust/src/bin/problem3996.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/even-number-of-knight-moves/description/
+pub fn can_reach(start: Vec<i32>, target: Vec<i32>) -> bool {
+    todo!()
+}
+
+fn main() {
+    println!("{}", can_reach(vec![1, 1], vec![2, 2])); // true
+    println!("{}", can_reach(vec![4, 5], vec![6, 6])); // false
+}
