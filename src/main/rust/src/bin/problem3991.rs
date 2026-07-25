5999bed80245e802690a8888256b979a65f03ede
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-07-24 23:35:52 -0500

    Solve problem 3991

--- src/main/rust/src/bin/problem3991.rs
+++ src/main/rust/src/bin/problem3991.rs
@@ -1,38 +1,37 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, VecDeque};
 
 // https://leetcode.com/problems/sort-array-using-prefix-reversals/description/
-pub fn sort_array(mut nums: Vec<i32>, pre: Vec<i32>) -> i32 {
-    let pre: HashSet<i32> = pre.into_iter().collect();
-    let mut answer = 0;
-    for i in 0..nums.len() {
-        let mut possible = false;
-        if nums[i] != i as i32 {
-            for j in pre.iter() {
-                let j = *j as usize;
-                if i + j - 1 >= nums.len() {
-                    continue;
-                }
-                if nums[i + j - 1] == i as i32 {
-                    nums[i..i + j].reverse();
-                    answer += 1;
-                    possible = true;
-                    break;
-                }
+pub fn sort_array(nums: Vec<i32>, pre: Vec<i32>) -> i32 {
+    let target: Vec<i32> = (0..nums.len() as i32).collect();
+    if nums == target {
+        return 0;
+    }
+    let mut queue = VecDeque::new();
+    let mut visited = HashSet::new();
+    queue.push_back((nums.clone(), 0));
+    visited.insert(nums);
+    while let Some((curr, steps)) = queue.pop_front() {
+        for &len in &pre {
+            let len = len as usize;
+            if len > curr.len() {
+                continue;
             }
-        } else {
-            possible = true;
-        }
-        println!("nums: {:?}", nums);
-        if !possible {
-            return -1;
+            let mut next_arr = curr.clone();
+            next_arr[0..len].reverse();
+            if next_arr == target {
+                return steps + 1;
+            }
+            if visited.insert(next_arr.clone()) {
+                queue.push_back((next_arr, steps + 1));
+            }
         }
     }
-    answer
+    -1
 }
 
 fn main() {
-    // println!("{}", sort_array(vec![2, 0, 1], vec![2, 3])); // 2
-    // println!("{}", sort_array(vec![1, 0, 2], vec![1, 3])); // -1
-    // println!("{}", sort_array(vec![0, 1], vec![2])); // 0
+    println!("{}", sort_array(vec![2, 0, 1], vec![2, 3])); // 2
+    println!("{}", sort_array(vec![1, 0, 2], vec![1, 3])); // -1
+    println!("{}", sort_array(vec![0, 1], vec![2])); // 0
     println!("{}", sort_array(vec![0, 2, 1], vec![3, 2, 1])); // 3
 }
