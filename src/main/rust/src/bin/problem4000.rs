7ffcdbaabb88de24672e64ec849a718c46245d48
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-07-25 23:21:00 -0500

    Skeleton for problem 4000

---
+++ src/main/rust/src/bin/problem4000.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/largest-integer-with-given-digit-sum/description/
+pub fn largest_integer(n: i32, s: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", largest_integer(2, 9)); // 90
+    println!("{}", largest_integer(2, 19)); // -1
+    println!("{}", largest_integer(5, 0)); // 0
+}
