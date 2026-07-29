b59dd37ccd024a65a0654ddd2c7f794188e3bba6
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-07-28 23:04:22 -0500

    Update problem 4000

--- src/main/rust/src/bin/problem4000.rs
+++ src/main/rust/src/bin/problem4000.rs
@@ -16,6 +16,6 @@ pub fn largest_integer(n: i32, s: i32) -> i32 {
 
 fn main() {
     println!("{}", largest_integer(2, 9)); // 90
-    // println!("{}", largest_integer(2, 19)); // -1
-    // println!("{}", largest_integer(5, 0)); // 0
+    println!("{}", largest_integer(2, 19)); // -1
+    println!("{}", largest_integer(5, 0)); // 0
 }
