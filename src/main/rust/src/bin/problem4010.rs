1d93a95ca4dd2ece5f14ef8fbf6952ad841d7abc
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-08-02 11:17:10 -0500

    Update problem 4010

--- src/main/rust/src/bin/problem4010.rs
+++ src/main/rust/src/bin/problem4010.rs
@@ -1,10 +1,21 @@
 // https://leetcode.com/problems/maximize-pair-strength-using-gcd/description/
 pub fn max_pair_strength(nums: Vec<i32>) -> i64 {
-    todo!()
+    fn gcd(a: i32, b: i32) -> i32 {
+        if b == 0 { a } else { gcd(b, a % b) }
+    }
+
+    let mut answer = 0;
+    for i in 0..nums.len() {
+        for j in i + 1..nums.len() {
+            answer = answer.max((nums[i] * nums[j]) as i64) / (gcd(nums[i], nums[j]) as i64).pow(2);
+        }
+    }
+    answer
 }
 
 fn main() {
     println!("{}", max_pair_strength(vec![2, 3, 5])); // 15
     println!("{}", max_pair_strength(vec![4, 6, 8])); // 12
     println!("{}", max_pair_strength(vec![3, 3])); // 1
+    println!("{}", max_pair_strength(vec![7, 18, 12])); // 126
 }
