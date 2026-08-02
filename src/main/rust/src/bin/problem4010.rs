5bb4f1a23b5c7794dd49b44292d4f568cfa9f816
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-08-02 11:13:45 -0500

    Skeleton for problem 4010

---
+++ src/main/rust/src/bin/problem4010.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/maximize-pair-strength-using-gcd/description/
+pub fn max_pair_strength(nums: Vec<i32>) -> i64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", max_pair_strength(vec![2, 3, 5])); // 15
+    println!("{}", max_pair_strength(vec![4, 6, 8])); // 12
+    println!("{}", max_pair_strength(vec![3, 3])); // 1
+}
