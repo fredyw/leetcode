aac4e738af67990e669d6499ca8aebc76199abb1
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-08-09 17:12:37 -0500

    Solve problem 3047

--- src/main/rust/src/bin/problem3047.rs
+++ src/main/rust/src/bin/problem3047.rs
@@ -1,6 +1,17 @@
 // https://leetcode.com/problems/find-the-largest-area-of-square-inside-two-rectangles/description/
 pub fn largest_square_area(bottom_left: Vec<Vec<i32>>, top_right: Vec<Vec<i32>>) -> i64 {
-    todo!()
+    let n = bottom_left.len();
+    let mut max_side: i64 = 0;
+    for i in 0..n {
+        for j in (i + 1)..n {
+            let width = (top_right[i][0].min(top_right[j][0])
+                - bottom_left[i][0].max(bottom_left[j][0])) as i64;
+            let height = (top_right[i][1].min(top_right[j][1])
+                - bottom_left[i][1].max(bottom_left[j][1])) as i64;
+            max_side = max_side.max(width.min(height));
+        }
+    }
+    max_side * max_side
 }
 
 fn main() {
