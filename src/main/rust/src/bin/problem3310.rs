4f4617465f73bd590a51b5feb98f4f1adcfd16b7
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-08-09 00:07:59 -0500

    Update problem 3310

--- src/main/rust/src/bin/problem3310.rs
+++ src/main/rust/src/bin/problem3310.rs
@@ -1,6 +1,24 @@
+use std::collections::HashSet;
+
 // https://leetcode.com/problems/remove-methods-from-project/description/
 pub fn remaining_methods(n: i32, k: i32, invocations: Vec<Vec<i32>>) -> Vec<i32> {
-    todo!()
+    fn dfs(adj_list: &Vec<Vec<usize>>, visited: &mut Vec<bool>, node: usize) {
+        visited[node] = true;
+        for &neighbor in &adj_list[node] {
+            if !visited[neighbor as usize] {
+                dfs(adj_list, visited, neighbor as usize);
+            }
+        }
+    }
+
+    let mut adj_list: Vec<Vec<usize>> = vec![vec![]; n as usize];
+    for v in invocations {
+        adj_list[v[0] as usize].push(v[1] as usize);
+    }
+    let mut suspicious = vec![false; n as usize];
+    dfs(&adj_list, &mut suspicious, k as usize);
+    println!("{:?}", suspicious);
+    vec![]
 }
 
 fn main() {
