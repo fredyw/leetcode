c3a0ea0b021f1b104cb9b1ec962c18717a09caf0
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-08-16 17:25:36 -0500

    Solve problem 4024

--- src/main/rust/src/bin/problem4024.rs
+++ src/main/rust/src/bin/problem4024.rs
@@ -1,6 +1,21 @@
 // https://leetcode.com/problems/nearest-available-drone/description/
 pub fn nearest_drone(drones: Vec<Vec<i32>>, target: Vec<i32>) -> i32 {
-    todo!()
+    let mut answer = -1;
+    let mut min_distance = i32::MAX;
+    for (i, drone) in drones.iter().enumerate() {
+        let distance = (drone[0] - target[0]).abs() + (drone[1] - target[1]).abs();
+        let target = drone[2];
+        if distance <= target {
+            if min_distance == i32::MAX {
+                min_distance = distance;
+                answer = i as i32;
+            } else if min_distance > distance {
+                min_distance = distance;
+                answer = i as i32;
+            }
+        }
+    }
+    if answer == i32::MAX { -1 } else { answer }
 }
 
 fn main() {
