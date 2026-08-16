e2c3f17297396c48dcdfd12e860231a824ccff96
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-08-16 17:28:18 -0500

    Skeleton for problem 4014

---
+++ src/main/rust/src/bin/problem4014.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/minimum-total-price-after-applying-discounts/description/
+pub fn min_price(prices: Vec<i32>, discounts: Vec<i32>) -> f64 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", min_price(vec![10, 30, 21], vec![50, 60])); // 32.50000
+    println!("{}", min_price(vec![100, 70], vec![10, 40, 50])); // 92.00000
+    println!("{}", min_price(vec![7, 3, 9], vec![100, 100])); // 3.00000
+}
