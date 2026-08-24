0fbfc4360b42b6665c3895bc4cc38b756e2ab2b8
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-08-23 23:24:37 -0500

    Skeleton for problem 4030

---
+++ src/main/rust/src/bin/problem4030.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/check-ascii-palindromic/description/
+pub fn is_palindromic(s: String) -> bool {
+    todo!()
+}
+
+fn main() {
+    println!("{}", is_palindromic("ff".to_string())); // true
+    println!("{}", is_palindromic("leet".to_string())); // false
+}
