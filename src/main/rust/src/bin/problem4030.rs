6b233d657554c2698643f3e606bcb1c1d49fcd6f
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-08-23 23:35:22 -0500

    Solve problem 4030

--- src/main/rust/src/bin/problem4030.rs
+++ src/main/rust/src/bin/problem4030.rs
@@ -1,6 +1,23 @@
+use itertools::Itertools;
+
 // https://leetcode.com/problems/check-ascii-palindromic/description/
 pub fn is_palindromic(s: String) -> bool {
-    todo!()
+    let binary_string = s
+        .chars()
+        .map(|c| format!("{:08b}", c as u8))
+        .join("")
+        .chars()
+        .collect::<Vec<char>>();
+    let mut i = 0;
+    let mut j = binary_string.len() - 1;
+    while i < j {
+        if binary_string[i] != binary_string[j] {
+            return false;
+        }
+        i += 1;
+        j -= 1;
+    }
+    true
 }
 
 fn main() {
