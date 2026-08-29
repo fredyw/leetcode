6064304431832ecff7af2cc34aa63d1d96a7921b
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-08-29 14:25:20 -0500

    Skeleton for problem 3853

---
+++ src/main/rust/src/bin/problem3853.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/merge-close-characters/description/
+pub fn merge_characters(s: String, k: i32) -> String {
+    todo!()
+}
+
+fn main() {
+    println!("{}", merge_characters("abca".to_string(), 3)); // "abc"
+    println!("{}", merge_characters("aabca".to_string(), 2)); // "abca"
+    println!("{}", merge_characters("yybyzybz".to_string(), 2)); // "ybzybz"
+}
