3e2c99c626939249d11bb9048e44444b5d7a6744
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-08-29 23:22:53 -0500

    Skeleton for problem 4038

---
+++ src/main/rust/src/bin/problem4038.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/count-integers-appearing-in-a-single-block/description/
+pub fn count_special_integers(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", count_special_integers(vec![1, 2, 2, 1])); // 1
+    println!("{}", count_special_integers(vec![3, 3, 1, 2, 2, 1])); // 2
+}
