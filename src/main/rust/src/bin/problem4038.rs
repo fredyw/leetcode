441679e339cb30417f55fe5eeffa7471952f844a
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-08-30 01:24:13 -0500

    Solve problem 4038

--- src/main/rust/src/bin/problem4038.rs
+++ src/main/rust/src/bin/problem4038.rs
@@ -1,9 +1,27 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/count-integers-appearing-in-a-single-block/description/
 pub fn count_special_integers(nums: Vec<i32>) -> i32 {
-    todo!()
+    let mut map: HashMap<i32, i32> = HashMap::new();
+    let mut i = 0;
+    let mut dupe = false;
+    while i < nums.len() - 1 {
+        if !dupe {
+            *map.entry(nums[i]).or_insert(0) += 1;
+        }
+        dupe = nums[i] == nums[i + 1];
+        i += 1;
+    }
+    if !dupe {
+        *map.entry(nums[i]).or_insert(0) += 1;
+    }
+    map.into_iter().filter(|(_, c)| *c == 1).count() as i32
 }
 
 fn main() {
     println!("{}", count_special_integers(vec![1, 2, 2, 1])); // 1
     println!("{}", count_special_integers(vec![3, 3, 1, 2, 2, 1])); // 2
+    println!("{}", count_special_integers(vec![3, 3, 1, 2, 2, 1, 3, 3])); // 1
+    println!("{}", count_special_integers(vec![1, 2, 3])); // 3
+    println!("{}", count_special_integers(vec![1])); // 1
 }
