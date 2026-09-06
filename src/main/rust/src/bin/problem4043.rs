b03bacde9289460965c90d629a685e2975400411
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-09-06 10:00:03 -0500

    Skeleton for problem 4043

---
+++ src/main/rust/src/bin/problem4043.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/count-rotations-with-exactly-k-equal-adjacent-pairs/description/
+pub fn count_rotations(s: String, k: i32) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", count_rotations("aab".to_string(), 1)); // 2
+    println!("{}", count_rotations("abca".to_string(), 0)); // 1
+}
