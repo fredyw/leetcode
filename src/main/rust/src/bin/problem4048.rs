e471fef42ca162384965a2227da2c29739819a9a
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-09-13 08:54:23 -0500

    Skeleton for probelm 4048

---
+++ src/main/rust/src/bin/problem4048.rs
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/count-values-with-equally-spaced-occurrences-i/description/
+pub fn count_special_integers(nums: Vec<i32>) -> i32 {
+    todo!()
+}
+
+fn main() {
+    println!("{}", count_special_integers(vec![1, 8, 1, 5, 1, 5, 8, 5])); // 2
+    println!("{}", count_special_integers(vec![8, 8, 8, 8])); // 0
+    println!("{}", count_special_integers(vec![8, 6, 6, 8, 8])); // 0
+}
