ea61b6a1848b38732c01fc080cb6d796e5525279
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-09-13 09:27:11 -0500

    Update problem 4052

--- src/main/rust/src/bin/problem4052.rs
+++ src/main/rust/src/bin/problem4052.rs
@@ -1,11 +1,20 @@
 // https://leetcode.com/problems/cyclically-shift-rows-and-columns/description/
 pub fn cyclic_shift(
     n: i32,
-    grid: Vec<Vec<i32>>,
+    mut grid: Vec<Vec<i32>>,
     row_shift: Vec<i32>,
     col_shift: Vec<i32>,
 ) -> Vec<Vec<i32>> {
-    todo!()
+    for (i, &shift) in row_shift.iter().enumerate() {
+        let cloned_rows = (&grid[i]).clone();
+        let mut_rows = &mut grid[i];
+        for col in 0..n {
+            let col = col as usize;
+            mut_rows[col] = cloned_rows[(col + shift as usize) % n as usize];
+        }
+    }
+    for (i, &shift) in col_shift.iter().enumerate() {}
+    grid
 }
 
 fn main() {
