77997bf41e0f999a499ade18f4bde672711345ac
Fredy Wijaya <fredy.wijaya@gmail.com>
2026-09-13 08:59:48 -0500

    Solve problem 4048

--- src/main/rust/src/bin/problem4048.rs
+++ src/main/rust/src/bin/problem4048.rs
@@ -1,6 +1,24 @@
+use std::collections::HashMap;
+
 // https://leetcode.com/problems/count-values-with-equally-spaced-occurrences-i/description/
 pub fn count_special_integers(nums: Vec<i32>) -> i32 {
-    todo!()
+    let mut map: HashMap<i32, Vec<usize>> = HashMap::new();
+    for (i, num) in nums.iter().enumerate() {
+        map.entry(*num).or_insert_with(Vec::new).push(i);
+    }
+    let map: HashMap<i32, Vec<usize>> = map
+        .into_iter()
+        .filter(|(_, indices)| indices.len() == 3)
+        .collect::<HashMap<_, _>>();
+    let mut answer = 0;
+    for (_, indices) in map {
+        answer += if indices[1] - indices[0] == indices[2] - indices[1] {
+            1
+        } else {
+            0
+        };
+    }
+    answer
 }
 
 fn main() {
